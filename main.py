162dae7731f65c54e004232a1e5a7e2b7c56f466
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:29:17 +0000

    live next Ff through guess

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
