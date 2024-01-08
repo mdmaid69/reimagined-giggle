ff7ce92b4fb228ae2bf9fab48621ef6fecf07a3f
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:30:03 +0000

    happy after look defend fashion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import numpy as np
+print(np.array([1, 2, 3]))
