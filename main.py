c1695ab64cd577d46f6b06eef8b66b3213978c65
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:54:23 +0000

    loss lend would drive comeout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import collections
+def create_stack():
+        return collections.deque()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
