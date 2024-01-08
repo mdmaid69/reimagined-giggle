21a3ddc787ac31314a7aa1249ed30d5b652ab13e
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:46:37 +0000

    speed entrance fair comeover recover

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
