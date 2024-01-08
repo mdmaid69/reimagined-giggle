5c0a70a47045523f519dd9c97bfc594ddbc10cbf
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:13:19 +0000

    tall bury ready again previous

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
