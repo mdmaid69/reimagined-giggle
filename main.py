0eb41055c749753ed4ca73f87e2e5053c6564d38
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:27:14 +0000

    upwards measure old one before

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def reverse_string(s):
+        return s[::-1]
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
