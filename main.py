26ac3ed8f5fd770ccbb17f8948fdedaa33b18f9c
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:59:15 +0000

    false success late crowd container

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import random
+print(random.randint(0, 100))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
