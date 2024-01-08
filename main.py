9fa2c04fb345ebf0091e6d0a6fb4795e779e68e8
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:01:38 +0000

    tough mainly variety soon wet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
