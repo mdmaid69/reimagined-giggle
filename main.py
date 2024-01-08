a2418711767ef408a12ed1909f5bf0feb4a29a86
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:34:37 +0000

    estimate approval coat form giveaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
