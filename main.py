5c09830a85c78e92cdcb7751fc276257d091b85a
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:31:57 +0000

    whole dig hard lay position

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
