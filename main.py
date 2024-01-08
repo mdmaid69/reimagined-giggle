e9fe98c64f33a8ece23b58c6189ea2d3bf7c48b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:57:10 +0000

    count stop rather right top

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  def cube_number(x):
-        return x**3
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def subtract_numbers(x, y):
+        return x - y
