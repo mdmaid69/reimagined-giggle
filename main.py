a5dc971e17542849e26c49894978890217c5c15d
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:45:02 +0000

    send wherever grow plant discussion

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_stack():
-        return collections.deque()
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
