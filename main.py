7fe2d31bd6a0b8f01055b1d6cc4bbbfce1646acf
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:07:50 +0000

    damage connection dead central form

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
