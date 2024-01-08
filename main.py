a7cf01e14b605990b2859c24ed037d2885d7bbbc
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:35:16 +0000

    reject buy mess convenient minor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_file(file_name):
+        os.remove(file_name)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
