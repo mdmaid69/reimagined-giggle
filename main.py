cbb9d75dd150ecb3f7280e6a72ba1e0eeef05eb3
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:47:56 +0000

    handle under traffic star listenfor

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
