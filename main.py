790d2e340c2c48ff48edde0dbf1fe891645c17e1
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:18:46 +0000

    might except order into curious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
