9667b8685823a4c44ea77c5a29b467868b1ff256
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:48:04 +0000

    clothes flow forever appear march

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+print(sum(range(10)))
