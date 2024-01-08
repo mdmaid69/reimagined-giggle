8edf74ef6d2787cf6f36d565ff0c0cf93a1f296f
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:25:11 +0000

    bottom create return angry that

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
