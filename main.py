b01e9aa859bb8778ec5d9b85c40a01cc18954559
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:38:48 +0000

    exam number equal tell cruelty

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  def calculate_area_rectangle(l, w):
+        return l * w
