e276ecff5aa161f4922123b9ec55849de84fe686
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:44:04 +0000

    welcome little formal public early

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def find_max(numbers):
-        return max(numbers)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
