c2ba8c18a0324ce8fc2b2a187a70c7934c47796d
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:31:35 +0000

    them exhibition heavy medium rather

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
