0830c0e8744ed8a2b322f50952351b113c0d86a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:27:03 +0000

    system at alive delight listen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
