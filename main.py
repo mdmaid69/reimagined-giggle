553773ac0c651adb9d32cbe692924b9fcdb47b24
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:36:16 +0000

    provide score capital lot deaf

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
