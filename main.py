a256b49af4af84fce9f3265cca73f90e234f8f4e
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:29:50 +0000

    deal face tip period steady

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def subtract_numbers(x, y):
+        return x - y
