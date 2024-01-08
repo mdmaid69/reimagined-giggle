02027c3ef05d0e599c257d62c7e98afbf795a397
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:32:31 +0000

    practise worth shallow claim task

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+print([x**2 for x in range(10)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
