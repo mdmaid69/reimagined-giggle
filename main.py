025b00d3b5d56080f469d5007705080a3154aeb7
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:42:16 +0000

    often invite poison getup rely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_current_directory():
+        return os.getcwd()
