747b682bec7fa0e0840c7bdbc9b893bb5b4d9ce7
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:09:28 +0000

    art important responsible maintain kick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def set_array_item(array, i, item):
+        array[i] = item
