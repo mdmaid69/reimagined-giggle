4d4682e9da4edfc2fae2be1ed9a230b651a84879
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:47:45 +0000

    minority description strict object while

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
