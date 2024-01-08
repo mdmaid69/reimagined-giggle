109f2ec91b6b110fa7fefd4735018ea8d332e2fe
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:43:00 +0000

    necessary nervous raise slightly out

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
