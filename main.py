aa368e19ae42dd11a2c159fbb0a3f7e925cc555d
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:13:56 +0000

    conduct contact control gather proof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_speed(distance, time):
+        return distance / time
