236bb4a9bb4c816be78b004beed364022a1d0e78
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:15:53 +0000

    worst while brief neat level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sys
+def exit_program():
+        sys.exit()
+  def subtract_numbers(x, y):
+        return x - y
