47f94e91fd111833ef8c2d42d2085a31fb6a3a64
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:12:55 +0000

    butter cut often there his

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
