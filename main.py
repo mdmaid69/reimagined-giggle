742275c8bf568e315c27b54f8e9d9fa9d3a5ca35
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:38:59 +0000

    invite too convince fall somebody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
