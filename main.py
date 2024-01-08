32e49f25574d7d9edbf4f6424c84f3261382b122
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:04:43 +0000

    strike slowly hate direct thought

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def count_elements(lst):
-        return len(lst)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
