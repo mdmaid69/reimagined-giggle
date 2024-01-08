d66622b10dd781cc148bd1592653c880b38656a6
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:45:49 +0000

    likely bar lie quiet search

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
