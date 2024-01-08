04773c293e1c3c4abbcc072664424cd4c9b95cf0
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:28:01 +0000

    typical idea giveback certainly fair

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
