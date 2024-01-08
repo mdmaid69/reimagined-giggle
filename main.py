470cca04dc682e751c57b45250d4d64268b971a8
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:37:36 +0000

    shall settle kind once same

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
