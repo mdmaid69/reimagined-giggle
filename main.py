ee424c024c39dde2ce72749cda7c66eb6a35b6d4
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:46:53 +0000

    study rain look buy risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
