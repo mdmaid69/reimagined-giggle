f3b067958e20421a4cf158560d165ab99af1826d
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:46:26 +0000

    limit back demand work everyone

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
