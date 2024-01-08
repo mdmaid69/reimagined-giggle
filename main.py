3374e537e3f6cf12f97ceb6be6fbc5ff15ab1b49
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:36:08 +0000

    ability comfortable personal popular neck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
