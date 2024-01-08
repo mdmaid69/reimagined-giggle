23a686cf6dcfec69a61504034c6b248e3b7c9c77
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:22:05 +0000

    welcome die event prepare the

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
