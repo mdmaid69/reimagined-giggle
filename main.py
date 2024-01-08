85608aa6297641f06d66427b86687dbbe4eadb46
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:13:58 +0000

    meal strike behaviour force include

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
