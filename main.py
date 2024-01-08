f9bdf32b760ab9ce5427f4753b3001913eec5cf5
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:36:55 +0000

    joke safety wish why like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
