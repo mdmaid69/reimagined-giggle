d72f86a8ebd1d35032651f563dc1402fbbd73a32
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:27:21 +0000

    rate dull up speed back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def add_numbers(a, b):
+        return a + b
