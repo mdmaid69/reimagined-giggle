ce475af0d0b14da8f137f5a48a1df5eff64f166d
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:55:02 +0000

    calm famous track claim face

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
