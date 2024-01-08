b3a9fcfbaacf51408aa352b888a5a384bbc0bbee
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:23:45 +0000

    bring children jump geton know

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
