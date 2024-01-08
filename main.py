22a2b57ba302eb6c398878da09d5436064dce249
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:55:26 +0000

    hardly land describe prefer limit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
