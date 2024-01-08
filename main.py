4b6d0bc960e8ffc9650ce7e552cbdf2ebc55abb0
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:52:53 +0000

    switch serious all brave situation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_stack():
+        return collections.deque()
