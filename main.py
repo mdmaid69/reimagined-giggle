8eec529035a2f0ff31a454c420800b924044e4a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:07:31 +0000

    quick hers exploration limit raise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import time
+print(time.time())
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
