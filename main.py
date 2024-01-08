621815426b796aad72eada873c4aae6bc4d646b2
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:35:28 +0000

    while rule watch little desperate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
