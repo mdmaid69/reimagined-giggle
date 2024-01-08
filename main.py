94ef23317ee57c1530a537e452e3fbd1a5d86c99
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:13:31 +0000

    refer interview break none beginning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
