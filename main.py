5ac1b0ea30b8d546e13993e1d6f34cf22fe29214
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:13:52 +0000

    next big welcome shallow juice

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
