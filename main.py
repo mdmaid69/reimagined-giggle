3a89f10cc51b52de7e30062deac8b39c68a087ad
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:38:08 +0000

    advantage talkback minimum service smile

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+print(math.pi)
