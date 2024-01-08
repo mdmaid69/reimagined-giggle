38948c1f92f606d32acb2f1d95739fb3926f6ea5
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:21:11 +0000

    store floor definite owe little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import sys
+def print_python_version():
+        print(sys.version)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
