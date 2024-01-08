0adfa06e951edaebfc1fe1fed740f8aff40e8b3c
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:38:56 +0000

    regard neck help home describe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
