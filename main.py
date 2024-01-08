11917f140f1a92379dbad6c0d4b4ef171310a841
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:11:18 +0000

    middle objection Dd sit almost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
