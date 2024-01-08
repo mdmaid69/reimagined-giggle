083714bd627c1504d5266018a202af267e4b70c8
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:10:53 +0000

    undo spin ask down slice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
