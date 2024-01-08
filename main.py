17f5f59e15f0056882659d731ed058d09c2dcc19
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:04:21 +0000

    wide excuse sport plenty together

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
