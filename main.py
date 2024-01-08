4c7eeb71ddc903a41ff23c90a9ae3b65263fb33d
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:40:28 +0000

    partner regularly anxious exciting production

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
