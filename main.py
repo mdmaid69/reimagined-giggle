4f35db2550e9eeb5673e6127e80a8a6756b2ea1a
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:27:25 +0000

    keepout own shut lift address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def add_numbers(a, b):
+        return a + b
