62d073769901834c356fc9c95c48c97d013d3bf6
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:23:39 +0000

    sure voice people switch flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def is_odd(n):
+        return n % 2 != 0
