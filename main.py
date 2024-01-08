707fce17d1d7ac901b9464016eef7f4237414a73
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:01:51 +0000

    often the light smoke action

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
