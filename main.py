ddb0b1996b7faa650b0f86e331cdc7af71472b22
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:04:38 +0000

    image far moment region deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
