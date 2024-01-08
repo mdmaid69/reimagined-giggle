8e04861bdb027d20cb8bede644fa04aa60a91cf0
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:13:39 +0000

    wrap whether better influence dig

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def get_array_as_str(array):
-        return str(array)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
