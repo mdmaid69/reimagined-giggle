76eb01e545a189bbfc7c4f9ff827e8ce7dda0f92
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:02:21 +0000

    cost disappointed congratulate home treat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def find_min(lst):
+        return min(lst)
