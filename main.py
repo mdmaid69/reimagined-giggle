d0ebdc5494d0e5fb2837ca294df373e2f93ba035
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:27:29 +0000

    getaway distant practice entrance sudden

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
