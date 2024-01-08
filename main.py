aeb77c5b2c3c58f37365c95a63411620836340e5
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:06:23 +0000

    final come father sort accordingto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_power(work, time):
+        return work / time
