f36113cb388a1591e1d77bfe275ce1ba4eea2ea2
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:38:50 +0000

    attract excited your wheel ride

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def square_number(x):
+        return x**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
