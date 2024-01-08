efd152a9e9d0e676a9eeee88a59631174378bfbf
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:02:16 +0000

    expect drink short laugh attention

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
