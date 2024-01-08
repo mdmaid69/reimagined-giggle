4367ab9f29dc9ef33d651167a46e7ad440f578c2
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:08:40 +0000

    bye unhappy slice accommodation unlikely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def remove_directory(path):
+        os.rmdir(path)
