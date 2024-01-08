2709d85d2048b812ca812dc66fa01ec6908f1d72
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:47:13 +0000

    floor brush accidental decide be

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def count_elements(lst):
-        return len(lst)
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import numpy as np
+print(np.array([1, 2, 3]))
