e37aa143f7c876493b4276fe15a651fec85c2bd2
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:00:41 +0000

    Qq content effort me wave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
