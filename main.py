2b35e0040493f32bf64b71a50101d7f4a9a0e60a
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:49:57 +0000

    comeup weather off thin hardly

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_acceleration(speed, time):
-        return speed / time
+  def convert_to_hex(n):
+        return hex(n)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
