572ac64088e965d92e9f0db948194f53aa26afc9
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:50:51 +0000

    view motion bury like dangerous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
