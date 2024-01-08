6d21dbe50c98d2ad2bd6f63621236b6bf0269c0f
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:36:35 +0000

    strict recognize dangerous physical fight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
