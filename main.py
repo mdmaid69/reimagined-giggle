49259fb816c119532720989be9ef053edf301a3e
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:44:00 +0000

    trouble row medium destroy complaint

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def divide_numbers(x, y):
-        return x / y
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
