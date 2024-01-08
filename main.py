b81e7520fd3257781d8619f574f6f21cafd3536d
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:20:33 +0000

    complain man pencil steal anybody

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
