154d4b3c6082d9778fc04a922c54dcd1630729fc
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:39:38 +0000

    responsible wave catch breakinto less

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
