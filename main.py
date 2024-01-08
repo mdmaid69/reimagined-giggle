a81ed61a43caf8ba72f5daf144cbdefca03bbdfe
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:13:04 +0000

    let exhibition able 564encourage opposite

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
