8d234134f74cd8534db3201fe22e947185f560c0
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:14:11 +0000

    watch obtain need extra see

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
