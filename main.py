d0e45ec8be72ac2244c2e9d92d51f52a3a719cee
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:00:53 +0000

    boat thing evidence this yes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_area_rectangle(l, w):
+        return l * w
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_slice(array, i, j):
+        return array[i:j]
