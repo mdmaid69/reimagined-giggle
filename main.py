25f52e6208042ece137e618dbe2c12fa8d841264
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:38:44 +0000

    tradition flight sell crash our

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
