d8085f1fa4b85693c88074d2da00e7c9754dd592
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:29:38 +0000

    afternoon stop original next however

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
