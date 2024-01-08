5f07b5248b58af14cc68e47942fbeec79eb9c002
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:24:02 +0000

    accident interfere persuade drawing allow

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  def sort_list(lst):
-        return sorted(lst)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
