8ae5696423fc15d722b5417afa5e844671f04db7
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:06:41 +0000

    promise past unlikely wild disappear

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
