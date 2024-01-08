46b9e6cb647c68c41bc1240f953d8054ade47c4b
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:29:02 +0000

    discovery no injury short previous

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def is_odd(n):
-        return n % 2 != 0
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def get_array_index(array, item):
+        return array.index(item)
