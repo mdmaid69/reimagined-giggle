bf4d76672c26bba49ed6816a8ea1f3c9f683c3cd
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:02:58 +0000

    mean bath regard smile fun

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def divide_numbers(x, y):
-        return x / y
+def get_array_as_int(array):
+        return int(array[0])
