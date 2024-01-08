f88a8ce4491546542cf2f71eefaa83aa850a6b8c
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:01:31 +0000

    miss come home class else

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def get_array_as_complex(array):
+        return complex(array[0])
+print("Hello, world!")
