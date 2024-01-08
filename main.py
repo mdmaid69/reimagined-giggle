7948d43495fb94a3e7969e772d4e911db9a49ba8
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:00:13 +0000

    closed fasten snow branch production

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def count_characters(sentence):
+        return len(sentence)
