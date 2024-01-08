87ba0b666ec595b4f16063fbc28c9818c58a4f4c
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:33:31 +0000

    march watch great dangerous responsibility

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def multiply_numbers(x, y):
+        return x * y
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
