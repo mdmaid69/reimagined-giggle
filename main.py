5191452b02f26e060ce9d455e3e77e1c0cb01f17
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:33:37 +0000

    authority one attractive ought finally

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
