fe2ba3ed6618c7994f5768e04edb31fac42dbfe8
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:04:25 +0000

    convincing row takeaway dead every

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
