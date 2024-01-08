60a85767ac7ccd051b1a511b0bb9a3b461818870
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:05:09 +0000

    scream tap spring onto stage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def is_even(n):
+        return n % 2 == 0
