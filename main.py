b9da9bb7cffce96b343df7f65a58c904dd2348b3
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:48:44 +0000

    interfere when well disapprove left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
