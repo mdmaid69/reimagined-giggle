229da52967da7fdc3fed798458bf26361a640cc8
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:07:23 +0000

    imagine dance surprised smooth relief

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
