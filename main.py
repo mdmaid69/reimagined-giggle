1e1f8399a0ae17d1653dc76afba76820a75ea1eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:06:32 +0000

    main maintain weak exactly gentle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
