2895dad34f5a2f9b8ae558056eec2214f5cf786f
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:05:12 +0000

    company few sort mind game

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
