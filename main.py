15f5f2c3ac5520d65abfeb81530d3b17d832da7f
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:12:55 +0000

    trap command wise goon shopping

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def cube_number(x):
+        return x**3
