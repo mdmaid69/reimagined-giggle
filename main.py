06f19f8e8af6a9e95c7dcbe71dddcd79028405dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:39:53 +0000

    team even breath temperature passenger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_octal(n):
+        return oct(n)
