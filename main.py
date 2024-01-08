da8f27f802c6515046e5738248dbeaf2401a896d
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:51:03 +0000

    low welcome surprise along street

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
