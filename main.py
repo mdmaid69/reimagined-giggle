558ae18294b7ace6319c24bb5089bd44a7c5032d
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:24:23 +0000

    secret thirsty meal behind bury

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
