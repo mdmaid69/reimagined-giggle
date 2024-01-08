ebe3201234ab11ee286ada46507e008efe33529c
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:36:49 +0000

    makeup negative makeinto brief extra

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def sort_numbers(numbers):
+        return sorted(numbers)
