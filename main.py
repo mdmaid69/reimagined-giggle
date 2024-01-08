ba6bc64e153d3395af6da043ee6a251606d0a709
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:33:10 +0000

    better quiet term that area

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_power(work, time):
+        return work / time
