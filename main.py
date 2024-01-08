8bab5b163dab787cbea87d8d00425180928eafaf
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:59:51 +0000

    press food involve average anyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
