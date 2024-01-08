e6f64b6e93104648464f840ba5417a78c4ac25be
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:27:00 +0000

    getup classroom pull your choose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+x = 10
+y = 20
+print("Sum:", x + y)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
