eb891669e0d5ede0668e928045732290a60161de
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:28:34 +0000

    sign form really talkback fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
