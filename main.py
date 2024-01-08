7166e7b72836b98ed69f4177fd23be182dc1e187
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:16:00 +0000

    post play Itself disturbing black

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
