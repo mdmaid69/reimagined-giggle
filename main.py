05cb41c578ec9320153756979389f10d97cfdb40
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:32:39 +0000

    risk sink fire press tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
