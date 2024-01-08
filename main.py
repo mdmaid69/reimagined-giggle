6369ed090f40ae42d61fb3aa3c11e37c4195ad4d
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:29:06 +0000

    main behind act getup separate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
