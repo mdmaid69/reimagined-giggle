47bc934b6da772b28593c3f0ae844d3f205af511
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:43:55 +0000

    married cost soft declare survive

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
