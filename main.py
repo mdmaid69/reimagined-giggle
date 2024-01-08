ee628ce70ff2c17efd9182949cdd9db7d67b7759
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:25:59 +0000

    emergency morning professional blame practice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
