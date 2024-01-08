9654e3e4f7649792920fd5e725f3c745c31a4e80
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:30:30 +0000

    address free safe sudden minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
