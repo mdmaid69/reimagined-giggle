658774e4bbf99fdce98a84ed2a67118bf464f618
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:56:44 +0000

    will ahead unite hide attention

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
