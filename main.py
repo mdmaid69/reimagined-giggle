17232e2d12c1654e8ac8cc24aa744a4592145d25
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:25:08 +0000

    afterwards pipe crash bet bread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def reverse_list(lst):
+        return lst[::-1]
