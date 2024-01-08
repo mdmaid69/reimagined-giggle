0aa7ee7022af706b339311a502d4bf0317594001
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:22:50 +0000

    quantity its luck lower protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
