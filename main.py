cb89720ffa39a684eeca4d157092d64a9eb39ab5
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:00:07 +0000

    hers comment scare you unfair

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_from_string(string, typecode):
+def convert_unicode_to_array(unicode, typecode):
         a = array.array(typecode)
-        a.fromstring(string)
+        a.fromunicode(unicode)
         return a
