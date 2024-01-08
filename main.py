5b577ff127e223f74cd19057e3461e5fa86c7f6d
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:09:00 +0000

    frightened float opposite want 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
