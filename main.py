b169c3090e2e91f47817ecf29524e6800ac0c741
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:01:41 +0000

    ear confidence squeeze through union

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  def convert_to_binary(n):
-        return bin(n)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
