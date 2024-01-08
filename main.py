d1b07f9fad7ffd94f2e47acab96e553ddd426a3e
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:30:05 +0000

    occasion load already bin protect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def cube_number(x):
-        return x**3
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_work(force, distance):
+        return force * distance
