ec4baf76af173644ed57fa111dc78848658ca27c
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:25:43 +0000

    blind hello proper gooff dick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def cube_number(x):
+        return x**3
+def calculate_perpetuity(payment, rate):
+        return payment / rate
