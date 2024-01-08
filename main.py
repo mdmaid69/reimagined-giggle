9584c79092aa7ed4ef2fda04f6038444dedac67a
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:24:06 +0000

    justice comeout snow recover they

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
