d64fd4fbc9fc8cf787b9056b87d0a783fbb4b20a
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:20:43 +0000

    Gg tie frighten thinkof dream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
