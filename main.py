72547b5d86d9a155314cc45660e442a891226ed9
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:43:16 +0000

    slip present sure hour powerful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
