35f403d3d66dc81c4a6bc6f7b1a2b30330cfd7ca
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:48:17 +0000

    lay bear dangerous imagine programme

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
