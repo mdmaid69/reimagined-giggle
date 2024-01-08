af20cb6c715f2f0539f1a1ad3bf70258255c7210
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:33:31 +0000

    your reject fire pool Ee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
