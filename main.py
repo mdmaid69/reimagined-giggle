57e5533ab984d4e7f8e79292172e8feefe2c6a14
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:40:37 +0000

    rank sentence computer pressure care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_power(work, time):
+        return work / time
+import collections
+def create_priority_queue():
+        return collections.deque()
