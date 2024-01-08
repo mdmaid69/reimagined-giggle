11b0977d88503deb452d344e93d6b41b2b1963d6
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:56:25 +0000

    expect pause divide disturbing bill

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import datetime
+print(datetime.datetime.now())
