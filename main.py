3f99b8eb9c54b0f197778d1dd892c4cd18b8d3f7
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:30:17 +0000

    breakin introduce slowly pay welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
