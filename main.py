247cd3ce5c9e7f093ebe0d707206d5a243a76404
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:54:52 +0000

    impression flow effort radio twist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def subtract_numbers(x, y):
-        return x - y
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
