82d33da704da4680cef0bb49ec3ad63d65f029de
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:49:43 +0000

    runout lower brown person light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
