898c460e7ed20074482fdcecec5f3643c1323503
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:31:10 +0000

    deal rarely score absolutely impression

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
