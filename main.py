832e19319fa74d34fa27f269d56cf41f69266f9c
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:56:58 +0000

    shall content generous thankyou goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
