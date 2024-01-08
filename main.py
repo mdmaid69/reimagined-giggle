4165fed7c20bc6671d510f4c17ff6b524f01006c
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:03:28 +0000

    lonely train sea recommend middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_directory_name(path):
+        return os.path.dirname(path)
