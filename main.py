f245c7d771b28098aff8be2242323697db5d9d64
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:19:29 +0000

    forward condition him cut plate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
