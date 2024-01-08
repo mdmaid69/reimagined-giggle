4cdea8a78f4e4760ee3116023490266b2adb4f4e
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:38:23 +0000

    patience Kk hand open see

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
