3e78a892af2a5dd0fd99a54010a70609a329c49f
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:20:14 +0000

    face downstairs whatever official increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
