34869ccfbfdcd9b72fc6baefc4ae9d7ccd94e889
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:43:43 +0000

    active send create dry certainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
