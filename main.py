3fecc4dbd6407f4c9bd9e7a9b33fe414f1223f8e
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:35:04 +0000

    pure bright collect politics play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
