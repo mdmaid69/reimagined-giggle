0bddda609d9e6f3d8f8e363da37e097222bd2489
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:48:34 +0000

    wheel flood turnoff often after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
