fff08b4714dae29b4f3f9ca16d36fea161864572
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:19:14 +0000

    find thirsty tend man with

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
