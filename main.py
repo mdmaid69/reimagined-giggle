7dfa2f4bcef99b1c48ed6fc48280074744e59eae
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:50:56 +0000

    slowly bound hope ever mix

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
