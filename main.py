dc41757e44ade5e490cd42895527b0b61eb39bde
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:52:58 +0000

    better experience damage comefrom wind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
