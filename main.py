f9d90dec7e2685d3fb218222c72ef2fd39244102
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:57:05 +0000

    treat theirs reduction rule branch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import time
+print(time.time())
