e7c22e7d2127957bbd1a426221aa0b1d7fa9ac7b
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:10:13 +0000

    tune no back needle edge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
