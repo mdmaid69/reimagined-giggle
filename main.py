7000e1230ab67b5ff7c764c8faa8e08ab5103ae7
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:39:50 +0000

    receive tall sing bet lay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
