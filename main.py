e3f2b02c6874027524800773435c74f036843187
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:28:44 +0000

    you degree base somebody sport

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_area(radius):
-        return 3.14 * radius * radius
+for i in range(10): print(i)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
