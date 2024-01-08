6ae7929f3856c8eb930490f95420d585f69c6ab4
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:00:55 +0000

    this goaway makeinto hide challenge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
