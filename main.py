ff2ef3d9f2ad833bb806ce9dfbdcf38ebb599f91
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:02:27 +0000

    brown copy you print list

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import platform
+def get_os_info():
+        return platform.uname()
