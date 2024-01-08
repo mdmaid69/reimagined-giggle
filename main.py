c593c3a91cd8afa14ab627fece42b83d307f9cbf
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:26:46 +0000

    Tt party there foreign flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
