b076be6a0ffdfaf058c60205b1e9f336ccce7426
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:54:35 +0000

    either ring progress check needle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_density(mass, volume):
+        return mass / volume
