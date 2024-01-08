019435a7c5a0e2f23faf7b82cbb43716ab479c87
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:16:32 +0000

    grow natural meanwhile approve pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
