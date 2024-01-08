8329e4476dacb858e9ba515c4e287b8cc5e059cd
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:17:57 +0000

    them maybe split father wealth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
