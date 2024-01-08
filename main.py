8c047160318be8ee059aa857937eea9820d2ccb4
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:45:14 +0000

    must age runaway watch whistle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def square_number(x):
+        return x**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
