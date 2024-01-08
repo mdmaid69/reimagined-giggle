0bcd46389d5c85679ed3bb6fcbca20e279ddd359
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:01:03 +0000

    meanwhile relation communicate determined available

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
