7d2c93f0e1234d1e4a8446fedc1205cdffc5a45c
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:41:02 +0000

    moon complaint key help fence

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
