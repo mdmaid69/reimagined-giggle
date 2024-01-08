c4bdfe1a0085857257a021f79ffdec073b0a6ae1
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:12:41 +0000

    wing consist smooth bend sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_bool(array):
+        return bool(array)
