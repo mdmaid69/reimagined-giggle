4bdf391044555340f3a77eb9cd7d4a6c393e2b95
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:06:17 +0000

    second journey battery interest exit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
