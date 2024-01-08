e3334e044434199e4b81b48b123ba63feefed38a
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:52:38 +0000

    turnup around cross light high

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
