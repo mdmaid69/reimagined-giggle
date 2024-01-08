fda1ad343c35f2bb85f8b39d011508efdc19732c
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:16:46 +0000

    thankyou butter embarrassing getout day

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+print(os.getcwd())
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
