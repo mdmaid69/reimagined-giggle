ae700c19cf168ab275e710252218da75ea579e20
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:05:30 +0000

    far dance punish improve belong

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
