0da852fae58a629c07b0be2e5429ad55a73ece62
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:48:00 +0000

    start this land light obvious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
