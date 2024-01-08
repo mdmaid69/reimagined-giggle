b7231dd730ed7bfd1d42b0e939289943db357cf9
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:12:44 +0000

    sport manage possibly test flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import array
+def reverse_array(array):
+        array.reverse()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
