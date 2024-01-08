d891c4f7d732de4fad4c707946ba0ddc06ff4e57
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:27:33 +0000

    combine laugh block silent keep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def convert_to_hex(n):
+        return hex(n)
+import os
+def get_current_working_directory():
+        return os.getcwd()
