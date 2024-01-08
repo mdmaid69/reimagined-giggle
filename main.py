3ae33364ff1e84e79cb6da67ad40c6776d3475bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:09:04 +0000

    knock event theirs lead artificial

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_acceleration(speed, time):
-        return speed / time
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def count_words(sentence):
+        return len(sentence.split())
