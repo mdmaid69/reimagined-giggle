fe4780b7ccb66d7ae61921ee337c20d865ac20c3
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:46:22 +0000

    happy trust trick speech full

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
 import array
-def get_array_as_set(array):
-        return set(array)
+def set_array_item(array, i, item):
+        array[i] = item
+def is_odd(n):
+        return n % 2 != 0
