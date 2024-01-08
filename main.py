796641a09385861fe1291058df59084e71a83d5a
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:51:45 +0000

    best inside anything charge official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
