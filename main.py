4ef0af0c4667b2c75215c1f160b7ba534b456224
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:37:02 +0000

    please neither since accordingto meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_item(array, i):
+        return array[i]
