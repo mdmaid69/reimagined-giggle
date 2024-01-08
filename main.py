91f797c6a3d85686bf7e2589b2c865a8e853064b
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:25:32 +0000

    smooth proper rescue contain better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
