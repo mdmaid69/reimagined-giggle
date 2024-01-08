23f4221267997d214c715149c254a4a94282a367
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:15:40 +0000

    guess strong piece attention medium

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_pressure(force, area):
+        return force / area
