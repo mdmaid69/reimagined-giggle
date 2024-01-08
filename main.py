fb7def4bc7db08601d2a69377eb66b098fc7da0f
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:25:58 +0000

    leave roof anywhere affect themselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Uppercase:", text.upper())
