efd27dab22aec1f1d989b7ce52758e5f4bc85a91
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:05:10 +0000

    except their strict same instruction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
