c14d5272e7b3e25cf4ec86d67edf9e71b9b47fc0
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:28:11 +0000

    nice might over holdup Ii

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
