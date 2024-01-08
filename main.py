bcef6f26f3c73255d5fb5813222aea268b27cc6b
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:26:52 +0000

    aim conclusion complete stir suddenly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def is_odd(n):
+        return n % 2 != 0
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
