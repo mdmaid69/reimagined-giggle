2dece6ef7e517a1a955fcc5e75c2639ab67e31c7
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:24:43 +0000

    rise us close medium room

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import logging
-def log_message(message):
-        logging.info(message)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_bool(array):
+        return bool(array)
