a557efd518405f563a4b222f86c4c198b29c4175
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:24:30 +0000

    limit bang innocent base cut

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_odd(n):
-        return n % 2 != 0
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import math
+print(math.pi)
