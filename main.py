c8b0553a95c0bf5accf77621597bc553b204aeb0
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:07:28 +0000

    limit order hardly swim song

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def multiply_numbers(x, y):
-        return x * y
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
