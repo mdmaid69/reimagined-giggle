bea1cd5bfcce79f082e054e71fc50da769f7cf76
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:26:34 +0000

    hold oil funny recognize wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def get_array_as_set(array):
+        return set(array)
