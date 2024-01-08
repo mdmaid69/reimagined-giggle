249ae304750ce589f5b67b769add25949354e8d8
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:03:18 +0000

    little suppose sink feel Cc

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def find_min(lst):
+        return min(lst)
+for i in range(10): print(i)
