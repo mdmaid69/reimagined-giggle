0a0ae2fa117afb50eacb1b0c90db4a1e15899de2
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:59:41 +0000

    lead speed develop disagree boring

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  def is_odd(n):
+        return n % 2 != 0
