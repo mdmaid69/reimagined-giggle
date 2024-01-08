71bea9bdf01eabff80e96e53b9fc6fa1b98afe63
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:37:12 +0000

    through exist calm few tape

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def square_number(x):
+        return x**2
