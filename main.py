77ca1afaa05479b5f343dabf1906e40e96a963c2
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:07:07 +0000

    hard congratulate type advanced comfortable

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import re
-print(re.match("h.*o", "hello world"))
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+print([x**2 for x in range(10)])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
