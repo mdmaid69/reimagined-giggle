602f021d3d2e273df3610480471bb7074640b0a6
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:33:20 +0000

    take Zz school favour carry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def get_array_item(array, i):
+        return array[i]
