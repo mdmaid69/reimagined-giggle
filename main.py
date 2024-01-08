c78f93bda231d9a26d9d76d923e829331b66a3e4
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:08:14 +0000

    well positive Dd mouth public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import datetime
+def get_current_date():
+        return datetime.date.today()
