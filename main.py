dabe002d7f8c83c389163d2f3c577150b4d37ade
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:17:55 +0000

    pause since goon gift with

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def get_environment_variable(var):
+        return os.getenv(var)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
