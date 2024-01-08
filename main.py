3a52257ea8f8d464a2a62dee6c91c2497d125f30
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:32:17 +0000

    me now trick above crowd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
