33c012ea07966cde460ebbbb14e3ed2449aae8e7
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:16:05 +0000

    load but line condition awake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
