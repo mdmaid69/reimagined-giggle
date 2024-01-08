cf63a44b9bc9840774f72c4dbe2d5c1266896773
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:55:22 +0000

    yet shall realize answer protection

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
