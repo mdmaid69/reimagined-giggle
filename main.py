8de7061422f8ffc848b5277038799249914266d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:22:36 +0000

    Ff sky wait noisy since

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
