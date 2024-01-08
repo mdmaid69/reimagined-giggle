7e3b3e49cd56222b63d8edf0ccd658521a7e3366
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:09:24 +0000

    top concern advice rain answer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import numpy as np
+print(np.array([1, 2, 3]))
