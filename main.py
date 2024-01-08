3ac42512b69cfd8b95fcf856ddab995f61128045
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:21:24 +0000

    politics print standard sudden sensible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
