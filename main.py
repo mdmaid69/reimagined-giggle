479950cd0de9aad0cb24c95d4d425de39fbbdffa
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:50:47 +0000

    lesson near belief recently vision

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def count_characters(sentence):
+        return len(sentence)
