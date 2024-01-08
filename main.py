e306258120fbd5e73a25b4cd512fea313d26b38c
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:41:58 +0000

    coat class would theirs need

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_priority_queue():
+        return collections.deque()
