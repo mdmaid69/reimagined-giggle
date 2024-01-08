3bf011982fda71b11ef3ca894c38f2b8419dd6e8
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:43:49 +0000

    stick bridge scale rain walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_area_rectangle(l, w):
+        return l * w
