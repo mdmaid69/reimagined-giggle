dbc423563dd59ea12d425fc173f99fc855c0ca42
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:55:06 +0000

    with learn love theirs little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
