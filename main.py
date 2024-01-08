671b6f20220ad10407cd1a51321cda0be5f937e8
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:55:54 +0000

    hello direct fork already similar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
