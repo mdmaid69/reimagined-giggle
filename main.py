62ffb13e28c3f54c59bf73eab4b8407f9596e9be
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:15:55 +0000

    base drive stiff copy tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def greet(name):
+        print(f"Hello, {name}!")
