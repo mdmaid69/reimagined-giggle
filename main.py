53874d18658b8a39edbd1403470cd103e5757f64
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:44:05 +0000

    feature sad famous hurt furniture

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def sort_numbers(numbers):
+        return sorted(numbers)
+def square_number(x):
+        return x**2
