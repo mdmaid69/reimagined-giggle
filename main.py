345c6b5696dbe7f4ad1f04a51e2ada89ab3154c9
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:47:14 +0000

    keen dry maybe rise section

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+print("Hello, world!")
