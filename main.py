ae5435977f69ca2293f1aa08dc4522bf6bcffcc3
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:52:38 +0000

    meet geton trick stiff watch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+for i in range(5):
+        print(i)
