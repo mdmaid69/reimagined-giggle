72bd2b853ec318390b9da7d6aecf93d8459bd0d8
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:25:05 +0000

    obtain common equipment consist movement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
