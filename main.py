d0a1cc1b2deb2c3ee51b461ce5ed85d6d63ee94f
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:17:26 +0000

    promise though runaway performance keen

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
