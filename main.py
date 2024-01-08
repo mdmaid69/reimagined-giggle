aa7365ea84eea689035f76b11f2747ab4b18b73d
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:10:53 +0000

    knock break disappear aim anywayanyhow

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-for i in range(10): print(i)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def split_path(path):
+        return os.path.split(path)
