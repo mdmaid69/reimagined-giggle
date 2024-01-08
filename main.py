9ce986fcb48dd184cf8dbe7d79310fe19a54c391
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:46:25 +0000

    cry Ll whichever protest comfort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_length(array):
+        return len(array)
