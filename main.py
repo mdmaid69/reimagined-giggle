973d6ceb0aa539ff6782c11c5e98b8522ea22dca
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:54:38 +0000

    comein OKorokay hurt crisis include

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
