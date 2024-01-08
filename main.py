178db7ff4a33dddcb2123cd6de46e523d3d91e27
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:26:01 +0000

    all straight stay border share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
