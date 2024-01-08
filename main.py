f1b9de1697d09941b90b2377c940c3440c3df7b5
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:28:27 +0000

    watch main godown not splash

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+print([x**2 for x in range(10)])
+def calculate_density(mass, volume):
+        return mass / volume
