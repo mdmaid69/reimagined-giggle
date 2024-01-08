98e764f6d49ab72a7def198ef67bc6b092b7c87d
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:06:28 +0000

    past attention myself supply holdup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
