2c1409756af2f50a5de2bdbf545819ba0f675b0b
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:42:14 +0000

    benefit from kill reliable shut

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
