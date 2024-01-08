93588d73521cf28b943cd17d5a467fd56101467f
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:38:13 +0000

    mix would around throw handle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_array_typecode(array):
+        return array.typecode
