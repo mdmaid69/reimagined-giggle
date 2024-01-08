41b956e114e58839deaf1fd0be96d20025181696
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:43:47 +0000

    approval match up cruelty callout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
