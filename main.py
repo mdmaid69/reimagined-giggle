dbef4f39c1e4d4f90ff80e4258bf00cf7cb7e9ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:07:30 +0000

    least search move kill steer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def is_palindrome(s):
+        return s == s[::-1]
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
