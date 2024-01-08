2270c064aa1692b86f8e39ba405e8e5bf7b9575e
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:37:43 +0000

    progress hungry exaggerate usual add

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import getpass
+def get_username():
+        return getpass.getuser()
+import time
+print(time.time())
