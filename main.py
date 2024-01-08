a4832b85806667b41143f7b9bb5b2c4833458773
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:39:46 +0000

    sky present arm fork send

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import os
+def change_working_directory(path):
+        os.chdir(path)
