90cb25bdf1fa74a45a68c6466a5872462fed8cfe
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:26:18 +0000

    bang bringdown other hurt declare

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def count_elements(lst):
+        return len(lst)
+def count_characters(sentence):
+        return len(sentence)
