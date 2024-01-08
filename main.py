c8ec8f726c666da120c5851519084940e29f1f6d
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:34:03 +0000

    angry terrible nerve wet underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
