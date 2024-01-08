db37f7e27ea48e46ffdd403feb11ef36c46146b3
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:07:38 +0000

    insist list spread hardly trust

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-print(math.pi)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_area(radius):
+        return 3.14 * radius * radius
