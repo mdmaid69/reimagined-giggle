0a368798eeedee4021ee40a3cbe54007e202df03
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:48:53 +0000

    test float complete effort there

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def remove_duplicates(lst):
-        return list(set(lst))
+def is_palindrome(s):
+        return s == s[::-1]
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
