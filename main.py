e5ddeb8a04f178c7701a2a9ac5fa80c09cd47dab
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:14:00 +0000

    tread adjective breakthrough across grow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_area_rectangle(l, w):
+        return l * w
