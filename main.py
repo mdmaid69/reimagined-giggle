ad1f3e0ab65747645aac6f60729055e4004aad1d
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:57:20 +0000

    degree tie bet treat thin

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
