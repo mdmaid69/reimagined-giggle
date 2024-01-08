3a7c59273e2de42a06c968fcc9a5372db6b09065
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:56:15 +0000

    example turnout giveout expression distant

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def greet(name):
-        print(f"Hello, {name}!")
+def cube_number(x):
+        return x**3
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
