1f40eeb61d913e7f88a93f6b5ae9fb0216654692
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:36:12 +0000

    broad owe shrink throw surprise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_work(force, distance):
-        return force * distance
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
