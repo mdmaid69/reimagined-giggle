5dd7a4bcb99528cbccd2232b5e2c3943f46e53b7
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:51:11 +0000

    copy criticize extraordinary about rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
