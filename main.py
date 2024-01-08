7866538fde71bcf0e3d588a48afb1c15b507ded1
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:01:57 +0000

    circle rub workupto problem path

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   def convert_to_binary(n):
         return bin(n)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
