8498257733c62ea94a479b524da0ebd8dd9f64b0
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:35:00 +0000

    accordingto case giveaway whether listenfor

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def find_min(numbers):
-        return min(numbers)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
