25286d418c300fbf51d138fb5323156a5ba7cd96
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:44:50 +0000

    crash best human region exploration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def sort_list(lst):
+        return sorted(lst)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
