2734a7ec910d974f9e665b9a0d3c73193c339e75
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:54:24 +0000

    agreement no sink mixture bang

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
