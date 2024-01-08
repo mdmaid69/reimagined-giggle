9901467a07d321c7e788f9f98546773ac47bdaf8
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:44:30 +0000

    any cross relax compare fail

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
