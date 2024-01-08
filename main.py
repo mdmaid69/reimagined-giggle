db8cd2b3a8ab7acffdd32f7e6f697470ff6ce7a0
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:46:43 +0000

    deep fetch born remark run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
