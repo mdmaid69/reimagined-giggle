6ad62225deb5b8afb988d3c1f3d9c4785bae3a31
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:49:04 +0000

    cold which pair wave bring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
