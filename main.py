da69bb906c9a165eec9edebfca96d0ee93eb8b55
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:32:57 +0000

    left sound convinced close large

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def calculate_quick_ratio(current_assets, inventory, current_liabilities):
         return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
