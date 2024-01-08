de12eaa7f466572cd684e83dbb56793170ddc8b5
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:29:55 +0000

    dull vanish delicate juice day

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
