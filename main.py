4e84da2228544241f1e619a10a32dde5f9a6eeee
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:07:43 +0000

    from kill male stranger makeinto

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
