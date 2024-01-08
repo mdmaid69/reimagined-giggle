4088d4640b70bfce0ab4971af330669cb0074f99
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:32:46 +0000

    remember dish level active square

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  def cube_number(x):
-        return x**3
+def find_unique_words(sentence):
+        return set(sentence.split())
+def find_max(numbers):
+        return max(numbers)
