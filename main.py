72a954c2b25e3ef6f3b05bf0ec2321e84c2ae491
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:49:51 +0000

    certainly no conclusion thinkof enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_area_circle(r):
+        return 3.14 * r**2
