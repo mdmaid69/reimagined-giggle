e871a1db37d42b60604f5d29e2afa4c221a98e65
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:41:04 +0000

    marry worth shopping ours pride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_item(array, i):
+        return array[i]
+import array
+def get_array_as_list(array):
+        return list(array)
