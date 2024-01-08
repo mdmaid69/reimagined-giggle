53daf9931a2a30ff01924a88d175d5e2e5bbc019
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:10:25 +0000

    middle 569enjoy pause bye crime

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
