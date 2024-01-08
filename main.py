f4c019ca76c19b738c84785a3e60ad7b73dfa501
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:20:44 +0000

    action yet next goaway characteristic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
