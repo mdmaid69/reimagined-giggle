8d1a444a5ba6dfe467d087c5b36da200db379ac1
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:45:40 +0000

    belief tool switch season bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
