812b61ec4a124d116160ddcc995ae0db74f4147b
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:40:14 +0000

    negative dry complaint home keepoff

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def add_numbers(x, y):
-        return x + y
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
