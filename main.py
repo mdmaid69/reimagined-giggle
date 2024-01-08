247901920473e0e7c8b20651441bdf44544748ea
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:26:15 +0000

    argument lay your calmly own

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
