3723edd79b5343f37643cb85850ea27c8b6f6e92
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:38:17 +0000

    bored straight improvement follow turnover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
