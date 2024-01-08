e603f1580c873f24e3ac8475b774dbc3fb08634e
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:12:31 +0000

    small tape team finish common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
