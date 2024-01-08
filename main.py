4090b41e5debd6f7f9e641efcef019af8faf84c7
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:15:03 +0000

    bury holdout plate joke visit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def convert_to_octal(n):
-        return oct(n)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
