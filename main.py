145ae1aa0c3e134f0e6136bf1e74a6dc024097a8
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:53:01 +0000

    port pass cut blame embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
