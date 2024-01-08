fe7c52301f2d2d6e5a059ab0ccf40ba6dba9fb1a
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:06:10 +0000

    freedom up they keep strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
