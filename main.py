c49d4439b17c851b0a94798a4713bc5a7dda35b4
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:58:10 +0000

    since brain church nonsense congratulate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
