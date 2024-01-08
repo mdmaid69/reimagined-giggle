dfa2cda5b71b13a8e186857f28b309629891480b
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:44:01 +0000

    organize complain charge so previous

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
