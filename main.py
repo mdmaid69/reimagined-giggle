3b0bc8142014ec5ce59ff35b328b95a90a304e4d
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:27:38 +0000

    curious makeup secret ground recommend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-import platform
-def get_python_version():
-        return platform.python_version()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_speed(distance, time):
+        return distance / time
