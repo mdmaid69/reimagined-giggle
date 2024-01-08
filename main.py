d70454bcad30f41e0f434cf99256009ebe81b00e
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:42:37 +0000

    weigh could whistle protest thorough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def reverse_list(lst):
+        return lst[::-1]
