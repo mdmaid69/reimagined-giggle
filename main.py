59adc8496dccf1e13134a7bd9339795ce6b63da1
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:57:35 +0000

    shock anything uniform tend model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def reverse_array(array):
+        array.reverse()
