035bea7ecf9915fb7796d479f78f593fa950fed5
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:42:12 +0000

    build below burn speed convenient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
