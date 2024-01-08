1806ebf78c012d25f0ca8abca8433b0f2476ae4a
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:58:21 +0000

    poison club everybody polite round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
