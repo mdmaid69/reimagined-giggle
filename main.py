9660eef1149d05798b3b44684cd8dbaa74cb97aa
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:27:03 +0000

    dream travel accidental copy bunch

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
