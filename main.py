6dcaa205012cab41059d66a4311b034f6925b8bc
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:47:22 +0000

    delight proper strong keepoff congratulate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def convert_to_octal(n):
-        return oct(n)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
