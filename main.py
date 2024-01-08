7e136f46f4d791bb9a5e07ae5604a1940fcd4f61
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:51:32 +0000

    word failure bin ball so

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
