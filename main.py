3679ee1ba199ac68e6daea0f1e87ffcb8304de76
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:27:07 +0000

    flood serve word over twist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
