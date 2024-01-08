894e2ba430f6d9eb4cf0bf3e6b7d01b8d400b7a6
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:26:19 +0000

    noise geton this terrible sport

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
