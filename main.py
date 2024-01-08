9ae75473b129394a2c2f1edad1cec0e119d9ad30
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:26:32 +0000

    clean declare classroom patient crisis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def count_words(sentence):
+        return len(sentence.split())
