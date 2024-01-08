bb19bac5f8536a3d4ba08aac9856317d41d24ea7
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:02:18 +0000

    go hour shame urgent worry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_work(force, distance):
+        return force * distance
