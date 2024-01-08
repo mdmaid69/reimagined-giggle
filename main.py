2e4c7fd7ecd73f49cfcd6db512bd25a85f2bcd0f
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:25:18 +0000

    set kill whenever black exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
