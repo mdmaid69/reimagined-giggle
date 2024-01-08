e0516c7ac855aa7fe769a92d35110c768c143f7a
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:08:37 +0000

    sure false pleasure relief must

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
