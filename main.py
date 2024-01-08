32a5ccd141813abdf2c87802b1026fe36786a1fc
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:19:08 +0000

    murder still door have choose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import time
+print(time.time())
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
