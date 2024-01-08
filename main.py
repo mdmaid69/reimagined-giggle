2693c57490dd97e43a31a5208ef2bf5bda9f5993
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:09:40 +0000

    shape minimum notice private unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
