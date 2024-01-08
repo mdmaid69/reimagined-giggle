e8247c5ae911b348e58304c597234a22c5b78bbc
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:34:54 +0000

    declare bound attractive joke describe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_volume(length, width, height):
+        return length * width * height
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
