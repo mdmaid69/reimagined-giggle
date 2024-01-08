02cde7aa1759a47f51b7afe2d5ff4fc5c0080527
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:57:26 +0000

    occasion petrol confuse roof match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def sort_numbers(numbers):
+        return sorted(numbers)
