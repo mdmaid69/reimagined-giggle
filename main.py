63f6a626f833f2c153a1e9201b17683159034ee0
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:26:23 +0000

    exam disapprove back succeed deaf

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
