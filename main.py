7d515259afe74523f60d2fcca1879e2dac53d2f5
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:04:41 +0000

    appear fork shut do question

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def convert_to_hex(n):
+        return hex(n)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
