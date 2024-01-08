787c122b49674c42138da5ce33b7a0b10ee8ec28
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:24:14 +0000

    back steal steer bite fear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def subtract_numbers(x, y):
-        return x - y
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
