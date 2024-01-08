34bb4248e498d145a8536d6c80f99b5cc8c04a1b
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:39:58 +0000

    responsibility narrow angry runinto trousers

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def convert_to_octal(n):
+        return oct(n)
