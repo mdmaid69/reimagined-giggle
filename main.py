8c89186654fd3aa6079a45c8bfc80378562987bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:20:51 +0000

    relationship open constantly area clear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def get_array_as_set(array):
+        return set(array)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
