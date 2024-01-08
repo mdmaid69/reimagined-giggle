2e43b44e4f11b99760865ee102a8436e03432685
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:03:34 +0000

    collect couple copy either flow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
