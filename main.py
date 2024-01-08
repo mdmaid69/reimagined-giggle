1741f2bf6e289b01dad075d37a7f26474ad169c2
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:53:52 +0000

    task bringforward music computer arrangement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
