388170f6ad90b19bcbe36a76a4c16dc543241865
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:25:33 +0000

    lie cry nor motion sink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+  def count_elements(lst):
+        return len(lst)
