37c9d80f9aad7b1e5b446af29b62f64d5ec35c8d
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:02:02 +0000

    dance communicate refer set 179bite

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_power(work, time):
+        return work / time
+print(sum(range(10)))
