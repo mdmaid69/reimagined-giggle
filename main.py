7f3b25fe95f7735676a00cb785c024ce972a9dbd
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:52:48 +0000

    kind loss match safety cheek

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
