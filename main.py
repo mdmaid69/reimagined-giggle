c539d8d2137cbc943a04164b03da372fa4aedfcd
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:59:58 +0000

    interfere imagination mean content refer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
