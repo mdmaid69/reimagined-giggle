cc252a6c5a4ee1f9f0444a3f19dbcaa8b8a0170d
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:07:07 +0000

    spot position surface interested weigh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
