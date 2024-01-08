46da7d332bdba30d69413d3bef4b6bf04fe28188
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:56:34 +0000

    whistle let pattern rain beneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
