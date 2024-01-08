40fe243c0a28b6d349c75bbb2f77f364c7ece364
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:01:10 +0000

    rent balance ourselves 572enter quality

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_typecode(array):
+        return array.typecode
