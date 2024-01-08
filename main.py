ccc4a8ca2dd78d258f07a6d90ea6f2f167e641be
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:38:00 +0000

    borrow we rate whenever getaway

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import tensorflow as tf
-print(tf.__version__)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
