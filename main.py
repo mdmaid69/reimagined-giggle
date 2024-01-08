218855e5fc7f6a68410af34da9d91321c958e3fd
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:21:15 +0000

    advantage connect rain mend wish

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
