4da3097ab0b2d7e6c5298bec13d70fa1a2426dd2
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:22:38 +0000

    official until valuable letter performance

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+for i in range(5):
+        print(i)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
