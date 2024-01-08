a7a62baa476dc4359870c39061285d426b85b2a4
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:52:02 +0000

    lookaround 573entertainment empty offence pencil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
