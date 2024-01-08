5d57c6bbb21237790e2f0bc803796735eb26f7e6
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:26:35 +0000

    getoff dinner coffee mark interested

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
