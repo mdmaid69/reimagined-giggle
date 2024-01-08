892850372a26e87535f38ce58dd29d709bf3073a
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:47:59 +0000

    little before free deliver criticize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_current_directory():
+        return os.getcwd()
