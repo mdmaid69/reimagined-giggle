c6d22b4bf8deba3a2fe351677a5c6da8819dfa89
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:54:13 +0000

    fly breakin die connected ocean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
