066af20c65a39f6130acc440157721de3f93869e
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:03:30 +0000

    material connected protect interview trip

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  def is_odd(n):
+        return n % 2 != 0
