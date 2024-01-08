e7fd5c2bf3fa07397b81a4ba1aab10ab517632a8
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:34:18 +0000

    joke scrap whether like wander

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
