aa9f95cd798e5325ec2ca427d55662a6c106c04b
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:36:13 +0000

    thinkover old public pay lunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
