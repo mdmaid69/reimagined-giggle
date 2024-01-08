bb7243515edfade650c9ab79743359e23899a578
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:32:39 +0000

    week immediate favour show disapprove

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+print([x**2 for x in range(10)])
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
