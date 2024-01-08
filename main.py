8d4b205551d92d8ae912ed775c19bba5c3bf4f7a
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:17:01 +0000

    sense bringup then beautiful theirs

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
