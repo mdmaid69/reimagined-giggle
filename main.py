ef09cfe0255d5a49d68931038a01c7896e090d00
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:53:35 +0000

    afternoon respect silent once off

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
