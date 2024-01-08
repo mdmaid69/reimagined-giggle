7887c34c3dcc2ace35e07efa5d36f5704a9d1e08
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:33:05 +0000

    punishment hello thing care match

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
