68b189089cd2abcdfdd4c9d227165ab6ae02142b
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:25:34 +0000

    flow still breakinto plant appear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
