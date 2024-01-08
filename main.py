cc822206fca322c79fe3877e87592040991867af
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:47:05 +0000

    treat performance partner worst pot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
