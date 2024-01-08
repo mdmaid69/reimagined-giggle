fcc56527ddec575e034d01193b51a1a44913dbc8
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:55:32 +0000

    takeout story gap wonder substance

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_power(work, time):
+        return work / time
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
