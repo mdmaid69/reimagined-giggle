6401e6925060f13047ed5c67640342785cbd3974
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:30:50 +0000

    desperate suggestion material wait smile

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
