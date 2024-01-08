afc2469861271736ef6c40715a1c76fcb92bdf8d
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:30:20 +0000

    treat difficulty experiment practice transport

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def is_palindrome(s):
-        return s == s[::-1]
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
