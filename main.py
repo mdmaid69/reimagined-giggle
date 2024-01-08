fdc062aee29ea90f5b874f60492794845880c809
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:20:37 +0000

    nowhere dick separate forget honest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def get_array_item(array, i):
+        return array[i]
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
