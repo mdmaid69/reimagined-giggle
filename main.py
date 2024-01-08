cc88c7c15ce5fa4c50994ac7cb8884c76d95d440
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:38:25 +0000

    try collect minor wipe workupto

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
