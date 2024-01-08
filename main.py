88ce8f4e4e37d855a2499a36bf84a6115bfc1af2
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:21:26 +0000

    achievement figure fine term stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
