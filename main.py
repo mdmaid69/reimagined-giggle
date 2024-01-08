5f3a7e3d0ac2336f254312b6d0340c1d981964ff
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:06:48 +0000

    scheme tour unlucky mess supply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
