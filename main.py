b4a1bd376f739b17389a5296d96d4857a3b23105
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:26:47 +0000

    offend public programme by visit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
