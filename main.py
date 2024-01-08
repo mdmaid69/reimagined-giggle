96d8e0a0590fdbee95928b3c6e9b074766d66f25
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:58:01 +0000

    wrong now debt whenever food

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+print("Cube numbers:", [x**3 for x in range(n)])
