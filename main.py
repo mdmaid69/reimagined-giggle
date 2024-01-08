14c1a5d50254f37dc9f83c18e37a3502ba569b70
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:12:25 +0000

    conclusion pool free nigga course

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_cosine(x):
+        return math.cos(x)
