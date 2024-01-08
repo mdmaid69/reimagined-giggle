9aca7c62fa8edba4377981b836e3f60782c3bb0e
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:06:16 +0000

    Gg dead tomorrow decision raise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
