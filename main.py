0b913364987d154e0a7653af20b2f7297eacbf6c
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:00:42 +0000

    politics everybody convince front gather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
