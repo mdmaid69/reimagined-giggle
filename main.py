cdc10f6acc0f8cebc0f205190ba9cf7331074345
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:55:00 +0000

    ours obvious tip rush backwards

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  def is_odd(n):
+        return n % 2 != 0
