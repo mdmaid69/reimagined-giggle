be44c2804594b19c57311739b0b95d5a26f27e7e
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:08:45 +0000

    reasonable support content us key

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
 import math
-def calculate_sine(x):
-        return math.sin(x)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_exponential(x):
+        return math.exp(x)
