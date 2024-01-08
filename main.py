e46d31774a7640726434cb7384f439ea94eba7c9
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:39:59 +0000

    eat rather raw nearly word

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def square_number(x):
+        return x**2
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
