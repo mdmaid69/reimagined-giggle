d01237d10a1639f6fbd330216ba0e6820caf9b86
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:42:13 +0000

    have scrape estimate accordingto manage

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def square_number(x):
-        return x**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
