6ff17c38f1dbf073f5cad8d09f1eecb510453b52
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:23:09 +0000

    hold freedom pause excuse belief

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
