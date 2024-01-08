f7b79dfee56017743b95f81e09a146d6869e1e68
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:25:48 +0000

    hello catch abroad lesson practical

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
