ac2fa416af46b80cbe216ae1c4fff83acb4c3fa4
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:06:47 +0000

    uncertain acceptable serve cheek shape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
