4e92bbe079e4734e74b0f00693ddd6028399f3e8
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:12:25 +0000

    balance fix by school club

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def get_array_as_complex(array):
+        return complex(array[0])
