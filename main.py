413b7702c02326a2c10194f76e8134d3342905f8
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:08:56 +0000

    warning consist control choose talk

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
