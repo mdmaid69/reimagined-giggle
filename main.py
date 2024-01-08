c3b964bd25e2fc21534b1e0dd45b21a6c15c707b
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:09:37 +0000

    burst single either worse exchange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
