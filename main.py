4190229524d9a7b3f9057bd96114d05602b07ce8
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:47:52 +0000

    fail cap cover breathe hide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
