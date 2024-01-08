1cb4994670e8875c1050e302895cf32209e0510b
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:19:26 +0000

    comeoff Cc harm search cheek

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
