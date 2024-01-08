718afdb2c06fa94e2eb8909b973f4706fa3e0fb3
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:33:55 +0000

    by breakin exactly point equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
