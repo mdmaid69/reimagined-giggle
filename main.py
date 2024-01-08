92490ff43db23da893f2c4dd77cebf40f769610a
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:59:41 +0000

    then trip centimetre comeup system

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import random
+print(random.randint(0, 100))
 import array
-def clear_array(array):
-        array *= 0
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
