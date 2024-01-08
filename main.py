e7497fa6cdad84a096c54eebf09aac99a79eb9d5
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:03:36 +0000

    combine smooth translate swallow lend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
