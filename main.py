b924d2c6335eca8e6564861bd478f5cc54621c05
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:49:36 +0000

    afterwards not careful break crash

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+print([x**2 for x in range(10)])
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
