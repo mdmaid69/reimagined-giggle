30f3cdeeef5b583063d5cd34742cf1a0a8fb4e2d
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:19:52 +0000

    proof description time mend supply

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
