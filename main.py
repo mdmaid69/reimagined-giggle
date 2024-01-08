685ceb0c40b50aa60a9c5fe37e82da68b5d8a087
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:42:44 +0000

    copy comfort lookafter Jj otherwise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
