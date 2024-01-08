a572b27afdb9232737e9ce3a80542074627b41b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:28:22 +0000

    walk surprised inform tax explain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import time
+print(time.time())
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
