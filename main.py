18a1e84c9061bc1327d8ef020a5aea6d44620bb0
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:13:47 +0000

    crowded content opposite decide when

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_area_rectangle(l, w):
+        return l * w
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
