810c68e635d14f16d979129c68cdc8b88dad72f0
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:34:00 +0000

    jump image unkind study size

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
