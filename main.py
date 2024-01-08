53c1382c68f1012aba57ebbac35d48d585b90e2c
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:29:41 +0000

    decision such intelligent wear over

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def count_elements(lst):
+        return len(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
