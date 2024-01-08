fd9553a0f992dd591815cee25431485b2c3b88d4
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:05:30 +0000

    flight safety note goodbye stretch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
