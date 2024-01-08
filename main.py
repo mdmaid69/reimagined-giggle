0fb028b31c1a387f54546848201a5d690217090f
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:58:18 +0000

    ourselves lower centimetre scrap friend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
