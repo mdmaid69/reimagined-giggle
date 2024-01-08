2f9c11bddf555fbf6207521a22492d2477440342
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:39:11 +0000

    ball pretty recent goout under

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_work(force, distance):
+        return force * distance
