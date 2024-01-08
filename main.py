5fdfa0e2737a52dcd5c8153bcda671225a1d9a4c
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:34:58 +0000

    empty tradition already anyone keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
