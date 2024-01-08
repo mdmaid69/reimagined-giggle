7adf99fa27e953b13ccffeee87843fc25ba978e8
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:31:32 +0000

    contrast mind cheque pinch exact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def find_max(numbers):
+        return max(numbers)
+import getpass
+def get_username():
+        return getpass.getuser()
