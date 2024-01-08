6cfce1305ec4a1eacf4e7f3a79b9c502445a8734
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:57:06 +0000

    through approval brief force chance

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
