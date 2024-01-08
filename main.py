7be2fbf644905e040d7dd75940375792c4cd43d6
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:14:41 +0000

    transparent whisper justice lead emphasize

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
