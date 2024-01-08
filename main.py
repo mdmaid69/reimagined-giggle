fa21140746a4c86b2b7e2de6b505879fa9a3e8f3
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:03:51 +0000

    sweet fight government weekend distance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
