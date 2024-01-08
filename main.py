aa713dc97e6bdbda790ed5d4d468b6f635cce5b3
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:49:18 +0000

    must use Ww one value

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+text = "Hello, world!"
+print("Words:", len(text.split()))
