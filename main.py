0c7ee6c69f2690d24c89325c7ab55ea30f5e4303
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:38:36 +0000

    government strength but trick distant

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
 import array
-def get_array_as_str(array):
-        return str(array)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
