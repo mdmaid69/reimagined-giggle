65eb16bc6ae51ec6f8204893d25957400b9fbde2
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:58:22 +0000

    grand reason dance safe closed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
