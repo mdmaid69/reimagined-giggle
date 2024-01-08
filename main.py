fc0a21b446bacd1a0191c1f7d4d7cd356d436a9f
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:11:43 +0000

    Ee suit embarrassed back tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
