414c7c6e42412bdea3b71ce0cb7f227c41e3f64e
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:07:07 +0000

    advanced crisis shall high ashamed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
