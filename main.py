77450bd58a5fc978b9a6f74bd379a34cfbbe65a3
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:14:59 +0000

    sense interesting entry feel fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_tuple(array):
+        return tuple(array)
