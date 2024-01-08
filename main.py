036d7e2082755d4679f3bc87f7b452ca3e126d8c
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:03:56 +0000

    while switch slice everywhere address

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
