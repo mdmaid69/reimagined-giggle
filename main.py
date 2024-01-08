83f75db3369b4e8f879278b00c4960525550654c
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:04:06 +0000

    regional Ii breakinto fear pipe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def reverse_string(s):
+        return s[::-1]
