444cf3567bd5dd59df99e6a9b3fa3f7f79679022
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:20:39 +0000

    capital invite whole fire over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+name = "Python"
+print("Hello,", name)
