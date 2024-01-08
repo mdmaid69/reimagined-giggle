553c3898b0d78ff8324ab99a34e23b94fc8a7690
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:41:37 +0000

    very breakfast wall distant Ss

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import logging
-def log_message(message):
-        logging.info(message)
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
