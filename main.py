feea960f7d24c73aa5551c48ca4ea61158c6e595
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:46:02 +0000

    vision plenty unit back exist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
