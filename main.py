23c87653499a9a325757ca40572f41d065d9c030
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:49:22 +0000

    argument wear negative pause distant

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_min(lst):
+        return min(lst)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
