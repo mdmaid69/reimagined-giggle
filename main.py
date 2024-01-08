c2d7d972fa3bcd546917d1aaf0e0608c82f81a09
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:12:32 +0000

    blame tradition journey next national

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+text = "Hello, world!"
+print("Characters:", len(text))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
