1dce2174043019a3a206bd7e7c4c5b1827ecf82c
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:45:15 +0000

    myself offer collect wave quick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def split_path(path):
+        return os.path.split(path)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
