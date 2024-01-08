02e3a162b95ff967c9c2aa9619c3acda1f6f3048
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:19:51 +0000

    complicated noisy snow fashion produce

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+print("Hello, world!")
+def is_odd(n):
+        return n % 2 != 0
