e9b44f04820e365a316598958deda4ed718ad7e9
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:09:38 +0000

    there kind arrangement anyone father

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
