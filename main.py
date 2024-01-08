141979f1735547c3e288a5cc06ef0003ac452422
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:09:18 +0000

    help study opinion nonsense leg

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
