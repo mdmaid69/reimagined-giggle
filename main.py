b806345524dbfcd411f5a45604f1d7424a2d78f4
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:56:39 +0000

    standard of juice our worse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
