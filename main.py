8f4dd0340768c1390b2c1578fa171c777c5e5517
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:13:49 +0000

    marry lot print plate certain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
