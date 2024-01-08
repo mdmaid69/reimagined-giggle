238b02b5e38838d09aee49d9798c9889fb1e7fe2
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:17:36 +0000

    work tip blame advantage winter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
