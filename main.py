b3075871e85a38a3dfc60697db59b3c408b73cca
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:00:46 +0000

    passenger disapprove see study table

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
