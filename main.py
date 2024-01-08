0401b81e76f9c2004742cdb28a18a568053508a1
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:58:23 +0000

    hers before job discuss see

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
