c2c893d3bb84bba0a5bc9d5c64e1318739e86702
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:56:58 +0000

    building various modern air comfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
