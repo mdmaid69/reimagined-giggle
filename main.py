39ac1fefc5c5328eee2b486ce07d0d982ed9540d
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:36:05 +0000

    count picture bye reject inform

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import sys
-  def get_python_version():
-        return sys.version
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import array
-def get_array_length(array):
-        return len(array)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
