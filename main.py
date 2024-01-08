4175d143e344c7a47e588486975c675649df9d24
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:09:25 +0000

    word new kick sport future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
