6cd3ddc5ed77e007dc43ebbe06960fe2e0750335
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:34:35 +0000

    smile nearly drive music steady

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
