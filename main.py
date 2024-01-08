fbe3934b009e626248b9ff852f50b17267220846
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:01:50 +0000

    in profit care every evidence

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import tensorflow as tf
-print(tf.__version__)
+  def count_elements(lst):
+        return len(lst)
+def is_odd(n):
+        return n % 2 != 0
