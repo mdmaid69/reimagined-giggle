f9349f4c248d53f4d64c8870be51a4826280a93a
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:03:34 +0000

    serious unfair nearly underneath Tt

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def is_palindrome(s):
+        return s == s[::-1]
