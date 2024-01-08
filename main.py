0b6728e634df8fa099343be4534bc7f5277b949d
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:13:21 +0000

    battery birth interested report talkinto

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
