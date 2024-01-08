be46d6f22c99d66654c1899ee811fe0b364d8e2f
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:27:26 +0000

    legal pupil sensible chief influence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
