23dd7f771efa32c3cd788cd8baff9455c455afda
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:01:00 +0000

    lesson care explanation man keen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  def is_even(n):
-        return n % 2 == 0
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
