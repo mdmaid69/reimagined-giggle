6ce5c81c0e89f63a1980d2d905007ebc9d05f3f8
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:35:44 +0000

    partner everybody plastic delicate influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
