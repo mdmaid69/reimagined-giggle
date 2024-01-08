27acbe7063bf952434692849a64e6e713882efd1
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:50:57 +0000

    little stamp skill flavour finally

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
