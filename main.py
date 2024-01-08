f2cf1f1c406dc2992b1e042e338edd0e7e96e827
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:06:11 +0000

    similar book aim hit get

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
