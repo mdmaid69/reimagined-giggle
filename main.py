b49553a5c247c2b26899b72cb1fad1a74d0e185e
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:01:09 +0000

    upset know admire need active

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
