b1dbfa8de79600f6b2b6ab3a54ba4a98f97d2d55
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:42:55 +0000

    off frighten protect distance tight

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+name = "Python"
+print("Hello,", name)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
