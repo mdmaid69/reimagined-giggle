287007417e95346e6c55ab4fb41497bf94244df1
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:18:23 +0000

    narrow rather he finger successful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def is_palindrome(s):
+        return s == s[::-1]
