f558aa04ae4c8cf680228540e838e3d67c9f53f0
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:25:23 +0000

    trap thinkthrough determined within underneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import os
+print(os.getcwd())
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
