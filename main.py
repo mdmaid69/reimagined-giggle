f7f9d26ed542870beb03152b421d457d66abc7ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:14:02 +0000

    part easy unit reliable whether

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_area_rectangle(l, w):
-        return l * w
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
