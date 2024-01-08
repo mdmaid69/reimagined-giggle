e210cbb41ab61d887e676ab8f5e8061b22b6e73d
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:00:12 +0000

    through design nature cut satisfied

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
