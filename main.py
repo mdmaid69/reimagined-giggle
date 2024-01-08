1867bddd7807517e82c2a87f10064d8865a51997
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:17:13 +0000

    cheque shoot summer mention about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import getpass
+def get_username():
+        return getpass.getuser()
