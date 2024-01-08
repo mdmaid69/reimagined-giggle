63e92a461c80c5ba93171a536357419035aa9597
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:35:23 +0000

    but none loan welcome treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def append_to_array(array, item):
-        array.append(item)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def get_array_as_list(array):
+        return list(array)
