5cf6d3938af6589db0abbdcb1e8267426d379604
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:06:20 +0000

    communicate bread decision file season

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
