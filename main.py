26e0536ea1bbdf70bc5b9890632b66dc60b91ffd
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:09:10 +0000

    die boil pen bet workupto

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
