fad00ab5fe4fda663258c23bea2fc274435e5030
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:25:15 +0000

    ordinary reasonable poison polite weigh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
