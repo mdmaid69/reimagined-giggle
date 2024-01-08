a0faa75a27e978b4837248d1151d5f847da1acad
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:11:42 +0000

    appeal post confuse blame raw

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
