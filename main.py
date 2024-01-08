2a0b9935c18fa38ce154336c4fceb0531d880b28
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:29:20 +0000

    very pour rule obtain practice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
