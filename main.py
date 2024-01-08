286c10cffe2a11bcc952d1758e66efe6ed984996
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:41:32 +0000

    564encourage turnaway hold comeup buy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
