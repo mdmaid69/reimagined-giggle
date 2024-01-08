a183606d48fbffd6459615ff32fc381732b57ddd
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:18:35 +0000

    day runover power autumn thankyou

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
