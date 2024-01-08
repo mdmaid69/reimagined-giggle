a0c9e4183073ba47194d2b237b9121160048aa5f
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:39:15 +0000

    single concentrate afternoon sleep mine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def square_number(x):
+        return x**2
