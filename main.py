cc1b5703dc68f694fb1fe7cfe997f00940bf33f5
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:26:57 +0000

    press black strong hurry general

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
