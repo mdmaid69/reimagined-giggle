169e2ef618a3218905edb316d4cf4564f91f02cd
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:36:34 +0000

    steady actual afraid introduction instant

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import sys
-  def get_python_version():
-        return sys.version
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
