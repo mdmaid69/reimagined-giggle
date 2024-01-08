e122914ec5314ba1e0d8ca4a0b3732c29f00ca70
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:54:40 +0000

    base less really possibility lost

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
