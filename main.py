c6e4c44579264460e9231bac73b3a14fe482382c
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:51:39 +0000

    seem group address temper show

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
