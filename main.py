f5691a29eda04db1e691422f7d33e9d2cd902341
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:33:13 +0000

    mistake dare pretend giveback share

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import sys
-def exit_program():
-        sys.exit()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
