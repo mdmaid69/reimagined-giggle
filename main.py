99024620bba82adcafc1ab15057a5d19380f02f2
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:47:28 +0000

    new crazy cheque rubbish reserve

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
