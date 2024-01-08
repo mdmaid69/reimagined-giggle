0fef5745d24348618c21e0abe860441afd467a23
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:32:07 +0000

    join seem cough message loss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
