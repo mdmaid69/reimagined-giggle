96068c8a9d872ac96128aac91bb9510a15ab1851
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:47:11 +0000

    tread meal instead peaceful talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
