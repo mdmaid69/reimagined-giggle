c6749a8549af2a8ddffbc0e4f646587be5c8fc92
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:12:30 +0000

    Kk dance further minority instrument

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
 import array
-def get_array_item(array, i):
-        return array[i]
+def set_array_item(array, i, item):
+        array[i] = item
+  def add_numbers(x, y):
+        return x + y
