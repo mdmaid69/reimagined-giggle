3086db1c8fd92ad63013bcec4ddade2d17f9492b
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:24:04 +0000

    forward decision neither stare movement

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_sine(x):
+        return math.sin(x)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
