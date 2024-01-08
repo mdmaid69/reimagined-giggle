8ccb21e1759ad152843e4a7cbcd33c212d7fcae0
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:32:09 +0000

    physical clearly permission good around

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
