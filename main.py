d5936e3e50bb9a3297392b8d41f190dd1e27d866
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:48:40 +0000

    wake regularly rescue ahead bear

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
   import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
         plt.show()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
