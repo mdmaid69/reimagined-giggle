d903e3abad4f2ebb216fef7ab5356b0b56d941f7
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:24:13 +0000

    free top impatient then dry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import re
+print(re.match("h.*o", "hello world"))
+for i in range(5):
+        print(i)
