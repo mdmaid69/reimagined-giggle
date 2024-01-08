0f35a7e93d8ecdfdf318767fb2179f798ba177d4
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:02:12 +0000

    business split nearly object interesting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
