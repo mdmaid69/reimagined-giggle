62a3228e394a332dafff03e4c940cf4f4d44ea2d
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:16:54 +0000

    whether load drop funny clearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
