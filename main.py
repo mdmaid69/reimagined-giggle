3a65086b819c58f8facdf7849c98e10432a9e03e
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:19:28 +0000

    can rate opportunity band near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
