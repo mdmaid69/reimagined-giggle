19f7afd2565b9af828f326afe408141bf88c21d0
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:06:53 +0000

    getback thinkof unfortunate wipe person

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
