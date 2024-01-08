460f0771c6bb0acf0723c1034883dead6a973bf4
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:59:34 +0000

    present he relationship brave stage

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
