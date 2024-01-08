59154ef4335bc18e1900cec5d2dfbf014397c093
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:37:31 +0000

    show smile clearly strength scrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import platform
+def get_os_info():
+        return platform.uname()
