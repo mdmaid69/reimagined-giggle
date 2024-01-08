761abc012b1007f552b6a2efc56c404ed6fb2278
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:55:58 +0000

    Ii fetch possible label follow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_density(mass, volume):
+        return mass / volume
