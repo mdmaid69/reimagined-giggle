9fb534f6c57f4f70f57a427a85d65b779771c670
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:10:41 +0000

    protection you sharp react dare

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def is_palindrome(s):
+        return s == s[::-1]
