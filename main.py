fd434f12c59f7d2c994d598f87333bec99bed922
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:29:28 +0000

    ignore getup persuade should below

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
