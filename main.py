294aa916974f01b8c024778887cf8b228f0cb494
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:56:51 +0000

    because wash delicate still onto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
