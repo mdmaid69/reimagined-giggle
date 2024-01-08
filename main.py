b0446a4a1d866678581357fa7ac04515f094540a
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:19:09 +0000

    experience fall not impressive every

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def is_odd(n):
+        return n % 2 != 0
