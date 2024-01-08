1a184b7a656565201527c9f52e376ab0fa60294e
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:00:22 +0000

    if open pride keepfrom upstairs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def square_number(x):
+        return x**2
