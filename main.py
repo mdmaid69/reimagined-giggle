b3cf334515675398dc493f040692deef836133f7
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:04:12 +0000

    rich record patience breath trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
