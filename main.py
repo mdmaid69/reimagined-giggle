c24e8b641c650c2d9a60d5359b75a8902ae9ac69
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:04:50 +0000

    mainly limit niggaacademy charge exciting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
