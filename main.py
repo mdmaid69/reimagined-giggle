c995cf4d7fe7b540bec76e9f08639b2227005c4f
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:49:11 +0000

    though phone spread definite rob

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_current_directory():
+        return os.getcwd()
