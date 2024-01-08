a58f7744bd9e5928548c9b209e32af683026c4ba
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:58:43 +0000

    late cough talkinto blind solve

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
