5a5d67255edd9a195ab7f806c42b8fe8e5f85e38
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:31:20 +0000

    steep formal there human limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
