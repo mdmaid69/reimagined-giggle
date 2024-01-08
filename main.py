82b1334317761055d5a3563d3778dc21018824e3
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:19:15 +0000

    artificial habit our chat after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
