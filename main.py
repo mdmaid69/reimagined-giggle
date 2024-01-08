0f33d843d54d44180c12172d88fc28200a3ac301
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:45:20 +0000

    actual behaviour tour serious main

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
