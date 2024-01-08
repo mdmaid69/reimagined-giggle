7a3ffc9cb88d8ed3d5900479d0fd63d196e10e7e
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:31:29 +0000

    sure crazy moral combine nothing

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def cube_number(x):
+        return x**3
