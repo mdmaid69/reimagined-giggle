05d2068d7ebd88988002ce7cf5a34e3f4d9bd36d
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:44:16 +0000

    gas face means want repair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def get_current_working_directory():
+        return os.getcwd()
