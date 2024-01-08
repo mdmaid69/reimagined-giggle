419978a5c3f7c153a5c8d5d3dddd0720b1e46ca9
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:48:06 +0000

    lift mind comefrom left mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
