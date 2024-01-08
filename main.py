24fcc6a1bce172421a486ff3c9a0b52df1ffb803
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:06:49 +0000

    arrange secret at ashamed makeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print("Hello, world!")
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
