f243765b32b5aa57de8622328f33634bf09e126f
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:52:34 +0000

    spray partner kind achievement duty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
