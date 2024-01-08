77188043a364f76be234a7d12f25cec474e063a4
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:16:30 +0000

    tip it week short reply

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
