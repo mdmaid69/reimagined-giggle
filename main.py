901476496f56857146dd0d21b1bf5d23dc9c0bde
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:06:27 +0000

    contain somehow disapprove wild throw

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
