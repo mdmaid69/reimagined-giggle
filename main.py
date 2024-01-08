4995fb8b8fd8b381f6b1be1be8934f363319335f
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:16:03 +0000

    appeal trade post branch regret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
