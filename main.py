7fa3a215d4d83ddbe51578ab14588ab2b6d776ea
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:07:57 +0000

    kind out scale black grateful

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def add_numbers(x, y):
+        return x + y
+print("Hello, world!")
