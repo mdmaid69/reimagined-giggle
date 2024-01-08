b3281bfbf235d49ee3fc543cfcebe2333f0151a3
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:09:17 +0000

    reject clever relative danger independent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
