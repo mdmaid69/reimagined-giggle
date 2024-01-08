33b3935f2414fd6272235842313eb9cec3901dd5
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:28:42 +0000

    appeal lonely sure pay own

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_area(radius):
+        return 3.14 * radius * radius
