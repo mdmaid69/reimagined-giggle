cbc275f6f33cbe38dbe8840a225ed6046b80b92d
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:53:01 +0000

    protect shut neck several last

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import os
+print(os.getcwd())
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
