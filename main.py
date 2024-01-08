97ea6833826b3b0d529013b3ffb23807ec5ebdfc
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:55:07 +0000

    drop read slightly loose hide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
