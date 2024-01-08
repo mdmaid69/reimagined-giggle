7e4a578286a7b0201751d87b12c205761c6cc1e7
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:31:17 +0000

    equal underneath play across 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
