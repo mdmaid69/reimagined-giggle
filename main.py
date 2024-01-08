bc7493ec10f2987f6d46dae5492a70fc64b0d7af
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:37:15 +0000

    progress moment realize depend region

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
