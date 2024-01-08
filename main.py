8bd11dc376226cc1c4fd905ae072a7bed81507fa
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:11:45 +0000

    breakup precise event custom only

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def count_characters(sentence):
-        return len(sentence)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
