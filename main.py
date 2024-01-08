236d76ad56327cb6324435185fc8f0bb6d5192f3
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:16:14 +0000

    load majority possession innocent trust

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
