dc327526b6eda5fedfbdeecfe2656d5d67db883b
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:19:26 +0000

    believe complete before behaviour shadow

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_volume(length, width, height):
-        return length * width * height
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def find_union(list1, list2):
+        return set(list1) | set(list2)
