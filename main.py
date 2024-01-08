a9df61542de487a6fb084eb5adaebe7492d0e1ff
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:26:19 +0000

    force possibly payment seriously message

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
