4575a982f21515d0ff33aa797e972341afd2efc4
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:30:40 +0000

    average letter unhappy collect although

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import sys
-def exit_program():
-        sys.exit()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
