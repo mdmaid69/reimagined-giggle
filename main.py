304bddb51826768033b6422e3bc2d6720d7c94b0
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:27:16 +0000

    pump either rubbish that reply

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
