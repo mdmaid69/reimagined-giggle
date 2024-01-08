2715ebf118aa9e5164b9c6029daa0631f213a337
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:56:56 +0000

    sick possible flat will stay

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
