c4ad575545e3817e7640ea0e553c117d3e289b38
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:17:49 +0000

    confuse coffee ilovedicks attempt thanks

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
