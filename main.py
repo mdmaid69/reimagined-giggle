c7da99ce4d81410295dee8a912b6cd7e3b6d58b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:24:48 +0000

    certain near cry drawing comeround

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
