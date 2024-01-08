67521f5d1733cb9e5fbc45c69b3b1731d9df4bf4
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:32:34 +0000

    tread comedown favourite total advantage

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_str(array):
+        return str(array)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
