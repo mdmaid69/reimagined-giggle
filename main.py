588259783e6f0b93d9c7a7b512042df21c6417e8
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:54:55 +0000

    chief region pause fortunate address

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import json
+print(json.dumps({"name": "John", "age": 30}))
