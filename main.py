ff77da4aa23773c5d0750fe47560e8c8cf714d26
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:04:36 +0000

    have wish during tall conversation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
