2d3509d7ea3d19fafe05ac0194bd2d38e9dad6da
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:01:22 +0000

    clean maximum drive few border

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
