cfc359a70b7901972eab8eb0a9a633362b8c641e
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:40:49 +0000

    board advanced risk keepup secret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
