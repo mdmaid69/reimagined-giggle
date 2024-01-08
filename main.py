42586486106ea682a53b2a8e9a1e77f5b100e888
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:00:13 +0000

    prefer walk whenever 565end good

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
