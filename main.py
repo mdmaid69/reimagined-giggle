ade29fc43d2bd0a1f22b6d8fef76b098caaebcc0
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:18:32 +0000

    breakinto difficult someone word culture

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 import datetime
-print(datetime.datetime.now())
+def get_current_datetime():
+        return datetime.datetime.now()
