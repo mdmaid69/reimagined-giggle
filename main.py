6db2eeb2823aade94b15fca681b497eb3b4f24f0
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:31:38 +0000

    blame chat sign stranger protect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
