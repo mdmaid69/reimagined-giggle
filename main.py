3783154e8b2ea645d92570648026db01cad9d078
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:03:08 +0000

    OKorokay arm Gg drive smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
