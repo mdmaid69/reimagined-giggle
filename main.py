d6621587ab647144e2f659ab9e07c94384f5e160
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:53:03 +0000

    press organize serious natural new

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
