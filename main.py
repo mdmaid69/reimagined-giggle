7658dfc48a658ae0286ae01f2e9c6427db59a80f
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:43:07 +0000

    arrangement question quality lower within

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
