44f7ee08b6854e5776601b49ccb6a4921e4e56ed
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:01:08 +0000

    near switch towards extremely fun

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_perpetuity(payment, rate):
+        return payment / rate
