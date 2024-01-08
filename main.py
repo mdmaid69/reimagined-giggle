c1712da3a205a4c17782a4e9db48b06113ea8434
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:28:59 +0000

    broken only dare dress contact

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
