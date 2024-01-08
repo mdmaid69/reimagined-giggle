a816f193fcf9fb680de5a62a61da8bef2feec7af
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:26:35 +0000

    inform sea scheme example keen

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
