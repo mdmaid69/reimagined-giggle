8c6bc5bafb73ced3652f311fee1e8dba556a68be
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:58:20 +0000

    damp right doubt sea classroom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
