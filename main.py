65ebfb679b7f08e0eeb8b961cea34205ea1a6685
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:39:26 +0000

    matter delight exchange smell care

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_acceleration(speed, time):
-        return speed / time
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
