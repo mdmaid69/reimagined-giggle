72fe9179ec53678c30c0a21013d3dd72bcd7be17
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:07:57 +0000

    brave cross sign company train

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def convert_to_octal(n):
+        return oct(n)
