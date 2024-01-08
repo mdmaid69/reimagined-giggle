7b90d1b82601a29487015d6cf5ed957df150b39e
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:07:32 +0000

    visit friend age fairly middle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
