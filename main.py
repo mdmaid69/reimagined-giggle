a211eb4d8038791ff838e1e4b8aa5ae11e9146b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:05:13 +0000

    speed tooth friendly stamp flavour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import re
+print(re.match("h.*o", "hello world"))
