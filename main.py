5f39382275a64a27da8c9e786d5b736ae3447a74
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:58:57 +0000

    alive Pp fail sing workon

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def greet(name):
+        print(f"Hello, {name}!")
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
