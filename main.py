4b91f2cbb61887704e1c5afbfcc793f5c092bdc0
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:21:20 +0000

    represent at sleep sticky shadow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def add_numbers(x, y):
+        return x + y
