6eb71b0735966a29ee4bfa5e50d2bbbca04112cd
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:59:45 +0000

    succeed pay fashion shoulder last

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
