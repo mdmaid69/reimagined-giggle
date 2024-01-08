98ab197907a2348d8b8d3abad4b08d2dabd745af
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:42:56 +0000

    tax afternoon regard brave powerful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
