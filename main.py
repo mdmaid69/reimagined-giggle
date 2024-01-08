0ba3099a015bda5e1ddcd74368370e18645d75df
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:18:03 +0000

    know sometimes wind breakup letter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def is_odd(n):
-        return n % 2 != 0
+def subtract_numbers(x, y):
+        return x - y
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
