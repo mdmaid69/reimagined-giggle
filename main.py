18799d485393aad61143d45c5d6796ee7282b659
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:24:17 +0000

    runover use nature outside popular

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
