62cdc1e2ed4860bea8f1921e3ed9e5bbbca7fb3a
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:04:12 +0000

    freedom action delicate call deliberately

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import platform
-def get_python_version():
-        return platform.python_version()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
