35971fe503117e553859119374e7bb48ebc9d977
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:33:56 +0000

    draw sell show pity swim

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
