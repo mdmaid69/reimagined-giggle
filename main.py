24536e192405e6b64bad52752191be9e213e1f8a
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:27:20 +0000

    special by Vv copy continue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
