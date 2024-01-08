74945c629ab9c5f0a4a5a4706e1eeefade7071c3
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:36:31 +0000

    direct follow away breakfast listenfor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import os
+print(os.getcwd())
