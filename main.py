c3f5d66555fea6c3f96354a68539ad4aa60a2269
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:51:22 +0000

    no attract low operation past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def square_number(x):
-        return x**2
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
