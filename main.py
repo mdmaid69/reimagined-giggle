71d460e988e731733db5d4e2ccc20c27fcbe4fb6
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:57:08 +0000

    no act hurt agree brown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
