f6d0a0919719928bbaf75afacd375afefbd7d239
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:55:08 +0000

    cover fault proud operate scale

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def reverse_array(array):
-        array.reverse()
+def get_array_typecode(array):
+        return array.typecode
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
