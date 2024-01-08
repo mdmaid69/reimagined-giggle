274f09cb4771309c5850acddf1f583958efba720
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:03:56 +0000

    further fail company face speak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
