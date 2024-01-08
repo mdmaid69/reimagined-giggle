0f9be1885f762517a34c01812e951cd47ceedfe9
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:57:53 +0000

    underneath shop mystery stick sight

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
