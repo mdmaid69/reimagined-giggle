4082a9dedf70d089f7955989e9e68df10a0f54b9
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:04:02 +0000

    light straight during exchange damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
