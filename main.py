085f400b22a4ac57cf50bba1de945ccd31dec06b
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:47:11 +0000

    official break Gg slice rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
