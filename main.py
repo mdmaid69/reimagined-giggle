ac6b7926e363f01d3e004ef2d9e975fa5ad84514
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:13:17 +0000

    still cheap crash health pretend

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
