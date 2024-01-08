5a9e98996e66ec5f2e68f4c88bd8e02891555a6e
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:57:09 +0000

    exam grade force crisis break

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def convert_to_binary(n):
+        return bin(n)
