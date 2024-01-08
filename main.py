a32d05d2e9e5ac370651da71ed09e1ab3b6b10c0
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:38:46 +0000

    board guilty complaint phone collection

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
