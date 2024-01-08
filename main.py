0cdc3162141882ce321d4de0079e68f9458e490c
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:14:00 +0000

    downstairs shake excellent under justice

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
