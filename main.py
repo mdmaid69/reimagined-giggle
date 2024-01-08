b13708c5fbdcf30684ac50e8d8d03630b3504e19
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:23:48 +0000

    thinkthrough thinkof meeting bet connection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
