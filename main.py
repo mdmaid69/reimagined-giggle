eefd2e121555ee980d65851ceb14ce60a87f9251
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:15:27 +0000

    many dark press sing match

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
