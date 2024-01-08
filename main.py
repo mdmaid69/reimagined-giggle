39c5bef2906c1c8226998357613d2bd478250faa
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:15:00 +0000

    air city exciting other off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
