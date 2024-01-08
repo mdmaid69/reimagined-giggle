0e7d3a03afb17055ac9dc146ca987e5e8409f79f
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:09:12 +0000

    hurt dish their touch pronunciation

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
+import time
+print(time.time())
 import csv
 with open("some.csv", "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerow(["spam", "eggs", "bacon", "ham"])
         writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
