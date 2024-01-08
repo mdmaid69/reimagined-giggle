18a5e30896f7f1b680013f7a8ef382bf70e174fe
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:36:32 +0000

    them rich which good curious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
