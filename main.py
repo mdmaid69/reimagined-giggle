74371f8d20543ebb562cb424e9c6c32f1e19b7eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:34:13 +0000

    office wire yourself let size

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_area_circle(r):
+        return 3.14 * r**2
