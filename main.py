f3ecc7fa8caa8eb80987091fe3f892243409ea87
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:43:29 +0000

    guard slide note otherwise measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+for i in range(5):
+        print(i)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
