ecd95e38ab70fe558fd01b3ab7e2b350242c6ff0
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:25:13 +0000

    emergency neither front past push

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
