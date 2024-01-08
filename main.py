8b80de39223f4ff2a4532994ec71c986d4fcb98a
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:55:27 +0000

    window Ww bored take surprise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
