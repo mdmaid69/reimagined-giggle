b9f9c7855adcfaae672f020bef1921e15d92be51
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:56:30 +0000

    fast floor estimate read happy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def get_array_item_count(array, item):
+        return array.count(item)
