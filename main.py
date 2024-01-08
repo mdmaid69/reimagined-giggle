f1c78afbced5273fe7ba0e28a9667213bd947ac3
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:41:14 +0000

    available practise include meet tongue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
