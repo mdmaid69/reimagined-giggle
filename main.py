167c59677501ce87a775f8e0132b5d7b4b012115
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:05:12 +0000

    steady dangerous today dull question

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
