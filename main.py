01acfeba72f3da9610d9f558531ee18a743ad0a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:14:17 +0000

    know among family bother trial

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
