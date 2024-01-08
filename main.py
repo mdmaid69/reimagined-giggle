3128b710901d5ed9db97f473795d7507d448355e
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:06:07 +0000

    pair careful zero occur war

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def count_elements(lst):
-        return len(lst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
