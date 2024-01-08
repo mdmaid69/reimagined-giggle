af964d9db2d18c6250bef4bc0a9e8166a3adb023
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:15:44 +0000

    rely clearly name breakinto possibly

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+print([x**2 for x in range(10)])
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
