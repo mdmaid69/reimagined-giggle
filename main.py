346effebf6bdec58761bd7b60a46a4871e1f9ede
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:08:16 +0000

    know past like later exhibition

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
