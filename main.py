019e75503618d0289b587bb46858cd3aacaf52f4
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:14:17 +0000

    result realize Ee reduction shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-name = "Python"
-print("Hello,", name)
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_array_as_list(array):
+        return list(array)
