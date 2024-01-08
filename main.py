8da90bfce4d9bf5aaf116d6d757698bcc84b5042
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:20:51 +0000

    possibly wide live runinto second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
