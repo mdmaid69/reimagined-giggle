21a7e11d74e5eb0d4ec156f857f1a0e35587509d
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:24:19 +0000

    point natural giveup work Ee

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
