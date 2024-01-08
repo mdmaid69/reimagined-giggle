71d79d4c8552d4300fcca14168d276efa36eaedf
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:39:12 +0000

    draw now cover wash accidental

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
