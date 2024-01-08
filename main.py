a8d222430083f9e10f186f8032f1ffb6fed42cc0
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:11:49 +0000

    below father old rise authority

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
