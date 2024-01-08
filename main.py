532af269491319d07f0573e32580c93b623c4c44
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:18:06 +0000

    lend area war flight programme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  def remove_duplicates(lst):
-        return list(set(lst))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
