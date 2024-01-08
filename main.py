a9f6b0172593f6b9dec084ef8917da9f2c1aa3c9
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:11:33 +0000

    surprised forgive doubt arm goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
