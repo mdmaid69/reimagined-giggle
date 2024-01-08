ad9b658556019296a44d27f93f44cddd87344b46
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:50:53 +0000

    clever branch firm stamp divide

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def count_elements(lst):
-        return len(lst)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
