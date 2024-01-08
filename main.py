6f1545d880f68ad2c4cc846508fc256f2d23fac2
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:29:18 +0000

    workupto yours dear priority once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_odd(n):
+        return n % 2 != 0
