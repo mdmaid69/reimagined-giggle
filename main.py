bae3830b31443b8eb80cd77dc5ab0aa22b7867cf
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:21:03 +0000

    voice trouble your wing among

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
