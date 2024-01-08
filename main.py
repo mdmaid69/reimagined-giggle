4b288a9a84661da1ab025fc2b849776854b1d731
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:00:12 +0000

    story imagine admit once relax

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
