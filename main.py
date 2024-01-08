8dd0c51f88b04106e320bef48674daa9cdcf0f1e
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:14:39 +0000

    every few test smell can

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def greet(name):
+        print(f"Hello, {name}!")
