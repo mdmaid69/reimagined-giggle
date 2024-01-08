de161b9c7bbbcc02c6e900dae0e048eff946eef7
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:32:32 +0000

    punish national stay pile chance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_min(lst):
+        return min(lst)
