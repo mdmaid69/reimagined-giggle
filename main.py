2a34b39bdd2aed25b1163851b1db9bbd70bc703a
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:04:33 +0000

    block room head question smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
