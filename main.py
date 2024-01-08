cebb9b8dc99867cdc3e84a827d1d862bc2aa0c78
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:16:18 +0000

    much unlucky fix wind secret

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_typecode(array):
-        return array.typecode
+import time
+print(time.time())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
