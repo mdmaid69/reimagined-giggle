e5248afe366e8e77d9933f0e0af1199eb23a3059
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:42:49 +0000

    Oo speech follow transparent opportunity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
