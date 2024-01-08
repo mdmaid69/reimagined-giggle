2096811e21febb650771dccc9a06398e874ee7e7
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:51:35 +0000

    performance complete reduction begin with

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
