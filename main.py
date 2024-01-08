ca1522a2563d17f84294c03a44ebf3014a1fe9c7
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:07:55 +0000

    claim company march advanced seem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
