bd7c5c47429ec27adb46c68af04483c79b08d1bb
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:20:41 +0000

    regret through safe fond rule

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
