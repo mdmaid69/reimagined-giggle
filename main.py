fc92f8a352afeadbd77ef6b182a667e1a3ad7713
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:20:41 +0000

    onto embarrassing feel carry duty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
