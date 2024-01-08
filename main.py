1243828e1165ea7c4f669dc4ba7be126b3432bac
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:30:18 +0000

    oil open past coffee wrong

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
