5eb33dc44fbd340e0f09c7bc4613dc98d7e68bea
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:58:50 +0000

    ignore various deep answer into

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
