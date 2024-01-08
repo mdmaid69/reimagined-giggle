abb625e8346c345fbff76ab38f9dda064a94642f
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:24:39 +0000

    whether negative long because splash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
