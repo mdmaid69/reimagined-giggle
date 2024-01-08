ada6b7a5bbf331c3dd2635170fc07d11053a94c1
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:23:32 +0000

    shine workupto refuse extraordinary keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
+def convert_to_binary(n):
+        return bin(n)
