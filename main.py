f120a6332ee2378a767fda1cbbc950ccbf186f91
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:16:20 +0000

    move outside criticism comein either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_base_name(path):
+        return os.path.basename(path)
