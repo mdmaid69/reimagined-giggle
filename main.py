1a54a17dc4076e454e50a72017219a35132cf753
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:26:10 +0000

    calmly cut except fact copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import time
+def get_time_since_epoch():
+        return time.time()
