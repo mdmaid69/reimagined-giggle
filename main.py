2858f18f9c6ab7c38d17d22523d4a88e0b33b6fb
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:04:32 +0000

    organization comeround speak under our

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
