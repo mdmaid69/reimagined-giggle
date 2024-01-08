3a7f5763f88eaba760b8428e88b1dd8977b6c3d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:09:42 +0000

    progress nearly comfortable quit peace

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
