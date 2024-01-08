9fc3eaef7ba37ff5f05ae552369428ec561ad12a
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:49:02 +0000

    wind save him ring theirs

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def subtract_numbers(x, y):
-        return x - y
+import time
+print(time.time())
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
