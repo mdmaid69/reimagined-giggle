692eb6f20df5629c66562033eb446da000cce4b0
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:17:05 +0000

    environment previous grateful answer keepoff

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def find_max(numbers):
+        return max(numbers)
