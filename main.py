a0eed6234af4f4cfc7310a863864f8f0086cd7e7
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:49:39 +0000

    wipe smile limit sink offer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def convert_to_binary(n):
+        return bin(n)
