9dd4a0378209af47467f9b0e1d7e9a68dd62609e
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:30:41 +0000

    woman upstairs guide introduce one

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+for i in range(10): print(i)
+import getpass
+def get_username():
+        return getpass.getuser()
