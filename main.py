292c850e56334bd6c8a6325ad1af55d338b691f3
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:46:04 +0000

    sell intelligent measure would during

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_work(force, distance):
+        return force * distance
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
