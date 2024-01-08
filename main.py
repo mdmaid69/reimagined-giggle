0ecb77d8c2748dc3f80d52a61af1dac3ae673a2c
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:38:11 +0000

    flavour now trick copy breakinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
