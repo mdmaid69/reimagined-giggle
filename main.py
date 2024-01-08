6b881b3c9bd4dfdb39493aa04ab0d7e57a0bb650
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:01:32 +0000

    cope turnover display clear nervous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import datetime
+def get_current_date():
+        return datetime.date.today()
