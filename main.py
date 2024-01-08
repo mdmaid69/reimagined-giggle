68c74af6007abfa9415f862fcc16d20d4df1007a
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:30:11 +0000

    neat exaggerate pupil loss piece

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
