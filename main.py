cb45aaf48cf71e31b8d53aba22b0a8676ca771b7
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:32:41 +0000

    declaration possibility show rain mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
