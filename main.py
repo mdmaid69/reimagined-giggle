dc4db7f3dfc69cb3b5a211f3908c6a8e3b6cbfdc
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:27:12 +0000

    tell post her fierce tone

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+print(sum(range(10)))
