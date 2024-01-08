30a8ae8b5199ef25ff775da43b6aa142af182104
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:59:26 +0000

    Jj getaway press knowledge post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def convert_array_to_list(array):
+        return array.tolist()
