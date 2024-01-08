c2f4ba65f069448dbf45b01e65cfec3a1d7b5481
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:56:41 +0000

    everything instruction public rush actually

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
