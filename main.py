c457c961fc5bc92366f71efc358f38f21e099127
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:45:28 +0000

    wait nerve complete hole age

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
