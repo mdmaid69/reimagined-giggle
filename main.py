8e9916906feb7a9011ba697851166407b85eb0b0
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:36:53 +0000

    juice serve marry pronounce no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
