7eed1b4eb03b967e91932c1d077139976f3d1974
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:19:53 +0000

    post harm trap warn emergency

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
