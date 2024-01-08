8f9720e26a8e8a4e10b343efac9d3df6c5bfea96
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:29:34 +0000

    zero crazy them middle reaction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def convert_to_hex(n):
+        return hex(n)
