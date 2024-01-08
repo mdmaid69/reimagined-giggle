baf8d5f2713682e30944501e1921daa326b0fede
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:55:21 +0000

    fair company charge objection 565end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
