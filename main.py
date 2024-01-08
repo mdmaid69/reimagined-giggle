2ac27d882d798e4442cbef35df0e630268cb6c28
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:14:36 +0000

    extremely confuse interest thing translate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def divide_numbers(x, y):
+        return x / y
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
