3178d7c759a4eef7151ce0969849c95a0434471b
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:39:20 +0000

    nerve regret letter sort correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
