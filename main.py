549213a6673ac68976ddf425d3c023477637c7f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:07:25 +0000

    degree decide ourselves lucky substance

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
