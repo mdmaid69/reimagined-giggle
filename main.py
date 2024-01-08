b5a8266e19f17969538f675516a7b8be72861e88
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:28:20 +0000

    unfair basic extremely burn solve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
