d33d3709ffee3f9bf636d6420921095ac742fdab
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:40:29 +0000

    fly receive month lesson refuse

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+text = "Hello, world!"
+print("Reversed:", text[::-1])
