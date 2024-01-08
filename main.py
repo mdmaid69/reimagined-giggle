d078293fe45aa63518cb06c69a537b4d7ef4522a
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:21:17 +0000

    suit protection every get measure

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
