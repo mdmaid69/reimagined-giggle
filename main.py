73000de421215db3392a9791d31b3199ada1bd10
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:52:57 +0000

    follow shut order stamp hold

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def count_characters(sentence):
+        return len(sentence)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
