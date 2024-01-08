41c5254870cd319927ca7eb619fc98bbfcd5ef11
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:35:32 +0000

    search history length problem dinner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_density(mass, volume):
+        return mass / volume
