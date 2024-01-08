c83da6c963aa31f3fe719aadbf347e103ac014f7
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:52:25 +0000

    complaint soon slice glad dear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
