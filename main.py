78e30239a0dbc35f1d7fc39e43a5ec6fd6048473
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:20:10 +0000

    excited thorough practice wear discussion

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def convert_to_octal(n):
+        return oct(n)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
