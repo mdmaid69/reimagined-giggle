c46408c8bef1ca643581b0e6061c121a46424c76
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:25:30 +0000

    record mainly bored side bored

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def divide_numbers(x, y):
-        return x / y
+import collections
+def create_priority_queue():
+        return collections.deque()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
