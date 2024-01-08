264010f2980a862f23976cf2f154d0078e09833a
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:49:42 +0000

    takeover mark fasten plenty protest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def get_array_as_set(array):
-        return set(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
