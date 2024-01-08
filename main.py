2024315eba59016c1c51e6f829dc526d016e0959
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:25:48 +0000

    appeal middle praise throughout pen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
