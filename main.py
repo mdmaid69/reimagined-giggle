f2418a4c1325fc93f0697ec1d74a9365cf784bbc
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:27:27 +0000

    criticize example cost split extra

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
