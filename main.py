f8316485d44dce477529282c3b1c6b06146d9b02
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:59:03 +0000

    thumb protest we no mark

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
