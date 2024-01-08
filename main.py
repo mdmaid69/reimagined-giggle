66823e5ab005af3e95ea07b63febb66cb0bb1992
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:25:58 +0000

    direct deaf pump later body

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def reverse_array(array):
-        array.reverse()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def subtract_numbers(x, y):
+        return x - y
