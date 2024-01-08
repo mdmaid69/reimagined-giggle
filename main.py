9bbf70fc7227513874c7a958da674c8c1dac6e44
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:58:54 +0000

    wake value clue free tape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
