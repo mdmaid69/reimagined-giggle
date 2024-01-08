1ceb2ed8eb1875b6b503fd8ecb37f141392de882
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:33:48 +0000

    typical everywhere beside attitude getup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
