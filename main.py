cb69e5b92dabc1516153e51ec522fc4b8486e5de
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:53:56 +0000

    science discuss tell last correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def add_numbers(a, b):
+        return a + b
