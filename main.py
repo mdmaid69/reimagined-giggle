edfe07b7845c3ef0d493aa30e3d806bee0fa6ccb
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:43:01 +0000

    extra plain say calmly band

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def convert_to_hex(n):
+        return hex(n)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
