cb743d42a0dfac6e6800de36f6d5561eede96265
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:49:21 +0000

    nigga arm here listenfor because

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def change_working_directory(path):
+        os.chdir(path)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
