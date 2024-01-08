1e527a28284c126193b907802dbdc67dcdefd164
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:18:29 +0000

    apologizeapologise science criticism upwards need

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
