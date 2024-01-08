bda892a4ec776b8d5e1fa4633055a7c09d6a96cd
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:03:25 +0000

    help protest his consist replace

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
