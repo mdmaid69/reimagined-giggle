4f8bb10fd11d08aa5ec56da1aa78aab13f775207
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:40:53 +0000

    breakinto close turnoff dance clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
