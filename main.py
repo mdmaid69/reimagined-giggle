31ea6a2872c4e5b770d1109a43ce8f3c76674f11
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:35:54 +0000

    keepfrom here bus that water

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_density(mass, volume):
+        return mass / volume
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
