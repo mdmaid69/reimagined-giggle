273cb812fa54a5c0d2558bb843134a3fcc30631c
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:00:16 +0000

    post completely unhappy lost squash

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def subtract_numbers(x, y):
-        return x - y
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
