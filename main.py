5dc3e428684930f3c46bd8d9b9eab514cdc977f7
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:20:44 +0000

    bridge beautiful cry preparation place

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
