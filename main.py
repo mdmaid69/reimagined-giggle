80de82513579437cfb527c2fd2a5be9dac62fe02
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:36:03 +0000

    right display turn soft type

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
