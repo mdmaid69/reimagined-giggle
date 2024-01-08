07b914640eab7fe545a41bd5adef64a6fbab5474
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:15:57 +0000

    near pleasure book tend school

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
