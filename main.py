e60b8e2c21d06f44a279c2b0b98be3ceb371b206
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:04:14 +0000

    check person undo schedule cry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
