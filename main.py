73daabd920f480630948cb6c92d3756b134ce27d
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:44:14 +0000

    house invitation 571enough relief calloff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_list_from_array(array):
+        return array.tolist()
