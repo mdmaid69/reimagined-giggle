653d0778aa30a0861366dba709fc64784e51ff54
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:07:14 +0000

    square related shadow deserve file

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
