11b056873d8fd24e2e5d651ab841eff2fc20c1a6
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:18:29 +0000

    buy modern solve home regional

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def remove_duplicates(lst):
+        return list(set(lst))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
