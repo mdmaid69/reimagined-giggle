1ebe83f94806d61fb981cd45cdf0b87156c25cf9
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:24:07 +0000

    just concentration nothing substance power

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
