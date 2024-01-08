fc86da5cdae6419aa9062adfaa4a256941f3e354
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:00:44 +0000

    load perhaps sensitive slide work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
