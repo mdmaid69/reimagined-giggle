581717545a75ecc615585889b364589afe30fd2a
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:07:56 +0000

    570enjoyable obtain think human stir

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
