21632d5b3c3095074f6599c7fc3e29fcc424ef81
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:59:11 +0000

    wave of who attention scared

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def add_numbers(x, y):
-        return x + y
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_speed(distance, time):
+        return distance / time
