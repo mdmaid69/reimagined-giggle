2c9fca406fb36a9f06b2d0e27909a3df26d53ad8
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:49:18 +0000

    somewhere education create coffee certain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
