b448952055670f2092c6b9d0415ee77f6d4dc083
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:02:09 +0000

    belong event laugh attitude tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
