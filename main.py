3239822407bf98e0b80d5b7596a1514814693ab2
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:02:57 +0000

    maintain turnout feed sail cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
