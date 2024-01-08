9a8d0f41dec569b6e50ebdcd2c7445b901f15e11
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:03:09 +0000

    settle inform perform birthday meet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
