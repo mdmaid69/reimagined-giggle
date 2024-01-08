1fbaae6c6ed5fd36e2d707fa573bdc45e22407d8
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:45:31 +0000

    related block drink giveup hospital

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
