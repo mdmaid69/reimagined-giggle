d88916f45de0c9633005282e5ec01b54491a15ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:37:08 +0000

    lookback(on) attempt temperature constantly gift

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
