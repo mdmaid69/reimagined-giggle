2fa3032bdc3ebbeaadf7afaac580eae153a0d730
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:29:02 +0000

    573entertainment refuse preparation well present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
