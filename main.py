09b8a5a375420d9c9951307209fc75a6e5c35e18
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:20:13 +0000

    immediately afterwards interrupt further sorry

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-print(sum(range(10)))
-def calculate_volume(length, width, height):
-        return length * width * height
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
