ca810067bbcebebdc055d3994c6deea6ef089c2b
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:12:43 +0000

    apologizeapologise later return need possible

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import os
+print(os.getcwd())
