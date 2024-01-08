c152a303ace0e1b916fe547b6d4e0c7aa58b655c
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:16:09 +0000

    school later provide mess possibly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_counter():
+        return collections.Counter()
