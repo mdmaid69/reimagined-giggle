67fd0a08148457e588490a72a614d786c095fbe4
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:26:43 +0000

    sound bored have broad office

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_density(mass, volume):
-        return mass / volume
+def count_characters(sentence):
+        return len(sentence)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
