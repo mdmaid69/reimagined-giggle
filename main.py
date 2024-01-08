280356f2bdd1a0b7ccf2c824873b0b44bcc12f4a
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:46:29 +0000

    sport major limit Pp serve

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-print([x**2 for x in range(10)])
