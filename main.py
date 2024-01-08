e9d5729762ce87c981e55e6a7a6673d28dc6eb62
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:20:47 +0000

    good instruction sad 568energy secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
