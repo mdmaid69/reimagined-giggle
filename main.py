45fba5cd44c0909699cf9e74b8b3d941c0f0919c
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:11:12 +0000

    compete distant neither post capital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
