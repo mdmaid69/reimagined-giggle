9ae999935b6f7c65438c233a68788b83c16a2f21
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:00:45 +0000

    impression practical shut winter face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
