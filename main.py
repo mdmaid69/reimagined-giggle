8ab8b19220199837c601cf7a1907362d4f47e44e
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:33:26 +0000

    snow person cover lock feel

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+i = 0
+while i < 5:
+        print(i)
+        i += 1
