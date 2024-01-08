129a6d130022519decdb6ab62331d65c9e973343
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:32:21 +0000

    mainly little approval issue oil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
