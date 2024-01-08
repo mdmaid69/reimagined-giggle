908af9967a1933d5c35e659a14d009af0c2eb3db
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:55:41 +0000

    over nervous somehow movement in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
