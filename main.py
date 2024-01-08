7ab0f3cc8578f02bdab4c20db9590f92ab48ee5a
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:04:30 +0000

    on expect hit sky myself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
