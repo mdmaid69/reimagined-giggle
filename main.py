f4366349a957402a7e36d394cb22778b1c55bd3c
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:25:46 +0000

    time anyone uniform sting quickly

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
