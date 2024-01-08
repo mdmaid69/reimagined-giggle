f60e18286cbc316f1a0f463e310e23f550675011
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:11:14 +0000

    quantity total advantage fire repair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
