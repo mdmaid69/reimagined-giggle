9472cc91eb6e996c09411d4cd7f62252287dc1ff
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:23:53 +0000

    worse over either later lose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
