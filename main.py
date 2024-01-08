9440a790936c91bbb5dbb2c04a28b80cc807e4f8
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:26:26 +0000

    clean border yet size note

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
