6fda6b40db703c23fbdb89d7a4a0696dde003f2c
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:05:24 +0000

    key comment introduce path impatient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
