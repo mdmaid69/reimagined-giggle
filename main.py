4fc4844a9d7f25ec8d085f45bd34b49f8eff6029
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:37:04 +0000

    region turn deep that turnover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
