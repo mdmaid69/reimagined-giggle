136dd06accc70e97af4e35d95580a10b7e252afb
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:08:42 +0000

    beat accidentally value rise scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
