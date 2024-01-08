479891c28be792c859c6cf9a6c0fbe63c4eb583e
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:57:51 +0000

    narrow takeoff sing unless down

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-name = "Python"
-print("Hello,", name)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import numpy as np
+print(np.array([1, 2, 3]))
+def add_numbers(a, b):
+        return a + b
