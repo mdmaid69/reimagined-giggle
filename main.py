f02513ef9dd59ea96a04a3d6be50c5e8a803860d
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:46:52 +0000

    peace clothes how squeeze 565end

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
