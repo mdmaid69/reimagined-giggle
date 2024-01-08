188925e5d06e65742bedb901bf253d8dcbab22da
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:19:38 +0000

    have patient circumstance responsibility information

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
