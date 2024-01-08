34f6b9c572ea712d59e6d22c6d625de065eb1562
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:38:35 +0000

    change trade goon seat past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
