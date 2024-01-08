4c4153afc76d99af68411887de13244eb36af82d
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:02:18 +0000

    free murder lower because original

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-import getpass
-def get_username():
-        return getpass.getuser()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
