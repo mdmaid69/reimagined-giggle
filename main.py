3782d83f7cde26a5dd99e30db605eba529acafda
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:23:46 +0000

    hope bottom must pattern argue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
