fc2e106a33af3804783bc1df40f417bc2d384ce1
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:43:33 +0000

    pump this favourite explanation city

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
