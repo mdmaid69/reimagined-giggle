f90b89d4710ae9c86dc19db2219d95c07bbef2f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:37:47 +0000

    describe software support refuse approve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
