3396a09bea65bc2551516e81a1a050f04c679526
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:56:45 +0000

    thin pull forgive field level

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
