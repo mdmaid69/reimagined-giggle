02da5871724f9ebdd6cb4bbea80965a47dd7aaab
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:17:19 +0000

    tradition sudden sky name respect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
