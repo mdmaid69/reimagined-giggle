db13ad3f2b22d2373ab5f8613402772fafe77b07
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:53:34 +0000

    exact 566end comeout affect left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
