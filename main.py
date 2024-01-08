fcbb9582ffdf0561beb6f9a86c8ae419eea996b4
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:36:16 +0000

    juice habit heavy telephone personal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def clear_array(array):
+        array *= 0
