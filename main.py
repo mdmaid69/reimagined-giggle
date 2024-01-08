b9aec935f9316932eca0d15a92c095d31f18e17a
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:25:19 +0000

    any taste middle trick abroad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
