a2099c72b775151e132975191a113f9357b1ea87
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:11:33 +0000

    possibility top there confident combination

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
