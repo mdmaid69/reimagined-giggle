170ecca85467e78535a30b69bc938da9e2cfc092
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:17:54 +0000

    reaction dinner yours snow possible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import array
-def get_array_item(array, i):
-        return array[i]
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
