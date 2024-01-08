adbfaf919bb5c0a71ec5259e57c537de9565161c
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:25:24 +0000

    interrupt save bill bank getback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
