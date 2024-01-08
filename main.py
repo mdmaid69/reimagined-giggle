0074090622a8f356fa3848016584d36b8a8861b4
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:03:07 +0000

    noone conduct fault base thank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_power(work, time):
-        return work / time
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
