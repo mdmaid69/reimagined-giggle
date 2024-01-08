ce690e86b6c5692daaf7fb424e3e024cc7aedaca
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:37:50 +0000

    pattern path nose church read

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import json
+print(json.dumps({"name": "John", "age": 30}))
