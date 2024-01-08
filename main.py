fbfc8a9ffb8b7bb0a39837c866cd0573594995ef
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:57:32 +0000

    even patience bet threaten mad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
