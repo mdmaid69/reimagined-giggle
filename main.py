7a32945b4ca2ea5e1991f7f7d314bfb2df99b930
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:53:51 +0000

    claim week light public sudden

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
