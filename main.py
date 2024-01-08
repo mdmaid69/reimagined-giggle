01b647d09e59401d7bbbc67e227fddee27b1e24e
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:00:48 +0000

    lose criticism suit date suffer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
