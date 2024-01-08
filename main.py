26bc2e5c900694898fc494f24193facf0c138e0a
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:05:07 +0000

    level storm blind idea press

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
