1f5269cd9cc52212add3b06006452b74ef9593a4
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:26:18 +0000

    slow dance communicate variety guard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
