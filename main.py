b00090022443c57cd79c8b7d8ede819cda80a267
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:52:29 +0000

    impressed bother accidentally refuse comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
