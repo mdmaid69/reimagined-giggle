194572555fef85afadc0c1299d3ebff137938c36
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:22:55 +0000

    ring opponent sort old remark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_user_string():
+        return collections.UserString()
