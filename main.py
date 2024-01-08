1c4ae9eae878ad56a40fcc30b7ac732f3af69515
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:31:31 +0000

    at wish hold much attention

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
