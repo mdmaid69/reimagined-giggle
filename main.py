aa92aba64f4f33aa1d618e2c1064fbf9f2cb0e00
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:29:31 +0000

    calloff afraid slow hold marry

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+print([x**2 for x in range(10)])
+for i in range(10): print(i)
