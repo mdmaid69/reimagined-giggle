756752cb799e2cf5687eb90b76b6d489c3a29d15
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:32:46 +0000

    behind price pupil possibly base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_max(numbers):
-        return max(numbers)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
