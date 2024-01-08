4d2074f7870ebee8af1fb8ca6fc31ef082a98e80
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:34:10 +0000

    thumb mouth load effort why

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
