f99fb1411c179e4b8427664ed6e828323808a8d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:28:56 +0000

    drink plenty rob bar key

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
