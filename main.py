9fd4281ae34f639cb49b8216311cfb3e85ffddad
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:23:54 +0000

    thin crisis murder skirt strike

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
