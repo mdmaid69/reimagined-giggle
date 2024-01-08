240bba49678b768d6275309cc6694d56a81d0ce8
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:14:02 +0000

    broken little perfect contribute embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
