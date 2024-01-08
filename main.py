5ffc3edf4e3783aef574baa02fce92df65323c41
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:53:40 +0000

    below wherever whistle goaway basically

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  def reverse_list(lst):
-        return lst[::-1]
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
