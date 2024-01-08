263bfaae03daa9c836138f3f23e50ead2d5980fa
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:14:24 +0000

    fall figure or talk amount

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
