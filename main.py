5aae624a638b1f9918723a49216483a7fc34f547
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:24:01 +0000

    comment criticize responsibility wipe complicated

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
