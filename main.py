f5937c0fb28e720fed207cf8739700b9b6105a7e
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:05:19 +0000

    either compete tidy fact pollution

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
