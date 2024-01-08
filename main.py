bbc2063b37734e3bdda235f1c4cac809180328f0
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:35:40 +0000

    turn last price start test

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
