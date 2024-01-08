a7180be77921663234f95a4bf87221251f4660ba
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:21:51 +0000

    door instead gift public represent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
