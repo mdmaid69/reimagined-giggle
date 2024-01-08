671f26ed0eb9f6e148db76160ab13a96a0ccd105
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:34:00 +0000

    quality swim waste will laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
