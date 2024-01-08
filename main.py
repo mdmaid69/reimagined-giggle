22097f4b8d69c57bc55468b0eb51b55e024ea732
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:45:02 +0000

    national considering arrival time party

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
