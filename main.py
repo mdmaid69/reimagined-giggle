2aeb95da84fae89b205a0877097adc41ed4460ad
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:37:01 +0000

    cost declare city workupto approval

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import random
+print(random.randint(0, 100))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
