046c8a4135883f2f963954f70f0015894d16ae4a
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:12:58 +0000

    scale content separate during long

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def square_number(x):
-        return x**2
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
