ebe75dff510e3dac06ad14cad83a1de4a1985079
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:06:40 +0000

    part thankyou tip confidence criticism

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
