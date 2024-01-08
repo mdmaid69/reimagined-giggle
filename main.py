a1065f0eb9b1ec5ec429cfa928a583d38262908c
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:44:12 +0000

    embarrassing pick damp goaway they

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
