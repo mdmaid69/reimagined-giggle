5e48a96fc3cd786eee612052bea9418ce9cde238
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:15:46 +0000

    wash unkind justice replace love

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import time
-def get_current_time():
-        return time.time()
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
