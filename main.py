e0751be6bb976be74f18fb180f87d36c122ea8ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:45:05 +0000

    typical tragedy mail turnout must

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
