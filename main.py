635310277096c431d5a150fbc654959ae6d92b1b
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:36:45 +0000

    wander accordingto fire past sudden

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+print([x**2 for x in range(10)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
