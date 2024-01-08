4646dfe90b8eff41433bb122becb56220f296bdc
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:14:44 +0000

    hope gate movement term instrument

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import collections
-def create_queue():
-        return collections.deque()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
