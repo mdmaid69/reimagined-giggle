6779032ef4c2d367e223d25b9ede672f639f77fd
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:09:39 +0000

    her television throughout border communicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
