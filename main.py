28cff4b49790f8596913314bfeb93b6cae6c76cb
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:48:48 +0000

    turn weigh club room its

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import collections
+def create_user_dict():
+        return collections.UserDict()
