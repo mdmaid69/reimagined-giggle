4f3f0451c597437972c9a6e04b778a783db91d50
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:05:57 +0000

    wide lock trust run far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
