bc9fbe435b0a03b610e6fc921886bd256ca2927b
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:38:23 +0000

    mood temper common arrive claim

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
