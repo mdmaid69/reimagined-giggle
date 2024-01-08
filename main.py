08ea085791646e5d6b3a1cabb7670ea5a9f31bda
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:59:48 +0000

    party wealth something violence left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
