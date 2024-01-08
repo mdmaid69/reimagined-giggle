6820f9cfd0211af8a403f42d94d452b9abbc5ab2
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:42:02 +0000

    comeup disapprove car callback serve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
