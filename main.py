1aef24015c56607af561fa88ba246b4739881aee
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:55:47 +0000

    never active block bother obvious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_typecode(array):
+        return array.typecode
