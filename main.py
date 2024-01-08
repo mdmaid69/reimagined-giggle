bc617eb20a542c9a660d8b7a553e5061e1ad5270
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:23:15 +0000

    dare tour close passenger statement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
