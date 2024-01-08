b728d70c470fcc0175e8ce13636777d6477603f2
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:19:22 +0000

    knowledge skin hungry direction image

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
