dae82ec41b9aea20193f4590943d8e660c72e380
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:26:30 +0000

    mainly admit firm unusual party

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
