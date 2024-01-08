4505e32cab3592d27f3412bb6047cc13f351ec64
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:39:59 +0000

    miss gradually again claim pay

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
