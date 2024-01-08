6eeb0cce0b79a97741cc34ed50326ba5baa7d08c
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:28:36 +0000

    trade over process nerve save

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
