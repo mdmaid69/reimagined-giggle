8e6b7bea91986050b84c71fd1145c710acb02121
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:12:16 +0000

    willing demand increase confidence personal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
