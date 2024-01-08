75916f096f1686815960a8cb1d877789c44475d5
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:38:32 +0000

    gooff male divide he like

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def sort_list(lst):
-        return sorted(lst)
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_perpetuity(payment, rate):
+        return payment / rate
