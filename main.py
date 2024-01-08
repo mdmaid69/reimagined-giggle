ec3bd063c6f1ef608a76fd14d665a4d9f20e4043
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:23:03 +0000

    suit extremely any fit nigga

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+print(sum(range(10)))
