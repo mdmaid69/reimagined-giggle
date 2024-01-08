441b2df9891ae06c4ab99115886541a4ccd867db
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:51:10 +0000

    pile could programme demonstrate funny

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
