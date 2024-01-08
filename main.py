6967b919103ff9c63da39185738cd146cd5c7d23
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:19:07 +0000

    yours advise goout read towards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_as_frozenset(array):
+        return frozenset(array)
