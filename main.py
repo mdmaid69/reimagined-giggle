85c5af90b7c2dd339758620702e84f86962ad5c8
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:10:44 +0000

    Gg look exaggerate minimum rule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
