aa14a63a31aabd442bb15a74e50b937635b4fe86
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:17:55 +0000

    myself port ordinary high slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
