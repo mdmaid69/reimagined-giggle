458dd25d098c3fba7e11289c3a56b8b2b6d4a9c1
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:32:00 +0000

    run see mean Jj hospital

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-for i in range(10): print(i)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
