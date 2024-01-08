eb630a584f53af9f7c9325d29de774eef9a6c336
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:14:53 +0000

    luck satisfactory spread value still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
