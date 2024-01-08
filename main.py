5b7d0dc34d5fff9141e444c1831cb7ac54bc2fa8
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:27:16 +0000

    point afterwards stamp charge young

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
