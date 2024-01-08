8bd5cfe463faa54e7740392047530e4937e17823
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:33:06 +0000

    home situation reduction risk listen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
