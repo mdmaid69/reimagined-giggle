52bb1e7bbce27a15ac99ae5a11fb17bf2d239dcc
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:50:04 +0000

    file back this nearly corner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def set_array_item(array, i, item):
+        array[i] = item
