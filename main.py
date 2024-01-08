4ad0ab8210e58ae67bbe76a0f72ac344f3571b52
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:00:35 +0000

    address abroad party call married

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
