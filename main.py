6b277036b44d93ce40cd3b348ccf078143f07ff0
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:50:12 +0000

    different divide forever fire holdup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
