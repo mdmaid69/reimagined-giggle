fb4ca867550fb1eebf362714a79fb4285f1bd030
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:04:19 +0000

    switch push tap solve probably

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
