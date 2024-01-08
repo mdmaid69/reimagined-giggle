4d52936fdb8d8b9d273a901b0b94c0ae5dcd6a71
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:24:51 +0000

    sort church still figure accidentally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
