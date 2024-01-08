b989c8e7035cb25639fad3fb697be29e85c8865f
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:48:55 +0000

    blind paper evidence thinkover note

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
