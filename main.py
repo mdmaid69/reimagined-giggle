5674bf0b471821b06377a6be6f15be409ae3d892
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:42:22 +0000

    fall lift dress nose floor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+for i in range(5):
+        print(i)
