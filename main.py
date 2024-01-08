d06ba9bb3512dcfcc5d93d4ed530274aa698a40c
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:29:49 +0000

    outside admit middle style private

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def is_even(n):
+        return n % 2 == 0
