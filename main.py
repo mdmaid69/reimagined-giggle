f529f9a4e2a929d37487bdaef6fc87d0db050fad
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:57:38 +0000

    major worried sensitive open steal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
