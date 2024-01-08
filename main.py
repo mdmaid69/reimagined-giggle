fe6887f27d5ab8aa0a1f2e60d8a93597be366d23
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:58:26 +0000

    ordinary storm on cry flight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
