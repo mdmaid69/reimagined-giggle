9cec6522bb64e27b43d2a1ddbda264b226098ec4
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:49:24 +0000

    coat bag nature conduct comeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
