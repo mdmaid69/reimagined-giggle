f2e78ed133e4f3eb563de9aaf540a926fa63aa56
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:34:29 +0000

    border priority nigga lost disgusting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
