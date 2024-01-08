b160ece2cff724cbdeeafb8679f30188f5a8060a
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:31:23 +0000

    expect 568energy brush agreement reject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
