a8c63f06811bac573729ac35ee649495c5368888
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:05:33 +0000

    drive mine turnon arrangement happen

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
