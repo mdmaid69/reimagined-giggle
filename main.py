75a096da968fb655a0ffd8244f5f5f3583f3bfd6
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:50:24 +0000

    when cinema interest fashion bear

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
