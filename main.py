6612a3dff637575abcd885fa120326af69f014ea
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:10:31 +0000

    path sort her dick delight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
