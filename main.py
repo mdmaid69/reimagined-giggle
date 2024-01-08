112d69788222d457451f0ea9d22d2a8d375fdf58
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:59:45 +0000

    backwards get upstairs anywhere split

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
