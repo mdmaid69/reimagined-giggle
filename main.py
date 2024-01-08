8bd6b3aaa4540d8f119ccd8d3f77b4280cad4c65
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:18:42 +0000

    advanced precise getoff beat door

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
