b8fed807cee8d3ab6a2a483a2c9aaec1c5e1585d
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:44:05 +0000

    recent wide narrow after tidy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
