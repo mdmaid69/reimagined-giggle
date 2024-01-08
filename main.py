3f1215b168d0684aeefd3ab31bac94f4b145ad5e
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:53:38 +0000

    machine hard information nerve little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
