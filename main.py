d06c71ba0d127ddbe1d10ce2c0065a55d1127944
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:39:57 +0000

    raw available best its matter

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
