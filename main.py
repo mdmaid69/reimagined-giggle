39e17876299722cb91563452fbd17bbf5830cfbc
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:40:20 +0000

    care either right upwards but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_power(work, time):
-        return work / time
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
