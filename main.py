836ff66c638f0af7a186f37579090f3cbefcda59
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:09:12 +0000

    giveup proper stranger never trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
