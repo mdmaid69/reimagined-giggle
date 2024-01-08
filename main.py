eccbbaea77e42467c5a0f480784e6f3a9e9b062e
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:45:31 +0000

    ticket such instrument amount lonely

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import math
+print(math.pi)
