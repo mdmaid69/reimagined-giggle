2cc130e7e57170c72b440d56174f1c4ab9e03651
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:28:03 +0000

    man calmly keepfrom interview past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def convert_array_to_bytes(array):
+        return array.tobytes()
