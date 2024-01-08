a0c6410ac0a31f5b970fe3fcc40557603af8ee12
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:59:32 +0000

    glad shame brown concern produce

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
