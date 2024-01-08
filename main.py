917e90bea08489b0dcf9871187e9be1ae15ad2c4
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:57:11 +0000

    bang when tragedy model holdout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
