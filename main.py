c61a7d934046284a112a7b3f4a140597a53def3a
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:13:11 +0000

    mess pain respect above wheel

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import numpy as np
+print(np.array([1, 2, 3]))
+  def reverse_list(lst):
+        return lst[::-1]
