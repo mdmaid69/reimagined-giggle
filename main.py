b65f2e6b6a55c9b18f44a47f161d3c83c4ec63dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:18:35 +0000

    smell repair yours petrol unit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-print([x**2 for x in range(10)])
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def calculate_area(radius):
+        return 3.14 * radius * radius
