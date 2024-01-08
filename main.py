87a7e541fdb035a312b31521d3c2fa0c9c9e576f
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:53:02 +0000

    bring arrive off run pile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
