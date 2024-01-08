39207fed73a7b855b457802ef17ad6bc6e351825
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:32:18 +0000

    remove warm influence phone talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
