cba32936379d71eaf41a9f3e3f9143fd5f0d75c0
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:25:53 +0000

    runinto break recommend thanks market

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_pressure(force, area):
+        return force / area
