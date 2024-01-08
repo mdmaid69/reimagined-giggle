7aa7ffab0b586c0b6d40cd10bc0368855775544d
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:24:27 +0000

    exit across rarely trip past

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
