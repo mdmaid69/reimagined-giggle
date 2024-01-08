f97cffe517e2a476d06a4289b280fb0941620ba7
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:22:40 +0000

    need lower face nice correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_density(mass, volume):
-        return mass / volume
+def square_number(x):
+        return x**2
+import array
+def convert_array_to_list(array):
+        return array.tolist()
