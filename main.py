88fc9f71017d92ec88bf5c4ccdae7bac738d20eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:32:02 +0000

    block material achieve region back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
