ec19b155e7eeb9e4d3606c1dd31034a36c172b04
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:46:27 +0000

    task snow near contents lookafter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import array
-def get_array_length(array):
-        return len(array)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
