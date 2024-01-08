0671cd905b93e92bcd8e4e436888c380e227fb79
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:35:42 +0000

    get section act sensitive family

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import os
+def remove_directory(path):
+        os.rmdir(path)
