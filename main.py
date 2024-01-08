f14486c2ee885c7af731cef10a997a50361ec0c7
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:54:00 +0000

    raise short shop regular embarrassed

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
