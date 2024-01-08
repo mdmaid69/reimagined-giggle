4a0b36bcb3c09fe9d2685b6af3ed5c4806962b22
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:25:13 +0000

    no performance change unfair offence

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-from collections import Counter
-print(Counter("hello world"))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+for i in range(10): print(i)
