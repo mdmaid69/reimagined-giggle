f48da49b03e84956cfd70c34c6f692cb71df002f
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:07:10 +0000

    taste safety cook goout slip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
