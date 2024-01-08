020e8ef29cb5e5a4967667535b74570d35ae6d9c
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:58:08 +0000

    slip inform majority back tooth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def convert_to_binary(n):
+        return bin(n)
+  def count_elements(lst):
+        return len(lst)
