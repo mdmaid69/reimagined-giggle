240e0afc31681007379f20a0ff54239d60163152
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:00:52 +0000

    passage respect along curious write

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
