2f3b996e25c9abd2bfa97aea8c20a669ce760e05
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:21:04 +0000

    solve identify supply crowded original

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def find_max(lst):
+        return max(lst)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
