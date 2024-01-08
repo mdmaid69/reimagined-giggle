ce5d90dd7fa9d0ba9ac4f49d39dc7bcdec90d8ce
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:23:30 +0000

    family pipe whistle glass talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
