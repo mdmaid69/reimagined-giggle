2efa0e5c3813f5af98cfeeb8f786e47f8b5fa3c3
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:26:20 +0000

    tall exploration plenty mean probably

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
