f9e75bbf43c78ce39702aa3f4b44429de9eb7265
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:27:35 +0000

    confused deliberately circumstance pay oil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_mode(file_name):
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
