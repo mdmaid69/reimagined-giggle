a02906682a7d186b4595c00f899b44f2323418c3
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:42:32 +0000

    down classroom vote comment touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
 def calculate_force(mass, acceleration):
         return mass * acceleration
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
