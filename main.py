8ab1d88163ef7a69f5ee7dc8e11fd6e275d5086d
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:24:36 +0000

    hollow sing comefrom goodbye break

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
