d0846467068e7dc9638caec9fcf6948ef50051b3
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:41:37 +0000

    forget narrow commit always announce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import collections
+def create_user_list():
+        return collections.UserList()
