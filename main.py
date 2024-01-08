df9a8439526af77648d74c69c019b82b10db3573
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:40:35 +0000

    underneath square more meet fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
