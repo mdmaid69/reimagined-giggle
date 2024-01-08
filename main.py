38eaaec7c11c3cf752631737a72384da0df32eaf
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:14:58 +0000

    through underneath bridge top represent

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
