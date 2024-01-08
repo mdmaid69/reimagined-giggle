45bf37b43a0abf66398d6e776d0a3b8817975d48
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:14:44 +0000

    welcome serious seem habit declaration

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
