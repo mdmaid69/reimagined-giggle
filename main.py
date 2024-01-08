41216d0d01472be263f793b8f803928a97c00254
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:05:44 +0000

    history blame anywhere majority thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_as_repr(array):
+        return repr(array)
+import random
+def roll_die():
+        return random.randint(1, 6)
