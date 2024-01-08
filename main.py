3ff0b813a4ff100d245cd3eba2d5301166ce45a0
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:54:29 +0000

    culture suffer exam choice off

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
