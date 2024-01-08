73b8614c8649a6fc1ef96c22b3c2ea8e1629fa08
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:21:50 +0000

    would comeover path boil great

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import re
-print(re.match("h.*o", "hello world"))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
