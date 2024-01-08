55bfa43c39325f8ef634b8097e2a6bf9c6a803b7
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:48:30 +0000

    art harm dish sport height

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
