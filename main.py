66eefa7b8c3c6ffad38e81afb429539132cf5031
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:02:46 +0000

    only soon who report computer

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-for i in range(5):
-        print(i)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
