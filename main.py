7d852ee8fc5d1bc07bb39f7313952ff64c49f1e7
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:57:45 +0000

    hungry necessary early after trial

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
