caa6475b9ee8fbc72f5c306b19c4b215398aa2f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:23:29 +0000

    offer formal below callout exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
