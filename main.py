d3b2a15d8646149d62641bc53101d3605549a841
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:01:48 +0000

    spring shout stamp put factory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import array
-def remove_from_array(array, item):
-        array.remove(item)
