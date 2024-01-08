182cee606e881f8a4d38b42551bfbdffd8bcf2fb
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:52:21 +0000

    by warning godown wind getout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_power(work, time):
+        return work / time
