cee64dc6cb7a16a829aefc86f5349dc63013ad11
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:43:13 +0000

    proof hear provide storm nurse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
