d34ac34fba87270977d7b1302ad5c3b292f6435c
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:44:40 +0000

    low Uu hurt touch discuss

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import time
-def get_current_time():
-        return time.time()
+print(time.time())
