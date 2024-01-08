f4d8bb189cbda1a32563244146cc07546991038d
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:53:17 +0000

    fair disgusting difference talk keepup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
