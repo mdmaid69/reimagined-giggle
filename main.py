92c290cd1f6eadd769cb88046ef7c122eda11d65
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:56:47 +0000

    head lay smart dream remove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import time
+def get_current_time():
+        return time.ctime()
