50fd729f904859eeec66d0a1a34ed16af0cda472
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:50:40 +0000

    takeout exaggerate deliberately tone hurry

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
