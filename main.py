9ccaca0bf28e0241d40ec508713f186f87f3558f
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:30:03 +0000

    shall what drawing bringback shallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
