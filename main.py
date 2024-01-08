a8921cc10a26cd01b3b0ea19bf29c796f32211a2
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:23:19 +0000

    tap fast message prefer occasion

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-for i in range(10): print(i)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
