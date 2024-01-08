9ba088a083dabed0def6250f912ba39e5ea57fb2
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:45:06 +0000

    urgent popular result film method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
