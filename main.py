009bd9cf1647c6133455f487be9262211aa106f5
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:09:31 +0000

    basis comeoff name transparent strike

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import re
+print(re.match("h.*o", "hello world"))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
