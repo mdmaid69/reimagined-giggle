dd1835835bc0b552c61fa05dc8c51a6f13da88ed
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:42:31 +0000

    sorry circle breakin serve clean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
