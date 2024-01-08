f6ee06e57e4e55556ba9f086d20bfd5e19e32feb
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:22:16 +0000

    position help confuse bright part

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
