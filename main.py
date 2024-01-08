03a779d885011034f255511e37c6ce15926d3107
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:04:14 +0000

    long one stay afterwards confuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
