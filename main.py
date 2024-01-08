132a60f4e21ceff99b6b03dcf35d44733fae1052
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:51:17 +0000

    safety set circle compete principle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import itertools
 def get_combinations(iterable, r):
         return list(itertools.combinations(iterable, r))
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
