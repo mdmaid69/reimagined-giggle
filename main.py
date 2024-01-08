eefb292464ccf3049c7ac7fb3ddd23dd7d1f013c
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:16:39 +0000

    by piece cheat face moon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_array_as_bool(array):
+        return bool(array)
