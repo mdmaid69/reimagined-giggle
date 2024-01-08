e7b75b61e1924f54a8aaab00815f0ab98bf8132c
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:58:00 +0000

    bend sweet bin hot used

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def reverse_array(array):
+        array.reverse()
