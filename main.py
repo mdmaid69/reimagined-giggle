8af32cfef1e8671226663fc1f175682a65fc8ce6
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:27:44 +0000

    tour pattern clever swing fat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
