7e6b7d17efd70fe39a9611572cb7b800af11235a
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:32:04 +0000

    pretty announce test Mm traffic

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
