3c7155715214ae7a6ad76733cf25a714501fdd55
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:04:29 +0000

    wrap moral love turndown 572enter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import collections
+def create_user_string():
+        return collections.UserString()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
