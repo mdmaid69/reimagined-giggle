f346696524a3deca8280de371eb3b6f6bd438400
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:52:02 +0000

    conclusion welcome breakdown strong sail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
