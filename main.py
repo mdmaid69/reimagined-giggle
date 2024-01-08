a6f1a5e675528032ab3903eed1c70ee933ae7b17
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:32:35 +0000

    roll workupto change poor world

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
