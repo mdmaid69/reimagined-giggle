5d4be66ff2f7480b339d1507623c5970b2cfc21a
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:10:44 +0000

    apparently set cough long accordingto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
