d82822447fba21cf14748dd9364997e27c80c8da
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:08:27 +0000

    wind mark soon could major

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import os
+print(os.getcwd())
