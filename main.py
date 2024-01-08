67023b15c7cc58428fa7963311f732e09cf9617d
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:30:31 +0000

    emergency dear error we threaten

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
