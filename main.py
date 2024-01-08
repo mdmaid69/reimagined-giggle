a86956d7e0eba0e7e5de56cc3614b46ac588c0ec
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:57:26 +0000

    come through keepaway shame himself

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
