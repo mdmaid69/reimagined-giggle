a4a1e353c94792532e84ad19853ba03a3876b8be
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:23:59 +0000

    down lead century conclusion within

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
