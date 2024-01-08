f8d9109cd6ee6fec4ccf466ca9162fe4ad18de70
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:43:19 +0000

    score beyond car reply project

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_density(mass, volume):
-        return mass / volume
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
