e86f4363a3d34ce26ed6e5efd5129897c04b6d43
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:36:27 +0000

    destroy produce shoot connected bend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
