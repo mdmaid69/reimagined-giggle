4a7942ef36fa8882a46aca542f787443dac65b34
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:14:23 +0000

    respect spot permission street cry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
