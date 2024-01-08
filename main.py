bce3dcc99ca83c7e407c92b65f4d29bd73ac1a11
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:44:21 +0000

    sentence disappear available line check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
