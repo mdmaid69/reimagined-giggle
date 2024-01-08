b7c0d826739403f5d28379ff837a1b615510d9b2
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:26:51 +0000

    rescue art lead dress open

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
