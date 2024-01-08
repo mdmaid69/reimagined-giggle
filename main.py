b5e1acf161ba6e6ce5ffa3913546668591a1887b
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:04:03 +0000

    mess tip curious Gg bet

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
