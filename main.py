3f14fda83c1f21151c9fbc1f69b9cc146560977b
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:21:47 +0000

    spot character takeover lay method

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
