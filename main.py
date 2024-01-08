1bb624e559bf585101d61361ca3567f8df1306f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:58:10 +0000

    capital very sweep surprising command

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
