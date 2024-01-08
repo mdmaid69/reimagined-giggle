1b0e6cd9b422f822aca0225f456a2db3c76481eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:17:47 +0000

    taste 179bite mixture impressed protest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
