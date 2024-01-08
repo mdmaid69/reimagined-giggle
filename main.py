7b13e2da487164877acf38233424765e19248908
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:39:30 +0000

    improve matter reaction card picture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
