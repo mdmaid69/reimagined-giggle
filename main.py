7206d55cbf618d43aa2ba4eb406cf85c6b0875f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:26:20 +0000

    demand snow official sink tap

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
 def calculate_interest(principal, rate, time):
         return principal * (1 + rate)**time
+  def sort_list(lst):
+        return sorted(lst)
