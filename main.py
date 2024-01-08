5c75291244e815857167b7c2c8b4f46157b48e18
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:50:33 +0000

    spite history fierce precise fork

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_priority_queue():
-        return collections.deque()
+for i in range(10): print(i)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
