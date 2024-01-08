a307cbb7243bb6c716925a7d17f21fc0ee4c1b9b
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:13:43 +0000

    grade unite mistake accidentally but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
