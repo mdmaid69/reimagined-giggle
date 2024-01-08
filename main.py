5da85578f0ec59679dc6d4edb77b4074f96edce7
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:08:49 +0000

    safety position ours bound week

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
