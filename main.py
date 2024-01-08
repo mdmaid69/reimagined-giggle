984d1369ffa4b36e1a5ecfcc4875ee76d4cff6a1
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:28:11 +0000

    note typical speech lesson react

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import platform
-def get_python_version():
-        return platform.python_version()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
