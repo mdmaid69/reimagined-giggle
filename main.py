8de9f5c5f3bc50960eea4f5731d040907b71595e
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:32:07 +0000

    fact suit stamp tap nonsense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
