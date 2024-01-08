b1acac80a6f06617da80db7ab072a72ba0915de4
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:33:05 +0000

    hate careless takeout division leg

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
