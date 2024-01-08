898c2366f90c8c9528acb915f42e580a38d1ca9f
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:04:46 +0000

    return forward comeback week look

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
 def calculate_work(force, distance):
         return force * distance
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
