8c77ae44d36e4b270e82e42de389b592201ed95b
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:05:37 +0000

    consult stir warning bar schedule

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_perpetuity(payment, rate):
+        return payment / rate
