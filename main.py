1940e33d93b0eb09f855f3b3e043966675ea1a40
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:03:15 +0000

    division software wrap fast pleased

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
