bcadb78d52bda260eb2a8925eef785181196d608
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:05:01 +0000

    exhibition fit clear fond wake

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
