e019a56c8dfc00ae7d4b6e9a6bf8f53c80d3e913
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:34:39 +0000

    your church material journey you

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
