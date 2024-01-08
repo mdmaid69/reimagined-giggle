ee2bf2ff8df42cfd00718016c738bf0d7e1d078f
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:57:21 +0000

    notice and lock unable pile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import numpy as np
+print(np.array([1, 2, 3]))
