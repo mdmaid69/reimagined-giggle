de0dc19dbb2014b5ae3db3cd10c46c60ad777e1b
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:21:49 +0000

    instant floor yes urgent half

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
