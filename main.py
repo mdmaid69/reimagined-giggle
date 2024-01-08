ff6247141a4950b999c55304a1da75054030badc
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:43:24 +0000

    one improvement top rough pen

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def reverse_list(lst):
-        return lst[::-1]
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def sort_list(lst):
+        return sorted(lst)
