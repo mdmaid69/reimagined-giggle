1ceb22dca73fc2b2f71df6826d6ae8ffb3562331
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:54:55 +0000

    he range afterwards please promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
