6fe748290f1774ea5af93c9949925163216fb429
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:56:06 +0000

    accidental score tread quickly hardly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
