2926982c87c970f2bb5134d1c380bb8abb30605e
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:39:30 +0000

    kind wide direct talk argue

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
