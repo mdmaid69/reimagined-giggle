e70f517b7aff904b0b53716cd01c464afeb9ea46
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:13:05 +0000

    intend ahead frightened contrast freedom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
