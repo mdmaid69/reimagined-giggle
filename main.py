00bd8bbd5cfccc699a53adad95557a36f764567e
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:38:08 +0000

    Mm tired prove name may

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
