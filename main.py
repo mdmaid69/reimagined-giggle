59039835fb859b25d7ff38139a1010151a8af94b
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:59:22 +0000

    what alive tonight display rather

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def divide_numbers(x, y):
-        return x / y
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
