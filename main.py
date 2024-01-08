35eb07a5c722bf027889b1cd365a16c27e2bedc0
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:49:09 +0000

    material insist count copy row

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
