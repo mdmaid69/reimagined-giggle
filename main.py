659866ef2d7a910c6406e3b8f142060e2ad0439b
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:47:37 +0000

    reason decide game air minute

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import time
+def get_current_time():
+        return time.ctime()
