a14d1ec3c31ef9bfbeb48572b5719bfa47780165
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:10:04 +0000

    nature loss once now switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
