1b31ac7301db43df966b32d6c66d419728bce1a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:47:25 +0000

    draw goon raise or house

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+for i in range(10): print(i)
