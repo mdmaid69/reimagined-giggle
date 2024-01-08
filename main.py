92c916e8607d34e683d0975f3388bd836b210fcd
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:31:31 +0000

    pass improvement stir sense lucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
