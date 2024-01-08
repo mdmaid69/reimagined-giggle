978b6ed4bfdb7e544cc5ae7343ed9a7d69fb04fd
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:06:05 +0000

    message split unit sort thinkover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def convert_to_octal(n):
-        return oct(n)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
