5e8459305ce4054ac2d9732ab4de65bfa82c0ffd
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:47:53 +0000

    road bend weak list anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
