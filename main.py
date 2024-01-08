54b0a12ad7a5a473cde49c20a013d6eb52407a98
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:27:14 +0000

    scene job understand healthy practise

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+print("Hello, world!")
+def find_unique_words(sentence):
+        return set(sentence.split())
