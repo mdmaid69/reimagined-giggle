aa04c5f34126190cb6818ce78ee293b836866200
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:55:39 +0000

    weigh narrow realize speed tonight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
