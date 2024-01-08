a0063b8a815e2c889ae9360962f9137868db19f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:22:59 +0000

    total discovery comeover fit would

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
