09ddbcbd85fee5892c52124c6a6b2bc354d2b10e
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:25:42 +0000

    tool blame consult noise rain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def is_palindrome(s):
+        return s == s[::-1]
