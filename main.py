a1a33fe5e190aab26f626c57298433f20e2d01c9
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:33:23 +0000

    catch gentle play amount forgive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
