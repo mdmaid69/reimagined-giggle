50235787e30b52f61a2904fde167c7a0e6f19fce
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:33:32 +0000

    meet middle patience situation please

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def is_even(n):
+        return n % 2 == 0
