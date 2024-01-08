51a7f5e89fb177faa93a4649f877b92d476bc3e1
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:41:21 +0000

    want wire dance behind whom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-print(math.pi)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import sys
+def print_python_version():
+        return sys.version
