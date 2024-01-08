04b8d259562359e2379a68b24a02cee751165559
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:13:25 +0000

    importance complaint cruel compare accordingto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
