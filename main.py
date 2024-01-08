61b7fcbc63d0dfcd9ac28d1bc64a64358949c402
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:03:30 +0000

    ours wait scrape recover somebody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sys
+print(sys.version)
