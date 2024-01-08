d6ffa016d61b822ddd43b5fc7ba2715754fb3253
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:37:52 +0000

    bear lock Cc announce giveup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
