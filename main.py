61f3ae79749743ec408b87b4cd8ce2a9923bf063
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:50:18 +0000

    error slightly complain wipe finger

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
