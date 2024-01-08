3e892de48ff78b9acc2576e93fd6f4e5ed282305
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:11:21 +0000

    call clever actually now himself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
