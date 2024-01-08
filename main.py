f14997a388f6aeb90425cf618e8076fdeeb9ecdf
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:43:54 +0000

    through you pleasure enthusiastic back

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-for i in range(10): print(i)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def is_even(n):
+        return n % 2 == 0
