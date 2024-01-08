75c3948381670bdcac7becee190349d2c499d3b3
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:39:30 +0000

    wet fail imagination part minute

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
