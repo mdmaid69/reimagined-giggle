cd51c624a64ad68ccde4cff36c9269b346a83fea
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:40:33 +0000

    advantage store experiment live example

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_user_dict():
-        return collections.UserDict()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
