6af1d5ccad84740351e64f7037dab285776e847b
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:49:59 +0000

    centre break fight work duty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
