e5791d2a561a6eba88a5a61b1bb14437d3c53516
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:39:45 +0000

    desperate terrible loud thinkover become

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import os
+def get_current_working_directory():
+        return os.getcwd()
