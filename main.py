d6f3f6b962cc5b7d47a4a8cf04305cf214cb0369
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:15:27 +0000

    impossible last night inform towards

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_size(file_name):
+  def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
