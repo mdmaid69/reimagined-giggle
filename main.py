682d08f9c91663e3a9582a2db9bb00dffefa5748
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:31:08 +0000

    cruelty part pleasure love survive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
