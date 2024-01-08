d72ec5862fa5d1d5d9889abd83106afe4185e151
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:48:18 +0000

    trouble objection today fat cum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
