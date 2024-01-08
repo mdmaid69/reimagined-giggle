057a17f39f67798662757dd2b4c2e41c684be7ce
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:54:50 +0000

    meaning ours describe blind lost

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
