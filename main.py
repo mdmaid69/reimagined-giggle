8f2a85b3936807f0c048b5a703403181bf7a3f79
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:17:34 +0000

    among explain loss home accidentally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def square_number(x):
+        return x**2
+import array
+def set_array_item(array, i, item):
+        array[i] = item
