fdfb55cb8c7f4d77b2d8e07804d476d4cb712fe4
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:18:12 +0000

    best squash live step fork

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def reverse_list(lst):
-        return lst[::-1]
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
