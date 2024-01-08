4cd1f1f118dacedfa3cfd90be757cbe232363652
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:23:05 +0000

    entry find Mm whole neither

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-print("Hello, world!")
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
