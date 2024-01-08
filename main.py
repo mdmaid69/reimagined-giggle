ed2ac6104af542fc77ec198257e266331108f68f
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:52:37 +0000

    present time since takeover single

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def get_array_as_list(array):
-        return list(array)
+  def subtract_numbers(x, y):
+        return x - y
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
