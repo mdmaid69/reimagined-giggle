d9a5df3d29fb8f47a46a01746b044f52b452973a
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:12:09 +0000

    best any disappointed exaggerate punish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
