56baa5c302786e8b86de8ae71ddb923a52ce3d5e
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:02:55 +0000

    on home authority free her

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
