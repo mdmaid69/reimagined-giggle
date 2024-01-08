60b3a170a5c912eb355495c13c81e4d4ab49519a
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:16:25 +0000

    spill shallow great charge possible

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
