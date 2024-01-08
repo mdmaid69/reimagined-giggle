6545ad71ae84b6b8c39eafbb81c3bb8508ccb404
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:50:01 +0000

    method type scrap actually ready

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
