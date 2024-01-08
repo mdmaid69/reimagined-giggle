3e0268eff4b0b577d77a574da5c39d38fdb94337
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:30:05 +0000

    sudden car concentration curious content

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import sys
-def exit_program():
-        sys.exit()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
