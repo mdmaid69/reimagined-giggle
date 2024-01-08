339f6ebd0a490626f5c950e53a3e5f9ce2b336d9
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:20:39 +0000

    now related squash raw send

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_queue():
+        return collections.deque()
