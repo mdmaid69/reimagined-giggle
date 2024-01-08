9f2e0bb1e50e23e492fee474853b3ea7afc91080
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:20:53 +0000

    temperature far trap create if

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
