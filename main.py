96d62d8a3559e56215e6579c2a8b426aaff9c44e
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:18:43 +0000

    afraid Ee quality near he

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
