92c1e20a311327fafcd64a441105d19dde8fabba
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:25:53 +0000

    geton any breathe bush hide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_length(array):
+        return len(array)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
