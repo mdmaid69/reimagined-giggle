d8f6b0bd3e1523afcfb3bfffbb700d497bda23fc
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:00:02 +0000

    near he criticize suddenly life

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def convert_array_to_list(array):
+        return array.tolist()
