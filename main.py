0019cbcdec28c74f08b56c9db30deb09f80d99d0
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:27:46 +0000

    phone rely motion kill post

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
