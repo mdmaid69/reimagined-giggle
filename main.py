172ffeb60d5ab1c633da6a04441f957918b4c704
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:16:34 +0000

    another thinkover wind hair glad

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def cube_number(x):
+        return x**3
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
