c74e414b34bd075b1e38f23c1e9f4b2fbb85cf84
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:13:25 +0000

    public amount far bus together

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
