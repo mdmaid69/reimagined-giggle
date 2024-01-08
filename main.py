c07e3992cfebddfa4994b538dffddb7aedfe06ad
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:07:04 +0000

    instead drink side begin glass

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
