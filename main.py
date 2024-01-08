16d742dd25f35281dba912c7492d1b6f47b43521
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:20:45 +0000

    wall instruction profit Ee tight

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_power(work, time):
+        return work / time
