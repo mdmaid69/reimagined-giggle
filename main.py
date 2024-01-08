a0f4af64a7d842be18585a912db72d166bc550b4
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:14:59 +0000

    accident powerful over shock only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_work(force, distance):
+        return force * distance
