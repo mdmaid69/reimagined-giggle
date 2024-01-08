a3b37734ecf6a1d6ba1a9bd32c9a59a6ffc451e1
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:56:19 +0000

    control tired equipment shelter public

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_density(mass, volume):
+        return mass / volume
