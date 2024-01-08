f0c2239117673a214bb1a6c264ce6954335b0c48
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:50:16 +0000

    blind wrap fold walk everything

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
