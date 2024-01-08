1e36552625dc3201f54d54e684a64c0a33ddac7c
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:35:12 +0000

    remember mistake clever remove stare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
