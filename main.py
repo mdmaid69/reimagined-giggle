3a7dad082e7ad51e23577592d143e4580383ce5b
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:36:22 +0000

    branch top collection owe have

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_int(array):
+        return int(array[0])
