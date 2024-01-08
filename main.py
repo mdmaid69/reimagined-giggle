8c5f0f2edca025403d4d9f1063c9566e62d0cdf4
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:36:02 +0000

    relation maintain limit lend little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
