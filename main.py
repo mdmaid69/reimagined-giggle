dab7f71ef7fcdf3aee1f52f97c923ce1c9a34573
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:32:21 +0000

    exit underneath play arrive imagination

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-  def remove_duplicates(lst):
-        return list(set(lst))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
