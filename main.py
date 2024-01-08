8bb176c66482b85b467b71780f7fe2f774acb06b
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:22:36 +0000

    measure tape rely could recently

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_arc_cosine(x):
+        return math.acos(x)
+import array
+def clear_array(array):
+        array *= 0
