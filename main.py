7e5272b39b72a3100dacdfd6bd8bd5296d1c1b27
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:53:46 +0000

    authority statement lie game spare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
