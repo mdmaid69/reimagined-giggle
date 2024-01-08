b7b377c5081013bb5cb20818cb147e219f4c528e
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:47:41 +0000

    develop getup narrow properly plan

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_power(work, time):
+        return work / time
