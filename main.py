50858de34249c9edcda6335c17c8af82306c1993
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:55:19 +0000

    yours miss obtain dream finally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
