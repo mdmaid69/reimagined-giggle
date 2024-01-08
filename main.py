210ce7cf375db27f7b93165fbeff22e38c024712
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:41:47 +0000

    too equal in train shout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
