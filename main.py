4285b2e6e690c7f1c2c0a6e91c210e8e9e41cd4e
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:32:06 +0000

    deep order worse shine continue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
