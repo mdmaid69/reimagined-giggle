9b9522d9b1023538a57aff55406470b6a8f5a224
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:19:29 +0000

    still minor apologizeapologise charge remain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def subtract_numbers(x, y):
-        return x - y
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
