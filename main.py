72cbb77f5526059b7698b27310d1a5c78054d814
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:54:16 +0000

    conduct contents behaviour contact replace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_exponential(x):
+        return math.exp(x)
