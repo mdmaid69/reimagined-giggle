194964a1244a7a2ad5d22a4fef4be47ceac9d97b
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:06:47 +0000

    congratulations key sense back pull

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
