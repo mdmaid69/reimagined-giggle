39600bb00ad1be63f07dadab2fb77070815d529c
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:40:38 +0000

    handle measure lock month drawing

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import logging
-def log_message(message):
-        logging.info(message)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
