48dcd5d0be9903bf6a3ca27db1b3d6d8b9438e1a
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:24:28 +0000

    567enemy smell spoil occasion across

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
