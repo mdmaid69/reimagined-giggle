148e2db7a07c076a738386fc512204efa47b72b9
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:44:53 +0000

    getback shock impressive play Bb

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def square_number(x):
-        return x**2
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def calculate_perpetuity(payment, rate):
+        return payment / rate
