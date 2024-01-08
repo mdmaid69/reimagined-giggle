54f4982bbfb17f81027503353a3f200954c748cc
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:18:46 +0000

    argument motion beginning justice test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def get_array_as_bool(array):
+        return bool(array)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
