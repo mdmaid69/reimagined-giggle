d6c65f4b4bbc44bc13e1db7548b8149c39de595f
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:00:55 +0000

    usually more warning lot floor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
