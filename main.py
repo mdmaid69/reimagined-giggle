56afbfc8cd4fa8c85c1e136b75539d3ce82477de
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:28:12 +0000

    perhaps illness same defend warm

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_average(lst):
+        return sum(lst) / len(lst)
