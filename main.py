da58e696e5a513eb17bddd438681a6032e96fa28
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:07:58 +0000

    make sort standard direction dare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
