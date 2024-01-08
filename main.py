7f988b4a07e17094f2125aa1069cdaac75677c40
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:43:30 +0000

    minority story mass recommend little

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def get_list_from_array(array):
-        return array.tolist()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
