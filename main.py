27b86636298a985ed9a7391b580c2a8ca492e6b9
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:21:53 +0000

    estimate impressed wear bite bright

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import sys
-def print_python_version():
-        return sys.version
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
