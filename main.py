49ef9fd3b21757ef6ff798771c275dcb65618ca6
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:37:15 +0000

    sick stop responsibility hard fast

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
