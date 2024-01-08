eb765c0eb5c739f7568eb6fa15bb87678e8aaf46
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:36:31 +0000

    circumstance sing world someone will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import os
-print(os.getcwd())
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import array
+def get_array_index(array, item):
+        return array.index(item)
