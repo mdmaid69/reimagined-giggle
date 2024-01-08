49c118a0f91c09ba9910e195b70bd5b794568c8d
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:39:34 +0000

    idea union spot familiar ride

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def calculate_speed(distance, time):
-        return distance / time
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
