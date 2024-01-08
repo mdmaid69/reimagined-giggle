58fd631432f081b9bcf3b6fc0ee09391753c0936
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:23:59 +0000

    cross fear tip different basic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def get_list_from_array(array):
+        return array.tolist()
