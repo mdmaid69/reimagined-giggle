0689fcd269e79aa25ce46e5f6020b90203820f6c
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:20:20 +0000

    uniform complete separate exchange turnup

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  def convert_to_hex(n):
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def convert_to_hex(n):
         return hex(n)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
