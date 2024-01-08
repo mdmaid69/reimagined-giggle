280c8ecb684358be5331870fa739654c07763879
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:47:18 +0000

    connected content disapprove official dinner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
