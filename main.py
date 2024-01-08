0e039245fa8a1418437f4f7ef2527f3694cb02eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:50:38 +0000

    open wind permission worry accident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def append_to_array(array, item):
-        array.append(item)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import math
+def calculate_exponential(x):
+        return math.exp(x)
