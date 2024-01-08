472d26a6461d1ead6c14bd602bb9a09bb9b4f5c8
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:56:39 +0000

    ashamed awake draw miss impatient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
