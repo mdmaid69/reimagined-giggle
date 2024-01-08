109562cc7babb3d2a62a28f5e0c94a49feceffb6
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:18:12 +0000

    square breakinto many peaceful somebody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_power(work, time):
-        return work / time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
