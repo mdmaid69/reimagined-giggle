6cc077ce245208fbe2abe842ae0c26ff3a50c9a3
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:29:38 +0000

    behind place key store available

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
