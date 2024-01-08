5c62a160e5f05abd006fd6c61ebb24056b301d1c
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:50:57 +0000

    embarrassing comeback yes discuss protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
