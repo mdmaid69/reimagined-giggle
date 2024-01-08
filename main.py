17de8245a44f3d6aa2f194a2c9a152b09845c7af
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:40:35 +0000

    public wander dangerous opponent mixture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_error_function(x):
+        return math.erf(x)
