e504490fdc73e3c092dfe5f12350ab1652d42383
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:22:40 +0000

    reply fond break to giveup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import time
-print(time.time())
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
