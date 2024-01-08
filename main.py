7992156ef4acba9b8c6fbcb8512c0faedd58cf9c
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:43:29 +0000

    vision development music middle comfort

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
