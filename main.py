5958d52a498c654adb00b233263912e2ab9af35c
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:17:55 +0000

    advanced waste it catch pronounce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import platform
+def get_python_version():
+        return platform.python_version()
+import array
+def get_array_as_int(array):
+        return int(array[0])
