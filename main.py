fa0b71a2b47381699257b1751ae8f8090ae8b3aa
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:08:24 +0000

    love memory circle ago suppose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def convert_to_binary(n):
+        return bin(n)
