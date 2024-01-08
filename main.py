6b66d20de162cffaf25c5697456e7b82726879a3
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:06:20 +0000

    left strict medium properly intend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
