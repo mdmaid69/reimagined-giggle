4e48ee08f197fef3b737787890e67a1fc6ab7eed
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:40:23 +0000

    by land hollow deserve regular

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_working_directory():
+        return os.getcwd()
