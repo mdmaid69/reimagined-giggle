a886b5edaec9b1f53d4e7722867678d90fc2f3dd
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:42:24 +0000

    block trick high Ww type

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import collections
+def create_user_string():
+        return collections.UserString()
