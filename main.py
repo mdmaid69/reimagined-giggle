e08a42dec6f656d89ad21a599e1343448f04fca0
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:21:44 +0000

    reply bang expression process moon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def find_min(lst):
+        return min(lst)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
