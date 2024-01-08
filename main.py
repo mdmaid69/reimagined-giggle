ad8ebafa66c10c2fd7f524c394db052eee533450
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:57:21 +0000

    advanced rough help support male

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
