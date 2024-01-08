b7258faa5365b3fea42401393d13d5272ab00360
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:36:50 +0000

    too medium succeed rid search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
