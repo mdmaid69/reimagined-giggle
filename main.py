238364523a8bf84827a4d8cb8f9f5d02e4b6b302
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:15:33 +0000

    dress ability suckme prefer accidentally

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
