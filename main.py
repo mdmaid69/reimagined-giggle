b043d8bc997624b42b3f32850caf3faf420c3437
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:11:17 +0000

    difference weak visit factory contents

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def get_array_slice(array, i, j):
+        return array[i:j]
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
