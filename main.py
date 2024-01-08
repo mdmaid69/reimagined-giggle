4832632a377ef290d01b089ac02bd0c18e888403
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:41:49 +0000

    manner worst wash other reach

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def cube_number(x):
+        return x**3
