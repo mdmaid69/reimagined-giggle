d162877aa46776a76c38e0f13338426d633e610c
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:57:49 +0000

    relative build clean damp lead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
