0223b8b67bb8848c6db94341a7e29df4cc652671
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:29:04 +0000

    comfortable direct naturally government swallow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import time
+print(time.time())
