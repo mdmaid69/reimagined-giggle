4728a08e9d19e34dcc7416d0650b3dc258f2e8df
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:04:58 +0000

    connection warm come anywayanyhow tie

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
