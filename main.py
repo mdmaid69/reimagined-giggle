58b15ee1a1f8ad4aac8770956d084c80eb520121
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:08:13 +0000

    wild available nerve declaration blame

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+text = "Hello, world!"
+print("Characters:", len(text))
