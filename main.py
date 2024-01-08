e1e8e799cd5491506829af3d2c8c6c65c579ee86
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:49:12 +0000

    serious everywhere imagination explanation neat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_directory_name(path):
+        return os.path.dirname(path)
