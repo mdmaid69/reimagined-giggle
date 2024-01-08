b228199eb51a119d87ff47e72822f5a9d02aa499
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:35:07 +0000

    mess runinto behind transport deaf

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
