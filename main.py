d777e4f534212569f19affa28fdfa4378d7ec238
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:47:57 +0000

    start ready description progress matter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
