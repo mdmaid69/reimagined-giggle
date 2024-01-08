18756a27dc93d482c4f8e9d60f275f786260b9fb
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:24:07 +0000

    big swallow pronounce cheat somewhere

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import time
-print(time.time())
+import array
+def reverse_array(array):
+        array.reverse()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
