06f991c5187d902245527375ccb868ea65bd5b64
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:13:20 +0000

    post expect describe key keep

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
