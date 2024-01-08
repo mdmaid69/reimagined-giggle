4b515c77b36bd486107d8ec5ee5c0f6caf2e63a2
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:12:55 +0000

    hole female tap gift meeting

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import tensorflow as tf
 print(tf.__version__)
-  def count_elements(lst):
-        return len(lst)
