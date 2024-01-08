85dcf0398027463d03a9bcaca2c6587c65de5c1f
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:46:30 +0000

    minimum slide party arrival this

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_average(lst):
+        return sum(lst) / len(lst)
