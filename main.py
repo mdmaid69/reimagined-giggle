82ba2c248797db0ef9efe44dbeab80c96c43af80
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:56:24 +0000

    wave 573entertainment persuade single bend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
