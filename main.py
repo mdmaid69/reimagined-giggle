7e72b51531abb1bd11ef7d9f5030426ba5a6f386
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:11:08 +0000

    possibility design leg suffer see

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
