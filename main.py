90271c52537c69feabaa58d7c179bfabcf5d1938
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:06:18 +0000

    brush practical careful hide OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def get_array_item(array, i):
-        return array[i]
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
