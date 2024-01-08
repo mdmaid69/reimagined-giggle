61398eab087da31e91efb949220fcb873a0b0112
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:02:07 +0000

    similar normal characteristic mean correction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_density(mass, volume):
+        return mass / volume
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
