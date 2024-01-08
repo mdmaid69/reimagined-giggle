ef0b50b2f6a65b414ed9a673f3c62baf66ecdc38
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:20:12 +0000

    your tidy never there strong

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+print(sys.version)
