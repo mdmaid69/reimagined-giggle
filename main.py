5a89567cf495c445a54c7b0021961863a4b99774
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:42:17 +0000

    pretend art talk bound 179bite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
