63f860d100c67612337778cc8826d4e0baba490a
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:41:25 +0000

    later tour afraid decide crazy

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
