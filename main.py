213968caf47954dba45fecc7de362a320ebebbc7
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:42:07 +0000

    juice possible breathe operation whichever

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_typecode(array):
+        return array.typecode
