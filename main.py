648e1d1e1ac47f1c5af019346d3aaa33c448943c
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:22:36 +0000

    push variety steam ball variety

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def remove_duplicates(lst):
+        return list(set(lst))
