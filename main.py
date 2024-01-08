d94e62c9df1f7b10218930703b60dc44a9d4cfc8
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:18:20 +0000

    too want national central strict

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
