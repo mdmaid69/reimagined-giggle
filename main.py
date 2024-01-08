5b26ea70815cb3448eebdf1bc8372e7b45b4e059
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:22:11 +0000

    difference address there benefit sky

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import array
+def set_array_item(array, i, item):
+        array[i] = item
