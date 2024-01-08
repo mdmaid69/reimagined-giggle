005b1f6421e4f2cdd4c3dfb33416cdfd285c7200
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:17:55 +0000

    repair purpose light square act

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
