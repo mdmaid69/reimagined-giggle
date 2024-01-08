92540ecf65dac06370cbc14112f6c57e487d2911
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:01:02 +0000

    remark row solve by tongue

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_min(numbers):
+        return min(numbers)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
