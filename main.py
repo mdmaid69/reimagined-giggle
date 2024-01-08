595c7454bcace6b3bddb0358e53eb90a9ca492f5
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:25:35 +0000

    company wander for lot minimum

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
