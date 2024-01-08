a7e3491009974094303d724f57fdf69315830f41
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:59:41 +0000

    indeed loose against obvious quickly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
