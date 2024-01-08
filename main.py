15785d706f1b308c291f3ee1f0a4a663c439094e
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:49:34 +0000

    blind consider object insist subject

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
