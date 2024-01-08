857825863a9a615043a2484e6fb2af49cebac851
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:22:12 +0000

    wonder secret environment spray order

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
