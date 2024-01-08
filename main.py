0ff4cc811dfb98e1e11fc02e3e0fbf415c3a83fa
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:27:48 +0000

    exist manner business maintain birthday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
