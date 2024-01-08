d7afc58da0a352c13277e61c9e55a863b7a1895b
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:28:54 +0000

    yesterday warning backwards too face

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
