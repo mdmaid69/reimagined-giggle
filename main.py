03f33af30ef3e284fddfeda7bfd54680bb619e5e
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:11:43 +0000

    debt together grow thumb except

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
