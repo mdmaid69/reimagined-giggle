d088501dc7f836fefdc9c83b7c65caca8c395057
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:27:40 +0000

    duty introduction politics Nn private

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
