18e3982c1bdd4de62569b0a85b15fd76b347e6f5
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:03:18 +0000

    change left noone nice standard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
