3cad543f7541ac07ba21b901ceab53c0c44826aa
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:29:51 +0000

    children question direct programme general

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
