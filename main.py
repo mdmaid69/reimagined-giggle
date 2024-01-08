08b58540316767b47af29c8e0629c51881aafcc4
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:35:37 +0000

    workon interrupt serious weigh wish

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
