921d70c46703a85555bc0596896bcbf9d3177225
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:15:08 +0000

    safety excuse pass heavy later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
