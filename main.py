76f4be8181c65f3c13010e7aa781bab06b89794e
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:08:07 +0000

    embarrassing pleased destroy recent thinkover

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-print(sum(range(10)))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
