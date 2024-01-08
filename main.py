77f5ca1cfc3d2ff5b54f06c05c11073723cedc2f
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:05:06 +0000

    shout Cc rest day however

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
