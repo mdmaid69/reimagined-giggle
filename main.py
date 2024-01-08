2b7967912191777c7addaa48dda33574ca0c2333
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:49:25 +0000

    bang event ilovedicks laugh national

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
