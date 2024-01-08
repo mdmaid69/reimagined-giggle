b264438b55c62b89aa77cdb468d83cf36c5b449c
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:42:49 +0000

    admire store between communicate slightly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
