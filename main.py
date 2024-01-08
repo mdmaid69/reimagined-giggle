2a6480b4cb820f074c7c44bf6eced8472e3b22c2
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:20:19 +0000

    lead back please combine its

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_power(work, time):
+        return work / time
