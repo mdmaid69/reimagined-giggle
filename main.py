3ab4b57d4d40c44b84659005d593870bc982705b
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:58:46 +0000

    argue autumn collect hang eye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import sys
-def exit_program():
-        sys.exit()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
