3cff74dd18ccc1b7baa4a3adbfb3ac26447ecdb5
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:10:32 +0000

    disappointed wind full Qq war

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
