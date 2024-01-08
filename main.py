7d9b1987f49785285b301edc81c1a96a9561683e
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:28:17 +0000

    personal crazy touch please recommend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import time
+def get_time_since_epoch():
+        return time.time()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
