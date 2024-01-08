1920b51aa4c0bc99065e5c3a963afd4ee975781f
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:21:40 +0000

    temper desperate laugh meeting slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def extend_array(array, iterable):
+        array.extend(iterable)
+import os
+def remove_directory(path):
+        os.rmdir(path)
