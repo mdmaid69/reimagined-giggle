d0c9aa1528487fc0a434f1af55f4b64c6c7c0a81
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:45:29 +0000

    nowhere fire calloff absolutely calmly

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
 import array
 def write_array_to_file(filename, array):
         with open(filename, "wb") as f:
         array.tofile(f)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
