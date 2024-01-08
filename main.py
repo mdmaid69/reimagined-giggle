ccf5de698c2b0422ed15bf4da9cf49e6ee51acf6
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:36:44 +0000

    steady memory practise public save

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+for i in range(5):
+        print(i)
