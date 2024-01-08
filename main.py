e761a9f68e926ed05a2b549d75127d1341bebceb
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:59:25 +0000

    holdon towards switch design fashion

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import getpass
+def get_username():
+        return getpass.getuser()
