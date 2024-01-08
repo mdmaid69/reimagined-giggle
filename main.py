954d5eef8e2761ceb3613c457abc736015959fd8
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:29:29 +0000

    petrol trousers know unlucky male

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
