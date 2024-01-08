48c4dca19583d05d05382b50ef293da230973237
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:53:57 +0000

    work next nose keepup trust

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
 import math
-def calculate_floor(x):
-        return math.floor(x)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_arc_sine(x):
+        return math.asin(x)
