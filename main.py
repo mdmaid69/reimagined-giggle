4a06511f4747d17b131fe09c8a605b22a5a02d62
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:49:44 +0000

    agree free mood actually practice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
