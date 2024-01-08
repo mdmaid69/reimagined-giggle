977ae135154b2b538b75250c8ae6fffa81626bba
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:35:58 +0000

    argue workon keepfrom crowd 566end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
