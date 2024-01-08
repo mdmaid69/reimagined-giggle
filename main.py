d340c10f8b59d4cd9f9e33137e74e3509287d6bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:54:32 +0000

    knock protection send agreement woman

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
