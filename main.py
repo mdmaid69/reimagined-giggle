c33da283a74f8fc60c5a83b06e05031dd3233c39
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:26:20 +0000

    past hollow expensive pot important

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
