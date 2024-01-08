525237364b96a80caf24705c927df941a71c9855
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:05:15 +0000

    make laugh above apologizeapologise plenty

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
