3d4b04e3b60b73e799b9ebeaca924b711eee6321
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:05:06 +0000

    sweep clever point shame finger

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import sys
-def exit_program():
-        sys.exit()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
