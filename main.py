ad8ae0f7b38a00f8a203971daf3737128dd7a58b
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:55:43 +0000

    confusing march effort injury interfere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_work(force, distance):
+        return force * distance
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
