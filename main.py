5993c6b92166597b63d58508447af9fb81a1c71d
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:20:19 +0000

    show right compare scheme concern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
