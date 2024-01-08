1a07c488dc47c7ef2bee8ce96087066c5536ee7b
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:01:12 +0000

    amount drive better earn than

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  def cube_number(x):
-        return x**3
+import collections
+def create_stack():
+        return collections.deque()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
