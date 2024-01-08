1e62885920f375ae9d2f53fc2c3e979693c0c564
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:08:36 +0000

    slightly image declare soon teach

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
