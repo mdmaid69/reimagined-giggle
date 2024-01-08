fc770ce3ed47c361de2157519c123e569af828bc
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:32:42 +0000

    tradition build spray picture go

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
