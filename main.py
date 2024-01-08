020839c05054b6f7cf45c20bf23cf8b061deeb33
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:04:16 +0000

    opposite sure steal no precise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
