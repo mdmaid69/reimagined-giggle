1597c08c6c1fa3e09bca0998dea1ca1de0bf0346
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:21:53 +0000

    trick emotion ball Yy among

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
