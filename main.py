803f80a58321af84dea54780bf01bf9671c10bcf
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:03:31 +0000

    tell same alone level will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
