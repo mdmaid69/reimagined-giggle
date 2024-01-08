66a8337850f3d8921dd2c98cf2934765148d9c37
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:00:05 +0000

    responsibility national 565end person concern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
