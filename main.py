5efac47cb8d091e27d7b59e746c8d704819d9a72
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:14:45 +0000

    completely secret information yet push

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
