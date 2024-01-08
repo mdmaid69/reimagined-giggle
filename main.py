527fa8727cf8fe63ed8917f13f2297f3d666e21a
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:50:17 +0000

    noisy later bus shopping buy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-for i in range(5):
-        print(i)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
