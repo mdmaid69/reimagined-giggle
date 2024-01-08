eb50649b683dcdc4d7d27e3bd99dbb7725f17e66
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:09:51 +0000

    scrap major journey someone basis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import sys
+def print_python_version():
+        return sys.version
