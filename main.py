6e5102b36570e60d8b1312021e567bb0f23bfb68
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:11:10 +0000

    lead right goodbye high shall

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
