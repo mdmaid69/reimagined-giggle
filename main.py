e6b44e9a3694351a8559c906fec4783bd88d21e4
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:06:46 +0000

    comedown day plan floor exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
