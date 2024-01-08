ddd6395dd93e63d7352c8f8424bf0f1245931168
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:01:05 +0000

    each case petrol tidy proper

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+from collections import Counter
+print(Counter("hello world"))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
