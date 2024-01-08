3acaeb237e9f225a204614b4ec395dafe1308a2e
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:31:06 +0000

    need arrive organize relative probably

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+print(sum(range(10)))
