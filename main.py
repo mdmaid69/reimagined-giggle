3c0a0e7ce3eda9535b8e1dd8487352a4b38320de
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:57:48 +0000

    behave though determined flat street

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
