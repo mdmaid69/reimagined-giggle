bb9b26ff84aad3c6bac9a7677089e6bb68a4bccd
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:17:55 +0000

    proof near moon clean attention

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import time
+print(time.time())
