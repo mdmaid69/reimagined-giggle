b807a6e7642b32df4d8501246661aed8da812120
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:59:27 +0000

    fear 564encourage calm turn suddenly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
