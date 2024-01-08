d686c99e7c82ae0ca0d3692e35930166b3b64805
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:23:41 +0000

    frame software open reduce single

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+import os
+print(os.getcwd())
