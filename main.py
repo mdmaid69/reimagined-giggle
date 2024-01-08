1d461dc92e143a3bc79b23c0092ac0c3f4b08f29
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:20:04 +0000

    doubt fond pressure disagree remove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_dev(file_name):
+  def get_file_device(file_name):
         return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
