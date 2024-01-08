dbb63f309ec977cf1ae2c189cdf370b5b33e9d1c
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:45:38 +0000

    estimate fierce though angry heat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
