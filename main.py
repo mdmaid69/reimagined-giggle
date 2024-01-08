7f28f8405c812a6981fd2a35ce302ad8a708f90c
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:31:02 +0000

    breakup first empty genuine search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
