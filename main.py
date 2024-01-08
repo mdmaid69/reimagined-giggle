e1ed07201ad6fec8e5bac7584a560d2cd34f6f4c
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:06:11 +0000

    performance name relation large complaint

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
