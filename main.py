8572ea5ea7e7855dec65831ddd6ac01a4ebd2369
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:19:10 +0000

    separate undo too from politics

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
