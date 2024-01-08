49ce4dcacd3d9c676fda6a1e873ec4bc5160de5e
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:27:54 +0000

    happen birthday maybe learn order

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+for i in range(10): print(i)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
