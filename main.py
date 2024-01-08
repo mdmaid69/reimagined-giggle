754648209ca69546d46ade06410e77cb6e8bc878
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:28:46 +0000

    address hear wire form separate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
