832c2bbc341bfe894311eb3701d7e2f13b81bddb
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:31:49 +0000

    respect negative constantly turnoff needle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
