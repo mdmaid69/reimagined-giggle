638b3ca6e706b371512dbc021e41276aadfd7d2e
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:07:50 +0000

    point fat close dress advice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import collections
+def create_user_string():
+        return collections.UserString()
