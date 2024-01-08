e8f2b696d1ceb68cfb7b5f50499fca456339eccc
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:49:31 +0000

    fail plenty she affect make

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import datetime
+def get_current_date():
+        return datetime.date.today()
