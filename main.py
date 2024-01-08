bb33a2556be2eb20fc89a1ec95086e4dc7ba8dbd
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:08:09 +0000

    female something send attach cap

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
