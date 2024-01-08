c083bda42cf3cbb8be882588a9819b7ecee9b498
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:45:11 +0000

    grateful range breakfast tomorrow future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
