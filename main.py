77b1916768225bacc9f5073f64a80a1d9c586bee
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:09:49 +0000

    estimate cum explanation obvious later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
