7c483b541269966dcbb906b9b367f96540c61bf9
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:57:00 +0000

    command ticket meanwhile obtain among

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_acceleration(speed, time):
-        return speed / time
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
