2a3906bd482b605a31cc9dfc2e3a564091f2900c
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:32:15 +0000

    combination project move hard reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def print_python_version():
-        print(sys.version)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
