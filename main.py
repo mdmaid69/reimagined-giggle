dad2bc9c8bd4d3146d477e25951b1a287885a7bb
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:16:00 +0000

    sensible young recommend week excuse

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def square_number(x):
+        return x**2
