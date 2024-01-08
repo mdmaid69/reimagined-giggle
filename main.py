53a8da6f2e54cdc74c80db1efe004f45a21eb2bc
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:29:51 +0000

    communication introduction light arrival pencil

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_acceleration(speed, time):
+        return speed / time
