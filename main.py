52a4a9d559f83bb91aa3805d5e41ae1f9e1befba
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:39:06 +0000

    circumstance pen opposite loss and

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
