034b76b730b88a10d0b75d23fe1e42b028db9a89
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:34:12 +0000

    rank gentle extremely dick callout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
