aaf79a28051990f34589d91e4ff1b0b1364b9a4f
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:28:19 +0000

    reaction polite line as makeinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import random
+print(random.randint(0, 100))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
