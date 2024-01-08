b227ce3bfa6d0654ab71226c34ec9cf5afdcdacd
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:42:08 +0000

    relief total bank gift centimetre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def reverse_array(array):
+        array.reverse()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
