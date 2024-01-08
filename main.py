7ad67d12cb6c3b21561efe72e17a5d68dde58b1a
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:26:14 +0000

    strong approve might talkinto picture

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 text = "Hello, world!"
-print("Uppercase:", text.upper())
+print("Is palindrome:", text == text[::-1])
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
