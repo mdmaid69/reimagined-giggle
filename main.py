c1b44ff3ad3e1c32fbb0837be84a6013f51283f8
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:08:44 +0000

    angry late than hers vanish

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_list(array):
+        return list(array)
