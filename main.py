1c60694ab22b8685ed77dfded88f7422f451a8dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:06:12 +0000

    what frighten pass happen further

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
