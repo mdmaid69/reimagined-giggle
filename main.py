41fc748805d1cb2fcc63af662451477638f85963
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:34:23 +0000

    whistle all excuse neither perfect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
