a2670a6449463b5affe1f775812cd0f57d77c024
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:06:53 +0000

    separate remark ready else system

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+print([x**2 for x in range(10)])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
