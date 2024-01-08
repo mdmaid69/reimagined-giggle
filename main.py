434d0ff1a9c8ee83231c6aa16043cb8b3fdd882e
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:18:46 +0000

    where form wide hard voice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
