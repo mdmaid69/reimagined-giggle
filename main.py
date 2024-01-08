7e8d688c8dc1b5bd2cfc90d330fb093265f45af9
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:00:00 +0000

    second it tax successful lower

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
