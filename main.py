f00543d168bc89353d9f970822d9d372c3cb1a14
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:09:12 +0000

    stage getout watch so suppose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def reverse_list(lst):
+        return lst[::-1]
