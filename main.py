dbe4b08818165a7109c62da1746c04b0314e88d7
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:58:29 +0000

    complete between rob disagree stand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def reverse_array(array):
+        array.reverse()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
