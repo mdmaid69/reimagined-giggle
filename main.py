76387fe0873af19877440cf3fdf8d7a59cca8bfe
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:46:17 +0000

    lead sign contents trick great

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
