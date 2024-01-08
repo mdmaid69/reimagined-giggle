414252d3dab3f9ff1999a114b0991d7d6fae7b8b
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:04:00 +0000

    race dead Gg handle bright

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
