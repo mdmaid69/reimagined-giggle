8e6382499677c3dd6e3bdb1e786d316f240daf3b
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:07:36 +0000

    crisis book shall top steal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def find_max(numbers):
-        return max(numbers)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def reverse_array(array):
+        array.reverse()
