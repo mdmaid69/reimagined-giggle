f1c848e352fc6f54a5864f0cb910bd24ed28d3ef
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:41:50 +0000

    runinto cinema right activity stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
