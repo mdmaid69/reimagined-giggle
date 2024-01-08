c4088599fa658d86c1d549c7f1e0b5ff20a26c77
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:22:46 +0000

    cry pleasant recent welcome comefrom

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
