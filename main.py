a60c8b711d0a25bf3eb5e8e0cb612f3b18067f9e
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:47:01 +0000

    firm talk up characteristic false

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_base_name(path):
+        return os.path.basename(path)
