0da957198c97e17bf05b0c506b0ba80e13fceb73
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:07:17 +0000

    small shop cheque continue pair

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-print(math.pi)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
