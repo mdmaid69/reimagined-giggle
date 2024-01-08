f986df93fab3acad262f4917f759de63c18c8b13
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:49:19 +0000

    object manner declare circumstance solve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
