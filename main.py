029255a49e95bc723bf18bbecbd7a5880aab551c
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:16:53 +0000

    low crazy experiment play possible

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
