17168778926ebea8ccd4ca282dd445a99b7f333a
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:50:04 +0000

    noise personal generally flow believe

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def extend_array(array, iterable):
+        array.extend(iterable)
+import os
+def remove_directory(path):
+        os.rmdir(path)
