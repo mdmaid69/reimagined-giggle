44b4a3c717478064ef9205ff32d06e1a68570dc5
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:23:36 +0000

    pay wall hard together disturb

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
