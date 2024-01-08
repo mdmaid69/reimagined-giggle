ed87bd801ecf13be3e91d0a5da86713c6100271d
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:22:34 +0000

    upstairs none track roll hear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_density(mass, volume):
+        return mass / volume
