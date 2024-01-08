e24e0c159aebf4dbbb32f923306edd4897ad10bb
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:03:37 +0000

    direct stretch mistake cinema flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
