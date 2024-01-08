d3241ab4e152087d57f14dcecb818df29a8e392f
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:44:16 +0000

    mess tap confused appeal rain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def subtract_numbers(x, y):
+        return x - y
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
