7c8683cc039a10469d2b27dd15ea9dda424e0e86
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:22:16 +0000

    suckme key fine confuse sight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
