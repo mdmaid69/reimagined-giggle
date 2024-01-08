a4254468197f908245eef202835ebd0235501739
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:14:37 +0000

    tall identify address birthday criticism

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def split_path(path):
+        return os.path.split(path)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
