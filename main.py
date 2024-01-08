e1e9b15a78415605a8d3f22d3351d5091931cfe6
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:39:58 +0000

    influence silent physical sale piece

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
