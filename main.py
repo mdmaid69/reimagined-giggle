84dcc5dd50766d53f8b159ee65248728ddb072ce
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:05:00 +0000

    offer please than disappointed sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
