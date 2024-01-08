b31e736be458143f6d5257cf484cfe6bdd2d3240
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:36:09 +0000

    simple authority steal friendly row

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_set(array):
+        return set(array)
