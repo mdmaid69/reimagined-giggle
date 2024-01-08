e39f0f79a83311defdab0d6d8357d1a905d5f90a
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:36:51 +0000

    note cause enthusiastic add own

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
