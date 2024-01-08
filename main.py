1ee4a90de1946e66710f07d1a62f837300789192
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:34:39 +0000

    aim shop task broken together

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_inode(file_name):
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_ino(file_name):
         return os.stat(file_name).st_ino
