1f370c1d1f97e1ba62934cedc08eeba9babcff12
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:48:32 +0000

    frequent pair could confident collection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
