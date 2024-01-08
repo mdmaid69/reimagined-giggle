c558db1f787a0e62d5f9b77a4f0f85ac28f7efe7
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:47:06 +0000

    holdup put border break corner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
