88b396d9f576ecf1b47fb62046f418d8933d6b5b
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:17:28 +0000

    bunch building priority giveaway centimetre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import re
-print(re.match("h.*o", "hello world"))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
