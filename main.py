aa3e454fae00393ed2aaa0a2b382fe8f6293aa7d
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:30:54 +0000

    wrap considering emphasis reaction surface

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
