68b2a2dd8a4bd489fb3628b3f3f21962ef8ea8d9
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:11:30 +0000

    content strike ear summer foreign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
 import array
 def get_array_as_int(array):
         return int(array[0])
