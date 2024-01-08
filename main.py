314b5d2e61024a95ff443dd03aa377455c484ed3
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:44:31 +0000

    fasten wall order relative holdout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+text = "Hello, world!"
+print("Words:", len(text.split()))
