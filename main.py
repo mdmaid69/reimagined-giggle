bf6d6bb2a71f43bba9598c43cbe015ba8bf87240
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:13:48 +0000

    accidental bottom gather forget whom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def count_words(sentence):
+        return len(sentence.split())
