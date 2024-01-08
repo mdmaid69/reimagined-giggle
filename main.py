f05726d38fe39539ff0bd23f0486ba8167eafd0b
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:57:43 +0000

    air limit question substance perform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
