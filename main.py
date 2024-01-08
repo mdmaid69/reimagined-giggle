3f6a16cf3d47b84fd97f5e20f4f9924ca68b9557
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:06:04 +0000

    tradition basically his follow forever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
