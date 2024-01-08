51e856cde10ebb05c0cc9eca5f9c6f9ea6702bcd
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:59:33 +0000

    keep position hello heat second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
