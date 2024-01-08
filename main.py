47d5f8989420debf5e6561b103a413459676ef2e
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:31:18 +0000

    problem rank reserve trip trap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
