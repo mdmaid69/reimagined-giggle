343b07cdbce9068b0540b5445ac0bde67f16f073
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:31:45 +0000

    open national about various spread

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def clear_array(array):
-        array *= 0
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
