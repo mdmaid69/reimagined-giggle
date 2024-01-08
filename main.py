2b14f774a89180f29ef467b41544d68bed92705f
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:31:41 +0000

    judge politics begin theirs force

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def convert_to_hex(n):
+        return hex(n)
