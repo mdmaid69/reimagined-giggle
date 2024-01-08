87ebfac5af59f10de61e339f2d4ad4ce23cb72c8
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:21:13 +0000

    disturb switch we lunch pressure

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
