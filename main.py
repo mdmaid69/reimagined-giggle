bdc023949edbe79c42471d7b6b5deaa3ff918063
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:21:01 +0000

    shock quickly between get runover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
