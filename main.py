92947e5fce209ce710750c55ad2c359d1e82a74e
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:00:20 +0000

    fat stand pick thumb bottom

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def convert_to_binary(n):
+        return bin(n)
