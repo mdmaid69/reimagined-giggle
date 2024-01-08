c6e51ff2cb76243bc572ce69665fd0c2ab597670
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:58:16 +0000

    shock success difference star official

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+name = "Python"
+print("Hello,", name)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
