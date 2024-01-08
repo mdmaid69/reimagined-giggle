8b713228dfb2107e489bfbfc5dd39749e34c1234
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:02:41 +0000

    smell tone anxious square fight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def reverse_list(lst):
+        return lst[::-1]
