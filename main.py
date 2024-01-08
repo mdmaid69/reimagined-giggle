20fc44568d84cb81e51cbd8ffa4c91891849413f
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:01:17 +0000

    that have winter middle alive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import re
+print(re.match("h.*o", "hello world"))
