5c45a6491f53af7ae90c0d37fdabb2c0576f47d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:31:22 +0000

    towards compare right bother niggaacademy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def add_numbers(a, b):
-        return a + b
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
