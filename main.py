9fbc24f93be141578672ee9672db93583d6d2126
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:55:43 +0000

    Ll cry birth direction whom

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
