711b6e22ad1968c6b6cf8c732c1529f290d5483f
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:24:34 +0000

    owe upstairs division delight strength

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def remove_duplicates(lst):
-        return list(set(lst))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
