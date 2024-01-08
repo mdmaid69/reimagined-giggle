a119311cac512afa1f815be3f3c248844d5cb22a
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:44:11 +0000

    ground television lucky total only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
