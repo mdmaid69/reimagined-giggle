2e40016232c681a38852ad61d0642c5ca8db039c
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:29:05 +0000

    house concerned hurry against race

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
