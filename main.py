6c4083633bd84e96e54a2df73a462b693dfef10c
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:26:45 +0000

    platform pay tape Yy free

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_acceleration(speed, time):
+        return speed / time
