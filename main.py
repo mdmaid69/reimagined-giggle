4bfc223776ff31a242533fbc2546e41852f3efbe
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:48:05 +0000

    basic all Gg base positive

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
