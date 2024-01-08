19753f43410a020fce585baf19721719e966625a
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:20:26 +0000

    hate flood love weekend though

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
