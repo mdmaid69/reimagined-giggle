8f9e5b3acbebe87c3c5328eb349d6efb76221252
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:54:28 +0000

    now oil pin science heat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def find_min(lst):
-        return min(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
