a2214a3d2931216e3f52a7c385f808e300e1015f
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:43:56 +0000

    quantity nose ready broad weigh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
 import array
-def get_array_as_set(array):
-        return set(array)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def get_bytes_from_array(array):
+        return array.tobytes()
