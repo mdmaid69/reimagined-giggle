7df9454443434c4c671503bb94881953d79f7fcc
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:31:25 +0000

    post me quality floor nose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def find_unique_words(sentence):
+        return set(sentence.split())
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
