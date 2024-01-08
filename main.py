0bf8eedc760e919abe14fc99904584f6f3b2eca0
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:02:18 +0000

    safe spite any laugh steady

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import random
-print(random.randint(0, 100))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
