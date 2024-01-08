e61995bb38d7886723bd7c6cef2605256c0c018a
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:01:29 +0000

    someone straight skin crowd soft

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def delete_file(file_name):
+        os.remove(file_name)
