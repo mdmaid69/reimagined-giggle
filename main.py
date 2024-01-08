7fd5999c1ef5977ccd35026cc7fde68b5ffdaf09
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:53:32 +0000

    children mind include company dress

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
