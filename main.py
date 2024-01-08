fc2f87859ebc47ff48ed3b95e72f342489c99ebb
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:38:16 +0000

    justice traffic expensive shout mother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
