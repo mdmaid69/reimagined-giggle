8403b87a73fed1bb784ea37ccca1efa802c21bb9
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:02:40 +0000

    underneath catch because Ee moral

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
+def divide_numbers(x, y):
+        return x / y
