4516c1bf71bfa29dbcdc73e7d42beb1ab3fca552
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:11:10 +0000

    involve history war cruel nigga

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
