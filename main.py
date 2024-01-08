f500e3dafaa5aaa0ac7eda4de46ceb9bfeff35f0
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:22:47 +0000

    but wait base naturally memory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
