f659955c53894b1e3daf50df83b4820008af22e6
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:28:33 +0000

    Pp downstairs early cut able

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
