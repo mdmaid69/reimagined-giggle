bc1ddf8fd61c14529793908b5bd5f2a139b8c381
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:15:38 +0000

    still advantage this second familiar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
