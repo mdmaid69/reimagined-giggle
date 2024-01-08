c3f7bd1365cfe674861c055f15023d5dfc231c2e
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:44:01 +0000

    demand adjective set information better

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
