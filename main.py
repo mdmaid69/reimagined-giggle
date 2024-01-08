882abf4daf1a30695ea25381818ea154b5ca3673
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:21:05 +0000

    poison there sticky waste government

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import time
+def get_current_time():
+        return time.ctime()
