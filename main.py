de1485303f27baf7d2914ecf1db19f6f86f82e89
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:57:31 +0000

    meeting remain slow ocean fill

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def convert_to_hex(n):
+        return hex(n)
