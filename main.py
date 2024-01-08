28c4cf5b9f04069ca8ab6b893f62cba0abcbe300
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:09:40 +0000

    suckme coffee his advise similar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
