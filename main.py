4443a736ce9768f79b18c0fb497ecb5091b73257
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:40:43 +0000

    display block band first teach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
