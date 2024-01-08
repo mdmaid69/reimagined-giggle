90e1835c6a63c7c05cf59ad0a62c97a64b53e9e9
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:15:29 +0000

    love pin firm politics fly

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
