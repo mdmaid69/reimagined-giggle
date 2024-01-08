83babb3aa07a00ca4e2856f5fcc9b922c197b5c7
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:01:56 +0000

    demand everything calmly appearance comein

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
