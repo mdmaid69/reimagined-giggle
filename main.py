a41b2d6b1741167aab77a643f833e9c8be2a3f11
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:35:04 +0000

    hardly ring word considering prefer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
