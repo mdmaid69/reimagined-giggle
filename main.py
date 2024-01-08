f5032150f635ddd3c9acbaa720e2ad731b6c4262
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:58:10 +0000

    ill keen form lead bet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
