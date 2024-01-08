1b94e6c0a6ac3965760fba09b77f1c0bb8cbb78f
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:20:20 +0000

    or freeze position round bottom

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def get_array_as_str(array):
-        return str(array)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import platform
+def get_python_version():
+        return platform.python_version()
