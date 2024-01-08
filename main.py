8d76a9a63151356ab8df77f597378e2cc913ca54
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:40:58 +0000

    also deaf present lucky she

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import getpass
+def get_username():
+        return getpass.getuser()
