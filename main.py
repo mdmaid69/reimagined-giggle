c60e73d823f07485f09a968ebec039a99b7459c7
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:26:25 +0000

    several real never square get

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
