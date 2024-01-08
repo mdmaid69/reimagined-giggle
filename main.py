163a0c74f68ab16c745045f72bed3bce70d0c1d7
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:31:57 +0000

    minute steer pretty wherever accept

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
