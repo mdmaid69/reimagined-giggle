be5461c06afb40d12c2cc355a0050c0a44a41762
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:03:29 +0000

    funny lot Vv listen position

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
