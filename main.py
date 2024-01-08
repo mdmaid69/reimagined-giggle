da9dc8dadb0c9b47930af69d70eff5a92daf361b
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:05:00 +0000

    grand eye good turnon suffer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def is_odd(n):
+        return n % 2 != 0
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
