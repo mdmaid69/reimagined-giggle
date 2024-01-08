5f5ebba2aac5aaf573986a489354a8a4514c8ae4
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:22:30 +0000

    empty rubbish train address hang

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
