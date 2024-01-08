85a0e99c9fb1c350d3f65cde4b057f729cbe1c17
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:22:38 +0000

    better way wake age happy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_density(mass, volume):
+        return mass / volume
