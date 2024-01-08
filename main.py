82134ddd67262971f180da5c9e1eb4c8838b612b
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:55:59 +0000

    569enjoy plan listen swimming nothing

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import time
+print(time.time())
