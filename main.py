a53ab36ddfc2d56c4d2393ba7e421822d3a2d936
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:38:06 +0000

    quality hardly deliberately air cruel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import array
-def get_array_as_float(array):
-        return float(array[0])
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
