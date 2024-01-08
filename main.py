0c015a07824521a074ad7b1151d532db72edc833
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:06:09 +0000

    mine concentrate force programme fun

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
