6fff1658e7b4bf99506a4152ea74e7cf0d5c2dc3
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:58:57 +0000

    worse keepoff agree worse noise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
