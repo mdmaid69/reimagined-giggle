9964048ee0887d0e62c8d6e7398dd93e529f5dee
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:41:05 +0000

    rest smart strict with side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
