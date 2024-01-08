0d21b817a02492f339f8da351d5876e172f54881
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:31:53 +0000

    service know behind nice anyone

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
