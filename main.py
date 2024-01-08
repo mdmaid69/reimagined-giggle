9d4d4ecf038bf715068fb70ee373fe7824a02a22
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:32:36 +0000

    trade call pay keepaway back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
