57d65ac022ad5d7b35c6eafc1cc5677697cdf8ab
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:16:37 +0000

    load mysterious yourself deserve another

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def reverse_string(s):
+        return s[::-1]
