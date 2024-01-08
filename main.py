408fae33ffb01a39f74cc1631679ec19d843ed2e
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:43:11 +0000

    confident treat independent getaway light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
