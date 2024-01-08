e15f6568dfd0dc03e056bf338d40fa3b77b7b0cc
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:56:18 +0000

    practise escape risk peaceful probably

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
