9e6457a9b0bbe1ee7f1108ebd4bc8f3e144053dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:48:20 +0000

    broken confident study punishment freeze

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def convert_to_hex(n):
+        return hex(n)
