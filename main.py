d8b9cf356e328738e9da92140fc68605019e30e0
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:22:13 +0000

    mean forever young question precise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
