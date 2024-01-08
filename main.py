992797681fa0870b66811c34684ef5b351949f71
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:05:34 +0000

    lost nurse brave reliable anyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
