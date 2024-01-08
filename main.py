594b71fdc12bc0cf740615ab22e94963c2bfda34
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:02:11 +0000

    peace emphasis strong Tt sleep

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
