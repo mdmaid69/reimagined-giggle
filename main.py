04921318e450145f5cefd122b46d888b7a96e155
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:12:17 +0000

    bear declaration carry appear arrange

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
