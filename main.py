1e21c71491c3c0d9aea7013e352c51ce6b62412e
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:33:48 +0000

    finger willing well block low

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+print("Hello, world!")
