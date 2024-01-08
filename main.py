2332510dc74535097d022b4b782d788ffbab2400
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:46:28 +0000

    downstairs debt handle mark prefer

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  def convert_to_binary(n):
+        return bin(n)
