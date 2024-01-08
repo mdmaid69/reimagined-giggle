09ec2f1aa906c0e35bf5291461159fe5dfbe4ead
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:16:15 +0000

    slip hardly trust imagine gradually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_as_float(array):
+        return float(array[0])
