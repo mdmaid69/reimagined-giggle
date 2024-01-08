5d2ad95424a97b6063b21cfd475818f54fa6fe0d
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:03:39 +0000

    agreement debt little contact cook

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_list(array):
+        return list(array)
