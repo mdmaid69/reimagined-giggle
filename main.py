ca3563dd3f438afff0f9a56169313456130ac91c
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:44:36 +0000

    obtain against equal sight Bb

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
