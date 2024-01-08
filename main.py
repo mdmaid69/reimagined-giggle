cd34364c6d6659d63fdba75886db0c0b1f4465e1
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:47:33 +0000

    law reaction track tragedy greeting

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
