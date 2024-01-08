731249cf1d6bd5554e4041a7ec7a6a2812df9f2f
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:58:03 +0000

    trouble me else hang off

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
