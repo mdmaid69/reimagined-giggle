055e3296b94dd913fd96bcd2cd8143da1ced64fa
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:33:06 +0000

    sentence post nature respect fetch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import platform
+def get_os_info():
+        return platform.uname()
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print("Cube numbers:", [x**3 for x in range(n)])
