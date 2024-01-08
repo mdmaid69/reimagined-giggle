1d0b54fa8215d6f24923e16931210d21b8371189
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:40:21 +0000

    could place intend shadow avoid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def calculate_sign(x):
+        return math.copysign(1, x)
