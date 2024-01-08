4f1e005fcc2fa487d10f2805b5e1f4840db93493
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:01:13 +0000

    no patient flood creature satisfactory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
