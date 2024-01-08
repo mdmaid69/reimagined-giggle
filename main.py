954c4bd239be25fc4bf7b40212fea89672c3213a
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:42:40 +0000

    born decide tall speak attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
