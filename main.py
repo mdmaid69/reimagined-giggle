f6752fee8002afe67c2e216927ef9b65cd20dfb7
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:19:57 +0000

    roof or soft dare as

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_work(force, distance):
-        return force * distance
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
