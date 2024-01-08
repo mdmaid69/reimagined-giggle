5547a77f09f3748248d8c9af4328c0f94586e352
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:41:13 +0000

    mystery emphasis along worry dinner

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
