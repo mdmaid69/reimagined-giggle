3c69a6b6e98d14500284d18f9f03c1f0ee091994
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:57:59 +0000

    trouble holdon control 570enjoyable match

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
