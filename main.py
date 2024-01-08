44120c2833431e18785eb00001eb6b90256400a4
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:51:43 +0000

    mention flight up step less

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
