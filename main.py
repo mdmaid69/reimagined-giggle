98175b836e1fb1e0b67107d31d09d325963eb6eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:04:56 +0000

    money rich regard luck official

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
