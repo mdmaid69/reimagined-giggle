4f8e1463415f49a65cb1e3f1875af0ef5f51f865
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:02:24 +0000

    undo door stand fresh address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
