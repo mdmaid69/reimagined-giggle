4556be24e4ad8d5d9baba64d7ad79da3e5da6aa1
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:36:27 +0000

    turnon word attitude club escape

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def cube_number(x):
+        return x**3
