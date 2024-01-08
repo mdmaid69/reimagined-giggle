2f533eba2763c323d2737103f4b3837a2a488320
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:06:40 +0000

    sight typical life satisfied part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def reverse_string(s):
-        return s[::-1]
+text = "Hello, world!"
+print("Words:", len(text.split()))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
