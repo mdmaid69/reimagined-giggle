e49f636456c4c193e03fbec39e64cca87030a19c
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:15:48 +0000

    sign comeback load peaceful exploration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
