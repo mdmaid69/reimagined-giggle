84fca1d616a426e7953979ce36e31721d47cb993
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:45:42 +0000

    Dd means memory port arm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_bool(array):
+        return bool(array)
