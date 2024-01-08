abc0ef0da1dfed6261367d49d7a35cb864733990
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:28:30 +0000

    borrow shut loose favour fairly

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
