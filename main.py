f3af1de924e416b7ca150b1a71e530fef33188de
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:06:06 +0000

    proper always even too stiff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
   def get_absolute_path(file_name):
         return os.path.abspath(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
