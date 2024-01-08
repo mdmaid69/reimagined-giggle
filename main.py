c97ad4f40fad396524d4d403888aa55a4dbcab5e
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:29:42 +0000

    event spin whisper trousers Ll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def split_path(path):
+        return os.path.split(path)
