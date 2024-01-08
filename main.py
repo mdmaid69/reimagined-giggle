aa84b998111c2314df47138ba506cfb3971d7d4b
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:23:46 +0000

    bang persuade 565end instrument first

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
