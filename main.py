ab1feb0ed4c31aefefdfd51e998c8e5f27c67743
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:35:55 +0000

    tone trade stir absolutely slightly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
