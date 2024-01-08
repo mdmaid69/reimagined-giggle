b9d22b6d9232e5994c8dae5ca24a41cf36d48d62
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:01:42 +0000

    physical transparent government rush sign

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
