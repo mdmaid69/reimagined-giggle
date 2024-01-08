fe8aed9a4114f910efcf82ae0f03030010dfbdab
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:13:32 +0000

    rob connect greeting project regularly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
