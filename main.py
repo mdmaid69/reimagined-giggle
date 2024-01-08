3cbb2b468e3c9f290a3beed78e3b1ba77aff826d
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:45:30 +0000

    force lesson only head perform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
