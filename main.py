46831935596258d2b6a6dec910f51ba7d2141438
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:56:13 +0000

    attach where depend gap boat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
