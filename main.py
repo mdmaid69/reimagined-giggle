7d19857d4eda4243f8c888c50b99928f8fcf0824
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:21:11 +0000

    567enemy style grow ocean breathe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
