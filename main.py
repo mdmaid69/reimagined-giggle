2e90545a21ce1d5c789b95133f4ed2bcf6405a16
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:20:22 +0000

    brave start willing union unable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
