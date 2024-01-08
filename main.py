1751e999135ff9ff1de99fcbec2309ea7a9418f0
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:11:49 +0000

    strict though sell border place

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-print(os.getcwd())
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
