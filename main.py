c69f821dbfd3dc73b388f1a48dd31c3313d5d1ce
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:43:41 +0000

    surprised meeting tear guide content

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+print([x**2 for x in range(10)])
