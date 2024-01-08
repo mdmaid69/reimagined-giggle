60b63b1e022835791bf175c4b1c42d2a56438c82
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:43:30 +0000

    mine exit push scare official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
