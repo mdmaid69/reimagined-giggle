bf4059b006a371b5fb2d8ffa010e3b1bafa30502
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:13:47 +0000

    month tax pin neither wise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
