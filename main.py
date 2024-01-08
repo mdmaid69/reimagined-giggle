83b27ab6ca1cea4dcfd2f595ebfa98733323d565
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:57:41 +0000

    fast content form over touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def is_odd(n):
+        return n % 2 != 0
