fa530a330ea07d31b7bc96e8d56c7d4bbeff83c3
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:28:41 +0000

    tough challenge service wall take

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def square_number(x):
+        return x**2
