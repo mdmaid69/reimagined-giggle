5bbb28bfe32428db4762f3f9ff913cd7cea701b0
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:23:26 +0000

    drink switch suffer pocket leg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
