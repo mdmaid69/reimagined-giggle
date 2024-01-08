c7ad0b682e1f42091134062dc4f6f664fc4f1402
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:20:20 +0000

    care release file exchange clean

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def find_max(numbers):
+        return max(numbers)
+def calculate_power(work, time):
+        return work / time
