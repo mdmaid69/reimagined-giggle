3ca5870aa3cd9182a224469713c35068d2d19a9a
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:39:58 +0000

    welcome spring yesterday curious star

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
