f6636258cebac609323d68a6893ffe094c00c2b1
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:52:15 +0000

    turninto invite control scared stay

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
