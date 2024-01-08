7f5aa6ebf7f96c84273bb9040c20c996dcc8ac72
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:04:46 +0000

    murder board space wild bottom

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
