c2153efa07d8cb64a033cbc41d9f7455a0e28395
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:25:17 +0000

    unfortunate plant live lead run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
