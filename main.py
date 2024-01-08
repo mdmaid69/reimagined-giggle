1a1e52e331f495dbebb4f5a4ef22f121a5df1207
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:32:09 +0000

    plenty smell lucky male forgive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
