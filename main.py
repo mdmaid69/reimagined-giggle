8f72be0fd3b865477546067f454f0881004403ed
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:09:55 +0000

    bunch sea now keepout radio

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def cube_number(x):
-        return x**3
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
