ed3d544f0f087eda5bba3ea101fbe402891daa95
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:32:24 +0000

    worth correction naturally thank inform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
