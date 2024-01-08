598cb72de8dc80c3a3a6cf3730cb89c1d6216949
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:42:21 +0000

    record shame serve appeal hungry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
