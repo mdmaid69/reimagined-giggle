7802777bc8671b8ee83dc6fa024b27d23686d719
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:59:10 +0000

    praise pronounce mark congratulations week

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def greet(name):
+        print(f"Hello, {name}!")
