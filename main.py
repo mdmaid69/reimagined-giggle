8d9590a29b079b14b67addfa153275451b829404
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:01:01 +0000

    disturbing criticism appear preparation wash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
