230b8a7f78390f697d1b016a0b7df6be18e624eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:08:45 +0000

    suffer table please spray wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
