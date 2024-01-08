8f5c91bf786f80b1979d18ce19f8ea805fedba6f
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:08:21 +0000

    sensitive challenge sort tongue free

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def find_min(lst):
-        return min(lst)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
