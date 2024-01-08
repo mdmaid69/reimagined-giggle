c834a5df695991022c79e41a665229dc31a346d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:35:38 +0000

    likely ill fun experience front

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-for i in range(5):
-        print(i)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
