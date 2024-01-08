4050d291be00482fdddadb79054684151cb7a3ef
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:37:09 +0000

    round look needle uncomfortable rule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
