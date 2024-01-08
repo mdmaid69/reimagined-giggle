20ddadc10567342b304b48eb76a0c9d03e32faf5
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:39:49 +0000

    572enter neither prepare right practise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+for i in range(10): print(i)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
