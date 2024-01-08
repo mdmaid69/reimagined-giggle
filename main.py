bb8e86aa1cf45c9cb17664fb1fb2cc4ac1ade6be
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:04:36 +0000

    state future news build temperature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
