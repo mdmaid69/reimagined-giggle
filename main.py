6962eab077ba297610d96ee65cf901f386ad663a
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:30:47 +0000

    issue she rely talkoutof hollow

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-name = "Python"
-print("Hello,", name)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
