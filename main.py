b062e73867d745e207672d6f53654f283c9b655f
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:54:53 +0000

    gift breakfast example gather visit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def square_number(x):
+        return x**2
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
