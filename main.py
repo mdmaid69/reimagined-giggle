b6b551913abca171c0412ffc3aa73b7675c9382f
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:25:58 +0000

    memory defeat blame limit film

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
