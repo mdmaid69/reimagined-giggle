9aa02586bcffd096c6527be62b56cf97147ac10f
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:35:46 +0000

    sort slowly emergency speed form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-print([x**2 for x in range(10)])
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
