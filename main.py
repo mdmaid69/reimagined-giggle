2029463ba265325d3a9b5c5f7668f2ac5847b9b2
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:00:37 +0000

    contrast concentrate between score this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_item(array, i):
+        return array[i]
