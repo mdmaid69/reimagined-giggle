78043708ad826a3e5c618447d35cd47bc940230d
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:37:01 +0000

    disgusting boat out time completely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
