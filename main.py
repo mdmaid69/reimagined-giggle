141d360ecddbf1ba8261589be81da37554b1170e
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:13:56 +0000

    nearly clearly cinema disagree cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
