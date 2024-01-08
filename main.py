41e7c663b5f0b773c5169fe280b360613ff3369a
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:07:39 +0000

    treat in information now situation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
