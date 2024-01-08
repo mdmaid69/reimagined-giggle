5ce2214a1ea6421a360fb959f76e8f52721101e0
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:28:13 +0000

    narrow talkback theirs bar us

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_memoryview(array):
+        return memoryview(array)
