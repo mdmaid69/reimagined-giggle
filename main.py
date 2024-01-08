74e195c6b265cb50a2bf2532577f44dea74d7c07
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:07:10 +0000

    music congratulations talkinto bend usual

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
