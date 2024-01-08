04dee9155f07c5b46f5d4db352e7a40cb09eac62
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:47:46 +0000

    future the right urgent arrival

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
