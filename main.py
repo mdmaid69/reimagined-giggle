ca38f4b063af2e9c4afe843acb15d752884efb34
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:48:33 +0000

    store favour eager sensible smart

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  def add_numbers(x, y):
-        return x + y
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  def reverse_list(lst):
+        return lst[::-1]
