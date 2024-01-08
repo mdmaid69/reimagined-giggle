b8d7f4aa14b78594fe3d3b916dcf88a9b0abb83b
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:55:59 +0000

    slip mend 568energy strong basically

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
