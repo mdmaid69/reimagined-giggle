743c1f7cd6b6c252b0e462c82be7996075ceffc0
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:34:36 +0000

    broken call muscle sort behaviour

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import time
-def get_time_since_epoch():
-        return time.time()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_speed(distance, time):
+        return distance / time
