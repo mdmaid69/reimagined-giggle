fe9ba1556022ce964182e4ed0fa29ca8e71deb92
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:41:12 +0000

    shopping thinkthrough afterwards regard blow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
