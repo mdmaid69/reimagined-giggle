6ba46e919df633619be5c470c5f4a0e7614be491
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:22:06 +0000

    the whenever wave ashamed total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
