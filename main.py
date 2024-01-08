e7d131ac198223dee6633c2c4e01d0a39ffe61cc
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:00:33 +0000

    some recover destroy bite leg

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
