16e6bba94d81a378d5bd6ceb006b343328ccd4c2
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:26:35 +0000

    bringback soft study underneath dig

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_average(lst):
+        return sum(lst) / len(lst)
