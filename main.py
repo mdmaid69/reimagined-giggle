fd877a7759d3dc94fbdcda213fcdb971330c7491
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:46:21 +0000

    glad body train taste start

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
