b28bf4e9db70d7a3b3bc32d2702bd99f5f592f45
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:11:41 +0000

    instruction order combination morning announce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_buffer_info(array):
+        return array.buffer_info()
