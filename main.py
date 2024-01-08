ca24c7eb5a205995409630754a532fe58fbe199c
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:51:59 +0000

    yours rather worth operation reasonable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+from collections import Counter
+print(Counter("hello world"))
