fe7d0ffaacabee7cb2e84a724c0f9c356b368246
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:21:43 +0000

    sensible first disapprove instant careless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def clear_array(array):
+        array *= 0
