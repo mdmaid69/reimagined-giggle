0fdbb67bfc97c242c8d0f22b08061df0c2d9aa13
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:46:06 +0000

    pleased spray partner separate behave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
