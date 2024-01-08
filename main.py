29953d93eea216c3196ac727cb9c6a566c17c2fd
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:51:19 +0000

    scrap pile sense confidence now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
