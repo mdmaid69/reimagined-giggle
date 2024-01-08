cec08a30ede5039813a604d76eaeab5fd8de5267
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:45:25 +0000

    test scrap load accept pipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def convert_to_hex(n):
-        return hex(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
