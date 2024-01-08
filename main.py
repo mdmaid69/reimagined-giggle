61c216e522e498fefecc1a720f3281e1f7a727b9
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:45:51 +0000

    breakout show feel waste advice

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
