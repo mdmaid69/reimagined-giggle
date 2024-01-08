ccdcae4b7b4c1de62d0aa696eb66da2d53bb7680
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:53:31 +0000

    rule such deep your mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
