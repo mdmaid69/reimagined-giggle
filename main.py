33644ad6348a55461a19bbc9f20bad336e8287ac
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:52:48 +0000

    permission however expect burst failure

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
