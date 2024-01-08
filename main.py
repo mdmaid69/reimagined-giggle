1efb76be51de957b6c419a17a60c3154b90c57c6
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:23:34 +0000

    complete late better different file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
