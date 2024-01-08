c897192e4194d7522ce10fae03375b6f2db55b24
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:32:10 +0000

    inside study weak underneath round

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def add_numbers(a, b):
+        return a + b
