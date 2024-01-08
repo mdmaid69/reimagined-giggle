47b979ba64ea0ef013d10b378f30d4a419d1ddcf
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:34:51 +0000

    nowhere black hospital violent manner

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
