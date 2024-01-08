3eefda7a93842fee667a6c513f73993db9f9b150
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:04:29 +0000

    repair brush trip chance protection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
