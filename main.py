d26857572d1344790dda32d4f7ff970a1a308a95
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:13:15 +0000

    sail strength even doctor study

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def cube_number(x):
+        return x**3
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
