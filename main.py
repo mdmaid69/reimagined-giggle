06664246e8072d9038405a7206af0c24ae771d85
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:19:41 +0000

    Ww probably personal live sorry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
