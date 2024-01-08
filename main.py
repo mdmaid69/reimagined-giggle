226c2e50349b3f4b9a933292c9d457959ac1f275
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:37:33 +0000

    month trial dish peace sharp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import time
+print(time.time())
+import array
+def append_to_array(array, item):
+        array.append(item)
