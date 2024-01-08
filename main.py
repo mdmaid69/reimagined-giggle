f1657cebb829d971d0897e443dd19ad95b220bff
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:21:41 +0000

    him not turnup callup in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def greet(name):
+        print(f"Hello, {name}!")
