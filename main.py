488a367d2e4768dc659b13922d833ad9a5480be8
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:19:34 +0000

    practice chat nose Ff building

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+print("Hello, world!")
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
