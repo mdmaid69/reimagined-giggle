e31b389d5e9e5f0ca2350ef957e17628d35b70b2
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:26:49 +0000

    street exactly fortunate deliberately 566end

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
