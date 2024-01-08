f18fe9c2800d7d7a770939e993bf426bfaa67b00
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:09:16 +0000

    Uu bin matter pure string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
