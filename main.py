6bb2217298108344f35bd3b9f782b6c691a67c26
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:50:01 +0000

    combine turnon confidence variety probably

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+print(time.time())
