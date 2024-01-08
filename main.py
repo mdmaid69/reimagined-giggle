be83b3a06e97af0839a5f40aaa0c8fb208c8050a
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:36:29 +0000

    turnaway we tend pride quit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
