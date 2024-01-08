a2df9003fd7d4ed503c21c7a3fab8713c61e281a
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:23:23 +0000

    mood pretty ready aim arrival

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def multiply_numbers(x, y):
-        return x * y
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
