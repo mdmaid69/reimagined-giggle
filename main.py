d5b7615008c6a361ab4440f7aae3a218aad3f825
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:42:18 +0000

    finally nigga wherever upset sudden

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  def multiply_numbers(x, y):
-        return x * y
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
