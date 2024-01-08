cb3c6d91b61036ba14583bcb893609f474f53259
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:05:16 +0000

    criticism breath divide list them

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_array_length(array):
+        return len(array)
