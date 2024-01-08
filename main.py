b7d70b837124202fc7b4216852fbcf8424941bbd
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:57:19 +0000

    proof season comeback task feel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
