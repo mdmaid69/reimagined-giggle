2ca3a5ad5399fba906355ca4705dcfdc7d4e840f
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:38:06 +0000

    mine turnoff just steer we

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
