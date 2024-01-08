6094aa40315df0088ca554d54e570a79f7294ec4
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:26:34 +0000

    bend Gg excuse everybody talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import collections
-def create_counter():
-        return collections.Counter()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
