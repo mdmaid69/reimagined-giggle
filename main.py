6c06a227402e9fe1dc94ddbc7782d17fcad305eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:33:52 +0000

    tooth approval young old wall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_typecode(array):
+        return array.typecode
+def reverse_list(lst):
+        return lst[::-1]
