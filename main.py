8af70be346ab5d31ec7df1a0da8c7e4deb53c237
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:23:01 +0000

    holdup wind remove where unit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
