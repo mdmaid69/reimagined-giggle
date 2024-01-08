38500386a54a6496b1ad373c4ecbe9dae606adc8
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:10:42 +0000

    project here disturbing weak pure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def add_numbers(a, b):
-        return a + b
+  def subtract_numbers(x, y):
+        return x - y
+  def subtract_numbers(x, y):
+        return x - y
