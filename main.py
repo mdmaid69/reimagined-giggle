5da92b5d8f1329b29e3c825c04efac9e0f25d5fc
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:29:30 +0000

    takeout learn store snow past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
