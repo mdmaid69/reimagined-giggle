56ab9f320187ab7c0fbb071968d35ba474859d87
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:21:24 +0000

    prefer mess create cover worst

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def add_numbers(a, b):
+        return a + b
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_work(force, distance):
-        return force * distance
+def get_array_typecode(array):
+        return array.typecode
