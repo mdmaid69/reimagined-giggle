f00e50fd02b59735cd9bff43f186cfc4779e4984
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:13:26 +0000

    shopping sharp central target promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
