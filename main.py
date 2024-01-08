9ee7be021bf74c02070946e5ec43d82f88c48611
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:26:31 +0000

    interrupt depend display also oil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
