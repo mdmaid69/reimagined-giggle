ff26c2244a89a7a52d14f8711984aef1e98bf6bb
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:20:34 +0000

    climb wide rob ourselves round

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
 import array
-def get_array_as_repr(array):
-        return repr(array)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def get_array_as_frozenset(array):
+        return frozenset(array)
