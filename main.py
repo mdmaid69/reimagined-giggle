75d96cc1c13821f0911df203fc4d18d8eba60f00
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:33:18 +0000

    factory eat target male carry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
