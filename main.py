c359dc41ac3e881c3ff364c0468e76b4e3fc6998
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:50:21 +0000

    notice cheek rest Ii suggest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
