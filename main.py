5ecad0c84642889d23c95046dbad138607618bab
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:36:17 +0000

    nice available autumn touch scream

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def add_numbers(a, b):
-        return a + b
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
