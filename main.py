fc8d7a0d72552de17bb372b93fd64ec44c615d75
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:41:09 +0000

    company luck minor risk bang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
