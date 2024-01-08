d805cda66e453b49df95f1af37d5e3b787c311bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:01:45 +0000

    Ww accident amount goon him

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-print(math.pi)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
