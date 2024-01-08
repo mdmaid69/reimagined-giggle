2d7e5625d2baad92571e0f8fc41bcdbcfb92040b
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:18:09 +0000

    memory dig wealth mess turninto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_density(mass, volume):
-        return mass / volume
+def get_array_length(array):
+        return len(array)
+  def square_number(x):
+        return x**2
