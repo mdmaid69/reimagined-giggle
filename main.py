7f131f6500ab6059771d410ab42d2b00f3bc1ae4
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:16:03 +0000

    steam scene comein who slide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-x = 10
-y = 20
-print("Sum:", x + y)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
