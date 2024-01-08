0165e88a858c0680352f749496e13045a99f069d
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:28:32 +0000

    after soil rush manage come

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
