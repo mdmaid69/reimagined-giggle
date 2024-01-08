38f3163748dedb40bd3568828a7aaa5a901eb4ab
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:21:22 +0000

    in complicated passenger in attach

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
