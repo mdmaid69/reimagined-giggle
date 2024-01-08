88d8128d5a0a4940efc6179f21d42c7261e9a1c9
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:06:03 +0000

    language imagination arrange shout cough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
