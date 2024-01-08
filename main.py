21bc9e7b46823411ab8434128e9c1f12b0f80b1c
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:55:03 +0000

    close seriously year opinion rather

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
