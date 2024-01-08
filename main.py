133c55e83f351024fa8c7e15a29a3660380d9d80
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:12:40 +0000

    ilovedicks conversation justice explain best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
