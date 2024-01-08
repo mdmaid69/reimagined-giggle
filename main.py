bf2716f29ba63eb385ec170823316a3451667947
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:05:20 +0000

    apply travel shut represent 565end

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_area_rectangle(l, w):
+        return l * w
