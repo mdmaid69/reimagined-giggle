c93fffa9c78032b719df72ce42d397ca1791ab2f
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:43:09 +0000

    remove admit keen platform scrap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
