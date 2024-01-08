4c2600b3a26cb7ed82668b70e9f96162d4ac696b
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:32:53 +0000

    mine circumstance keepoff at eager

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
