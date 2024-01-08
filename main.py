8a6a65145109f3f5aaccce11295d6eea3acff554
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:30:05 +0000

    wrong lunch environment Uu reduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
