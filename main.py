4e8e334685206608136b7853b52c5c1f26acdfcb
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:42:54 +0000

    repeat autumn medium zero adjective

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
 def find_common_elements(list1, list2):
         return set(list1) & set(list2)
-def sort_list(lst):
-        return sorted(lst)
