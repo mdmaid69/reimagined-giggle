f47d8f51f15bb3a8ff04f5dfabc631f4f8d279dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:11:23 +0000

    replace carry shut friend strange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
