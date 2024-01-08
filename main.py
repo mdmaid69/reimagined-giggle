286622d02bae9bf7023fd49c8fcbd9fa8fb80cd1
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:12:00 +0000

    wise education entrance excuse connected

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
