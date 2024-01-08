40ca735c79e3ff58de925092f086267711c9bb62
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:56:41 +0000

    heart whole rate correct will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
