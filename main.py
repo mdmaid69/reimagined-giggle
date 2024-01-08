04771355b3e3786b096e2ad768b17b3b3c94add9
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:05:21 +0000

    rescue common thick trade noone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_length(array):
+        return len(array)
