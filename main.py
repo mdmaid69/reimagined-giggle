0076815efcc02ebbd64a186c977906f8de8b8725
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:12:43 +0000

    right list language soft cheque

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import sys
+  def get_python_version():
+        return sys.version
