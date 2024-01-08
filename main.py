9960f3120df13d426080cb3f648aa19afa5d269f
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:43:17 +0000

    fair declare spray giveup practise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
