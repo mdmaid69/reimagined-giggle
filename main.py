9f13573ba4779ce4182d1fd9d6664e79e498b45f
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:20:11 +0000

    substance true nonsense exchange contribute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_bytes(array):
+        return bytes(array)
