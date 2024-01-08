e61f891334e6fbf6aa5dcf0266a7b6b55c15f735
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:53:14 +0000

    bush begin kind whichever really

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
