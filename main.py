33fb8e0e954e69b8e73cbd3d1e0ca13364b7d8d1
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:49:47 +0000

    man target immediate slowly relationship

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_area_circle(r):
+        return 3.14 * r**2
