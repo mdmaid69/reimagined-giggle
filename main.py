90c5ebdbac14ec6bf4ad48f48bf7bcd615670222
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:12:16 +0000

    fashion fact school appeal advise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
