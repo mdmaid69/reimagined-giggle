65d23623874a45a1c8e1aaaff4deda89449821be
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:19:43 +0000

    task future on in tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
