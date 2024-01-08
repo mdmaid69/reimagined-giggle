6943dff04eb92f5a43331e220ca1e61e33b02dce
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:05:53 +0000

    factory nigga comein match name

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def get_array_as_list(array):
-        return list(array)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
