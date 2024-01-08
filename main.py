cd12b943478b902f116ca2ad4e308be5de5a8718
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:58:14 +0000

    gift passage prove represent hers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
