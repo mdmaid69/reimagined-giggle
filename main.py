26a5ce03f1f473081a58d66b31b928dbca314eb8
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:51:43 +0000

    away wind try jump rid

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
