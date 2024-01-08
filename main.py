d1f65b7ebb34e9bd1d56bd6e79faf8cd539d6a1a
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:14:35 +0000

    talk kind workupto complete safety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
