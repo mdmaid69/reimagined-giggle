4f0461fc4933ca6bbeb978195aaef5811a091250
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:35:39 +0000

    shout unusual grand disappointed both

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
