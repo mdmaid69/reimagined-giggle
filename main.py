875036f13b68b2e77f1a4bd2a6ecc55ac3cb8f44
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:59:38 +0000

    comfortable wide now computer change

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+name = "Python"
+print("Hello,", name)
