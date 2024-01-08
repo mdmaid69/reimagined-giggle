151b845ac6303ec6ab129003cad2de888b4aa5e5
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:40:12 +0000

    pleasant hard one past beneath

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
