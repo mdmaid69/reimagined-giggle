f10ac785dad29d0e85dbfa2552d35e3f3cf33dcc
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:04:16 +0000

    able face under pair journey

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
