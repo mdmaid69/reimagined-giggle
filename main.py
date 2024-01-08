1a428caeb23d6a37741ec0951cdefd020c926585
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:07:11 +0000

    black well goon gift formal

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
