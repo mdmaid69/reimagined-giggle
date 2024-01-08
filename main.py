affd02e4f11bd63544e9630fa254d77cd031b5dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:02:09 +0000

    detail mix deal pipe cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
