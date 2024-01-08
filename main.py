fec42540fa7a757f0d41bb242829c441bcc2ae2f
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:25:30 +0000

    dance abroad increase design seriously

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import datetime
+def get_current_date():
+        return datetime.date.today()
