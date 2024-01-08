be34d673684abd219089ddb0e94a56d85a61bb19
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:50:46 +0000

    somebody uncertain plan Qq object

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def sort_list(lst):
+        return sorted(lst)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
