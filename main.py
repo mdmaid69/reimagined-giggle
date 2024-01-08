c8d157a5d7bbe248fc0d949e15158223e828228d
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:10:36 +0000

    564encourage up bang anywayanyhow practise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
