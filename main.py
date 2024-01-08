34d7f9fcc1cc2522694b4c6e2efb253914e82238
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:12:12 +0000

    yourself violence stamp quality turnback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
