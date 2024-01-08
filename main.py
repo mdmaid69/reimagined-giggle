4a46979ca8e1d6ff49654f73a74547e97f03a0ef
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:09:57 +0000

    no message general day needle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import sys
-print(sys.version)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
