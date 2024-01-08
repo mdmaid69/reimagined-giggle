8846628579e69f6d720e97c445328dff82bb95cb
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:38:34 +0000

    possible performance evening return conversation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def get_array_as_str(array):
+        return str(array)
