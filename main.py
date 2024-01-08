c50d9b7489581013b55cc5fc80d7d9025bcc1a85
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:03:34 +0000

    beside opportunity weather impression judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
