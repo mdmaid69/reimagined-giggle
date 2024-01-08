c215829a57bcff55596478d90a4f77a6502edfee
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:10:12 +0000

    callout terrible gather disagree nurse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import os
-print(os.getcwd())
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
