c54d7477ef39986c3fd5b560ad463b7d786f156c
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:37:34 +0000

    shrink criticize yourself unite flat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def convert_array_to_unicode(array):
+        return array.tounicode()
