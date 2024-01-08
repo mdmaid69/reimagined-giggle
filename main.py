074a564a15057ae60736bca17f36da4425e150ac
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:11:10 +0000

    opinion circumstance song one meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
