5629707ae8d7f5cf5a22f8ede53d813f65d1086c
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:48:40 +0000

    under rub uncomfortable scared argument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
