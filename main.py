7ed17f79a248eebafa4fb52abb8ebd135366194f
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:43:37 +0000

    just pay tooth remain shoot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
