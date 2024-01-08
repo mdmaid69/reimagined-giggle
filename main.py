76cd160929bd6b3255d53e2bc90ad5b57b351f26
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:35:18 +0000

    properly furniture fight yet reasonable

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import logging
-def log_message(message):
-        logging.info(message)
+for i in range(10): print(i)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
