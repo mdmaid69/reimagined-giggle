94542f2121c7d2ed2d9d3af86681c655964a4656
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:51:06 +0000

    aware fetch confident wide regular

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
