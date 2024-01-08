9647db79dc9289f5bfcabaa12fd610d82aea6b11
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:36:56 +0000

    married get address on private

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_speed(distance, time):
-        return distance / time
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
