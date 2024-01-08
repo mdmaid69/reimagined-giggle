46b65522a807553cb265415afd338684afcec218
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:45:12 +0000

    second takeout amount theirs fortunately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_max(lst):
+        return max(lst)
