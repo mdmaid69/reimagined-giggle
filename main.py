a00286c243c0a4db4dde247728652bbfb091b91d
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:25:44 +0000

    eager television correction mine shoulder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
 import time
-def get_time_since_epoch():
-        return time.time()
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
