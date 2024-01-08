7f1aec875bfb40748e6880a2bda50d235a45c425
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:24:28 +0000

    schedule song warning rank another

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
