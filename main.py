f2be2898a973f665594043ee8a465b1a2c5e23c8
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:50:31 +0000

    ago polite rarely insist climb

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def calculate_area_rectangle(l, w):
+        return l * w
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
