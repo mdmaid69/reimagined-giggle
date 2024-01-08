27c5f80f559bf4aaafbe504211cab8e33dd59c5e
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:35:11 +0000

    embarrassing arrange slightly declare muscle

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_power(work, time):
+        return work / time
+def find_max(lst):
+        return max(lst)
