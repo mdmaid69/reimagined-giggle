c928e1e86842fa5ee54bbc00bb0f5398c48fd449
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:32:20 +0000

    strange sign beneath pull 568energy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
