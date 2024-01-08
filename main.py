f4fe7b165c78c710ee798648bb6db4dae2eff848
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:37:15 +0000

    we bridge influence report separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_min(numbers):
+        return min(numbers)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
