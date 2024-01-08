c7c1c663b52f00cf433545cfeb24cc3f74e1051f
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:30:38 +0000

    sort commit fault ashamed dick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def greet(name):
+        print(f"Hello, {name}!")
+import sys
+def exit_program():
+        sys.exit()
