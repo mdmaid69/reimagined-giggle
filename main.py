33d5d5096499b702a1da57728af3c270e2513e0b
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:08:57 +0000

    put bound effect bill reliable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
