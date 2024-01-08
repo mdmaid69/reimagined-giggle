63b181a54b2dfb211b09dfebab9ad3cd4f4e549a
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:54:11 +0000

    forever turnaway quantity roof city

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
