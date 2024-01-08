784a3146f76efd1df56297c5245b84da884359af
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:47:04 +0000

    hardly nor hand drop tie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
