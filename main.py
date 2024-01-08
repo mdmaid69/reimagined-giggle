48f4602da22ab395118b230f8d303bf33f1b7817
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:13:32 +0000

    hand Zz time force flow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
