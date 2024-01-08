44ee2a1814baf5b5aeee3b02ceddfcd5e29bfa18
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:17:30 +0000

    natural record poor fortunately rate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
