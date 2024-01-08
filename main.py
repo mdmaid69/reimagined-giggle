ea8f252e9d40acf56edc6de7aedb5a3f6f83fa77
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:29:54 +0000

    beat over expect get from

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
