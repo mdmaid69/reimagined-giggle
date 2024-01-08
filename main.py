d246933c47d18389ac7b3bd01d88809d107655b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:57:00 +0000

    less shine proper Bb intend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_speed(distance, time):
+        return distance / time
