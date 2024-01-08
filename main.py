1512c18b922e67ddb3e978d43228f1e4034b37fe
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:33:43 +0000

    platform order ocean Gg correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def reverse_array(array):
+        array.reverse()
