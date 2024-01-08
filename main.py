5bd2fb3e2bc02e22e3784ba0c14f29b65327d808
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:41:11 +0000

    embarrassed art whenever plenty brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
