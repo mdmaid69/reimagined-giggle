d83e5feba9009a681df465341e8c2bd1f3811fc7
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:30:32 +0000

    raw unfortunate clearly cry respect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import time
-def get_current_time():
-        return time.time()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
