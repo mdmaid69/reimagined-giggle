a81e929c56d7bcaef078cc26bb4051da5a4ede26
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:28:29 +0000

    truth respect idea body objection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def get_array_item(array, i):
-        return array[i]
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
