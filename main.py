93161ee3976ecadd41509c6b8cff00b0e6c22097
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:44:16 +0000

    scrap train arrange runaway comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
