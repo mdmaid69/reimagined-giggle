298f85ee5b1f5b35829c251f418146ad8fa530ca
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:45:56 +0000

    wild square onto arrive along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
