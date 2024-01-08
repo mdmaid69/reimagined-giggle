a88f5dbfe7584741a4ae67d43c2da6fa60c71af0
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:58:46 +0000

    Mm block thing clue improvement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
