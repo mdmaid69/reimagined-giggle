d293e7cdbd42e6ead92a09cd763dfff83df6d49e
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:53:01 +0000

    curious on time fast habit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
