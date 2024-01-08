2e6108a7e4e59c5d88315fcd331bce18e4208400
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:02:31 +0000

    absolutely piece slip quiet themselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
