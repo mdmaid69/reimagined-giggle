aa5bb3851f35d78f4de85da7dfedcb330437473a
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:23:39 +0000

    plain snow combine steam demonstrate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
