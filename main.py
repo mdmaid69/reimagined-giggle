7685221f954d88fe4b15b75318f94c668ba67fcc
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:18:22 +0000

    copy mixture deliver it move

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
