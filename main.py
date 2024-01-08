09dcc087ceeda8d53c88dfc21ac10ec9a9e2fc60
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:33:20 +0000

    second story bank wake recommend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def convert_to_json(data):
+        return json.dumps(data)
