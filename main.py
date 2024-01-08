064fd33967689b65a0bd0ec6f819d9ad4d50ed5e
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:35:24 +0000

    wind turn before century leg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
