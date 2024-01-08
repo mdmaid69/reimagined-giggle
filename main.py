ccc156fbea6c2e94708b10c21b0ab81e1a529680
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:48:32 +0000

    excuse thick well noisy tune

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def square_number(x):
-        return x**2
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
