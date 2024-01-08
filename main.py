8f52009dda5d95aa29b063d148db3d425df44620
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:47:58 +0000

    tread hurry march grow spot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
