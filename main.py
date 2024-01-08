d2c5ebd912cddfaff322edb56cdcf36dc11e92a9
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:14:25 +0000

    turn nurse owe today right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
