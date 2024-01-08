c1e6efc26d266ed8220d54074a21d50ccdad9da9
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:22:16 +0000

    range suck push opinion decide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def get_array_item(array, i):
+        return array[i]
