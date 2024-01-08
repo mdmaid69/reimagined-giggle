ab674c871ab0488a919a27a4ff3f40d0bdb901a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:47:17 +0000

    when now this young science

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
