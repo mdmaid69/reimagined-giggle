3ce4a81cdba4396e95d05e4b66f3aea87a171ff0
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:20:54 +0000

    whistle fire class right cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
