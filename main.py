3ad0e364d5fc368425290420686f24b8333ae8fd
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:26:41 +0000

    gradually mention base Ll knowledge

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def reverse_array(array):
+        array.reverse()
