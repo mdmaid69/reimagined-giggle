d038dd51b5171459047b293cf50eee090bd6689a
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:43:18 +0000

    scream pipe refuse pure cost

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 def calculate_current_ratio(current_assets, current_liabilities):
         return current_assets / current_liabilities
-def find_min(lst):
-        return min(lst)
