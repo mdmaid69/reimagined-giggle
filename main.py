05183df2f1145857ec44bb46b27310bf0c18a467
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:47:48 +0000

    cover man behind swing trial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import datetime
+def get_today_date():
+        return datetime.date.today()
