f98127139536a42c3250fd7f502f0775ba05a96d
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:16:13 +0000

    chief before shock politics tip

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def convert_to_octal(n):
-        return oct(n)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
