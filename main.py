e87e47b96c65d8ad71f92702c69f2f31a210be24
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:14:46 +0000

    promise match off record afternoon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+print(time.time())
