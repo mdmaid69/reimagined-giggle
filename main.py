9002b458a5cbc5df454d3d2d2ee97d28d1142847
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:12:30 +0000

    length world whoever expression pocket

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def is_palindrome(s):
-        return s == s[::-1]
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
