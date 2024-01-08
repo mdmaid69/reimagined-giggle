f599c0270106aba286da45894c0b42b4eade73bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:46:57 +0000

    tip appeal OKorokay something hollow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def is_odd(n):
+        return n % 2 != 0
