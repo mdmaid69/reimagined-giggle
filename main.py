3df2d20e754dd0273e41d69c626bc7ef799e87e3
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:31:14 +0000

    birthday shoulder platform but continue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_work(force, distance):
-        return force * distance
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import time
+print(time.time())
