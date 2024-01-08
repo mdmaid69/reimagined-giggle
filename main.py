dc910cd456e567fc6d71b6d43f7892127ff143a7
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:17:27 +0000

    undo try plate age moment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
