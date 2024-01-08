558458636858ede9d631c434736b3bbaa1b9c8b1
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:41:40 +0000

    snow skirt habit connect surprised

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
