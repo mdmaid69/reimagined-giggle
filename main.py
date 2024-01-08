8677137c5541b34eebd1fe79143c74cee5a844b9
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:28:51 +0000

    cook come tragedy emergency treat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+x = 10
+y = 20
+print("Sum:", x + y)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
