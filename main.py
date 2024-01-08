a4faf20e6eff1e26bb76dc1ab823be6f865b1be2
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:18:58 +0000

    change terrible pollution aim range

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+print(json.dumps({"name": "John", "age": 30}))
