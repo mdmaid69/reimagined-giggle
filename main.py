44604b2aa68bad41d709fa06e1992c038fbc23cd
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:32:40 +0000

    again disgusting advanced talk address

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_as_float(array):
+        return float(array[0])
