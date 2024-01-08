159f8de3d14ce048b89512e6e3560111cc76d718
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:03:33 +0000

    workupto disappear part copy divide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
