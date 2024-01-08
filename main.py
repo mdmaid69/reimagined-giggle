e6158c0fb1945aa87cd3020564349e1a73c08cd5
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:46:38 +0000

    well mistake warn spring visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
