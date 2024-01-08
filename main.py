eecffc64650f330bb840660ce16eb0af1442ff9d
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:05:34 +0000

    let short arrival gentle polite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import math
-print(math.pi)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
