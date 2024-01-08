f884e64a331c3629cf525f751f527a773e4f6de6
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:53:22 +0000

    introduction protect wild among environment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
