ec9ae719096fb4f893ef0db5a06672a4e936704b
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:05:13 +0000

    relation distance operate hurry upstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
