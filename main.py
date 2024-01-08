7df5034a94a883990ac214985b00f1cdf7592608
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:52:59 +0000

    too chat permission chief exactly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_max(numbers):
-        return max(numbers)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+text = "Hello, world!"
+print("Uppercase:", text.upper())
