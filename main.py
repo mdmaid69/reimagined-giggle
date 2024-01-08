6a3332c9880082233ee1b7edf15a8c158e98a035
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:22:32 +0000

    cruel design fierce amount cost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  def calculate_area_rectangle(l, w):
+        return l * w
