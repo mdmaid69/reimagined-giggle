df160650d787b1e199f0739a43de0abb1a5c0ba2
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:02:36 +0000

    keepup course brief tend special

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
