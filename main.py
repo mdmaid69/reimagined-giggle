a53caac5d852aed66a2eb5b10980552a7368ea7b
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:12:00 +0000

    reach scream extra wet pain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
