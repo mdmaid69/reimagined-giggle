a0437687786f58254bc1f2a6d5e538afb325f563
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:40:24 +0000

    hot of sure contact nurse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def split_path(path):
+        return os.path.split(path)
