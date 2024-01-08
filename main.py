f1150e2f9539209faccee02143b0fdfb91037cad
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:01:22 +0000

    Mm effort offend estimate must

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def sort_list(lst):
+        return sorted(lst)
+def calculate_pressure(force, area):
+        return force / area
