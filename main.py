6abceb9f55a71c8fff4705a6891da8463d7d2479
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:53:38 +0000

    callup paper turn plant unite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
