e2819aa325c12479a79540be5254c153637eb088
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:05:17 +0000

    reduce quick matter pick delight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
