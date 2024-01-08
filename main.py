dc64f42da659d12649a03b7f569d2621a8ae3edb
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:03:39 +0000

    entrance month undo ordinary keepaway

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def is_odd(n):
+        return n % 2 != 0
+def find_union(list1, list2):
+        return set(list1) | set(list2)
