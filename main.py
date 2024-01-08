b7332449bdb0fbc30ce49bc31ae5732590df1e76
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:46:28 +0000

    respect loss opportunity classroom depend

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def remove_directory(path):
+        os.rmdir(path)
