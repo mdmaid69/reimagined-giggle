4ab13786260a8aafe13f9687e6daaf3be86666fa
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:38:05 +0000

    design Bb main mine foot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
