45cf19aae00fde2f52f1b071acf7c5180158f841
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:26:04 +0000

    effort get wing pupil keen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import array
+def get_array_as_float(array):
+        return float(array[0])
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
