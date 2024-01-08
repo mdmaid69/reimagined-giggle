ab0388c6dff0bf1ff425f2606cce0ce4cd08254b
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:39:40 +0000

    finger regard doubt shallow clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
