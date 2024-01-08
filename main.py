4d14ee0546abff3a80f7292b512bac0d6468decb
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:27:30 +0000

    anywayanyhow dream constantly must expression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
