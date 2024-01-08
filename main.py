76c84a3da21a189ec83eccf6da0cc39c9d90c698
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:32:10 +0000

    breakup thank past occasional trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
