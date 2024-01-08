e9845037498c0d3242dac30e4d2eaf9c37fb7d48
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:52:42 +0000

    squash promise preparation water Vv

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
