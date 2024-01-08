52b4d3cbdc8c79829352450e6450529b84873fd1
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:17:44 +0000

    minute whenever awake get lend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
