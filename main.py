026f0f1bfa28c9fbb53049a88d833d68db911594
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:23:30 +0000

    nerve breakthrough act effect confuse

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
