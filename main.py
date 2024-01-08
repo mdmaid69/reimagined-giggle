672ef7ede23974fa0cc70cca77ebefe0a506f198
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:34:47 +0000

    tune correct reach trade upset

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
