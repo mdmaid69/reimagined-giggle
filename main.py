c23ae57bb1f614134253d0096d04e219b363130d
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:07:14 +0000

    almost week independent blame content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import math
+print(math.pi)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
