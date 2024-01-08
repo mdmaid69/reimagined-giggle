b98429e58ef742e075870772ea2ab99f0c5d49bc
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:38:10 +0000

    crowd start further realize but

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_circle_area(radius):
+        return math.pi * radius**2
