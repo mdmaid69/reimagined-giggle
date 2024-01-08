562a36aa8e58e84d27b0f927091d93a468109761
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:56:40 +0000

    trip emotion different belief snow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_power(work, time):
+        return work / time
