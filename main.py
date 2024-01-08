e3b8e3fcafe57a63a0fbbe258a22096c46d15485
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:24:22 +0000

    government ring relief shade last

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
