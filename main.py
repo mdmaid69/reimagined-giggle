9d4a22000bdbff0751469a5ae3615fbbec039cbf
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:29:14 +0000

    boring none 567enemy fair night

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
