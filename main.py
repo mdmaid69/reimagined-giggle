e43a6bdc0bfe2ffbdb45a6768af751382a59eac6
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:48:04 +0000

    recently history pleasant short whom

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
