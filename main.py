e59474f89643dfa87f440c2071c2665f6320f7e3
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:16:39 +0000

    treat turn some left bad

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
