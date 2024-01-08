7543dd32bcb6a56a98f0dd12d3bf39a7d3ce35bb
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:23:18 +0000

    takeout dress separate excuse choose

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
