0e616a272911a410224311d44ac9f651c4442d3e
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:46:29 +0000

    fear achieve tie impatient shrink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
