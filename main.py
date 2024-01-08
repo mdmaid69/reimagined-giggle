d06d47126fa794edff9e00b4704c9e5460b00811
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:44:29 +0000

    cross sleep pronounce hour innocent

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
