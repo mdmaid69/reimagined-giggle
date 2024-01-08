acda8c9fc79dc303609f8a8f1ca5c0f47950f39d
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:50:04 +0000

    more know plant right limit

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
