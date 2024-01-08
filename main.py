f12eea838708b6e96e7e7e5d097de2f460352f90
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:12:43 +0000

    want space basis substance 572enter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
