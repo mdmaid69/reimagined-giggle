b32aa898690dd98e6e71e63fe69a7464c6effccd
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:58:07 +0000

    present rough care expression most

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_density(mass, volume):
-        return mass / volume
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
