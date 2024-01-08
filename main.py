55a71bc471cc435b7d34bbc0127be2492570d5dd
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:10:46 +0000

    themselves come unusual fight exist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
