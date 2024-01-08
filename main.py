4fb621f314749e2721869234ce65cf2b574e1e50
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:50:56 +0000

    naturally influence mixture someone but

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def greet(name):
+        print(f"Hello, {name}!")
