6619451ef05c84dd4e3d72f9286add5c6c87b18a
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:58:28 +0000

    concentrate expect comeround hour joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
