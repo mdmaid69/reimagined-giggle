55aad9772fc2805f2ac9d57bcb59c6272afeb2b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:53:11 +0000

    knowledge poison turnback branch stretch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+print([x**2 for x in range(10)])
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
