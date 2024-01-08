f77a6149d84c44626112d0b02450c2d65c3869ad
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:06:29 +0000

    motion talk active scream drop

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
