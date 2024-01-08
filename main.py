c4dea07e616ef6150b9f2e86e7c76d3424ac9983
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:06:25 +0000

    afraid definite finger escape ask

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
