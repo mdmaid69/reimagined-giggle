e00ac68db6c50cdf47e02e0145033c311c0bd966
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:12:21 +0000

    remember sore suppose during unable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_density(mass, volume):
+        return mass / volume
