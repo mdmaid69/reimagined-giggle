320ab463a1590bc08a7ae5a0c78189ab2434e738
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:27:50 +0000

    571enough lonely shoulder lay mainly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
