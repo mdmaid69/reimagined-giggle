a732182af28db253ec97eb190d17616bf07d68cb
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:23:24 +0000

    well breath 571enough hour away

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
