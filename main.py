c15b24d7af1ee7fd5b91e3703266a0bfafd8a085
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:09:56 +0000

    charge before close fairly stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
