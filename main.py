c472f47ca596ece3ce4083cf441afa19e8d7d4ff
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:06:50 +0000

    sometimes appeal spray guess tip

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def is_odd(n):
+        return n % 2 != 0
