39c8c7b98cf18424e0dea0ce371e1b97da647f85
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:49:49 +0000

    might strong characteristic full improve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
