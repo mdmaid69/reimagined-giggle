5a80ce446591a687aa3a969bf0783f98429b5e89
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:10:09 +0000

    through frighten concerning past offer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-name = "Python"
-print("Hello,", name)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
