580c61cae297617f53b09c1f55663752c1321d8c
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:25:08 +0000

    bush variety board complicated issue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
