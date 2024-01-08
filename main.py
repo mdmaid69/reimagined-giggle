f697d7db3cbc4b94676263fd1c8d2e8c91ae0038
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:41:22 +0000

    edge task our command himself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def count_elements(lst):
-        return len(lst)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_int(array):
+        return int(array[0])
