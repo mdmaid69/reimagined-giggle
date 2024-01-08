736d54422b993a292a4d3e2f1e41c1eefb9128da
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:21:24 +0000

    come book independent abroad sense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
 import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def check_if_array_contains_item(array, item):
+        return item in array
