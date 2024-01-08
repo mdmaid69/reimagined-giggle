4e00939036f227eadb9faa4219d25009c05d4329
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:21:28 +0000

    mail expect cover replace need

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def count_words(sentence):
+        return len(sentence.split())
