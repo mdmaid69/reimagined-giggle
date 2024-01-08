326d58a66f1e89874c32c4b2da7e4a6e1f90f799
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:39:44 +0000

    loud rough throw ask relief

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
