c3a9b6af4331807a68eca6735262cf6b1be78e34
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:15:57 +0000

    scared after female art work

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
