4fb50066224d92c9a13d76c8e389a3bb8d65564a
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:46:05 +0000

    fill breakinto label worst prepare

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import getpass
+def get_username():
+        return getpass.getuser()
