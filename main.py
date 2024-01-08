d8ac0be1d5b63fa6304fb5c3ece4d56c6769bfd7
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:16:14 +0000

    miss water appeal seriously confusing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
