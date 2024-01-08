89a1513d50f7b2fde44b12cbea7cd7a4eaaf63a7
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:00:18 +0000

    only get compare do equal

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
