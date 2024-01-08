125786a222494c6cc1b9f7331ac89a4b893d5d5b
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:06:44 +0000

    Ee behave everything class harm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
