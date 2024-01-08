0f48d720f6f1305b507fc39efc1f4fe1f1e053a0
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:13:50 +0000

    pleasure solve rub middle shrink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
