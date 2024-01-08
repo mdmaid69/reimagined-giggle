b611dea50c56c0768de7231640b60174faaf6e32
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:51:19 +0000

    justice persuade even first contrast

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
