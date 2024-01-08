7bf6e276df9bee964c59a010fc70a6b362bcf09a
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:00:54 +0000

    minute window find criticism still

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+name = "Python"
+print("Hello,", name)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
