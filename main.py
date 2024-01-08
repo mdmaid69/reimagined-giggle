2699ca7ce0ab66cb7e78519070682c5469948406
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:26:54 +0000

    culture what pinch ask will

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
