51df489ae14a58ea8a05f8625c5f7120d39ce157
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:56:41 +0000

    565end open seem gooff recent

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
