27f39cbfeca136a76c5f84eb17db878288b95524
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:14:20 +0000

    protect increase bringdown centre but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
