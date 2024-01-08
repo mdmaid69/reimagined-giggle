3eb04aa27d8a2e4b8059cdfcc0d904298d48f514
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:21:11 +0000

    unfair ride past tool fault

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
