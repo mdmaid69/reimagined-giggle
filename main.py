2576cb6ebe6870590ab5689e12b60e089db2c9c5
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:10:04 +0000

    sense shock raw progress whenever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
