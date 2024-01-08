e2a28a14e7d6109d8c9ad290238df3eba0adad5a
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:25:29 +0000

    hit shadow fond standard trousers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
