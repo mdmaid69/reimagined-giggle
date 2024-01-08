c4cd2b04557da9cd77be3583e008d9e28102f3e7
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:28:21 +0000

    next equal practice production face

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
