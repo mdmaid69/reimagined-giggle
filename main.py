97f14f9c27a1ad29305f0da6d1c8ca71fa7f04e1
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:44:42 +0000

    depend fashion pocket voice method

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_as_str(array):
+        return str(array)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
