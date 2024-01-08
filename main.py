cca256bcb15e5e693bf318df0a6a2e22e9b9d3c0
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:22:20 +0000

    look flat combine delight smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  def square_number(x):
-        return x**2
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
