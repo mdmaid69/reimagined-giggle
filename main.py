a0e3f5b91f202a75eb1e61bc7ebb4d7dd534618e
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:08:42 +0000

    exactly combine air turn fashion

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
 import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
