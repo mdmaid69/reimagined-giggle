d5ab8fb420c4e5ca0738529e5482416975673e2b
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:59:05 +0000

    part busy certainly national influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_priority_queue():
+        return collections.deque()
