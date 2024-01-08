230668e1929416e9a5e634c8f3d84a75f4523500
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:05:54 +0000

    nervous possible turninto principle seriously

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def find_max(lst):
+        return max(lst)
