861492e0e6c543e72084ca7034ddb965ac06ea99
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:03:37 +0000

    afternoon find talk fast agreement

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
