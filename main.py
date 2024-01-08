be828d9dd78c4b6b8198600f3a73634c71af42ef
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:36:18 +0000

    confident between upstairs much plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
