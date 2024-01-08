1cc96c80eb17dc8ca222b4ce1886b0e36704320e
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:06:19 +0000

    emotion honest Hh up ignore

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
