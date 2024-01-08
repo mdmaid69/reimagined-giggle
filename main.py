fa2d6f2084df264ea747f8510e876c2610d96af3
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:52:01 +0000

    meaning roof eat workon fail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-from collections import Counter
-print(Counter("hello world"))
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
