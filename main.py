d490f2ff757ece49e3e5c7ff5fc98d40ee40b435
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:56:35 +0000

    no exam central start crisis

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def count_elements(lst):
+        return len(lst)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
