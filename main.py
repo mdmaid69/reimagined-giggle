91da6f5cfcd9657e3ac6a8f5eb23230455d7c855
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:03:39 +0000

    set hard beat commit doubt

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def subtract_numbers(x, y):
+        return x - y
