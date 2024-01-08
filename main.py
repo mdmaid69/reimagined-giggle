064680951a98a51020e036ca505fb4119d84c5ad
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:19:41 +0000

    type approve zero opposite preparation

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
