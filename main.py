f62d9208c54f47ca5e872f0abde8b36646b50bb5
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:50:05 +0000

    receive material interfere date connect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import collections
+def create_priority_queue():
+        return collections.deque()
