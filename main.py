1d2741f7fcabf58cd2867c8c84865472167f2278
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:43:06 +0000

    inquire describe increase callout quantity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
