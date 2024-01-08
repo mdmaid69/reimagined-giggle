64833dc0e0c19fce5c9be7a62a03eec851d5d142
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:47:26 +0000

    exam second belong stone rob

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import os
+def get_current_working_directory():
+        return os.getcwd()
