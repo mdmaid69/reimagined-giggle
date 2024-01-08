06c33f4697f8dc13e1e8207d2c541438e054c365
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:45:32 +0000

    suit do Rr near important

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
