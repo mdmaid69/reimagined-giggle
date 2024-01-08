ee6c9221e616b8a7756abdc9d04a3bd5fc6c18ac
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:38:31 +0000

    related press bye punish finish

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import logging
-def log_message(message):
-        logging.info(message)
+  def add_numbers(x, y):
+        return x + y
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
