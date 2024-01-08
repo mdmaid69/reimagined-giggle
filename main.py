b6d0703ceacd9f7a9223b2223c9b943bbc4a7c90
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:18:16 +0000

    reasonable centre side consist back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
