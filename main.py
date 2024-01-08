26b50fc4bfaadd36da815387ab7abae2ad0e7fbc
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:01:01 +0000

    programme shock refuse goout press

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
