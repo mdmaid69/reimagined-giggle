1107932bab8cf2ac85d24607d95591c9ae76bfc5
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:49:53 +0000

    every over set 564encourage replace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
