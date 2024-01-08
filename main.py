4fbff2e4ed3b4c8e0f0aae1c89ed4da09f0cc967
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:59:08 +0000

    board sleep age spin odd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
