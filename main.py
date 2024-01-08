52d595a14b345dfa3f78321a8bb30e2b90cb060e
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:21:58 +0000

    unfair far container constantly sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
