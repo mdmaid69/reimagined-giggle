32117f4f10268511268efd3069f29d9c0e2772a9
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:43:22 +0000

    exist wish yes dress sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def check_if_array_contains_item(array, item):
+        return item in array
+import json
+print(json.dumps({"name": "John", "age": 30}))
