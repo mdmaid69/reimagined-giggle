4d3f207f68a656c030ba033adc3b54d9ddcfa570
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:13:29 +0000

    birth 565end careless quickly pronunciation

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
