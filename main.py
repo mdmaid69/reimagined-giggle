995fa434e905f4c470a5d965139825ef56e0307d
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:18:27 +0000

    wrap rub react major block

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
