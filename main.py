a4386dfbfc12f575cbe7cc71d16bee8ec28ee046
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:08:32 +0000

    hang afford centre rub ours

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_pressure(force, area):
-        return force / area
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
