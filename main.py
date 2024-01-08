5b905c947cb7936e22f98320238a0ae089c7c5f3
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:20:15 +0000

    father holiday argue suddenly dish

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_power(work, time):
+        return work / time
