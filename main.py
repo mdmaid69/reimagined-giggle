527f2ecb3f6f031e61fbf4e5c316f8e4a8e92d33
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:52:15 +0000

    question hang pencil ring splash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
