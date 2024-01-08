7e8cc73014beb45df1e128013a1c51e960bac298
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:00:52 +0000

    mark when judge turnback word

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
