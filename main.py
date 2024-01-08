fba82f38292e8c2d73fe78fac6472ccb403994b7
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:58:07 +0000

    delicate here main use Ee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  def square_number(x):
+        return x**2
