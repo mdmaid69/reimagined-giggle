ef221da6c2a73762016aa8e9d3e1f6076cdc5ca6
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:09:49 +0000

    worth experiment disappointed everywhere explanation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import collections
+def create_counter():
+        return collections.Counter()
