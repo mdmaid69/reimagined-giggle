70e9421d7ce1a6d01c91d20113c38089a32f265f
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:12:21 +0000

    any identify suppose public goout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_length(array):
+        return len(array)
