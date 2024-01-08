941bac30008048b29ef198181e62f517783ea36a
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:28:22 +0000

    cap thanks ever his let

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
