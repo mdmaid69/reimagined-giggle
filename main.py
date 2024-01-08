87141cd5a1bceafa5049c4a57d05d996cc582fa4
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:44:02 +0000

    before another slip bite wrong

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_max(numbers):
+        return max(numbers)
