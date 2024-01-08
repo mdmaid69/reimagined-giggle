32847b03c479d775a8e31ef8d6e40846c77fa2d6
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:31:53 +0000

    suit loud wrong pull different

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
