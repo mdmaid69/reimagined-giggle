18415d6fff52fafc896e2e508e0d70c7a5b7e84a
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:54:14 +0000

    bright patience old cheap hurry

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
