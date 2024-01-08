fa03e1a7ef5551b79b4b34c34cbd9ae905fc6677
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:55:09 +0000

    common beside mix mess method

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def get_current_time():
-        return time.ctime()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
