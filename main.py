c498e6b6e4d8c2a5633c5cf29816561120466cb9
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:42:15 +0000

    please fast lower justice bath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
