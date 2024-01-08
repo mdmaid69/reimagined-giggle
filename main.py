74379df7a825126fcb3ec176dcd7fd5b10367293
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:29:32 +0000

    release smile behaviour radio laugh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
