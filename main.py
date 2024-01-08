d0e037dd1d3e436d1309f33421bcff61f2735e3e
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:25:13 +0000

    upset probably thirsty think development

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
