96c25d301e249bafad01533b5dbd99fb42657c1b
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:05:39 +0000

    silent want pick sharp emphasize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
