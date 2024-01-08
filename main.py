d82eb0540a80b354c5a15c83814000bd68faf7cc
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:18:25 +0000

    chance direction chief usually attractive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
