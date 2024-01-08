09ef46021c24b5b2eba6395e2fd79a73b47d2d85
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:59:05 +0000

    accordingto busy outside confused teach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
