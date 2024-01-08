35ed7781f1af42c7f5b9e9bb77f38ce635bbb51a
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:35:21 +0000

    attractive refer now praise lookafter

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
