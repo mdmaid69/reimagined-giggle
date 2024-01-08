ef0a5dfbcb4aa7cca7f912769e2df53e5aee054c
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:23:53 +0000

    less glass advice name slip

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
