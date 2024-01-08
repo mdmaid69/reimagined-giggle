a577948baad099f03db27dc3289c044b3d985a54
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:16:35 +0000

    profit fail hope receive joke

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
