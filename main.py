3e45e5685b435cff58ff9afeb2439b3de24dc09b
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:38:08 +0000

    recognize touch turnback trouble crash

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def is_odd(n):
+        return n % 2 != 0
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
