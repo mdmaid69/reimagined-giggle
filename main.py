e1f192f10fff571b499b7fea91d528928099451e
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:43:03 +0000

    worse profit roof murder plate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def extend_array(array, iterable):
+        array.extend(iterable)
