50b4cbd7ea74bb2bebf3fb7d786d89c5c13f3a50
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:04:21 +0000

    ground wise emergency fail upwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_min(numbers):
+        return min(numbers)
