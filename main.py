e93c2e533ed2c546261741f607762da57d174ba4
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:48:43 +0000

    recently stop past left lead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
