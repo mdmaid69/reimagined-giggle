8f53dc9ceefc6dab5075b7b459411dec01bd901a
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:54:38 +0000

    flow before precise Itself disturb

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import datetime
+print(datetime.datetime.now())
