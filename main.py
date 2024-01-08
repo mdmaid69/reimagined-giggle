404b37a210f4037c233767816c98f87e25d87c2d
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:44:06 +0000

    pause dig gift press across

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
