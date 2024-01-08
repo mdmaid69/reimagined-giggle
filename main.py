a064350c57005cb856ba6dc056bf8bf38647848a
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:51:34 +0000

    himself pour firm peaceful turnover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
