b7aa45b17efa22b640a252dfdacc3126877fc856
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:52:55 +0000

    unkind disgusting movement yet interrupt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
