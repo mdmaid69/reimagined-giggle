b315ac378ab352b25801841b73da9447e857b472
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:14:10 +0000

    share almost innocent laugh custom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def convert_to_octal(n):
+        return oct(n)
