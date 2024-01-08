e96bb708b7207ffbcb84b7f0c61291d6d7b471ce
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:48:33 +0000

    estimate line negative emergency wheel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
 def calculate_amortization(principal, rate, time):
         return (principal * rate) / (1 - (1 + rate)**-time)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
