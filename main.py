5eeabadd05807c1b0c3d673a433d85656e924139
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:57:19 +0000

    confuse shrink statement major lookup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
