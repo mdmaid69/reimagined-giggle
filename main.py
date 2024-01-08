363c095f870f430fe971137e2e26ced2d7873c16
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:56:24 +0000

    clear not team think juice

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import math
+def calculate_cosine(x):
+        return math.cos(x)
