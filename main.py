5d8f75d0c7dc74d3f65cdae46cca5a5dbb258e9d
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:09:06 +0000

    equal lot find colour workout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_as_list(array):
+        return list(array)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
