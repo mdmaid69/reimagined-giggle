a1beb2d544848448485a28d2c15c9eb439f9032d
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:17:32 +0000

    obtain name easy contain convince

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_current_working_directory():
+        return os.getcwd()
