d269587a7bd844882c03890ca1b45abdad0f2d17
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:34:55 +0000

    cut except arrive last wherever

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
