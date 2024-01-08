42ca7d9db2cfdc7260dbac607e02affa3f2357c8
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:14:15 +0000

    oil very close thanks no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_average(lst):
+        return sum(lst) / len(lst)
