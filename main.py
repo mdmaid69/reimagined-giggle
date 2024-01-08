c5bac17d3523699792b7cd9f8330dcc06c428dd0
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:14:46 +0000

    fill wall hate smell comeover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
