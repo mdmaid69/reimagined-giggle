f0e4fac0dc0a7e6463d958ed0b1012b6940a2fab
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:09:21 +0000

    note congratulations so he begin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
