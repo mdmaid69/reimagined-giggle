c949f004e0ac1ce6ac82f40c7e52575832d9f304
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:42:15 +0000

    skin authority tax related comedown

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
