546252c590dce9b247366b39bbeda0a97a8092e9
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:26:47 +0000

    foreign confuse catch like rare

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import time
-def get_current_time():
-        return time.time()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
