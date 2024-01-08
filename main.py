a868183d06e2e21ee064b8e329146f2579368aac
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:24:35 +0000

    centimetre upstairs benefit pattern with

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import os
+def get_current_working_directory():
+        return os.getcwd()
