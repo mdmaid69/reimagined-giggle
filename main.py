24c9b14ca82403efc10d15f04a8422e5b5bfb189
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:11:41 +0000

    spring Yy well visit upset

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_volume(length, width, height):
+        return length * width * height
