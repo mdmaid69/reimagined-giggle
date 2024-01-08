7db1bbc97eac36d85522c8cf79e87e5277384912
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:56:55 +0000

    quiet responsibility deliberately team whisper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def square_number(x):
+        return x**2
