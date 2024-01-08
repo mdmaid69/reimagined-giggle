f4a97b9b801668d02e9c41afe6a24c0db6d76781
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:50:04 +0000

    condition during distant other definite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def add_numbers(x, y):
+        return x + y
