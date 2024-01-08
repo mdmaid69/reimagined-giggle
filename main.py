3eb19d06a57024eec0684af930df6653a2dceacd
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:35:26 +0000

    come luck little force manner

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
