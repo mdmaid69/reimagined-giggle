be6799fa8c8e761704e503835222c14fbdf57df4
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:27:09 +0000

    around anything communicate shelter basic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
