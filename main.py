a961609d2619480f08e03fe0b36cbeeca38801d6
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:56:12 +0000

    serious comeout today congratulations makeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
