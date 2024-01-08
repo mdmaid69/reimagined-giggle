57a70d9737d59aaf1e65844cf91184c177a420c8
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:56:26 +0000

    holdon wrap clear survive inquire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
