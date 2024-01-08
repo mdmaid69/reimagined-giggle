288d92b649e2e19707450f19a0da5b169defcb49
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:39:36 +0000

    use balance situation approve runaway

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def is_odd(n):
+        return n % 2 != 0
+import os
+print(os.getcwd())
