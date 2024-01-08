5e2b44a437fc6e06bc3d33eeb88d1bda533feca7
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:41:00 +0000

    breakfast both straight whom freedom

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
