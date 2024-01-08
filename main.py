6590494f1c883968b24306c05c1cd6664d6df1a8
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:17:49 +0000

    available fire bet environment ticket

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
