0f5d0c88ad9267cde522db5d16f2b5590fe879e6
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:58:01 +0000

    although react schedule patience motion

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def is_odd(n):
+        return n % 2 != 0
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
