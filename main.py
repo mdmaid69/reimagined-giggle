d2f8fa62b8525e81daa05959b0d4fb9bfb2811d5
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:50:54 +0000

    before tend that trip Hh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  def convert_to_octal(n):
+        return oct(n)
