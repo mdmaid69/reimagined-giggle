fac2a674454f8bb615de21520b2d19a6dc731c4d
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:45:08 +0000

    then male wheel meaning perhaps

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
