5932f7249aff81cd3c09ad6151e99b7b0d1598a1
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:06:19 +0000

    depend hate rely describe steer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
