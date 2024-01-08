a5a73ad85f2864b3b5641cbe53511469d573744a
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:07:19 +0000

    under Dd sing march unusual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
