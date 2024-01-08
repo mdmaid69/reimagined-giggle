d3ef4d0831789cc7ace43af5913dcf4aaa33892d
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:29:38 +0000

    design fashion so where plenty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
