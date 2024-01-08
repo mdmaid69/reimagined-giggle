7d19319cc21cc8d372506cf871ea23371e5e01b6
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:49:02 +0000

    special few contact trousers already

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import getpass
+def get_username():
+        return getpass.getuser()
+  def convert_to_binary(n):
+        return bin(n)
