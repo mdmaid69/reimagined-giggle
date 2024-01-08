1dcb4551e35fd539bb5a37c7f5b8604e7f950e07
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:54:48 +0000

    before sentence lonely instant bye

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def sort_list(lst):
-        return sorted(lst)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
