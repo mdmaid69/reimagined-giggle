7e6163b9d6b6b8d78258d598d8a26987c2c337db
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:42:08 +0000

    inside rain justice promise imagine

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def sort_list(lst):
+        return sorted(lst)
