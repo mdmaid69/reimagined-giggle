4d92864a8576dbeea74bfa805c9041c7a0673a95
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:36:17 +0000

    sea familiar woman remain question

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import datetime
+print(datetime.datetime.now())
