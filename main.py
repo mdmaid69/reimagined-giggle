750f47a503fbcb98e79c4ce7b89dc26e27dd4ae1
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:34:33 +0000

    spread pile ring arrangement hurt

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def subtract_numbers(x, y):
-        return x - y
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
