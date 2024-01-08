eb3cae861633113152f4c0172e397215a15c3284
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:05:58 +0000

    rest frightened improvement hold suppose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
