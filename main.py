a5476b956dce380930503513e3a2d2ff935db3b5
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:35:50 +0000

    front afraid amount might exist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_current_directory():
-        return os.getcwd()
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_directory_name(path):
+        return os.path.dirname(path)
