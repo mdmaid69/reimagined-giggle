3afc7791b03ea5258e8e995f0e22f7ddf3c4ffae
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:43:14 +0000

    pretty wide giveback fence appearance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
