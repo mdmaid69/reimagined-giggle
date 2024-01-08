adb165f3caa2c21e119d0043ac1685ccd8c882d2
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:13:42 +0000

    everything in unable street estimate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import sys
+def exit_program():
+        sys.exit()
