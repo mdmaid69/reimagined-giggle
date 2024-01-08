e55eea352083cfdea7ac70d4e2202c0fb1d974ba
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:57:36 +0000

    please bang bound position worth

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
