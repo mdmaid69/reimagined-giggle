fd242a7942658d20dc3f7623a318dfba10e2c8b5
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:51:16 +0000

    family turn ignore better shut

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def is_odd(n):
+        return n % 2 != 0
