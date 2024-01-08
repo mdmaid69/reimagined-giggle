5aa7d2cd6d6c003a87ebe384fea31548b688e91f
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:31:44 +0000

    closely firm fall voice person

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
