5a8e54453a9e79850fd2f5099962c1556516cfe0
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:48:30 +0000

    opinion day needle no history

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
