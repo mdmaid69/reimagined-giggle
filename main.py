742db3d567ce65582e2aeb85f87602bbaed2528f
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:56:09 +0000

    permission middle strike interview moon

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
