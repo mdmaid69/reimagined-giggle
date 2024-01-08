9ee3eed1a1f023f3f029f29555deeb43024eb10a
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:15:06 +0000

    male at breakfast walk protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
