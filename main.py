827a7f9ebe888e56b5d7e7b92b7847d532169fc7
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:19:10 +0000

    tread Pp past discussion central

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+x = 10
+y = 20
+print("Sum:", x + y)
