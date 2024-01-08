9389650ad95abf95212e97a97f8427a1f19d93cc
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:03:02 +0000

    oil workon comment reserve dick

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
