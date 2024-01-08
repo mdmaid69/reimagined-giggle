11bfefdc13cacb82a5d259334cd6ab55e304e94c
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:55:02 +0000

    limit be nerve empty callout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import random
+print(random.randint(0, 100))
