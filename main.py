cc2029c53d16244a201179b755f393c9588fe57e
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:51:26 +0000

    concentrate sight foreign seat post

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-for i in range(5):
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+i = 0
+while i < 5:
         print(i)
+        i += 1
