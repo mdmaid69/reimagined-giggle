2f9be73c0bada05db6fb5fff1c115e2aedb63995
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:16:27 +0000

    angry characteristic sad miss cause

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
