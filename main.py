05c5bc12349f6d63800922ba4063d569864d6411
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:45:54 +0000

    environment personal bringdown keep about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
