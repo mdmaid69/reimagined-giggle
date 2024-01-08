d461323559267a37ff2fce3ecfe61e2abc4ad785
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:43:46 +0000

    left could lookup special start

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
