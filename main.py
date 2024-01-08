f2f56b4c48abc44d9253695c93d65d98ee22d0f5
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:25:52 +0000

    street interview busy expression bound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
