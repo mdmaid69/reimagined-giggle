4fbc8ae0afa80987018539e4f0fcd4a11e5db673
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:48:38 +0000

    book peace respect show description

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
