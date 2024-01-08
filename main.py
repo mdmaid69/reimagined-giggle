0505d873e948ddafbbffbcfa33f0361a4fc79dc4
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:59:41 +0000

    off print test young live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
