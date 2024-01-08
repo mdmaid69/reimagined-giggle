c168b71612a5d5212cb07fe0ae1d49c8762943b2
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:00:30 +0000

    else brief label steep adjective

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
