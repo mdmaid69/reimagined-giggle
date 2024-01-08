20f8f99c535698bac3fce46bc128ca6f1d8d8d12
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:59:36 +0000

    stupid back morning store immediately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
