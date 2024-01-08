0672be879745a880159870174064fb92c3f95b7a
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:15:32 +0000

    wire spare clear clean process

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
