fec5770a353bca096f4790803ee4bd98f6bb234d
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:21:31 +0000

    giveup whenever this low comeout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
