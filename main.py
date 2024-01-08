0be8108dc1fc3fca111d9a533f8fbd4f34da684e
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:11:56 +0000

    comeout culture speed water traffic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
