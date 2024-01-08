297c933ab3c4a4ef2a9282c688c07ef32027d7c8
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:36:50 +0000

    mark worse convince flat effort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
