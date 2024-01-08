c7b0ace08e46fddc7b9c3c5836c45690f7ebbabc
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:57:34 +0000

    easy worry terrible spot throw

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def sort_numbers(numbers):
+        return sorted(numbers)
