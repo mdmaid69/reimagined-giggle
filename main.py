f444c0a2792b6c3ecb8e7245bbbc8423121eb877
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:33:57 +0000

    glad cook save top underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
