2281ac00a538731a075c813318a4be9c9d179dfc
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:37:57 +0000

    large own separate below thinkover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
