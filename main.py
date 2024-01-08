d83134d1f106e434b395c0229853886b0a386d12
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:33:49 +0000

    Vv nonsense winter final pass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
