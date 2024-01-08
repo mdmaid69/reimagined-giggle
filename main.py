00095c4ca711cb56c3a91c7dd99f0e5bd9aa8408
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:17:36 +0000

    deaf successful long again answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
