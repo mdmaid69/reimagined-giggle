b4ea304ec94cdb48a6d2475a2bd4596d9fdd4aa9
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:34:51 +0000

    every path boat suck official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
