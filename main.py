bd56df000adad05a67a3b17a4940cf8f81acf3b7
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:58:56 +0000

    emphasize emergency tread big crisis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
