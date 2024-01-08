9002a9f93a739ba49f2396d980402973776059fe
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:42:30 +0000

    interruption often discuss stamp giveback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_length(array):
+        return len(array)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
