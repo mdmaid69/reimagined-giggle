290a9048d3188a944011c9ef63d023deb30e059e
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:16:56 +0000

    behind finger show nearly side

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
