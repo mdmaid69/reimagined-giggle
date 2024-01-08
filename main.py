3cddaa2d7f6a7a4c7a4fe472fd036f549dadbdb0
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:03:36 +0000

    person advise illness charge lose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-for i in range(5):
-        print(i)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
