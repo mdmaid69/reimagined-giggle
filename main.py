d019b03d9630fe87b7a2911a3c5bd8fc01cfff09
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:57:19 +0000

    lend have serious shallow degree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
