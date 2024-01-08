9adb1dabcf422c6a37be1cf6c902c05a24ed83f7
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:12:47 +0000

    similar breakfast independent wind path

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_area_rectangle(l, w):
-        return l * w
+for i in range(10): print(i)
+def square_number(x):
+        return x**2
