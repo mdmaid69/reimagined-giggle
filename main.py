770a7f7d41c4238492aa7210ab77ffbe1e4a7f18
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:39:16 +0000

    attempt circumstance meaning needle town

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
