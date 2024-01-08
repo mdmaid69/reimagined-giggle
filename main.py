d3f118f9c623e2fe0785c2e2627cbebe6155352d
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:38:29 +0000

    suggestion leave bear anyone talkinto

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import matplotlib.pyplot as plt
   def plot_scatter_graph(x, y):
         plt.scatter(x, y)
         plt.show()
-import time
-def get_current_time():
-        return time.ctime()
