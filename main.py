d25c3f87e19f337bc5ca966e2c9120df7bb0ceb4
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:25:20 +0000

    plan rich dance objection exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
