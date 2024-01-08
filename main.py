266cef9473a49514d3a07824fd371c2cc2755b48
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:07:55 +0000

    floor opposite fasten downstairs flavour

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def clear_array(array):
-        array *= 0
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def delete_file(file_name):
+        os.remove(file_name)
