9696032a8120c9e9bfcc1bf8ae49c907ed90474e
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:48:22 +0000

    embarrassing file Tt behind obvious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
