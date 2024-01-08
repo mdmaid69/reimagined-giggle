8131d3e79e0d55351d3c489803065b8cbee63c37
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:23:16 +0000

    deep combination damp window noone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
