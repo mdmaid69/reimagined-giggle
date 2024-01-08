a60f3c3ed5bfa54fc053ea3ee7e6198899e47365
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:39:01 +0000

    nature Mm plant job address

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
