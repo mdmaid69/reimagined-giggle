e34dd0486474806e575a5c76c808f8f9a8f9cdce
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:57:17 +0000

    then please flavour light turnup

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
