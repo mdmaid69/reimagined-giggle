61f4c919aec3194fc137b5aa65073286b70de49d
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:28:27 +0000

    neither turn war whom bet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import os
+print(os.getcwd())
