4bb7473a6665efeb3376bc1553f6b0a5c2c830ad
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:48:55 +0000

    quiet lean shout load achieve

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
