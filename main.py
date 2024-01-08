159b5294c2d75333fcb88e7d2ba9b405da357a10
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:37:49 +0000

    bit excuse slow direction belief

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+for i in range(5):
+        print(i)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
