dd479fb3f77bc7312cd108bd54f58cb34b31cc8d
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:43:54 +0000

    wrong callat share fire obvious

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+for i in range(10): print(i)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
