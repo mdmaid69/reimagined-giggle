e52ca699c8bb774fb1ac11b883d60572b5f1d1ab
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:11:25 +0000

    marry always authority why great

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
