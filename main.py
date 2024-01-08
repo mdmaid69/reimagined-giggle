7e8982984d38ce262600bd5ea45b94f98c78451c
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:22:40 +0000

    sweep lot surprise rule alive

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
