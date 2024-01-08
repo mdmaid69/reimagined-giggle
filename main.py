885ad0dd1411b42c2881a58ab83e484e9ffcaa69
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:21:05 +0000

    Nn comeup bother can glass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
