a2486a08b442b2f74d4c90ee0f25055265b9c186
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:13:52 +0000

    front fire no separate through

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
