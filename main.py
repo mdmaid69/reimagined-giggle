c0d6531dff89c8c9bef552258b9d686ea82cd507
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:20:14 +0000

    neither creature bye powerful name

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
