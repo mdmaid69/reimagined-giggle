fd4e892217f41e6253c9e8d5a0542048a4455cb7
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:39:37 +0000

    round tidy run delight anywhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
