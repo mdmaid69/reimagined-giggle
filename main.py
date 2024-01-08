273c283350167a0b191e575d9b958162b5f3ce50
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:49:06 +0000

    shopping excited below period buy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  def is_even(n):
+        return n % 2 == 0
