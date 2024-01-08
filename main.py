1adb5b7ad49afd053e0a462b04a454d9af14d1ee
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:53:49 +0000

    clear out test score please

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def add_numbers(x, y):
-        return x + y
+def convert_to_hex(n):
+        return hex(n)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
