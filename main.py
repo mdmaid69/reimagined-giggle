2f2528176b9d83f317b1cc3f18ec7fd132fe6142
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:18:54 +0000

    land light cruel pronunciation chance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_user_list():
+        return collections.UserList()
