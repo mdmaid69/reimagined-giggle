d3df5499103f3dd7836ab4b4f26d20bc0bd8197a
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:32:04 +0000

    before maintain skill along female

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
