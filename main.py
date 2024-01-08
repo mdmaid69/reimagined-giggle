70131e633ab433455893ad615c854b8867216973
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:26:04 +0000

    increase emergency rough this forget

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
