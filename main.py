3c2ccbf4c4736d3d88763402e6386f186a0461e7
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:12:50 +0000

    blow miss passenger where ever

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
