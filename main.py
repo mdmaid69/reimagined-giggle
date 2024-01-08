2bb65543d104b261c1e4118fbad797a6a4e6cffc
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:15:53 +0000

    praise dream runaway when personal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
