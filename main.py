344e6e6a1a7ea194031b24320138d4dddcaf27b3
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:21:19 +0000

    correct broad family become mess

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import sys
+def print_python_version():
+        print(sys.version)
