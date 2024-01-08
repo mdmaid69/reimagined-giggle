8097f65e2998bc13967c6167c4d2fb49c02c532f
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:05:16 +0000

    test lucky neat hurt fetch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
