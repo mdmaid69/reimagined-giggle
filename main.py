549a4c6dd79eebc932e823b73df0553ed54c0578
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:10:59 +0000

    store beautiful win mainly none

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def clear_array(array):
+        array *= 0
