bf2bba2b233733834dd0f3cdd0f7ca25f0322abe
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:03:32 +0000

    favourite risk pollution this failure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
