0144587c140a6b610b218cfb392ddeb5006a3d90
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:52:56 +0000

    wire sink stare goaway stone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-from collections import Counter
-print(Counter("hello world"))
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
