3393ccb6fe6e497440d24a4801c99ffeb6e84f83
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:48:06 +0000

    whom among but attach command

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
