1fba4b85677bc258661d04c84b0e77fa18abaff4
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:40:33 +0000

    decide well label open actual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def add_numbers(x, y):
+        return x + y
