94c8f6a3f391a7db12dc837321de115b788b4e19
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:16:59 +0000

    extraordinary motion fortunately private many

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
