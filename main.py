dd88d5e08d350d4c5a3f426cedd9bce9c8c908bc
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:03:45 +0000

    thinkthrough weak back through shelter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+print(sum(range(10)))
