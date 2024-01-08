e6badbbb401d1f11be1fcf41a35eb94fc0577e7a
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:33:54 +0000

    along visit Qq passenger centimetre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def cube_number(x):
-        return x**3
+  def get_base_name(path):
+        return os.path.basename(path)
