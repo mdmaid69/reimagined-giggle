646c1d297b7e8439a32bc65688a0cacb59475c85
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:55:28 +0000

    protest exercise emphasis keen car

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
