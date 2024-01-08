4df798edc0be6f3f485facb28b4454f53f861414
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:54:44 +0000

    throughout suit pride impression own

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
