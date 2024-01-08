628049f25b629a2ca92321cec31dd8d6fa067f74
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:00:10 +0000

    temperature tight street anywhere instruction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
