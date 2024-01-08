08474b00a9388aee0667d54af35b6cd1951e0f11
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:24:19 +0000

    approval event level dry among

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
