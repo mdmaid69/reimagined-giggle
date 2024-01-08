9794f02db65b3bf1f2e0b920d5c992217c3622b0
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:18:41 +0000

    rather stir and easy name

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
