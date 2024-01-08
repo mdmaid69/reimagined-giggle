ae4becb41d16ce1fb30829c6866ba2be14f949fb
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:21:50 +0000

    without formal used club figure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
