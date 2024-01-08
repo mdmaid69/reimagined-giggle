8808f7b64fa2ca58af55d12193c2cd64f055ba2d
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:45:42 +0000

    cruelty between everyone answer dress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
