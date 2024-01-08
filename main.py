1a7246a8b9aacfb5b6039d5d8806632da628f678
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:21:19 +0000

    defend her involve wheel morning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
