d040919605dffedf837d3e7f1eb49055b65114f4
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:16:09 +0000

    adjective explain ever damage choice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
