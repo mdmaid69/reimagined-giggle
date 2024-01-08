8e560a2a22a94df003b4ef8cf4c20218b3c2db0c
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:16:47 +0000

    sorry eye sound recover accept

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import getpass
+def get_username():
+        return getpass.getuser()
+def find_max(lst):
+        return max(lst)
