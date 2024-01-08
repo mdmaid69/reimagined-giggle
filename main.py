47df692644c655e784fa80d6f182150239e05e34
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:18:32 +0000

    there to familiar instrument direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import os
+print(os.getcwd())
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
