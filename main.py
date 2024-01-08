2011dd45a2fe0b110576347e5b4ba9fe47bf3245
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:45:41 +0000

    cost commit body touch exit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
