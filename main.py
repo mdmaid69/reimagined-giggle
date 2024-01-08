9be88e51276d9fbbb062d99dfe24667675760ef8
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:43:55 +0000

    quantity connection foot regular compare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_unique_words(sentence):
+        return set(sentence.split())
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_list_from_array(array):
+        return array.tolist()
