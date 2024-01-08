c91c72bb744b4efcde3ae2c4514d012bf450b3d0
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:04:42 +0000

    harm getoff smoke Gg broken

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-def greet(name):
-        print(f"Hello, {name}!")
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
