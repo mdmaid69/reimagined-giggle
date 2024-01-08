b5f1da55454875c0f55a7345827a022454a6515c
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:52:28 +0000

    interest smile hungry settle opportunity

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
