cef849307d32da6f65984557993dfa9e2c1df8ce
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:10:10 +0000

    by address conclusion back board

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
