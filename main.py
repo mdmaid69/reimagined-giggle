216975a8ed81d797625191a3afda5324b8fbb378
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:52:19 +0000

    contain guard shock dream rest

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  def reverse_list(lst):
+        return lst[::-1]
