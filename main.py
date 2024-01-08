f2f6658bbdb8090613b4054afe98732461d1a099
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:37:12 +0000

    commit understand innocent appeal remove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
