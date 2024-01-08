cab11099e8ec901e2ea7e6526f50ca9c7a06922b
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:32:36 +0000

    watch once keepup breakup science

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import array
-def get_array_as_str(array):
-        return str(array)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def check_if_array_contains_item(array, item):
+        return item in array
