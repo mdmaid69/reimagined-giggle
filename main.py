01cb015e7d8177224e2acd691d320a424a8a7ed1
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:04:27 +0000

    tax major total effect sing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
