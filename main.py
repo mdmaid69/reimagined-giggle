3a1a80dc9927c76bb845c8b6af045502b1b3e793
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:39:36 +0000

    count everyone protect early objection

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def greet(name):
+        print(f"Hello, {name}!")
