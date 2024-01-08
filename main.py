3a7205e1ba8641e07cd7617f1f330a1a186ad821
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:10:50 +0000

    mad permission die from that

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
