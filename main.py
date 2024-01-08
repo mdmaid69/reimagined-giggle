4fd18ec98bef6e8e9d4bab380877d4163d27f19c
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:47:36 +0000

    better illness neat share wide

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
