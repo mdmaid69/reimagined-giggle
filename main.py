1fffed51c831fae86d333a86dd886626deb8243c
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:46:00 +0000

    bottom production complain question between

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def reverse_array(array):
+        array.reverse()
