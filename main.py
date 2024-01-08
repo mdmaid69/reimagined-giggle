4106764a9541d3d52f9de062dde9dd4f362db2c7
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:30:58 +0000

    escape tidy then news failure

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-print(random.randint(0, 100))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
