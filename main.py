a5f0ba6a6120304c2e1b94419314212fcf46d316
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:04:27 +0000

    point square operate cause mouth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  def is_even(n):
-        return n % 2 == 0
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
