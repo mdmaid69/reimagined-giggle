9ca509fcb3fd327b0bf5b4d6f9592d574ad7b3f4
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:37:09 +0000

    degree declaration attempt happy complete

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
