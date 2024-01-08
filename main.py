9be4ce8ee139f5de0ce8048c81fb71586782157b
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:51:45 +0000

    several now feeling ride confusing

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+a, b = 0, 1
+while a < n:
         print(a, end=" ")
         a, b = b, a+b
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
