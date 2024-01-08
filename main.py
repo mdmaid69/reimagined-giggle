faa16697988b715f939ece16767e21d7b76ccf20
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:49:16 +0000

    physical brain freedom undo theirs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
