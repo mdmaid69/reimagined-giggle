6f1fbd19ba748af44597ee7a0891afe1338a863e
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:01:25 +0000

    profit fact set unfair well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
