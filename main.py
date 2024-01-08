29fafce5490f78fc98288bbc57b390b04fc0010f
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:02:20 +0000

    join fly design comment so

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
