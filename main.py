e73e21835fe45ba498cac3a9ed711b4f17aa4d1e
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:35:10 +0000

    trade will heavy team seriously

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+for i in range(5):
+        print(i)
