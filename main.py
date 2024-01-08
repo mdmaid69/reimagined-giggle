b5c376759cea12565f98e116fdf23c71bfab8109
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:50:27 +0000

    identify ashamed advise protest post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
