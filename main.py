5d419a552f5e3b8bdfa913e1f051191fd0c10a4e
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:53:02 +0000

    all month 565end height Gg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_index(array, item):
+        return array.index(item)
