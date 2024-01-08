24c143f8362867c0e58c2b2005b6641ee0d0baa1
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:00:35 +0000

    attractive listen pencil already true

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_repr(array):
+        return repr(array)
