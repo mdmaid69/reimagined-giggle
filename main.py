aa1ac5b424f9cdc9f171a04442af1179e88b9f18
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:40:36 +0000

    language sensitive remove method care

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
