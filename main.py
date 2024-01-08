78d76633c1f4d74decc63669abba20f1aacd5d6d
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:57:39 +0000

    sleep payment realize scared lay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
