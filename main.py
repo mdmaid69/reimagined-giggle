8f16efc8180cf642c88b7b8d2322ed176dd5dc98
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:00:18 +0000

    confused payment base getout fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
