6b3a350e9ddac1d2d360452735eb603a8b5fd00f
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:51:47 +0000

    will steep price characteristic win

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
