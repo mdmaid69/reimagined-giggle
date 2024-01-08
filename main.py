8c83fe73d3a23a22b3325cbd236feb068dab2c1b
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:43:00 +0000

    nearly me near small film

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def square_number(x):
+        return x**2
