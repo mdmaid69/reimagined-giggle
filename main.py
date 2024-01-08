d83d20942bb7101318d7bc0044412b6cd7ea782f
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:16:12 +0000

    Uu begin side sad fond

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
