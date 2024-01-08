139c1d7ce61316e6dff86471b3e59d98ff9940aa
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:20:50 +0000

    while nerve average weigh basically

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
