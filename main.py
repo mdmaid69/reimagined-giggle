91be31ff02b1f884ec2c21ec6adfea7d057f1220
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:46:35 +0000

    better case influence ask common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import random
+print(random.randint(0, 100))
