4fc4b97cfd6b79eb6a88dd7a2326aa03ffad6793
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:24:08 +0000

    produce marry thank window threaten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
