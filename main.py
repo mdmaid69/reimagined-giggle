68292a674589d812033d76174d877b74fcb622d8
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:20:21 +0000

    shrink admire stage arrangement character

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_item(array, i):
+        return array[i]
