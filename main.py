e17157958c96d9ce695b2620c793aafed4e53cdf
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:59:18 +0000

    refuse free secret them different

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
