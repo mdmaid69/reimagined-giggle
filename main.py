133dd71816438c5640c778651df4a87480bae4b4
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:59:46 +0000

    first hurry natural hardly strong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
