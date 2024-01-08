637c58a083ee4f4be6fce4663bdb8b573818aba4
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:54:30 +0000

    about hour line about here

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def get_array_as_float(array):
+        return float(array[0])
