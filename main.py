45f4a168ff98bcdb0ede4d606a4309dd67e9b675
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:37:56 +0000

    past brown wish talkback believe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
