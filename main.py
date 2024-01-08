bd215d420cb19dfd6ba9f38efec43f8374b1c318
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:45:20 +0000

    legal speak previous though mail

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import datetime
+print(datetime.datetime.now())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
