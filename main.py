df10630f8ef882100fdd7e0ccb4f63c0569fc99a
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:32:48 +0000

    except speak still owe interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
