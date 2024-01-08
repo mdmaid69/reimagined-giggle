0b2e7651bc3d55d9223e8f9b06b357194e3ed334
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:13:53 +0000

    laugh wall than alive closed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
