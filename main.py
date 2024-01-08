50b5210a703c4468bb2a2aae24f4270069760d9a
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:57:05 +0000

    back watch mention further attach

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def convert_to_binary(n):
-        return bin(n)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def count_elements(lst):
+        return len(lst)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
