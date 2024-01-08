1d21b57b9965531da0fe5366e38c79872c44ca3c
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:39:56 +0000

    workout comeoff approval remain smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
