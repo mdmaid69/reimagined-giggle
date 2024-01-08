99b4cb366306e69ec7204d64b497cfab7a8527bf
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:30:45 +0000

    book in difficulty very die

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_user_dict():
-        return collections.UserDict()
+name = "Python"
+print("Hello,", name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
