79d1f6b91a0c8c68fd6048380b7b4e875ac0492d
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:05:43 +0000

    tear your whichever shelter which

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
