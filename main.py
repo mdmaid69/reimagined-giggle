e3c749f24cf8686cab381159005c8b15e45a5936
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:26:18 +0000

    turnout through measure delicate ball

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
