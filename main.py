e66a13eb56c5b9208f1f18d4384c95320e3dbed1
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:31:30 +0000

    number shine board which material

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def cube_number(x):
+        return x**3
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
