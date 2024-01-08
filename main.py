ef92c59e275d6a78c36c95a319902d3576d88441
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:37:58 +0000

    shock blame smile wealth development

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import tensorflow as tf
+print(tf.__version__)
