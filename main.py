801de9e4654dc10756ecb58cc3fe4bd442d21a8f
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:38:15 +0000

    ashamed substance unkind guide mention

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
