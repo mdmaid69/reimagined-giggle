3ada2e554ecf92a38033fc4261c95f617a6734fd
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:17:43 +0000

    about lunch successful pinch fortunately

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def square_number(x):
+        return x**2
