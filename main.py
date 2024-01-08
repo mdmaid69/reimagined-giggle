2041f147435f333f67c98fe30ff94958cb1b5427
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:22:28 +0000

    flight plant fail afterwards demand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Words:", len(text.split()))
