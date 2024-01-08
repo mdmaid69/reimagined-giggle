98bed22e17d3758a996f47a8fd8b0034186873d7
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:14:10 +0000

    person pleasure peace afraid thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
