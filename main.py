6e318b1a8daf4a12e4938a77aecad8e2921e6f50
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:53:08 +0000

    concentration spoil actual thick good

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
