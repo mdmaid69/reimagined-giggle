26bd4bf3f850fc6d65f2015de30e419afba73dc6
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:22:15 +0000

    black combine confident arrive rule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
