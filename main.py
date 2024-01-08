ab1d5c5f173c90005c3aed227df8a524a1418360
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:12:41 +0000

    distant cut callup when doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
