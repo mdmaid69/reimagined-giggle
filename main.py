6929bd7f515a002956f22629891954f42b276c08
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:47:01 +0000

    worry measure platform second watch

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def find_max(lst):
+        return max(lst)
