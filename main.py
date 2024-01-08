4d97f6a46d8cb34731931814993afce2d035dde9
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:21:03 +0000

    it must disturb sound if

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
