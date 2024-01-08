187a7e8da0b1e3ac69b8b822b8e305dcec576a03
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:35:50 +0000

    walk turnoff turnout damp blind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
