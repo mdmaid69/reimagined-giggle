d7bf8af6432ca4b9b3c416c90b7506293c492786
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:57:30 +0000

    joke which check programme agree

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
