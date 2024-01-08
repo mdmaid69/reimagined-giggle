ca29902aefe3e5d747bba2741a750485b7a5b362
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:52:51 +0000

    world tradition spread land cover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def square_number(x):
+        return x**2
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
