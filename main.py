0dd1fb804e3804c9aec914dcc0e85ae5c9aa2eee
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:03:09 +0000

    take boil switch cut stupid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
