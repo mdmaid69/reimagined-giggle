fca6028485dee8f118a1c816327ddd5c610faaa4
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:11:06 +0000

    broken convincing nonsense related union

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import random
+print(random.randint(0, 100))
