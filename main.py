49d791ce12b940422cb16af373ab77b5253437b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:31:11 +0000

    wet progress tight shut cap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
