3ecf84b96d3429fa765775f4b90a03c949e3386c
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:31:04 +0000

    stone operation narrow male joke

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
