96ae2e169b64e9ac820e3c7599c3db368cc7f5b2
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:09:12 +0000

    shop immediate onto holdout the

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
