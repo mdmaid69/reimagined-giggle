ab3b56a83b52ce13c5cee1f97ee0f6d31eaa9c0c
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:04:48 +0000

    godown hurry recently suggest rubbish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import sys
+print(sys.version)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
