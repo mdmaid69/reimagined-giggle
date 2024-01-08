89719a552225b0ff6a7e161d2fe078f952db41dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:16:27 +0000

    571enough check shame justice for

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
