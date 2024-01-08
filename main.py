3d1c3c5dc741303680feff642e4e1409f2d548ec
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:08:28 +0000

    quarter whether mother crisis cruelty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def get_array_item(array, i):
+        return array[i]
