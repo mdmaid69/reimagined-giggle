e00cde7887a1b8692cc87f280b60ad5d093dcd3c
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:20:15 +0000

    side visit across different turnaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
