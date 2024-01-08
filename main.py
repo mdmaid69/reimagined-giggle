60af3663535de20523a64da158250a800dc8d5ec
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:17:24 +0000

    no system positive sail major

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import sys
-def print_python_version():
-        print(sys.version)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
