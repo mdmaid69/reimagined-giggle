a4c70312b2ecf8b3cd08b34a39db95c562c4f653
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:42:20 +0000

    happen within join manage introduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
