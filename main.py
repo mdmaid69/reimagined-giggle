9907cfab6f89b57a09e2d613c4b29a7d7054573e
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:04:19 +0000

    music capital spring home under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
