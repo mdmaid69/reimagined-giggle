a27e7191e585f5aa5adbbc7a6957af61f4c69329
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:21:58 +0000

    Rr unfortunate regard only person

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
