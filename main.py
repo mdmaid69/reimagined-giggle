d83a66b1136743a756571656e70db46a77c589e1
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:29:52 +0000

    nurse thirsty value trick manage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
