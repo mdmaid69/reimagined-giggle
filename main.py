00cac6d54ea3155fc9415190e4298906fd32353a
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:47:07 +0000

    under ground scare separate part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
