7853d2825103ee895f6d1263224158b26d171101
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:37:20 +0000

    escape pen laugh mess already

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_speed(distance, time):
+        return distance / time
