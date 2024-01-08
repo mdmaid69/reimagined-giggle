add25643f3de4bb9914bd69bb75230b88c982338
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:02:15 +0000

    immediately place supply health inform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
 import matplotlib.pyplot as plt
 plt.plot([1, 2, 3], [4, 5, 6])
 plt.show()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
