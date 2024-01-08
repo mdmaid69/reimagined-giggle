fbc91d81015fa9de50d879f0646669e8fb39a0f2
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:00:52 +0000

    gift turndown unpleasant goback(to) sign

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
