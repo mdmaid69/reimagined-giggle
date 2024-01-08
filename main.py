0db8bf70f3ebb617fdea55683213fa07eaa30c49
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:24:17 +0000

    undo deaf hit passage disappear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_str(array):
+        return str(array)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
