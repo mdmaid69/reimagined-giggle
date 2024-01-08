1e3667f3c75c54cfa33f9daaf408d16d448e783d
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:26:42 +0000

    adjective less feature empty tour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
