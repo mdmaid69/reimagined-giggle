27c42ae068b2cb47896f37c6536c32047bdc8ef6
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:14:37 +0000

    exaggerate decide weekend left separate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
