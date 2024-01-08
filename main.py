634ce7b2540302c8035cc8fc42ae5bc77b22d88d
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:14:19 +0000

    modern common acceptable sleep die

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def count_words(sentence):
-        return len(sentence.split())
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import sys
+def exit_program():
+        sys.exit()
