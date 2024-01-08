5dd510e0e07de0233f1948d25ee2e343c1652472
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:52:03 +0000

    exam 566end behind start music

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
