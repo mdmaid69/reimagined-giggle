107ca2329f9d7d81960b30c4bfba82e09b566299
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:56:10 +0000

    disapprove Cc better stop embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
