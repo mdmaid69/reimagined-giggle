4581cf8220bf8e478750199032679b6776df9c3d
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:01:16 +0000

    there birth which frequent afraid

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+def find_unique_words(sentence):
+        return set(sentence.split())
