d44916a12dc69b54ef3256c94c7e08ae6237f940
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:53:15 +0000

    standard song busy niggaacademy avoid

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
