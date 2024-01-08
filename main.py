aba4ba9af5ca3383703ff70302cb84d5cee0de61
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:23:15 +0000

    flow neither minimum supply change

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_pressure(force, area):
-        return force / area
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
