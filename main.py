2d52cb3c4cee5e9f8cccb2d4d8deade1dcb531f9
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:11:42 +0000

    already language some demand match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_current_directory():
-        return os.getcwd()
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
