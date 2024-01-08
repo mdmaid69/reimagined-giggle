8bf7aebb31e4276b8a896abfd58fc32166776dc2
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:18:57 +0000

    train consist window traffic unlucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
