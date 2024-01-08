f92998c819ef497ed01a6b51d5c773211047651e
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:34:36 +0000

    freedom steam able emphasis set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+def exit_program():
+        sys.exit()
