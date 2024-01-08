034cdb303c77706a4325aaace6fe5199043cf33c
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:36:26 +0000

    recently nothing stranger comment spend

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_density(mass, volume):
+        return mass / volume
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
