4ae7586175f86a4102f9ce20555c44ffa0552858
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:34:00 +0000

    drop draw neither Tt concerning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
