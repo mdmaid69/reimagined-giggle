24773c1a4bee290504a552164b2995f7a139deb5
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:48:01 +0000

    spin exchange away fork shock

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
