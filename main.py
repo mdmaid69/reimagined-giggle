107bebed12023af56c64479762ae580db40f7a22
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:10:29 +0000

    proud tidy I variety identify

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
