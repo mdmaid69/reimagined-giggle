feaf4c4a7ce796fb68629efea3ff3fb8f0b35d16
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:28:16 +0000

    owe lot doubt demand least

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+name = "Python"
+print("Hello,", name)
