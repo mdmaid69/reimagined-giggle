b3b2f3884f9a33bf46bc5738bd689ad249abee55
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:30:56 +0000

    breath apply university direct list

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
