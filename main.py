b406cf053233804984b4a45204893bb37e44df8d
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:39:54 +0000

    behave money game wash dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
