6473308d7116484132789b85f0d4ae87819b1322
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:46:08 +0000

    rescue makeinto plant eat odd

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
