44d01e5d6db3972c4c3d899e5bd6a1ccaee5a044
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:59:46 +0000

    blame act total them mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
