1bd2d4849608ed8f8b80f7e71926eb85558c1d95
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:52:05 +0000

    favourite dig tell suffer explanation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import sys
+def exit_program():
+        sys.exit()
+import random
+def roll_die():
+        return random.randint(1, 6)
