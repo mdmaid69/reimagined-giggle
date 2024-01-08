b2820e95b788095af0b9ce8f7b21c091c18fc5ff
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:40:52 +0000

    stare hurry balance shade exam

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
