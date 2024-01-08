53d421205b3c3d05ca3755bae4c3390c6d8fa7f7
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:54:23 +0000

    mad week plan giveup finger

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-import logging
-def log_message(message):
-        logging.info(message)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
