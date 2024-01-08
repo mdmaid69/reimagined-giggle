1c85f8b05f870e547ebcd27ce1d9ec8cee19da05
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:17:18 +0000

    around refer belong twist sweet

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
