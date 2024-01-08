d066a721303d94a85560e84afcc347403304287a
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:23:14 +0000

    flow too make depend home

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
