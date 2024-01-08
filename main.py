082780f0d24652ab060ee9cf2c09966890faf6d6
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:52:34 +0000

    broken deliberate often our judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import datetime
+def get_current_date():
+        return datetime.date.today()
