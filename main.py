7e71a583726e7eb5fb49d7bd68af53d62422ffbc
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:20:13 +0000

    often left part opportunity care

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
