bf84b006bec1a26fba0e4507f4590015bb8e86b1
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:07:08 +0000

    correct accident copy introduction above

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_min(lst):
+        return min(lst)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
