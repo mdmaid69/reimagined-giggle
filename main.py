680990306ab8e0a5c2e3b4304f604b1cf8b59ba7
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:41:16 +0000

    him there quarter study nearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import os
+def get_current_working_directory():
+        return os.getcwd()
