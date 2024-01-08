1abd2754c5fa599420ed44e3da2e91e97a9818a4
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:54:27 +0000

    pressure fetch light fashion nigga

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
