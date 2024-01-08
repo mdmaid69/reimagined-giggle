8606646e8cae67d612cc7ae8c5d87ce17b017376
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:28:31 +0000

    expensive last release switch pile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_pressure(force, area):
-        return force / area
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
