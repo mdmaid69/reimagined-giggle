a086b773c39f866257be9eb86c8ec6e0a4844757
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:23:49 +0000

    attention kill set fortunate birth

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
