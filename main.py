b02a80ad6e1bb360f9ad1854845a64c1677049c4
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:02:16 +0000

    congratulate rain react official wipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import getpass
+def get_username():
+        return getpass.getuser()
