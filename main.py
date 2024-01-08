3907dccec2b54e701f2c593076eae70c404a610d
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:27:27 +0000

    course respect bring shut performance

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
