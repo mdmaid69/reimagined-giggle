75d4ef63ca28ed3248fbf1eee5593a597d081ed5
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:40:55 +0000

    switch secret cum conduct final

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
