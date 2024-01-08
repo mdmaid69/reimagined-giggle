72d89d68ab73e97ad6d2d15a6a848d877d0fda1e
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:07:10 +0000

    accept room quantity later unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
