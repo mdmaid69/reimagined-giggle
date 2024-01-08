50074fd27e2f008d2dedabfd0a87a2d8db394d83
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:42:35 +0000

    may many gap design benefit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
