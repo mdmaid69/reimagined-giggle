74d4898888eb38f45c50f947223e099fb98785ac
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:42:36 +0000

    wash child as discovery distant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+name = "Python"
+print("Hello,", name)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
