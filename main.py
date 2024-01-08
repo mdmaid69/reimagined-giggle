a118ad2fc13351573d206b67485501fe090ca72a
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:24:45 +0000

    poison old corner trouble risk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-print(math.pi)
+def calculate_power(work, time):
+        return work / time
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
