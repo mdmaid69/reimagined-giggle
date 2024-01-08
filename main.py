5079d0702b941fc9edc78180e21e67bfd8eaf6e1
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:39:41 +0000

    both avoid weather favourite absolutely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_list(array):
+        return list(array)
