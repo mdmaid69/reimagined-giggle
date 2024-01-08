9867d086393bbb7eb01d845a8c314a5e9187c3de
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:14:51 +0000

    receive bite open thick result

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
