66934cd839edf4b254b670a3b6ff754e29744e94
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:56:20 +0000

    dark tax switch exam accommodation

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_int(array):
+        return int(array[0])
