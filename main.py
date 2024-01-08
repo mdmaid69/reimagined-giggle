bd17f8674db38b1259360518c82126e0e57325da
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:23:46 +0000

    yesterday frightened react her picture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
