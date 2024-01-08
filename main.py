fa78fe1fe8ed80e04069194f2481878fd2aecd40
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:17:07 +0000

    join tap mouth fall corner

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+text = "Hello, world!"
+print("Uppercase:", text.upper())
