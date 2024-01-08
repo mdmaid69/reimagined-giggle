ab7a54806672a4824cd28b11523b496d7a3b9043
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:45:24 +0000

    remember moment turn concerning apart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
