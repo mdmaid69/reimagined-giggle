05453b032867fde7a7461dd873bc2ca41015b5c0
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:27:08 +0000

    complain part change invitation danger

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def count_words(sentence):
-        return len(sentence.split())
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
