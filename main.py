9aa0812b68233296d63fd42d75b8163d7255a5ec
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:44:47 +0000

    hole effect relation emotion relation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
