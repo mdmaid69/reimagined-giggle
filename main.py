c7349c45c0a4063538046bb78d14f8f0c91caed2
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:58:22 +0000

    block 569enjoy shine major high

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
