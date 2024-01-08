f3930180143621c32317dd64f69ac2acfaacc61d
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:21:29 +0000

    attempt post crime pattern live

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-for i in range(5):
-        print(i)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def find_min(lst):
+        return min(lst)
