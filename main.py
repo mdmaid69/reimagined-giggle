16a6b1e28d5766cb355e4e4d47fbcfb0ad3db0ab
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:40:37 +0000

    confuse nothing train certain around

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
