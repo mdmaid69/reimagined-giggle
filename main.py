90871b1b5ac4b6c2a091866d2e8ac31bc611d167
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:41:52 +0000

    question completely quality brown tell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def convert_to_octal(n):
+        return oct(n)
