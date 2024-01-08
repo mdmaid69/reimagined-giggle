4ddb470b12343587fd6fd2dd5b5b4f3aa8932534
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:04:48 +0000

    our colour size purpose money

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def add_numbers(x, y):
+  def add_numbers(x, y):
         return x + y
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
