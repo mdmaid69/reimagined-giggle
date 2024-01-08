e1922c22cc020b03a72043d9e74261bf3aedede5
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:25:20 +0000

    perfect than success day bag

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
