96ed3daef207755230bae7fe051959e67c6fffd3
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:53:50 +0000

    something innocent fortunate respect thinkover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
