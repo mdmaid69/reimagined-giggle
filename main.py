0522c0b8b9dfc1d0b74f3e01067024738a90c6d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:58:52 +0000

    holdon dance left drink scheme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
