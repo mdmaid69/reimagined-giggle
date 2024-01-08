d7ad674d8d951f0a3396f7f0464044fc4087030b
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:02:35 +0000

    married share print exam inside

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
