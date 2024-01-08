7cc4d4bff4cc5e088382c6a16c6678124bff6351
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:00:54 +0000

    finally freeze theirs cause back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  def cube_number(x):
+        return x**3
