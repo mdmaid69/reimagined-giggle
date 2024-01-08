73d0940f2abda5df5f8549523ee93e59eb58c352
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:36:37 +0000

    afford plenty at suckme discuss

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
