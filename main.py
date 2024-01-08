33a9cfc35821a024918ba97e88e813db83d58dd7
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:59:07 +0000

    reaction rub sale section famous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_priority_queue():
-        return collections.deque()
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
