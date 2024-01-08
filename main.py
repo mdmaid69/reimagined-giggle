bedf3d879d3a03de1deaead69f901872572afdc6
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:15:36 +0000

    shoot spring private count rate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
