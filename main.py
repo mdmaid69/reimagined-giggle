768c6d28bbda80c50a50b5fc294f9aa24e65b7dd
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:55:12 +0000

    private direction ashamed point impression

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def delete_file(file_name):
+        os.remove(file_name)
+def count_words(sentence):
+        return len(sentence.split())
