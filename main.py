8161383c37e078b20ec3f90a42a399597abb7d49
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:20:53 +0000

    address about book keepfrom aim

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
