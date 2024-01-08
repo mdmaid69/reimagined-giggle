8bba6be37891a9f943961df4f3a01cc975d86b5c
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:43:55 +0000

    owe godown left all mistake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_area_circle(r):
+        return 3.14 * r**2
