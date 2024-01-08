1388f85e5c15f8e4e189fe104334b10c28eb73f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:39:02 +0000

    confused on crash back Mm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import random
-def roll_die():
-        return random.randint(1, 6)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
