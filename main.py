d0c2b1a1c35da9b5fc1a211721f1d922a699b906
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:00:44 +0000

    still woman chat hole butter

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
