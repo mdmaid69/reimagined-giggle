cae6047ef6547a19f6d0ce05419622b17377f899
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:11:56 +0000

    noise anywayanyhow absolutely temper month

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
