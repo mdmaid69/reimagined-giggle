6f4e32f2e2d5fcece9c6dd466c1157854bb585fc
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:03:39 +0000

    who clean the nurse exchange

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+def count_words(sentence):
+        return len(sentence.split())
