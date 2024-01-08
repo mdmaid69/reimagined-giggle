fa9dacbe8d5255de34f506c09a38b457d165c523
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:58:51 +0000

    pipe night shame spare hungry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
