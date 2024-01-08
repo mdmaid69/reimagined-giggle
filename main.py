a8c950ea0b0118fd256e73b36ce457cda8336d81
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:51:04 +0000

    hollow figure gradually paper window

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
