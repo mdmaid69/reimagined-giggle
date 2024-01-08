f99d622996743bcb8cbfd00f369489b035b4de37
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:26:02 +0000

    right frame hardly nice set

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
