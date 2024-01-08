bb578b1c3325cfdf402f43632e6349be3fcd6424
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:46:37 +0000

    test none something tooth giveup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
