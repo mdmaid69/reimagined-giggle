ea4a4642f4b6959aba3f29ad811670743dbaf552
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:47:43 +0000

    stir communication powerful combination the

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
