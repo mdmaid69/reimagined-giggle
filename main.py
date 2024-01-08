bc1462a45bb1780ec9709763bb068df677c6d569
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:16:54 +0000

    nice lead off callup dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
