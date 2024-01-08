52a432baade772ff7b0293806260331e24f881a8
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:03:35 +0000

    legal little convincing fashion cover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_sine(x):
+        return math.sin(x)
