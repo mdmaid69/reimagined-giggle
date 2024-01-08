f16eda0f0252bb4ef54ad57063fe02b848138dd3
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:09:51 +0000

    lead bringforward trap approval possible

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def exit_program():
+        sys.exit()
