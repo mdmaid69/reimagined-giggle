9d135d560c29a23c650c2bc0f4b58df4a4fd7da8
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:38:37 +0000

    contribute coffee boring waste chance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
