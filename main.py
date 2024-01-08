42e791d89376a796e2337d2ed9cad6198b42bcc2
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:37:28 +0000

    on naturally fence sister quickly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
