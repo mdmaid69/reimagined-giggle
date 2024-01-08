9ae9a9b9bfd4d6cc215bdee46c5fff1082c3391b
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:41:18 +0000

    pronunciation school want board certainly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def add_numbers(a, b):
+        return a + b
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
