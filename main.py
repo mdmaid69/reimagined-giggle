63765ed2510d9fac5c0bf54e38901a6dbe0a1e5a
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:21:53 +0000

    hardly much machine steady between

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  def sort_list(lst):
-        return sorted(lst)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
