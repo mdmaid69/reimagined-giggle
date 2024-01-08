5e6ee41e7a6b3bcc32b6671a92da66b5f1c11430
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:06:55 +0000

    strong forward confusing conversation load

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
