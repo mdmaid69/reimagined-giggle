433904dcb86e2aa1579f73bba9d240b31b31c913
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:27:17 +0000

    gate check underneath 179bite dick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
