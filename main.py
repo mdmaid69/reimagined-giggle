8bcd06de2695c7db93872439ee03531a0370427a
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:27:42 +0000

    first material its pleased put

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
