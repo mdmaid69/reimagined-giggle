422997b0d7ef1c59cb9a694449b2478444e5485d
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:03:00 +0000

    whatever reduction complete book punish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
