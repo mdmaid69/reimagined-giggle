0b98a97951bd2c31671985e477c49f02cb5ea32e
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:03:24 +0000

    sky leave glass of boil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_union(list1, list2):
+        return set(list1) | set(list2)
