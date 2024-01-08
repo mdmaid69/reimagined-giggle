5dac33ede1ccd0aaeb4573af70aa9129b191f774
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:52:05 +0000

    snow gas talk keepup connection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
