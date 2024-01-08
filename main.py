6e9eae2054dcddf7a0834268b25112c832d83946
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:21:16 +0000

    neither cost habit pipe slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
