3d257ff567e4497a43fd69477f39d6864fc8b71a
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:23:01 +0000

    general disagree autumn grade dead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
