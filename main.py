2fb9717233e88eb4a50e23af72f83a7996413d67
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:41:25 +0000

    female bush port moral importance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
