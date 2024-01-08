090bf334fa419bf80565542b22008582836ade06
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:29:23 +0000

    delight trap wonder gradually will

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
