dd31edd0c5d7600c6e5580964f4085b3097e9182
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:11:57 +0000

    obtain back acceptable phone hit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
