fed8fc9ddb991cce335fc858e72b515d42840a8e
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:58:13 +0000

    last might noone similar translate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def clear_array(array):
-        array *= 0
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
