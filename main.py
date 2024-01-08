d477c00c9ad2f6e8ccc72bb706b086f2c5021f6e
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:28:40 +0000

    keepout real wander major nor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import datetime
+print(datetime.datetime.now())
