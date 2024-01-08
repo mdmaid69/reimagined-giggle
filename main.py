88517bd2b2598b839b3eef7bfaf98f0f48372ebe
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:17:46 +0000

    patience maintain centimetre improve weak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_work(force, distance):
-        return force * distance
+def convert_array_to_unicode(array):
+        return array.tounicode()
