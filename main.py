c0599c5118b724d9933a103ac9a8f3a3fda5be4f
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:04:31 +0000

    and without brief hot decide

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
