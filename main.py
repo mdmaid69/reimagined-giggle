8b7dd77eb9775b39b4751a62fcf1b3fc2996318f
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:46:23 +0000

    noone Ll feeling lay breakout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
