71f9320c3720c81896513f67475e5bac95639ebc
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:21:34 +0000

    decide contents opponent period mass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
