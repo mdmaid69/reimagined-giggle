3fc7145e32de2cebcba77a0daadb59a3a7c4f70b
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:29:00 +0000

    creature expect tight cough hers

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
