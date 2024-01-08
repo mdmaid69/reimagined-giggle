e4af30371cfb458f69ce0930b8d1ceb360f3688e
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:14:36 +0000

    team powerful hollow position shop

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
