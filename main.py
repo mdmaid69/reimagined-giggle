f25d0abeeef9d620592a8f9148c7de1d33060066
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:54:30 +0000

    pronounce order involve amount married

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def multiply_numbers(x, y):
+        return x * y
