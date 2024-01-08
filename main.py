dbe8c6359e6f1898afcca4a448ecd61772287531
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:27:46 +0000

    Qq estimate store wave distance

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
