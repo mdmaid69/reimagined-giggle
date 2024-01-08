9afe853b3e6e58080b069251305a5bebde627819
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:38:53 +0000

    trust experiment town mystery past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
