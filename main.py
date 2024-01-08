bdb6f9f5863f8464f3cb67ae7c97502d74245445
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:03:38 +0000

    strict own imagination consist education

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
