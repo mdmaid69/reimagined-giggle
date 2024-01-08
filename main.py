9f0490df86365741a2ae8e10068519beaa2ad836
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:14:44 +0000

    turnoff remember fork taste start

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+text = "Hello, world!"
+print("Characters:", len(text))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
