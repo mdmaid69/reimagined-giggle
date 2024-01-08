a9373448dfcad24061a1fc667c05ce537509fcaf
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:35:39 +0000

    effect difficulty strict character evening

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_work(force, distance):
+        return force * distance
