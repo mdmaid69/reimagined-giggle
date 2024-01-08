d51b8149bfc836470c59828712ed342f9d2d222c
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:27:39 +0000

    bang successful cover cinema soon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
