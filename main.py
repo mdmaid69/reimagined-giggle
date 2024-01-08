d744719c22b68c7e121bbfc95e61ee61c02a349d
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:25:42 +0000

    exercise equal run without bush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
