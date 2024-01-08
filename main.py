24505a0d3133b557ccb626cb990221dbc920e4ec
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:09:10 +0000

    film unfair 179bite upstairs floor

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
