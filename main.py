30726770b64fc9e20aa003fdd1802a2c460a3b83
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:00:39 +0000

    shade movement central shame drive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
