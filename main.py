a8a18e80bcdefb9404618dc183e9ec0e3a9678a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:53:53 +0000

    arrival under worried area fetch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
