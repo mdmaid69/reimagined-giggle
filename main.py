fa74cd9750d0583f7e33e5c88b0719fbfdde7f1f
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:29:47 +0000

    single write club healthy neck

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def square_number(x):
+        return x**2
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
