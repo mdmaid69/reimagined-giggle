b308615e7af427929d5dd56e05903edc7bae05e0
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:51:17 +0000

    sting close fine shock race

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import tensorflow as tf
-print(tf.__version__)
+def calculate_sign(x):
+        return math.copysign(1, x)
