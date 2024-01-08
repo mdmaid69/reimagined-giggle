ba8479d1a545d522a761b01019441685c6a0bbdc
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:00:45 +0000

    glad extra blow beginning contents

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import random
+print(random.randint(0, 100))
