fabb53e8ed9595f43e7daddac2ca07c2be0c90a4
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:07:41 +0000

    improve disappointed environment furniture best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
