88b416b3574b838074191ce4cdc459b97692554e
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:50:57 +0000

    some city Rr dream 565end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
