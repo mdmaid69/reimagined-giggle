f7f5c7c986e64aa22a37eb4d9a7432dce6d745c5
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:53:46 +0000

    priority heat read treat watch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-def find_min(numbers):
-        return min(numbers)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
