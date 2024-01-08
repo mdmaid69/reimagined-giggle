4a28a1de506bf3c06bae008bfe6449dd4c0dc73a
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:47:57 +0000

    cry false fill clearly Cc

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
