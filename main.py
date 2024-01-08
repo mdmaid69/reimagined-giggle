c103f04c326baf59423181d2e0564f7ce9bf78c4
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:10:15 +0000

    correct certain moral busy pin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def delete_file(file_name):
+        os.remove(file_name)
