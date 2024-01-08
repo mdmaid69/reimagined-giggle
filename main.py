932636919ceb86cbb0aaf3818dddeee6f01e94e4
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:01:41 +0000

    flat violence fold usual Rr

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_speed(distance, time):
+        return distance / time
