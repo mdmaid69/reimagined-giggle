697fda01527cc7064cac45249c6d000791eaec72
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:47:45 +0000

    sleep famous concerning makeinto second

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  def sort_list(lst):
+        return sorted(lst)
