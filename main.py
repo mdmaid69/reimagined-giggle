c6c9d8f81ff1203c53fb6952e41d889d36b6de63
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:42:12 +0000

    joke wear rush claim nervous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
