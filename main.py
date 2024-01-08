3f5f324b179a40f10a4552694afe75689262c116
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:16:45 +0000

    recognize flavour ever travel add

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
