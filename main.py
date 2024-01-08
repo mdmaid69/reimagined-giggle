7e210e26dd6595ddd5cdc4faffd3ccb6167d6f53
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:06:11 +0000

    Oo male belong tell search

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
