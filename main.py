2354600c3d852329672196eaa7c7e6a35429b951
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:12:48 +0000

    underneath mess hold break family

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
