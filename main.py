e52b4adb2a46c4eb79cd88594512a322f9cac6f9
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:06:43 +0000

    downstairs ours bag active fierce

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import sys
+def print_python_version():
+        print(sys.version)
