fb892e6b74eb801ffc97fb3173ec22858805ae27
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:39:09 +0000

    comedown sudden reject flavour several

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
