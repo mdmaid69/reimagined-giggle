43f62832c9393f8bac1d9e212b593b466deb2f19
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:03:10 +0000

    difficult short thinkover minimum brown

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def append_to_array(array, item):
+        array.append(item)
