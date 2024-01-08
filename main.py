0ba2dc111665985f3721e4317036157b3cd9f2fa
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:36:30 +0000

    blame strike satisfactory away recovery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
