c3419c059997e69e60c8a72745acb4a43945092b
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:52:52 +0000

    apologizeapologise imagine concern hold best

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
