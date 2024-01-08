56d74873ad6f5ab558a19f157d1552984d2707c6
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:39:53 +0000

    vanish reasonable hair discovery flavour

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
