2b0b6c4bb8d4514417ed84f1a50ed94ba71c08bf
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:29:22 +0000

    deal basis bored right reaction

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
