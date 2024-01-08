7904a7b380144ff63ac7a27c1b3d81cf8f7ef00d
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:06:59 +0000

    dance union hospital example except

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def remove_duplicates(lst):
+        return list(set(lst))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
