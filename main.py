eb09ad8f87499e9fe228f4b2cebb5511a8b7a10d
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:35:58 +0000

    combination rate criticize check mass

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
