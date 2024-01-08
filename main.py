9e6b441b04745d788c37912b5ac72ba2ec49fe2d
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:04:22 +0000

    edge skirt crash invitation people

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
