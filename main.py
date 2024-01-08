aa89496ea24927330fbd916738374675751a805e
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:53:45 +0000

    embarrassed extra seem at somewhere

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
