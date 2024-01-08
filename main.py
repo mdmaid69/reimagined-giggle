45983181479891555bcba1db80e8b93f97c42fe2
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:41:17 +0000

    practise chance through rub doctor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
