bc3e3ccbdf9a1fda2b126da3eaece72246aecf1f
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:22:52 +0000

    range include recognize variety club

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
