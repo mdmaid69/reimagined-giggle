7ff8133bfcda1bcdaf744eabc11cfd09a2686fa3
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:02:02 +0000

    rest dare soon agree takeoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
 n = 10
 print("Even numbers:", [x for x in range(n) if x % 2 == 0])
