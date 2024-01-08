5b66d6ebe28a48b3232f19764c7809b1ccce80c3
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:50:05 +0000

    distant mix broken slice possession

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
