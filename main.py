cd0781922aabe7e771a22261d303e6cf3cadbc3c
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:13:37 +0000

    debt shadow pot year bin

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_item(array, i):
-        return array[i]
+def sort_list(lst):
+        return sorted(lst)
+import sys
+print(sys.version)
