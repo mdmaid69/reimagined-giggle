ac07c7ce8d968443fdeab1954eac180f515fb34a
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:46:35 +0000

    board pain allow break fierce

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def add_numbers(x, y):
+        return x + y
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
