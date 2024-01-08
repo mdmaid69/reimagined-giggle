a627a22bb7dfaef0a7c512fb81d73ea4cee175a3
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:31:37 +0000

    shut bank rank become string

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_density(mass, volume):
+        return mass / volume
