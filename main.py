311b184561e247cc1894bdf555213ae644eae180
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:50:24 +0000

    takeout warn achieve Zz fight

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
