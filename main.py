49c534b21d77413a02d320d6f40150c8a778744b
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:16:43 +0000

    receive influence mine fail arm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_array_as_list(array):
-        return list(array)
+import sys
+def print_python_version():
+        return sys.version
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
