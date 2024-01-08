c60d247a8a55790a4d45337a68c845245b2b075c
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:15:19 +0000

    familiar judge bend world thick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
