1a22beab6add86ab7ee7428a5bfeec6655fa82ef
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:57:29 +0000

    gather hers once earth wide

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_speed(distance, time):
+        return distance / time
