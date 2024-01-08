9dbe34ca97d7064fc619b7f436b6d0d5c5ba72da
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:00:50 +0000

    save trick trial wrong slightly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
 import array
 def get_array_itemsize(array):
         return array.itemsize
