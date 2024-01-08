a850e46072f30a5b360dbdc795fd38842a4c4827
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:07:28 +0000

    use crash relative plenty fall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_area(radius):
+        return 3.14 * radius * radius
