f74901aedcbf129bfe8b37f8bfb6cc8215eea6e6
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:26:36 +0000

    indeed scream measure handle manage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_int(array):
+        return int(array[0])
