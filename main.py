dc4431437e647414eb7321add67a9e370045467c
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:39:27 +0000

    confusing Ee hair power hers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
