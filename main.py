13dd843b2830eb668479c1abdb04837b77d9ea97
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:13:53 +0000

    form brief quit full whom

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
