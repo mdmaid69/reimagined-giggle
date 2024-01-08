6f5ab65d3784561450385f19089ce484e4a1a73b
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:04:23 +0000

    vote getaway glad bread unlucky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_min(numbers):
-        return min(numbers)
+  def cube_number(x):
+        return x**3
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
