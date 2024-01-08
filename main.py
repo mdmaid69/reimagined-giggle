1f0da7e8dc4fd7ff431cc574bd5f907610ec0a73
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:30:04 +0000

    instruction before patient summer whole

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  def cube_number(x):
-        return x**3
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
