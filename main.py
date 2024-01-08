f584a61c0f31943fa72ae2d8e975064379f944cd
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:44:41 +0000

    independent dangerous reply fall professional

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
