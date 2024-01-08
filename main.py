e1e7ccad0d498f5beafe9393fbb5bc6c639cc8d6
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:07:54 +0000

    swim able Mm equal talk

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  def cube_number(x):
-        return x**3
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
