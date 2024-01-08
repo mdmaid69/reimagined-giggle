f37564c257399e05d4076c6a3eeca3de85fb9a71
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:20:18 +0000

    teach fairly willing temperature lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_current_directory():
+        return os.getcwd()
