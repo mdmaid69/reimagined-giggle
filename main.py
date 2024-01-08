238ce9a0e3b59781ae4319c5e19b145592fb189f
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:15:08 +0000

    reliable whom journey consult pocket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
