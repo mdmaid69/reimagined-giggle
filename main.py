15d469330fd3ab9c47d7b4b2aa50cea93e0b2934
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:58:43 +0000

    recent when worth everything mother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def reverse_list(lst):
+        return lst[::-1]
