b94e28408b1115aeb3f43183b3c79f318f1655a9
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:44:12 +0000

    invitation sail arm doubt fun

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def find_unique_words(sentence):
-        return set(sentence.split())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
