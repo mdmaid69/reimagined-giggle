6ddbd9ca32c1eb01b469c4cd1fb12120d83eea4a
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:56:52 +0000

    rarely public door manner shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import random
+print(random.randint(0, 100))
