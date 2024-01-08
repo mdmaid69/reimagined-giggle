7966d94e15bd4eacccd13b060c7877ad8d339176
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:59:45 +0000

    better independent but truth welcome

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
