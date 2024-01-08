c85a57729b51291efc39cb3a5fc8b5fce2a8a90a
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:40:50 +0000

    complete write godown oil false

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_force(mass, acceleration):
+        return mass * acceleration
