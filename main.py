13ace5660a5e6d3eef6200a3aa71e38ad1739965
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:15:06 +0000

    piece alive replace in improve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
