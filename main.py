a4a116c7b60bb89701ca8621850a61a76b85ee9d
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:25:41 +0000

    soil mix while model time

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_pressure(force, area):
+        return force / area
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
