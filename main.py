bca16b35e4e4bc172f0231a88df83b07b2d2bab3
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:48:53 +0000

    deliberate hurry problem hope want

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
