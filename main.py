f0b46a668f44bc53dbf7ac1eebff1308a929d689
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:15:16 +0000

    personal object indeed theirs government

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def clear_array(array):
+        array *= 0
