39ca4ae23f533704f31beb975ce9776c80384d61
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:38:11 +0000

    ourselves nor voice country attract

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
