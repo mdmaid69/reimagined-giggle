ac142fec35582febd19d002e739d1b9c753b5b03
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:37:00 +0000

    accident accommodation cheap after brief

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def sort_numbers(numbers):
+        return sorted(numbers)
