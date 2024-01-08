90d3c22b0d0aa62dff82fdc372a00e96fdde15ac
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:51:02 +0000

    lower narrow keen language store

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
