7b555da71abdc970c3034d3182c9a599166ef39e
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:58:35 +0000

    original hurt something story bringforward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
