3d30ad82798e4b047c50fbbdea35d5cdc77c74b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:16:32 +0000

    bin study terrible opportunity operation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
