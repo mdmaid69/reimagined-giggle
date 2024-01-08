c27145c1aa2614773d0ed1aaa3b3d7109f24ebcd
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:13:42 +0000

    unit before let trust talk

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
