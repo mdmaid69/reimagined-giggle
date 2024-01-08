8131572b69779dbeb1a972ada5c7b3b4a6902b08
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:11:44 +0000

    keepoff find no love large

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  def calculate_area_rectangle(l, w):
+        return l * w
