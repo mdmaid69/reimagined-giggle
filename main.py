22a39d82d2d83a5719488e254d2d793fd3abceab
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:15:50 +0000

    slip getback team each feel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_set(array):
+        return set(array)
