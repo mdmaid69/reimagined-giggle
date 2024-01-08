6e81687274d2c926886e6f691f9d72186b7902d0
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:22:06 +0000

    brief everything confusing muscle trousers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
