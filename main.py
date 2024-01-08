a8dc4b3733f49fe2f126354a6a031cc5d0cb912a
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:23:10 +0000

    example breath underneath formal talk

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
