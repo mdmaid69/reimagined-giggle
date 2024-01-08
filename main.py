1ce82edb41b5ad36804d6e6e020351ec184ea89d
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:53:47 +0000

    since fault without real instruction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def convert_array_to_list(array):
+        return array.tolist()
