e47eb72c6a6aebf4843a13b54f81e2abad814729
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:02:58 +0000

    impossible string well dark lonely

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
