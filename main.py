49527ac57d5dc26942f61a71b24f59a8e6e53296
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:36:35 +0000

    approval soak activity hide properly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def add_numbers(x, y):
-        return x + y
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
