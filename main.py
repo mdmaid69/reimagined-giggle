9ed3e966433fe1e358ed12ac6f8ab5cd7e4c71ce
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:29:01 +0000

    unpleasant offer shape foot muscle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
