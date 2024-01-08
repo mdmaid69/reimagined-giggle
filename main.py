d7d01b67085dc3936f30b3bc4e5c2e8299d99d53
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:53:20 +0000

    brush purpose return seat since

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
