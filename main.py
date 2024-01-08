5607596015e3e382ea88f1d30d242403d5ff8510
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:27:12 +0000

    foreign match string pollution unlucky

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
