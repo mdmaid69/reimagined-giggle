ae77407e0b3b0d36bd75c706232dd82e9ef5224e
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:33:44 +0000

    hit too tour object bush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def square_number(x):
+        return x**2
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
