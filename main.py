aad50da18c199b60640e0f5fdba02b2c1682bc11
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:56:37 +0000

    there combination case prefer rely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
