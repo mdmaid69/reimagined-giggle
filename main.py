8f05f25e7c6de8e4eb77d905acc9e3bc5f7a5888
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:53:59 +0000

    typical stir medium loose connect

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
