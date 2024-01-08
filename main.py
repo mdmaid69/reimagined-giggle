06a846e818388fa58be08485690c2c4efaccbc2c
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:43:21 +0000

    principle pronounce Mm female admit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def add_numbers(a, b):
+        return a + b
