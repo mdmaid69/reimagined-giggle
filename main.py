18c2dfc96e157c7a21a925607fa6f3b1be645111
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:28:44 +0000

    brush slightly attach force Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
