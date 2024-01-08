ceed4eaaffa6e9bc7500a6c85b3166b62193fef1
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:17:03 +0000

    radio takeback ear clear worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
