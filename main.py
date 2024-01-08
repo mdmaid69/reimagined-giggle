68d7bd771d894ffb722d02fad3cfebe02cf0883c
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:44:17 +0000

    worse serve degree odd aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
