99fb3fea9b06e9986a64439e09ddd1fd4192aebe
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:45:58 +0000

    shine steam trap drive insist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def subtract_numbers(x, y):
+        return x - y
