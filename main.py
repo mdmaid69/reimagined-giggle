1624aa35285cf6d06f7089898c610bd7f193323c
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:22:20 +0000

    interview model keepfrom acceptable explain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
