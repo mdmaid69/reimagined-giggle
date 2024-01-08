998b1c78221714407d63be3c87dc655f38450c80
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:38:52 +0000

    interested nurse Tt ticket harm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
