c3c93572a2a2a7df77e1a0f32bd0c97abb4e567c
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:47:44 +0000

    pay spring slide process reduce

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def clear_array(array):
+        array *= 0
