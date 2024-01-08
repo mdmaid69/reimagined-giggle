d92dd9cbadda4cff8b2db8c06008addcc537e281
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:39:29 +0000

    bridge sound once measure complain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
