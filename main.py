9753ba1fa0432cee694fa895295398a41d31ae75
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:33:10 +0000

    black shallow telephone bring luck

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import array
+def get_array_as_list(array):
+        return list(array)
