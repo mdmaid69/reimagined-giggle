60eba5106a9e5129a123401e8cba1612e1353ee8
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:42:46 +0000

    brief positive offence dance such

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def create_numpy_array(lst):
+        return np.array(lst)
