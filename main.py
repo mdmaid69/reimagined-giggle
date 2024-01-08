79201f4b6444df3b80c9e60e0b233bde392fb9db
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:23:56 +0000

    ready slice 179bite tap present

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
