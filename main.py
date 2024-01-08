c3fe65e27b663eacea1ec7bebf973242ec8efe3a
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:52:50 +0000

    whoever string sentence scene lost

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def count_words(sentence):
+        return len(sentence.split())
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_as_list(array):
+        return list(array)
