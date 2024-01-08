19ef776312bbd465d1b55c40e1c0b1f52b3901bf
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:29:35 +0000

    weigh pause runaway balance sing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
