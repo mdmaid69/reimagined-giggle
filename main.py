4a3c1cfb5017fe6a01666896aba78779085275bb
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:15:22 +0000

    push perhaps objection will teach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def is_even(n):
+        return n % 2 == 0
+i = 0
+while i < 5:
+        print(i)
+        i += 1
