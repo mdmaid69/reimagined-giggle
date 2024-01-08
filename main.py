778a9efec75b620f3278bd5f3fe11ab77be008d9
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:41:38 +0000

    fast shake difference note tongue

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_stack():
-        return collections.deque()
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
