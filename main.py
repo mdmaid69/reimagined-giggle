54594ff1d1354f93196323c2ba3780642b647913
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:31:05 +0000

    whether gooff public bound season

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
