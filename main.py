e3bd6b974680c8e5a44daef6b1722adab33271c1
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:43:50 +0000

    interested favourite rather notice same

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
