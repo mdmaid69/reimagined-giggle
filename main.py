2d4a12d36f49c9d7f5064c7b9b1bd7a4417242d5
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:47:43 +0000

    creature skirt still fight sweep

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import collections
-def create_priority_queue():
-        return collections.deque()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
