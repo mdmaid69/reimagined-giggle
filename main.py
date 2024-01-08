a7b47a8b95127ce6c473a193372506970737b15e
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:02:33 +0000

    open rubbish thinkover judge few

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def reverse_list(lst):
-        return lst[::-1]
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
