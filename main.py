b2c09c31c93d51bf6ff9b1cb18a124fe9f7936a8
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:08:43 +0000

    lunch ahead forgive bread drawing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
