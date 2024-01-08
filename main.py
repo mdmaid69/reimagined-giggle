cf04bf2b3cc169304c800700d03f9323025b57bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:08:07 +0000

    beautiful trade patient steep point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
