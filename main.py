8337bb6ebe82c466ad18f15962c97db094672ae7
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:35:07 +0000

    undo worth scrap discovery freeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
