7c270b822d9a9372dd763619c70ee11a18691fbe
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:46:56 +0000

    blame fun half tune lower

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
