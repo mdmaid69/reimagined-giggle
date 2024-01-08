29761b7456a3b497bed2a0203cc4ed7eea6f8c91
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:40:37 +0000

    possibly keepfrom takeover pay platform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
