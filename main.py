2ca92c74fe8d2cbb3117b5e28a3626c9ea63d604
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:52:22 +0000

    central natural content force judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-for i in range(5):
-        print(i)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
