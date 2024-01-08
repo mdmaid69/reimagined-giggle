9b75d259b8ca4110419139ebda696fdeb0e5ee25
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:03:16 +0000

    sail convinced morning real close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
