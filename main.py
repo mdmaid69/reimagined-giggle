081e7e53483502ef25fff0bcf5a45481346e8388
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:31:16 +0000

    judge answer perhaps improvement address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
