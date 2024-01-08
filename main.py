f57499afa56d13adc45f41b0104c064eea36c44c
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:34:32 +0000

    careless hour regional giveout same

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_acceleration(speed, time):
-        return speed / time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
