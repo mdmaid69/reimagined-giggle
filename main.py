7feee34608de3393d3f9c3d598ca2540ef9bc360
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:17:38 +0000

    sleep steep edge splash easy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
