4286c516a75c23b580d608ab0924c343804f1c32
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:29:53 +0000

    spring together any nor black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
