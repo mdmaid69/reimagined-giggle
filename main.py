075c2ea4bae53a31a311262f606234956a140846
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:40:26 +0000

    knowledge travel I purpose class

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
