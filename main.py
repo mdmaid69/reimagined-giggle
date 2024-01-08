b0f483a0e10cb75424b7f718946c4537c3d8be0b
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:30:07 +0000

    draw spring across argue identify

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import platform
+def get_python_version():
+        return platform.python_version()
