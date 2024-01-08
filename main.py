2139f55b3f38f3be1f8fbb65c21fc8f4e4a3c1bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:58:08 +0000

    add insist side wish behaviour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
