d16cfda56b7380b2324b12ead985b76b157ee635
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:59:29 +0000

    difference runinto adjective another record

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
