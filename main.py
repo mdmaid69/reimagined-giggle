63482705ad91bf53f87b21a94819cd806d9fefb7
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:17:20 +0000

    stop by confused blind interrupt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-name = "Python"
-print("Hello,", name)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
