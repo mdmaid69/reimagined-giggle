71eab5ed3ecf7f952e7b0fec51c6537b3d634901
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:03:24 +0000

    hole wrong below card artificial

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
