fa4c4733b3e2935946ba2ac277dee1a4c40ba042
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:30:32 +0000

    talkoutof powerful what refer before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
