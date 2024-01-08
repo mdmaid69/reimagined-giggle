87b52785c246c69d3c743ff1b268aca8279c88df
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:42:27 +0000

    sense shake pool stand whom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+def print_python_version():
+        return sys.version
