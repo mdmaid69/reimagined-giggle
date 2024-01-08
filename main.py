1f18098edff063c40eefccb154c5fc6f7d05d578
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:35:22 +0000

    back of collect phone stretch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
