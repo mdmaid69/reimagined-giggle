18dbde89398dd6a5f53bb7ab48f37e475fd11583
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:32:27 +0000

    nurse history period thankyou once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
