189e2af518c0a16a7311eb9b8f7559dbd6ad05bc
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:05:16 +0000

    scale pair nurse lesson pleased

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def append_to_array(array, item):
+        array.append(item)
