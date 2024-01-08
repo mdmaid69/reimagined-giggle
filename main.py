6dce0f1fef3fc7d60ba1f1da9874329cc390f09d
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:56:53 +0000

    throughout callat draw squeeze control

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
 def calculate_average(lst):
         return sum(lst) / len(lst)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
