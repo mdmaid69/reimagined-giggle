937fe866a873afb7bc12caea357d7e069ffa3aaa
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:02:05 +0000

    passage remember proper or interruption

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
