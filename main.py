c3c3478724979ca7676a75d5e9fd5889bb01aa6f
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:29:09 +0000

    bringback reliable rob kick well

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
