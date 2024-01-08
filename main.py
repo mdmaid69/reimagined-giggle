8b9aa2af40890cb027164e68e3179c9d29dd3318
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:45:37 +0000

    choice fear from kind poor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
