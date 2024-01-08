8371484bf3d5cb7bf9356fb18849bc33a5faecce
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:14:44 +0000

    direction study separate club make

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_acceleration(speed, time):
-        return speed / time
+def get_array_as_bool(array):
+        return bool(array)
