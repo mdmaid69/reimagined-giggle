dcec0f5cdae1f5b77f1cf64f9dbfa470b22915c4
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:16:22 +0000

    somewhere Bb awake service best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
