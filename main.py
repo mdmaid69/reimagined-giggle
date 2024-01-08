00d71a1695a4c0b2bc8c5560f4f3387c83a25435
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:15:11 +0000

    mark square test tap Zz

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
