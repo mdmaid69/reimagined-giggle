2d53f07ca491f76b84900497a4bdc6ef5bca45bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:40:16 +0000

    tip feeling downstairs cause perform

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def is_odd(n):
+        return n % 2 != 0
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
