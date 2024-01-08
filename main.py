feea2c377b8f9419c6fea94d40af3a8d39b06c28
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:52:32 +0000

    discuss sad office tip official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
