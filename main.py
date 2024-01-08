91f905134e4de80d079ae1a30d4c4ba2217108cf
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:29:59 +0000

    open pleasant rule usually brush

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
