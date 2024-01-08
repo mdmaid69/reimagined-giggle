ef44096577c604c8f5df407dd6bb8ee9dd8b7cb5
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:42:37 +0000

    any name exciting win order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
