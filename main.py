1deb915c1354800b79e1c357a9301cda9796f5d4
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:58:08 +0000

    cold ball food send thought

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
