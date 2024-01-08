d6c4bea3030015459b8c28ff578335782f30cc24
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:31:11 +0000

    that club tomorrow wherever group

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def count_elements(lst):
+        return len(lst)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
