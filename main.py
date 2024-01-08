3b266c9f1a9ba07a541d4aeddb8dbae1b8df0ead
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:59:07 +0000

    prepare blow switch bus star

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
