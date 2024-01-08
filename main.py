16927d390f66d0220cce8ed5e1d890f2230cdf26
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:28:47 +0000

    runinto probably drink sea reply

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
