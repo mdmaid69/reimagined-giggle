8cfc2f2972782bf2a34cbdb5bd03c84e9fd2a583
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:52:34 +0000

    summer closed train comfort trade

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
