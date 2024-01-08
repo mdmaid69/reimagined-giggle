a9580cc3e21413e5c0a3f2ade82dc0557954c97c
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:51:31 +0000

    some noisy forgive flow write

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
