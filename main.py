7eb0de7c90de7d00e127023e8e01856ca1a02891
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:36:18 +0000

    ahead job unkind beginning space

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
