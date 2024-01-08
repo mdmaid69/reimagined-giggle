d87875f3da80a424193b904f60bf3cf8686ab14e
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:56:48 +0000

    pause Qq upset comment something

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
