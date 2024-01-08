4b93ae5087634d6c2b645b1b71f3632be2485861
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:16:48 +0000

    rub charge money major back

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_force(mass, acceleration):
+        return mass * acceleration
 import array
-def convert_string_to_array(string, typecode):
+def get_array_from_file(filename, typecode):
         a = array.array(typecode)
-        a.fromstring(string)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
