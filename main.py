efe7a5b7bc50f64e6681299f317ced7aeb70cec9
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:54:51 +0000

    rate drink plan regular heat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
