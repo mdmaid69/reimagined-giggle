d7fc28e2f99052a57639e38e4a869f3c63158dbd
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:45:54 +0000

    later herself practise loud separate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
