79b9816b21c55bbb3b791e51c89fd82d9193887b
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:41:09 +0000

    day clean herself fill definite

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def square_number(x):
-        return x**2
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
