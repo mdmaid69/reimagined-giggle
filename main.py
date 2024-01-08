9561886b7a8a8fccc7a2c0cec9444658a0a08cb2
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:18:33 +0000

    lean share city offer art

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-for i in range(10): print(i)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
