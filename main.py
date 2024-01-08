c8fc5352dd187022d3608743fdc05356bc3ca8f3
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:26:45 +0000

    interesting whenever rubbish mix quality

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
