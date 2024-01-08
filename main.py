328b3eebc63a96a0d4143b05dbc21b685f32a14b
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:36:30 +0000

    represent turnup put indeed objection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
