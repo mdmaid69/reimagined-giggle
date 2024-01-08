70c6fdafdcd51d53b03a12915a2a593d7cdcd3be
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:15:25 +0000

    dull shoot separate explanation comfort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
