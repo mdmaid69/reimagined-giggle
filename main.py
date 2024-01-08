d5d083a74a3eadfbc251b5b5f195a0fa8bce149e
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:21:58 +0000

    here narrow open punish holdon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
