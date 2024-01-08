04a045f6220f6c5a5c822f3d9242dc8606612852
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:06:33 +0000

    new couple beyond fence no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
