1209b6f1d6e0fb38f8c481026504bb99e5d68b14
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:44:36 +0000

    talkback high near through paper

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-print(sys.version)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def set_array_item(array, i, item):
+        array[i] = item
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
