033484873e11d8d9e035eef72eb2901e28e7df60
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:28:22 +0000

    natural our production scare afternoon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def reverse_string(s):
-        return s[::-1]
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
