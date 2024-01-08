a513b55d46a0e568285d7710ac10818594591baa
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:32:43 +0000

    apply because callup school lunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
