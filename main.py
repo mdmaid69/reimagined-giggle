21103284f57cd97aa6042b1d8033686d10f70b7a
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:09:39 +0000

    complaint method positive famous difficulty

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
