4f7be666ea5a6bdcab644e467a7e21ab3ebc209f
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:32:23 +0000

    glass check wheel thank shall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def is_even(n):
-        return n % 2 == 0
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
