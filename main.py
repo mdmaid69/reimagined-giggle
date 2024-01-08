f73001982c7f2b81e553bcab968e6fe6bf62c63b
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:03:26 +0000

    load occasional part should apart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
