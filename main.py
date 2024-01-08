de6eba33af4aecd6d5683670e74b98f4a2c2d71d
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:07:41 +0000

    boil dark exam window calmly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
