3776090cbc666bd9032da5c7c0b5249da84993a6
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:42:36 +0000

    trust refer state lose confuse

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 text = "Hello, world!"
-print("Reversed:", text[::-1])
+print("Characters:", len(text))
