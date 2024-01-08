5bd543a1cf9d053baf280bf0c33700447697869f
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:10:28 +0000

    slowly takeback comeout must mistake

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
