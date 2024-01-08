0f6d6426e593dbd66ef044d01b49bd4d3c78310d
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:31:22 +0000

    Oo confusing exaggerate cost yes

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_as_str(array):
+        return str(array)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
