3faac233fc628a357d149d50a4101f8a20921522
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:20:58 +0000

    breakout store ordinary belief injury

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
