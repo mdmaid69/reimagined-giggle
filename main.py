edc522161406d58ce93a0c84ee37ef97e8d9453a
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:39:38 +0000

    drawing fairly target shock deserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
