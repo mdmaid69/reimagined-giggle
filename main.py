6c04203047d94f28e9453e638658b33aef3ad5f7
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:27:53 +0000

    stone vanish winter continue brother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
