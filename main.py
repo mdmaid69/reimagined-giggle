79012335f17b7b299165e900f36f6fb225b651f7
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:55:17 +0000

    note conduct activity right leg

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
