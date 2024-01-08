dbebdde3da1b6ff6bb24745b267d0eede017e048
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:37:00 +0000

    rate spot storm cook aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sys
+def exit_program():
+        sys.exit()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
