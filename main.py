de422867038d497f4460a800bc6086a0ae92c7df
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:50:50 +0000

    able high behind towards must

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
