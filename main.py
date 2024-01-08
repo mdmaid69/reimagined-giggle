e8448788e27bdb26be6c555bfb2fd34c35fbe895
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:28:57 +0000

    admit cook shelter importance defeat

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
