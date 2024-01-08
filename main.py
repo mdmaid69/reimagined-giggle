63e85c07cb7c310c66f5e4dc4163bab99c30aa7e
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:11:28 +0000

    die unhappy picture naturally popular

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+print("Hello, world!")
