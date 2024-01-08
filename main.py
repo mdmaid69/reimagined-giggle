e000560a23bf70ea6710a8de39b53f12261b990a
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:28:01 +0000

    yesterday right true workout float

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
