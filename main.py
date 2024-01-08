0fced209937dd4fcc62718a469a596a137ea5f74
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:00:48 +0000

    thick taste certain apologizeapologise suggestion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
