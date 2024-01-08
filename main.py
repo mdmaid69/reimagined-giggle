653bd1219b1d19a5f275087f772851700c040d3d
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:00:25 +0000

    bin file basis love within

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def square_number(x):
-        return x**2
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
