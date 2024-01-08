ff478eac8e9b1fc68e0cb5fc929e4171f4d813ba
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:49:10 +0000

    fail law head mess like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+print([x**2 for x in range(10)])
