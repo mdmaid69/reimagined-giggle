05914e8103f9bafbbce9874b6c0cdd6e0f88d9b3
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:46:40 +0000

    rid mad address big foot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
