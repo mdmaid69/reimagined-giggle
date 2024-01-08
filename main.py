41faf3c44e895bea57946f2a50a1705d8abe16d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:53:12 +0000

    soil so live with more

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def convert_to_binary(n):
-        return bin(n)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def count_elements(lst):
+        return len(lst)
