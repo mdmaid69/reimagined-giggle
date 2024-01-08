5f8971cebcc82d6bfea4a7d3fe6676810e8bb190
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:35:11 +0000

    bring common broken which bill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
