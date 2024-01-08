e515fc3a6bf944ef07ab9b0c1585da2fcee5e0f7
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:10:47 +0000

    over hair threaten address feeling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
