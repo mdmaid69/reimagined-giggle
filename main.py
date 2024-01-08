8d6fafd8396f2a874c230b000bd1ce2346eeda01
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:29:54 +0000

    move standard without word gather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
