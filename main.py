b791685e821277496b46879058ec8450dfb2ec07
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:49:57 +0000

    class care dance line advice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import collections
+def create_counter():
+        return collections.Counter()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
