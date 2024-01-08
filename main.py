0e74e28f36ef19e403587968e414bd5a0aa74222
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:57:30 +0000

    advanced every better whoever emotion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+print(sum(range(10)))
+import array
+def append_to_array(array, item):
+        array.append(item)
