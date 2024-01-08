2a2cf177a8d9463b397142e52b1c1696912cb215
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:32:02 +0000

    stop plenty pollution bored comment

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-print(sum(range(10)))
+def convert_to_octal(n):
+        return oct(n)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
