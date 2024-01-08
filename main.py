d745ef2f16f8256f48c27cd39597b7beb6d70cf6
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:19:56 +0000

    statement step welcome not happen

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
