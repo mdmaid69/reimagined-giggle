f70bd12c57889166d657e9004969cb39d9aa5c6f
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:56:14 +0000

    once sick great manage think

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
