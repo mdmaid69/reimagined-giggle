3df3ebebb73f0de95368d902872068166c85584f
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:33:59 +0000

    round stage than by clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
