fd0095299456513c8176db9bf8910fe406b26167
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:52:34 +0000

    after we scale contrast fork

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
