4d18c1a4f7d5206998b2e206ea0e89693a65f488
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:34:55 +0000

    570enjoyable report period pay basic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
