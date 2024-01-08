a1049c76d571a39a89f1748953f5edcc148fb592
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:37:45 +0000

    somewhere birth factory treat tell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
