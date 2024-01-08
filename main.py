252cd52a989bf2f6623cc8d568353e3a3c03f1c0
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:01:10 +0000

    community safety prove kill her

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
