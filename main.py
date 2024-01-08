5aabd4ea08831a7c641f184f5f289737db7d906c
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:38:34 +0000

    bunch Uu clear back Hh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
