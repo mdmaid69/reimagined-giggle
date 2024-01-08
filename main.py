41d9ebe95e831588729fd24ee32e95f0234a9076
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:59:11 +0000

    sentence receive each admire estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
