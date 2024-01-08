18ba6664697d8e7be457c83a5df1e895b57e43b0
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:11:32 +0000

    and hand makeinto shall valuable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
