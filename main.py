5edc4ae10ebde3ae3de18f51f21f16d74615a185
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:56:03 +0000

    emphasis sad ticket race uncomfortable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
