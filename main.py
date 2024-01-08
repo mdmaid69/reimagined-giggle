d65c4d64493f712d952fffb95d33642879c1e0fd
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:38:33 +0000

    burn rough storm talkback interested

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
