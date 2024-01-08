f48fd2a8c6a4b0745e9b7779fcedce271fe8576c
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:52:16 +0000

    pronounce makeinto unable hurt impossible

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_stack():
-        return collections.deque()
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
