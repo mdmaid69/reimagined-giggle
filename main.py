4fb88ca91c37f9eb8c884bb48c444c287b2685aa
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:57:29 +0000

    geton among ask her plan

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+i = 0
+while i < 5:
+        print(i)
+        i += 1
