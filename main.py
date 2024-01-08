fa5c3b5b3e1f4c224602df43e0e79c6e1e8cd984
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:38:38 +0000

    choose aim foot collect question

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
