5dedcabc212687eab351e96608665451b9fe5b13
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:13:55 +0000

    connect while hit hold match

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
