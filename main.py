2fd5a1d2852964a871804f914f62bb81dd38f325
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:27:31 +0000

    want heart operate relationship arrival

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
