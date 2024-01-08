48bf7ee64bed7e60aaac14e3f7506997d90b1ffb
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:05:53 +0000

    mine happy listen corner nervous

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
