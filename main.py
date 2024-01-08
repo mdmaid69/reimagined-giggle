16736e94a398b979c60bb317d496868e4ea53fe2
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:15:29 +0000

    church increase hold suffer quickly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-  def sort_list(lst):
-        return sorted(lst)
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
