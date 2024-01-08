04bdb724582d1b96f4f4d44f97cda7819d6006a2
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:29:20 +0000

    get maintain least cause until

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
