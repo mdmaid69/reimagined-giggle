17cf03453897de4e864a21947000796c55c911dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:38:08 +0000

    turnout shall sleep takeout fear

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def convert_array_to_list(array):
+        return array.tolist()
