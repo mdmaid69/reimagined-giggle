6f641a34947c57d60723af790f15cdaa523567d0
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:32:44 +0000

    over lonely use obvious turn

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
