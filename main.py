3f4362e86f488fbb6d61e4717f712a9d96562e31
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:14:05 +0000

    earth sell band beyond violent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_directory_name(path):
+        return os.path.dirname(path)
