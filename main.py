aab990b6dd8b28f453d53bc17cbbf4e0a8277102
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:18:32 +0000

    week Pp bin day attention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
