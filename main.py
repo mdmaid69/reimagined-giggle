e3c5f3c757f0271025a1c85390637f9001c69b7f
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:27:31 +0000

    secret previous doubt shadow still

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
