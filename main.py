17cdccb86d5c10e5d4517511266410a227876b01
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:22:09 +0000

    consult really goout enthusiastic total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
