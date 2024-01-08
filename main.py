18eef2de7e2406c73af276665d15f78e711c727c
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:15:35 +0000

    open attach name style share

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
