bcdffcaec2bb6fc24f4c86a7e5d21731edcab67b
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:18:10 +0000

    neither unhappy rather battery degree

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import re
+print(re.match("h.*o", "hello world"))
