bc670c6a5393f17057f57d853675459c4ee64450
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:43:51 +0000

    flight succeed sure hurt proper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
