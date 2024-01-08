c9ef67e5e954fc7561be0399884e938015f60eb7
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:13:26 +0000

    coat mark exhibition shame safe

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_as_str(array):
+        return str(array)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
