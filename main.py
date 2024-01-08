98831186b67d7eb633ad81e14f5df7c0c0474cbf
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:16:09 +0000

    difficult may convenient blind takeback

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-for i in range(10): print(i)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
