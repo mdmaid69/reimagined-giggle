398185aae8ffa51b229f90d0a3d23e6c68162edc
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:48:43 +0000

    how simple sting minimum point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
