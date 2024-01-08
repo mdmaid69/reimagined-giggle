30dcfd19a3f4fbe7f8cc9b664f2f45644a5e9c57
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:35:05 +0000

    criticize be sing temper law

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_density(mass, volume):
+        return mass / volume
