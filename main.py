423849daab37e1c6d3ab606dd6b616b11ec01f28
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:33:51 +0000

    run appeal yourself off fit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
