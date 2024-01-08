3fe8380c45d4102804417cedd2a20cf6cee22ef7
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:39:45 +0000

    Qq likely waste immediate along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
