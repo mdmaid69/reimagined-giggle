cdc8587c4e60ff81800388335ce91a2df78d33e5
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:31:54 +0000

    crisis at divide error behind

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
