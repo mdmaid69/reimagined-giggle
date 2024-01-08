3a675db0f25544af3a65b6fb21a5533f84578d69
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:50:20 +0000

    plenty fair stand here welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
