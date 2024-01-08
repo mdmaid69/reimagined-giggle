f9d589fa421c810c6df37868487323cabbc87001
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:17:32 +0000

    pick admire 565end tonight job

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_stack():
-        return collections.deque()
+def is_odd(n):
+        return n % 2 != 0
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
