f75bccb49bb89ee786e461459cf6b091226beaaa
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:38:50 +0000

    lock coffee he wide however

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  def calculate_area_rectangle(l, w):
-        return l * w
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
