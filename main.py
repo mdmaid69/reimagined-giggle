15b2dadc4f2ea1eedf622eda1c2ae7e25f1ef081
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:03:09 +0000

    ticket exact busy talkoutof here

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
