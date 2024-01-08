2e43925904f42aaf5e448a5f823c89fe820a35d9
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:02:33 +0000

    somewhere project dream object lucky

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def multiply_numbers(x, y):
-        return x * y
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def append_to_array(array, item):
+        array.append(item)
