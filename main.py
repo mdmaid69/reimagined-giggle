8fba0f9cbe4891adb24f7d7de4f1cca72c98da13
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:50:45 +0000

    blame stay Jj precise immediate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def set_array_item(array, i, item):
+        array[i] = item
