30fdfd2ab77dfe1f9ee0f097160b329f18c9bae8
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:59:42 +0000

    fortunate project guide bringback hurry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def change_working_directory(path):
+        os.chdir(path)
