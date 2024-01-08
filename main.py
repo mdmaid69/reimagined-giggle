75e6a55af4e0b9b553e8884f6722dbd098cfc4dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:29:35 +0000

    order line design weigh without

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+print(re.match("h.*o", "hello world"))
