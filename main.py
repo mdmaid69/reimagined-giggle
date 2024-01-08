8c6a870fa7a1ced3e658aec27c65d904c09402f7
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:40:50 +0000

    copy runinto grateful bottom I

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import array
-def get_array_as_list(array):
-        return list(array)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import sys
+def exit_program():
+        sys.exit()
