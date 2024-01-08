7439fcee4d696b61c2d228deb8a9a7df12e09c11
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:51:23 +0000

    stop loan cheap fortunate justice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
