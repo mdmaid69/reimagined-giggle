7251564bbd470adeca12fefa47a7d8004a26c2c4
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:04:30 +0000

    Oo detail pump famous variety

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def add_numbers(x, y):
+        return x + y
