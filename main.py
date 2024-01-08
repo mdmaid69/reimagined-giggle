50067c82c5885056e38409d68cf31e53a59b3bc9
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:50:35 +0000

    impressive division whoever Gg official

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
