ab4092ea22967cbef3f7e43d74a173bffdfe40ba
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:25:04 +0000

    lookafter middle call produce hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
