f70d8cdaa756d0c1e03ae5fe78a85ec8c2aeb3d2
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:50:55 +0000

    plenty cover range beyond safety

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
