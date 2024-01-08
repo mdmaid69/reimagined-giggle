36360ab780815ff455f95e01ca827733a49a02dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:39:58 +0000

    Dd again giveout food in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import time
+def get_current_time():
+        return time.time()
+  def convert_to_binary(n):
+        return bin(n)
