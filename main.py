f82ce2be25eb8286556182534a936c634c1d4b2e
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:23:05 +0000

    neither various priority twist matter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
