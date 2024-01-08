cce3981efd265e6a5a172de522e64eb123956aaa
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:47:42 +0000

    share nature interview telephone design

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def get_array_as_bytes(array):
+        return bytes(array)
