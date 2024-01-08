d55b4bbdd173111691e7a1ec53fc40a856e52f04
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:28:26 +0000

    discover brain talkback punish die

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
