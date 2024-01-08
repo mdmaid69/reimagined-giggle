74f81bced8ebbbc8bce23ba62ccaf60e6d98fab3
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:18:30 +0000

    himself gap workupto sport model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
