3c8220fcb1384b789efa4a6f99dd052d5fd71577
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:27:54 +0000

    computer successful vision getup ride

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+import socket
 
-{body}")
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import array
+def get_array_as_int(array):
+        return int(array[0])
