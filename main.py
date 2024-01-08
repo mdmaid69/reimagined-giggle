27628c482335a5efb5e0102423965f7902550e15
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:59:04 +0000

    goback(to) survive yes point everybody

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sys
+print(sys.version)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
