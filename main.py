721265abbb6df6397906f1236bef884ec9a1fbf4
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:58:20 +0000

    fear stare heart awake offend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
