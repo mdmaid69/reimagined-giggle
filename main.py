5460c43beffb79d09861c440cf20f6c673b774bf
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:19:10 +0000

    bus apologizeapologise regular call dream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
