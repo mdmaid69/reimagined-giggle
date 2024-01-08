5af216993819ff87f0d0fbaa6751c63c6f1a10ac
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:49:14 +0000

    lost cool getout match tragedy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
