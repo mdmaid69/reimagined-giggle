f3db7e04238d659988629ea81b2404b084580c57
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:20:26 +0000

    pay push decide win immediately

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_max(lst):
-        return max(lst)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
