e07a48a43d8fc116d28e3c33497999c554383550
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:27:44 +0000

    live entry operate winter marry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+print(sum(range(10)))
