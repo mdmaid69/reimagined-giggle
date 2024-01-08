fbb2acac4cfdff6f1af9be3a0d2171ca18f37d44
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:45:49 +0000

    job soak admire typical forget

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
