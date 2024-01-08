751faed607be99ce0a51e15c3d13e19b9fab1227
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:03:59 +0000

    custom confused relationship skill guard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import sys
+def print_python_version():
+        print(sys.version)
