0b2fe2070c2205daa4fbb74e309163963546cf7c
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:37:29 +0000

    weak bring correct beginning black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
