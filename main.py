f5e6806548b47bce4844ab862d0463c016c0c41c
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:28:12 +0000

    want married name organize steady

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
 import os
 def change_working_directory(path):
         os.chdir(path)
