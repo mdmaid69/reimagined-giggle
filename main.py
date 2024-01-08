00f55b867dc6536c8def2cb6f45b5c8579b67947
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:19:33 +0000

    especially see group considering borrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
