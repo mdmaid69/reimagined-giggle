01ea9723aa42e8208c213e485e02908523cf0ee2
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:23:19 +0000

    learn square yes breakin behaviour

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def set_array_item(array, i, item):
+        array[i] = item
