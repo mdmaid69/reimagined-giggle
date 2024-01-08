4699d62f6fea7e02711c0a66c43062c1fc30ef69
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:41:06 +0000

    building chat powerful crash note

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
