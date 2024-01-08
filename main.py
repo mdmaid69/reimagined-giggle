a3c0016a3aef3e4d12b31db2a696fbb546493063
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:07:31 +0000

    among protection style cook speech

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
