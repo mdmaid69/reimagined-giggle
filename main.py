7ce091f75877003f682f913fd6c257d2a572574f
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:56:32 +0000

    content precise fast another impressed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
