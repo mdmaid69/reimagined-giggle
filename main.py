fe073b4c9def70d661d5465a90400fc823ad6659
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:13:02 +0000

    protest close flat heavy side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import os
+def remove_directory(path):
+        os.rmdir(path)
