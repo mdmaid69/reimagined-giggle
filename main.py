ae1fc546f6faa79cbd29c51c5ee883a0c64b1518
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:33:28 +0000

    once trick trip smile comeback

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import sys
+def exit_program():
+        sys.exit()
