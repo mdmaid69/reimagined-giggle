f100aa31fb46298d2c5769a4591ee43d09504258
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:37:52 +0000

    worry shallow edge over Mm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
