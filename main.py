774a851133d2879a60c50bdbfc2be70ad00be597
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:36:49 +0000

    smell admire pool after since

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
