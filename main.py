19ff10a5ef8be8d1d320a3a2dfb005a0c9fd91b2
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:24:13 +0000

    wild day price plant mine

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import datetime
-print(datetime.datetime.now())
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def is_even(n):
+        return n % 2 == 0
