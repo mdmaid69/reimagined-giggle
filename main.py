ea25fd4b9378045e6f09b7010ba4132b29cb0a30
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:38:37 +0000

    black convince breakfast later instruction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
