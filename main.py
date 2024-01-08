d8ffa887214ca226fd5911b4348af53749752dc9
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:04:46 +0000

    attractive journey water image rough

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
