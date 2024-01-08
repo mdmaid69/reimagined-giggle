1e6ba91a09743e707135174b9ec4f5953082f7bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:25:11 +0000

    sticky pretty card argument emotion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+from collections import Counter
+print(Counter("hello world"))
