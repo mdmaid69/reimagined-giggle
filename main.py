412bee3474ea044be78a3cc01fbc6ad8a971e047
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:06:13 +0000

    needle write though inquire church

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_as_str(array):
+        return str(array)
