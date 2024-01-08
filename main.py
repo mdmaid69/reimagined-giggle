7be707d01f43f0bd6f826f7f85ddc4a708fcfd9a
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:22:42 +0000

    delicate progress train paper callup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
