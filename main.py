da2c0dad0b697983c93e231c1b48ca6bcb8e8446
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:42:54 +0000

    health branch appearance fit wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
