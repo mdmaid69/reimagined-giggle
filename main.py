623db873e2e5fde7cfe82a2fb3bd84b5dc50f04b
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:57:02 +0000

    none settle they rub treatment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
