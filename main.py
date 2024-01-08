bd3187fefe7cf3eafc2602634af5d08aa8d7b7cd
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:34:59 +0000

    deep cost continue lay journey

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_exponential(x):
+        return math.exp(x)
