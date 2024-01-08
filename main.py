fcc03a24a4c0a9f00b3435f1fb7768c12183495d
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:42:40 +0000

    actually effect somewhere instant punish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
