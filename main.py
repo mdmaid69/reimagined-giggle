fa8915dcbf6c34dd54e69404564bdffd8b41aaef
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:33:48 +0000

    spin major pretend contact workout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Uppercase:", text.upper())
