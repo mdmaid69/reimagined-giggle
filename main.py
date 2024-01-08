0e67e26d638325ea9fade930f1cb6c870e9c78b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:19:24 +0000

    hurry sleep who undo someone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_perpetuity(payment, rate):
+        return payment / rate
