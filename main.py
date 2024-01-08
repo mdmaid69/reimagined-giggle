f1835bd8736178070e63e87af918947aa6ee397c
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:04:32 +0000

    cross correct desperate comeround thankyou

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-print("Hello, world!")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
