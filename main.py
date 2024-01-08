dfa6ecf768757ac951bccb7aa5fe58b342e7165d
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:33:39 +0000

    priority gather dry convinced 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_base_name(path):
+        return os.path.basename(path)
