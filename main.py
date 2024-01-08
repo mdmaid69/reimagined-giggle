f02deb1ea2beed35a68987a44890ec242a21739c
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:25:47 +0000

    getaway above simple nigga introduction

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-print(sum(range(10)))
+import platform
+def get_python_version():
+        return platform.python_version()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
