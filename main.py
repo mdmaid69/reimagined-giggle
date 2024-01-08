6cb56c2ca85d8129bc6a71ec75c7139208c90c90
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:20:54 +0000

    hollow male Ee rough brave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_palindrome(s):
+        return s == s[::-1]
