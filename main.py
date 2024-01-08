34ccc74714b751dfa76262464817b57be3fff428
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:37:16 +0000

    personal give theirs towards exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
