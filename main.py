985b4012eb3b5cd941ba2a6a3872505d64efe23e
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:50:34 +0000

    naturally range keepfrom nor stand

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import sys
-def print_python_version():
-        return sys.version
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import time
+print(time.time())
