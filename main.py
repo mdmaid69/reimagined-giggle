e39ece3daaa9e748dd51d22fed6a4b93fa639ea2
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:59:41 +0000

    beat so sick scared welcome

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+i = 0
+while i < 5:
+        print(i)
+        i += 1
