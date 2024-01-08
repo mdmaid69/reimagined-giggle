0b4647b7d4b4b823021114eb79c510f7bdd16925
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:46:52 +0000

    fault agree foot furniture repeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
