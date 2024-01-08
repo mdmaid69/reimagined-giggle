d36e157b1b1004366e5ec85cdf375bdae436e6b7
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:31:15 +0000

    well pay 573entertainment block usual

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import collections
-def create_queue():
-        return collections.deque()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
