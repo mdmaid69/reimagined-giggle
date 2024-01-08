7022d53819490c3324d5014e8bc19b4c3abce26e
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:34:36 +0000

    shade Jj object temper undo

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def cube_number(x):
-        return x**3
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def is_odd(n):
+        return n % 2 != 0
