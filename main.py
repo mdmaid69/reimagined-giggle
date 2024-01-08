72ca1503537005ad2cfd19975875535b6129fa65
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:19:10 +0000

    heart cook knock brief phone

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
 import pandas as pd
 print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
