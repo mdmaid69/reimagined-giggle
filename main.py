dcdc75a44287628bdee1f7d909cb12017a9914c6
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:05:40 +0000

    bringback anything eye cost art

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
