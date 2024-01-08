020ef1aa64b03355bf2698b1ea4345a75ab4f483
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:10:23 +0000

    get problem follow young mind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
