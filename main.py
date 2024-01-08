352a19fe1b5930cca04f4e6a4d4eef6e2ba49fb8
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:34:39 +0000

    lookafter be bend decision watch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
