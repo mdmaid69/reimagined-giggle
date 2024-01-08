03b033f72287ad8a04a9ec0124906cb66e41441e
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:09:50 +0000

    pump fence world home back

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
