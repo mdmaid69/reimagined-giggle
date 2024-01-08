36166959e7474cb0788fdc2cab52f1078e7d685c
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:03:17 +0000

    square stop drawing possession object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
