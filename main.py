8d9aa50fcb162873810ee00efdd52512f566b095
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:34:20 +0000

    friendly woman shine increase quarter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def reverse_string(s):
+        return s[::-1]
