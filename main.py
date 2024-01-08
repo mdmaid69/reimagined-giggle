7ad1b8dcf1744fd63f5b76c480db3e465b4d33ab
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:02:19 +0000

    noise cough drawing splash patient

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
