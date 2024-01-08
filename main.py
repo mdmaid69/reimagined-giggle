b70d3751e13efd2f46b2ccc92d5ef609d7088c93
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:44:48 +0000

    night accident hair deep skin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def reverse_string(s):
+        return s[::-1]
