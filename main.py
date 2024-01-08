f906d57798ddf1d8e65dbaa5c4e1f7eb6a2dc003
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:19:22 +0000

    speak slow tap copy neck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
