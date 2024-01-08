9ce9c84e7c85ee7d1139257d1776b32d96dc4ab6
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:51:29 +0000

    we I start regional have

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_current_working_directory():
+        return os.getcwd()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
