c86a4bf7a8ad7c0bca6cabfb2f40185c7f491d26
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:10:22 +0000

    turnoff ocean pronounce from agree

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-print([x**2 for x in range(10)])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
