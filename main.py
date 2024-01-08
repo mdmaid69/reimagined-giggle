2916dddf74f38f015140a8b103b8e73ec26c2c9b
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:04:40 +0000

    design bet really area design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
