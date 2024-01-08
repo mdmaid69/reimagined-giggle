fb0f17328cf98338256fac1fac34060f2853a5e9
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:30:11 +0000

    nigga hand petrol comeoff runinto

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import platform
+def get_python_version():
+        return platform.python_version()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
