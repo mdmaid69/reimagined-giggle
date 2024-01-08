735d241a257e6bdbd5a9fc929c6bf09324da824a
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:54:23 +0000

    head breakout appeal without emotion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+def exit_program():
+        sys.exit()
