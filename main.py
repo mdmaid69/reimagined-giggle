d8172e52d73d37d3a6f222ed951100dc747b7bd9
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:51:05 +0000

    offence reduce along bye goout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
