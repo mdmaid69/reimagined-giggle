ff290187c9b33c2985466ccabbb1c4240e9f9460
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:28:39 +0000

    squash large everybody everything press

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
