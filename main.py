ed86dc5cb16aaf6b098948329bfa86484014bed8
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:27:52 +0000

    unusual string post splash cheat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
