ed351cd1de1e61dfe37ad9b7b8458d00989199c7
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:56:40 +0000

    ignore giveup touch follow injury

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
