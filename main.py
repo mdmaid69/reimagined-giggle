b80b0a9c18d023ff83238c03fb38d2bea7e6bae1
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:41:57 +0000

    makeup refer loan thanks estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
