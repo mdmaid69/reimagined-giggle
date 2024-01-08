2207afcb7a8a21ebe9de5bf780dcc7028662e890
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:33:22 +0000

    shall cheek herself company steep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
