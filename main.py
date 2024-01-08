b5fd12482e9826f71a96fac2be3a10f3913b5b90
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:17:09 +0000

    aware male mess less uponon

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
