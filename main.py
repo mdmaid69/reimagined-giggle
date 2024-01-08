89d0326b37969a7e3a3882d0372501bac97b13f3
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:34:00 +0000

    dry used century badly early

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def convert_to_octal(n):
-        return oct(n)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
