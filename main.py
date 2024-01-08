eb18cc76674154271b6fed32d1e90afa93e02abb
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:00:10 +0000

    naturally noise send discuss gap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def set_array_item(array, i, item):
+        array[i] = item
