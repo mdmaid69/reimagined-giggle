b42368f23f53bf09228088483df829ca1540743c
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:51:06 +0000

    store lot exploration die loose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_array_item(array, i):
+        return array[i]
