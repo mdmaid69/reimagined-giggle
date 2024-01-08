7eefbe3d0bcd4443ef709051da022556d70fce97
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:54:58 +0000

    lay necessary protection flat mystery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def is_odd(n):
+        return n % 2 != 0
