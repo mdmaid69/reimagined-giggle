203e969926ff54f7472b27e15849e77f14469ac7
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:20:38 +0000

    moment may still contribute rank

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
