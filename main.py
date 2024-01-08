c01d52d81fe05bf30b1b88c88cf7da48aad045a6
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:49:56 +0000

    precise turnback scene trust attract

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
