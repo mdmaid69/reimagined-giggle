ae9cb957175b955816c222893ed2547738cf4443
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:44:38 +0000

    couple mystery connected responsibility passenger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
