159444e97ba09c67354740a9e008a3ce0e659a8d
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:43:46 +0000

    regular smoke getback wander recent

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
