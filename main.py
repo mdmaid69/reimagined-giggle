7df26643b1abbc849d8e6a94f834154163978ae2
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:30:36 +0000

    underneath combine lot side mood

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def square_number(x):
+        return x**2
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
