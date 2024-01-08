e0e77329eceede08332a3c74da0e0419823aeedf
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:48:21 +0000

    share flat month speech shelter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
