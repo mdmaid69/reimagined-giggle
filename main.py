bcdaa455547168c4c7e48ab851974781bd83d5bc
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:26:28 +0000

    ready many unite sit turnout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
