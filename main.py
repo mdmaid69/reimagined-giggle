8fdba721d9585048b9af150fe8597407061f798f
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:45:53 +0000

    sail scare plain odd knock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+def exit_program():
+        sys.exit()
