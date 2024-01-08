7127f1eca871757b8a64719bcdd5fb9e9e64eafb
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:28:58 +0000

    young objection thanks insist come

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import os
-print(os.getcwd())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def square_number(x):
+        return x**2
