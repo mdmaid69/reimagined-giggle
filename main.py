b0841379b071aa143565432fe10dc7537045af0c
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:48:52 +0000

    war niggaacademy gentle turn thinkover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_density(mass, volume):
-        return mass / volume
+def count_words(sentence):
+        return len(sentence.split())
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
