dda7b3bbbdf7046a874310bf3ede9deb74d06969
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:07:57 +0000

    shout tape weak row throw

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
