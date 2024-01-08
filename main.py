02d2c6191fcaeb9520a9b49359c9a19b3b666c06
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:47:08 +0000

    gooff naturally wipe rely experience

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import getpass
-def get_username():
-        return getpass.getuser()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
