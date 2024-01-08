44f7cb763d23a48419c78d9fbb4cd395890106ac
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:54:08 +0000

    sometimes comment behave exchange spot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
