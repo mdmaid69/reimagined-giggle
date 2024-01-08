5ea47bb6389fd45be2c04eef6b9c29a183f92de0
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:03:02 +0000

    free runover bad normal keepaway

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
