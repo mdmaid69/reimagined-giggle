d0438412ed44226cb948e1554c173750c9efd169
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:38:42 +0000

    colour partner snow thorough getback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-for i in range(10): print(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
