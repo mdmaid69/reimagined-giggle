9879393fa82766de700a6590a8f835319691f475
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:24:18 +0000

    trap union workon movement tread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_area_circle(r):
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  def calculate_area_circle(r):
         return 3.14 * r**2
