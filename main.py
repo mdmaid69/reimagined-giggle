f3e0a0c910279a28aab1546eba03a76b688760b5
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:33:23 +0000

    correct hide rarely interview minority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
