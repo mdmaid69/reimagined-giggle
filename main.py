b111446747f066b20e8ea25f772517a1a9aeed7b
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:35:53 +0000

    Rr live onto exam set

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
