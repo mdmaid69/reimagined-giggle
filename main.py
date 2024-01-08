14710ead9b218fe35b83c204fe4bcfd6fa41c4b3
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:01:25 +0000

    harm somebody report flood movement

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_speed(distance, time):
+        return distance / time
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
