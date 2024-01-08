358761004df63d744bfac22a6d0f9be8fa267289
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:20:25 +0000

    brave convincing charge blue advanced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import time
+def get_current_time():
+        return time.time()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
