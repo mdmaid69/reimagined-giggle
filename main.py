91171de8cecc1b06888cf4bbf9b112edd4d5a14d
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:30:20 +0000

    round wish find 571enough dish

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def get_array_as_bool(array):
+        return bool(array)
