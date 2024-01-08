50224c099e6cca3e106df65cb8482ce13c4fa69a
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:44:33 +0000

    hope address clear wish confuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
   import pandas as pd
   def read_excel_file(file_name):
         return pd.read_excel(file_name)
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
