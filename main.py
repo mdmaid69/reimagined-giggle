85e3c2c88cbb802a563a3f6732dfa3ba83ef603d
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:39:56 +0000

    size lunch closely character walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
