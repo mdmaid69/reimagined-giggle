de833b7b399dcccbe90e3171af10be6dc95e80ce
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:33:18 +0000

    report lookup return bear respect

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def get_array_as_float(array):
+        return float(array[0])
