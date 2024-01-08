9eba1e0824cd275093c444a5599d2315dc22e405
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:02:07 +0000

    small protest reliable fast hers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_array_to_string(array):
+        return array.tostring()
