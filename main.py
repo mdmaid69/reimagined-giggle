c95967eea8e9d8b27362e56b268366a555c792a1
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:56:37 +0000

    advice surface this thinkof circle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
