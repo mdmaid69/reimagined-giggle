d203e08476d0d453546119df9e543dbd7138b2ec
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:25:52 +0000

    far at keepoff runover sorry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_volume(length, width, height):
+        return length * width * height
+import array
+def get_array_itemsize(array):
+        return array.itemsize
