15fa31e041364988530de00d8aaa8826dc5337ea
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:51:47 +0000

    sound touch eager callout neither

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
