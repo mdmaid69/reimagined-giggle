81e579bceee32dc17a90df4cd56baff08b68007f
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:42:18 +0000

    behave declare manage open stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
