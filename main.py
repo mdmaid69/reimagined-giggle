21518c5fd58262a42980c2a97e4b677c66774643
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:57:46 +0000

    be speech receive arrangement pretty

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def subtract_numbers(x, y):
+        return x - y
