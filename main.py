37909384d564756fc1b8437ab91ce17a26ac6649
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:08:59 +0000

    television clean check hurry machine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+def get_current_time():
+        return time.time()
