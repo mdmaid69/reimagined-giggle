a710ed08abaea48b6b3105a498c1851af1a4a776
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:55:05 +0000

    hospital practise physical my actual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
