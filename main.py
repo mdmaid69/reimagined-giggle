019077bc041f405b621a7ffeeb6c2e544465fe13
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:06:02 +0000

    scene true under pick goaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
