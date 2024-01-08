86e67471131df252e7686f5498a95868ab3926c5
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:32:00 +0000

    path obvious right trouble declare

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
