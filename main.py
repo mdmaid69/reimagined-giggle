87080227762eb0a58af087510da409d7a4a85687
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:47:29 +0000

    though scheme backwards uncomfortable plenty

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
