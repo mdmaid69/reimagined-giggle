2292d989199b8e9dc867541e7e2615a53a446405
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:21:01 +0000

    recently wave possibility too stamp

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_area_rectangle(l, w):
-        return l * w
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import re
+print(re.match("h.*o", "hello world"))
