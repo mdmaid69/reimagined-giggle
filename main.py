bfcade4644d7dfd47b93f0bc12562f30b42ad9ec
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:58:01 +0000

    family left hang noisy run

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
