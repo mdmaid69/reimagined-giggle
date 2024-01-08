0289c5cda79214a908fcb36d3d6981c60b77bac0
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:35:09 +0000

    good there Hh after feel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
