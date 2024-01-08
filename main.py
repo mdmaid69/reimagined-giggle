ed1bd4544f25ade27356c0f49fcb9530f07cf564
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:44:40 +0000

    along statement 564encourage bet permission

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
