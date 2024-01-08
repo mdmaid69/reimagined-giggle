abc4d7121d94dded8a51784449d659cbe5865882
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:17:58 +0000

    dream touch announce thing trial

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
