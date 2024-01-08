89cb5fe348813107c4695d3a82243a050beea50d
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:24:35 +0000

    remark door value again mind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
