23fb0e190e7c99c4bbf785f13fc9c73ac3d6e245
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:43:13 +0000

    mean sit grade compare wake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
