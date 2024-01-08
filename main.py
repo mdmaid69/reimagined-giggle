ef5f738068333185e81ffbaf81e876237f184622
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:37:00 +0000

    public doubt morning above interruption

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
