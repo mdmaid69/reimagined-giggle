eb4282ff276927a9c6a3992adbce2e78cce8fbfb
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:00:05 +0000

    afterwards transport shock fly difficult

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
