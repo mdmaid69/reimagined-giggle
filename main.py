7d2c6d02f83a2e474efa49309690bce6cc6af92a
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:19:45 +0000

    pull reply she sound police

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_work(force, distance):
-        return force * distance
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
