5360f5512b6c025fcabb599de98f240e2e263306
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:36:33 +0000

    experience load boring by shadow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
