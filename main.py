2d70fad4d64cde3cafc67c6c7a071bc6588498ba
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:12:00 +0000

    himself confuse just bear now

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_pressure(force, area):
+        return force / area
