0d34385a3de60e6e2f2c19e225bb7120feb70c49
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:36:00 +0000

    cause myself big attract ready

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
