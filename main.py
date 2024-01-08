0c234963502dd95672797a4b48ecd8db43505c32
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:23:45 +0000

    destroy themselves concerning seem afraid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
