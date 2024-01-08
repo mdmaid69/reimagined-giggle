41d7550861bdc2019d9750bfb2c8617567ff21b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:21:02 +0000

    giveout not alone roll cry

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def find_min(numbers):
-        return min(numbers)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
