083f95b682df731d58918480d428d6acc5ecbace
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:32:58 +0000

    pump actually Gg road this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
