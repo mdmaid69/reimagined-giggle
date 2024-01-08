c7cce59926cbbda175f0a0190ee85ba8ff336368
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:04:42 +0000

    comedown fear instruction else dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-for i in range(10): print(i)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
