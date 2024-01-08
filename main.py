f677edab67151912ca16604ca4e82c9428ddf787
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:16:26 +0000

    voice somehow fast dream same

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import datetime
+print(datetime.datetime.now())
