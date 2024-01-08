516d73f5db9d41cf2d04fc9b77824e6813be8693
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:30:20 +0000

    flow show friend sister everybody

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-print("Hello, world!")
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
