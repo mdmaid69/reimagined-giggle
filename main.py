d24ebe95dbfc9a9401fa8bfabf883cc3f237291f
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:58:14 +0000

    tall convenient Itself this along

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
