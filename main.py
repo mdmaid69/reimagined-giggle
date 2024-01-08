0e543e7e2600eb84cf4a220bc6e455e451057df7
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:06:57 +0000

    married none name silent improvement

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
