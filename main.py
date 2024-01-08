0b6e92b855c43acfe96411ac5b71f96a93c315bb
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:04:01 +0000

    prefer too majority week motion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
