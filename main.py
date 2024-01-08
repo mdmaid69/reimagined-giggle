1c5075ec1cce0e1b9f0f0e3232fcd0b1dab99dfb
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:59:17 +0000

    effort clean defend method identify

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
