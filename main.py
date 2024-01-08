18fccb279f5918b6ce3bf707862a5f547e4c7cb7
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:18:17 +0000

    fly you responsibility sensible human

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+print([x**2 for x in range(10)])
