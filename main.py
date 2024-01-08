5f5ad34b519521771c6f3670184c84ad61cbab4e
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:38:44 +0000

    heavy family welcome hungry take

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def greet(name):
+        print(f"Hello, {name}!")
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
