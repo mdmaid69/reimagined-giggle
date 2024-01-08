e3147838e5ade320614754166b3aa05db40679ab
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:19:24 +0000

    report discovery callup turninto fork

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def convert_to_octal(n):
+        return oct(n)
