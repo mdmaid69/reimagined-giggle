36cc36d9ebd18dd431d0d76be2a0fda11bdab435
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:08:09 +0000

    left shake identify punishment film

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import collections
+def create_priority_queue():
+        return collections.deque()
+import array
+def get_array_length(array):
+        return len(array)
