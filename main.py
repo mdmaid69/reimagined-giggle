77f6c2f0a985a8f0f680cd491fdaa464b0d27558
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:22:27 +0000

    happy search process border search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
 import sys
-print(sys.version)
+def print_python_version():
+        print(sys.version)
