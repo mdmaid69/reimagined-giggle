629f1a0ddf8cee8d5e9a9a6488ec98015cb8de76
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:19:12 +0000

    blue moment sure community behave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
