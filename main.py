87aa56a8e707fa6fa03e23ee8e01a26fb2456c6e
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:12:29 +0000

    secret thin pupil jump lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
