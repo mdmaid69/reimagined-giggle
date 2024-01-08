ca79843c00570738a190c9f2a1061b85e113297f
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:43:42 +0000

    damage even sister pin information

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
