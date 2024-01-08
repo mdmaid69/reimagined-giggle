c16ebb54b896537618021fe548fdbd086c78ca77
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:16:25 +0000

    name set level future stand

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
