bb47cbd4bc454cdf909a0e546e39c754a131a611
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:08:53 +0000

    think education address please cheque

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_work(force, distance):
+        return force * distance
