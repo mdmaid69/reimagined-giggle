3cf4c8c13c7d82f329015c059e601f6dd573d686
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:19:49 +0000

    above create rob their frightened

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
