6bf63a556390e243b40c4cfd881e2f820d848c7d
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:07:11 +0000

    fair name buy sing easy

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_power(work, time):
-        return work / time
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def get_array_as_str(array):
+        return str(array)
