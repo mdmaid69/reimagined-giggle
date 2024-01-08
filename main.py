f030c8c8ee94d7ce8541b0024757a2e55103b51e
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:06:15 +0000

    Ss could spin whom exercise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_item(array, i):
+        return array[i]
