7cf570d46f688236972615227276e505a5bdc570
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:26:26 +0000

    connected estimate glass weekend through

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+print([x**2 for x in range(10)])
