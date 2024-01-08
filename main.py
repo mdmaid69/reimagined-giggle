07d7bb2f5613d51670e6042321f7043c766815da
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:08:20 +0000

    precise as issue spoil joke

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
