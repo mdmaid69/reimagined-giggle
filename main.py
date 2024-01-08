64f39c479b4e9158c3e0b014d3302218561f7db3
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:06:48 +0000

    disturb bang demonstrate material pattern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
