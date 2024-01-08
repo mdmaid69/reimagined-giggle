313c081c1c379715f2038cef9791d948e4e4a20b
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:19:33 +0000

    sink talk rush callat project

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def is_odd(n):
+        return n % 2 != 0
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
