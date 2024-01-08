e3bd1e51db0099ddf130431e500f3dc55e43dc4f
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:29:37 +0000

    among laugh money she rub

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
