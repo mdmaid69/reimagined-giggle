d9239a03952da34031ee1f3e0b8c90ce3d66598c
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:25:21 +0000

    leave true congratulate lonely variety

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def find_max(numbers):
+        return max(numbers)
