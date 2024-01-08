3f3a9b4352a89d89e4950f96680a59b930f8cff7
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:51:30 +0000

    crash rate school thick back

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
