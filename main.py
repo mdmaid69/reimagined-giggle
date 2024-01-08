d8e079a273204e549fda59a68b99f768c2116ef6
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:17:35 +0000

    conduct comeround do say true

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def reverse_list(lst):
-        return lst[::-1]
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def find_min(lst):
+        return min(lst)
