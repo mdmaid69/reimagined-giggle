0181415bc49077dc2d9d7ef813e74b2bde4436c8
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:52:03 +0000

    scared team slightly watch platform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
