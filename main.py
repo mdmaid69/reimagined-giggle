804e659a86044c7abfcd6f2f3d6419b7bd2c3874
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:29:06 +0000

    game several far lie takeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
