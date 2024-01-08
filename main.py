fec79667958e9d590f1fc079a0e10da5879f32ac
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:46:43 +0000

    bet and brown sail operate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_sine(x):
+        return math.sin(x)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
