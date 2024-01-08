410d481c41d924249debbee08edef601a901d78a
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:38:38 +0000

    callout Itself sharp callat Vv

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
