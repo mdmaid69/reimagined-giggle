12b85ff2464adaaa258d2a4b1df1f54281a78240
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:22:38 +0000

    yourself defend not find line

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_blocks_allocated(file_name):
+  def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
