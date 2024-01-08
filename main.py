381a87d102089814bb20fa0abec5d0a2a9b309fe
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:08:43 +0000

    funny accidental wherever statement about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
