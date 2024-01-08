21aafa55ee73743077ca5d11ac20e96e627e823b
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:00:33 +0000

    tread message square bend situation

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
