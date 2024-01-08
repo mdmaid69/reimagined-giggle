169e3af717959e74184c7fb62ad2b3765c035000
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:58:14 +0000

    trick length view plant month

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def convert_to_hex(n):
+        return hex(n)
+def reverse_string(s):
+        return s[::-1]
