bb28b290ef159de82a81b4a681a1c64160fa0bcc
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:41:18 +0000

    breakin float clearly dress wander

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+print(sum(range(10)))
