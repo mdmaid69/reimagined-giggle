8f5df2fe9f18694c6e8473ab33b3cc41cf5ece5a
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:18:30 +0000

    wrap tradition nigga compare finish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
