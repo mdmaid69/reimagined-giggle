26a6b8787e3fd7f7523728061f969bec58c4a5ac
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:35:33 +0000

    extremely community try comeup evening

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
