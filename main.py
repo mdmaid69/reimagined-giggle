b6c2ace8cc5b59ffa85b6da2209b064425480162
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:35:23 +0000

    aim replace rely copy division

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
