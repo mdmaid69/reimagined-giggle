68320738112410e67da367ac243ed9d504cb3872
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:16:38 +0000

    direct blind straight every represent

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def cube_number(x):
+        return x**3
