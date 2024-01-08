62d9561fc2c7a528533c366561e2326b1f241c4f
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:27:43 +0000

    walk yes service general bus

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
