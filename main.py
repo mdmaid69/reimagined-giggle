9f44e89d64428c20aeb87e1d3e69d36150da88d6
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:44:47 +0000

    surprised new demand private basic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-print([x**2 for x in range(10)])
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
