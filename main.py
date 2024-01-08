a01f48ef6ae98ce2d6919eace549fee493fdf158
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:57:43 +0000

    then case exactly fond love

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-for i in range(10): print(i)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import datetime
+print(datetime.datetime.now())
