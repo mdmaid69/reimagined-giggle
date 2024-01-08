6eb6007d775f407801b8b93e5f814aada4dc0ded
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:40:09 +0000

    bringup trip sound generous further

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
