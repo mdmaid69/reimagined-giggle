16eaabca2876455e5b3156f3f6db24cdd2f2cd18
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:32:24 +0000

    tradition secret swimming bury at

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_str(array):
+        return str(array)
