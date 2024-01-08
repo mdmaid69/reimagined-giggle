d235153632dd9c15032bce05c086225e68893bff
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:18:14 +0000

    Ss gooff clear cruelty high

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import tensorflow as tf
+print(tf.__version__)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
