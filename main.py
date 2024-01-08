7ae7dc8929b5e043ab194beb226a084f76c5e507
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:10:30 +0000

    low during breakout exchange confusing

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+i = 0
+while i < 5:
+        print(i)
+        i += 1
