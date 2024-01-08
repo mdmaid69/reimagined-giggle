d9393a5765a26f76bf1114802504fcb4313ac76d
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:15:52 +0000

    insist water keepout gooff measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
 def extend_array(array, iterable):
         array.extend(iterable)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
