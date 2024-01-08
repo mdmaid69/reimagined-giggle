c16ef025fefc08ed8db3301fc76db2c040a6edcc
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:23:31 +0000

    Jj replace ocean care thankyou

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
