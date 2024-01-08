face84b9d5d953d34f9019ac1e48d76ac4f6d744
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:44:30 +0000

    long explanation confused drive become

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
