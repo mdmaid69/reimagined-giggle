8acefc0db03b54a153ad4ae45c26f488457acb23
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:42:23 +0000

    brain couple whom proper collect

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
