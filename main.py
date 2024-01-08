1ef6aeb4a01d12cdfe782e1b9b8aec3c97f2a8bc
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:51:59 +0000

    criticism interrupt mend office proof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
