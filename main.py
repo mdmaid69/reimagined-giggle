70aec285be625cdbd8e666cb1d9c5332af0a230a
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:18:49 +0000

    miss relative 567enemy real join

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
