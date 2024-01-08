79ee9ce2dd8fdafdb303aa122d86efafef9c564a
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:05:31 +0000

    disappear meet workout fold question

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
