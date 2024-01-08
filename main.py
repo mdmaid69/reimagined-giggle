6fb9babd08413de96c1968a689dbf843e8a1b589
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:12:13 +0000

    comeoff threaten sleep hard plate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import collections
+def create_priority_queue():
+        return collections.deque()
