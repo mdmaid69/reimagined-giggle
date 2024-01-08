fbcbc6ee03d65a9c6a8b901b4d65793752fd92a0
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:15:01 +0000

    justice left welcome opportunity disagree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_as_bytearray(array):
+        return bytearray(array)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
