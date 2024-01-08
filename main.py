85fc96be42b89af383101212fcaaa434e5a17479
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:43:26 +0000

    sign lay single twist seriously

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
