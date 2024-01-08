7ea2cec1617ddd72bb9157c50f89c76db6efff70
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:16:34 +0000

    tax happen contrast skin scare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
