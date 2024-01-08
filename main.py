b7c6746d01585b082601bb39f4a3794c66ac7e54
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:44:33 +0000

    importance kick nowhere underneath collect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import sys
+print(sys.version)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
