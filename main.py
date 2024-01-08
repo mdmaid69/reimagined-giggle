404bf86cf8afceaf47d2f5a9dd63e54546d750a0
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:35:38 +0000

    before careless second ring claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
