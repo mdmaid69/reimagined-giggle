3430122839079235270cef297ab8fac4e8b10190
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:25:07 +0000

    interview present introduction fresh arm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
