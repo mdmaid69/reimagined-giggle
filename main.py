8aad7623724c0d2fa4728ac9829446f3eb0f65cb
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:53:19 +0000

    main swim freedom behaviour fence

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+print("Hello, world!")
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
