a77b0bf453cfcd83e59008dfd6a83cdb8bbb254a
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:59:41 +0000

    schedule conversation year no care

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
