9f18c9a049b13d49d32c9600a66c47fb5d2fbbd2
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:33:25 +0000

    nerve balance interest declaration off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
