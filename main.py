f218c2aaf07fe2bda30189c7591f629b194a33bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:21:50 +0000

    marry in idea sense heat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def cube_number(x):
+        return x**3
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
