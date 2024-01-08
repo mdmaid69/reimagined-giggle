e9cfa4025638b7051ecdd9462a7ed5a730d0ca1b
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:30:54 +0000

    circumstance whole nature oil forever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
