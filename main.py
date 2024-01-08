76157923f82a076989e0987d6b7b7030d69ddddf
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:28:35 +0000

    often body cheap complicated affect

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-name = "Python"
-print("Hello,", name)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
