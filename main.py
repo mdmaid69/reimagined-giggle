4669f0a45e5bae7a688d33b186ce38af85ab7285
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:43:23 +0000

    sail juice reasonable somebody time

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
