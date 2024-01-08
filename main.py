c72caf16d01ac41e64825757540f5c63c38bb056
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:40:09 +0000

    upset later brain me moon

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_int(array):
+        return int(array[0])
