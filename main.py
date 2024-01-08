aca3a16de7b2965d123ccd58d1f4767565c79945
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:36:36 +0000

    trap be account ordinary become

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_set(array):
+        return set(array)
