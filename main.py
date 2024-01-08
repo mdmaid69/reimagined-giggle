8fde2335760b63fa5e2064885e5557e8c489c507
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:49:14 +0000

    urgent relax plan tune interrupt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
