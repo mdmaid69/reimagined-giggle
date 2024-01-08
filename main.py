23399519a4e4db214071cff95dd532e090a278fc
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:09:00 +0000

    life see sign whichever hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
