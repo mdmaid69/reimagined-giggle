3196a36cfd484daa06a4be85a4453829c6bc0a66
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:02:16 +0000

    generally contrast mess petrol keep

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import random
+print(random.randint(0, 100))
