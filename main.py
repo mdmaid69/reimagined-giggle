c0b96395a75206a5b64bdc1bfd8fefe555b5f0d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:59:46 +0000

    happy crime comeoff unpleasant arrangement

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
