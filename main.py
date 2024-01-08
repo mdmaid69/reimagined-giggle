faa178175b45f47aec255a8c72faf12046842d9b
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:46:58 +0000

    smell it book compete watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
