043089974e62dec50ea480bd0434cdb3ee1f9c3c
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:37:39 +0000

    machine in entry birth whistle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_list(array):
+        return list(array)
