7ec6d1407c11c7219a46e3e880307cb5644500cb
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:48:58 +0000

    failure friend war mess much

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import sys
+def exit_program():
+        sys.exit()
 text = "Hello, world!"
 print("Is palindrome:", text == text[::-1])
