f2d02781d80612846e15ecf6a653608261b876e1
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:53:05 +0000

    close later sing black second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
