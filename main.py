61d8e2a652ab3cd7fd631cadab625d18f7e41457
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:59:14 +0000

    prepare promise invitation cause sound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
