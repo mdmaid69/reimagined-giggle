1083aea88110d6003f99b0b89119984e9dbda7d2
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:50:50 +0000

    strict side reduce comeover keepup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def subtract_numbers(x, y):
+        return x - y
