cbc88797ecb9ed450bc21df1f2228ef22d1baf5a
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:55:57 +0000

    further worried quick pay tone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
