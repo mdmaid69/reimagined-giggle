b398d2bcd41f79a31afaa013a0cdd5e1516c830d
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:18:05 +0000

    female they lay miss shadow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
