cfdc20377095013f6f931631e296fcbee95871a9
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:58:19 +0000

    Yy gas month few separate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import random
+print(random.randint(0, 100))
