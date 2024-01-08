15ea6627a0ad03afec8863bfef07374edfd19b67
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:28:38 +0000

    and side invite recommend thin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
