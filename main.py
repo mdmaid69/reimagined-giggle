de1da31469c6619617220e820114683cc9d11066
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:19:07 +0000

    decide broad style violent own

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import platform
+def get_python_version():
+        return platform.python_version()
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
