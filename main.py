865c064355fd9e4112372bd5980f02223bc2ef27
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:36:09 +0000

    whenever curious action hungry fit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
