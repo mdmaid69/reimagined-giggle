4aeaa9f3d6acddded7a69aff42a0c4ba46a8d9cc
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:20:23 +0000

    free pronounce ashamed however field

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_array_itemsize(array):
+        return array.itemsize
