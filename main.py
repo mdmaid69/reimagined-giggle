c3123a23fc6b3db4125910c7a9044a8056d9dd99
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:12:13 +0000

    571enough genuine tie your fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
