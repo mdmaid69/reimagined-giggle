59f064636688dec6992cec09b1b7ac76a765c7a3
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:55:06 +0000

    pronounce talkoutof keepaway die fly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
