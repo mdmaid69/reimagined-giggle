344fac74f088528598766d10df25f072e01705dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:21:05 +0000

    report disapprove right pride brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import collections
-def create_queue():
-        return collections.deque()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
