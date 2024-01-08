4111bfc978d2a8fb51cdb424b32960ab90261855
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:36:55 +0000

    view for bag fire corner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
