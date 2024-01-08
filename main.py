94df97e17035011b8b2c5fa8f37151a1abd6e506
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:23:03 +0000

    rubbish public meanwhile more recent

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import os
+print(os.getcwd())
+def is_odd(n):
+        return n % 2 != 0
