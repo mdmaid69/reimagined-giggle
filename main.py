fbe47c7da6a78f0a7501cfb5e6476ea4a2ab5fbc
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:43:49 +0000

    marry discovery ours contrast sail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
