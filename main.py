be2879b75d5ccad168fb7b03501ea203cd76d768
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:33:14 +0000

    know its pot improvement friendly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
