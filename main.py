fbdcea233e7b267519cad7610ae524b307e5515a
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:33:55 +0000

    defeat afterwards arrive start fond

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
