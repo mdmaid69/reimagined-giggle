ea3b87f3fdace493641b3d6cded6523a0ed09650
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:30:09 +0000

    consist vanish mad breathe hard

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import array
-def get_array_from_file(filename, typecode):
+def get_array_from_string(string, typecode):
         a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        a.fromstring(string)
         return a
   import os
   def get_file_blksize(file_name):
