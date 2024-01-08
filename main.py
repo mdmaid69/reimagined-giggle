40d0ba8c471fd70dcff959e9ffbcee8e0d42f4e7
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:39:49 +0000

    point common brave worth order

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
