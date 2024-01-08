147e27e9082ea8e136ee7b0ad5c4c268081ba781
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:48:37 +0000

    repeat sensitive male behave occasion

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import platform
+def get_python_version():
+        return platform.python_version()
