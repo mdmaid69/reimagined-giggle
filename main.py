f9aa134c6f9946935925077df24818722300d2fa
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:36:47 +0000

    pupil guilty method after reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
