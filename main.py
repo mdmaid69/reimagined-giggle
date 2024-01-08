ac161f39fd83721a2a8830d229beebd4b37ddbe1
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:43:56 +0000

    slide matter work human disturbing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
