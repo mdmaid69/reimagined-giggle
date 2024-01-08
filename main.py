bd1997a281a7773c426f46a4990b3cc28049aeea
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:06:39 +0000

    temper notice heat level left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
