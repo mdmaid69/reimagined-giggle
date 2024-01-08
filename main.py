b6af73e9b49e0d9d7b63c4da565567fc53037e08
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:42:35 +0000

    recently occur justice pressure act

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import collections
-def create_counter():
-        return collections.Counter()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
