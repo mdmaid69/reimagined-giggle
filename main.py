0b57e4a6224ef7efa5bc3526401cd6adce8f91dd
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:15:22 +0000

    pour splash Ll beside lock

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  def add_numbers(x, y):
-        return x + y
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def divide_numbers(x, y):
+        return x / y
