b85b8e620861171ed27e4c6391c14d5fdd116829
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:59:07 +0000

    high walk properly workupto coffee

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def multiply_numbers(x, y):
-        return x * y
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
