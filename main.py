3a925f496d32b35bf93b95c416e29d218d5a4fe2
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:33:41 +0000

    squash simple feature invitation Rr

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
