70d7977aba12e30e089bc709fe4ee1877fd2565e
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:58:33 +0000

    late takeback impossible young return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
