597ddae998b51866a818f74e5df6d926693cf0ee
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:03:04 +0000

    favour hurt goup and few

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
