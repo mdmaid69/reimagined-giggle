28d421d4c87be75fd30b299bb7990e09de7be62e
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:45:58 +0000

    eye leave estimate along although

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def square_number(x):
-        return x**2
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
