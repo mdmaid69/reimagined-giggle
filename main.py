04fa7147a3fc4c937b8cfb575727de0adb01c6d8
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:28:57 +0000

    rather minority fashion the produce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def remove_duplicates(lst):
+        return list(set(lst))
