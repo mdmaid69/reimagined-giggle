b6716e90eb5297b6aa17e2c88e4ef53327bb9cfd
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:29:44 +0000

    branch might talkback manage interest

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
