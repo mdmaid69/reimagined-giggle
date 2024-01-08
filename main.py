f3941e86e2515f1a97b508ce0c896eff68ea522e
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:44:04 +0000

    follow transport main experiment none

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def create_user_list():
+        return collections.UserList()
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
