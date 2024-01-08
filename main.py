9042380446a27a4efd28710feede1264bc631afa
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:28:10 +0000

    makeinto attempt find watch object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_density(mass, volume):
+        return mass / volume
