5e83208d2b57673947a40b339f80db58426ac102
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:18:10 +0000

    before loan bill onto walk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_power(work, time):
+        return work / time
+import array
+def convert_array_to_string(array):
+        return array.tostring()
