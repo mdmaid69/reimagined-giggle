b9f425f84b81799c56964cbe85ab78db4c2be55b
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:51:18 +0000

    in this tense hollow excited

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  def reverse_list(lst):
+        return lst[::-1]
