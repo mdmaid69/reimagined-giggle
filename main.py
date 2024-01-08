51412dc1b5ffa5ae246283d2362f761d4349895b
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:43:18 +0000

    guilty read hello thin same

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
