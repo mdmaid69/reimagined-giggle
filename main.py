50740832e715151f14ff57d6e3f7be8ac9209abf
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:56:57 +0000

    now today guard fortunate fierce

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_acceleration(speed, time):
+        return speed / time
