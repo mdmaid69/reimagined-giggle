52a88882cd67a5ee1603fe62c7651e591268dcbf
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:29:28 +0000

    round avoid unable environment school

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def greet(name):
-        print(f"Hello, {name}!")
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
