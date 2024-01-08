721cc7350b42b4203519b86d94b18a0710406e35
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:43:37 +0000

    develop break public press after

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import time
+def get_current_time():
+        return time.time()
