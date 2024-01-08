8d7995bcc1e71c3a34ce1dea36e4f1d4367df1db
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:00:36 +0000

    try them scale amount fashion

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def get_array_length(array):
-        return len(array)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
