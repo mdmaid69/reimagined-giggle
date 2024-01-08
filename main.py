86eb6eb955158c0e55e3a2f4b89dd769e8a96cdc
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:05:11 +0000

    confidence bunch object fit get

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
