b32555ce82923f3b8d4dd6b0e6f0bcb00b9bf194
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:02:02 +0000

    split flat damage plenty meal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
