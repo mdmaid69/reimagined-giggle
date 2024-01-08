e348607fe98b098a6ca237fe4b4484689203c25d
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:47:39 +0000

    smell lie bye telephone statement

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
