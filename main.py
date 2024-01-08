a44daa75bd5fa93dce6d2b6d4f34aeb082412cd4
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:45:51 +0000

    past fact goon money recommend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
