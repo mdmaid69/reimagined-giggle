eeb56e43fac6344d5eb0269cf3b6349c4e038c56
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:33:39 +0000

    soak health memory mad hold

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def convert_to_binary(n):
+        return bin(n)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
