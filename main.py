81f509edf63aca21daed38cd62e3599ba753c675
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:07:54 +0000

    previous blame bored tall form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import array
+def clear_array(array):
+        array *= 0
