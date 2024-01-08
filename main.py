14263075d4f8ace5afef49b38d2e409047536e60
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:41:23 +0000

    tap be finish it any

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
