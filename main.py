efe4e455f81e6f4d7d7636a7fe084f65a67e8bef
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:44:11 +0000

    season left far group search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
