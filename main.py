002e3bc6098297363322b48bcdcf4747c95e453f
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:14:43 +0000

    argue official home copy move

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-x = 10
-y = 20
-print("Sum:", x + y)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
