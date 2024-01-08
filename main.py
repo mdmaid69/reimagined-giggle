c9357754ae42e739fbdcaa7ef74888589830f663
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:37:21 +0000

    calm our leg appear rubbish

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
