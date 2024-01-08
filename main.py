12a8f2e33c0326de938c318c990b21f3388164f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:25:09 +0000

    period front teach cold raw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
