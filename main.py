60097a452591d46d54649a3f33415997cc83b0c3
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:34:54 +0000

    suppose tap respect stop imagination

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def subtract_numbers(x, y):
-        return x - y
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  def convert_to_hex(n):
+        return hex(n)
