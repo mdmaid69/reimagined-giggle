ad6d149894ebf2610266ef530a84c731d0ec4116
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:13:41 +0000

    private mend realize car ourselves

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
