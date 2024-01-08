98191d9c737a9b241b7fbdfeb43863d9fd463ac3
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:00:43 +0000

    blow before me really personal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
