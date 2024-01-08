dcac0090bb9d0c6fddaffe90ddc1fa4141710679
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:35:25 +0000

    complete minimum least tip hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
