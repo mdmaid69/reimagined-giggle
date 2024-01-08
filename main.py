681e5418566540732279dbb6863105a9dfb7d5b4
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:58:53 +0000

    harm stiff pretend word risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def calculate_area_circle(r):
+        return 3.14 * r**2
