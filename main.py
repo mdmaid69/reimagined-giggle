1c7b9f7757c694cd884a064ffa83068ea23787cf
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:52:54 +0000

    memory cool tax stretch clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_directory_name(path):
+        return os.path.dirname(path)
