b96eb8e6242f49edc3b07ae97d0ff0ee26b55712
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:44:20 +0000

    various arrival value season bank

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-  def is_odd(n):
-        return n % 2 != 0
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
