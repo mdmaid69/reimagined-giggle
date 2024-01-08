0e0e079da9dd769a63877e18a8fb2c287187c887
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:01:45 +0000

    only little giveup broken movement

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def add_numbers(a, b):
-        return a + b
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
