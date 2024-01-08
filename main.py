bd62630bd46098ad9c2f3ff050a0112ba5cd6bde
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:02:04 +0000

    scare main desperate so shop

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-print(sum(range(10)))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
