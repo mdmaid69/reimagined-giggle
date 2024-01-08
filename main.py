729b2d6531d16e37a038905655c501a01ec93923
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:27:55 +0000

    breath ago press care Itself

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
