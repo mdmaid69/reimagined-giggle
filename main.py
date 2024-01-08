4fbacc3914f16aa4236cd2d31c16aa437ebc7bb2
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:14:10 +0000

    deaf bag sweep telephone control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
