8b6ab8d89eb7da4347467a642f8038ffe557571c
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:44:23 +0000

    but callout off majority 572enter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def convert_to_octal(n):
+        return oct(n)
