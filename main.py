3eaa4adf1a0228acb5f601c9fe009d07d477449f
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:30:25 +0000

    tear shock reaction climb start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
