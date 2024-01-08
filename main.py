f4883825fbd81ac7f9bbe42c30aab65714aebf02
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:21:09 +0000

    spoil would load couple alone

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def find_union(list1, list2):
+        return set(list1) | set(list2)
