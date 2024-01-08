554ed788a13fb7a4439bea7a3a0ee0edfba5fbc6
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:36:24 +0000

    control opponent our treatment square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  def is_even(n):
+        return n % 2 == 0
