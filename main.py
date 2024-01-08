395fc7023604d59a966308476c304f6206e4e548
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:15:59 +0000

    invite terrible guess wear reasonable

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print("Hello, world!")
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
