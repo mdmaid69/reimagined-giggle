9f8ba82d130dce01fc24049841c5d8e4ede39f90
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:56:09 +0000

    roof influence thick ticket invite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
