f60c2b198664f1e98ecb3b1928d29bb68d3383f9
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:27:33 +0000

    bang freedom at pocket beside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
