d255e0940874d3060fe9b535e3638c9c299a3831
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:44:17 +0000

    star feeling off example ball

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import numpy as np
+print(np.array([1, 2, 3]))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
