db91a8c7f1ce32efca9bff3df083e506b6bec1a9
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:21:10 +0000

    me dream unfair lend care

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
