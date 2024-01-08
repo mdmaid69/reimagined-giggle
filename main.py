93464ef011ab0b9c44064bc37983adbad3f097b4
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:21:50 +0000

    occasion rule wonder limit journey

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
 import collections
-def create_user_list():
-        return collections.UserList()
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import os
+def change_working_directory(path):
+        os.chdir(path)
