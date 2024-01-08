f6f0efbedee282635d2d98853e0977e7d8829699
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:26:54 +0000

    him suckme afternoon doubt upwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
