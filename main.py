fcef5aa27e1d37452eba67d59c199501f925cf4a
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:40:41 +0000

    clean cough towards sit left

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
