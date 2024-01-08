d4fb8a8790aded6d0272836bd314586a1f38ed76
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:36:57 +0000

    near to across normal meeting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
