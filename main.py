5aefe311d974457b2aed2aa675adfa9e70742ae1
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:35:07 +0000

    wall phone indeed threaten memory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def divide_numbers(x, y):
+        return x / y
