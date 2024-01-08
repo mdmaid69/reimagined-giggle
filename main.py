60b392f6586675cf2cafa8c5edccac6c72c357ef
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:11:31 +0000

    unable spoil machine government leg

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
