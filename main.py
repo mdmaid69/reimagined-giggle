60f4f289e56abe132dc79e6c4e74fd7776c894ea
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:44:43 +0000

    suck mark height plan its

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
