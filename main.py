e97d34952816c5a47a57ad534a58a992688f0e86
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:41:26 +0000

    address use protest pretty exchange

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
