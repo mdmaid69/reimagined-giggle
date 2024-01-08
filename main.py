9b180ea9c32cd74850f4ea6960a46bc88013a20c
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:20:43 +0000

    thorough spend concentration business bringup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
