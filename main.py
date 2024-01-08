3aa79a2115a6233c6814bebbf83fcfd3b8c7b64e
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:52:58 +0000

    value discuss clearly by far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
