54ef3822777daa1add5cd5082396a5f4e3a48ab1
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:06:44 +0000

    uniform identify spite sudden house

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
