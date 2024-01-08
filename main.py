be1da56da99e32c8c875aa5215b9448013215301
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:05:00 +0000

    peace crowded force bad grand

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
 import array
-def convert_array_to_list(array):
+def get_list_from_array(array):
         return array.tolist()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
