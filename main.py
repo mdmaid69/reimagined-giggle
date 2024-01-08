6375e9edaec8664d43966de56233c6df49542655
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:08:54 +0000

    choice hardly base test space

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import collections
+def create_counter():
+        return collections.Counter()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
