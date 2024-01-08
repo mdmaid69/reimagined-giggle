cd28c7af57c53dc46d39ab6d3b645e6e596012ec
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:59:55 +0000

    connected sad principle secret Oo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
