430f07b34f2c61b6e32a8b048720e2cbecd6386c
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:34:16 +0000

    uniform 571enough terrible dress prize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
