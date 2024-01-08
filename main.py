45738bea8ace33de5bbdbc2bc819fc929808c4a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:10:19 +0000

    rubbish shall set through drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def add_numbers(a, b):
-        return a + b
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
