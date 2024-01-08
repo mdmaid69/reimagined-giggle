f360e392c94b90b77c068941463db4270560ea4a
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:19:16 +0000

    score scale everything recognize pay

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sys
+print(sys.version)
