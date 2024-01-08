cb445eec83a8f762e8b22185d67222e5e00a5239
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:36:33 +0000

    ordinary active always rank heavy

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
