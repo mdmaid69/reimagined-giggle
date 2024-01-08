21e18417c53aca45283e4dc2f7d559fb1f911fe1
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:12:11 +0000

    call breakup sea disturb mend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def sort_list(lst):
+        return sorted(lst)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_as_list(array):
+        return list(array)
