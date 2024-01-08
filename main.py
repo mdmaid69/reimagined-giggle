d20e1a7c113d1a12b80e16fc5795912c84a23ce9
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:32:21 +0000

    square eye shock back how

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_acceleration(speed, time):
+        return speed / time
