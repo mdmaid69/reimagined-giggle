d39ff2adf8139158e750663e97fbd7d03abfc1fb
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:33:37 +0000

    permission field no speak react

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
 import array
-def get_array_length(array):
-        return len(array)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def convert_array_to_bytes(array):
+        return array.tobytes()
