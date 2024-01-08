a671ad23e2f065a54ce1ca270d390167e6ba6bcb
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:34:23 +0000

    opinion quality hope sister share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
