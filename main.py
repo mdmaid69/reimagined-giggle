9e00371f47e56383c1a43c9662e1deccb70d292c
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:14:20 +0000

    ask 566end favourite corner mother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
