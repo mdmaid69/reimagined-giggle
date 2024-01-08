a9669ca39f85ea1049e280a1cfdb257df4c670c4
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:55:05 +0000

    workon bored offer excuse lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
