5d5819c6c4eb272002652bf60bdcc63d15d59c15
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:22:16 +0000

    measure careful example warning necessary

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def sort_list(lst):
+        return sorted(lst)
