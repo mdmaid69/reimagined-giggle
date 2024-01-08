26fa4fc8cc47ca1b76d44b1ce782c9042a832217
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:58:48 +0000

    content sail gradually sound natural

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
