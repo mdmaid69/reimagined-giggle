edb68c2963d42702a7e814481ba7bd31af38686d
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:19:25 +0000

    safety agreement difficulty either determined

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
