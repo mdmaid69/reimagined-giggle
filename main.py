546787f9090bada5d71e5ffb5192262e363ee801
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:26:17 +0000

    safety permission noisy reduction hold

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import time
-print(time.time())
-def find_max(numbers):
-        return max(numbers)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
