606e27e75913f31a5f97cd1c922f3f87b0e6ec0f
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:25:44 +0000

    niggaacademy capital field translate no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import collections
+def create_user_list():
+        return collections.UserList()
