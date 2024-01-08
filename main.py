d5a9d646aa233c09b27e8062d555a1af2751d3db
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:43:07 +0000

    necessary along advice tense lucky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
