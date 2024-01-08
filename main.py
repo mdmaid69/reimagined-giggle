ff369a3d970333523f98b9365208820e4c1130c7
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:54:38 +0000

    his job male broad taste

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_acceleration(speed, time):
+        return speed / time
