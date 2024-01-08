1a55ad625b506bc9952e50c10f4dd15d75ec40e2
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:18:47 +0000

    prepare support or march style

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import math
-print(math.pi)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
