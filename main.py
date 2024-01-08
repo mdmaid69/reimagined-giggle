e50ef4e4fbc5f41c9b35918acd5bc7ecb8abd95b
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:56:07 +0000

    promise mixture dangerous their catch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-print(time.time())
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
