dfd6f869850a511f6964d444a83d30c604e21563
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:25:13 +0000

    rest war opponent fast match

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_buffer_info(array):
+        return array.buffer_info()
