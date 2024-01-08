fc1c6190799a84b0ee4febd475843336d2f38374
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:33:30 +0000

    Yy month belong direct test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
