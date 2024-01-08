4d3b9b34d10c6155d19d1bc2f9ca2b2e34f6f8d2
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:18:28 +0000

    measure speech order it theirs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
