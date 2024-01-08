df2dcae1b90991e8e9e63aa017fffe1b44a91669
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:53:55 +0000

    bush system split strike precise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import time
-print(time.time())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
