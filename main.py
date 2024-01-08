2b032d883d69d5b934e4a442e0f275b8a2498ef2
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:39:46 +0000

    belief Rr or thirsty matter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
