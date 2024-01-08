3423ae27e96a6e371ebb76323441165e589dec45
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:58:40 +0000

    swimming born street breakinto tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-name = "Python"
-print("Hello,", name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
