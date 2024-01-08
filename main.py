71d666937809ef533613301385b693f8f049ab5e
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:22:23 +0000

    enthusiastic into offer moment burn

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print(sum(range(10)))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
