20446405ae4c61d109a9cf3240226694b85e44f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:54:45 +0000

    leave introduce rid computer clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def divide_numbers(x, y):
-        return x / y
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def split_path(path):
+        return os.path.split(path)
