c39a1d0bededdde3d886b4114e3c89ca78997fdd
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:32:13 +0000

    between means second him 565end

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
 def add_numbers(a, b):
         return a + b
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
