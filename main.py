bf5b3cd7cb34f23940d106e03ac69d4e588dcce4
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:26:00 +0000

    yourself think statement definite however

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
