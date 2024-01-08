6f724c388cdc379f9b676a15486884e61a32922f
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:59:39 +0000

    laugh hard section rent steep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import array
+def clear_array(array):
+        array *= 0
+import array
+def set_array_item(array, i, item):
+        array[i] = item
