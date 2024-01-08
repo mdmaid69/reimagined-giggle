ed517409f6afad0d03266d757d48ce159a5481de
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:16:28 +0000

    within complete environment strike save

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import datetime
-print(datetime.datetime.now())
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def find_max(lst):
+        return max(lst)
