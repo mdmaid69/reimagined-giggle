409173f4bdec75ebc07312fc4faaf263cceedea8
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:14:48 +0000

    fear recognize sting preparation perfect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
