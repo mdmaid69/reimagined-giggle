f705cc8d4ff7a0736564a5dba240d7fdc7581e20
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:33:37 +0000

    disappointed taste near settle off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import numpy as np
+print(np.array([1, 2, 3]))
