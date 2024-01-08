825c577c1a8b5ca22ca6f232e1c1cfb9782a0fd3
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:40:12 +0000

    Yy thinkover lead black equipment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def reverse_list(lst):
-        return lst[::-1]
+def add_numbers(x, y):
+        return x + y
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
