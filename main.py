654c7fb33e21374362b77ad4a96edda6a827be4b
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:11:02 +0000

    fold concerned go back anyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import numpy as np
+print(np.array([1, 2, 3]))
