c9b2fc61b85329e17f09b8469b52d091c2e23953
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:32:09 +0000

    influence example on wind clothes

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
