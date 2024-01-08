ecf624a8151b307ddf3e5e9f2789851d0eed3c83
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:42:08 +0000

    surface study church train giveaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
