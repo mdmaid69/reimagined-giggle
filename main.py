06e0aa2096e42f06d61d369a8cca089032175764
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:02:05 +0000

    type centre workon promise possibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
