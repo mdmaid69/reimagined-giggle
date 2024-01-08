a61f309c3236708fbfa536e6ad7a02ae20bd39a0
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:26:28 +0000

    likely exploration lookafter stay reach

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-print([x**2 for x in range(10)])
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
