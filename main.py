10bc9448d6f3f40d2e74390413abcbd605cdbb97
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:15:39 +0000

    shake heat bend impressive tough

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
