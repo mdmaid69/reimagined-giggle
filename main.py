71f909265dd60c34c39a0114716a0660c9b902a4
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:03:12 +0000

    flat give crash plain obvious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_stack():
+        return collections.deque()
