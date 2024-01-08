bdd3773116523f1b2bf5761fa28ad3866fce3c44
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:26:20 +0000

    would contrast along lunch destroy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def count_elements(lst):
+        return len(lst)
