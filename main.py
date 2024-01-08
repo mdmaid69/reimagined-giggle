9e4384a806743be4d272b514990fbe071ed4077a
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:10:33 +0000

    getaway much child basis trust

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
