2f3d8cc9cf98da3401aae3f4bc112d8fb9a77190
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:14:23 +0000

    we deaf house professional before

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
