cbd1d4c6c510e1ab5fe50df518ad55dad29c1f3b
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:06:05 +0000

    breath disagree drawing moon must

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+print("Cube numbers:", [x**3 for x in range(n)])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
