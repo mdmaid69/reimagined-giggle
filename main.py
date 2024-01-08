55b20ccb2b1c94a3f325a4c1575a9a4096d8f16d
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:46:24 +0000

    own appearance same else base

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
