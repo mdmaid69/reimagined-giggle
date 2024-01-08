172d05c9b33fc85d8e9070ca42d3b55e99ea5195
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:41:11 +0000

    frame test service benefit aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_list(array):
+        return list(array)
