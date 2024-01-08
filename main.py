8d36a4f99adfb01fb5c9cc126b8b8057cda2b402
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:00:53 +0000

    load dangerous talkoutof geton length

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
