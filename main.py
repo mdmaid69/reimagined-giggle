77f32ba285a8936d5464bb42d69da2afdbae5fc8
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:14:10 +0000

    back original miss congratulate person

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
