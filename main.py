b3236ca4b21c3a6d5f03de321bf4addd4850fa0c
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:36:07 +0000

    treat work Hh group well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
