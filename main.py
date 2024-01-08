dcdac77f0ca1f32bad2e83d0c40b045fdeb4246c
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:33:28 +0000

    original rare turnon able factory

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 import array
 def get_array_from_bytes(bytes, typecode):
         a = array.array(typecode)
         a.frombytes(bytes)
         return a
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
