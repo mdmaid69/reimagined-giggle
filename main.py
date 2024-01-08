fb831fddcf4fafd45c5d4f27db82d398736d5870
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:43:06 +0000

    trap possibly safe lie check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
