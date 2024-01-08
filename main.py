6958b4f10d7855e4f45627f28f9e9b339f46bb5a
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:38:57 +0000

    holdout afford loan confident innocent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sys
+print(sys.version)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
