3b030cfd5970b27841b9ae407cb40377996da80a
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:35:17 +0000

    bang will important make target

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_list(array):
+        return list(array)
