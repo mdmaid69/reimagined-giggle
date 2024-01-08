441f2434e81bd7c7493f127930c289378fff8db2
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:21:20 +0000

    must we near believe service

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
