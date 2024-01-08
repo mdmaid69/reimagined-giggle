5f94d19e9f693853da5460819c6c296f332cffa3
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:53:22 +0000

    home since basic lookafter it

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
