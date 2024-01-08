8fea9da61c2fb5189b48699d59c2a1a9bccf48da
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:24:45 +0000

    something sale odd speak behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
