4ac9a5980da6fb10b3ba3b692c9da5f5c8a7cca4
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:15:08 +0000

    cheat deserve fun limit rule

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
