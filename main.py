4108187b1cdd19d7b47ff114db63859a0ad65b16
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:59:35 +0000

    flow bury accidentally environment wrap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
