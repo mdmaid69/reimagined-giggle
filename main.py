60282ea75f1321abcf4d0e44a8749266e38efe95
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:28:47 +0000

    572enter turnup pause wipe complaint

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-print(sum(range(10)))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
