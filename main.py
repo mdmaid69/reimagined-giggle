0d35fa4f395fd74f318adfc96500234e58a821d8
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:06:55 +0000

    lookback(on) fast plant improvement accidental

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
