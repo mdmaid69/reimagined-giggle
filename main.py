4f7ab31fce52575bdcb35569124723f91e1b45cf
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:02:22 +0000

    switch test store interesting sale

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
