6c78d81cd55463c630c2dba850e9482a3d4456c0
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:46:00 +0000

    impossible study change escape pick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
