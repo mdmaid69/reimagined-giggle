258e4f2efe5066f0237030a1b214e0d404080606
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:32:06 +0000

    ring grow realize communication future

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-print(os.getcwd())
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
