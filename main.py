02e966956e2e99eb86f8d3bca8fd08164389a14d
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:44:57 +0000

    trap with maintain throw feel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
