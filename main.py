f5323e78b62bb956b239a6fd89a4895fc65c8a13
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:19:56 +0000

    battery some custom narrow rest

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def find_min(lst):
+        return min(lst)
