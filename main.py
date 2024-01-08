8578668755411e2bd279d2ba0c558cc4a27fac03
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:37:16 +0000

    stare tradition wrong lot string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
+  def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
