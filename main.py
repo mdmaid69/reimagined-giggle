27451af6e01d8369b1c67e0535c05b6ca8e40f38
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:50:40 +0000

    runaway branch also well sweet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def sort_list(lst):
-        return sorted(lst)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
