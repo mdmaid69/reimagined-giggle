4395f8d5ad8c9680b932a077d7ddaffefc7024b3
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:14:03 +0000

    ought tool travel happen show

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def remove_duplicates(lst):
+        return list(set(lst))
