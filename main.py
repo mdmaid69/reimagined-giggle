258d0986684236cbb1b06fd52c8c5ccc43919b74
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:13:10 +0000

    frightened already precise prevent for

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_as_list(array):
+        return list(array)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
