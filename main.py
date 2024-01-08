6b8dbea8be4a98ced86229bd015e186607105d73
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:53:34 +0000

    soil very wherever innocent polite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
