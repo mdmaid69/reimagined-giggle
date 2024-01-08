79a3d9f18366e51d252e6680624533c2e00bdd60
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:15:04 +0000

    busy sort Hh past formal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def is_even(n):
+        return n % 2 == 0
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
