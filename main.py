74053c3469cbbe20c02f575660eff17bbcbebd27
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:07:33 +0000

    welcome mean old late like

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import random
+def roll_die():
+        return random.randint(1, 6)
