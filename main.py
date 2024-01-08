b603540a8402cec28edd5042960f6b1fdb2f2fe9
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:25:03 +0000

    problem trap talkback steep fail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
