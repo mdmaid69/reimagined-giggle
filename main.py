c3acf20e2460603b43d9c294cbacb7213c6e6e04
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:46:48 +0000

    holiday frame sister suit bus

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
