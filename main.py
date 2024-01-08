b6a1486015472502af30d0b9678834536335543b
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:12:13 +0000

    effort conversation 179bite turnover runinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
