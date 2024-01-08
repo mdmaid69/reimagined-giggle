abc01bd3c7617176b518eac24c71e6bf39414052
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:36:54 +0000

    safety scene alive negative warm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+def print_python_version():
+        print(sys.version)
