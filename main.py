0ada343a1d7dcf3f313fa0e9f293412f0125cb14
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:38:42 +0000

    thinkover small rent pen married

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
