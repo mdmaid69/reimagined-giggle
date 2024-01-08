58f73110a3f5efd47fa264498ff1e882c1f093fc
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:21:51 +0000

    interesting appear feeling process term

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  def sort_list(lst):
+        return sorted(lst)
