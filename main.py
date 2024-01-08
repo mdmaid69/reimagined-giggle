048b57be204ef874c4f832f5f434a268cc1f2e6c
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:36:40 +0000

    male for song practise description

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+name = "Python"
+print("Hello,", name)
+def is_palindrome(s):
+        return s == s[::-1]
