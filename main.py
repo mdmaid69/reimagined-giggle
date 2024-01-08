185a7c446500ad1aeb9b1c6a2924ff47ffb54d7c
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:33:58 +0000

    tend police whisper polite come

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
