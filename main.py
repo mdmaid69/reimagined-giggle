0c4121f5fce7e9c5ceacebfb4db949681d14bb93
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:51:15 +0000

    process relation month normal gentle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
