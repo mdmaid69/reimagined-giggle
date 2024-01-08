9fc86a909b9ce4dd5298c47c767c8074a8ff2e18
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:43:22 +0000

    thick hard burn Ee number

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  def count_elements(lst):
-        return len(lst)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def reverse_list(lst):
+        return lst[::-1]
