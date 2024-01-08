a6ea3faf850ed2dc98a206f3034117e2b9c94b3e
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:23:21 +0000

    fault listenfor interest close pity

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def remove_directory(path):
+        os.rmdir(path)
