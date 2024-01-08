af3dd781d3a8ac7e506f7dd1457d1b1f2ee3e5b9
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:00:32 +0000

    shout breathe violence report ability

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
