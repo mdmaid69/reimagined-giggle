e674708f826f507265198ef6e701419fca4b9da8
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:18:44 +0000

    spring lookup light please 566end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
