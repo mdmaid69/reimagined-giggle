03f39a55427bf89974026935def1f67d785929a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:07:36 +0000

    foot female battery cheap delight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+print(math.pi)
