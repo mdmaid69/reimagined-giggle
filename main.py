015a83d73069c52d483441700127d62671fd906c
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:31:21 +0000

    doubt project giveback mystery square

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import time
+def get_current_time():
+        return time.time()
