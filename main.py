192c66dfacd604d63e08a327eacd4060244c1990
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:23:29 +0000

    pen money minimum achieve too

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def cube_number(x):
-        return x**3
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
