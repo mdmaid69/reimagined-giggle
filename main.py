5787da5e9fe53c2cfbba083f63a99731c83cb6ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:48:20 +0000

    consist nature considering formal turnover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
