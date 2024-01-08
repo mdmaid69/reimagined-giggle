67795afd43e4452920fd298faaa609d54979af00
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:00:24 +0000

    yesterday contents badly rely balance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def remove_from_array(array, item):
+        array.remove(item)
