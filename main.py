cb682fb5b143dec2bc2666ea11300f4bbf022e35
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:59:47 +0000

    say translate human squash towards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+for i in range(5):
+        print(i)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
