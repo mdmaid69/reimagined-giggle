be3703e204bfd9fdc0bfc085d5230f903c197eb0
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:41:53 +0000

    common relative future get importance

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_priority_queue():
+        return collections.deque()
