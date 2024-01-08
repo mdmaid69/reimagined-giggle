d4141d599851f77b9935e1bd77db00086d1c0396
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:28:10 +0000

    correct one pleased plan ordinary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
