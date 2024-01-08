028137eeecfc733f29b37e36d20a818c3dac01fe
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:17:59 +0000

    fine broad call trousers corner

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-print([x**2 for x in range(10)])
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_density(mass, volume):
+        return mass / volume
