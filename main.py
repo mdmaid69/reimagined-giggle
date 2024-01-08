b4b584726984eb0e878d9bf40448f5e2e5e74758
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:26:17 +0000

    blue delicate tour promise thinkof

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
