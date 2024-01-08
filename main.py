9c0de9acce5a152eb43821943c7fb46e500764ec
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:18:18 +0000

    behaviour Ss up form word

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
