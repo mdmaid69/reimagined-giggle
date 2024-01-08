306259ddba41b9a79e68c999058357e1476ac9fe
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:50:26 +0000

    model wet guess spread mark

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  def cube_number(x):
-        return x**3
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
