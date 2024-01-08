105624eeaae260df9ff6a7db0bcdc7498b4bf872
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:48:28 +0000

    apply tragedy closed flow exit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
