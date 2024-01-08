ee6e36bd64eb9ec9589fd77f3aa800ab26d44f8a
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:49:43 +0000

    572enter minor hardly support side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
