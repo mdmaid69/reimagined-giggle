03c52342239c88b1fa4f34ccdbfd77e004259254
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:34:39 +0000

    Ii either comeoff late scene

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def cube_number(x):
+        return x**3
