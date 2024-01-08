667f61cf9c0ab7ce8a4aad59cc609ca2df05f7d4
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:27:42 +0000

    air duty burn scream Bb

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def divide_numbers(x, y):
+        return x / y
+import os
+print(os.getcwd())
