9dd9d3ff41be7d6a76b432195ec365370dc4fdc0
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:37:51 +0000

    after large except produce pile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
