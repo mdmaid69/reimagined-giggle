89ab51eb6a5e75b456c2fcb6e8fea4799c05802d
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:17:20 +0000

    label beat on spite form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
