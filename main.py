5955b1abea15d23f409149936d23de21ee65e431
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:49:03 +0000

    accordingto dress while polite holdout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
