ba500b020288df3bfa047b0c82491133213b241d
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:48:38 +0000

    tie without Cc matter large

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
