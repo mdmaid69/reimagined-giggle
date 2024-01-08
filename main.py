12bb12692c11f23fe556cea08af002b60d433027
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:50:16 +0000

    talkback hate sign boring lookup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
