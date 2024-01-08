5d979caecf4add5244b4642e3486bac1c1b71ff3
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:48:50 +0000

    tonight worse above dig side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def find_unique_words(sentence):
-        return set(sentence.split())
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
