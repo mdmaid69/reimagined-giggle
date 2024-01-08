285b722ee7e99f2f09ec220fbdb40051b2835e89
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:16:30 +0000

    mainly brief rain depend provide

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_pressure(force, area):
+        return force / area
