cd12a1e03e8c952c73f2fd56c9e4b2825a9fb0d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:38:19 +0000

    variety scene like cheap serve

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
