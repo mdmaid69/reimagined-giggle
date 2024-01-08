90a8033478a2ead33a25c74c7f8dee50b6b80d7c
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:05:18 +0000

    distant ours twist hope yet

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+print(os.getcwd())
+import tensorflow as tf
+print(tf.__version__)
