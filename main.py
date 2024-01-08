9a48705d428418973fbe832aff975077714debae
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:29:45 +0000

    nonsense or downstairs lift promise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
