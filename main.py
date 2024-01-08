963897b864092c23a05fbd462e97e050758ef868
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:43:34 +0000

    dress common talk lie emergency

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def convert_to_binary(n):
+        return bin(n)
