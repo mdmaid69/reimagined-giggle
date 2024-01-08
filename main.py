4b41cc65b9835850759633cace73cc32076aa97c
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:45:47 +0000

    unhappy range evening mark lonely

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def add_numbers(a, b):
+        return a + b
