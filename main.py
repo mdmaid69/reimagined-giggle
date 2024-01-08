1bc6c5190c87156c25002b1b9cabd7a97f697fb8
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:15:48 +0000

    sit drink near decide gather

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
