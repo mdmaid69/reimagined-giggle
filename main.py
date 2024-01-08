c2d6b1c205b15cbcd91df0a452a689284199e975
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:55:04 +0000

    rarely ought declare workon because

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
