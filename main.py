9ad98e0816d972dc4ea8663f763fafac24bd941b
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:14:00 +0000

    pretty together ring hit offence

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
