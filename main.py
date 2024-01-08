5ab33fd715581f48a7fe591d3c4badfddf7e9c4d
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:28:01 +0000

    shame both test but label

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
