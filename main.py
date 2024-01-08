b1caeeb09814c814f7e8f6ae13c109425c780299
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:46:43 +0000

    authority not join or kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
