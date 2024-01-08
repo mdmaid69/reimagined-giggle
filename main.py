b44feb9f13d1ca537eef7206107884691f893e8e
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:04:08 +0000

    crazy fortunate offend shop quickly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
