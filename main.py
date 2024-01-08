1740e50ce7e44f8865199ad30bfb19bb02a92c3e
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:58:06 +0000

    workout raise during tall sick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  def reverse_list(lst):
-        return lst[::-1]
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
