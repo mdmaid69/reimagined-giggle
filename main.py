e62f6e3b82ca0a29c5246086cf1d2a57e766cd83
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:08:02 +0000

    opposite only chat complicated maybe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_area_circle(r):
+        return 3.14 * r**2
