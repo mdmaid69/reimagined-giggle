865a51dc692e0a2fad19b34507150c95314479b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:18:17 +0000

    smell on low underneath whisper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
