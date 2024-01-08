3cad0c1a15403b27724435a3960b3d21a2666dad
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:17:18 +0000

    bury main rent present leave

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
