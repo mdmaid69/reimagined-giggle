ecec644aabc14a0f25546ac638f83b21f23c901b
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:35:32 +0000

    there summer simple gooff compare

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import math
+print(math.pi)
