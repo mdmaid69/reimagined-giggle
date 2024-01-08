5384d46f2a2e85adc43b151dd8bdfd78c2852eac
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:12:51 +0000

    case sticky mood pay drive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-for i in range(10): print(i)
+import collections
+def create_counter():
+        return collections.Counter()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
