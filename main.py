5a7dd7895e781b49efbf8867533f254594fc387d
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:27:35 +0000

    famous only common still fact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
