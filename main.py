818d5a1cc482c6fbf2968c2f312912c68f85db02
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:49:15 +0000

    concerning load world sensible change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
