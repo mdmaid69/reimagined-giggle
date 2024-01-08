3f60b6a013b63d7bdb50ff840834a16dde70f06d
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:14:49 +0000

    wave gap nonsense lost boring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_current_directory():
-        return os.getcwd()
-def is_even(n):
-        return n % 2 == 0
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
