9efa68daf3c164706fd832cd8e2bc8563867c258
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:41:54 +0000

    unhappy appear final would noone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_string_from_array(array):
+        return array.tobytes()
