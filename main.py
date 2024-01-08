e4cb590923b13bb2be037166767347de244acf20
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:25:03 +0000

    number stop Cc wise scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
