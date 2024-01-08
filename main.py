a0a6121fe747542cd4c176c3f89861abb4e2e193
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:56:10 +0000

    tap back time scare about

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
