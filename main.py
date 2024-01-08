53ad7367cc9271254f908c53c940b4f0020c5b89
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:48:43 +0000

    that tongue key alive treatment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_power(work, time):
+        return work / time
+import time
+print(time.time())
