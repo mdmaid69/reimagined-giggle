0b32590497cce59d60870f2bd22436ab6354bcd3
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:10:16 +0000

    contrast suggestion breath tight eager

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
