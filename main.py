478e3207fa777c6cf5e8cba7c45e0f358d8aa18f
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:33:45 +0000

    thinkthrough floor mother already course

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
