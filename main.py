c5096cb35819f82ae714f4b979b5a4cc556608ba
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:45:08 +0000

    thinkof single OKorokay concentrate treat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
