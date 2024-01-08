417af5eb9921a10b6c80b52648bb68349b514139
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:22:51 +0000

    switch getout pour write lookaround

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
