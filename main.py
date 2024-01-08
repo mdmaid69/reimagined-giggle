5f313a8c8832718f58971650ea267e8cfe69927b
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:42:08 +0000

    how fast size heavy serve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import sys
-print(sys.version)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
