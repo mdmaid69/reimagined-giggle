0fbeee635e26c319757fdbeb7d8fc086eb7aff78
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:48:22 +0000

    scheme favourite such spoil land

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def convert_to_hex(n):
-        return hex(n)
+import os
+print(os.getcwd())
+import math
+def calculate_exponential(x):
+        return math.exp(x)
