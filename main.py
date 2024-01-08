ee6c975ac8a9180e207353d5b91050c6e7a90607
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:47:27 +0000

    along doubt low mysterious broken

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def add_numbers(x, y):
+        return x + y
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
