9b2294b399a1b893cc882d3c54d36a3fbc526b80
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:39:23 +0000

    gather high window off sense

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
