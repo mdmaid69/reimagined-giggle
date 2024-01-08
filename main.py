dcbca4b6bda5a5b2021597e19f340c4a78745697
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:43:22 +0000

    platform education delight estimate turnback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def count_elements(lst):
+        return len(lst)
