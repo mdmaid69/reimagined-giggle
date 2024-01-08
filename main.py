04af7738356fc5583b45d16636190bfab61e56e5
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:27:50 +0000

    complete than just breakout identify

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Characters:", len(text))
