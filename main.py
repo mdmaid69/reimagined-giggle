21ee5d6efb3e72cf8ad39907c37d551d3960e8b9
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:02:40 +0000

    rush seriously squash hold design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
