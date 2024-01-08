6fc43ce40eabcbd9cdbe7fd84e34d49366e9d510
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:42:27 +0000

    road dick build tread confident

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def reverse_list(lst):
+        return lst[::-1]
