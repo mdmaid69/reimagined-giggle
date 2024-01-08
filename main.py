86f9265179c9b25da820cb0c20cd5d99ec8064ad
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:20:20 +0000

    chase pleasant scrap unit rather

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import tensorflow as tf
+print(tf.__version__)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
