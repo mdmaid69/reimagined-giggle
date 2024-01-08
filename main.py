8a5d515ec29dbcc887aea530d244d847ce4e4e29
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:52:55 +0000

    steep laugh much getoff grade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
