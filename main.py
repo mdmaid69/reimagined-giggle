56a06f29c259b43924c4059ed3f7208fef739eaf
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:54:59 +0000

    tense disturbing shade month afford

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+print("Hello, world!")
+def calculate_density(mass, volume):
+        return mass / volume
