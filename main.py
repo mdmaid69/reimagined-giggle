6511d16a35684465e4be93d5fd391ef172c73a98
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:50:27 +0000

    then dish authority convince sea

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
