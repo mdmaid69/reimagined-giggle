09042d897648348faa116c01e81fdd9e1bc004ee
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:24:01 +0000

    path silent to type breathe

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def reverse_list(lst):
-        return lst[::-1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
