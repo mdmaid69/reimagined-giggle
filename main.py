8513cb93c249f6ea3d39e84fe9ca2f33fdf4df24
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:57:18 +0000

    want still no distant well

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def clear_array(array):
+        array *= 0
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
