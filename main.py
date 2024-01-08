f72d6723b67bf42dbd462050291b4003ce0e8e66
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:41:27 +0000

    in out miss brave office

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def find_max(numbers):
+        return max(numbers)
