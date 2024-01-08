d3e752198e87100d828f6e2183140eee25eac7f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:38:14 +0000

    used limit characteristic increase compare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
