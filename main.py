2594d62ca594c864c54a4cb45b7cdbd8b2e16660
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:01:41 +0000

    beneath later star separate connection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def reverse_string(s):
-        return s[::-1]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
