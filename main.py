e82b997df6b7269d739d80b9f4fc2f2ad6261cac
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:46:34 +0000

    bear apart basically few joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def get_array_slice(array, i, j):
+        return array[i:j]
