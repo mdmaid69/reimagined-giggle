cc50b76da04fe8d01240658f73c5697cbd56b239
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:32:18 +0000

    Vv obvious surface suit several

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_exponential(x):
+        return math.exp(x)
