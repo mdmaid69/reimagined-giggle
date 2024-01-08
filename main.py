b8f954dcb4cb863afe485d2437ad7b32d4247dfc
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:17:28 +0000

    work no see drive affect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
