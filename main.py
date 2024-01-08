27abc66cf8859142b600bc08ff0848ce1a86fff6
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:34:36 +0000

    telephone avoid comeover generally suddenly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_float(array):
+        return float(array[0])
