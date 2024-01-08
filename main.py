e08d30a8f239773458e559def7576a94c33f51d0
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:11:43 +0000

    566end load bet black tip

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_volume(length, width, height):
-        return length * width * height
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
