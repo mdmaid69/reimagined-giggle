c419dd2758c7b68e38ddf0e829583fd82176d595
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:38:04 +0000

    laugh bit describe come pride

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def split_path(path):
+        return os.path.split(path)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
