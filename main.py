9345d09e499103d2855003cf36bf0955deeb4505
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:17:46 +0000

    finger drop 572enter freeze along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
