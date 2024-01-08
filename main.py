6af397d171cb6292b45b47a3fb9c9b2e1099d5fa
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:34:07 +0000

    breakthrough downstairs necessary nothing standard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
