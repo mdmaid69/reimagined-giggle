765d53cf6a374a2299a3542e712261883dee5e30
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:56:23 +0000

    round class circle plain pile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def calculate_volume(length, width, height):
-        return length * width * height
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
