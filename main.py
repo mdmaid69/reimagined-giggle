c9164b71fc780c54fe93f3d1d9224f11e1d6f553
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:30:14 +0000

    dry score hope Ss pattern

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
