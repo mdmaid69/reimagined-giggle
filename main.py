aa6a33c2a252c31ec85ab657cc9aa409f831c003
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:50:44 +0000

    shout comeup taste object lesson

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import platform
+def get_python_version():
+        return platform.python_version()
