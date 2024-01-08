6fcb8bf3d9becb503d5df1c0a6d4ed1624f7426d
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:14:08 +0000

    high uncertain keepout picture up

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  def convert_to_hex(n):
-        return hex(n)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
