1015517bd6067ed99814b5e0e701f8d06c59a22a
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:23:37 +0000

    accident medium at it settle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
