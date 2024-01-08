e65b7b15d3ea4b5cb8a13cb6a64df4bdc28f6a25
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:00:31 +0000

    but backwards treat protest pleased

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_current_working_directory():
+        return os.getcwd()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
