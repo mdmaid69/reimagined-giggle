c20c59a36b0453bce861994020b1c08cdec89c12
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:32:29 +0000

    actually powerful runout have wrong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def find_max(lst):
+        return max(lst)
