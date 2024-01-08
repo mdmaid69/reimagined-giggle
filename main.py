63b8f3da73dafc6581dbe3396b3bc5ad6d4cb02f
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:44:40 +0000

    pollution increase me contents immediate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
