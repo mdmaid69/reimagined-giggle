8bdf80c9a761413ef00f92d050e7d2cf9e26eafd
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:12:27 +0000

    story protection ability exit television

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
 import pandas as pd
 print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import array
-def get_array_index(array, item):
-        return array.index(item)
