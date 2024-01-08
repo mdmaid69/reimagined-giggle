f47488b0a5001aa518a13fcb4a9a591f4b083ddc
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:24:17 +0000

    send free store open uniform

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
 import array
-def get_array_item(array, i):
-        return array[i]
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
