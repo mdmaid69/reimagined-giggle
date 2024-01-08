fd069cee64b9a987dbba3566eb7842e0ec9f7edc
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:06:07 +0000

    mix invitation dangerous contribute comein

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def set_array_item(array, i, item):
+        array[i] = item
