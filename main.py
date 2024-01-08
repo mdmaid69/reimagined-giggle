c7a102b9903f5d754ac3fe889be1f3f522de1678
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:27:47 +0000

    roof different attitude knock shut

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
