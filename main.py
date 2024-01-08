487a3ef2fd733b5d0ae8b033404a4ed637a4a895
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:09:11 +0000

    upstairs quantity decision pride practice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
