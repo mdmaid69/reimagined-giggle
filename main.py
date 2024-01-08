34e697cd67f73e350659889640e860f613264d15
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:49:57 +0000

    pleasure under among flood production

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
