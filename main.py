98bba111b5d4ee841d6dfc7dba325502da56ec84
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:05:08 +0000

    stick break bus promise too

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
