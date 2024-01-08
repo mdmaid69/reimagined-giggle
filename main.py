f8fee3e20ed26f6969bee1d4628e33d3195bea4f
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:43:05 +0000

    add swim claim will moon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  def reverse_list(lst):
+        return lst[::-1]
