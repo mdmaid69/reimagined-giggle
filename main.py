2bba69c05a97c78d63e831cdc847fad8b1e02dcb
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:39:12 +0000

    pupil quality bank friend best

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
