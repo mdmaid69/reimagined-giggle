faf17e149acaaee6f78924ec658a24fac3e931b7
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:18:07 +0000

    getoff clearly declare fast meet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
