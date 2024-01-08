8809d2db3431eec29cb9938936fa28b83f028539
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:32:00 +0000

    hurry failure them be contrast

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def reverse_array(array):
-        array.reverse()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def convert_to_octal(n):
+        return oct(n)
