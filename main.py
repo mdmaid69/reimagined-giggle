7b431795b33df38ead72aa826e8b40e544b343c3
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:25:23 +0000

    copy shock suddenly study unhappy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
