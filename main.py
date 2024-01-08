d150ece1f0b5baab3bcb75a361de4ab456214b86
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:01:46 +0000

    amount seat week exciting takeout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import tensorflow as tf
+print(tf.__version__)
+def calculate_density(mass, volume):
+        return mass / volume
