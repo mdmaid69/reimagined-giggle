eabd09c02948b84f8cfdd5e6ba6ede9e0e83371b
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:07:19 +0000

    ticket justice difficulty human conduct

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
