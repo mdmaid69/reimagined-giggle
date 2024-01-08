f1397a7133b2e03f06feaa5518e104d30fbf3994
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:46:23 +0000

    present accordingto home heavy decide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
