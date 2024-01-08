13e67f754a82d26cbd1924f355c4334a950820d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:24:33 +0000

    trade tax cross sky attempt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
