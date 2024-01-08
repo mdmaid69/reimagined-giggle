d723b1f02883a25c6cc95251cb6ea3769732ff6c
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:02:50 +0000

    invitation crisis interesting event knock

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
