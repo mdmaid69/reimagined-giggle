7a783abf12d07e25131a3d0450e5e73113a2b278
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:32:49 +0000

    centimetre terrible key professional concerning

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
