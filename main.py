74bcc2134d40729bad18510df3f2d68c32caa238
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:42:18 +0000

    manage risk offend area gift

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import sys
+  def get_python_version():
+        return sys.version
