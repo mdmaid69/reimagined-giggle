5fe6e4947037f562da16037f997ba495ae4a8ece
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:02:09 +0000

    today law name 569enjoy nowhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+def remove_directory(path):
+        os.rmdir(path)
