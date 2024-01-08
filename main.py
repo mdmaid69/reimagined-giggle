d17d52bf0b1d8b9c5dfce467d5a04ad5422e1de5
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:39:11 +0000

    piece luck bath perfect paper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def append_to_array(array, item):
-        array.append(item)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
