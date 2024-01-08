86c47641a26dd0e525efeaa16c4048356f553f43
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:24:18 +0000

    there drive passenger judge dead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
