df4118e34d21c1f79fc5a5fb387a0586f92f814a
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:39:40 +0000

    number doubt handle onto risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 def get_file_size(filename):
         return os.path.getsize(filename)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
