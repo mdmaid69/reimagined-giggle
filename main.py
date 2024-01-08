e0a9f36215812ea3ef8372260a1a493bb06ae9b5
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:51:59 +0000

    idea although picture plan date

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_power(work, time):
-        return work / time
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
