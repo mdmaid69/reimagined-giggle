f6680d8d2edbe4d777fdecd5c10cf390f7dc3234
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:44:50 +0000

    sign smart dry fast secret

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
