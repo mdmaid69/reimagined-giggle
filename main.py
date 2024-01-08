3901b2f7a965d0555a3a8bd5351694927806f575
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:56:24 +0000

    patient defeat contribute her form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
