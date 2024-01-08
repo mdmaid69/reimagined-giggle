5588f1e0f0866e3e1e924fa46f82d06ef45c3dc5
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:16:51 +0000

    mistake involve affect Gg mainly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def print_python_version():
+        print(sys.version)
