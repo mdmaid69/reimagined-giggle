f13d2f1857e2861e70e33f4a03eb897d6677e828
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:03:50 +0000

    increase comfort shock glass behaviour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
