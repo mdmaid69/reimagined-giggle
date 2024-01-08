874e58f99c2ef26cc5d3c17518cca41109cd15e2
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:58:04 +0000

    result sorry clothes like ocean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import sys
+def print_python_version():
+        return sys.version
