54669479ac0f50e8e7ba4af0407986ecd8334ed3
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:59:58 +0000

    always his suggestion discussion deserve

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_int(array):
+        return int(array[0])
