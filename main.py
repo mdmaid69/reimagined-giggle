c13f8ddce3071f00e59c900a7e404878137b5854
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:26:37 +0000

    damp slice contact room attitude

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import json
+print(json.dumps({"name": "John", "age": 30}))
