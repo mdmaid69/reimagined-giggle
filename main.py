172ef16e8eb75452669961bcc0d946edb9510a4d
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:43:28 +0000

    wise cost skill grow male

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def get_list_from_array(array):
+        return array.tolist()
