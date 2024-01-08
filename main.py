c92d122559d05a7683d64f0ae0b52fbf94a58261
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:25:21 +0000

    declaration scream imagine trouble succeed

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def get_array_length(array):
-        return len(array)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+print([x**2 for x in range(10)])
