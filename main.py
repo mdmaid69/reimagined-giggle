ed354c0dc4b465833aff20f033aa29380d816e09
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:31:24 +0000

    than mix hang visit wake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
