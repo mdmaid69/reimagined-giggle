3c5505cf4e10c1b481e7b0026ec3255618ba8c71
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:32:22 +0000

    which test way claim payment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
 import array
-def get_array_index(array, item):
-        return array.index(item)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def get_array_as_list(array):
+        return list(array)
