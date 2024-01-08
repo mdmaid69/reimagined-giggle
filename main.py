d0709af4fb7fc5ad7c11e42c05deb5c324730406
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:16:18 +0000

    moral floor exchange deep answer

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
