24df2d01c8dcb120c4dae2d0a5b68ed8853fe494
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:18:30 +0000

    cool people control glad plant

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import platform
-def get_os_info():
-        return platform.uname()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def reverse_list(lst):
+        return lst[::-1]
