51dab408fef849b39426d13ccada08db274e8bb9
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:20:07 +0000

    condition come content certainly whistle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
