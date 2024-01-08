b012f812f941f51146e743950bfa194827224203
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:01:33 +0000

    suggest add above do class

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
