42d1359d953b00b4005e3251d65b01dae43d83cb
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:00:14 +0000

    change flavour lookup accidentally entrance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def count_elements(lst):
+        return len(lst)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
