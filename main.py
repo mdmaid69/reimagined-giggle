6e13f4d05fa5dd6dc8ef2f88168ca11e7e4cfb16
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:38:55 +0000

    marry stiff future escape ill

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
