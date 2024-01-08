c77a9eb8d4509ad6e3ce197bb8a1d09d854a7bd3
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:28:34 +0000

    wave interview safety half under

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
