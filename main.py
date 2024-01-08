35e10126b4226729582814480fbd659c455d4e49
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:29:57 +0000

    computer so busy excited snow

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
