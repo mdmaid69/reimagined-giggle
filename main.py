e93699ba3cc89f14c91230d3207664dd208615f3
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:27:33 +0000

    come able anything beyond even

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
