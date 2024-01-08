a5afc5cabdcf0a4ab908093e03cf98f7f456ee92
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:46:19 +0000

    pocket interfere crisis mistake shrink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def find_min(lst):
+        return min(lst)
