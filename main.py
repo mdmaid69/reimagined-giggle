50a185c65e708e52cd5af85dda9cfaf115f39331
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:51:05 +0000

    opposite change release branch record

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def subtract_numbers(x, y):
+        return x - y
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
