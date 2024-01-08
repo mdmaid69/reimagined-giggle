145c3d642db1e15e3bb94bd3ba80aac5b50f1598
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:40:10 +0000

    love cheque poor Gg false

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
