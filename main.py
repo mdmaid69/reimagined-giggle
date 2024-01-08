67add21ed7b675a7f6055eb46435555eebf015bb
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:11:41 +0000

    either distant strange fill zero

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def sort_list(lst):
-        return sorted(lst)
+import math
+print(math.pi)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
