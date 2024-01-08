ecad122161eda099a65ee38fbfd6cfb2e23e94dd
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:18:13 +0000

    afraid world uponon high shade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
