d04edea6d4ecd84c0756c1422d03a6c64ad65cab
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:07:59 +0000

    to smile passage Bb pause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
