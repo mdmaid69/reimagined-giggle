dcbd29319f03a69de473f458c6dab55cb65cb512
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:11:16 +0000

    feed sleep die relief sort

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def convert_to_binary(n):
+        return bin(n)
