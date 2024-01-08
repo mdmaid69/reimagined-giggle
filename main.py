20dbb43ad6e930455f290d985e04f6f5b9ee27a9
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:55:11 +0000

    broken during argue custom programme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
