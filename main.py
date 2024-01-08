72f32d75835c8544945a87710f13be4bfdb48836
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:51:30 +0000

    load please centre sensible Uu

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
