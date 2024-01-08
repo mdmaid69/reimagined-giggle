a6a6797f258ada848a4e383d2384c7242703ddd8
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:47:14 +0000

    less day payment takeaway emergency

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  def convert_to_hex(n):
-        return hex(n)
+def get_string_from_array(array):
+        return array.tobytes()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
