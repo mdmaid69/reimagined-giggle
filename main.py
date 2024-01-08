c643c8f0e7421d7de8d35d4001932fa2cd6fcdd5
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:29:49 +0000

    always broken lead nor measure

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
