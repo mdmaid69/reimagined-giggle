1c122c347396a09197b4013d9096f4024f3d594c
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:26:21 +0000

    write remark nonsense turnoff foot

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
