edcefb2d23d787ea9ea9e2377b1644a045dd0aab
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:41:30 +0000

    grateful up edge hole face

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
