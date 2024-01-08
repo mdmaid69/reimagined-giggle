199a37b3fc1d5d0a52991121f43b5909f6176cc6
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:12:00 +0000

    few stamp phone would off

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
