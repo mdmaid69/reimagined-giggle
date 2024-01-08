dced7d1d2db63d482c160a63eefd4699837558f1
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:57:15 +0000

    conclusion getup vision trick invitation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_work(force, distance):
+        return force * distance
