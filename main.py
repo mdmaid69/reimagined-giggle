65139fd7432eb5a5638e31d5417fc7dc0527f971
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:58:03 +0000

    pay same basically satisfied discovery

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import sys
-def exit_program():
-        sys.exit()
+print("Hello, world!")
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
