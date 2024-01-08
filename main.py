9d0aa3ba6fd9e394bd4c831f8487f0cbf2acf6b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:24:57 +0000

    issue form drink replace lie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
