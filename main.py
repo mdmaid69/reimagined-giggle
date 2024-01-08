9470899c56051e686c74273752e50284027ce290
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:27:10 +0000

    face instant thanks admire nigga

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def count_elements(lst):
-        return len(lst)
+for i in range(5):
+        print(i)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
