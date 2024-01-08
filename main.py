5eee83ae9ccbf4074bc00a6be4a603aa1392d3bf
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:33:17 +0000

    right board pour difference mind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
