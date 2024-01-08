99f5c3d96ad6b6da60cfd47fde7008f47f80c8c2
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:53:09 +0000

    average principle hospital turn Dd

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
