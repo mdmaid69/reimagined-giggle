027869048401540ba2e6f32330092726b946775b
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:41:12 +0000

    famous clearly once little sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
