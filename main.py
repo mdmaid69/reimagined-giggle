d234095b4b3842cfa984a08bc2d4e6be77e9bf88
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:40:48 +0000

    but tonight succeed since ignore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
