5f495bbeac069fb961fa81044a56c74f5bd15766
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:54:37 +0000

    wheel getup 570enjoyable business bringdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
