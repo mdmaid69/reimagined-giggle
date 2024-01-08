d8fd2f758a862c1f2cb940e6f2065e7dabe81001
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:24:15 +0000

    mend collect hard sight bear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
