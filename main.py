0c57772b4b799d88f8d498eb9579c0ee1d25608a
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:25:44 +0000

    drop trust disappointed company up

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
