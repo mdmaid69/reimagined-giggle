89483f49bf02a57e45732ae23f572ccaf4decfea
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:54:39 +0000

    walk matter term organize ticket

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
