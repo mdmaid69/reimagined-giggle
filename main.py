22d69b62497dac61d45fa514a8865067fa578ed4
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:58:54 +0000

    instruction entry produce light love

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
