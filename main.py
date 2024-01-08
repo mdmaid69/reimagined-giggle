2048dab8ad37859686c098b8c46b876a0b37d21d
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:52:59 +0000

    goodbye necessary step card relationship

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
