bb39024b86a0f4cd7ae30ab2753f6ebc814ab8c8
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:00:54 +0000

    drink godown doubt suit every

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
