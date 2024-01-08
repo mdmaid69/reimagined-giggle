bb6539d2411558ce768c86b0aa2765321d268150
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:43:36 +0000

    embarrassed beginning burn rest image

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
