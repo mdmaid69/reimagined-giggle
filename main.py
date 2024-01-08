90ba4ef810f15cb727939e1cec97e4f33b7997e9
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:25:04 +0000

    downstairs market slightly health add

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
