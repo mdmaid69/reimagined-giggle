ba0d0b23e61975ae9bb443185d26ff44fc9b79ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:56:04 +0000

    bar coffee dull behave term

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def find_max(numbers):
+        return max(numbers)
+import time
+def get_current_time():
+        return time.ctime()
