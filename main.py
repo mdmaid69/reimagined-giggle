0d73891ec724a727aa5db3fd0fb3e4e2327414d5
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:35:18 +0000

    shop shop home injury back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
