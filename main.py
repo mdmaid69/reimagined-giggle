fadad6c22d6b233205181bca4c83e35af9fad308
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:21:38 +0000

    few generally service intelligent stay

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+for i in range(5):
+        print(i)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
