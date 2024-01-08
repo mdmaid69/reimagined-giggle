c1a9af75f6011cced905f65280683f569bf4ee39
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:27:46 +0000

    continue issue damage artificial Nn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def multiply_numbers(x, y):
-        return x * y
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
