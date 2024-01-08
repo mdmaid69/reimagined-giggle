f706b1c1d043adeab6c72fa56d6c14061453e373
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:39:39 +0000

    keepfrom collection thinkover male morning

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
