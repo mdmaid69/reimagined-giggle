2935c3c086318b110257610c64356add8e27466b
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:13:39 +0000

    physical release off with art

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
