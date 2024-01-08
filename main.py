eb8b79df2ae6b205b09fb591cc2b19263d1d72f5
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:51:08 +0000

    many trap behind create operate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
