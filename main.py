0a58cf83ff10eb886af7d0483338f883d796c673
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:43:40 +0000

    prefer deep notice desperate estimate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
