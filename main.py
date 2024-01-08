c7b52f085c46a7d4bb9a18881f760900b268e6cd
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:48:45 +0000

    radio believe in wash disappear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def sort_list(lst):
+        return sorted(lst)
