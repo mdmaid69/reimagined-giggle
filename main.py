97ad78e43c22aaba2d1ff77ab287d3c703ac0b1d
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:56:39 +0000

    herself walk form post most

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import array
+def get_array_as_float(array):
+        return float(array[0])
