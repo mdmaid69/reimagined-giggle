14936ae126072bf597b504aa2c8b1780dc70965a
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:06:12 +0000

    foot sure suit block bank

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import time
+def get_current_time():
+        return time.time()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
