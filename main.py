3910a5a0d9215cf712c0069f477720d3f95972fa
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:29:59 +0000

    towards ilovedicks give smell scheme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+name = "Python"
+print("Hello,", name)
