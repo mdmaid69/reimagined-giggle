983154426c782cac24f14b824bfef5c94929a21f
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:00:57 +0000

    mine scream family listenfor kill

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
