cbd2214f640b255e0b670fdff5090a196c803563
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:19:39 +0000

    trip scale bunch upwards skin

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+print([x**2 for x in range(10)])
