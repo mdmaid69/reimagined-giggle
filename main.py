fd9e0900e3d817402aae41f108b4878743e24bc8
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:15:37 +0000

    worry urgent react frame since

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
