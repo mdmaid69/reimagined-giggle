436f614d95b476a1075818f0f5e77e794e5ce554
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:01:24 +0000

    familiar excellent distance muscle cap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+def get_current_time():
+        return time.ctime()
