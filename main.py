bda6e5fa8dfe60184f9fee8c059ba2b8468de748
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:18:22 +0000

    private moment tall bringup moment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def square_number(x):
+        return x**2
+import re
+print(re.match("h.*o", "hello world"))
