bad01d0fb24e6973febb7f735dd2f9cc6395c418
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:49:46 +0000

    turnon building in science wait

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_length(array):
+        return len(array)
