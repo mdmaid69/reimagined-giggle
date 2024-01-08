e84f5ca61d515066aa2c8b02588b7672f145bdaa
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:26:31 +0000

    represent rough display ashamed steep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def get_array_as_repr(array):
+        return repr(array)
