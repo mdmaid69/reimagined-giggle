d5a9fe6c11295116dcb3217f1107a0764cb3abbc
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:36:40 +0000

    suddenly pride suit dig Cc

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
