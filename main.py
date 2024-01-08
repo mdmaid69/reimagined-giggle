accbf14d639c7b6122d5cfb98a753f641fedab5a
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:48:24 +0000

    might practical receive couple moment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
