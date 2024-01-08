a615b899b164d863a997f0f8d13fc0714eacc567
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:32:17 +0000

    later health pure quarter thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  def square_number(x):
-        return x**2
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
