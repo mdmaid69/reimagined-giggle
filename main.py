6de999a3d4e99acfcb866bdf70c3654392c427f9
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:37:42 +0000

    you suppose tidy guess father

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import os
+print(os.getcwd())
+def multiply_numbers(x, y):
+        return x * y
