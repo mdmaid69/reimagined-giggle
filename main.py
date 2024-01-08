2a5be389d398c4e924433f50558eb075870a29af
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:07:03 +0000

    once pay few exchange negative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
