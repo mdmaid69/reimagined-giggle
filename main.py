7d27e91a6951e0ec862b6bd200a62b5790799a81
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:34:14 +0000

    draw like suit where find

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
