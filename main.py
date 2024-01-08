d906199a71ac2c2685df2b841dedbf07630f0da7
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:43:33 +0000

    worry may little region thinkof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
