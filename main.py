0893da59d3aef100ee616ef44b2cfbc81bf81dc9
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:16:07 +0000

    exactly confidence sting happen look

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
