e7bf75e0ff4dc276137ba5f7f37ca7c852a92dd2
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:43:41 +0000

    change bar land commit behaviour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
