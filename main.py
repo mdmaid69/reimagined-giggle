00a3ece3e93f4d32a24f42c0f3fc92e1f55aa27d
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:05:27 +0000

    slide organize across game soil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-print(os.getcwd())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
