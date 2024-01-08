669c59b1f4672f6a18b399795af9d290d2e0bfab
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:39:25 +0000

    let Ii produce pot intelligent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import collections
-def create_stack():
-        return collections.deque()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
