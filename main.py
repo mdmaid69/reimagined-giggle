504ccc2bd8bf7bed35b8d61fa6d05217076a9804
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:00:37 +0000

    564encourage over discovery roll hide

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-i = 0
-while i < 5:
-        print(i)
-        i += 1
