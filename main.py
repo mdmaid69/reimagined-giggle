8d2252ad5bbe0a92e8537282b0d2094f8ab1cd94
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:22:12 +0000

    today side rise dare increase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
