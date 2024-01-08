2386e1f09f4652b8918ae49a6ed0fdcaa23502f4
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:26:05 +0000

    small excited contain walk go

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  def count_elements(lst):
+        return len(lst)
