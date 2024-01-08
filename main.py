03c0a267ef2a1ff7baf3156484b9f9982c262f42
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:20:12 +0000

    bet development recognize eat book

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_hex(n):
-        return hex(n)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
