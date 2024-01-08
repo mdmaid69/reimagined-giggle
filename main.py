b9bc558ec8a4702cfced747059f989507c68ac3e
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:45:19 +0000

    motion share their attention doubt

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
