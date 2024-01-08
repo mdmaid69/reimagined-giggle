4b889ef140e8bbddf3ce64c38f69501844e9c9f2
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:37:40 +0000

    can unite Yy figure relief

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def count_elements(lst):
+        return len(lst)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
