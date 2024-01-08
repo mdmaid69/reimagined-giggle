0a0229b4d530d8b54fce5949301476489d1d071b
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:03:26 +0000

    miss with reply circumstance love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import sys
+  def get_python_version():
+        return sys.version
