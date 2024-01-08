d080e226858d2876a9af8568753ae66d6d9ce5cb
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:51:28 +0000

    boring there opposite surprised runaway

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
