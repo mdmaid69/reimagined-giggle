11984eafcc5d14b59bfa160fe7cab5ff85f22dd2
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:23:27 +0000

    furniture carry day conduct unable

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_pressure(force, area):
+        return force / area
