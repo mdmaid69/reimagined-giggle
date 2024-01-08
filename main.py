1379dbd6ec6d2e8bd14c8c5d09fb646bf300e2df
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:51:37 +0000

    nigga concerned professional clear organize

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_binary(n):
-        return bin(n)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
