975ac66e256d738c1bbdf9debd87daa659d01281
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:00:35 +0000

    surface vanish age slip variety

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
