4215b2574902e079f9aabb72464dc7011066fed3
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:08:00 +0000

    shall increase snow result train

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-name = "Python"
-print("Hello,", name)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
