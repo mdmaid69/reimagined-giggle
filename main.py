c189f3a215fceb5155075031b3d003b7b7ddc702
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:55:47 +0000

    accidental ourselves will drop check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
