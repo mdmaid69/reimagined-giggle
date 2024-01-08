8701d953c38f69224ccf190b415fc7bf538169c2
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:27:28 +0000

    top plain popular space believe

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
