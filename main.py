bf02f91b8057606abb0aeac51d3fb6933c56aafb
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:42:57 +0000

    set know date hair remain

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
