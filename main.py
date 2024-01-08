9d0c2ade2e5cdb22bd61eeb455cb82c8424d8537
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:41:52 +0000

    temper imagination family during breakfast

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def is_palindrome(s):
+        return s == s[::-1]
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
