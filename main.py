35bf0302a76fca7b023eeb147983a3b841fc3738
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:56:56 +0000

    holdup store activity contrast basic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
