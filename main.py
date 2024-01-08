4b22b03bb8c2b1c860e39dc0aeff3e4b5e5afb32
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:29:43 +0000

    match towards hit decide team

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
