72834343fb3ac82229a2efaabb238f667219f78f
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:56:33 +0000

    upstairs towards expression division joke

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
