2ec03077f6c6bb30322e78a500edf0bf6045646b
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:15:17 +0000

    separate step relax discovery indeed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
