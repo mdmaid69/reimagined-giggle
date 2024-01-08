728a7886f5fab652fb22988a020e33b789b659e3
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:39:34 +0000

    under ours actually rob takeoff

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
