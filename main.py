25aeead213a99a5ecfdf4acd963abd1560b7135e
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:12:03 +0000

    bar slice kick rush date

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
