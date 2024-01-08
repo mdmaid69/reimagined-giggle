ffb139afdad44acc3a2576e823dc5702729bf06d
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:14:55 +0000

    decide correct check meaning still

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import json
+print(json.dumps({"name": "John", "age": 30}))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
