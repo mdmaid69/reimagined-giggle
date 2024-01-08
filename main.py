d5448831482824a346c4ff0b96b9c57bb920b6ab
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:36:53 +0000

    put joke take block stone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def cube_number(x):
-        return x**3
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
