05e1d927ac9b7865e572a2ebb6f9a55c52b9db8a
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:40:19 +0000

    declaration reduce mark imagination giveout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import time
+def get_current_time():
+        return time.ctime()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
