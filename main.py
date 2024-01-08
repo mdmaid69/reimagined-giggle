83556f2ac77e303be914f0455ca574ec526d6dd6
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:20:12 +0000

    length chat fasten serve stand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
