7006bc86639577fec2128748aedf6de61af1c1c1
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:43:07 +0000

    then gradually communicate goon argument

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  def convert_to_binary(n):
-        return bin(n)
+import time
+print(time.time())
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
