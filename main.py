7c7d777a4893f8fadfcb3d3bdc2cdf8213b0d478
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:50:01 +0000

    protest fight speed influence man

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
