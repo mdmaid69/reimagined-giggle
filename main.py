2a37b6d544bd2eef33ecc886d2cb58b5e0139538
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:21:53 +0000

    stretch final pull 566end kill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
