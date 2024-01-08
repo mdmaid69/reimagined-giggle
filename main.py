40f4d1749adc27ef065410245372fb5231379f78
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:57:22 +0000

    shut bear close die disapprove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
