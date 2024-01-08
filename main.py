5be0398e04d8ad00eaad90fca7f8b46fd1b7fe6e
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:30:53 +0000

    female offend deaf turnaway considering

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import sys
+  def get_python_version():
+        return sys.version
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
