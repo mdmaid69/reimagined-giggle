0cd62afc7a73e97fd4db009e49eae836d3468930
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:33:38 +0000

    smooth thought confused sister transport

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import sys
-print(sys.version)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+print("Hello, world!")
