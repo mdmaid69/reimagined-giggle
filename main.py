a18d54494f0badfb6b24e39627c922e89fa06d26
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:12:29 +0000

    language crazy day must water

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def sort_list(lst):
+        return sorted(lst)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
