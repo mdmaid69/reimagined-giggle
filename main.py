4c0ca512d53abdce3efe0d0dd1857f018c292212
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:04:52 +0000

    satisfactory run hair consist performance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
