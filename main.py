ff55dcf4642e6b931e1d824bf46f557cd737080b
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:25:18 +0000

    quick defeat team fortunate criticism

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def get_array_as_set(array):
-        return set(array)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
