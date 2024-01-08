799ed71c2ac46f1b803c398539f6949154ea74bc
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:10:32 +0000

    taste us plant wise heat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def is_odd(n):
+        return n % 2 != 0
+import getpass
+def get_username():
+        return getpass.getuser()
