e7cecbcaefd4f938cdd516c49b37850572c39ede
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:13:17 +0000

    teach scheme copy since getoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import sys
-print(sys.version)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_pressure(force, area):
+        return force / area
