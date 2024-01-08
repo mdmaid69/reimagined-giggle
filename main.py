2eabf2c5623b3871f8164cfef961ac5506bd14dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:41:22 +0000

    will then door appear little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
