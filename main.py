4b0e3e2ab9d9e7ee7b21a861829d2ad6cd06aad7
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:07:30 +0000

    guilty unhappy spend war supply

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
