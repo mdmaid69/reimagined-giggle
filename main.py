66e67d51156d3db4dfb7e99a5a08f6a0399bfeaa
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:55:00 +0000

    television post record dear wing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
