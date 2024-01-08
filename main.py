b79174cc22de53e06b64c4a033ed4ff301aaf0c8
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:08:52 +0000

    vanish might before recovery complain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_density(mass, volume):
+        return mass / volume
