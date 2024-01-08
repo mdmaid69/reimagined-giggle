afb9dcd093805b28e50a6ee895350fac533d06d4
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:25:57 +0000

    school swing live understand all

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def get_array_as_float(array):
+        return float(array[0])
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
