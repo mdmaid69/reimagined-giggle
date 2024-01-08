f7729eed7c835b79cf4854068f72e86f237478d5
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:55:58 +0000

    region display guard pleased worst

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def convert_to_octal(n):
+        return oct(n)
