cedd49aacbaf02a480ba4b20890759f3ac66f45d
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:08:38 +0000

    past hold person hurt history

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_area_rectangle(l, w):
+        return l * w
