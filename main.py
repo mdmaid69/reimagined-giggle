c983b5bc69225e0e27639e0692fe4c923a4210c2
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:27:24 +0000

    complete estimate clean shock practice

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
