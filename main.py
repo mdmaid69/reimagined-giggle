6ee193803245c7ab9b8d355a4938792232517ff0
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:15:08 +0000

    state surprising someone equal main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_pressure(force, area):
+        return force / area
