f681135bf93b454ac6c4355f11c97467eb7d5b62
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:14:33 +0000

    national neat butter draw mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
