bcbf8693202ac7e2476d5bd3e8e5ca3276495c72
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:13:26 +0000

    impressed hospital opinion square upset

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
