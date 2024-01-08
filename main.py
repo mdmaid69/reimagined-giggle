5b90a63e879c319cac84be4e7ca44ebcba55ebd5
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:01:08 +0000

    shallow body impressive point guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_set(array):
-        return set(array)
+  def delete_file(file_name):
+        os.remove(file_name)
