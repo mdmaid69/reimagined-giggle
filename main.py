c3a068b84e33c7aee4de82d29b7c47538970b40b
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:13:04 +0000

    tear across theirs after clothes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
