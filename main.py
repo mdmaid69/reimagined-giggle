0dbfd03ba348e8bda91030967ccee986e5574b7d
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:08:38 +0000

    splash neither feeling image ignore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_acceleration(speed, time):
+        return speed / time
