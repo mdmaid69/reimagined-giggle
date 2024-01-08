5b2871636b1a7a17ddf7c99449abfacc820036a8
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:55:45 +0000

    hold motion promise sensible chat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
