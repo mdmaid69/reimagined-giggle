37b591eb59438589a134896fa9bada5302e58cac
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:03:35 +0000

    tragedy dick along hard track

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
