550fb9001d5c823961fa0e755bded5b85b2ffe9c
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:22:41 +0000

    spot free break tidy habit

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
-def remove_duplicates(lst):
-        return list(set(lst))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
