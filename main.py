65b0ece513ea44ff013000128fd6c771ac6c98dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:35:42 +0000

    easy field basically figure urgent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
