0bd4f375f974811dc553d9294b9d976860536f46
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:26:16 +0000

    correct feel turninto advanced shrink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def get_array_as_bool(array):
+        return bool(array)
