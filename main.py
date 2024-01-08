228059aa5c24fb93eeab0e328858c3564a5f6a4c
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:07:33 +0000

    challenge best show cry fault

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def find_max(numbers):
+        return max(numbers)
