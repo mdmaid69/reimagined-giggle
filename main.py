3a5e0a42242a52f2393abdf1ca1004346bc2287d
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:18:50 +0000

    care event public hold justice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def convert_array_to_list(array):
+        return array.tolist()
