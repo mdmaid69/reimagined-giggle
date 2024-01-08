50b8c55235ab714dfb058ed5cf1eda0eb25ba611
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:01:11 +0000

    hope education positive arrival gradually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+print("Sum:", sum(numbers))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
