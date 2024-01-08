897e89d243e1ebbf61f9d8c36c09903b672efc5d
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:05:09 +0000

    display favour intend become maintain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_density(mass, volume):
+        return mass / volume
