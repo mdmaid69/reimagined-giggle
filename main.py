6380cb9f7b100b9b2ce6dc6fb561ad61ab542e5c
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:36:25 +0000

    various tongue agreement be laugh

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def square_number(x):
+        return x**2
+  def sort_list(lst):
+        return sorted(lst)
