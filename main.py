b5331b5e4a5cd1249c27f8c1f8fda1740c30fd56
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:31:40 +0000

    getout history truth large basic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import tensorflow as tf
+print(tf.__version__)
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_arc_tangent(x):
+        return math.atan(x)
