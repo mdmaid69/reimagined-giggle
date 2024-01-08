17d8106f58bd1469d86a407f128d6693d4166e9b
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:16:13 +0000

    still opportunity argue reply car

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import json
+print(json.dumps({"name": "John", "age": 30}))
