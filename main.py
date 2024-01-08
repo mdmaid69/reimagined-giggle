ad85900c77b0e290eee070de5e09b8858c0b544e
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:12:47 +0000

    confusing war takeover bringback little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-  def add_numbers(x, y):
-        return x + y
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
