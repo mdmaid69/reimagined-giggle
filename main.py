5552f6e7135de13d727fd4dc5f72ad6a2c2c33af
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:20:15 +0000

    branch steep believe water statement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import array
+def get_list_from_array(array):
+        return array.tolist()
