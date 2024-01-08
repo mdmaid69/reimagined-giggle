b003a4a60d5fdbcd23a6cbc8476a0ed8e2f8f802
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:29:15 +0000

    friendly violence from past dear

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
