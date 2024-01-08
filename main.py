f267f13abb05c0789215ab8d887595589f7eb06a
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:25:06 +0000

    wipe change square old fierce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
