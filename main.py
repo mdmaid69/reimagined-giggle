eba6c93f07e750697aea6bc6e70a5d877aec59a9
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:49:38 +0000

    cover scale thorough hide congratulations

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_item(array, i):
+        return array[i]
