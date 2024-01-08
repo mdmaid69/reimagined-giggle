63781f1d0bbeebe002e6c85b0c43fd8c1b3887b1
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:51:02 +0000

    cost argument deep power attract

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
