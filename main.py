8a651fdc08e69b5d2bfa0f3e8555e03c26f5299f
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:15:09 +0000

    accidentally season heart complete heart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
