320c354e3bd5a9f48b8c92ccef19e7da0257fd52
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:39:41 +0000

    yes lend mass Hh smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
