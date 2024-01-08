4b91735e9f563b2afcef5556b6557509d7c6d85a
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:03:19 +0000

    home drop bound remember touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
