69d0c0fed301704448f71f2eeed8b909f1b28b4d
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:41:54 +0000

    spread doubt drive mail period

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
