ea07141672b998ed0e2e795d344738e8722938c7
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:43:21 +0000

    curious maintain lookafter stamp show

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
