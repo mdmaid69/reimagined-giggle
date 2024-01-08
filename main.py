5f5a48fb816b883920351e02fbb05620e21c13d7
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:18:59 +0000

    film land loose activity street

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
