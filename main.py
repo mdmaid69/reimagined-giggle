12ec4cf88e58f1d293ba53453ca753d12ef7afa5
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:21:24 +0000

    invitation exchange takeout arrangement confusing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def convert_to_binary(n):
+        return bin(n)
