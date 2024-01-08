274e387b2c3ee2c0e51106b0e4b0404ba255988b
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:09:15 +0000

    impossible block exit chief talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
