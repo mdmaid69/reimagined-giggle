095a162b219339683711486459403fbf2f826663
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:16:12 +0000

    care spend complain long argument

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import math
+print(math.pi)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
