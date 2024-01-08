958563b1cdaf3ddaf547a9ab2fb796a24fe77a1a
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:42:12 +0000

    worried practical attract cook boil

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
