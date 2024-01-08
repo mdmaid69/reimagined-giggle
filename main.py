fdfd7b9ee47a3db1d641ddffd3cde3587dcb1898
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:26:54 +0000

    apparently avoid common press suckme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def is_odd(n):
+        return n % 2 != 0
