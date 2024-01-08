d44f36cf157392b9af74afa4b91eda3fe559c79c
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:57:00 +0000

    issue rank bunch extra force

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_floor(x):
+        return math.floor(x)
