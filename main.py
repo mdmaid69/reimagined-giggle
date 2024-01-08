eb69273e6b65cb18873575f82feeb0686db4ca8d
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:34:09 +0000

    always put half port tired

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
