d78ba2ff8dc04fd1f5ace7884b3cb284a75f783e
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:57:45 +0000

    spoil work ilovedicks underneath solve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
