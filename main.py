28b56f465989cffe3db00d43b022756c5bd6cd42
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:57:37 +0000

    memory concentration problem off patience

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_as_bool(array):
+        return bool(array)
