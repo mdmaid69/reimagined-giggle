111452ed102b7c90fff3adb0e2bd33f041f60f55
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:27:09 +0000

    real spare event fill steady

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
