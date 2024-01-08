c02e19d07951c0c8d503728baec338c3fcbc5102
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:28:04 +0000

    failure forget gap round forever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-text = "Hello, world!"
-print("Characters:", len(text))
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
