8d74ce910e791344ea586728cde8ac2abadd8b5e
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:17:00 +0000

    positive professional after some worse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def sort_list(lst):
+        return sorted(lst)
