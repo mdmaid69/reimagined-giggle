903f517f2fa82a9870f160298b9d4f3c764088ef
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:53:08 +0000

    story law answer study run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
