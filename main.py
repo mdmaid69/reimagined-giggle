b111d602096fac0a7fb8b0f30c17604dd247fd52
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:58:12 +0000

    peace sting smooth position skin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
