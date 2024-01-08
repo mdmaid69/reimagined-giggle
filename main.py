325271cfc926ae1dccd218adb4cd7f05cb939dc4
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:09:43 +0000

    with expensive somebody health worried

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
