5b0705cfdeaa27ffe7e563e47e088d9f97f6e4e5
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:18:47 +0000

    inside near waste bag repair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
