2f267af0c25e9a0552df54d33e3419a42588182b
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:44:28 +0000

    smile responsible other surprise office

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
