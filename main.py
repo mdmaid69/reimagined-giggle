298b8405ee82513c191102a777e3f7167ce9ccde
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:32:57 +0000

    comment calmly manner cross nose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
