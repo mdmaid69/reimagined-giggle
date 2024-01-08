37b63661fbbfeeba3f25b0e20ddf1a61497c5ab0
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:49:09 +0000

    pipe write spring refer squash

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
