e759e178d1cded8e89be8fc2ca341cdec49f540e
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:45:02 +0000

    flow together oil sense many

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
