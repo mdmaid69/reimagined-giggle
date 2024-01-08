e53603a2b8326bbb2de17f72c27e0b4adba451fb
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:03:50 +0000

    takeout clear amount control drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
