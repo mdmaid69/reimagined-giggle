7e763df4259c9de5e2a2d6d8a73e57363331ee6e
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:20:26 +0000

    interrupt by deep land have

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
