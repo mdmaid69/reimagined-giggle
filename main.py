adc644bd46cc2fa5074a964ad77d87469fd99c29
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:31:06 +0000

    hers present narrow whom spite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
