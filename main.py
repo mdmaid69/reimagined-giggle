550a6067c8da1161080f51061d5c8bb770dd02e1
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:57:09 +0000

    noisy brush surprise arm serve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
