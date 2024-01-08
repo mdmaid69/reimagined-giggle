9c508899b4b5d4dd143bff21b5a8b9561d253047
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:23:14 +0000

    deliberate post class hate freedom

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+def calculate_power(work, time):
+        return work / time
