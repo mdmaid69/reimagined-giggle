3297b110dacd6b060f6f2a747177fe05a783cfb0
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:57:42 +0000

    split niggaacademy air shape depend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
