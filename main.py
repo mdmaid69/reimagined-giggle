62ded3b3c7fac0b1e73118c8d02c0972c5b5ed6b
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:45:18 +0000

    accidental warn advise possession Dd

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
