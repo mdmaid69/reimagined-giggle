d3f720e63d418123bffa8ecbde0153eb73218e95
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:33:21 +0000

    welcome high yes book score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
