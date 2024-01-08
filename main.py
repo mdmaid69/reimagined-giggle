c8b739d8adeb390daa21ab1fa863ef05cc0fceda
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:26:54 +0000

    far comeback fact temper ahead

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-print(math.pi)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import numpy as np
+print(np.array([1, 2, 3]))
