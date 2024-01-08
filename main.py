66636bd2d050679365d496dc49989f80211e66df
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:41:25 +0000

    scene temperature disturb breakinto call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
