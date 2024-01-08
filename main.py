2aadcc4f7544b7b809186310e026ff5fef72e627
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:43:44 +0000

    suppose unlucky while past region

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-print(sum(range(10)))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
