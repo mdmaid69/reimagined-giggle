3a95d823028eac21c884b64c4561a687a36ec9a4
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:48:50 +0000

    perhaps slightly water or promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_density(mass, volume):
+        return mass / volume
