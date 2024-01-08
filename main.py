ee158ded551b22599b56e42ab8f3d545e338d431
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:11:23 +0000

    dinner sing performance drink no

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
