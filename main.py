7bcd65d17c602375620564a2fb3f5f29f1245965
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:28:22 +0000

    switch off excuse check cause

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
