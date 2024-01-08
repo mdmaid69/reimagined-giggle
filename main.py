903c5a185942edddec275effdd1f52ec282ab99f
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:42:57 +0000

    slice interruption wherever complicated draw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import sys
-print(sys.version)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
