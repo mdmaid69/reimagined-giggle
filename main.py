1af5085499a4676d1b2a58f58e6d9be8d1185e22
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:10:23 +0000

    Rr point bad decision breakin

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
