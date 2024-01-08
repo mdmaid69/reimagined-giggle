8d0a1dcfec22d6f68235f8764e2c1f036249752a
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:47:23 +0000

    fond it generous look car

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
