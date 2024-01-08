0f9a88c2af8402e6b3b100fbe1624e46e04f3408
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:46:53 +0000

    job silent clean reduction replace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
