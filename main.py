11eba377e9376e661e0a2f0f4316bc5cd25a6386
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:34:28 +0000

    least ordinary turnback greeting woman

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
