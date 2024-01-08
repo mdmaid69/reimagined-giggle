a00ffb5933cc9beee59154736def7456609bc893
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:34:35 +0000

    his Ii wise takeout fact

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+x = 10
+y = 20
+print("Sum:", x + y)
