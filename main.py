d8d90afe0185995770fa7b7e768f0998f01fd964
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:00:23 +0000

    head thought approval keepoff fresh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import os
+def change_working_directory(path):
+        os.chdir(path)
