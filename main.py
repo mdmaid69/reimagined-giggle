a643d83c974d882edecd45fb7954e6e8a64f6f77
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:28:55 +0000

    flood fix hold still name

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  def is_even(n):
+        return n % 2 == 0
