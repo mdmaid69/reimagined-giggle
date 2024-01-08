45864dd80574f36ad3108546f86efbd24de40f83
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:54:54 +0000

    disapprove goup sick fire dish

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def add_numbers(x, y):
+        return x + y
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
