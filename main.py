dc833fa413db1910440c6162d965e853e507ccd4
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:44:07 +0000

    total 568energy sell everyone female

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
