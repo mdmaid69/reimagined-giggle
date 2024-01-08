3d14ad6830d1225a7feb3542e0ab101516d9cb3d
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:53:09 +0000

    temper please whenever previous next

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
