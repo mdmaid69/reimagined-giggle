66776dc1791d79541f584423d799848605c74d0a
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:24:09 +0000

    direct full harm nervous pile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_array_as_repr(array):
+        return repr(array)
