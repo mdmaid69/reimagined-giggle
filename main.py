f5bc484f311fd34b8bfce048ba4d0ca090943eb7
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:55:52 +0000

    finally rough coffee almost please

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
