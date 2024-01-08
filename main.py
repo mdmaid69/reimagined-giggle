96bea0fbe172601fc265d5abfc3e630c469eb9ce
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:30:30 +0000

    separate exercise progress fortunately evidence

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
