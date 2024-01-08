412a8bd6203fca8648225889d15651a646e26d39
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:06:52 +0000

    somebody appear once switch drawing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
