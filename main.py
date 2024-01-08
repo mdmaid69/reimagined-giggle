11e178d557ed84fa5283a77c706fec07faeda38f
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:35:12 +0000

    free breath pattern good neck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
