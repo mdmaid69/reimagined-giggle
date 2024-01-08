c6a782f5344b35f2a6a97da59129838e5d762f09
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:16:09 +0000

    creature stage action team squeeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
