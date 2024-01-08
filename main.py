9d0284bda619f00d13b4c439828eb3d3119ab7cf
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:25:15 +0000

    schedule recently distance mouth love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-for i in range(10): print(i)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
