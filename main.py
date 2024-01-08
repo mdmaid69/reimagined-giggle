4cf8d3b24e6c16cc568d16ce9fd5a73474b4bf8a
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:43:50 +0000

    share eye best towards beat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
