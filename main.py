083926fd6280db7099664195bb8f7730e7e88dd2
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:18:03 +0000

    one afraid advantage cold complaint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
