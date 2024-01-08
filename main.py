f80d2552a46539a36cadaf196d1cd9ee3e5d12d6
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:12:49 +0000

    divide meet shut cold thing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
