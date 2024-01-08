f54338f96f1e74f8c4b7d755b40c3a5a1001cad4
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:13:27 +0000

    regard ability count perform song

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
