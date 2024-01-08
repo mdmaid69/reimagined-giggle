41083c24ce4653755a796dddf1e77cce05388707
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:35:24 +0000

    ticket workout probably wild vanish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def find_max(lst):
+        return max(lst)
+def find_max(lst):
+        return max(lst)
