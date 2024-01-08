07070a7e92af76757e194d6bf425ecf84c85d1cf
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:06:54 +0000

    vote dream drink wild best

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
