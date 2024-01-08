105d3c59dc32ccb15cf9df3836a3b771204b7fe1
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:27:23 +0000

    receive university suit respect inform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
