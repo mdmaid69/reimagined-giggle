b072f6cae30fdfdaea2d3b0317cbf38bcde44f46
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:30:45 +0000

    glad scared wing tidy permission

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def count_words(sentence):
+        return len(sentence.split())
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
