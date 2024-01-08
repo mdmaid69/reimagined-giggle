15b44f53019286f99b0d5d2fd0c68b3cef8a81d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:48:02 +0000

    mood pass bridge battery heart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
