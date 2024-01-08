91117ad0c7fbeda4a023a9a95016b850da34cde0
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:06:10 +0000

    mouth tongue recover natural mend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import tempfile
 def create_temp_directory():
         return tempfile.TemporaryDirectory()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
