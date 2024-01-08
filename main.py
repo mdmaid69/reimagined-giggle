70cbbed10a4fa406b7fcf781a825963290687ea6
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:33:46 +0000

    transparent water abroad flat giveback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
