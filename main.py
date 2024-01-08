7e3aad3dc991fd799327db1c036841dbe83d870c
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:08:26 +0000

    send roll male warn work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
