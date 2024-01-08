5712b3af2572fc6ad79adf6f0eaaf7e12f8e7763
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:50:10 +0000

    rate show health store body

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
