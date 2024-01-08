23a01cef9d975113f397734218e086d6b95949eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:38:38 +0000

    attempt curious whoever century mouth

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def append_to_array(array, item):
+        array.append(item)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
