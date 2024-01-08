39f4078fe48aab9d199b8bc5d81ed214010c7bb7
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:37:02 +0000

    there used range dark yes

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-name = "Python"
-print("Hello,", name)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def is_odd(n):
+        return n % 2 != 0
+def is_even(n):
+        return n % 2 == 0
