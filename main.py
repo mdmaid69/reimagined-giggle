996354c64b7c871cc380ad36dc31fd794ddfb25f
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:08:11 +0000

    tool ground own runinto slip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def convert_to_octal(n):
+        return oct(n)
