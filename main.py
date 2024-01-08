cffe966441442c0bb843e6d2aa2be099aa7bbed3
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:25:34 +0000

    means shake justice split illness

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
