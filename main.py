36be722f87acb7e718cfe0622d699c67dd14f25a
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:44:33 +0000

    age give bang drop neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
