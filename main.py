6b53db46dc2942729973293d925e6a5106e7d446
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:23:24 +0000

    destroy realize everybody force who

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  def cube_number(x):
-        return x**3
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def split_path(path):
+        return os.path.split(path)
