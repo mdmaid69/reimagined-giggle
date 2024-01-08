9186d662843e9a0a2b8b56e255c86c9a4df974ed
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:28:36 +0000

    keepoff attractive subject achievement past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import os
+print(os.getcwd())
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
