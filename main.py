9eef4af907a1d4b8d0d9dc06ae19605113c39856
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:33:15 +0000

    satisfactory why ball comedown worry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_max(numbers):
+        return max(numbers)
