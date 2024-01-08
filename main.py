2bc27e42dfb75fbf1a41356e4d86b7090f2ec1f0
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:31:45 +0000

    cry that return detail respect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def generate_random_number(start, end):
+        return random.randint(start, end)
