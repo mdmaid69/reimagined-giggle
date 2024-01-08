a8d477daee589323f0f38eaaf89b911f0f3f5f14
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:59:18 +0000

    need prevent doctor opposite cheat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import collections
-def create_counter():
-        return collections.Counter()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
