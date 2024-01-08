81aa6ac9b8f61bfa7d2dea65c5c3dcfaa993d19e
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:03:34 +0000

    city beat ticket pause improvement

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_speed(distance, time):
+        return distance / time
