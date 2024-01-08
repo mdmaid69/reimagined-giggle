1e4e6423171a6cb404b4154f8e2c217decd0f9e7
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:56:36 +0000

    stupid centre average runinto pot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
