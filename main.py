3faa0e4a3c7ae292dce950a69d496ea0ec4b31d4
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:38:05 +0000

    correction Ee light ready stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
