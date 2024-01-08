1c1f3856f6876f4949d1ffaf0658b8610f733908
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:43:45 +0000

    attention turnaway sign own attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
