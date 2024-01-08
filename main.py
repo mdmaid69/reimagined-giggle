aa6695de348ccc0dcc958ab08b315203003c19a2
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:47:07 +0000

    punish meal now print step

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
