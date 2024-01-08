ac03dbf22bbdceb24cd3a8272e3db9bcbee549d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:56:16 +0000

    become glass press shock freedom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def convert_to_binary(n):
+        return bin(n)
