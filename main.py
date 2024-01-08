e00145456890adf9bc8700aba19ce723fa36e7c9
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:50:41 +0000

    affect forget tough quantity risk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
