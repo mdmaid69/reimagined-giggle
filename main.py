244020726961ef3b5ec50cda9fcd21b29709d94a
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:59:28 +0000

    roll score moment front impression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_index(array, item):
+        return array.index(item)
