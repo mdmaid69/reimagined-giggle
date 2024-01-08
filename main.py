c7d4a762b7c07199478be9d3f66446ef570aca3e
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:03:51 +0000

    organize shop century frame cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_item_count(array, item):
+        return array.count(item)
