815cdcf8bd43378a9efe903e8879536113811041
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:53:55 +0000

    minimum put simple quick rub

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def print_python_version():
-        return sys.version
-import time
-def get_current_time():
-        return time.ctime()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
