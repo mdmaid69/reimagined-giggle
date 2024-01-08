1f255b4803d9afab7cdaee70f8b2a4b92eaf17a3
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:03:44 +0000

    lookaround angry him so stone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
