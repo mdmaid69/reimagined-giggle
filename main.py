c8015b8e0396bec0c7f9012d65413d8d751d030f
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:25:42 +0000

    turnback decide thanks always classroom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
