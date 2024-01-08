c566b536b44327b8c7b27691f2469f59238ffc5f
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:49:43 +0000

    put shame spend distance breathe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
