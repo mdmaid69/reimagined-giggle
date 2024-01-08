920c5685aeadb77545519a71118e6fc9b493184a
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:18:51 +0000

    solve close but pair at

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
