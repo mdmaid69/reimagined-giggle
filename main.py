5cd7cdd3c7fdf67111a948193c9fc6f6106601f7
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:44:21 +0000

    measure attempt load hurt equal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
