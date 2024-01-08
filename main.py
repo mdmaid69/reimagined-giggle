a39993809beb66972b9d56b124431757fc412193
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:33:46 +0000

    vanish present plant spring exchange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_acceleration(speed, time):
+        return speed / time
