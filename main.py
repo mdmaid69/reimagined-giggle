98aae2c299d3bfd3e821585ba87b2b4e2197c91a
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:14:59 +0000

    love hole dance replace giveback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_area(radius):
+        return 3.14 * radius * radius
