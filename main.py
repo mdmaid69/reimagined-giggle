e4174d468099182fe0926f6378a068b3b8aa4615
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:44:37 +0000

    load convenient me throw discuss

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def count_elements(lst):
+        return len(lst)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
