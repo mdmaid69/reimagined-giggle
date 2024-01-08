bc58f1ab0552ca9688d2411e27de5767d9bdf1bf
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:57:34 +0000

    pen treatment refer once miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
