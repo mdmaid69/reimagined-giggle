e9bbed7231c7d9143cf4d9036f2da91178257bfd
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:09:08 +0000

    bored education choice several noise

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+import unittest
 
-{body}")
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_as_int(array):
+        return int(array[0])
