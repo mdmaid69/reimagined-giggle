99a9e5d7317cc8bef8480024030f11ac1d9dade8
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:25:10 +0000

    relative stop pause shall bury

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def add_numbers(x, y):
+        return x + y
