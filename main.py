1e8c4cfbd58a73ca7173f463fe0e732bbe35482e
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:05:22 +0000

    recently change disappear first rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
