7f1ff3c53cd29f25dc6eb284635918165703ba6c
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:48:09 +0000

    total achieve continue door open

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def subtract_numbers(x, y):
+        return x - y
+import array
+def convert_array_to_list(array):
+        return array.tolist()
