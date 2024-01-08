496ad66a1c6341d2ce9d02228c2f35db0ae3c8bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:12:39 +0000

    file appearance calm duty escape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
