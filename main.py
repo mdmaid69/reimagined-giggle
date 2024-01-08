56c78edc69a6111080714e2f7637afc20a5f7455
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:23:28 +0000

    well themselves any threaten car

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
