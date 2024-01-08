698b9a187ba0953f017e43bab9bbef9437f556f3
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:40:03 +0000

    fine so maximum pay beautiful

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def cube_number(x):
-        return x**3
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
