d88254a54e1253b9c757d98123196fa4dd62fea8
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:49:35 +0000

    pay busy 572enter lie soft

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
