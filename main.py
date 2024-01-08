cecdd69539f1f7eb92438f5185088597382a9f03
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:45:23 +0000

    rather test stand emphasize judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_acceleration(speed, time):
+        return speed / time
