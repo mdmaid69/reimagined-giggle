4be288daf630b2e92745156a8e004e5507432098
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:57:40 +0000

    gas comeup Ss tired rather

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
