32eb46ce6c74ba4ac88d520ab66537a1921f33a2
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:29:05 +0000

    give drop fat unlikely coat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
