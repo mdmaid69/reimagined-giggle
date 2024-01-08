a52eee3b68ea6cc87a1d11e37cd9cec45c004b74
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:39:43 +0000

    rule pour bear once later

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import numpy as np
+print(np.array([1, 2, 3]))
