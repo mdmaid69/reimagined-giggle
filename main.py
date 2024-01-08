ce174c98de7ebaaf7622136a9a70307d4c69ae73
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:50:08 +0000

    substance weigh snow slice damp

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
         plt.show()
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
