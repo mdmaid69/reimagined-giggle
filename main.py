d048628d3a428ca5fc97fcfc409ef81b31a6bcee
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:34:58 +0000

    place blame money ill congratulate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def find_min(numbers):
+        return min(numbers)
