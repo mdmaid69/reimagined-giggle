b741bfcd98d468985a517ace6505111c6ae59192
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:42:07 +0000

    breakdown address serve patience hope

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def cube_number(x):
-        return x**3
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
