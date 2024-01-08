96d3e54f6791220bfa98c27e17fc01d8cb94206a
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:39:36 +0000

    home guide used 573entertainment crowded

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
