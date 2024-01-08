144d680ec53fe9786c00c62992ccc6b824993d6a
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:22:48 +0000

    recognize word right their eager

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
