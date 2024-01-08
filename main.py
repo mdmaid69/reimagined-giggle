30f983804eeece2d3b216e21aee36a5b71786bd7
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:55:39 +0000

    separate period beat bye address

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
