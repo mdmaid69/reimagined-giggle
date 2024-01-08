4c82cf841b424a7be50d5c7b500f54f8b1979f12
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:06:12 +0000

    age schedule develop cook shout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def find_max(numbers):
-        return max(numbers)
+def is_even(n):
+        return n % 2 == 0
+import array
+def reverse_array(array):
+        array.reverse()
