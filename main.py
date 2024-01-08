fa6e2b46471f3469e467ba1ba21a739edbe18ec2
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:52:51 +0000

    border go object no furniture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
