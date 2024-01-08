f74ae6903354cf599be6164d664727ffc2358386
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:27:17 +0000

    needle dark carry but your

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-print(math.pi)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def find_min(lst):
+        return min(lst)
