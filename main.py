dc0d6cb7c09503430512665a52b92bc6fe6e9ff5
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:15:47 +0000

    round still yours turnoff house

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
