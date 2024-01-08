59f44d7ebaec50a1b419ee719e49f91c371110ec
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:51:30 +0000

    wrap spend fall voice visit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Uppercase:", text.upper())
