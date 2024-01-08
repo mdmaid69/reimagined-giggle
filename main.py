aac0d4df8e3ef75003d3f8e45262456cd684f33d
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:19:02 +0000

    accept birth run convinced wise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
