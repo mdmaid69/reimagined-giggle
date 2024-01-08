5a215337e8c4164b6b4637034c59b91306de3d49
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:24:03 +0000

    deaf below memory mail measure

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import datetime
-print(datetime.datetime.now())
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
