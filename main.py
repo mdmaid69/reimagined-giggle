6da740987d9a4e9263e6e39b718f8ad976b5e2ee
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:30:12 +0000

    matter object scare please 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import sys
+def exit_program():
+        sys.exit()
+import numpy as np
+print(np.array([1, 2, 3]))
