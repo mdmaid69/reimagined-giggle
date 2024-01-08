59a25cdc5d1aa0de9f1730cc91b2086eb746d78f
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:51:22 +0000

    stone preparation contact idea Ee

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
