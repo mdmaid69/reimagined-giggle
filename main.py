28ac4f275abba3f2e24df734f867c9cb87796ede
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:55:40 +0000

    everywhere possibly food deliver catch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def find_max(lst):
+        return max(lst)
