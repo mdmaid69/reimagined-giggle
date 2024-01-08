888671cf3ef944c81bc4032f6cd4c7356bd9fdf0
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:47:16 +0000

    grateful light so less production

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-print("Hello, world!")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
