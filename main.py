6d181569c17b469430050c29168ed2f144fca521
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:05:39 +0000

    runout board majority wise reject

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
   def calculate_factorial(n):
         return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_floor(x):
+        return math.floor(x)
