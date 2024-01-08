28bb39422cdd4f95c590abb722b325ecc7d85e0a
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:00:14 +0000

    walk correct have bottom right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_user_list():
+        return collections.UserList()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
