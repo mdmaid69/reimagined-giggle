f3a03d820f510607256a02ec11f9382d9eba3b2a
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:20:40 +0000

    contents trust theirs time spray

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def is_odd(n):
+        return n % 2 != 0
