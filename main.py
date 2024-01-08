d81b5ee96d873d911823f336ee62348bb0c3a420
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:15:26 +0000

    leg equal dead no whichever

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
