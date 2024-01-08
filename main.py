2d7c5f5ca11dc17f2775484c3729ff0312580f24
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:00:15 +0000

    prove community original nonsense mark

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
