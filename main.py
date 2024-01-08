4bd44ad7932aa37f2b45fc7d5ff0dd2d10adca4c
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:13:31 +0000

    corner opinion set promise pull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
