63577880d07a24dc6a25875a54019338ba837c0b
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:35:34 +0000

    sound dinner middle too emphasize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
