05db0cd7c8b473f3f3794a310437fa061649e765
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:11:34 +0000

    instead state cruel born settle

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import time
+def get_time_since_epoch():
+        return time.time()
