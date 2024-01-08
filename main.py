2ceae5894a67c55a68832f0ff4e566aaf72a1dc2
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:13:56 +0000

    hurry hospital cut spend dare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def add_numbers(x, y):
-        return x + y
+def get_array_as_list(array):
+        return list(array)
+import sys
+print(sys.version)
