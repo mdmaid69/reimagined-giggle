ac977f965388f6f7f9f85f59b9f30ecba3926b89
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:38:35 +0000

    study healthy arrive none freeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
