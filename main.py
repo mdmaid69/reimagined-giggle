4ff94680c20a8943ad66c235213d1fa6fb705e5c
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:53:35 +0000

    tough breath medium genuine score

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-print(time.time())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def convert_to_octal(n):
+        return oct(n)
