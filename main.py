855866f0e1055f5b87b25d67d7d4082298064cdf
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:40:31 +0000

    between after pot false advantage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
