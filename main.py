6461904bf02e9789ca0fefd92ab031b64b7432b2
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:28:50 +0000

    black model Yy slight bury

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def add_numbers(a, b):
-        return a + b
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+x = 10
+y = 20
+print("Sum:", x + y)
