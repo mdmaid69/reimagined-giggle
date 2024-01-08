d8bb1380ffe8a48368b4e24e9a0bcc62f093cf4f
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:19:07 +0000

    getup needle payment direction indeed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
