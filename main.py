c99b296f0935a3720df486830d3c40bcfc954f68
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:13:42 +0000

    quantity cross hour shame interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
