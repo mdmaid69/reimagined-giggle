d8115cb1863c40d40ed7d7145b85ff64d7a8c7b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:08:48 +0000

    character distance impossible cough such

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
