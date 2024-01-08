5aebf3949cc058a6ca76bdf5356565d97d704872
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:08:24 +0000

    careful break guess comeback hungry

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
