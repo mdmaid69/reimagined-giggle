d08b90a641f2834e5f0facf5266bffd42d324857
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:48:00 +0000

    entry lean whole worth reaction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import math
-print(math.pi)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
