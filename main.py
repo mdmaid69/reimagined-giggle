bb3387dc4a48057ee6120c905cef8f2955ca05b4
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:56:15 +0000

    object congratulate watch could rent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_current_working_directory():
+        return os.getcwd()
