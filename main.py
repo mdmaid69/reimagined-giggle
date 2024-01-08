12f7916bb6fd98038e97863f0ec25916c8a14e60
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:12:32 +0000

    callout Dd mouth fun correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
