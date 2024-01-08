fad3b76b1a2d06d48a374fa8de512498ec1f73fe
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:25:21 +0000

    flood offer nerve young book

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-for i in range(10): print(i)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_area(radius):
+        return 3.14 * radius * radius
