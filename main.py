24b1d4a0e1c92d33722cd5b9e20059e601139db7
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:39:15 +0000

    trip call lend turnback regular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import collections
+def create_priority_queue():
+        return collections.deque()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
