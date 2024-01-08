bfbecf9293c733e948634b7fc2336378111aff4e
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:35:00 +0000

    broad contain defend almost turnaway

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
