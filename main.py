a3211d161192ea544b58dccf3cf23d6c1ad29033
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:58:15 +0000

    abroad violent sink hear calloff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
