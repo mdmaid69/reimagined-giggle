1405d6c7efc7d4a9ed67ef2933d5354e12d4d07b
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:05:55 +0000

    generous phone thought advise steam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+print(time.time())
