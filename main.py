7780b51d197b0c67230e09d0d9ca183d62003f08
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:24:29 +0000

    relative sure please breakthrough little

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def count_characters(sentence):
+        return len(sentence)
+import datetime
+print(datetime.datetime.now())
