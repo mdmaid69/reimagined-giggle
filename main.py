8d0c6a30ec7ec78fcaca60497cd4f90e88dc696f
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:48:56 +0000

    success band again family guess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
