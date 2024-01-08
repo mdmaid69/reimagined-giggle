fdb9afe4db75f587d88c7428e01263ae30e86298
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:19:20 +0000

    lose question but hope food

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
