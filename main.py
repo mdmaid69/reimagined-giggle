414dc1b462750a554f70eaa84c196ed37fd91aba
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:02:05 +0000

    disturbing common capital neat apologizeapologise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
