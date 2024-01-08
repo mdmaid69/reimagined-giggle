70f9fe175275b670edba10b08d8c25b98abfbb48
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:50:40 +0000

    mother load bit bright order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def subtract_numbers(x, y):
+        return x - y
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
