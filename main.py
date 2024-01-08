9f7746a7aeffe4d1352de2f93be96825965e7a52
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:02:53 +0000

    clever material act equal approval

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def remove_duplicates(lst):
+        return list(set(lst))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
