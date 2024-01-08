e159ba2cde7c8f41b9e5781660ed6dc71c2e69a9
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:26:02 +0000

    wet ought rush television there

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_speed(distance, time):
-        return distance / time
+  def get_base_name(path):
+        return os.path.basename(path)
