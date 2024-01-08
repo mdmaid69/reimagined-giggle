040626f83dfeec49a068df3d23de0c9726ca840d
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:11:14 +0000

    my niggaacademy sister or may

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
