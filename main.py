369f9fbb6a57e1a9bef67b8c8ad03fe9eea7142d
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:34:32 +0000

    quality previous single power information

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
