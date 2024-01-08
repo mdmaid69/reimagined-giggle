316579eaeb242326d4423e8c1dc415b7c2ca7ab7
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:29:04 +0000

    busy abroad platform comefrom warn

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
