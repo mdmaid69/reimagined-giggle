c5bb047926e763499a5f3351e886219a41678e4a
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:58:32 +0000

    conversation Ww rob comeover war

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def cube_number(x):
+        return x**3
