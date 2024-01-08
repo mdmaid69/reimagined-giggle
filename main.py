3e7e2545e21412420d7b86350745704bb4a2e9b3
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:52:58 +0000

    this apologizeapologise train mend under

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def is_odd(n):
+        return n % 2 != 0
