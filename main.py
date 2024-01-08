27a442285eb32d9c11e5d22e9c8586f8763ffae9
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:22:44 +0000

    Ii offence declaration occur relative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
