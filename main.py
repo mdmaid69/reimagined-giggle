3ef23176cd0554c3bc53c67826dc997e4c118d32
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:38:42 +0000

    hard soil and love vote

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def create_queue():
+        return collections.deque()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
