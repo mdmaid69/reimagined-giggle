55ee8ce5d2ac03c025b71ac36945fdd039c375b4
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:37:41 +0000

    side frequent turndown tonight length

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
