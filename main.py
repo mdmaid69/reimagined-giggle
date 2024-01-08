3522b49ea8bc9ad3036df2ef8fbb73228b82553b
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:20:08 +0000

    hungry anywayanyhow square dig keepoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
