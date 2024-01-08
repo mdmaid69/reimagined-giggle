1dfde01470ed4ae4d35514a7a188c3f8745184a4
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:24:59 +0000

    fit sudden inquire discovery workout

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
