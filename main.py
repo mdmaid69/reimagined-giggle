0f527fbc72ed1f72cd57cd3bd55e8cec77dbdcfa
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:54:49 +0000

    pull imagination lookafter shelter well

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
