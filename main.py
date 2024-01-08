5f0f4c4e4773a2df9a21cd231e5b6dec6cbdc9e0
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:18:16 +0000

    complaint frequent minor responsible statement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def create_numpy_array(lst):
+        return np.array(lst)
