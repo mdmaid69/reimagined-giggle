6f2a3bccd8d20ca50e50f0c875d4e0953446b07e
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:52:18 +0000

    partner soon sweet card rid

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
