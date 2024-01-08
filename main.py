470cfb917c2ac251415c4a620a39340c5b5ba84e
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:18:22 +0000

    involve destroy time priority whichever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
