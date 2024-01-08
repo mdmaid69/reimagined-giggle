c90165fb61b1d4345120c590dfd645d2ae51dcd7
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:27:55 +0000

    seriously maximum great produce crowded

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
