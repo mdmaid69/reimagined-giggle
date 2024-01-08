5f3ddd523a6d516f618c70b6b6ca50c1e1000768
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:25:29 +0000

    flight tomorrow just develop comefrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
