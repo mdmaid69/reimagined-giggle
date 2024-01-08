263ff4a9af3cd5f235109587c9147a51b411f9d6
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:09:35 +0000

    note hate suit nonsense detail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
