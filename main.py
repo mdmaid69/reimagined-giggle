7b5b0f55c3e756bd08982d9028cf18a1372f372f
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:48:28 +0000

    material now same make workupto

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
