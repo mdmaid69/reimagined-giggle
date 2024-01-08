3e5007a71d480510598b5facbef1e3ae0f6b0d1b
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:30:00 +0000

    certainly development angry description tone

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
