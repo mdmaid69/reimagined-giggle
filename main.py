10c357c0cf2ba06f5ac15bf2999fd8f7b34c9af4
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:33:05 +0000

    let sensible both step print

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Words:", len(text.split()))
