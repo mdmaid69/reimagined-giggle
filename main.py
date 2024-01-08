3e8f5a5080f5693ec37a223e6449b0f62566e342
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:45:06 +0000

    means set cut away corner

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import collections
+def create_user_string():
+        return collections.UserString()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
