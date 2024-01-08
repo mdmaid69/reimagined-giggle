cfd6ed239cb8d4853902af78bf43dcb3edb63241
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:53:58 +0000

    software hard water announce breath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
