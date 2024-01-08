c95cfbe95335d31aff82708b918b135d5656cd8d
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:33:12 +0000

    pupil platform find speak guilty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
