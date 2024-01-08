32d2465b5d1b4cca465121112a999220a37b71a2
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:48:13 +0000

    circumstance manage that trip correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+x = 10
+y = 20
+print("Sum:", x + y)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
