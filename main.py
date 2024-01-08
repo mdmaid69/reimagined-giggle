2128e2a6ebae33439fbfe3837afc43fb391a6c1f
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:38:02 +0000

    climb play slide beside respect

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
