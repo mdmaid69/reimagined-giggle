fe386cd5a39a0eec4c64acf81379c2e69c82a89c
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:43:48 +0000

    behave interview tax tense apart

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
