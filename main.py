354fafd0995f9e8c63300e404a361ea4faac77c4
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:47:58 +0000

    early narrow music doctor we

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
