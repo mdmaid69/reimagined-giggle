3c619a632e921c8ad2847f0ff798549c803c504a
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:51:20 +0000

    butter equal admit sure Nn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+print("Hello, world!")
