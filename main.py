65cb82f2b463b2971f2a9b28d0fec3eafb332fc3
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:19:24 +0000

    friendly not getback concern burn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import platform
+def get_python_version():
+        return platform.python_version()
