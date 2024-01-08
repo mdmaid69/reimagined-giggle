9f93fc9efc1fe78bf9e1a50475d13abffd44d9d5
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:14:42 +0000

    man callup future look move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
