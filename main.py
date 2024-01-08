9d08a2e3195a5b1e026c0cf5bb04c3e83952f4c5
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:12:51 +0000

    block gift condition dangerous embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
