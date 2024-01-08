9f6342915088a667b62fe5813f62e06b7a5879ce
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:29:16 +0000

    possession lonely town runaway plenty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def convert_to_hex(n):
-        return hex(n)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
