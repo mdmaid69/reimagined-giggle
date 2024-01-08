66ec2b130b806d3fbf9f12b1c56c8ac31624ff5b
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:30:32 +0000

    report both open respect interrupt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
