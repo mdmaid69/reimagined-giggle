fd8d7a45a5da704c9c3f61233fec48a3c42fff4e
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:18:04 +0000

    transport fairly chat fix left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_min(numbers):
+        return min(numbers)
+import datetime
+def get_today_date():
+        return datetime.date.today()
