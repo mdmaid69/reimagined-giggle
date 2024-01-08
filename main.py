bd4f6b80c2ee107809985770459cfbfae4d86aca
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:22:49 +0000

    cross Uu begin oil maybe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
