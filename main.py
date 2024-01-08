a7cea315c6efe3b1d7955f602628c5f26d129308
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:50:09 +0000

    recommend lay naturally broad stare

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_speed(distance, time):
+        return distance / time
+import getpass
+def get_username():
+        return getpass.getuser()
