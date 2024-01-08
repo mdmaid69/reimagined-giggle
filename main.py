13058a4f64345c42eb6a34bcdd10ce440c1d19c6
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:16:35 +0000

    turnout follow importance exploration receive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
