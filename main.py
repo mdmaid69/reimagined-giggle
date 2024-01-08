ef240297c423695566d3abd3300335c9ded88f70
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:46:20 +0000

    recently illness steam excellent defeat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
