0f2858e7bbf2599cf159b76b7cdbaa1465abcf04
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:14:27 +0000

    total whistle wave single the

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def is_even(n):
+        return n % 2 == 0
