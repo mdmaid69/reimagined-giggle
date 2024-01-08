e0e750b3d87e2a6c1e4c641754d44ebf57a242ca
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:11:55 +0000

    polite recommend through telephone regional

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
