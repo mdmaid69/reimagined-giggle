856acdd94e5040665974b0217f7698476f909f36
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:23:39 +0000

    minimum twist telephone fresh belief

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
