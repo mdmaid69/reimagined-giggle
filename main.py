3f8968526d34f36f76d8c858c4564b19efa4d769
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:51:07 +0000

    swim ourselves juice minimum comeround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
