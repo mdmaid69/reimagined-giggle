b9c8850f74828533b26921a4d45ea796a3568e8d
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:43:38 +0000

    so along spray shut guilty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
