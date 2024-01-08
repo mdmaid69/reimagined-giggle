b4212602fe310d6b24bfad504edfdddf9f842eb9
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:16:07 +0000

    space pause clue winter special

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
