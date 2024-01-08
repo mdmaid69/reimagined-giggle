190c85ff08b1ade8e276bc5c9a1a60a0309bbdea
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:54:45 +0000

    meeting guard fire bet coat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
