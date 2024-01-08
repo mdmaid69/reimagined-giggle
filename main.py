d006912afab7064acd300c127382c70fcac21c43
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:28:09 +0000

    common Uu spray list stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import time
-def get_current_time():
-        return time.time()
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
