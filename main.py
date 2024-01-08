39c6d949816564843f2a02562eb141801f39c427
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:54:12 +0000

    nice able responsibility switch popular

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
