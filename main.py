7aa7a0fe8d05336d59e0fa5403abab2c49be3620
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:32:04 +0000

    holdon runinto food weak 564encourage

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
