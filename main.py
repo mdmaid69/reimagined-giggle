77af7bdde61b048533ff84b471328fd4ae4f3320
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:14:24 +0000

    already workout pair figure criticism

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_array_as_str(array):
+        return str(array)
