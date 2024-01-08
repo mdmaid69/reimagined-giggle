3737b364f40e8f443f38ab7f2d3c7f0d27d79542
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:41:59 +0000

    tool main Vv entrance practice

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_as_str(array):
+        return str(array)
