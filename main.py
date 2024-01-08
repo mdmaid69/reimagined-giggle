e04d1cbb9c23c2ccc77d0ef37581f5bdcf407373
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:20:47 +0000

    worth violent steam walk influence

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
