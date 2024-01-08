f7bc981570dd25dc50da5da89977229b3355bd32
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:47:46 +0000

    giveup after operate damage circle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-print(math.pi)
+def calculate_absolute_value(x):
+        return math.fabs(x)
