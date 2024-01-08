ea4cfb008eb0a9ce6147a3a6c379cb56846860b3
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:45:16 +0000

    used dick change coffee sad

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
