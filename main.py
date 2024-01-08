b0081ce16240bc8f137bf75e17504ac639861197
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:28:25 +0000

    market Nn book partner strike

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+print(sum(range(10)))
