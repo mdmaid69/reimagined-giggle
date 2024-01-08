da703b566c7b4d60e2575115471a63f12f777622
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:03:46 +0000

    rather tough bridge city because

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
