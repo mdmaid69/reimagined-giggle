f92c396dede8add6cd04ea24d387ca4df22c6db5
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:57:02 +0000

    bet absolutely vision region attempt

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
