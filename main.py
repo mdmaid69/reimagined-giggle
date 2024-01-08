39f3ef542a6e4e7460f8bc20574f1d1b370a6d10
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:35:37 +0000

    pay lie sail recently murder

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_volume(length, width, height):
-        return length * width * height
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
