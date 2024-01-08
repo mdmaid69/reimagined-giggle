abfe251833cefbb60dfb220357e7535bde730032
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:25:10 +0000

    sure show runover neat debt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import datetime
-print(datetime.datetime.now())
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
