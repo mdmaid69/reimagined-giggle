782057092793e214d631574096cfbbe26a1c4364
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:46:44 +0000

    fresh tongue range opposite imagination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-for i in range(5):
-        print(i)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
