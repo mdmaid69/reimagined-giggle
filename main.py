07c760b250fb55d32b2fecc4e28053b98eee7e03
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:57:00 +0000

    plan healthy lot it relative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
