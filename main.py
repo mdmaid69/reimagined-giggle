a426f8c87ed7dfc81c09cefe0fa432235453fdfc
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:13:22 +0000

    fashion famous corner bath interrupt

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
