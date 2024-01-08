8ff116c8801fafc04c2d231db255a663de93c462
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:23:27 +0000

    safety frightened stranger performance opinion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
