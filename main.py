062d5e0ceac5e306a73c26dda8ba6cc5fb69e69f
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:58:59 +0000

    pretend necessary keepoff transparent computer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_user_dict():
+        return collections.UserDict()
