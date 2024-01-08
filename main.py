27d5f6fe73bc4810423092403c08ce1c00442b99
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:22:56 +0000

    smile spring comeout vote welcome

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_volume(length, width, height):
+        return length * width * height
