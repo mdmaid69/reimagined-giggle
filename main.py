e5aff9cb5345367bddb466aabdb93b8896f639ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:52:46 +0000

    talkback complete want mass country

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
