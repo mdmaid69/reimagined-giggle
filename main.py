d3e0c3075e54a1b35d2aa969e817c25065d91810
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:15:57 +0000

    smile list hit scream perhaps

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+print(math.pi)
