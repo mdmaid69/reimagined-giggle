5c1b10899d84da9f6c58e424d26c384cdae89e39
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:54:24 +0000

    pupil hear exaggerate believe whatever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
