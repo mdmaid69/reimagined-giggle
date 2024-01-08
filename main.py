62d49ed7a86643bc1c9bd1c6b9dac0d8a1806f1d
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:47:53 +0000

    pronunciation start cheque goup roof

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import time
+def get_current_time():
+        return time.ctime()
