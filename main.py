e169f3b73cb94b135d8fae88885a42d05899fd78
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:48:30 +0000

    sharp there false underneath cause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+for i in range(5):
+        print(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
