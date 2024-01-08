0fe77ec227ea91c6016e30072e22ba6d7b247810
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:37:40 +0000

    free shelter punishment battery against

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
