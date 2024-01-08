4e06f2b7d0aa6bab915991c58a04399685b529a2
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:24:32 +0000

    hurry when storm shrink whatever

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
