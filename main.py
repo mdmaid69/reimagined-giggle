ccc144904c01c0a9ac7c8e86911661507bcee512
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:43:07 +0000

    interest congratulate violence either you

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
