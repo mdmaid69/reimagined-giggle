cc955ce56f777a260e5c1cb5b973443c5253c151
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:55:46 +0000

    blame buy eager conversation 179bite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
