c120d1bb42bb17fcf7a8e73d8406e5302576894e
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:05:17 +0000

    certain doctor gradually light welcome

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def convert_to_binary(n):
-        return bin(n)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
