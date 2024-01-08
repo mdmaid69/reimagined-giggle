449b8a1b75d266a27ffb691add6c883fd9be4831
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:25:27 +0000

    embarrassed share moon frightened port

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_pressure(force, area):
+        return force / area
+import array
+def get_array_index(array, item):
+        return array.index(item)
