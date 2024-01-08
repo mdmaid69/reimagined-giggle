6497f1cfec90f986fe4333fc16235a09953f7e4e
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:29:18 +0000

    practise ahead strike us busy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
