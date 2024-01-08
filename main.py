6478f79fa4c46d005e3a0ba2d68c5001cb55c3b1
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:17:38 +0000

    modern upstairs hand praise plenty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
