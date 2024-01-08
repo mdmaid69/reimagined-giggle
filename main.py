d88119f0431f3abce86c985c36d1237af9682cf4
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:52:05 +0000

    bin comeback strike medium urgent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def clear_array(array):
+        array *= 0
