44c768543f5ee7c1838c4b8b13721fe7aca6dc1e
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:26:01 +0000

    secret feed suck talkinto class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
