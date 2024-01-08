355a354e76ce8e628cef76953ed2e42400fe6552
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:27:25 +0000

    nonsense sense pattern almost hour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
