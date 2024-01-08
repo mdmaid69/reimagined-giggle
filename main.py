499bc879075a1b3d4aad60cd464c209ba58ff36d
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:03:39 +0000

    journey club relation level quit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_power(work, time):
+        return work / time
