298acbe63f66c531b9ccd634ba96c6bc1f78faad
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:07:14 +0000

    to murder regard than container

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
