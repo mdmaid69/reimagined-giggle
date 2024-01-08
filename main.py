ba34de3cd544b4c9af05275f935d20fae5a11900
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:36:06 +0000

    miss far born food shall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
