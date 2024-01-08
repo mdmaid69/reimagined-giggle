1ab8ef76525eca188c7e20c1b77fe624c7d67505
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:31:11 +0000

    used far laugh subject down

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
