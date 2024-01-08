e7ab36f9ca65d8ee4e5405d6dda4cc5c4b445840
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:20:06 +0000

    fly shall justice whisper enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
