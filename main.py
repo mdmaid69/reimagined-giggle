73a2e5e0712690a7bf270db6e5edc636ed5a8764
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:16:43 +0000

    yesterday dull whisper crime father

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def square_number(x):
+        return x**2
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
