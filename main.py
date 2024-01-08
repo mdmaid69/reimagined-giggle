ec674dc0a139d35c2edaf2ed49eab66b7f9487cd
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:17:08 +0000

    stop part turnover mouth wish

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
