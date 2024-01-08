96f824ef723abded8ee83c19b814cb6fdebaae04
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:49:43 +0000

    mail available square future temperature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
