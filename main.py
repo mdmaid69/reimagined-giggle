9a84a221076094217c29121aaaa8690b6d7aa988
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:36:20 +0000

    square need measure opposite crowd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def square_number(x):
+        return x**2
