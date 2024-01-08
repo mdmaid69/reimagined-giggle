4c5e94130e44e64683ba51620697c77483511a48
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:21:46 +0000

    coffee choice education sort circle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import platform
-def get_os_info():
-        return platform.uname()
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import math
+print(math.pi)
