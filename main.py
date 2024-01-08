1aeffcfd352f973ced2a929e7044afb2021dc566
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:21:16 +0000

    demand which rain whisper dear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
