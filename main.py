30fb69dc0d33d67416f37e887ab8db15b57a9cd0
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:05:45 +0000

    smooth Mm mess lock produce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
