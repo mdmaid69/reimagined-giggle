c96465f1cc5c013e09db1cc5f52d12b96cae3dfe
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:50:45 +0000

    claim goout but model effect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
