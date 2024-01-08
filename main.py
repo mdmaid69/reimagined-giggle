c05f89b5d3e6061d69f24b90bdaba90429c7a5e5
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:54:40 +0000

    limit basic national hope bear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
