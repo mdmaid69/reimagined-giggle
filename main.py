660546f2153c3a13478004ef8f3f0c3385961af9
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:26:56 +0000

    behaviour our excuse paper Ll

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
