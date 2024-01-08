b4653a5709aa561ec8322ec865b3fdd201023270
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:53:42 +0000

    season police if past total

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def append_to_array(array, item):
+        array.append(item)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
