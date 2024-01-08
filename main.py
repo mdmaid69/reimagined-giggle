80381c06b532bbf8c99ab754b4552808ac195955
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:33:46 +0000

    sure cum condition clean raw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_typecode(array):
+        return array.typecode
