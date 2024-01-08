309c02f502d834e7f9e51e04bbf0d898a41a7933
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:32:17 +0000

    accidentally comeover later nerve evidence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_current_directory():
+        return os.getcwd()
