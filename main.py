ad2e6c911070922d89ab6cfa77f59336cafaa981
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:38:28 +0000

    pause care limit advantage myself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
