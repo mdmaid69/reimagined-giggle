176aace64ccc1189d5f9e0ffe5b4d57fb28b6d23
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:09:31 +0000

    shock peaceful simple comfortable anyone

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
