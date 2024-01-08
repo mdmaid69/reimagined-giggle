7d6cdac447edc65e867ae2b3db9f7c0f766fffb2
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:14:32 +0000

    memory slow clothes port except

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
