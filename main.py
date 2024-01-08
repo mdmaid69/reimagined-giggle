b02c9ff750a9526922b2c19601aff3f0e29a1c97
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:14:36 +0000

    funny yourself anywayanyhow address guard

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+for i in range(10): print(i)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
