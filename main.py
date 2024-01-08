2bc413edd52bd3f9e8e185c0ed92f45d05aebec0
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:53:28 +0000

    effort height expensive fault then

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def divide_numbers(x, y):
-        return x / y
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
