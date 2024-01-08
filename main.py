5d60c2e7245c7d9c1d06ce50d0d188d77069b42a
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:32:59 +0000

    workon runover age realize and

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
