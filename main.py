97cbc40bac3f3b7222c7bfe20ef6a08045b106a2
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:29:41 +0000

    else disgusting now still advice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import getpass
+def get_username():
+        return getpass.getuser()
