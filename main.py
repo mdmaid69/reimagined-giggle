cde72e00f2cc41718c9372d8551b964af10d0c1b
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:13:51 +0000

    female receive takeback drop clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_density(mass, volume):
+        return mass / volume
