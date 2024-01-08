15b4da806fd35cc99d3a9501642d81de0546ab34
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:36:26 +0000

    expression watch wait street start

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+text = "Hello, world!"
+print("Characters:", len(text))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
