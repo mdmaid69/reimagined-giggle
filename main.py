6be668f064428696b7d0f0476e60f2905a1e40b1
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:23:10 +0000

    expensive improve delight hot good

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
