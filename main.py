05e819d52cff25f3bad2e0c7f7ebefe9eab2d1d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:40:53 +0000

    scare ground comeout mend remark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def sort_list(lst):
+        return sorted(lst)
