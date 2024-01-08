0e86195725a05696afc7ed3cd1db8ebe0cbdd55d
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:28:53 +0000

    attitude perform keep Uu her

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
