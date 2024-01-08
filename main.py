5a2f610bcdf9c74ba084f997b5a447c5a6b405cf
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:21:34 +0000

    still badly cross they regional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
