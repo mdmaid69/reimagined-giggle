1c19426a30a1ac46874694e73c7e466448ebb35b
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:11:44 +0000

    century slice hope state defend

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-print(math.pi)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
