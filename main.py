f710cf2e6f3a97d1b3f58d0bad77cd16eeaf3074
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:02:14 +0000

    describe favourite loss for little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
