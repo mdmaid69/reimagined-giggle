a15de40c59cab0e38a28f05f6301eaae2e4a7a64
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:51:28 +0000

    keen dead regional condition goodbye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def set_array_item(array, i, item):
+        array[i] = item
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
