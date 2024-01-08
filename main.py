eb81133b78cf19c0bdec731ccce4baae42695f11
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:20:57 +0000

    prove tonight label illness save

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
