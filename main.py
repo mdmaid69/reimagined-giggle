9861dbab5f64a761703948dc0ae2fda847a0903e
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:19:56 +0000

    male communication discovery buy terrible

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import sys
-def exit_program():
-        sys.exit()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
