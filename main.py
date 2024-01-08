3b676564dcb0e9b4a7dccb3dc95cdbb3f95c8840
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:16:35 +0000

    wave hole drop calm trick

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
