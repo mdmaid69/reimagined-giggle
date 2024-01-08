7afdb6f1eea06e08c924c1830e90ed607ac37744
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:54:49 +0000

    no interview polite range vote

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
