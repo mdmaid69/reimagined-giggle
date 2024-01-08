25f7ae52407cfeb4a0c81b88555b3c60d7b2c7aa
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:47:23 +0000

    opposite nature off around jump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
