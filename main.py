d0b6fa120ac8f37bdeadd5ae461228ca23c1550d
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:14:33 +0000

    perfect suckme what sleep noone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
