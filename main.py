7be49f5bc4b2869c518de95abb4e62cc5f50fc02
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:32:27 +0000

    bridge figure entry survive behaviour

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
