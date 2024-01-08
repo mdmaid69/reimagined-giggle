941eb5a5e4a6027eb078745ddba8561ce138250c
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:42:47 +0000

    age desperate after reliable show

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
