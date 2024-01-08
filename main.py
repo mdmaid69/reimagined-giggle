463a62cc96fe3e21f4e203e13d845a9021a25043
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:42:28 +0000

    mail march correct hide equipment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
