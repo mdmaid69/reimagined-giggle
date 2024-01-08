cd8f68790c5345e160e7e6bce49f224f7c8b9b09
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:23:25 +0000

    consist suppose building impressive owe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_force(mass, acceleration):
+        return mass * acceleration
