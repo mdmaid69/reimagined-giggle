ff1a89d64280cfc076d30ea1223951da7b70823f
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:44:31 +0000

    land strength both geton science

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
