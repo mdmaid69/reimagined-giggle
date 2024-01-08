e3bc40a93121d0b61443febf9ceaf5357fe38c2f
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:30:48 +0000

    trap turn complete practical show

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
