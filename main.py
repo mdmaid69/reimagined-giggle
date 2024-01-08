fa9605737f71c5b3178f2b791538a9cbeaf378a8
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:38:40 +0000

    live mine exploration many opposite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+for i in range(10): print(i)
