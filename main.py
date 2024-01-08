b7be08b8068e3c25e7b4112ff1e26c896e9052fa
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:06:33 +0000

    develop hers explanation through man

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
