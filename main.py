3e5a0c577ed7f3474e8c9a8e4164a79912362f09
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:27:45 +0000

    interesting ignore floor beyond accident

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
