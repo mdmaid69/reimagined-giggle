38353f6e8845f4bf9c07ff160f64d79bee06313b
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:11:55 +0000

    announce forgive jump load under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def reverse_string(s):
+        return s[::-1]
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
