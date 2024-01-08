f2841bda036e0deb84f0f78d6a29da49c9ac23dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:53:04 +0000

    normal now dig human chase

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def get_array_as_set(array):
+        return set(array)
