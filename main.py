79f408e4cefdbd84c83fe2ba812ba270587ec8f8
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:42:33 +0000

    accidental while middle hear report

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+print(sum(range(10)))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
