a46faf33a92b8e8c58f04abaa3b9d8f072cf55bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:10:30 +0000

    lay convince instead on among

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
