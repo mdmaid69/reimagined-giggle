32247fef4f32dd43d12a375a99d151f19bd1ea27
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:34:55 +0000

    brain knock excellent confused force

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def convert_to_octal(n):
-        return oct(n)
+def subtract_numbers(x, y):
+        return x - y
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
