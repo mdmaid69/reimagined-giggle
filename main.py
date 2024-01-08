451167423165764e1e1678f719e9a06ddd8eccd4
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:36:16 +0000

    detail no note turnout turninto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+text = "Hello, world!"
+print("Reversed:", text[::-1])
