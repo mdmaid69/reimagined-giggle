d98f4295ea047055d6322ed3b571c32c41043480
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:38:16 +0000

    trap comefrom software knowledge thinkover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
