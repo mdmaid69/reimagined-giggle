f12e5cce3b053d02e501d9d9e5c543bb7559eb8d
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:21:30 +0000

    century effect whichever sail 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import time
+def get_current_time():
+        return time.time()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
