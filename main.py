64d8487715eda10195af193d98ed00d3dfdb5612
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:34:34 +0000

    advise surprised advantage comeback house

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
