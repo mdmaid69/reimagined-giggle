eec50d458d53b191af9c696de610c0d2cf6d3b45
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:41:33 +0000

    warn sorry niggaacademy foreign back

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_volume(length, width, height):
+        return length * width * height
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
