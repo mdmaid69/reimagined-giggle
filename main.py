e9b8bcc1d44f5142898750765fd23bcce0634afe
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:49:28 +0000

    improve sharp description head emergency

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_exponential(x):
+        return math.exp(x)
