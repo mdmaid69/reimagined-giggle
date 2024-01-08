e603f52225b8f619fa892aff1f1a0cde654edcd7
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:26:08 +0000

    whoever quiet fear work correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_pressure(force, area):
+        return force / area
