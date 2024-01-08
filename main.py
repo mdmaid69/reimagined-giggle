20607d55f2519d321b196e2b857600a2a5d96ae7
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:16:09 +0000

    fine favourite legal instant pull

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
