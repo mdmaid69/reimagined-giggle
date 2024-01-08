64eda53c7badcefe783a5a1cbcd8f336d134e262
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:02:00 +0000

    common unless against turnover progress

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def add_numbers(a, b):
-        return a + b
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import tensorflow as tf
+print(tf.__version__)
