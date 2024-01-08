23fd1c21d4d6ed5606c2d98a7920b64a773aa7ad
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:03:10 +0000

    may satisfactory above small false

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
