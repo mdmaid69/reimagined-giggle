004ad9881be5236907bd73e4b08db027153f1bb7
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:00:30 +0000

    various throw bag minute welcome

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
