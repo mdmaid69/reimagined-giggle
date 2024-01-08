8b022e390c0e2fb892f55be1faceb9d0af8b2a00
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:36:28 +0000

    fall underneath year design definite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
   import os
-  def get_file_blocks_allocated(file_name):
+  def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+def calculate_work(force, distance):
+        return force * distance
