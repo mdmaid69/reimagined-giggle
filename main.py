f3d995607932ab251cec4da318975c2946e175b0
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:03:10 +0000

    card office along bread rain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
