b2bad8e9b881d7f861fb7da7b63f703b50a6e69a
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:09:51 +0000

    shut always average arrangement poor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
