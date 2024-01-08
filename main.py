a956d9d7ab0efa8d4396494b1752b1b449bda9bc
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:38:30 +0000

    feel no relax house personal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import sys
+print(sys.version)
