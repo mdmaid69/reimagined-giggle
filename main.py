39e001620086d584e0802c8cb85620f0f6dc8b93
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:41:15 +0000

    interested left swim back constantly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
