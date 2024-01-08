e79b3aee260a6a3f201f105b90835850da56818c
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:29:00 +0000

    clear organization takeaway star each

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
