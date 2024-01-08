44469ba10f4d414fdcbb50847efe010870f1e4cb
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:59:02 +0000

    yes your fetch sort breakdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
