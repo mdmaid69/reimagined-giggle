45859c827d88a66b1c7cadb17a266543057dcac9
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:50:23 +0000

    law stare deserve bringforward tape

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def cube_number(x):
+        return x**3
+def calculate_density(mass, volume):
+        return mass / volume
