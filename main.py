951505a0d58f1c9b2afb3456e7f914f7d53ad88c
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:49:45 +0000

    person sail public correction contrast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
