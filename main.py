cc5bde9085af0bc64f035b6cf7b13a7cf47cbada
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:22:44 +0000

    whistle manage dream keepaway big

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sys
+def exit_program():
+        sys.exit()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
