2b203d87b9b8d7630d04bb3d6062448aa6868040
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:57:16 +0000

    fresh crash kick if break

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
