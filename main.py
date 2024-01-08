679b9dbdd86eca0fdcd1d04d26f987008c5c6a75
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:05:38 +0000

    lesson left off test trust

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
