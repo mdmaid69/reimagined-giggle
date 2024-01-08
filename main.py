831a20fa4835e2c82cd07951760d3587053be330
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:11:14 +0000

    search breakinto today lost public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
