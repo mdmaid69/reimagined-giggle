3813e8283ef785bcda9d9c194d77922bb01b6efe
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:40:39 +0000

    mess something whistle past bottom

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+name = "Python"
+print("Hello,", name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
