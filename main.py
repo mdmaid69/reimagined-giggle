656e22ce7c9cdee53492df693d831a418cbe4c6a
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:10:38 +0000

    solve turnback contain minute no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
