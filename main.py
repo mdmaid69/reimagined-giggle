f588e920346170189a08e538e59d6d5c29c72223
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:28:26 +0000

    kind naturally teach cope deaf

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import sys
-def exit_program():
-        sys.exit()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def add_numbers(x, y):
+        return x + y
