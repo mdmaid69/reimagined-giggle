b8c74db0007ef63298f3a0572eb3d6be431206c6
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:43:39 +0000

    mail extra pocket arrange dare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
