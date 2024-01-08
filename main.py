84d91889eecade10e4bdb3711b44b776603c84fc
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:49:45 +0000

    even comeround sometimes contents fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
