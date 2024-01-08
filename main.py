d76a12cb292c4bc4afd6ca968a0a5591b87860f4
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:02:29 +0000

    space afternoon damp favourite progress

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def convert_to_binary(n):
+        return bin(n)
