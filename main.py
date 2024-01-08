0b0549c3d7c7e1dda28cae258f5a6e2a93bd7d49
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:34:28 +0000

    indeed shout suppose lost dance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
