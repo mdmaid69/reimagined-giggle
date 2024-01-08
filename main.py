13578a53cef7770bdec185f256456105e568de11
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:55:09 +0000

    long crash doctor some bet

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
