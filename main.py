203f5246e1bd5301fcc93b62637d6d00d844c636
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:53:37 +0000

    or demand but flow past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
