0b5f8c496a18469207ad0d6f88d1b9de10d5f9dd
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:24:55 +0000

    period whistle thank settle interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
