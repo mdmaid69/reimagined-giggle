7a1253dc8926aac85c9e16ce0b6c7153359ce9bc
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:35:55 +0000

    family wing yet watch hold

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def reverse_string(s):
+        return s[::-1]
