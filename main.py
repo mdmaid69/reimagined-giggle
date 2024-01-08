c65cd323dae289b3621e8d3f4d572ff1e27ee8e2
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:35:28 +0000

    because bye spin sudden clearly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
