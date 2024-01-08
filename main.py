df8a4b2cd20491fd0c86f99c7c30deb36c277e6f
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:33:11 +0000

    wait fault build any expression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def get_array_as_list(array):
+        return list(array)
