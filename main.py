9d3fd0fb7760c2d696de809f703fc9b11d68b876
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:12:00 +0000

    situation at sign division impressive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
