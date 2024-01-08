9c96700aff755b238fe7f9e173622a56e19bf97d
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:58:00 +0000

    stamp suggestion Qq region available

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_min(numbers):
+        return min(numbers)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
