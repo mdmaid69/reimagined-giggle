31d0398dbe750617077564b05f609e9cbbe23c1c
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:48:53 +0000

    operation sail content light human

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-print([x**2 for x in range(10)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
