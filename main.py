040188631b37a978d43e661c5b39cbaa6bf63967
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:16:39 +0000

    original ask remain clean recognize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def divide_numbers(x, y):
+        return x / y
