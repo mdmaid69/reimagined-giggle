8c2e369bb4b9b11a19a4efe8136172e635e51287
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:07:45 +0000

    round rid wild coat declare

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_current_directory():
+        return os.getcwd()
