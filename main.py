6f2385ef268ca58dbe32aa3baa997cbe44a53e91
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:17:13 +0000

    workupto high OKorokay turn perfect

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import re
+print(re.match("h.*o", "hello world"))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
