e250dc83edce04dd5c053d4d4ea6f1376827a3cb
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:57:34 +0000

    deserve tie talkback happy chat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
