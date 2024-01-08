ecb8af09381c1566c743cfd9651fcf379bc4dc5a
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:15:50 +0000

    Nn task along smell build

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
