a04a54b1908ef1607f224bb7ef00c70665347289
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:00:38 +0000

    gather round range quit afford

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
