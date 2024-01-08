aa89e9be86721340ceec3ac7cf309600a051c5a2
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:00:40 +0000

    possible tap willing quarter approve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+print(sum(range(10)))
