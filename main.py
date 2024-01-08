de2a017a9033902cda6c71c23a13ca076a2f4911
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:41:23 +0000

    rob care will section bag

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
