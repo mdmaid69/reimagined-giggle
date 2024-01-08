4a7d8cb382de73dd4007396ba78426efcd692adf
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:10:49 +0000

    willing charge tour afternoon failure

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def get_array_as_repr(array):
+        return repr(array)
