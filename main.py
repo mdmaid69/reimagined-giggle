3b039131d9debcb3e67e3f200c17ade1e993e7d4
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:54:50 +0000

    objection sleep breakup condition difficulty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def remove_from_array(array, item):
+        array.remove(item)
