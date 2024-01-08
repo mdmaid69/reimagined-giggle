8853586d5ffd1c05b22a05b5eb086db88ebbe848
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:12:08 +0000

    night behaviour sleep hospital relief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
