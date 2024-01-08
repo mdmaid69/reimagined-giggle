cbdf83448aa47cdca1853f2ee955244234786f76
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:08:10 +0000

    word release ours height believe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
 numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+print("Average:", sum(numbers) / len(numbers))
