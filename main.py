e9841ebe19edea3aa8d0cedba7594a35c29b6c55
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:46:38 +0000

    final life term personal give

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-  def add_numbers(x, y):
-        return x + y
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
