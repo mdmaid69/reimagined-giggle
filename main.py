339bfaac6dd8e4b82c0178e9c5cbf081b8aa4467
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:46:19 +0000

    niggaacademy turn bear unless represent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_current_working_directory():
+        return os.getcwd()
