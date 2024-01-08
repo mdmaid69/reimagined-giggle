d3db94489585ac3c8ce54b8f1e5729390a7f6809
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:04:44 +0000

    reply field rent within put

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
