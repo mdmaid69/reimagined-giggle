c37839b8ddbf9671d3ff9f8debdca7af5b934d4e
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:32:26 +0000

    by inside welcome appeal shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def split_path(path):
+        return os.path.split(path)
