d305e2f4ed3f1369f5c51dd2aaae81b4f1c80fb2
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:02:47 +0000

    when breath match attractive face

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
