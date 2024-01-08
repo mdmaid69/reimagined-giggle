c7a5a520957ab6e65f751ee2b48426ae846aaee1
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:54:06 +0000

    impossible motion neat near inside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
