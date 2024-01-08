6973876aa7567b941ac725c132dae25ffb9256ef
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:51:15 +0000

    squash tool colour lot spread

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-for i in range(10): print(i)
+def sort_list(lst):
+        return sorted(lst)
+  def add_numbers(x, y):
+        return x + y
