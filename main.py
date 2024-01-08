97cd7705f26675bfaaea4c64308ea661035ea329
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:03:19 +0000

    hope major crash provide I

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_speed(distance, time):
+        return distance / time
