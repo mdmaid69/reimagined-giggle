c9566d93fce864e375451e566cd16f3a9be4d7c4
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:53:53 +0000

    swimming talkinto back both study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
