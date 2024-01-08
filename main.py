8c840c8e33ed972309dcf5828c353df9445383b4
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:30:30 +0000

    deep copy floor can system

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def convert_to_octal(n):
+        return oct(n)
