e82c650340c8efa2e24e70101a127be9f3c5ecbd
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:19:11 +0000

    minor send probably medium message

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
