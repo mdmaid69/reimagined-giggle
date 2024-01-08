d9dbb1e5273fc8bd0a7d5ccc4266ec06ed7af577
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:15:47 +0000

    now mother moral lean hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
