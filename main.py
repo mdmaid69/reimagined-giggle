1971598ccfe03e882d926dcb7ffdb6da96338176
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:33:16 +0000

    score vanish fill beginning new

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+from collections import Counter
+print(Counter("hello world"))
