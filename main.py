818f9e090683c9f129396387de331d61ef65350f
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:23:04 +0000

    work suddenly bottom chat piece

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
