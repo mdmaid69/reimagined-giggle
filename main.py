ee08cf99aea549688d844873ee8edbe97f05a29b
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:11:56 +0000

    gooff remove neither mine joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def convert_to_octal(n):
+        return oct(n)
