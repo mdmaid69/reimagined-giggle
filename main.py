edb8a7d7c2265b46dae0e4997ab3eddceda6e9f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:02:46 +0000

    manage basically whichever public snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_stack():
+        return collections.deque()
