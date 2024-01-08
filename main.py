875235167801afcffccbfc46db9ca2dd1a437e27
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:03:58 +0000

    division knock consist slightly better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
