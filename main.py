ab60de808abeeed9ad3134d9b3ec4508bb3a89c6
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:48:39 +0000

    actually order afterwards waste scrap

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import collections
 def create_chain_map(*maps):
         return collections.ChainMap(*maps)
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def insert_into_array(array, i, item):
+        array.insert(i, item)
