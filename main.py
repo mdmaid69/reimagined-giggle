2194a1579167073df6ef792cbbc2295424b780dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:50:53 +0000

    now smoke stir subject could

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
