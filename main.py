495da53872c946437adee05eaed23da827485ca9
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:28:31 +0000

    cheap idea side keepaway main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
