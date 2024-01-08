786b654e34bf15db41c46c759b0ff1dfda006ffc
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:34:20 +0000

    listen permission control plan rare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  def cube_number(x):
-        return x**3
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
