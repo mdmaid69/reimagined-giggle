0f001a5fce05a81d051f71776c71130b27690f6e
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:13:20 +0000

    idea equipment face Kk independent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import collections
+def create_stack():
+        return collections.deque()
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
