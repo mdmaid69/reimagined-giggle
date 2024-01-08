66490709838278685a0889d5bbd5e59fb39d4ace
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:06:02 +0000

    foot not intelligent in thick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
