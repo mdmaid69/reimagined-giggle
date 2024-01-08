947e79a4e6539585757408e9126aef5d5ba0e5f7
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:29:00 +0000

    reason season conversation dry height

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def square_number(x):
-        return x**2
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def is_odd(n):
+        return n % 2 != 0
