8436a77cebb07d19ff080404b1e29105d51a53ba
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:44:32 +0000

    note corner criticize instruction calmly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
