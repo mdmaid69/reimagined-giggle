a3b185a6aa717e7e53c11dd7d6e207a31c96a460
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:24:26 +0000

    scream quiet long drawing deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
