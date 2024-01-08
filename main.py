7bd84e6c153fd30ba0d654be60bafa47c4006233
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:03:30 +0000

    answer possibly outside grand flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
