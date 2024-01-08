257f3c6274f04e0198a2306efa9e726eebed9a8e
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:13:31 +0000

    hair sure clean Ee butter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def find_min(lst):
-        return min(lst)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_power(work, time):
+        return work / time
