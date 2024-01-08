661826593322bf77cfc4ca22106329d0f87d6c0e
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:00:02 +0000

    possible forward interest left uncertain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
