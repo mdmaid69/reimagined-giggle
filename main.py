f0379adfd69592f96bfec8d68391fcd57c5973bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:18:53 +0000

    ourselves comment scare themselves refer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_acceleration(speed, time):
+        return speed / time
+def find_max(lst):
+        return max(lst)
