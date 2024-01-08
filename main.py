9deba616ed61a20e9b57e8682ae55e78fdb37f1f
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:13:47 +0000

    murder lead translate anything forgive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
