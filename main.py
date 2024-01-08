99105f40fb66434f4dc22397c44ea5fb008aad5d
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:45:32 +0000

    start corner hate interview run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
