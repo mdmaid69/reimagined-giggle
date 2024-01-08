1c1c703fa5c1a4f2124a07d165de15e2e8896f5d
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:42:22 +0000

    bath plenty crash prize rain

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
