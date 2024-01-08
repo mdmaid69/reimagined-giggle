b2095be5a56914623e47b9df365d5636e26946c5
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:43:55 +0000

    failure this tend present worry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
