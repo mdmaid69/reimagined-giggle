9eaf61a29af3420fefed69f6faf3d600bf50c35b
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:20:47 +0000

    fire dish exist common regularly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
