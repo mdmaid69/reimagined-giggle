51cf982da3e7c25d6387fb7dd0701efb3c494603
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:33:56 +0000

    willing fond wise instruction dinner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
