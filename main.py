89ff093f3996026a95b2446b02aba574f2006814
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:02:21 +0000

    sail demand shut light sister

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
