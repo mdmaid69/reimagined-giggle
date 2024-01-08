575cf667a552d295b1db6143b763285045983bd2
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:07:15 +0000

    appeal day so kick compete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  def add_numbers(x, y):
+        return x + y
