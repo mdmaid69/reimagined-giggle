f07d553aa588ed5e1864ecc55f7be845db61ca41
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:49:29 +0000

    gate callat against tend phone

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
