a94dd6d81025e5cee4bbe6b9c185d6e43266a6b6
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:59:42 +0000

    pull sweet after should explain

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
 import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
