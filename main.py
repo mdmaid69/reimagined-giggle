dcb16b77d8a8b64b0e2a1881f0c15fa7464e72cf
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:46:09 +0000

    beside like happy key sky

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
