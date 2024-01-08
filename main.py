26e599b25bca10144f9e0420753d3a00eb1502f0
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:36:56 +0000

    view gradually nearly hello early

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
