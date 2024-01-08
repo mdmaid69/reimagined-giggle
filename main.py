7720d82948f5dd61979496a864e0bad804ad63e1
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:48:30 +0000

    often surprised connect row thinkover

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_work(force, distance):
-        return force * distance
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def square_number(x):
+        return x**2
