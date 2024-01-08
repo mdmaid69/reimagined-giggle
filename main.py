033a979e4bc2a38e4dad9a77fb80700f6a53a0cc
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:19:42 +0000

    mixture position temperature object must

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import math
+print(math.pi)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
