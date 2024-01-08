2cbf91395c2111c4ec005cce649408ea07bb71f4
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:24:02 +0000

    short otherwise heat back hold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
