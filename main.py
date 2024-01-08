4af598faa4b0247862894add2c85088089441820
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:28:51 +0000

    flat downstairs hold score later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def find_max(lst):
+        return max(lst)
