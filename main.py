469507551245ec9aed7d54a5c4ed5f526381108b
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:32:41 +0000

    cross reserve rush earth freeze

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
