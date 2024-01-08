03d1deb0f541206fdfec4b3608de30b6b3ef1d26
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:02:27 +0000

    friend hide nonsense difference development

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+print([x**2 for x in range(10)])
