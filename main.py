fa02e773f400d5e9de9805c1a936b33da4c547aa
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:25:50 +0000

    scrape emotion far rub children

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
