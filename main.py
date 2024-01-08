056b69c5c7c6601028394625c369cd8d24faa7ab
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:07:34 +0000

    suddenly Zz harm narrow beautiful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
