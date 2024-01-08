8d57c2c66e7ad4b9f7af227e4660e7c492d67f80
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:41:10 +0000

    agreement dear flat various comeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
