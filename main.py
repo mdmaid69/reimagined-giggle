ce4725462859a3c498d1f525bfe6cfbdb53fc71a
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:27:42 +0000

    estimate round contribute that wake

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import sys
+print(sys.version)
