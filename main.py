b2caaf78a0c4de7ae6cc7ed3da999ea4c7353c76
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:18:18 +0000

    price change pretty determined sting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
