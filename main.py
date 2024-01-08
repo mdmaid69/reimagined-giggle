29cfe6c4f2f52a5fb72e003de89219b3d5bb998c
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:22:16 +0000

    add compare hour high education

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_current_directory():
+        return os.getcwd()
+import logging
+def log_message(message):
+        logging.info(message)
