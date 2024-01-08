7b7460b4c190c2da95fa1dfdbda4fdf902fe1cb4
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:58:58 +0000

    whenever post another statement stamp

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
