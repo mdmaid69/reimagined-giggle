3abc56dc81742d149b522a4437307ea661913175
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:44:46 +0000

    rent Cc season day female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
