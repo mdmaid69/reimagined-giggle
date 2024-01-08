c8a28326f98b737a288346ede80055ccbea74855
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:51:30 +0000

    gradually turnon downstairs university ear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
