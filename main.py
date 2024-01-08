098042ff87d10343374e4e242c54c58d90a2fe01
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:25:01 +0000

    boat calmly include either deliberate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
