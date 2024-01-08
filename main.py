db205bfd8c6506923ba778a88d5f44abaa046ccb
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:39:13 +0000

    deep little mind odd phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
