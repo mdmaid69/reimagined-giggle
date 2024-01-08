c9c6c0d7c18ff169cf7bda85844a76f559cabcb2
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:57:03 +0000

    talk understand fence increase tonight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_current_working_directory():
+        return os.getcwd()
