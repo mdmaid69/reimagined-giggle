dab93ef44c3199e0c7fb69d2eb3c4cbe314b3022
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:44:40 +0000

    generally table share money switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
