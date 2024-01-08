af7c28fea7e38796570bab0d83ed171ef6d85690
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:08:38 +0000

    either science advantage shout especially

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
