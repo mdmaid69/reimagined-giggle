b3d4a925a2b2a9f94ddf770322f83f8a5688eba3
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:12:51 +0000

    direct whisper insist repair afternoon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def get_array_item(array, i):
+        return array[i]
