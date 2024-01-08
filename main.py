8f956c281fc674a4134a75273c2e1e128dfd4553
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:41:29 +0000

    deep why shallow none convince

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def find_union(list1, list2):
+        return set(list1) | set(list2)
