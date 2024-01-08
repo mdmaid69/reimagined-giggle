89f747ade409ba66edd9e3355e0ca1d41ddac92c
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:18:11 +0000

    near murder scared ask later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_area(radius):
+        return 3.14 * radius * radius
