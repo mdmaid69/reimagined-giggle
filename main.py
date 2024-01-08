76cc7096ca431fce79800af47efa0772866a2e31
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:16:54 +0000

    situation 566end water news cry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def subtract_numbers(x, y):
-        return x - y
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
