1e9f72bca1368ed55471244af0bc47904ff2fa0e
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:17:22 +0000

    thank under repair pride tend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
