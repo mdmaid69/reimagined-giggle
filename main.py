a263c47905bde250eae30bbfa6a4aecb33513614
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:24:24 +0000

    exit chief broken hospital best

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
