6d12073deb0fa608e6d623226ac6a73b0299074f
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:26:18 +0000

    each loan computer after yes

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
