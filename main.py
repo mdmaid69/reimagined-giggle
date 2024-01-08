97c22eed5cdcda21c6231407bd3ed66b215aa3fd
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:56:59 +0000

    trap sick thumb clear sale

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
