b37b20ab95ed1a5f8a0e20fca45e80f080935630
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:43:46 +0000

    tend unpleasant vanish transparent angry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
