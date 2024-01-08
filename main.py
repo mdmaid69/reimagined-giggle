94026931d2c5ccb2c8dff9fd8d199779c0850d5f
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:29:44 +0000

    office wave impression blame notice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
