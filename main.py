36ce088fb4735937ef0b240840d9d20337e7c8f3
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:01:32 +0000

    complicated reduce tongue movement criticize

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
