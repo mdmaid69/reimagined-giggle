1123b9bc931795bda0826cf72d6b5636a88340cf
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:30:39 +0000

    grow average flow prize stop

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
