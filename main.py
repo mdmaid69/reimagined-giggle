203e7a19d0315047e220a206919ec6a7329c7eb2
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:15:27 +0000

    godown soft content petrol achievement

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def subtract_numbers(x, y):
+        return x - y
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
