bd32f4dfaf079e155bc9edfb1061c3f6c86604c0
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:06:20 +0000

    back giveback time stage design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def square_number(x):
+        return x**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
