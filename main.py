ce5054716233bc438cf5645291741ac66d67ebc5
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:57:50 +0000

    hole race Yy getaway forward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_area_rectangle(l, w):
+        return l * w
