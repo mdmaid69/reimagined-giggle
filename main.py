f56cd05a87e208147c4acb08e7909ca4f1761939
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:25:00 +0000

    store roof interesting attention indeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
