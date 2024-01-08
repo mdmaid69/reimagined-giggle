d6ae882c17e0a237e6f5a63e31decbef12e26e39
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:25:32 +0000

    know voice wonder straight petrol

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
