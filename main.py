6e9f120f83971ad0fb7e8c8a5e9585621d1c30a3
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:33:35 +0000

    aware innocent opinion shape compete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
