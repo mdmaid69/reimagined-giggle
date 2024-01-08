5d014b9d6e6f455167fc9b2af6f6f44bf84bbb2b
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:17:11 +0000

    advice Itself lookback(on) thinkover scared

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
