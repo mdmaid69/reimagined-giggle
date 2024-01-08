2bbaa7fb86e5235a8f56cae5baf6f9f7a7a8e02d
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:19:42 +0000

    flow wing tradition stranger yourself

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
