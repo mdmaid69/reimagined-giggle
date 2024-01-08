868d335d5bf850f47b601bca79a836fecbe8c456
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:16:51 +0000

    notice least rob across state

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
