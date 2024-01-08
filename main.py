82210154de9ecd18e32bb080bb4460bcdea37f5a
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:42:13 +0000

    command important whisper goon Uu

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
