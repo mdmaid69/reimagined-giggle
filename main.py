4cf8dca09fae3ce96d9801a4a1bb0baecb891074
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:53:22 +0000

    wild keep lookup weigh trouble

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
