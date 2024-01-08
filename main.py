f16e55c2af45da4bcf1c72ef8aa722a200d8134c
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:16:38 +0000

    child ill wet stranger also

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def subtract_numbers(x, y):
+        return x - y
