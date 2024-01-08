b340551134a38339fd47c100052075e0f1bc7687
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:39:23 +0000

    brave OKorokay squash forward room

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
