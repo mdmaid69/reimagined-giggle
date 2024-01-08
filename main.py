cbd7333d5531e8fcf25ebd9a5597b66e09d096b2
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:40:51 +0000

    pull fail gooff production week

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
