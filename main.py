1be5429fffad2c1b155272a830d5eb162c68e7c9
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:35:16 +0000

    date many everyone equal hello

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
