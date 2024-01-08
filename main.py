80df254922607f1fc0712ee91b0d4f43002a4f45
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:09:23 +0000

    mistake fail comment that whatever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
