2dcd5ad843fbce238d42df9843d2a8950aa27d44
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:26:26 +0000

    chief comeup centre achieve lay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
