1baf9bb3f013ea117cd1542bc9264999a22933ee
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:17:15 +0000

    some giveaway nor exhibition by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
