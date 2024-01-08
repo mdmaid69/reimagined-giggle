79d61327b9f89ec8b47056719e0e981487019e6b
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:32:47 +0000

    bye danger praise but hardly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
