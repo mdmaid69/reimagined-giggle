f3e16d64db6917f73249b3f46b82402ba6e415c0
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:21:27 +0000

    instant fairly seat find damp

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+for i in range(5):
+        print(i)
