fac5d09e8076ce09dd66fadec171f83cbe118c7c
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:34:25 +0000

    drive picture enthusiastic place bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
