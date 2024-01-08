fed6b9300d4313247591556509947409997c294c
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:34:01 +0000

    dance hide spill by able

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
