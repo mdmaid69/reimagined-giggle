6fdf8a1df82b7a92f4da8593dd98265c7c470750
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:53:50 +0000

    schedule evidence subject thought calloff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
