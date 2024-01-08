3710e598d68ae34e9da7a6cf5f186694f6de4afe
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:44:23 +0000

    will hollow edge sail less

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
