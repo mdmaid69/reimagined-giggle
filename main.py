9a3c48f786cf680c4783d1afd47ed77de8eb4c69
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:51:26 +0000

    load brush attach boat factory

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
