28fd3be26ae19f5410bbe1e6ad42c15110e29487
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:37:09 +0000

    talk rich image failure other

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
