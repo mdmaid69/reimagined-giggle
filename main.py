f0c7884a0a14d280df973eb6ef687e2dc6797593
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:17:16 +0000

    head like imagine best union

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-  def cube_number(x):
-        return x**3
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
