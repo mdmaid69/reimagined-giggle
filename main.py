dc2bdc83b5df05a5e9e8312d24ff0481d14678e5
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:17:18 +0000

    callat market runaway paper family

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
