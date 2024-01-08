4fa5eba22ac58e6f0f27f9a667e5dde94bf98541
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:31:45 +0000

    afternoon feature crisis shopping reduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
