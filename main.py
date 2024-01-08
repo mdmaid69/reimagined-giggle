8d0e33faee4c26edc8ddf24d1887674f5ce9bdc5
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:43:40 +0000

    disturb operate finger very defeat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-for i in range(10): print(i)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def change_working_directory(path):
+        os.chdir(path)
