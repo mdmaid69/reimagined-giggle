df447d71f0307ef8629ffcfb7aafb86b98aefe78
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:43:08 +0000

    bet thirsty dull busy centre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def calculate_pressure(force, area):
-        return force / area
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
