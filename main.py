c9edba590afd33768536e10f79b28db0798edd0e
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:24:37 +0000

    trap much influence debt declare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+print(os.getcwd())
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
