b64dba0ea4a0e12ae03209a9268c6dc49b3d5a9d
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:42:32 +0000

    in have guess judge Bb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
