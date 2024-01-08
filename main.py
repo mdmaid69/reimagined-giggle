4f3226f0cc8fe6680061f076b946fb6c3274df8b
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:23:21 +0000

    shine name callat runout soil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
