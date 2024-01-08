a04250317fdfd3fad9bd16082ededd1c40697345
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:31:04 +0000

    shadow far admire reasonable top

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
