40915eca08cb02e92492a16dcc84bad81e036ccf
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:08:16 +0000

    level left joke it last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
