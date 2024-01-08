2e3080bc25a2c1fb0cce2203d1820156dec55471
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:12:25 +0000

    occasional several comeoff that loud

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
