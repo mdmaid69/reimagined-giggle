e92c861c15fecb8514c792162d88883fafebf4f5
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:59:59 +0000

    pot trust cut debt smooth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
