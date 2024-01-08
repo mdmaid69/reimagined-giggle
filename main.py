074021aa01abeb8f1dd50a8644050abcc74fbc49
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:14:22 +0000

    dull hungry square everything accidental

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_power(work, time):
+        return work / time
