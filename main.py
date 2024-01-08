a2ad56460b8c19d4f8b84620678193ac9d02bb55
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:19:37 +0000

    against worst regularly love every

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
