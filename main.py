28f8d976d0a97857fd0e5e9d727521bfb3f2ffc5
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:52:27 +0000

    spend sensible although capital post

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def convert_to_octal(n):
+        return oct(n)
