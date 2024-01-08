5e8e6a73b504a1d796cb0f01e97e51fd514bce0b
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:01:17 +0000

    fine break behind chief lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
