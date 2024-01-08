f5744bbe9710eb74961b0a6da625a130bc7c89a8
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:33:41 +0000

    whole invite exactly time invite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
