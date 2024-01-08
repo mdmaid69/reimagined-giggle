2b431e64ab7cbf7ae3c6a0f2d2f121b2fd0d48ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:01:23 +0000

    relief pass distance far cover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
