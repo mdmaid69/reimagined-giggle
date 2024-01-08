89842d0500e1b0162f6ee22adaf951cc23d4452c
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:39:55 +0000

    guess bunch whether moral production

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def convert_to_hex(n):
+        return hex(n)
