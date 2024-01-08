fe913e873859028dfd0416abef2ea53c09977c9c
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:17:39 +0000

    expensive test mass town march

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
