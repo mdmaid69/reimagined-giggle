22542eaf1c8d5bc645da459b7b9cece78795960c
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:43:47 +0000

    accidental figure recover question Dd

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
