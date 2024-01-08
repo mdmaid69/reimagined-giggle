f399f9583f72c777ddd2e25916d0f6772746e56a
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:16:29 +0000

    while intend report situation upstairs

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
