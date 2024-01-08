72fdbf477ecd4e8feb7d08e2749a9b35676421a0
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:21:59 +0000

    worry off passenger threaten ordinary

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_as_tuple(array):
+        return tuple(array)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
