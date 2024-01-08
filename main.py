b32eefeade2e0ee0325641f8821607a3d1e7cb71
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:03:11 +0000

    little level proper environment sink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
