21fe517cf37dc7b52a3c5149d3caee7934df0395
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:12:13 +0000

    border light chance uncomfortable complicated

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
