e6009b9f5917dce648b3ce0786f6507d63fa051a
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:15:06 +0000

    shop natural minute breathe awake

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
