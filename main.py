9e7618a942edcf2f49db0b4c67074c1b8b796668
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:58:53 +0000

    closed history interrupt mark understand

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
