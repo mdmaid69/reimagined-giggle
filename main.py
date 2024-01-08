7082239b8aa82c80408afcbea927eea1971418e2
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:50:32 +0000

    single train fierce separate usually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import sys
+def print_python_version():
+        return sys.version
