fb7b4f16c3cba0cfd07d3d1f79cc82c3d98a491a
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:44:42 +0000

    tonight well us cheek event

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import time
-print(time.time())
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def reverse_list(lst):
+        return lst[::-1]
