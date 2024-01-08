8211d0dcf23edae7cc0f58ed62cfa926d276de92
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:06:29 +0000

    several respect injury fear crowded

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
