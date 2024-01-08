8e5a33b2b7d4831b8123b60054ff97145cf70f01
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:21:55 +0000

    lookback(on) to race turn various

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import re
+print(re.match("h.*o", "hello world"))
