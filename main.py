513cd6525be26887b206765ec83e8f0791a3eec7
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:50:10 +0000

    key reserve pinch surface exercise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
