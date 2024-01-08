8de4558a2bfe2835fe5620d56147610632bc70c9
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:49:32 +0000

    present race list Ss runaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
