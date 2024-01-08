5adc17d0383a61f0e278ff529445b784e6efb59f
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:37:12 +0000

    neck space big stir guilty

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_work(force, distance):
-        return force * distance
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
