13da34bef61fe954161939fc852d3025b383e19e
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:56:15 +0000

    pollution safe study maximum charge

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_sine(x):
+        return math.sin(x)
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
