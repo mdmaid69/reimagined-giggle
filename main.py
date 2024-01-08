fe157e4f6157d278c7727904ec7ff1a86049dd27
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:52:18 +0000

    suit thought zero shout otherwise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
