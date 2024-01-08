75291a5dde10a0fa1413c9ae65628f4d3352d066
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:31:04 +0000

    choice his earn direct example

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
