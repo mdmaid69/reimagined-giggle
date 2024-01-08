0d8af908d061332ea8c845a65069dc6953c6b373
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:59:18 +0000

    list correction backwards ilovedicks getback

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-name = "Python"
-print("Hello,", name)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
