5e94f4ae800e3b4ceaff425ef703dec96795188e
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:52:07 +0000

    watch birthday lie godown walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import os
+def change_working_directory(path):
+        os.chdir(path)
