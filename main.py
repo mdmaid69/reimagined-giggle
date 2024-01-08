1dc297aec4e59825a03db385412fd542c8377320
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:47:07 +0000

    never taste fall comedown worst

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def sort_numbers(numbers):
+        return sorted(numbers)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
