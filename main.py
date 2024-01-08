e34ad94ea1bb3b984a6f3416c75d63a209a43694
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:22:57 +0000

    contain cough bury either eat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
