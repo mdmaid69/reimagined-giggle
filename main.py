286fd7e7951f496e7f87fbb7e044ba7df2207183
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:07:53 +0000

    weak much active he moment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
