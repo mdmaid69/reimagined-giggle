96a65c5c5c9b9c6843077bbcb1569e70fe56bc86
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:44:09 +0000

    day education centimetre lookaround he

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import platform
+def get_os_info():
+        return platform.uname()
+  def sort_list(lst):
+        return sorted(lst)
