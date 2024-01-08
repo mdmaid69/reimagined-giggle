173078071ebe2901ff1b59762106a1ea3301c862
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:18:38 +0000

    our seem straight suckme effort

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
