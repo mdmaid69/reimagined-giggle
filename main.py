deca8212401adc145b92670c32ddf5a449e93687
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:03:26 +0000

    narrow influence punishment wire have

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
