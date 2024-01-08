87793c06354db578ae8e4a86699100f13debdf57
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:56:46 +0000

    result frightened goout might bringdown

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def delete_file(file_name):
+        os.remove(file_name)
