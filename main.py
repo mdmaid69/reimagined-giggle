8ef5a58d8ebd10b85bc63e9784bfa4e7b52af40e
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:56:42 +0000

    hold plenty slow consider similar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
