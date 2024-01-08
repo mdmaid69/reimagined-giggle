52e476b8d5b427e07e2d618c348ec44bd0064039
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:35:20 +0000

    cruelty central would design disappointed

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
