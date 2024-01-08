0b6f979ee6abbea060491802c69e964975ce27fb
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:49:07 +0000

    giveback born roof down sore

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def remove_duplicates(lst):
+        return list(set(lst))
