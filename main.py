a9eb3619398c9f2bc31a39f25ea00d5132e645a4
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:20:04 +0000

    really thorough tense system sometimes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-print(math.pi)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def reverse_array(array):
+        array.reverse()
