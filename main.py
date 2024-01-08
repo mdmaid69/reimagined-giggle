3eab17c8bba8fc35a898ccfc42d1d4c384e39291
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:27:52 +0000

    care instant bit he Itself

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
