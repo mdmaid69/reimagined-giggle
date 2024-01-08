57f19871938c2253e990656037ec43b303e1ed15
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:48:33 +0000

    power below put generally capital

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-print([x**2 for x in range(10)])
+import math
+def calculate_sine(x):
+        return math.sin(x)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
