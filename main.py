ed5dcc25514ba30e8bbeb6adca42e607dcaa24c6
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:53:24 +0000

    school discuss entrance manner wide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
