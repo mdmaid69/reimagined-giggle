e1cd6b361bee5b9541b8dc58d813d8dece8398d8
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:04:29 +0000

    top ring card suppose edge

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
