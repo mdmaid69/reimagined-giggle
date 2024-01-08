2a4600093f8f76feadbb3559a1320f8d2b02accd
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:40:45 +0000

    either towards space adjective book

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
