107bbe2215bec9407fc77ab993fecd29c27e13a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:38:55 +0000

    unlucky lunch rid interview content

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
