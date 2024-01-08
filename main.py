94ba778c2ae8859adef6d3e0a506b9a37e3af343
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:40:35 +0000

    sure tool Qq judge live

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def add_numbers(x, y):
-        return x + y
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
