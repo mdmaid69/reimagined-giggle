40f9c9afcdaa5a17f53b60c0a542b8e48e86ebc6
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:18:25 +0000

    willing through press opposite ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import numpy as np
+print(np.array([1, 2, 3]))
