f1b671480c4b6d0a1f808fdc7a44a69de5068b2e
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:20:45 +0000

    grateful again badly estimate rent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def sort_list(lst):
+        return sorted(lst)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
