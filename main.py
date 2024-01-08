c7263baf7f18f06b258b5e1c4cf1784e644d872b
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:10:57 +0000

    Itself work happy closely experiment

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_as_tuple(array):
+        return tuple(array)
