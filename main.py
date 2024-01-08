12af78613f164d977a094bf56228341292becd59
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:59:06 +0000

    shape part definite cap again

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import sys
-print(sys.version)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_floor(x):
+        return math.floor(x)
