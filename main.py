6c6f589269ae8b89388d10b40c0de84d48ed8b12
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:02:12 +0000

    likely work deep compare hair

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
