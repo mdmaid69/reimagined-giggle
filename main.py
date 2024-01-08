477f74451934995d9e991714f8299cb9734a2b7e
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:03:47 +0000

    exist region command exist love

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
