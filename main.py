67deaf48888156eb17786ca73277f848d2620e53
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:34:36 +0000

    advise eye possibly any sink

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
