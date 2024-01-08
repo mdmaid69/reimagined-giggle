971edf4ff451e30e5685ac609b0056927db4dcb7
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:37:25 +0000

    suppose close breathe study crisis

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
