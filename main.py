5e200ac74dc4c001fb05c9721ed336f49e4d9680
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:21:33 +0000

    store opportunity behind somehow destroy

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_work(force, distance):
+        return force * distance
