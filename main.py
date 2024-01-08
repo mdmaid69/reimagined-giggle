7ae576767a51cdd1ff4e93b77927f7eb076eb3cc
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:10:34 +0000

    turnon repair keepaway succeed worry

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
