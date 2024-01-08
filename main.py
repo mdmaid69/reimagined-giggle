6ba342b5f84828c271bd235f135a155316dd6409
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:21:45 +0000

    corner determined suggest drink next

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
