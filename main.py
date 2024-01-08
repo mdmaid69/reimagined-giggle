02c6d0008f5a79a543c28438eed507bcddd55dab
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:01:25 +0000

    male shadow pile common cruel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
