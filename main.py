c80cb953fb357edf1a81daeacd10c7cc51072a29
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:20:01 +0000

    bound protest passenger will smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
