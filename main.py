c112131e11527ae0981d7c752c41e60067737f13
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:53:10 +0000

    travel wipe ours oil free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
