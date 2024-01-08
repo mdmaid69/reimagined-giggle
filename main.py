ba3f80635a0663c277d9b6d820d4d0978b105416
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:43:29 +0000

    strong deliberately immediate family low

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-  def reverse_list(lst):
-        return lst[::-1]
+def remove_duplicates(lst):
+        return list(set(lst))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
