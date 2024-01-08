c072d93b52aec938d6f3a127f6c92785e77d6b80
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:19:46 +0000

    entry Zz seat Oo see

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
