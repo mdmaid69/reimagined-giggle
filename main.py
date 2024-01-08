f564aaad9ca5d1c74999a2b0bf1da5b2aa967915
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:52:55 +0000

    remark actually yourself kill noone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
