c1bb40bf292f4dc7aaf0926b24dd5f46a371d38e
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:13:14 +0000

    respect surprised that sometimes recover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
