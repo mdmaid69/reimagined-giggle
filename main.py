233fc66cc808526d52e8063b861b6ca8de08943f
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:03:46 +0000

    shake address any believe wonder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
