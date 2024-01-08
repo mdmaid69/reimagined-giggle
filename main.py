e2980926b823d11802cdb5bfa9fb071c416452fa
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:31:28 +0000

    whom moral hope round female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
