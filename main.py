2e5097ebe6f7bd1c31c4a2f574b56992fcba18e7
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:46:26 +0000

    dig discover I smooth after

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def count_elements(lst):
-        return len(lst)
+  def convert_to_hex(n):
+        return hex(n)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
