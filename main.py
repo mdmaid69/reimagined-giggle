7e60054b91cdd9b452d304eb278eaf6c8d594a80
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:52:39 +0000

    compete uncertain lead though wash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def add_numbers(x, y):
+        return x + y
