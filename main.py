237098d63a57173a7746acfff3b704634c073e8a
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:53:29 +0000

    represent moral attention inform field

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-for i in range(5):
-        print(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_length(array):
+        return len(array)
