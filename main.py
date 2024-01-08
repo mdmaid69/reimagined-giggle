8cc7e4860895d57432d48061468cce81ca2f3717
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:13:45 +0000

    dress debt rate neither wish

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
