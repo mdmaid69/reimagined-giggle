7950dcb5a1728bbd3296de1d36a2e1991614c050
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:53:06 +0000

    thorough shoulder move light care

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
