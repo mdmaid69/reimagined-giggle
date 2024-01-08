50179f916dc9e2bc35447e1d851dc25f5f152a62
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:34:31 +0000

    sing contain minimum spring must

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import sys
-  def get_python_version():
-        return sys.version
+def is_odd(n):
+        return n % 2 != 0
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
