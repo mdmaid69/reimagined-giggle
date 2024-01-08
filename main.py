5cd6697b4c94f4c38bdbb3b816ee7f98a0ee5c50
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:37:15 +0000

    treat payment Ww bread quiet

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_work(force, distance):
+        return force * distance
