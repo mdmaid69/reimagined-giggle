3f59818a553347c871f2f474cd5bef78fb6580bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:12:27 +0000

    guard difficulty dig fire wind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
