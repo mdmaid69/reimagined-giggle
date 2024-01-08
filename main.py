b3fafd77f582fbca86c20b0cb39bfdd5eefcf2d8
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:11:53 +0000

    foreign body history look along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
