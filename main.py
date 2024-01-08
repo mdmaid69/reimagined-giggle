26588c188ebd770ebd584f098237e3ee89f38653
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:52:34 +0000

    inquire ring well negative approve

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
