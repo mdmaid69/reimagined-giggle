ff5dc97cca009331e6260003d48243f3d5edde79
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:22:04 +0000

    complain strict table pleasure beyond

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_work(force, distance):
+        return force * distance
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
