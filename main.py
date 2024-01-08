92cb71624c7a55491e295ed9e63294e49342eae3
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:02:24 +0000

    remember instead total subject fortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
