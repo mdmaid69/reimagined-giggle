69b3d2e441b2f08e153e44c77c2ef8715f9730a3
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:17:00 +0000

    paper complicated Ee idea scared

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
