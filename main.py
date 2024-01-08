4f3172f1d777fa71d0ee8e3c9de671cc3f72a3b1
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:06:37 +0000

    dig community lot otherwise answer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
