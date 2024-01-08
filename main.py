abe084b23277b1ed52e7d8d20fc459459f6a1003
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:58:31 +0000

    fetch ilovedicks behind book mood

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-name = "Python"
-print("Hello,", name)
-def is_even(n):
-        return n % 2 == 0
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
