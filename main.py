a1fabd851c6fb68e0d0a294c0385d9fe46490e11
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:20:51 +0000

    artificial neat know scrap uniform

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def multiply_numbers(x, y):
-        return x * y
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
