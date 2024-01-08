51fe9bd2a927eaa4dc3ed8979eb45942e0f7bd64
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:33:43 +0000

    anyone visit splash nervous lead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
