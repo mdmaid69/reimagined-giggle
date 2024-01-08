77df7091ccb4cbb34b8b146f7e1c54bdef5cc3a1
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:59:23 +0000

    comedown calmly bet unlucky real

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def square_number(x):
+        return x**2
