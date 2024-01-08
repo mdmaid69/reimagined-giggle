f2a54f76945c8dfa24d08eccbf334bfd0e7422b7
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:05:39 +0000

    deaf either us next sensitive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
