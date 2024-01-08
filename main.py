bb30cc8ec200d3920e910c4fd420b69543f621ce
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:43:29 +0000

    runout sure correction discussion wrong

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import numpy as np
-print(np.array([1, 2, 3]))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
