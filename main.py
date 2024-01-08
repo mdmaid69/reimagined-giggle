ceb8846e25bb90b609c24df44cc795e2ce93adf6
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:57:47 +0000

    brother sleep share than left

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+from collections import Counter
+print(Counter("hello world"))
+  def calculate_area_rectangle(l, w):
+        return l * w
