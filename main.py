edfe331905f3894940774fc77e3e35812c5d1446
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:23:37 +0000

    bringforward compare night today middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
