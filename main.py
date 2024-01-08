98253d1b6509126b5b0f873290759e258d315e5a
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:19:28 +0000

    soon unkind look twist exercise

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
