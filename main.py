458be07cfaab39602d66996f96eb76ddfed1c27a
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:38:25 +0000

    class sticky excellent little entry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+text = "Hello, world!"
+print("Words:", len(text.split()))
