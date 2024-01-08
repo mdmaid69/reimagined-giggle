637d3e8554aa182dd348c7c3fb0e6da8aaf6d787
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:26:37 +0000

    prefer definite final so work

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
