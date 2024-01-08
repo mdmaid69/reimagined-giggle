69cf410b6fadbc5678508c0d344c30c502329575
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:52:11 +0000

    tone takeoff business pride getaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
