f1e37c60f9fe75fbd00967767ded09ce6f6d59c3
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:27:33 +0000

    understand head generally accordingto next

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
