2085520e832fe6851cb66579655107830c0824ee
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:10:39 +0000

    result walk someone switch target

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
