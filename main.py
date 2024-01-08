fafc93f0934756d2f8ad5f153d5ee334caf7565a
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:41:23 +0000

    computer circle somebody myself sick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
