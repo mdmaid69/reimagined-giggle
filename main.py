b5394d4a293c6f6409d57e4a32b338a2f98845b9
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:19:11 +0000

    congratulations taste letter bath accidentally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
