dfe944a7e31845e0a184db7d797bd478cb315fa4
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:33:28 +0000

    firm mail 567enemy means information

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
