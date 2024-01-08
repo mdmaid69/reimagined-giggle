374f376bda228d69ab2c8d8f3d4358313f39f481
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:13:10 +0000

    dear delicate strong Qq right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_item(array, i):
+        return array[i]
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
