389c89f08113ce001683ff8c089e6c3bda59aee0
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:03:41 +0000

    collect sticky correction trip congratulations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def get_list_from_array(array):
+        return array.tolist()
