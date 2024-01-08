681d659eb14844eef35d26924c20c2f69c05508b
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:09:20 +0000

    further pattern hold offer laugh

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
