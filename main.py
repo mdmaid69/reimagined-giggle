98e4598a308426a741e8a2b44d8bf88fe0c73404
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:35:43 +0000

    cook weather off crash recent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
