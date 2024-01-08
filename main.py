f189bf6b12c5061a3ea82825e60bef08e917ffc5
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:09:10 +0000

    out contrast colour experiment blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
