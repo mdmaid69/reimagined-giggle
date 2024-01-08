a86a6e30c2c50568c9a75ed5986ecff70896f111
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:08:37 +0000

    some turninto shall male summer

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def cube_number(x):
-        return x**3
+def calculate_volume(length, width, height):
+        return length * width * height
+def is_odd(n):
+        return n % 2 != 0
