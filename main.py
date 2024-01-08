077c22c1ff11d85ca97f51e221325c8c5aafd830
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:46:46 +0000

    excited feed forever visit hardly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
