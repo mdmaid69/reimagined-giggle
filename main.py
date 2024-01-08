cc491de01c0ca9a3bb2ccbb15f8038c6ee900635
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:01:41 +0000

    lookafter ground dinner worst ashamed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
