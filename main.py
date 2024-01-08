a25ee42270f88177b2162123cd0a65756458c42e
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:04:22 +0000

    Pp rain perform state stranger

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
