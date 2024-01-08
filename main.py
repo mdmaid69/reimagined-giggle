505868e899f3a52db4bee7098633ccd9cc2281c5
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:30:50 +0000

    correction arrival rain several holdout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
