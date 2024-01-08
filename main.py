af335014f2dce1637ae74abb223f841c1e1583b6
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:08:45 +0000

    nearly taste strong doctor new

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import json
+def convert_to_json(data):
+        return json.dumps(data)
