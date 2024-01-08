50dd4fd1d9d907e599db4cd78e584879a120c3d1
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:10:24 +0000

    proud course by Dd perhaps

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
