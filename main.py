eb0bc92aa01b2a2d8577e317a3c2e9bcca8c4171
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:36:32 +0000

    quit frame pay swing tap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
