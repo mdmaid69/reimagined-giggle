4f94d1df92e6a7af4e3fe36394d4002af151903c
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:44:27 +0000

    sort gate nervous form time

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+name = "Python"
+print("Hello,", name)
+def sort_numbers(numbers):
+        return sorted(numbers)
