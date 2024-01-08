3d4ae641d0887a60bff6f3362f03dc8bbbf9bb2c
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:34:08 +0000

    afternoon mine joke inside clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_current_directory():
+        return os.getcwd()
