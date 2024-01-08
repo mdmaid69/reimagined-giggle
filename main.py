8815a45b1294b157c9d7037a9ea58fbda9868276
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:54:44 +0000

    bite lucky weekend Ww of

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
