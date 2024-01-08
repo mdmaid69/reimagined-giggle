7a807a4d23018b439b577550c0d509523e934acb
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:12:55 +0000

    giveup ability pause character difference

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
