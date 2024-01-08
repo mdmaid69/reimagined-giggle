f598579b3f8f6b824c9b6b195c3eb850c011aaa9
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:51:05 +0000

    turnon lesson cost string area

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import datetime
+def get_today_date():
+        return datetime.date.today()
