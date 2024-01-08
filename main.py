5c43ee9bc5a04a78b0c70d7a6d3b749e4f0e2256
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:41:27 +0000

    yes land exchange shut knowledge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_base_name(path):
+        return os.path.basename(path)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
