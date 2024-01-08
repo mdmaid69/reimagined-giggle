5823268f01e4261aee86ccdcb51aa79555705495
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:24:01 +0000

    below anything appeal expect accident

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def reverse_list(lst):
+        return lst[::-1]
+def add_numbers(a, b):
+        return a + b
