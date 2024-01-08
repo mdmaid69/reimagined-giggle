37228758cbabe9205ff9ace9a4e103af49af6027
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:57:51 +0000

    storm fight underneath trust holdup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
