408beabd2d32042578f30a8dca20c51071dd9a6e
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:46:03 +0000

    Tt introduction for hear protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
