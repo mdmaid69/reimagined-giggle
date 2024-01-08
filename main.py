11a7fea8430025491de5a2f5a5a45a2383bf15df
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:42:04 +0000

    comeround book callback determined win

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
