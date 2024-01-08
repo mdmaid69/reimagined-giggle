f5ab2b6761381820dbbb508d568b07ee7dd488e6
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:33:54 +0000

    scrap before weather bridge condition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def clear_array(array):
+        array *= 0
