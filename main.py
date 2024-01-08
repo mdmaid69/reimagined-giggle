2cc65e1da3a9c57eec6213a37dd694024524cf18
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:08:29 +0000

    street wise country rub agree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import os
-print(os.getcwd())
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
