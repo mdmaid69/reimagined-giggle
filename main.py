8eb55914b87f1f95d1948ab76f6de7cda5fcfdb9
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:02:12 +0000

    approval compare effect surprising control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
