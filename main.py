5391130bb8251e0337db455fd093ceb58343ab01
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:23:51 +0000

    else stiff but impossible sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import getpass
+def get_username():
+        return getpass.getuser()
