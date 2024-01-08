fb8f6fab3a1425fffe2e62ade46d52b7cd007e2e
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:43:38 +0000

    accordingto phone past Oo further

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def remove_duplicates(lst):
+        return list(set(lst))
