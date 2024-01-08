7520c35a05e784b524bffcb7de5ac594e41aac13
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:58:29 +0000

    wall check runinto swallow near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def remove_from_array(array, item):
+        array.remove(item)
