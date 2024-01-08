b03e0fbac0bb42e464e77d68ed148a670ef83678
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:17:30 +0000

    minimum precise mind wind spot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_list_from_array(array):
+        return array.tolist()
