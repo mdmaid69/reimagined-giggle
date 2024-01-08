8c6e8ebface55baa9c0f6efd0c7d4c14b50849d4
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:13:50 +0000

    mend fill bury within warning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_exponential(x):
+        return math.exp(x)
