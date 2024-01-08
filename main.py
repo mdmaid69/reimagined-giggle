c8366500f2205a72598b2f06ce59bc30ea3c61ad
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:10:03 +0000

    accordingto gate cap profit Vv

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def convert_to_octal(n):
-        return oct(n)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def divide_numbers(x, y):
+        return x / y
