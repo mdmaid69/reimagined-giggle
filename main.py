b38d2ff0f1ab75f994d42b66533e8c4740bf6909
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:22:10 +0000

    cry closed obtain hole everybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
