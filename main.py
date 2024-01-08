e23a573f8705488ea9f0f594f5bd5f39c1fd5a47
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:46:32 +0000

    know callback schedule list prepare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def convert_to_hex(n):
+        return hex(n)
+import time
+def get_current_time():
+        return time.time()
