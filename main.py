43008c8b88abe53138a09e32f90864c8d2aa9712
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:24:08 +0000

    pretty single goup nearly shrink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def add_numbers(x, y):
+        return x + y
