e78328ed3b1d8a8220816d9620f241ff04a82b00
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:19:38 +0000

    ever furniture bush thank claim

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
