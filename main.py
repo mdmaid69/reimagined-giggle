1373a942a76601cdb8c75fb3ce5ddf8a8cb8fd80
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:22:03 +0000

    side blow soak impressed store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def reverse_list(lst):
+        return lst[::-1]
