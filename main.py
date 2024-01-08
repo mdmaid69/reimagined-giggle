575ea549908cada8a766cc7f1467c0abbb5a8c48
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:42:16 +0000

    tread will measure wrong basically

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
