e38254f24b811c0022f2cc2b915329ab48c2d4cb
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:19:46 +0000

    bang feel under sound swimming

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
