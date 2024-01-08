10194603ae7a9a8492e2712b718a6a03e5ddf7ed
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:15:03 +0000

    listenfor direction meet calloff slow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def count_characters(sentence):
-        return len(sentence)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
