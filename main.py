bcd06bb8987ea41f47935021c40fce737f6571bf
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:23:56 +0000

    drink embarrassed 566end male sight

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
