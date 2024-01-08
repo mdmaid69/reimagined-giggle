e415665ad7f722087c0cb60059553d65e84dc3c6
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:47:42 +0000

    whatever pin he variety praise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-print("Hello, world!")
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
