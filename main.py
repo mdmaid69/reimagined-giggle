ae4224bd11798392d85e999cf9b5df9eba0d096c
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:17:11 +0000

    bother everything today except wrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
