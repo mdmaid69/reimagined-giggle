eff55e924e50ba18d8ffafb490bff860f4cc44cd
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:54:57 +0000

    cross active ability sorry Qq

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
