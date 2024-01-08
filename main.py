dd94bb62de8759056afe3d0f9f215e2a4f425436
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:49:36 +0000

    Ss skirt worse matter well

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
