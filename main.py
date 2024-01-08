0cad9ec3a535bdde2aac540a90e79a1e94433362
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:53:03 +0000

    interested basically instruction record tight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
