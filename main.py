fafcc9ddd9352754663cb079139670c760301b5a
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:21:57 +0000

    sleep school may regret find

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
