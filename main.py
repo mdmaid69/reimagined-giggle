983ab5e5ec048e2fc88148fdf5be77d061fc56a6
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:44:42 +0000

    generally sleep loan pile makeinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
