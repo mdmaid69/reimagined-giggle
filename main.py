5a392b13f8a888e8d913cad639199211ddfd10e9
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:51:17 +0000

    command forward quarter brother same

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def square_number(x):
-        return x**2
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def find_max(numbers):
+        return max(numbers)
