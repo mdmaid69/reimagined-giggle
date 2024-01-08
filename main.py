f73b79002e93db93460bebf3617d50c88b4d3b65
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:21:38 +0000

    create keepoff wander trick middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
