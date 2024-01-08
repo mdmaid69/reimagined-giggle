5e33ae89d8d249fd760bd1982ef873302e94609c
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:50:00 +0000

    majority subject dress hand runover

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def find_max(numbers):
+        return max(numbers)
+def reverse_string(s):
+        return s[::-1]
