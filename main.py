7dc519a46352b0b91852074096c6d249599cf921
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:11:24 +0000

    agree key whole Rr slow

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
