130d5b2912ae459bf33c2037148919d394a5555c
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:53:07 +0000

    again advice tall point shoot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
