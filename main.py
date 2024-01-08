9a82a879038e5ab8d970832017184a8f79f87d3e
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:37:20 +0000

    reason lookaround height smell port

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def convert_to_hex(n):
+        return hex(n)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
