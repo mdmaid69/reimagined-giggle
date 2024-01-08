d45b412e6877f184c56e8c72e8b8ade8a67a08cb
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:16:33 +0000

    suck limit tip appeal window

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
