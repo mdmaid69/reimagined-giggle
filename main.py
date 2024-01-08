2abf5ca8e962850d3ceb390e8af609ffb2b23428
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:39:23 +0000

    several then definite none clever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
