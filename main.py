1868cbe269cab2879b12d1a045cbf40fd5ef956a
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:40:36 +0000

    agree bag communicate believe needle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def add_numbers(x, y):
+        return x + y
