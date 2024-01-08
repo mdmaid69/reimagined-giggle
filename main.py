00d7fe4bda3b88a3112ae9c5b12f847a82db6325
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:51:06 +0000

    angry hurt awake harm terrible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+print(math.pi)
