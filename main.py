e2b643a9d162bfa455876fd60d280ff9b0fa7d21
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:30:48 +0000

    wash kind bag mention wave

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
