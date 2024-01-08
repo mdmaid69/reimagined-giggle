d9604e8493dcd08dc418a8fa3fe86928ba618d27
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:16:33 +0000

    smoke nose can bringforward point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
