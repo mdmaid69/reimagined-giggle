954a502b6b114ab9a5b2674273d5d688fd22aa40
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:10:43 +0000

    stage accordingto food explain pile

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def is_palindrome(s):
-        return s == s[::-1]
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import os
+def get_current_working_directory():
+        return os.getcwd()
