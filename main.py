eb4a02c33a91b5923972a3f18e0c3bdf585411ef
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:31:09 +0000

    clearly store weigh bus damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
