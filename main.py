08aa9e4bfc28047e2a47e2dd6ed905636963243f
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:03:54 +0000

    somewhere practice view spare petrol

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def greet(name):
+        print(f"Hello, {name}!")
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
