2bf709c5c08412ddae3e71a99ee79efab747b28c
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:22:15 +0000

    suckme sky equipment best container

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
