e172c1483a3ac665c2c55c47192e18b05be236de
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:02:44 +0000

    noisy eat imagine scrape what

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
