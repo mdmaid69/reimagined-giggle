d39ccec97b25b4d2c0004238c5d2813316091d96
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:12:50 +0000

    radio finger block formal contact

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
