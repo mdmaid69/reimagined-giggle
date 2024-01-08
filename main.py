e62e8335c0e20f54882ce0bdfe5e40e6fdbef3bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:14:29 +0000

    Ff way confidence slice intelligent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
