1b4516eb3cb95d8a42c6a4f72895521a3a69d0f4
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:39:43 +0000

    club yesterday environment roll dish

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import sys
+print(sys.version)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
