9cd2f131e898ed624827bbb45fb264ccfc4cc67f
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:54:31 +0000

    right sense task refuse debt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
