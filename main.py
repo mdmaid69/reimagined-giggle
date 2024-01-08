f1578b1a27413b26919d386fd38f017e46458002
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:33:16 +0000

    protest target frighten difference mad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
