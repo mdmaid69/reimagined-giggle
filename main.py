259bf5760c8aa93ace781aeb2307a3ea209549ef
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:11:32 +0000

    immediate deep loan market confuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
