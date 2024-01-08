131d5339f549f88e19e862b08a2787c7b2312d34
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:48:54 +0000

    risk tell morning sudden dare

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def count_elements(lst):
-        return len(lst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def multiply_numbers(x, y):
+        return x * y
