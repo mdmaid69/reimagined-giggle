a7d4106b6dba55e5181da4457d324a6a18fc499c
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:52:04 +0000

    workout spare any ought be

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_slice(array, i, j):
+        return array[i:j]
