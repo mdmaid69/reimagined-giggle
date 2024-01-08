303f3dbd31b1a434e033eb633b1daade3766cc6d
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:43:33 +0000

    care major forever mail criticism

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
