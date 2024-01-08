de402b1bf68afb7767015d63cb91395d0a8ed00e
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:47:05 +0000

    pin identify tonight bus ashamed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
