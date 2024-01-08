d1aa34df773846c148dd64fb0b09afe1dd5697fb
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:13:20 +0000

    calmly demonstrate fly none you

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
