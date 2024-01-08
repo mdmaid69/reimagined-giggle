99f2687c3e286fc226f1ea34ddd156767fba9386
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:01:32 +0000

    contrast price steal protect pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
