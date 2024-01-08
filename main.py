81ce54aa2db073843e0e6b5ec24e5b35e3b0c0b6
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:09:54 +0000

    allow party wish mean can

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
