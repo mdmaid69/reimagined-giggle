9a072ff38872f52159b8cd0d7a32fe3079efa940
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:04:05 +0000

    feature available load answer to

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def calculate_acceleration(speed, time):
+        return speed / time
