fa9badd9302aad2ab61374f1525bd5991e830512
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:57:55 +0000

    wheel friend winter steam turn

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
