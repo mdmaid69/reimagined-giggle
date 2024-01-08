bb949d549142fe0eddbeabbe79dd9e490106cd28
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:41:42 +0000

    quarter town cause slowly case

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import time
+def get_current_time():
+        return time.ctime()
