963add4e23f013a252797f618ce03d5449ecafba
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:06:29 +0000

    pipe maintain stick clean exchange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
