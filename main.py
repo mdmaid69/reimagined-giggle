61e1fec53d7e9425c5980268f6b098a93aad9784
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:54:09 +0000

    spare cross makeinto responsible shop

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+print(os.getcwd())
