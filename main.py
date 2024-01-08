83239162b20d3af3b463ca83972559c67a1dbcd4
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:42:23 +0000

    minimum straight insist body breakup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
+import time
+print(time.time())
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
