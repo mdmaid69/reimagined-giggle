125fabae22f6faf28fb035bfe3ba2789392bab37
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:40:45 +0000

    lower level justice where contact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
