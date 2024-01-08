a73519786befa693219125bdb0e14a775824f312
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:12:34 +0000

    smell mad pull when cause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
