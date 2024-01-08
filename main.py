eaeb5b780c60d971ee0ba9be5eed512410df27e9
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:07:47 +0000

    sleep study wander build perfect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
