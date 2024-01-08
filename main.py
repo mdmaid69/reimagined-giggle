977697a68f8a7ffd8e811f234a79bdfc3e311212
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:00:48 +0000

    meanwhile duty speech aware approval

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
