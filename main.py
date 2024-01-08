7a779f1c94755a7d2cc02fa7fbb56698a31672fa
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:53:06 +0000

    apart Yy safe pen from

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def get_array_as_set(array):
-        return set(array)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
