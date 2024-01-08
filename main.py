93281badc7dab0f9a59454a248b6202644449305
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:51:34 +0000

    loud pinch adjective car breath

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def convert_to_octal(n):
+        return oct(n)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
