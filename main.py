e752de9f9616d9060a378395da9f2ae8b609efc0
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:30:04 +0000

    classroom into schedule shoulder hit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import numpy as np
-print(np.array([1, 2, 3]))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
