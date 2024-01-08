e4ba3c84ab66a2979ef7760cf563e685450c6321
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:13:54 +0000

    light comment as win process

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
