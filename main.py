cee201b598e3707232fd58d8291e1bb1ee1879c6
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:25:57 +0000

    bin everybody boil shall before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def calculate_area_circle(r):
+        return 3.14 * r**2
