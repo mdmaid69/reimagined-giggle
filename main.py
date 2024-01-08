3dd29a8dfc8c5819abb1dcea22d3e62a575f74ca
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:22:33 +0000

    responsible introduction workout callback window

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
