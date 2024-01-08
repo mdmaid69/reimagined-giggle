1906af6d8a57d726b69377ffa6f6d5c7d823f13c
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:20:22 +0000

    taste prevent begin lot positive

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_float(array):
+        return float(array[0])
