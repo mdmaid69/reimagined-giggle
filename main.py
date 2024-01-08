08cf6155b78aabb924219cac4a0ec94846a9d315
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:42:46 +0000

    limit rich rid who church

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  def sort_list(lst):
+        return sorted(lst)
