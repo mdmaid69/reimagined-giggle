67eb8414f030aedc3dd540e12197e91e53a8683e
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:49:28 +0000

    race whistle track fierce cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_bytes(array):
+        return bytes(array)
