533a9c335c90eb2163c31400be13755eca2c7ec0
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:41:31 +0000

    apply prevent through square repeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def divide_numbers(x, y):
-        return x / y
+  def split_path(path):
+        return os.path.split(path)
