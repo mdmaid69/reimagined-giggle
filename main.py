d0c929f42e53943a506a91079b0c6dc0508dc893
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:14:42 +0000

    apologizeapologise scheme confusing least rush

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
