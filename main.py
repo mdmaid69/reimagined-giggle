955f359db64f6b59607918d2eb8e09fca160e107
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:54:40 +0000

    send design protection book turnaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
