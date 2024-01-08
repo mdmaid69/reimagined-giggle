edbab4bcdcbe6e0bcb0ff7aaaf223122f71263a0
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:14:30 +0000

    set neither immediately decide yours

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
