fa916406d749d980fe3fe18c1170ccd4412fab68
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:38:11 +0000

    this spray story cheap connected

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
