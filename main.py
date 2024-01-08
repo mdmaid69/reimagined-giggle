b11b480a1ae0bee86ec466dbe2f367aaba7700b9
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:37:54 +0000

    179bite wheel should cry thanks

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
