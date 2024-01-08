c8e4c295517af3a6523006b306957754fc789782
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:48:24 +0000

    shall possibly suggest there song

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
