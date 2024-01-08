61ed53d9d8a78b428c9386b138e3dcaaf7e6c80d
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:25:33 +0000

    cut point continue head finish

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
