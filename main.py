9828bfdc30df4f9017fb03decb1936214cd7f9e2
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:16:30 +0000

    tread catch sure live normal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
