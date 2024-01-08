33b519c8dc8fd32b09859ae0905379940bd3ef4d
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:03:21 +0000

    variety big Ll terrible idea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
