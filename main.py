7e7c61069fd05c8a0ad96199962d0c9ed80719da
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:51:58 +0000

    tip shout miss how face

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import math
-def calculate_floor(x):
-        return math.floor(x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
