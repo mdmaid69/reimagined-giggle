9a81e2ab23e07d27d48d00f331c2365a1518fc6f
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:21:38 +0000

    big pool present similar quiet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def get_array_as_list(array):
-        return list(array)
+import sys
+def print_python_version():
+        return sys.version
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
