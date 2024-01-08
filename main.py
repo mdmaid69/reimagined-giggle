87fa7f7b3bd4fcc089832ba263e01555cbaae35d
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:10:07 +0000

    care likely convincing cry steady

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
