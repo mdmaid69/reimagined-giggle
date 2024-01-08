175efff8de76cfc7c9c78fe2e907935f5d156c61
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:22:14 +0000

    pencil provide shock next bag

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
