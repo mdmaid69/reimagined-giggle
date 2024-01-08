e2993dfbab5522c418c50f9f5d64a7066d76b0fd
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:09:50 +0000

    unfair argue much convincing patient

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
