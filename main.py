9082e357916f546744ddfd1d48aeed5ceaa2a1eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:19:06 +0000

    smart change bound distant future

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_max(numbers):
-        return max(numbers)
+import collections
+def create_queue():
+        return collections.deque()
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
