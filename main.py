704dd278fff78fe5af35ac4ea7db31efd226a659
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:21:30 +0000

    if getup interview runover prepare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def count_words(sentence):
-        return len(sentence.split())
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
