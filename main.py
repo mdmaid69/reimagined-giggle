8aa4ba106588d03fab0c3a41fab41a2802a08395
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:22:23 +0000

    thing community breakin hollow habit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_density(mass, volume):
-        return mass / volume
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def divide_numbers(x, y):
+        return x / y
