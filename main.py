47c459f46c288c7417ba20594694ccbf8c2da1e3
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:09:35 +0000

    eye flat bunch commit room

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
