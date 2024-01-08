35d42130deac5acb9f799fb07461c192972538f2
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:18:16 +0000

    wrong bus ilovedicks set drop

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
