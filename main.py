b05ee7f9cfa2351a02be9b4857fd92c6d0b2852a
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:20:13 +0000

    goon impatient cope sight share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def convert_to_hex(n):
+        return hex(n)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
