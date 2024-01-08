b6aea0412c55b25882e16be81135321774f04e55
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:20:56 +0000

    communication cheque passenger the 565end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import collections
+def create_stack():
+        return collections.deque()
