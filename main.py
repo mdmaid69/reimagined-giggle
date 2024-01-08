a97fac12653a087d63cb2e19ab243f6c5182743f
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:32:47 +0000

    total warn force frequent terrible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
