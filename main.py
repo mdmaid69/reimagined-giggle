c32997e5425b5f34efac230dc9c518c4bbd6eff4
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:12:37 +0000

    beside morning over return separate

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
