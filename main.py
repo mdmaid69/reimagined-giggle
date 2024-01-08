02c93e70622fb1f8b85df11694016063056a79e7
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:40:45 +0000

    respect never undo takeout respect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
