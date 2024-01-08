9e66fa748cfe26473201b43e40b69c72e01b212f
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:04:37 +0000

    about deep work switch figure

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def count_characters(sentence):
-        return len(sentence)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
