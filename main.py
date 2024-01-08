b1dfd711f4770e3e0eb6dd0cae225a0d0eb9e128
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:05:01 +0000

    kill whistle bye handle quiet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
