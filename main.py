811e156041a69af3191a70a4bf4e7e9465969490
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:35:59 +0000

    stranger year train whom busy

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def get_array_as_float(array):
+        return float(array[0])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
