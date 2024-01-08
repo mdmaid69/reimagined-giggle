ffec87cd363c342f45449099edeea8ff7e4be92b
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:59:27 +0000

    song avoid forget sink comment

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def count_elements(lst):
-        return len(lst)
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
