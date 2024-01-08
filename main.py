ccf9e38e53dc491a7c36448489f664901823e36e
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:49:48 +0000

    than organization ordinary glass sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
