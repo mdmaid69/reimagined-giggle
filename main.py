c5c06f73dc54ccdeb83bf9736ea40c94c61c586a
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:07:54 +0000

    choose raw sale blame risk

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
