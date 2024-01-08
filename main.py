f6f98dcb19e6ed740ab7a63a8bc764692c67e71a
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:02:57 +0000

    cause steep leg plant politics

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import collections
+def create_counter():
+        return collections.Counter()
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_item_count(array, item):
+        return array.count(item)
