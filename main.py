c765a09ee9eda9dc581bc8ea3367ef3020d877e9
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:39:32 +0000

    emergency hour list attach hungry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import time
+print(time.time())
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
