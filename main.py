ea8460decc1090fd913ec6ad4c929a54810664ac
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:40:57 +0000

    prefer crash risk broken loud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
