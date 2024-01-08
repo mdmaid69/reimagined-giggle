94cafb551925c1792d4727d27692790bb8e69632
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:16:31 +0000

    eager almost by workout runaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
