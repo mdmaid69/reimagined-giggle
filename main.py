57f77ee51b55d2b4e3608512432693e0b48ca160
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:50:11 +0000

    instrument tie present not cinema

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import array
+def get_array_as_repr(array):
+        return repr(array)
