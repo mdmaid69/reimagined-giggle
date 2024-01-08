4a1e08466e7fe2167795967616f41ed654dac41d
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:30:56 +0000

    platform thirsty plan how worry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
