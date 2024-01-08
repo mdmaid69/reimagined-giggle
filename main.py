6ade294f681838464aff13a2e3ee001a3a7dced0
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:46:32 +0000

    worry loud wealth we level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
