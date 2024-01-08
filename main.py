f3960743811476d172f27728bb66b89c5d19878d
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:29:15 +0000

    low weekend himself satisfied noise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
