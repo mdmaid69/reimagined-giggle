5a4764f138477ca9e85b661eea9221844bd112db
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:08:01 +0000

    partner wander tense trade bend

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_item(array, i):
+        return array[i]
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
