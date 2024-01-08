467f5d9dec677fbc8430321ee3dd901c75ec6322
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:30:05 +0000

    glass beneath period impressive note

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_volume(length, width, height):
+        return length * width * height
