d76f8e75ccdf041f4ecffac5b2508b1ac1fe7b18
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:24:27 +0000

    prefer people with claim send

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
