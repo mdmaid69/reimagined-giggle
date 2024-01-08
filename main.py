982ea3bd1a62742346d7c2f1a8807ff7bb5f6b4f
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:51:36 +0000

    complete long book fork obvious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
