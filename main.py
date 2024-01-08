230263f8b6c5049232df2a0eed442d1ee4dc01c6
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:30:44 +0000

    course lose sick doubt public

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def convert_to_hex(n):
-        return hex(n)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
