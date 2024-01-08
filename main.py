b7a91c906a74da4de6a86e456e24e0288b6c1df8
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:20:18 +0000

    back brain cry keepout joke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
