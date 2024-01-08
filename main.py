5183ea8e2ece1eb8a8ed4d0a17a6dd3d0498b4aa
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:32:47 +0000

    keep 571enough basic himself fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+print(math.pi)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
