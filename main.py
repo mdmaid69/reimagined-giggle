47d4e4cdc6220bf9eb421714976ca9d81773e350
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:53:36 +0000

    rough win distance operate description

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
