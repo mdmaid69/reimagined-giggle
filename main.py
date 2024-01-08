5d1f619171d6e7b50c50e5fa8ba3fbfcab9042fe
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:16:28 +0000

    complete slowly rarely without possible

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
