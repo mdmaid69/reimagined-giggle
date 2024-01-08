d5241b6b908ee5ac47005cbaba1414cd12e8d961
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:49:06 +0000

    improve note about praise after

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
