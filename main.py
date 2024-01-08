b358500a1c3e33795ce422b6fab42557b88106af
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:38:44 +0000

    likely prepare store choose amount

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import datetime
+def get_today_date():
+        return datetime.date.today()
