65db99bdeaca4d9a687cd1b7c255457a55382c49
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:01:16 +0000

    leg refuse stamp mine you

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def add_numbers(x, y):
+        return x + y
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
