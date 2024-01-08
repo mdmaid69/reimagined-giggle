6bd77da22aa15cff425484cb46dfb25beff58263
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:13:46 +0000

    sudden check finger wide Ww

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def is_odd(n):
+        return n % 2 != 0
