28a4d6ff485e18d5c23879a55b0a50124f3eafed
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:29:53 +0000

    sea behind ordinary interested measure

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
