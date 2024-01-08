54eb04dce426ec22c00e6015e1852aaa59adf0a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:33:49 +0000

    worried ilovedicks connection pay bush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
