d0348477a7f4a202456ff06d123fe63f4b979344
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:57:54 +0000

    turn none patience getback outside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
