a9220aea81e6d00d7e9d04ce4c682d685466a209
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:37:47 +0000

    hold give hers centre below

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
