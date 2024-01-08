df6168748657b56ee15a9a9593e25216e29d8088
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:44:10 +0000

    butter or increase backwards effort

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
