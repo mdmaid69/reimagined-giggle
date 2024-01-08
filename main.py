f5d75e5fba099d04ce0fe3bc385efe13ddb01d26
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:45:56 +0000

    exhibition get autumn string neck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
