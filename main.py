0b7295d2e4ce2dedac4e3041a0915459e7e201d5
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:07:57 +0000

    air lock fashion sit middle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def sort_list(lst):
-        return sorted(lst)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
