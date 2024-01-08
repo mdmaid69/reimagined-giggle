dee385828f4241739097da6d76766e726bf72bc8
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:09:48 +0000

    exist judge size dead switch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
