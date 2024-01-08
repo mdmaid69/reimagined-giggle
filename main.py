5784bbd839cf783c1d5b04485e0102e3bb54343e
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:05:01 +0000

    goback(to) tune sure influence frightened

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def get_array_index(array, item):
+        return array.index(item)
