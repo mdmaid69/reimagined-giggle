0d4fd3ab3104557662c88d4dddd994e0b7a8214a
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:15:12 +0000

    scream production leg relation brief

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-def remove_duplicates(lst):
-        return list(set(lst))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
