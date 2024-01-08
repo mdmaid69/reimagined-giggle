dd6e2faa15e2cfcd39dce48ae40c2748e8eea7c0
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:05:14 +0000

    can reach live clearly cold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
