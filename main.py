8d2e06740d5cf048dfa8c72e6f8ab724aac2ff0e
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:55:39 +0000

    stamp direct declaration combination thick

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def sort_list(lst):
-        return sorted(lst)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
