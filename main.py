1654617e688c2f1401a6b3d84467387c24422dd4
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:32:02 +0000

    create wind treat ball treat

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import sys
+def print_python_version():
+        return sys.version
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
