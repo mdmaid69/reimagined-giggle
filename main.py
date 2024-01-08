4e1324b6754fa56e514fe18c00392001f178c702
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:54:20 +0000

    dry cry less possibility hungry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import time
+def get_current_time():
+        return time.time()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
