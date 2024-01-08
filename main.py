d22a1fd1212b7f2e2de074cc3dc1c91760e16b26
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:50:03 +0000

    press spoil exam condition well

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
