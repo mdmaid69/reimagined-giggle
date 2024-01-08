1aeab2f498dac598c2bbf2648ee30a4d032af03c
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:06:46 +0000

    accident turninto record present section

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
