9b36cfdd0e93fda0ed859c0e26db7ef46833ca76
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:08:45 +0000

    shout owe ring yet takeoff

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def get_array_as_str(array):
+        return str(array)
