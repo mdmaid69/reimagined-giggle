3bed950bf962e8aaa28145c90dbdb09c34e84177
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:44:53 +0000

    please patient close our appear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
