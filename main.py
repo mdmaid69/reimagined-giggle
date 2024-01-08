e62d8c46ce9e9795ea80bbcb61b52ad452bf9936
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:04:26 +0000

    disgusting trust way throughout later

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import os
-print(os.getcwd())
+def square_number(x):
+        return x**2
+def reverse_string(s):
+        return s[::-1]
