2382ca6d623cb839796d88c3d7525d35ae1a1f43
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:52:39 +0000

    trousers upset stretch 573entertainment nor

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
