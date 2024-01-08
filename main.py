e5d6eebe2e937e59604839e4e528d0a5cfbcd3ec
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:35:37 +0000

    custom cause unite people handle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
