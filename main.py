ea15a6c7fd96472dfdd2a709a98c8da3247abe07
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:54:36 +0000

    566end land actual them share

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
