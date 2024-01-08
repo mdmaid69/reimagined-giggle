567e1bfa5a9cfc97cabbc0e522a1dba160821452
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:54:00 +0000

    either little seriously cause illness

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
