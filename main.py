32e22b4981364fe3bdbe67d858128a4d14fc6307
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:12:02 +0000

    brother even doubt base makeup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+print([x**2 for x in range(10)])
