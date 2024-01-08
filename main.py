83abac7289154856e8943889b56f32ca106578ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:51:27 +0000

    print deliberately bury cause little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
