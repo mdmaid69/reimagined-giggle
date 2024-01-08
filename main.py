ecf61824d90433dad6554ac3420f25ae531af2f9
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:00:52 +0000

    piece pair wind cool attach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
