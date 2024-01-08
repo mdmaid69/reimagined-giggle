5ffad99d835708b17c5ed40e24a35eba317539f8
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:53:19 +0000

    range figure support before actually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+def get_current_time():
+        return time.ctime()
