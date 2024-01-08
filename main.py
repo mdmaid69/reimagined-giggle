c6a08d4aa76b0d85a85c92822e1ec27edbea52e1
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:23:44 +0000

    person matter lead bet each

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
