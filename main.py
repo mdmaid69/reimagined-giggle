e4243311add5c22f00e14de06f697a003f9bb49a
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:23:03 +0000

    unit write Gg impatient tradition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_power(work, time):
-        return work / time
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import collections
+def create_user_string():
+        return collections.UserString()
