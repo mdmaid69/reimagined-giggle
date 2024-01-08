be8de6b9f23a5bd138071762f5984d57bc1bca95
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:21:20 +0000

    repair else goback(to) copy school

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
