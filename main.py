8193d53dca7cd084448f3f0dfa232ea1bc49d635
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:30:09 +0000

    separate petrol gate fat concerned

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
