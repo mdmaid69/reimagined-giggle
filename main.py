b96365fe0454ad0aa52dafae2630c6160a98e183
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:28:20 +0000

    ticket eager suggestion surprised grand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
