b0ec05c5aedf681e067ff0e4671c03c147a44c65
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:20:22 +0000

    upset art little rather duty

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import datetime
+print(datetime.datetime.now())
