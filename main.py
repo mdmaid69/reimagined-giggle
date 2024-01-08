6c4aeb7faa325aecc5660120d8d93556bb78a664
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:10:13 +0000

    where spill content angry cheek

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
