d4be5c1f7f33fd7579ab53280f008822ba89992d
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:52:58 +0000

    preparation hide frame blow personal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import getpass
+def get_username():
+        return getpass.getuser()
