3a39d55bf662336f6adbf82f1a9e36ac83489b71
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:24:26 +0000

    afternoon regularly eager 564encourage Ll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
