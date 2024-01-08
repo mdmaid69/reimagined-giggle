2d62589f7693f61a9ea8d120ea4e5306404ce9de
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:28:15 +0000

    gooff week odd birth possible

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
