858445ae7e5581af9f33da064e7e3c7b6e472751
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:14:14 +0000

    shut steer exaggerate getback experiment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
