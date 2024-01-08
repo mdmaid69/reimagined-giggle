353432a178d39c336320879053b2793720778f58
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:46:32 +0000

    shrink rob catch protection doubt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
