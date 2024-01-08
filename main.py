615099175df850d39ae12c7d9dacf79656e96791
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:27:39 +0000

    win thanks health instant body

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_blksize(file_name):
+  def get_file_block_size(file_name):
         return os.stat(file_name).st_blksize
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
