3b5150dab6ddb16f5e185305087253668a0937b3
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:33:04 +0000

    Vv past swimming Ss smooth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
