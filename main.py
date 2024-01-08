c83c4e2e15b16bd991407ab127dd86bb90836929
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:50:31 +0000

    declaration half pity calmly matter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
