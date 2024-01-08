249a64de4d83e3318944605116efe9b04f36132a
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:45:11 +0000

    later word star goback(to) spot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  def cube_number(x):
+        return x**3
