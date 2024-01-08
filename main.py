0707331c2e78fcfa09b4086d68e254e80587a7ce
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:30:50 +0000

    gap move buy free example

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+print([x**2 for x in range(10)])
+  def remove_duplicates(lst):
+        return list(set(lst))
