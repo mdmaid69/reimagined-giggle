4ddbbd70f712443311ede592213c9ed0642f9e00
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:51:32 +0000

    freeze news bush sell calloff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
