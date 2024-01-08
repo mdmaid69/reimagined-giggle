ca24099f39be2b565f8294502bc1649280d63c23
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:50:56 +0000

    small might couple very clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
