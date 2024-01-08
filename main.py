e2429dc9681b6dfb06966d7d4a9e2c27b41891d6
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:57:59 +0000

    over voice wing aim politics

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-print("Hello, world!")
+name = "Python"
+print("Hello,", name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
