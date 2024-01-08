b2371f264205b858529fe953a74875906d085f69
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:27:22 +0000

    organize insist mood stay guide

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
