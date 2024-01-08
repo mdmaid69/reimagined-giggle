4cf24b505032aea39503a0b528127e651d19059d
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:41:25 +0000

    familiar nose stick success people

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
