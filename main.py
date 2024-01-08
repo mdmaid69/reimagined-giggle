c238d7851574c9ae53054b54685484bb288bb98f
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:53:29 +0000

    face radio board by reduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def split_path(path):
+        return os.path.split(path)
