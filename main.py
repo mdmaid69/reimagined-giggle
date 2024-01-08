37a8cb2ca22717fe33c7cd05fe903400677ed6d2
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:15:17 +0000

    workon building one reason yourself

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
