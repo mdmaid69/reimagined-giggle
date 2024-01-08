b14cef77f73f7a70ec8508ee00e320ab722524a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:40:07 +0000

    offend pain land worth mass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import sys
-def print_python_version():
-        print(sys.version)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
