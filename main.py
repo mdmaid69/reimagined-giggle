aaf35e0013bf250f6ee678e55e5a92dbce687af6
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:03:37 +0000

    scared typical rent kick floor

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def square_number(x):
+        return x**2
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
