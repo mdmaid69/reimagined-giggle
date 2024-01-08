a3f411e290d172062541657320b5c963b9309f57
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:00:18 +0000

    collection hope game gentle appear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
