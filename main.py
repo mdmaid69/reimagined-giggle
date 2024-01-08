daee9d402d2cd63fab16388cb498f09bf57f2a67
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:26:52 +0000

    lie model single judge morning

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
