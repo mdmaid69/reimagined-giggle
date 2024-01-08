4ae9b8350db6585644265538a5dfeaf613e27641
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:15:02 +0000

    unfair me step dry plan

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
