7fdcc9610e71a1f013c5a291f081b9dea52f9e9f
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:31:36 +0000

    neat they reduce invite bringback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
