1a72730a4fa6507e979c9985631fe558fdbddd48
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:01:38 +0000

    attention cut tend bend Ll

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
