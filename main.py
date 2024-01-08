b7886261d4b05c6531e2d92c4e502799c1807722
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:21:57 +0000

    not emphasis spin other stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_set(array):
+        return set(array)
