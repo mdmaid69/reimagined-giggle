d8b22712a59f49f4bfccd46f513e862194fed77a
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:11:29 +0000

    lookup Tt steam cinema woman

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
