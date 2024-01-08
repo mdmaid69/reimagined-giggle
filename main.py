ce311b2584c01638929ff77997bcc2eef83bca4b
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:50:50 +0000

    ill turnup aim embarrassing sky

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
