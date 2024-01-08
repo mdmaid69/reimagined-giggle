502a45cbc9898a3ffbf80ac90cc0c4f8144fb7c5
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:09:04 +0000

    pool meet marry remove mood

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def square_number(x):
+        return x**2
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
