8e8a93ea84fa0e3f283a562097c5b367e5a58dcf
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:49:28 +0000

    bad suggestion among tool embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_set(array):
+        return set(array)
