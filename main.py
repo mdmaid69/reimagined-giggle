cba27c3f0bef75ec2a002ae6c9c7a5d5b4876dee
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:30:41 +0000

    various change separate total subject

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import random
-print(random.randint(0, 100))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
