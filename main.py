8fdf413b78ddaeb959befb8e1c09c4de735e6bda
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:15:32 +0000

    peace important passenger content operation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def is_odd(n):
-        return n % 2 != 0
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
