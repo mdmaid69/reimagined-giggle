d654aebfbf2072d111c4f4cfd4629470a0aff0c0
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:29:36 +0000

    scream temperature name ashamed takeaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def convert_to_hex(n):
-        return hex(n)
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
