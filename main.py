c8599ee768ee15fb766ff3e0ed2535a9b66f5e28
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:36:46 +0000

    your language less arm side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-for i in range(5):
-        print(i)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_sine(x):
+        return math.sin(x)
