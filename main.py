d4bcc278604e0d36bb7f00b4d2aab0d2b5186106
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:08:03 +0000

    regret surface giveup invitation float

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
