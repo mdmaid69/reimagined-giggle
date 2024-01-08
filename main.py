d1daeff787fa28ff4e8a481dadc3132ac522aa22
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:29:42 +0000

    ride kick callback majority anyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
