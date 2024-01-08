a2699bdb668e69849e9ca23f64e36d0161f07c7f
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:55:38 +0000

    572enter prefer workout hot male

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
