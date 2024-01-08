3aee54349f2ff3bd9432e949184538d9f6828d2d
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:16:26 +0000

    low connection suck winter uncertain

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
