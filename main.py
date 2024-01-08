8540e339717a7896ecfd9078eedbead3ecdd2087
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:26:27 +0000

    left everybody blow once friend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
