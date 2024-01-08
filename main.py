20243460c3396ff14e6a43d1ed042aa244798854
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:43:47 +0000

    contrast glass involve punish machine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
