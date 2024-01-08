5cf2c83074601175e7b7b9dabf88d22d5aac49d0
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:22:18 +0000

    minority exaggerate attractive false tune

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
