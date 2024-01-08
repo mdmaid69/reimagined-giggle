376ffd3363e965f67afbfd5f06d341d2113c14a3
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:49:19 +0000

    environment book shop on relation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
