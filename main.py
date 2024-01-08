8a8a993d4548fbb6eb70786d99559e6da10cbd78
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:41:22 +0000

    born calloff medium floor well

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import platform
+def get_os_info():
+        return platform.uname()
