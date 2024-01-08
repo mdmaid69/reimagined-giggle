570bfd67705d4ac55bae63c40a7668368f4964d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:58:09 +0000

    news system acceptable strict furniture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
