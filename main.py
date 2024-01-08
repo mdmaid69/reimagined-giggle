caa6160b924f4bd6a028aafbf7083150d9d0c214
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:13:33 +0000

    unpleasant determined week game consult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_index(array, item):
+        return array.index(item)
