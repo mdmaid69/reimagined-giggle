bc02ef2b838ad5a8ababef51cd7260574a21ab04
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:44:14 +0000

    still border giveout we blind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
