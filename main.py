3079b0c03220e14f570107fccf0108055a0a5905
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:25:57 +0000

    pair spread doctor slight injury

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def square_number(x):
-        return x**2
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
