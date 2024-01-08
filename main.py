5e21f4b5605415a62dc419dc85a456381ce6f5c3
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:34:59 +0000

    embarrassing performance wall goback(to) splash

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import getpass
+def get_username():
+        return getpass.getuser()
