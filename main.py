c7416be6f0fc37d49ca522f9bdd51dfd3ef7aa79
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:37:03 +0000

    reply strike summer loss block

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
