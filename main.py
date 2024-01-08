1de0ccc5e1938b4d7415f6608b730bf5f8a0d43c
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:17:30 +0000

    communication watch Gg judge middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import collections
-def create_user_dict():
-        return collections.UserDict()
+def create_stack():
+        return collections.deque()
