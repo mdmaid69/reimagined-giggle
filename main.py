1d23537a156033da5fd44e18e6b79b1882941e44
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:10:26 +0000

    feeling just politics hurt often

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def square_number(x):
+        return x**2
