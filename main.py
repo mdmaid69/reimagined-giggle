081d6fe42816663dba701e33beaaef73d6be3d70
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:50:56 +0000

    refer shrink physical influence breakinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
