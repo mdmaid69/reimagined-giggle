5e4dafe1dc09b813494992d815d9598f52f8ae41
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:17:09 +0000

    make lookafter main fact whom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+print("Hello, world!")
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
