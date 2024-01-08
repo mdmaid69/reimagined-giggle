db2ea5a13496b17543b683099c18f2ec2319b111
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:12:06 +0000

    mystery listenfor alive introduction onto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
