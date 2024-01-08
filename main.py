485d6d008df8a63ec963dec12a639643aab3a32e
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:54:05 +0000

    basis public education reply so

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
