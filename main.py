bf8e08aba39e2d6d99c4b9a85f696cfbb63690c5
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:28:45 +0000

    OKorokay sport related such calm

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
