1fb49a73afb004f071845af722da16c9908f322c
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:40:34 +0000

    difficult leave bit occasion very

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
