ad1602b5992e891b77a779e274f30f1eb9096d2d
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:05:06 +0000

    Qq turn mind by break

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  def convert_to_hex(n):
+        return hex(n)
