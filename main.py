315291c3c303d60a6e6397f1ccb4e8b3c3857f73
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:01:59 +0000

    block operation operate wall dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def split_path(path):
+        return os.path.split(path)
