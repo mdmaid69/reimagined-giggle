7c4d71aa659340440a14469349e94fc40366aa0d
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:29:07 +0000

    opportunity confident period into tear

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
