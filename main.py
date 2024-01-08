74674bb4120ff13005b2dc4ea9a29b18dd2d56e9
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:29:50 +0000

    bet die indeed remove yesterday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
