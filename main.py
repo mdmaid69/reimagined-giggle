c863cdb6518a09e4904f9daf1c87e8fc9f4b7ac9
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:31:33 +0000

    continue rest needle inside trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_length(array):
+        return len(array)
