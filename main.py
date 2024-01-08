5e22deee1e0bed6b593337d49ecef182bfcfcefb
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:41:00 +0000

    precise thumb tour cold opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_speed(distance, time):
+        return distance / time
