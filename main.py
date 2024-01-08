39f02a9baed11b7a51175ed7a8db1a742505824e
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:00:37 +0000

    joke deaf more alive tragedy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def delete_file(file_name):
+        os.remove(file_name)
