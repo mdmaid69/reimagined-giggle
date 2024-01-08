baf608d9563e8d99e5d36327851a73ca4ea1f170
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:58:14 +0000

    force walk mix education father

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
