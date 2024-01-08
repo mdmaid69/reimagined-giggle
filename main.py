a51af074fd30b3aaa5cc9ed560f6df4f5032971a
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:10:11 +0000

    normal sudden sweep new choice

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_odd(n):
+        return n % 2 != 0
