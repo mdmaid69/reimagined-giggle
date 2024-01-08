babf08b19ba469ec23848fd786231f6eed989ad3
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:19:37 +0000

    line effort few tool strict

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_str(array):
+        return str(array)
+def count_words(sentence):
+        return len(sentence.split())
