9e3f09ba10ba63505727b63de901e9585fe623ad
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:36:08 +0000

    moon lock estimate ready convincing

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
