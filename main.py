0e987552b866bd4f4e5241e82e366b7728faa260
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:33:11 +0000

    detail bear separate risk laugh

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_pressure(force, area):
+        return force / area
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
