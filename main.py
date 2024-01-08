39c27b50e2f773391aeb3b61c5756a053f0162d2
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:26:12 +0000

    round brain type course space

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
