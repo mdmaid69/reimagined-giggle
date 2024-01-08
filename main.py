231a3a45b2a472951b4dfb4b0309afbb83164390
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:07:00 +0000

    powerful polite hope even pot

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def reverse_string(s):
+        return s[::-1]
