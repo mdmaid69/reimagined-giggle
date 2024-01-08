fd4966e42b52d5c264a3378a5a1e872833a7343b
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:14:30 +0000

    fence family kind increase false

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_acceleration(speed, time):
+        return speed / time
