093d66c67e3e373a64baf81c9f5fde5803b567ab
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:32:32 +0000

    generous appeal different wash throughout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
