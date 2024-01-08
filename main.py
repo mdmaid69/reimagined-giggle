aab0720f9c09a597efb25d84d68f975d127a6b34
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:01:36 +0000

    include Bb frightened OKorokay yourself

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import sys
+def exit_program():
+        sys.exit()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
