db174c401c3dc45fd72bb70a3f55f668bf5ebefb
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:04:35 +0000

    comeback simple fortunate cinema weigh

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
