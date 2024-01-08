48f7b0b815efbeef33ab2a07031e1e071b9ede8d
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:48:54 +0000

    another choose bother tend attach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
