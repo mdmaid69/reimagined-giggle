cd247a2bd089c9f0d513ebedda8146d8342cda78
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:52:11 +0000

    hope strength official pen scheme

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
