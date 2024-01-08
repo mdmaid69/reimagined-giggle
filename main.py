f957663d42e895f4e68ef5994b487385c310f2c1
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:24:43 +0000

    steady expensive teach shame compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
