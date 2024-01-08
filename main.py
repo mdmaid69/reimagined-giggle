126a1a427d70b1194110b6d60f20b14cd6ebe9ef
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:37:44 +0000

    exercise prize connect lower side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import collections
+def create_user_dict():
+        return collections.UserDict()
