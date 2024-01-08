484864aea6342cd1913ce66e5ade74504d1bf4b0
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:40:33 +0000

    suit possession guard sing towards

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
