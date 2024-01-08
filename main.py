e0f414e554345b872dea8e315a5e0f7ec68c2b8f
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:59:04 +0000

    small conduct good regret swim

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+for i in range(5):
+        print(i)
