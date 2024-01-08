ed6290509e2a943ec0b5250b951f9df9c0593eb1
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:54:58 +0000

    favourite female government waste worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
