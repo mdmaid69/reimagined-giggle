04cae2e7de71e73427c6d31543258d15063ea3af
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:41:41 +0000

    throw argument bottom average degree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
