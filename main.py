fcf65a491529b66e253cdda89d15f0aa1583d3ef
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:30:42 +0000

    although free deserve unusual tour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def square_number(x):
+        return x**2
