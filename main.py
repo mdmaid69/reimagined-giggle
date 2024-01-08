ec75153862c275711da3d6fb8af698c6b58e1123
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:06:46 +0000

    mix tour careful raise find

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
