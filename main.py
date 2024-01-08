43a3def4204c6eee667cf9b122cd372fce2d33e9
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:06:52 +0000

    want practise trial cool short

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-def reverse_list(lst):
-        return lst[::-1]
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
