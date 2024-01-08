4c5b2a7077e106653e8d35e8336f8a5b46afaa16
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:15:59 +0000

    walk dig help feature attractive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
