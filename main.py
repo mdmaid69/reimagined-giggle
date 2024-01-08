aeafc6f59e040c56099198dd2e23a5976493b7dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:36:22 +0000

    brother unkind good boring break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def find_min(lst):
+        return min(lst)
