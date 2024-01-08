22cefabff296242b49fe6c7b43c77575e9e10401
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:43:26 +0000

    567enemy completely minute quality will

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_acceleration(speed, time):
+        return speed / time
