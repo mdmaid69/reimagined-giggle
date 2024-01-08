dcabbbf1574c4abfa0c0d7b35feee61f1ef399d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:59:44 +0000

    Ff pronunciation mass clear system

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def is_odd(n):
+        return n % 2 != 0
