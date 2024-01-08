3bcdf1299703c87bfad1b154a8a2a21151573230
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:35:42 +0000

    behave community exercise fold closed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
