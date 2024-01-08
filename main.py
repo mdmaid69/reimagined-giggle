7193ae0472a9b05f237d68eb552d1124ac7b5206
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:11:29 +0000

    remember cut natural typical rub

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def reverse_list(lst):
+        return lst[::-1]
