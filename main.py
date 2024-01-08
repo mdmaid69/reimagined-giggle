9374fc483932fb6c9efead327fa9e55ab188ed10
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:09:43 +0000

    necessary free cause produce shine

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
