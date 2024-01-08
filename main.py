871e8efddd9a0f35eb39943bac113fcbd86d4708
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:27:30 +0000

    means company simple laugh urgent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
