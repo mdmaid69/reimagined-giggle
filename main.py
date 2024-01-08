4561dd97627067e3a0f8339dd3b018d0ecdaf95a
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:55:39 +0000

    crash model strike share famous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
