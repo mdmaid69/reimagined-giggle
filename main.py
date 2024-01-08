42ad6cc718140e3520887c343fd0cb1594fdc636
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:46:38 +0000

    right bus rush one roll

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  def reverse_list(lst):
         return lst[::-1]
-def calculate_force(mass, acceleration):
-        return mass * acceleration
