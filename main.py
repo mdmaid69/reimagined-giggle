e169635061e404ff8241e2f8ef2c901e3c0adac4
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:34:38 +0000

    this seat shoot runover lay

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
