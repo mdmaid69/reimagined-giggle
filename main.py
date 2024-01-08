88b4b201d0155a3e13e8972515d4f2f16c2a033d
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:49:10 +0000

    kill except fair wild strange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
