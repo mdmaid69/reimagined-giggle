b80b75b54048d0c847003d1ffbb9a101e9f306a8
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:31:56 +0000

    region here score cheat ignore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
