53c11a78969e7b2d20cff9d496a99021e44a4f51
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:51:57 +0000

    she brush conversation stamp fail

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def cube_number(x):
+        return x**3
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
