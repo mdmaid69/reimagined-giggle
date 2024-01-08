2659a739480b3f4888db797f70ff54010064ad10
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:48:18 +0000

    first defeat music division impression

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_perpetuity(payment, rate):
+        return payment / rate
