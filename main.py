1d252219c017fdde6d53a8a6e9fa2f59b4222b09
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:33:11 +0000

    confident peaceful tough report 566end

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import sys
+def exit_program():
+        sys.exit()
