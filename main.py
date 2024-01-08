4da44b9628ed8125d1f4bc47e298bc845006c288
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:16:18 +0000

    shoulder complete cheque disappointed sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-for i in range(5):
-        print(i)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
