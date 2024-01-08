162349b600d02e232aab4dd7919bd04a3b47c38c
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:15:54 +0000

    phone above now towards feed

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def reverse_string(s):
+        return s[::-1]
+def square_number(x):
+        return x**2
