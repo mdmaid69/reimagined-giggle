aa5b040572ba8c664163f6a9161a73f4cb28d494
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:11:26 +0000

    comfortable shock pretty flavour lay

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_speed(distance, time):
-        return distance / time
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
