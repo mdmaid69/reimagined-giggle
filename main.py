034b4a81acdd15d7acd16c20720e379967423136
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:08:56 +0000

    swing hide people poor attach

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def clear_array(array):
+        array *= 0
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
