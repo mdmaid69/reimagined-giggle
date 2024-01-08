65fb8a968f468a705a7cc7366ff823dbce2cab4a
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:47:40 +0000

    nose activity dinner embarrassing condition

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def multiply_numbers(x, y):
+        return x * y
