e42c7d1e3ff4a5cbb346aea417b08f30b375018b
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:25:50 +0000

    communicate seriously payment keepoff physical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
