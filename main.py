b050dd83f662701ae201b9e9b5ddc842567a1976
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:31:47 +0000

    soak serve callat film continue

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def find_union(list1, list2):
+        return set(list1) | set(list2)
