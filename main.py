3fe084d5cd9b1ccda5aec73cd9244e6307c1147a
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:25:01 +0000

    valuable create news run tip

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
