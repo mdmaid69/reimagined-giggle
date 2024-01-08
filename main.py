117db5af25c62ef07ea2b795814892588d551fa0
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:43:05 +0000

    car kick ordinary underneath entrance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import sys
+def print_python_version():
+        return sys.version
