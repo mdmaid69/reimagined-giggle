b82eb8b14e62ebf98af968e3f265c7500ac66ff5
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:58:59 +0000

    sore this total harm both

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
