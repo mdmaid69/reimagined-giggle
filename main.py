ddb4c6980dff7e550baff9bf01c3d2966b5d6605
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:32:17 +0000

    total actually bridge history holdup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_current_working_directory():
+        return os.getcwd()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
