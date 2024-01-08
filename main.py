c412f2c12c19945bc60e79eec00e5dfcb266abe8
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:35:48 +0000

    forever comeback journey injury effort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
