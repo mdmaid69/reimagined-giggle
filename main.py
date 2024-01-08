2fe5f28ba4d525c9e054313bf0658bee380042db
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:13:46 +0000

    urgent point sister friend soon

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
