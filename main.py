c49be82efbbd4bea66c15bbadfb9a7ea0e590ac4
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:35:22 +0000

    deaf hold carry invite chat

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
