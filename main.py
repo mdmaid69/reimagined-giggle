816cd319b0000ce60c792a263f621833dfe0d20f
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:54:44 +0000

    back appearance equipment exhibition sport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
