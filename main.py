51413b9cf9afa196594a993d8a801073725ffac2
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:08:34 +0000

    mother quantity godown find minute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
