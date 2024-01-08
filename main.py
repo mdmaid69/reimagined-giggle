5cbff77441080a36b38622fb989f19d00acdf0c8
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:55:31 +0000

    blue distance flavour concentration into

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
 import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
