374bad780ae51499cc9609511bf3e5809e837964
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:07:52 +0000

    wrap attitude much clothes protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def multiply_numbers(x, y):
-        return x * y
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
