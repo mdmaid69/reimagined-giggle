477fe59fb521784fa0d992f587b15b5fe688bcd3
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:25:24 +0000

    then interrupt shallow appeal us

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
