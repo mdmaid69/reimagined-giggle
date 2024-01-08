8823cddd8ed8cc006226482057f9ab97f91d6666
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:27:15 +0000

    address forgive chief snow neat

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
