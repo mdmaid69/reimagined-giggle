552f63be8571fe422c852976f76ed4854769224c
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:16:55 +0000

    send soak sight instead aim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
