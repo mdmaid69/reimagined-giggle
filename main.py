695cc88d40833bf12d5d0aa688198c5063ed1dca
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:05:22 +0000

    holdout introduction bury concern left

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_current_working_directory():
+        return os.getcwd()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
