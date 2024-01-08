677885b7746a900e391b3b96923beed4bf16ade7
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:10:04 +0000

    love estimate prevent interesting unusual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
