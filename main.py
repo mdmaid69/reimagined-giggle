8385cf2e5d4a83aa7538a4bc533966bb1443c757
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:22:59 +0000

    injury feed opposite dare storm

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
