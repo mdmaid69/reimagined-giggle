4f4c895ac023f816a5e1ce5511f7927a68713c60
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:43:48 +0000

    freeze we priority no city

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
