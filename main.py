35aee40e0998a3277ba7c55db23fb4c8ffead6bf
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:05:07 +0000

    shock live hungry whistle work

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from collections import Counter
-print(Counter("hello world"))
-name = "Python"
-print("Hello,", name)
+def calculate_volume(length, width, height):
+        return length * width * height
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
