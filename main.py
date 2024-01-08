18efc3fa8d10538e496379a9adae2bd565a149dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:38:19 +0000

    fix wonder grand influence pain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def remove_duplicates(lst):
+        return list(set(lst))
