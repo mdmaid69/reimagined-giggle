22d7d4c439e3bcc87fbe38d3dc4416d73e65ab40
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:23:51 +0000

    giveout getout of curious fierce

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
