9caa67aaed639ec1e9ec0dc5de8f9ebbeb9820d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:23:10 +0000

    vision custom smart check calmly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
