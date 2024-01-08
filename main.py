f2577208ddd04f905d5bbf69dbc1ab5cf0668c03
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:18:13 +0000

    performance turninto snow unpleasant trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import collections
+def create_priority_queue():
+        return collections.deque()
