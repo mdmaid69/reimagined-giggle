fc79be9325278c31f39414ab5b57014c8cfa3afe
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:01:31 +0000

    star our too better urgent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def reverse_string(s):
+        return s[::-1]
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
