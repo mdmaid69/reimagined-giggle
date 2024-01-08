ecc4a8351964588f7d95b50c406a1da1d758ecc6
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:58:31 +0000

    land rent different unhappy anxious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import random
+print(random.randint(0, 100))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
