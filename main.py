57a616e143e872065f1863eedfac948a9d481e44
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:44:06 +0000

    art flow even mix round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
