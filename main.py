703694097de0a2d194e3584766c0f714aadd67b7
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:15:33 +0000

    everything woman work so blame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
