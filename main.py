6c2f9d43e5c4be6296afb202ba62211c354b645e
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:05:13 +0000

    land major instant left early

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def convert_array_to_string(array):
+        return array.tostring()
