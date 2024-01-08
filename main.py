2131745deb91cc17673db9f6ff7621cc3d2bb68a
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:46:12 +0000

    breath intelligent remain fence along

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_float(array):
+        return float(array[0])
