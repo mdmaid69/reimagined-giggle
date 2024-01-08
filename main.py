e7b03982a344e41ed940dcdb9661fbc37b249e50
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:41:48 +0000

    everything guide he later connected

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def find_max(lst):
+        return max(lst)
