005f811323c326189b3c4d135f2cfa4597ec1e5d
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:09:32 +0000

    nice pen room shoulder listenfor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import array
+def get_array_as_bool(array):
+        return bool(array)
