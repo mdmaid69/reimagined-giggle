2ccc6d166378cc7e9714087803ae4396314dcfbe
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:16:52 +0000

    medium mean similar petrol emergency

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
