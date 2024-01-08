9d0b49b651dfc9490b2e7e79b9ce71c1b558e3d0
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:30:58 +0000

    mood emphasize rent trap go

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
