4b9060ea3b80a89f4a3db19b04ce4791daa227df
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:47:14 +0000

    slice once lonely market mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-x = 10
-y = 20
-print("Sum:", x + y)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
