9d1ab1a8a5e7de919e7ef40dcce61aceca37ce97
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:54:39 +0000

    party event exaggerate tonight through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
