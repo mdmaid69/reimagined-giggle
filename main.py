087dc7afd82028465d1e2a12fcce7a7bf7a2345e
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:04:33 +0000

    type path alive doubt before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
