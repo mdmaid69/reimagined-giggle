b3cdb963a40b26814b25475465942084cfb5d8b9
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:23:31 +0000

    pronunciation politics wander bringdown ever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
