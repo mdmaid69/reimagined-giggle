038813af5fc6c7ae5947fb2b63a577f9e9ea101d
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:40:00 +0000

    572enter lean so late funny

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def add_numbers(x, y):
+        return x + y
