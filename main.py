f7f7818065427ddce0e906f50b3ad6c71225835d
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:04:14 +0000

    mess turnout admire excellent excellent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def find_unique_words(sentence):
-        return set(sentence.split())
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_array_as_str(array):
+        return str(array)
