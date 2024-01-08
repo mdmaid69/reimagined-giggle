cac24a8d05247144770c9419bf2f08301e334cbf
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:12:27 +0000

    murder name cool head level

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_stack():
-        return collections.deque()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+print(sum(range(10)))
