8fb710966f84e041a2a1fb3ff61f99e55d389e04
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:35:28 +0000

    appear our noise introduction keep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
