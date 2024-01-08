e2ef422b6b150a32952eae08d3352d618f5d1fc3
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:49:47 +0000

    make actually activity low weather

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_speed(distance, time):
+        return distance / time
