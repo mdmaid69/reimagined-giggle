b475475e917128178d42a3aaf6ad279c776f26e0
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:24:21 +0000

    mother few beautiful bringup estimate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_density(mass, volume):
+        return mass / volume
