19ce2ff732276ba1cf56a419f0aa18897126447b
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:08:00 +0000

    front less refuse appear leg

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_density(mass, volume):
+        return mass / volume
+from collections import Counter
+print(Counter("hello world"))
