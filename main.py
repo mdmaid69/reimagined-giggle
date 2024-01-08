6a9d22bb99ca13e2d802c958421b1e5cf298be56
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:53:29 +0000

    tone sharp of finish pour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
