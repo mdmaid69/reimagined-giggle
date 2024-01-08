501090ce6e83140536703b9175c60e28302d43f3
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:04:28 +0000

    hole wind greeting quit hole

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
