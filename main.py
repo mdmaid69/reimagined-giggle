22b3dcbf20675672f66a81f30a6b23fa5614a43c
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:46:32 +0000

    mass positive judge find brave

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
