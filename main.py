04333cbff1c8511ef64cb6e5342a81026736c2c9
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:54:28 +0000

    public apologizeapologise stick previous hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+name = "Python"
+print("Hello,", name)
