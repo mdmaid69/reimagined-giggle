d9cb582f89eaa9c44a4c1f66e64c017f6ae819ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:21:27 +0000

    confidence ignore mail this prevent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
