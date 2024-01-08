2c961a9bcb69fbf1f45a00bf96b6c6dce8c99e7e
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:01:47 +0000

    wind place provide rough average

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
