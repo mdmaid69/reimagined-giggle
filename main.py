fd6698850d546493e9fd4ad8c064390bebc1be9c
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:50:22 +0000

    Qq back build top shade

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
