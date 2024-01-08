ee3eaabaa17505eee9083b4ada651ca969b23c82
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:14:29 +0000

    me breakout ask clean ours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-from collections import Counter
-print(Counter("hello world"))
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
