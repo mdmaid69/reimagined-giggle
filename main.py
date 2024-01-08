f07182e46706553b7781db9d6cd8535f4e290cf2
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:56:35 +0000

    estimate beside half bend shout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
