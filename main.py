bfe3750bdf309658f4313fc54cb969dd63b061ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:02:50 +0000

    workout shame care across within

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  def subtract_numbers(x, y):
-        return x - y
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
