f454920ea8c7454b77e82ca9df9c7435de02bf7f
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:23:05 +0000

    aim bend couple understand lesson

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
