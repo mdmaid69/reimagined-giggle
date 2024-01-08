93101af0e63882f5cfbfcd8bc9a0b61a4bda5541
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:34:18 +0000

    dark talk surprising key steady

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+print(sum(range(10)))
