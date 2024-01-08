d580aba4cb278ec4d16403f04ffd42613cca1cbc
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:20:59 +0000

    kill own operation earn comment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import re
+print(re.match("h.*o", "hello world"))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
