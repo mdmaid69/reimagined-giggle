494f6d679c8e848f2ae48f4bc03309cfe61f2f57
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:39:36 +0000

    than minute table clearly already

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
