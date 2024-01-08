64a91ac9dd708279114565d5fb48ca9ac9bdd75d
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:47:22 +0000

    runout will year miss say

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-print(random.randint(0, 100))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_index(array, item):
+        return array.index(item)
