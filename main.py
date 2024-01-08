15389327e19f6ea4f36032d4123acf07ffae7086
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:27:04 +0000

    threaten later buy centre push

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def append_to_array(array, item):
+        array.append(item)
