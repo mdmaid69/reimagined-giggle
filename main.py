432a107044f7a73ae2f3ee1f1c452dc6fad72fa3
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:42:17 +0000

    again lonely nigga little generous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def add_numbers(x, y):
+        return x + y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
