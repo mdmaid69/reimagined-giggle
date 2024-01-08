5bf94e49ff18e1e7f6db2f1f56c3d2b3fb60aabd
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:47:44 +0000

    buy group top release over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
