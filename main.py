b09a5e5515dd45b4d9384416c05c96a1720ffed7
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:16:39 +0000

    sweet woman young such side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
