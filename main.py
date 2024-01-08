48e4a8a04fddef5597c6c498812a0ead728e46fd
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:49:28 +0000

    spring clean block gentle under

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+print([x**2 for x in range(10)])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
