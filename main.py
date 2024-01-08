5cc0898af8de7192f02b5cfdf8bab3a90aa72ff1
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:32:57 +0000

    declare runinto child giveback regret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
