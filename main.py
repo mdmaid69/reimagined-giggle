1edf680e78da123ef1cffb7a660f91697253acc1
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:36:05 +0000

    fix distant another sign hold

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
