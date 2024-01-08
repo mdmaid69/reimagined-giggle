5dfa904f9f661b81c5e347746def7348382b1050
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:08:51 +0000

    everything train important straight cheque

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
