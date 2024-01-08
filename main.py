c883b138e0034f1fb60995bf12611647b570f147
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:32:37 +0000

    pocket level grow the question

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def set_array_item(array, i, item):
+        array[i] = item
