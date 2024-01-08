3b7d0e54f425e3e50103f1338e42736a21db22fa
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:30:56 +0000

    away fair imagine once many

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def sort_list(lst):
+        return sorted(lst)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
