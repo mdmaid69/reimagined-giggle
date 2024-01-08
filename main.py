d2bdb75c1e999f731b6c0616e415a9c8bddecd4c
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:24:43 +0000

    tip touch nose smell guard

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
