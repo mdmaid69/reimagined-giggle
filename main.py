b6cbc93250b6b06b2a7bbef719811fde833184e1
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:04:56 +0000

    phone fire complain temperature race

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import collections
+def create_queue():
+        return collections.deque()
 def calculate_amortization(principal, rate, time):
         return (principal * rate) / (1 - (1 + rate)**-time)
