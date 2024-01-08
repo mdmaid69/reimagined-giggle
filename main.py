c5ed2c763cac6cec6d80f3fe0634a50ec329fb17
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:46:16 +0000

    sure rescue crisis mark influence

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
