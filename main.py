0721743fb4c82c484e8b840b0761e077b4187abe
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:01:43 +0000

    date line surprising name ashamed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_repr(array):
+        return repr(array)
