3f6cb37515732d51d9f44651341674acf43aa7fa
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:49:22 +0000

    remove probably there section extremely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
