b6a0cd88fbf1083626ef4677ad32aaabbf0529c4
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:07:18 +0000

    delicate stand spring approval interfere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
