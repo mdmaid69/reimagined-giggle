4cf3fbd8d0f5af1308cbd152e9cb61de92972b2e
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:57:23 +0000

    last secret job remove weekend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
