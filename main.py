2d70cb1b235a6e31a5b2bc5389541f4fcf6f059d
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:33:54 +0000

    turninto workon over alone argument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_current_working_directory():
+        return os.getcwd()
