e668eba8816629a081821ff776bb19e350cdd75c
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:21:07 +0000

    term tour apart hair Pp

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+for i in range(5):
+        print(i)
 import heapq
 def push_pop_heap(heap, item):
         return heapq.heappushpop(heap, item)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
