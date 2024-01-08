7b2b46b14213a1bace62ebe93d6ba69129c6e2e3
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:33:52 +0000

    favourite ask easy pick all

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
