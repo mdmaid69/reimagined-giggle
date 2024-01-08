2de97cd713beff0272e7276f4783b3a5f2851756
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:38:19 +0000

    doubt thanks perfect uponon advise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
