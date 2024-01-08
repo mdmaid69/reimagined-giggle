538be8e456df1461df5a9b5ca9e6a2c151f50381
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:53:43 +0000

    contain chief stop scrape simple

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
