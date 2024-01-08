4b8bc2173e509711b4ce90490530fd79a2718d1f
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:41:07 +0000

    bringdown anyone unfortunate turnoff mean

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
