e311a7680992341fdcf0a31a508f56ae6bfb681c
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:39:31 +0000

    about emotion importance bottom unhappy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_speed(distance, time):
+        return distance / time
