7e0e82bfedbe4a59a326e03acc0c01a3197626bc
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:00:03 +0000

    professional squash shop ill satisfactory

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-print(sum(range(10)))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
