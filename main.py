14363f30337297b8c55fefad805b41a6ab0007a3
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:23:42 +0000

    brain any combine fact push

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_max(numbers):
+        return max(numbers)
