390d58f30c9b3e80eeb78820827b5ba729523dab
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:28:38 +0000

    please Nn thinkthrough section lunch

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
         plt.show()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
