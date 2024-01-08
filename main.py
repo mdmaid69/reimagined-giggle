31bd195cf0eef7a9085ebb3768f370c7df9ee4f8
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:40:29 +0000

    off keepout swing scrap public

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
