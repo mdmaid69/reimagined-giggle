e4418711a96c8717284645cc12461749c1a351fa
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:49:54 +0000

    usual average journey floor afraid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import collections
+def create_stack():
+        return collections.deque()
