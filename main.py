a7fdbf3a296ad672ac0b1c753b566c90dc6820e8
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:33:18 +0000

    my last understand clear position

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
