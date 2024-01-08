ede8806347e7e02ffd4ace984cd2f3ade941a71c
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:29:22 +0000

    knowledge race fear eager proof

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
