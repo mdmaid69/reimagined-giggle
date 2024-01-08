6137702ef551d7f8fef5d894201e704117f669e7
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:41:02 +0000

    lost spread manner circle murder

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_speed(distance, time):
-        return distance / time
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
