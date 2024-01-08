25dbc6235420cff876749c41a26c1420d4a7ca77
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:22:44 +0000

    flow trousers block connected lower

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def find_union(list1, list2):
+        return set(list1) | set(list2)
