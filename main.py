c3ee10d27745fab976ea7622b7d9eaa1a4de1d8e
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:59:45 +0000

    main load become school mad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
