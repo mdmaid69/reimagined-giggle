0fde10acc7b0bee5835b78550e291f8dcb00b064
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:18:33 +0000

    software surprising takeover serve week

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
