124204fa572e31b5417da02ad1707ed21eec1f7c
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:38:20 +0000

    concerning definite weigh autumn emotion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
