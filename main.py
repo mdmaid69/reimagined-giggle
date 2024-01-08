9d9fd874703ff5689a21fbf89c63cca6f41bfd4f
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:16:53 +0000

    mistake result reduction along wet

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
