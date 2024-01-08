831d52f49af7bec1a04e8a399b9cdd5db2a8d95b
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:26:51 +0000

    drawing afford dig tool straight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def multiply_numbers(x, y):
-        return x * y
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
