b5861362eeb80f7352d622269fc340491bd720bb
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:47:13 +0000

    sound violence film factory tell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import platform
+def get_python_version():
+        return platform.python_version()
