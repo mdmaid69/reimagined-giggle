023647e686d2a6c7277e6aaa8556245fb6ee29ba
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:18:36 +0000

    you allow scream bringback damp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+print(math.pi)
