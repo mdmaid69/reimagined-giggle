a6d60303afe49f4ff9af3352ea2924a79015c638
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:42:03 +0000

    progress high breakdown friendly remark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def clear_array(array):
+        array *= 0
