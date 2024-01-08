b036f363bc97cdccc6cfcf27f3012301a8d8a6e4
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:56:37 +0000

    better base advanced strange enthusiastic

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
