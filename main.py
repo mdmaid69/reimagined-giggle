76861b935fa0aa85a53dc01e6949c3bab6f75c7e
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:24:00 +0000

    hour flat hold anxious face

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
