284e64b6ad5428fe84ee5699ad167ac04e40a102
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:57:59 +0000

    report keepoff lot pity offend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
