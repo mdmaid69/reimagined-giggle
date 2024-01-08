ee98049a98e441fe92ed5e7614c4c633f46ac4a2
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:32:39 +0000

    before meanwhile exhibition appear under

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
