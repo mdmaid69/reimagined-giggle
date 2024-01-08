624f1e27e9f9a032bc74324775de83bbabc6ac39
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:17:58 +0000

    fairly final coat takeaway excellent

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
