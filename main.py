d808658eaa3cfd55b3b22c9d275477b0137291da
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:00:05 +0000

    street peaceful marry argument fear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_area(radius):
+        return 3.14 * radius * radius
