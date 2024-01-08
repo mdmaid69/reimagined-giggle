81dd8d5ee26f2eb89183566d2b645b37a220aeda
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:57:29 +0000

    hardly spread hardly class central

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
