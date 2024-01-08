080cfec966deaa6c5f09ab6dffb7701a281daf94
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:08:51 +0000

    your ball tax then hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
