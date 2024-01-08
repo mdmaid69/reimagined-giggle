1b1bb3b4c55bce99d529e5703ca09b2d745ab2e2
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:31:25 +0000

    more hate press anywayanyhow chase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
