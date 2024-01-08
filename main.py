cf558d1ae3d8a1c9ea686b9b838a507ae9344c56
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:29:41 +0000

    getout easy beat sentence sense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
