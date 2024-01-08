451ce81d42536ba498768c708befdb19dadc85d7
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:43:13 +0000

    still passage although frame lookup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
