3b985f3ed1f8c4664157c3d80cb99bb76564a781
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:17:18 +0000

    almost complete break friend exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+print(math.pi)
