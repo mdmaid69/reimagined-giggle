2b2d366fba4cef6aa824fcedef5a3eaba88c87f9
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:43:13 +0000

    divide doctor embarrassing all fortunately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
