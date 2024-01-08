cea6eaf2b3827e8ab00186cc27b68551bb0dd6f3
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:49:47 +0000

    maybe Pp realize Ss down

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
