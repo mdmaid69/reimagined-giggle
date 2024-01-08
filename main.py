7285e1a4811750f7815b3042fb8aa0968f4e5c65
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:34:02 +0000

    march message birthday turninto shake

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def reverse_string(s):
-        return s[::-1]
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
