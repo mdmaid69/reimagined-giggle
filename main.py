875d8f6d6d1da781736edab8c71bbe29c38ec48a
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:45:06 +0000

    term sound thankyou bet expensive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
