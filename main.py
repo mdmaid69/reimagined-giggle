5577bacc7b3dfc8bbb7a7f21a5fd90dd5eb6f029
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:22:01 +0000

    tooth programme lookback(on) always open

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import collections
+def create_counter():
+        return collections.Counter()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
