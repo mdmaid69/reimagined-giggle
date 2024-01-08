bbb743829e4624c42084f93cbc0a370a93253e97
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:25:31 +0000

    behaviour step odd autumn Vv

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def split_path(path):
+        return os.path.split(path)
