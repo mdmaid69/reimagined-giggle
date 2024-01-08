923cacf41ac8d8ec3267f13b83a4b983a59222e7
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:11:28 +0000

    still study ashamed several save

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_int(array):
+        return int(array[0])
