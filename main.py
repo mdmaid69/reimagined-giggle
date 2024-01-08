29ce8f7a105654e694c42eafe12c46c75dc80c61
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:41:38 +0000

    exit cruel long slight myself

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
