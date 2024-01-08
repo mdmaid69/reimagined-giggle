b501e54e586e4890d44effe0ba4bd320a6d13d1c
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:33:06 +0000

    yours excuse dull hungry very

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_power(work, time):
+        return work / time
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
