58903c246605826d0387d4b7e8c7f325e9ef2d80
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:57:53 +0000

    goout star ticket present rise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-print(math.pi)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
