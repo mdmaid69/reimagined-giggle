742c061e61d6bef806c8f60e46df7085b994fd6f
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:30:54 +0000

    squeeze fix instant trouble rescue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
