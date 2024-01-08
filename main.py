1bc778ceebc354d84a5918cfb0fb47d2e1bc7e3e
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:48:46 +0000

    chief connected runaway better drawing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
