4a34403e6482d411796adc8aca5755b6202231dd
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:51:55 +0000

    dangerous respect step private sight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
