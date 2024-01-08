3a89be7abcd2903f9d647d5f734bc63d388d3178
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:39:03 +0000

    pleased Ll keepfrom fork dance

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_list_from_array(array):
+        return array.tolist()
