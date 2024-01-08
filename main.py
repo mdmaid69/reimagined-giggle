0a98a4fa842d1aee8301388047a559adb56795b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:20:37 +0000

    win interested wall only survive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+print([x**2 for x in range(10)])
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
