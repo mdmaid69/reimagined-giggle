60bd473bfca195a1bc025c72363bd53e05e964c0
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:32:13 +0000

    approve better drink wear journey

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
