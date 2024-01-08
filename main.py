2f8028060f3937b0a21bc9c5fa1f44ecd03d5a97
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:31:13 +0000

    already weigh mysterious rent please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import collections
+def create_user_dict():
+        return collections.UserDict()
