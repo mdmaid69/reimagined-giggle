9c96580e12df5392bc1dcd1ded1961b40caf7cf0
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:16:17 +0000

    inside motion official soft left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  def convert_to_hex(n):
+        return hex(n)
