a4a60367101b965e0d08c889526a9d246ba6c0da
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:23:02 +0000

    belong takeback organize object handle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def sort_numbers(numbers):
-        return sorted(numbers)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
