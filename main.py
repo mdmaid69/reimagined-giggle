46ad6e026aece124897097acfff889dce9a70ddc
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:11:59 +0000

    accidentally study as target whom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
