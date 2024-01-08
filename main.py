b5e56020c4acf3c6dc84ce9db37a6775e2e67b97
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:38:34 +0000

    lay deaf announce produce season

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
