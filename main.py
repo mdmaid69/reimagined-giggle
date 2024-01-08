2efa66a9fce2d22af5ceecc397f554df8b1e9e91
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:56:49 +0000

    scream hand world patience turnoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
