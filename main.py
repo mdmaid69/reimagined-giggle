a62ff70d70f622d62801f211cc63d1be5fe8c6ce
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:58:18 +0000

    treatment collection start shine world

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
