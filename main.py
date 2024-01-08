906052f08c6dc66d0a307c1c63a586008ff9df58
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:33:02 +0000

    turn point need wave long

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def find_max(numbers):
+        return max(numbers)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
