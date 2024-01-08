ee50b66ae296e710a48676780f3ec9656bca7703
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:46:50 +0000

    polite please suggest light journey

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
