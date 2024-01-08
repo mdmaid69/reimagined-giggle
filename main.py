b1a65b9246a46305a28a1644b8aa54c8fed935e1
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:20:07 +0000

    off allow his entrance fall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
