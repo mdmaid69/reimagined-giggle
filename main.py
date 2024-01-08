ccac0eb785de2530aaba27170c86dc35966bd0fc
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:27:22 +0000

    ilovedicks heavy suit will communication

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_queue():
+        return collections.deque()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
