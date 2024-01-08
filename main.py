ddbe8d51f3af8db1da2928a8f6ef0bc24edc4067
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:22:50 +0000

    convenient popular best know undo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
