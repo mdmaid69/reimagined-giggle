87e00bbf371e167fa649f648d39cb45aa2d80583
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:37:12 +0000

    world mark roof bar trial

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
