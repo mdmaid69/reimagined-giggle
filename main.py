6df55b6047360f255e2f330db90468582df48bcb
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:20:37 +0000

    right strike target degree pretty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_power(work, time):
+        return work / time
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
