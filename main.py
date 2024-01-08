5b63869af3081c3f4ddcbd10d21e3d666332a6b0
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:11:05 +0000

    bread skill since difficulty Ff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
