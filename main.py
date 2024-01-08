ee75eb35c1f08eaa6f112eb5d4a701ed92200c84
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:44:50 +0000

    protest destroy one behind mine

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-print(sys.version)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
