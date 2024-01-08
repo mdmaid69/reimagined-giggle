b571790aef9b87dc3e7ddf35a272520a9ed5671f
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:50:47 +0000

    character language bin offer runaway

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
