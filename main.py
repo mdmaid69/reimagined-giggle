479efb33836c1a0baab1f1c15dff60f6097d8d9b
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:48:42 +0000

    getup look remain famous wander

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
