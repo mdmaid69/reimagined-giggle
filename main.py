e7dbf4db83eea587b7efc87b5fc44ddb42e38327
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:56:10 +0000

    about tear waste wave service

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
