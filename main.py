e75983ae2a604b9cba1a0c21c6a15e07c6ead097
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:04:00 +0000

    step achieve within favour reaction

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
