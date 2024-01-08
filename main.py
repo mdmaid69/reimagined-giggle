cb17ee8f9949ec3bb8ee540e4d6bdd82198eeb77
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:16:44 +0000

    disgusting breakup through kick odd

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+print([x**2 for x in range(10)])
+def convert_to_hex(n):
+        return hex(n)
