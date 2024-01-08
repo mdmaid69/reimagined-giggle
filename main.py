b26d7c40ca57fb35a9e9eb59f3c266ba2b98b7e5
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:37:23 +0000

    fit runaway fast determined this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import datetime
+print(datetime.datetime.now())
