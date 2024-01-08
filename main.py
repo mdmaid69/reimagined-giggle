f0f6cbe6bdbc853f6541eef06f523d92d70442ca
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:30:23 +0000

    getaway pour rough Vv target

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
