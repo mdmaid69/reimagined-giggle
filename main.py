7d053802460df07bb8e540fd3c4be092db71dc5c
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:53:41 +0000

    fence success characteristic his peace

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import time
+print(time.time())
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
