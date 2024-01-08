88d41ce1be74511af4735fdcdcc203a7c0f3c3fe
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:41:06 +0000

    slice tired store police likely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_as_bytes(array):
+        return bytes(array)
+def find_max(lst):
+        return max(lst)
