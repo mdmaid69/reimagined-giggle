9467cc48db93301f035663ec7854544287a94651
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:44:18 +0000

    lose knock stop rely quarter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
