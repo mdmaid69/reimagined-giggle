0e17aaddc5d1ff25d8aa21887abf1c0aa06d1e9b
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:13:35 +0000

    vote if help bright Itself

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import collections
-def create_user_list():
-        return collections.UserList()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  def convert_to_octal(n):
+        return oct(n)
