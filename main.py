d627b9b2c47b62b2c3eb8667bcf31e3330234880
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:51:46 +0000

    guilty communicate hurt Ee save

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_array_item(array, i):
-        return array[i]
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
