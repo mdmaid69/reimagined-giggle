73f796689e00413b82331d7b70eb322a9499bdcf
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:35:54 +0000

    Ff flat bang place eye

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
