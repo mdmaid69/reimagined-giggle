e114a577a9511eb6674991668a0b267cae3900de
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:25:06 +0000

    spread union want discuss custom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import random
+def roll_die():
+        return random.randint(1, 6)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
