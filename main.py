7988928e9a0109acd87d7e5ee8e475b0a049d32f
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:51:10 +0000

    position run announce destroy wherever

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
