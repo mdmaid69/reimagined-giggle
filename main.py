4c331f166fcc85a691db82babc64e1d847122555
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:07:44 +0000

    deserve uniform help spring separate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
