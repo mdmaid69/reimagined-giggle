2caf660101821ba1bf09c3aa15a0be0af6e7ead9
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:26:23 +0000

    estimate size onto again teach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
