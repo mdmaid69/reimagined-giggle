99168b3cb9559c521d8826dfdda5bef78cfc97fe
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:48:01 +0000

    coat giveback find divide Ee

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
