87cad357617fc4091a3900bc50e47ea3327ebbfa
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:48:02 +0000

    recently glad study occur ground

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
