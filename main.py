24fd1199838b8f226bc034949f591c46010bca53
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:21:46 +0000

    pencil arrangement roll weak holdout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
