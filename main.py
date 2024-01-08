e7e4ae828e3ccc2c8e34f41479023806474738a7
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:22:45 +0000

    lookaround style game machine consist

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
