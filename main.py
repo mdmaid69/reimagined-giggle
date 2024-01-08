115a382feae5a3b459052f967acd1b776637e61b
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:06:07 +0000

    view further pressure mean glad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
