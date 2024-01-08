96afe128a04509a4b07cef118461fb01b098cae9
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:45:46 +0000

    again above form fire like

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def sort_numbers(numbers):
+        return sorted(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
