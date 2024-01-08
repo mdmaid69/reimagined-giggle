519aa765e39f559f9005b173a412a2b66d3c4db4
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:47:13 +0000

    somehow legal cum reserve shadow

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def sort_list(lst):
-        return sorted(lst)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
