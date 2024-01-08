1b12154729d4f3fd15167fe71a673b2fce136a45
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:01:43 +0000

    official fire order survive straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def remove_duplicates(lst):
+        return list(set(lst))
