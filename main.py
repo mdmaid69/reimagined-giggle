ab13b4d88eb52fa6ea13b86f5c1bf29f5f76179e
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:24:54 +0000

    talk effect then spend play

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def add_numbers(x, y):
+        return x + y
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
