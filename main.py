c311a9ad181ddda156e604098244b233050b6638
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:04:42 +0000

    train remove file Ii advanced

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
