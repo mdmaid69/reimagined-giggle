e364f03d16f51028de830bc665a54a239a0815e4
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:35:57 +0000

    card unlucky black holdon bringup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
