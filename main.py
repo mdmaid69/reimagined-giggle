76a6df3e51eb125ae051035b630ff59cc999e15b
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:52:30 +0000

    explanation job thick remark fierce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
