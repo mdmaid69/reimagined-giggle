1b575ac4c7aba99919b6b8b7c5f8f81e8efc61a3
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:14:53 +0000

    when and tread accept gap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
