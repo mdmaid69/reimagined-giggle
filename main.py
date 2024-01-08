66f9615ccf91a129ceb9120a970e4ec0780c4b41
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:25:34 +0000

    tip taste talkoutof father anybody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
