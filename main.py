7ef36526627bc6e65cf86197d2ec5e1b44460bb1
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:03:10 +0000

    it shout hard sale consist

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+for i in range(5):
+        print(i)
