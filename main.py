2aadff34cde2e024885a3b8b43d2e635b1cc27c0
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:36:11 +0000

    sentence fear early run fair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def reverse_string(s):
-        return s[::-1]
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
