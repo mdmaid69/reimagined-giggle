0df4197314d54c508c070c3a927173a8f2b94486
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:10:36 +0000

    cook who survive luck sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
