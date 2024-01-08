671280d7d3d055c4dee8bfcbf290d30272f430c0
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:40:20 +0000

    train under unlucky ought raw

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+for i in range(5):
+        print(i)
