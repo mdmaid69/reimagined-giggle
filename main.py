7e15e2f019cd830bb12a19d60e6443d599c94139
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:45:01 +0000

    bar home blame correction frightened

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
