779b8a5d016b21bf472f8851675e8660a8bcc494
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:03:12 +0000

    ride separate goup before stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
