44490b1170a1ba5a13b82b77482fc163d157c114
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:54:05 +0000

    none help emphasis well take

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def count_words(sentence):
+        return len(sentence.split())
