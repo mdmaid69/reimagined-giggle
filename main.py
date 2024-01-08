8ea368ba9e2b9a8f4a67842df81325254644082b
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:38:51 +0000

    figure large nowhere basis right

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def subtract_numbers(x, y):
+        return x - y
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
