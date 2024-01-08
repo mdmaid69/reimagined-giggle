38e5ffd7494971bdeb28ab0a2fe41dd7ff7e5869
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:21:18 +0000

    urgent use chance breakout national

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import sys
+def exit_program():
+        sys.exit()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
