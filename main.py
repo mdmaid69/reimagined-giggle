74aeffa0c835159a9f1922a91c40694ff0d0dead
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:28:24 +0000

    passenger like listenfor yourself disturbing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_int(array):
+        return int(array[0])
