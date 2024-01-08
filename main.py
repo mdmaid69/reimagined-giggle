00c2f002f77af66c3ee590615194efe41c082827
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:10:03 +0000

    runover shut mind vanish count

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import platform
+def get_python_version():
+        return platform.python_version()
