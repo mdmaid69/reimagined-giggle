b77b2186bc5abfb2b2f0b18289068c049764a807
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:17:24 +0000

    police organization play group apply

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_work(force, distance):
+        return force * distance
