825737d4917a453cd751b9ab8b4ba4d579cf70ff
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:32:46 +0000

    land partner bringforward time open

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_power(work, time):
+        return work / time
