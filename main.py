9b834ce7bcec5b5de755819412760727be02bfe5
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:22:05 +0000

    any some but moment hers

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def multiply_numbers(x, y):
-        return x * y
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
