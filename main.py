70ed919b59866fa17159c7cb2e3799f2a451d8dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:43:14 +0000

    flat part murder Uu confuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
