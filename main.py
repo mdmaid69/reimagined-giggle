69ba6dac9a959552d9807667fc07d8604889c83f
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:52:18 +0000

    goodbye excited stiff print score

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
