b2221f44d486b37c0fb09e3e2f573e0576cb1a33
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:34:26 +0000

    ball wind moon Oo charge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_current_working_directory():
+        return os.getcwd()
