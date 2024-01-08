7de2e9f477b7af365066dd87efda9e238ef926fd
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:07:20 +0000

    motion plenty round mess pleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_uid(file_name):
+  def get_file_owner(file_name):
         return os.stat(file_name).st_uid
