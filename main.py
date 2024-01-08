8ec1420ec36463c5b157ecc58068ce9a20f08948
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:52:22 +0000

    naturally have boat then stage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import math
-print(math.pi)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
