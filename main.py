62865d2fd3c4e6fddcfd6228a0eed9e6b257543f
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:45:22 +0000

    beat evening stage service hide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
