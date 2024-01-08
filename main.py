4b28330df2fdecd169c739dbaee02db026927119
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:54:57 +0000

    die hair custom month Itself

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def cube_number(x):
+        return x**3
