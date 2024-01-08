2c0b972b096654f69f405648077c977c8573cdf4
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:49:14 +0000

    separate no either my flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_force(mass, acceleration):
+        return mass * acceleration
