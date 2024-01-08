e1b069a63debdab26ca238d3dc13ae1f5eba8fde
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:11:22 +0000

    rather lay workupto store thorough

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
