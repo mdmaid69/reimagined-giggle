4610ff2016e2fbf849f2b6d31dc6559938fd35e5
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:06:31 +0000

    house ball talk shelter angry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_area_rectangle(l, w):
+        return l * w
