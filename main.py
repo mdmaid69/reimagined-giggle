0af07aae61a53d2dca36be03cd34dab5a56b61c9
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:54:41 +0000

    place straight passage character constantly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def find_max(lst):
+        return max(lst)
