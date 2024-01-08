7117e853d956365daa0d20229c808a21a6b587a4
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:09:53 +0000

    ring kill turnover information calm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
