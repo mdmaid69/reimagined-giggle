22798dc0fc453c39cbbe80b0cd8d7e60cb0c9418
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:20:17 +0000

    worse show hello hungry manage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
