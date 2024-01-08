335b46756aab3d45d59f212df25b0fe37779b3f1
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:38:18 +0000

    so chance funny measure left

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def convert_array_to_list(array):
+        return array.tolist()
