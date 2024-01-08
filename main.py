c9e6076d93b098c0419c782a5058c01e866829dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:28:20 +0000

    able correct hate wait move

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
