128e155beab9a729a3ef6960d4465e5156a7d9a0
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:42:53 +0000

    mind pay poor nice chat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
