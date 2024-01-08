db7d5d5115e9300de04244638b27cff2e07812f4
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:15:20 +0000

    something tight small evidence towards

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import numpy as np
+print(np.array([1, 2, 3]))
