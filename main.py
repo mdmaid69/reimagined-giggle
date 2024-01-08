a0cea367354816671753530b67181314a7b2973c
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:07:01 +0000

    delight spare maximum unpleasant instrument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
