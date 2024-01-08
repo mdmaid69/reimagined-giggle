887e13b885148e93a520ac3cdcd9b6c1cf0d548e
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:51:04 +0000

    offend clear keepfrom like sentence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import numpy as np
+print(np.array([1, 2, 3]))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
