059708355b8a70238b427df532a2ab8e93539369
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:35:50 +0000

    deal Ee material flow intelligent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def reverse_array(array):
+        array.reverse()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
