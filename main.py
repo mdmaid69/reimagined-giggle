054cd4e5f3d89cb8b5655ead468016af0dc8d76f
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:02:40 +0000

    age share treatment work offer

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
