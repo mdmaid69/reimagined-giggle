cb8ab07c2b9776f79c229da2fa0f3f1adde76d64
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:34:59 +0000

    offer interview such giveaway pretend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def remove_duplicates(lst):
+        return list(set(lst))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
