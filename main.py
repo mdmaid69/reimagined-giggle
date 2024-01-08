aa16c1cee44ec3e69b65a16a3ea67a58544af9c4
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:23:11 +0000

    swim roll heat last natural

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+print([x**2 for x in range(10)])
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
