9b0d083253b22f1e12f24df77ce7f82fa654812c
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:17:53 +0000

    someone 565end loan exam pin

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import random
-print(random.randint(0, 100))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
