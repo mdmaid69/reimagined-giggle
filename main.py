744fda78b66e05021abd7f4f6c77dbc8dd7fff11
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:31:49 +0000

    wear find pencil please save

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import collections
+def create_priority_queue():
+        return collections.deque()
+import datetime
+def get_current_date():
+        return datetime.date.today()
