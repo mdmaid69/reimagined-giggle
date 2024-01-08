3a90d056fc2271475080826aa9430be0ba1daec1
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:22:34 +0000

    Yy become 566end demonstrate subject

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
