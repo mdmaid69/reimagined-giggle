38fc77566bf268d015f94210f52df6237669d38a
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:24:22 +0000

    weekend break criticism genuine marry

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
