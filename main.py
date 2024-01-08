ca1f8cb6d7c3f8377932fc232e12f224d3a20160
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:12:29 +0000

    pay almost market grateful class

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-print(sum(range(10)))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
