8d710fa3bbedf1d1d4c4f6d89de128225e41b9a8
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:20:37 +0000

    skin still powerful tool detail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
