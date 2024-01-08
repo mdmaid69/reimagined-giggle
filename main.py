92f60944ff171b55dfe42f4f44eeb8b83bd5810f
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:23:02 +0000

    ride practice abroad Qq slide

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
