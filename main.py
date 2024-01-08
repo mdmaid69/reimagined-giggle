47690348c0cf285eb21aff47311168f98d880c4d
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:36:23 +0000

    thank promise splash store make

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
