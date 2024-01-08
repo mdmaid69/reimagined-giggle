692c7472b73539abbc15256b1e998d8573adc53b
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:59:13 +0000

    charge something better organization ordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
