7c38f46783fab584f9f41b0bf2a40f915419d52a
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:12:18 +0000

    rank confuse safe must principle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
