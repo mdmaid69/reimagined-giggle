405f8930f376ee2e05889472af9f6ce87758fa19
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:42:08 +0000

    ago seem place fine nerve

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_power(work, time):
+        return work / time
