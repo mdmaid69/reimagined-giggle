54bba9fb309cbe799866afd7159ff2abf9cf9565
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:57:42 +0000

    please Ff fix typical careless

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
