2408ef96f19f909575b472e73083a9b7d566ab37
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:03:56 +0000

    complicated stiff through spare shine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
