4c62dbf2b96c741707edf37621072eff252a63c0
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:23:03 +0000

    half port part other suffer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
