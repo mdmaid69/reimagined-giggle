9f796b7bc7203c8e7802ce87bc337bddbf8d54a7
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:20:41 +0000

    count talk depend stay bite

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
