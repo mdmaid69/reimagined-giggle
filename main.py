8e5c579080474e1938e7c536035833d8c522525c
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:20:49 +0000

    danger sweet fight except comeover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def multiply_numbers(x, y):
+        return x * y
