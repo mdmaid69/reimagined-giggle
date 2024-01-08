5f1716fc009bc7b1fc92bd02be89e4bd9e9c4384
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:49:46 +0000

    without anyone thing own birth

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
