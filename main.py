95b7abcd58bff7d13edc5e0032405ccab6cc8f4e
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:10:20 +0000

    spring Kk practise comeup emergency

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
