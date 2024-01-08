948a246f351ca25853cd99487932db557b47b5f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:19:49 +0000

    past guess stop lookaround culture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
