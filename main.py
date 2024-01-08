d2f5adda8d233b4c5b3bfc944a43bad8cddcc3ca
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:36:51 +0000

    something breakin night bet workout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import sys
+def print_python_version():
+        return sys.version
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
