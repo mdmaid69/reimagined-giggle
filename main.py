482ec21bbd124f72a1efe659edcec380c3ad4ec1
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:41:38 +0000

    turndown neither will ticket party

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_work(force, distance):
+        return force * distance
