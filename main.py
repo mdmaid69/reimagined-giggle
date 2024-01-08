98c3fd74f8f24dcb8aa54f82965731c5274f6fde
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:07:27 +0000

    giveout keepfrom satisfied neither turnup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
