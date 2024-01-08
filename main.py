43205c159e90700af7eca8d5a25d92e282eda5d2
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:24:38 +0000

    advantage discover can total home

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import random
-print(random.randint(0, 100))
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
