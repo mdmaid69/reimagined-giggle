f282677655659165774a5aa79a55f797b381442e
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:29:46 +0000

    translate offer achieve impressive reach

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
