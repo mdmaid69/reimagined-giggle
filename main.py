cbd3aa4708f843db2a0b846e4630c43bd1681bc5
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:22:25 +0000

    dare murder such exactly day

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
