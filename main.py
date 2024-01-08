27144086dee7fde0c67d9efb89328dce1aa2d1d5
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:23:34 +0000

    fence tip all above board

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def set_array_item(array, i, item):
+        array[i] = item
