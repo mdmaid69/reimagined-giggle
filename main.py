9947d64f1b9329686003f7ff774ddd7d3113fabd
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:03:24 +0000

    out plastic closed noisy trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_power(work, time):
+        return work / time
