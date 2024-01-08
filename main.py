946365388cb50392f954a38ae3610d92a3a7083c
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:09:12 +0000

    scrap steam reach trust angry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
