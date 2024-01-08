b72b8bc082419d5f58ef52052b8faaec4059dcb8
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:36:56 +0000

    chief state trap colour immediate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
