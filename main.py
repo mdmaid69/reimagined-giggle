b280e7438ff149d98e41f121e8a3c9fd373559bf
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:09:09 +0000

    treat interest return early walk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def cube_number(x):
+        return x**3
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
