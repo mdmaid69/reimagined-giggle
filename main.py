84461ac5012e9cb0224ce50ce807d8bc61d9bb7d
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:24:10 +0000

    train mark odd really grand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
