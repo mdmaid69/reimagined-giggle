522e0ebaf0ae0b996d9e1371ee24a38f5ff753e9
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:13:08 +0000

    deaf equal gradually entry story

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
