f0f766d1ec440f39396734f4d2c983192708be9c
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:55:12 +0000

    doubt result act another cut

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
