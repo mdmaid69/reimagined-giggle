7cafe8aa4f404a8d320d366a7a343653b59f7061
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:42:57 +0000

    noone method operate busy treatment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def find_min(numbers):
-        return min(numbers)
+def convert_to_binary(n):
+        return bin(n)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
