d7137201ec67d5a8d5209f1471a684111fd415ea
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:13:13 +0000

    turnoff tap describe trap talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
