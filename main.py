fa98441a14ff8ac06f3d7d3b65c893f5a1312014
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:53:52 +0000

    rescue sleep contact disappointed Tt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import time
+print(time.time())
