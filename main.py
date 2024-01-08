cecbfe970db3d22b51d235fe6b24fbf6bcd1b73e
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:02:43 +0000

    soon opinion tip shop train

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
