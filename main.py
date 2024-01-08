e364a06ebc14183b2043023b0830e452b036a4fe
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:17:01 +0000

    basis exercise skin land Mm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_power(work, time):
-        return work / time
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
