2c0ddb46a8aa813984df4967f2bfc37596cc792d
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:21:33 +0000

    spread runaway chin fold crime

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
