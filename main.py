9918d9bdd0c9720f85a1a4ba431c98d43185c0a3
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:26:30 +0000

    poor blame excellent in effort

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
