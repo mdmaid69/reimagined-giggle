56d2fd797e462c12dc889f787737303635ffad92
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:46:47 +0000

    her tradition cross breakin dear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def find_max(lst):
+        return max(lst)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
