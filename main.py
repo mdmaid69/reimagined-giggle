aaa0af23a3393a1b3f868e15beaa3b877664d0ce
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:00:27 +0000

    ring rush central side so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def add_numbers(x, y):
+        return x + y
