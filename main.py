6e59fd4bcbf0795e14d1f5de00cbc7971fef0f61
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:10:43 +0000

    earn apart visit production sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def print_python_version():
+        return sys.version
