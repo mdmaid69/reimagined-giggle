743ffa2a82a207cef470e37370a30e3d955a3ca3
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:52:09 +0000

    soil film slice day miss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
