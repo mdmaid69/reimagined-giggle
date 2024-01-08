ff6ae88757e3b595f890b0c1dbcb68f465966b0f
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:34:51 +0000

    damage goback(to) accidental die unhappy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
