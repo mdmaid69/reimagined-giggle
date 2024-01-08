b6f7f4b943a15e663124b77a8ae658ce3216ec5b
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:46:02 +0000

    anyone help shame around rank

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
