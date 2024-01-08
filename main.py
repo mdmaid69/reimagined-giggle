812023c24502643733058059404c5c26f0a8a32f
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:17:32 +0000

    organize match himself bother later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
