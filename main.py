71f284cce20e5c9175dd56a2872d8609858e4691
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:55:57 +0000

    actually chance soft declaration disturbing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
