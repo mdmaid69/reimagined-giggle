c0812a3b81bf94bca4d3b1ca9e565935f9d99ccc
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:55:00 +0000

    tonight future bin stretch confused

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
