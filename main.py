482936543334c46fff7e7c6833631c2636da9400
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:48:38 +0000

    neither unhappy comment wash degree

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def add_numbers(a, b):
-        return a + b
-import os
-print(os.getcwd())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
