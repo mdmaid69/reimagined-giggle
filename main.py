9b6d5ab47848571faf8334d8653cf193c1f4eb91
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:19:11 +0000

    Ss rough laugh regional physical

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import random
+def roll_die():
+        return random.randint(1, 6)
