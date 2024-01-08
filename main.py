c76e1347cf0aee7a9ba1bb7e9f4e08f78f2ad6b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:34:14 +0000

    arrangement weekend backwards measure splash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def reverse_array(array):
+        array.reverse()
