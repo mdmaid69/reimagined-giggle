354639433ed623bf48b8bcb1e818e8606e5a7be6
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:45:25 +0000

    borrow their odd silent foot

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_acceleration(speed, time):
-        return speed / time
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
