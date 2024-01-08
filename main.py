3736f660b91e66cad0ec3e68e46ee33fce3a4b54
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:46:39 +0000

    drive pin cope answer surprised

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
