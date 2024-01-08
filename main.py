b6348d6eb6ff02e866e016e1e61afb8f1c1f7ab7
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:30:43 +0000

    live excuse afford exit subject

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
