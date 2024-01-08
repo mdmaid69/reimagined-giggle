1d746a342b8078f26155fe47c88048db8f7d4053
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:56:19 +0000

    drive replace minimum outside stranger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def divide_numbers(x, y):
+        return x / y
