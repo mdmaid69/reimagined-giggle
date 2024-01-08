3605f0104c8ce785b947a4f60325ee8dddde4245
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:49:05 +0000

    coat afraid Tt simple getoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
