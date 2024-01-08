c43f727789f1be491c868a3571ef7be5cbf95123
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:16:07 +0000

    different lost stone wire affect

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_unique_words(sentence):
+        return set(sentence.split())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
