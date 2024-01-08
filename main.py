8a13e435d8e74616169ddb328c046c0976f55a96
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:08:03 +0000

    hurry roll mess write peaceful

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
