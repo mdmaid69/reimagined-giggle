107af355cc5a168db9e6d4c7d507f37ea60ddb25
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:13:35 +0000

    pollution charge embarrassing age order

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def add_numbers(x, y):
+        return x + y
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
