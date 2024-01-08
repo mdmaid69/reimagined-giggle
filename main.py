534322a3a1f2e765ab1bdd370bc9856766a82c0a
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:54:58 +0000

    list base factory path declaration

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
