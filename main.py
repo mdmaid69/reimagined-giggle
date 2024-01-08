7eab9eb4b0b022b10830ce627b64e252d18a4cce
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:33:42 +0000

    ignore turnoff within picture 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+import array
+def get_list_from_array(array):
+        return array.tolist()
