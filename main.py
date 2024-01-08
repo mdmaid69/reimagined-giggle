91003a94bbb9f14360c3d83c9e37c9d4cb1e11e0
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:36:26 +0000

    concerning now come exam face

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
