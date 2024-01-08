638419c73e2ed7930806066de127411f38f2b056
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:33:57 +0000

    lend official public pinch poison

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
