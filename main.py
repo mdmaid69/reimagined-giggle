182332c2a56031683e8edf99c422b2ce41499108
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:39:53 +0000

    choice just pain grateful fault

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
