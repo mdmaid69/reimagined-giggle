5f71bb77aeef1681c7fe29c4eae933264a0ab99d
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:41:30 +0000

    during lookaround cap involve concerned

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_bytearray(array):
+        return bytearray(array)
