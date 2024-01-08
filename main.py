9ab76474a701912dcd90333fe0a4bd4ee3707bb1
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:32:42 +0000

    let tap safety tune truth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import getpass
+def get_username():
+        return getpass.getuser()
