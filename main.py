62b037238d99d77b558cb084e78f6c84a549bb3f
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:38:30 +0000

    speak Ii simple hers lucky

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-print(math.pi)
+def find_unique_words(sentence):
+        return set(sentence.split())
+def sort_list(lst):
+        return sorted(lst)
