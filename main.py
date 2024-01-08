cab0b0ea760c4cabf92a3e6926f9ce2abaee5e7c
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:19:06 +0000

    height ability uponon room steam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_even(n):
+        return n % 2 == 0
