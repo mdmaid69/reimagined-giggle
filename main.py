077449cb7039e89d1266848f337161568fdc4ff6
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:45:58 +0000

    present aim bet trap someone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def split_path(path):
+        return os.path.split(path)
