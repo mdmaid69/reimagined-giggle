34620d1a7ccca29340705f096e6500a3842547e0
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:04:42 +0000

    bill deserve embarrassing study month

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def remove_duplicates(lst):
+        return list(set(lst))
