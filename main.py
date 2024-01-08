396345a48cb9ef5513204d40cccedb7b96966eeb
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:31:17 +0000

    young bring sink size gooff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def count_elements(lst):
-        return len(lst)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
