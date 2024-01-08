5567099c995094a66a4bafa8e6b3c408d24d21de
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:14:58 +0000

    mistake face confuse length risk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
