f549e85d852dab7baa843abbe3d14c88167c6c83
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:30:31 +0000

    comeover previous keen plastic tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import datetime
+def get_today_date():
+        return datetime.date.today()
