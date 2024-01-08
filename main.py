5b662d792934bdb7b88e9b4dfc38c1935efc7d58
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:59:41 +0000

    friendly direct sink mysterious complicated

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_str(array):
+        return str(array)
