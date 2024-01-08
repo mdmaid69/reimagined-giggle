ef7506e8693dc9e72da1c8a91fcab977dc38b1a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:49:11 +0000

    criticize interesting break scared sore

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
