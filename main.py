1efd37be4b154561317190b309d0402f1f8129c3
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:47:39 +0000

    flight measure usual occasion spread

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_density(mass, volume):
-        return mass / volume
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import json
+print(json.dumps({"name": "John", "age": 30}))
