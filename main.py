81ceeb17f69e157c05ae3581dff616309845891d
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:09:19 +0000

    professional fortunately line that bath

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def count_elements(lst):
-        return len(lst)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
