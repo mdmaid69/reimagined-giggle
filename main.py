8db82c3f4f56af130887b83211005b1b150f2247
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:40:13 +0000

    recognize vision since shine talkoutof

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
