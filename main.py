5b8c1de9458b47338d4405f4228969ea5f9f27f4
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:56:41 +0000

    that prize until sale road

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
