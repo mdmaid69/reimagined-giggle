15d0796a3671d6249a225415c783902d627b5ead
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:34:41 +0000

    can empty steer beat letter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def find_min(lst):
+        return min(lst)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
