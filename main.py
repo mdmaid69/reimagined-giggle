b0a92ef82396d28c9a6aa65138df6398231f6f0a
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:08:50 +0000

    in makeinto outside means together

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
