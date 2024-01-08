e68808140ffeb4dec3ace0e7866315b048f36940
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:12:42 +0000

    frightened notice put able plenty

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
+def sort_list(lst):
         return sorted(lst)
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
