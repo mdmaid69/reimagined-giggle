2e070c0a4695cae9c1ad56dafe8da805bfcbc7ad
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:19:30 +0000

    average strict slowly regard dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
