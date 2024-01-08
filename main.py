2cf957595b6052f6f4328eac7ccd4e28104bc286
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:25:48 +0000

    shut suppose argue impression risk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import sys
+  def get_python_version():
+        return sys.version
