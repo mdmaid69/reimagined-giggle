ffd2d3f0cace16c69470d039cc74d5333a197692
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:58:52 +0000

    fasten Vv team continue reach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
