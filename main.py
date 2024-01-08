376369f57b30916b66fb01d84812da2538fe83f7
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:46:13 +0000

    almost community breakfast cry callup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
