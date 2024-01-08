3365f854ff2dc99c8055958db18374ef730ed3d6
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:00:39 +0000

    Cc flood lay explanation trousers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_exponential(x):
+        return math.exp(x)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def get_array_as_list(array):
+        return list(array)
