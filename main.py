7534f6558b28d8ff36cc57a2d2fbdc503391de47
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:39:01 +0000

    post shout suggest conduct greeting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import sys
+def exit_program():
+        sys.exit()
