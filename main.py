ef5e07429587eb4a0726b3f2b17f4246063535df
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:20:14 +0000

    bury danger go back rely

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
