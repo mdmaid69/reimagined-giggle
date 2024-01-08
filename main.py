d4f802795a0d57750ed8fdd54bacfcfca05afb2c
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:07:08 +0000

    herself transport debt balance throw

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
