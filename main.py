59949b6ba63e75c54a6a109864648384251f6510
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:43:01 +0000

    hurry die score dangerous school

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
