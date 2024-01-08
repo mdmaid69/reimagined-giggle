8b415b6eaa4b6949995fd78360eebab8f76fa246
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:25:37 +0000

    bath treat block now spray

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  def subtract_numbers(x, y):
-        return x - y
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
