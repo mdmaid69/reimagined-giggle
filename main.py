57ebcee9ab2ee7b214ed58a59b27210602a7ae3d
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:04:48 +0000

    discussion full nearly receive fortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
