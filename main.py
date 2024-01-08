32e7c69c824ff53f99ee299c81087e22b9790ab5
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:06:32 +0000

    major before land fierce murder

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_counter():
-        return collections.Counter()
+  def convert_to_binary(n):
+        return bin(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
