8bb8cb5ba71e89307598d1ff533fa0fb2c449da6
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:43:24 +0000

    contact notice 573entertainment spray compare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+print(os.getcwd())
