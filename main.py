8b4176d80ca4e96470cff3789651840f74f62156
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:19:26 +0000

    sign contact active pretty pick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
