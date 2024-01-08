b32f09539f4025972ffc9126b3f84e22d87c6582
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:56:23 +0000

    its hurt community best path

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
