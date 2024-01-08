3834bfeec430e72e145ef7b5bdec33f58b9a1c75
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:40:38 +0000

    majority method public trap search

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
