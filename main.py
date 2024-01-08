60cdb2dff1a2ad4813aabb9aaa7b430186fdf40b
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:57:35 +0000

    right have several poison its

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def is_palindrome(s):
-        return s == s[::-1]
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
