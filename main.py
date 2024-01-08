7dcd79a43792f8225e784f3ab2b97cce3f408603
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:26:23 +0000

    professional powerful call train rare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  def remove_duplicates(lst):
+        return list(set(lst))
