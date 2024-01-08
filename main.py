d3a41514da560ade3db56088d6db7a23b890e985
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:53:07 +0000

    close contents realize escape star

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
