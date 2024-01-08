fb7b2a62fb4a9d2fc83304aae19159e9a152868b
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:34:43 +0000

    available everywhere blind 569enjoy important

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+for i in range(5):
+        print(i)
+import collections
+def create_priority_queue():
+        return collections.deque()
