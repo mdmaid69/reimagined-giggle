7f99cd063b897fff369a77842e52953f29483438
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:58:41 +0000

    phone complain possibility voice hope

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def append_to_array(array, item):
+        array.append(item)
