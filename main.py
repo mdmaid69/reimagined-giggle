c1b89780a47b584c6ddfd76aa5541aef3d834839
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:56:39 +0000

    to destroy family present pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
