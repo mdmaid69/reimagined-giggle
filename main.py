351f9b85dfa50c680d3eba34e8145c8f9bb71f80
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:53:44 +0000

    remove organization subject tense side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+i = 0
+while i < 5:
+        print(i)
+        i += 1
