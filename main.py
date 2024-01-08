2fa502266cdaf035b118bf360f0c0e32644cb704
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:44:51 +0000

    Oo trick offer makeinto circle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def square_number(x):
+        return x**2
