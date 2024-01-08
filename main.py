4fd5c31e73f987dd001f1a3ca5ec97e7e6c96d1d
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:52:30 +0000

    turn cheque mention voice sit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
