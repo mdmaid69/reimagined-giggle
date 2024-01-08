b8249c8ca6fa9c2ea8800a357a888f7049db84d9
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:27:17 +0000

    possibility many funny finish hope

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-for i in range(10): print(i)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
