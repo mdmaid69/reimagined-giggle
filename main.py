b015af4ef55930afec98bbd32ce23f9e558b658b
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:58:37 +0000

    rubbish develop frequent sure thinkover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def subtract_numbers(x, y):
+        return x - y
