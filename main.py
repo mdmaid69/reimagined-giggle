f4d660815f82dd8071770f3c437a2edd9bc423b7
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:40:55 +0000

    slide bus picture window instrument

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def calculate_speed(distance, time):
+        return distance / time
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
