391cbe792a23247ce661445ad52187cab7d07bc9
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:23:30 +0000

    record backwards check takeover instruction

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
