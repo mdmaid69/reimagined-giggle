bca742eb1a0f0d7d505c217bc9a6fce50b4fc28e
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:50:41 +0000

    refuse education complaint keepfrom tense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import collections
+def create_user_list():
+        return collections.UserList()
