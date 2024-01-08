d1b0977f5e37369b5d0e9f180dded8c6373567e9
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:36:50 +0000

    butter regular promise expensive constantly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
