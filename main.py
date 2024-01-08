30a46490d5eae367e4ed856778dc5baebf3a21ac
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:41:22 +0000

    himself embarrassed neck complain acceptable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
