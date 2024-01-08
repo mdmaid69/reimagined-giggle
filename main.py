f4522234ab63a51cd594423ae71b4ef6035d33b5
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:57:07 +0000

    final mystery underneath claim since

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
