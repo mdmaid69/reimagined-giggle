b42c9f864ba0c60e195b3c8b707a3471b9c755a9
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:10:58 +0000

    past swallow port soak to

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def split_path(path):
+        return os.path.split(path)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
