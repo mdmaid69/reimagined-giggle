0ffd80918de4533080f26e67bcbc08f5b0c87921
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:22:47 +0000

    team different reliable comefrom conversation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_current_directory():
+        return os.getcwd()
