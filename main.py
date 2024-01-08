b7a7a77f337377461e06001449d7d85540e45724
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:52:26 +0000

    among whether nothing definite section

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
