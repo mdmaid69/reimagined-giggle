eba066cc257e3affda6c14effdb3de6afd03b368
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:12:21 +0000

    stiff light image exchange thorough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import getpass
+def get_username():
+        return getpass.getuser()
