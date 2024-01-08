0ebcb4dfa1da375f91dcb920b7999d95d5dea3b7
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:35:09 +0000

    crash near cut unlucky notice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
