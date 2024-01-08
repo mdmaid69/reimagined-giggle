cd10ba4eb42c6cfe44801d0d8407249770171e3f
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:41:55 +0000

    grow remark half few imagine

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
