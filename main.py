c16282e190714be743b60e1afff3fdf0e3bc8cdb
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:32:23 +0000

    repair confident off pinch hang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
