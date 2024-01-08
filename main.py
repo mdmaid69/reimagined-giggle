8c0a3190cf54a43702abdfb3876b166f93f2e142
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:15:30 +0000

    black emphasis patience blame history

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+from collections import Counter
+print(Counter("hello world"))
