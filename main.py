8edee7151f5a02b854cdb86a7fc47f81a8308ad9
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:01:23 +0000

    lunch minimum check repair satisfied

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import os
+def remove_directory(path):
+        os.rmdir(path)
