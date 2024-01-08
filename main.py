1eb9f518e2f0da182adf1d39bfed3aec78169008
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:36:00 +0000

    taste emphasize prove us cruelty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
