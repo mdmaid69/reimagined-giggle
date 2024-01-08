da562083cb8feae846fd47cfd13a08200e009639
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:06:11 +0000

    slide spare human language sign

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
