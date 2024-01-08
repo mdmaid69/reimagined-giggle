cb8c716c0f7b2b86f8c81b4789da98543260bd37
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:08:30 +0000

    upset win hold pump hot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
