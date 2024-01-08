4fe991af0ae62a8fa1c4d3f82de188d53213023a
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:30:50 +0000

    after give go surprise save

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
