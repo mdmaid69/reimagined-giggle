ed85a7d86a24089c8047a3fd2695cc0fcca84b2d
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:58:54 +0000

    angry clue pinch translate night

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
