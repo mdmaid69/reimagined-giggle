66d0b6e04d0bb89c09ab753c11f068aabf3ce2c1
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:31:12 +0000

    goaway actually worry should everything

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
