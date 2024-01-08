303a84d616b089bef7b6c79d0bbbc17bb2697aa2
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:34:52 +0000

    challenge completely table run scream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def count_words(sentence):
+        return len(sentence.split())
