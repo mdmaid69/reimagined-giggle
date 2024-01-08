0c5357b0724ab1fe75595170481cdb95b2408c7c
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:12:06 +0000

    ask brush actually inform smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
