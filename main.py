76404da74bedbf1da96356ea91eeb030731841a7
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:41:17 +0000

    pay anywayanyhow foreign ground pride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
