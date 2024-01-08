5565d0ec42aa08c9deff206c93ec50f1542a7854
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:41:51 +0000

    Ll approval as passenger steep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
