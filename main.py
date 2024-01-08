15f04a28f2462c4844eb66be537f217530210718
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:46:02 +0000

    basic window bored flow father

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
