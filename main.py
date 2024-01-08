c1000b58d1022bc6aa0f2f1440ec5a9f45b45e28
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:58:42 +0000

    politics experiment dull whole punishment

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
