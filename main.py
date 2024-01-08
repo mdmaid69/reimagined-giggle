a68d8da972f996b6d2de278141e7990d342d61cc
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:25:36 +0000

    refuse natural question light thanks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
