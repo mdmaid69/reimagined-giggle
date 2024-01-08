46350c17f78bf20b087fd5b0bf74b7f11127a054
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:07:21 +0000

    custom medium judge reply touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
