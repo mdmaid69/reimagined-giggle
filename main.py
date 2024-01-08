ed28394aec352c2b275141ba03b2f31f4e813d0a
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:57:34 +0000

    load secret least moment understand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
