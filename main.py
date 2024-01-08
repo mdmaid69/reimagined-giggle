f47e4ab2ae6dbf3041fce5c254b1384247f43595
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:33:28 +0000

    mend thumb generous school otherwise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
