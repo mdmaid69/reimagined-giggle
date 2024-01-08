5065b2287af0d4632af5ea239602539253d5f52b
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:32:38 +0000

    its book workupto set generous

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
