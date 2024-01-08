3775212ca41c6f548b51e904da83438e3fe4fda3
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:45:00 +0000

    lose we separate properly near

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
