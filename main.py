6bb20f35aff282c0451fe76c09070b9309d1b1c7
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:23:55 +0000

    commit convinced lend somewhere during

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
