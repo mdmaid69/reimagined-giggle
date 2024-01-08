34270c6f315424bf13474a3c91e5f87acbebdab2
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:02:47 +0000

    ought matter maximum present terrible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
