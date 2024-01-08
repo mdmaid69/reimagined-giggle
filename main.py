0bf4d2e3a0c536afc3bc103791afe5f2bb0ba8b2
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:19:37 +0000

    trap string runout public declaration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_density(mass, volume):
+        return mass / volume
