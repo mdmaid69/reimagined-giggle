5375be07c1379747351c9407b6f2ef321f2bd3aa
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:22:26 +0000

    appearance perhaps uncomfortable lend past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
