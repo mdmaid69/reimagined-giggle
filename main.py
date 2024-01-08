e9923ff2f4b7abb792a5d33d40bc2632a5baf30b
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:15:01 +0000

    pronunciation guilty spin extra pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def clear_array(array):
+        array *= 0
