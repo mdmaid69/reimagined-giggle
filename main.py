8731fadb00d303740998d3febe2432b71cf2eef2
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:07:26 +0000

    child after me sort hard

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import sys
+def print_python_version():
+        print(sys.version)
