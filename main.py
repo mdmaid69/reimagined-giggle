78788396aad05f9fc4214e39690dd4fed7c4de6e
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:00:24 +0000

    Pp generally we small condition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
