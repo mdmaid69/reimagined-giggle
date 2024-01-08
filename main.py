2d5ac196d2090ce45931185d14228241ced1f6ff
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:21:32 +0000

    square do above environment price

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
