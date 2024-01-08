54af0ed3dd4ca4256e06dd6b909aea97e77357ed
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:09:15 +0000

    talk stand piece flow have

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
