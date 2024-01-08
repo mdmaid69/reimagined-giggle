6d382cdd421a7b9a9bcec53368fbeba0982674af
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:59:18 +0000

    ear takeback real environment in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
