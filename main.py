cafd0a7c7b4457c9ae29859473be2b94fcfa27cb
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:37:55 +0000

    classroom takeout deep soft attractive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_area_circle(r):
+        return 3.14 * r**2
