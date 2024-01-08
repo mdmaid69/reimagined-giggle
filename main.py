2ac43865c6d59dd678cc50a0e7de17adc6f4e8d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:38:23 +0000

    deserve birth shallow crazy practice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import os
+def get_current_working_directory():
+        return os.getcwd()
