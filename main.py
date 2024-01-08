d4e47b4eabaa55397947c9f0b72b33f03440c09e
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:45:04 +0000

    benefit goon cough afternoon similar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
