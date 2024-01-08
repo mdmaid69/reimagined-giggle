166fa5cb64ae0105c398fc16aad32bdef2945464
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:00:55 +0000

    place previous autumn condition lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
