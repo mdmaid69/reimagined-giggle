90119e2babfce3eb3d5ae7c98ae8a28ff49cccf4
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:03:20 +0000

    open creature come effect appearance

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
