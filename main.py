90a284f948cb196cdf9d10c248d197c456f31160
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:02:03 +0000

    breakfast judge least flat further

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_max(numbers):
+        return max(numbers)
