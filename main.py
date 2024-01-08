9a741d2c953526b5f439e661366edebdb89b31af
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:05:42 +0000

    nowhere cheque payment price dress

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
