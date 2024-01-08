c693fb4fe66d7560ea708834417602251fcbb6eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:32:09 +0000

    exactly wander loose class drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
