e40453a9274333af8408470d3752f24100c24e24
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:08:06 +0000

    part mail shop wander prepare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def reverse_array(array):
-        array.reverse()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
