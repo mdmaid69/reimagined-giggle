e90c187641838ea9a02c988caca4c6bfab84e4c9
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:24:47 +0000

    whenever stamp suckme frame equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
