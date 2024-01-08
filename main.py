d54a419d85d0c04e77bef7f22829218112582a0b
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:12:07 +0000

    scream boil rank constantly medium

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
