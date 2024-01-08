b6b443b5f7dd9f2f47d7dcb94e9cd934451e8234
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:45:03 +0000

    disturb fork figure benefit ear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
