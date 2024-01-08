a9e0eef6d78bc3b3eb32f68a3fd0f9eee0429ddf
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:10:14 +0000

    information will sell 566end advantage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def is_palindrome(s):
-        return s == s[::-1]
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
