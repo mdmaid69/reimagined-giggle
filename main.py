4c709955047b9146a8d7a4a6bb300d53789dcca4
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:29:36 +0000

    remark compete separate anybody slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
