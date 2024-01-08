13e0677f555f505268443dbad8c47311bcbc630f
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:46:16 +0000

    replace advantage snow somehow speed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
