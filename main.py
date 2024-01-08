527bf0fa23f8e948b7f23b0d119e8cd9ae55f127
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:18:06 +0000

    comeup tour sea obvious burn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def calculate_area_rectangle(l, w):
+        return l * w
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
