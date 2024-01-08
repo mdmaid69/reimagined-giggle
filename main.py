60b841c05f90ec6c64aed34e652827287f9e13c0
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:39:00 +0000

    even clean speech autumn tidy

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_permissions(file_name):
+  def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-for i in range(10): print(i)
