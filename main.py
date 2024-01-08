16a006b73d1923a66d9c546150bd60558cb1e2a9
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:19:28 +0000

    regularly lean criticism beautiful responsible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_typecode(array):
+        return array.typecode
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
