70d4031625a0be3561911caab10ac71dfc9ef9d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:28:52 +0000

    rich forever trade breakin common

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
