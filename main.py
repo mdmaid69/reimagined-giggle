4805ef3d17fa63d492b86cd099f100b2c9572574
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:20:53 +0000

    willing thinkover concentration unlucky bright

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def convert_to_binary(n):
+        return bin(n)
