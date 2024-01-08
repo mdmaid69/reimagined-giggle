37c6c105f1f223eccb61aef6b6f87f92395b1964
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:34:18 +0000

    tell matter about natural dig

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import platform
+def get_python_version():
+        return platform.python_version()
