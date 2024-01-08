bff6191163c654af16982997772e0683524fda7c
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:22:13 +0000

    true emotion declaration should extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def sort_list(lst):
+        return sorted(lst)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
