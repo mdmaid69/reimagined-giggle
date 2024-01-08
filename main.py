251f4c83ce6398b91b6631691fd6605c40adb28a
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:12:53 +0000

    rent sink sorry return complaint

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def get_array_as_list(array):
+        return list(array)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
