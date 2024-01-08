ee087883f01906c776545d25938fa226cfb78e48
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:54:14 +0000

    transparent history private unlucky best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def clear_array(array):
+        array *= 0
