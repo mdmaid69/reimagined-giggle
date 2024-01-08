5b5c8f2ca21a064ecc8c90655fad75faae2c8efc
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:25:51 +0000

    hello chief prefer information service

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
