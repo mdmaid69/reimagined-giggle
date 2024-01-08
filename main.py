6cc9671131b0e798b2a5eea25519b20c79930350
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:16:20 +0000

    cause interesting press spare themselves

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
