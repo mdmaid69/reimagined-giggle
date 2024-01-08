23c6cb228eb8999408f0c598b5bdbe35262fc607
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:47:20 +0000

    neither buy unkind declare 567enemy

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-def calculate_power(work, time):
-        return work / time
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import json
+def convert_to_json(data):
+        return json.dumps(data)
