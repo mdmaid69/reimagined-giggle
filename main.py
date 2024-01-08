f1dcf97fb722e0a1007d0b4f3c92900256ffae42
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:59:33 +0000

    escape too pattern level subject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
 import sys
 def exit_program():
         sys.exit()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
