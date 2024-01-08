863627a7c5c8207f6fc852279bf4facdde392ea3
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:43:37 +0000

    explain present slice lonely culture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def get_array_as_str(array):
+        return str(array)
