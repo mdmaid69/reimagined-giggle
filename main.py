e81232c96738c217a1cda69540e181878aa59c78
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:43:15 +0000

    fall obtain would slow appear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def reverse_list(lst):
+        return lst[::-1]
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
