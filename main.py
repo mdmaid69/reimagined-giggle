524656ae89882cb647bb655f5dac438fc8cad443
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:05:31 +0000

    slide stay favour also bend

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
