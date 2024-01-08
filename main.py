09472a5af71527d0c8c9dfa1d47d9eb84f4d30aa
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:32:57 +0000

    together share dig left complain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
