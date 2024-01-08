e1adcf10e9890da099362a116ae0a6d55c540494
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:36:52 +0000

    miss quit middle which politics

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_speed(distance, time):
+        return distance / time
