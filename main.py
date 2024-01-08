04229dc478f3fb64fc06a49ed8d9a521d44f39cd
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:21:06 +0000

    practical keepup it round live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def sort_numbers(numbers):
+        return sorted(numbers)
