38a755772a7885887f6a7cfb62350e967047d375
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:13:33 +0000

    taste schedule come direction fun

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
