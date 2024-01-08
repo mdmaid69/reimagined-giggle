5f53dda1856f5b93306739c5ae9cee17c3476c95
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:51:16 +0000

    right mad pencil confuse after

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
