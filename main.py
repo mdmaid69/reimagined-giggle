37a51554ef3e5c4d1b2711c6dad014f4865f7194
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:23:05 +0000

    excited really strike invite shrink

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
