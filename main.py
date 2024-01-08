f5d7b0474e80d53f6a005bdfdef7df724c275a8a
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:09:12 +0000

    decide gap thumb keepfrom print

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
