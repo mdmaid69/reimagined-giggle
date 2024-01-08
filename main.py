8ee4dd336e00fa5ce7dff1a97e8814bbe51a9a5d
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:16:55 +0000

    the clear standard breath brief

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  def convert_to_binary(n):
-        return bin(n)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
