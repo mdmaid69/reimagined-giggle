97330477d77123d601e51b04c3eaca103ee29960
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:39:56 +0000

    matter future then lower active

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def find_min(numbers):
+        return min(numbers)
