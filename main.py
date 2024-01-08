5525960667063b895f3baf8f135b34176933cd95
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:46:47 +0000

    heart cost description sister well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
