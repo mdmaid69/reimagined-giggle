59c059befb5aeb2f63dc5d6c2d2292e2b3d9b623
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:54:19 +0000

    science forgive talk help tip

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
