b220bc0ef350c6c8c5d94feab27c3adb075c428e
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:36:03 +0000

    pretty equal the share car

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-print(sum(range(10)))
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
