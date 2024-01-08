ce2fb24e09b19fabfa6834addfa451194aa3c770
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:34:38 +0000

    gap giveback mad peace surface

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_sine(x):
+        return math.sin(x)
