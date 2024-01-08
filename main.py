a9e247bcf7f8698a6aa961be0a27873ab870eb15
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:50:55 +0000

    off other permission takeoff separate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_work(force, distance):
+        return force * distance
+def calculate_work(force, distance):
+        return force * distance
