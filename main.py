adea3f8ab8d94893da1e9d9f4038878c2a90cde3
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:57:40 +0000

    learn Jj affect wait coffee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
