6ca5786b0ab6059029cdc26aabafdbac01977bb7
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:02:14 +0000

    569enjoy art belief shelter anyone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+print(os.getcwd())
