3225520fa567f92a2620014b178bb828bdfca3e6
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:17:31 +0000

    before size press keepoff against

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def count_characters(sentence):
-        return len(sentence)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def sort_numbers(numbers):
+        return sorted(numbers)
