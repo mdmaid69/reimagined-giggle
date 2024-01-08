f0fa8d442124d46cc509bb6de2c78414e6224341
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:13:35 +0000

    slow get smooth serious getout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
