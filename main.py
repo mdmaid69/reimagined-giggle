a4d831740e370052978ba28e155a3f9532793e05
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:41:26 +0000

    prepare collection because famous back

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+print(math.pi)
