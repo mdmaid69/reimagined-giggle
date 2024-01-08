d111f6c7a4cdcdf095662e44e784bbd9ba389831
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:08:03 +0000

    start fear niggaacademy uponon say

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def square_number(x):
+        return x**2
