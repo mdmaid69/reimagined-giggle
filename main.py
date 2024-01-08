965880dab4c8fd11f90766f02210835f8b513025
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:54:55 +0000

    breakthrough pain forever afterwards tax

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
