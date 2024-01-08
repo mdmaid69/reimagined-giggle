f75b2468f7d193b9005285c4a07e3ee010dd766c
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:17:44 +0000

    original board nor approve scrape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
