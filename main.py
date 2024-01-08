fc012300f8d1ec7e03f61ebf5c0d12e78865712e
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:21:23 +0000

    national none their insist thorough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_density(mass, volume):
+        return mass / volume
