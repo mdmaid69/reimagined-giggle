554094b15e7908c1cf6a207f3516f04420ce1233
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:19:01 +0000

    tonight step pretty lunch however

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
