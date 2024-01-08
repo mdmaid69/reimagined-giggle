50301f560eab0913f2ed72095da7a573d5e92281
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:21:54 +0000

    convince yes reach left meeting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
