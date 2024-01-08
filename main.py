9c071142a33c4bf7fd2582fea110aee7c87d4e37
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:22:03 +0000

    fight contrast preparation deliberate remain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
