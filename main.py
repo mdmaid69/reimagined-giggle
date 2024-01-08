6a298f077c997bb1e767f01025034ef329954e64
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:38:59 +0000

    such keepup slightly breakout strange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
