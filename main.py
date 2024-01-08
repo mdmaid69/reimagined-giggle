4fee7d41e366d42540d7bfee88302b6b58b56228
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:18:34 +0000

    be dream seem offence basic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
