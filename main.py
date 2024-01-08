596faa2bb11c59ff83c700ac4fa72489c5390bde
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:13:44 +0000

    shock rise hurt one Dd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
