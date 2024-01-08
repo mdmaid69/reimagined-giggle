0a9c2754794b13ccb44a8acada8127c62d839e27
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:05:09 +0000

    thorough clean form after hate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
