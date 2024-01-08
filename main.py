e5a62e5794cbaa37dd970d15071ca4258c4bfaa4
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:28:29 +0000

    arrive long level familiar foreign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import os
-print(os.getcwd())
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
