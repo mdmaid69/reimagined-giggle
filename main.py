f91afc1609a986d2766eb4482307d531ba34d288
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:13:56 +0000

    finish polite powerful floor indeed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
