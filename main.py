da121849a83282244de8dce740a5c208029e3ba6
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:14:56 +0000

    floor pour left subject generally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print("Hello, world!")
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
