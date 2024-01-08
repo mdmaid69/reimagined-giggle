32885e766dd6dcf7d5d214381270daa67b7f0067
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:16:43 +0000

    drop stiff tend miss down

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_memoryview(array):
+        return memoryview(array)
