bd85fbb3bc3bd0a652b616b6a90e286ea63d8355
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:15:15 +0000

    scrape communication guard shall Yy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
