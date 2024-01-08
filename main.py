915bd9a4c66f03c37548d560bedf8f62739d1586
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:15:14 +0000

    Itself 566end use eager off

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
