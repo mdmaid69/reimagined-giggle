40a4281d8a030daae7870139c348ca555e5378b3
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:28:37 +0000

    art comeover influence previous comfort

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def is_odd(n):
-        return n % 2 != 0
+  def cube_number(x):
+        return x**3
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
