a82dca74bb563ab97356f37ebb9f4663f4e3eb60
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:29:55 +0000

    attract exam negative Jj indeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def get_array_typecode(array):
+        return array.typecode
