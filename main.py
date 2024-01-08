58a25469a165e065add378e5f4d89d356673a88a
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:15:01 +0000

    under visit consult face curious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
