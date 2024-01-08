66e170b15ec6f446f6a93ed41d0eeb0357b27141
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:33:22 +0000

    talk temperature oil whole makeup

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-print(sum(range(10)))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
