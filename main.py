303e1163652d0eedb97adc8a15c340d5a5f393b0
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:40:56 +0000

    expression sister test imagine journey

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
