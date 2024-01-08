9a0c9c822e3150bc8609aad0f2fb7fc8e2504001
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:31:31 +0000

    near worth comeover swim lower

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import tensorflow as tf
+print(tf.__version__)
