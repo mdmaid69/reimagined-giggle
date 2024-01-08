2f90d7a2cd4327f33a3ba59f8dca39c526c8921f
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:32:43 +0000

    future switch keepout path disappointed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
