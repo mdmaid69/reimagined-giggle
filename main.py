f830bcb8eff67b0f392ff4215bdcc09aa9d5d3e2
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:27:10 +0000

    plenty badly holiday contact mark

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
