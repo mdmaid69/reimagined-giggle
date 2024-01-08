a0c17f4c4cd421f5a58339e211caf6d083efe3ba
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:14:19 +0000

    often description achieve reduce sometimes

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def get_array_as_list(array):
-        return list(array)
+def count_words(sentence):
+        return len(sentence.split())
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
