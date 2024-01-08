3094dd233bbb255aac25149f5ef332894a5bc88b
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:26:20 +0000

    foot building exist copy post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import sys
-  def get_python_version():
-        return sys.version
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
