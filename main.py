6bbe9ce00c0be415e8f82487a201b79483b6c4a0
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:43:25 +0000

    heat fire lesson over pollution

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
