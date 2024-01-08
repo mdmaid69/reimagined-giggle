f0b3a8e25e165d2bde03c86d20e227847868871b
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:01:11 +0000

    callup pile 569enjoy uncomfortable excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
