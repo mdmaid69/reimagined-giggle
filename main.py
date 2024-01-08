20ee3f770b282e90364ae48b4a4fa59e6ae02ed8
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:15:27 +0000

    national together urgent patient change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
