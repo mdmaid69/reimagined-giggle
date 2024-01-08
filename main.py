f6bdba0ba36cf892e5d0d633d0117c1e9a815c9b
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:12:29 +0000

    Itself closely crowded criticism it

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_repr(array):
+        return repr(array)
