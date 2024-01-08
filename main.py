ad3eb133a9fa8feb66491c7cc25da24c7ceacaa8
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:06:44 +0000

    improve realize money crazy straight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
