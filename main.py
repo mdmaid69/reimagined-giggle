902af15f81c1f058f2236a40cfb9724d381032b7
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:00:02 +0000

    yourself off speak judge innocent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
