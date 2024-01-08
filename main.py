0e55042d783ba9c969b9bd6040faed60a88c88cc
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:24:50 +0000

    bunch search model punish situation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
