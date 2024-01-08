c5e1f88f283010a878cd445c99f5e44e428e993f
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:50:15 +0000

    feed pleased recovery organization improvement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
