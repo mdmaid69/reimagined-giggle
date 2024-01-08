adabd7ab598659ec3dd63c245b29d814bf46fe0b
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:56:42 +0000

    continue wait stop mine will

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def is_even(n):
-        return n % 2 == 0
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
