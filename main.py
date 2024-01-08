1ea1a97134f776eeb3a1c1609220e3772143916e
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:42:42 +0000

    between write measure chase love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def sort_numbers(numbers):
+        return sorted(numbers)
