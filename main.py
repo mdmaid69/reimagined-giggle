576131bcbe08ddfeb82671b8bc8ebac5820541ed
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:21:44 +0000

    scrape low expensive some step

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
