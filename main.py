57624981f2429a39cbf9d25cbb1fa9856cefc7b1
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:31:43 +0000

    discuss busy young report negative

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def count_elements(lst):
-        return len(lst)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
