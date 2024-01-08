662c51c6f36b9dfb4f9d9a7ffd20ec170a0f1a0b
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:44:08 +0000

    fall suit attract public complete

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
