cc5992babb5c183cb93edd95176ab63a924a903a
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:28:30 +0000

    or except adjective love form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def find_max(lst):
+        return max(lst)
