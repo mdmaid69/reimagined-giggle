2557bfe455a4c1d75bc5bdefd70fece56e59f244
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:41:33 +0000

    destroy react ocean comfort inquire

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
