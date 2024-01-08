aaa3a71dbc5f68c3b2e5c6d30ecbc2f62d1422f3
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:32:33 +0000

    fierce praise dare tragedy complicated

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import datetime
-print(datetime.datetime.now())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
