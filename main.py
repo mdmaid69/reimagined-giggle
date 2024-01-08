eaa823b27005b9216339c077f5d5b84003b313bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:09:57 +0000

    sticky survive finally early scrape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def divide_numbers(x, y):
-        return x / y
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
