70028c52dc7a494b47237db76c4ab7a09921eb9f
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:47:34 +0000

    Zz tight count fail then

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
