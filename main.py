5fe326d5a2f713e00f19d22285641e3c2fda1a30
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:27:21 +0000

    bring generally brown bad carry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sys
+def print_python_version():
+        print(sys.version)
