7c7a690f9dbb85d06f2cf6c5865c56415467475a
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:15:42 +0000

    exit instant breakdown space except

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
