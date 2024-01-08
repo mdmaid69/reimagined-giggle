2db66f2b459c14747fdc65cdc297d3db9850a816
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:28:26 +0000

    until create narrow preparation flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
