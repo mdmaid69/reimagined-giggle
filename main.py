e18382b12a9b9eb78ad3ac5bac2ec2c38de706a9
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:55:55 +0000

    look usually share Ss pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_current_directory():
+        return os.getcwd()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
