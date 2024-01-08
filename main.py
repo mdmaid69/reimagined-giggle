9c6bb7346b8fb239f34b37e4d6af70e97aafb1b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:30:12 +0000

    damp before close damage bread

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
