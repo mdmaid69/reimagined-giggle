8d2efc3dc791c319e523660153eb6961d1f55531
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:20:24 +0000

    trade meeting such every switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
