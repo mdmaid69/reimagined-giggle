de132ba0281ae482439a3d7df3a1f98bfeebfb00
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:28:18 +0000

    female wander rid calloff hardly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  def is_odd(n):
+        return n % 2 != 0
