0ef5c8ff0a486a69cd3e7ec83104354c38ae83b2
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:49:19 +0000

    influence impressive tread train cook

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
