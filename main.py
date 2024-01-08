a46c538c26d267f3be0319ac4b9fc5a91ff47c72
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:25:10 +0000

    impression tongue claim switch genuine

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def convert_array_to_list(array):
+        return array.tolist()
