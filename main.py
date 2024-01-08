f04b182465419fc2d9e867de944810d07ab60459
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:47:16 +0000

    answer comeback mouth only play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def split_path(path):
+        return os.path.split(path)
