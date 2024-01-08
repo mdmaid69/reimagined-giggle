3ab5d3a1ded7587207d1b43b429c88bb4d7d6785
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:50:16 +0000

    later many makeinto concentration advanced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
