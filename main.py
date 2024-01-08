daa294e650f7725ab98cf6719cc2c17133c38f94
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:54:08 +0000

    rare attempt sure getout choose

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-print([x**2 for x in range(10)])
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def sort_list(lst):
+        return sorted(lst)
