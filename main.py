3d09cd62809dbf1f3ea350d8a2bccfa523cb9bd8
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:17:11 +0000

    private turndown square cheek issue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
