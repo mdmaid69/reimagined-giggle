f83b90196945bf77bb06f709af5ef8cf48f0faa4
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:47:39 +0000

    arrive how amount pause final

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
