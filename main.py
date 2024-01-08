0015151ce7a218ffbc759294ccf21299574042b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:51:51 +0000

    black shake block poison though

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def sort_list(lst):
+        return sorted(lst)
