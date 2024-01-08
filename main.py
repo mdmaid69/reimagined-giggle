5f83bb9c474fdb8b7f8d8712af55bcc4f18ec18d
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:59:35 +0000

    world poor hospital proud bend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
