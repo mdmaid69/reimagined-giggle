e725ff02de10822de292c0f77460130e60467166
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:54:54 +0000

    dry us arrangement activity represent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
