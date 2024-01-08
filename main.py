6b0e3338b1abf693177a9bb011c7577ff4543379
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:22:09 +0000

    war wind guide on cheat

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+print("Hello, world!")
