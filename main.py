ec16abb068ec1b411ad54b760e5027fdcc6ba100
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:55:45 +0000

    exactly furniture paper grade beneath

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
