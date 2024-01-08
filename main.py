a1c9f85eec026c832a4592b24952b05c83b789be
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:11:06 +0000

    impossible row turnon wheel beneath

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def is_even(n):
+        return n % 2 == 0
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
