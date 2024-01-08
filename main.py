e9e42a86d3a61a47a61d0012f9cf56d14ffb8f00
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:53:50 +0000

    accidental that mistake secret snow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
