1345baaea928213417fa0fa904dc875dd44276b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:27:59 +0000

    takeout learn uniform exactly meet

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
