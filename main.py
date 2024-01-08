2ec4e1652f0e43e6b84dd4a01bf8c5a91956d71e
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:49:17 +0000

    order night cost always cope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
