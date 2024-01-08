bbae1182ef36fb96f84ab3a6fb14aec749c0d56e
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:23:33 +0000

    address action formal image objection

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def count_elements(lst):
+        return len(lst)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
