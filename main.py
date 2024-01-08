65e1e68102422945029610422be3e3cd62f9818f
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:02:54 +0000

    twist material pencil excuse rich

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_volume(length, width, height):
-        return length * width * height
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import time
+print(time.time())
