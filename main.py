21c7ae5cacf85b40740753f8dddb724d7591f88e
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:02:29 +0000

    service breath when keepup fear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
