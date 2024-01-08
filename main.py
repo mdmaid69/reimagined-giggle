71183ca51ce3d3578716818d4a16c8365606966a
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:42:02 +0000

    decision break advise sell beside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def split_path(path):
+        return os.path.split(path)
