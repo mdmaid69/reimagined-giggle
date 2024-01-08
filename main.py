29299ac2290d0203c09e431266a85c7464f54d38
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:45:09 +0000

    unfair Ll example risk difference

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+print(sum(range(10)))
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
