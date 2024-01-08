6cad9acafb63f95b7e8affbc7923b403223a6e24
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:57:07 +0000

    zero central work treatment mass

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import time
-print(time.time())
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
