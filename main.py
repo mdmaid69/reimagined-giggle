db609b0f5e1fd6afaac8e062c9c86bc0bebc9f6b
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:45:02 +0000

    typical scare carry mind tall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
