b1cd994518560482a2ed1f80b213ddb04ece5b1e
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:58:30 +0000

    dance exhibition tip mine attract

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def reverse_array(array):
+        array.reverse()
