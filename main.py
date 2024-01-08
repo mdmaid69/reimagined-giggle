16298fe77414cc2724afffb81154926ded3ec44e
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:18:57 +0000

    address keepaway affect language crazy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+for i in range(10): print(i)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
