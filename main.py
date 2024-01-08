3b3c7d204e0753e96db5cb954234503b52d5bd58
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:20:48 +0000

    journey anybody like agreement allow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
