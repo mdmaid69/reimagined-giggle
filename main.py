36442d6a6be01c86cc94b1aad738316453f08db2
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:51:41 +0000

    thanks fast fair benefit spin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_item(array, i):
-        return array[i]
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  def cube_number(x):
+        return x**3
