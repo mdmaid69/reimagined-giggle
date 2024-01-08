ec05ed212f7afc495f04affa844492aacaec4d42
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:38:57 +0000

    healthy form yesterday season through

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-print(os.getcwd())
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
