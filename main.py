a7b85c4d9dd70a92b9c81b7b1b85455af4a7ece3
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:40:44 +0000

    immediate information safe sound little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
