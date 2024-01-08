a7c59ace36e59ccf268c122ae9cd0bff9b083542
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:29:04 +0000

    mistake thumb love direction collection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
