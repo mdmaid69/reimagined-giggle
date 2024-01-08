b46267edb309f45d13b9679119a8fb4ae46fa64c
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:59:56 +0000

    describe card lost imagination on

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def cube_number(x):
+  def cube_number(x):
         return x**3
-def is_odd(n):
-        return n % 2 != 0
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
