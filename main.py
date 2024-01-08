2686fb617405d707d54adff73cb99c890fc602ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:41:40 +0000

    odd juice odd law shop

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import datetime
+print(datetime.datetime.now())
