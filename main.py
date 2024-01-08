c2692ef358a557a9e36334ab97c67c48361b8bfc
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:17:48 +0000

    none really us nearly perhaps

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
