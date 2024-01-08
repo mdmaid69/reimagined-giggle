6663fff100c82cec27da67be4c5e5f59d11fbb51
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:51:27 +0000

    study anybody pretty wish move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
