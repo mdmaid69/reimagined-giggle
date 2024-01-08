9761c7397f2f1e938deb9cca1cb10c5a82bed92a
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:28:19 +0000

    contact container peace blind come

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
