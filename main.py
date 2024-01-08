82b2571a9de7a19121ff591877bcc5efc399e596
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:14:57 +0000

    close runover instant concerned key

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
