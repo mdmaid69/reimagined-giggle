5d5556f50e253de8c7fba552ab329217ad16171a
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:11:03 +0000

    try mass impatient ready correction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
