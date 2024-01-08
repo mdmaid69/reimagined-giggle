fd70f60ce3c9b3edc09677851ed235438c683c35
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:01:33 +0000

    collect replace behave active comeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
