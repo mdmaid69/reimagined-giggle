8046e526fb660026565be1edb89c82da616001a7
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:44:37 +0000

    bye float ever touch goout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def convert_to_hex(n):
+        return hex(n)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
