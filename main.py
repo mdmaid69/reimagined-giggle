e0ac349d0a4017e1d9cd20741630302d96d92b27
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:34:42 +0000

    true achieve easy achievement rate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
