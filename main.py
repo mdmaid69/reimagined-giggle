3182dab6f98d1776c3fbaa5d59cd31b32040999d
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:35:36 +0000

    learn indeed interruption settle uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_list_from_array(array):
+        return array.tolist()
