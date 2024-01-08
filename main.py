d2cc4796179ebb3c428778fc205474d257fea783
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:19:02 +0000

    all change snow reply drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
