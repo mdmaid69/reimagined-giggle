2dce1515d15b77973ee9c4c022852eb82900ea44
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:35:33 +0000

    wide force break cover bite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def subtract_numbers(x, y):
+        return x - y
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
