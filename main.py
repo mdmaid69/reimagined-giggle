b394f78a52b4e3800698909bd2c889d7dce31783
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:52:56 +0000

    for may experience professional proud

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
