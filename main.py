38e0e998d8000a5a0dfdb87ba94f4190d552f51c
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:11:48 +0000

    ocean knowledge runout government tone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
