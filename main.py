84486e1680a5fa207da3d22bcb666208b04a2533
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:56:01 +0000

    prize myself sharp treat available

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
