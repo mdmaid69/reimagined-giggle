c572553c22d61f957e6728014cdb75d45abf9618
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:24:30 +0000

    minute tune part fit common

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
