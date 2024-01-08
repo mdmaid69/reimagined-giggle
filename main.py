c15e21078be7eb2f6dde252f0f43d74cebab91f9
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:30:29 +0000

    ask land basic takeaway among

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_speed(distance, time):
-        return distance / time
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
