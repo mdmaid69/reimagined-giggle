b187ffd35fa01e9be7281d30f800b2aba1fab4f3
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:30:23 +0000

    choose long however path rain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
