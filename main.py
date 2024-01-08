f76cfe63cf5575360e4509095538dedf110abcc7
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:07:39 +0000

    month close sort search thankyou

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
