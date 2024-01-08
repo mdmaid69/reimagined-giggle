4cf1c3d066a8dfcab5c6a678f20f5c921172505a
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:27:42 +0000

    air duty depend mysterious we

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+x = 10
+y = 20
+print("Sum:", x + y)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
