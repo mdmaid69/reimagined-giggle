774648baf4aa6351b2d4805a011a36e25552cc51
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:06:47 +0000

    top danger tread probably at

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import numpy as np
+print(np.array([1, 2, 3]))
