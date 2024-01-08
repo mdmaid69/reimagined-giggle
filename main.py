9393216fe1deadb89123e3a3f3638af6b41579b0
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:00:03 +0000

    ought minor unusual theirs hospital

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-print("Hello, world!")
+  def get_current_directory():
+        return os.getcwd()
