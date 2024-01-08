e7897f04c2fcb066076338ee5303693910a3f84b
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:18:51 +0000

    buy personal sight wake unit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import sys
-print(sys.version)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
