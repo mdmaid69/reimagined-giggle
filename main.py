c567b7c0ef5c107334d33a4af1a79f443eb2ca02
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:57:58 +0000

    call soft really offer spoil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
