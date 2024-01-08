08ade4a31c9dcac8b6e604c314a3595d5babbb6c
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:06:59 +0000

    fast raw uncertain negative strength

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
