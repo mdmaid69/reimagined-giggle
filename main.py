894dc3eb3f7f8f80a639d269861d937fa6b28a77
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:56:59 +0000

    emphasize spot relationship every true

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import array
-def get_array_as_set(array):
-        return set(array)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
