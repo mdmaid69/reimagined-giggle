fc5d017e77d1bb98dcbda3f6d8c63e7f42ee01cf
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:09:49 +0000

    grade Ii correct weak friend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
