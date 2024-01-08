aba8d033f288dc0b598fc2b5038b3712284ac2d7
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:18:45 +0000

    trick sweet cross develop violence

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+print(os.getcwd())
