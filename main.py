28b57be79df7bf803f414a3921aed95317f6164b
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:13:05 +0000

    keepoff still shrink gather either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def subtract_numbers(x, y):
+        return x - y
+import random
+def roll_die():
+        return random.randint(1, 6)
