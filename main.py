5244baec20ca4b0ab2fb3ea3945e3e104bdd7edd
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:34:05 +0000

    makeinto meeting live delight deep

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def sort_list(lst):
+        return sorted(lst)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
