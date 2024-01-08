1cff09c9986d7423e2d4660ab572fcf368bd181a
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:03:14 +0000

    terrible form cool whole little

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
