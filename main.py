5ca2985377eff1a75faebcbf35c44f3d4d931ae0
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:32:16 +0000

    as allow music entry supply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import numpy as np
+print(np.array([1, 2, 3]))
