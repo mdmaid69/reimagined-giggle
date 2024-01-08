ce0509033dee270422e99faaf6fc02551c3bbec1
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:22:40 +0000

    run instrument least alive corner

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
