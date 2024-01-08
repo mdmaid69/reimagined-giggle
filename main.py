e9cc97ffcb3a6c9bd14afe3396d05b11936a860e
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:43:30 +0000

    brief attempt heavy relief story

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_user_list():
-        return collections.UserList()
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import os
+print(os.getcwd())
