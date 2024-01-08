af0df6ae83a561c2dd0f5ee35af968bc0037be52
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:54:14 +0000

    lost 571enough lot embarrassed raw

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
