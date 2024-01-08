91979f3fb05d5b4544e9c83eb9440adaf2f2097c
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:51:08 +0000

    basic holiday spill nice wrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
