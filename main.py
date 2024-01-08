2c98892d012f6abab9456666a04869c46d5b5a2d
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:19:40 +0000

    taste part amount comfortable damp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
