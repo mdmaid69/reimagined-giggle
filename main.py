4b511ea04f14ae788fdc52e456d5328c1b89f7fd
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:09:19 +0000

    guess available patience telephone flavour

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
