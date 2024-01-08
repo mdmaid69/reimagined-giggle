0a67540efb5ebcbcc2ec91ba294161116f678cf5
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:45:54 +0000

    advice remember hit about finally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
