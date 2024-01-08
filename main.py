fce748271b02cacfbf49c187ea288dfc733e6c30
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:31:32 +0000

    release commit drive splash contrast

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def subtract_numbers(x, y):
+        return x - y
