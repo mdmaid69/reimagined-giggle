3817662e6848a65eaf46e4df21df1d32c588d7f8
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:57:27 +0000

    sport see race over holdout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
