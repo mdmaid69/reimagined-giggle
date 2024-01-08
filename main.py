49b85d6e89806d6bc28ad2360b6e3a53f5f0d94d
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:45:03 +0000

    art rich already thinkof workout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
