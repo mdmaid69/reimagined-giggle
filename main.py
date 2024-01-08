3c6db493b59b09f56b3f8810e701f4bf9c52a1a7
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:22:49 +0000

    relationship common become centre fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
