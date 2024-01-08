04ccd58dc4cc58fc7f615936145f7c1c55c53ecb
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:09:26 +0000

    operate familiar follow trade exactly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
