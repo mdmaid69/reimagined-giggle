749739c9a6273257fb476e60937b4e001ad2a8c6
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:31:02 +0000

    distance juice cough possession container

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
