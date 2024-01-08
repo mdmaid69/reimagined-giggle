5c50d6ff8ec8fca88839b6411461b694239f3185
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:38:55 +0000

    lunch prepare weigh brush shelter

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
