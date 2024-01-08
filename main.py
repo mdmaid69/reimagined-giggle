b6f03a85df7ba6b3cea6a08de00623cefc1fc99d
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:30:29 +0000

    inquire stand makeinto more common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def find_min(numbers):
-        return min(numbers)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
