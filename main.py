e43bc49f8ff4e3ce00e8606301684cdba895a262
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:56:36 +0000

    shame warm dry fit answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_current_directory():
+        return os.getcwd()
 import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_length(array):
-        return len(array)
+def get_array_as_int(array):
+        return int(array[0])
