9d9e6c3b0e845e95b86b2d66eddb23a4b3dad8c3
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:48:59 +0000

    load inform zero occasion mainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
