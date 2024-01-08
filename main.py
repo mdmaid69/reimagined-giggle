ad29a846ca48c29e264e98e1e44bb5eaea06d48e
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:11:00 +0000

    journey breath wind police doctor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
