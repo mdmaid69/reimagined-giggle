4493393ef3824494d7d72920188f05bd52feafcf
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:37:11 +0000

    holdon prevent trouble absolutely fair

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
