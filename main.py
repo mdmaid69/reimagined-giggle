732d325a804da157d60f7cae568fa275f622fd47
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:09:24 +0000

    pollution against well pipe against

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def count_characters(sentence):
+        return len(sentence)
