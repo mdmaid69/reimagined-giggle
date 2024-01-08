0654c6c0ca5b651de45db29ff63d79a73d6017d7
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:59:30 +0000

    downstairs steep build comeround interesting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
