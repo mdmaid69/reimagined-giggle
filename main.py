95f0997328923ed27f51ec760916abdb77a7d6c4
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:26:41 +0000

    constantly bear record Oo concerning

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
