7106d935c7b3aa5496c8232a9acca65a5642acb3
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:37:32 +0000

    front avoid autumn deep face

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-print([x**2 for x in range(10)])
+def calculate_area_rectangle(l, w):
+        return l * w
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
