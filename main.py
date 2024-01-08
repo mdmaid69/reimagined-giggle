6580bf10d703dea72e1c0f1bc6d425fedfcd713c
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:24:18 +0000

    scream pair spend shut excited

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
