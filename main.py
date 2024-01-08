dc5880ef018a930f6f9dba3c6a4576a85a0aac28
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:35:35 +0000

    brain hide serious account minute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
