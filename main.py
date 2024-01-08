5641ef2940ce0a56e40b4763e1c0dbab20ca386e
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:56:26 +0000

    pattern play cause move fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
