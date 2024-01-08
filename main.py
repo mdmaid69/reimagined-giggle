636b664a1e5394ead38b0f4ee338ad535cb20627
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:55:35 +0000

    shoot feeling getback getoff house

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
