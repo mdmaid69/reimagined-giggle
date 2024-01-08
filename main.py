36b5a9117e6cbbc70ffd1c2c9fbb00df1eafbc23
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:29:37 +0000

    plate relation mess treat powerful

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+print(sys.version)
