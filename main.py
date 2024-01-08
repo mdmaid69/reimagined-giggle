4e368926f8cb846a721bc2fb422e80e343ff18b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:55:45 +0000

    excellent kick quit miss face

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
