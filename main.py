d8d57404de147eb4bae8d0111cd55fcbccd855eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:59:23 +0000

    relax holdout heavy any determined

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def cube_number(x):
+        return x**3
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
