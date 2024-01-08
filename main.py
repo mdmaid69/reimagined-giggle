b44fa77cb7938aa8243f67865df618c5926b823f
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:03:43 +0000

    under cost wipe come recent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
