be62c9073fc85228c3eee816f2f0f982239ea474
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:35:26 +0000

    put pay flood sky rescue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def create_user_string():
+        return collections.UserString()
