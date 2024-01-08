d09025e3d44eec711e2ae53f39c52b6cfd5f6fb9
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:33:23 +0000

    coat ought it upwards miss

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def cube_number(x):
+        return x**3
