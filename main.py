b69b3dd045f65b68afd0ea38955a54fd19d44f1d
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:52:21 +0000

    promise weak damp exactly minimum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
