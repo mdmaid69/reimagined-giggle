01db44249ca061f86d27ce5ad78efbce98593798
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:28:53 +0000

    gentle experience connect worried main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
