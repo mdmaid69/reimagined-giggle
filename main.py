6ef8374d1d9c300f0cd6ed00eed2daba3469545c
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:23:28 +0000

    every entrance cum rank pause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-name = "Python"
-print("Hello,", name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
