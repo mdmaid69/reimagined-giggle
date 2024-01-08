2fd5d5aefdf117416e7812f742668b7e53165d11
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:06:43 +0000

    connection unable fire dig needle

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import sys
+def exit_program():
+        sys.exit()
+def calculate_speed(distance, time):
+        return distance / time
