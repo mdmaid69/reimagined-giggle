f838746622100ea0f2e0531da2be71560098dc0f
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:21:47 +0000

    settle face interesting like unusual

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import sys
+def print_python_version():
+        print(sys.version)
