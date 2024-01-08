e8b84a5ec9b07c9885d9742d9c09829cafa72493
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:57:46 +0000

    runover nice stick joke sale

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
