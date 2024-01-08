c6b7229e7be5f4b1bcf473a3cc8936fac1937f0c
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:39:38 +0000

    573entertainment usually getaway holiday day

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
