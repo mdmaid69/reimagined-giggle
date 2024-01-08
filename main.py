ae08dd91d9db3dd35d05ffd895425c37434fd89f
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:44:41 +0000

    anybody object land majority laugh

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
 import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
