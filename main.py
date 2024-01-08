317359d4e912a089f513f62f2b6632b267ec83fe
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:29:45 +0000

    accidentally shape receive copy some

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
