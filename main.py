26fdb3a071fb2cd1f74ae6a5680344fd39be3ea1
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:08:54 +0000

    miss answer fork curious in

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
