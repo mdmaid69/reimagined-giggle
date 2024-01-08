f81002f5dc2576817cf44bb6fe503dd822bd064e
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:02:38 +0000

    limit instead value open generous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def is_even(n):
-        return n % 2 == 0
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
