3695fcc0678ee597a5e17cd28c5720afd30801e2
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:22:12 +0000

    holdout decision thinkof shut describe

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_max(lst):
-        return max(lst)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
