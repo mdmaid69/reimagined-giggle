265dd7c7c11c412b08adc5c6084e81d9783ea316
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:51:10 +0000

    bang treat without confuse left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_density(mass, volume):
+        return mass / volume
