f9c40e5e912e73ad0dad3799fe9a11e373908c66
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:39:15 +0000

    direct down cause character come

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
