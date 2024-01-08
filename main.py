94547c130776bf97212899676f22b08bd1a5cdb7
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:19:08 +0000

    protect also score sudden world

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
