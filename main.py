2a6976a6e966dcc9d3a33fcc699110c41a684d59
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:12:17 +0000

    turnover environment sweep pride thick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
