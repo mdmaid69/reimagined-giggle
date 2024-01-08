a8ce05d35d7ffd85162bb7bba45e745a778ae991
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:12:15 +0000

    form cause match unable whisper

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-def find_max(numbers):
-        return max(numbers)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def remove_from_array(array, item):
+        array.remove(item)
