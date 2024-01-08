f15d0b3f5b9ff920c461214daad76e1eb9e6aa95
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:54:35 +0000

    needle wise up communicate negative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  def is_odd(n):
+        return n % 2 != 0
