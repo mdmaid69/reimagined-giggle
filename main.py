47b6cec3f6939dc45523133764d7e2071604fa51
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:38:17 +0000

    clothes note thick turnout dry

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
