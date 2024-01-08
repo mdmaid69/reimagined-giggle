c7033a813181f996bcdcafa0e1ad890f4e82ad22
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:46:59 +0000

    offer trip considering influence call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
