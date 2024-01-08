dbfe9e8dca6f65ee7c21f7fa36185912c2c96568
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:57:40 +0000

    long warning year line consult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import sys
+def print_python_version():
+        print(sys.version)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
