967dc774a53c26887c81813d3dbcd1a4982c754e
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:19:26 +0000

    turnover later standard inquire impressed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
