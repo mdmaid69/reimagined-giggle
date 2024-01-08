79dfa18b850b9748d750d7378f9fa7ed87e877ec
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:26:52 +0000

    welcome pipe start play talkinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
