e8b0678f6bec3ee62fb140d5dfd266d26f78a2af
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:11:51 +0000

    account model anywayanyhow pronunciation pipe

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
