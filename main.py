fcdaa28b504acaec5a76659d19b3af25cb703689
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:09:14 +0000

    sharp true frame quality bother

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
