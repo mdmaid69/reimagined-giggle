7fbe1c240128b92616791da9e43eff07654c75e0
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:44:11 +0000

    tense plant stamp shut matter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
