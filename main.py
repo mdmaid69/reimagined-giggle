4356363f71c3ce96d731d317e62c7ae5dd6b3807
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:57:48 +0000

    easy treatment easy shake live

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_power(work, time):
+        return work / time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
