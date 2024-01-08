2b38bd119c86ba16ca9311dab1d6bdc58523ed8e
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:04:03 +0000

    period choose shopping holdup which

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
