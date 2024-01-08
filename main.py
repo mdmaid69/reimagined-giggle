a1d8d186cc4ed910e27114c6115490446ecb8b78
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:50:49 +0000

    slightly risk settle basically show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
