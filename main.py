77ef026ef4f5bfc2473bc2bed59a662f92cf31c6
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:38:40 +0000

    something judge experiment introduce guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import array
+def clear_array(array):
+        array *= 0
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
