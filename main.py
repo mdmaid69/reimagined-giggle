3028c97b9c7062fb8321b001f45df6733dbf30fd
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:56:39 +0000

    lookup unable care relax hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
