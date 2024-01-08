3e1c9f5e0891688cd40531f4a95fb538f01fb228
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:51:07 +0000

    give Ii many stay part

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  def count_elements(lst):
+        return len(lst)
