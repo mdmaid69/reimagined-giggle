d607e17930b618e72d3b19927406010e11dc7dd4
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:16:39 +0000

    measure boil completely night conduct

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
