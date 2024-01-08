b4f1d0d5c0f9cd11f14d5f2bf7b9cde5a761147f
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:01:17 +0000

    model positive grade cross blow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
