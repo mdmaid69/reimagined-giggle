33b71ff5694362cdd14654753adcad4e0d825d26
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:25:49 +0000

    everyone exhibition creature sudden swing

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def calculate_pressure(force, area):
-        return force / area
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def convert_to_binary(n):
+        return bin(n)
