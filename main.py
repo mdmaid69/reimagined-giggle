f89ab2812cd1518bd8b235a873cc56fbaf41bd21
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:04:45 +0000

    central pull treat recover community

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_list(array):
+        return list(array)
