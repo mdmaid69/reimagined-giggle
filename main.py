3d03a12a31d361733a5f6dfc6fc98f88418f3b75
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:33:06 +0000

    wash gate admit back trust

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
