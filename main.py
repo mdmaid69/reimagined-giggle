c5f646afebe4aa81952dc2ced09956eaa94d32fe
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:18:22 +0000

    total pride commit characteristic jump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
