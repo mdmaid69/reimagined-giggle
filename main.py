048650a65933ea0d4f855e182eba8957e03c9c25
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:51:15 +0000

    environment work they man in

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import time
+def get_current_time():
+        return time.time()
