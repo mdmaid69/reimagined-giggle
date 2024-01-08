184bb2e9367b2ef31b2dfa9f1ea5a9c4f63089fe
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:50:27 +0000

    tax unite burn hard prefer

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def calculate_work(force, distance):
+        return force * distance
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
