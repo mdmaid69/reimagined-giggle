c3773f162f275816f78fc6023698ca57e2e73e2e
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:43:38 +0000

    cover spread slice extraordinary introduction

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
