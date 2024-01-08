1a053d9a53491438a1073f85f8f12e53527a656b
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:06:37 +0000

    certainly well matter he climb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def delete_file(file_name):
+        os.remove(file_name)
+import collections
+def create_priority_queue():
+        return collections.deque()
