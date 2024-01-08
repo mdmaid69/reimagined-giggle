7dd07af4f058b231bb06365408fc38633f47544a
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:35:47 +0000

    fact love force night compete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
