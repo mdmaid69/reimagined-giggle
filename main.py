50eaeff42307d37376d2a75054572165ff7863b1
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:40:25 +0000

    exam Tt share design sink

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def append_to_array(array, item):
+        array.append(item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
