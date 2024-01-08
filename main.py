bcf9a53674c1e8d2268841e2cbcfc65be7d84ada
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:29:49 +0000

    commit passage full holdup fire

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
