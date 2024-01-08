9cb0754b91e737b2a216832e904c3e288fff8a81
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:38:42 +0000

    wipe related material holdup curious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
