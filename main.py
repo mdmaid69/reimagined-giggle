0fdf698fd58b62b5b4555b09fecd5208c5765f8d
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:26:44 +0000

    hear fair comeup responsibility shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def is_even(n):
-        return n % 2 == 0
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
