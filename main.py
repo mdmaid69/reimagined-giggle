fe2e98d4ae6178f894a049923c2fa62f3a1b3a5a
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:07:11 +0000

    slowly breakup upset tonight goback(to)

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_error_function(x):
+        return math.erf(x)
