e6758bd72172e2523cf01d944d3923603a7a7ef9
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:26:06 +0000

    woman again disagree score sticky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def find_unique_words(sentence):
+        return set(sentence.split())
