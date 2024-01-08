6770fe80c4ff3035ba4f49e3a98eea19fdb05e3f
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:36:40 +0000

    lead provide upwards interest bright

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
