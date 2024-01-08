cf8b75affb4ee323bd7fe76f124d2b01b9b6825c
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:56:21 +0000

    degree occasional me break goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
