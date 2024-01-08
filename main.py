03658d22940ddc7d7ee51b23092b4dfe5fbaf499
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:41:09 +0000

    sit meeting risk ever right

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+def get_current_date():
+        return datetime.date.today()
