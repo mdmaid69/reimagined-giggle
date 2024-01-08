43dc42d319c045fb73e8bc6911ba0d70360b57f8
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:02:23 +0000

    keep thank woman crazy fat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
