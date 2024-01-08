ec39ca16bbda7045e40e332d1f9ecf6a33946430
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:08:18 +0000

    runover beginning wash climb fold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-def square_number(x):
-        return x**2
