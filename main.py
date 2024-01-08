b882f1b22271b73a24712fb9a7d74d657bd29396
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:29:49 +0000

    afternoon apart talkinto rub scene

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
