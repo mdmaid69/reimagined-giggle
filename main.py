07c71cce991fbe09a824725a9a7cf4942140a3e7
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:49:21 +0000

    colour undo worth divide creature

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
