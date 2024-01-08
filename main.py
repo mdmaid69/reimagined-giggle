fedb648dcd45fdeeaa31afb5759e65124c45de9b
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:22:39 +0000

    peace very offer possibly open

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
