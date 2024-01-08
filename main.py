6be143ccad3c511c60eb4156f914678eb2e680f5
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:58:31 +0000

    statement decision disappointed content regularly

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_perpetuity(payment, rate):
+        return payment / rate
