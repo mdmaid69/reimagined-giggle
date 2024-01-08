55fbea24eea1df4a35598045c75ab6ad8c54951a
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:06:04 +0000

    finish work activity exciting high

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def is_odd(n):
-        return n % 2 != 0
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
