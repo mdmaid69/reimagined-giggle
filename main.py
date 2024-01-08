ae9e12168493e1ad8e4e4450b57d7877cd326a64
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:39:17 +0000

    stop club match popular towards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
