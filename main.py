0ce22579c108d7a17b6fa5e72e7cece6196084b6
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:52:29 +0000

    child evening content circumstance Vv

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_pressure(force, area):
+        return force / area
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
