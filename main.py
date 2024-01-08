757cdee57c0a0405b055fb0eaf1e1c069ae75d3c
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:24:42 +0000

    least ourselves plant always approve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_base_name(path):
+        return os.path.basename(path)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
