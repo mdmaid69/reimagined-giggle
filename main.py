44f0780ca8f8e81d26780c8fb2297a62f2ad1e79
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:48:55 +0000

    skin stretch turnout law school

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
