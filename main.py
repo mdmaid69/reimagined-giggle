ee60eb64b4949bf7f83de796f7845fb4d1f12c37
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:26:35 +0000

    back learn stone sense minimum

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
