995221e18cb8541410953fcfff0b85c6ef271cd0
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:56:12 +0000

    reply rest welcome odd responsible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def append_to_array(array, item):
+        array.append(item)
