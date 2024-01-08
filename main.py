fdc8f34ec9f07c02dae834ef5b53dda1a0f07898
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:23:20 +0000

    achieve favourite correct excuse well

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def reverse_array(array):
-        array.reverse()
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
