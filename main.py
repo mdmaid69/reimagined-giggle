33074bdd5b1c10a42b4021b051e4e10db7e956bf
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:14:24 +0000

    plan shock makeup join dick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
