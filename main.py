a6f4d7381398f072fe1d3ff1a6bb520a7cc7d3fe
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:31:15 +0000

    she subject total fall join

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_set(array):
+        return set(array)
