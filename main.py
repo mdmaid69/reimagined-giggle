8c9251e393f85c971fc5ab99763d01511c86c395
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:12:37 +0000

    admit exchange shock opportunity stone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def square_number(x):
+        return x**2
