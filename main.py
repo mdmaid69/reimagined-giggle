8807ed22bb61545ea0d88811a733c81a73c4ad93
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:28:21 +0000

    agreement sick read fail front

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
