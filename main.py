4129274e5f5ec3a496db1671a1b9606a2aa3f244
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:45:50 +0000

    male little frequent responsible complaint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
