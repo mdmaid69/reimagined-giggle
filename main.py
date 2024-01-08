510cce029d3489945de7396abd24eedb87249c72
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:47:52 +0000

    add talkoutof demonstrate give mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_exponential(x):
+        return math.exp(x)
