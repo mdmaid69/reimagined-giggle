7025c9764fd5455944d19557a3375eeb9df80a58
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:48:28 +0000

    immediate pleasure shock bath shut

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def set_array_item(array, i, item):
+        array[i] = item
