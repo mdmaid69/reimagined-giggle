3cd68625d15b4fe135f8627e3d0767f12de42880
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:13:52 +0000

    risk capital under right only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
