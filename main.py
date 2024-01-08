4f7eb1fb8947d31b3223f286b4211e7e992e906e
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:00:26 +0000

    injury money takeoff side treat

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_density(mass, volume):
-        return mass / volume
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
