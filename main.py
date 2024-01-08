0bcb1b60a7d46d5f6879639d3446b8a0440eedab
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:00:30 +0000

    concerning survive turn lost bath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
