1a6f25b56ff0c92a3a74c0b6ca7bfef758b96920
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:55:07 +0000

    again board try something process

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def subtract_numbers(x, y):
+        return x - y
+  def reverse_list(lst):
+        return lst[::-1]
