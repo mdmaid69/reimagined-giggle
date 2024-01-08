8caa6f885dce35af0cb4c09a244d47b49c8bbb36
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:39:28 +0000

    it negative wash immediately wear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
