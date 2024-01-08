dd5c69e7ae11f004de6ef43c09849d9d63153b9c
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:01:07 +0000

    feeling myself drink soil national

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
