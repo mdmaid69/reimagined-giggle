2a11f7edbbe9685e834c5185344cc4a1ed9ae440
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:37:30 +0000

    cruel answer react past point

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_volume(length, width, height):
+        return length * width * height
