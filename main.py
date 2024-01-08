7364b0ef532a7f94eac103d38cb57505025d5a04
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:37:46 +0000

    relax serve each us something

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
