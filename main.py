a54a7967297ce09af7205ac3303eef405ecca3f8
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:30:58 +0000

    club sink office mess about

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
