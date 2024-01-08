7d9adacc149b337353178915bff74776b903e86b
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:22:53 +0000

    between bus pick stamp nerve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
