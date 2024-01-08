d14af65a720c9450fa126e704b567f6b529c7e53
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:12:54 +0000

    drop advice march set strength

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_acceleration(speed, time):
+        return speed / time
