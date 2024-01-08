3764acce6705a064ac26b8e96b9b52275a6446a7
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:14:02 +0000

    buy congratulations spin foot radio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
