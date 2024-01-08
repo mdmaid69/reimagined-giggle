3ba4c88867fa448488a145047b9babeb5355fef1
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:07:36 +0000

    characteristic cause confused kill post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
