278af442335447614719f420ed50fd28e47d2e56
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:20:03 +0000

    education different Nn dark father

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
