333cb7ad676039613e370651bc38874f257f8964
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:05:37 +0000

    survive think copy continue makeinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
