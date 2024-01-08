a8bc9421605014a020f434c1e654bbd83e5c8ce9
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:50:52 +0000

    contribute discover smile taste snow

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_min(numbers):
+        return min(numbers)
