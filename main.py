60d50c8f3c189dc3aa76d326fc8f5c7500fd38c4
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:47:33 +0000

    nearly various hello cook close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
