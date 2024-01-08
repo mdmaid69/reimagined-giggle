1ae768fe1b0aea83c0f97bdffd7827b4db49b449
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:02:30 +0000

    bin move mistake physical cheap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
