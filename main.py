6bff7f205ee270b0026dbf8e0da6cec46213312d
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:16:07 +0000

    breath bite skill book attractive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
