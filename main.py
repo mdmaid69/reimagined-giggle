f5bf1eed73feee5457fa10c90a9c2aa67ca3f67e
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:30:33 +0000

    none gap goon train comefrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
