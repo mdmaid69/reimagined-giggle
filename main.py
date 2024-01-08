959a34f8376302952dd6f74bced0fa7007adf893
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:37:50 +0000

    declaration reply collection but row

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
