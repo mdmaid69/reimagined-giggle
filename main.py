b0beb0646543af23c159b568d4a2d48373562979
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:09:26 +0000

    conduct account modern aim provide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
