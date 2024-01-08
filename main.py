628604003b21ff2ee26479dd5d39cb4cdad7fcd7
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:11:08 +0000

    cheat black suit negative wheel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def find_min(numbers):
-        return min(numbers)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
