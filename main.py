8ab4bdd34b81a04029caba30690e235b44aa330c
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:08:11 +0000

    mixture degree now phone something

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
