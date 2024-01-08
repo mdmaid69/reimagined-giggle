4a5fb0e411827768dbc9816709b44876c9426b58
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:22:58 +0000

    trust person difficult proof criticism

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_volume(length, width, height):
-        return length * width * height
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
