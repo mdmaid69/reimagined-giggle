058339376e3a8c4030b721c24b25f441c638bf3d
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:37:21 +0000

    want cheque programme below part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
