3ce1079643c80bee07ed2fa83e736f53270e99cc
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:38:21 +0000

    character bear butter top hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
