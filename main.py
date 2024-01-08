cbd2768068aa56f8d63eb7326f4dc6f5804190bf
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:14:23 +0000

    proof score choice software secret

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def reverse_list(lst):
+        return lst[::-1]
+def square_number(x):
+        return x**2
