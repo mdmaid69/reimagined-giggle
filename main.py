3fe1559381e7af05601ceb56659cecbd0365fa50
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:37:27 +0000

    female cruelty plan set Bb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
 def get_array_as_float(array):
         return float(array[0])
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import array
+def get_list_from_array(array):
+        return array.tolist()
