e5c7bec78f86de6724911887ee7251605f996532
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:12:07 +0000

    callback stir charge too else

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
