4fde6ffb1a61fe2c9cd4c5a4e065fd79a4d02f36
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:53:21 +0000

    check computer generally strange regular

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
