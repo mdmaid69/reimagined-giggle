844fe185027aa2e67f99069624477b0550ad5ec2
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:20:35 +0000

    squash doctor pride convinced warning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
