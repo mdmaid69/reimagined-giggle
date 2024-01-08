ce850bb03f09f9f686aa8213025fc7cf219de57e
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:43:46 +0000

    change recover pass low kind

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
