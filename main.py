79ab8f3251e952b5fc32f90e8e918b2c1a8bd744
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:55:36 +0000

    free practical store wish thankyou

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
