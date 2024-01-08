86c2236cee0b2913c2b064adb80b1fcd160b573c
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:54:18 +0000

    there roll Nn port move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def find_min(numbers):
-        return min(numbers)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
