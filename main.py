b1e74ab41be166d94ae5772a2b90533e8a48e1de
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:55:30 +0000

    club calloff history put fight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
