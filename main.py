ccaf6f59b020c46d074ce418b99fc5fe984e42f0
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:29:09 +0000

    score season brush waste now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_array_as_bool(array):
+        return bool(array)
