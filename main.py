37eea02b45853e6e2b6b69d43f34d4724afb082a
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:23:40 +0000

    dance programme distant everywhere freeze

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def count_words(sentence):
+        return len(sentence.split())
+  def reverse_list(lst):
+        return lst[::-1]
