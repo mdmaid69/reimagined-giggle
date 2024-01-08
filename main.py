7502aadbc5af91d9e8c6fc4b92dd01e85da43ba7
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:06:43 +0000

    lose cough complete brother wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
 import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
