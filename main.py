5a6f4be5931e9c5ac5352ca400a535a35ad32639
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:46:09 +0000

    face chance how if beginning

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def calculate_work(force, distance):
-        return force * distance
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
