2dd31faf7231f63545d4670b76c881b9fcd2d534
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:55:06 +0000

    run choice sticky sure important

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
