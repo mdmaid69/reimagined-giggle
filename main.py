defc6dc5a3610d0d3bae1025df769f7109fb4684
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:45:02 +0000

    copy challenge impressed label team

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
