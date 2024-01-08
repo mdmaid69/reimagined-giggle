7921c210783a8e3606d9c6296e38d761c0d383a9
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:10:37 +0000

    familiar calm prefer demand guide

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
