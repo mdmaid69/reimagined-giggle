a71cec58b97cf4cf5752aff335cd42c20304c54d
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:27:19 +0000

    consider period generally describe bus

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
