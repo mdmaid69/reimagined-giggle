630b7233e8df5b9606ce1c60363129661c27b4d6
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:23:14 +0000

    small somewhere doctor power matter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def remove_duplicates(lst):
+        return list(set(lst))
