9e06b489435ca01ee0997f036869cbaa89dd8fc8
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:51:23 +0000

    teach start welcome rate scrape

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+for i in range(5):
+        print(i)
