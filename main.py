171d1eab08727eb9ba0316edc6fdb79e76a7408a
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:45:52 +0000

    valuable operate rest them point

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
