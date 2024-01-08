6cfe75c83b0480a61701b990ebf3da01b268df0c
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:42:19 +0000

    minimum share right under disagree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sys
+def print_python_version():
+        return sys.version
