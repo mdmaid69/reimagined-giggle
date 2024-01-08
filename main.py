ba4d91191453a166d35ef6f9ebe9957f7a41c39e
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:23:34 +0000

    cut union possible drive sky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
