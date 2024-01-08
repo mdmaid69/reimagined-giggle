15e3590178aed9e29dc3f27ee9b4a5e29cb8f9bc
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:13:07 +0000

    vanish habit conduct cool zero

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def reverse_list(lst):
+        return lst[::-1]
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
