dcb32a890c0a65d116f707ffd72f020ec333f2a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:58:35 +0000

    ahead loss arrangement grade peaceful

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  def remove_duplicates(lst):
+        return list(set(lst))
