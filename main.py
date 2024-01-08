36f5c22b3f6d986cd217b9472b27dff822c6d0ad
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:05:49 +0000

    people callback later ought completely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
