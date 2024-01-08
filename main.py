e8ef3843851280394f880599fa0125e0ec7353aa
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:29:53 +0000

    under many remain bread how

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
