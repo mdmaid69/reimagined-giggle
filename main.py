1f73b23ee829f26a8a8c71223d1fc72f1818da85
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:56:19 +0000

    lift big belong so sign

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
