360c37934f59fcc77ad927f0ab2f021d7f8a21b9
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:45:54 +0000

    dick feature crisis thanks steer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
