a81a1fd57fc3de34940c5491256ec9b94ccdf059
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:57:55 +0000

    mistake weekend broken world once

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
