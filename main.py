b8d6cc869a2142fb45bbd34184beab65b4948990
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:09:02 +0000

    565end tape tidy shall poor

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
