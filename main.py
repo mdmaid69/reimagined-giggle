b7fa01351c611b0b3a10dee73eb87b2b9fc0d44d
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:19:04 +0000

    pocket warm settle thinkof support

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def clear_array(array):
+        array *= 0
