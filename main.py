fe54b8fb5671588aec2379f22138c015c6e89313
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:29:52 +0000

    rank song comedown hit suddenly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
