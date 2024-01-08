54c96509574cc64dce05a4df9c52eac19843f2b6
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:35:15 +0000

    soak guess tend begin skill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import numpy as np
+print(np.array([1, 2, 3]))
