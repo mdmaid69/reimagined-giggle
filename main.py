716ab98066af20cd20070085aa768664210a6756
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:09:24 +0000

    too scream rent angry harm

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  def is_even(n):
-        return n % 2 == 0
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def reverse_array(array):
+        array.reverse()
