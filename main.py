b93944f33c7c7d967c14eb7340a19034da20b73a
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:26:06 +0000

    warm score frame copy clearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def pop_from_array(array, i=-1):
+        return array.pop(i)
