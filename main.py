51c57a6c54650809d62c2abcb7773fab62aac5be
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:52:11 +0000

    arm goon its lost study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
