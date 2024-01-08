dba8090719afd135859203f334959c261a5aa088
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:51:42 +0000

    first circle suddenly goback(to) break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
