97200d9d868dc57419467c7f3af445b1cd77a155
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:15:25 +0000

    effort dig cost rich pick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def divide_numbers(x, y):
-        return x / y
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
