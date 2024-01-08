5b6a6456e6fd4b2fe5637a2557275961fa74a204
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:45:52 +0000

    study rent quarter smile breathe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
