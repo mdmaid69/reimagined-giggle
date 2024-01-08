c3b8c54eb894c6c7ea53ce4047c96e855ff43029
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:07:14 +0000

    cross square warm recognize sit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def find_max(lst):
+        return max(lst)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
