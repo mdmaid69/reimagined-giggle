aca39accc3c9cf878baf059a34b41d040bab0242
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:18:55 +0000

    comeup first callup concerned future

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_power(work, time):
+        return work / time
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
