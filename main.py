98d2f0e6240291dc4df414eb70968bab1da391b7
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:23:29 +0000

    blame could high train hollow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def reverse_list(lst):
-        return lst[::-1]
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
