ca508a23d33ae5a46439c6704193310c11147ce5
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:15:54 +0000

    teach sister grand mother fast

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
