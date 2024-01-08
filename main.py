17167a0ee4aef6c4b8f3a4c1fad4278d9744e883
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:27:51 +0000

    count world include goback(to) time

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
