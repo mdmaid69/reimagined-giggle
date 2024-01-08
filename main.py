1197c6b0774c4a5e52ae3a4b2a8c479528755ec7
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:12:11 +0000

    find however from possibility ear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
