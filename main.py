6fd3ea7d9c01e17788bdadc1038fd11d1a347f37
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:01:26 +0000

    punishment compare no grand congratulate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
