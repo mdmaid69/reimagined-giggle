63292683f040530537cc185d7c6f63a0dce6e8fe
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:42:14 +0000

    really before better responsibility no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_array_as_bool(array):
+        return bool(array)
