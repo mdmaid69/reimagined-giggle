034bbddfc0ad0845cc84f2a04691331412a94e5d
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:40:34 +0000

    common poor lower 571enough direct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
