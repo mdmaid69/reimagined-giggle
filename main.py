bd540533b924cb45b620652a3c8e99a22767a460
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:45:04 +0000

    before limit determined change week

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
