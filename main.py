965921a6c320be77418ac85e8a0a18534d2764fd
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:08:41 +0000

    slightly keepaway impressive door concentrate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
