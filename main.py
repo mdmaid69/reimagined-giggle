e9eda6972d91d7eac7d4b5c02af33b5b312df512
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:47:40 +0000

    match degree emphasize attach before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def is_odd(n):
+        return n % 2 != 0
