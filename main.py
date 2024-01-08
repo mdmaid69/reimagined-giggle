c0825a68be08aca96cd3b09e196f7c94254fda44
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:59:40 +0000

    tell roof contact address slide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def square_number(x):
+        return x**2
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
