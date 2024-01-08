fed73998c2c2cc1ec33197ade6ae09c4cacaeff5
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:25:38 +0000

    land follow empty mass copy

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
