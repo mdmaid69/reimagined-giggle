b4b70938ce0cfbe973fd9e77b7987691bd7be1ea
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:35:03 +0000

    wild attention floor account moral

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
