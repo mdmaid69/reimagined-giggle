fe99797d5f16f55852761a6b22fc4877a44377bc
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:48:43 +0000

    afford outside love frame air

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
