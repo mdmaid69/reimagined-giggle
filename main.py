3fea1e8d1c9e9353dab38369a1bb2f7e49bd5054
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:29:32 +0000

    condition intend advantage list seem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import time
+def get_time_since_epoch():
+        return time.time()
