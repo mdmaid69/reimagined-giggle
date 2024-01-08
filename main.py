4faadaebfaded17b84f5903769a06124a4facf4c
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:30:48 +0000

    this thorough hate limit Zz

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def get_array_as_bool(array):
+        return bool(array)
