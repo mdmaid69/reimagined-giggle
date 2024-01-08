48a8d631301a9102f19730bfb8fb52a790ce4fd7
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:12:43 +0000

    count congratulations violent eye fire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
