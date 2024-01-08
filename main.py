f4e337e1bf92f75ededb33cf967b41b175328a1c
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:47:24 +0000

    drink scare roll pronunciation touch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
