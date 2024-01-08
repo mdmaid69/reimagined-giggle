18174e8f8951aaf0aec258270f3fd0f526bacb1d
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:34:22 +0000

    strike quiet wish wind proof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
