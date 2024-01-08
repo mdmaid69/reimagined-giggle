26ac019d37a1314d470ce72c3f04439b38469c01
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:53:39 +0000

    claim question train foot official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
