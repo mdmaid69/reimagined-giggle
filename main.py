b96460342ff0526620915fe0b7a3739e669ef097
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:13:02 +0000

    roof force deliberate throw invitation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area(radius):
+        return 3.14 * radius * radius
