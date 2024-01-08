ea5d40128e686dd3a82dea7bddc5e5e2c4bc472f
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:24:59 +0000

    discover average reason chin keepout

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
