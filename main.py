64c48eb034c92aaea2f31140bf128d9e06acc058
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:44:50 +0000

    then fault post whistle at

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
