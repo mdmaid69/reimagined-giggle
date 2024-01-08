c7e7f4b7abc8a0f2eafed6c5d4c466774b796264
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:12:34 +0000

    several forgive address computer lucky

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
