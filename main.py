0c1795ba64e6fd9c5249da8cbc763306dccb03a1
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:33:40 +0000

    guard fast doubt jump board

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def cube_number(x):
+        return x**3
