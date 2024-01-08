aac844bb225ffd38060dad12d7ea67d35fada7e9
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:47:25 +0000

    century compare pretty attention damage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
