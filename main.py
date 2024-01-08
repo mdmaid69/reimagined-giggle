9f2ae3dc28c5def816659d17535d25b97ae44442
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:57:46 +0000

    friendly collection let receive show

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
