2b619cbb70baa850d14b9910b5e889c74de4cf24
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:41:58 +0000

    mainly arrange fortunate finally past

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
