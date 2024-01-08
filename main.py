6018ba0a4318682b1232f2848d229801c7810aa6
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:41:50 +0000

    schedule breath crash thank which

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+print("Squared:", [n**2 for n in numbers])
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
