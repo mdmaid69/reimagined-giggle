7ecb2f8358b9ba3d8551f01f39e0d8a5fd9a3d9d
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:03:21 +0000

    thank snow myself reliable clean

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def square_number(x):
-        return x**2
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import collections
+def create_user_dict():
+        return collections.UserDict()
