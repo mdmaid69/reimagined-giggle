5e0143ae7d24f2832ff089a38beb746c3420d1a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:02:46 +0000

    whistle completely wind church to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
