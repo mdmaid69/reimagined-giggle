a96d8ee8ee0b1cd1cc482833e41787082a49e03a
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:06:51 +0000

    plenty moment hide runinto lookup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
