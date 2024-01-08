84e6830bbcc958d47cbfac3e60476c885746c4c2
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:56:22 +0000

    treat almost service effect bear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
