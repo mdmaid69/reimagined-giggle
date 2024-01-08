b7da9f99e8fc88a0164b41c2e0b36785d53ff85d
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:30:43 +0000

    wise wheel translate stage computer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
