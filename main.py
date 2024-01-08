1bcce3cb8d69ccf55ebd8c7bdc45af6bbae0c33a
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:46:31 +0000

    odd hurry benefit pollution crisis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
