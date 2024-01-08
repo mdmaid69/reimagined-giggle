aa2055b6b38afebed8b49633313d034edb23d947
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:10:07 +0000

    closed lock remark effort drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  def subtract_numbers(x, y):
+        return x - y
