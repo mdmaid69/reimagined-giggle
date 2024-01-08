1f874759bae3c4225ca7c96f0686627c672c3625
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:06:22 +0000

    freedom legal cheque disappointed experiment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
