c8ef8c8d5da982419c864156834b756652cd5521
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:00:46 +0000

    adjective satisfied many hello complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
