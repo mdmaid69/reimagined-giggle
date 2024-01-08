4000faa29fdaca49e57fbd83abb004e652b16f1b
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:02:37 +0000

    test work purpose wander breathe

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
