3e94b081a3d764bd547cccdf22c997e8b40af4ed
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:56:33 +0000

    concentration takeback trap give all

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import numpy as np
-print(np.array([1, 2, 3]))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_work(force, distance):
+        return force * distance
