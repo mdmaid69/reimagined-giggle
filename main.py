438f9807809bffaec577dfd4cd56ae9c31b57b91
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:29:39 +0000

    whistle since date nature Ee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
