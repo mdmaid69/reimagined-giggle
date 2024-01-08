b9e8b5f77a8a4d503692448806e696fae69b3762
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:04:33 +0000

    confidence attempt neck extra shock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def add_numbers(x, y):
-        return x + y
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
