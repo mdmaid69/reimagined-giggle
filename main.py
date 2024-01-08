399b4e71116c522643fc9333e3ee1c24b6034acc
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:19:08 +0000

    heavy cheat agree book phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
