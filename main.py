3804a9e9e8617ed0573fa6c1acd7d23aa4820f8a
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:43:03 +0000

    visit unkind activity statement equal

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
