23d0102481347b367884940abfccb750bcdf72d5
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:52:46 +0000

    compare warm OKorokay refuse belief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
