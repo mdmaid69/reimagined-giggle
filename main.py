c7e168099d5677a4785087f8db1b285b74ff4b1b
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:34:49 +0000

    slice sharp exit part not

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
