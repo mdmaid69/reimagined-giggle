7f9b3c1022ee8ec797cfcf52c53d1ed25cc2d6bf
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:49:52 +0000

    soon middle trick turnup party

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
