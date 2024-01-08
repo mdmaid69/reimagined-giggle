212d84ea53b37cd9b5023e7a66e7cfea6d3a3d15
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:48:23 +0000

    entrance through say takeover round

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def convert_to_binary(n):
-        return bin(n)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
