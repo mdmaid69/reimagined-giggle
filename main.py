948010f3ce32c7cb90be968a55232df00219cb22
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:02:25 +0000

    cost ourselves blow favourite respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
