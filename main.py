51871380e456706626ccef5c4d335108cca1cee6
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:41:08 +0000

    fun recovery justice Kk characteristic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import datetime
+def get_today_date():
+        return datetime.date.today()
