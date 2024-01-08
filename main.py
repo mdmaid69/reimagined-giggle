78579b3dd7f11c1264e142884653394ed34214c2
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:02:35 +0000

    join tear past translate compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_floor(x):
+        return math.floor(x)
