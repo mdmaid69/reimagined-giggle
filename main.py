057ab73384429ba0522ed94b6e99cca79ebd2acb
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:40:39 +0000

    again Ii their value wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  def is_odd(n):
-        return n % 2 != 0
+import collections
+def create_user_string():
+        return collections.UserString()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
