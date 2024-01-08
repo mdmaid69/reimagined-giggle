e9bff92ef2135cb31ca0b770ba0eb179ef8e8503
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:14:02 +0000

    attitude visit play its blame

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_int(array):
+        return int(array[0])
