d9c2f6c080f66c81b1733005e868bbf752965728
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:28:44 +0000

    survive pinch taste flat impressive

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def get_array_typecode(array):
+        return array.typecode
