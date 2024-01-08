fdd69b9ced4d01c4e65db2125c13224f815f8f77
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:39:47 +0000

    especially ashamed threaten slight impression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def count_words(sentence):
+        return len(sentence.split())
