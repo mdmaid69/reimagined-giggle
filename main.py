ca9a436bf6d269c22b761fc5c681c6d91b4ab9c8
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:19:20 +0000

    getaway apart slice ashamed deal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
