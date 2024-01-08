1ed91698cb46b4327a55ebd42052bdfe19087452
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:03:05 +0000

    shake phone wave not keep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
