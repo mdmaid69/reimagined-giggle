7e5b98baaeab4299f62305afdf22255d06204ebe
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:33:41 +0000

    contribute genuine sticky godown certain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def copy_file(src, dst):
+        shutil.copy(src, dst)
