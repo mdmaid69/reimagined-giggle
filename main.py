1bb1e108ca8a69bf2dc9139df92483bc48cbb94f
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:31:27 +0000

    region although operation base jump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
