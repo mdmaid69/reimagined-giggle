824a2d7979a412f589f785c93bba0f44cfe8c68f
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:00:35 +0000

    save brush pull pretend quiet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
