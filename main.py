bc4a9edbb0d4b873cc91fdc6bffaa49eb1fe1326
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:54:20 +0000

    let you increase silent she

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
