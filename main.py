b4840f6eaaaaa83c5761246d961dd8a74fc6fe2a
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:11:21 +0000

    pressure injury very bored guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
