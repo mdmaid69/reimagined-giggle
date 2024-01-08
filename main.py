3345c5129fcb7a7491baadea454765bc5696d763
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:50:44 +0000

    level of harm clear exhibition

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import sys
-print(sys.version)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
