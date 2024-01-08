4157f557fcdb5b10dfd9e0b82d2e7822a38852be
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:52:41 +0000

    quiet clothes relative turn though

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_pressure(force, area):
+        return force / area
