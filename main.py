11dd2c78160bbd2b3362b09751a6d355598466ee
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:56:54 +0000

    shame name myself attitude reserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
