029130513a2351db3f71605d944a02aae1ead853
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:14:30 +0000

    bringdown moment constantly bin female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def reverse_list(lst):
+        return lst[::-1]
