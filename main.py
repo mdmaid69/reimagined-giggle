61543efeaf0ec9f3a852629d9fbe183ca304cfa8
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:16:48 +0000

    independent include machine shape entrance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
