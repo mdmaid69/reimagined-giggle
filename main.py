89b0a2a42de50c8d46545d4bc38d9ab7e005b70e
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:19:23 +0000

    heat wonder sure method law

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
