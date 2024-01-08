e7569e24c1434bc50128dcdc9f26e45599479d58
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:02:56 +0000

    comfortable shape prevent otherwise fix

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
