923b741a537d1cd141107ef0b0b0adb3269b946c
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:52:58 +0000

    each block free point compete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def clear_array(array):
+        array *= 0
