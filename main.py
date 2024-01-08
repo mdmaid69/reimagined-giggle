90fa7566c54577a1939ea34e345c6a13ee346532
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:17:46 +0000

    smile warning ignore below name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def remove_directory(path):
+        os.rmdir(path)
