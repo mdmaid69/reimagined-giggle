f7660a8d310ef29746a805b538cf1c3297cde61c
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:29:34 +0000

    pupil same motion Ff getback

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
