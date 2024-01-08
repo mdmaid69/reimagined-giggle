d78b6745fabc72b3186c92581303e4f8036ad18f
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:12:17 +0000

    flat related peaceful save label

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def add_numbers(x, y):
+        return x + y
