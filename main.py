9a34d743e8e9f22be295e1a28102fb8a3ddbcb34
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:56:47 +0000

    decide happen union callout sight

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def convert_to_binary(n):
+        return bin(n)
