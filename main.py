ffa3c67bc81ec29954a8abf391c51f986fadaa56
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:29:03 +0000

    company song off favour fair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  def fibonacci(n):
         return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
