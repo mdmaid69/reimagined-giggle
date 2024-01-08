eb1943b69382a8693c947fa2d5ba4abb9e9807e1
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:40:24 +0000

    unit full account communicate splash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
