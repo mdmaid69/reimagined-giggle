0221fe12e4138b1f6551e707f85abf76c85e2941
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:31:37 +0000

    abroad instruction whichever office style

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
