0ab4b0ecd4e8e277d72fac9372e710b9e2ad8233
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:17:10 +0000

    much female twist only argue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
