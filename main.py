2340522ccfe57fae5e6c91f27e4d86a036da1fbf
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:03:02 +0000

    she connected compare pleasant and

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
