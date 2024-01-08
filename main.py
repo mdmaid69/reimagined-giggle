74464a9c09d8cb39b06647a08570313762424dfd
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:21:44 +0000

    thanks sweep level trust middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
