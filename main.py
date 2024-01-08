7c5b7c97f85c1ef07744c17b7d01681ecb349cac
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:18:17 +0000

    face key contrast wish cook

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
