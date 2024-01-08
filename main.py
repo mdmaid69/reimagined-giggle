79a034386d47abb34cfc80012cafbef46b044bd3
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:25:12 +0000

    pair extraordinary damage print string

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def get_array_index(array, item):
+        return array.index(item)
