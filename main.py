4e846b84c1b3cc17b3c479824b33eab787c78ea9
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:28:50 +0000

    embarrassing clear common still repair

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
