addefe574f5acebf4101b7efd554fde772d2930e
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:41:46 +0000

    book fast achieve either fit

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import time
-def get_current_time():
-        return time.time()
-def find_max(numbers):
-        return max(numbers)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
