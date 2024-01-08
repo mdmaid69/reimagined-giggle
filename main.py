dc2e2e94286d0e0345750f9e96f5b2fbafb40d7c
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:48:59 +0000

    correction relax wise below frightened

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
