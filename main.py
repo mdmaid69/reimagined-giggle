8b188ada876cf2ea4b4c1db5652727ee6c2327ab
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:11:16 +0000

    believe Rr float press for

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
