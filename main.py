a9cf9e144b9f9d06192d7e0d685f6294131f1efe
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:05:18 +0000

    mix strike bottom push believe

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import sys
+  def get_python_version():
+        return sys.version
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
