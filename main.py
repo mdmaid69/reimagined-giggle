98777030ed7786aeafd0c06389a9ec74db42cd07
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:08:41 +0000

    appear loan narrow time cum

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
