04b75e35b94ec531422d9d2231357ac33711c514
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:10:00 +0000

    support instead plate precise ago

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
