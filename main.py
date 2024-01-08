b4e710aea0dc5ac4ef14775d8b72c29033ae65d8
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:36:35 +0000

    dance cover mixture provide rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def set_array_item(array, i, item):
+        array[i] = item
