4ab293355f6ec69cde7e487b481553fcbfae0999
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:45:34 +0000

    left average seriously scream injury

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_typecode(array):
+        return array.typecode
