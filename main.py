83ada538f820ea7cc9247186fcf9fd4c0fb0dc2d
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:07:53 +0000

    joke job cover worst ball

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
