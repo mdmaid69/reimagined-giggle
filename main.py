8f659eeab10721108f172ac6d366ec0752d4abcd
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:48:21 +0000

    face between upset big exactly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def clear_array(array):
+        array *= 0
