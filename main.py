2a9992c10dcae0967b753dd9b1510975289f0b13
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:17:53 +0000

    holdout Kk teach boat play

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_acceleration(speed, time):
+        return speed / time
