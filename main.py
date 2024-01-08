be70f58aab9e2ea377bd166c34294184289116f0
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:43:30 +0000

    sink imagination nature unpleasant sticky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
