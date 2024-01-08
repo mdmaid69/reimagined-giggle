0717b0ce47708a6b683cb9c3bf6a1fba9c979774
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:23:49 +0000

    teach along to care 568energy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_work(force, distance):
+        return force * distance
