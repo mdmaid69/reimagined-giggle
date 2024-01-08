813683ac19a83f3c27198728050c6b2948cb5076
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:37:09 +0000

    anyone talkinto disgusting runover rob

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
