0a8ed4ae0392aa963f2b9ce1d5a4aa241f9aa68d
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:48:09 +0000

    her land hour across table

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
