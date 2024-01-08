a801fb453103cbc6096093bc38a97447f6d47952
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:02:56 +0000

    female very tear gather already

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def convert_to_binary(n):
+        return bin(n)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
