d548568478bbd0d2a7b5e5d072c25a20f4a86bea
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:10:14 +0000

    advice along fit spill pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
