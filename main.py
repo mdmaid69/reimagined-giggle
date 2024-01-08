719a435ad075fc78593247c9625fbfaef001c8b9
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:15:02 +0000

    stay cheap man strict fortunately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import math
-print(math.pi)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
