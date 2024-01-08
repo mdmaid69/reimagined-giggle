2648cee4f7b98862a06f24e73a88cbc873bfdf53
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:20:48 +0000

    brain argument whoever Hh autumn

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
