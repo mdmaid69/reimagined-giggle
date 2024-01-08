94220732ae09eca3c1b5ca001a7da47bd632ca0d
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:52:31 +0000

    heart among by public mine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def delete_file(file_name):
+        os.remove(file_name)
+  def sort_list(lst):
+        return sorted(lst)
