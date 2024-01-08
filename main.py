cdc57be3abb30aaaf57540f6911c92f83c0a7db3
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:22:24 +0000

    also before read second cross

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def subtract_numbers(x, y):
-        return x - y
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
