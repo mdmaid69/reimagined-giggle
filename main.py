a931a7fd4111d4b26f1bbd4de9e86d84689a7c3c
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:16:17 +0000

    565end offer supply even public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
