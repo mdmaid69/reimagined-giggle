76aab06f2d083cf57eff1f3369ac52ff78b991d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:50:37 +0000

    bet talkoutof rate fairly crime

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
