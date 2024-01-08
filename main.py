6508889811ac3c7d1f64faa83de2dde07f857468
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:05:21 +0000

    about world could right taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
