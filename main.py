cc8b58b35994ecf0256745a4967b3859fd6379a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:24:59 +0000

    opposite base steady sing hungry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def reverse_list(lst):
+        return lst[::-1]
