416a562c44f76f1e71dcc68dcfcc7d96ec3ace37
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:03:35 +0000

    afford add squash not breakout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
