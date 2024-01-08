4be5c3fa2a0ae4ec4d8feaf05ae5eee6c10a9261
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:34:08 +0000

    organize holdout arrange tour justice

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def convert_to_hex(n):
-        return hex(n)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
