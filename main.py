97ef4c37fce52ceffab43b95281dd83a43c70e9a
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:41:15 +0000

    might free cut whistle job

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
