1ae920a86d9d76655ec23ef295eb41d7ae038a1e
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:44:02 +0000

    boil cut subject treat across

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
