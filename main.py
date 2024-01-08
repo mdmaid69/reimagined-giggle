aaea2e50d2cadb29be04e49c342abed40385c227
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:18:20 +0000

    mass fly fat slightly birthday

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def reverse_list(lst):
+        return lst[::-1]
