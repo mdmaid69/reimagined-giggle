0699349465a1cbd82c61382813bba8cda66eac35
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:32:52 +0000

    usual track switch force suggestion

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
