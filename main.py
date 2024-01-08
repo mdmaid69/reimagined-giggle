5af3e73619509ac4de10d0b5775b99385c280237
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:39:12 +0000

    lot pride lock threaten boring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
