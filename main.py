985523c72d15498ab942ddbdcf9eec74fae6db48
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:33:41 +0000

    clearly once open music reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
