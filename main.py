0a95ee3f5ca5f7c94fbd0e6bbe9ad3fc506af599
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:32:52 +0000

    today chance keen morning callout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
