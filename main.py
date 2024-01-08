04ad4e51056eba673c8c308f2f23267320785974
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:49:57 +0000

    sweet happen lonely achievement high

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
