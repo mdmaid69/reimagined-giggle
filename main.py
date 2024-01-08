7bd12080958acaa786caa925aca0f41e6048d4f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:40:33 +0000

    bet talk teach suffer bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
