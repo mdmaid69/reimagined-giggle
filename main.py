40f1e7807042c83d370b783395500cb6d7bcab32
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:48:41 +0000

    name goup object ago holdon

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+print(re.match("h.*o", "hello world"))
