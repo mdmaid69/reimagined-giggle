14a22d2375edc4685554ec1b534aca688a37a364
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:34:25 +0000

    praise please computer follow through

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
