14b6efba48b2b8bda8d8b51b0d31f7b5d685e749
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:37:16 +0000

    imagination doubt Qq arrive switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
