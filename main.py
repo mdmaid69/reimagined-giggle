17f74ab78342b383dc68a971c3fc6552edbd7287
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:00:18 +0000

    apart oil voice shall order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+name = "Python"
+print("Hello,", name)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
