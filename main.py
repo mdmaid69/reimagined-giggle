4b0af235d7f38d5419d3c4b175a2b6ae20fabb4f
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:54:32 +0000

    believe female settle Hh fierce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def reverse_list(lst):
+        return lst[::-1]
