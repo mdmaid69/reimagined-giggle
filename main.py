fb03eb5b63056b6a7cb20426f107875b1477c175
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:44:26 +0000

    argue anxious direct vote result

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
