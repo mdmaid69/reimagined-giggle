ba4bcda63affae679439691406b58631691148ad
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:50:58 +0000

    prove talkoutof sometimes limit accidental

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def get_array_as_set(array):
+        return set(array)
