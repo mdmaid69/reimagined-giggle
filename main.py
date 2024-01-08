f35ce54e9539edfc42d442393bddc351c6908bf4
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:19:29 +0000

    gas anxious creature convince wrong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
