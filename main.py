100b1514a6bedaad9cf99b3f32c94e6630dc20cf
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:28:14 +0000

    dull over proud scale brave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
