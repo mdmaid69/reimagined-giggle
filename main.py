041fb619a97b554d7f57c43664bb00823b31da2f
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:41:16 +0000

    ourselves spend deliver something there

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
