695c7871a5be1bbd0e60485959a2e03ad668fb1f
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:56:14 +0000

    pull drive duty official together

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_speed(distance, time):
+        return distance / time
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
