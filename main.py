c898cebfd5b5838b6b821cef4cdb3a3d74ea20a3
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:36:47 +0000

    dance opposite exercise old wash

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
