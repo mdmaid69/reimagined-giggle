3432af4f1babc83a20bc9555d4f8cb0437690a6a
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:59:29 +0000

    board tool disagree positive amount

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import os
-print(os.getcwd())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def remove_duplicates(lst):
+        return list(set(lst))
