07b87774e76b7e26b5a75caaf2954734e524b416
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:10:33 +0000

    stretch hide actually now front

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
