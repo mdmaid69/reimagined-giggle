f9911d749f6792831bc1045ccba9e45d9e733e9d
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:48:19 +0000

    emphasize pain influence window cap

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-from collections import Counter
-print(Counter("hello world"))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def remove_duplicates(lst):
+        return list(set(lst))
