2ce59136a41ba4c9a3f9edbd70b04702a14f0abf
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:57:16 +0000

    imagine her obvious heat brother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_current_working_directory():
+        return os.getcwd()
