ac58bb18e84ed95ab96437ba40a708762ed107a1
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:07:54 +0000

    each number world visit steep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import datetime
-def get_current_date():
-        return datetime.date.today()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+print(datetime.datetime.now())
