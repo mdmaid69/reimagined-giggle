5a57594cddaecafc61d5901d1764c35f23bb87d1
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:46:06 +0000

    project grand waste ability embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
