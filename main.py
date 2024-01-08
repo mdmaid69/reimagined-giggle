611f893e15661f0385543d8007c9fd204abf80d6
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:57:48 +0000

    connected file right too spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
