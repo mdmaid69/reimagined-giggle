3684452c7e2be0d1ed012298e625a41f59bd6b23
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:03:54 +0000

    suck see attention sport closely

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import math
+def calculate_floor(x):
+        return math.floor(x)
