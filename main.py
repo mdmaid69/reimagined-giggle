b0acccf5ce1f8a67b6e3962406e29916dda64f7a
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:43:42 +0000

    so point bring most and

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def count_elements(lst):
+        return len(lst)
