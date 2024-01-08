46424479742daa690eb041108f08ef4952b05f42
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:02:18 +0000

    better attention cruelty tape sink

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
