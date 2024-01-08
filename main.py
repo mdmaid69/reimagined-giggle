c864b1443b9c5969fa2c701aa13cbed2a7eb7b27
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:29:04 +0000

    mysterious game transport notice borrow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-def exit_program():
-        sys.exit()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+print("Hello, world!")
