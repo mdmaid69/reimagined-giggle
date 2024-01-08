ecf8098b8240629ec24c910ed9a1d39f8f403b00
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:33:27 +0000

    issue lock right pollution guilty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def convert_array_to_string(array):
+        return array.tostring()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
