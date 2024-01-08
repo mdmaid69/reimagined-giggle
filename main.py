70e3a4411733604d00a8d6acd187ac3d9a1e4de2
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:04:19 +0000

    arrive loud rather finger pile

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
