d149153e089ce36a0e12a9b124d4d93f9879875e
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:36:58 +0000

    unfortunate news willing cruel throw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_typecode(array):
+        return array.typecode
