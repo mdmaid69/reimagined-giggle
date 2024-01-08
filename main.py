db0bc5ae8c90a25f0ca8f50ecfadb17a0a8dca3d
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:31:19 +0000

    what wind insist turn before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
