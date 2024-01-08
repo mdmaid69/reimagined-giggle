f45f8c4d892b0991b81ac5c3373ca3da1d163db4
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:40:38 +0000

    minor string translate connection tough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
