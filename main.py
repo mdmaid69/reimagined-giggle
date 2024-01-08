a581b4fff2fbdcd2de2d3c45670b3c86cc365bd5
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:58:53 +0000

    him wish class where male

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
