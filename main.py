7bd1e74714101b533e0b123b6e962ca081a1fb86
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:52:37 +0000

    lead offence serve here straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
