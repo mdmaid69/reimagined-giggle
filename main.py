33d3c2f2cdaf9d0e9ae5833d8067cb29b6f38be7
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:33:12 +0000

    line within underneath both through

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
