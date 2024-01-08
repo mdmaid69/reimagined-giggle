0ed4ea33771f8ef583e06049a473ff96e64cf71e
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:36:47 +0000

    beyond length ring ordinary worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_acceleration(speed, time):
+        return speed / time
