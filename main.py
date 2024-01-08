f4eddd059720610ba2167229ecda8c73623e0b39
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:44:11 +0000

    practice fail narrow of recognize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def square_number(x):
+        return x**2
