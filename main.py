24f59c7df5101c447e8b68773d33c4b4b5dffef7
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:27:32 +0000

    beat lookafter period no quantity

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_volume(length, width, height):
-        return length * width * height
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
