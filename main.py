79696a549f4aaf6e8d376b28f01a181a516177a2
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:32:33 +0000

    list doctor organize almost surprise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
