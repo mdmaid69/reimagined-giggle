079628b785e5baca66e62a82116cba2c3d128bc5
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:55:40 +0000

    callup believe woman try up

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
