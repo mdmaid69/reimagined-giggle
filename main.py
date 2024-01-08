8f1d6506f0fe3efc8fb29ad963fd10ca53f27043
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:24:45 +0000

    shopping comeout cum group disturb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
