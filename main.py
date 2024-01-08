426f0477ab5eca035e86939ab33c08e5d7e989c7
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:37:39 +0000

    means clever objection by target

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-def find_max(numbers):
-        return max(numbers)
