3866dd3564352bfbc85ca73a2d7f6c357a46adb2
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:37:32 +0000

    however happy touch burn wrong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
