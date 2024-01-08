abd753b4b420bb007e6e9e3f98c33714f5d051a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:18:58 +0000

    present react argument scale cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
