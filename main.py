47fb24da4a0334a8c2dd6ae69eb724394a8a66b4
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:44:38 +0000

    basically temperature field suit principle

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import math
+def calculate_floor(x):
+        return math.floor(x)
