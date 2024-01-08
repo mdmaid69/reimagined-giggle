8095966f59f439be311f3bca3707552b5745eb6a
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:31:23 +0000

    plenty front excuse blue effect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
