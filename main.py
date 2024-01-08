eb229991ada8dde35229ce6f0fc1a7db571081a0
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:37:40 +0000

    service ear complete callup disgusting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
