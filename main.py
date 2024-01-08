907254438a62208491641853e9f99dcd0f5461f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:02:38 +0000

    meanwhile luck note exaggerate exercise

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def add_numbers(x, y):
+        return x + y
