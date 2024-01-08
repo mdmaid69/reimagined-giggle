209314e400e54979e8d1c6797da741b91eefa612
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:00:55 +0000

    kind cover whoever generous comeoff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+print([x**2 for x in range(10)])
+import sys
+def print_python_version():
+        print(sys.version)
