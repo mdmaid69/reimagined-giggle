49961b6c2c9edb05800e7c848bd8a524d1db2573
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:08:46 +0000

    pile slide joke tooth we

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def get_array_as_set(array):
+        return set(array)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
