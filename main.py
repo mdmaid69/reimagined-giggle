f53d03b82d756b0d2d24f2ce3930a62f7050148d
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:47:29 +0000

    present runaway central unhappy change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_bool(array):
+        return bool(array)
