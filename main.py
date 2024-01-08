f20afab6a2f71a8a4a060b5e61d0ebc03638d6da
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:48:21 +0000

    direct record scream mine need

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
