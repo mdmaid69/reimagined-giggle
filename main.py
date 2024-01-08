c1251d708924931e11b17a2a6f31b8077d667c6b
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:28:58 +0000

    available although natural impressive change

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
