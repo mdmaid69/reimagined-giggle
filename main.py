4930af037bd9baaf5a2ee44f23ba36f38ca678d2
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:34:28 +0000

    favour support early clean we

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
