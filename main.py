82a448aecb9cf977127cea82e0531351a6e3bb0a
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:17:22 +0000

    thought beautiful praise address demand

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import collections
-def create_stack():
-        return collections.deque()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
