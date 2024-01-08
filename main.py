5d81d517a883d6eb5d25bd98c2fa30b296bcee75
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:46:45 +0000

    edge address period turndown memory

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import sys
-print(sys.version)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def get_array_length(array):
+        return len(array)
