2991165dc236cbd5d8cba76b67c2a319fb523594
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:24:02 +0000

    afford quick another extraordinary for

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def sort_list(lst):
+        return sorted(lst)
