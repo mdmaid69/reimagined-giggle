3b3f5c71b9a0d72fa6e79cf0382e2cf07aed1e15
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:45:46 +0000

    list sweep agree tip underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
