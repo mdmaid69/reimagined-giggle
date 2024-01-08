9c088d974e17d901a13fe88652f841e779ceaca1
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:57:17 +0000

    bet cum hardly relationship broken

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import sys
+print(sys.version)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
