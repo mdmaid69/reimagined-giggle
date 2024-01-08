a3ede79f7ad414224f575da194df609e29177cd0
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:20:05 +0000

    right we gate usually content

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
