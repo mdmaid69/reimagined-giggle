24ce34ae307987c86a964bfba75979ffc959533f
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:51:52 +0000

    hurt crowded or class mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
