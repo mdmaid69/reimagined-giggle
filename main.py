1d711b06a2c2a132a5321b5b120215aab2e1b9f4
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:29:51 +0000

    matter achieve entrance convinced forget

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
