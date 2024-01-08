9a69d9ed503d1f10074cfc23ad5e67bf218965ea
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:59:17 +0000

    through tone term light lead

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
