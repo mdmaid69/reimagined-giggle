e46ff740190ed05cd871c1004720ead64fe68c70
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:03:45 +0000

    increase deaf complete underneath tidy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
