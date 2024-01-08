874062c3e79683923b0b8dc9699d72e9d39fc45f
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:16:40 +0000

    agreement weekend report empty boil

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
