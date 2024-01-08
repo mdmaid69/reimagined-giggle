da234cc3b55e4ea1e21caa98474fe7468ee00357
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:26:46 +0000

    sorry regional spite worse pipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def convert_to_binary(n):
+        return bin(n)
