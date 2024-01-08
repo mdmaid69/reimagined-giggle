c34c24588faf21fb4dee505d02269c295ac04704
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:44:48 +0000

    try start rubbish this regret

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
