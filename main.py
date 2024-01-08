70ffafe7010158ce764cf8fd6dcda7332ec20455
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:01:48 +0000

    need pure that beside risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import tensorflow as tf
+print(tf.__version__)
