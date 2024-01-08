d6b33d423317bbc657f790305effa4abd121872c
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:24:52 +0000

    punish weekend slight opponent determined

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def calculate_power(work, time):
+        return work / time
