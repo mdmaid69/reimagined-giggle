511250a0c062b21fc4e2281ebcbd37df8bc4d810
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:56:29 +0000

    cruel receive drop world suddenly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_power(work, time):
+        return work / time
