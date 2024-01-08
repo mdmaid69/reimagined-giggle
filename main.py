ecb527edaac6e6a41fa47ba911d052689dab4fd9
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:03:59 +0000

    accept turn forward accident satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def remove_duplicates(lst):
-        return list(set(lst))
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
