06426451b0a0707a656e0b58beffd86c91e5f6de
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:18:16 +0000

    wise payment change Mm married

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 import math
 def calculate_ceiling(x):
         return math.ceil(x)
-import math
-def calculate_floor(x):
-        return math.floor(x)
