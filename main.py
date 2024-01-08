7234f6b4b9ee564714385ba3d4711334e075c218
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:50:54 +0000

    question back wind base lookup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_work(force, distance):
+        return force * distance
+import collections
+def create_user_string():
+        return collections.UserString()
