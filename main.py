839895a570a015e196c94d53c7d1d4069e98fa26
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:48:23 +0000

    move comeoff takeout Uu contents

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
