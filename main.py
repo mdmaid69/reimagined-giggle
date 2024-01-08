918b76f81807585e2af76f981eb881df7c10c9a3
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:36:50 +0000

    road constantly fat skin represent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  def convert_to_hex(n):
-        return hex(n)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
