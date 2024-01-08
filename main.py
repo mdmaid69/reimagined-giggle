2935fb63b3a942fecef75fd3560f0f7b74dbdc05
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:12:20 +0000

    sore human nonsense equipment noise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def multiply_numbers(x, y):
+        return x * y
