bf7d9e2b5c03f152c51df72c8049ad4318b87c4b
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:31:26 +0000

    fast value certain reply later

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_pressure(force, area):
+        return force / area
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
