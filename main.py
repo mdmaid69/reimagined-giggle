402ae69cb2283b5c5a154ef247a6c35e852fbbda
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:52:10 +0000

    eye achieve until settle case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import logging
+def log_message(message):
+        logging.info(message)
