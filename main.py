4f04fae315335342f39c167693172525714242ec
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:27:23 +0000

    visit hope often border process

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_area(radius):
+        return 3.14 * radius * radius
