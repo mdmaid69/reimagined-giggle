d34077ba7220b3b21297afba6529aad0539bfd7d
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:22:03 +0000

    quantity address display cross wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
