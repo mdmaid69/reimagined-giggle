14141061699bf25969f58b24d99335600627f389
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:26:39 +0000

    undo description over rub little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
