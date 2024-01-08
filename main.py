2b436940f625cd03de21244be1b5f9556af289da
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:22:45 +0000

    side stay challenge giveout visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
