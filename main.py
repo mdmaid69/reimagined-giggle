88acc6356a082f2bb6a45a2e606ba35951c31af4
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:44:28 +0000

    someone dig friendly leave wave

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import time
+def get_current_time():
+        return time.ctime()
