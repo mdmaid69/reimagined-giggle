2f22a226db922eff9d847e988995ac0dd8873346
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:18:18 +0000

    ahead goon part collection tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
