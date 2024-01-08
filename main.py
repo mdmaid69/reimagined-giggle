c1a0471335eb89c940b088338618e48b0a543331
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:48:58 +0000

    criticize recognize practical takeout water

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
