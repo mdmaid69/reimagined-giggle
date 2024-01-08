f58dc4fbd4c3161d1b5b0a14b87410f07d6d4c23
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:27:00 +0000

    Cc Uu main giveback lead

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_int(array):
+        return int(array[0])
+for i in range(10): print(i)
