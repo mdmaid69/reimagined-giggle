96ef39c93712e44e6376d104b35d13fc98540a88
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:08:07 +0000

    cause takeaway turnaway valuable operation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+print(sys.version)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
