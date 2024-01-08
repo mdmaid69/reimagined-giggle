2b93e9dbf6db3c6e9caa2033f8977c74e00ca010
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:58:48 +0000

    except objection bringdown hurry Ss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
