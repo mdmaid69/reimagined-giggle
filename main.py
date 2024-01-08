16fbd082007218b8e48a5721636dc250527ae07b
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:29:37 +0000

    clear glad connection neither welcome

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import sys
-  def get_python_version():
-        return sys.version
+def get_array_length(array):
+        return len(array)
