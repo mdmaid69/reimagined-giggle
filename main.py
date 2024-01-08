89f2617b65b7735010a1cce3845ff49efc42d4e5
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:32:49 +0000

    wind excited show thin similar

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
