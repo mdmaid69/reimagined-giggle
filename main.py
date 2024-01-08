0dfe49b06eeca8e56632ecba5af262ff81455e73
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:04:02 +0000

    careless contact tune wash trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def square_number(x):
+        return x**2
