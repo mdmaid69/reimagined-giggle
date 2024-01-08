8403a0aaa416be41c500a161f9ec1bb9e823cdab
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:55:58 +0000

    pick cause possible study like

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-print(datetime.datetime.now())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import tensorflow as tf
+print(tf.__version__)
