0f9728c0c44e9bda7d6668e342ae1a26b8f09bc4
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:07:48 +0000

    guard threaten love actual quit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
