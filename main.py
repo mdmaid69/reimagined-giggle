4c73fda37d3517bc4d32b1cb04470631542dafc0
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:11:20 +0000

    kind wash visit count advanced

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def find_unique_words(sentence):
+        return set(sentence.split())
