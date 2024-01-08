e0e3d9baa8763db5347835a8ac49d3f1046b9aca
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:30:38 +0000

    ring trick skirt pity several

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def square_number(x):
-        return x**2
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
