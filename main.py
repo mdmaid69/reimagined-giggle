6e85ec2e909641caef1a7786014d72ac5aa0fd42
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:45:17 +0000

    warn care responsible Cc safe

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def greet(name):
-        print(f"Hello, {name}!")
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import time
+def get_current_time():
+        return time.time()
