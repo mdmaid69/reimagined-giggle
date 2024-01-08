32d197a4753a0c721f6d80ccec25a1ac538c4de4
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:59:03 +0000

    bring eat goon lift contain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import random
+print(random.randint(0, 100))
