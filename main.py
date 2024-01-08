d739b9244c6245bf14f485c26d9c00a06dd6224f
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:10:39 +0000

    well yours pin catch different

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
