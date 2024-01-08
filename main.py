fbf714ee40c9e77a572f8bb90384848450fba00e
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:50:23 +0000

    complain pipe conversation shallow spring

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
