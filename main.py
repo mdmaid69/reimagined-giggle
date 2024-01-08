211fe62c0794d4b4bd90cef381c2dee2f15baf00
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:01:03 +0000

    display probably machine sink instant

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import time
+def get_current_time():
+        return time.time()
