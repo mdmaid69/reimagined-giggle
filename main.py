1bb9318c0d12e819df3a798eed7a7bfdff07fff8
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:01:37 +0000

    tradition goout example blame remember

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
