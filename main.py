dd7ec11e98a1d132c18570541437f12ff2bdd7a7
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:44:37 +0000

    strict yours extraordinary sight rare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
