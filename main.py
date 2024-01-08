46caf9af1f4004886a22b399dffe0b8e0e6c2d7d
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:52:19 +0000

    hospital neither anyone would free

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def find_max(numbers):
+        return max(numbers)
