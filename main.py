abf0d0237caac87a2081f2b4de8f48735e74dddd
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:50:29 +0000

    tape reply bend return weak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
