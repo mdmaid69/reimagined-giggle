bc58472acca3e503e3771d6e945befeb70b753be
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:51:56 +0000

    reply smooth my transport mean

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+for i in range(10): print(i)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
