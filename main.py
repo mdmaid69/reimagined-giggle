5490d609d6c92fc5a329f3d8e694cadf71330fab
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:15:46 +0000

    extraordinary surprise lookback(on) future giveup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
