d2df0c8b8ed95f06982b0ba56beda919adca1f14
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:20:33 +0000

    correction communicate habit group prize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
