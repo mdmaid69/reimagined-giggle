f00b8c53470f0680ef3ab527d5377e5622ed5ce6
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:19:03 +0000

    bye discuss blame dress they

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
