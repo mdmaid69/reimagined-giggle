d9811971517f4c74e8e601b1258dbf38aaac3bfb
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:56:10 +0000

    drive marry why winter wake

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
