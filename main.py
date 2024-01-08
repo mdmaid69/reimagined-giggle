b1bf3bf42bd2f4e4aea91e2522bfd6d91cbfb82d
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:16:31 +0000

    opposite tooth conclusion splash sell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
