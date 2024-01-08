9f78418fcdebb3cd067c2616b41b50757100db05
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:21:55 +0000

    live quickly consult benefit crisis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
