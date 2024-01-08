f977aa2d19934990c6f04d1b21395d13408647b7
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:56:52 +0000

    team home process cheek teach

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
