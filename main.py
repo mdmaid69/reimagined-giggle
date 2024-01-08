893d1224605b05ff1c31e06fbb2bd0cf0383a99f
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:22:01 +0000

    among majority spare rough character

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import getpass
+def get_username():
+        return getpass.getuser()
