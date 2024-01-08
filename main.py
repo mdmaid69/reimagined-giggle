a5571c174f2c1dad7d06257b6dba29ae9fedb32e
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:49:25 +0000

    himself vanish ordinary back method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
