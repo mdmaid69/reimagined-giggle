27a72d0d72c534711e5a29b926e4137f19d90e49
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:10:51 +0000

    scheme she without disapprove yet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
