c3d6e743ececae81aaa5cc8433b65e833d5611db
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:30:33 +0000

    exhibition small broken half life

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def clear_array(array):
+        array *= 0
