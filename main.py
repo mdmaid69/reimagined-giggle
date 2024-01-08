50669c3be9bd7a3819213f1224ff6c4dc4df190c
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:12:26 +0000

    comeup advantage run real fence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
