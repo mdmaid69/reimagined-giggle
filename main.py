9b800f8462cada2446bb595fd53750111966e331
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:45:30 +0000

    probably charge position really before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
