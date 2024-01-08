b897e0c43eb7e2d50061a2494016e903bb5d03f0
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:59:51 +0000

    bother past tear emphasize will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
