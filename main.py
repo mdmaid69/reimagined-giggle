2eebf483564811bba749a7dec6907b8b31d3ef16
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:19:52 +0000

    pour ground sting answer subject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
