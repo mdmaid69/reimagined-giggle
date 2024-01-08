e5c7333e23ce597e1f266ba9fc3561ca7a9d4a15
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:53:03 +0000

    pocket pain intend flavour feed

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_acceleration(speed, time):
+        return speed / time
