ef5dd35eadf3f69a5626ace9936b0292ab421927
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:32:44 +0000

    they have each declare interruption

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
