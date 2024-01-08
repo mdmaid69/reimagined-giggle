d6706d6963bf64ea0ce3de5e62cf387543a59c95
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:49:00 +0000

    taste supply lie laugh nigga

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import datetime
+print(datetime.datetime.now())
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
