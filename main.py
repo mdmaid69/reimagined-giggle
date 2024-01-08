e0fd64909e11ffe99c669fe9f7e020d07872ba84
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:26:47 +0000

    figure holdup rid excuse such

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
