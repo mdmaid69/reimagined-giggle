995ce51b359177b63c80b3dfebcf51f289dfbd65
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:05:40 +0000

    its to star should teach

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
