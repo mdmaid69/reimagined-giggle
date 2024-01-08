a2fcb1c72d084a762d5ab03b0dc41cd26c46d3e0
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:44:32 +0000

    fire happy waste announce plate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
