fac69e46cf65b63cde0c1f63589afb3eb92a3756
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:20:38 +0000

    content surprise bar goon closely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
