4f3573e9dcdfe09d4a2d5a6a142a5b66378a5620
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:14:13 +0000

    invite walk valuable listen fortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
