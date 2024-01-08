4a9ba54274fb0a4f01c2b9a80590ba96e5dc4ae8
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:05:29 +0000

    slice effort callout pick pour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import time
+print(time.time())
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
