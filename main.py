a4fe61b2d1a4badf6d1b5f1c1f38c9b88d6e9dd6
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:46:58 +0000

    in paper trousers wide not

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Hello, world!")
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
