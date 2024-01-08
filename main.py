1cf184fa3e22110ac1ba3ca9e26937c2aab5ea41
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:55:36 +0000

    slide nerve business world interrupt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def is_even(n):
-        return n % 2 == 0
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
