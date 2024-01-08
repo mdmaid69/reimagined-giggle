92ae832760cf58f4c6b546ea12c7841b2336bb24
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:28:02 +0000

    swim mixture path giveaway reject

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
