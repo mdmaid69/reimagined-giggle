dc09ff5b9929c85621862d5f8848676a5a20a91d
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:13:51 +0000

    middle genuine insist gap idea

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-print(sum(range(10)))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
