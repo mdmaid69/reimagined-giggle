bddd066e3ac2db731460e5767b87b8dc20f0fe5d
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:28:51 +0000

    arrangement wheel still ball man

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
