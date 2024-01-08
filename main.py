22febd83e0208ce5dadef65912cbcbef1080d919
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:38:55 +0000

    trade everybody many my score

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def count_elements(lst):
+        return len(lst)
+import collections
+def create_user_list():
+        return collections.UserList()
