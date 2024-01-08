b49b56655f4aa3a4d8bcfe7dab40706138862533
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:30:01 +0000

    sticky nearly born another afraid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
