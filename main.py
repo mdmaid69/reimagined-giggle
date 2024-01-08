127666f09f06109378bc96aa08d7f0d53c8fedae
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:22:52 +0000

    government consider smile polite relationship

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
