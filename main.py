d1a5e81ace96406c3bf49e48a16c0d4143b8df5f
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:34:52 +0000

    certainly boat what excited apply

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def cube_number(x):
+        return x**3
