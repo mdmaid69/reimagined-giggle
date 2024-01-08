3bed2a131e8a754f6adff0fbc2d9e2443d28eeac
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:36:51 +0000

    case again Zz defeat married

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def is_odd(n):
+        return n % 2 != 0
+import re
+print(re.match("h.*o", "hello world"))
