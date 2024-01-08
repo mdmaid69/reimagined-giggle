7dbe8b2cdbea87a4eaea2406f32ac2d6ce6a3698
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:56:33 +0000

    expensive luck copy tell forget

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
 import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
