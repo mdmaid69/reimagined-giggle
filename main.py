4b9e2923f367a0861fc40d8346fa011823000ad1
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:00:22 +0000

    operation comfortable goback(to) promise moment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
