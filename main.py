b8902e6b68aaeadae9f7c36952359ef9b7465302
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:48:08 +0000

    activity thin lock slip regularly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_floor(x):
+        return math.floor(x)
