06a25764ee559c6199f8407f53a87bf473b3aefb
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:10:42 +0000

    tragedy cool clear personal never

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def add_numbers(x, y):
-        return x + y
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def find_unique_words(sentence):
+        return set(sentence.split())
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
