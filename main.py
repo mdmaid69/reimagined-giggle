98c3945534daf2446e7806a29fd3fdcb1992b526
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:09:17 +0000

    turn spend minimum crisis crazy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import os
+def remove_directory(path):
+        os.rmdir(path)
