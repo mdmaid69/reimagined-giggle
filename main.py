5e41e1bd0503baa1a35245e85451e19ad802abec
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:33:02 +0000

    concentrate concerned anything base comeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
