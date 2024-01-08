922a7726dbe39e7f96b237bf404d37fb3f440659
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:15:23 +0000

    hope undo clean maintain word

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
