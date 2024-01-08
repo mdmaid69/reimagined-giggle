2ec46c725a3093592c92d9d499bc6e4eb199492b
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:07:17 +0000

    Bb help straight polite wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import json
+def convert_to_json(data):
+        return json.dumps(data)
