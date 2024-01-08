fc2095e43a0add0a8983f43dd03a0762e03160dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:53:28 +0000

    soil favourite comeup contrast nervous

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
