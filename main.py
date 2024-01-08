50342d4ac3c1043588ddc6c1656acc833e941d46
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:30:59 +0000

    hungry cum tend shut through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
