6f97b455374983ead1bdbf41b14ac6e7dd6fd522
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:17:47 +0000

    boil escape into quit often

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
