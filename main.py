bd50ca14f808763b8d7ba16da2cb9411ce461e1d
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:38:12 +0000

    plate spill promise estimate during

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
