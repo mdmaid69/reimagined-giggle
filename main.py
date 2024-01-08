61068b982253d3470e5fe920db08aaa65f92f1c1
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:06:57 +0000

    poor temperature group organization performance

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def square_number(x):
-        return x**2
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
