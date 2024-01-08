b3ff623612b45e12dabeac88da4dec2bbc51f73b
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:10:00 +0000

    big appear history dream by

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import sys
+def exit_program():
+        sys.exit()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
