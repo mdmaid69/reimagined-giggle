a0649ef3e8558fafc9263c9732bd6490e442551d
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:28:33 +0000

    gentle spring purpose through fond

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
