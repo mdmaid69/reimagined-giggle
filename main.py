fd9b98e021b524d7b509d34adc551e618710f099
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:47:24 +0000

    search heart swimming beautiful lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
