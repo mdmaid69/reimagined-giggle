f7df3d31cda075f02f867e6122630c29c8b56f9b
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:00:43 +0000

    573entertainment fairly roll swimming desperate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  def square_number(x):
-        return x**2
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
