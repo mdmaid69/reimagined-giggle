0447c67cb704c5cc39451f12d6aa325010550d92
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:52:25 +0000

    572enter no position direct water

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_array_as_int(array):
+        return int(array[0])
