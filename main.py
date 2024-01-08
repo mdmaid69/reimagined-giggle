a7477b7cd6a447fad6e0d13be329346919bec72b
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:45:30 +0000

    in discovery sweet likely means

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def clear_array(array):
+        array *= 0
+import array
+def get_string_from_array(array):
+        return array.tobytes()
