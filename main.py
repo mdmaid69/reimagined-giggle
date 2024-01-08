406ab8fe7a8ad4dace2df367babbf80d49860118
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:23:18 +0000

    balance translate practise zero immediate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
