1b8f06f747f10ba14401238bbb90a7f1cbbb4511
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:20:09 +0000

    accommodation everyone earth cruel glad

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def add_numbers(a, b):
+        return a + b
