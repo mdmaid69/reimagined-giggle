34febfe5d6d0763606039d0dca6467da1d1db09e
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:34:24 +0000

    dress listen about yourself pull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
