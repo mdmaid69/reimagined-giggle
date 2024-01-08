e0df2a03297d93a361492ab41d5ccd1c5bf3e0a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:53:46 +0000

    congratulate 566end closed niggaacademy stare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks_allocated(file_name):
+  def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
