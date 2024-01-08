e8b3d4259abaca5721a44f7c6f0d2122631d6fc7
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:16:01 +0000

    difficult hit chance command breakin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_volume(length, width, height):
+        return length * width * height
