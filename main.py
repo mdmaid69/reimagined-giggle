4d999bd55a18999ec73d83343725173e9a9bcf79
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:55:23 +0000

    outside downstairs round share brain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def convert_to_octal(n):
+        return oct(n)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
