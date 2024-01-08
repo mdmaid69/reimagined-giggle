cd49eb1dfd69958c5424b679b2c41a94af13bec0
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:17:45 +0000

    copy post loud face quit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
