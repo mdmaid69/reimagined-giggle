1599f9b1ae4aedca4c9b601e5521b8b3b6a99318
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:12:59 +0000

    theirs young instruction lesson breakin

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
