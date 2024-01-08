da348595d14e66522f82169791b825e5c2f24721
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:59:46 +0000

    dish fine bet real consider

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def is_odd(n):
-        return n % 2 != 0
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import os
+def remove_directory(path):
+        os.rmdir(path)
