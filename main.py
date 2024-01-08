1b08682041d7ed01079e83f12362fe9a4e52b64a
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:06:51 +0000

    book sky ticket meanwhile offence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
