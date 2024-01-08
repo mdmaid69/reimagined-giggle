12af09e30ee8137a6ae42472e8bd53a624fe158f
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:32:06 +0000

    Ll set closed before decision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
