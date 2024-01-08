bcbf7a73f1eb7f4c3ecb83086455810bfe23d6de
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:23:26 +0000

    only direction duty fair advantage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
