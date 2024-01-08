609011003298f82e8733120b89ccd995c3360122
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:11:26 +0000

    even wonder greeting snow ought

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
