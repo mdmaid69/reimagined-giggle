ac4c5db23ee16006b52d56b60a55efd65196c5e0
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:59:10 +0000

    breakout discuss raise patience not

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
