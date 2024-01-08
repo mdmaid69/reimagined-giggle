382da84435187735aa30c0fed31c9c19fb814fb9
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:07:42 +0000

    choice burn children face condition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import sys
+print(sys.version)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
