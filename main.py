cac7aeba10736a8a87151cf41a9a02969b0fedbb
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:26:39 +0000

    aim sense feed substance 571enough

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
