6e2dc22cde982d611e4adf2b5093551c05ede8ba
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:17:43 +0000

    cap wash task when soft

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+for i in range(10): print(i)
