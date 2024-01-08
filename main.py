fe0ad16a995a840c6ec3036518d49f87f64bb734
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:22:07 +0000

    watch final dress expect yesterday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def add_numbers(a, b):
+        return a + b
