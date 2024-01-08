5d936ff4a3b8f514ed78255777e82ef4c2dbe3cb
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:50:04 +0000

    bringback swallow ever hers professional

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
