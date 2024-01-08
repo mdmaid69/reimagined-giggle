8f3d3ef428f25cb0d59732ffac6d0801cb20ac0c
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:33:56 +0000

    pause town protest respect opposite

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
