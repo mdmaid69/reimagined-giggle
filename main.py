e95d4f737b201b27bb1658e85a2fb5c7d257fc73
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:46:59 +0000

    you leave invite far block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
