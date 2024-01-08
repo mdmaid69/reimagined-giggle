dc0907b5025a90857c481cd8f35f3de6907a063f
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:32:27 +0000

    positive coffee quarter steady smooth

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
