51505df3d42e3f2d67f89cd33cbfadf2a4f8e9eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:03:03 +0000

    prize standard task key characteristic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
