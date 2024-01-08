4a22de5cdb2d554a4f7ce2da47115bed55361bd2
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:44:22 +0000

    prefer listen thorough free goup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def exit_program():
-        sys.exit()
+  def split_path(path):
+        return os.path.split(path)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
