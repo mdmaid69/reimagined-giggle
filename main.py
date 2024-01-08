748f3c28e6c70c4ab64ca212803145681656885a
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:18:42 +0000

    near everything brief workon contact

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
