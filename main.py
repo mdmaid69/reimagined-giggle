6ff89da4c360fea3e6bd99a96559cfd733743460
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:41:56 +0000

    concerning curious definite sweet open

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_length(array):
-        return len(array)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def cube_number(x):
+        return x**3
