fa1b810ef1e14155d00c9e1ad6dc3f5aa9950ca1
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:18:50 +0000

    bringback unable wide gas game

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
