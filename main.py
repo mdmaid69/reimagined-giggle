d8d59e78d498543955b117320e9a9f84ee5ec6f2
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:26:34 +0000

    please kick mysterious go error

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_exponential(x):
+        return math.exp(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
