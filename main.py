27e49c7df6c15b7c4b898e981dd587192f46671e
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:36:13 +0000

    protest soon remain contrast under

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
