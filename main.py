9efae2da2a1de800af84f43d90f6fd83e2195870
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:54:23 +0000

    avoid should roof passage turnon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def clear_array(array):
+        array *= 0
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
