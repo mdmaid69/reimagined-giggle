1382027606fc512acc9618357e873d750430e694
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:41:16 +0000

    work while hurt scale factory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
