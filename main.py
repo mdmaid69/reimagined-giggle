4ef9eeb3944932b3035ff83a4fe547c2c081b77d
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:54:53 +0000

    Nn difficulty hers call again

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import getpass
+def get_username():
+        return getpass.getuser()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
