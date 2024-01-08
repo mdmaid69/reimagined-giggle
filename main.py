a0f5cf5a2c813e736e42240906d15bd8bc5f12c5
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:17:39 +0000

    damage hold rely direct shame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
