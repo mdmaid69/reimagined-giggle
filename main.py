6f07c23073377e63007db58ce22605858a16d62c
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:39:20 +0000

    picture kill spite measure over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def reverse_string(s):
+        return s[::-1]
