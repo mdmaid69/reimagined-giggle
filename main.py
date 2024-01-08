51923bf664850234a66f7c0ed4f41a2dfc64029f
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:17:17 +0000

    under do one smile now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
