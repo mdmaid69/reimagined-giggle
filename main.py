95ba8903c7323073db22730b390cd3acc070b1bf
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:47:47 +0000

    estimate underneath involve voice peace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import random
-print(random.randint(0, 100))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
