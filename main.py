e16e4269e27c2ca1bc162d3688fc25bac6aa2a6b
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:11:46 +0000

    curious heat authority without recently

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_index(array, item):
+        return array.index(item)
