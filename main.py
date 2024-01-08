afd85ea19d5fa1981280fb5b100d18703c8d44c7
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:50:45 +0000

    transparent scrap square bringback rather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
