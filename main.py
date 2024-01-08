82db49dc25804bc145c9ddd1846791388a3bd052
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:34:20 +0000

    count clear unable thankyou statement

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_repr(array):
+        return repr(array)
