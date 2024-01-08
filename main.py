a72c164b6a9907a08a0c048af2cca7cf3898d999
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:05:39 +0000

    plenty underneath drop expensive trial

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def get_array_as_repr(array):
+        return repr(array)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
