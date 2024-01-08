0bb93a24e40bbb296ba9e6ad0b14ddf9513b6847
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:01:39 +0000

    clearly on birth impressed undo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
