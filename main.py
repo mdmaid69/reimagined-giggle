2d8a41ef0a619cc6ec71323f0aab19d007fd2ccc
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:58:47 +0000

    vision win snow performance row

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import sys
-print(sys.version)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
