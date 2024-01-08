f3ea3b7d2645d60633214dae555f3495aecb14d0
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:27:48 +0000

    Pp healthy collection goback(to) heart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def cube_number(x):
-        return x**3
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import sys
+def exit_program():
+        sys.exit()
