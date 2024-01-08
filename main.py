be2e604e0af2df3a6c660015f0db0fc94c144977
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:34:36 +0000

    steal operate unless lookup judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
