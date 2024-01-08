d79a4559dda38e67e9e4b44e001d0742fcbb1a77
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:26:00 +0000

    comment Pp anxious runinto healthy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_speed(distance, time):
+        return distance / time
