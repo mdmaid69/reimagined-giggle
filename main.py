a82bb139664c23f901f3a98a6b730a1bee63ba97
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:10:03 +0000

    Zz science everybody themselves getaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def get_array_item(array, i):
-        return array[i]
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
