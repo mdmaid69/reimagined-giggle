fa4326fe528a16097e1a5a1031a7cca7ee4b5434
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:58:06 +0000

    react hour several row absolutely

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
