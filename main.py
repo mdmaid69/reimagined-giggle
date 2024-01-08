2692bf5719ff4e1b5b0fa09c9788d3df316d03af
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:44:22 +0000

    little correct argument relief soon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_pressure(force, area):
-        return force / area
+import array
+def get_array_length(array):
+        return len(array)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
