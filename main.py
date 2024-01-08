5f6c0d9e11aef88e14578af11eb1c9615721a580
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:02:10 +0000

    shout tight kind permission plastic

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_sine(x):
+        return math.sin(x)
