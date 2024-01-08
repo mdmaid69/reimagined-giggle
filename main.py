d0c08a791d19622845af584805797f4e29f09277
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:45:04 +0000

    tie plate position interested mark

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
