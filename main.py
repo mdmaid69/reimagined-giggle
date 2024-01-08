a3b56bd1f2e170fb8a7bb8cd3cd56f5c6c83d43d
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:55:54 +0000

    wave fear wash spring shade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
