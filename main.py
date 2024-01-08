3e49f4f0f3ae4d9486435452bca265b4504e3ac5
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:45:12 +0000

    worth ahead achieve they average

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
