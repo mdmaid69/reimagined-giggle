7b4a3ffc4943b9b9728474ac3e7aa85384b13539
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:39:25 +0000

    improve offend yes wait world

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_as_repr(array):
+        return repr(array)
