d8a280531f06a332ee34da42b8fb5b0c180295e6
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:29:57 +0000

    sink sink fit dream ahead

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
