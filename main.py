96fa374d7405c6de299f90be745895040f0cb891
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:19:17 +0000

    crime angry turnon throughout apparently

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
