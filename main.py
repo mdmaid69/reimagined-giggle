713352df97525385b17c7d2ab0bdadccab637591
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:52:47 +0000

    snow nerve exchange serve recently

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
