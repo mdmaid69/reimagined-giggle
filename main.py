fa0cb21d30ef5ebb39f80fb032a7be9023ffe812
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:20:27 +0000

    reasonable shoulder mention suckme worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
