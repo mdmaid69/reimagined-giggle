e585153c7252a88d372f8f828d948a79fa4d73c8
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:24:11 +0000

    move discussion rob tour beneath

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_density(mass, volume):
+        return mass / volume
