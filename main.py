fce8c07db314105e1a0dbd8e02b439b2e90546f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:20:20 +0000

    attach promise different rescue up

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_work(force, distance):
+        return force * distance
