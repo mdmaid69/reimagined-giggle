732d3512eaf585273aba24ee4405d7a252525907
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:09:13 +0000

    sentence occasion thankyou talkback comment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
