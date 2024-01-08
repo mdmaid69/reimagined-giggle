b004467d6b7c9b22dcaf5ac943fc77819fabeac4
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:53:36 +0000

    awake anxious runaway different can

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
