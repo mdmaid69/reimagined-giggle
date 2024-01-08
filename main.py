9d20c9099bd5d459bc263c3dc3b1833d3f8ce120
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:46:03 +0000

    limit that edge pain journey

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
