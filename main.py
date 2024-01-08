e9ecea1762bc923d90f667e78af55dcafc4f8b23
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:17:38 +0000

    lunch grand bright flood state

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
