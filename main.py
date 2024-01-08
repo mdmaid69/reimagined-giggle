7b778944903306bc96bf6eaed50d352e4f9b12a1
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:46:04 +0000

    though it ignore mother undo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
