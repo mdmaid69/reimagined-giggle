de04d946aaaf882d5a96fe290f1cf788f41bf86a
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:12:02 +0000

    dry pause whether wash position

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
