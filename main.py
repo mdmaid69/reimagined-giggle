93751aeb01a43506b1383c553a8e8a9ce986e510
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:08:23 +0000

    head likely over together sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
