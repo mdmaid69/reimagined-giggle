602f991b2af4854cb6143b3dcebbd4a92f05bb30
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:53:42 +0000

    fair maximum dry excuse sudden

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import json
+print(json.dumps({"name": "John", "age": 30}))
