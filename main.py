1212602bd2023c4d8a6a0df93b5d5430d87153b3
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:11:00 +0000

    that rate remove road equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import datetime
+def get_today_date():
+        return datetime.date.today()
