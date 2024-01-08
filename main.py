e5f51636ce7b53645187dcccae20e0a6932b2db1
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:12:09 +0000

    birthday bet involve later squeeze

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def convert_to_octal(n):
+        return oct(n)
+def sort_list(lst):
+        return sorted(lst)
