a5e21160bad2420ae87281812a6b4bbd100438bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:53:13 +0000

    mysterious concern unhappy relation shopping

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def clear_array(array):
-        array *= 0
+def get_array_as_bool(array):
+        return bool(array)
