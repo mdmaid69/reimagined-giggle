9d0aed1b73931e70de27ce654312065beffcff64
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:28:45 +0000

    straight polite limit fortunate runover

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import numpy as np
-print(np.array([1, 2, 3]))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
