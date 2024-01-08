6c58ffccb781eb68d0071d846f7b915cedf8c52c
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:58:45 +0000

    connected brother vanish pride bear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def convert_to_hex(n):
+        return hex(n)
