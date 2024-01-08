2990e60f02fe46695905140ed0b0e2a3a1393ead
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:31:22 +0000

    forever along dangerous few minor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
