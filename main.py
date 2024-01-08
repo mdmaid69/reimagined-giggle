45cbf59b60a2a6fdec605482b410571b469e7584
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:55:24 +0000

    hold beneath she period runaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
 text = "Hello, world!"
-print("Uppercase:", text.upper())
+print("Is palindrome:", text == text[::-1])
