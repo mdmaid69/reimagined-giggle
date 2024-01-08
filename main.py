a837b3f5933ffca846283117ca9f7c389bf34121
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:43:41 +0000

    workupto friendly also around prefer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
