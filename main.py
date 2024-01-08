0fa7beabb25dcb1dee50e677f03edffd91685fb9
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:09:53 +0000

    lookback(on) square us hurt negative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
