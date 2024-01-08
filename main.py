f37e59bc70c7f902af339061f323d652da459c2a
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:33:14 +0000

    content say fact soon cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
