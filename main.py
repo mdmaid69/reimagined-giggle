e592107e1bc006f9849bbb0961aa34733acb7279
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:46:36 +0000

    have door base else concerning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import array
+def get_array_typecode(array):
+        return array.typecode
