b4d598846c7f3e4c594c350c19c9f90793d63772
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:45:45 +0000

    thumb beside boil order break

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
