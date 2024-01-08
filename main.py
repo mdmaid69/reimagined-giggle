fc8460248b0c9a62b5e09c79fba79623d2a2fb34
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:00:15 +0000

    material bringforward rest above care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_as_list(array):
+        return list(array)
+import sys
+def exit_program():
+        sys.exit()
