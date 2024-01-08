fd729b74acedab3800d88d9d91efae01753b1b9b
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:34:39 +0000

    need somewhere store independent sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
