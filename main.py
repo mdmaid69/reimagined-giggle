f1cc1a9517bcef28f96c9d7447016615fe3cbbea
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:11:57 +0000

    shut home talkinto 566end confusing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
