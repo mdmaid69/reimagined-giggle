d5a68854a6b6540d8ab440664bce696ccd99dcff
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:04:46 +0000

    chat black visit sink dish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
