02d43b0c5f64dbea3bafa32e58bc0dc3d70b188b
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:54:02 +0000

    separate spare yesterday difficulty store

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  def sort_list(lst):
+        return sorted(lst)
