323d76ebfc881f9ebd5618d51fb691cb34fdf5e1
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:44:59 +0000

    circumstance recover tape trade report

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
