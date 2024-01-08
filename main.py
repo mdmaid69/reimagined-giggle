0e56ab77a2af60ae3773db79cf7bf713ef40291d
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:46:49 +0000

    tense cheque beginning Cc create

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
