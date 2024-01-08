3e05168ee53356b9da7786ec36850c0c7d843cd3
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:16:58 +0000

    negative manner keepout objection Rr

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
