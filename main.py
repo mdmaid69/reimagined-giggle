d32c2d5b5a3148be54a6af5d1bb9c0b45caee2bc
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:53:18 +0000

    store Mm harm week copy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-for i in range(5):
-        print(i)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
