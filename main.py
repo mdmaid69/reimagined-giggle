e3896ce156c3b945db9ab60943b45c5dd62874f2
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:19:05 +0000

    loud slight giveaway air black

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def cube_number(x):
+        return x**3
