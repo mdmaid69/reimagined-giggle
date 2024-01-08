45fd960791731bfecae25bc1f7fccb40c7b7d61b
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:31:05 +0000

    block ring fasten clear address

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def append_to_array(array, item):
+        array.append(item)
