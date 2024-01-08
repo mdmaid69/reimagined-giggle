8fdf033349fc6611b2c3d6fadcee45fa8e8e7d2d
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:51:57 +0000

    lock basically cinema shade bye

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
