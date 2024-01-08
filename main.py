bfdf8d248263bc6cc93a08afbb001ad79fa0a9f5
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:12:19 +0000

    legal among opposite evidence scare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
