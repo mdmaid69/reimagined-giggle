74d582ecdc4ba133ade649553ed888b600b44647
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:36:38 +0000

    ask 572enter work famous cross

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
