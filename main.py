ddc88427e8264987dcb196854fda1d56b4c1eef2
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:12:39 +0000

    band hole contrast spill frightened

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import time
+def get_current_time():
+        return time.ctime()
