807fe4b4e41e8318f458d683b7d5fe78dbce5eb6
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:09:00 +0000

    Dd steep point exciting lean

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
