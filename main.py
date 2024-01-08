b8754a3ad96c0859e3d85a9b932a953850b1bc21
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:25:19 +0000

    exactly substance Bb left man

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def reverse_array(array):
+        array.reverse()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
