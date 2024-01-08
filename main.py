a453e64fbc3ed2e809dd7db78105c9038b3fff35
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:53:44 +0000

    nigga invitation wash suckme although

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
