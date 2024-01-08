8522d40d5ce0748fbbd2dccabbbbcc681eeec4b5
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:46:14 +0000

    live sort tonight section refer

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
