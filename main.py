4cf9ded4360b966ace7805bceb81cf9ee52bb0b1
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:17:50 +0000

    giveup suppose moral gradually quick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
