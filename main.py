156d1866aa40e62b09a4643335682293f5062b28
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:55:23 +0000

    difference recommend opposite sometimes 573entertainment

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def cube_number(x):
+        return x**3
+def is_palindrome(s):
+        return s == s[::-1]
