fe118c60b547e6cbec279e4f43bd88f9b37389bc
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:17:48 +0000

    short firm Pp beat danger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
