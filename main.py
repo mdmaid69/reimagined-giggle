8e81ee4347848a08aeb7c47427ac6df57d19dda7
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:24:18 +0000

    crash friend tip material kill

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
