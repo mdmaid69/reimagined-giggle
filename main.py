ab56e59379ce480b66c6f242bfba1945ef07c164
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:18:45 +0000

    set waste drive gather picture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
