16a3767998b81d5f86a5394adfe40ab5eb994810
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:25:17 +0000

    politics evidence afford radio discussion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
