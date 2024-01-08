4f46d91c78f37bc0f805ad650b6428feaa7f1fbe
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:37:15 +0000

    whoever cut corner corner contents

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
