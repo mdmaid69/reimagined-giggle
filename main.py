67c2e4a4b765592beaee658d24769b8afcb1caee
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:23:20 +0000

    song situation design part holiday

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
