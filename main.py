58b3eed6b72507ebe9d9a52583149e7edfebb6da
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:10:31 +0000

    so back rubbish basis count

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import time
+def get_current_time():
+        return time.time()
