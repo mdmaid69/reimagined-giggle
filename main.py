b36e299b48f848e6bf83ce54a14bf23b1e0a7656
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:33:52 +0000

    discover sail reaction create although

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
