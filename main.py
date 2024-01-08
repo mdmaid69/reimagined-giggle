1aa0855bb2f61edb28035321b76177bee23f8560
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:18:42 +0000

    telephone deliver lookafter cap use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def reverse_array(array):
+        array.reverse()
