ffd287f7992a79d660895ef5f20abd78f49cef5d
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:37:30 +0000

    argue impressive charge message trade

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+print(time.time())
