595496c6847995dce6d63dd79710ce1f1fb68f10
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:35:08 +0000

    direct marry Uu convincing against

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def convert_to_hex(n):
-        return hex(n)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
