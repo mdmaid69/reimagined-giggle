1e490b6d5d8f5fccc1c9571c315324e6ea7f1ea4
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:38:47 +0000

    professional lend previous cope care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_cosine(x):
+        return math.cos(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
