ad264cfe5907a1e6c452fa8e6f79a977e83f2388
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:47:48 +0000

    sister beginning distant term cruel

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
