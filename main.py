f8656e627b58c43f9dfd0bbe4c6c72cac017db7b
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:34:02 +0000

    official minor open form warm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import datetime
+def get_current_date():
+        return datetime.date.today()
