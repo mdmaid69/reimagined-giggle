3301ced0ee3bab7844e2409536c75758c8228f82
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:16:04 +0000

    block quickly quit usually person

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
