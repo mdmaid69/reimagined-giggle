f1845a5741e7c9ef0869cb950228a088a5afe280
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:18:25 +0000

    imagine hospital Mm minimum show

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
