68d944e355bfa30ebedb61b78b52c51cd86e9c37
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:41:11 +0000

    hurt ours there almost save

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_repr(array):
+        return repr(array)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
