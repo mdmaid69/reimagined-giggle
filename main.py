0d391ce465418fd8f44536c9744f85efc681a724
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:49:12 +0000

    police hit exercise yours street

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
