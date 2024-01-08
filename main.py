9424de641f518992bbcb3529f8e873e2ad664761
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:02:03 +0000

    means for use walk town

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
