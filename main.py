b5586175515a4bbd2e553df30cba620aab6b4ab1
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:53:57 +0000

    571enough switch without offend active

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import sys
+def exit_program():
+        sys.exit()
+import collections
+def create_user_dict():
+        return collections.UserDict()
