7cf87fdf6122ca0b8b0f230353925d689dc3d935
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:19:11 +0000

    comment anywayanyhow better soft opportunity

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
