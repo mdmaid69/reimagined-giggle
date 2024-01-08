d5f97460976f5df4d23b3f2d62f452aec0fb1277
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:47:39 +0000

    force display exact opinion risk

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+for i in range(10): print(i)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
