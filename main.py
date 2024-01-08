78aa30cd22f1414e0717f3cb78daea7b5125cc3a
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:41:37 +0000

    need rule interfere work herself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
