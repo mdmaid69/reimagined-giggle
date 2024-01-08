89a5b8788117b5f7be77c5353b381c328298d441
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:42:25 +0000

    just lesson reply ourselves loan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
