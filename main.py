74a6bb2dcde7c660625b863fece519d6b23e0a57
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:00:14 +0000

    goback(to) row cruel square holdout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
