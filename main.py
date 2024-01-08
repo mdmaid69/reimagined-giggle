d57b0333a321964719df1723d95dcda87effd8f5
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:07:48 +0000

    giveout town not reliable spread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
