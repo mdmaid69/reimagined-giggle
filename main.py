8ffc73be6a88653d946d472d42456612997db33d
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:57:55 +0000

    damp broken around tour supply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
