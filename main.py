2ae6d5c6ba196cdb42732bff72aaf5b1110c9053
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:47:55 +0000

    possibly lend tell again cinema

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
