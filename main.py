c35b493916e69a1464f137d69619fa982530f4c8
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:22:40 +0000

    steer afterwards present maintain would

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
