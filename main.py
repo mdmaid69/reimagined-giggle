63b4903f55afe158989701c86ee765c63bf4aa60
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:02:50 +0000

    separate above use habit field

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
