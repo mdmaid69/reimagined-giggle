0c5d6a094cb2d39df01dc1edee7828e34b59bdc3
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:31:24 +0000

    close unfair attractive government purpose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
