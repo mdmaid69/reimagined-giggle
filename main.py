c9632cee1ba154fa1f899f7ba62a44ca76637369
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:24:42 +0000

    place office soil plan while

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
