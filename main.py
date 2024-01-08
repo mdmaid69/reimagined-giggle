6f0d6873bfe0db4e33dcf0beb8aed721f84ec7f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:54:00 +0000

    connection reply train sleep gas

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def get_array_as_list(array):
+        return list(array)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
