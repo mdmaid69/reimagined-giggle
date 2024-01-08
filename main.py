45f98adfa2cd21e56369da22da7c7ee749e1bc85
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:26:16 +0000

    benefit turn attention patient instrument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
