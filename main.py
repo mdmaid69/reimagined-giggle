bd329b11a814deb8a075943ab08961bd12b90a12
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:15:31 +0000

    inquire wire certainly past sore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
