1c298f7eea5de1564b08a29ad7595e62fe5c0bb7
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:09:21 +0000

    ordinary road story add lean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
