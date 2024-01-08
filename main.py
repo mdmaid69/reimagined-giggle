f521911bca366915b274a8c087f24b4290d72ca4
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:43:29 +0000

    into terrible comfortable Kk lean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def add_numbers(x, y):
-        return x + y
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+print(time.time())
