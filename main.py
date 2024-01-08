0fb7438d226f5a37773f5ba04174e7569205163b
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:53:28 +0000

    sea negative plant cum dull

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
