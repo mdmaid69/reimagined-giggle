b177e97ce4ece336b82a1446cb19b68fa66b9797
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:28:05 +0000

    stamp fly takeout average trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
