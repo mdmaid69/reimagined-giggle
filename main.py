73ecc214bcce82cdb73f68dce6650b7473f17d07
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:52:50 +0000

    distance unpleasant past motion Ss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
