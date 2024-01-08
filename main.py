a8ddd19690c7331072462685e585270066225aa7
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:53:38 +0000

    tall book warm of pull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  def reverse_list(lst):
+        return lst[::-1]
