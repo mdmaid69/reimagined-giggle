9e4130aeac8187e9b5186f273cd931b29d600e31
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:07:36 +0000

    chief Cc mean takeback sore

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import collections
+def create_queue():
+        return collections.deque()
