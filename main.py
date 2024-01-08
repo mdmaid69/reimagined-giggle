e9e024f1962040240186f8a6a201ddf2025bb6dd
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:43:12 +0000

    fine dead discussion or reply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  def add_numbers(x, y):
-        return x + y
+import getpass
+def get_username():
+        return getpass.getuser()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
