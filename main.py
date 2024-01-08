027d592f4760f765591a3328b9c77194af7f43a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:54:54 +0000

    wind soft whole himself wash

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def multiply_numbers(x, y):
-        return x * y
-import sys
-def print_python_version():
-        print(sys.version)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
