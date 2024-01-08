cf8b156a4749583205e31229da6cca85ca13063e
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:45:41 +0000

    turnover ground broad adjective tired

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def count_words(sentence):
-        return len(sentence.split())
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
