d1d723deb64cfd716dfd8392dc07cb39a231bd2f
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:50:02 +0000

    view effort skill suggest loose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def reverse_string(s):
-        return s[::-1]
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
