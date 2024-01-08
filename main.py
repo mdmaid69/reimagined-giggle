a8d171bf7b0db8b6600d224707dfea17c2e73a23
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:28:32 +0000

    last provide clothes collect apply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import array
+def clear_array(array):
+        array *= 0
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
