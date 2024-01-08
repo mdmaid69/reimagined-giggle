c8d102f962b8b613f8f8661e0b80e56880cffaa3
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:22:40 +0000

    regard allow false evidence snow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
