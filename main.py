4fb6a0c270d101902e13e9a354bae70a177217dd
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:43:38 +0000

    change pair drop rid drink

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
