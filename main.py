9164ba245fc6d78d1cc6145478fde68ed1c01009
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:37:30 +0000

    one greeting live sick whatever

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_as_complex(array):
+        return complex(array[0])
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
