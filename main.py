2a6ef7c2978280c3ed5d6aa43e93bc7d74703500
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:30:03 +0000

    wish habit keepup scream die

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
