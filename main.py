7a496dbece2554cb40bf82254b11f9b303421f8a
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:01:13 +0000

    who reply legal fine official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
