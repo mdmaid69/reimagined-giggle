60500f9c152b8ffb855c7445785567235c61e16e
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:54:13 +0000

    inquire taste window fond term

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
