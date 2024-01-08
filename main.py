50829e197adaea43d2ba7a7a14b4cc3445e32655
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:36:47 +0000

    waste reason shame afternoon communication

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
