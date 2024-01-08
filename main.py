8693c45c5362136d839b3820bffee58321cdf8be
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:26:57 +0000

    recent ask sit find coffee

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
