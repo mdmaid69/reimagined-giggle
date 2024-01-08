045807a1458b0bd4af3ad7bd4af5a8ee914f8c47
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:01:43 +0000

    he frightened one undo could

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
