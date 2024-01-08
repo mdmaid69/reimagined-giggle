07dfe5bc0ee04a92f91022a974f35e2bfe67c788
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:07:45 +0000

    injury slip soon name 568energy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
