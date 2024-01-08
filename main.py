017cb4e2e67a31f0384db0a2507ce36831859d51
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:09:00 +0000

    coat correction morning single conclusion

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
