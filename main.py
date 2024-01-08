2325d99f50a7b03c1039d6ef8f518f6c4f660d35
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:24:40 +0000

    help language book marry surprised

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
