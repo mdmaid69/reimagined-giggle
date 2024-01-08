1a429370359365dcb04a5a4b591332626d4f3f82
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:09:48 +0000

    soak survive knock pretty direct

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def sort_list(lst):
+        return sorted(lst)
