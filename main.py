0e983d9108a879f49f062f565a393d05f73424e6
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:14:33 +0000

    report unit thinkthrough 571enough related

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+for i in range(5):
+        print(i)
+import sys
+def exit_program():
+        sys.exit()
