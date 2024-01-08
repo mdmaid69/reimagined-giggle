b966df665a63a94aab668b87518bb35bbcffedcf
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:16:15 +0000

    be tooth bear fast freeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_counter():
+        return collections.Counter()
