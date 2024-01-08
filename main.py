8a110c77a516c7054e7b6a60996e5b65a2d38bb0
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:09:41 +0000

    ourselves open weigh scream national

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def append_to_array(array, item):
+        array.append(item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
