64b7f05db7c7fa51ee81289d8e39209db48f24e5
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:43:12 +0000

    city separate sit excited wait

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
