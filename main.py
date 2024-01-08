d1e64861583f4a4ca5e08436f70c5b79d600fa07
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:41:31 +0000

    number of shall wise difficulty

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def square_number(x):
+        return x**2
