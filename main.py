fd3d2df2b4692f0b8bc2e4a3340d6698da7da4b7
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:41:28 +0000

    dig judge day plenty slow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
