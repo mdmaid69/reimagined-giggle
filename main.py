05b2f34ce189f7fefa5c8065abe6d64eed8834cf
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:26:51 +0000

    dark maybe size chat shoot

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
