5459eda0d4c54cc3126b68a8c4bb00e798a04e71
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:25:34 +0000

    whichever result sting sticky answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
