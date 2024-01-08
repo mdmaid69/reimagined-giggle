b634b68e0b1a99648945805d54f40c4503aca011
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:21:28 +0000

    state criticism knock onto apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
