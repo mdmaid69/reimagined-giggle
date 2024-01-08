e558ff7b9cb28714bdb8ca038fd8183750b085ff
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:37:13 +0000

    luck shop pain fetch protect

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
