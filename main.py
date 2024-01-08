68d74f46cad03a113442178d2d4e9eea598e78b0
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:35:46 +0000

    instruction damage already switch introduction

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def convert_to_octal(n):
-        return oct(n)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
