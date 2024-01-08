1664cc8beb62087b3698c7b4cbeb8f04407f2d31
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:52:22 +0000

    ear aware home stir odd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
