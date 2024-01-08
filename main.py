2afeaa0f6e89e12158c04bc57bf65e0270ba2fb7
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:43:36 +0000

    funny image rest scared clever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
