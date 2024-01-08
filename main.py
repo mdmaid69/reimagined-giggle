d955cf5f1cf107c5587c9655d107151359bf03f5
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:22:36 +0000

    pencil move material tight cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
