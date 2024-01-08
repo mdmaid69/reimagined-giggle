6456c2c97b6f9093cead8b944f704b005afd83a4
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:12:41 +0000

    mother gap beautiful typical ability

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
