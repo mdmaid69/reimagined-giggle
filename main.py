36fc6d0ee8eb5a4065260fe172417f459197cdd7
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:53:56 +0000

    absolutely excuse behind last battery

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
