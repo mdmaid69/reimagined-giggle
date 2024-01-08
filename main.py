23558e190a3b8cec3c1ec7947d971882a343f26d
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:30:47 +0000

    basically choice brain let message

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(10): print(i)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_speed(distance, time):
+        return distance / time
