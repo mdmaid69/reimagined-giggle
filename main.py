d93a7fed585e18861a341268094428cad15e8f5a
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:44:12 +0000

    no with load past however

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
