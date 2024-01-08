65a6bf2e9b8d8203938bcb9eec82e075ac7ac4c0
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:43:51 +0000

    reply succeed education tooth side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import random
-def roll_die():
-        return random.randint(1, 6)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_list_from_array(array):
+        return array.tolist()
