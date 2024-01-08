7083ac1c2d9c779cdfa50691b174eb070230476f
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:47:22 +0000

    or alive normal give possibly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_list(array):
+        return list(array)
