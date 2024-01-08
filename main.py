1b46239e5ceee3e7e7de18ae2091f3d86709ba1c
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:29:03 +0000

    Ii lock dance mess earth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_sine(x):
+        return math.sin(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
