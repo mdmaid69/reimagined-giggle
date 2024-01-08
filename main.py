d94acfaa738691b0fb39dcda6aa64fad3ff0a397
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:30:38 +0000

    trade fun beside week grand

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+text = "Hello, world!"
+print("Words:", len(text.split()))
