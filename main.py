16a41bbe5765c3ea7a58be958cad48fdf4fd4b94
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:57:24 +0000

    male block complain follow gather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
