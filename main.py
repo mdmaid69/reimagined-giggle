f625e898af93e02979a7a0f878e908ac38a15cce
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:30:38 +0000

    after side consult take over

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
