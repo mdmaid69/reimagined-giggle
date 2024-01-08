bdb8b549ce0c2fbf5a52dcae97753cbb5b8f79cf
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:44:22 +0000

    goodbye remain nothing impossible Qq

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
