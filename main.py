c80f989248844e79be8b27f5dd3b2d18538c0690
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:48:03 +0000

    stretch dance wash boil guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
