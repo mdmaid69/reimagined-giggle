37ea32eeb7a4835f128c80560dc770b972394176
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:08:58 +0000

    wide either mail relative bored

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+print(sum(range(10)))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
