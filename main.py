4593d9b5a8e5d62e18ec4d0d7a3a4c1064e2967b
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:14:24 +0000

    thought takeback complete strange recent

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def is_even(n):
-        return n % 2 == 0
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
