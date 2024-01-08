ed6b10d9a09d8df19aa1424bf07e3800756266e9
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:54:30 +0000

    moral communicate visit recommend there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_int(array):
+        return int(array[0])
