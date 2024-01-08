feeee38fb1a617a545154e49b12a419012b430b9
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:47:12 +0000

    thick hardly convince break impression

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
