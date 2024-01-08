95398e953891259e8ee903b1008acd42e9a2824a
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:18:51 +0000

    row advanced grade safe handle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_area_rectangle(l, w):
+        return l * w
