1ffac3886fe8fea17e69699c075b6b1563fd23cb
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:12:18 +0000

    problem side comefrom squash plan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
