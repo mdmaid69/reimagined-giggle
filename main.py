e803a01d24e276d5012fecc48bf80396f27b44a3
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:41:36 +0000

    late die make chase type

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import platform
+def get_os_info():
+        return platform.uname()
