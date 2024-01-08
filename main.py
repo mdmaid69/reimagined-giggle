c91abef84dfba71a580c8bbe7742336efc94c5e2
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:42:43 +0000

    lock thorough dick they below

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
