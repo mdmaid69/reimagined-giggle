066f0e8bd138e68d6cd5cd8c0993d695894fb7a9
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:32:16 +0000

    towards glad herself list cool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_item(array, i):
-        return array[i]
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def reverse_string(s):
+        return s[::-1]
