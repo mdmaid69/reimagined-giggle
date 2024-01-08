9093e8bb2046f61c120f39e3fe77e885b5ca1fab
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:10:59 +0000

    minimum last independent once clever

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
