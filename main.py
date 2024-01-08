43f55e48991ee9cb5cd34fcc87ad045057310dc4
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:37:14 +0000

    take negative holdout age impossible

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
