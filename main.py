668d89fb200283696f980e119e6e5598e617870f
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:52:29 +0000

    excuse blow pronounce forgive study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
