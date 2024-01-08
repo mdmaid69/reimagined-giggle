c56c96a3df184c088f7068c4b3b1dc7338f1d7ab
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:52:09 +0000

    secret congratulations tap Uu hospital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def reverse_array(array):
-        array.reverse()
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
