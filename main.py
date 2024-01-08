204cc30e8c3f04f323bf01df53fe29646338eedb
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:05:57 +0000

    behave your takeout try area

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print([x**2 for x in range(10)])
