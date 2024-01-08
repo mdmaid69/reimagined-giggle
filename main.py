2adcff8d8903601f85ff7c2d6bea29c6b0a095ad
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:17:59 +0000

    cook section bury characteristic necessary

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
