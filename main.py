c048020baa1d9654f917a96a1f143d835d2a02cd
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:06:10 +0000

    obvious oil runaway belong for

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_power(work, time):
+        return work / time
+def find_max(numbers):
+        return max(numbers)
