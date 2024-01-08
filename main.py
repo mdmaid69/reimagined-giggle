bf977cea52f0593ce9a92f618c950d05f320049f
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:09:00 +0000

    total let little zero suddenly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
