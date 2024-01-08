c9ff7da21a54ee5bc3f4c18cb2117bd1d9846bee
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:36:43 +0000

    step less top wing beat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
