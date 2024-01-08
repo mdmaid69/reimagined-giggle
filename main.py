400d919e74bec59e1c5fbc2c8f0f053eb2c676d7
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:04:25 +0000

    art love light breathe tooth

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def square_number(x):
+        return x**2
