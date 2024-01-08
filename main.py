d1ccd87a9b636c7811221c593a073d006fa4b4f1
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:25:49 +0000

    deep probably branch into throughout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def find_max(lst):
+        return max(lst)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
