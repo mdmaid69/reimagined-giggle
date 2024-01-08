a95a47abe62396404b82908ae1ba69d376231f0a
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:03:05 +0000

    occasion project taste breakup wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
