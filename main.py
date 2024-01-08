69725450db955dab3d296c5473a07c7bce0ddee6
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:05:28 +0000

    talkoutof name lookafter cover foreign

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
