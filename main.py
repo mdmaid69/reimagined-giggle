eeb9fc6cfa49a9ea3204c78de9faa119578d2bf3
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:51:59 +0000

    commit meanwhile like keepfrom typical

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
