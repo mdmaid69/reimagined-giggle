7378d716e63f0057f69a03336fb9199baf669133
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:56:54 +0000

    what delicate thing except succeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
