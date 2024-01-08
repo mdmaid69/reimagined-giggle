38d7a198c6e77de3a03f85826d061491b7239f9a
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:08:18 +0000

    wealth not holdout Ff chance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
