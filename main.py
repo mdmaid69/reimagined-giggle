3fa4031eba811bb0f011356309926abf47602483
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:01:09 +0000

    criticism song impressed paper badly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
