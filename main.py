5f4a973ac662ac661e0ae55f5695d5468ebaed82
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:53:00 +0000

    store practical emphasis reason classroom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import collections
-def create_user_string():
-        return collections.UserString()
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
