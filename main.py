420ff8e3b1a9c5d06ed3c036579df8ac320add36
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:49:45 +0000

    protest floor wild want spread

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import sys
-print(sys.version)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
