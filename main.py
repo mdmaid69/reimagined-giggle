311ddd29c59b7f719bfee81e9e412e653cd97e55
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:21:04 +0000

    test trade perhaps terrible why

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def convert_to_binary(n):
+        return bin(n)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
