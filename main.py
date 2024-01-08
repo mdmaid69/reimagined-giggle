151a768e7ed48c54f42c46616d4f1b0d3eacaa4b
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:10:07 +0000

    keepaway square cope truth design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-print(random.randint(0, 100))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
