0c6bc2f41847b78786f15e68c5beddc7f92766c2
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:40:51 +0000

    worst permission science lead alive

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
