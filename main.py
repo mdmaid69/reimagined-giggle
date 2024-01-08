6ae7ffcf9670006942b0340059a6bc1de8941b55
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:09:35 +0000

    steer slide bit proper error

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
