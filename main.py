dfb7378fd7b19c57ca42a4940f5c24e404016d37
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:12:32 +0000

    adjective bang himself sweet finally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
