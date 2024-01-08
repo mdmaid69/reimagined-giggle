fdb518d44198cce74d136bb249cdab46b503f81d
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:02:54 +0000

    separate sort Ii wide repair

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def remove_duplicates(lst):
+        return list(set(lst))
+import os
+print(os.getcwd())
