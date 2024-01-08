868bd43822933d1b63e14bbcee8145ef1e0f6e9e
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:07:09 +0000

    butter coat motion safe wheel

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
