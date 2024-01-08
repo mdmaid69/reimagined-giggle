d5fe8a96fbc7e474e94b7d17c87460d237a12780
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:15:41 +0000

    rob unite talkoutof music hello

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-print(os.getcwd())
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
