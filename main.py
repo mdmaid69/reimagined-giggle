08c5babd97d8b2b06e9feb01c2dcab8a5efb964e
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:46:04 +0000

    shrink dry powerful Ee government

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
