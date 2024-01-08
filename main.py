dc424d2d75fe04349b159c6770282323b9a94aa9
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:28:23 +0000

    in this shall pool string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
