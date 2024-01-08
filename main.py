c2b9b6a8463c390419f9fe36da46dd0e77617a4b
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:39:54 +0000

    bringforward pain official fairly autumn

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
 def fibonacci(n):
         return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
