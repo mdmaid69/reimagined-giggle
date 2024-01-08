0d8e39a5bd24bcdec58379e6eb29a2e99e33cb48
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:48:27 +0000

    lost or free approval clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
