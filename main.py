566c9448cea485cc97ed00b7ef241e6e7ad0e831
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:32:01 +0000

    570enjoyable thinkof lucky shade level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
