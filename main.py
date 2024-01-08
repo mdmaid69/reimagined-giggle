d30997288bf293710c6841d4a6faa4892d8bc69b
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:50:48 +0000

    surprise smart accidentally left form

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
