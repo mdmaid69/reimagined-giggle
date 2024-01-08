183d16a16909ae06fc55cea1c001b1a115e24c91
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:28:57 +0000

    principle zero announce game born

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
