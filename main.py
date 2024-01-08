ee70ebe150db128638a0e74b1a885800ef814c92
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:19:04 +0000

    task priority create process popular

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
