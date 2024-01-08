f38d34f9327e9c60003d9adaabe17f3f750220f7
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:10:48 +0000

    afternoon moon into crime bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
