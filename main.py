cf7f57cee9ee2a18372d7d020a25bb6323c3f197
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:14:50 +0000

    produce board number forever completely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
