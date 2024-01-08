9fb6fa4425451a1c57b039133d42c33b37205921
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:43:30 +0000

    when limit contrast lookback(on) basic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  def cube_number(x):
+        return x**3
