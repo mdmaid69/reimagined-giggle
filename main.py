7c8a21baf4d23bbc6968836b7059a12cf9c68a00
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:01:55 +0000

    each stop generous seriously thankyou

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
