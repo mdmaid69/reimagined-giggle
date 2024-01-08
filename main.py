b2296c1bcb0d7efe7566fe5db221da62ca2c3465
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:34:13 +0000

    connected head pronunciation destroy division

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def calculate_power(work, time):
-        return work / time
+import sys
+def exit_program():
+        sys.exit()
+import array
+def clear_array(array):
+        array *= 0
