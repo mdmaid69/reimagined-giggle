50a07d4987a8cd3fac49529724fa5631ae11842f
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:19:34 +0000

    calloff thought urgent giveaway study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_floor(x):
+        return math.floor(x)
