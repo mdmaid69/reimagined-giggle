3d147fb6601d10dd1f0bf1cae55de9dc66ae77e8
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:12:11 +0000

    mess oil fly walk outside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
