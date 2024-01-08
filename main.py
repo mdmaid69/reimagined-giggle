d3b339ee8fa9f0c077174d83bd504a2ea1c8bade
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:31:33 +0000

    cut pay suppose wonder train

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import tensorflow as tf
-print(tf.__version__)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
