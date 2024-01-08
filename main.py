1540dced27b4d9ff9b66dab482d3ae0baf667c62
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:49:28 +0000

    married nurse ignore giveup exciting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
