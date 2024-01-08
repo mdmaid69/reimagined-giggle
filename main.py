0766cd59ac506b61ca27e4fd31575f3c7abdab63
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:21:44 +0000

    attractive emergency government out task

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
