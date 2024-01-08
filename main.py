943cef026fb3f84d141180caa8f36634575cd785
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:08:55 +0000

    present supply whistle acceptable weigh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
