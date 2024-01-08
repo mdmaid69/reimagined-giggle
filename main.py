9df26124ff9e93bb9d7a116f3a7349b2384d4f9b
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:43:52 +0000

    gooff odd strike but explain

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def is_odd(n):
+        return n % 2 != 0
+for i in range(10): print(i)
