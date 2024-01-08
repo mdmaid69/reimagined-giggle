8369cd18fd89940d25dcdd5c494f7f91011755eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:06:23 +0000

    hello takeback able through side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
