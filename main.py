18608e33c6fe379a052a406aa49d2a32b075c066
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:41:13 +0000

    situation history they table innocent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import collections
+def create_priority_queue():
+        return collections.deque()
