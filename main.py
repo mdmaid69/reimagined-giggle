bcdecb5e15f55807b9d7b4b46fa08b7fafd1900c
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:55:38 +0000

    the Cc damp it preparation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
