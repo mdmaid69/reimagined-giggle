fb2a6c29d2c8cf4d8db64125715ca736d9a23cc3
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:42:48 +0000

    shape modern club criticize sticky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_as_set(array):
-        return set(array)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
