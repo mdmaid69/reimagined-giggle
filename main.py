a6221d6a72b9382d39f36411c2c1a9734e365dde
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:13:27 +0000

    claim your song positive unfair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
