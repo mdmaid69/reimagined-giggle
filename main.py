b64a9a4391b25d57c6ca0fd56a8c4522b59db935
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:26:24 +0000

    mistake sentence each means tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_max(numbers):
+        return max(numbers)
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
