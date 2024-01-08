db11b69eebb25a3a057ffa87c5eef1034b4b0faf
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:06:23 +0000

    damp exaggerate fight Kk reason

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
