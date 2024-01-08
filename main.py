f02f51ad3c600ddf0cf32d5a73c7785f3919a67b
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:13:36 +0000

    vote possession strict easy pattern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
