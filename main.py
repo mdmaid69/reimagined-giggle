8a6ebaad62c6071dfb2225336348c40d175d8a7b
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:38:36 +0000

    tour if aware Pp takeback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
