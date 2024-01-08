ff481797e343cac5c0346746661fa1fd95d66c34
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:22:42 +0000

    system what pass boring satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  def sort_list(lst):
-        return sorted(lst)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def remove_from_array(array, item):
+        array.remove(item)
