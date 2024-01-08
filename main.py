f05a4f4aad98866f011651eeb356c60f2c14edea
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:36:27 +0000

    foreign deep afford command modern

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
