04cf84ca9546ffad544bac5d7a9126fe738db1cf
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:18:13 +0000

    come makeup quick bother interfere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_volume(length, width, height):
-        return length * width * height
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
