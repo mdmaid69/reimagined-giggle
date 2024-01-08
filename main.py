41083dbcfc5a7adba67285ae9086a0c104ebe9ea
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:46:50 +0000

    public autumn relax nervous cheque

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
