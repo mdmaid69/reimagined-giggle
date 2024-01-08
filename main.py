2a54200d438192f08b99a885222229c7092805df
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:43:44 +0000

    owe basic ask blind sail

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def subtract_numbers(x, y):
-        return x - y
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
