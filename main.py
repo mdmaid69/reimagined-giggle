dc4701c21038638e878c905186f5a711dea0244c
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:33:43 +0000

    question beginning worth sorry rubbish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def find_min(lst):
+        return min(lst)
