f24cedfb21faaeb8c744498a0e1b4429a6cf63d6
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:44:31 +0000

    project keepup dark walk distance

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
