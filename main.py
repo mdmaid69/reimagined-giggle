88715a8c6f446160075e6ac64081e56c3d6f88f3
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:28:08 +0000

    gentle damage belief exam Cc

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import os
+print(os.getcwd())
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
