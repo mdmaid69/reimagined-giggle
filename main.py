e6ff59f074c22e1a62744ee34e1bb8cdf1975dce
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:50:44 +0000

    steep thumb until yourself half

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
