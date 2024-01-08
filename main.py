f28eec5595ab16c3004686c4469c3bd88e1affcd
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:34:20 +0000

    unite spin turnup force card

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
