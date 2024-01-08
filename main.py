2d3fa94329554d9bedfcac8aaf300e8d87407a42
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:03:33 +0000

    fast large success active crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def clear_array(array):
+        array *= 0
