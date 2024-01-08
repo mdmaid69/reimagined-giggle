d465bc8716faebf4c7f1510d663c31d8d3ecd6d0
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:15:30 +0000

    luck every mind possession achieve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
