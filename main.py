43d6b9dfdb9ddbdedbe14ea157e8c138bbb87ce7
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:20:34 +0000

    another ground rank shallow shout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def print_python_version():
+        return sys.version
