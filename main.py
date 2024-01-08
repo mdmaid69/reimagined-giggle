d777eee3d25ae97e3edb0386f70a1f28cc45c23f
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:25:39 +0000

    approve compare reduce corner comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_area_rectangle(l, w):
-        return l * w
+for i in range(5):
+        print(i)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
