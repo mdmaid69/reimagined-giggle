3eb92ec9550163d7800a015f25acb555b1881922
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:03:48 +0000

    professional plain opponent clean wash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
