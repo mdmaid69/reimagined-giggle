9b16b47e3f0f4f5ff60b259a8da60d8e512645e4
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:49:23 +0000

    ring rain right rest eye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def cube_number(x):
+        return x**3
