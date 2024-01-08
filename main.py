754fb47e989b1544e7d7be416c96066413ab74ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:41:29 +0000

    everywhere unusual description 572enter wonder

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
