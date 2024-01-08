4fb99567f026a0c0bac971841f87fb1c8b72ba43
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:28:08 +0000

    declaration fear hair consist kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import sys
+def exit_program():
+        sys.exit()
