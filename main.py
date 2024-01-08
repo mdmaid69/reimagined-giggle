b7a84733a9ff89dd30759ffda3b8f4cfafcd9fc2
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:24:51 +0000

    national head full answer coffee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
