bf8cc049eb031a993fb961dfe1eb003247f23155
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:39:57 +0000

    speed roll us vote thorough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_priority_queue():
+        return collections.deque()
