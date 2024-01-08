39357a06512e94e6605a1f2c05f66cbbc71e63e1
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:29:48 +0000

    scheme in wrap unhappy dear

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
