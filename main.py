36cf6e76c4a1d91158752f5dadee208cab928c67
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:07:21 +0000

    press relax seem point opponent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
