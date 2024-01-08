252dc13c908143bcbfe2cc4f24241d858b38452c
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:18:25 +0000

    weigh Bb building Dd shoot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def is_palindrome(s):
-        return s == s[::-1]
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
