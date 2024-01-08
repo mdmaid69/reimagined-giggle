2036a2a86e67149400a7e72f7fcdcc5425aa1289
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:30:28 +0000

    thought out thumb female middle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def get_array_as_str(array):
+        return str(array)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
