46eac73599dd736bddb3df972017a94c6162aee2
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:32:45 +0000

    guilty rest walk level direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import logging
+def log_message(message):
+        logging.info(message)
