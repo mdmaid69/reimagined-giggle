ad4e9848e7578b41d5486534f8731d4d5401d455
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:05:41 +0000

    empty in negative price form

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
