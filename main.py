8f7560879e30a3aabfcead4a0a95561df1055c64
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:01:18 +0000

    scared collect interest from loss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
