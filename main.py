c3d590246a4f792b5dfdba63ea420e2afe68957f
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:33:25 +0000

    nose word downstairs steal behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_buffer_info(array):
+        return array.buffer_info()
