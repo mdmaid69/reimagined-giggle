1489a4f15df29fc72be589a10c933c90ea727fed
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:28:41 +0000

    similar find love dig choice

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
