1ff0ec81b02fd45ab9caffe0ede13ddb0223233c
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:32:15 +0000

    interesting appear change target entry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
