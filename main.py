023516370d64185ff13d6ec9e5832473236d472d
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:57:07 +0000

    decide occasional comeup similar equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
