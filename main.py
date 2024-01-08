30221421657c09ab70bc457b466f8491d79687a9
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:42:01 +0000

    find people hers flat closed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
