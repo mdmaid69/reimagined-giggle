92b3b95a4a646590c8d4ca677f875f64379baf67
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:32:02 +0000

    worry union relief to less

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+print("Hello, world!")
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
