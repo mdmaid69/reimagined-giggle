28c6144287a675e36fb4cdbc5869371562062fc6
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:50:02 +0000

    odd its in freedom swimming

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
