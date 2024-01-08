e43ae3ce72ff4abf98950e6873d0ea8ec99c39de
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:31:46 +0000

    charge himself lookback(on) really doubt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
