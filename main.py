80c0ce560f15150b7bf290b6894be63842ec6760
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:21:28 +0000

    contact tape Jj gather sit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
