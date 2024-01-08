b8a7ba600d00461f7833001bf4d63eeac27375b0
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:23:13 +0000

    bringback drop count to surprising

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
