573f990009dcc876e2a86029dfa931980e7f2e85
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:38:12 +0000

    off mystery affect Tt concern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def find_min(numbers):
+        return min(numbers)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
