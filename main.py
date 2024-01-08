21b253863efd5f7f780889e900526d14540b5501
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:31:29 +0000

    crowd climb fat flight slightly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
