73f7969fad6ea1a433d48cfe006f6d89025f6516
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:12:04 +0000

    artificial whether let situation hair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
