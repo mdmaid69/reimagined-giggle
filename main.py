ad0f0f87c1f8b088c263d805ef8f87b4fa94cc24
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:33:22 +0000

    rush break constantly memory case

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
