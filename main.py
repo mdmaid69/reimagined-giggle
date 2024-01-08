154fc342d97db4415897b31042b7ac85f9b5b7c6
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:12:33 +0000

    occasion tie discover doctor fear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
