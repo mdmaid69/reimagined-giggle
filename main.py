f85d7783f232a8be27870441e2fb34a6a10f7050
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:00:27 +0000

    head anybody describe violent earn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import datetime
+print(datetime.datetime.now())
