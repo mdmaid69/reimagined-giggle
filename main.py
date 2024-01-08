472a6e413e6703650933fcf5905085177ac2b265
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:15:33 +0000

    relief offence smell area disgusting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
