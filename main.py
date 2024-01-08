5ea410af380fb44551f94b1453b6b5cf83d0d959
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:15:42 +0000

    estimate address boat interview hold

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
