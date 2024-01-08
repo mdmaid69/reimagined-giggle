2489690308cacd8365adaa054b265de8b71d81b9
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:21:31 +0000

    report adjective test live stir

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def find_min(lst):
-        return min(lst)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def set_array_item(array, i, item):
+        array[i] = item
