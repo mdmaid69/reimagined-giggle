adedb9931e600ea11d9ad143afba2f830f784881
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:38:54 +0000

    first when father common official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+from collections import Counter
+print(Counter("hello world"))
