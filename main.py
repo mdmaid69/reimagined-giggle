af60bac0088d977cfae0a731a009c37bbc804dff
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:57:02 +0000

    attach immediate different poor attract

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
