e02f7e0044aa39f5e72e7cbd876af2bcd940c07b
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:39:18 +0000

    wait introduce respect build because

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def add_numbers(x, y):
+        return x + y
+import array
+def get_array_as_set(array):
+        return set(array)
