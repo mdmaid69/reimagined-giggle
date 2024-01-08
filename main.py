51c9b58b5d82aa181c830a221ac79cbe70392b67
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:30:51 +0000

    study neither free real practical

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
