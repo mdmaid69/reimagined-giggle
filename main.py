e332e77c744e1c455edb11d300bb55fcaa492cc1
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:15:27 +0000

    failure slice natural gas calm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def multiply_numbers(x, y):
+        return x * y
