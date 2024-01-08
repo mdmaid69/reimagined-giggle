e4d543ff6c1c1d30e35248fef464b7878ae3f8f7
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:20:57 +0000

    will holdup space look disappointed

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
