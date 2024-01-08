c157d43d369c87fa994f15326bba9938e7a925b1
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:06:15 +0000

    prepare closely hit famous comeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
