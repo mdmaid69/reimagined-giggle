95a820a73e2775ab597c4f270f15f99bbad968c7
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:46:40 +0000

    to cry style repair load

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
