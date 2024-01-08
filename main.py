2cc601ee13a88368e725f0bd7bcfee3ff3663eb2
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:10:02 +0000

    once much choice patience ball

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
