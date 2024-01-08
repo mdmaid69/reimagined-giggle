b5bf166d6e62b6726bef7985ae3c0e70d8ecad4b
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:42:02 +0000

    along official discussion want good

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print(time.time())
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
