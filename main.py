c9f3c434dfc5422a4152d43c146b14e7f3fa8478
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:19:58 +0000

    bury remain mark agreement shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
