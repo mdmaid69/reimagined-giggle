0af8f07655b0cf3d815ec30038edb7931f49139b
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:02:58 +0000

    drop reaction weather rescue sure

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
