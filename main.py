b5cc70cbbece84894cb0ce7e85fa23b0503125eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:47:51 +0000

    nowhere snow build support turnup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
