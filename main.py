bd8049de583a00d2c76f50a525c4117d61af2ffc
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:31:07 +0000

    turnback tune regret wave operation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def count_characters(sentence):
-        return len(sentence)
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
