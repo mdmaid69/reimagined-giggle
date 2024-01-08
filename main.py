caf61df2de3e4015ac9e09dd3fbd604080a49543
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:23:24 +0000

    healthy wrong university by poor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
 import random
 print(random.randint(0, 100))
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
