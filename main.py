1893601a56645af791e161ebb09302edffb3cfda
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:50:24 +0000

    alive wheel black accidental mad

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
