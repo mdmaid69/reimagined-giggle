fbe6619447416c746ae5b724c087a631d143ac53
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:10:52 +0000

    plate pair dear doctor worst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def convert_to_octal(n):
+        return oct(n)
