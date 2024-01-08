2b81328220c824ed115d5771bcf748e2046c7435
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:33:03 +0000

    on value light boring smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
