d09790e7d5bfad688ad8c4a4eff341462210f560
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:21:57 +0000

    answer entrance used private pleasure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
