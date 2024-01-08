d4ad459336346882aa3b7fd316a016db3d9b2bdf
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:55:48 +0000

    doubt hold determined fun correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def delete_file(file_name):
+        os.remove(file_name)
+def subtract_numbers(x, y):
+        return x - y
