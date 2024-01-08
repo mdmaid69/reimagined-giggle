9033579b5f46352d25c036cb1dd37e376481eb08
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:55:48 +0000

    side talk secret murder rely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
