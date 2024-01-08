f183bae10c48c430c336fcb3a99cb56d13aff3f8
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:13:57 +0000

    attractive exercise poison Pp cut

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_item_count(array, item):
+        return array.count(item)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
