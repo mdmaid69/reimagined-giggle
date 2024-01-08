8df89ce8ad1d209c9505720422157c9006acb64f
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:35:15 +0000

    cross taste future rare sweep

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
