db4d611ed922ffcd731ce7896541c36abf05ca71
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:14:03 +0000

    play bored wealth Oo awake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
