ac2dc0b440c3f348456b0b727155690bff0ba1f8
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:43:43 +0000

    press ahead broken possibility turn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
