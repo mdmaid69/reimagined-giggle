74a74607966be627b42b5f487aa903815851f97c
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:20:03 +0000

    sometimes along yours apart wish

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
