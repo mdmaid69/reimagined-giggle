ff5d0b047508ed5486f4f38643007762b2827da4
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:05:34 +0000

    well comment cut obtain carry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import time
-print(time.time())
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
