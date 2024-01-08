3cdd8fad32681e0cb5dcf802bd407de322049742
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:24:17 +0000

    beginning tidy ball feel mean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
