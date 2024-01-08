2c92d698d9f0bef157ff09c0e354e43eee2cde9c
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:04:00 +0000

    theirs bet gather more perform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
