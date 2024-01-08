4c66562e86090ae59ca8a4bec8547599293582f0
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:04:03 +0000

    thought forever visit future stranger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-for i in range(5):
-        print(i)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
