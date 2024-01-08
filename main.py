1a4c29ec1db09b4d7e245ade48419f3c6d380f67
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:33:39 +0000

    sister lunch sleep anywhere hang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
