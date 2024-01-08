cc66f53f1f956225a344aa0519ac877235059955
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:21:42 +0000

    translate famous over whatever nor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
   import os
-  def get_file_nlink(file_name):
+  def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
