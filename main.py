05664146289dc4106cac08a058be2cc4a60dbea6
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:10:45 +0000

    add payment complete house far

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-def square_number(x):
-        return x**2
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import tensorflow as tf
+print(tf.__version__)
