df1ebb0d7c10a6c9905d1b084f5ac4c42db245b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:04:26 +0000

    believe keepup name appear naturally

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import getpass
+def get_username():
+        return getpass.getuser()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
