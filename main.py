b6c4b6cdde1854312e1038f2874cb142d3648079
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:19:14 +0000

    holiday boat flow care finally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
