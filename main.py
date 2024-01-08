a8c86b5bd42595014ad1fb60d1237c392cff49db
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:33:31 +0000

    lend female any plenty admire

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
