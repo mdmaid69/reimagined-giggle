ef39b1a717cc9fa33a1e4f1169f5d5b583f463e7
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:47:04 +0000

    probably fear basic year shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
