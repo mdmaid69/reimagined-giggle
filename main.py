f408e44d9fe727265604eb7449ad65731c682721
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:59:33 +0000

    shout breakdown supply discover hold

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
