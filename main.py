17b36d899e2b821ae556878a190c5e256b03107c
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:20:59 +0000

    score share better shopping hide

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import time
+def get_current_time():
+        return time.time()
