7fc13f0bd8903483a4cf7ea7feb75a77db70933a
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:45:04 +0000

    share squash passenger side loud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
