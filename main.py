eede43aa2c2b680b7e0a38a5ffd2f88aa8340be3
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:34:49 +0000

    give heavy modern ring pump

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+print("Characters:", len(text))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
