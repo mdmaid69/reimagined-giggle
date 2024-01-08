f7906e8a1776e37d66fb91bceb78a863b0fa0e35
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:24:31 +0000

    famous burn interesting gooff thanks

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def add_numbers(x, y):
-        return x + y
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
