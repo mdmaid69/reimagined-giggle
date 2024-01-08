8e2c23ef26f945f96601b258a291c0fba9e42b2d
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:26:22 +0000

    to prepare ready let limit

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
