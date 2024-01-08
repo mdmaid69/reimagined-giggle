079f4582390900a5e1c050e75e60a8d9a65d85e9
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:00:19 +0000

    surface cry city male generous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
