5023ecdd58bcac7a005222d8ce221e0eb6e803bf
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:16:43 +0000

    nerve word car mean wash

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def is_odd(n):
+        return n % 2 != 0
+  def is_odd(n):
+        return n % 2 != 0
