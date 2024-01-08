65269c0af4365d38215b6b2c8ff018e3835502f8
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:56:27 +0000

    demonstrate conversation expect shopping hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
