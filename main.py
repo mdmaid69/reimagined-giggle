0e62444af5a0e418affc14b8451a002e1ccfff6f
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:57:45 +0000

    why makeinto rain spoil admire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
