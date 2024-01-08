a3c2871e75e8fbda729f42af9fa750a0bc10e8dd
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:18:07 +0000

    term should hungry error pair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
