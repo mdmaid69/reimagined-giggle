83603533cb17437a3e86525e46f8fe986589c7f4
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:16:00 +0000

    hurry door union say Ee

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
