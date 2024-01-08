061824bb691bc64808cbaffb88e8f013da05bd5e
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:27:10 +0000

    afraid sign do geton Ll

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
