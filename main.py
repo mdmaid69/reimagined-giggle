030f136b9c1531ac2f2ce1f3e083d553400af226
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:08:04 +0000

    slice content bus inform stiff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def reverse_array(array):
+        array.reverse()
