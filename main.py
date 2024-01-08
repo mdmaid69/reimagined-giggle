0fdaa76e1680fcd721d0a53a3455275f607634e2
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:34:02 +0000

    callback welcome under bury why

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def get_time_since_epoch():
+        return time.time()
