2cdea2987e3e8d477b186c194f0aecc13d3628ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:32:12 +0000

    tap look where success to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
