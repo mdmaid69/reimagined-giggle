0db9092090ec8666900c7740fba39f79c2ce4503
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:05:51 +0000

    length excuse world law wide

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def find_min(lst):
+        return min(lst)
