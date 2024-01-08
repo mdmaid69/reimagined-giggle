166a198562c539aff659b314f9e2297c23cdae23
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:40:25 +0000

    born everyone soon deliberately previous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def convert_to_hex(n):
+        return hex(n)
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
