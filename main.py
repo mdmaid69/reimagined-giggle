18719e467bc3b0c9a39ce5945a54ee92de587ba9
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:10:29 +0000

    boring book none report rescue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_average(lst):
+        return sum(lst) / len(lst)
