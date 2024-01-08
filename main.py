3c8f1c982f5ef43c55f444ba0c8a268637f3f108
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:25:52 +0000

    lookaround measure difficulty remember dinner

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def convert_to_binary(n):
-        return bin(n)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
