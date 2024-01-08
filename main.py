51c6f362137f78bdd3fa1f8ced60426166cfe42a
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:55:08 +0000

    dance afternoon surprise strange Uu

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import numpy as np
+print(np.array([1, 2, 3]))
