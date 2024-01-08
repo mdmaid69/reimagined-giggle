7739cd8c4ca31e2e4bb22704ea8dddb04fa3d56f
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:42:37 +0000

    artificial table opinion excuse message

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
