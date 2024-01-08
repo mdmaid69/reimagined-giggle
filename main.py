af30f5eb82aa1c02556202ac6b3ebb25e36d634f
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:59:46 +0000

    injury company should front goon

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def cube_number(x):
-        return x**3
+def calculate_acceleration(speed, time):
+        return speed / time
+def subtract_numbers(x, y):
+        return x - y
