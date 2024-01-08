c86dc27152cf338953e98a88ad5dc5b66f9cf446
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:05:05 +0000

    upset bringback road experience compare

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def cube_number(x):
-        return x**3
-import collections
-def create_stack():
-        return collections.deque()
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
