951ad7c97aadfd17a7188d6d21068d6d3397ab2a
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:09:42 +0000

    deep sleep big dig every

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
