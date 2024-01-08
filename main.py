ccb16d9c08868e08b7dc4f0cc162b5ebfb337649
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:40:05 +0000

    splash perfect taste train public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
