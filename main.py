f579fb3c2cd5f4b485fa4431840efc196c500c19
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:04:07 +0000

    impression office suggest apologizeapologise shop

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_density(mass, volume):
+        return mass / volume
