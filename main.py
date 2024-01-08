1768f3e474d5f1f8a25736f4c9d025efcbac4ae9
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:51:54 +0000

    stone probably best dinner precise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  def sort_list(lst):
+        return sorted(lst)
