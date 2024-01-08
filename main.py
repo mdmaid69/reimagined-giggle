b7c08c278aa45012e34f46b132c91b087dc2636e
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:54:05 +0000

    family teach live reasonable match

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  def sort_list(lst):
-        return sorted(lst)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
