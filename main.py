a6bae6128f88a84e835d0afc6dd9dc559150adc2
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:49:45 +0000

    steam laugh quiet official progress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
