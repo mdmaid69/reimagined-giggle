a406d356915184a065e8afc1502ff6e85bb65887
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:00:48 +0000

    change question crowd breakin hot

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import getpass
-def get_username():
-        return getpass.getuser()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
