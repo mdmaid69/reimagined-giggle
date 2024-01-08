6d5daf2a03a64d9875b16f2e9a6c251e2bbf1b04
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:26:27 +0000

    film exactly close poor whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
