4ea7d4397aa0fa715d253f70fd0c93a66ce9534c
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:24:20 +0000

    sport phone claim display far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_user_list():
+        return collections.UserList()
