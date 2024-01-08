8281ede281f58ce8b225cd38a08f6c609fc9d02e
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:36:47 +0000

    excuse little raise face used

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
