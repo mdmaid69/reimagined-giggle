f798d3c324ca8fb8ffba735d6e6c0d33dc0e67fd
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:08:16 +0000

    survive brown learn part contrast

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
