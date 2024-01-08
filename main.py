689c4c1e3f7a07a956a239f0b9a535f81dc2a416
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:19:46 +0000

    he breathe sticky wild everything

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
