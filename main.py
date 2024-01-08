55a49f8cd2b13b097fa2fa7b0b9f37effc1bc6de
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:15:59 +0000

    actually though order possibly pleasure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
