c03af09332f18f32255304270a9fbcdb49ca96e4
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:45:19 +0000

    recovery high she contents square

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
