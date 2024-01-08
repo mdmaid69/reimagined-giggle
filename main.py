cbfeb90a85977b50937cb3581fe99081969fcf9e
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:54:37 +0000

    not off building cap else

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
