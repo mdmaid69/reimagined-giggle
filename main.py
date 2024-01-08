dedfb9b37a133b7c8d21ba6890c7bfe4d462c71e
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:15:27 +0000

    under nature keep warm recently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import sys
+  def get_python_version():
+        return sys.version
