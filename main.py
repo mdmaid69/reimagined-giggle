9ce49caf5f97451db357e4684ad95868bd08f1a2
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:59:36 +0000

    complicated blame news contrast progress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
