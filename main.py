3afb938628da7e8a56e007acc3612a433c7a5348
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:20:31 +0000

    development petrol similar comeover mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
