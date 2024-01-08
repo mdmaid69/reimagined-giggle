961835008e47e38fd7783d75b1df73a48780214d
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:17:42 +0000

    border warning house callout welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+print(os.getcwd())
