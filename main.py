2f7d61cdeefba1b2e1649ddd10454a742622580e
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:24:09 +0000

    dance range tread bored Zz

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
