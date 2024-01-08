1e82b55b8b17bec76c48440e9bf59f5c5b94efb0
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:47:02 +0000

    interested snow lend ocean community

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
