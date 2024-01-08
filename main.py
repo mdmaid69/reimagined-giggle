a5c2dd078b77fef8676b517759282fd04c2b9b46
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:51:15 +0000

    change shallow release copy crazy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
