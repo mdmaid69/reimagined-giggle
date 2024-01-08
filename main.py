b82d49ac48caf692ef0cfbb1d633c528ae0c3f02
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:01:08 +0000

    fold brave dear like definite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
