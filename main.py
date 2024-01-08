41459e366828cbc84d10bc8e26fd80ac825b17f4
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:00:55 +0000

    substance pen than doubt or

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
