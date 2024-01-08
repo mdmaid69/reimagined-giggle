c41a2010904acacc4c36f4f74ee0eb5619f7c0d7
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:28:58 +0000

    marry callat split attempt protest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
