e1bcb7f309102115c34b128543648eb3b0f27c1c
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:53:58 +0000

    business exactly further 570enjoyable quantity

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
