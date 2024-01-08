c130d912782508dc69a1fe0badee2efa9c7e5886
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:38:32 +0000

    official creature tongue trade wander

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
