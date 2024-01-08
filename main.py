a8655494077f21f4eb8222fa077cccd40e318dd6
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:42:27 +0000

    post similar giveup never straight

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-for i in range(10): print(i)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
