9f764b41757daab7b93ffc3899c9735be1ac73b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:40:37 +0000

    Zz occur last traffic tooth

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
 import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
