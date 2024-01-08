85b09d3d2535cfe741e2220081d3c94ca21c4cc9
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:36:52 +0000

    there uncomfortable exchange that unable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
