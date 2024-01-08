2c6774836e2f28b508155b74061d94336e3f3388
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:16:07 +0000

    mad beautiful us next soil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
