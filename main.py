827d8dde976e46ef072b312a5dc8dbd356ac35c9
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:59:04 +0000

    fact cry peaceful them object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
