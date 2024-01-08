8c7cf9e17a29f27668ec4ed0f7064cf801140ac1
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:05:55 +0000

    children tip judge trust slight

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
