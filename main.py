e0046145ea79d87e1c6183ba8491ba41f73c0e04
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:01:37 +0000

    fear responsibility learn result attention

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def convert_to_hex(n):
+        return hex(n)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
