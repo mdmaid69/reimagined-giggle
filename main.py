6b3f98c590c1c5017796311598623b3fba5304bc
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:40:40 +0000

    used bite plenty famous behave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
