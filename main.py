44399a7b11ad7bc0af6cf1679dd00a9cd1158034
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:43:47 +0000

    strength tragedy wrong since score

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
