39a2d04999e4085a2582347df3626a09edd6c43b
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:32:41 +0000

    pot separate not plan its

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
