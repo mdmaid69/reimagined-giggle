f2ec3da3c06b49e500abc8889b0584c19b98dbe2
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:27:05 +0000

    favour respect heavy uncertain shock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
