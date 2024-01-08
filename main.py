636ea859d9730e58d82ef05b7d853cea78192452
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:57:26 +0000

    high complain Ll suit row

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
