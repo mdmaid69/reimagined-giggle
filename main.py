3d1cc4a60e55b43f5d2c109a80cde271b929b4ec
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:13:55 +0000

    comeoff otherwise spread university thankyou

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
