c10cd47ed59cd7409e2e23a0e0b28f5e0afd848a
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:03:16 +0000

    exhibition remain popular play dish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
