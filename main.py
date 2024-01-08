3cf3d6e27e71645378dfc1c23281cb96d91fe0fb
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:01:07 +0000

    best commit sorry lose pause

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
