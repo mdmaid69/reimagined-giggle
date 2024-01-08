7fee7986f89e0c6c489dae22843baca43dc0481a
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:57:12 +0000

    total on design much important

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_area_circle(r):
+        return 3.14 * r**2
