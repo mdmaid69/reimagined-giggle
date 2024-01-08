fdc60739cd932cb95858d7f4226a3d3c19d72dad
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:52:51 +0000

    upstairs recommend game sense dream

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
