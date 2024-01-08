a589390e62a24b58a57e460e657df1a6bf6dfcbc
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:17:07 +0000

    impression unpleasant fear nervous squeeze

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
