0193acf1c65cd8754d74b6fef652249e1fe4342e
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:48:17 +0000

    today behind meaning wave hand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
