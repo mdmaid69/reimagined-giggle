54857b8947db37b3dbd395690315d1b25e2764b9
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:06:54 +0000

    exist learn telephone child distant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
