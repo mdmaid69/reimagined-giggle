27c8bb709f6b0c64924165f05c265c69fc16b4fa
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:05:44 +0000

    noone base spread this artificial

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
