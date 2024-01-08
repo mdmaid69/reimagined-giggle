1b2ab07ee2817c53c4b8e086ced7f3a3e4b446fd
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:09:11 +0000

    mix load lookafter him care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  def cube_number(x):
-        return x**3
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
