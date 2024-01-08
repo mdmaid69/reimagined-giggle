5950d528df2852fcd2be07692c5a344b71e5ffe8
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:28:56 +0000

    doubt surface valuable obtain 568energy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_power(work, time):
+        return work / time
