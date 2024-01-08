3af13b8db2149c0e32e3a64565e818f257880b90
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:13:26 +0000

    squash wise environment church clear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def remove_duplicates(lst):
+        return list(set(lst))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
