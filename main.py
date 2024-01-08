ef1ac8765fbdb42383b195506dffd797160a9be2
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:39:27 +0000

    classroom already which callout blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
