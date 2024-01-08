28abe6b887dcbc21065508b1f8ba4a8ce22235f2
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:25:18 +0000

    science pronunciation round boil turn

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
