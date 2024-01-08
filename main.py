e29036e6a3d2b721ae1bb35f68a8d251625dde7c
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:06:00 +0000

    beautiful jump rarely 571enough high

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
