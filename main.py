24118f5f50418e05205f862767d05afb54ffd944
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:08:26 +0000

    pencil eye new left mother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sine(x):
-        return math.sin(x)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
