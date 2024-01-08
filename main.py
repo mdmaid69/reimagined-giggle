82458ed934e0073306d00ec690df3866511bf5c8
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:00:19 +0000

    relief final exercise must cheat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
