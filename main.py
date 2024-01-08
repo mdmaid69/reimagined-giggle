1c70273ef641fa268696fa286d207539e11f0db6
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:16:01 +0000

    enthusiastic rest repeat stop cheque

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
