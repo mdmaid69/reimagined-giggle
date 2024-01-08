2dcefbf9e5b21a6f4c9f77cd8fc13135aae4ddef
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:38:36 +0000

    bus communication turnup test story

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
