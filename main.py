3e09162d7f2e138379b57a0701bd8ea5961f53cf
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:20:54 +0000

    list sting swim risk backwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  def remove_duplicates(lst):
+        return list(set(lst))
