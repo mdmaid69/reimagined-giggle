e4c5b93aad2f5837a9c1df90a51053f2afe465dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:01:10 +0000

    previous view draw still spoil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
