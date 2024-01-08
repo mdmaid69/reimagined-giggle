49bc69f315c4da55f241467dd98a6cc89e879324
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:03:43 +0000

    speech size little comeup goup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
