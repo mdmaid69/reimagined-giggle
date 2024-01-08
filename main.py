d3b9a99091403fd474c285ae9e46926f7799c182
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:48:32 +0000

    slice winter chance please return

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_power(work, time):
+        return work / time
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
