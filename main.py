80108a776931b66e2cd0ce9f2cb7aa62cd92530d
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:13:38 +0000

    transport whenever see think development

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
