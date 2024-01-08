d3990265eca93fc8c6605bc30addd4366e581f9c
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:04:32 +0000

    fresh in rank but boat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
