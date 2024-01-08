6ba6f423839f019075391d9f4832e88b6fcb5a67
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:40:29 +0000

    tax become importance turnup strong

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
