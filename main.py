fedc276c32dce558c8c827e170e5352e6dfc57a0
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:42:55 +0000

    shop fill refer nigga extra

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_tangent(x):
+        return math.tan(x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
