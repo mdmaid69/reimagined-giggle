c6df30b5f5f05b5ccfb3f8db47e14f8add0960e2
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:37:47 +0000

    kind spoil study depend total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
