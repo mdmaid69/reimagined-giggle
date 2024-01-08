f0f2c165978adc70c2d10e31074ed077582ab251
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:35:12 +0000

    operate broken computer him pain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+print(sum(range(10)))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
