153ed780c3720391e1ad6e489d48ab1b1e292844
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:35:25 +0000

    face mad win complete by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
