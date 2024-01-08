3e5e976fe6ff1a1fefc0d52d4b5a970e22e5be93
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:33:25 +0000

    possession track plastic total comedown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
