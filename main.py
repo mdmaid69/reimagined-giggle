b2f4703230e5ac1aba0b54a15f9ce370448b9178
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:03:58 +0000

    hand new different myself sharp

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
