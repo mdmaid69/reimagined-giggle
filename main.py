6e97afe5ec255cadccd54ee0cb40ee996b544561
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:36:23 +0000

    scream test acceptable object window

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+text = "Hello, world!"
+print("Uppercase:", text.upper())
