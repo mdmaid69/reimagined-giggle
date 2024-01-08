ea065ec06c1c4ca87cbb05799fa2df521defea9b
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:14:54 +0000

    me real pride row worth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_length(array):
+        return len(array)
