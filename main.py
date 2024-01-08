2c5743d635a612205a4729cd61b3de6bd4c3ff05
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:09:05 +0000

    pull trade whistle generous inquire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
