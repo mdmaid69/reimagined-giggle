6eb1bfed5482dc9193f78b23a96a8825974665d6
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:38:13 +0000

    away mail deliberately punishment well

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def get_array_as_float(array):
+        return float(array[0])
