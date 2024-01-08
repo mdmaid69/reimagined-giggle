d6f63520fb6051ac5a26ac2b7f12fcf414f28d8f
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:57:51 +0000

    live tell basic continue available

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
