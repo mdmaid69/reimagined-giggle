74d85197315f564dcd9da9cfcddeb667dfee11bb
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:46:59 +0000

    especially less break production winter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
