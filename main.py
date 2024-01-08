d5b66ca519f608952fba508928162515ff44ab7b
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:47:16 +0000

    space influence process workon total

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
