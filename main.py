729ade1bc57a2970e8d27364efc493986b20a6a1
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:15:42 +0000

    plant strike silent runinto help

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def convert_to_binary(n):
+        return bin(n)
+def reverse_list(lst):
+        return lst[::-1]
