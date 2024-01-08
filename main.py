01d26e1730bb310d8903622d55549f6f3ce2e17f
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:38:38 +0000

    someone anxious even stretch set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def sort_numbers(numbers):
+        return sorted(numbers)
