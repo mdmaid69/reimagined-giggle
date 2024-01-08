eac2b0e40fc73422664a996e9d6039b22175d4fd
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:11:36 +0000

    woman rarely giveout comment nervous

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
