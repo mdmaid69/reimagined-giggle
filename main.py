9f6b6e0da0a10af405103f49f759ee4544b439f0
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:56:34 +0000

    ignore or precise feature surprising

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_array_length(array):
+        return len(array)
