2390e000cf505a0cab3bf15de37f4e553e9eb826
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:55:56 +0000

    calm hurry lose common carry

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def add_numbers(x, y):
+        return x + y
+def count_words(sentence):
+        return len(sentence.split())
