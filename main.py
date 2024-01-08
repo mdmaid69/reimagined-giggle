1e9cdba34763cd3e883727a9c36db03dbaf07e98
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:24:09 +0000

    own watch slow somehow like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
