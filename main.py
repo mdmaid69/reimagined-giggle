17c55ae50f6fa7873ab213ad99e1659c501b1f62
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:06:03 +0000

    takeoff bring organize sink fork

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
