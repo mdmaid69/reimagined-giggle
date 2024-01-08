33337ccc1af2a67c50e9a1c11022d2ab65f720f0
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:19:19 +0000

    crazy drink that butter push

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
