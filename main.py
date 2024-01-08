099c725085f8c5ca77a560136be504a988320d21
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:01:19 +0000

    serve rise mass yourself people

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
