ff1d8bdcd06583829be77f976dfa8d85f18c0e5d
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:10:01 +0000

    extra race performance 573entertainment die

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def remove_from_array(array, item):
+        array.remove(item)
