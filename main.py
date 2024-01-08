9f1226a36c9822b7dc4f4b8923a74679b783b3c2
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:41:08 +0000

    cheek sticky read thankyou weigh

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def find_min(lst):
+        return min(lst)
