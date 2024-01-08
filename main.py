09388d2199a3afd84108c054e24b097354e9227e
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:42:30 +0000

    standard float name buy skill

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def greet(name):
+        print(f"Hello, {name}!")
