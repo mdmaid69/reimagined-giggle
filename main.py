ada2897fb1195c40550f8ff7306faaeaeea0b2be
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:58:10 +0000

    act Cc tell interest holdout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
