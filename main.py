bc0856e52e31f2b9f7828a21cebdfff1cda31a84
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:53:19 +0000

    young behave present anywayanyhow improve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
