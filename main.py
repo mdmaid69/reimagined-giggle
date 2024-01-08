9b9ac70e320b64cc438a2e835a6b9f89c40493ab
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:13:52 +0000

    practical error bath I visit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
