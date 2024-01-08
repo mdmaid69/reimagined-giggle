0bc34242903f2aa68989ec19c30b7cf21ccbffa3
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:34:57 +0000

    thinkthrough afraid season goon hot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
