82ba8a03bdaeef161bc118d463930c150e9d574a
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:09:52 +0000

    father comeback point hide ball

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
