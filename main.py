33127e658c9951bdc3109475014e9f8158b0a035
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:49:10 +0000

    fork nearly comeback write everyone

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def sort_list(lst):
+        return sorted(lst)
