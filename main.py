72cd74b3b4618cf44b459c91cb7590459b0f6fdf
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:53:07 +0000

    reliable backwards broken wire straight

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import sys
+def exit_program():
+        sys.exit()
