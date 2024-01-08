2318fff8782efbf51fa2f02b8603c95dcefafb6b
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:23:37 +0000

    giveout wrap storm bath dish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
