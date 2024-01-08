1e5a180f0da66ed9909738b7ef7f165d6e7b201d
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:55:58 +0000

    prepare stop keen feature its

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def get_array_as_float(array):
+        return float(array[0])
