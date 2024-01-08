a9e77611ebdd715c7d3f4519d84acda30a3c2b40
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:08:56 +0000

    quick study spring whisper refuse

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
