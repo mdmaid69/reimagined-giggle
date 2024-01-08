0d91ce71a4ac47237e6521f76f4ee8cd1128fd78
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:32:10 +0000

    negative argument aware support medium

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
