54eb439466fb5cdc4bceaa13b32acb901ec258e6
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:50:42 +0000

    switch little seriously care firm

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_pressure(force, area):
+        return force / area
