7b67de1e1b952cf0a13e2e60d1b5cafbcda13d14
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:38:28 +0000

    continue turn scared bored common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
