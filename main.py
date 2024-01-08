cfbb7e04e1015cd9788dae3a1148beb8ead5a0e7
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:52:03 +0000

    jump before difficult crowded anybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
