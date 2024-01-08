4e71f1e3315abafa45a0e25b654357045015aef4
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:18:42 +0000

    them shade outside call patience

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
