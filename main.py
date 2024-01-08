a97bec1ce6fb08ab6abac04029a6b2a1ab5d3b62
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:05:30 +0000

    contain inquire Tt both exist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import random
-def roll_die():
-        return random.randint(1, 6)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def sort_list(lst):
+        return sorted(lst)
