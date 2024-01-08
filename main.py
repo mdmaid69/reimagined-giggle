d54e07bc73926cbd89856b9134883ee81208426d
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:41:24 +0000

    wish friend positive much practice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def cube_number(x):
-        return x**3
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
