a07e3681cebf08247d832b95c945554fab72cc1d
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:18:31 +0000

    shout thick congratulations destroy strange

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
