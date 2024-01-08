f388fc6ed4af5c250933e81f682aff53b8777da4
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:16:34 +0000

    before pretty condition news hate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
