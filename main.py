edf7f6cbb2daf1d9a7608d6df8ee63063ae1fa4c
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:51:42 +0000

    walk cinema whenever suffer lie

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
