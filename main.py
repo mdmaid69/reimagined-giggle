2c17cfc6c4d95dd5fe46cce92f9a6efc9f59ff21
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:52:58 +0000

    total spring pot although impossible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
