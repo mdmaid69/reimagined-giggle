e3cd7649e5f7b388ce006d173969fe0a3736e4c4
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:11:48 +0000

    tense hit calmly terrible command

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def square_number(x):
+        return x**2
