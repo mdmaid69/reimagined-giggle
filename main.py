f715ec7de9e0222140e2d6a0d30bbfa1303d97ef
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:14:48 +0000

    cum eager satisfied slide deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
