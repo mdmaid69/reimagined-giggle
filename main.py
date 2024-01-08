421287ffe5306576671f36d2a2c3b0e19a8dbdb7
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:03:29 +0000

    through cross inform pity peaceful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
