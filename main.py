ecf5b565450e64af6d25aea82500afa8158067a3
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:25:27 +0000

    edge deserve protection prevent shut

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
