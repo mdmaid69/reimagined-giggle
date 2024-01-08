25282670f834ea7d1fd07d258fdf87e16ff08927
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:41:39 +0000

    but business remove whistle lock

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-def log_message(message):
-        logging.info(message)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
