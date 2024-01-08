29efcabd1586cc93d4a93b089c34240a8baa3897
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:35:35 +0000

    suffer drive heavy patience finish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
 import functools
 print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
