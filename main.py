9ec54a62985c12b4bc68157edbc9837e75a2e64e
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:52:12 +0000

    dream ticket skill talkback unusual

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
