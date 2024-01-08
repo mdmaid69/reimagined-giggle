427bbdc9533af0c12c847c910e11e6d3158021c6
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:41:30 +0000

    shop turnaway Itself comment door

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
