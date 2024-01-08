ac03625785f28674bf3193e7446df848ab4a8b45
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:15:58 +0000

    forward real ocean above broad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def count_elements(lst):
-        return len(lst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
