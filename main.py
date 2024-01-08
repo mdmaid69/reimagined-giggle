6a448b0893dc7ae3ee9fad7716b96011b8c204eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:45:35 +0000

    destroy interested university mad nice

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
