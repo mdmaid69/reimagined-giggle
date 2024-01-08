eceea509ebeed27618490460dba67f2274cd2a4f
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:50:31 +0000

    circle clothes exam action 571enough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_float(array):
+        return float(array[0])
