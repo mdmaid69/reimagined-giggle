7782cb2594493af6d101fe1cb4283f171a958c3f
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:46:02 +0000

    film about sister treat report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
