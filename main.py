61ae0b417233bfc604384e15f570d07df3f2dcaa
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:30:48 +0000

    suppose offer opposite period practise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import sys
-print(sys.version)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
