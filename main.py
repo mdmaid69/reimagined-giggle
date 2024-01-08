b602c6f6b191d297c754083491dafae3f126de05
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:19:15 +0000

    round bring slow load bother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
