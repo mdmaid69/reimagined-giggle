a62cb18f6b25f2c6a3f60ebb45beca0cf4305fef
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:20:54 +0000

    appearance zero shut catch finally

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
