301594daf01becef9db0c05fe287f8ae8ff85513
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:51:28 +0000

    aim bother taste claim stare

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_volume(length, width, height):
-        return length * width * height
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
