0e066d428fac4a90e34b2f90af7336ff1130809d
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:43:04 +0000

    powerful ahead speech dare pressure

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_power(work, time):
-        return work / time
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import sys
+print(sys.version)
