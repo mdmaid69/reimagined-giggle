153b640c7330a9e1a5e8d92de77277e7e913b527
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:46:40 +0000

    shout rise exam information quantity

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
