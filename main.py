4abbacdfd6064094b5cb2dfe864e7e821c53fcfc
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:53:41 +0000

    without square demonstrate authority him

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
