ea80d0f27b637a6f49e6883dbdcb2c412ace42ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:43:42 +0000

    opposite direct convincing bread plastic

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
 import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+print(json.dumps({"name": "John", "age": 30}))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
