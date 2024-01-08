968aee55f0837f61f6c785857a7a4cad35f7a6c2
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:47:48 +0000

    disagree appeal comein this steady

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
