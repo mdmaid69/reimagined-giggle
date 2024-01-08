def5f80774ad42083916ecc226ae7847394bb534
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:46:11 +0000

    charge involve complain point takeover

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
