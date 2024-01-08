c4fd9c005cebb659033accc980c503bd975cdc8c
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:16:47 +0000

    test total agreement wet culture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
