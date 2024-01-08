fa4bcb66e24c5ab82adf63467d3a1628617b8636
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:12:44 +0000

    mind cross government happy 565end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_sign(x):
+        return math.copysign(1, x)
+import array
+def get_array_as_list(array):
+        return list(array)
