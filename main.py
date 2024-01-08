d179d76dd827f405479a72e91988ca20b0751724
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:47:29 +0000

    force everybody appearance attach punish

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def find_unique_words(sentence):
-        return set(sentence.split())
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
