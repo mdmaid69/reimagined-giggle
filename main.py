4ce1ac82e9c395da26886a270c3d6f3de32a57a0
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:27:26 +0000

    eager reply approve dangerous manner

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+for i in range(10): print(i)
