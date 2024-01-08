83a0d3f5f51dadcb0aacc29642ad121493c87477
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:19:11 +0000

    play contribute desperate badly beyond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
