623ebd7bff1a0165045183d2f7de73a93cd09cd6
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:49:40 +0000

    fit speech argue desperate wild

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def remove_from_array(array, item):
+        array.remove(item)
