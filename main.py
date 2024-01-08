e336040f574684f8cf020c53dd763285c3d5d81e
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:59:48 +0000

    freeze Ss light first body

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def is_even(n):
-        return n % 2 == 0
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def square_number(x):
+        return x**2
