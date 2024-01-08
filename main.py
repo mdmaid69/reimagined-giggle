435aa8a2a39c181174b5e9217302a08bd172448e
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:14:35 +0000

    put unit grow defend of

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
