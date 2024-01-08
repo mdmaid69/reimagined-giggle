a8e55b24fa5ce253d904b6f627c01871469ee8c0
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:38:49 +0000

    hair bet measure square hear

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
