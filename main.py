1e180be5aab87a63a16260088dd49afd3c8bfcc7
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:25:29 +0000

    century face pay bend later

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def find_min(lst):
+        return min(lst)
+def multiply_numbers(x, y):
+        return x * y
