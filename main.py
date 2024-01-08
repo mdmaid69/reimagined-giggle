0a47ed09a294854cf79d74767ed7962fa2efa8da
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:30:54 +0000

    violence total represent shape value

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import getpass
+def get_username():
+        return getpass.getuser()
