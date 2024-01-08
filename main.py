ad52290a01c8d8a9be69049eca76a21be9baaa27
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:12:36 +0000

    communication beautiful hope breath sky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
