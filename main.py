2f80f6bfcf52b0c7ccb9ad5bc71573cdc356812f
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:11:49 +0000

    home available rid hello number

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
