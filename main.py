47bd9eda12b93e4a5310314484214845c206b9b6
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:14:16 +0000

    mistake safety convince mention town

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
