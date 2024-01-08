e9967038cfbf26bfa83818c855cca4d2a9b55cbb
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:38:16 +0000

    tall post cause trust contrast

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
