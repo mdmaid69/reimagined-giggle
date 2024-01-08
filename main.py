bb0946a4f60f6aaabfb6796f32e9d131f7411e07
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:05:31 +0000

    convenient impressed flat flow offer

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
