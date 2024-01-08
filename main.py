ef657c997878f84910a3322ce8492a89e7b0b597
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:57:02 +0000

    part live neat stop mouth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_average(lst):
+        return sum(lst) / len(lst)
