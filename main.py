858ceb393eb11f591a5e6f79f3931a47203a8682
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:14:51 +0000

    work disagree if place proud

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
