b8e913c5d093543a6fc6742ff71b6c03605c74af
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:28:06 +0000

    before able reaction recognize Yy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
