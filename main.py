f2a4399033ca950bdfb77392df5e43ec3d72f089
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:48:57 +0000

    exit else reach word house

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
