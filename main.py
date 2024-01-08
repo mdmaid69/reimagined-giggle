92b9cdceb5d77fa93c0e4e75e3e78e081d09f074
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:59:19 +0000

    temper neither generous happen see

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
