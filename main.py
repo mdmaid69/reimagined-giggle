f81dc23565a3ff7106c8a90fad676d73776f3c49
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:46:15 +0000

    advantage myself moment flow be

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
