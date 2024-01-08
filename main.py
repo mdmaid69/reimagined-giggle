f12c529ec9eb9c49e3530e9134516bb823dec72b
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:29:40 +0000

    he professional suit water science

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def multiply_numbers(x, y):
+        return x * y
