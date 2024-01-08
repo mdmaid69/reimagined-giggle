1ff379b57d81d784a04b59fbc725353a370e2fe2
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:53:42 +0000

    underneath steer demonstrate wish admire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_current_working_directory():
+        return os.getcwd()
