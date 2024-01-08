c32231a708b23ea6b73e8fca2a493436dfc1d774
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:14:32 +0000

    underneath trip march limit pleasure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
