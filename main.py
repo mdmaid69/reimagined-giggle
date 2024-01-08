e5c7363d4e3a8a0d5985abb9e4407bf41c650f47
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:53:26 +0000

    anywhere sad trip complain ever

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import time
-print(time.time())
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
