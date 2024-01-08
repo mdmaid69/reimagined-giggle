13dbdadb0ef3ebd1376e47bf255d38959bfc95d2
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:48:12 +0000

    fact pour advantage getoff score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
