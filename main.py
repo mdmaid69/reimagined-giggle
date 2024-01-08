6e957bd53eee19c1c51948ce26f9d9e93d87ba7f
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:54:40 +0000

    damage weekend begin course also

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
