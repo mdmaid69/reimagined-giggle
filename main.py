64f0d63268a932b3792badcf72d21674619d68fd
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:36:03 +0000

    angry lock right waste thank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
