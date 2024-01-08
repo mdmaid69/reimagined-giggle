472e85e48cb5023c4cb13b490984f13cac7378cc
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:55:43 +0000

    future chin disgusting themselves skin

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import numpy as np
+print(np.array([1, 2, 3]))
+def remove_duplicates(lst):
+        return list(set(lst))
