6093922f74fbe5b49fa87075180bf3dcc3261ea5
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:31:47 +0000

    suggestion system settle light bringforward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
