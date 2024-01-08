1a976f03412db81f7db961d56258404678c92dc4
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:07:08 +0000

    prove many injury price takeout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
