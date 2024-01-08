5efafc5a74b38fc1b61dd4707e099dd7b6d4fea6
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:57:52 +0000

    plain silent identify judge later

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  def square_number(x):
-        return x**2
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
