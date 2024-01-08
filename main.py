cc4d5da56bbc7e3c140d8665edc5820a9a9ad33f
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:26:11 +0000

    taste point question perfect excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_speed(distance, time):
+        return distance / time
