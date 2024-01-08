b63c17d4581e8bdad1c793eb618f17d48c526309
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:13:05 +0000

    left show pass pupil beginning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
