ae999e45ff5af6840bfa9a1197f38dd877f38996
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:47:55 +0000

    comeback power exciting keepup damp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
