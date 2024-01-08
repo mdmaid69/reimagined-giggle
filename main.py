0bb9660eaf68ed28564437742300bf689c4b3456
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:33:54 +0000

    quit moment lesson serve pipe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
+def count_elements(lst):
         return len(lst)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_factorial(n):
+        return math.factorial(n)
