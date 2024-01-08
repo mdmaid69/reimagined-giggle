17d2a9fdd0aa6b373e07599d3daa82ec0da5e9a6
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:18:02 +0000

    badly since giveout claim risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_as_set(array):
+        return set(array)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
