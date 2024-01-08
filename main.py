ba0d864fdcb51823af5493402f559ef24d061a3e
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:10:39 +0000

    straight tooth wide everyone stretch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
