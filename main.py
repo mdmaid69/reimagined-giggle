fc8dfe8b62857e60bd9e7ef27138c299db11e540
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:29:12 +0000

    active choice design century closely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_current_directory():
+        return os.getcwd()
+def convert_to_hex(n):
+        return hex(n)
