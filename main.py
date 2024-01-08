7ccf5ad6a796349c1adb5e143e909b705f362be4
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:37:05 +0000

    combine family cover leave forgive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
