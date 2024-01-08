b4d82685eedeb55107911f1dee7d4a2c45397442
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:07:08 +0000

    runinto uponon boil gooff chance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
