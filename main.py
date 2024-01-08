05ec8e5fc6f115a18d41bf60d53816bffe110ba9
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:37:46 +0000

    show temperature on prefer study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
