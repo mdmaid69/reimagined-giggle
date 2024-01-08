e9cabd0d602663e72e8fdc9110961c8909b4f2a2
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:48:23 +0000

    uncomfortable section shallow nice appear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
