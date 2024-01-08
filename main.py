c8aa3f93bbd85cc874c991e174b8367b2fac7920
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:02:02 +0000

    police equal tax Kk relative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
