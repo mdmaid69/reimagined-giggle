1c059fe78c67ff357f9e0244cee8eb2f45e71346
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:09:29 +0000

    repair realize capital unlikely ignore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_user_dict():
+        return collections.UserDict()
