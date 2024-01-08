c27d9f55f221c33eedc460695d7d78b9f4f0e529
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:21:12 +0000

    trousers point principle married dress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_current_working_directory():
+        return os.getcwd()
+import json
+print(json.dumps({"name": "John", "age": 30}))
