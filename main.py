d5e5f50cf7bbe784856611e8d29b32b7acad48c2
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:08:24 +0000

    forget unusual succeed week shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
