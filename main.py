7f9b5e3e7d02bee52562d67192f7e45d230a1ebf
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:49:20 +0000

    569enjoy complaint necessary pay live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
