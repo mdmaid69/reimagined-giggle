39ef9c46aff3d499cbe5face057a23162c089860
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:59:08 +0000

    ahead limit noisy plan sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
