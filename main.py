7ed770572c708166044ed0770c63b3468626f626
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:53:24 +0000

    shut pinch search glad clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
