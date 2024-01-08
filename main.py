3eba852ac4ca1f4c3ef1a9cf73d777f1fd21e119
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:55:30 +0000

    crowd clear serious swim talk

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
