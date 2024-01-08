24448ee2006824ecff55eaf6e97071c9d7f679e6
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:39:47 +0000

    scale tune choose move only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
