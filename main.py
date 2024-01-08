8286a79d0176a439df97ddde3cdbb4191dd2fe5f
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:25:27 +0000

    rain that increase cold solve

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-name = "Python"
-print("Hello,", name)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
