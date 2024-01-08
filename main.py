4963d381c7e4f24c0203e943c002b33c6d25b5e3
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:57:28 +0000

    welcome suggest cover close hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
