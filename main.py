849ffa6d826d3b484d3da0fa0d9352999595f50f
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:02:34 +0000

    spring mistake building block Gg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
