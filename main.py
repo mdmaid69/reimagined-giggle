9f1c10a7dceb2408222e09820d13958fb18ba30d
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:17:38 +0000

    healthy even undo bit yes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
