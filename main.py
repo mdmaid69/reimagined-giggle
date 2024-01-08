1106113b7f0a19d94dca546608f0a58c64db22cc
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:49:14 +0000

    admit show wild holdon stay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-  def convert_to_octal(n):
-        return oct(n)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
