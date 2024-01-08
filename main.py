6e3109b399c37e2e7cbdfece133c0781fa487e6c
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:14:51 +0000

    comeback worse skill remark ought

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import platform
-def get_os_info():
-        return platform.uname()
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
