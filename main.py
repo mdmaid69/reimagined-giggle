2d576f6ef4be749cd9d4b5c4a02dd5fca1070c87
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:41:40 +0000

    neat comeup hold hers duty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
