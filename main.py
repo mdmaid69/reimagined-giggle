64ccea927d09e659496a3451025d91e7e69390b4
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:03:33 +0000

    noise line week front pipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+for i in range(5):
+        print(i)
