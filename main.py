2cabc8476ac9bb6e37e0b985999f04a3d065618b
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:30:16 +0000

    light pocket speech mood cold

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-name = "Python"
-print("Hello,", name)
-import platform
-def get_python_version():
-        return platform.python_version()
+import sys
+def print_python_version():
+        return sys.version
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
