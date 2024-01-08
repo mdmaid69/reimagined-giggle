2c848a4cbd4385ee8de54e7df248f7623706c12f
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:59:49 +0000

    clean anxious forgive information square

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
