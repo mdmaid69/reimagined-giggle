8bc6b4cc98b87d22352e83e5c71bac218f357158
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:00:20 +0000

    repeat connection second model cry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def convert_to_hex(n):
+        return hex(n)
