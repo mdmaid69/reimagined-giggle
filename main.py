480bbe364712c9f22552738a72a6326401fce429
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:41:55 +0000

    block at turnoff would centre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
