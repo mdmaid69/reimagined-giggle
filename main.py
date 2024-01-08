25f465029ee55f4bf3873f5f09ba92d530891db6
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:54:05 +0000

    flat secret relief female crime

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_speed(distance, time):
+        return distance / time
+def count_words(sentence):
+        return len(sentence.split())
