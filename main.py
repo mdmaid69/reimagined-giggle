9c894950ba3cf933c05d71e9972c9e42bd91183d
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:37:31 +0000

    freeze mine break order file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
