fde6ad8349a26252cb129a0fe4d6f60f0d548447
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:14:46 +0000

    fit pool chief quantity clever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
