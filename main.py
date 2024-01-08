686a288bddd544e5d28f42333a22c6c8dfd81553
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:10:30 +0000

    peaceful otherwise style cut anxious

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
 import array
-def get_array_from_file(filename, typecode):
+def convert_string_to_array(string, typecode):
         a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        a.fromstring(string)
         return a
+import collections
+def create_priority_queue():
+        return collections.deque()
