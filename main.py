c11c0966ec2117c5720e9b786ad9e0c8980bfe1c
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:45:48 +0000

    worth permission recovery television single

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
