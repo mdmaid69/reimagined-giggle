1c20ad89e7573edb013e0bf5f05860717b22226a
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:49:50 +0000

    question offer mind above in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
