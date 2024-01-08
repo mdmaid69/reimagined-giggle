bc4846823f68d955d947d24945f6c48e620a3b4a
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:01:49 +0000

    change quality accidentally weekend evidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
