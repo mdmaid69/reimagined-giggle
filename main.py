bcf1e4de4ccd94b08999daaa64d5ef1e1a194d32
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:43:13 +0000

    glad move pretty band flight

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def is_even(n):
+        return n % 2 == 0
