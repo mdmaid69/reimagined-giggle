a75057ca571075ba079f5f8ec859696fc141c715
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:24:45 +0000

    too sign most date hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_speed(distance, time):
+        return distance / time
