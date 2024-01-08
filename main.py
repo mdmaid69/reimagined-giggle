ee233179ee023c85f57af76f7091904ca320d502
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:25:11 +0000

    goup loud pass politics medium

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import array
+def get_array_as_repr(array):
+        return repr(array)
