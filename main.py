f0a302e3a1d25fa7469d0500c84346b8e7482985
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:37:02 +0000

    order left ours Hh acceptable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  def remove_duplicates(lst):
+        return list(set(lst))
