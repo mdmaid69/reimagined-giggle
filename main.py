e75f230b03dd7c40bac5d33c73697897293b6c36
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:03:11 +0000

    skirt make own hospital nowhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
