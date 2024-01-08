eb2c5cd4a591affd4beeac8560d5a6c03c57c508
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:21:26 +0000

    otherwise question whisper better loud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import math
+print(math.pi)
