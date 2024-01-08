26f669c29606e32a60cc269266d2d8c6d1e9ffcc
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:13:16 +0000

    learn scream exactly pipe mainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
