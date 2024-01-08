c6517c64e24fd300da20e311b77d73234e0343ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:14:41 +0000

    everywhere motion example letter embarrassed

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_list(array):
+        return list(array)
