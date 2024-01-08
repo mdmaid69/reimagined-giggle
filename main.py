1a466d3c22158cc1f8434122fd35801a1d539a36
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:46:03 +0000

    top easy short equipment blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
