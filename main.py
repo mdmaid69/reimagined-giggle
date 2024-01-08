3695c4d26d63e73df655128eb4dd50ee17db6917
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:49:49 +0000

    little track programme before common

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
