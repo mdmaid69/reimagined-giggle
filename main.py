c01d564e68605881cfc6baf507a8d5715e3ab3c0
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:54:19 +0000

    ready suggest inform trade exam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def get_array_as_memoryview(array):
+        return memoryview(array)
