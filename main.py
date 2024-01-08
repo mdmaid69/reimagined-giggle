ebee6b0288ef5c8e7056e7f010e5ddb1cacb172c
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:40:38 +0000

    fond hurt protest perhaps broken

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def is_odd(n):
+        return n % 2 != 0
