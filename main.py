5640f40ef81fe2cbafbc2bd035f6db50226f4424
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:18:18 +0000

    impossible convenient run sea display

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
