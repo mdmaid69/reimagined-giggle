c284cb587540f16bba2eee5d3388fdc843ee32bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:38:57 +0000

    jump fat judge hurt record

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_power(work, time):
+        return work / time
