39a5d045359296edb75db47a3a88880ac8a60237
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:30:22 +0000

    teach care rich spill lookup

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
