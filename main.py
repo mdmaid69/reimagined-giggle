d18ad7d9a0e2899d21de9eff4827432d076cd7a2
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:16:14 +0000

    kill let while key rise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def get_array_as_tuple(array):
+        return tuple(array)
