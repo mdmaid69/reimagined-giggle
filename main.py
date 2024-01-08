e646ae18ee225526fbd1b1eb04890158e0674a59
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:28:08 +0000

    exchange thirsty quit agreement bother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
