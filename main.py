919f8ee1f98bc1fc8676e98cd033d2253e3215f4
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:50:29 +0000

    embarrassed broad disturb straight round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
