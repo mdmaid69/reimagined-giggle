72c20f14232c8fd21de03fc0971399d0e72fe569
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:39:47 +0000

    terrible supply lay trust account

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
