b12cb54964b1630f051f0c2982bc009128ff7405
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:36:57 +0000

    lose benefit hand bread train

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
