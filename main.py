90568e24fdf7d23b18694121a5d13535ec60ea87
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:06:06 +0000

    clean work exist office steer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
