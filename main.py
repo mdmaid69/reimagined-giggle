a5e47b708e0093ecc3df48e8e3124a2276e81308
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:17:04 +0000

    run track point they spot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
