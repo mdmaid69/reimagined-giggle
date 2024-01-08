a6b9814e578ddedb55c1c86f69eeef804c424d21
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:25:17 +0000

    belief treat clear file necessary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
