e7ad6b13d260189879be7c9334f378b586e77664
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:51:42 +0000

    high business loose public main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-print("Hello, world!")
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
