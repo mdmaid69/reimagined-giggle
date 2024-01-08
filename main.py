bd5e3ef308b2b9ecb8b88400f4e880a128366111
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:11:24 +0000

    abroad wide form ourselves to

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
