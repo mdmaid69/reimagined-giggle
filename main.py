36c802b8d1267b07d1ae4b0351b14834e9e85f35
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:45:19 +0000

    seriously calloff deaf container turnoff

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def cube_number(x):
-        return x**3
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
