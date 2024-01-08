e4e8b914ab100607eded42421a31d29549c34cd7
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:31:06 +0000

    search loss among sister basic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
