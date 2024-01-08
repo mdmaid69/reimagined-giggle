e868a80522a8c1e512d987b7b65a5846a2447b22
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:15:42 +0000

    first valuable little damp matter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
