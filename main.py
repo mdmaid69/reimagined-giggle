f013c74da0c49959f0481631ef0878cc97d04cc3
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:58:44 +0000

    workupto forgive muscle clear bite

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+print(sum(range(10)))
