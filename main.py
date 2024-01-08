fefd36a9976e40adc4d8f54b4598bd6f154eb86c
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:14:04 +0000

    splash runover positive attractive small

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import tensorflow as tf
+print(tf.__version__)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
