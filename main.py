f4cf2f4a305fcab171f9e38436cf4e74bdf9f099
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:10:20 +0000

    once your about permission recently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
