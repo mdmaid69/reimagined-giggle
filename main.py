9d86441c4535c2be591c81096c9d54963dd9b0ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:41:32 +0000

    experiment high patience equipment left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import collections
+def create_user_list():
+        return collections.UserList()
