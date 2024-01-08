6cd7c58650b5e89da38d2870e9514e34d70c14a1
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:24:12 +0000

    Kk past name where phone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
