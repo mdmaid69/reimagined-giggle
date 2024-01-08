533444b50bf13a01a7700719d1f746a101d1d242
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:50:42 +0000

    tread precise meeting during any

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_force(mass, acceleration):
+        return mass * acceleration
