541e2f23b91820db124401e8dcab6dede5fb8447
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:32:55 +0000

    nowhere danger scared war comeround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_max(numbers):
+        return max(numbers)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
