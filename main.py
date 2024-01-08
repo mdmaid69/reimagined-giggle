af61efcea8322fc3f04f6b02519f8b6c90d28680
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:21:02 +0000

    fierce direction breakfast brush shame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def add_numbers(x, y):
+        return x + y
