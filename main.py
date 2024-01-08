2d07a5dfad8cc9a28f6506fc76e5672f5747fb73
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:18:14 +0000

    deliberate zero shall science scrape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def subtract_numbers(x, y):
+        return x - y
