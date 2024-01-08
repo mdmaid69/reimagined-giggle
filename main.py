eaa02283d61f85e381dccec3bc6b00767fccbe0c
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:23:43 +0000

    reach nor sight responsible journey

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import os
-print(os.getcwd())
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
