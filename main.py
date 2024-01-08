aaa0a40d5f2566e0f68ce9bc50315420e5b4eb09
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:01:13 +0000

    base over underneath side learn

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
