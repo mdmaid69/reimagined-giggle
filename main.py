eadef9e1640a258b34b6a5f76957101b0a47aa9e
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:46:49 +0000

    place damage rain breakin shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
