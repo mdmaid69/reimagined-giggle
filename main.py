a00af45d4532279955f497d004a81101a9b66bdb
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:39:10 +0000

    intelligent uncomfortable in cheap yet

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
