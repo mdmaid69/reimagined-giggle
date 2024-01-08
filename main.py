cb87ff79d7b9667a40f7e0fbafa3b01e5a89c47d
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:33:48 +0000

    Mm dark correction mark equal

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
