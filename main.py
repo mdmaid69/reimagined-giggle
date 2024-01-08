962b15807a99ea5f3fed262c50f9a70713a55a2b
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:19:10 +0000

    frightened skin pump greeting consider

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+print(sum(range(10)))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
