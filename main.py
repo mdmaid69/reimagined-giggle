81a0f0d7b334afc942448c5626973524872aa7dd
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:11:13 +0000

    passenger negative die uncertain answer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
