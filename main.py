c0f72d4702dd6fe4b72f6d9fa1c27bfcfaa72e68
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:37:26 +0000

    weak shelter flat cover unhappy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def cube_number(x):
+        return x**3
+  def subtract_numbers(x, y):
+        return x - y
