960bdcefea1df2f25ea908fb18899cedf821f00c
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:21:37 +0000

    cut than discovery important male

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
