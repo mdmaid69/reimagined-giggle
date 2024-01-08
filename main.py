58c9205d4057a400d4276846c2b518011e09584c
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:10:12 +0000

    will shape separate balance than

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
