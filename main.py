69e03d8b47a8b1c9bb8ec0da3042f3b0e57171d4
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:56:32 +0000

    cover sure recently whom hold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
