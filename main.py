dc5d81cad693c1146004343322ffaa021afb2bc2
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:02:45 +0000

    use march born but drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import random
-print(random.randint(0, 100))
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
