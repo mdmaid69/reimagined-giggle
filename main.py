92ff22d5db684c1c32723f0c4d019f5e8d1c4ce8
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:58:24 +0000

    square blind press compare unusual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def sort_list(lst):
+        return sorted(lst)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
