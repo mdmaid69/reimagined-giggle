bd940df36317abd34f3e5b9bcb106c6d88057e2a
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:34:34 +0000

    frame deep since 572enter couple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
