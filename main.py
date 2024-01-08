fc540f026dcca8464f5f31fed58e7b7072464ef4
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:22:59 +0000

    roll violence this have draw

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_power(work, time):
+        return work / time
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
