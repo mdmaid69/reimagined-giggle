fcc03f5343051f0bcaf590fc9483f79eb885330b
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:25:32 +0000

    kind artificial car feature spite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
