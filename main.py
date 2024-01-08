6830ecb3270b528f8086f78cf02b5ab014c7e303
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:15:57 +0000

    ride exploration be breathe better

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import datetime
+print(datetime.datetime.now())
