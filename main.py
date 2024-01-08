8d0aad48fc1b93e1ea26a70ebdc5dd999a43dabe
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:46:36 +0000

    official bottom just explanation properly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
