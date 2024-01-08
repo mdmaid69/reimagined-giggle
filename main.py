7aa7472bc88e1b4a4e69ab14a214660e0da50ed6
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:10:52 +0000

    reply exchange murder regular tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
