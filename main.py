c5348701f9570660ef5a9d013e1127782d97e8e8
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:50:39 +0000

    recognize expect scene basic approval

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_pressure(force, area):
+        return force / area
