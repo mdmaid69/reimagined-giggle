e07051e1d787e16ecf8e2776cc2bafafeb4fff66
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:07:31 +0000

    comeround he cry male pretend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
