80dae04766ac1f0136885ec4eb2aa4a21f922c7f
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:09:00 +0000

    undo unhappy their push march

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+print(math.pi)
