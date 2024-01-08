9c32e2b19fef411ca23fb31b2231b4995d196796
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:42:10 +0000

    increase much measure seriously long

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-  def convert_to_hex(n):
-        return hex(n)
