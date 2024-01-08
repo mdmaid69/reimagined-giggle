6d8d814d019535660985a61c668587671774a391
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:28:40 +0000

    practical example bar pause reject

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def cube_number(x):
-        return x**3
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
