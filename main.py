223dca40ada64f0772163ebf304c9989235d19c4
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:10:18 +0000

    wherever justice practice all contents

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
