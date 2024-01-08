bab28f5f7210da6fab604c35ff172f95cdbbd324
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:33:09 +0000

    right ilovedicks Ee history why

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
