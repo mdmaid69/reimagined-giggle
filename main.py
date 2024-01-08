434b178b8f7da9c267c26e5976501126cc3ad3c1
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:47:14 +0000

    dance pretty variety than mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_as_set(array):
+        return set(array)
