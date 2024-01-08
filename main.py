ae3830831f02f0754c7b8d247bd86c725c28588b
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:20:44 +0000

    professional 569enjoy tired statement add

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def convert_to_hex(n):
+        return hex(n)
+text = "Hello, world!"
+print("Characters:", len(text))
