9aa9b597cfdfbf0f831dcade7ea87c2582e46bbe
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:46:03 +0000

    say sleep late lookaround relationship

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def reverse_array(array):
-        array.reverse()
+def find_min(numbers):
+        return min(numbers)
+import os
+def remove_directory(path):
+        os.rmdir(path)
