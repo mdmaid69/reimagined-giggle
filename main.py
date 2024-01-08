b427753da50f2a05b6df9c5dd60c8fb25b35ccb7
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:58:14 +0000

    away turnoff air work wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_as_bool(array):
+        return bool(array)
