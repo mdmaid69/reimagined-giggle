354e12011ee926fd100821a4d73c1be193edd2b3
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:55:28 +0000

    stamp swim scared characteristic themselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def sort_numbers(numbers):
+        return sorted(numbers)
