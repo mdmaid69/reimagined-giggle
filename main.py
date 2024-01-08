f13e82f9dcd53b7b69c05dee98b1799750fdf053
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:40:47 +0000

    sign dare fit put unit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
