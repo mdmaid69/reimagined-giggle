28b962b4759c7a65d2f6544944957cfd82d6aa7b
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:13:38 +0000

    shake whole in murder fall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  def is_odd(n):
+        return n % 2 != 0
