1e1028b12ab70ddab433e117665ea1841c47df8a
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:26:00 +0000

    table settle immediately burst hold

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_acceleration(speed, time):
+        return speed / time
