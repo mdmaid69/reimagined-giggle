8acd088143d2366395584291497008f34d7a49c9
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:24:19 +0000

    divide opposite worry worth crisis

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def reverse_array(array):
+        array.reverse()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
