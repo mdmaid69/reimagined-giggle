b861622ce74455bba3cf0b868aaec5a2f11356fa
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:53:00 +0000

    noisy night warning thankyou press

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
