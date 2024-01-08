68f371f436cc30304586b7520a5772badc45b182
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:52:07 +0000

    fun whom past close scrap

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def add_numbers(x, y):
-        return x + y
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
