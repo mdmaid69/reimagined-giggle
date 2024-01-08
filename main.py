b747ffedb7689a59c73247cb61cc4e6a99be6408
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:38:50 +0000

    present stiff sure size go

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
