1f17d45d47489d2d371eda736234c15ed8c08939
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:34:17 +0000

    suggest deserve soft skill obtain

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
