4d97a305fa79246cc32804a6a034234202b6a8f2
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:42:22 +0000

    interview area murder bunch raise

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
