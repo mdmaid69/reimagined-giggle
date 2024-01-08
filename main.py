8a0acfb6ebd19015dba005b8ddf69a024497c9e7
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:17:05 +0000

    chase clean familiar market kind

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-name = "Python"
-print("Hello,", name)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
