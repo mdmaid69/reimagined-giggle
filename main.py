4f28e86a8537be01fcb261a5177d71e8718e8c75
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:56:15 +0000

    yesterday air tip acceptable opinion

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
