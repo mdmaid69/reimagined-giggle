d8d9965676ef0141fed506f65359d1785b6d383c
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:47:26 +0000

    tradition content Yy base never

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-x = 10
-y = 20
-print("Sum:", x + y)
+def remove_duplicates(lst):
+        return list(set(lst))
+import os
+def get_current_working_directory():
+        return os.getcwd()
