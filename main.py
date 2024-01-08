2d00dd7900ad416d16c96b529ed0f5e4086595b0
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:51:12 +0000

    callback turninto no tool yourself

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
