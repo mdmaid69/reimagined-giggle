9fc19ad654eac7dc63dcf0605a2732308f51a8a2
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:34:17 +0000

    sensible inquire stiff bound seem

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import sys
-print(sys.version)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import collections
+def create_queue():
+        return collections.deque()
