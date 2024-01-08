285a611aec3f6987f84d4fb28cae524051b2365f
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:11:47 +0000

    wave makeinto common give taste

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
