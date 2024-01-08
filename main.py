ee48b5ee3defc42fc8004f172d08a947201536eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:19:03 +0000

    realize upwards touch runover large

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def count_characters(sentence):
-        return len(sentence)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
