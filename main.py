7b23d9bf623a58a2a6679ecfa195a03882adcc08
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:57:00 +0000

    bringforward turndown copy something any

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+for i in range(5):
+        print(i)
