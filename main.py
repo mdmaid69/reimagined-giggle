6ebbf10e0c6d6dd83822b5dc7613e692f9fca053
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:30:48 +0000

    exchange right tragedy confused introduction

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
