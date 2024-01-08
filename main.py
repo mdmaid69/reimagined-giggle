9d4b0466b19c8ed2a42cd46856fa82d2bc17baba
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:59:08 +0000

    teach firm willing fit emotion

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
