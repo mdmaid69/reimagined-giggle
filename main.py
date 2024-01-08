7de3daf2b92f6a6dfe87bb06db68ead51d9cec62
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:41:56 +0000

    minimum tend interview shut chief

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
