68a49568cef5e4a2c4f02b51c13d333e0bed4d5d
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:47:12 +0000

    black cope away exchange start

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
