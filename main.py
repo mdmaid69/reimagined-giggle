3ecffcd7e4ac858c39a8d7abae4c686f759c2a94
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:42:46 +0000

    autumn thought post immediately scrape

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
