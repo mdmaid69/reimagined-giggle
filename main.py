4365f564dad36a9c3d24f7e3af8a066c959aff69
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:59:49 +0000

    fly violence dull share low

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
