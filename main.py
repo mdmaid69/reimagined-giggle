6e361be7ea03a274850176a4f9c124d372aa6d00
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:53:32 +0000

    operation address loose tip congratulate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
