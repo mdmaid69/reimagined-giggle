9eff3d6e22d612ea506a0da29c6e764740e7f372
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:02:34 +0000

    sudden street same wet activity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import os
+print(os.getcwd())
