6c82a774b4d1aea18fe1667577226a469c950cb6
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:42:29 +0000

    564encourage succeed attract curious peace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_stack():
+        return collections.deque()
