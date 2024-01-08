e549f025e021ec336b198b1fbd6bb7354eabc38a
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:05:22 +0000

    instrument still perfect destroy spot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import os
+def get_current_working_directory():
+        return os.getcwd()
