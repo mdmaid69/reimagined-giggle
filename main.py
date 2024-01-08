47276a5259608b954e5f8c9e870e0a6a52f456a3
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:54:34 +0000

    noisy cum secret reduction responsibility

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
