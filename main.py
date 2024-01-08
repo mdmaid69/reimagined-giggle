5bafdad596950e36d3ba708bc0335e8ab7341b3f
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:52:30 +0000

    skirt obtain plain somewhere either

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
