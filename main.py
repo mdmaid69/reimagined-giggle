9c37029fcec61d5756a09b43f51d09dc027841dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:46:51 +0000

    train us deserve either please

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
