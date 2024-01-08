8db7b5f1734eb1f4a87fbcb1d3c7e39b12dc9243
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:13:55 +0000

    improve change contrast expensive talkback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
