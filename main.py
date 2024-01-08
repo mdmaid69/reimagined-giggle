08ccbbcf07d35742805b6152ce3235e5ced98e66
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:56:33 +0000

    Pp responsible average influence interfere

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def divide_numbers(x, y):
+        return x / y
