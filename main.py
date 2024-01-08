1ff1834a3b67108dd628db103e315b65b12e56b1
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:55:13 +0000

    slice stupid cap sense bang

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import collections
-def create_queue():
-        return collections.deque()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
