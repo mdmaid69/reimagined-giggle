f36fcba81f55b4edcd35e5166dd143bf6410adff
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:59:49 +0000

    simple shallow middle middle burn

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import collections
-def create_stack():
-        return collections.deque()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
