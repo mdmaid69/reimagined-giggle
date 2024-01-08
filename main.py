ba3266a718cf2a8f9c981fa106950aee2a2dd04f
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:22:24 +0000

    ability extremely possession worse correct

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
