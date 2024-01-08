8f48e64bf21d6895d31aec52e33681b1cc1afdb7
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:57:35 +0000

    be above fear power effect

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def count_elements(lst):
+        return len(lst)
