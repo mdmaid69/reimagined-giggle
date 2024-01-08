902f2987789e8eeae20b985906d7db6320671360
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:41:33 +0000

    keepfrom allow comfort uniform expression

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_speed(distance, time):
+        return distance / time
