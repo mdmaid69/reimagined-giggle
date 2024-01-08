446e7c49dd95496f603f2b7a2c7c274e1bab493e
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:14:28 +0000

    beginning test satisfactory face whoever

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
