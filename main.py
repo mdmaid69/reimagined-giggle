94528b89022dfc59e8dadc43c5b4ec5a524c4e7c
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:17:59 +0000

    prevent pride sea work minute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
