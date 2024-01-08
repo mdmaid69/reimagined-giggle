3e793e99c99512d857080e969f6be9abc20853d5
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:44:07 +0000

    brief suit this recent tip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_float(array):
+        return float(array[0])
