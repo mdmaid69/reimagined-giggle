41288b99f1134ec309db39a5848e7129609a17e3
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:36:06 +0000

    admire clear refer each smell

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
