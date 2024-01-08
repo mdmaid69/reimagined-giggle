fdc85a7226e71079bf9664236c3622c40cf836b9
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:28:35 +0000

    apologizeapologise from combination glad statement

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+for i in range(10): print(i)
