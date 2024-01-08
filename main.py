15f6c24d3d687dc624a9a58c9d1bbe1f24c497c9
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:25:26 +0000

    deep creature receive chin brief

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_repr(array):
+        return repr(array)
