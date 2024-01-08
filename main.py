3bd5d64707e5e166c32dca24f8f11d832feaf33c
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:01:00 +0000

    treat swallow skirt journey tap

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
