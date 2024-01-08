b3451d098e416ad4a4b3cf6578579a60de11bc93
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:33:45 +0000

    soft other across describe involve

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
