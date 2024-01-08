1c64a3ab265da2efe5ce23e6d9b8417a585f8f5a
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:16:25 +0000

    Ee convenient their new bath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
