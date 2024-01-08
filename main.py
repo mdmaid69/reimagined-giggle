104f1f0309b81098415e4cf6b7e344a805ca5b80
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:13:33 +0000

    crash argument poor runinto free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-print(time.time())
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
