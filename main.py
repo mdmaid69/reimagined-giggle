0b7ecf3101038e105899b644adcd64b0fd468133
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:14:17 +0000

    on cause lead entrance admit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
