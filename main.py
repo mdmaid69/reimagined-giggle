0aac406fe3fe51e9715aa988c423310c97961b62
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:58:14 +0000

    turnout could somebody joke computer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def remove_from_array(array, item):
+        array.remove(item)
