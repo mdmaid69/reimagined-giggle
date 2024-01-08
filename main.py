7d98bc0c6ff0ff6abaa298d0593f7ce7fe7d78b4
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:43:05 +0000

    length number basis carry follow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
