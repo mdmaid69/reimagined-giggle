5a4112666b214ac14bdfbdc67978e2413c024039
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:25:01 +0000

    possible close strike deliver determined

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
