ade2f7edff602c1ff7b7c2e7c586e8fc83696496
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:08:01 +0000

    it home certain arrange scale

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
