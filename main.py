b64db9ddb35994728d3e43376e024ed111e85d73
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:12:47 +0000

    seem delicate nature door hard

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def cube_number(x):
+        return x**3
+def convert_to_octal(n):
+        return oct(n)
