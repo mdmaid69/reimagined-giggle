4e6929c72271dc38da14509acdeca4bdeaa50dd0
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:02:55 +0000

    stop just ability just decision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def reverse_string(s):
+        return s[::-1]
