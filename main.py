85ca67b72b6ec01a674e28caefc1fa8723ceaf70
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:33:04 +0000

    weekend exit rain block dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def get_bytes_from_array(array):
+        return array.tobytes()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
