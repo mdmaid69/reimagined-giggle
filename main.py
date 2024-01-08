37b51786715f3f1ca75b2f4b40d3218107a60720
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:57:10 +0000

    pause middle least agreement heart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def is_palindrome(s):
+        return s == s[::-1]
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
