16beb5769210d8316c0498a888dad9b7e2d8dc0a
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:15:09 +0000

    snow deal bye past path

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
