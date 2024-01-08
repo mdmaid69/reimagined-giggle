eb0344d3dc0791e61350078be74f8e0fe9769014
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:08:16 +0000

    none forget for Yy welcome

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def reverse_list(lst):
+        return lst[::-1]
