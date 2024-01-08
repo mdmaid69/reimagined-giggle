9041db94a99041a418ea410f9613bf6e74e8bd9d
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:40:52 +0000

    down try point used rule

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
