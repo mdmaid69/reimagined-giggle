b3970490efde23ba5b0501506fdb35f2876afc65
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:17:37 +0000

    offer discover pour sky wind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_acceleration(speed, time):
+        return speed / time
