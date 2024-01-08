a5ffbfd5101bdf3a90bb426e3cf7c5febac019f8
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:36:43 +0000

    right face actually experiment OKorokay

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
