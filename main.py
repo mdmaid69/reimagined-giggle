0dfaa0b8c33a1234c8d355b5099ea78ce40c2d0b
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:10:59 +0000

    swing check call thirsty emphasize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
