e95408515c9ad20c063d6968897fbcf15055543b
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:29:35 +0000

    Hh equal whoever answer disapprove

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def count_elements(lst):
-        return len(lst)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import getpass
+def get_username():
+        return getpass.getuser()
