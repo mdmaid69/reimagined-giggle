79062ba475eb86b68302b7dc391c1cd7df03b924
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:12:27 +0000

    none bringup yours objection copy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
