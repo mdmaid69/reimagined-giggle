bb0e43b9bcb42b420c3df0ab4ca39dcc62f7ce66
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:17:42 +0000

    result decide switch suit unit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
