5ecc50e9f735896c6cd9b92353245efe500d2878
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:37:45 +0000

    hurt brave coat expensive sea

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
