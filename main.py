b047c2e016f6ceb3530a33fa9b4782d60f338151
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:45:58 +0000

    downstairs getback man emergency scare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
 def get_bytes_from_array(array):
         return array.tobytes()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def is_odd(n):
+        return n % 2 != 0
