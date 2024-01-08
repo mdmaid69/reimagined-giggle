b8cf623d58ccbb5c9f1983a15c90791f28849e23
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:25:12 +0000

    miss interview prepare pipe send

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def add_numbers(x, y):
-        return x + y
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
