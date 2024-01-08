788fe9549e75d03008993b8656d52ed6fa82683f
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:29:21 +0000

    from reasonable takeout too 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
