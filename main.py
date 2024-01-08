fa1fedb4dd045a65d4a05f28bd6d2173b92eb73b
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:45:27 +0000

    challenge standard lookafter run nothing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def count_words(sentence):
-        return len(sentence.split())
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
