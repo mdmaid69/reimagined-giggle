2ee5d690a8876727d8c7defe8ed21759ea522435
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:25:22 +0000

    glass surprising dance country hurt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def calculate_density(mass, volume):
+        return mass / volume
