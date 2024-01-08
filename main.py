c62cad9086cd53b28c702ec3dc92745249b871b6
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:46:59 +0000

    switch float wall today prefer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
