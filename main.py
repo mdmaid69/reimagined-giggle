0f53dea1f4d9b8755824b2cb2f41479b555536ef
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:46:52 +0000

    stone purpose benefit buy sentence

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def count_elements(lst):
+        return len(lst)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
