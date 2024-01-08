21e7ca05122e08f8d442efe69a24675f431f5778
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:14:43 +0000

    delicate suckme left arm injury

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def sort_list(lst):
+        return sorted(lst)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
