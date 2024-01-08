3a88197b5867e87229740b95564cba18ada210bf
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:19:32 +0000

    condition ability style calloff smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
