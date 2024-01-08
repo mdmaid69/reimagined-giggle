88ad72461a4b05776a11cbbbbee1d50168c6ef12
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:43:00 +0000

    touch separate approval belief add

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import numpy as np
+print(np.array([1, 2, 3]))
