bdbee73744c5d1654e71cff687f420be45badcac
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:54:48 +0000

    criticize fix worried takeout balance

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
