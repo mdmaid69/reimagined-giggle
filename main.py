39ed9d12270e2a02c612d6ac690a815669cfbab2
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:49:40 +0000

    before Qq impressed exchange tough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
