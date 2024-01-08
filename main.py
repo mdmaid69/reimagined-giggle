1ce0bb5698a54badbe3cedea831ba19ea3746acf
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:40:24 +0000

    charge neither terrible notice easy

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def find_max(numbers):
-        return max(numbers)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
