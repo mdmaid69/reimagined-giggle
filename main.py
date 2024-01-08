1035d3928e5d4af0ee23814a34d114574e8117db
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:16:54 +0000

    satisfactory race swimming may necessary

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import sys
-  def get_python_version():
-        return sys.version
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
