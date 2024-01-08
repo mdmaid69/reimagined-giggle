69d162f9739e4affb301814f113ae632282d0d1f
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:41:15 +0000

    goout argue letter worth hate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
