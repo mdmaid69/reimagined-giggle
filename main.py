b52fe54b8ce5f7614af63924cb0727162b14de36
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:34:18 +0000

    kill concentrate sort pay work

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
