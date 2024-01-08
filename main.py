10fa1435a929f6a7eecd0e7ac837a4c9c7e04dde
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:16:26 +0000

    programme offer successful 573entertainment mind

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_power(work, time):
-        return work / time
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import numpy as np
+print(np.array([1, 2, 3]))
