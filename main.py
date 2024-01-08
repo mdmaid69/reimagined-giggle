437beb4fbb0f5132851f731f9f0ab37e9d5f2ba9
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:04:58 +0000

    strict slightly brain medium convenient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
