abd553e42535d629a5291b470fcd43c2ec33ccb8
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:22:08 +0000

    trap face search sorry content

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
