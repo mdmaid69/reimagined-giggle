78cb50e3d085c364af5fad67758300d2d723bd51
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:00:36 +0000

    cost young press trouble separate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+for i in range(5):
+        print(i)
