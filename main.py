a6842ad983b2ebde39f18aeceefc9b37994376e7
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:24:15 +0000

    separate hold post machine communication

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
