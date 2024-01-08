f7d369a71eb16fbe312baf5f81d8c928cbaa2a93
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:50:07 +0000

    actually clear adjective approve wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
