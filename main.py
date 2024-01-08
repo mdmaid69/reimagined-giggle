4666dd2b5ac730c73b9248f6bb6a20640748d924
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:45:51 +0000

    back thing normal object blind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def count_elements(lst):
+        return len(lst)
