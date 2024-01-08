e4ef4fcdd0569e1daf398ceaee5b05c92301bc5f
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:54:58 +0000

    solve music listenfor scream strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
