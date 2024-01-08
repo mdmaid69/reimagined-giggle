d7b3f19c95c881cc1298b87ea606e1b9b45e09a6
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:07:54 +0000

    square keen worst giveback tidy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
