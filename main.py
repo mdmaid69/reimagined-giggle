18db511f83530c7a7f0824c6d9783976edf31ca8
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:09:20 +0000

    crazy above wrap pollution shut

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+name = "Python"
+print("Hello,", name)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
