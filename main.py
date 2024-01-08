463ce693d280b5a64c338dff5f36040eadd57dc8
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:15:33 +0000

    profit takeover violence earn us

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
