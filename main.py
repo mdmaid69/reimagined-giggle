acfe21e641f6684b7fa6006a1053644e09f55dfb
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:35:20 +0000

    whatever switch chance defeat wealth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
