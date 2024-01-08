ae2a1ea63a411d2855bada98d1ef6db710422f80
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:06:53 +0000

    turnback remove pull offer left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
