02cbdefeaf043422859269ea30d03d785b58f7be
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:28:37 +0000

    stupid point listenfor interview extremely

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
