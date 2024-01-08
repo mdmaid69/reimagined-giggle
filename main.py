92dc9ec4f1553eb35a613505d1669a1d769e20e8
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:55:12 +0000

    clear direct put by positive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
