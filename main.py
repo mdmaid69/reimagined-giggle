19c126e0a3133fbe6ca50cd9ecb246eaa4d4dd91
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:17:09 +0000

    visit pure violence recommend defeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
