c6bd69e33c3b0e110beabf8eeb06369eac81777b
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:50:28 +0000

    bush hard contact explanation community

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def add_numbers(x, y):
-        return x + y
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
