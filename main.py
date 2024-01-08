54910ea6a979b15a0fcbe12689f92bc51cd6efae
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:31:10 +0000

    nor destroy personal decision comeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_acceleration(speed, time):
+        return speed / time
