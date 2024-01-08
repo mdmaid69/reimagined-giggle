0119096f4f496105587b2f1d8b89aefed4510b57
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:39:30 +0000

    typical movement fence dry lead

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
