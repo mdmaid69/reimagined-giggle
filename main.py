3aaa61669589a7957d4909f9e97da4a5beeaebe1
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:27:03 +0000

    570enjoyable wind throw confuse but

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def convert_to_octal(n):
+        return oct(n)
