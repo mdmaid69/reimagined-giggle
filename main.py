cb7340c29f13a4ddbecc2dd885fac27b6a358067
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:57:43 +0000

    detail sign furniture view ready

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_speed(distance, time):
+        return distance / time
