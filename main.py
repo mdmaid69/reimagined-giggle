02411ea732086b75e667a85483d538d38ecb1690
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:31:15 +0000

    theirs include fresh busy become

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
