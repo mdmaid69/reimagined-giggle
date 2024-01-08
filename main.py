dbf6fbd8711be2d7067c85e799a69897d22a57e1
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:59:45 +0000

    run pause that nurse sure

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import random
-def roll_die():
-        return random.randint(1, 6)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
