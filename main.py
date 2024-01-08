0f7680d150e3d10406d901acdd2286ec50353249
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:58:08 +0000

    education already factory bush drive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
