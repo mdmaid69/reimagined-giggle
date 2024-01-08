42639f6c3438f341605c3cddb69bbd627756f922
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:53:47 +0000

    along prefer reply marry thinkof

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
