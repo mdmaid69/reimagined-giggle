6d248bfdb524fb50b7277c65c557f599c7d87f78
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:19:32 +0000

    wind eye think Zz drive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def convert_to_hex(n):
+        return hex(n)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
