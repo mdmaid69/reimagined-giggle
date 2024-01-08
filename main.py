cf05a560cdac6f7a6b8d42b75fc0bf216fefb3f7
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:31:34 +0000

    reduce connected let definite trust

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_as_list(array):
-        return list(array)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
