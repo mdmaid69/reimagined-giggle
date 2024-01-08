01330bea59f6c05c0ca7684d5e590c06c63ab59b
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:54:54 +0000

    form cheap none Dd impossible

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import sys
-print(sys.version)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
