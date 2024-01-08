2bdf9e886b56afb39e942f4a977b0d087343fa4e
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:06:40 +0000

    unkind soft hers result run

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import time
+print(time.time())
