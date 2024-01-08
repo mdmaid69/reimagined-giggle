47d74631afd7f32cfc61e29c7d54136323702294
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:35:35 +0000

    sister bus man only argue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
