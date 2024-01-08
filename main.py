aed22a6c361a6f7507716063a960a7c14c4adebf
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:27:54 +0000

    brush formal hole permission detail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_current_directory():
+        return os.getcwd()
