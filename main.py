8d80f00caf853dc9854b9c329d207563537c3193
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:26:48 +0000

    convinced knock friend size punish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
