e379c9bd4c2fd83423dffa0e70965f7969f154c0
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:22:27 +0000

    tense introduce win sleep swallow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
