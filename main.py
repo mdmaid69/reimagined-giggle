fa8457554c30e31b39a340b8a713b0093221883d
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:31:13 +0000

    answer slide shrink gather naturally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
