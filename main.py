015e0ecede60dde7a35f2c9f32a2404fa1c77cb7
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:00:59 +0000

    night show bear fall region

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
