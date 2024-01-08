704376b952bac0d4790a84a2ea8faf53346e10e9
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:42:17 +0000

    lot bin again shall safe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+for i in range(10): print(i)
