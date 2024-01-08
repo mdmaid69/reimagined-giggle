4dd1ba9eda9d592c3b26ee1a10882ef946e037d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:29:39 +0000

    include sensible feed scared environment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
