a9350cfac9e5b94c75350fab8528426483b23ef0
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:42:06 +0000

    science woman temperature chance react

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-from collections import Counter
-print(Counter("hello world"))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
