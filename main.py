f4c84f43b203cd4e48ebb32a447ab5ab37d0f3d8
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:31:00 +0000

    Nn Qq sink test tie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
