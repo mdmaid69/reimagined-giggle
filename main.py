45ee549a2c9cae89c1c094d79df53fa161dfeca3
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:38:04 +0000

    arrive consult begin odd check

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
