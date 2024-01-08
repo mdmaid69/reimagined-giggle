96e8464af9af262ed527d3ebbc3edcd61e57e340
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:44:35 +0000

    least Bb involve tune there

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-print(sys.version)
+def count_characters(sentence):
+        return len(sentence)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
