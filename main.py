4d10032242959042542016917cd3d14c38cff7b9
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:21:37 +0000

    either perfect runover road talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
