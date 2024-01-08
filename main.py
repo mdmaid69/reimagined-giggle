0eb1d0694901d216103e6dfec2a11b3c20ec7894
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:23:22 +0000

    professional far invite month bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_even(n):
+        return n % 2 == 0
