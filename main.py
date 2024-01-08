5ef3ad0028b2b00e06f613888d16fba0517e8a3b
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:24:41 +0000

    recover holdup remain sink gooff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
