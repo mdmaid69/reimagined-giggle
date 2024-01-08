797b28d1890fd5deaa91adb2d53ddd2ea83c2cf5
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:39:53 +0000

    ring excited occasion off birth

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
 import array
-def get_array_item(array, i):
-        return array[i]
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
