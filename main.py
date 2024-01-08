bbba897b5c12b21778bc68ec74bc65b4e029b0eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:55:13 +0000

    tidy change communication explain will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import array
+def get_array_as_bool(array):
+        return bool(array)
