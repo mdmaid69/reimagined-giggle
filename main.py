8035716f3b36fae27fbe76a67c6a87264b943c6a
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:04:37 +0000

    summer hospital strong among now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import collections
+def create_queue():
+        return collections.deque()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
