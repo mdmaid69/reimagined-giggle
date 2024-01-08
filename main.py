1adcc6220dab7f4415bf192a6f7adabe452627f3
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:50:58 +0000

    fit sort neat basic connection

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
