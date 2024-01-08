3decfb0752a8340805d32556ba681160e0a615c1
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:51:42 +0000

    in high honest brain condition

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-from collections import Counter
-print(Counter("hello world"))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
