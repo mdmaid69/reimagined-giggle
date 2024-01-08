aa7d1da3db75bdc6a69a05bef53993daff0f4238
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:06:40 +0000

    luck his absolutely chase busy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def square_number(x):
+        return x**2
