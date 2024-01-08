16ec8f6e3b37fb9c5c75bc24f7cb16ae693c955e
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:14:59 +0000

    phone confident spring programme able

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_acceleration(speed, time):
+        return speed / time
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
