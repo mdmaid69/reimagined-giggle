5f9efd3e64d0629840a84a49a92b4f981d4fe215
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:10:12 +0000

    child load catch possession beautiful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  def convert_to_binary(n):
+        return bin(n)
