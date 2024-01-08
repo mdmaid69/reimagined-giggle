5cfd4c02aaff900de087cbe494bc95eb8154f6f7
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:23:10 +0000

    authority cut label awake ground

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
