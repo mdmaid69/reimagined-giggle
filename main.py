6ff909df019998fee9466a46de19a36fb41f4b55
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:11:28 +0000

    luck afterwards top meanwhile stone

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  def is_prime(n):
+def is_prime(n):
         for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
+                if n % i == 0:
+                return False
         return True
+def calculate_force(mass, acceleration):
+        return mass * acceleration
