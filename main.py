13b5a93f14216063991e53c716a1b432a8429f5c
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:04:44 +0000

    trust cruelty confidence cut this

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
