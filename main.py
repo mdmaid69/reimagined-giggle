6daed24633d1c82d9599bf2b649e23c236e66b5c
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:22:17 +0000

    smell accommodation custom side as

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def reverse_array(array):
-        array.reverse()
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
