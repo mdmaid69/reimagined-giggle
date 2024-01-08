792fead02c738477ff6331774e9f1e8931caa3d6
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:10:16 +0000

    table excuse everything base pump

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
