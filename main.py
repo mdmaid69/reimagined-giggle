d1f66444e113ac4ab1201b21b7850e952dc4244d
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:28:13 +0000

    check anything official programme view

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
