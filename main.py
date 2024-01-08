eb91f50bd726527422931c2de0636ae90a702ed7
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:46:00 +0000

    pencil big matter quickly late

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
