42a68403cc4624f3c04c1da65979f18e19cd8078
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:03:06 +0000

    female doubt actually slide hear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
