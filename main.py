d7e973a743dc3963cfa20f4aeabb5b78b27d2cc4
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:45:31 +0000

    make check ground opinion forward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
