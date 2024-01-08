95d686a941e677eb4992e9df78b53e6a863465c5
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:45:50 +0000

    shock play mysterious load lift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def convert_to_octal(n):
+        return oct(n)
