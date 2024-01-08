07574c416e3cd71d84328930210cee3e4898922a
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:41:36 +0000

    talkinto police tear to press

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
