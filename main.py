1ceb06a93a67cfad5dd59a12e7e497380604b9dd
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:02:44 +0000

    ourselves get love proof sit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+for i in range(5):
+        print(i)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
