3709b311ae25c4d963f4f771900fadfbb288c37f
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:01:11 +0000

    fight smooth practice later treat

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
