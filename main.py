d0c87e655e80e41d1ad2cc164e00994d480784b9
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:34:03 +0000

    regular cost by operate expect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
