14909ea1312bcbb75b26fda0ce70a46cc79e0eff
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:22:42 +0000

    apart support only sleep exploration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
