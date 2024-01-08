27cee94d2f233da65898363d3e1f22ce1fd6388d
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:06:44 +0000

    natural difficulty basic high needle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
   def get_file_blocks_allocated(file_name):
         return os.stat(file_name).st_blocks
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
