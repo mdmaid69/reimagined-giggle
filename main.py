5e83999e10522d41bccd6fd31fa584c1e02958cc
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:08:27 +0000

    share account child declare either

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
