3939fa3e659eaba08aa0d1a2086a9b2c598ef98f
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:22:53 +0000

    surprise than certain bottom smell

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def sort_list(lst):
+        return sorted(lst)
+def count_characters(sentence):
+        return len(sentence)
