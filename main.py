6fb71a59746b934982f84158e0ae0c6397a00903
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:26:08 +0000

    dress deaf stand everyone brush

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
