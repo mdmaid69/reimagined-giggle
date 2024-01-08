d2ed4f1e32f91d08d4241a8f17fa25c60fb9cd97
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:25:22 +0000

    your sure busy godown solve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  def count_elements(lst):
-        return len(lst)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import collections
+def create_counter():
+        return collections.Counter()
