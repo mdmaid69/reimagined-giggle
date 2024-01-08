b6d067451bfecf30a83179079caf5bebd7a599dd
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:33:22 +0000

    dinner present workon from start

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def convert_to_hex(n):
-        return hex(n)
-def multiply_numbers(x, y):
-        return x * y
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
