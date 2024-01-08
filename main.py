b06d3dc255f2de9f1af277d38125e2884fe4b421
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:31:12 +0000

    top contrast improve collect ours

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def convert_to_hex(n):
+        return hex(n)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
