e2376242b8100a6636e5903f80889e62f9c53af0
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:27:08 +0000

    market report spill song movement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def set_array_item(array, i, item):
+        array[i] = item
