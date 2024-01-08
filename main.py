967a2272f0f7d1d44b13c927da1d66cf9aa2f50e
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:11:27 +0000

    turnover relation custom union union

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
