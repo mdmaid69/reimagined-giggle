cbe99d138be83cb3d9b9a55208c3f49b397d25c8
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:48:07 +0000

    report off stop estimate break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
