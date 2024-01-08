e7f751e76a6169a42eb3821776afe144cec0ea31
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:55:41 +0000

    fairly possibility to turn capital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_current_directory():
+        return os.getcwd()
