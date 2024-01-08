f6ad32b70817eb72d3b75c0257c785d4a1470714
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:14:41 +0000

    use up steep Kk attention

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
