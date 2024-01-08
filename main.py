de4d0d7f281484c5c4deac7b0da846d9c4d0c44a
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:51:57 +0000

    boil earn weigh forward below

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import collections
+def create_queue():
+        return collections.deque()
