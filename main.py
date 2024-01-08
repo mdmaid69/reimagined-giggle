85ab48b8b57066b33a56a13e5cab27b6006d92b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:56:45 +0000

    part gooff run bother spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
