78b93e302e22708fe5c52f56d0d5f7a3382498db
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:39:51 +0000

    positive year Ww absolutely exist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def get_array_item(array, i):
+        return array[i]
