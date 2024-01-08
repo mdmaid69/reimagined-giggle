fd40e0db06be94d383152a0139cce82f7ebe911a
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:38:56 +0000

    disturb shape moral side direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
