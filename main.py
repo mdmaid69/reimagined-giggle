f7e17710ac7e86fcaf6610dd4f99565ad0953929
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:07:09 +0000

    nose support there trial star

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
