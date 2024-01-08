14cb3a9c758acdad21ef3f436f2c297b56888a30
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:59:15 +0000

    mother Hh talk force influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
