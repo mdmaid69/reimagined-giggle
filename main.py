71351f8045d57892f947e9bc47a43994aff1bc85
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:52:22 +0000

    benefit brief doctor contact shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
