9a4a1040ecbf7918b20034b93e232178bd0174a4
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:23:59 +0000

    picture from view century search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import array
+def get_array_as_int(array):
+        return int(array[0])
