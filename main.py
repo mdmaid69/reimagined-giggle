7c4b60503923029a0da38b1c482626177d960375
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:20:47 +0000

    ground tomorrow think improvement afternoon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def find_max(lst):
-        return max(lst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
