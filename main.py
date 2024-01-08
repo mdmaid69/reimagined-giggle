139ec560cd29014d2eaca3a02ecb7fcee5ef3601
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:44:02 +0000

    seriously somebody though takeback mass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import tensorflow as tf
-print(tf.__version__)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
