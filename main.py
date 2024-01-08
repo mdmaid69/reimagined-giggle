6aea8be1524a62d63e794bd063107fb585373952
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:13:50 +0000

    fact bend rush communication drive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+name = "Python"
+print("Hello,", name)
