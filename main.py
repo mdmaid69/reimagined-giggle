50b088e5ae1356e32e69da5cea9669ce27923dfd
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:45:50 +0000

    wind be offend angry fresh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
