e3db62e27d7214926f5a00d29b2b92752dcfb39b
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:21:29 +0000

    public far chat soil whom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def reverse_array(array):
+        array.reverse()
+  def sort_list(lst):
+        return sorted(lst)
