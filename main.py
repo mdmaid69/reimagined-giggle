22128f25b8d9f5d71a9a93d94f2c0d43a1d62a89
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:27:52 +0000

    scream build only roll tough

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
