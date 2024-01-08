3ca2dfa3be82288aa188cddee223106a2e52f176
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:27:12 +0000

    turnup drive rely closed perform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
