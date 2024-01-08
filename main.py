f9e2e1bc629e79d16dfecf313020cf030e3f53cd
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:34:27 +0000

    declaration thank dare equipment conversation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import sys
+  def get_python_version():
+        return sys.version
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
