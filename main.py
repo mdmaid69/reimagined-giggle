8eaa22c5f6088df76d308caac995cda903263a5b
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:49:52 +0000

    guess moment sensitive weigh abroad

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
