86e507192a75e9c4496f70086ea264c7728c014c
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:06:22 +0000

    sign mistake point pile inside

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
