33fd0ea809acfd436cde48a1fb54e388224a96b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:12:15 +0000

    sorry hurt cut over estimate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
