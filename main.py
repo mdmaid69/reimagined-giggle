6df0854d220199ffdd507f8d3fee2b40215c84b5
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:50:43 +0000

    happy thick round smell shrink

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_acceleration(speed, time):
+        return speed / time
