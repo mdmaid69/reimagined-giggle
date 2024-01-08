da5e4d8fae25d9e55cb869080f302318db8017bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:40:50 +0000

    edge onto chase actually ordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
