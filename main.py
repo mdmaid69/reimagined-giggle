ef3d886276670b185cd2708b2adb6b06ed6b666f
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:21:58 +0000

    comfortable accept slightly vision accordingto

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+print("Hello, world!")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
