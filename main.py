5c1e103944cc8f2aa4a488db0bc0d2d53c727026
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:03:32 +0000

    nerve transparent disturbing coffee steal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-print(sys.version)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
