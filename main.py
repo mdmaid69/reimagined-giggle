7b564481df5599dc0679aad24e08b23dffc244aa
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:49:42 +0000

    history call content deaf powerful

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import sys
+print(sys.version)
