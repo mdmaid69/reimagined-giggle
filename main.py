8910f97e1d323a96c9ea9610dca3178eb26e929e
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:30:12 +0000

    colour go special chance object

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import sys
-print(sys.version)
+print("Hello, world!")
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
