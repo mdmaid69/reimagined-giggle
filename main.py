493c7dda4abc1bbe01a621133772353017cb96d8
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:29:18 +0000

    eye constantly duty convenient shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
