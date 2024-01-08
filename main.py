ee4456ecd50c7db1d493e9f0fcec8bb2cb7c4520
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:41:39 +0000

    back interview impatient condition us

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
