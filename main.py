c3874e8de699fa4534be3a37e14a0dececdf87d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:41:20 +0000

    language mail entrance feature wonder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import time
+def get_current_time():
+        return time.ctime()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
