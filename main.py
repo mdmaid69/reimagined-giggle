2ff9fccff6704c87fd5580e61860f65712c652a6
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:25:06 +0000

    contrast male card figure best

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def subtract_numbers(x, y):
+        return x - y
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
