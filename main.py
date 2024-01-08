f6989afd043a14826376ec2f9d8aee97a0b7a76d
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:06:03 +0000

    concerning exaggerate dangerous lonely foot

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
