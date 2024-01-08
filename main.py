781413f51b0b96896081f5f636e8ece996d35c20
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:13:43 +0000

    introduction post company slight full

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_arc_sine(x):
+        return math.asin(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
