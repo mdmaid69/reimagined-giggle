5c3fc4b760f4e728dbbb60192a2d551348322b85
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:15:36 +0000

    drop memory previous environment direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import time
-def get_time_since_epoch():
-        return time.time()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_as_bool(array):
+        return bool(array)
