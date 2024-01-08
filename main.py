41d19ef4fa66160c4b8630a4d43fff2c000d06c2
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:07:56 +0000

    value goaway information kick noone

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_density(mass, volume):
+        return mass / volume
