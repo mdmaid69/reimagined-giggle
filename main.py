f635c0088803cb29c964ce981554341152d9eb5e
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:13:04 +0000

    record nurse answer big complete

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import datetime
+def get_current_date():
+        return datetime.date.today()
