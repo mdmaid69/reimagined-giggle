adc6c82f3336085f8f644affb18a4c6d1b2572cf
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:08:34 +0000

    face delight middle 570enjoyable hello

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_work(force, distance):
+        return force * distance
+import numpy as np
+print(np.array([1, 2, 3]))
