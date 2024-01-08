4eb24d81ec4e413c57985d70007d77069cbcd8c1
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:50:07 +0000

    lie step whistle used than

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
