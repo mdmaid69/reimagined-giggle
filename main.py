52a877bcb8c079e1963e7c07dc54984487c109e5
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:58:23 +0000

    main hospital clean noise somebody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
