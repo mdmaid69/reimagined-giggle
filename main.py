d80c1e10cf18bb545c73757ee353c23c0283aa5d
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:27:35 +0000

    above share climb someone by

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_max(lst):
+        return max(lst)
