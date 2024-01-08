fc179f35c7e26226547696a21e650e580ec6de79
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:35:24 +0000

    blue offence ahead badly young

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
