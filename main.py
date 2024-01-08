c15133493a354b96385608ecc9b89abbaee560cc
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:37:05 +0000

    human violence wire tense raise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
