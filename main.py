9e0259c6fa0a8c328331a3dd6e0c249dfce741d9
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:52:32 +0000

    study clue makeup luck communication

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
