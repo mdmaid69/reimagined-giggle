b1184ffb6545839a282fcafc32a7cdb95c4fbee1
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:33:15 +0000

    post strike attention indeed government

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
