f30336b992f71131afc956dfd153ec739d6922a9
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:19:23 +0000

    realize lot connected mysterious insist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
