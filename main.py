02be21beb7c3a91cb5493186d3246deeac49f240
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:11:27 +0000

    back dress boring myself forever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def subtract_numbers(x, y):
+        return x - y
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
