f9d37e028447da3c91fe83fd69c4940fb241c861
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:14:08 +0000

    drink say what hold draw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
