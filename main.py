ae99848c0b83363fc97e9b50f3bf5eac052d9bd4
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:18:22 +0000

    interfere put belong wake dress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
