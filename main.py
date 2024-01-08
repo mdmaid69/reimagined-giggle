51cc9e0a412ea6808cab73c19ed75c784b551640
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:43:35 +0000

    run worried off supply much

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
