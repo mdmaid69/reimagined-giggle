1a2680012c7ce37688ba40a3859e35552a4eabe8
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:52:46 +0000

    achieve apply exaggerate tall freedom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
