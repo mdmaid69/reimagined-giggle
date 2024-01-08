0a8f3afbc26dbfa8b055de36896a6eafe89885fc
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:27:57 +0000

    him task drop reaction genuine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
