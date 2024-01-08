fbb1a5d702ab6f6d7a2c54e407054581456f1d6d
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:00:22 +0000

    callat embarrassing read best continue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import os
+print(os.getcwd())
+import time
+def get_current_time():
+        return time.ctime()
