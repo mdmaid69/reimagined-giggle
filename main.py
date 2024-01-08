07dbc997962bce30fced92610f5ee78f2e0d371e
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:23:18 +0000

    dress manner dry exploration wear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import tensorflow as tf
+print(tf.__version__)
