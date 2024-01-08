7efa0e6adf78c4f082e50c2da520ecefb2e2e001
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:11:50 +0000

    satisfactory pocket promise load comeover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+name = "Python"
+print("Hello,", name)
