8f03e7bc5384fce797591a15630a021723fee997
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:49:07 +0000

    sign 179bite smell Rr form

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
