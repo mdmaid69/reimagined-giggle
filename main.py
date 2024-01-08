4c0eb79fdd89725f7fec41f6e682a3384a38e765
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:35:38 +0000

    authority Gg all bite dress

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
