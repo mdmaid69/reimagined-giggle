f7df2edc71db1cc9705211be047c1513fc164154
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:29:24 +0000

    shrink keen keepoff fit rich

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
