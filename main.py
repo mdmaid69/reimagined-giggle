1ec75c0150b86c084cbebf05613c57ec0909e72f
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:16:34 +0000

    description at unite Ff major

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import collections
+def create_queue():
+        return collections.deque()
