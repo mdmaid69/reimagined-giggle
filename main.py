5f4fde1e937b2fac8842c8d237bef6ebf026bb86
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:02:00 +0000

    event trick several tie release

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
 def convert_to_octal(n):
         return oct(n)
