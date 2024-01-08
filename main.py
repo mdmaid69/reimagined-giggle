dc6ebea52851a432e692c3bfc2d6a5ebb1421441
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:48:06 +0000

    what unusual deep edge frightened

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
