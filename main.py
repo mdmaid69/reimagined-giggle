a11172297bb9c4dabf241b9a31ece6d36430c338
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:24:08 +0000

    dance claim failure friend dress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
