f29b93000e2e5bd84758839c8cae351653c3cdb8
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:41:35 +0000

    job along uniform 570enjoyable fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
