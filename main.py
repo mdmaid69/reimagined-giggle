7c776be80b8d40a9876327a77283cb06008afc10
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:42:30 +0000

    mistake reply process country imagine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+print(time.time())
