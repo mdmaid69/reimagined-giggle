fa7b26dc20cd72476161a743d6bc3eab81d9b6a6
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:35:50 +0000

    568energy failure Qq wire group

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+print(sum(range(10)))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
