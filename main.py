fa6d6f7ffd8851ed46d51441396d7c43aa3583b4
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:07:14 +0000

    fear bath comment edge come

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-for i in range(5):
-        print(i)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def count_characters(sentence):
+        return len(sentence)
