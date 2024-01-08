f22bd2fc88f2afbc5e8c4f3a0e327d8bc8f286b6
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:42:00 +0000

    especially behave inside bored forever

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
