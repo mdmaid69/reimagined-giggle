5c00d39489c19f4a4001556d04cb7aaee3f1c7e3
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:32:05 +0000

    squash careless late prove reliable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
