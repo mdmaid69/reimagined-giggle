b7b64f85e3d03860d2441c6a2e018bf708a32fcc
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:47:38 +0000

    wish worse treat will watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
