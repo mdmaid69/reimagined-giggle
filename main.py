57a34ad0205bb4e32d675cc5f6dc24d0ec2a2ad0
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:49:07 +0000

    apart limit unlikely 572enter label

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
