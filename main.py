345398ef05ed40cd078342c474f46e572e05d0c6
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:50:37 +0000

    drink keepoff unless limit bringdown

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+i = 0
+while i < 5:
+        print(i)
+        i += 1
