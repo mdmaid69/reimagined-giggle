f816b310d426f9923a32f445fdc98e69bba3c381
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:13:37 +0000

    speed hole break interruption recent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  def is_even(n):
-        return n % 2 == 0
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
