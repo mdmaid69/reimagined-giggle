5f7a8195b63454b1d0a2e26154c2c1a99be9171e
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:29:58 +0000

    protect although vote bother stare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_speed(distance, time):
+        return distance / time
