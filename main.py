7b1a808b3a946db8f731e7888db71dde7d1e8509
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:34:12 +0000

    suffer surface pity Ww sink

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_arc_cosine(x):
+        return math.acos(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
