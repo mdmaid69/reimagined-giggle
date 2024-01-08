adbe990cf2650b54edf1dafb7f3d45640d7a2a2b
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:53:23 +0000

    push post refuse snow goodbye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import getpass
+def get_username():
+        return getpass.getuser()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
