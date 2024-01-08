4ee5674402ad79a882077b5321b8069a1d8391f5
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:08:49 +0000

    nothing 569enjoy actual Pp myself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
