7730ce703dc9c3dca8c49b239d9a6e3c4100f554
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:26:02 +0000

    wake meaning similar square both

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_acceleration(speed, time):
-        return speed / time
+import os
+print(os.getcwd())
+def calculate_speed(distance, time):
+        return distance / time
