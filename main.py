9c678573ff1951bed3521d151aa865b4547602e6
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:25:19 +0000

    571enough range attract people solve

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
