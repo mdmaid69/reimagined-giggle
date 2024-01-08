4c99112ecc9898eb448f9fc96e1f1f4ce9d417e3
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:18:40 +0000

    supply divide file short morning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import sys
+print(sys.version)
