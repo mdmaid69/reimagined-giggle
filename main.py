ad49a7877814b35c479e1c672eb2af02eb3557fb
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:14:42 +0000

    confusing instead gap Ww region

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_acceleration(speed, time):
+        return speed / time
