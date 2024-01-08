75889c0f294151e5d71e1dd85d66d8b392797efe
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:52:42 +0000

    couple bus breath aware range

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
