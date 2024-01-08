8cdf53fb3e8fd5824fd6e2f4d95833445da37363
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:12:48 +0000

    vanish eat keep beginning tense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_repr(array):
+        return repr(array)
