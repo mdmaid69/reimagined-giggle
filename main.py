59ac6737bfb9f70c95be51d26a45a172b5cec84d
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:11:47 +0000

    explain ordinary juice line apart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
