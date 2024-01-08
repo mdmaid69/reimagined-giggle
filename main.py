1c9520332342b3d2d5b551232e9fa7a7140b9017
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:48:08 +0000

    rule lunch clue borrow cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def get_array_as_bool(array):
+        return bool(array)
+from collections import Counter
+print(Counter("hello world"))
