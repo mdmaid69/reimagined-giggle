f6fc61124deb0e23e211da48b111f2519f7b1e50
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:04:51 +0000

    organize history calloff scare his

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_min(numbers):
+        return min(numbers)
+def convert_to_hex(n):
+        return hex(n)
