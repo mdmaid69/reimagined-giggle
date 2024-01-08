657cc50d6d6f82dc6eb74ee3448f4ff5a3196ab6
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:09:30 +0000

    geton turn later floor roof

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
