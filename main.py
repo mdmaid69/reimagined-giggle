a1c32871e4da54bebe091925f663fef0b50fb03f
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:53:20 +0000

    present ride machine hollow runout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
