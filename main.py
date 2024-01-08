55f6589794fad7e9bfed441d50552f5e1457c01c
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:59:30 +0000

    serve pass unusual wise remark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+print(math.pi)
