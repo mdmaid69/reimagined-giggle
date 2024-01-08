b46f70eaad001f699052388ac44c3fffb39c6e20
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:42:30 +0000

    down Hh love practise manage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def reverse_array(array):
+        array.reverse()
