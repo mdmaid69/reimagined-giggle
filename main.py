65516adc00cfce81591b831a9ef06109032391db
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:02:01 +0000

    birthday freeze gift female trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def split_path(path):
-        return os.path.split(path)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
