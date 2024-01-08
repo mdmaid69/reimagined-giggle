440170a95b2a9c07b16d3de5f22d6a5455b4f300
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:54:10 +0000

    sentence forward split suffer summer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def reverse_list(lst):
+        return lst[::-1]
