91c6928d5c23d12202ee5bbdbde63a11d1fa051f
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:13:18 +0000

    turninto actual cap explain file

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import collections
+def create_user_list():
+        return collections.UserList()
