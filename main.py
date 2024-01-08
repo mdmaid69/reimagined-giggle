1e979548f94c511a419cb67d5e13ddd697b0a25e
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:37:27 +0000

    habit sick 569enjoy communication long

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
