40595c7917ad822e3eafac174ae42d77d07e7805
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:31:47 +0000

    bound error once callat cap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_user_list():
+        return collections.UserList()
