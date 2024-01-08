65ab90c14830c087b5739b0f24b2fcbabe373b37
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:42:35 +0000

    action touch door band rescue

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-print(sum(range(10)))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def get_array_index(array, item):
+        return array.index(item)
