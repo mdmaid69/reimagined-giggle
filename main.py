c6a228b743f6fca31c60b1675b3af57b831c5806
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:43:35 +0000

    connected shallow as switch car

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_user_list():
+        return collections.UserList()
