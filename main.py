e34585fef6afab18d2ceb01b9d7c379eb3cdbe65
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:09:41 +0000

    embarrassed reject figure pattern cry

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def find_max(lst):
+        return max(lst)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
