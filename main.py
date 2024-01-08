7d0521c036f0e94d5174b5de395c4044c36146de
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:27:03 +0000

    guess market lunch operate place

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def convert_to_hex(n):
+        return hex(n)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
