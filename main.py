a52d9bd54343a56df9ad0585369beb56fa869926
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:34:37 +0000

    song comeout favour story burn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
