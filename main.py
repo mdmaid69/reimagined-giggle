b3b281beb782ebf1f69f47eda46b19fdbfa9af69
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:39:02 +0000

    attempt drive hurry quantity cost

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
