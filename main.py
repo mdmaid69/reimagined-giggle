5f320754786474e921900e2e206b61a59b422c47
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:34:03 +0000

    month ever bother write total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
