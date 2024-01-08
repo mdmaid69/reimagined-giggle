1d9720158d27972a1f1a74ba0402308b617ba416
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:00:09 +0000

    clearly deserve tall cool might

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
