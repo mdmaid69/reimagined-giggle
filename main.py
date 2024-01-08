6d646739b1afba2f07ba92044326aa245a8620e3
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:53:25 +0000

    wide suffer quality broad collection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def convert_to_hex(n):
+        return hex(n)
