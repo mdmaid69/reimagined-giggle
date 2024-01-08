9d4e0178a31233f3b37e42f18f52f5e48e5f477a
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:40:56 +0000

    Yy round common undo sense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
