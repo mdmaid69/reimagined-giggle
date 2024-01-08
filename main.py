57f5b011cebc8b8d07eafaba3a59a8e1b1492964
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:55:15 +0000

    remain phone suggestion sort congratulate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
