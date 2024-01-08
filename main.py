8fd22c9415c2f6875f5e9a6fd7ccbf96ffe7301a
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:38:31 +0000

    describe main always computer suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
