aacc8a7feaba2c8f86f9f551e1f7a4aab3b7112b
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:39:52 +0000

    healthy range constantly sport watch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
