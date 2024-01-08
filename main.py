ddf3d067cebfa477c96a6060b74ae5ad149ad7d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:14:56 +0000

    accident model share back branch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def get_array_as_repr(array):
+        return repr(array)
