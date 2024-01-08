ab94b948bbc656e08912616ab3a4a0a3507fbfa6
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:52:54 +0000

    hate stay ordinary carry far

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-print(sum(range(10)))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
