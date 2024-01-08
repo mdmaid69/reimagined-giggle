137124bc14b04fa9a44d1cd0783b981cec5e830b
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:45:17 +0000

    lose name payment anybody against

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
