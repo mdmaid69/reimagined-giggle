592d6f63a214ad8f13e60ce7978dc860282304f4
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:43:49 +0000

    appearance danger sensitive crowded backwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
