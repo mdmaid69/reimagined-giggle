a1919d61f156b80765b1b3461fdbbab93290eec8
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:25:57 +0000

    try certain left flow error

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
