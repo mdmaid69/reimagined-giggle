27abb958762a83ab0b887fac78012671e659e56a
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:57:45 +0000

    bridge skirt stop spoil else

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
