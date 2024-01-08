a2cc1ba5a10e69963ad3adac4e1c2d0398579dee
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:17:49 +0000

    in term situation surprising rob

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import array
+def reverse_array(array):
+        array.reverse()
