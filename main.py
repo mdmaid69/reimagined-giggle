a9ee859f1fd7e5ffef70e6e57c996b95377d3f00
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:32:26 +0000

    nurse breakthrough mess borrow summer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def create_user_list():
+        return collections.UserList()
