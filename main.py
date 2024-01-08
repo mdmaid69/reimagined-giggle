5cb17f2dce7e374763f2bbeb29f295be01a7324e
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:02:30 +0000

    mess dry cover fear another

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
