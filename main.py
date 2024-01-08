a83538fe975f992cd2a6d632f491eb0f06d1cae0
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:56:43 +0000

    crazy bored no successful rise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import os
+print(os.getcwd())
