95c34deba23b77b9c24e83d0f50d395234e78dba
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:57:06 +0000

    before fix wall swim square

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
