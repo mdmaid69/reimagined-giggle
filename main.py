61248611e1a8c12ec72a4ade0aaefc31965cdb3d
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:58:01 +0000

    question colour whistle squeeze involve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
