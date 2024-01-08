0a552c3734bf22841c7d23614dbd0018bbb40e25
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:39:44 +0000

    event treat favourite agreement judge

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def get_array_item(array, i):
-        return array[i]
+print([x**2 for x in range(10)])
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
