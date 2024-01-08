6a301dca5256b2b744c767d3838911205adffebc
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:52:44 +0000

    lookback(on) shelter relationship care themselves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
