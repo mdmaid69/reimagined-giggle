ce49826e1f399425c3afec862d50fa9a0fd27654
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:34:18 +0000

    only run trial slice wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-print(time.time())
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def find_max(numbers):
+        return max(numbers)
