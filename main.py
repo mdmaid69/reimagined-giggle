0e670e4909dad977d4970a28262e5b852565daab
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:13:57 +0000

    dress difficult excuse read price

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_current_directory():
+        return os.getcwd()
