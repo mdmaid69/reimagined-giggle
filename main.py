66cebcb9e2e99ec09e7ad952ea0e5985080a14f1
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:24:54 +0000

    warm hand polite production well

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
