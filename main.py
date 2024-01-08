657bac5ea1103db4b66bd9329fc28af792856fa7
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:32:08 +0000

    all dig smile dark his

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_volume(length, width, height):
-        return length * width * height
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
