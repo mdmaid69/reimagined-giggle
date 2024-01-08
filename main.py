2184fd4fd26e6b4371c878dff15364997679c1f4
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:28:16 +0000

    yourself wash believe suck family

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
 import subprocess
 def run_command(cmd):
         return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
