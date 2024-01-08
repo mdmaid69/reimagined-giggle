731102dd93be37ed5c646ae47551aa2c464f94cf
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:33:04 +0000

    bag survive wake evidence match

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
