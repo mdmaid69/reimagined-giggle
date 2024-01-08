59f0545dfbc57dae9b732c26d30328d3cf593cab
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:32:30 +0000

    conclusion whether sick word name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
