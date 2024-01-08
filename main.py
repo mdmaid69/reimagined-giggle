8c7943cd77ae885a3f48e62aad112e8d675caaa3
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:02:55 +0000

    minor permission callat hole Ii

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
