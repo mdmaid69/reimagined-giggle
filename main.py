90ca12bcccafff0bc5b3beeea905c4a5707b3504
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:39:06 +0000

    front pick ever bush indeed

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
