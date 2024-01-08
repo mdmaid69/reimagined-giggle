24eaabd7440d97eef7c861b8e90b2135a01d2508
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:01:29 +0000

    burst activity dream confidence account

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
