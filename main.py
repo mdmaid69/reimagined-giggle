5903e03ef91822ca7a237b16a558ada7519508c2
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:34:16 +0000

    kind moment any sure possibility

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
