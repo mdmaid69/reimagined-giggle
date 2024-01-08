3450d835e5d34e304fa4677e5a474ed3bd8168af
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:29:42 +0000

    previous pretty promise comein plenty

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def find_max(numbers):
+        return max(numbers)
