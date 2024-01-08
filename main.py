b1a67a74db7792b93f2d2c838b886094baf14ae0
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:55:56 +0000

    among satisfactory last problem copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
