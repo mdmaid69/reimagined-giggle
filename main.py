e211989fba92b6b75f13112103c2394a2f9ddc3f
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:43:12 +0000

    frame next walk straight positive

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
