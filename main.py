09b38bae072f10d0ac0e1d16dc32577353f601e2
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:03:37 +0000

    sink complain uncertain inform high

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
