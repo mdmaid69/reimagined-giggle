6c63f39339e42dba7f4a2b04d26dc3e35162bf79
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:38:33 +0000

    word runout nonsense march Ii

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
