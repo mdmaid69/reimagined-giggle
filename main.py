9cca1ffeff65560a41160c29e1929379a96cd353
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:26:23 +0000

    someone light weekend argue moment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import os
+def change_working_directory(path):
+        os.chdir(path)
