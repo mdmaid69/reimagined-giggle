5f8f326fd8348469867af983395365b50ada6648
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:36:29 +0000

    dark major total dish language

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def find_min(lst):
+        return min(lst)
