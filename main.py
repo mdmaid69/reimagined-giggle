ccae667f14c22cfa25c45c346e56672b5843eb21
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:49:38 +0000

    replace deal pure geton support

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
