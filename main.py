e045c283b4f6701282c9c37cfa9ccb849826a312
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:22:46 +0000

    cold provide ahead pot belief

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
