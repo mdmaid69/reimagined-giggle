3acadc95dea502c258301383f7f2dff994228f0f
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:58:26 +0000

    produce unlucky spend personal have

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import logging
+def log_message(message):
+        logging.info(message)
