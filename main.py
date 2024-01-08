75b6bdd6d1b7ffa123f4dd4864e6a59dc569b12f
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:12:17 +0000

    exactly prepare wash destroy powerful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
