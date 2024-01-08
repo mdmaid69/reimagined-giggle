497fcabf11aad46a8aeb0c8d713c6fc1f536458c
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:34:57 +0000

    pleasant Ll cope earn proof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
