f29b446a8e179a872fbb9714aa13b468d4531543
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:30:46 +0000

    first listen strength rely declaration

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-for i in range(10): print(i)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_user_string():
+        return collections.UserString()
