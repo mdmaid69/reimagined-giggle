e0e17b62a8756c1e8b029be236003b28bd3bc53c
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:05:29 +0000

    centimetre but possession on workon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
