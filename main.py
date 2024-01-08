ae063eacb14fb1d179482749d944262731abc623
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:37:58 +0000

    picture manage body fire whether

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def is_prime(n):
-        if n < 2: return False
+  def is_prime(n):
         for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
+          if n % i == 0:
+        return False
         return True
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
