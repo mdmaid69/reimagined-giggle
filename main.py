9a65ef8b0efcb33ae29c237f466b2bbdf248d6c1
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:05:41 +0000

    form actual right disturbing maybe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import array
+def get_array_as_str(array):
+        return str(array)
