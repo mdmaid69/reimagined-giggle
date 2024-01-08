e21f1a60942b40837b1abd9ebe966f8238ca1162
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:06:38 +0000

    worse coat live whole payment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def change_working_directory(path):
+        os.chdir(path)
