c02609de28a1defd7ef91b5366c7b1c46e0750cc
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:46:30 +0000

    claim sit pay breakup rid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
