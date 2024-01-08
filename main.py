fddb7aeb66f3fa9eed3349b9d72899b7d23cf3b9
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:33:42 +0000

    imagine before rely land rarely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
