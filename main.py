df4baea9f96b15ebd6694f3709e214f315fe6732
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:40:32 +0000

    runout public laugh dance sleep

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_pressure(force, area):
-        return force / area
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
