77f898f9415dc2f5a1a42a12650cc66d33f70dea
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:24:38 +0000

    simple near check talk odd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
