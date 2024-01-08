ccd354a18bbd90143708fcb4997f816e2ecbc07a
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:10:34 +0000

    move deep admit burn sad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
