b6f32224fafd19d6a837b426b1f7c87218b018ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:49:26 +0000

    high fix post age happen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
