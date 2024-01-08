93ce377181aed9f8fe0ad3f110d38964ae96bde0
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:02:47 +0000

    number violent noise test ashamed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_string_from_array(array):
+        return array.tobytes()
