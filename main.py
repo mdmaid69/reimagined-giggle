5bd77812fe80e178ac7978b5ecba7b0c22be5bc8
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:57:02 +0000

    reserve culture finger connected of

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+print(sum(range(10)))
