43b08a34b420aca24d1b4524fd4301393985cbfa
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:18:03 +0000

    lookback(on) noisy close reliable adjective

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
