9e977c86194f66e7c7949aa9c86aa8e857c64843
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:12:13 +0000

    out yourself fire judge split

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def sort_list(lst):
+        return sorted(lst)
+def find_max(numbers):
+        return max(numbers)
