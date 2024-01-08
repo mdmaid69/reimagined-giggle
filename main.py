07673923d36a6a2bc3f1056ea9d57d2ae02bf60d
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:34:29 +0000

    fence style control splash exact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
