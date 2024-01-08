08bd5b71ea3f9de5623c5e711f26a197c46d4a25
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:08:20 +0000

    pair already suit exam care

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
