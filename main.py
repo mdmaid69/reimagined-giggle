b554ec5cf6503cffd70c697977d543c58d963155
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:33:13 +0000

    during keepfrom shop return kind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_acceleration(speed, time):
+        return speed / time
