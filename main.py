b96a6ecd1c8ebaee7d6f4fb77fd85db8ddf556ac
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:40:41 +0000

    mine related reach sentence lie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def split_path(path):
-        return os.path.split(path)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
