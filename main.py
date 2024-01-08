4d05b7125c2cabd4f6251584b9a585d0ceea4e3c
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:07:28 +0000

    handle minority child suck computer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
