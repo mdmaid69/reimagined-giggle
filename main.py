c3f58c895907cbbec27a0cdc9a50eb175d1cc5b0
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:40:08 +0000

    cheap test set shade backwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
