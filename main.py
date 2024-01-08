e97b6e642e17c2e333f4cfc74b1d6ec24ebd1197
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:55:56 +0000

    base guess desperate service effort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
