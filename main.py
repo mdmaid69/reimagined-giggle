f6311585a733ffa1e6b5dd07625e74582c14b668
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:35:53 +0000

    juice absolutely pain hollow weak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import datetime
+print(datetime.datetime.now())
+import os
+def remove_directory(path):
+        os.rmdir(path)
