01c1ca7dfb5147f22babb18cf8a5e292a69b39f4
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:28:27 +0000

    frame already recently charge accidentally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
