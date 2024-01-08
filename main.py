9b8802368ba51f27f4b9f424ffe0bf71be8bbd4e
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:37:58 +0000

    somehow his influence board drive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def reverse_list(lst):
-        return lst[::-1]
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
