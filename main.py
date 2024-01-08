af77866d7091f38fed676b0b346f24f8212105d1
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:55:10 +0000

    remember check emotion below worst

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
