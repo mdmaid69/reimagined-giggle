8b5947826f44aa77ecad463a81fea2557b91ef78
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:18:45 +0000

    of shake attempt transparent attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
