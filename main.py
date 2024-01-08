7c93e4b72e58f1e6d9f3e93c3b3f59cee86c2e9e
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:26:25 +0000

    passage narrow related spoil explain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
