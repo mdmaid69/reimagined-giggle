09c60a9525eb2d3d845ac9836a1329acc20761c6
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:27:37 +0000

    murder cheque consider fat fire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_index(array, item):
+        return array.index(item)
