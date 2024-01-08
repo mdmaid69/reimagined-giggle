b7f34c51290ec7982e651e5e5be706e5d3e9a609
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:34:40 +0000

    comeoff always sign beside record

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
