dba9c060aa0f01df8abda8524724fbbd5986775f
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:19:20 +0000

    hear behaviour tap proud considering

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
