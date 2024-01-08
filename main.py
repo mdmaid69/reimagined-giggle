ae13d868d5f29b5ccaa514516c375aa657f760cb
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:57:50 +0000

    steer achieve pocket makeinto on

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+print(math.pi)
