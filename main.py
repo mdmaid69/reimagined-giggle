d880668b796c7f0e3c810702536e9d7a8a7efefd
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:27:40 +0000

    otherwise earn Qq object interruption

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
