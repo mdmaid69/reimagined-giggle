06901b1cad38028e86166c630877b1096f253f8a
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:41:50 +0000

    perhaps murder corner lock corner

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import collections
-def create_queue():
-        return collections.deque()
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
