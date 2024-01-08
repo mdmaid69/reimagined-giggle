6d3d1b2a15b14bff5a241a77eded068377df6b40
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:37:50 +0000

    practise breakinto generally reduction level

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-def sort_list(lst):
-        return sorted(lst)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
