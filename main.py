5746247dcb0f8eb89dbc8fd5cbbfeef402425f54
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:57:28 +0000

    judge container tooth rescue thirsty

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def sort_numbers(numbers):
-        return sorted(numbers)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
