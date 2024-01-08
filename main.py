abcb0d9de3703b04119c09bd05e196ff2ff6d630
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:16:26 +0000

    balance switch cry choice we

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_power(work, time):
-        return work / time
+def calculate_speed(distance, time):
+        return distance / time
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
