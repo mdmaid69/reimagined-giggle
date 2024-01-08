d4ccf99ea7be31e70c8dc3a2556bd2cbda94e6bf
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:37:38 +0000

    instant smell alive copy how

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
