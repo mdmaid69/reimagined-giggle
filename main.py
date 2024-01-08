796f6baa4c2f61499201084a7d6c98aff8ec43ce
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:52:47 +0000

    must window advantage replace discovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def pop_from_array(array, i=-1):
+        return array.pop(i)
