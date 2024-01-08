8ce58a75fe84359f690a11199d86558f129aaa10
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:39:55 +0000

    no poison confusing by make

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def convert_to_octal(n):
-        return oct(n)
+import time
+print(time.time())
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
