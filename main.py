3b911167abf0e385d67cfd067be11ea53934b414
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:16:05 +0000

    lookaround wear plain holdon show

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import numpy as np
-print(np.array([1, 2, 3]))
+import math
+print(math.pi)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
