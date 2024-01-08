e476917767dd258ba5aa6bd15f8ab9a3e7bf0bea
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:40:37 +0000

    beginning office be free wake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  def is_odd(n):
+        return n % 2 != 0
