c7771d620348598f49caaed4e69cc5f218c3ca1b
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:11:46 +0000

    once reply then else unlucky

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import collections
-def create_counter():
-        return collections.Counter()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
