6a653f93a6d095050f97ecea2f39ef987dfb9229
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:29:31 +0000

    declaration sensible back official clothes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
