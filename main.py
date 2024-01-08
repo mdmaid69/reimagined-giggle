4c1455b9a2e90c0718fba5bd64a1db62fa6d244e
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:40:15 +0000

    boil float smile then interrupt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import sys
-  def get_python_version():
-        return sys.version
+def find_max(lst):
+        return max(lst)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
