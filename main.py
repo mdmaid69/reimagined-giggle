433e8bab8bb06def04e0371b25b2932101474ef0
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:13:40 +0000

    muscle arm raw strong better

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
