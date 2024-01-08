22868b8ab45e88a7f75f383a2a8fe4d5277e841a
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:04:07 +0000

    slice visit performance attention cover

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import tensorflow as tf
+print(tf.__version__)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
