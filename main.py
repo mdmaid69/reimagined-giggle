bf3310ff3c90f34710cead565ccfaeaedbe4645e
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:50:39 +0000

    discuss same recommend work well

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
