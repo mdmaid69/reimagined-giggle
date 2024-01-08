ed9f597a32eed733b2046e0aaa46c4effbd30644
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:46:18 +0000

    method system press there march

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
