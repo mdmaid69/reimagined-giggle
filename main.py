f0f0dc6a1b2235acf30915f1fe0df69e9cbc8391
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:46:10 +0000

    they shallow like personal traffic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
