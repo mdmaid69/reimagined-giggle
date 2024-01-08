8d022cac26a83234adef3c6c91f446233e09cbf6
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:18:35 +0000

    personal complain not use ought

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
