c4b909466af00771b8215e64f419cce051fe5ac2
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:15:17 +0000

    acceptable himself accidentally settle temper

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
