bec2d6ea6f28229fa5e6a197fef8999916b05625
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:41:37 +0000

    deep unhappy better kill danger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
