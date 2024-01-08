a2646fabde8cb03831e93ab38b61819e937deadd
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:48:21 +0000

    pick natural callup show bin

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def remove_duplicates(lst):
-        return list(set(lst))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
