0e35ba530ee03578f343d1a8e832d02d6d6122ed
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:09:45 +0000

    claim praise sound marry challenge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
