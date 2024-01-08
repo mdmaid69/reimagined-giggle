895cb451658c9f35526abbbf89df8ad007de8eb6
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:55:34 +0000

    cause any convinced instant Dd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
 def is_even(n):
         return n % 2 == 0
