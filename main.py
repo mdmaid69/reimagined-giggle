2c933ad8cfca7b211817d77de63e97fa8b998cf1
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:32:25 +0000

    bottom range history fit produce

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
