a58f200d05201284ae8c28f9d8354cc8496a94a2
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:04:25 +0000

    comeback listenfor choose show illness

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
