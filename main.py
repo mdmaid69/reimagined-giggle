514262039f5b4a834ff8306ea78136349db4e8f7
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:49:01 +0000

    further giveback Rr tell press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
