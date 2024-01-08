0fa63f1849c23e209fd3f4fc588422eced55ab43
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:07:29 +0000

    usual somebody somebody return home

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
