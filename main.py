7e76be066d8baaa54ebf87b0c0f48e280d3d5910
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:13:19 +0000

    eye every colour yet hate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
