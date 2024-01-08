6940d4e1e68869f6a904168de38c8754001ff046
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:47:31 +0000

    car activity interested sting couple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
