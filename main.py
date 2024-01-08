f14e5675b9142e6ea1d471b6065ab2b026706a82
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:58:00 +0000

    disturb angry dish taste round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
