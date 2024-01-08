54feb0d2f6d4b759fc7cacc7fb39b2867426d173
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:45:45 +0000

    trousers exciting shade care sense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+import time
+print(time.time())
