6c363238b0bb2f40338794637e8ca3167d517589
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:15:20 +0000

    identify unable plan trade bound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
