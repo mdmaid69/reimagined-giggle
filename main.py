42231723c43c1493227a16e04cc5ff9fd8f94de8
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:44:00 +0000

    clean giveup depend spill somewhere

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
