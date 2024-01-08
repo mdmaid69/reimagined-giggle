cb7431fb241a59c1d7fce8acce3019548b013f5f
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:23:29 +0000

    this along tip chat recovery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_cosine(x):
+        return math.cos(x)
