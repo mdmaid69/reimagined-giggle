12f1146636b486ef6e091e4a7de185ab2a59e684
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:34:49 +0000

    butter realize afford information responsibility

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import getpass
-def get_username():
-        return getpass.getuser()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_current_directory():
+        return os.getcwd()
