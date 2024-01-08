dbdb301af270716a90b9ef10c150bccc0722d9cb
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:09:36 +0000

    fall mention yes several except

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
