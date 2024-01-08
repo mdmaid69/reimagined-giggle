cab3027df7f58dc4cf90cb836addaa413f90efad
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:58:44 +0000

    go so temperature record Jj

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
