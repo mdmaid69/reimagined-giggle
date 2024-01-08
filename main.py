6976fb6e5deb7c5c8c9b77ea382ee3297722b096
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:54:13 +0000

    score whoever storm take travel

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_str(array):
+        return str(array)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
