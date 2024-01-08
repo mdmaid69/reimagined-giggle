3f731aa074c7779c6bf3d3677e6dc0d99d498b49
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:27:03 +0000

    tap slip nature drop form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
