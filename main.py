2c7acc547101414f7dc77c961be941417b296e73
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:31:12 +0000

    suggest fill ordinary gift aim

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
