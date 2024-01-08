3c13636aa68f9b40f4951944aa4e7032526eeb6f
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:41:24 +0000

    brain mixture separate fault story

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
