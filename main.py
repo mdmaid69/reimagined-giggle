044af298bebcffcd79cc94fc00d15b9790069061
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:25:48 +0000

    test hair remove feed sometimes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
