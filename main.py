1e444234ddb508cbb62ea6489f150a2c1f1fb228
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:07:47 +0000

    forward rush other reserve unhappy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
