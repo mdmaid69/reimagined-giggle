ada2bb9ba76653f10258bcb1918affcc40893c08
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:28:15 +0000

    light statement pollution lift news

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import random
-print(random.randint(0, 100))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def is_palindrome(s):
+        return s == s[::-1]
