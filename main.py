9602d7194a8c4e8277f0ebf48651a4966a092982
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:31:10 +0000

    miss definite bringforward prefer operate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def square_number(x):
+        return x**2
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
