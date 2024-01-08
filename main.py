b44ce493fcf272b245b1ceb604b60a419cb1f13f
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:03:43 +0000

    roll unite otherwise holdup narrow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
