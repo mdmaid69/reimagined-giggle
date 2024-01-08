b85a8017b03f4e018648fe1cc29336c35f068cc4
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:46:55 +0000

    marry cruel soft wipe deal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
