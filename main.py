df3a9fb8eb5ef68e5af154d5061603b402499d9f
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:48:00 +0000

    story meaning honest week mother

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
