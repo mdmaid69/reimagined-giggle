6c9a058c8848da27d8b49e0e01871c55681efd4f
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:17:49 +0000

    wash 572enter except violence kind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-print("Hello, world!")
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
