d9585186097e23dfbfb2df912b5c35695c181ed8
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:24:11 +0000

    generally ticket system story remark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
