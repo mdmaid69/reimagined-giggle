a6f1cb9e6ae7eacdfd17fc3c0c8df9f280fb9d3b
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:28:00 +0000

    spin carry true wire mass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
