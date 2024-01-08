e4307027d484fa1d6901e2e68795b23496105720
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:07:51 +0000

    original friendly clearly lock comeoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_item(array, i):
+        return array[i]
