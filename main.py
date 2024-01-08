df30de930c63a2f3f76fb4cbbb3809879763f8a2
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:29:47 +0000

    neither choose centre number return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
