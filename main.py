d4ad0a9e49704bf34b45309968eb923e504499c7
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:42:32 +0000

    traffic then copy corner workout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
