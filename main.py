1738f8980161c0f5a16a8206154ada75d1817bd2
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:38:51 +0000

    comeup transparent across partner slice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
