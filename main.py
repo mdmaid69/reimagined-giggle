419f9623f450b246c6c51a964351553e1b1dfbb6
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:00:47 +0000

    dig comfortable land stir least

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-print(random.randint(0, 100))
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
