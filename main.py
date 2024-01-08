876a62ac852b728a166df8d230362f0319c9cd16
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:58:50 +0000

    play surface mean belief difference

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def add_numbers(a, b):
-        return a + b
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
