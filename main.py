99ee69236e947b965388bcaae8ba860cec7bdf2b
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:52:57 +0000

    never remember nature important wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
