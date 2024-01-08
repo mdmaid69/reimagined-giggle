093c736fed1de1e3e7eb687fd5ed511db6fb81e9
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:29:02 +0000

    Jj test treat rough quarter

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
