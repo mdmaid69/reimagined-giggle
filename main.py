dc08cbd948953b0d53bb62f6accc9aac0c0bcadb
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:55:01 +0000

    involve note brother wing drink

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  def calculate_area_rectangle(l, w):
+        return l * w
