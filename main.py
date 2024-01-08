43612bd8bab73f6e540733addafb244b82910a4c
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:12:37 +0000

    deliberately far young hole ago

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
