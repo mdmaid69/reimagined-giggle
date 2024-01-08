2cc23b5f77d851a73d78fd0d048671acdeaafd93
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:57:14 +0000

    pile vision Yy have drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
