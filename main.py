0ca60e51c6d4cd63ea70c4034c39e392237106ce
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:24:58 +0000

    turn funny temper 567enemy surprise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_user_string():
+        return collections.UserString()
