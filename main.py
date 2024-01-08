9555689f958e0a3a405c9a9538b652e818ce49d5
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:07:33 +0000

    upset ago along condition command

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
