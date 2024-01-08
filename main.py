126df82402412911a96d3928fc3e39a57f343ad0
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:05:36 +0000

    fall makeinto guess object forgive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def clear_array(array):
+        array *= 0
+text = "Hello, world!"
+print("Reversed:", text[::-1])
