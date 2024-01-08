52ac2b33d64eec4aa2e65c0e77d82fbd5b97e904
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:49:56 +0000

    deep fear where shadow feeling

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+print(sys.version)
