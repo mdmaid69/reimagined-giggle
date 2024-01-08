36ea2cde398574c1e6cb178f316f85cc801b84a0
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:11:56 +0000

    please fierce heavy consult drawing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import json
+print(json.dumps({"name": "John", "age": 30}))
