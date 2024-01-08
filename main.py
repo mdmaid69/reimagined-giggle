c004a02ad742756198c6e2fa8b9fee430ba05f84
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:57:39 +0000

    explain process pull hello still

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
