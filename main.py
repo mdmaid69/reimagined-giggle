bf8633381f40d9ab668a1a93a320d6a5d0052c72
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:52:24 +0000

    approve mail dear mine please

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  def convert_to_binary(n):
+        return bin(n)
