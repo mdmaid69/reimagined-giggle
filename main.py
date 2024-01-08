de520429d28711d6ec179eeb06cd4b390866abcd
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:10:47 +0000

    slightly yet wide front action

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import sys
-def exit_program():
-        sys.exit()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
