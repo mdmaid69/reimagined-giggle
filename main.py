a117e744a3ab673e0f521f5925261d1210cb8fad
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:56:24 +0000

    then something injury address intend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import sys
+def exit_program():
+        sys.exit()
