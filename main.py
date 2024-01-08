97307d8e6704948335224afcd9fcc96a9bce22aa
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:03:50 +0000

    towards chat walk convincing appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
