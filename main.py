5c081ee9db1a2bb05d1541405d3ea39a1d939702
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:07:01 +0000

    miss pretty wash offend flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
