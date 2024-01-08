0cef2afb2f236148ec4b6f650fb3c6cc866c23dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:01:46 +0000

    mass this rough there wipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def remove_from_array(array, item):
+        array.remove(item)
