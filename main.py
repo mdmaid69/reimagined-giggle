17c4caf403bb2bb8126fb5ae8fef3a59fedf2c22
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:07:16 +0000

    ought last understand long sale

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
