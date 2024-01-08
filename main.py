cdbd66700982cfd69ff7dc77fd53a3c62037e2f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:44:09 +0000

    direct rubbish used country detail

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import random
+print(random.randint(0, 100))
