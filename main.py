d28366edbe9bacc54ac5ddbe099bf6414d6b4029
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:35:47 +0000

    just before gooff sky trust

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
