eb95ef6ea7f98aafdf24a6cb01cff92aaa361d4a
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:20:42 +0000

    difference early connection treat production

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import time
+print(time.time())
