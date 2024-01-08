7edb176035f0654679992c85ef74ae9b15989a65
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:23:36 +0000

    maximum choose release memory account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
