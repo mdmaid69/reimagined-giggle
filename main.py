b52194e557fc35a4e1f41d04a660ba856dcc7e6e
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:39:59 +0000

    interview low sure holdout search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
