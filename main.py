db76cd7a64dfa7e62b67cf3aa84f6c51a54abc78
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:37:40 +0000

    poison them low breakfast almost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import os
-print(os.getcwd())
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
