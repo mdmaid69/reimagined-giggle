4475aac07db144234f3b5eb5c2b4db3aea4b6caa
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:33:46 +0000

    operation fond shade kick future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_density(mass, volume):
+        return mass / volume
