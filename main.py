99850cd59e4f1d6003d805ab674b830b356a9259
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:44:42 +0000

    professional near cap mess tape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def find_unique_words(sentence):
+        return set(sentence.split())
