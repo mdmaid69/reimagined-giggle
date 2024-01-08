10f65fe9d654a1107d27f2c7378551f65a35c4b3
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:27:17 +0000

    relative steady everyone hot used

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
