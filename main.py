66dbabdb2a1cfc242169a39106f9dbda2c0b71ec
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:50:37 +0000

    suck advantage waste contents tired

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_speed(distance, time):
+        return distance / time
