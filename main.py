2f5a0d8763e1c95c7c8bb5632652d171780af3e9
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:30:07 +0000

    thankyou benefit thinkthrough equal holdup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def reverse_string(s):
+        return s[::-1]
