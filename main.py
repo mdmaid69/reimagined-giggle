4032b3d9ee360c1d28df97d7f5840131b2781d78
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:18:02 +0000

    communication closely open pocket convincing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import array
+def append_to_array(array, item):
+        array.append(item)
