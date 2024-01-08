9d2bae90ce7e26ff7aea5b508c5826121db3df05
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:01:20 +0000

    Mm ready rescue quality hide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
