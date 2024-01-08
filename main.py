3a5cbd4cabd1b2dd7b4f4d9717bbd8644707641e
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:56:47 +0000

    hit factory office back talkback

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def reverse_list(lst):
+        return lst[::-1]
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
