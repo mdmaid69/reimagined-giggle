cafc79a1eb51efed7edb4ea4415b2b45b02a6f6c
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:01:05 +0000

    admire advantage rush pair Cc

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
