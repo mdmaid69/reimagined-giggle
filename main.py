264a83fcd048da08fec8c735b915cb52cee37a35
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:24:07 +0000

    roll sport touch glad second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
