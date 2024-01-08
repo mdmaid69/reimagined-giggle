ff7bee509b65b94fa099e89f8c0e009869777d79
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:31:52 +0000

    weigh time leave load left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def reverse_list(lst):
+        return lst[::-1]
