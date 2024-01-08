9e077ac8d548b623b40b9c9d764607da7976581c
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:17:13 +0000

    pipe suggestion public turnoff education

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
