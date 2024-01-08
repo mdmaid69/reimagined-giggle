a174406a7b0cdce36962c82e7c0d6e90a69ab3f5
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:55:09 +0000

    back active face exaggerate post

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
