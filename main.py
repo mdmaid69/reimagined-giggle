b6e8f821e785903c15884e49b48fb63165cc6f86
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:12:59 +0000

    order steer division meeting whisper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
