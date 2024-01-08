817b8dfe71a6f41882eea0e95f55fdc731f39a4d
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:03:41 +0000

    another half transport harm influence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
