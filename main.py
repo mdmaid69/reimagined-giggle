ee2830f18fb99ac1e4e3b77f28bbd1ecdb47ed89
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:21:57 +0000

    tour total pull example workout

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
