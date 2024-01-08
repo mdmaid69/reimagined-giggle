1b3c531e2729a35cd8a9df93ac61ff2e14585909
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:27:50 +0000

    freeze this favour letter bringdown

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
