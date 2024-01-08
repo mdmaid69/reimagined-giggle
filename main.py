1fecd2839187f134d8e11bb9e47aa2dcef4eb59e
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:14:01 +0000

    father breakinto sort awake own

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
