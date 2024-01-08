eab882ea63f04c56bdcf3087e988ebc1230a229c
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:11:57 +0000

    blame demand now soil anyone

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
