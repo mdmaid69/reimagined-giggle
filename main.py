522f81b14011f42bc1e31e7279511eabd4375a20
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:47:47 +0000

    weekend benefit skin ago rank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
