9f9b3c32e04e4d1d6da1fa3555173f92596c5889
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:26:15 +0000

    watch increase pin reduction exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
