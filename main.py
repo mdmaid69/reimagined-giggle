8e9d94603e4b2d54855cd44deb1480c9e9e691f3
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:56:03 +0000

    570enjoyable climb pride lower breakthrough

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-name = "Python"
-print("Hello,", name)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
