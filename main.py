37d428877839d9b3eb0d86d5e6f82791b2337694
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:58:40 +0000

    wave time jump duty special

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def convert_to_binary(n):
+        return bin(n)
