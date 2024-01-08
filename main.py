660f30a926eaa51799f36aa009a11d4dff670360
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:16:48 +0000

    probably of pay still image

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def square_number(x):
-        return x**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_string_from_array(array):
+        return array.tobytes()
