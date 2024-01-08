4557e8d5f6e38dbb95785401a7edc195a8336ecb
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:32:47 +0000

    realize turnon waste positive steep

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
