2762b1cbc284cd171d3912dcff1af77c83136d47
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:14:55 +0000

    kill wherever pour along computer

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def multiply_numbers(x, y):
-        return x * y
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def reverse_string(s):
+        return s[::-1]
