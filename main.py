a0bcaa03080c3238ec76eea8d7a31179ed3f8f43
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:12:26 +0000

    turn case bringback fair everyone

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
