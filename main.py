2edb011a79f6c8253324f38a07a6fa07d6e7e917
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:49:00 +0000

    write lock switch wind rubbish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def subtract_numbers(x, y):
+        return x - y
