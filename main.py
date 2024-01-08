7bc2db101d7cf04b28b2b0ce09e44507a542cb60
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:11:41 +0000

    important joke shoulder emotion turnover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_sine(x):
+        return math.sin(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
