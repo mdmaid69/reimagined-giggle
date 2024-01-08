b26a93252c79a46f6f69a02189ce340a1989e3a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:49:56 +0000

    Ww mood activity week talk

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
