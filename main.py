3a80b4042d665323a3eb70b962dd932f99036955
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:18:13 +0000

    communicate movement minor example skin

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
