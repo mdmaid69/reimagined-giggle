be922e385d60a4224bd7f04639d2ad0fe037ccd4
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:14:25 +0000

    cough innocent on service measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
