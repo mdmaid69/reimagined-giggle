6a15e1f3160a24ed68f63c581322ec69a51b11f1
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:06:22 +0000

    want shop floor steer tongue

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
