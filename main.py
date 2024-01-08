ac51706415f7246cb3da6a129687c30acb5263db
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:46:51 +0000

    closely side time 569enjoy maintain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-import numpy as np
-print(np.array([1, 2, 3]))
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
