dc9f1d0dc4d9684868c409aebb515689dde11d72
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:49:55 +0000

    commit ocean operate issue 571enough

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def sort_list(lst):
-        return sorted(lst)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
