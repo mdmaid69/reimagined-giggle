e6c0cb7f481243e187e02b2d7d99e3612dfa0692
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:30:07 +0000

    charge 566end fond power thirsty

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
