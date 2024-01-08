ba36ed3e1b188bb31fe8946c9587dd6b459fb442
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:59:54 +0000

    month object damage keepup practise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
