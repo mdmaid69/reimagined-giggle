2aeac7fa954fbde93388425875c2070a5067d9ff
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:21:21 +0000

    part break phone emphasize point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import sys
-def print_python_version():
-        print(sys.version)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def square_number(x):
+        return x**2
