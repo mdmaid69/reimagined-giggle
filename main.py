8335fb2a80c2e6a663e3a55eb90a5e46b47a2040
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:54:58 +0000

    thorough cruel up hungry familiar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import array
+def get_array_typecode(array):
+        return array.typecode
