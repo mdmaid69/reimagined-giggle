87ebdc1b192f9f808aaf31bb7e56bf3fe42098c9
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:11:35 +0000

    operation later set road reaction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
