522c6246962880957993622347b46e49728acba1
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:40:45 +0000

    table characteristic get traffic hate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
