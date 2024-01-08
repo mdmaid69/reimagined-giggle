81045b33b1233f3a9e66222dfa828772af68cce8
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:57:15 +0000

    guard key instead age vanish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
