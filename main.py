279b9ef84465597269ec2db33b50d3c5ec29e1a8
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:02:00 +0000

    bottom recognize trick wherever breakup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
