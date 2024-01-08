4983af1182fd0b745ae748b5db1e9affa49ff969
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:49:35 +0000

    fall system cruelty compare before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_current_working_directory():
+        return os.getcwd()
