9e0c3c8c58e8e603e60ffc3c8cdc117dd72e2e89
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:13:08 +0000

    serious scream share morning organization

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def find_min(numbers):
+        return min(numbers)
