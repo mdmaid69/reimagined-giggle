ae2c108f7e230dae5f96fb106f440224003f75b5
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:59:33 +0000

    flat willing Ww trouble needle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def square_number(x):
-        return x**2
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+def calculate_density(mass, volume):
+        return mass / volume
