413da24a485afe5484b23d6c4d04390ceda090bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:31:41 +0000

    party fresh regular injury book

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
