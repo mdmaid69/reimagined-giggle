3a017c9735a4203de2e17acd6661ca46edd49723
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:16:31 +0000

    square habit picture mother welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
