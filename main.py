6e92fad3c25a6cb419ebb985d05c0dd3df60d40a
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:21:24 +0000

    moral fast forget general pot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
