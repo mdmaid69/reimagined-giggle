2c1d183c6de1a9315d97a866f753253fcc8043de
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:32:30 +0000

    turninto judge shame allow crash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
