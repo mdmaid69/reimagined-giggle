5c3ef81bb3567339cd1b58e2f4c19b8b1af09429
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:55:56 +0000

    store comeover takeoff bill move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
