7c322672c30950b68f3b5298f76e7275b38d1a35
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:49:39 +0000

    ear surprised concerned manage connect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
