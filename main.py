a98544da5eeb032b2996355e073aba9390bd81c3
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:59:39 +0000

    reduction fairly happy somebody completely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
