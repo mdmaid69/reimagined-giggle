32407ec46cf0ddc1d76a2b766bf79c4f8a79dccb
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:33:27 +0000

    manage concerned course gate anything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
