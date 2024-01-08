6e0ed31866b240a2c09951213e943cbbdaa4d61f
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:18:56 +0000

    mess rush brave close care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def remove_from_array(array, item):
+        array.remove(item)
