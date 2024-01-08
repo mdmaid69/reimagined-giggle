7603c12d56690ad187a2a16a418a07f3547ec9c8
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:34:07 +0000

    store meeting somehow sometimes deal

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_area(radius):
+        return 3.14 * radius * radius
