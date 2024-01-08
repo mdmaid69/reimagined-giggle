fc283feb92c316ea502ace1d5ad5af83d03c044e
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:24:40 +0000

    among thinkof exist tonight period

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
