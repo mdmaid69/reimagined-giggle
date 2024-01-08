3a919617b44b2dc334940285e81ef830084e73b3
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:36:12 +0000

    close protection serious discover tongue

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import sys
-def print_python_version():
-        return sys.version
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
