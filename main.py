898e22b18d0231bdb2b248d10d1549bebc7e2a73
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:14:50 +0000

    wave calloff work benefit touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
