d34f8a7e567e7431238dd97c9fd9a02ff457e772
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:10:18 +0000

    thinkof last chance calmly properly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
