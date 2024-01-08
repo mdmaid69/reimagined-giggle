d42f374559685e0e28951786515b555edfc864c9
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:03:49 +0000

    excuse left software brown giveup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
