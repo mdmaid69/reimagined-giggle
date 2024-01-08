b47e76c8cf7c78e4b34df6f049fefa0d00902a9f
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:15:42 +0000

    mean ignore pupil immediately children

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import os
+def remove_directory(path):
+        os.rmdir(path)
