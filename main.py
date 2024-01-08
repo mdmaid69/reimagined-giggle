7a8d54589a050edaafd4f6a87b6a1e4c1945ce80
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:12:13 +0000

    walk dry limit violence have

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
