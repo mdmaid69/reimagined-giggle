bf61c20336f64ea09d96a4b1d1748b86c03f4892
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:15:23 +0000

    whistle underneath talk ahead soak

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
