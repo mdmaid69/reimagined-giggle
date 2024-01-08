dc538cf28c02a2e3f4020d4be3212a03a21fe810
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:14:53 +0000

    report block wait meaning keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
