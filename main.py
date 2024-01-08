b70b4c093b5916048cde65ff6a1dff2b0747d161
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:06:57 +0000

    nurse fond drink least charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
