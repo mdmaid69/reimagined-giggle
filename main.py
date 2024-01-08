1719e8b66b09d7e5c6151e79681461a421f24439
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:58:57 +0000

    on turnaway of anywhere cheque

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
