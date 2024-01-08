c5d5f26e5dc48e94c0250a9e112c3914ddb0cbdf
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:46:27 +0000

    remain figure justice give cough

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
