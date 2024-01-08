91481b8653ea636ae3d8de4a1b94603dfd4146d5
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:29:04 +0000

    transport meanwhile respect rest embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
