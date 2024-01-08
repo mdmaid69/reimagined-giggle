d915ea2a9338bdf49862f18344d00f902f9f6f52
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:57:08 +0000

    support argue rate rise mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  def cube_number(x):
+        return x**3
