0b36d44002f307faa3923e9df6d8cabf31b20ac1
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:50:32 +0000

    cross difficulty tidy cover child

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  def add_numbers(x, y):
-        return x + y
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
