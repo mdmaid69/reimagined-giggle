5435c0b5f08afd035dce0e8514ed4be5c8ed6355
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:52:52 +0000

    hear want phone argue use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_density(mass, volume):
+        return mass / volume
