2dcd4583bd1def8c0f88976643efa31f4e41dd46
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:11:18 +0000

    general properly church heavy tomorrow

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
