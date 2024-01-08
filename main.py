192a89970ff900ad190ba2a8b0eee52df35617cd
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:03:41 +0000

    arrange should loss Rr brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import logging
+def log_message(message):
+        logging.info(message)
