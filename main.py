9fd17f8a60d4d60f7ce087d02395f74e2545c7b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:32:18 +0000

    light may study let chin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def convert_to_hex(n):
+        return hex(n)
