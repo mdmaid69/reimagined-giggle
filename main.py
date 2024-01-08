5f7de503dae01165a5f48c027b384fea22b50656
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:12:41 +0000

    matter large country position difficulty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
