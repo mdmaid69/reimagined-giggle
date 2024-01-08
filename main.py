8e8e70f912731ab01bd0c290d4c70a6b1e6670cf
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:08:15 +0000

    quiet law deep any suckme

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
