a54a3ab1a78fcbd3a77751ffe96c30bd1556e998
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:22:24 +0000

    delicate healthy spite smart against

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_speed(distance, time):
+        return distance / time
