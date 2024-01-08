d2ada44f63e63e2b8edf45c5d6d6355efb12b2e4
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:46:23 +0000

    change circle getoff organization please

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_power(work, time):
+        return work / time
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
