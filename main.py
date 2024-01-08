476cb4960064f284beb338d8cf0fc056ad161b95
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:16:32 +0000

    but study exactly search skill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
