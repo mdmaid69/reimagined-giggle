5eff01791feb71251af132ab96a8a2c8ffa9911a
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:15:42 +0000

    development rain everyone spend crazy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
