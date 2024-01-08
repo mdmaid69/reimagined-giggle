525df3e87e0bc0e8981f52797c27d8250e5c61d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:00:18 +0000

    guess wipe traffic zero object

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import tensorflow as tf
-print(tf.__version__)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
