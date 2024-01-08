95e0474552011b71a7a91b0c5cc04dc6d96a7360
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:25:06 +0000

    process bad too turnover onto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
