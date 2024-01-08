a1ade16a1ae9206a2c7d82b581630b44a1ea011f
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:24:03 +0000

    occasional sea keepaway avoid luck

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-print(os.getcwd())
+def sort_list(lst):
+        return sorted(lst)
+def calculate_speed(distance, time):
+        return distance / time
