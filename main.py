b0ea7b5691c68924dac36ba39ae607b60a7e1859
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:20:07 +0000

    breakfast play guide laugh trade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
