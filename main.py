8290b83ffe4fdd4effae58fa261696618878e46f
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:45:17 +0000

    shock matter news minimum touch

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print(sum(range(10)))
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
