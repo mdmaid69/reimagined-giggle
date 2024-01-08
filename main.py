673951eb775b33361314883b89aa10a4399a08b0
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:51:33 +0000

    collect meal workupto deliver Oo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
