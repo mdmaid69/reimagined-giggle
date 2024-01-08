c0cb29618148e3c5816ccb32e11b2964330a8499
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:16:58 +0000

    arrange conversation neat meaning acceptable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
