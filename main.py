9c4aaca8f404fffa9ba3cc3e2e622069bb434ceb
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:43:21 +0000

    evidence after concerned measure food

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
