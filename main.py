5d76ab17e6cddaf4c78e608980591d7b8f3fd8c9
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:54:15 +0000

    drink share ashamed smart makeinto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
