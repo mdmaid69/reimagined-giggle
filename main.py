0aca9d9d3df134f8bb2fb776ed762269053f960e
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:45:00 +0000

    afraid forever touch narrow steam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import collections
+def create_stack():
+        return collections.deque()
+import re
+print(re.match("h.*o", "hello world"))
