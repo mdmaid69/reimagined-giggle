50652c73605f9632abd36e30be7a3a7bc6918981
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:22:30 +0000

    Ll thick sign comfort OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
