a4732ec132e68cd090b0ec4206e9905cd435c3d6
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:30:49 +0000

    number protect introduction draw heart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
