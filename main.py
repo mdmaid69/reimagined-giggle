57706f05becfdd3a2672568585febcfed038eddf
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:59:20 +0000

    however hang accept achieve comeout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
