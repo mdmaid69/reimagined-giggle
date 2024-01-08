d7feaed46b8e79a1881efb2cc8e21ef62d96609f
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:12:57 +0000

    base this unlucky passage scare

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
