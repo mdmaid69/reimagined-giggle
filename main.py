286683eae3ad146de147b4326a13e6b4aa406b9b
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:44:43 +0000

    respect worth murder basic object

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
