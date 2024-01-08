3bac991bb159e257050f69e4a59faad930dd79e0
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:19:36 +0000

    crazy rest suffer report imagine

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
