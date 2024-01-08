ca409f934e7f873a968879e5ce25e0cf400b887b
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:23:29 +0000

    into set very miss he

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_array_to_string(array):
+        return array.tostring()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
