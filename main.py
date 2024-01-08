3a4316a3336a60eda5efdb0aa07a36144cc941b0
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:34:13 +0000

    battery present emergency at man

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-x = 10
-y = 20
-print("Sum:", x + y)
+  def convert_to_hex(n):
+        return hex(n)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
