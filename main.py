1fe4594a8746616b801f009a54ec5fe56567cd29
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:15:00 +0000

    bet where dear away quiet

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
