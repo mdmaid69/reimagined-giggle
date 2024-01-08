5391b4e7088556a0705f2ae909937032d8e20fcb
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:01:56 +0000

    hit enthusiastic behind 569enjoy indeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
