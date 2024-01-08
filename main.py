089bdccc18898e1bf849f6149964ad4cadaf1d5d
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:55:15 +0000

    activity giveback bother tap passage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def square_number(x):
+        return x**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
