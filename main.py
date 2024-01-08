197cf91bf82242e4fb8963b68a4149708c4eff44
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:44:23 +0000

    lend runover cheap Yy through

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def generate_random_number(start, end):
+        return random.randint(start, end)
