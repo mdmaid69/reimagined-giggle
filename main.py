f789e0fe8c860ba245adf3a0910d4837bf2a832f
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:47:09 +0000

    negative nor past shallow nurse

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import math
-print(math.pi)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
