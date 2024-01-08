79619e1f03dce51954d69586c5c3bc442a95dc79
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:30:42 +0000

    need hard this science bit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
