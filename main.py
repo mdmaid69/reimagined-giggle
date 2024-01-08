6a4f8e57d6335e96f07de0a87a139daa7b395504
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:18:55 +0000

    cost hang type sky Ss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import re
+print(re.match("h.*o", "hello world"))
