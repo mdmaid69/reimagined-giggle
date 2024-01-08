176495e8df53f599a2c470130e6165c746710685
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:38:04 +0000

    remark colour truth degree coat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
+  def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
