546e217f9c35894bcf5f4bd28afa1528e866df4f
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:37:03 +0000

    surprise already store method beautiful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_length(array):
+        return len(array)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
