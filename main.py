228a9a1221aba18010a0c08dc4d39680dd89e6e4
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:39:47 +0000

    especially either manner nurse community

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
