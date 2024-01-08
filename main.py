62bbc73697ebdac747b4945d1adfd952e48cd300
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:20:34 +0000

    book regret Pp strange favourite

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
