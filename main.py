7e99ca3f88f33ba9ee8ca2923c77ee578169e2ad
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:58:14 +0000

    clean police common threaten takeoff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
