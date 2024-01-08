e6b28863f1996bad26ddd0bef7663a480ec287f0
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:19:28 +0000

    only bank wire 179bite cough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
