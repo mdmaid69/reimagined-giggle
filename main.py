dad1af4160ed41e5b63ead71c4e3c9146e7f8b50
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:24:14 +0000

    frightened chat form morning 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
