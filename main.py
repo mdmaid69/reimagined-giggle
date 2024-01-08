c0afb5ddc7b8b1cb49166b1600e2c335772e6c0e
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:48:51 +0000

    Itself Qq trust fall accommodation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
