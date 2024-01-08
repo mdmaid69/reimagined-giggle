4dc8e09db7456ae61bce6a10c941995cee71c986
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:37:24 +0000

    repair forgive connect will push

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
