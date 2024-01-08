a32c1d5aadfb770f5b5bbd32cf0db3e5802d7ae2
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:11:56 +0000

    direct bound workon Nn hit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-print([x**2 for x in range(10)])
+def get_array_as_float(array):
+        return float(array[0])
