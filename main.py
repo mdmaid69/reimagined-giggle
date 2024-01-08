8be0ae387f076da75c89ff41f38ed4170ce7ff6c
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:14:44 +0000

    occasion holdup Vv square before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
