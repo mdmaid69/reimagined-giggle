fb4c1e5fb80b0665b989e56c0548fb279de37622
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:15:23 +0000

    necessary pause challenge indeed tooth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def count_elements(lst):
+        return len(lst)
