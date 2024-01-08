a09469382b7a10ef2ba6f7cc0b43291a17a40bf3
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:40:37 +0000

    table clean glad nonsense remember

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
