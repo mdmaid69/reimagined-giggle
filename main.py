dea089dd808c648e6c428886d014ab71fd25169b
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:42:54 +0000

    strong Yy sort friend flow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
