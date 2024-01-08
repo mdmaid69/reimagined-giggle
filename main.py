c76a6e228297b13c29486eb7ea886261111181e9
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:46:17 +0000

    office beautiful sudden least mass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
