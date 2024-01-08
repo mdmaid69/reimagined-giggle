40ada0e4f194cd609dbdeb15060b145bf90d960d
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:04:29 +0000

    life drawing innocent offer comedown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
