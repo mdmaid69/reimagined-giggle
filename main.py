3d1a91dc35e9d5923dedd75cad57a743bef81cfc
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:47:04 +0000

    discovery live keen possession heat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
