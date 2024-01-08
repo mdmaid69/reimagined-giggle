5f909d04dc7aba2676f6a2687a0eedc975a633c6
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:54:48 +0000

    spill news belong club bringdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
