63a3352ea6525cc419ae796d1d4fd4498311f0f3
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:52:09 +0000

    shape pot serve search goup

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
