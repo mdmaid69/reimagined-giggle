8845a32a2d77bb1fe5594883a434be0e8f06e62a
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:14:40 +0000

    whatever ago half experiment after

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
