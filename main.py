60cff7afcc5d81966934b024b10b80d81102cc6a
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:22:29 +0000

    circle everybody usually turnon official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
