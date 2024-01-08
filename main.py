958631eb43cfd975daf46d6caed19fff58c9475a
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:37:02 +0000

    bunch swim disapprove within nothing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+print([x**2 for x in range(10)])
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
