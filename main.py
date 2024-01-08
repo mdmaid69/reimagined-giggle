6d561f7c780583f2d21d4f3d5247189d23bf8716
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:34:06 +0000

    wear sky else file everything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
