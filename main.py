a84218fb4549a03604f355679da589dd06ccc9b9
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:17:33 +0000

    giveback murder regret fortunately present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
