275d09a7bd51edb3ec40fb6a70b5751a309f65da
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:01:01 +0000

    across 572enter although his somewhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
