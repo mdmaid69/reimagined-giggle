0a7339c59a63b6fe2a4045fc0c60604be1fa45d2
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:37:56 +0000

    minute mad make bag reject

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+text = "Hello, world!"
+print("Uppercase:", text.upper())
