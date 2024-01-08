60ecd10293dc47c2d06e43e3d9ee46e69f90b6f0
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:24:09 +0000

    just minor pure face market

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_area_circle(r):
+        return 3.14 * r**2
