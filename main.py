54c7d0c04fc3f4d9bc1061b344f827d584f10aae
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:51:57 +0000

    bread rank how breakfast that

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_power(work, time):
+        return work / time
