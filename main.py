c3ab841bcaeb3f34e6dfb9deef522c4df016bed8
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:34:40 +0000

    disgusting factory cover hour exam

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
