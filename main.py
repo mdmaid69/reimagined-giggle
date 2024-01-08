e10916c817976cbd97396984cd96656d33374dad
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:28:11 +0000

    bread charge difficulty complete protection

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
