64a1b6dd5b8a6170a2770b0a413412a80c8397d8
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:20:43 +0000

    most stop set nature step

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
