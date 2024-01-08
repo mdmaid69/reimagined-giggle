d6fe9464c1602291c7ed1af3a054abf4e9a72a6d
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:04:03 +0000

    divide attention create position hello

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def is_odd(n):
-        return n % 2 != 0
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
