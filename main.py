66fcef4d442b9e6bc7fe469e50181a3d945d9e4f
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:08:55 +0000

    father design vanish comeround little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_acceleration(speed, time):
+        return speed / time
