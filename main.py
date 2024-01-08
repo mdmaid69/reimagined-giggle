794b1566170ef8b0f0d22534a4dfb9d6ef119b1a
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:22:01 +0000

    level bet action wind crash

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+print([x**2 for x in range(10)])
+def calculate_work(force, distance):
+        return force * distance
