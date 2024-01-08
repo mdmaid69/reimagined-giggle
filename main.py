b8f39a9720aca9c0ae6a2bf8d753b75b261f6b62
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:55:16 +0000

    further callout goodbye stamp sing

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def convert_to_binary(n):
-        return bin(n)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
