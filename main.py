525d4778dc20868d9dda9b828ad04b39766ecd03
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:52:05 +0000

    disappear gentle glad spot swing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
