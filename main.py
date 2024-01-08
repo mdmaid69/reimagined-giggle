c695926f4c6c88809c44e35e09206325eb3ac12c
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:14:22 +0000

    respect decide science unlikely true

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
