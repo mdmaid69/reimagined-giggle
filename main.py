e39161525a28a56bc1c5838bd48f28bd1e10c581
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:52:24 +0000

    cruel guide fasten tell substance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
