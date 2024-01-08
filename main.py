ad2a5a46dd7639a8c7b546bd48080d82884e7f03
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:01:24 +0000

    exercise remove steer persuade protest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
