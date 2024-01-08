659103f21b4f21aee8bddde38308970159397b0b
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:52:33 +0000

    spin illness could sail though

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
