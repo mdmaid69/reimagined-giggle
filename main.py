e3bfa15e762c164e8a09563ccac708b28e685fe6
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:05:22 +0000

    company reply goback(to) basic worth

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import random
+def roll_die():
+        return random.randint(1, 6)
