8142b47952abd5b4fce095804e57a7e273f34df4
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:03:30 +0000

    communicate include teach kick steep

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-import collections
-def create_user_list():
-        return collections.UserList()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_force(mass, acceleration):
+        return mass * acceleration
