20cfca99467521f629ecb627c0e89ad6e4163436
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:02:41 +0000

    label comment thinkof software even

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+print(sum(range(10)))
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
