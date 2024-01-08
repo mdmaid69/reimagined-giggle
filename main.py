430df81380025f721e6f70725d2e4ebeda945031
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:04:39 +0000

    scene comeback announce moral invitation

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def get_array_as_bytes(array):
+        return bytes(array)
