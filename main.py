cb90f9ef73057d0d315bb9542bc43d28b65c94dd
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:49:51 +0000

    object many here increase write

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
