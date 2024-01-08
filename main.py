189148369ae28421bcd5d8c834a2e7a744e96d75
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:44:24 +0000

    left computer cool pot attach

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
