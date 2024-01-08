9c833d53bc08f38ac50f9a4895f3b6de330975bf
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:59:21 +0000

    judge night goon zero colour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
