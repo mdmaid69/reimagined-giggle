3676f00255124b6ff16aabeaa57afb5414af3505
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:44:59 +0000

    further voice worst pleasure Ww

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_min(lst):
-        return min(lst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
