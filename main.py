38992e0659bccfb17566e806eb4f953215df5e43
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:41:32 +0000

    impression shopping change feel climb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def sort_list(lst):
+        return sorted(lst)
