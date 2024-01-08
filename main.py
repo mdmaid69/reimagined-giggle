ebbeea0652fb81705439b9681fc1bd1d48bb6503
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:12:47 +0000

    watch without should deep discuss

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import time
+def get_time_since_epoch():
+        return time.time()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
