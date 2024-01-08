d1b80c256929a64ed36a02a9ced5062709097fea
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:56:17 +0000

    might through chat mark comeback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
