d633543e051cdec8a5428a3b365ff71cb6eece3e
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:06:09 +0000

    squash software press hour keepaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
