8179d24c773b33d919a13d252d5fb033b5b3785e
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:17:20 +0000

    runinto empty under shout spite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
