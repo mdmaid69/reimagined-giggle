3228652a29e00940c267e8a415226a3efdfe9095
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:26:06 +0000

    event clue most expression typical

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
