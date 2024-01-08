dae70be53d8b9d7fef859161012acce6a8577420
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:37:13 +0000

    closely block breakinto ball fly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
