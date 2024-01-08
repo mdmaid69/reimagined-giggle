e08becaebdcafba1f9184248453e16f329b3ab33
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:27:29 +0000

    breakfast Oo warn communicate speech

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
