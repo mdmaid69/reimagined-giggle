58b361c1746e6d31167458b0f32092fea362d651
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:48:17 +0000

    convinced fight figure attract machine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+from collections import Counter
+print(Counter("hello world"))
