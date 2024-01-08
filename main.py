4d004f7da3ff14470a71a26518f91010ec196e01
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:16:58 +0000

    contrast waste breakdown limit do

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
