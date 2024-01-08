a8d304af76090d979293b9a81c4e69f2d85dd4f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:47:49 +0000

    maximum threaten visit ignore who

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import sys
+def print_python_version():
+        return sys.version
