7b459e1f667aadca5e0523e259dde8a514b1c4f5
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:42:46 +0000

    back shine runover rank takeback

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+text = "Hello, world!"
+print("Reversed:", text[::-1])
