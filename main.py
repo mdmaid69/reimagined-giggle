59ff6fe49d1e1916ed6ea49df92369649ed77746
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:12:09 +0000

    card apply stranger conclusion tired

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
