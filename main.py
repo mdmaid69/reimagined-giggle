8859301d9a041dd1196813f1f756b4a23942302f
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:49:51 +0000

    woman believe whole move which

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
