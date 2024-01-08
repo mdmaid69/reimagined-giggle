07df5da20e449dc6d2279ae52d8159eeffffe80b
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:47:35 +0000

    freedom television top pen 568energy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
