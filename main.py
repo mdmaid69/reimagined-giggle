d076655ee4161ed33988574b8910ec4c8e2ec02d
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:04:14 +0000

    no voice unfair your official

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
