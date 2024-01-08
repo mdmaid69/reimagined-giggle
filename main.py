c6e14efbfe6899a3b3f455b29bf9d0af27918968
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:18:22 +0000

    world excuse ahead only best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
