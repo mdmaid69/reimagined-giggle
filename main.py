fe246c8a7c830d025506d74c2d9d84115eb197b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:05:48 +0000

    whenever rain own welcome lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
