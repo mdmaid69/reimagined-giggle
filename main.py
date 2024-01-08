734589bce0d2475d29ffdaf23b4879a9017d6191
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:01:09 +0000

    little break either coffee dress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  def cube_number(x):
+        return x**3
