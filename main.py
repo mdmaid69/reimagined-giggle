fec6223b3238d9dd74271d596137bf78ae55e92e
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:46:23 +0000

    pause call by temper roof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
