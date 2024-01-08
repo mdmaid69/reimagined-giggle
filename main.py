1414f07329448342b600794a7e35b9111c7897f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:01:51 +0000

    direction memory judge birth sweet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
