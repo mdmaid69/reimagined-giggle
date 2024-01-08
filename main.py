6e3b5197cccef3c36a133dfe385bb00d787e8214
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:33:41 +0000

    bright creature deliberately none design

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
