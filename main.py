9a066671a1f757e5224a098595ed299eff228642
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:32:17 +0000

    draw moment keep draw hole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
