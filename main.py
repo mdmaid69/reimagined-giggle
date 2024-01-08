766dbd93832a0932c694e497ffdfb52b69e50be5
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:14:11 +0000

    he decide anxious sort curious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
