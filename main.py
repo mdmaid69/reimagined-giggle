c51e94ed7301d4aa2e3a37e2145970b1e86fc2e8
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:09:30 +0000

    considering miss eat earth pattern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_speed(distance, time):
+        return distance / time
