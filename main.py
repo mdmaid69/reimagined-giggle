16dc35946248c5353a23a3bd67ac928715a38ddb
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:29:01 +0000

    tax water whistle handle love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_exponential(x):
+        return math.exp(x)
