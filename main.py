be58fec4e077935fda53b0a9eb548d97c7610238
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:39:27 +0000

    harm evidence temperature recognize keepaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
