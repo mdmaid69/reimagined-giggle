2e1bd87794b2e7f934117bbb57ac99b192c664c9
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:10:42 +0000

    show connected advise run interesting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
