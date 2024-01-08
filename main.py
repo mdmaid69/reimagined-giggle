f6c793d05851ebdf19b88909fb8a59256c1d6227
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:55:13 +0000

    later heart ask but dick

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def subtract_numbers(x, y):
+        return x - y
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
