e078da9701d7208d7d6920ae144243d76efe7fba
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:52:37 +0000

    second protect tour cope freedom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
