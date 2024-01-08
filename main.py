28e5a12fcce000b1c6eb033f9b903aa29ed9b1ad
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:46:08 +0000

    makeinto pattern fire row platform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import time
+def get_current_time():
+        return time.time()
