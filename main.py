c9a67f9bfe86340c2ff44d63d7c3a9ab271f0c21
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:28:45 +0000

    style provide drop Yy neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def reverse_list(lst):
+        return lst[::-1]
