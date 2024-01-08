124ffaebd05d6721747603540394a43c86e50649
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:40:56 +0000

    thing offend absolutely news character

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
