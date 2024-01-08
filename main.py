e1da5a5765936a37bcc37a5aafbac3791e67b347
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:57:34 +0000

    step sharp late wide plastic

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import json
+print(json.dumps({"name": "John", "age": 30}))
