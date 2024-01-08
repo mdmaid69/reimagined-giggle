185188a1ce109330134ac260f3531a792c902dbe
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:26:33 +0000

    guess Kk slow wait otherwise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
