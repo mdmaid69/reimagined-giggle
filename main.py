83d81a00727c4cbaf69a7e73cce392ca3f94fe4c
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:43:22 +0000

    pull home pleasant somebody such

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_user_dict():
+        return collections.UserDict()
