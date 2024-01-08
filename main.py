6fe1e9bed9a0184327a4d5f4034b4423b6dd82c6
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:04:32 +0000

    upwards declaration safe pattern congratulate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
