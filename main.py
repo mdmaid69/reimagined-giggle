1f577912df24d308021687d210007d6c589027c4
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:27:54 +0000

    would before neither finish sharp

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
