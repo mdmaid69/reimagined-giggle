2abd243aabae547ba8d79e03abebfd0c31b0f0c5
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:04:37 +0000

    hot dress choose yet Ll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def create_user_list():
+        return collections.UserList()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
