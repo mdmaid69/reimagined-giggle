b4d60b7cfad66f6aeaff0bfba8ad7b92926f2137
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:49:44 +0000

    manage everywhere shoot chin possibility

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
