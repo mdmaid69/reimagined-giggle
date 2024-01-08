49b4a7072df2a61471ac5bad85a1a6b5053c5e97
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:27:21 +0000

    friend protection own force pick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
