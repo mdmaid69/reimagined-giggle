b8d67a018f166fcced1196ac4bfd2a460cf37f12
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:24:04 +0000

    effect throw call coat part

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_power(work, time):
+        return work / time
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
