410aff2435a3ad7934da45c223f9b46d938a8f5c
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:24:33 +0000

    wire second maximum negative lookup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def get_array_as_set(array):
-        return set(array)
+name = "Python"
+print("Hello,", name)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
