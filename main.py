0566c3dc9147f9ba5632b4a9c5728d8426a5be1b
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:52:14 +0000

    straight shade among joke around

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def convert_to_binary(n):
-        return bin(n)
+  def split_path(path):
+        return os.path.split(path)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
