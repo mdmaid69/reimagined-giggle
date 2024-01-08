7526bdce55f9bc841c4ab9303e0b0f44efdae218
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:13:01 +0000

    disagree middle head Zz very

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
