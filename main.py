3d225718c8b403e0649445ac840ff4527ba36e48
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:34:37 +0000

    dance considering shoot vanish loud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
