962250bdaf12087672d20dd604fbd350ea6a2c34
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:23:12 +0000

    tomorrow takeout tap number rub

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
