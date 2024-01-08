73acdc0ab9dc3b1037fdf537e1f7586d1d1f1128
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:03:21 +0000

    walk talkback could bring moment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
