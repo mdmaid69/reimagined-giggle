100fec85bb8bcdcfbb410352eceead4997d935ac
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:55:09 +0000

    please flow eager successful black

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
