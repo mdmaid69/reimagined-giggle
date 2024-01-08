4569231df43be79f482db5e407e3c729ef1efda7
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:40:30 +0000

    scared point deep equal plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_pressure(force, area):
+        return force / area
