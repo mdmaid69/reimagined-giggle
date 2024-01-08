5f550c99c1b69993bdded5253a6b28e5eb4c69ad
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:14:46 +0000

    rely lookafter uponon listen juice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
