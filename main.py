a80c4ff075c6853d55b9c5940091f876db3b31c9
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:07:41 +0000

    public 569enjoy steady reason wander

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def square_number(x):
+        return x**2
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
