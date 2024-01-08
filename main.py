c04a886d7e1e69350e6c9e373c7913050ac067ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:16:17 +0000

    hold moral plan top because

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
