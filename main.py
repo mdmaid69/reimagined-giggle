4b1c7cd2372508053ca9ef17cb97b1cff3257273
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:01:09 +0000

    wave no repeat like home

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
