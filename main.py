897bb3fef890c7b5b511e7591549ffd67383e662
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:22:40 +0000

    list bang punishment before fork

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-for i in range(5):
-        print(i)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
