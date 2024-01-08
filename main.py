7dd0046e217aa5a690aeea8d29f68e9ce96fbbc9
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:33:41 +0000

    sudden dress either plan arrange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def set_array_item(array, i, item):
+        array[i] = item
