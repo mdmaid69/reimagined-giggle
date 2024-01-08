22fa2ad57d8f9065ab39550d036177304966bb28
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:17:04 +0000

    design avoid cheat country talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def reverse_array(array):
+        array.reverse()
