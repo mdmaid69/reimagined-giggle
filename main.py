122e5c3b3cc8805502d82b90493817676bce46f8
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:27:02 +0000

    cross now fit convenient genuine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def get_array_as_str(array):
+        return str(array)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
