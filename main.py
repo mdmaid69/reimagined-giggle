7c34e1345bd28c8f2fc41fcf4c73d98469c6a950
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:42:49 +0000

    able contents family habit under

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
