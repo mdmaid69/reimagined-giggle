b3db6bda477de5f947193182e46fd81bcfda6a49
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:32:02 +0000

    have she not pain reject

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def count_elements(lst):
-        return len(lst)
+def greet(name):
+        print(f"Hello, {name}!")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
