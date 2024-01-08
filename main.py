b2868c427b074a2427ccf42e36cb6d5717716dc5
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:03:47 +0000

    brush slice mail test coat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
