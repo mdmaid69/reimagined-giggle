a650b29ebb1eff8fb297a0edee6c38cd3ef9a3ec
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:19:15 +0000

    have meanwhile disagree square like

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
