d123daeda7782bd9e314772ccfe2b94365a383b1
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:22:25 +0000

    Hh me interview realize announce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+def sort_list(lst):
+        return sorted(lst)
