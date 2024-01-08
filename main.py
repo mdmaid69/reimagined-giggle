ecf7ba368884956c82afd8fa82169168093edb0c
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:50:47 +0000

    upwards fall in thought generous

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def greet(name):
+        print(f"Hello, {name}!")
