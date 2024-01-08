7813667276a48685f5d35823fc59a72271f21a14
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:40:18 +0000

    plan reach shame surprise game

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
