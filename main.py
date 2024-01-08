4359a9fe80a40d3a71eb66d1648078f5fb0c6bb4
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:30:03 +0000

    plate round warn touch back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def convert_to_binary(n):
+        return bin(n)
