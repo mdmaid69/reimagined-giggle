9726d154e092193ce2fc3a0fbd386e3cb881d00e
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:22:41 +0000

    underneath name aim talk make

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
