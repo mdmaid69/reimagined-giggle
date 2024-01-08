e1be50f5d1998f4ca71fd1c38f73c8f9a32cce6f
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:48:56 +0000

    feed circle ours comment decision

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
