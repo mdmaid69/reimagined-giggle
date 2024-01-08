d6185026190ba912846cc29f53c2a550191051b9
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:13:04 +0000

    teach calloff prevent remark tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_queue():
+        return collections.deque()
