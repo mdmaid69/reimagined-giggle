7c7ea1f521c573ab9cd4b2d5e03d1977074e4aea
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:12:58 +0000

    sometimes total recent boring face

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def reverse_list(lst):
+        return lst[::-1]
