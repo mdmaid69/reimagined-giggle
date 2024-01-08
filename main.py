64ffd9bf0f69df34bbccd77a82683c79afef84ac
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:33:37 +0000

    rent matter argument holdon belief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+text = "Hello, world!"
+print("Uppercase:", text.upper())
