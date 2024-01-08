36c211871f4b6a953f7b9aa33a2bfa5a29e5d811
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:31:56 +0000

    wash call traffic day lead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print([x**2 for x in range(10)])
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def square_number(x):
+        return x**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
