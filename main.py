bd2b49e9515d69bb940998591f08c57917ab39d1
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:11:58 +0000

    phone transparent border Hh might

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
