7cc304a1d8641cb6be47a55a529be2cc5d768273
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:39:34 +0000

    mind left old suffer beginning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_arc_tangent(x):
+        return math.atan(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
