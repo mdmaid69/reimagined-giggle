3f004cb2d656145e4679ad9cbd5bd75c1e3ff654
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:23:12 +0000

    later exist 570enjoyable dress loss

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
