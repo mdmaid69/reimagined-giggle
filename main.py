883aaddb7fffc3ec04522bf0dcd79bc52cc68b04
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:30:33 +0000

    Ll effort talk dry scare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
