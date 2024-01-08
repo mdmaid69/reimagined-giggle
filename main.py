b774bafbb8351c5fd22c490556b6e2b5869c6cc4
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:52:08 +0000

    notice have clean remove straight

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_work(force, distance):
+        return force * distance
