bc50be4877259f7b0c7a34ddfeff0fd5a847b1e2
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:15:07 +0000

    excited shine height move slow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_factorial(n):
+        return math.factorial(n)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
