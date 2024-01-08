3250c65121b5a719ae5325a7448d12940b8f20fc
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:55:31 +0000

    vote turndown workupto scrape hello

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import json
+print(json.dumps({"name": "John", "age": 30}))
