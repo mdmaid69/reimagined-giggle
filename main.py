044b31cb7cd6479649be4c088da481b0ad6cf11d
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:06:09 +0000

    separate child inquire possibly rule

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+for i in range(5):
+        print(i)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
