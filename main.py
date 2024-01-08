a2dec73898398d1cfc3edffdab87eeebd755d19f
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:22:09 +0000

    control scream time balance turn

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import time
+print(time.time())
