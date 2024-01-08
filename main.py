a1af72c7c34c2a289fb9f91a30b3693dc63b690c
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:12:29 +0000

    challenge comfortable every by brush

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
