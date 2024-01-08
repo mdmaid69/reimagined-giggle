5bc3b798ea91da70a894cb238584adc2df038d65
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:08:43 +0000

    accident march increase couple description

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
