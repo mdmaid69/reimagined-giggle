dc7f664f5784514a5f20a280e2fea2d571812630
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:37:51 +0000

    there now exciting market cruelty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def find_union(list1, list2):
+        return set(list1) | set(list2)
