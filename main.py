c8fc0ee9416e18c866594eb5579808e791ecacc4
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:06:45 +0000

    who hot yesterday board best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import sys
-def print_python_version():
-        print(sys.version)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
