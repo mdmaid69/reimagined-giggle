3de424f64eec5817f28d5ba148d869566eab7537
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:45:15 +0000

    free promise onto either blow

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def sort_numbers(numbers):
-        return sorted(numbers)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def greet(name):
+        print(f"Hello, {name}!")
