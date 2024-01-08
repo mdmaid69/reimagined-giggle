8a9cd95bd1e0f95e364e795b2467a52c15ca7071
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:26:10 +0000

    familiar top wear yes welcome

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import re
-print(re.match("h.*o", "hello world"))
+import platform
+def get_python_version():
+        return platform.python_version()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
