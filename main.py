879905bea0cc4cee52d7318cf3d1d4053136cca7
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:54:20 +0000

    band breakup different system better

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
