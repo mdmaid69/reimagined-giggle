fae73a27c0c68b13352c43f5cf70ea736d9cbd3e
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:31:57 +0000

    warn violence absolutely detail except

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
