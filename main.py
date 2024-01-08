06be99a54a29879a3b66ac01cbe94b8143321fe8
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:32:59 +0000

    relation fit sale lay regard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_cosine(x):
+        return math.cos(x)
