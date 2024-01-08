c9e8ef6a131da6153dbd200aedc1a4d034a3e4c3
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:46:44 +0000

    thankyou movement ordinary settle definite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
