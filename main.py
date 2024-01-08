f049ce18f7650aa20a61e7ac2a7bb4f6c5d5a943
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:28:17 +0000

    same responsibility gather Vv software

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
