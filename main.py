11305e3496f6c46a8fb705920f8664702e403975
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:59:39 +0000

    available direction squash crowded late

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_binary(n):
+        return bin(n)
