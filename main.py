8bb27a7c9814f498e4b5fea73f0436c2b9764cce
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:04:27 +0000

    far direct real push protest

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import platform
-def get_os_info():
-        return platform.uname()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
 import array
-def convert_string_to_array(string, typecode):
+def get_array_from_file(filename, typecode):
         a = array.array(typecode)
-        a.fromstring(string)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
