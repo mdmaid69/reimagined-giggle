c6127ab90836776bd0727ba1aa551c9f27ba86ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:09:34 +0000

    successful shoulder murder way miss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
