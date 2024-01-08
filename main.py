733cbc219c1432bfd4e02d26a5617432056f92aa
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:56:39 +0000

    must excellent close marry track

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
