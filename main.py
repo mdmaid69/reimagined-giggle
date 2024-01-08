4118c8d9de72a06bb1bde1f0d6149e4439e4b996
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:42:01 +0000

    thinkof smile sudden coat colour

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def square_number(x):
-        return x**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
