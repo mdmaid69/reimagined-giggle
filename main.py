fe6be6d9713744e92040c16a57e5e12f60b4a87f
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:25:52 +0000

    change game temperature climb permission

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_as_list(array):
+        return list(array)
