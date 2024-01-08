43f14c2c19696d8f054d504c0ea4312f7b6fddf8
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:53:33 +0000

    date back advice their shelter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import time
+print(time.time())
