d1fdcd048dff860e1dd11057444483d539bbec24
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:39:05 +0000

    ignore combination concentrate much clothes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
