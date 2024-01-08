9f457d19926a44e0596dde35a98038171f994fe5
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:29:55 +0000

    custom dry my his intelligent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import collections
-def create_counter():
-        return collections.Counter()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
