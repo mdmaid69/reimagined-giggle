2d8f4d1381d47a236e49586a7414bc4151a66b0c
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:46:55 +0000

    eye anxious search confuse base

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def convert_to_binary(n):
+        return bin(n)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
