d8587bdde6c26f5b1119b6080b9d86b3115066fb
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:36:06 +0000

    dance tidy Nn eager correct

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
