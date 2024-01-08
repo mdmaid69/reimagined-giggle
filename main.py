1ff9468426b827738e0090419377d71b9e9abace
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:49:25 +0000

    dry compete light pay underneath

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
