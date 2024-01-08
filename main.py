887481fe976fc2d74a12a9c601b8a425b4f37189
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:14:09 +0000

    tape bunch Dd uponon foot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sys
+def print_python_version():
+        return sys.version
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_tangent(x):
+        return math.tan(x)
