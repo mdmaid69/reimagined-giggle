e9ff38248fbcc73ba4ce51f240554a1206a3cdda
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:30:28 +0000

    wind transport tragedy Kk combine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
