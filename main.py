07a551b81ca5051460d92f4ed25cb15b5eb213ef
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:17:44 +0000

    public union average information float

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
