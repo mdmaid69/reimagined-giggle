7f131d04470a355c86f52cdc5e15284299c932bb
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:30:34 +0000

    meal concentrate circumstance store loose

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
