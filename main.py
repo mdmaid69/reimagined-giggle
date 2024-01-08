840fc8775ff379decb0c53c7592de3b1ef74be16
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:51:44 +0000

    serve 570enjoyable life release glad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_array_item(array, i):
+        return array[i]
