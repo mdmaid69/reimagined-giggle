da9a4c33cd966b74211a117e08a6502b80def1be
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:26:37 +0000

    runout special quiet runinto cut

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def is_odd(n):
+        return n % 2 != 0
