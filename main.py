261aa669125d26477ef234c34e11145549faca0d
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:25:59 +0000

    wrong instrument there somebody allow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
