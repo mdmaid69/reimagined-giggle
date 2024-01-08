7d5f595ed915a8da1a5f4647f8f92585e5e8918f
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:50:07 +0000

    remark fix failure instead judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
+for i in range(5):
+        print(i)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
