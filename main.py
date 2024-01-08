a81184981eacda6bf83366ebed44c6396d78b739
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:40:58 +0000

    small congratulations payment little man

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_floor(x):
+        return math.floor(x)
