e513ddaa8180de07d9d141bd6ce09752c8c63f7e
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:07:10 +0000

    nowhere relation discovery gooff who

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
