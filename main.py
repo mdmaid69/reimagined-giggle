76a9384150f8815329cdda69b087bb329454a4de
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:20:48 +0000

    happen business throw point tell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
