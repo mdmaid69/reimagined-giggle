d8f55d2cadfd50cf7b4e305754baef4941cd28e6
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:46:58 +0000

    conversation cause write twist believe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
