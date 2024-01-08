01b789a9a2473c9b29455f28703c8382c3a8a432
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:43:13 +0000

    generally else whatever compare choice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-print(sum(range(10)))
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
