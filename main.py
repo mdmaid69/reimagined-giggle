40fee41d6c7aaf74e9edae03d1fe6b0355316577
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:51:33 +0000

    load Tt petrol honest bang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_area_rectangle(l, w):
+        return l * w
