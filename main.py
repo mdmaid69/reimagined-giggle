17682a95383d45619538159b2cd4fed4d3bfa3f4
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:43:06 +0000

    recent autumn thinkthrough now book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_density(mass, volume):
-        return mass / volume
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
