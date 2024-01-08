62507cdc07a5ea05ff790b1fcb66eef783882947
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:22:59 +0000

    square exchange race benefit pump

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def convert_to_binary(n):
-        return bin(n)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def is_odd(n):
+        return n % 2 != 0
