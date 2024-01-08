3e344accee723c075409cf2aa208696859e2926b
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:30:41 +0000

    worry male weather time urgent

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def split_path(path):
+        return os.path.split(path)
