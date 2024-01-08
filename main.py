da9903232f3af7f409a15dcafcd616d550ac2666
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:32:37 +0000

    Bb only world scare post

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def convert_to_binary(n):
+        return bin(n)
