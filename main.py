adba23763325253d4025a521ab50567d66702851
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:38:43 +0000

    mess breathe closed hide old

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import os
+print(os.getcwd())
