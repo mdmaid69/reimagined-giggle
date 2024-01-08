c530a28e3f29d4a0be8d148133bcacfbea6abc73
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:49:55 +0000

    wing kill shall belong wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
