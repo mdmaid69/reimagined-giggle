234d54d09fe5b7842e3d4bddeb780066deb11894
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:11:58 +0000

    job plan catch himself tough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
