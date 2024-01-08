7031d29217b069fa0f7dd8c5220aeea2ca9f7f12
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:20:10 +0000

    birthday board ground dead evening

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
