2cdd96f8285dc0654940361b70f2061ce42f1cf6
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:41:22 +0000

    over tongue black claim machine

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
