e30514bd940eb6e55f87471e09a67c7aacddeb56
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:52:53 +0000

    discovery cheap declare shut lock

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
