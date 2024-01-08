c78247d3c5f52e732278efd5e4e9b0ce224ab3e0
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:24:20 +0000

    community moon trial bored meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_power(work, time):
+        return work / time
