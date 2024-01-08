90ebf04e672083b0005b66103acea66fbdaf0fd2
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:42:07 +0000

    action lost birthday level help

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def convert_to_hex(n):
+        return hex(n)
