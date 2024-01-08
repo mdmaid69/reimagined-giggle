522e05e170444de888dd6bf72ced9ca054967248
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:53:48 +0000

    thing grateful would her bury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import datetime
+def get_today_date():
+        return datetime.date.today()
