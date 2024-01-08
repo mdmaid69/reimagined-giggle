5afce14648e3aa7e336756a08dc207fc8405bb81
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:37:02 +0000

    home afford beside mainly bin

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
