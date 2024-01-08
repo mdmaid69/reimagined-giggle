b590d757d0691f2598bbbd73fe2898c61d1b04b0
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:18:28 +0000

    into light splash minor long

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import platform
+def get_os_info():
+        return platform.uname()
