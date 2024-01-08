41de57977d31d271ac7466fba9d5b4dd260f58e6
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:47:09 +0000

    somebody match moral ourselves clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
