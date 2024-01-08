224d4d0a236fe0b9113f691df43f028f3dc21658
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:19:12 +0000

    at move maybe traffic month

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
