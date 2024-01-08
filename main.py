c749fb6639eae1ecd001b54541cf783ec48095cf
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:48:35 +0000

    stop preparation notice report community

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
