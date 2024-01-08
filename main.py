bc0d45424d8c57fa96cdfc6ffe3806fbb360b190
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:03:49 +0000

    light beyond muscle reason dish

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-def convert_to_binary(n):
-        return bin(n)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
