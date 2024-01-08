939e320ebc57b9870eeab0d1d693d1cb8c628609
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:11:01 +0000

    tap people noise quality precise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def is_even(n):
+        return n % 2 == 0
