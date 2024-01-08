803224ebf0954434437c033d3fdac41472494cac
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:14:34 +0000

    direct confusing smile add patient

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
