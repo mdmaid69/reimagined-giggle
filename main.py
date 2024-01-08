f8663e7c8ca0dc97dd164d731fa8d13feea01f86
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:41:26 +0000

    shrink obvious inside breakin normal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
