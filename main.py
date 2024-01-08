2f8ac805b79abc053d085a474732f203a2f418da
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:12:22 +0000

    birth position or dream error

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
