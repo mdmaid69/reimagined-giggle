0da1ff1acb38d57213f251ad20a5f5f83d793c97
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:02:51 +0000

    today personal real close intelligent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def reverse_array(array):
+        array.reverse()
