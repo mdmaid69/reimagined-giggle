69dbd873ce150e1cd6e208e6993be3c1a2e1669d
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:11:36 +0000

    culture comeoff trousers declare knock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
