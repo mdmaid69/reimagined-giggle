7a5aa542b71215e9e1f9fc312c47c64dbf071d2f
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:00:31 +0000

    noone protection brave excellent violence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
