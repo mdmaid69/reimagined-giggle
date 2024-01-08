dfd45d9646ddf7f27997e0c5cf4598e31f85c554
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:05:17 +0000

    look suddenly sound operation very

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
 import csv
 with open("some.csv", "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerow(["spam", "eggs", "bacon", "ham"])
         writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
