748927a74b1e99097054b4c4688070e2a5d3b751
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:36:38 +0000

    instead event contain trap so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
