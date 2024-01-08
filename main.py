e59c8d6ef00642dce2f4ac5e1ac1686097b8435a
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:05:51 +0000

    pride cheek holdon press fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import tensorflow as tf
+print(tf.__version__)
+import array
+def get_array_as_list(array):
+        return list(array)
