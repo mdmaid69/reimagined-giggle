de46b3fa41d9f0eb38dcdaa8a43a139a2e586867
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:25:58 +0000

    increase paper pollution to apply

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
