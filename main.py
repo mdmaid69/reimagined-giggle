b89cf83a36d2dea50e3d945a933d419852fe9129
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:50:54 +0000

    blame reject dance truth comeout

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def subtract_numbers(x, y):
-        return x - y
-def is_odd(n):
-        return n % 2 != 0
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
