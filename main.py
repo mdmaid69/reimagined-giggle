739349659f6741941f9bd7f72d3a857d6bfbb5f3
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:52:22 +0000

    so team under cinema mention

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_acceleration(speed, time):
-        return speed / time
+import getpass
+def get_username():
+        return getpass.getuser()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
