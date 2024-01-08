b7e414d0405b3277b7a29d3b74f9b9fb0f1a7cdb
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:48:15 +0000

    only shelter opponent in lookup

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
