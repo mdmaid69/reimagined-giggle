9356c7453ac46076913c4e0f81ce889a85e18536
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:23:44 +0000

    thinkover unfortunate wave opinion luck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
