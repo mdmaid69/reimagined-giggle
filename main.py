71469402e62e6ea6ba3073650ddf7947d3cec004
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:51:31 +0000

    complaint fetch couple my both

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import getpass
 def get_password(prompt):
         return getpass.getpass(prompt)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
