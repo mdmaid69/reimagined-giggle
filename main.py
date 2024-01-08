04cd0f9213cee6b5983aaab07ae1af509cfdfde3
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:47:10 +0000

    branch behave equal deliberate complicated

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_power(work, time):
+        return work / time
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
