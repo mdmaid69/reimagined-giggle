cdd4f9c62aa2e6ac6fbc1667f100aae993d6fddc
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:48:57 +0000

    argument his he bringdown base

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+print(sum(range(10)))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
