450715ed05d5d492f9ec4c0755cac08c2efe3924
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:40:17 +0000

    pressure wear somewhere world cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
