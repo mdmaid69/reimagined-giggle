1af46591027b1949c88530d60ac43607885a9688
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:17:47 +0000

    ago smile turnaway broken community

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
