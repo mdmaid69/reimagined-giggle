d1a9f0b627b1707200676439e30a5781d50e7777
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:02:00 +0000

    rich it gather face careless

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_set(array):
+        return set(array)
