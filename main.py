fa75444e729e1ef455d57170b98cbbfdef3eeb33
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:04:27 +0000

    fall permission material own odd

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
