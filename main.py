f3cf13c6a652cd9095b865c119bca615f12cdeeb
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:57:49 +0000

    change loose born married press

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def square_number(x):
-        return x**2
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_typecode(array):
+        return array.typecode
