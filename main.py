3d5f4458242b1d40d446106b912d56d811700ae2
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:37:52 +0000

    responsibility bite meet position straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
