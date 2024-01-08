7b51e9cbcf184fa60014e40264606997d82afaed
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:53:10 +0000

    but moral show physical recent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def convert_to_binary(n):
+        return bin(n)
