d31d19331122dbce731629d8533f4a886bc57e45
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:07:42 +0000

    lookup but busy 573entertainment then

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import math
-def calculate_sine(x):
-        return math.sin(x)
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
