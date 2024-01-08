0dec5e9980a81dbbc006c1bfce329965d4309af5
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:55:37 +0000

    shallow substance platform discussion cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
