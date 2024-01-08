d084ad7023d54b53f60dd0f65fc09665b6c4213f
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:35:02 +0000

    circumstance noone hand claim as

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import getpass
+def get_username():
+        return getpass.getuser()
