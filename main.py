e2a8fde636e6e56fd40f272d0bdd1ae074d443fe
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:38:45 +0000

    ever accidental refer Ee be

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
