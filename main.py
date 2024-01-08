c16a06db90634874167c99fb9a3ab9389c47dcca
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:57:41 +0000

    patient music trade operate pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 def add_to_python_path(path):
         sys.path.append(path)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
