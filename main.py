5073beed6d151dd3ccfdae0269262363788a1464
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:20:36 +0000

    child bunch wet dig turnaway

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
