14ebb6fd6dce33556ed85d13bce748d4bcb69c45
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:53:05 +0000

    tear comment clothes cinema upset

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
