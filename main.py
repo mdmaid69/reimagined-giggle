9084bb521181e58365f4ac4f1646d12e13b2d715
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:07:12 +0000

    cause sense autumn always sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
