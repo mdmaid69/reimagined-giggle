48fc4e3e1e5be38ea35c2b73e098252e75a722ce
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:11:14 +0000

    have table length runaway necessary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
