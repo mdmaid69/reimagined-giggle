8bac0946e790e523a5f4cb117f072eb81f713075
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:32:35 +0000

    list behave doubt respect each

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import datetime
+def get_current_date():
+        return datetime.date.today()
