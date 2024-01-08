48ea3a7fe0ce97c9931be2bcd2a1a51dec5b9be1
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:19:12 +0000

    fit its smell call partner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
