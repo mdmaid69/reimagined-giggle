822f68223469a7ca936217681aac4fb1a10d9311
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:43:58 +0000

    scared tongue immediately afraid before

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_acceleration(speed, time):
+        return speed / time
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
