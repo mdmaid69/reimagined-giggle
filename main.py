6460c5904c4e7a8a32be6fc14b33a006840aafb8
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:57:38 +0000

    position wall minimum soon comment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
