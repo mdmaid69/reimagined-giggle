34b32a8efc50aa2a0d20974bc925a5b58a9b36ba
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:31:56 +0000

    stupid agreement busy skirt thanks

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_density(mass, volume):
+        return mass / volume
