27c09c5f1e84c62c7a342ace7dee77c86070622e
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:28:07 +0000

    perhaps error see approval Ss

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def count_elements(lst):
-        return len(lst)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
