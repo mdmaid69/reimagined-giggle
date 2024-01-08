43a0fc77d2b18ec3de2bde074cd26ff1bcf92b75
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:26:44 +0000

    off chase range goodbye runinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
