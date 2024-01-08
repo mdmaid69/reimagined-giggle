6dae9f56d76f9132ce21c199b3c9e9c16ff436f5
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:05:19 +0000

    around climb discovery add giveup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def subtract_numbers(x, y):
+        return x - y
