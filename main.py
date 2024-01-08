fefec435b89b72e2e5d6c89451c202862f6d5ab0
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:30:56 +0000

    spoil shock certainly like call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import random
+print(random.randint(0, 100))
