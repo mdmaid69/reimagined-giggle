b1fa93080cab3449b0a2da434970e3bb60f00fd8
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:00:44 +0000

    will frequent best translate blame

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import re
-print(re.match("h.*o", "hello world"))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
