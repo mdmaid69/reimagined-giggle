d5f69c1c7f682ca175e22ad3b7c1b5eb6daf637e
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:34:34 +0000

    recently reason thankyou increase reduce

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def cube_number(x):
+        return x**3
