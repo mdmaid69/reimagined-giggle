e2e81bfc60200100910043bbd3eb8d0855db6585
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:05:16 +0000

    fire around record number in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
