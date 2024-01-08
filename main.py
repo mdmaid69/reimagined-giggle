eeb02682869832fbe865bcca022a5579ad6b2c5f
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:38:18 +0000

    hand heavy enthusiastic back excellent

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+def find_max(numbers):
+        return max(numbers)
