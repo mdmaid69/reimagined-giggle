2387df74c04510fec34b3c067040d735ca24638f
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:02:56 +0000

    loss protest confused error must

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
