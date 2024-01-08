ccc8d1c91ebec4f69631d485fe4c79f55a2da799
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:34:21 +0000

    unless address ring statement morning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
