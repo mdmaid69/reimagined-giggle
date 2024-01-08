f19ee06ecb0fe287b47662df9a94412f9d1b455f
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:22:24 +0000

    rule feed spite grand judge

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_acceleration(speed, time):
+        return speed / time
