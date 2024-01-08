b266de6948ebfb05e26b843bb8d0ba4899e9425e
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:21:48 +0000

    set muscle themselves stick for

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def square_number(x):
+        return x**2
