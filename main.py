46f9c4cd30aff04e75ac43504afd3c5a068c4e96
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:07:26 +0000

    pure pleased power age thing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
