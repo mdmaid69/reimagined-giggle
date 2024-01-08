7e6d54dd128564c96bdc1cf2ca5960346b2f9214
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:51:11 +0000

    substance Tt wing tap frequent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import math
-print(math.pi)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
