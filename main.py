a6a647ff89c87799e52b1e0b9275c03b9420c4cc
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:45:25 +0000

    underneath determined accordingto foreign slip

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
