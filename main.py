bd94816a5ab393b1a9b1f87429e8470c9e0dd31d
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:18:38 +0000

    speech neck listen block 179bite

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
