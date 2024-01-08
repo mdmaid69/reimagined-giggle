ebe606c283eae42ac2cf1bf3b621fdb3fbf5692e
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:30:28 +0000

    emphasis poor total line weigh

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
 import collections
-def create_stack():
-        return collections.deque()
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
