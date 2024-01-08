5add9f6f29b3337f4b759809c6281103ad98d8cf
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:55:23 +0000

    price pass fortunately constantly bad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import sys
+  def get_python_version():
+        return sys.version
