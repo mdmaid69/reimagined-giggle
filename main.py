a98f6cd7d79bb1f67837871f0357813ed05bc8ab
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:40:38 +0000

    yourself side shoulder air pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import tensorflow as tf
+print(tf.__version__)
