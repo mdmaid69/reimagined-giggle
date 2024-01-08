b68924f2424e2ff5e48ace2b9ccec416131743c1
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:02:15 +0000

    snow realize vision house race

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+print(sys.version)
