0f6dd28cf340a8b9dcbba9a802be0acf331a34db
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:42:37 +0000

    bit either cheek last original

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
