7dfce13ada5e1e2ffec16bbb6d66d53ca24f4410
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:48:34 +0000

    sport touch bet plastic need

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
