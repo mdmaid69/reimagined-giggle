280b486f86b5220f720d285dfacef00e120bc05a
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:47:52 +0000

    soon declaration lucky easy promise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
