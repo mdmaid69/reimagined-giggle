2eb66f74d6e26431f6d0190a1ef64cfae9127f4e
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:13:32 +0000

    Jj Mm out ocean to

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
