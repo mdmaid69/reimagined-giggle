c459f9116752b53baa552c5a415136f58b0ecf50
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:49:06 +0000

    involve condition race against history

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
