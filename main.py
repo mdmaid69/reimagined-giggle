c72f3312d00b7dc73efc526b1a89819f35a278fa
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:47:09 +0000

    rich dangerous hurt physical much

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
