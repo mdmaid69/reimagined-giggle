d7b3f68459e8b77d854bdf50bbc18196990bca85
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:15:14 +0000

    shallow think choice 565end label

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def get_array_length(array):
+        return len(array)
