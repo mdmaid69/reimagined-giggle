03957f6269d72d1ad1f6c83ccef5c372fa55b87f
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:30:51 +0000

    mood emphasis increase example bus

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
