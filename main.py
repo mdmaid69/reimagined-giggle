9afd8704a7883e4f4878cd6ee0119a40f7072795
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:22:20 +0000

    body meet storm die each

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def find_min(numbers):
-        return min(numbers)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
