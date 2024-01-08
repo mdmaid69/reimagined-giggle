9edc68acac1875cd322ba6d966c3ffc626219521
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:54:12 +0000

    attempt some place ever worst

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
