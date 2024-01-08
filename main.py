aff6f32af1b349ec30d1ac0b422576daad02fdc4
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:49:27 +0000

    leave rate not concern situation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
