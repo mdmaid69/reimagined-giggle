9a1e2dc4b0423ac22ad8761733f30e0f9e853044
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:02:25 +0000

    relationship earn suddenly tired force

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def is_odd(n):
+        return n % 2 != 0
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
