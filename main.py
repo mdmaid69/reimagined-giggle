0822aed9c68eebc304dc7e13f42c1da101fa9b48
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:45:59 +0000

    remember where healthy exaggerate the

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
