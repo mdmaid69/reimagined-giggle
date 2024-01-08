7e8880206af1925560209e8e5ee3d1af5a55c20e
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:33:58 +0000

    message chance reduce breakfast breakup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
