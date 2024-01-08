2f560c9752d030a6079062051ce9192127a0b3ac
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:52:14 +0000

    experiment become fun ready note

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def convert_to_binary(n):
+        return bin(n)
+import random
+print(random.randint(0, 100))
