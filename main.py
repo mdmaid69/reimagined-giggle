bb78bc24a136e0c988119d7d4dde4552b2c74a35
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:45:13 +0000

    sentence crisis march admit term

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import time
+print(time.time())
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
