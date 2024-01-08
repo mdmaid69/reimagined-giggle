3fb1c49d1952fd5bc842e2f2b116c8e9da75615d
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:02:23 +0000

    sudden want represent OKorokay grow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
