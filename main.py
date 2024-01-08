5a8ece263267b5e868dde510b3dcbb2bb33d76cb
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:54:18 +0000

    underneath rather defeat cheap their

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
