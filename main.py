1aca0f704c23af53357d616c4114f3a1213fa5a1
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:11:55 +0000

    crash same high takeoff Kk

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
