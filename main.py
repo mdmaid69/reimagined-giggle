ea0f28ae8a1607d8b7410d10edfb8dafca51fbc7
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:40:00 +0000

    only bank scream majority late

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def get_current_time():
-        return time.time()
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import tensorflow as tf
+print(tf.__version__)
