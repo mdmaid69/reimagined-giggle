f573a3e9363610883db2e41478ab347cd264e58b
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:36:07 +0000

    wish neither contact load beat

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def sort_list(lst):
+        return sorted(lst)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
