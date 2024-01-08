26382701bdea650904044dc555a9e4c53848c952
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:39:54 +0000

    accidental supply sink hope appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
