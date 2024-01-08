6085e9198f509dbce29895b150aa90a4da41c8b9
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:25:04 +0000

    laugh themselves gas experience intelligent

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
