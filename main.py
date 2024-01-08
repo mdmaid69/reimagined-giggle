cb667c61f1258f9ce576af03baefa8ebba582835
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:02:18 +0000

    shock contribute wait distance comment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
