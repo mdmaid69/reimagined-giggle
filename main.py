a95892e8f77d8ffdf0a252f402c8ba96d753e246
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:53:07 +0000

    short freedom sleep there ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
