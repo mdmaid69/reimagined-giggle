91cf569a821db8ee65b5f49bc62e6dd3ce8ced82
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:59:56 +0000

    murder Ss shut block aim

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_speed(distance, time):
-        return distance / time
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
