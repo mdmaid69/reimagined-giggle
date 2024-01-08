5ea4789600dc1b1cba62f4799b52cf4447b274b3
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:14:48 +0000

    temper task refuse bringup view

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
