66b13093daadadd45b2e582d41d2d11b9c58c9ea
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:06:50 +0000

    if part trap lot tool

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def multiply_numbers(x, y):
+        return x * y
+def reverse_list(lst):
+        return lst[::-1]
