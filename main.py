50d9f28b0c78e329b74ae5f77be0ac4d9b9b4458
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:18:17 +0000

    dress bringforward drink best brave

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
