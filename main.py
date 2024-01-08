4002aec3e7984f009a2c913d90b7c6e5f495ce49
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:25:54 +0000

    brother friendly play aware sometimes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
