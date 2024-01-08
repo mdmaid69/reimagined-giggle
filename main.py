82438554c5a87ae6ba82888bb193c056d5e36ce7
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:25:04 +0000

    once thanks trial occur comeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
