49d3cc714b28bd13f31f6092e09e7ebf3bd3370d
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:37:12 +0000

    sore male only choice character

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
