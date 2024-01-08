60f47a59fa683598a7ab7d88703e78f2cf6ecb25
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:52:12 +0000

    572enter geton only either sport

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import time
+def get_current_time():
+        return time.time()
+def sort_numbers(numbers):
+        return sorted(numbers)
