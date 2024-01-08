c63aee53ae0903362691326396a854d15920861a
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:41:36 +0000

    loss near deliver clear fall

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
