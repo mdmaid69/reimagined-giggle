df1be4185ca382baadd610bdbf0bea48f282b170
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:43:00 +0000

    surprising report the emotion explanation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
