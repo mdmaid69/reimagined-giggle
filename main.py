d1a5e00465d96f759bb1007402de24ee5f142312
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:06:27 +0000

    let pity recent question dull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def convert_to_binary(n):
+        return bin(n)
+import array
+def reverse_array(array):
+        array.reverse()
