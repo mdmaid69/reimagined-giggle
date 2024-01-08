28b808168b9c0c01f65646fd774f991391c0ea9a
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:31:55 +0000

    obtain ask Ss decide normal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def change_working_directory(path):
+        os.chdir(path)
