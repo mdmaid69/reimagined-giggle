17742bb279912fd7666e690c0acee9399e00c67e
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:32:02 +0000

    few certain emotion yourself no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_tangent(x):
+        return math.tan(x)
