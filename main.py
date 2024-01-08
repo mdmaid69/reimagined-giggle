e292ec74acb85c63e7923ca13711839e7f1568bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:49:21 +0000

    greeting speed ourselves thank trick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
