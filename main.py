6ec65a8f4824884187fd1f495a935adea31be1be
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:28:02 +0000

    crazy opponent old zero needle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Characters:", len(text))
