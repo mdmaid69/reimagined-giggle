24a89f1a737f3724a41280164997e81e4108aa04
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:31:38 +0000

    pure priority impatient estimate rarely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import numpy as np
+print(np.array([1, 2, 3]))
