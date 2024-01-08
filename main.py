38a36d833cebcf21715e45f5f303d3d143e5e096
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:48:09 +0000

    live attract still turnover point

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
