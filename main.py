a036a0a0721cdd96ed30f7b72e7d5bfce9170f65
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:05:20 +0000

    fetch her lot slowly goup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
