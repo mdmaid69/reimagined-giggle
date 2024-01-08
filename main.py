7ca83050239d6be7febd4cbd86b2174e1d33fa09
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:47:35 +0000

    all art wake goout gradually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
