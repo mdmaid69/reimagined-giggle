6b3dc4ffcd70069b6345c6732d56f6367339a202
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:16:22 +0000

    outside cut mess medium fetch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
