08ab4676154632a3071a8bb69f710d673af8a928
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:04:18 +0000

    drop listenfor age brother truth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
