3315f25447e9efe090dd653711fb7918ec28492c
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:12:58 +0000

    which finger eat loose gift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
