37ddd6e78b1890501f9d96280e4e4ec2a39ef723
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:04:41 +0000

    sort finger possible calmly chat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
