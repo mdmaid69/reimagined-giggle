4bc537c33ad751c526a485322f9a161297b718cb
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:05:35 +0000

    brush necessary contain loose pretty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
