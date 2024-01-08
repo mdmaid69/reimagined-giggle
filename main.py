eacce1991bac05d9ffdf1507600793cff9614892
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:17:52 +0000

    mainly imagination worth adjective unpleasant

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
