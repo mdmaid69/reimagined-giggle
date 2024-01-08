d11b24f2280676a6515bbaf04001b3e8b452c60f
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:34:34 +0000

    night gift several satisfactory exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_itemsize(array):
+        return array.itemsize
