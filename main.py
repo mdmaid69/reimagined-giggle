5b2ccee0e483085101bc47f0fab54e993f840281
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:01:26 +0000

    getaway somehow gentle goup wonder

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
