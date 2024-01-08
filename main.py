18ef0382ac8e9987a6195ff77a1ff309abaeba7b
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:58:31 +0000

    plate successful lock band general

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_power(work, time):
-        return work / time
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import array
+def get_array_as_float(array):
+        return float(array[0])
