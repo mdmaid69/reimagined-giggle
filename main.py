3cfb6eb503895e74ae8ae10dcb47cc56b269b917
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:51:28 +0000

    front holdon official bread height

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
