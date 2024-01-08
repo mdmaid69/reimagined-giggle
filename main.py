2385419c2a05009ce8ac4bd6b2735d833e05e96f
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:03:49 +0000

    sail recognize separate mouth translate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def multiply_numbers(x, y):
+        return x * y
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
