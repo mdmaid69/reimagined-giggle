b3ad73c8783abcf2db855dfd0f16818cb86fa60f
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:01:15 +0000

    straight mistake worst treatment deserve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
