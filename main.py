4faa2d72abe356110762efc7ea76bda103612a76
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:17:58 +0000

    mess drop dig increase recognize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
