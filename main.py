6b211611bb326ca6448609407af03ca5e86e63ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:10:00 +0000

    food pupil dance friend creature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_power(work, time):
+        return work / time
