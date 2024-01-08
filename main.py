216b9867dcf058a32dc0ad18a70864d5c6b6434e
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:29:18 +0000

    opponent roll motion attract legal

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
