493a1f14e0bd6c2d17346d7d30c5be05d6f81288
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:09:00 +0000

    ours spoil feed will grade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  def square_number(x):
-        return x**2
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
