b168ab3573378e846d4e3108c69c5731be47e9eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:11:01 +0000

    sting laugh mass find combine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def find_min(numbers):
+        return min(numbers)
