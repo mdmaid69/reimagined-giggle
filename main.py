aeacb0ff68281b203c75d1bd174b850d6634900b
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:53:26 +0000

    provide well holdup contents real

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
