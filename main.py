8e459280cb5dfc7682cbc970e0ad863dd38d8d34
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:43:11 +0000

    pencil though square mystery grade

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+print(sum(range(10)))
