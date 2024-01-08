fe493a1a33d4c937db4c0d93a125ae928bb27717
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:43:40 +0000

    various kind invite true centre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
