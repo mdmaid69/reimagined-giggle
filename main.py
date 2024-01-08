3e99fe331c2bd46cb43ebc9643b173ba121b34c7
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:25:46 +0000

    right alone send free second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import collections
-def create_user_list():
-        return collections.UserList()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
