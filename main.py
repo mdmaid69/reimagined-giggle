fa0534463c87219e552bc29bf5ff4dd98aa3a5fa
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:02:52 +0000

    wide exaggerate idea us like

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
