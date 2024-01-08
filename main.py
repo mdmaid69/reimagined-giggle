25b0c9b7ab5bb70b38d3131f7b5070b97d7ef257
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:32:22 +0000

    push belief tight news prove

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def multiply_numbers(x, y):
+        return x * y
