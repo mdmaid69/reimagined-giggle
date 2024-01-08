49428ae07f54dcdf8ed1ed6fcee874ff7ec59f12
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:42:11 +0000

    hide perfect flight block neck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def remove_duplicates(lst):
+        return list(set(lst))
