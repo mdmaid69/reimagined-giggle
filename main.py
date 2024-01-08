d7f7c291a2df5494f1cb48f164e7f235faa34688
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:37:52 +0000

    pure wall try gradually switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import os
+def get_current_working_directory():
+        return os.getcwd()
