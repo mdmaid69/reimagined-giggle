722dcd5b8f5d768dc96e0f4530f94ce0be80f0b6
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:30:30 +0000

    moon towards image suppose under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
