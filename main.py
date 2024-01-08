8fa24ab60f5bf8c44df8830d60157318d1686221
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:50:30 +0000

    getaway weak natural occasional wild

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def remove_duplicates(lst):
+        return list(set(lst))
