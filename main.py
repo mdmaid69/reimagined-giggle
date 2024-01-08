a8349ee93fc72f3ea1aa3ced157262ee8c75befc
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:33:19 +0000

    fire within lead minimum lucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
