a188a0d13f79c163b14ea5aa646425ae87944bf6
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:25:15 +0000

    sorry hard plenty trade speak

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def subtract_numbers(x, y):
-        return x - y
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
