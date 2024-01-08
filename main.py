f4e7832cbe0480238ad4960ef6523c14e2004b42
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:35:09 +0000

    computer concentration spray Vv influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import time
-def get_current_time():
-        return time.ctime()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
