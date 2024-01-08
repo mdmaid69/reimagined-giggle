78d6e46131cf4dc870b5b6164b262bc1c8221483
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:22:20 +0000

    snow happy swallow pleasant play

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
