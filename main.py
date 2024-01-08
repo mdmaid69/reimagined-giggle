179a63b39039652c2c6d67600aec412d02917f32
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:31:16 +0000

    remark wipe please while turnout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import math
   def calculate_square_root(n):
         return math.sqrt(n)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
