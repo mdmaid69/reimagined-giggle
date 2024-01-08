021e1d390da330e753678833eb3a11d9f5b2224d
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:31:44 +0000

    tidy walk offend community prove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
