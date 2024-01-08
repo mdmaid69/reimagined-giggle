c1d6af170674ff96937a4d1890b6416938d4b414
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:56:37 +0000

    breakdown interview touch geton to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+def exit_program():
+        sys.exit()
