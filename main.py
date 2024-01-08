84ed81fe2c7826b0acdfeb4895ce3a5c11f99dec
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:37:32 +0000

    gift plenty before Qq wash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import platform
+def get_os_info():
+        return platform.uname()
