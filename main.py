c3bec613d0f56ff27640d41cfd120c68a8604dee
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:35:49 +0000

    unlikely thanks Oo stretch involve

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def find_max(lst):
+        return max(lst)
