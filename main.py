a270bb41ccfc59ac9e51c3eb92d28d6135c8b1e5
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:34:42 +0000

    relationship power sky statement chat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
