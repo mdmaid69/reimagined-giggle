6b48c99e8a6791b42c038ac6ab4d9b4d5ab244ba
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:45:52 +0000

    payment mend worried description mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def reverse_array(array):
+        array.reverse()
