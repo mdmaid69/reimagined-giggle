8225b8df37e36f46cc07028037d350617b7a4eaa
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:30:53 +0000

    laugh catch perform television best

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
