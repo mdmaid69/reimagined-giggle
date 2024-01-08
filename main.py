cf39b1fb4595e6272574e74c51d92dc614417923
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:22:20 +0000

    character rid test search exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def convert_to_octal(n):
+        return oct(n)
