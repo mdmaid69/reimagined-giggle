f45972424843c94ccbe589dabf4b38d8a616515d
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:26:19 +0000

    workupto year block undo second

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
