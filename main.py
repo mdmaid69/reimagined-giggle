50a91a1901e1638e74336939b456498de40670c9
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:59:42 +0000

    loose cost wish hair wrap

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def square_number(x):
-        return x**2
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import datetime
+print(datetime.datetime.now())
+import array
+def get_array_as_int(array):
+        return int(array[0])
