39bf184799c7133523080eb3a62276ba4fc66e4e
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:38:27 +0000

    backwards among goup battery sharp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def change_working_directory(path):
+        os.chdir(path)
