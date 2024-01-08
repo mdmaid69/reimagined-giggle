9548a54c6204e76623f49150e6fcc664172f5b98
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:19:07 +0000

    runout loss shout further I

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def square_number(x):
+        return x**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
