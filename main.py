9db0479d66664a59508951e914043c2476f5993c
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:24:45 +0000

    stiff rarely picture approve influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import array
+def get_array_as_bool(array):
+        return bool(array)
