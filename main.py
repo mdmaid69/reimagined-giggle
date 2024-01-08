97ec02a6fabec0ae2282e2472c42c096f2a40b59
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:05:48 +0000

    deep turnaway film runover temperature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
