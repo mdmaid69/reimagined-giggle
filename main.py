c41b2aa48cea6bc47e40f61ca5aeab837430bac9
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:43:15 +0000

    move open will mess speak

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
