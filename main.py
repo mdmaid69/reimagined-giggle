cb4ea2579d857afe4a2967fad9230cd26437aeba
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:01:10 +0000

    experience crowd pronounce wrong behind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+for i in range(5):
+        print(i)
