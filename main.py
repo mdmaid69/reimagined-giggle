1be331798a14b5fd9296204635da93770f299b62
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:38:59 +0000

    clever mark demand allow cum

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import sys
-print(sys.version)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
