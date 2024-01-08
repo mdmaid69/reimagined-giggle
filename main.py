a4ccb411a0d48cebd09938a38deefc9e899626da
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:10:38 +0000

    claim reason set giveback rely

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_work(force, distance):
+        return force * distance
+  def reverse_list(lst):
+        return lst[::-1]
