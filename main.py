5a9b6a980076e89c51643f5fcc17fe7fec31b648
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:17:18 +0000

    empty exist woman fly concentrate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
