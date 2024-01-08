f2d0d806ec68f38d2ee0644ea938a249e497e36d
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:15:44 +0000

    wide soon hear healthy absolutely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def add_numbers(x, y):
-        return x + y
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
