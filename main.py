1f19aab6b264d2ef1c5fe036c3609d30a94f5b59
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:43:30 +0000

    study software car pretty mystery

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def square_number(x):
+        return x**2
