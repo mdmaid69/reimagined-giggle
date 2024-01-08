f97b2570ef036abc29a2b68f898479caca61f070
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:22:01 +0000

    lie life lookback(on) number limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
