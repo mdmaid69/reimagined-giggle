3195118c3d295330d20b99e67a581ccb2edcbaab
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:39:37 +0000

    together effort spray adjective fat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
