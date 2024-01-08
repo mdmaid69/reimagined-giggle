1700e31dca1731717b6fcdcf7d9facf0c56ba76a
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:22:33 +0000

    moral class talkinto wheel already

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
