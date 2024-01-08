1fb42a0e95e69f1714690adf8fbe6faf778599fa
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:11:56 +0000

    quarter keen Ee imagine risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
