0369ef8b8a87fb43bb267628f04a65985e8377e3
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:39:30 +0000

    protection taste swing 571enough gentle

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import time
+def get_current_time():
+        return time.ctime()
