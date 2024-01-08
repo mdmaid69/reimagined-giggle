02a786be55aa8a48834ed19a0262979db5e4146c
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:45:39 +0000

    frame neither comedown respect circumstance

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import datetime
-print(datetime.datetime.now())
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+i = 0
+while i < 5:
+        print(i)
+        i += 1
