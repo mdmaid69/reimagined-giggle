023ded33d704c74aab24c2a29afebe0ea4c88cea
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:56:26 +0000

    start live shut event settle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_string_from_array(array):
+        return array.tobytes()
