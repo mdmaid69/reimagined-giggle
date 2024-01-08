de27b7cb2c5d6477b20591a614bb2928371d89ab
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:46:44 +0000

    sore plan party underneath cough

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_power(work, time):
-        return work / time
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
