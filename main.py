a7714141d5c78587e0766e821a13d4469905a338
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:46:24 +0000

    dream workon window soil trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
