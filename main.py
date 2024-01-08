2ff57978a2ed3e860166561de1c35948365b9042
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:45:15 +0000

    important takeaway behind attract rely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
