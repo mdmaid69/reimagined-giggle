d08f3839fd675157682ab81e28ef018688fc600b
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:15:44 +0000

    wire sister book question before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_list(array):
+        return list(array)
