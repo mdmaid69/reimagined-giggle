6fb29ae134286da45604ef31512f67d677416ca2
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:32:08 +0000

    already sense 568energy present solve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_repr(array):
+        return repr(array)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
