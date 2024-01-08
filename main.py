e5b2a1e0dfb3b9fd0dcbe0eb4822634625bcecb8
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:29:48 +0000

    possibly unlucky relax reasonable transparent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import os
+print(os.getcwd())
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
