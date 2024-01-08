6e985c28899208079f0de1d991f1eeadf37e2b3f
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:46:05 +0000

    at star listen likely situation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
