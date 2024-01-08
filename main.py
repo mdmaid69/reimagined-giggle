24a1c34f0df423775804c01b2457baf3542b570a
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:20:26 +0000

    determined breakdown breathe willing anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import platform
-def get_os_info():
-        return platform.uname()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
