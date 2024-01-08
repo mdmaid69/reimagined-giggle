9022782474d2dbeb609c3c4574720bade95cfa92
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:26:47 +0000

    upwards angry happen whistle generous

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_speed(distance, time):
+        return distance / time
