b018e64a4f733bc6109ec6e399f6b07a50ff50b4
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:26:50 +0000

    570enjoyable aware help abroad power

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
