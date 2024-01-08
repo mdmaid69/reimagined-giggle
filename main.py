6a81e9dab5678b2011bd8342cfd9bcc0376dcc94
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:13:51 +0000

    fork delicate over duty war

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def remove_duplicates(lst):
+        return list(set(lst))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
