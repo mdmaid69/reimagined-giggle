249018da873ec8a5567fd7a198d687141381a14c
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:56:40 +0000

    mother plain actual snow press

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
