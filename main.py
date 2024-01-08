4a6bac3919086e3e80a4794f737aa32330ae5a79
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:33:43 +0000

    Qq pool anything bag fun

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_work(force, distance):
-        return force * distance
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
