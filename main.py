342afa4265f697f98d36c676cf0b4d40cc1517ce
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:20:31 +0000

    bit teach buy beginning line

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import tensorflow as tf
+print(tf.__version__)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
