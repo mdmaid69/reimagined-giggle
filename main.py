263802a8f34ad70475c7b22e1414dafcd8aabcfa
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:32:13 +0000

    yet load hold generally cut

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
