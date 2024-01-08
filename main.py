5d0dbccaa8df624db07b0b96f1d87d37bb710883
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:56:34 +0000

    pinch region nervous ever stone

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-text = "Hello, world!"
-print("Characters:", len(text))
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
