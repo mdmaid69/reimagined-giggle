2f28d71c0421648567cf722fac5c806a514187a8
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:10:25 +0000

    throughout powerful keen suffer entry

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
