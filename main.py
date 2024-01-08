202a186fedd561611aeb60ce3340fd7418fc22be
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:05:47 +0000

    goout spin apart appeal scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
