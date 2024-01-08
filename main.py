977664494c3035c05ea1ea5d9df656dcd5cc0a9e
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:34:27 +0000

    comeout Mm bang prevent shade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
