30f4178802651cd1389e35d4777778f1d63ab692
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:44:29 +0000

    better every idea convinced control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
