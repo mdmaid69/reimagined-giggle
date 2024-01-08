50a7b258d6ce7f7e829a45f06630ee6b81b39396
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:10:31 +0000

    advise bend means suggestion sticky

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
