d817626ad143b1ef30958f2664c4cd87e2ba0e51
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:58:50 +0000

    breakinto stiff Uu style hang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_speed(distance, time):
+        return distance / time
