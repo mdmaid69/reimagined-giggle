37eeea1ce046fabb4e768468b0a9f1eaf2616a5e
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:27:58 +0000

    trip murder round position expensive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+text = "Hello, world!"
+print("Characters:", len(text))
