6dbd6be48dd738a5c88fb6836e540594d48d6e01
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:47:37 +0000

    aware debt concern free positive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
