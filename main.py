5be5e7bdfb28ae1adfad86c8e15565b723ed276f
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:28:45 +0000

    basic drop draw negative light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
