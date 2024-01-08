d2d3381e58fdf98d78ffdbca404d018202e6eaef
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:56:22 +0000

    smell cheap Yy grand tip

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
