bdf1d1948d22d8f03b67580df9624569b463342f
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:13:57 +0000

    workupto confidence accident commit light

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
