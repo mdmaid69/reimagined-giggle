58ce61f5a3a6ee00d60b3e5a6c273ca33b3c303e
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:02:07 +0000

    hate miss same poor start

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import math
+print(math.pi)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
