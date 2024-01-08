1928fbcb6d0bb8ed4481f3137102a368c7e0b774
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:11:20 +0000

    hide section meet ocean black

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  def is_odd(n):
-        return n % 2 != 0
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
