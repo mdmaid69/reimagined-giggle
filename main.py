49eee69c5694df4306d77277f1a4c4741d5e4d5e
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:17:37 +0000

    collection air bet company different

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
