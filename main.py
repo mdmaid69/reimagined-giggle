209d6d39f5e4a82adc2940d78ce682d9005bb4af
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:42:04 +0000

    language arm unkind post must

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+from collections import Counter
+print(Counter("hello world"))
