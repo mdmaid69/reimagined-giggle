e94533f6024a1c47a7f4e8a5fc017c7a0c5109af
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:21:15 +0000

    ticket pleasant capital unlikely about

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
