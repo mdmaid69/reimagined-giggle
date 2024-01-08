7fa446189b338d5c72e59c0be14cccb60d4b960b
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:25:31 +0000

    system shallow sorry used difficult

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
