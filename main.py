313a10e642eb395b6ad4cacf9f7bdf04efa810f9
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:51:26 +0000

    except count better cover phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
