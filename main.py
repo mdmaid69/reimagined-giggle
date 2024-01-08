677e7c1d75a76f74845add7dd34f22e82f0a4385
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:59:28 +0000

    not exciting attempt stage mark

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+print("Hello, world!")
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
