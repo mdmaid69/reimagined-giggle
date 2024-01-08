20d763005ef4e7458164e38364a7980bc9247589
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:22:22 +0000

    polite unit evening Ff second

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import time
-def get_current_time():
-        return time.ctime()
+import tensorflow as tf
+print(tf.__version__)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
