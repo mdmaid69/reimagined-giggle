85ea2f8c7257ff16a684a9dd6de04d9c6d5e9732
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:14:45 +0000

    send suppose number delight single

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
