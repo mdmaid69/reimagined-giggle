f2bbc9da8f04a6bbbe25d02b51643acf3fc69e8c
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:51:08 +0000

    string develop neat petrol present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
