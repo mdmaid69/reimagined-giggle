e66ffdaeb7a19c235314ef4a8bae5cd6a072c891
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:35:00 +0000

    pressure communication dick sky rest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
