78a9aa3bccc4895b5e1f3b7bde8aadc7c2aa798d
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:16:51 +0000

    takeaway why myself rule expensive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def get_bytes_from_array(array):
+        return array.tobytes()
