e9a070666c6b60cdb1455766e2eb5efe39ab5112
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:44:29 +0000

    keepup discovery mine convenient forgive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import collections
+def create_user_list():
+        return collections.UserList()
+import collections
+def create_user_dict():
+        return collections.UserDict()
