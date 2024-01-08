ea74f15cc494b938fea21202e9e6be95de81d90c
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:18:28 +0000

    involve cry crash speech underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
