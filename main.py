e8a95da7c3c8158646c80ee86aab6c60729b70a6
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:01:30 +0000

    label medium stretch final sky

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
