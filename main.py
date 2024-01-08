6870dc6d80b4aa4198ed5b29c337aabde3f9acdf
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:08:41 +0000

    join rent persuade speak basis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
