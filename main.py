ebbe301b35a186ec3e7209e04fb2e05e245b812a
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:11:30 +0000

    turnover undo fetch demand dead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
