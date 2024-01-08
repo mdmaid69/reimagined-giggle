2895323c7f86ce453d449ffc09805a624aee8322
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:19:45 +0000

    complete comefrom damage major impression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
