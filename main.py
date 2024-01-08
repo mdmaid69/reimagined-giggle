f697378966e8e08b79d5fda9e41f454cdb711800
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:10:57 +0000

    common tradition trick fortunately no

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
