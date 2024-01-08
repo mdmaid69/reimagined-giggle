0ae6a9d463a5c5acc0789e89e60ffc28e28ece95
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:52:43 +0000

    process confused goon bar comfort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import sys
-print(sys.version)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
