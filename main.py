b4c12c937e6efca76c97cd571891c9e8c51f7676
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:42:28 +0000

    dress habit interest modern machine

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
