51946b264a167549ba9563909fb0b014ef8c5ad1
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:15:42 +0000

    hate eat dear unusual on

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_work(force, distance):
-        return force * distance
+def sort_list(lst):
+        return sorted(lst)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
