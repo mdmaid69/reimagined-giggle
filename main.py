11cf95e6b1e7d1e07bb03c5208f18803dcd503d8
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:15:46 +0000

    balance for sound each doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
