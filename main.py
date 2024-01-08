a9d873fcc29278e20d7125ae4ee4bb96970987cd
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:49:23 +0000

    between few base dish book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def square_number(x):
+        return x**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
