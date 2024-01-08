f4d883311472b59a4a5448a54ab41e064149555b
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:27:34 +0000

    stop confused finish carry condition

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def cube_number(x):
+        return x**3
