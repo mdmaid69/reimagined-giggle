4ba2016778e262cccd124618b592d2e89bfa9c66
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:58:12 +0000

    cool cost repair share go

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
