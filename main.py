a9ca87c22b14596dda28dc64b03cb0e0c423672e
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:37:04 +0000

    fight rob black 565end mind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def is_even(n):
-        return n % 2 == 0
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
