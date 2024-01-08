28b1deea5ccb7483089539e1f76728b06c430b1a
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:59:17 +0000

    school imagination generous positive over

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
