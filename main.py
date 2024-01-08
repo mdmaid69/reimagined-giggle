83865bf53aed4bda3d350711321033eebe7b5d66
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:36:28 +0000

    fine move regularly convinced quick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
