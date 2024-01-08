fdfd1ef251286ac66a4dda45c0bd5c6c3d4a5764
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:39:42 +0000

    natural substance impressive tour description

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_blocks_allocated(file_name):
+  def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
