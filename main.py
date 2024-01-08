dac10ccd4f46a367babe5e3774f8dba1a535eada
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:29:09 +0000

    see thankyou black Jj suffer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
