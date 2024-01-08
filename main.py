87e9561e00baf384c980ec4aba9e47c5cd9a063d
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:55:14 +0000

    road callback responsibility declare advanced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
