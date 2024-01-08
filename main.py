0e55e9cbf95c56f35ec808161dd793285d741ff0
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:51:58 +0000

    dark ill whichever point favourite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
 def calculate_permutations(n, k):
         return math.perm(n, k)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
