a416f007db014c6d070647cedc71d8feee6ce5c1
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:46:34 +0000

    Uu than action sharp drop

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_density(mass, volume):
+        return mass / volume
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+print("Union:", set(list1) | set(list2))
