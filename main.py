6adcfd8b0bd3d7b1b63962d4315133a6a24b40ca
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:12:04 +0000

    further fasten mean book voice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
