79e2d9dd9956166c7d6738f1ee99d261627f6ede
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:49:21 +0000

    beneath mix generous fit ability

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
