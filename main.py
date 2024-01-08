74dbc22488741a777c0952bb3e815b21b27b2559
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:49:09 +0000

    workout turndown generous geton cross

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
