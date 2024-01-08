81bbc1824691610811d97ca7dc5fbbd97f37b0b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:21:03 +0000

    various involve true truth never

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import sys
+print(sys.version)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
