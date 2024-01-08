5c89d2a16ce9e939f3c91d94abf2da2ec7737551
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:46:35 +0000

    giveback easy leave waste dry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-print([x**2 for x in range(10)])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
