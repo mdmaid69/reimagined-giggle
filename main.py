92e905b669728bedf83c61a4861292a709b1e859
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:26:38 +0000

    advice piece determined turnoff frequent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
