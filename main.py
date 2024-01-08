27966030fa0b90cad3e63ffac0f7235b821e016b
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:53:29 +0000

    lean by lesson her wait

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
