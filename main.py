f72cc1840f1f8cf805da8658b1aec854631bb81a
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:54:22 +0000

    trial post how lead spot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def cube_number(x):
-        return x**3
-def find_min(lst):
-        return min(lst)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
