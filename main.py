a48fcd80c8cab3528875b02659d36ebf0e8de4bf
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:16:26 +0000

    damage flood print contact most

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
