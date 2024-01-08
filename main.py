f3e5581a9227db61af0e9cf35f75e66f40a7cb9c
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:17:32 +0000

    quick home win bend his

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def is_even(n):
+        return n % 2 == 0
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
