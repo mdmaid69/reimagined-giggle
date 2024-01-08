5c947c5f21e715cafc844ca68079b9c924c314ca
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:05:41 +0000

    pool considering near there extremely

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
