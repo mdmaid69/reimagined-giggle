f3ebb788a50f13f3f313f1a4d0a1f6d9ce10e970
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:01:09 +0000

    interrupt the answer science hold

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
