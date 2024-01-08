fc60158c7ee9e9c051905ddf12622a47ba519caf
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:48:43 +0000

    chance lower mark emergency female

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def convert_to_hex(n):
+        return hex(n)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
