260ca95e05ad4a59d8b670d1a7368b39856bd7c1
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:44:55 +0000

    buy bend please dangerous interrupt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
