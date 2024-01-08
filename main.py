eb200b7cbbd568c26b88553a2cd88952c903f911
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:40:23 +0000

    during game risk couple clearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
