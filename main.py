d42eaa605ecc3d35f2a235ab482474b4029bfb56
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:37:09 +0000

    welcome bet goaway tight corner

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
