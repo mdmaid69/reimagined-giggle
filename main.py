cf7f88a3e7bb95775d57641166ec1576604c7286
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:56:41 +0000

    usual path software hair hospital

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def remove_duplicates(lst):
+        return list(set(lst))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
