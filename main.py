1aae871830101c4ba37517146c5f6893761aec1f
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:33:08 +0000

    unkind react variety future load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
