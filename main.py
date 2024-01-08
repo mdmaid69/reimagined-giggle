e6fe0900c99113c8ea7daec532ebec4da1735a85
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:06:05 +0000

    occur study transparent steer test

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def square_number(x):
-        return x**2
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
