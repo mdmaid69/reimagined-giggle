fc28ef0968d9a0dcb8d7a509d4488687a623af3f
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:57:23 +0000

    image smell of excuse aim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import platform
+def get_python_version():
+        return platform.python_version()
