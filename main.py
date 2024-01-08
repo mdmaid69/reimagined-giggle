946b3e6f62b245cb68b10a9eaca6d1420dd2d093
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:22:34 +0000

    swim breathe seem right vanish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
