2a1bdb6aee23385d7dfd3776975402a7a12f3ab8
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:36:41 +0000

    getback look some yes rescue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import datetime
+def get_current_date():
+        return datetime.date.today()
