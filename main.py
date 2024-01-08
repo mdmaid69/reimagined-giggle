216138a1dfc0d7be2ca8058612ffd9590e30d5c0
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:18:12 +0000

    valuable judge whisper its person

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
