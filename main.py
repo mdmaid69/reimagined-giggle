8a4a0ca99d384876eff5b46633181e8087fb32a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:00:38 +0000

    lookup basic song tour talkback

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-for i in range(5):
-        print(i)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
