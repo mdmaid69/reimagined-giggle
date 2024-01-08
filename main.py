e74a3c61bd02188185c728f91d1399826639fbec
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:30:44 +0000

    close left search high rain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def is_even(n):
+        return n % 2 == 0
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
