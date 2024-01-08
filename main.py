a853f735a4604d11aedc35021b15e9f03ab54fd9
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:50:31 +0000

    cheque thought cry turninto either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_stack():
+        return collections.deque()
