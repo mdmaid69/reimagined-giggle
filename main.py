0d594ef314b1672709b2c6db735d57d73854f3ff
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:45:03 +0000

    set language deep hear tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
