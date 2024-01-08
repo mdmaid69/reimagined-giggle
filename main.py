8724818eff87939e7609279433da78155cffe76c
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:10:22 +0000

    insist jump build no base

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
