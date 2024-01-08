61735bf9be2044ff621020ce3fe46b3c70027652
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:09:30 +0000

    everybody mixture appeal machine suckme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
