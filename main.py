8a22e75c3ae969d49d9241f0f6019bdbbed0a0c0
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:51:01 +0000

    war sleep level nose post

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def reverse_list(lst):
-        return lst[::-1]
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
