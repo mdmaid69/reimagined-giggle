686d9cc7d2378b83050f1f993feee5895c38c6b1
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:38:06 +0000

    share kind professional interview measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
