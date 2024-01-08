6c47c8558147d210b60170fcda2f624c509f3fb3
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:24:13 +0000

    total shoulder hardly can share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
