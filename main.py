f277c9ebe1ada38c5f510fb7c442ab1eb856e916
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:08:53 +0000

    amount my top near bye

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def get_array_as_repr(array):
+        return repr(array)
