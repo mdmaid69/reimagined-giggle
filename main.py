431939aa33d0434c8afc547f7a8c198f15f6691e
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:54:22 +0000

    tough bring fun top rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_density(mass, volume):
+        return mass / volume
