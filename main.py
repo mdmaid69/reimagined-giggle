ef6603123766812642ef46bfcfaf6ed33dc125be
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:45:26 +0000

    name winter any organization bringforward

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 def calculate_profit_margin(revenue, cost):
         return (revenue - cost) / revenue
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
