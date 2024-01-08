bbb6cf560df392dfe2c5cfa0ab636dbb52b88654
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:29:42 +0000

    approve team Pp shame uponon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
