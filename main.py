ad419106c2b074a3cad4a08ae6eeb4f241fa8fd4
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:16:17 +0000

    brain comeback decide afraid block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def sort_numbers(numbers):
+        return sorted(numbers)
