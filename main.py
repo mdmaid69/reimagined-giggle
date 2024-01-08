954ab84d158003d299adad50775463cbcfa28136
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:43:34 +0000

    tread birthday concern loose season

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
