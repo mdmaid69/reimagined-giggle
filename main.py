8eaf4d30487267294c74a671b2bf1ed5ec0b963f
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:58:44 +0000

    bet smell somehow receive expensive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def convert_to_octal(n):
+        return oct(n)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
