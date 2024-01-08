7ee59d0c73a42a36e2a4dcbf9e44ec8afcac3e39
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:05:29 +0000

    change upset below reduction talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
