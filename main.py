41872657c28e8029baae0343611c5f997742ec7d
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:35:33 +0000

    capital workout dress Gg affect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
