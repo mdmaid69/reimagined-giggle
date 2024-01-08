a3319b46403d885e52e9b83ea2afe42107f79003
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:17:08 +0000

    demand character important tongue congratulations

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print("Hello, world!")
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
