b236bd3ce2fb4ce0bf5764d2732eb6bf4a6c339c
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:04:33 +0000

    slice sound crisis turndown law

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import array
-def get_array_as_float(array):
-        return float(array[0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import re
+print(re.match("h.*o", "hello world"))
