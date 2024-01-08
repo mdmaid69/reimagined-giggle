97aea21e6083b79901e3d6d3eac3b8281ab971da
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:33:20 +0000

    their trousers star ocean fire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
