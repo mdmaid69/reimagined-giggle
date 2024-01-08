fa1f62dd9ce90b3f3269c0bc1e460c07ea2f9821
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:40:41 +0000

    talkoutof notice run knowledge happen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
