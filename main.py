5358f8042808684b4c7105d63832bc8243230ba7
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:10:45 +0000

    again previous next rain raw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
 def append_to_array(array, item):
         array.append(item)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
