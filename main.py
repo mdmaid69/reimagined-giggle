742d3ae3d2f042bda697584dd37cea5824414e2d
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:10:36 +0000

    considering about comeback precise stick

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
