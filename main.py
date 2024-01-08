65a85faa0d93d1261d94c8899321ed67d36e5510
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:41:05 +0000

    among keepout activity sell sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
