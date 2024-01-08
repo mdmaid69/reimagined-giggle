feb2d4d687970e95acaf2b62fd3b970bda70f221
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:19:00 +0000

    comefrom understand gentle except issue

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
