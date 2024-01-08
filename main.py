048e37dd06b0ecfdb49579c470de0fc81c9b327d
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:27:46 +0000

    many field hear design cry

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
