85e9924f47c785cb0df2f8e3b7bf153b9ea20e9f
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:15:39 +0000

    memory talkoutof it better criticism

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import os
+print(os.getcwd())
+import tensorflow as tf
+print(tf.__version__)
