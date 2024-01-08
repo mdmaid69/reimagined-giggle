ef867364900797f5389d2149cabd5bf5752a83cf
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:15:33 +0000

    cool belong wave hope somebody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
