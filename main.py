e70ea554b487bbd26bb4a81820a12b2faf1f947e
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:50:27 +0000

    table tongue please idea damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
