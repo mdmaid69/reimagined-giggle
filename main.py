c66daca47338f7a36d3cd6fa4ba205f29e4adf0f
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:50:58 +0000

    circumstance hand exam common late

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
