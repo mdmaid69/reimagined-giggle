47362d310f855335f49d7f8ff978eeb2aca2ebd0
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:51:23 +0000

    swallow large yes under aware

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
