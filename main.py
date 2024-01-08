bc54869b2d6655b952c0b703f33fd9dbe57fddf8
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:59:57 +0000

    considering still on either face

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
