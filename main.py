71ac613c78642935f21eee18dff26956f3267ad7
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:59:31 +0000

    demonstrate and usual profit callout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def square_number(x):
+        return x**2
