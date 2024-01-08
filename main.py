2be016eae1f8c1d1a9211938d2e05d6381ce1c07
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:17:41 +0000

    contents bury shelter onto car

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def add_numbers(a, b):
+        return a + b
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
