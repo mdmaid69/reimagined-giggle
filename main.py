eb82a01b5db6fa00acfdf426906005c784c34215
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:56:07 +0000

    run circumstance point bottom often

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
