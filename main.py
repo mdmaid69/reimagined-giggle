c736e9de87aa2d66518a6bc47c0b1040b4fced10
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:58:13 +0000

    several among ring together sorry

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def add_numbers(x, y):
+        return x + y
