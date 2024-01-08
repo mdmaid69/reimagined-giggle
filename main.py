a51dc0542dabf55ecae7ca76e09df15b5d2a3290
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:04:10 +0000

    mouth Bb disagree intelligent brush

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import platform
+def get_python_version():
+        return platform.python_version()
