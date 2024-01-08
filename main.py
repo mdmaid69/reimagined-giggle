c17cfc86a949c06f04041173eb788038d714f4b0
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:46:14 +0000

    not gooff soon fail lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
