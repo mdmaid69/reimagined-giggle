53f46006eeada99482a1ba79a85ba1b900dac98b
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:50:36 +0000

    trap regional by case use

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def count_elements(lst):
+        return len(lst)
+def calculate_density(mass, volume):
+        return mass / volume
