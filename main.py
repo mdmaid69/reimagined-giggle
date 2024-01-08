e93ea67636e4a12263551754dcedcd3a20a47e0d
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:59:12 +0000

    impressed project meet light still

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
