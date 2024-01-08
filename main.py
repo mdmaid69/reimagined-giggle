426cdc033d4d552eb2a7626bcfec9416f3547e1a
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:40:20 +0000

    cheap wrap fast downstairs true

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
