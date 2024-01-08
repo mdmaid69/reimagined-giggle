575297320e5a976e507e871415b45be355121ba7
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:47:52 +0000

    bringback emergency fasten university together

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
