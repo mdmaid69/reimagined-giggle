9a0bdd598aee4971e046edd06eba93026187d9d4
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:49:05 +0000

    murder rest cheque earn history

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_area_circle(r):
+        return 3.14 * r**2
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+print(math.pi)
