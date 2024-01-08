75a2efb086892aee008523021a94ec07903a9c14
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:42:49 +0000

    circumstance exercise Oo equipment control

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
