451ddcce26d7c72a72657a9b7e821adc498b4afd
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:47:45 +0000

    right entrance boat persuade protest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
