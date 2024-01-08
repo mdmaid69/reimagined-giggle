1adb17a610eec4f7771f8bf3a3ea691fd4368310
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:19:22 +0000

    plain prove like occasional slice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
