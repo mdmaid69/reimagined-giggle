57f3e6a6ff8fbb6b0f3e460e00a03939eb6a83b2
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:16:22 +0000

    perfect gentle port always town

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
