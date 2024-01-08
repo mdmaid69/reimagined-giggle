669e507c6e2339d82db1878c1822487b334d33a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:17:54 +0000

    instruction tidy land how hurry

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import time
+print(time.time())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
