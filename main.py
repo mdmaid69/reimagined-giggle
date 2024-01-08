e56f82f95e0fb2e6779a38ea78c2320ecfb5da4e
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:44:10 +0000

    copy something never Mm smoke

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def find_unique_words(sentence):
+        return set(sentence.split())
