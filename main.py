576d69251d4c0498870053d3fc27a8659cdddfe1
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:15:17 +0000

    measure desperate politics pride frightened

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
