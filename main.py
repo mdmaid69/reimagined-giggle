256316e23c2a9b40b92dc9ad7e537e8211d44c39
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:29:38 +0000

    scheme nose table flood comeup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
