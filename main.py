fbea96edfb17688e998c52820846115bca6c5a64
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:14:32 +0000

    takeout refer excuse guess payment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_factorial(n):
+        return math.factorial(n)
