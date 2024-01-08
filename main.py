96dfce06afd2b92fb64ae109442e980e9dfd1f30
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:04:51 +0000

    scheme goaway lookback(on) anywayanyhow fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
