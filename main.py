d3e7635839b1b604726637c3062769726f1fcbd2
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:18:04 +0000

    neither complete remember married return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
