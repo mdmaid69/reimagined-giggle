ffd99bfca788eb261a2a07ed7c66f92707d42cc1
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:02:38 +0000

    half up order aim dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
