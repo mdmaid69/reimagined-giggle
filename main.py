0677e44df2450c4c55adba5a50970ea54188186b
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:43:13 +0000

    pleasure music beautiful contact expect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
