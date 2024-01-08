fb9aa0119af6edbecb31a033d97c1c27a1d34e91
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:59:15 +0000

    peace win special bread turnover

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
