3cad804784479dd7d51705533d709edecd164d5c
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:10:21 +0000

    rush artificial black grand group

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
