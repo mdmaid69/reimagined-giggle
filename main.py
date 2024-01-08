fe7262b4221a22a830c2e28635374bf68aab7c5e
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:12:00 +0000

    along deserve immediately feel introduction

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import sys
-print(sys.version)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
