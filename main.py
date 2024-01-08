a7bdf3dab115758b9223bf9bf7c0b2ddfba6c039
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:34:53 +0000

    violence dark idea dream corner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_octal(n):
+        return oct(n)
