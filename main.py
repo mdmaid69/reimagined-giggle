6dfba49da8437518efc2b7d115458debc8fac95c
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:37:26 +0000

    treat level cause right famous

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
