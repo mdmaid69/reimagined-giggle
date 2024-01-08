86d157b1b06850285f4841e21873eb1fae400258
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:31:51 +0000

    course manner mother condition bringforward

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
