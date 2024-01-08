84335b2d0b8e8804b14911b00d6de4ae0300b2e0
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:08:31 +0000

    path advantage sensitive miss mother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
