cbe74c983cf1aa8c97af7ed8365f66ce867cd539
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:41:44 +0000

    press release pin cut breath

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import sys
+def exit_program():
+        sys.exit()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
