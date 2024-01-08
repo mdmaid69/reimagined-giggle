13987052bdcd5c18cae7a206f6d3a27293b3b4c2
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:56:32 +0000

    generous eager belong somebody risk

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import collections
-def create_stack():
-        return collections.deque()
+  def convert_to_hex(n):
+        return hex(n)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
