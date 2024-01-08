c77e2bbf1a94d25eaa441832e93365ef43d1c424
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:28:28 +0000

    warm forever discovery pair out

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def convert_to_binary(n):
+        return bin(n)
 import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def get_today_date():
+        return datetime.date.today()
