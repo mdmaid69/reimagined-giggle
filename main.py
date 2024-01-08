e8bce1c030434c8c79920ba671a76d36e68ead68
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:08:53 +0000

    worried cheek undo uniform worse

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
