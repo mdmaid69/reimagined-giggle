6560ed2b01246422abf6f0ad48216a479965ab63
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:15:39 +0000

    length free custom woman store

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def cube_number(x):
+        return x**3
