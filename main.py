23052792dd25b2819e302732d9cb5371f08d99b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:22:24 +0000

    control different morning state breakinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def greet(name):
-        print(f"Hello, {name}!")
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
