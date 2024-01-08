f558eb8e1296f62baaeae8abdc46ac6b6c48b9de
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:26:08 +0000

    issue bunch care politics meet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def convert_to_binary(n):
+        return bin(n)
