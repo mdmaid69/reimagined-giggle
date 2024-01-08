d1ccb3a12aa9636f26904eababed5ef108b56a31
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:03:43 +0000

    importance just provide hour bit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
