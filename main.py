b3c25eb81ae60cb89b1d4271b0d932296a68da87
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:06:00 +0000

    forget crowd slice callup makeinto

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import tensorflow as tf
+print(tf.__version__)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
