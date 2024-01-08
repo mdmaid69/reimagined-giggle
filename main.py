4562a84b2859d789b6ab0c87bbfd6b20f86cbd80
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:32:24 +0000

    exercise available dead little ashamed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import datetime
+print(datetime.datetime.now())
