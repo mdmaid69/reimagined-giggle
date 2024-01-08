08770a73852ee2e19f2b2afedc3e6307c5b7c656
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:52:55 +0000

    Kk steep cross count natural

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
