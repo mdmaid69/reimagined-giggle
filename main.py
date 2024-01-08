48e86650a3f9d7b90ad0590dc78c84fe8c3fc040
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:19:11 +0000

    influence add rate turnback party

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+print(json.dumps({"name": "John", "age": 30}))
