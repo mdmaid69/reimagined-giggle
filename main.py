5e30dba7253a62b816bbcc653588f20b698337dd
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:00:39 +0000

    goout report lose rubbish print

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_area_circle(r):
+        return 3.14 * r**2
