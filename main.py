ef5266fc807af88f188e16bc1df5961f91c1e064
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:34:08 +0000

    any rely traffic performance do

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
