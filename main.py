450d4a3cf3d037b4394206d8f51faac398956f0f
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:28:18 +0000

    stiff satisfactory force handle report

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
