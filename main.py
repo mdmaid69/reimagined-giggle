30bc26b1dabff437cf3ae563e51541f84d732934
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:55:06 +0000

    absolutely fasten impossible weak remain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
