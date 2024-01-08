2ab4a67e7d754ab30fedde047c346b2caa032854
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:02:43 +0000

    its trade persuade hurt whole

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
