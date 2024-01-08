4c4d3fdc9603e2a990ccfff1d013ee42567eab72
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:52:02 +0000

    possibly other do why snow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_unique_words(sentence):
+        return set(sentence.split())
