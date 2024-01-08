158d9c0b0ff66885904217febe691caf7977a140
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:43:57 +0000

    pile Rr almost spend skin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
