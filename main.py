91d14e5e8ad9aab077db068b2e03a23abf194e37
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:58:47 +0000

    goaway glad shut disturb neck

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
