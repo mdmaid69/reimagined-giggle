0ee35c9c26a15cbd67463427083824087c17c449
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:37:38 +0000

    gate available off who sick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
