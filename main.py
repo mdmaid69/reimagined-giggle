cd7000b31f40b975bef5eda07bc9caaf35354541
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:17:54 +0000

    floor ago Oo boil fill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
