23e866ab0430948f1fbbcfe0315a284a29905313
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:59:51 +0000

    doubt rent whistle angry forever

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def reverse_array(array):
+        array.reverse()
+import math
+def calculate_floor(x):
+        return math.floor(x)
