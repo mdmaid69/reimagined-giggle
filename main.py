2dc2f945d60fd5eda79f7b5bc76e331a7954d7c8
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:36:53 +0000

    vision money switch crisis rich

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print(time.time())
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def append_to_array(array, item):
+        array.append(item)
