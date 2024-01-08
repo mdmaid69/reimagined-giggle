0999a5dcd094be55c41e3c55635198f7355d6609
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:30:28 +0000

    boat talk contrast out weak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
