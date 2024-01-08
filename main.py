bc55790b24a50476cea4c32f8cba4ea5fd196409
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:33:47 +0000

    behaviour supply please confuse 572enter

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
