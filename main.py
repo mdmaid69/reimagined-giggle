db80b2f7cb16535796b8c206e20d32f765f2943e
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:39:35 +0000

    furniture breakthrough wait attract bite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
