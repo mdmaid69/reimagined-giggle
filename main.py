4b1963b6a5193d71617ae71fdaed12ba6fcfe920
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:07:47 +0000

    subject wave wipe return argument

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
