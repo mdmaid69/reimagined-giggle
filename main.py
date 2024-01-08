2c09351fbe980e6855eb61a427ac2e797eab2162
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:57:22 +0000

    ordinary coat turnover even address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def split_path(path):
+        return os.path.split(path)
