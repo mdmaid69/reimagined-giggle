48114731e1c5e4c96b215cbbd1b90fb38264ed20
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:37:46 +0000

    occur ball day approval replace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
