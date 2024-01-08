e0e14b2fafd71eeee02606e35698c38a4247e7a8
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:09:25 +0000

    type storm Nn tread crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  def convert_to_hex(n):
+        return hex(n)
