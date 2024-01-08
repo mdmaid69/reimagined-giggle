60373ad7b019c5f8a3bd454ad568a87ad9b2ba1b
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:38:20 +0000

    long whole development recently peace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
