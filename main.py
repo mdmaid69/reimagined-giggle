8c9a84e79cfe41c06052c67d670736f60001b8a9
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:28:05 +0000

    activity goout familiar total perfect

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_floor(x):
+        return math.floor(x)
