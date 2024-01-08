6b1b90d08155a379c3d7a477c47322349ab6dc65
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:24:05 +0000

    turn pair rush tone radio

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def convert_to_octal(n):
-        return oct(n)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import time
+print(time.time())
