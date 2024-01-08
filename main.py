68446095f938b0e4dd5c1e3fc00ad6a61bd57651
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:55:49 +0000

    distance back disturbing right wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
