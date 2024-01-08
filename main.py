b9292fe976d723f4104b3988c3bfca43baadb8b5
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:05:30 +0000

    whenever pity pretty right reasonable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
