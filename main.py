0c710d7eb48e2e3b9e42c21f28e247ddb1f6b84f
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:04:23 +0000

    music try entry slowly preparation

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
