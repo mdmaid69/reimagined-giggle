0bd5e517ffaa6375cb7a639450b545b027461f43
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:41:16 +0000

    trap provide pin floor allow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_work(force, distance):
+        return force * distance
