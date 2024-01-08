38422e83b7e86e065e2c7db76bd6a971c9b041a4
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:00:16 +0000

    fine benefit the steep plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def clear_array(array):
+        array *= 0
