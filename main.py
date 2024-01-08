0c7eccc5364dd0f97d1a9a61f3b67505c85fd38c
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:39:55 +0000

    brief tidy glass sport pay

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
