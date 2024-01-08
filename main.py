9c62977481787b9b26f72f35e7c51e2ddd209bb2
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:27:39 +0000

    slide decide post worry drink

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_speed(distance, time):
+        return distance / time
+def reverse_string(s):
+        return s[::-1]
