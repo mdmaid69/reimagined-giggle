e5efa5bdfc0f904a2e203fa7efacc28766d8d1e8
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:16:19 +0000

    bridge finally land delight Gg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
