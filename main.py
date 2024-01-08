fb44e72ef9b29535f3da216f0cf00b5630a2f428
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:35:05 +0000

    hollow fortunate oil later stage

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_directory_name(path):
+        return os.path.dirname(path)
