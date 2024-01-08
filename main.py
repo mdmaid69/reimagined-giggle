4d1b4e03ffb921414076e2fba482d1402097dd50
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:55:31 +0000

    getback children thank hello geton

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
