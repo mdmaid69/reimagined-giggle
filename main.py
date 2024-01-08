015ba963eaac80a178a53597f29f88dc6145fe5b
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:20:09 +0000

    interfere one him pleasant ever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def get_array_as_list(array):
-        return list(array)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
