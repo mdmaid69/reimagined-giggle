12f605ef1fd6ebd5781874cdd1e897057b6bdcaf
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:35:02 +0000

    Yy port certain 567enemy air

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def find_max(lst):
+        return max(lst)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
