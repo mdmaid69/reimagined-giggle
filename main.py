ddf891505ba5eecedb400e450cb9f05a14cede24
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:40:26 +0000

    miss purpose affect most software

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
