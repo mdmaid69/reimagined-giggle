87bac7f819d06512e1f46d864a89be35e601063c
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:24:50 +0000

    clever ear inform value spare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import os
+print(os.getcwd())
