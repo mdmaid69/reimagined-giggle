3c3339e2616e16f26bcdfe20391fa4de4af18739
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:09:19 +0000

    tax fault promise wrap jump

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
