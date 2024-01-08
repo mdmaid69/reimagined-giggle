b1dc557ebbc6c7435ed2c4c2a00054d324dc3551
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:29:19 +0000

    respect recommend rather flow add

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def extend_array(array, iterable):
+        array.extend(iterable)
 import collections
 def create_chain_map(*maps):
         return collections.ChainMap(*maps)
