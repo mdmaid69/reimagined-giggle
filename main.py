8d597f7472d06a428fe37997fe2c805e8c9f6d5e
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:05:00 +0000

    share cross happen visit this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_current_directory():
+        return os.getcwd()
