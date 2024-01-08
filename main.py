9a0b43fde9f1458510848ec33d05451ac2039309
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:36:47 +0000

    slip fashion character shut accidental

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
