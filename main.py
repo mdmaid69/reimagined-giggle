26ddd9b2264a153cd7076e590c97fa5db9c8dd16
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:18:39 +0000

    beat discussion close remove into

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
