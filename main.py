3ecf6fd8034c1ec3606974070cff867f6b0b1ef7
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:00:57 +0000

    Ll reserve risk hard empty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def get_current_time():
+        return time.time()
