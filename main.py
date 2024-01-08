7daf01ca327ee4c4205ab1f20ee15b6f1b67d55d
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:17:02 +0000

    loose hurry school tax closed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
