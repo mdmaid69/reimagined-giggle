2bfeb85bf8a0834d8853cc8b8d8ed81ee324f4ed
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:43:59 +0000

    foreign pronunciation content relationship protest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
