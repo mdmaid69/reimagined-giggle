13c5ac5cc5ebe7c3c92140baf946b7c11956f374
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:49:40 +0000

    evening miss react base injury

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import collections
-def create_user_string():
-        return collections.UserString()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
