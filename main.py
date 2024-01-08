f6477af60d022032513fc96c4396d2d6027b862d
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:39:44 +0000

    education how tap thick block

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
