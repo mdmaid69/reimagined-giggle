7b807019bf32a01f579eb5086cf464e5b31e3a88
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:30:29 +0000

    land successful persuade speech honest

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_density(mass, volume):
+        return mass / volume
