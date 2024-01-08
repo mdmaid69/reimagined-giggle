4231d08abeb08d06192b17a500aef4247a9a0d71
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:52:17 +0000

    getout fortunately think relation herself

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
