ee95381355736b20bfbb0ce117ab656b9d167aaa
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:55:48 +0000

    everybody mixture calm serve male

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
