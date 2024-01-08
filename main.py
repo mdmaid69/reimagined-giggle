3f59212d054d2d981667fbc54907acf401f0f26e
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:52:29 +0000

    limit seat however bend no

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def reverse_array(array):
+        array.reverse()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
