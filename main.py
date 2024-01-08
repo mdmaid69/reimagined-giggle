1711c8a4c084383997e5a76ae513ca0e2df382a3
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:38:22 +0000

    care contact path act talk

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
