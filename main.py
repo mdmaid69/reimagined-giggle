8885b499ee5c72a5e33db68147dec9f91e1b7a53
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:19:07 +0000

    war comefrom sorry order wing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_tangent(x):
+        return math.tan(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
