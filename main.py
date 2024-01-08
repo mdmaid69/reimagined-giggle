2085d98bd1a4e362bc279c0b677dcaa2940736eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:58:04 +0000

    must as cough excuse myself

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  def reverse_list(lst):
+        return lst[::-1]
