b4201900f824638b9ad276d0ddd51b598db6f1ce
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:57:37 +0000

    statement cough dead last door

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  def convert_to_octal(n):
+        return oct(n)
