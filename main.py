3319d79e2b85b9f933e66554d1e8a95b6fbdf74a
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:29:42 +0000

    sure rely fall bet careless

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
