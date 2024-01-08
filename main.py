69325f6f778c6261d9e0660a72c5c07cfc9c6470
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:28:34 +0000

    concerning attract Tt town great

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 import array
-def clear_array(array):
-        array *= 0
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
