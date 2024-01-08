708dad7a51672298fcb56eb44cfb5af0fb40f56f
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:09:34 +0000

    fair coat kick search spread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def convert_array_to_list(array):
+        return array.tolist()
