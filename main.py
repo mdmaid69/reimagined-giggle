19677a0c0600b91ac4e652ba471df58e89378180
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:09:27 +0000

    regard better since phone hate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_pressure(force, area):
-        return force / area
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def subtract_numbers(x, y):
+        return x - y
