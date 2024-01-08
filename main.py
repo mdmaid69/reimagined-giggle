4931f0a44888c4d9dac7b5abadc4e6f1219b63f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:08:30 +0000

    fit repair wander favour translate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def multiply_numbers(x, y):
+        return x * y
