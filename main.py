85c53581d7b2508446b01fe378d48bccdb8e9c95
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:39:51 +0000

    message piece right drink actually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
