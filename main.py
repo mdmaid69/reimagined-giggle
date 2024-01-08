1d9a56332b4168c6a5adbd7b222fd1bd23491167
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:50:02 +0000

    do decision protest train public

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def greet(name):
+        print(f"Hello, {name}!")
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
