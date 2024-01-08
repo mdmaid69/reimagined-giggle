a15619badde038151810c0e25e6d41d7fde1a438
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:53:46 +0000

    tone special lunch bank level

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_power(work, time):
+        return work / time
