0314e4f2865bdf884524a83cf9c9e669303736c0
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:52:27 +0000

    upset connect manner dangerous crime

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import sys
+def exit_program():
+        sys.exit()
