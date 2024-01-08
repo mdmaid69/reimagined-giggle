235d562b62a424bc6311e7c57ee2d99ea8591ac7
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:52:10 +0000

    left rain spot keepaway achieve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import platform
+def get_os_info():
+        return platform.uname()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
