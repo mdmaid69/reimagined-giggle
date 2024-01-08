f84272e1875d170a1d4ae1aa628ef33569ee6ed2
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:22:54 +0000

    convince must interrupt better opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
