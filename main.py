bb77cccccba35e3823753fcbc2b429c7355ae9c4
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:24:26 +0000

    left offer borrow scream protection

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import random
-print(random.randint(0, 100))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
