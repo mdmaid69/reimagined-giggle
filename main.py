d29960b2166a126d760164979fa75c7ff748bd36
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:58:31 +0000

    expensive hard broken failure operation

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
