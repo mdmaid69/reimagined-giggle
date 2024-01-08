700ea0b9fe02432358856b3d78f70ea4bbd0559f
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:09:52 +0000

    mess Mm dance again promise

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+print(math.pi)
