70ee6d7b5ade382b2c2da20e40d4e29c5b5316cb
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:37:19 +0000

    fight send once suit gentle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
