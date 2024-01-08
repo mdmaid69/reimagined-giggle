98ec5d0668a9878b79cda48d4500ae4b5048d8d8
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:56:30 +0000

    kind experience responsible large promise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import sys
+def exit_program():
+        sys.exit()
