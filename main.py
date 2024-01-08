e48db7e46919161181971212cb9af46746aa0209
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:55:10 +0000

    if pause right description represent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_sine(x):
+        return math.sin(x)
