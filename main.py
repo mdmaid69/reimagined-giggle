f017142d3f12c42aeeda1cf0ed2e851c1cd22b31
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:42:33 +0000

    spot practise best broken honest

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
