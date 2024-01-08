3ac9bc8197c54337e5efd4dad5752961780ce829
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:05:41 +0000

    unkind summer famous supply slightly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_current_directory():
+        return os.getcwd()
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def get_array_length(array):
+        return len(array)
