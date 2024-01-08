a4984f514ed5b0a83468ecaa79686f52c1a2fa68
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:50:16 +0000

    bringdown earth tip branch left

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import sys
+print(sys.version)
+def calculate_area_rectangle(l, w):
+        return l * w
