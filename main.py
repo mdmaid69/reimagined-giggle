60828ed9467068e6675cd301a7fdafb1a1f0d308
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:44:42 +0000

    count course weather take now

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
