31390aac04b86eddf97f824b1378c2435441e06e
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:41:15 +0000

    wall invite better noone feature

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
