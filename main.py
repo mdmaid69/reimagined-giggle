0771a7a0d165ab58c00885f5cbdea3be09347f21
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:50:48 +0000

    characteristic love creature region result

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def clear_array(array):
+        array *= 0
