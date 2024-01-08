7af30f9c9ea01eccd3530a89810e59e78334ebbc
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:13:45 +0000

    air trade further frighten accept

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_current_directory():
+        return os.getcwd()
