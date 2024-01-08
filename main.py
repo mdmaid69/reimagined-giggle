0ca5c01e00e790b61d1ed4f68c392745d2887d61
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:19:31 +0000

    activity sign account admit anything

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
