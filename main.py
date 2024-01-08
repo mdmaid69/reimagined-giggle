9ddf920ecf5ce47d27bf6ff537e2b79e68948152
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:14:52 +0000

    accidental praise affect prevent soon

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
