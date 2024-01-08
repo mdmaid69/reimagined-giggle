259922e5bfca3519e9686f04ea6b8cd7d7784776
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:24:38 +0000

    success protection meeting aim fetch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import os
-def get_current_working_directory():
-        return os.getcwd()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
