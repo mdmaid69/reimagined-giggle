b6983097d72a39f0ed6c1aff60a37743b970f612
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:25:16 +0000

    twist spread because complete test

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
