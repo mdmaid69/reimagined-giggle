01ba71cc901d690a548038aab04b450f4e41b152
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:21:45 +0000

    discussion length whistle evidence wrong

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import array
-def clear_array(array):
-        array *= 0
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
