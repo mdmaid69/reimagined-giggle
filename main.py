727572f26acea7f08892857a3ebd2b47176f00d4
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:04:16 +0000

    Mm figure genuine beneath left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
 import collections
 def create_chain_map(*maps):
         return collections.ChainMap(*maps)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
