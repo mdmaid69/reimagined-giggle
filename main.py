3c46f134c7e35dfa3cab9aabe9cd6c44948bb4fd
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:07:21 +0000

    effort joke rescue underneath absolutely

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
