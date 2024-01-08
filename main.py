df35e7031e71d33a4545ee187deaf849469948d6
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:27:25 +0000

    tongue happy head flow standard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
