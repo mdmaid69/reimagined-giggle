08d7cb70655a85a8f31ee6286493fe49a714247a
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:56:30 +0000

    scrap surprised stamp war nice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
