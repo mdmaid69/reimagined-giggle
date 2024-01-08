c18075bc905dec2da8a1f96e2eb32c3ee54a3e78
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:51:13 +0000

    hold murder condition geton along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
