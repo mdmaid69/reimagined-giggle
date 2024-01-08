3b43d2c470cc067783568e8ccb793d000b599f5d
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:53:26 +0000

    oil tired bringback choose trick

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def cube_number(x):
-        return x**3
-  def convert_to_binary(n):
-        return bin(n)
+def find_max(numbers):
+        return max(numbers)
+def calculate_speed(distance, time):
+        return distance / time
