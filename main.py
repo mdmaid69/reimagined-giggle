bdb6890ba914df7bdddffd50b345bd1abcfc68b4
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:45:17 +0000

    improve style probably comefrom speed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
