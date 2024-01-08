756d3c131caf416f792b3f3943c20ea87adc11b2
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:47:19 +0000

    giveaway sister shock fun school

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import math
-print(math.pi)
+text = "Hello, world!"
+print("Characters:", len(text))
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
