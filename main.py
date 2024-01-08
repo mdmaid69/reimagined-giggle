1bd7685219c9e80be64fcaa40d6f7ec06419ed2b
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:34:51 +0000

    buy guess sit suggest threaten

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
