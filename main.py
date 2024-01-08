989e0b77c83506fef57a58123fc673a8f3cb0b82
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:50:19 +0000

    draw rub process demonstrate measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
