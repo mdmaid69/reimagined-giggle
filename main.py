24ab582f919806a66ce16bfd86abbd2466d27da1
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:33:26 +0000

    copy use field punish history

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
