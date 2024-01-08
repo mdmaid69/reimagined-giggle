7ceadde6ecd3eb293738e7f98add63f043cc4fd7
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:59:50 +0000

    murder warm about excellent example

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
