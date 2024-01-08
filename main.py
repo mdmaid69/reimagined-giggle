4bff9d8093333042811127be7715f56b243bfde8
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:19:18 +0000

    block love than impatient opportunity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-print(sys.version)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
