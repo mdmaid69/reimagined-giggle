f5efb0cb62c600dc4e5a8bf066b4b35d4c8f46ea
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:51:49 +0000

    onto condition likely community off

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
