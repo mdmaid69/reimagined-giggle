dc4f04416b9419076a5ccd6d26ee5f8436899377
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:02:57 +0000

    fresh attention flood where warn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+name = "Python"
+print("Hello,", name)
