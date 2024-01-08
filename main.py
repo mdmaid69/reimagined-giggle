a0ab00d5dcde634f9c270d0a0ddc57e1fb06690e
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:35:43 +0000

    concentrate weak behave return issue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import datetime
+def get_today_date():
+        return datetime.date.today()
