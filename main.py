0305e69222678168eb8b891716deeea2bf3889c4
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:11:02 +0000

    key they herself dangerous weekend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
