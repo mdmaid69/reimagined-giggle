895dcd7d4cef802d13e0b534aa4ff04f9b824ab5
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:12:42 +0000

    hope warn have second sticky

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def convert_to_hex(n):
-        return hex(n)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
