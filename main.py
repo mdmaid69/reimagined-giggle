327393529f2508238c5a683090f44c55d65ba70a
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:31:04 +0000

    crowd willing like glad outside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def count_words(sentence):
+        return len(sentence.split())
