63197960415abb0940e9b19bc1766267e7ce6a02
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:11:33 +0000

    top sticky gentle wander challenge

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
