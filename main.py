05b50fda41b5f568375905d57aa5b13bcea27bab
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:07:51 +0000

    full battery rest invite trade

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
