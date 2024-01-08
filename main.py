2c42c04fac57d4fdf3dcc07bf6d08fd9136d5d43
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:40:26 +0000

    remember voice takeout sense cheat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
