db2037b9eb677833776084c08c7b10178adc49be
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:48:17 +0000

    relax what curious check through

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+for i in range(10): print(i)
+def is_odd(n):
+        return n % 2 != 0
