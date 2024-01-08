70413a742c04751e452040a2cb991a9ae1b45322
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:35:21 +0000

    autumn exact instant unkind star

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
