70b021b945772424b7560be8263c03c7c3e0b9d5
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:43:24 +0000

    threaten lose prepare bang deaf

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_logarithm(base, x):
+        return math.log(x, base)
