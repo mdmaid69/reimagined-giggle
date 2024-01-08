ef70257fed86720f442d9a78ba16ffb690aff58c
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:20:32 +0000

    behind habit not trap leg

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
