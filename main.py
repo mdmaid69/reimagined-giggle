31e9acde95e8d8d2c8a1190dd09c8203e60f7a55
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:17:45 +0000

    amount give smell improvement far

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
