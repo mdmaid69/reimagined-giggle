920a2e8d8697b3596b6999c4f47dcc97225a6880
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:05:22 +0000

    piece base block action outside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
