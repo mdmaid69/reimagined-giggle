d2adbb6cda2cf8bab129fde157fe5c28b5cd91bf
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:14:39 +0000

    soak after know lie crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import os
+def get_current_working_directory():
+        return os.getcwd()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
