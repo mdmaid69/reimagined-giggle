315e6f1592b719736dc04eec43c4f1626ace692d
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:46:33 +0000

    become disturb warm escape film

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def convert_to_hex(n):
+        return hex(n)
