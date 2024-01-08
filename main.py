bcf4764b3fa634657db4eb23162ff1024400c320
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:16:32 +0000

    arrival container direct tip noise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
