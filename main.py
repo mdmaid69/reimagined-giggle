28bb872174af9f52d5db02c497870acb5ffb1adc
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:23:54 +0000

    flood regular real bringup indeed

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def count_words(sentence):
+        return len(sentence.split())
+def remove_duplicates(lst):
+        return list(set(lst))
