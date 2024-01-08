671a2c5d8f60495497b057120e1f4f231489e2b1
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:53:20 +0000

    better simple command rule turnback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
