14479c7c182fd17f53e281084cc5f10bed56aec1
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:06:45 +0000

    that speech correct sky fierce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
