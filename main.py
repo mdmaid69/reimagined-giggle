82d85881472b43b3c75075cd7f3ff7745f3ddbff
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:48:49 +0000

    opposite careless children track foot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
