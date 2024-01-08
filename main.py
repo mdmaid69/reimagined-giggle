6e1ac756bb4318a0d9d95179ec83c91d07273cea
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:26:31 +0000

    hold talkoutof knock use weather

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
