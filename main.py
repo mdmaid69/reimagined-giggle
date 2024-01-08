1c58b03b54e34568b8128ba67e20c4b5dbcc5d43
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:10:08 +0000

    makeup collect shoulder hit inside

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
