1c3022c8f0066f60773efe7ddc4b5b0e66bc5fe3
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:16:25 +0000

    film sure admire year threaten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
