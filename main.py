342e988c2324b3370d3421538d955b9cd825d3f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:24:56 +0000

    about thing state state bunch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def is_odd(n):
+        return n % 2 != 0
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
