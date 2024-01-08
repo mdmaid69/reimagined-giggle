8e95d8fd3eb93ee50628030d17ec2d82135c54f5
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:36:22 +0000

    report dare hate ignore string

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-print([x**2 for x in range(10)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
