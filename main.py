6a6ff8ae0b167b687523129af14779236a5fc9e2
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:06:15 +0000

    thinkthrough shallow turnback mass solve

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
