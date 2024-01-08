93dbb9d5737b808b4083a9fde7487d18fd97fced
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:10:26 +0000

    news duty receive average crash

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
