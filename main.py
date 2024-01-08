05293c5f35954144e521caea9dd3568788247a75
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:32:25 +0000

    up harm food area deserve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_odd(n):
+        return n % 2 != 0
