69680d06f437db0aab25d094f805d3999cffd729
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:14:11 +0000

    impatient stone turnback preparation exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
