fe6dee55f35fda889fba5d512f583e1ccdfc324a
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:01:01 +0000

    up trouble trouble bringup approval

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
