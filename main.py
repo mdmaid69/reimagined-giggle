eec1b230ed8f690a5734ed9ad2a0ad63fa934edc
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:02:52 +0000

    sail forget record confused show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
 import time
-def get_time_since_epoch():
+def get_current_time():
         return time.time()
