2b372b069eb25ae15dc68e75fa3066abc8dfd1d7
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:55:08 +0000

    Ii bad bringback surprise understand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
