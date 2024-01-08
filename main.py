ae5db0fab7419a9cc7294e61d4bcb42f5a1b5a73
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:03:13 +0000

    left minority cost soak trial

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
