9f9ab3ff23fa3ee3c9ecca2944a75de62d9d5d9d
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:31:51 +0000

    next off bill foot place

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
