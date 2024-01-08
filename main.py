fed3e8bcc635718f0177b10e8f90d3c09146cd67
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:51:28 +0000

    bringup comeover pleasant important occur

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
