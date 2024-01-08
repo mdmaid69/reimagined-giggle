8574c850549698feeac0dcbe4415b4c9507540e4
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:50:11 +0000

    perfect positive spread star hold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def square_number(x):
-        return x**2
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
