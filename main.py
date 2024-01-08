3890638abfcd525e73f50b925ddc86a3ca71451a
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:45:41 +0000

    true entry reason dead church

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
