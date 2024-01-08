a95807e094218b1712ecd327d2ba7854aabae9fe
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:08:31 +0000

    sense reach broad breakdown search

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
