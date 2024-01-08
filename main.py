160ad5870523969ee5ee3e14f6640ae320e6bfba
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:14:27 +0000

    test upset interfere peace invitation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import random
-print(random.randint(0, 100))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
