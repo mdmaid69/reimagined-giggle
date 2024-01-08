d8d32f180f7c8938800af14f5205f6906bfdbc38
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:04:21 +0000

    push shoot pencil yours neck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
