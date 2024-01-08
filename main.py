0b0346736e4c04aa3072ac2f87310cdb3c3f610b
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:18:01 +0000

    runover admire empty on shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
