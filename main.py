4e68ceb9e727bcae76385f83ba13f00f5d378de8
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:03:04 +0000

    steep thirsty prove slice food

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import sys
-def exit_program():
-        sys.exit()
+def find_unique_words(sentence):
+        return set(sentence.split())
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
