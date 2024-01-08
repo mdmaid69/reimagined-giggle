bcc3018f897fb45c14ce7ae28265552bc9cedc42
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:09:57 +0000

    recover pain want possibility exact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
