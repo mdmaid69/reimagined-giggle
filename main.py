265dcd1bb5a4df52e62cea1442a60e959e30cb34
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:08:13 +0000

    coat with bridge runinto object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def calculate_square_root(x):
+        return math.sqrt(x)
