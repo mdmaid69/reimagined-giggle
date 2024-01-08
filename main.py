1a4c7bfbaa8fcbe7abe3a27c3666c51e600887c1
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:20:38 +0000

    book tool upset one suddenly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
