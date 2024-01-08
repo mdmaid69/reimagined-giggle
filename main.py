0577e425f39d8eb86f893c9b8314cd9efaf4ab6f
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:35:38 +0000

    tense move share spend gradually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
