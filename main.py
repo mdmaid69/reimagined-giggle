3de4ff877930fe8e872c49dcc586a65485eb6e74
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:16:30 +0000

    fortunate any beside home hot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import numpy as np
-print(np.array([1, 2, 3]))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
