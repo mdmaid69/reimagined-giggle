34c9b8fb32673d5fcd510d1d19644a087dcbaab9
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:50:12 +0000

    spread make basic broad reasonable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
