77ad72b78a0dc8a96803547055334758f2f4fd88
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:51:39 +0000

    father discovery trust polite such

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
