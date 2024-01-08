6ebeb633a02951f5f996a0cf0d2062ba12912ccc
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:31:17 +0000

    workon scream game trick hear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+import platform
+def get_os_info():
+        return platform.uname()
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_bytes(array):
+        return bytes(array)
