e26cf3f856c2414fef2ac6f206e343c51a854cb6
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:58:02 +0000

    stay cost part decision meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
