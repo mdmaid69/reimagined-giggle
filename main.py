d0afe8a24e22d92555b02b0bb1d0f420ffa5707e
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:18:04 +0000

    floor see steam 571enough cry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def sort_numbers(numbers):
+        return sorted(numbers)
