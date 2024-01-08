5e96d2ceb3e38a40a402ffc0dc9654e594025a06
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:55:25 +0000

    taste day keepup original argument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
