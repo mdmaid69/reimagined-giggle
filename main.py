26ce1b8e8a27baca9704809eac92411e324eb154
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:21:44 +0000

    apparently pupil appeal official basically

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-print("Hello, world!")
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
