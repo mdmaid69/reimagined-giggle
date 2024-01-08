0a9ad829ddb5505cc53cde033f2fb4e8f487bcd3
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:21:49 +0000

    cheat partner business belief cheap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
