fb3213c6df67a79f7ddbf5384e2c929dceb7c3a3
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:37:01 +0000

    breakup its watch free steady

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def calculate_speed(distance, time):
-        return distance / time
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
