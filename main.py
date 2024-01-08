62912bc1c20ace42b2492e0ce54a96388f52cc18
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:20:19 +0000

    train boat rain content rescue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def reverse_string(s):
+        return s[::-1]
