3582f7b4f195d619a6b3f7056af4ba3e9486d8eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:20:30 +0000

    middle Hh care nor concerned

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
