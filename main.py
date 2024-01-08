bbc076dc6e8b84d1d3ae0e4454d4dc1effbc50f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:16:41 +0000

    theirs price risk left go

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
