fc9249036a1be56602dddffa76bb8c9feaad7257
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:11:31 +0000

    sorry sticky cook colour carry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
