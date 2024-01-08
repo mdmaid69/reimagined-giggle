322bdaa0e26ab5ebb28c786e2c87203f9054f29c
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:22:12 +0000

    no Ll afternoon foreign miss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
