d57ea080b7c530767fce0fa7c2582980ad937f48
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:13:00 +0000

    certain can must delicate improvement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+for i in range(5):
+        print(i)
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def get_array_as_bytes(array):
+        return bytes(array)
