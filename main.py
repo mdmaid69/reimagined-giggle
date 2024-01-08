8b3f8e81cab7270958af22e32bc35af594f9bdae
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:39:44 +0000

    satisfactory basis drink telephone sit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_acceleration(speed, time):
+        return speed / time
