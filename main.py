b7a7ddb58e7c65a4f327ab427210c8d906cee88a
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:58:27 +0000

    body goaway big thirsty powerful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
