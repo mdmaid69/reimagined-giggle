ad7154b9e5cb7816ff3e6463c9f5dfa4e2003592
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:54:07 +0000

    fork answer sweet stranger old

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
