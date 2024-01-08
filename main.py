0ad51b52e40e88d80f30653e5c83b61ad5405e2f
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:55:24 +0000

    rain stamp hide marry form

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def convert_to_octal(n):
+        return oct(n)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
