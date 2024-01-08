c48e727acf245615b71c4948df97a658325f4321
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:21:57 +0000

    Ii alive confuse small fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
