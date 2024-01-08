94181b2735a1cd98a1683ae336585ef542534d22
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:13:59 +0000

    fairly rush bound under sail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+x = 10
+y = 20
+print("Sum:", x + y)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
