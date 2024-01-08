71151b77aa1e52f886423602183e1ebef9b60cb8
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:41:42 +0000

    university after convincing high immediately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_set(array):
+        return set(array)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
