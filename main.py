ac1b3e5e2f45db67726c28f08a5425d90384b157
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:38:41 +0000

    punishment mine argument busy definite

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
