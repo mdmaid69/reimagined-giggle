8a6b9ecc8113cb0f3174bfcfb04d3f0fccc23e32
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:34:05 +0000

    mess blame mood vanish however

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
