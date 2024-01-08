5dde9bc18f74d89b5b6327a68a323afdff9cf0a1
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:59:35 +0000

    considering leave game load concentration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
