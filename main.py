ada8761574df64604ded9a3e234cd5b979d496e6
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:00:22 +0000

    plate along wide official precise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
