ecab9d94734036dc82d242ed28c1e4f45ccbda8c
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:19:16 +0000

    separate afternoon dare knowledge guide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
