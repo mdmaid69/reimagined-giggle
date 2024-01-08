2a877ad74d938160b0b4a5fdb9661dd14d98a467
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:13:55 +0000

    shake plastic until deaf smoke

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
 text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print("Reversed:", text[::-1])
