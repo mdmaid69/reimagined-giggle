c583a801a03f07d47d55597a64b91260d8d4f98d
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:23:11 +0000

    somebody surface underneath motion concerned

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_as_bool(array):
+        return bool(array)
