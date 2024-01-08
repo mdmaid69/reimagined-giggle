24b12beab24f26f1505a0f7e60b4a4071c55129b
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:22:12 +0000

    tour hot add favourite word

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
