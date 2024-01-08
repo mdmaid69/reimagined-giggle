063a710e33b8c3b5f73a5442a646ba00d15ad795
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:28:28 +0000

    first trap tooth fierce spill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
