8803e7bf31eeb5b824750f531643ca0c44972f01
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:44:19 +0000

    pair comeout attempt fresh invitation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
