48bec1a37054d63891e2c654c94642c94ec63ed8
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:23:23 +0000

    section case day full perform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def divide_numbers(x, y):
+        return x / y
