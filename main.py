f3b4cbbca7773e014d630c9aa595f3fddd6130e4
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:19:47 +0000

    fair aim temperature method case

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def is_odd(n):
+        return n % 2 != 0
