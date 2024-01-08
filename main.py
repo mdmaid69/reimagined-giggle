4074c6326441c9a92e330ce80b626e42c664c9fd
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:03:42 +0000

    speed modern recommend expect straight

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
