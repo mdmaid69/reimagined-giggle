f6313028e36bc9c19590ef07a8692adcdee73d86
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:04:06 +0000

    fail giveback trade pencil embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_sine(x):
+        return math.sin(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
