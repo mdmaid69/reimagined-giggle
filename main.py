6fb762089db58cab7cc593a71921dcde96208e2f
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:58:33 +0000

    serve address truth attach about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-print(random.randint(0, 100))
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
