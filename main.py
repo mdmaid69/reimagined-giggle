13ca7da8757490fd3044832996de979dcfb51510
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:32:01 +0000

    house offer hard month face

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_pressure(force, area):
+        return force / area
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
