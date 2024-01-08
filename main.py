1432b84f2f65141423ef8113b0307604a37e6f46
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:02:02 +0000

    concerned tomorrow fasten entrance wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_list_from_array(array):
+        return array.tolist()
