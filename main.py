187cb5ae6cb2b3469531694b58391b53f325b263
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:31:34 +0000

    priority ill goup fair hole

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_work(force, distance):
+        return force * distance
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
