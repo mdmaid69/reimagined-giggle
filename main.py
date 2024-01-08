4a6c6bcdf3ba3645b5761397e15c59daa5d3d506
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:01:45 +0000

    target pool false exhibition willing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
