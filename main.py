776b5d94aa02c96116a46edbb721118d25651098
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:30:01 +0000

    rarely produce ready rely towards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-name = "Python"
-print("Hello,", name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
