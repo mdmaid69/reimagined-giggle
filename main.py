1851b396f1205afcc0f5f9b5ea7550100b379059
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:54:01 +0000

    tip comment fashion tax quick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
