166f768a40d4c9e353e770e88d893697c4c6057b
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:52:50 +0000

    face space agree succeed invite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
