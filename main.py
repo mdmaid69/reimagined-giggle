6203937b781622e65fa72bba702969cea985a413
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:48:36 +0000

    understand serve steep seem obvious

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def add_numbers(a, b):
+        return a + b
+import os
+print(os.getcwd())
