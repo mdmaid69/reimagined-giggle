8f903922829b7129c50949a1b416711ddd3cd58f
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:30:45 +0000

    passage work basically plain worst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
