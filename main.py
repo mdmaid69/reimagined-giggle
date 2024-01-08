d05c92d6e24f976cefcfa8b085285164dd39cbfc
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:45:16 +0000

    steal goon could slip sense

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+print([x**2 for x in range(10)])
