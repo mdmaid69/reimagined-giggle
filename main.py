e1ec4d854f01522b8e464ac7fede5bc0d789259a
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:15:41 +0000

    plan movement glass prove sore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
