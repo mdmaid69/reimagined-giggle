14eee65c63d2817fecea5733e6f893c52dabaa74
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:03:34 +0000

    finger especially dress help awake

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
