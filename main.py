c8dd950dd082a9ad46964439839a7ec764246e66
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:26:38 +0000

    switch test soak case disappear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import random
+print(random.randint(0, 100))
