bc19623758b35751ae696839788c6b34a6852601
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:33:37 +0000

    love study finally forward degree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_power(work, time):
+        return work / time
