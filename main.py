da76c620e3f8d29e23e2f4be98003b8965b25559
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:06:15 +0000

    clear bound wheel flavour direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Words:", len(text.split()))
