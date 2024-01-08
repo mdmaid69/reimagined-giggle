d058e08c31541d3732ee4a39ced17fe68192ab43
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:06:58 +0000

    failure neither opponent ticket offer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def is_palindrome(s):
+        return s == s[::-1]
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
