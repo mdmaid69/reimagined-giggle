322e334945fc362b14476fb3c3e770127895fd3d
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:26:48 +0000

    claim noisy main slice combine

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
