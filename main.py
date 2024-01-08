cb76a92bd4951e7e08660495ac431d3b7c8991d4
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:51:41 +0000

    possible natural alive admire year

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import math
-print(math.pi)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
