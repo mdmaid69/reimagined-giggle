53236f3ae461fd04c8c372629655af10e27f7b64
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:59:25 +0000

    release late convince best compare

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_density(mass, volume):
-        return mass / volume
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
