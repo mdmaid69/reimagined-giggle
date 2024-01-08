4896d0a18ccf97ed311d69511f265a0417d55dbd
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:11:34 +0000

    start we married Ww question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_current_directory():
+        return os.getcwd()
