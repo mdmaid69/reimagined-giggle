eabca389f8afb7284caae8f74b6b4a14d01dd1eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:02:30 +0000

    common crime hole smart support

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_list(array):
+        return list(array)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
