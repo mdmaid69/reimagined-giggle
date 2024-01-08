6d03ad2984e94e4dff210a497fed60e7480bf54f
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:25:29 +0000

    tie land trap supply comment

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
