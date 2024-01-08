9e7d3af235098086878b00deb48cdf6fb152004f
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:13:29 +0000

    board cause image much unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
