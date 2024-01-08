93dd627f7079aa61e5f0f76d0000df83bae051ff
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:17:11 +0000

    zero neither sleep grow return

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
