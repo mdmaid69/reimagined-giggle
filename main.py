c634b81408af7d856d9909115a1992bb99f27e99
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:24:26 +0000

    scared objection themselves look declare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_base_name(path):
+        return os.path.basename(path)
+import datetime
+def get_today_date():
+        return datetime.date.today()
