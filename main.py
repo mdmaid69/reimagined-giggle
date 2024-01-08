2a625e2f7c17b8a663c831985b01908b93d7f5c2
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:05:50 +0000

    smoke year Ww Hh government

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
