f65ef61184e97e88695c09020273dde48ba5fac5
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:30:16 +0000

    week cheek light somewhere way

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+print([x**2 for x in range(10)])
