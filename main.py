302e252714c70ce935bc9c3ac78d642acefd29a7
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:14:09 +0000

    frame comeround anybody black thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
