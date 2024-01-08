8a7fbf67229cf6488efd71aabe98ef439178479d
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:41:19 +0000

    strict winter different project step

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  def remove_duplicates(lst):
-        return list(set(lst))
+import sys
+def print_python_version():
+        print(sys.version)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
