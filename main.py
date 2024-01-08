79f06b84ce9851136a38f2c57e3a6438b90c1855
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:34:51 +0000

    570enjoyable tidy unite by community

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
