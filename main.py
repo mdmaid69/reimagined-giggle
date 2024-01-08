871a928fafcdc7efd1d2ad1f95a6fbe1f1af25b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:04:28 +0000

    university stare rest show Vv

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def count_elements(lst):
+        return len(lst)
