24276e12a01b036f23b897845c914ced0fb34aba
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:06:06 +0000

    deliver walk release scale throw

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
