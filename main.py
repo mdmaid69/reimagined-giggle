e3486af293a118e38ed07c06d0c9e86e45b43479
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:40:10 +0000

    tool birth quick depend acceptable

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_area(radius):
+        return 3.14 * radius * radius
