33d007a198363ba538cb41293941c2bf7ba97a89
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:14:12 +0000

    amount justice unite likely sticky

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import datetime
-print(datetime.datetime.now())
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
