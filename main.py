567f482ab76e1f504adc7781ea93b279ab34ce7c
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:56:05 +0000

    mind of feel fight police

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def find_unique_words(sentence):
+        return set(sentence.split())
