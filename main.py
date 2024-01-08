f15357529355fe428d87d64ee3e298f11ad569a7
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:10:36 +0000

    truth absolutely wave pinch scared

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import numpy as np
+print(np.array([1, 2, 3]))
