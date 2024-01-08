a4fbe9d40f91736379f8a43b4f5b2027185c77f2
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:27:15 +0000

    either crowd increase tread welcome

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_acceleration(speed, time):
-        return speed / time
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
