e8a0e578d799f4be31b59a4e3705fe7400bb3cbe
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:48:45 +0000

    figure half more young bringforward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def find_union(list1, list2):
+        return set(list1) | set(list2)
