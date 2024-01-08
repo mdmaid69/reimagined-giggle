3bdb811de68d8a33055f7967ca0a338c8897c75f
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:21:56 +0000

    shape stranger burst praise steal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import array
+def get_array_as_str(array):
+        return str(array)
+import numpy as np
+print(np.array([1, 2, 3]))
