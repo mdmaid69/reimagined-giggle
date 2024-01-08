729ba501402a101857737626f43b56bee699e10a
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:48:12 +0000

    question fear borrow increase late

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
