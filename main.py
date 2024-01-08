3d87829e020d0d73af02df86826ca6fa33a7531f
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:27:01 +0000

    blue certainly friend laugh such

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
