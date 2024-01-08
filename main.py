fd6d870b7b967f1953605ae285cfc78815d59601
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:38:21 +0000

    geton occasional sort its awake

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
