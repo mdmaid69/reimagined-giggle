94c20bfa42b2cc8d7ec44691850615c01538bc50
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:58:47 +0000

    top quickly female neck pool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
