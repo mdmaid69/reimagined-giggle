5b0dfd89442860b30968f089b8afdb6fe1886baf
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:47:32 +0000

    down centimetre run mine peaceful

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
