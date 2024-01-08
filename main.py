9c11d5f8917d47d33d4cb2e39305be9a4742b5c5
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:53:34 +0000

    needle less persuade unpleasant feed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
