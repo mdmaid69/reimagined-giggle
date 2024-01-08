76678ea9b764fbccb39fd51e6817c689e6ee44b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:26:26 +0000

    lookback(on) standard fair goup exact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
