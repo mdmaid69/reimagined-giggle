e1cef4b5b6496c9e580000bc9ff44babbf69bf6e
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:45:48 +0000

    drawing still Mm woman print

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
