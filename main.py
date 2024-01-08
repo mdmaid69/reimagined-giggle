a461f27514ff187aa0d45a67d563a49adf955f7a
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:31:53 +0000

    under wash hers message miss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
