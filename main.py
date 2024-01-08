50cc76263b73b6dc2916451d8534ab82dfdad2fa
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:34:57 +0000

    deaf small television bang television

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
