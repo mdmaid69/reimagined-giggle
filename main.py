cf8ad0ee496cd7eda4f6d3905283cd53872ba40c
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:00:25 +0000

    comfort neither office objection concern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import time
-def get_current_time():
-        return time.ctime()
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
