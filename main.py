d04cfc02174617fdb0481a4513794f4275528d86
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:24:55 +0000

    difference couple acceptable trouble correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
