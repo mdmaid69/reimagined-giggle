8e9a4cb276d64e66fc39d7ed8aee9dc0eed10d8b
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:10:36 +0000

    serve prove pride please 566end

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def count_elements(lst):
-        return len(lst)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
