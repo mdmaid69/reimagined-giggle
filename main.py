bf6ca8605e01135a004204d228bfa6b8e4f0029c
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:54:09 +0000

    measure design divide sort gradually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def convert_to_binary(n):
+        return bin(n)
