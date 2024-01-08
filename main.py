524036653809cecb812ef4992387df00d6a07b2c
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:34:24 +0000

    possibly body deep coffee follow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
