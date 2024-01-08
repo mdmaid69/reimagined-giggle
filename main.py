d92100d2d41a9a195c2844110b31d1ee7d7f57e6
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:53:35 +0000

    passenger mean interesting loose street

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+for i in range(10): print(i)
