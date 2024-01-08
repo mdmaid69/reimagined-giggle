0169dba58623ff38641366bb3ed85a0a5623d9ee
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:48:22 +0000

    discover comment amount doctor quiet

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_hex(n):
-        return hex(n)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
