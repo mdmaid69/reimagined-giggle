a5fbf8263c783608125ecb2ae25cfade3ca9af37
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:18:06 +0000

    immediately choose several final confused

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
