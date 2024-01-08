93441c475c455c19fc99be53b14b3f7b644a3c85
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:19:14 +0000

    positive form surface relative talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import random
+def roll_die():
+        return random.randint(1, 6)
