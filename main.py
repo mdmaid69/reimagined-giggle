b5df24f4c94db2d4d2fbef521f0584ae084b888b
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:26:50 +0000

    arrange callback 568energy prove tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
