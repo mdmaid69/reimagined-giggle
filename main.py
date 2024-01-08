69df7bd6be393b0052b2819b0ad70afca2223493
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:07:40 +0000

    comfort bringdown interesting its discovery

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+print("Square numbers:", [x**2 for x in range(n)])
