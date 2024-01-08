28f9228965fa3e01ea83483ddafb5055a126fa1d
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:12:23 +0000

    slide meeting notice provide normal

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
