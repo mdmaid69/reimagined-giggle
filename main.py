709707188f72cbd53d2b3d4847d2536754fc7b65
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:16:50 +0000

    exam for contain below begin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
