ba274ea8d8b208d678303c93e615f3b1f55729c0
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:47:10 +0000

    concerned sense adjective police statement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
