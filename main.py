7ccb3006265f99ff1a9e1c668ef85a3d283d86ac
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:11:25 +0000

    guess sick wet dangerous comeoff

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
