dc3be8fa7993fa5d4d97622a95f8e86f1e424f2d
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:57:31 +0000

    suck proper guilty talkback switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
 import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def get_list_from_array(array):
+        return array.tolist()
