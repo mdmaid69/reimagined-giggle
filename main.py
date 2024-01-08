bfcf8699b62b52485adb664c57c2d908b09edea0
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:56:13 +0000

    foreign than own create official

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
