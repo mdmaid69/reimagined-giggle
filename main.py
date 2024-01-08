14cc300f68d9c522352c8bc01f59fde3a1aa75b2
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:20:12 +0000

    start trial in job many

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import tensorflow as tf
+print(tf.__version__)
