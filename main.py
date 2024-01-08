20802926ba7b5d2f22a00306abf183e7c7241c5a
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:14:54 +0000

    rise count punish statement train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def convert_array_to_list(array):
+        return array.tolist()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
