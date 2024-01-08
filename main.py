44bf42072a34189742436d9251fc2ede68db924e
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:32:38 +0000

    constantly spend trap cap fault

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
