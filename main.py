9175bfcbc32a7e8f8d866add08878e6a0853c5a1
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:07:14 +0000

    pretty factory wash brief shut

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
