b301640cbd9109e5499fe88f351ff245735ad719
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:32:26 +0000

    find greeting 570enjoyable prevent read

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import os
+def remove_directory(path):
+        os.rmdir(path)
