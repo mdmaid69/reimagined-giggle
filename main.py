38cce619d0f486be63f62eea6a4352eccd20e91c
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:00:07 +0000

    apart moral successful wise basic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_min(numbers):
+        return min(numbers)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
