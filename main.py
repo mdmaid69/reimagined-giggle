e0ecbc760ab22f7f3473fd925c9fd992e8b2136f
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:45:39 +0000

    balance bin length boring climb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
