8297a5336fead381ab6a2dcbfeb5e7d18fbdbb26
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:31:54 +0000

    score journey equal strange condition

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
