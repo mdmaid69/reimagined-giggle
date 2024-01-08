eaff5ff1df7775329ca08158fcaabf7ec305399f
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:31:52 +0000

    pay maintain against bright owe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import platform
-def get_python_version():
-        return platform.python_version()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
