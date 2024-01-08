b35630a11e528175b9ce2f68cc787532674515d7
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:15:50 +0000

    complete respect medium rent turnout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+for i in range(5):
+        print(i)
+import array
+def get_array_as_int(array):
+        return int(array[0])
