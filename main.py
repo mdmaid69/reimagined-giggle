3182121f447133cc0b25cf6d54b39505115875bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:39:10 +0000

    Yy exactly dance pure petrol

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
