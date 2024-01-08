1f59d349841732ec24849f9c9049d857229e9a35
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:15:45 +0000

    artificial score journey money no

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
