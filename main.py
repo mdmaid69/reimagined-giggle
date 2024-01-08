d1e80e420ee15e05f67da04ed464f27844167dfb
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:09:39 +0000

    excuse lock exact plant themselves

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+print("Hello, world!")
