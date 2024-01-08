f0656869abf99a8343ad9406d82ce13c29992218
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:37:54 +0000

    charge large callback book rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  def subtract_numbers(x, y):
+        return x - y
