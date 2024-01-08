76244031074d3f28b723fa56c4dd7970f5119a84
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:32:46 +0000

    base beat music step claim

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
