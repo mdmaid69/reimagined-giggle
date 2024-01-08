4b15a7b0009150b5d2f4313c85eca8f4c006d9fe
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:12:59 +0000

    holdup holiday obvious Pp fall

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
