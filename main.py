26dd154489e3e2adf5d134b1fc65305aeef016d2
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:31:25 +0000

    contrast innocent introduction of deliberately

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def square_number(x):
+        return x**2
