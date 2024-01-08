ef0e93dbf90cff1402be4d892a9bc16fe61e01c3
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:39:19 +0000

    job event apologizeapologise promise use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
