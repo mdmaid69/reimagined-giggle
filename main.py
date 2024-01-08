e4b8c63fda25fdb7542fcdf0ff2425ea3bb23fa4
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:23:47 +0000

    mean afterwards comeback communicate reply

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_exponential(x):
+        return math.exp(x)
