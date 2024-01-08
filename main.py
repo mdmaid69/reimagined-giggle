e3cab5036210c8df697d6cf723bf0f3938bae18f
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:43:41 +0000

    her sit bend after without

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def subtract_numbers(x, y):
+        return x - y
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def iterate_over_array(array):
+        for item in array:
+        print(item)
