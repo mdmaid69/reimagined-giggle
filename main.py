0ef453c162aa02f038d5ad7266f3e3b47a65e514
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:38:34 +0000

    left modern surprised persuade wipe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+print(sum(range(10)))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
