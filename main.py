cbf6c3cd8fb72df3a0cfd9e3ca52bcacaa14a1dd
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:21:33 +0000

    wherever section yes comeback grateful

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
