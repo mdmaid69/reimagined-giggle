f03a230f60cd4fd07977c0973bdbba21a8573df8
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:19:09 +0000

    confuse communicate emotion supply poison

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
