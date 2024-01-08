affd291eb6e8c308a1394cac8d44f749fba0399f
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:52:05 +0000

    short plate complete group burn

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+print(sum(range(10)))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
