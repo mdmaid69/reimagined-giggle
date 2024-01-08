c210f936990d825e081d8aac3cea271796d6316c
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:27:15 +0000

    refer activity force money prevent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
