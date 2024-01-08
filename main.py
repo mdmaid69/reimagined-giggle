ffdae30776c3d0667fcf70222198e452eb27e4c1
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:21:59 +0000

    comeup please benefit famous wherever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
