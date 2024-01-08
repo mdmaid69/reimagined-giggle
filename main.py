c5966df78c5cb35e0163281d9860f6dcbb64e849
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:49:50 +0000

    design before use free ring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-print(sum(range(10)))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
