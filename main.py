16fcf8955c3322c88255cfe700f1253208ec2764
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:06:12 +0000

    terrible like emphasis cut could

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
