d033a5262b3c25acdc8977024b567d9c4637367c
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:09:49 +0000

    scream health material value once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  def count_elements(lst):
+        return len(lst)
