cd1725d9113233fa48cd24395569f6f7e1236a55
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:32:20 +0000

    record on lend worst heart

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
