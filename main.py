62427e16ce0fca4579c17bd2c00f87fe578a4185
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:59:59 +0000

    pleased duty able dress embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_user_list():
+        return collections.UserList()
+import collections
+def create_counter():
+        return collections.Counter()
