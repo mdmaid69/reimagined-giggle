a3741342123f2ff0ef12b480cc330856ccafca73
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:26:48 +0000

    shade undo takeover calmly none

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
