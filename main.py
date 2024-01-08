19d807b57b723684667d6239ae414e20bf1e151e
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:15:52 +0000

    holiday art ball shoot own

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
