f914385ac43f77f64ba935374ac8d1f34aa8237a
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:29:30 +0000

    immediate yet scream calm mine

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
