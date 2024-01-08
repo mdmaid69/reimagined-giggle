c3ea04611942f2e0682fa3c53299832d9877240c
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:50:00 +0000

    spread lost minor remain treat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
