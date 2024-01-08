ab139ac435387cd8a6cfcb0f331138b153315651
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:32:40 +0000

    telephone tense free upwards fail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
