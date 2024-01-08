34cf0a99a5fe56c3095780c37bd3f4350f9ca032
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:57:58 +0000

    in section instead loud deliver

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def find_max(lst):
-        return max(lst)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_float(array):
+        return float(array[0])
