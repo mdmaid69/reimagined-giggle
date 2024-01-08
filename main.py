2fd2cb028c3a34212e3b110a30181e98653a8521
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:51:03 +0000

    home mail opponent declaration pull

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+print(sum(range(10)))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
