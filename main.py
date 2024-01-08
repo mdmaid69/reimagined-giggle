e5091def117f8e08ccbf0f50f9ec28c3012cf936
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:24:26 +0000

    by relax sleep storm clean

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
