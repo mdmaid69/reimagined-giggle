f9c614d0505e4eadf60714cf3a3867567b1470b4
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:28:32 +0000

    little glad sticky opinion cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
