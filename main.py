ebea044b13397784858d5155eb024e1699f0b54d
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:17:45 +0000

    workon natural behind reliable smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def delete_file(file_name):
+        os.remove(file_name)
