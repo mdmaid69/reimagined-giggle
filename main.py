36dc7e4d00fb9b04bb7b2c443280c81984a40789
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:45:58 +0000

    cut anywhere somehow cruel dear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
