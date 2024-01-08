8d7beaf2de3a881db1607753560de5e1ce819ef5
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:02:04 +0000

    blind surface middle excuse recover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
