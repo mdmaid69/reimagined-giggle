8c48b4b062d5f7319b31b3280d545b6b2498c29a
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:40:28 +0000

    gap conclusion deliberately callout clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  def convert_to_octal(n):
+        return oct(n)
