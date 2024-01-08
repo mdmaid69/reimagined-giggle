cd31310a18d239247866b2e1cc9096ef73064435
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:16:25 +0000

    approval apply have appeal tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_unique_words(sentence):
+        return set(sentence.split())
