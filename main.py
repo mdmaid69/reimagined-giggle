2136563a4728fb05382ec977426c853c761800bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:58:26 +0000

    sticky city offend bath lose

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
