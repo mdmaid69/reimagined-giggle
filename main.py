a3c0ad061ec24b13f0b4ada3c4cca4ef47536f2e
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:11:50 +0000

    keep determined bunch priority issue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_density(mass, volume):
+        return mass / volume
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
