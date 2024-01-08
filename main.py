66806b549a579df9b9e05b6bedf091221370af7e
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:54:09 +0000

    persuade entry on Vv thankyou

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def get_array_typecode(array):
-        return array.typecode
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
