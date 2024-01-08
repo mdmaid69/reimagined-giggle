5195e2c915622b4edd9aa4cc3b1fe3aa73933c6c
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:28:24 +0000

    advantage special congratulate responsible surprised

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def is_palindrome(s):
-        return s == s[::-1]
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
