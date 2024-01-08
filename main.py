046754ed0d6978dabf043cf6695f137a8b1dfb85
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:26:31 +0000

    store tonight chin calm taste

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def convert_array_to_string(array):
+        return array.tostring()
