17529dd55697da4fb01b5d3df68bb4a5b2e20825
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:42:14 +0000

    responsibility expect middle recommend attention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_as_str(array):
+        return str(array)
