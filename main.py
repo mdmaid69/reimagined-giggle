18405e92ee627e450df81db58fea16eb7e007ce7
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:18:08 +0000

    badly sure drive may holiday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
