24cca0bf724272869acac7e4cd88180fae1e4f81
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:24:06 +0000

    price extra convenient favour bright

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def get_bytes_from_array(array):
+        return array.tobytes()
