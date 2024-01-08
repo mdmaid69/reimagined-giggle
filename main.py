75d926a470d097ce0583cc453a811337f65abf64
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:34:31 +0000

    workout dare compete separate calloff

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
