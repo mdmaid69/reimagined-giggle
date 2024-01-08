3d81c0f98ee5e2eb19bfff2980effd5ded95c4a7
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:38:57 +0000

    shock switch wave sit wander

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_mode(file_name):
+  def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+for i in range(10): print(i)
