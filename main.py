be566b56e2136c2e971e50463096501b4076b959
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:13:32 +0000

    ear follow careful firm design

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
