e55d1fae19263c2a250084e48184eb21e7b91738
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:46:33 +0000

    pleased talk dick leg goodbye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
