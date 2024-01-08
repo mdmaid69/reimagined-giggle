4a3bc36141cd057d38d79cc30ec3ff4cd4cd3979
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:56:39 +0000

    over listenfor tongue everywhere before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def find_unique_words(sentence):
-        return set(sentence.split())
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
