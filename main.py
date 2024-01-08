0b2cdfd64c1d5a7b6114d8c0a778e86eab1e72ed
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:26:04 +0000

    turn ill include alone guide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
