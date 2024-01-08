e3d77167d98633dec787aec1cc145aa37348fa73
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:30:18 +0000

    honest die carry news weather

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_max(numbers):
-        return max(numbers)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
