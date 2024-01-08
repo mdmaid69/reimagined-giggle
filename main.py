82ec03ee9903119d6461f80b8fa19e23715e96a1
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:31:39 +0000

    ahead comment breakout imagination brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
