7f3d327e08028c74581dffdcd1785844c5ba74ca
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:11:45 +0000

    square disturbing adjective too collect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def remove_duplicates(lst):
+        return list(set(lst))
