deeed8edc70c7f4c85fe3daec74c21515292c9c6
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:14:39 +0000

    project know fun shall arrange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
