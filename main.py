6898647e3c0de0e406a0ea0e4af9cf8f2d02f936
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:41:33 +0000

    indeed like land dare frighten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def square_number(x):
-        return x**2
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import datetime
+def get_current_date():
+        return datetime.date.today()
