1ecb1a6aae308346074e39569b9f6cc03ae0fae6
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:47:16 +0000

    that surprise straight ashamed bright

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import datetime
+def get_today_date():
+        return datetime.date.today()
