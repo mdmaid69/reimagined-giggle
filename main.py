eb8b4ff506c7cbfbc6ad67a4c2ed059c80edc636
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:11:24 +0000

    whatever cost left drink yours

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import math
+def calculate_floor(x):
+        return math.floor(x)
