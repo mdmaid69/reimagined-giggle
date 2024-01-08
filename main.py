4b071f51258193904d7cfb0bdb13aefef17a8655
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:06:56 +0000

    shout education remain raw score

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import datetime
+print(datetime.datetime.now())
