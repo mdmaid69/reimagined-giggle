f96755afe1f1e375363ac6e85daae8ee8d4953f8
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:27:14 +0000

    demand temperature tradition comment wave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
