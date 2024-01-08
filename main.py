f60d25dcf4b0736d8e6e57e02eb39961590bb1bc
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:26:46 +0000

    cheap Pp age lead angry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
