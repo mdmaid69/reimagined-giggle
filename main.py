ad9dfc20ccc98f9dff1bccecd7e8e9eea8a0e749
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:32:27 +0000

    match comeout inform exchange very

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
