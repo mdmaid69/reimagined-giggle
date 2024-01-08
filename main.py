b8eaa5c6dafcad16280d902e6fd57e46252bac76
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:24:14 +0000

    task port refer otherwise taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
