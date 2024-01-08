4913cc9ee421906e6775aa177d050dd5655147f0
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:38:46 +0000

    march book bringup find centimetre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import sys
+def print_python_version():
+        return sys.version
