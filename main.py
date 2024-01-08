0b2b196233a85ce32d1c4627f05ee9fc29606b5b
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:27:09 +0000

    splash dull clever ready regret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
