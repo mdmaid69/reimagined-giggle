eae50c6ee7a1f96aa416c475fb44cde7ed6015b7
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:02:33 +0000

    expression section past wait bang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
