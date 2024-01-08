6a56ee356d04668d2319d8456e039d72b604e2f9
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:24:56 +0000

    produce fight thankyou matter sea

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
