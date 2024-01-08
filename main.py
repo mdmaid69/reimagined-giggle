c49e2f051d719c49d80f47c6868202250ee8baa5
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:58:46 +0000

    cover workupto bar but generally

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
