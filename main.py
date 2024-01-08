3a9bfe4bd4ce91519f809b5fcefd1d11ecaf33a0
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:27:22 +0000

    rest smell meal history pull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def multiply_numbers(x, y):
-        return x * y
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def find_min(numbers):
+        return min(numbers)
