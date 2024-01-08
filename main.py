597d5265c8ad25500d24c0f9ad0dcee719daf4a7
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:24:35 +0000

    produce interrupt exploration cook pressure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_power(work, time):
+        return work / time
+import math
+print(math.pi)
