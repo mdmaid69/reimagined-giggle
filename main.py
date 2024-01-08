51c51fa2ba2d6e18f13b52d4ab460cd8a24fdab0
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:29:12 +0000

    approve strength chief hope approval

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
