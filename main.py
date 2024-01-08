6f9b0833dcf5595603452e50633a045b2668304b
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:51:42 +0000

    below limit willing tip foot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
