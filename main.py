4e2601ba14220272502b7972d2390b3178094b84
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:10:31 +0000

    573entertainment weigh solve avoid finally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import getpass
+def get_username():
+        return getpass.getuser()
