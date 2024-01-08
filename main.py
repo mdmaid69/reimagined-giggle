896e56f5d6458d2b46167201ff451aef52b4a207
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:16:31 +0000

    last programme fact shock accordingto

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
