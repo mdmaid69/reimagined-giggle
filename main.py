b5d1a5afc1302b9d47cc1bda67cb6093c845054f
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:05:11 +0000

    sight punish emotion room under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
