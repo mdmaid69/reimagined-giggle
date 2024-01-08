fec0f3fd7cacb1ac83ee5bc1e08a50f6fe57512b
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:03:15 +0000

    here burst makeup wild this

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+for i in range(5):
+        print(i)
