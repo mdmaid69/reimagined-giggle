941c06235ddb1c8a3fd2bbe3f482223909ce61ed
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:55:06 +0000

    swallow prevent hit level both

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
