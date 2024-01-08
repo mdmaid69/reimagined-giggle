07db966dd84d3da282904f7a65431929f6969292
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:28:09 +0000

    mess me object switch time

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
