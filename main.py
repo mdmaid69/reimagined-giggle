d835b05feffb3491cfc789fad7fe186c70404285
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:04:50 +0000

    sit bus suit variety failure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
