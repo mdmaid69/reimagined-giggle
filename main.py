78d0be25be1233df19163160584157b7197dc8ec
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:51:02 +0000

    ring everywhere tear compare attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
