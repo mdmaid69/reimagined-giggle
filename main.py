5b30aac6c0e80d0fd45ab53c34a68dd44fc27d9d
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:51:45 +0000

    record turnout die relationship instant

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
