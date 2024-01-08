6b61f25478d50dd678362835232019be01e1c84f
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:29:20 +0000

    communication opinion both sure want

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
