2f666811f36016107e07a3fcee21414a98bf7ac9
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:33:17 +0000

    rescue zero situation tidy return

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_max(numbers):
-        return max(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
