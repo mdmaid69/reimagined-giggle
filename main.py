37c0151247e1ca5c93c10d1099ac9d39cedb79b9
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:09:46 +0000

    worth couple share active branch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
