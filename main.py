8c432261e65f9ada88fce662075782ed1a0a0219
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:46:46 +0000

    interfere lookaround later complaint discuss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
