e5a368f2dc63524fe3867ffc3b44218795a0bde2
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:57:02 +0000

    572enter head sit develop next

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_speed(distance, time):
+        return distance / time
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
