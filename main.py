2e5edc8917eaeadabded8ec5b7f6d18afb7bc496
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:38:12 +0000

    plastic battery workupto interview rush

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print(time.time())
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
