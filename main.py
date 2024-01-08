de8ccb391ecccb0600d670b71532e4cc5beddb36
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:45:36 +0000

    complete majority interruption control lock

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+for i in range(10): print(i)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def convert_array_to_string(array):
+        return array.tostring()
