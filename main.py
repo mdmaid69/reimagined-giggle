b355fc7fcdfcfc6d57eea6a8d8031031ef409958
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:25:20 +0000

    perform female famous wash wander

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def count_elements(lst):
+        return len(lst)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
