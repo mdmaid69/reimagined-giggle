15024620583dc9aedd589af1b0f063b4106c2da3
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:32:50 +0000

    reason information ago win comfortable

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
