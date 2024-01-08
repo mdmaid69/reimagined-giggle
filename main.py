10a02a12b5be57852fcc65a5d4d2c6b7c6aa5548
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:41:11 +0000

    recently hear pay low turnover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+def calculate_volume(length, width, height):
+        return length * width * height
