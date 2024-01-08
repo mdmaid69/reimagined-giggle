c6f064a363c47f1ff7e9e7f0d0f83aed828d2234
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:19:10 +0000

    lookafter furniture slow chance average

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_area_circle(r):
+        return 3.14 * r**2
