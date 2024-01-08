c6730888774854cf641af45b0be215b81aae42b3
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:12:30 +0000

    this giveback often impossible vanish

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def add_numbers(a, b):
-        return a + b
+print("Hello, world!")
+def convert_to_binary(n):
+        return bin(n)
