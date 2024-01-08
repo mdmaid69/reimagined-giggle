fd1fd024fa2f7e1a692f69a9494813eb3acd1e58
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:49:49 +0000

    appeal lay will uncomfortable workon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
