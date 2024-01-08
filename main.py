1fe7659e2fea5afae1ebf2e080ffff8c7a3fae33
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:41:17 +0000

    when raw sudden lock colour

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  def remove_duplicates(lst):
+        return list(set(lst))
