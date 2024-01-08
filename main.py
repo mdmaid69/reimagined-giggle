1e7d643392a32fa604e612f9322e53bc70dd107a
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:56:57 +0000

    deal doubt complete demonstrate Ee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_as_set(array):
+        return set(array)
