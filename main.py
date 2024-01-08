1b750662b7b531b988ec52e568e4dd13fe7b9ff4
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:49:01 +0000

    add cut confused cruel spite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_set(array):
+        return set(array)
+def find_unique_words(sentence):
+        return set(sentence.split())
