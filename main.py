85ae33a8700c4f2588ef1bbb07986082fb5fe8b9
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:39:25 +0000

    operation family treat speed soon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
