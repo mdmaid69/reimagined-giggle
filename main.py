0e0b74447aeb32727fea3769e455f0221da9e0f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:06:37 +0000

    spot week operation roll fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
