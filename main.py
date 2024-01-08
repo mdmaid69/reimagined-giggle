f84fdbc45f7941b62cb5b2a57b5c1d09310f352c
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:03:19 +0000

    warn admit brother wise properly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
