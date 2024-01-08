fb7b86cb676b2e4932361cac8b9025c265efc384
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:54:32 +0000

    bet Zz treatment false attract

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
