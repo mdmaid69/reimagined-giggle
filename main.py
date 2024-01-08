3a5802315a60a4258c03eabf5ca0dd0c14616e29
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:00:19 +0000

    slow apparently harm book pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
 import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def reverse_array(array):
+        array.reverse()
