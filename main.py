b8b67989cf8eac8fa062d9ef1b63bcc09c27bb2f
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:24:34 +0000

    getaway comeback picture trip less

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
