b451d73d0adf450fbed1e96563338597f7afc93b
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:02:35 +0000

    instruction store smile when round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
