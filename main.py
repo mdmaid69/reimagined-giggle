7d84cc9901e29660dde6c2d54c3fef3d5232c437
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:19:30 +0000

    drink record pupil whenever underneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  def count_elements(lst):
+        return len(lst)
