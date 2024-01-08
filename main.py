ed3a63d956868c25dc33fa5f7035b78dfea1e334
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:38:32 +0000

    sign must uniform film speech

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
