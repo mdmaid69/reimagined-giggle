f0739e0d1c54e8bc11bd8824812770a303ac9e33
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:06:33 +0000

    keepup already region eager point

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
