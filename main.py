bff921587f4c576cd74dab8ea2f950254e5f4e48
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:54:54 +0000

    impatient strict operation drop proof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_density(mass, volume):
+        return mass / volume
