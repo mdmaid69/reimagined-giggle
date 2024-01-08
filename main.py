e89621cbb8876c8510fd19415b22ecc751c820d0
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:32:19 +0000

    tough thick callup undo shine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
