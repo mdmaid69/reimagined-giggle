8783d669e0370cebae35a76443febd302e950287
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:22:10 +0000

    lift justice attractive language fashion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_max(lst):
+        return max(lst)
