9febcedf5c5004ad8149a31c8520e6a5c3f10d33
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:07:05 +0000

    communicate machine than tip onto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print(sum(range(10)))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
