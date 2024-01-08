13e2f343872d46901c7cfc50b9176bc6384685fd
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:38:42 +0000

    boring yesterday bin affect admit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
