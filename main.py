26373ccc265c50612da307b656d7c412104ec8b4
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:18:16 +0000

    nigga excited trade hurt patient

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
