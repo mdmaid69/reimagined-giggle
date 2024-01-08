9389d931dc486dc9e0da75f08835e0cd48686de5
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:28:42 +0000

    brief practice reasonable dull short

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import sys
+def print_python_version():
+        print(sys.version)
