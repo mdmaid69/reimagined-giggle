3f4f18e30cde40de8fdf968f992c446c1a314b0d
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:26:45 +0000

    confidence scheme frequent block comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import time
+def get_current_time():
+        return time.time()
