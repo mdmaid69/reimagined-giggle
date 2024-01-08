fb3d3ceee85478f67b3944d99bc0e1a4c01c24db
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:05:45 +0000

    closely round street call ourselves

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
