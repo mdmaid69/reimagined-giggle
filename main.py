26b2fffb824e0bd30c139095b20b65bed34139c3
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:57:57 +0000

    treatment necessary high practical modern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
