b7df551799307f6e42ccf7a952d01ad9ad9f2538
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:33:23 +0000

    age desperate Gg equipment return

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
