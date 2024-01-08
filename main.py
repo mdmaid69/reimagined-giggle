3f3a2ac31f9562c7d6eb9e24d3fe2df87871fa99
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:21:32 +0000

    wonder detail trap strict talkinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 def calculate_mortgage(principal, rate, time):
         return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
