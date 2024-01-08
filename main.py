36f970fb9294e70774c9c8c4a23ecf77d5e7f9e2
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:53:54 +0000

    serve complicated sail season apply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import time
+def get_current_time():
+        return time.ctime()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
