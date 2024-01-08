3563f31dff6f3a1d053ec137ae99ec7fe773b188
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:10:13 +0000

    keepfrom ourselves shopping already arrival

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
