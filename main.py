2cf7959f38ed7d27501c45447fff5262698ab3ba
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:06:01 +0000

    tread well entry sink commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def cube_number(x):
+        return x**3
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
