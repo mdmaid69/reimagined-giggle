83ce15b8b213956bf1d59d9b168a1f0ad9357377
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:26:22 +0000

    valuable letter enthusiastic mixture scrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
