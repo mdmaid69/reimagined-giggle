f9d08b47ef5fc36008271c9f9eaa38deb94cdaa9
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:04:08 +0000

    uncertain pronunciation break good reason

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
