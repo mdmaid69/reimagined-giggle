3202a0a113b27ee8ced82c6bb01c6521be1d2c83
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:34:22 +0000

    mysterious confusing start comfortable stone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def get_array_typecode(array):
+        return array.typecode
