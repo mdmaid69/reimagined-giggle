4900961a6e1e75c04c4f28925710ae34d8f4d457
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:43:54 +0000

    print general could callback familiar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import math
+def calculate_sine(x):
+        return math.sin(x)
