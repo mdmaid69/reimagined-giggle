98ce85865973afc832238aaee8a4645911105ed0
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:23:56 +0000

    turnoff high legal score light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
