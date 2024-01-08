1e89a0fa6145aefc2f2cd1c4021c64e3bdd6f418
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:27:31 +0000

    serve apologizeapologise wish introduction see

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
