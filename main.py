ff373b3f14076a9a43b095e572d61a14522dc43e
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:38:46 +0000

    disturbing tidy regard turnover brush

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
