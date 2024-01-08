cef531115f45f111827b3374dc6fa2333c888018
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:42:25 +0000

    back modern favourite sick door

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+for i in range(10): print(i)
