34783d657bc30df4a4cd1740cff3883edca881cf
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:39:17 +0000

    soon trip fix getoff character

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def square_number(x):
-        return x**2
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
