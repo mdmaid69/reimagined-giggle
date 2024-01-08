b9335c9225558f3d492a6aa6f3ea671ed31bf7d0
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:54:14 +0000

    contact firm left unpleasant lookafter

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
