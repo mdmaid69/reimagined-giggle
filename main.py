44073aae3692de9908f73166087151f173537f30
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:10:55 +0000

    main benefit brave statement pump

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
