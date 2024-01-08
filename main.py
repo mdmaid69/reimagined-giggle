dcc03abffdfe99d30053f1e8aba9187e39bfe4fa
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:00:25 +0000

    criticize under different care search

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def find_min(lst):
+        return min(lst)
