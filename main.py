1c00ce2e794d3cd47f74431cc190f482ad9021b7
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:48:47 +0000

    possession fork sign add prove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import sys
+def exit_program():
+        sys.exit()
