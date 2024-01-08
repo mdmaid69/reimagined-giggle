ec85629b4987d0072270ccdb958336ba24dba3d6
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:24:45 +0000

    arrive mysterious injury for shop

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
