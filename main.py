77fb03cd5355617e8699800fa47a5514e6b30f02
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:15:31 +0000

    build circle boat along original

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def reverse_string(s):
+        return s[::-1]
