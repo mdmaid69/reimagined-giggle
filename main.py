427169f1f165a15cd628c7b853a6a95f0187a1b6
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:02:14 +0000

    sure your popular pen apply

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
