d8a78a45b9d4cb13e26a6aa6228e830317e8e57d
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:27:18 +0000

    bin bang scrap exist tense

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
