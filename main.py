b8bcf152c1d463dd29260927038014cab45c35aa
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:05:38 +0000

    otherwise floor acceptable important deliver

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
