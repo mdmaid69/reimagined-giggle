2ba20643f5931c59f5c8c83643a2d700e66ff3b3
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:44:24 +0000

    comeround drawing giveback often freedom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
