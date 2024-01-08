f1f4e9358d96c9ff17796490333b5d5fb1167a91
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:45:28 +0000

    trip copy as file exact

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
