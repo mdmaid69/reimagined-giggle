f8fec13871ba4eba7458cfbe07b4d72591ac3f58
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:35:03 +0000

    begin delicate cost top breakinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
