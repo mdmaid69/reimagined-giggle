20e2b891596511a8254dd67abf1415ce95f50186
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:43:10 +0000

    same interesting shoot rubbish violent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  def count_elements(lst):
-        return len(lst)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import array
+def get_array_as_float(array):
+        return float(array[0])
