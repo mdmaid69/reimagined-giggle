e24ea6d413a9d08a84260966dea6d323030e7c22
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:40:27 +0000

    love ticket into violence pin

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import time
+print(time.time())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
