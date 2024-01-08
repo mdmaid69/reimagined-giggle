311fb126b583ec522d202305335cee9fe51c162f
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:25:40 +0000

    compare study getup dull makeinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
