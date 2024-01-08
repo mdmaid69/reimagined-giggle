0f28132ff6a8202920e3c5da87c963be737ce60d
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:57:26 +0000

    shut above brain heart Rr

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
