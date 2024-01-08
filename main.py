34cd58d93d77aa53f1d82df5109822cb7f681ccd
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:28:15 +0000

    position surprising match only Ii

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import time
-print(time.time())
+def square_number(x):
+        return x**2
+  def is_even(n):
+        return n % 2 == 0
