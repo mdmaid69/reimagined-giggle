41fe534437999d6283c1608887d2239e72b8b080
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:37:00 +0000

    address below juice rule awake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
+name = "Python"
+print("Hello,", name)
+import os
+def get_current_working_directory():
         return os.getcwd()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
