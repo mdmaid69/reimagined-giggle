272912538a3691b58ad0c97e341f562a799e4b56
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:07:42 +0000

    disappointed sudden impossible press base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
