2b45d89295c74de4cd982d917ebfc008b6f138f2
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:37:46 +0000

    rain thumb relative doctor backwards

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
