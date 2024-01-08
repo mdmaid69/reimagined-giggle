1e7d3d4cefbafad45b8956eafefa1977917e9e16
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:42:53 +0000

    embarrassing present everywhere persuade Oo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
