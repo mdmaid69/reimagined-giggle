05d2e088b23a2c3c8b20a7c68f627c7f4840f6ee
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:35:56 +0000

    often watch Ss Ss cost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import getpass
+def get_username():
+        return getpass.getuser()
