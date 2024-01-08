9fc0c68751013600bd91eb5ac6eaf49af2243f96
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:52:49 +0000

    attitude achieve sudden aware major

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_work(force, distance):
+        return force * distance
