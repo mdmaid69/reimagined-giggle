9b5f72679b942aae5e351ba689974bed2e2a4129
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:55:36 +0000

    nigga shelter bag lookup tool

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def add_numbers(a, b):
-        return a + b
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
