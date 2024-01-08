8a513ef5447b5989598d26c915595537c5e84532
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:09:03 +0000

    central point doctor Hh pull

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import math
-print(math.pi)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
