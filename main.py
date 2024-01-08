7f2611fad26a74cfffb8b453ba0dc909f6d51812
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:52:03 +0000

    row rent extremely recognize inquire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
