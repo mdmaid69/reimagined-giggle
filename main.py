0055be8efe87e5337683d0a79b85416a25fcecde
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:40:26 +0000

    apart rest somehow contact travel

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
