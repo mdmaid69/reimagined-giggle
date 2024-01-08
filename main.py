5e2eae7ea3b744350faa78dc01798138356cd281
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:47:16 +0000

    burn listenfor the least tone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import math
+print(math.pi)
