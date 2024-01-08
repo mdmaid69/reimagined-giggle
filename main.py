14a5a600f5d37be6d2cf1e5726ba9914e3ae0c06
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:57:39 +0000

    somewhere life handle tight advice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  def convert_to_octal(n):
-        return oct(n)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
