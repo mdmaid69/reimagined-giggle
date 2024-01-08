147642b6257f97a3ff6b22dcc0fdd578543cb0c9
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:13:15 +0000

    suffer yours disgusting part birthday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import sys
+print(sys.version)
