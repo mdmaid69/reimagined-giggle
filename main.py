89788be346bd31103e82db32f3980dcc1dc4d303
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:03:38 +0000

    copy protect recent cheek respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
