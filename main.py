2acb91e596380d10b020a1a209114ed47e8a37f0
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:08:11 +0000

    connected entrance high grow old

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def get_array_length(array):
-        return len(array)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
