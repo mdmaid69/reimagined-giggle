08db26cd4bc92306c41a6b5de1e009ff1133d59f
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:50:25 +0000

    brief amount basis cry support

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
