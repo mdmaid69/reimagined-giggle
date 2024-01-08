313f6cb442a3329fa5b1971234739823df39dd50
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:02:56 +0000

    familiar unable shoot future turninto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
