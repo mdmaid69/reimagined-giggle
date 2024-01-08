805e7da37e8e8bbb972254a94e9d4fc50d151ec0
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:11:54 +0000

    mess I boil fill course

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
