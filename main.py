791f97c725aabb53567ab801ef778312c0d83844
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:36:45 +0000

    place introduce value plenty central

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
