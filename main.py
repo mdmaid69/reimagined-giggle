fd6032c774e7d84c9f81c20e44411c6b8424572a
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:42:02 +0000

    case taste small tie flow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
