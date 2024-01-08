b7665f2aca4e4072560289d64191d37568f009b6
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:24:02 +0000

    afford correct prove switch important

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
