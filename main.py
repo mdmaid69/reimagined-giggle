3caebcf6ab6c7065e70f6bd6dd83f327b6ae262b
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:49:17 +0000

    this mess radio should constantly

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
