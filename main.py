79562f26664be398d3269ab0b62133bc85b5696f
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:59:16 +0000

    street stage rescue Itself immediate

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
