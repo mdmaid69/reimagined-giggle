58333f5ddaae02d9c30392df1907906057db6d79
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:36:27 +0000

    call word church slice interested

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
