148fe06e975a5a86743d33b2d2c56e78cd5145e4
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:36:45 +0000

    ever answer culture side dance

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-for i in range(5):
-        print(i)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
