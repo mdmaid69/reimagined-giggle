af0e835fd5c0163c718ad8bba5e2f3dc3350411c
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:45:45 +0000

    regard afford flood fire arrive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
