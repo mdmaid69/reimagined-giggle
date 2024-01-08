7e299824c4b9d81ea8650f621c7587f867754e66
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:33:02 +0000

    minor treat main lookaround mystery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
