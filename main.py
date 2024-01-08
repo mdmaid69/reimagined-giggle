f8ac8bba029ecf650dfaf88576ecf93a65e2baa3
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:22:03 +0000

    birthday remove theirs charge prepare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def reverse_list(lst):
+        return lst[::-1]
