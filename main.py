d4e80929a7e3fbede9631ed9d10cc15eddfa68f3
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:03:11 +0000

    string amount spare recent bin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
