75eb1c764dcb376e6d8750bb4fad48c66a06d382
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:47:01 +0000

    cruel black label sorry rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import sys
+def exit_program():
+        sys.exit()
