cdfca45923371c81c984831acda1685ed71834d2
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:51:35 +0000

    fight regret health pair male

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
