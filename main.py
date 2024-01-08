b60e61218dd242ddd5d847c6c2a0328f392b8889
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:06:17 +0000

    control either after comeover fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
