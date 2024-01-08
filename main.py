622a9de16429c716bab3e739d4d4a1006a9cd105
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:29:10 +0000

    careful style sound behind kind

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+def get_current_time():
+        return time.ctime()
