78c48b6e9a06a58a81e689c1aebd0a5f55dfbebe
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:43:22 +0000

    thought complete second poison bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
 import array
 def remove_from_array(array, item):
         array.remove(item)
-  def convert_to_hex(n):
-        return hex(n)
