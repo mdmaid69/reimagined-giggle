4390a02d64949757c1fbd55782c181b2bf15157d
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:14:35 +0000

    blame afraid shelter crisis belief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
