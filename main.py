2641138dd2fb479c2af7a139b5d796db75d91541
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:18:32 +0000

    shock ever trust act switch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def count_elements(lst):
+        return len(lst)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
