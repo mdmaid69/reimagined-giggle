0844ff6b5a8b06a1db560db369f4d539694ce3a6
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:20:24 +0000

    safe blame love agree holdout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+name = "Python"
+print("Hello,", name)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
