da1c4b48db126977786c05d44d4a4ccfc0fdc162
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:35:16 +0000

    stupid fun culture drive perhaps

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
