46b3fe74b30c71724c4e3d17c7354f190b89559f
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:26:30 +0000

    attitude since walk past formal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
