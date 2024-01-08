d5fa9a86b435f43bf2d2ce13588dbe45fa9887e6
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:35:00 +0000

    trial scheme me lesson danger

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
