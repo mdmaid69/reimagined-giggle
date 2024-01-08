f3ce4ae2019a8ff78792672f38ba27b363ac8307
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:34:42 +0000

    fashion build wipe 572enter attention

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
