f4e5116c8f6aca82e50ca11b0e793612eddfac8a
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:02:07 +0000

    move advice listenfor control clean

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import os
+def change_working_directory(path):
+        os.chdir(path)
