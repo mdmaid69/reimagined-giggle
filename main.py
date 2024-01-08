3aed76c20cbc4db71ea12371a949bf7749528380
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:25:03 +0000

    legal nose question fortunate on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
