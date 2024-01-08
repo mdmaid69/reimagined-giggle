b5675899a0fd87f3060bb57bba0f44262a651696
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:44:43 +0000

    makeinto pause thinkover couple workon

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
