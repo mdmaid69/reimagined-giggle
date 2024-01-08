e997f7351f892b5e29f2bd32d86c2832f1bd30fc
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:40:04 +0000

    trouble size teach promise ashamed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-print("Hello, world!")
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
