4b58ecfebdf807b27f61c6812555fc53f6c786fb
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:39:34 +0000

    skin lean person skirt air

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
