1a5187335dc437c68de405cc0829d4a30886ad85
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:45:27 +0000

    forward night receive closely experience

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
