7f96a90e70e68d681f91fe1cbe48cab1d5c7e045
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:03:56 +0000

    almost please sort both trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
