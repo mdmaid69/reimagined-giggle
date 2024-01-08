477cb4609490b4f0d55e7fd75313f0fbe19eb93d
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:37:56 +0000

    bang hard string content finally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
