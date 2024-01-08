d8532c157b3215782e49c9abf12d143657364e27
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:24:13 +0000

    runaway turnaway polite complain opposite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
