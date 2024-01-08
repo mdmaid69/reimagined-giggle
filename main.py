81f02ad240e624812b69d946f2e723d2bceef6c2
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:42:10 +0000

    confusing declaration introduce rescue copy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def sort_list(lst):
-        return sorted(lst)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
