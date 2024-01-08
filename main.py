60274f3db11d895d0455041bd995155da69333df
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:15:50 +0000

    centre work comfortable funny class

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
