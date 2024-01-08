dff6ad73105840b419b38b835939002cb5603b6d
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:20:33 +0000

    maybe flow wet tidy point

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def convert_to_hex(n):
+        return hex(n)
