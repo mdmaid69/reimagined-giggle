6f0ef9411b4ca0e6211db2ab62be8548b9529f26
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:38:04 +0000

    used swimming corner secret fortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
