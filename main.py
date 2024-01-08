1dff0746776bbaa6f0669cf7ac116f6a346b18bb
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:29:05 +0000

    heat stamp curious discover business

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
