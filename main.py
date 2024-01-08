b1ba4580b968f7e601559ed6f3730b0425f0cd14
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:47:37 +0000

    rather room search over suit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def calculate_pressure(force, area):
-        return force / area
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
