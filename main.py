d38f6d49c119d64f06cbe67cfbce41d73e52df17
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:47:10 +0000

    obtain spoil squash whistle grade

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
