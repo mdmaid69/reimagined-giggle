8ebca9d02e751fb23d08962e92da28492ba7e79d
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:35:49 +0000

    obtain region definite instruction track

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
