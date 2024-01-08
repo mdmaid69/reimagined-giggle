92b64d8a1be3c4a4f215c8b1e84f7ba5daf7786d
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:22:35 +0000

    demonstrate certain kill bringup correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import re
 print(re.match("h.*o", "hello world"))
