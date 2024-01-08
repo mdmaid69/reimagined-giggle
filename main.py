376e47d5943d83a84c695b3479058194fe3386bb
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:25:44 +0000

    official design march price violent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
