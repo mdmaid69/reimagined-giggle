9e53c5b5003bdec21ab2eea3345c363d0beafee3
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:37:50 +0000

    keepfrom behaviour draw backwards building

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def delete_file(file_name):
+        os.remove(file_name)
