3b1510afd45da41fc6c71170248a9f4f0d47cf4c
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:14:55 +0000

    shopping music properly border pick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def convert_to_octal(n):
+        return oct(n)
