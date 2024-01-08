5175cc60d1eea517d634ce54f891231816f7a0d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:57:28 +0000

    government advice music cost stranger

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import os
   def get_file_ctime_ns(file_name):
         return os.stat(file_name).st_ctime_ns
