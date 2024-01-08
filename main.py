293178a116a390025292000c96c724a4a07ef353
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:30:12 +0000

    wish play original defeat who

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
