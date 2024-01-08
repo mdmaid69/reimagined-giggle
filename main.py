e6a70f0f1764abb82867ab049b77eb8ed731742e
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:26:12 +0000

    away sea introduce seriously disagree

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
