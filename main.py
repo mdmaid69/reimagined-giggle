6405c0bafeaece0c14f9e429b2dd152fc42c0174
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:30:50 +0000

    lock result wet pin remove

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def convert_array_to_string(array):
+        return array.tostring()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
