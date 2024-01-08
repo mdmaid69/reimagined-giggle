7c73187636687ca229f47c1246f6fbea26f32aa1
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:48:14 +0000

    respect exhibition available threaten excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
