dd630ce809e923cff320f867a0751f14a7ca933f
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:55:21 +0000

    hole best divide bet little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
