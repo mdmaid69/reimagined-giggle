0e0dc67c7c85ebd97a1e7e26905a0237e71877db
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:10:25 +0000

    shop mark wide journey sit

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def get_list_from_array(array):
+        return array.tolist()
+text = "Hello, world!"
+print("Reversed:", text[::-1])
