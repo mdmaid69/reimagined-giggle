f54f84fb957c96c0120968e08811ad68b27e7b32
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:55:59 +0000

    keepup ignore combine direct manage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
