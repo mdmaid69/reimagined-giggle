8f0806612b96403d23f08bff0a49cb88ac80dc32
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:56:27 +0000

    advanced angry after loud rent

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import datetime
-print(datetime.datetime.now())
+def calculate_work(force, distance):
+        return force * distance
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
