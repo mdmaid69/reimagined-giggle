4513839b04cdef92713bc095cc9b20149df47da9
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:35:49 +0000

    attract better direction conclusion thinkover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
