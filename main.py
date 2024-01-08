40229e15e7184e0bef10fd97894ffad9a709fc2f
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:33:41 +0000

    put crime balance face protect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
