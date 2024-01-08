3890062fce4c10d193b8d56587eff6fae20137f7
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:21:39 +0000

    bored fine trouble free case

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
