a78eb0bcbc9ef65fa2d37467a75fd50f8cc18bf9
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:07:47 +0000

    goout react whisper independent female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
