2b756c55031abeb5c3b2239659e441e02e730781
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:09:35 +0000

    crash wind please cross appear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
