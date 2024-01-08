51a3fc7cc5bec99b59d2537707404b505b3f5e9c
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:47:46 +0000

    content available reserve reduce swimming

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def cube_number(x):
-        return x**3
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
