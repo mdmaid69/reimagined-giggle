c550df3b5111ec82b24ffa8f51369a8ed6e4a369
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:31:55 +0000

    correct mend brush appear confuse

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
