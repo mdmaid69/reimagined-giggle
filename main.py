5c6ad44c2b93ca5c501751dc46485743c22f6400
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:28:49 +0000

    impression judge whenever switch under

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
