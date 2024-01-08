b1b3d49c8d985213a56d96af458dc68b9e955578
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:22:03 +0000

    ignore me cause depend exist

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
