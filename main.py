52d0116bd07c2a974a7e031db84a48abf9ad33d4
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:03:54 +0000

    quantity marry keepup receive to

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
