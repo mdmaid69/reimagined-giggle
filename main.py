d5cbd09be40f3f3c1a5a8a6971f21054c51164cf
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:34:22 +0000

    steam bother draw nigga basically

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import tensorflow as tf
+print(tf.__version__)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
