5ccf395c5914e9e34f463b7cc428b6f93564f2ab
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:03:18 +0000

    secret clear pass own comefrom

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import datetime
+print(datetime.datetime.now())
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
