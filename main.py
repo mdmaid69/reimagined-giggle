08dd144a2db98393b6b465ff33350b2834c701e7
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:16:22 +0000

    cause all male quiet community

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+print("Powers of 2:", [2**x for x in range(n)])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
