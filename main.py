995891b0ec8caed61dc42b59a4e33a7dbcafc6a7
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:36:49 +0000

    file window list beat fond

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import sys
+  def get_python_version():
+        return sys.version
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
