c373d5fd3763ea0701f0d5344e93181cbff9d3ab
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:12:19 +0000

    immediate left poor material prize

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
