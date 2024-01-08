b036f51811ddc2329a7ad54514bd0ff71c492b82
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:52:29 +0000

    sleep age die cruel complaint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
