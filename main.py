d4875a86aad5325b26393a15affe73cf813aaece
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:33:27 +0000

    cover connection moment transparent consult

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import os
+print(os.getcwd())
