008eb3025814af6e8dd8b6c99d22ba142b8e5e8d
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:21:06 +0000

    round represent she still classroom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_float(array):
+        return float(array[0])
