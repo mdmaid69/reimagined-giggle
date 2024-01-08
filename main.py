7e2fdd8eb7af0243cda16c8de28e5ace889fa4b9
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:48:52 +0000

    flat outside discuss generous close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_as_float(array):
+        return float(array[0])
