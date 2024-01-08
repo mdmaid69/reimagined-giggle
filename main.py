91fc7e45b56972ccae833b2fd5e607fcdd99067e
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:20:00 +0000

    lock jump or special way

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
