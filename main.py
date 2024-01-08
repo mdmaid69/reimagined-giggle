ecbdfb79f90a584bc4693f2c2b8e2e8bfebba02b
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:42:18 +0000

    spring maximum study age giveout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def get_array_item(array, i):
-        return array[i]
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
