bd56fc1699a0b4b46c9b2019bd88b840ab1ad627
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:32:56 +0000

    anywayanyhow mad there before side

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
