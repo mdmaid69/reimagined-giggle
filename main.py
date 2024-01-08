5baf7f56a47c91da999ee67262c37cd293e17513
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:06:37 +0000

    about offer lookback(on) quit stupid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
