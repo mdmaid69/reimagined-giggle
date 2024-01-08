fe269b14de13c1a5d3be056d8b71390d1299a2e0
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:32:55 +0000

    limit stiff basically contrast justice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
