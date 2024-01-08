7dfc40e5b4ae7f7711a9cceafc73cf3f5670986b
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:39:09 +0000

    visit minute evidence say fork

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-for i in range(10): print(i)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
