ae78370b998849a6ce6b0f9de3698652d1f58f73
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:46:41 +0000

    talkinto work advise here brief

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
