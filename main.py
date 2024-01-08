bdfafbabc56bc279b8e8ad63b170d175695a5a8f
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:04:18 +0000

    pinch no date breakfast sing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
