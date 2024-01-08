e45d2807d7611a2ba472515a23d61cbdc83f8e4c
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:01:55 +0000

    better apart advice slice hope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_density(mass, volume):
+        return mass / volume
