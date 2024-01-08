e8ce0b57bed70f0ff6fec473b8957c6b33d7a286
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:37:58 +0000

    sense in thorough class bringback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
