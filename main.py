02358937983687cb17f1d7bde99a708927254fe0
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:24:53 +0000

    lose knowledge unable low cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
