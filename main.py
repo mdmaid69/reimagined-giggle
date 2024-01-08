a82558da43daf84e616d84f908c54dfd9420e046
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:43:48 +0000

    lift them switch kill level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
   def set_environment_variable(var_name, value):
         os.environ[var_name] = value
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
