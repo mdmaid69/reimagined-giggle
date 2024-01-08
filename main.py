90c5310a10deefc06950eeb1b77fed89ea1b550f
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:07:10 +0000

    advantage frame snow worth calm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-for i in range(5):
-        print(i)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
