1bec7df07975dcccdcb4c45f5974f0e1505800be
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:21:59 +0000

    pile runout equal hospital write

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def greet(name):
+        print(f"Hello, {name}!")
