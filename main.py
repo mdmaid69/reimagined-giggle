08a90a1409bf2d6b4489d9fe0c58e963f904346a
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:55:51 +0000

    couple stiff thick me especially

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
