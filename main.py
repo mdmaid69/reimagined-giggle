fe77387344d534b4e65073ecc7c1e77aaf82fc53
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:54:03 +0000

    extremely holiday maybe alive ocean

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_power(work, time):
+        return work / time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
