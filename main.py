8ec5e121bd8632312990a8578e37af666c2c75d9
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:52:29 +0000

    pain ask blame common care

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
