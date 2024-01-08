763ec680f5bd93aba87d2b6a7cb62767ca9e597a
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:20:01 +0000

    steam mad past thanks appearance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
