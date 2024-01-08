cd319072f5808803ba1d26916e1e494f68c0c759
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:17:25 +0000

    grateful freeze back cough punish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print([x**2 for x in range(10)])
