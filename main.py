85f30cffd2e7f1067986b3c03283ee3f294bc72e
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:28:35 +0000

    cook depend trouble ready straight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def convert_to_hex(n):
+        return hex(n)
