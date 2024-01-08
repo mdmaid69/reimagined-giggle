9f763b407e4e311981189831f6910c9b5c9dd9c1
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:39:45 +0000

    equal only body keepaway lift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
