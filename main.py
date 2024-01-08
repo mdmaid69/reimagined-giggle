c1617f61d172607ebe3a8e39a2061040c63835fc
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:47:12 +0000

    Kk well wake hurt together

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
