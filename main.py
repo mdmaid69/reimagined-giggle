cb4a3ad0a588b64d8dc3c6a9e86b25ffcc71d3e9
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:58:28 +0000

    destroy pump slowly pipe favour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
