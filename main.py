1e04ac9c76fff1bf67b66588f8392a20f77ee848
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:49:11 +0000

    influence doctor do circle create

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  def convert_to_binary(n):
+        return bin(n)
