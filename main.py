60e4ecdb312588fae65a1c81378de78047edd1a6
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:29:02 +0000

    obtain fortunately only minority battery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-print(time.time())
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
