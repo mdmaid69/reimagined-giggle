67e743bf4f5a69cdbb474bb3ed85148088ee19ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:29:58 +0000

    progress passenger rob admit hard

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-text = "Hello, world!"
-print("Characters:", len(text))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
