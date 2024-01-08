9f69075b0ba446867f229e1d9d46de660ab33026
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:38:04 +0000

    badly beneath terrible geton attach

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def get_array_item(array, i):
+        return array[i]
