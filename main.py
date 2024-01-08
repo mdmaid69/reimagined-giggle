ecb8dab48080d97da1e5ea966a4d48b1e925916b
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:58:29 +0000

    beginning harm wheel takeover as

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
