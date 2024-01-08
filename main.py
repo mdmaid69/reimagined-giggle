f2dcf4578e5fb58da8a0c15e74af0af9e1f1ab1f
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:02:56 +0000

    workupto invite pollution fall side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
