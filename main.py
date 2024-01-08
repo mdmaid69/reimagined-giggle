d1e801bf58836845497a5e8c7e9c80fa90a9a6c3
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:24:30 +0000

    whom nothing Dd hollow some

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
