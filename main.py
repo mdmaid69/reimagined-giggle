fe99d688b9af5c84c43f005123556a857b9b1e96
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:31:33 +0000

    repair rain care describe occasion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import os
+def get_current_working_directory():
+        return os.getcwd()
