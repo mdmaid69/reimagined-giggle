08e9131b89ffcc20f6d7d1700a6081d92172f4e8
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:10:40 +0000

    defend scrap mention tie scene

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
