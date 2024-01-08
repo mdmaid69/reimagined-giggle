9ea64ffb227bfd2d3dce6dee7d15daf7a4557a6a
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:06:59 +0000

    transparent club platform poor nigga

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def add_numbers(x, y):
+        return x + y
