f04ec1ce1edafdac2471776fa89022f280214142
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:23:23 +0000

    tip yesterday comein demand worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
