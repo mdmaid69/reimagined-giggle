4777eda099de779d91b9462b2f3ecca5f1b572c5
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:48:32 +0000

    one hello turnout keepup anybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
