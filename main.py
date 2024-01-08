3f452746ce85723ac0a1d23ddeaff282f1e16961
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:06:19 +0000

    obvious fill lookafter worst identify

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
