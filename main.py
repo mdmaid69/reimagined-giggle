2a493c7ac0b74d7788863866476998a101686cdc
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:19:58 +0000

    ashamed job appearance song long

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
