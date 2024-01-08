fd91dfc4799124df663af3d5c0a0af12f909b629
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:39:05 +0000

    separate direct let runout surface

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
