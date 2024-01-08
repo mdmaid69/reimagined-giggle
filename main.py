5648ad2d96d4c405565b2f89ec53fd628b38b71b
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:37:02 +0000

    angry bend top exchange call

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def convert_to_binary(n):
+        return bin(n)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
