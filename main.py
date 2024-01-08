f9163e5a923ed2592d6ce5fc29660f10296916b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:41:53 +0000

    high fork guilty cheek noisy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_pressure(force, area):
+        return force / area
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
