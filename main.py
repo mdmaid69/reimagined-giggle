05be5043a4188239532a3a4a400ae85c0103ac74
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:56:58 +0000

    getback pencil justice sentence possession

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
