02315876316ea7b503951e81f1967a73d514f600
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:25:42 +0000

    depend fast describe apply remark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def split_path(path):
+        return os.path.split(path)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
