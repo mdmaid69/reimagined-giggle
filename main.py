9dd8ca3b2ba6c0e1e7ce2a2cbec64fb1359dff1d
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:50:08 +0000

    giveup silent seem stiff love

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
