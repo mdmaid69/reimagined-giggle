03e64607679e3b59d33e24f42a90a9972d152ebe
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:59:17 +0000

    pencil bank shine guide press

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import sys
+print(sys.version)
