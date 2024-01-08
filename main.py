bd2698b5277ef1cd7971fe551e09c569343770f4
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:39:50 +0000

    dare makeinto true occasional tidy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
