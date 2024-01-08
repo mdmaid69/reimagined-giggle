51e089bbe5b1e4d0cbcdfbf88dadad19a5606e4e
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:17:55 +0000

    Vv family stupid clever both

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def add_numbers(a, b):
-        return a + b
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import sys
+def exit_program():
+        sys.exit()
