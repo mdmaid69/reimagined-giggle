7e4edc073f9087acc1e6b10d8cc7b44a21f70f60
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:18:00 +0000

    comeoff gap identify improve heavy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
