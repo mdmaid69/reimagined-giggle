62dcfe455be6c67640c1bb8a926ef581cd49988c
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:36:14 +0000

    while like affect reason greeting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
