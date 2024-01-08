128773c62f906eae2666533ab28dda526f2c4152
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:30:15 +0000

    pot least health mistake goaway

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
