7b11f83fe8bd6b55ced29ea9f840805dda7201dd
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:00:05 +0000

    test connection earn medium activity

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
