b42e6d22e440f499a48edb408474ceccc61c8100
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:36:40 +0000

    discuss deep dangerous call mine

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
