119b99509ffd87f323f1f2c8f8e7537fee6a9a07
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:29:47 +0000

    popular why dream debt usually

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
