59ac0b13d7bd733167fb70fda30c69812dce224b
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:36:57 +0000

    trade hurt steer sign mysterious

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
