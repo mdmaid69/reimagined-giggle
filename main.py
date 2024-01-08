bce3f9edc34ffbdd9965f54e44247dc3884664f5
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:56:11 +0000

    protection murder Tt trick punish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
