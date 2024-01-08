373a58313607438cbe75e9aa5fc4b5d07d083ef2
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:02:45 +0000

    repeat image new treat 572enter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  def count_elements(lst):
-        return len(lst)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
