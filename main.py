f37765857511402f3d996dfbe044a572a215e1a6
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:17:42 +0000

    friendly strong announce goback(to) classroom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_combinations(n, k):
+        return math.comb(n, k)
