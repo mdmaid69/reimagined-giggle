86603cd1c5a3cc64986f5a4da6e1b7ef6bf79e49
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:15:58 +0000

    well defeat private modern rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
