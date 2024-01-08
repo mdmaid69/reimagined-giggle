29baf95304bc0ecb529e7d16649ca504afb3979d
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:30:35 +0000

    wash statement forever television in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
