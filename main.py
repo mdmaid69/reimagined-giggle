a199684588b07cd46f7613d37df49c122b09ce97
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:47:18 +0000

    load just relation brain account

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_index(array, item):
+        return array.index(item)
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
