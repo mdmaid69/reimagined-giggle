3078e0001705ba1229ee98d5697d7be8e698d66a
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:35:09 +0000

    lesson callback free room characteristic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_list_from_array(array):
+        return array.tolist()
