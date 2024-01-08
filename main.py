8d6833f0f8fb521421f9670f0e286c70cdf05ab0
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:05:50 +0000

    border level benefit success throw

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def divide_numbers(x, y):
+        return x / y
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
