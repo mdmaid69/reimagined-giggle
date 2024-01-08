fd0899c8a686c1bff1f0d5cc2b1dd0ba8cbaee7f
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:22:36 +0000

    they effect goodbye price affect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_union(list1, list2):
+        return set(list1) | set(list2)
