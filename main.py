8da84ffb66caccc578ae70bcc9d2276f157bb1b6
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:12:19 +0000

    welcome definite drive mood quit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def convert_to_binary(n):
+        return bin(n)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
