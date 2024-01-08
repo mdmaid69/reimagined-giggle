106470406abc8b60fcfe2fa6a9ee271ce339b0e1
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:23:36 +0000

    house left business test form

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
