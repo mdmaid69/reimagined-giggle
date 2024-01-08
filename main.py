3d53e25e527f65dac58148fff607c7254b99a293
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:53:29 +0000

    point Zz fortunately one tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
