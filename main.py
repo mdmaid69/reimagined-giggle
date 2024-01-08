609d6a97b53395ac99bb3f5137b4182a025eeca9
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:57:05 +0000

    record rush entry neither luck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def convert_to_binary(n):
+        return bin(n)
