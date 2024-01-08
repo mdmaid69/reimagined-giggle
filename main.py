e00ebb973083c79a6c9d8134157d0e1e683ad917
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:26:20 +0000

    process Qq involve adjective find

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
