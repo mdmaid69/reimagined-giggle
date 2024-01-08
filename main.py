c915079635aa816b45418017aa056fd1e39924bc
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:27:24 +0000

    exciting complain hole could hurry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
