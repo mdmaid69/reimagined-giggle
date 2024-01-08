bf9ffdb339e3d4c950680c9b84aef159b4f09e8c
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:23:16 +0000

    strike club scrap other bank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
