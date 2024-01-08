ced16a355c8578f53e59b76ba9b181669f46f9a0
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:53:11 +0000

    both month live state shrink

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_item_count(array, item):
+        return array.count(item)
