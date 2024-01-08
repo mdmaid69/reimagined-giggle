d005825ce3e27e6c6027ed43739f63838b26da22
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:25:14 +0000

    present permission coffee healthy aim

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def reverse_list(lst):
+        return lst[::-1]
