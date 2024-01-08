4fc70af9ab2b25670f4da604ad1240793817da17
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:24:47 +0000

    people message apart point turnout

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
