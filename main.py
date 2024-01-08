87cdde307deb97f57145f8e586aa61734584e10e
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:39:04 +0000

    runaway arrangement own line tool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_max(numbers):
+        return max(numbers)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
