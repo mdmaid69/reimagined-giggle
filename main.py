c32e4417381db5df98d9ea2992d4f520ca60c279
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:50:24 +0000

    Ee report naturally nice partner

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
