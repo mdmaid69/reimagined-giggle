80590506d634ba05b4abfb7bbf14bde485a7add9
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:50:52 +0000

    complain politics get great recent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def add_numbers(a, b):
-        return a + b
+import os
+print(os.getcwd())
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
