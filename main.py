23bba39f7e930e63754d035330355f9db844b69b
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:59:27 +0000

    fun rest loan police uponon

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
