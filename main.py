788845d323f9a225bd9aad4785f48e137dd3f373
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:04:54 +0000

    fat plant exaggerate rarely goback(to)

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-print(math.pi)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
