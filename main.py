20591ef4efe5827359f4d648594c07e9aad1e2fc
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:52:43 +0000

    community discussion yourself steer discussion

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
