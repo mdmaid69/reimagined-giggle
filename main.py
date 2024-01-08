2342f5ff965de425e3f1c26f3603e7e7029f18c8
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:57:18 +0000

    game fast call bottom change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def subtract_numbers(x, y):
-        return x - y
+def get_string_from_array(array):
+        return array.tobytes()
