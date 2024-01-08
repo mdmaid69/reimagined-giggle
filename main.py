1c2601a4a1ab69b096bdd5bb5cce7c437e95b097
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:03:56 +0000

    position limit mess seriously risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
