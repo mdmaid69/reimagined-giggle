af2a60ccd1cb1086d6d0887275e79428ae0aa8a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:29:49 +0000

    suddenly settle list opposite strike

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_pressure(force, area):
-        return force / area
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
