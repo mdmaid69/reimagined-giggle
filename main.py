c6efd58c0f546fb9a256b2bf3cff413a76866009
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:39:19 +0000

    workon careful so bite throw

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
