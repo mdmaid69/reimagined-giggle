f8e2c5d63c4cb1769cde7594f315dc55b5b58825
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:39:19 +0000

    count butter purpose include talkinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
