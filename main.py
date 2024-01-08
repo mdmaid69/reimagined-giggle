125e61005e040e592ef6c57443d9d98d61dd7e18
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:43:21 +0000

    steer talkback instant total ashamed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import collections
+def create_user_list():
+        return collections.UserList()
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_typecode(array):
+        return array.typecode
