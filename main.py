fe93c033840da81e775238df0e9c90daf4c27240
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:35:35 +0000

    remove greeting squash without backwards

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
