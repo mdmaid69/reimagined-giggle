9ed8c3def1f8b96c345cf873492f3d7aa20e2e35
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:38:22 +0000

    towards leave sensible bath cough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
