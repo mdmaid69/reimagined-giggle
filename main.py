56cc89ea5eeb8516af3c3ab6ba14c24ab3befbcb
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:19:11 +0000

    declare official ability loud 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
