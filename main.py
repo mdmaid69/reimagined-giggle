d9d9f941f3d8948683fbfbcb5378e2f8bc774231
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:43:50 +0000

    view lay appeal which too

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import numpy as np
-print(np.array([1, 2, 3]))
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
