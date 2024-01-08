2bff9bf403dc0f749f123ebebcb3b6bcbb9561b9
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:14:40 +0000

    interruption introduction thinkthrough cool hair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
