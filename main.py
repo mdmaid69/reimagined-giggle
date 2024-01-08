2ee61307cac2cbfd50d5dd1baf7e80e4c8fd855a
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:44:01 +0000

    nice describe weigh pupil body

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
