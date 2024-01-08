b553ed6118a803e1e3f7bb204c274938687f9f10
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:25:57 +0000

    crash already cook confident recognize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
