1bdfce5075c5be23aa949622a85f268356a3ca85
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:05:52 +0000

    breath first best receive active

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import random
+print(random.randint(0, 100))
