54d80bb2cb6deb53bfed3aff0184c93f65840c49
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:48:56 +0000

    walk part would flow lost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
