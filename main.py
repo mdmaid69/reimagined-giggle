53cc4e461ef16ff0feff27739283f673bf692e75
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:11:37 +0000

    part day nervous brave still

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import platform
+def get_python_version():
+        return platform.python_version()
