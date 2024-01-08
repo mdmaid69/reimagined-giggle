b529ea8ba15d348372766d0edda057c38d472c14
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:27:31 +0000

    same nerve ought goout completely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_density(mass, volume):
-        return mass / volume
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
