551fff1087adfaf9a86fb56c5524926f10b2c47e
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:38:41 +0000

    easy 570enjoyable unkind fear against

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
