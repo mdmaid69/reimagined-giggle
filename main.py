48218f7030abd193e9c3b74d0e72708dfa8036b0
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:53:13 +0000

    smell pencil what greeting yes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
