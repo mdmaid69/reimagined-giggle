69da8cb332b80c04915b261acd585b650fd37c91
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:14:09 +0000

    kill emphasis factory rather whatever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
