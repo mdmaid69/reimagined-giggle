96341cb853dfd65fbc633078a7382b4875aa323e
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:00:50 +0000

    ill yesterday interview vote light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
