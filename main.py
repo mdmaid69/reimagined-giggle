c337138abd54bf5f3d860d12f1d52e4f2c7e3d3a
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:59:58 +0000

    embarrassing makeinto tip indeed carry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
