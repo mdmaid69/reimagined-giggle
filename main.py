220a234c825a0b994919d80a1288c7aaccd6565b
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:26:37 +0000

    bringforward exactly mother callout work

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
