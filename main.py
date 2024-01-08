ce0f6be32c103614a3720d106d9f218e19e5128f
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:27:36 +0000

    beside fold cut cost detail

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import datetime
+print(datetime.datetime.now())
