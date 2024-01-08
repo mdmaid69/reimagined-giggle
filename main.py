c35aa74d83af7d865761e360add2e732f66b72dd
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:41:21 +0000

    shoot afford whistle thinkthrough possible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
