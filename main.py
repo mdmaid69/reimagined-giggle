946b35532c1e0bec613b19be61530a56158e395a
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:12:02 +0000

    tragedy slight window evening reduction

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
