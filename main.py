d138f6ae47468a190024b9d1c0de66796d1f1c6b
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:55:40 +0000

    talkoutof open prepare vanish cut

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import collections
-def create_stack():
-        return collections.deque()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
