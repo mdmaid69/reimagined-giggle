45dd07b8c93d35d36fb51f06c2a26ae9cdec8fbf
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:38:48 +0000

    chance she mean deal communication

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
