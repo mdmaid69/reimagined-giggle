5c3b1b79c845243956e8137af9950afc2113a7c7
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:19:27 +0000

    shine argue scrape fall damage

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-print([x**2 for x in range(10)])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
