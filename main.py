e5671d345913f1f565493bb9ab04d4594de53e55
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:49:50 +0000

    wet nor stand shop top

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
 import sqlite3
 conn = sqlite3.connect(":memory:")
 c = conn.cursor()
 c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import random
-print(random.randint(0, 100))
