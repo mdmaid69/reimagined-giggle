e95f2e907a5ce9e8032675066be9e52781d1ec89
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:33:31 +0000

    watch cruelty attract criticize waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
