4dff0945b96dd7f116c07ca73b0bfe6725e12edf
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:02:42 +0000

    wave apply tall shoulder check

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
