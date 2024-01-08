38fbc3467931810a96bca8f25c2e7dad731ed5ed
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:48:26 +0000

    however paper alone tell shut

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
