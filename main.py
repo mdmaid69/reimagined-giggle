2fd81936c4a8c2c6ebfa7ef869af822ab72be5e0
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:32:00 +0000

    nowhere summer suffer compete most

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+print(sum(range(10)))
