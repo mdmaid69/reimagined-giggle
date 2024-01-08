307785d6da1e808844a546914c070ee7960b4a50
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:33:43 +0000

    hear ability speak case study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
