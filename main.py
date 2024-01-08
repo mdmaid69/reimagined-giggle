c6fbee3f10f6ed8ebab34fa85f9aa37d9b737a86
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:45:34 +0000

    low fork once square warn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import time
+def get_time_since_epoch():
+        return time.time()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
