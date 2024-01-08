54974b57cf5b218f10cbf845ee0b9a22b6a94f4e
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:42:24 +0000

    among brush power odd country

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import numpy as np
+print(np.array([1, 2, 3]))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
