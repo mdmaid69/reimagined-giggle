25ca6f03707f1e0f8ca380c24ab7e83666e028e4
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:20:55 +0000

    bang no instead back quickly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
