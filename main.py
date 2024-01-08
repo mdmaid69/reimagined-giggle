705714d96cd5c8189f5a17c985109849e2e87cea
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:50:39 +0000

    cause strength across between usual

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
