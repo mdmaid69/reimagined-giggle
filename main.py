5ad904de2c6008e562a1b1ba54a3e8dd1948007f
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:35:11 +0000

    explain pressure injury result play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
