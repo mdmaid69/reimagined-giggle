e7dd93d9e1ba1c6c4ce297e8d19bdae7d438ad7e
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:45:21 +0000

    giveaway near lookafter finish small

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
