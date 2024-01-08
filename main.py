ecb16176b8d917c7b5d1f62b1b2aefb3f88d2ed7
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:10:29 +0000

    getaway bite machine convince desperate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
