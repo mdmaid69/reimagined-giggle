ad40db96900a63d06650df74e0043ab1c33467a6
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:36:42 +0000

    bus rain doctor meaning perform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_floor(x):
+        return math.floor(x)
