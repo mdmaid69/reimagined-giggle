5fdc3e1c9203febf3689515302b58057f3504f59
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:01:28 +0000

    Yy below theirs vanish every

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-print(math.pi)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
