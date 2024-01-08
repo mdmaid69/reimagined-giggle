91802034b618a7ebdc3bf6064c6dc13b36247ed1
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:40:05 +0000

    criticize border between maximum uniform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import logging
+def log_message(message):
+        logging.info(message)
