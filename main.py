ae8b36c8c705681f27d0eb5e52705675e16ee86b
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:11:31 +0000

    trip remark swallow rush carry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 def reverse_list(lst):
         return lst[::-1]
