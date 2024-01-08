36533cd7229cc16df8c9239708c8d49aa39e0749
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:51:11 +0000

    car deaf priority determined space

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def remove_duplicates(lst):
+        return list(set(lst))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
