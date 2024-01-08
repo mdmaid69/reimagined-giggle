4b91fa25960a10d829fba378a3c833259121da2c
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:01:07 +0000

    contain support agree relation follow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def reverse_list(lst):
+        return lst[::-1]
