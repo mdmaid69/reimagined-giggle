40048214c2e9bb465771dd5d7599eb7581fed7b7
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:52:36 +0000

    music soil write turnaway bored

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
