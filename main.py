78a1e6e7525924812469b56299820fa9d26e1e11
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:44:20 +0000

    lean mistake compare gift wipe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
