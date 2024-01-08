fdf776d7b80f860933059492200ecf0378913528
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:29:45 +0000

    today introduce famous rank put

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
