a1f60d0af6f2faf0976d1839a681153e12326a9d
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:34:43 +0000

    under interfere flight meet whole

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-for i in range(5):
-        print(i)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import sys
+print(sys.version)
+def calculate_area_circle(r):
+        return 3.14 * r**2
