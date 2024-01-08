0ac0de2a39913a01a5806b40cae8df4152ff55d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:10:05 +0000

    office dish risk rough disturbing

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
