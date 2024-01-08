e8ef3d0b66cc3b0e985064b2479a3cf356ba3031
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:15:53 +0000

    fit middle seem agree otherwise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
