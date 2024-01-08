a6f1b420860660934314cefea8163f775bb6dd1c
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:58:32 +0000

    wherever temper spill either ability

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_work(force, distance):
+        return force * distance
