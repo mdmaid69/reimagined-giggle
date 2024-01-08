48ed30ecaa76c1130ecbc9ce8971db1f508c78ba
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:36:27 +0000

    lookback(on) almost side shape must

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-  def sort_list(lst):
-        return sorted(lst)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
