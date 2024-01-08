490128aa1f4bfbcb61714ec2f2c6e56c0cfe4b7f
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:59:00 +0000

    getout live wear hang firm

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
