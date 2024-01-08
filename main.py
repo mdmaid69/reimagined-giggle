307139f26e7d12d787dc1bac4785cb2400bc97fd
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:12:07 +0000

    standard more unkind upstairs same

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
