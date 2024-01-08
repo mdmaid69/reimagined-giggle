cc952e332ac068e41ed07d4ae5193d2596f855a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:29:17 +0000

    habit relative lucky Yy treat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def is_even(n):
+        return n % 2 == 0
