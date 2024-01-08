4a93c393f05e4bb4b02f96ff4d957f21582c8f82
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:46:26 +0000

    taste burn hate personal across

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def split_path(path):
+        return os.path.split(path)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
