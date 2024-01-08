64336c1d4965a790e21dfedf7cc6940c40b30ebc
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:25:23 +0000

    meanwhile serious plain material adjective

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-print([x**2 for x in range(10)])
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  def reverse_list(lst):
+        return lst[::-1]
