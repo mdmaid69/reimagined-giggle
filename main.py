9e5971947d9bdff8980332ee22437475d37beee1
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:52:10 +0000

    able drink regular score considering

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_work(force, distance):
-        return force * distance
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
