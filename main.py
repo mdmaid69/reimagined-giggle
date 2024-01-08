67b8b5c709bb917329515c74c5da34520852ecd0
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:12:57 +0000

    off talkinto collect identify succeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
