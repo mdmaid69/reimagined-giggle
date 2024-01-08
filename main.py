efc1e2f042ec0afdd5bf40c3fd64ca71c6bda946
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:46:21 +0000

    permission basically coat neither tool

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def find_min(lst):
-        return min(lst)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+from collections import Counter
+print(Counter("hello world"))
