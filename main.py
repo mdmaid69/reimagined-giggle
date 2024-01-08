4cae2c2ea32d35b8b17069b718afb8f9827bcb4a
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:07:50 +0000

    know light affect maintain not

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
