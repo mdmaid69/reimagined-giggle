da4908855a43927c5b9d6cace0c76a19bf951a59
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:08:53 +0000

    means wash Ll argue cruel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_perpetuity(payment, rate):
+        return payment / rate
