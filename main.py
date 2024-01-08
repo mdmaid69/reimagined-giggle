c568ca6d661b300240f638b96be9251cf1f29558
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:46:29 +0000

    real official describe week sometimes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_floor(x):
+        return math.floor(x)
