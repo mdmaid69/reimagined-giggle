e8707b8c03fc92541621560701b681514f94e1e9
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:31:27 +0000

    another 570enjoyable work hair feature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def convert_to_octal(n):
+        return oct(n)
