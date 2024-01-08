4c6b14d3cb1c9ec2cedd13650ef44dc5379e74bb
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:47:11 +0000

    relationship objection exciting shout mean

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import os
+print(os.getcwd())
