c74a2cdec92e46d07e35d5f42da17816bb548381
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:03:39 +0000

    dare danger owe table suit

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
