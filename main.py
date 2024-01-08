0f4b0514a720d7fb9c614c26813063662b133413
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:27:48 +0000

    rub unlikely inquire formal willing

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_list_from_array(array):
+        return array.tolist()
