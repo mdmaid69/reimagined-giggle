a0e7a5cecfcf0194c5accf2fc85f2d765ee73b78
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:14:43 +0000

    ours no law flood ill

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  def sort_list(lst):
+        return sorted(lst)
