ff2a8af83fcd3a2981b871261493b8b75d22673c
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:48:39 +0000

    guide block open study start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
