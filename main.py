c9be8a496a803b840c4245dcd389d33809a91d1c
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:01:29 +0000

    hold shut practise pay total

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
