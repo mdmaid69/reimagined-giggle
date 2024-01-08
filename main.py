0e24b7deb6d0d11afa3d82233800b4e5071203af
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:15:26 +0000

    mind use breakup offer control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
