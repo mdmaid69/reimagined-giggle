772fcf92a1fa642782fb54c22ae991feb1bbf0be
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:54:42 +0000

    war decision pretty desperate please

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
