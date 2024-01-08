fb12d54927d7a28465e4ca98a0d513f4c4954a9d
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:00:44 +0000

    wealth cinema telephone march argue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
