3499802468ad5095fb6534507c11b6d10ac2afd7
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:22:14 +0000

    water close boil show enthusiastic

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def cube_number(x):
+        return x**3
