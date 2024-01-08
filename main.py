fa275af2d19636d697e42afbf190c072fc759563
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:25:35 +0000

    through attention loan borrow probably

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def sort_list(lst):
-        return sorted(lst)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  def subtract_numbers(x, y):
+        return x - y
