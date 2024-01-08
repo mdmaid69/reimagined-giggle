093699c446dcf85cf3f67c7fc4283795590d5e07
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:23:53 +0000

    empty continue responsible opinion giveback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
