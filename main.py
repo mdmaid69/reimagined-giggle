eed262a5f12c15aa6d4a8f03f4becd64ca10dc00
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:38:51 +0000

    interview waste worth sticky talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
