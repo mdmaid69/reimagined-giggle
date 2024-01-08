55fb656da07ef938271a36f1930c882722195555
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:30:08 +0000

    organization price talkinto play supply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
