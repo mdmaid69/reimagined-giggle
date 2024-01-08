fed6e5c2d5f1b1f8e15fbb83a17d668751be2c3c
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:22:50 +0000

    use too street use argument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
