9d20e198b36eea239dbe205b12bfda6b26db39d8
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:50:54 +0000

    state consider meanwhile settle chance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_min(numbers):
-        return min(numbers)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
