9f06970b239f27775cd90d79122f033de945bcac
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:54:50 +0000

    powerful Rr takeout professional already

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def is_even(n):
-        return n % 2 == 0
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
