e56559c9c926126b186446b5e035d088db7e5637
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:17:32 +0000

    sound walk noise getout connection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
