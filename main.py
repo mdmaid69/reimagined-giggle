52aa7c27973f95a8ead5be9d66a5e2f7dccb46c6
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:23:47 +0000

    lot smoke fear never tooth

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_volume(length, width, height):
+        return length * width * height
   import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
         plt.show()
