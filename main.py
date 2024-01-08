d4b2d2b57fc28b2a1ec288110f613732d2548590
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:33:05 +0000

    opportunity separate travel justice much

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
