9f2c37a7beb4f3e7c9efefa11d85457765c379de
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:32:37 +0000

    family fasten on edge Oo

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
