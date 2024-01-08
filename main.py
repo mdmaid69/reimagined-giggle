253b484739264afeea66290860583c333f3919dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:27:37 +0000

    properly perhaps pure independent themselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
