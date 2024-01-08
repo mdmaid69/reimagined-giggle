a20d85e016ba42ad06eab38ab425cb71d855e52f
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:18:38 +0000

    tax boat sister noisy protest

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-print(math.pi)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
