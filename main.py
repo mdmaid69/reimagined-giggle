7ad509f6fae9cc6e5acbac445b244a490c2a7fe7
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:57:32 +0000

    refuse whatever sort theirs news

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+def calculate_acceleration(speed, time):
+        return speed / time
