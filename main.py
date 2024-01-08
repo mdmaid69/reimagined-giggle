805f67cf9310a82b4f0cf23e7f2d40cb5cf68ba1
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:48:02 +0000

    tradition complete pleased discussion beautiful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
