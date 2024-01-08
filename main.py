4c8e095933a9afaf35d1db79eedc77fbb9096155
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:46:00 +0000

    scheme present vision mainly bag

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_density(mass, volume):
+        return mass / volume
+import os
+def remove_directory(path):
+        os.rmdir(path)
