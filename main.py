83ca63c94a404fb962c9bf750d0284384d2a6856
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:46:16 +0000

    path attempt sound thin across

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
