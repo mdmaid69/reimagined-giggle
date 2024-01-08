3ee0beffb807f792bccfee8113f416629f850a51
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:25:04 +0000

    side mood plenty include trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
