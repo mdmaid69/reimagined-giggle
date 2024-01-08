d7494ad2fe07b03724a04949ea3282109b00b1eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:07:43 +0000

    certain mark sky glass possession

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def add_numbers(a, b):
+        return a + b
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
