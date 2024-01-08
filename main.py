0e29a0096dac3f136e212f79c5d61b9566079372
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:17:52 +0000

    sort present behaviour hard shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
