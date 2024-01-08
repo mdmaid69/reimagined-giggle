ad2750efff738dad1b07c140a872ef43ae0d17ab
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:40:09 +0000

    business regular female job talkinto

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import re
+print(re.match("h.*o", "hello world"))
