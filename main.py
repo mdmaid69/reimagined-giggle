c7cf978db64f8ed97fda427225ccc9749440f45b
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:26:30 +0000

    yesterday half our cover trousers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
