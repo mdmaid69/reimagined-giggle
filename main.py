7f52f012754e8bcc17c945cedd62833c8738ab95
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:25:39 +0000

    butter release Cc post speed

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+for i in range(5):
+        print(i)
