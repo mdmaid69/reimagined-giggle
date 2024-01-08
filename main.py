8468542e819b9eb5ceecf21ff9674a8909330382
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:04:47 +0000

    imagine lookup temper their soft

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_density(mass, volume):
-        return mass / volume
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
