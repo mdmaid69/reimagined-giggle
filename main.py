a4d978b6e589e493dce4dcb507d9bcc710457bcb
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:51:36 +0000

    patient comfortable by turninto position

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
