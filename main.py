716cf87fdfaf30e7a1de2b0907a52694c55141bb
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:10:09 +0000

    detail equipment plenty less division

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
