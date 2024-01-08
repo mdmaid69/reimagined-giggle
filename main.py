fb569c99b294e5ac016ec86c701cd4375040e076
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:53:49 +0000

    temperature most anywhere comein comfort

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
