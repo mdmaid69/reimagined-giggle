67a29ac8ad98232a625990ae0d47e31b7a88d016
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:40:55 +0000

    successful total normal perfect turn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
