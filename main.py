7fa09cb7c28eefbcbdc56c11086e5a74fe867c6f
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:07:07 +0000

    effort rise partner terrible frame

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
