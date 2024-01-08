c8deb91345cff868d387dd42122eca9e9142061d
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:30:18 +0000

    storm contrast present stranger base

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
