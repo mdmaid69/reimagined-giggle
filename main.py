23b4031f9ec511d4d892fe49c84e72fbbf2f1d04
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:19:57 +0000

    ground introduction earth proper beginning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
