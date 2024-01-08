403f7d520c952754443b6ee3dd9987081c83fcc4
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:01:19 +0000

    concerned worse odd rare it

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def subtract_numbers(x, y):
+        return x - y
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
