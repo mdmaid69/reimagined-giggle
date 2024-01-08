c962042d6fecf68ec4ad37a7347d49574bb15957
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:32:24 +0000

    tense ground plastic degree meal

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
