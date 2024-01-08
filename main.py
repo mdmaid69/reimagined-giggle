d5b4d077c8cc0496cc817a60e076cfba5a3113e0
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:16:24 +0000

    air thankyou sleep hurry escape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
