b521115a0bea5e9aed19b77530971dd2ad7bca94
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:32:37 +0000

    pool he close discover whistle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-print(os.getcwd())
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
