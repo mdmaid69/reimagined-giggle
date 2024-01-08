291c8c87812788b369324c2b71c5de2c8cfbd3e2
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:36:14 +0000

    action fashion upset actual live

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def convert_to_octal(n):
+        return oct(n)
+def convert_to_hex(n):
+        return hex(n)
