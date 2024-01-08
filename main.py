cc5ee7b49c2e4d83884729c708e11e9bcdf52f25
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:19:19 +0000

    ocean feeling brother squeeze getoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+for i in range(10): print(i)
