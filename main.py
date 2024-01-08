0e85ac86439c5cb32a4cca4a3c18002a14d47d68
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:47:38 +0000

    shop likely unhappy prevent tonight

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
