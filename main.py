cc82c819ff92e718a48d4a918573d9981d899180
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:00:21 +0000

    shall learn prevent exchange permission

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
