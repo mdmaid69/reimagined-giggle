7e98bc07f903566861564b4e2653735cbfa4ad63
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:20:14 +0000

    spill exchange business remove remain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sys
-print(sys.version)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
