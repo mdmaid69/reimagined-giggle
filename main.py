ac4bcce90bde9f8b3a624e1bc7022c212232cda1
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:10:02 +0000

    emphasis him excellent defend stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
