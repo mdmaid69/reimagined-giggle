1f12161f4d3645aa4fd2dfcff12006f28e596565
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:11:08 +0000

    make pretty throw life reduce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
