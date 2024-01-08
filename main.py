8726473e690d47571caca7a4734b6ab1944d64d5
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:29:15 +0000

    black home against vanish influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def get_array_as_list(array):
+        return list(array)
