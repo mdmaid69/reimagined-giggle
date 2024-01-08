7dba3832aa8dbc38ffe7e4d06ca5018432b22570
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:00:28 +0000

    somebody almost as add actual

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def add_numbers(a, b):
+        return a + b
