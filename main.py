395ce9d8d0098f73f94e8b7301c33e121ccb94e2
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:22:11 +0000

    character trial cope basis badly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_power(work, time):
-        return work / time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
