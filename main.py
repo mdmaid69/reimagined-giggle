cd8efc5b5be3df072a57edae40ea1ed8b11ae0bb
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:05:04 +0000

    address quick clever unlucky miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
