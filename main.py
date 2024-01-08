731257a6d1e62b2b9f14ec1bfde04b82e9227987
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:42:21 +0000

    complain protect deliver perhaps main

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+for i in range(10): print(i)
+import sys
+def print_python_version():
+        print(sys.version)
