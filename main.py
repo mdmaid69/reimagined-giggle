f6165649f2ae58b08bba14150f3ec2e947e1acf8
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:11:09 +0000

    why year office heavy catch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
