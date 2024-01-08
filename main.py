451cd2aa556cc9989a5e26cbb7cb3214252fe51c
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:29:13 +0000

    full godown dream breakdown central

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def find_unique_words(sentence):
+        return set(sentence.split())
 def calculate_area_circle(r):
         return 3.14 * r**2
