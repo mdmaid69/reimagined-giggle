bf45b99c11fdc9cb25cfba62a6b7863bef5cba26
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:54:22 +0000

    leave pity downstairs goup solve

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
