6e8a07357eaf4dbf904920cf1887a9284389f9ef
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:42:50 +0000

    real argument next frightened most

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
