6f498273bf4017ec8256b70bf8145c9e468bbe16
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:08:36 +0000

    point opponent game getup environment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
