cc5335e57561d2c79792c7f2d86a9eb68b9ae1de
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:36:42 +0000

    organization workon inside once boil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
