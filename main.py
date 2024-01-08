f668df19e4255d13002ae585780e11bbf7cc24c5
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:35:53 +0000

    thankyou break promise theirs precise

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_slice(array, i, j):
+        return array[i:j]
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
