1ae0aa47d4f9428733790a97b40d12ade2ffbd51
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:04:28 +0000

    niggaacademy around question responsible generous

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_bool(array):
+        return bool(array)
