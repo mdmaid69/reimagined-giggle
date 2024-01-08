b96b0b99a7a42490bcadeab415d9da4c5e84ac9b
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:12:20 +0000

    result male 564encourage easy defeat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import os
+def remove_directory(path):
+        os.rmdir(path)
