af445c95fb5e632b0b31adc07af63938d166ec6d
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:33:23 +0000

    dress whether image achieve jump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
