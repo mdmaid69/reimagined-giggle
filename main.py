daa0c0b15510d6d608e6f9cf3bd1cc51dd4d8cb7
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:29:59 +0000

    call wealth partner poison show

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
