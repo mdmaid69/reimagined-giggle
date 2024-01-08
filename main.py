eb5a5ec20f7f2920fff28e58872b9042f5ec61b0
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:51:19 +0000

    water part slide score comfort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_item(array, i):
+        return array[i]
