4e422ae9ef4e9b7f821f7863c9ed45ccaaa42cec
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:01:47 +0000

    laugh thankyou float promise careful

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import collections
-def create_counter():
-        return collections.Counter()
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
