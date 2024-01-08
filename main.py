00d696b1abd383d6c9d4a46c63364e199386b6b7
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:19:59 +0000

    key scream agree either either

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
