35b34f647dad15183b20ad41a8ec8671924ceb3f
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:38:49 +0000

    but far stop certain detail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
