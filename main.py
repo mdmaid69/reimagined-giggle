0202ecf52f36f6a51b1e9408ca474bdbd028fc34
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:59:06 +0000

    us defeat comeback secret father

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def find_min(numbers):
+        return min(numbers)
