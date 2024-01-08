6a2ce13c71d1422cad6d144225a41f9472b3a025
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:26:50 +0000

    house determined while 566end nor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
