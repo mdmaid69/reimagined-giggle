2585c949cf9ebcc36d7f497f283776ecafbadf37
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:45:45 +0000

    improve used careless delicate find

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
