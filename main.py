55804eb286e1f0d49cc4b4ac0b8d7a0ff7204e1d
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:01:11 +0000

    drive sticky congratulations determined scale

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
