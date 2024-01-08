c2675d90a9a132a560f7fe0ff061c23e0cdd05d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:19:27 +0000

    reject connected confusing dream tap

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def find_min(lst):
-        return min(lst)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def find_unique_words(sentence):
+        return set(sentence.split())
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
