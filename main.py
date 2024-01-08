7e477666ebfe67a11152d0a08e14233260eea02c
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:15:10 +0000

    follow thinkthrough charge worried combine

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
