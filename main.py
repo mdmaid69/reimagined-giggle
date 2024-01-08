aebc966b7d431081252cb607071416260feff698
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:47:00 +0000

    float record comfort build imagination

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
