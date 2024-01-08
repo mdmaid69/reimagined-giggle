b2f768a93e79593e64ba4cad019e18d2bfa69cbb
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:28:07 +0000

    target Hh fight play shock

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
