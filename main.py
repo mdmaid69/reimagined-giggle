fddf3b19d28963d882915d56fc2584ceaa0d4821
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:07:49 +0000

    truth remark rid process tend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sys
+def exit_program():
+        sys.exit()
