502f1dc344d32e7b719fdf2f70c3b07d00b45f3d
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:09:16 +0000

    head real independent rest sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
