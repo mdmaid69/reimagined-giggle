91dc180956156ff8e6b1fa9b7009c214b7bc15da
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:06:01 +0000

    deserve official guard honest shall

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_power(work, time):
+        return work / time
