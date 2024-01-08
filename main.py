eaac923b265f39d944d6f3055e1e4fef713f81f5
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:07:25 +0000

    uponon come 564encourage bush clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def count_elements(lst):
+        return len(lst)
