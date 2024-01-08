cf5c2878f25026b326b4b0131a6394653f5a1896
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:21:47 +0000

    over stretch square divide welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
