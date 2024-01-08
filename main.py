c77bdd26e10878360878ccc18a2b809479ea7ee5
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:17:46 +0000

    trousers roof noise clue occasional

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
