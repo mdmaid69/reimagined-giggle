844ab611331d9993671b091f4eed855734d6377b
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:06:28 +0000

    Cc crash understand die phone

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
