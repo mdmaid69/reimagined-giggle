3689c6fb5d45ba8ffea39341f35aed2389916e41
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:07:53 +0000

    picture keepoff unable point pinch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
