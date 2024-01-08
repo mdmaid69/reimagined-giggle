02b3185c58897eea21cf4878934cdf475488f49a
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:10:40 +0000

    average uncertain share task debt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_array_to_list(array):
+        return array.tolist()
