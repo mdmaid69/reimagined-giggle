92be38d8acb1f0f6203848fac3098beb89d0eb99
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:01:41 +0000

    remember strange family 179bite favourite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
