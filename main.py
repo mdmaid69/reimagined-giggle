5fbd5029ef532e208dc2b1054464b15cf895c15f
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:25:53 +0000

    mad sport especially large mother

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def is_odd(n):
+        return n % 2 != 0
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
