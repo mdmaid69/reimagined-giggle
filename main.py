721fa10f6a28be3b8d8f19d1842fd09b7744e720
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:31:29 +0000

    lean cool yesterday future naturally

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def is_palindrome(s):
+        return s == s[::-1]
+import re
+print(re.match("h.*o", "hello world"))
