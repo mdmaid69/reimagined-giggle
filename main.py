606e19168c5e26e974d213722c76f89b478d8e1c
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:46:00 +0000

    complaint foreign skill process guilty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-  def subtract_numbers(x, y):
-        return x - y
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
