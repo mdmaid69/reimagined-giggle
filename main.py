0de972a471d422692d93384efa18441c137c8471
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:56:59 +0000

    false strike question fall improvement

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def square_number(x):
+        return x**2
+def calculate_average(lst):
+        return sum(lst) / len(lst)
