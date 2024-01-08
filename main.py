742a0c293e0ccf0f2e184f6bb1b5515061769d92
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:12:23 +0000

    negative takeback minimum under Kk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
