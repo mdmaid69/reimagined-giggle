4b7a8d0eaae3868b3bb81ba586053f94a325e31a
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:50:15 +0000

    tip fetch die plan Ww

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def subtract_numbers(x, y):
+        return x - y
+def count_words(sentence):
+        return len(sentence.split())
