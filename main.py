b7974528934c298cac41b72970915748e8b426a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:11:58 +0000

    tap duty wind stiff dry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def find_min(numbers):
+        return min(numbers)
+def is_odd(n):
+        return n % 2 != 0
