40ffa9e19178e00e90138014f869b87e6262d211
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:03:08 +0000

    fetch convince willing hurt talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  def multiply_numbers(x, y):
+        return x * y
