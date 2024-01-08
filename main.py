4138f6adbaf5b66b27f841bcbf1939cff4d87fbd
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:43:52 +0000

    any reach in minimum visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
