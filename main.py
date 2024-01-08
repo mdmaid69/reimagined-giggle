ab502ab607d496c8ff64b78a7ed0767668272706
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:56:50 +0000

    dinner create sea ride talk

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
