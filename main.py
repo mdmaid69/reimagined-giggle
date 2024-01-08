c825397ea4180c2edaf0e965103509ab63c95f43
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:21:25 +0000

    introduce note bother murder equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
