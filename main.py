91c35834460b7758b07d07bb63a2ee3c477f401d
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:42:06 +0000

    original soft usual forget also

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def calculate_area_rectangle(l, w):
+        return l * w
