d3ea89c76c83d8b6f16aaa996846844c321011c5
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:03:02 +0000

    list noise back bang short

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def count_elements(lst):
+        return len(lst)
