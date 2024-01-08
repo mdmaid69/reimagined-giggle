cfbfd5afd8e26d2c7467b1e04afb2f6f24d8eb71
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:10:20 +0000

    sound way lookback(on) freeze under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
