f6faab7b924ef3f9d0d8798a6b2afd5ae55a4412
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:31:26 +0000

    increase miss score mine balance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+print(re.match("h.*o", "hello world"))
