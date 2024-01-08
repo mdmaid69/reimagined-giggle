9b2a14332312a7516bf33c22fd057187dfdc88ac
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:27:49 +0000

    roll voice floor knock complain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_tangent(x):
+        return math.tan(x)
