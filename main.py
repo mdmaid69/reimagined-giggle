8f3dd9aa2dd6c6bfd3040dca1de9e178c774f996
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:48:10 +0000

    trial choose cool firm characteristic

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-from collections import Counter
-print(Counter("hello world"))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_acceleration(speed, time):
+        return speed / time
