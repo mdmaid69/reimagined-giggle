bd66440fb8fe161e2c511e82f5189f5915bf29a6
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:52:02 +0000

    commit godown normal affect odd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def split_path(path):
+        return os.path.split(path)
