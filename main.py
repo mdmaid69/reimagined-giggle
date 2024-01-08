feef2e6ca73575151485205369912c5eda7031c3
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:52:33 +0000

    intend nowhere border buy university

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
