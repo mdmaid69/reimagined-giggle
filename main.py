de4c5416f4a3bf8c170817594badab6b767e4f81
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:42:11 +0000

    swimming occasional error giveout concerning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
