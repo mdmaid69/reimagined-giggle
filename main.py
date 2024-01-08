0229dd34b9a97e584d6900c296f6c4baede13d91
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:17:17 +0000

    feed blame successful match store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import json
+print(json.dumps({"name": "John", "age": 30}))
