f5a8e9daf4f3a66eef4d8795a746faf138e2d25f
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:02:56 +0000

    bear course natural thick shopping

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import os
-print(os.getcwd())
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def divide_numbers(x, y):
+        return x / y
