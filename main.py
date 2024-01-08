8fd8f7dce53dad7bc89a510825da658fcbab0720
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:14:30 +0000

    slight front judge today hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_counter():
+        return collections.Counter()
