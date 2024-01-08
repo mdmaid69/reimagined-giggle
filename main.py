7d1827a19afaa884aa202e60d1d7b5cbc6538f31
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:25:25 +0000

    once basic car unite arrive

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
