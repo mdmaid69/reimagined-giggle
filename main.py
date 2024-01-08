1444be596b6aab5474d1b343bc74f9da306360fd
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:32:14 +0000

    564encourage form choose thankyou evening

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def subtract_numbers(x, y):
+        return x - y
