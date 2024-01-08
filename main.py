64034df1093a3c983895cec326e8403068223b6d
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:57:21 +0000

    disgusting main conduct convenient constantly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+print(sum(range(10)))
