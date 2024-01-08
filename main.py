3a7cbbf248ee533ab0fd25234d30f59fbc4307cc
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:29:58 +0000

    lay gather oil marry church

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
