3d12f8917bcd17168914603350befde54bb5fcac
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:23:41 +0000

    arrange mistake slow apparently hole

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
