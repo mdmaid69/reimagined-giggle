2a2e04a1e789e6ca49ac4eb86d61db50610e7c6f
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:45:05 +0000

    plan personal bright pile attitude

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
