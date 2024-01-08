8fd2dfa320cbda85b408a4679ff371fd1ce7887b
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:32:38 +0000

    joke black laugh wherever considering

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
