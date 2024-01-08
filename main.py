7c09d514a3ee72bf21e66046ff3d9b7c85ba6ba5
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:04:55 +0000

    behaviour proof contrast boat careful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
