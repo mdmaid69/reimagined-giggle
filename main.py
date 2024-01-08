ad76be8ebe47a92f69a928a25a514913b9b4464f
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:08:18 +0000

    so below uniform keepoff total

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
