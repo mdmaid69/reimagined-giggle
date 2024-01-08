56ffd002927de23a621b798a47f1959842059836
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:52:41 +0000

    good peaceful harm forget hear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_base_name(path):
+        return os.path.basename(path)
+import sys
+print(sys.version)
