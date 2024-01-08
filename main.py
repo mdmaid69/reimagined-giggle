b18bbd6440a0ac29b39f9f257e3f556c17e56f97
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:33:29 +0000

    gooff breakout everything Ff possibly

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-def divide_numbers(x, y):
-        return x / y
-print(sum(range(10)))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
