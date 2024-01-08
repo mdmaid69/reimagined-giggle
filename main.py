74b82941f50b3d6c5a7ae687c943e1ffb6721fe4
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:42:48 +0000

    else as wild official term

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
