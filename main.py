5ff8fc7b76c8e54dc3872b8b3c4e4312c0c4a66d
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:47:54 +0000

    lower objection chin realize as

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def is_odd(n):
+        return n % 2 != 0
