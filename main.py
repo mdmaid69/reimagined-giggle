114151e78df9ed5f83b00e01693a2a77b515f6f0
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:16:36 +0000

    year demand goout callback turnover

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
