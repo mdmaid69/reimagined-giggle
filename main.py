303345f9473e64df1f055e90a2a5dcad5af602e1
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:31:39 +0000

    square away scrap tend ignore

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+text = "Hello, world!"
+print("Characters:", len(text))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
