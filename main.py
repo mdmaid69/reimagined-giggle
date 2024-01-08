630c5b0deeba6519771f4f4ea9b6eb0edd09fafd
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:35:15 +0000

    hang high town separate suggestion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
