69fc793d6ac3ae6d01ea3bc95e9111b828367874
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:02:20 +0000

    wake regular swing recommend floor

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
