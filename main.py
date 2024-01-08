cc03eb0751cee93aae9b7cc08f19c1e848fb3c45
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:23:40 +0000

    after authority conversation rather swim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def convert_to_binary(n):
-        return bin(n)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
