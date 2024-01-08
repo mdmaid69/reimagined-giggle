c723ee77fa78e7253ac8171cb3cdb0d0cc099ae5
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:53:48 +0000

    environment of represent preparation whatever

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
