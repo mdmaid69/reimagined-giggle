edd4268993544405452d8dedc96d489a13bfb9a6
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:22:14 +0000

    their type fair chief Ss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
