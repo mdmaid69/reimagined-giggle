5319e487f857eabcc268403a95b2e7fe15bc393f
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:12:32 +0000

    bringforward greeting stupid while down

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
