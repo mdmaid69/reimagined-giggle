94cb98a8d03c12b7adca59b2dbee7338c4f1d852
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:02:39 +0000

    pollution ring risk news glass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
