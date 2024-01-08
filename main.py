3bc49318fc01fa359e6eaeac158469bc17e3b330
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:32:20 +0000

    declare around shock mean scared

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
