1f130ea1ca22b8c58b36678a627eb182e18ff835
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:24:00 +0000

    story woman pretend control repair

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
