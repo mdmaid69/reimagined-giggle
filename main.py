33ae821da641794afdb984cdcdc4c53d2440c17b
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:26:37 +0000

    judge union fight better perform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
