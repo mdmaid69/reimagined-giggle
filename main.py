8177665f2b5ddba1da64798ec96cf924e7d0d374
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:21:20 +0000

    quality total circle file visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_sine(x):
+        return math.sin(x)
