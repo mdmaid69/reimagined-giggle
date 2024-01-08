8132055cc14d943ee02206e0bdf6779497cef1f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:09:29 +0000

    silent weather appearance stage meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
