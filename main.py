f4de0ff50d23fecfd6deaf02c466b2634a8f208d
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:20:43 +0000

    print birth confusing limit your

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
