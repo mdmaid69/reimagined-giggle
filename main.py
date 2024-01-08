b903a9dcc2ef84f37d3c7b4d0aab19b8c7bf3690
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:19:41 +0000

    else clear Ff moon ought

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-print([x**2 for x in range(10)])
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
