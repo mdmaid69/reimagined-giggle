7b1e8b43b56055ff0fc3e3c987fab78f4a6371af
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:31:00 +0000

    respect sister hurt moral divide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
