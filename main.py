d3b711be2d27b635ef4e13c5504080a60c8c38c3
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:32:38 +0000

    comeover art peace before study

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import time
-print(time.time())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
