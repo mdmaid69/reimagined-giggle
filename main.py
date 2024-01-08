f2d391d06f914af99a945623a68d753a7014b15b
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:11:39 +0000

    him whole either secret pencil

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import array
+def get_array_as_repr(array):
+        return repr(array)
