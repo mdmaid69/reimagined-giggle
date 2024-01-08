f6bae4d6de4cbaf3b575c5bce79a0423eec22e51
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:23:39 +0000

    type new awake grateful modern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_speed(distance, time):
+        return distance / time
