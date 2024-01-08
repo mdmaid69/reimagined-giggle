6f7f429afb8e5cdf72d2b9e966648cdb0bd33c7a
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:44:41 +0000

    rubbish bend strict so stage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
