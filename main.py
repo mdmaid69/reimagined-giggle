95dfec2742647441da2e37b95d591c83d512d8c6
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:54:12 +0000

    this precise Kk share there

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+for i in range(5):
+        print(i)
