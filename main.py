3ac71691196cf011c2d03953cf1f670c9612d53d
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:08:48 +0000

    heavy polite full sick thank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
