2078dc1cf6c9ea959044ac71e8a26e70f19f92ee
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:52:37 +0000

    night the clue perform lot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
