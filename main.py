adc409d0c4f8db3f32210795a435820bf54e82a7
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:26:40 +0000

    lost uniform OKorokay be sell

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
