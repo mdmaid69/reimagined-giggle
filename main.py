99484b6f8cba43235edc8aa94a2361a16be1eefa
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:15:17 +0000

    forward score turnaway use shame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
