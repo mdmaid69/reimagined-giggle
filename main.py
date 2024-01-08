149fc67697d1cac83feb5a1bfd7d6f9dba92db90
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:07:38 +0000

    brother quick fence lookaround excuse

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+def find_min(lst):
+        return min(lst)
