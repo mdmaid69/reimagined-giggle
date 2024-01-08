aad983cca5c573bd278a11f2c6e21c7e05f758c4
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:28:29 +0000

    supply height past by post

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import os
+print(os.getcwd())
