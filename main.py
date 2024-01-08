4038e897cd962c0db81a805eb4853184bbdca29b
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:23:35 +0000

    personal behaviour whom piece attitude

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
