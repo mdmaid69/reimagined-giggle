7459ed8b0c16a62c0be3d71ca0b78f38f9264788
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:02:32 +0000

    wind lean talkinto body climb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_area_rectangle(l, w):
+        return l * w
