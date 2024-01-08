0e5871a7b5e9ac7d3fcd16641bb4ffabc4f7cb78
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:05:07 +0000

    tax often Tt anxious interview

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
