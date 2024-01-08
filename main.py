9f053b9e17903865650df40a5c3f56069411470e
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:58:40 +0000

    immediate blame by uniform might

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
