7a263de289bf60b2ab4345c657f2c79bce4c9e14
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:07:48 +0000

    speak adjective afternoon low sudden

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def square_number(x):
-        return x**2
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
