68a051414342e0f921a95bb9555e6289f00de982
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:54:40 +0000

    stage talkoutof holiday extremely stick

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import datetime
-print(datetime.datetime.now())
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
