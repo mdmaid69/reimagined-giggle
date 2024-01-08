cbdf3646291f257c5e6950d526ba2ac417e079cf
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:31:05 +0000

    complete strange save information before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
