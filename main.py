9bea467bb1521bf8578af7dbebefe79b451aaa4e
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:54:43 +0000

    develop claim since matter recognize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def count_characters(sentence):
-        return len(sentence)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
