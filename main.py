b7fce56dc89f404d13a046d42cd45fde0711890e
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:14:14 +0000

    juice place choice relative at

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import time
-def get_current_time():
-        return time.time()
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
