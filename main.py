fc07ac5a9e525f1812fb287e146d218dac4236f0
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:59:07 +0000

    safe bank unite weak fashion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
