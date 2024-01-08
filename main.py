527c9d6bd03e869f46aed3693d024ecf32afc6f7
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:44:59 +0000

    team hers Bb dear phone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_work(force, distance):
-        return force * distance
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
