ca9e5c9e677ed8c5ffd0bdd0639e5b6ca1904567
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:15:29 +0000

    leave land rather bottom image

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
