b3abb3fba93db090949b6d949fb7ac821a104ca2
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:16:02 +0000

    bringdown reduction ask emphasis exam

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def square_number(x):
-        return x**2
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def count_elements(lst):
+        return len(lst)
