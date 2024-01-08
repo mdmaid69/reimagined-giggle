00109783f5d2a76631d41b02e35dddafcd6249a7
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:55:43 +0000

    such prefer tall rate broad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
