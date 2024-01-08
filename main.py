1c9206f1f6cb7c6f02fdbe810c7627c9c390a910
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:17:33 +0000

    authority throughout pupil minor takeback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
