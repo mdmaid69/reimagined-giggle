b26c845ad3afcc8915fcbc267c3c58f6fd6b9563
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:59:15 +0000

    sail rise swimming but account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
