d3695aa1b37b381967e041bd4e60d477592eb5f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:48:43 +0000

    bend recognize dangerous last press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
