2ed3e1418b47eea7471fa68042576f17e5eac2fb
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:16:50 +0000

    authority side poor open common

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
