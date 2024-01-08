3989eb27ab42187d3f062e0026efeb061c4e939f
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:55:19 +0000

    thumb spot drop treatment that

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
