93ab30cf7fefee120b966c7830e5e478f0ed869c
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:04:45 +0000

    range quick combine ashamed warning

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def count_elements(lst):
+        return len(lst)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
