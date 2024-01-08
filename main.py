3e8f57599e9e5059ac241ca3491c0e33e6a8db6a
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:50:16 +0000

    bound just start chance among

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_force(mass, acceleration):
+        return mass * acceleration
