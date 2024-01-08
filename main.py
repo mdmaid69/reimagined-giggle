46059774c09a6b59e6ffb32277e41025a39b685e
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:21:22 +0000

    score finally actually middle careful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def get_array_as_set(array):
+        return set(array)
