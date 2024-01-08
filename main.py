e4c77263e55d018e6564b739fd7e08b821199450
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:12:28 +0000

    cross frighten expect lunch 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def add_numbers(a, b):
+        return a + b
