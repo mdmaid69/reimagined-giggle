50c2fa230ae3b7f8fcc9c68c713c5071439e964e
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:54:51 +0000

    hope type squeeze ahead harm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
