9d71cab3357ddd1a731d447a818184bec8cc5b88
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:21:05 +0000

    heavy place personal makeinto flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def convert_to_octal(n):
+        return oct(n)
