dd80a68fcb5a2f3fb07c7109ae9858cde1aa3317
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:19:01 +0000

    squeeze leg Cc neck thin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
