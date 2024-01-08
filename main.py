003f714968048635eaa83799060ec168b18fdd97
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:15:09 +0000

    meal plant wise square yet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_ceiling(x):
+        return math.ceil(x)
