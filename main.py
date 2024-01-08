fc9b7ffd63bc21c8689f4bdc0427d83cbc95e59f
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:44:32 +0000

    light forgive one respect spend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_tangent(x):
+        return math.tan(x)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
