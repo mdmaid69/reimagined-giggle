e72aceb3cac24b54e4c034f5ad13059ad5c83340
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:24:03 +0000

    buy let young commit design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_floor(x):
+        return math.floor(x)
