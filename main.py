91e170401b1a8495661928cef764175cafb42346
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:20:04 +0000

    water hide group car wise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
