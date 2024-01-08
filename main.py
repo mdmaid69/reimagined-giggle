e7b1d0499f43c088a9ee61f080405c10981bd5e4
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:23:05 +0000

    innocent unlikely stiff row description

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
