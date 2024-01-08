9983b680e34bf1c1d7978cd224bbd3644993be19
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:45:39 +0000

    sound cap ball lift force

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-def convert_to_octal(n):
-        return oct(n)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
