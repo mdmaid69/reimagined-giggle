ce79218dee9fbac7000f8f17667a5670a522ae26
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:55:16 +0000

    eager fear pool last behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
