c204b51e842d8fc6e165342ec6633232c1e3f7d2
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:06:09 +0000

    drive club sound by bye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import os
+def change_working_directory(path):
+        os.chdir(path)
