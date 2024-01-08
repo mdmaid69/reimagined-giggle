3f6c3aaebe31173188f0fcce48b6e5b9291298c5
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:54:39 +0000

    impression below really book across

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
