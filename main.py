075ba13fec55f6abdf7899f5044b8b0bda53c213
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:13:06 +0000

    frighten flood below that help

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
