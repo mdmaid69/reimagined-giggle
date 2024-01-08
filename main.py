9617a3d9968a9e0e441932dfe6f1a1a5a6732552
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:51:45 +0000

    anyone disagree spring wander talkback

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+print([x**2 for x in range(10)])
