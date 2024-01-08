0cfa5161d569dd8737123caa9f574f116ec05dee
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:24:32 +0000

    yours attach complete sleep traffic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_blocks(file_name):
+  def get_file_blocks_allocated(file_name):
         return os.stat(file_name).st_blocks
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
