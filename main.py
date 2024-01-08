5b92df0cbf5da16f68f6b6ad5a3139dbdfd41681
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:20:02 +0000

    below spot other comeback dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
