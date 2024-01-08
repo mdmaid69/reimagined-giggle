ef87c06554ba2437029dfda2b40051b18127909e
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:22:07 +0000

    sort art Tt process quit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-x = 10
-y = 20
-print("Sum:", x + y)
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_list_from_array(array):
+        return array.tolist()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
