4ec10faf704c46d0d37f2c8bbcd4f699da5d4159
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:48:11 +0000

    after rate protect neck circle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def subtract_numbers(x, y):
+        return x - y
