aa74c007464e5b61911ddedd8cc9211065f35d02
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:36:07 +0000

    comment this round want lead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-print(time.time())
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
