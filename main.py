c5528c32bd19451ff72da36ad9e62bf2fa60b8ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:39:17 +0000

    much mystery they behind step

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
