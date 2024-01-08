eb28b67f0dfcf0457187a45f666ea860016f9fb9
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:17:02 +0000

    congratulate change steal place exchange

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def split_path(path):
+        return os.path.split(path)
