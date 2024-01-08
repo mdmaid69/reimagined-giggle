2e88081d9ca57a6855a5b967b94b90a909fdd37e
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:21:55 +0000

    direct get typical getoff card

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
