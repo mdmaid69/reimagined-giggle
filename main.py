1b67dad0c3177b02c0aee0b92deb40f3308a0bce
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:17:01 +0000

    onto border speak much operation

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_pressure(force, area):
+        return force / area
