a789468eee21f8e04d2294d2119c609e9f2ae9c2
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:03:43 +0000

    copy black understand satisfactory priority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_work(force, distance):
+        return force * distance
