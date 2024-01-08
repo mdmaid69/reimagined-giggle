dd7d097594e13ddb86eb357ca78e431ea9308923
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:42:08 +0000

    herself path around follow last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
