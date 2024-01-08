3eb727cdf95b931d024e5da7983f8dd81ab2bbe8
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:09:58 +0000

    soon Ss subject accidental attitude

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import os
-def get_current_working_directory():
+  import os
+  def get_current_directory():
         return os.getcwd()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
