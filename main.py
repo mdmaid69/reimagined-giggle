fba50bd698efa24889837c614f1d6b0c1e317384
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:16:58 +0000

    wake information improve habit proud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
