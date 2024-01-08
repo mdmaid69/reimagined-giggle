1631fa85ad408b1e05b8cae5c71a77f0f25d62f9
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:21:46 +0000

    pronunciation coffee spray interesting dull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def append_to_array(array, item):
+        array.append(item)
