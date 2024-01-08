4ea3aecb6828794cc5cda7a3cbf1d02f873e0638
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:20:41 +0000

    married start gate bringdown niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
