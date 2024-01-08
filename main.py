2cef3a8b17ba37abe2b492618a90fd9082b0fec0
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:39:50 +0000

    takeaway protest exploration type excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import os
+def remove_directory(path):
+        os.rmdir(path)
