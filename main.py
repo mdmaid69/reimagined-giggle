65d52545e2bbcf87c78dccb3d2c0c41ed598a54b
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:38:59 +0000

    enthusiastic that where subject art

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import getpass
+def get_username():
+        return getpass.getuser()
