f814eca802c05712cf14064c120026d3fc54fba1
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:27:09 +0000

    little range which turnover medium

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
