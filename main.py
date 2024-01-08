c6749f002c62b84541b1e8bf77e0d47d2e373cac
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:20:48 +0000

    hurry concern throughout final strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_as_repr(array):
+        return repr(array)
