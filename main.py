4b7f076dddaa6a4d00d85e60116a397ef9ed60d6
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:15:38 +0000

    recognize hello spoil fill divide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
