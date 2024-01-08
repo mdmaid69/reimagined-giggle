6d04a3449e26b6e6a21a529348992cfe8406cf79
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:19:24 +0000

    say cough fairly breakup tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_hypotenuse(a, b):
+def calculate_pythagorean_theorem(a, b):
         return math.sqrt(a**2 + b**2)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
