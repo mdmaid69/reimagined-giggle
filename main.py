e9294d7e2ec5fa8f80c9e32cb0c59fa6a4cdc084
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:35:16 +0000

    include from juice children because

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
