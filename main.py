cc21c41d2cc39ca4c0a8fb559835b3dd824299aa
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:11:12 +0000

    threaten improve business meeting clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import math
+def calculate_sine(x):
+        return math.sin(x)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_repr(array):
+        return repr(array)
