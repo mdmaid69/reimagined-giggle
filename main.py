c283b9fdae703f38ef1aedbc2017b888b94bd71a
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:35:27 +0000

    height properly positive attention obtain

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
