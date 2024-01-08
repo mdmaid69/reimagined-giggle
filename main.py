28c2ea0ed28e4dcf33d5ce5ae8a02b7cd9ba7a15
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:00:10 +0000

    wear house keen announce government

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
