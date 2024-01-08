8645b3e33e59fe4ae1f830b45507daba6e7694fd
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:11:30 +0000

    scale complicated inside though remember

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
