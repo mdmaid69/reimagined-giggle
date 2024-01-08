cab4283e8024ae83657679d26ceee608148ed335
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:47:25 +0000

    score tip goon lookafter point

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
