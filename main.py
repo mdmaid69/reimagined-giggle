0b7204cb0c7617b4f02895cb73d32718ac722b0c
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:17:39 +0000

    attach course change wide relationship

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
