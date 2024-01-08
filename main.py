8783b5326b8892d4f9a3dc6bc57aaf6007550337
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:14:35 +0000

    glass profit level programme delight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
