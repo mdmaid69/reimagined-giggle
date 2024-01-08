bb3691043c2f1562d344147add64bff1e612eed2
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:50:46 +0000

    use juice throughout keepfrom quick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
