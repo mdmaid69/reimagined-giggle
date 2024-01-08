b6f23e142f25d9375bcd9113cfee7adc2d8617f3
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:12:46 +0000

    vanish past among cum tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
