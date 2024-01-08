ba01069b1530a6d179de983a1d058f087b50c97b
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:42:23 +0000

    blame easy any seriously lookup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
