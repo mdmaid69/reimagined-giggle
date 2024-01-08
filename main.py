3bf5a5e4509a83f0241b5905463d15175a142b79
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:35:44 +0000

    beyond couple scream squeeze tear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
