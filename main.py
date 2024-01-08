84df2eabe3fa0ae36e429a703b89f55932a2f10d
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:41:41 +0000

    broad unkind use book they

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_current_working_directory():
+        return os.getcwd()
