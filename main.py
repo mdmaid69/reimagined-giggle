248fa591ca246eb1369f7b4e014e1522a6c5c643
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:18:00 +0000

    one cost total tongue reaction

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
