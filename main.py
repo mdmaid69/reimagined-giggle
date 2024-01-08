ec97208182fd7573fed26f7ee69a082ad163c647
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:46:16 +0000

    family organize steady eye confusing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
