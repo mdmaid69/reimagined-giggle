bda032a0c1f15fcd47ad8cf2ad8874f569ee78ce
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:12:16 +0000

    happen turndown same clean record

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-print(datetime.datetime.now())
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
