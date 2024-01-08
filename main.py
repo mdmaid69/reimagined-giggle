28bacf7807cdc709424e36dde6d9ba2ed0beb95d
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:05:07 +0000

    whistle step past uncomfortable principle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
