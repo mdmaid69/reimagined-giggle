0331fd56b98494a9f5b2ea6109f613de6740730a
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:15:08 +0000

    exciting freeze Bb safety the

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
