629d65b3f86ab35883b09618f0a98420615a180b
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:36:06 +0000

    weigh sense health steer nonsense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
