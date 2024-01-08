2f2ec439dd4edd20f833fac2da643849fb6eecb2
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:28:28 +0000

    finally work dream Cc basic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_pressure(force, area):
+        return force / area
