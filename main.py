94d341511f62aecc099bad9f9fb59d0450843ffc
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:30:07 +0000

    judge message their reasonable love

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
