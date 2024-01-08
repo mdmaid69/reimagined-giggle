000cdcc96e3a419630f88ed570507587a300d0a6
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:59:50 +0000

    twist must yesterday impressive workon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
