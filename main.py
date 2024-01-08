1200ffa1a31bcae779764c15614186005c12cb2a
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:05:23 +0000

    tragedy office risk eat letter

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
