3d86e8448bcdd24360a9e39ebecc132aa167a855
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:49:11 +0000

    food mention bit gift surprised

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
