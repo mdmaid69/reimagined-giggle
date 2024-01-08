09da67998be7d5630bc97ad20ff0b7fe37a3e02f
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:24:50 +0000

    nice means breakdown practical holiday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
