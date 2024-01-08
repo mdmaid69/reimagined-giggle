5a0ae0df3b23071332df959bf73d1b9a1b910f79
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:41:03 +0000

    Tt fasten sort top amount

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
