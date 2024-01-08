526145c816c77d3cb239db1af4489898c0f4ee13
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:07:08 +0000

    telephone run fast sharp advice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+for i in range(5):
+        print(i)
