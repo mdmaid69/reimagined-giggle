fb5b47470e1b9d461dc4efd4208c88447c9b35f3
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:12:51 +0000

    work poor right star getoff

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def find_max(lst):
+        return max(lst)
+import array
+def get_array_as_int(array):
+        return int(array[0])
