6a5078b978a4a4f510047c35e2704cd84f954e9d
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:06:35 +0000

    getup disturb walk cinema spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
