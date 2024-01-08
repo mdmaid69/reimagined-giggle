b4ae37437373fbc45e629429118db987ab9e70bb
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:52:36 +0000

    middle nor gap performance grade

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def square_number(x):
+        return x**2
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
