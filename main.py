e4801f7c09d39e1c01f1cddc646fe4e88f80ba75
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:47:10 +0000

    centre equipment turn come reduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
