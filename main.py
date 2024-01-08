8b1ffe14869ac4c045f8673cc01d8d377f354bf1
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:44:51 +0000

    forget safety down much amount

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
