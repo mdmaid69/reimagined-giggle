dfb87b79330025a1ced381fd4436e06f0e351d19
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:37:56 +0000

    arrange correct bright ability physical

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
