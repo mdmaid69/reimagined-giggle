b64c66a0b1d99aab7cd589651554f1a91ad1da9c
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:36:21 +0000

    freedom communicate ticket tape Qq

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
