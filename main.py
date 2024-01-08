4862fe0913b6f91a4bdecddc8b1f672000a615ab
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:19:50 +0000

    knowledge emphasize round certain bringforward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
