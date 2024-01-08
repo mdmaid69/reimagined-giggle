32b9c74401289141f99d2b8ce967fe9885f3733d
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:30:17 +0000

    make mean especially another mail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def reverse_string(s):
+        return s[::-1]
