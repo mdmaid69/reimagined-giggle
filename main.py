fad935fd6cd21105a553a9d8435ffae8b40ded36
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:17:22 +0000

    approval bar go hit duty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import random
+print(random.randint(0, 100))
+import array
+def get_list_from_array(array):
+        return array.tolist()
