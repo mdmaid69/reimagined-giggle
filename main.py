d681fb2c3cb9b7ca74e92387e4d66677679fffeb
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:05:45 +0000

    future Zz disgusting seem turnback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
