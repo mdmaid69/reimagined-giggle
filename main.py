455dc35925aaf0defb95ca91bad87f21c2decd65
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:20:50 +0000

    fast allow plastic impossible or

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
