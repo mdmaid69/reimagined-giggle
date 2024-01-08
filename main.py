4246509b0e0668b47b92c5708a770742fa7c55dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:26:49 +0000

    present violence club fight silent

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import time
+def get_current_time():
+        return time.time()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
