7e58069581454ff7f1b427e7f226bfe9dd240d4a
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:36:24 +0000

    flat new ready minimum slow

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-for i in range(10): print(i)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import datetime
+print(datetime.datetime.now())
