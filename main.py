897eac56217f276085e4d5e02e06160ce408e5a9
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:44:27 +0000

    trial song turnout form keepup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+for i in range(10): print(i)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
