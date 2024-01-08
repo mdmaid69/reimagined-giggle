bdb1946280c026e259895613c8ef8760de834671
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:32:12 +0000

    possibility brain try valuable estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
