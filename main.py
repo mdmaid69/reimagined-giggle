e522c345e41845a6e3352712004f2c0cdd579ad6
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:40:27 +0000

    wipe soft connected dear unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+print(sum(range(10)))
