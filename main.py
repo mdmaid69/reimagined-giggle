14aaad865fee64f3fd3581bd263ea3c1adfcb322
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:57:31 +0000

    dress Mm persuade thin dance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+print(os.getcwd())
