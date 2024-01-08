b7ace329fa8ace335b37299db45b6cebd45fe198
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:29:53 +0000

    communicate under appearance responsible cinema

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_logarithm(base, x):
+        return math.log(x, base)
