d994f1456c6543c9c6388692561a5ebd1976b40d
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:53:25 +0000

    grand branch lie appeal practice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def get_array_as_set(array):
+        return set(array)
