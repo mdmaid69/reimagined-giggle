699f90a85d391a147c79b923c6a67c569e31729d
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:41:03 +0000

    vanish fierce intend wave naturally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
