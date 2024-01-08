12c6b3329ac70f8bf89888fb0efb7f706202cc3e
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:03:57 +0000

    choice disapprove supply raise do

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-  def reverse_list(lst):
-        return lst[::-1]
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
