d0239309243db458155466b12f9ccf8b90caa858
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:43:18 +0000

    have service mass song noise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
