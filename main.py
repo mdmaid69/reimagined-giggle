64844003f66b7944a9c4d018822ecd7d3f6d6a4a
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:02:47 +0000

    reach catch language kind save

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
