b009f4d0e6b859d8cd1190362b17b5c42fcded17
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:40:05 +0000

    each song crowded nothing round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
