589519205ebbcd70a3d788271be3d9aa70f77533
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:14:56 +0000

    walk unite score grade tape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
