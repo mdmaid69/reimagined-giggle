dd10bfe1d6069978f26f8259964de92a56e82aa4
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:13:30 +0000

    maintain steer government matter slip

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_acceleration(speed, time):
+        return speed / time
