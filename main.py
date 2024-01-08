67a2a63272fbbeed22a9ad58f508ff1d545d8786
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:08:05 +0000

    produce on impression single fresh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
