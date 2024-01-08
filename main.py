4533b20b8df19985bf9325ffe5fd4f8c9fad533f
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:56:07 +0000

    set permission issue pause circumstance

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def find_unique_words(sentence):
-        return set(sentence.split())
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
