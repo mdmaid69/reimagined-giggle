aa88964d4e7e399f5a27e22183fb8bcfbba9ad12
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:39:49 +0000

    closed beat place pocket side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_length(array):
+        return len(array)
