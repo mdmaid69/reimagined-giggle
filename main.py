57d6f6419e7b337b317543ff881e80fde4598913
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:38:26 +0000

    sharp satisfied steal throw callup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
