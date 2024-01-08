b108b29277f2b0f459d9b12d0c086ba08081c53f
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:16:08 +0000

    uncomfortable would perform around this

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
