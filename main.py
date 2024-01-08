6d54f9973589002fd5bf5f46ae20377579eefb6a
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:34:09 +0000

    need extremely admit figure meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
