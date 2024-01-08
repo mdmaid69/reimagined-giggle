33268da834ca0e0cabacdf1b3e7fede1b955dd22
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:07:24 +0000

    hurt almost improve job fork

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
