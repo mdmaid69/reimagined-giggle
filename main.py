c106327ebecbf152a2a8b72a0f4b5f319aa5f1b1
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:35:10 +0000

    serious impressed fall make command

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
