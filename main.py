8b303f383d021cf2b0ef6614fdb9aee6304eb024
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:03:02 +0000

    realize either importance rare laugh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
