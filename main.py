c8ce025e8c2321bcc037076ed16e31507f1b8820
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:16:48 +0000

    furniture nigga high equipment consist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-name = "Python"
-print("Hello,", name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
