aefc8ebcf2b8a7a822da7e931621743a67273f5f
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:36:33 +0000

    fetch shelter chat straight motion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
