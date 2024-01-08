8fd943998d2d52d8e6730e8c005dfc36081f2d62
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:24:24 +0000

    beside attention until good turnup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
