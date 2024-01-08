bde8477652b988f74402d7884aea63b863df153d
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:34:49 +0000

    preparation smell deserve criticism comeback

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def print_python_version():
+        print(sys.version)
