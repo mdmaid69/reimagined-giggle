8ea826479573011a93dc4cfe34913365154fb5b2
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:55:25 +0000

    why turn clean the description

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
