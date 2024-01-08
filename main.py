289c4a6498eba2db122ecd099715932ffeeac8f3
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:43:41 +0000

    murder attract confidence bring jump

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import time
-print(time.time())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
