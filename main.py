622ba2fefe2e1b56b8d0e71e7890b542dce661e8
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:15:18 +0000

    polite every objection size why

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
