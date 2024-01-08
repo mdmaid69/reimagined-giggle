bbb2c4ae6b97a0c4558635ce2cf9d70ba512999b
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:24:36 +0000

    valuable calm telephone float even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_power(work, time):
+        return work / time
