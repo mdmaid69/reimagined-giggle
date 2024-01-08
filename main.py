70f58484148987aa3846060eb4111ef7dd0f4102
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:15:03 +0000

    reduce float exaggerate hear limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def clear_array(array):
-        array *= 0
+  def calculate_area_rectangle(l, w):
+        return l * w
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
