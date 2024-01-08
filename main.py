e847a8178780dc03bc6def0246c61ba6418a217d
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:23:50 +0000

    sing characteristic then fight myself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import time
-def get_time_since_epoch():
-        return time.time()
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
