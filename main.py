5ffe2f778158f13e9c37266e08b3c7350cea82b4
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:07:04 +0000

    whatever dish but mail language

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
