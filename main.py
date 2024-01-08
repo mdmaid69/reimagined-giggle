b90c40263375a023421381aa6612349283ec2d5b
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:58:53 +0000

    news nose centre confidence principle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
