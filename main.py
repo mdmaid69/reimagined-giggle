c118dfa8dc5df8853b0e01917ce5efea2b78788a
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:10:55 +0000

    short confusing memory quit direction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
