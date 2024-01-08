42ef17405ebd1857c3908de905ef6e3c620927d5
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:46:44 +0000

    say understand win set spoil

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
