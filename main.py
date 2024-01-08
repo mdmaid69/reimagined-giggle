c61739ff05f0fa169797be515cd719f214f9d6c4
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:00:53 +0000

    below science lead pool today

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
 def calculate_perimeter_rectangle(l, w):
         return 2 * (l + w)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
