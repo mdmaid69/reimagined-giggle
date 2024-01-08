ac1027591a3abefe514ed302b742cd02640f186d
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:08:00 +0000

    tongue road immediate snow back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def find_max(lst):
+        return max(lst)
