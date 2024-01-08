1d5becb290400152ca3788a3903cf103e99aaa6e
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:25:12 +0000

    one tip match upset well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_force(mass, acceleration):
+        return mass * acceleration
