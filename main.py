4ebb97157bd4e515063460731b8e452033a8f185
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:57:04 +0000

    hot middle proud damage improvement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
