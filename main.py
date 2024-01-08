4c8a95ab3456cfe98536c32d599f269b2abc9cac
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:14:31 +0000

    flat pretty high increase instant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
