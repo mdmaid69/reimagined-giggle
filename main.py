340be25f7f2daee56f4a152b186920334d0fc426
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:42:44 +0000

    neat relation careless great giveout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+print("Hello, world!")
