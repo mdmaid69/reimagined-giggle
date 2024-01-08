6dbfad6ce4fb61ce1552525e64fc1ff6de74afc8
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:03:41 +0000

    bringdown appeal average natural turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
