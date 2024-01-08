33721d954496b0fdb6f4a69fd71e413c1ae1c4a8
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:43:15 +0000

    issue pin measure also 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def is_even(n):
-        return n % 2 == 0
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
