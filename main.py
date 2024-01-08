fb35fc628a5a0adbe0e05b44f6af1de7c2729c35
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:20:11 +0000

    home trousers floor then apparently

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
