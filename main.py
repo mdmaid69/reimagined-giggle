e8a8eb6896edfae5cd9e3ef373497e23e53726d4
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:29:06 +0000

    innocent crash replace authority listenfor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import time
-print(time.time())
+import platform
+def get_python_version():
+        return platform.python_version()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
