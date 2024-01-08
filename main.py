e91cb952ed00bd5a22ae4502ad0bef1b1744c31d
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:52:24 +0000

    embarrassed taste black store meaning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
