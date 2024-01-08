e7cc65fcfa87da6be7dfaf4dcb5431020f3e3a96
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:58:41 +0000

    basic post ashamed proper invite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
