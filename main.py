38f0379dc30df355a90aaa7e1f0bdabe099f0f9b
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:58:02 +0000

    willing complete area mainly foot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def cube_number(x):
+        return x**3
