007fa0eafa78e6186387addf780068ef1f3cb60b
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:10:06 +0000

    strong keepoff look sleep term

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
