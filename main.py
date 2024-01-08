60d4863e044000eb304dd02e71c5016aaf586842
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:29:14 +0000

    bringback act turn frequent journey

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
