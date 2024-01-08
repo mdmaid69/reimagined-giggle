1ffeab0dbdbffa0cbfea25daf4ec9c3051e2382f
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:02:42 +0000

    original material popular challenge stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
