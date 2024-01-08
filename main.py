8f40f629fccdc900bce68ca9423477a8385c16bf
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:37:53 +0000

    recently suit chase risk turnon

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
