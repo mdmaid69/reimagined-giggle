84a26aa3319f1b30b3d91c7fcdbcffa80d34a472
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:17:19 +0000

    method Zz Rr guess especially

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_speed(distance, time):
+        return distance / time
