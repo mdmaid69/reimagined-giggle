26732fe359cec10e21b2059e44bb49ba20a5284e
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:41:57 +0000

    another related date check careless

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import sys
+print(sys.version)
