c91ddfba1695ce4abf9c938f44d68062f435ec3a
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:51:45 +0000

    hers considering much nature close

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
