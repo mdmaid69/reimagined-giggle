1088c67fcb22cc3d50199a5d835f30c0de4ea139
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:13:30 +0000

    rise sign nowhere weak agreement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
