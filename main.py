fff0c3b2bc254b7f28d7b33face4f22f08c576d5
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:12:29 +0000

    occasion professional careless scream favourite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
