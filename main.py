a1e195455b0914e1b3ce4d51837ee665c719ca9d
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:24:16 +0000

    manage wish content pronounce away

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
