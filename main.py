e7e72ee963a0764176762cd7d73a0fc100354f17
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:10:59 +0000

    embarrassed file angry bunch circumstance

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def reverse_list(lst):
-        return lst[::-1]
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_area_rectangle(l, w):
+        return l * w
