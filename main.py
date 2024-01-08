33afe14f7ad72295a5713fee02b8136198f1abd7
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:20:44 +0000

    pause doubt education confident leg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-print(time.time())
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def get_array_typecode(array):
+        return array.typecode
