f56c1c33dd4b692bc4767a1bc6731b25298d934d
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:44:58 +0000

    season thankyou edge common target

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
