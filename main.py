22fe934be469b8c0dd79723cbe145e886acec5fd
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:19:31 +0000

    recognize pupil relief manage university

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-def count_words(sentence):
-        return len(sentence.split())
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
