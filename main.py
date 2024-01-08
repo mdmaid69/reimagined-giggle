a711348726860ed475f085f6c87e3d371d48abc9
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:59:49 +0000

    instant estimate name error listen

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  def remove_duplicates(lst):
+        return list(set(lst))
