3672f38bdbabbad2d2ae2b06cbf6b63eeea20e0b
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:01:59 +0000

    keepfrom visit impressed produce transport

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def is_odd(n):
+        return n % 2 != 0
