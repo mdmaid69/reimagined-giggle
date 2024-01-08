9f2f1f36e429acb22e8d612c80f84fd6582913c8
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:28:32 +0000

    order complicated either set 179bite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
