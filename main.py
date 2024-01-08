76c90f3681baf491256a828af0656e42c2d67559
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:06:44 +0000

    everything no giveout fast spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import getpass
+def get_username():
+        return getpass.getuser()
+x = 10
+y = 20
+print("Sum:", x + y)
