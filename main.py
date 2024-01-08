603c6e59124acdb183792dc703cf6e5599465e7f
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:32:41 +0000

    unfortunate worse hurt money occur

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
