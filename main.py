6b0fd0b2f90c44e9ae32f95023e0a5eaf2eb6f5a
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:45:10 +0000

    deliberately greeting method where recognize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def subtract_numbers(x, y):
+        return x - y
