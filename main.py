ae51515f287fa9d938ce000011d00bb0e4beecaf
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:27:11 +0000

    street computer follow what improvement

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 def calculate_density(mass, volume):
         return mass / volume
-def calculate_average(lst):
-        return sum(lst) / len(lst)
