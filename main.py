5545d83fb7cba83355357448cae44d3eda6a0086
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:36:18 +0000

    skill black report meet laugh

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def is_odd(n):
+        return n % 2 != 0
