b1b9ceacb61b381122bd74494267cbaa5b229b99
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:08:46 +0000

    squash lookafter whether interest strength

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
