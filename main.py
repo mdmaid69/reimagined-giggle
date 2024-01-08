ab4dc9e224b425ad57ef25f545f1e018a54776ab
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:04:09 +0000

    state through behave Ll dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
