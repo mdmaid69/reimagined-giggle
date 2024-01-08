dea4453fdf3d94a13b256fea0df36d86cb163271
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:40:39 +0000

    wrong school total wire attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
