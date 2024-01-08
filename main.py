4d47fa411a25038fdb73e1128e8d584c0c591770
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:04:43 +0000

    prevent approval wait her dream

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
