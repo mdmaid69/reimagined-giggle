18730aa4d4b5a1615a3790c2c90dd08f63adaaa1
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:20:31 +0000

    shake careful public crash belief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_power(work, time):
-        return work / time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
