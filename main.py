2b438eebea590d6c3191e47e86803f7ccdb3b1dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:54:55 +0000

    answer calmly foot fall protest

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_index(array, item):
+        return array.index(item)
