4d7d6661c1f97f6124ad01a7122b3c1932537f1b
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:29:05 +0000

    rarely business tomorrow generally less

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+print(sum(range(10)))
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_itemsize(array):
+        return array.itemsize
