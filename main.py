d7117cff78cc3a5a1fc724ed418d1e5f0f2217a7
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:46:21 +0000

    life there search wrap wrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
