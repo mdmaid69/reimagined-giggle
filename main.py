731196258d30f6c1474082c9a4a9e2318608b962
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:17:30 +0000

    makeinto repeat reasonable for bin

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
