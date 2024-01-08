231ec4fb2624b2ea4f35302b052d2cf701cba200
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:34:11 +0000

    promise further spread heart comeover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_power(work, time):
+        return work / time
+def calculate_speed(distance, time):
+        return distance / time
