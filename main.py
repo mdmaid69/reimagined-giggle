089de209de800af4f0ab5c34a7842d0071e875da
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:42:45 +0000

    bush interesting cinema complicated ought

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def greet(name):
+        print(f"Hello, {name}!")
