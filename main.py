5e27659fc985b5f22e65d170758fd000cd24a6f3
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:30:11 +0000

    ticket mixture uncomfortable decide still

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def sort_list(lst):
+        return sorted(lst)
