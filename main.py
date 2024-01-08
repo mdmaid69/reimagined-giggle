63f6f94174bbebc482af0185a4b10fea806ecfb4
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:51:51 +0000

    terrible proper big professional everything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import time
-def get_current_time():
-        return time.time()
+def get_array_typecode(array):
+        return array.typecode
