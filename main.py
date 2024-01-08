07658850a17cb9f35ac8d9c539c35e829f476685
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:13:35 +0000

    activity getback create achieve she

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
