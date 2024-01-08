0ffda77b20140cce3faa1d7f99df0d0e5c640540
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:52:40 +0000

    fire ill impressive set bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import getpass
+def get_username():
+        return getpass.getuser()
