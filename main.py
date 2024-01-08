5cdd0cf34d2ae79cc81ebc96c2ccca1df89e1a1a
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:07:33 +0000

    while grade figure towards risk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
