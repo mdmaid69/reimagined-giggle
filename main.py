46b8f4f9dc6f91a8c4dd29422e724e02fc40423a
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:28:49 +0000

    stone need basic giveup last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
