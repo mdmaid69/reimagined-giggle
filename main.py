eeffacaa8eddfe1d98a8fc40c49c34968836e465
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:48:52 +0000

    after doctor joke tape reasonable

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_queue():
+        return collections.deque()
