c3876e51456d6dbbb8beebfe412e2bdb5f6b3f30
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:20:45 +0000

    forward repair shop arrange personal

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def subtract_numbers(x, y):
-        return x - y
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
