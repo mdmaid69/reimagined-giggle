133dce1480c692ae9fa96461c13fe95d92d15e67
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:39:44 +0000

    think subject gas ask aware

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def get_array_as_list(array):
-        return list(array)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
