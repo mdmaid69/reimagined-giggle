fc17eabf1f9bdf86840d0b00ff2dba6f3cbc97a8
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:06:37 +0000

    plant cost tip job suit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
