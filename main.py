12a7cc0c27c2792ebce48ff2c40df23456868de2
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:45:10 +0000

    giveback refuse should belong fire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
