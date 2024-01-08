c47ba5b9b1a072a8b0906193da08341529008383
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:30:39 +0000

    you impossible message them scare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def calculate_variance(arr):
+        return np.var(arr)
+import random
+print(random.randint(0, 100))
