c961fdc87ca9fabbeb94775749754017f155e6dd
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:05:09 +0000

    Ff pure almost bringback somewhere

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
