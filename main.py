4bfb553c02a52ddc9881dfb55eba9b29ca4eff89
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:52:45 +0000

    independent behave national border begin

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+print(sum(range(10)))
