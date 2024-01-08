6329cf9b1c82fd19cedba3009d87bc5d96e43df4
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:07:40 +0000

    dress move view sweep explanation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+i = 0
+while i < 5:
+        print(i)
+        i += 1
