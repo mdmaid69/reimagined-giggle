eaefa0867c1248b566c38d4ed8920dea83a7ff47
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:38:55 +0000

    sale interfere jump yesterday near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_list(array):
+        return list(array)
