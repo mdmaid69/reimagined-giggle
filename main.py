16f358279a285cc6084e8284df29f66f893da916
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:33:41 +0000

    trial free nowhere choose persuade

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
