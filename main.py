105b5d44e0f2f5125fcff2358c7e1b8c1b07e5a1
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:59:49 +0000

    mass profit show nothing hard

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_max(numbers):
+        return max(numbers)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
