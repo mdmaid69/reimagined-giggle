d8ff62de0dc35fa24add7493f941c90ff56e41c8
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:10:12 +0000

    money married declare shop ought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_sign(x):
+        return math.copysign(1, x)
