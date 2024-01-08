73e1f00574c21e417dbe1ccd1b1e3d110fb42346
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:56:26 +0000

    mind shall geton after nearly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
