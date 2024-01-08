0ace00090452811eac52c43bca689980ace94583
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:45:37 +0000

    slightly deliberate cry lot tool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_exponential(x):
+        return math.exp(x)
