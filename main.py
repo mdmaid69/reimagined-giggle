e14854abeadd70336f81a61fc2e22bf74953ba72
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:19:43 +0000

    time question switch block dress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import time
+print(time.time())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
