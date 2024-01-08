1c549b2bdd49ca16ffd928ca0dd98f7784972780
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:41:47 +0000

    everyone whether occasion smile Ee

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
 import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
