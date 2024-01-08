4c8443023e834ddb80af9c0171b8109dd426f3bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:55:45 +0000

    oil ever worth dry act

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
