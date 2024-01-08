abb79890450e8a9c2b4bdc2ad837ed134a1b2c71
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:54:46 +0000

    excuse guilty read disappear produce

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def is_even(n):
-        return n % 2 == 0
+import sys
+def exit_program():
+        sys.exit()
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
