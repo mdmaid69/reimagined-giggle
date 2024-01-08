30ba339f1035d3ab42148f234149072775e24c1d
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:56:22 +0000

    there short push owe wander

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
