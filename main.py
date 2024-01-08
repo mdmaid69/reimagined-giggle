bd5fab6d01c8fe51ca7d00acc6f7209458fc8286
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:55:02 +0000

    past offer violent steady takeout

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def convert_to_hex(n):
+        return hex(n)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
