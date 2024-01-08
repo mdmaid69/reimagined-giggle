6c1431ac204b95fac1247059d7653758c39fcaa2
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:11:54 +0000

    dangerous able mistake beyond please

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def add_numbers(a, b):
+        return a + b
