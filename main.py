7d2516ccda66885065a6ee5b272b6c93f9b3389e
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:28:55 +0000

    keepout drink schedule pot design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
