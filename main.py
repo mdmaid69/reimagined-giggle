53c1434898c7d13d9e3ba826cbf44135463602b6
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:07:52 +0000

    sign long noise look scared

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
