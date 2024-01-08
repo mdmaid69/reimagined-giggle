4f6ce92a8173fd3ccde3df521ac7bdd252a34012
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:44:52 +0000

    traffic slide lie my fork

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
