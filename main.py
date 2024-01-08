2e5359bc5f7c63a08f8a8d9bc48ba59609302948
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:49:43 +0000

    radio square intend town building

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
