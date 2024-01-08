adff7186ce680e755ac777cf2ebf36ac1e94e234
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:36:21 +0000

    flight comeoff change moment tap

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import re
-print(re.match("h.*o", "hello world"))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
