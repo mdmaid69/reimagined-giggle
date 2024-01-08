1d47c712dce9d4735fbe9e893528d4962d5436b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:24:47 +0000

    or speak yet offend bring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
