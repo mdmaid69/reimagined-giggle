1fd6d01ff292661345d81bba01b2ceaa39820087
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:40:19 +0000

    getout normal break reply warning

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def convert_to_hex(n):
-        return hex(n)
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
