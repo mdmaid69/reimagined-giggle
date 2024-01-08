981620772801b8b823f0b47fc210d6bf5196f241
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:36:17 +0000

    letter memory hot information fall

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+print([x**2 for x in range(10)])
