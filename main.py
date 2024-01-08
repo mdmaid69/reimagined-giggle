c6ee293d72e9c250e31a45e650cbd749206ad50c
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:13:28 +0000

    friendly pot raw shop right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_as_float(array):
+        return float(array[0])
