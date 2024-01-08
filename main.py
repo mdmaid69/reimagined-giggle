caf7eb394b8cd42988e5b44702c5b46882fb1323
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:17:30 +0000

    minimum apparently test pull card

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import datetime
+print(datetime.datetime.now())
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
