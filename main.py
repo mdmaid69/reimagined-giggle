b36aa4703e08dc756b8d5cf9bc141c5c54445d36
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:03:43 +0000

    pair more whichever best makeinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
