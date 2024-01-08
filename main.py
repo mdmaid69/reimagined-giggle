90083784b9cb6a7cedb196448249753fb24defab
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:19:26 +0000

    offend he dear ilovedicks favourite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+for i in range(10): print(i)
