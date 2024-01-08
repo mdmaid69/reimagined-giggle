34a224905ef9337520ebe595a0e1138d5415d3d9
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:15:00 +0000

    cause start argument settle favour

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
