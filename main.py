b0fae0035bf83362ad29b96b080294d445da69e8
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:30:57 +0000

    opponent complaint contents skill better

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_float(array):
+        return float(array[0])
