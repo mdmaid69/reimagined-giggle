8f06e0f492d48e8ff5b333bf256bf8f3b2674f19
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:44:08 +0000

    leg guess dream people within

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_length(array):
+        return len(array)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
