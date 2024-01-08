db3e1c53efa2118892ce62fbbed3460127286b50
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:08:26 +0000

    this plenty wide the glass

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
