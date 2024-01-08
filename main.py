6e280a2c269b4e70d685882dc347ef753bfb648f
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:16:03 +0000

    bear ability march family mess

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def remove_directory(path):
+        os.rmdir(path)
