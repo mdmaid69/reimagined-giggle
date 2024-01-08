69657d107702d0e214baa7ba9ee3f4813e0ac19a
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:36:45 +0000

    talk seat disappear acceptable visit

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  def cube_number(x):
-        return x**3
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
