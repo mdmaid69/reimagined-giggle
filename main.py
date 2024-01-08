9258e7bd93f5bb03a909c47483aaaf426df43440
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:03:08 +0000

    minimum somewhere science both should

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
