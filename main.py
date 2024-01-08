e003ed097f81e725370805ffb1bce2395c6dcf41
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:04:06 +0000

    concern accident cough base muscle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
