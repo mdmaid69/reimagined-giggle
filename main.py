910d48fd813f7e2ab6609bce86b91b6cee9afaae
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:43:29 +0000

    ahead class doubt smoke turnoff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def sort_list(lst):
-        return sorted(lst)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
