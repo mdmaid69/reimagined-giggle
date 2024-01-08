564e140abbdcc831f28350cda7e39f8e64e95bb5
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:41:38 +0000

    would comeover rank lookafter getout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
