ed4931f39af84f67daf21bcd44ea09912088f424
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:21:03 +0000

    compare workon clean translate backwards

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
