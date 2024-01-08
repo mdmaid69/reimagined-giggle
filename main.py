6a9106732d91780d6d14c36804b6258ffebe16f4
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:36:14 +0000

    doctor demonstrate message release accident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_exponential(x):
+        return math.exp(x)
