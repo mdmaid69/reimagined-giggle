7a107157d4637b3a76017a8c1cf66356c60b3fd3
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:08:36 +0000

    guard during second opposite design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   def fibonacci(n):
         return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
