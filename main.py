8e24c524dad185aac2385ba942655d3f4184bf04
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:12:02 +0000

    flow plenty load basically content

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
