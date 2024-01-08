26d5dfbdb8703d8acd36e6cb38a9b6d621896b2f
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:32:10 +0000

    skill tough building trust paper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
