1c171369339c539d4a23e2209172e687b57786e9
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:23:16 +0000

    great round tall guide swimming

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
