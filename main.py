2355df852f63f1903467532e3ff9cfe29a0c146b
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:57:20 +0000

    equal need cost through copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
