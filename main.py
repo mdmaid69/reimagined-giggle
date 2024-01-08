14b4ae90f93da7b8b05015482abd2c580d14f755
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:37:25 +0000

    away getoff heat father comefrom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
