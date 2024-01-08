2596a099ce54700f4f93617babe4467714ddfae4
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:06:06 +0000

    fire minimum destroy talkinto important

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_array_to_list(array):
+        return array.tolist()
