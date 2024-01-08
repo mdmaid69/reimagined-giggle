8685d194e7e8c470a2f5b478a95ae5ca3e3dc4b5
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:23:49 +0000

    away herself picture who snow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-text = "Hello, world!"
-print("Characters:", len(text))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
