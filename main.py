e2429b313091b075e21348660bff53013447c31d
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:54:40 +0000

    key conversation hand attractive pencil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
