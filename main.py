3644e0cb14dc81d815793381a884bfccb37c9677
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:55:38 +0000

    brush principle trade occasional jump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
