7f841b7f7597201822e6aae58f27dd869ce17b69
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:17:38 +0000

    lesson roll family price star

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def sort_list(lst):
+        return sorted(lst)
