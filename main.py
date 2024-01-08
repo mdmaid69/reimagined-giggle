5e7693316d98a5f209d11677f70dc09a9558c00f
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:45:09 +0000

    doubt value big film man

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
