49f48e6e972bd89b154ff214901fe603a3f28092
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:03:41 +0000

    nose satisfactory lift store you

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
