7b26bbfb03f1768d5731bb59bf74555269b21a13
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:27:13 +0000

    information ill bringback flight sharp

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def split_path(path):
+        return os.path.split(path)
