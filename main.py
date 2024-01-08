2fac7afc6557f9ffaa3473f9675f3c88681e2cef
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:51:38 +0000

    live design like image reduction

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import collections
-def create_stack():
-        return collections.deque()
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
