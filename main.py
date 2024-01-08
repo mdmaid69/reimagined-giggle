6964522406649de938b54185b997e16f6bdb1f5f
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:31:21 +0000

    burn responsibility start wall describe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
