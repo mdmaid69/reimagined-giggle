65a4b69b5ca41eab44e779ac9ada45a41bdcd09d
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:05:54 +0000

    job spin rain spring from

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def is_palindrome(s):
+        return s == s[::-1]
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
