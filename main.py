67f96fd8cb1740bc8cff0397ae259b93819aa03a
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:31:05 +0000

    wash once strike rescue remark

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import numpy as np
+print(np.array([1, 2, 3]))
+def count_characters(sentence):
+        return len(sentence)
