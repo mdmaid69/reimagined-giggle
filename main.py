184580c77d64d54423af71802880a9fc9faee05d
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:49:54 +0000

    branch connected throw guess such

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
