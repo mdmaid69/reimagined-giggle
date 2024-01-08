55d97a2e8dba5687c9a5754acf62009ccba8cee7
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:22:07 +0000

    bang unlucky illness yet comeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
