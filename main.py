c3d71853a432c804c548d59ea89972301d20decb
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:17:04 +0000

    regular tip right very arrival

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def clear_array(array):
+        array *= 0
+def calculate_power(work, time):
+        return work / time
