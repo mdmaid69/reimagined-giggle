95f8d8de73220e8839eee958546045549d11d0e5
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:48:59 +0000

    neither change lie argue border

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
