3d8347bc7d869d372a5d4593efce79112ed3a70e
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:23:55 +0000

    cause be change 573entertainment give

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
