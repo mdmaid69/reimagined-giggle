0777723617b1d6ca9e3648dc5ef3a8575d3083c4
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:34:46 +0000

    immediately like twist uncertain defeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
