d09473364c474c2441e9a26c6a0dfa8a7699b008
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:27:59 +0000

    arrive notice male opinion own

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
