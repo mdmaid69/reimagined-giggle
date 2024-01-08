a8cf8c036e6f2bbc985dd6a51614ebbbd6a2c3f4
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:22:25 +0000

    pass cough talkinto low pollution

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
