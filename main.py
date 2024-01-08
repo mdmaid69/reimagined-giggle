0c22a0c539730b937878286c0f115472cc015a7b
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:07:43 +0000

    once plenty attractive argument borrow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def pop_from_array(array, i=-1):
+        return array.pop(i)
