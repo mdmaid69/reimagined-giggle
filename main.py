93a0fcbe68d9707e6812b871ae542e99c9e2ebea
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:53:17 +0000

    attempt label see bar 570enjoyable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import array
+def reverse_array(array):
+        array.reverse()
