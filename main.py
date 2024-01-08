903ef708a6a6be15a3a7b1089c2afbdae0ea0f53
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:37:55 +0000

    load powerful makeinto start good

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_item(array, i):
-        return array[i]
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_area_rectangle(l, w):
+        return l * w
