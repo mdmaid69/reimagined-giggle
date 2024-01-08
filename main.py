3184325b2e6861480324aff9ac4de9d997a41f0c
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:56:38 +0000

    difficult after 568energy maintain content

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def find_min(lst):
+        return min(lst)
