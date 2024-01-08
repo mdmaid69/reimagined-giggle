281fd6f4a0584df7c9f55d3093c121a543b8176c
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:02:47 +0000

    negative Nn degree patient alive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
