abeb2572eb9c233cabf8e51fd7a9c45df3b78e7f
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:01:41 +0000

    typical trousers once score empty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
