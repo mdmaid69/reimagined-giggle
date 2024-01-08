1b69ee82a23c302e97c587cc09abc58d915550ac
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:43:25 +0000

    excuse brush closely repeat law

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
