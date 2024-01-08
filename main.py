a5d2cc616b4b9fa7f33bd1f56dd210907ae84b5c
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:44:07 +0000

    slowly film store they letter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import sys
+def exit_program():
+        sys.exit()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
