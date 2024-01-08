04fc0675b35fd6af79794a9e026569143e5c5bc0
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:41:48 +0000

    tip closed neither no show

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def is_odd(n):
+        return n % 2 != 0
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
