b313a9bc724663695c3d363b6cfaa9fbe6ed955f
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:44:16 +0000

    neither lost defend leg warning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def cube_number(x):
+        return x**3
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def insert_into_array(array, i, item):
+        array.insert(i, item)
