b4a7a19f34cc66d98e77a4abf6369e61ed0021f7
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:08:30 +0000

    eye hear range sign production

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
