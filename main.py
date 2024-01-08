29b18e94e86d77c94e930032feacf2366aa25903
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:02:06 +0000

    runinto objection glass top embarrassed

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def greet(name):
-        print(f"Hello, {name}!")
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
