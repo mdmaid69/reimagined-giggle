1c34721f79be2eaf927b404553ae50c1d129ac7c
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:58:17 +0000

    dark circle straight every different

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_user_list():
+        return collections.UserList()
