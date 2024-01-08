498c4fd81cfba2aa6c840cb2ae8ca58e8adc969d
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:22:10 +0000

    relief quality dinner so add

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+print(math.pi)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
