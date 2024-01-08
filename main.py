e12eee991299a3f43909663e4e081d994bf58378
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:18:20 +0000

    smell not obvious programme drive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-print([x**2 for x in range(10)])
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
