55e54e2bf48d8b69946d39749f0712a4f18bd1e7
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:28:56 +0000

    flavour towards him summer kind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_list_from_array(array):
+        return array.tolist()
