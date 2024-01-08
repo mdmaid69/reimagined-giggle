21385dd61035d907f77decde415fc0d98766118d
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:39:01 +0000

    them square telephone sorry recent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
