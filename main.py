9286ea3fde3c7e0047377f2e3a511c456ae7a79d
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:03:59 +0000

    touch phone lead nurse start

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_pressure(force, area):
+        return force / area
