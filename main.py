020d56b444e1f8febb8e3df31822d9ad0fb75e01
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:13:06 +0000

    turndown thing always hate cost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
