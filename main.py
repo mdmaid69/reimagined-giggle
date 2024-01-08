5cb9a4b9a906dee3b746e355563e9999d192f6c4
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:26:28 +0000

    standard field calmly attempt would

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def sort_numbers(numbers):
+        return sorted(numbers)
