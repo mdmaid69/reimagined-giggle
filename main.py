e873dd366889b5fad8aa448fe6c2c6f22b592d2e
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:00:29 +0000

    dream yet fast remove somehow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import datetime
-print(datetime.datetime.now())
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
