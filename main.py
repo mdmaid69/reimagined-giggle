0d970cf4a619626c7328acc2ff878a5b851ff41f
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:05:33 +0000

    scream Tt fly tell exactly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
