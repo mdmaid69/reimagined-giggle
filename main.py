e80bb5e73df15ba7f63832c897ccaa15919d9a21
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:08:56 +0000

    scared maybe suit cough approve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-print(math.pi)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
