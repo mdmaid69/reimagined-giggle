5bcd0144f9454a8d0bba49c6dca3a78825890c97
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:47:08 +0000

    translate perhaps choice proud compete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def square_number(x):
-        return x**2
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
