4c617aac2d9ee18ce488aa9206da75b6ebd67f6f
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:40:23 +0000

    surface personal run main head

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import collections
+def create_user_list():
+        return collections.UserList()
