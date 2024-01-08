14050766609aabb4de4660cc3a4045d92d9a7f53
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:45:13 +0000

    mainly immediately boring say shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_base_name(path):
+        return os.path.basename(path)
