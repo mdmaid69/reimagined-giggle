3f7323dd053c0b75c9524d1edec554982aa1692c
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:07:36 +0000

    nature 566end shop compare cap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
