c35d5381734cfb031baa7c6c625f4ae9ffddc281
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:41:59 +0000

    separate warn cut sensible spring

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import tensorflow as tf
-print(tf.__version__)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def square_number(x):
+        return x**2
