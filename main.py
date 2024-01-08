7a9b528e593bff79ff97709b7f2af17e28dc95e6
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:00:50 +0000

    connect accident effort risk interfere

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_power(work, time):
+        return work / time
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
