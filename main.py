5d533a11902761d627f3d993412e8db797a765b5
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:36:50 +0000

    convince sign 568energy involve bother

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
