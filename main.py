9ab56fc80c14b92fb1af69541229bc2fb98617f1
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:30:58 +0000

    bus ring approval tour book

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
