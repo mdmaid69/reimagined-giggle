57a7de2bebd2004eb910ddf930f79a492fb386f7
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:27:23 +0000

    deep silent takeoff there show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
