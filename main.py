b71a9750526f0fa32f6e5c8d2d79520f25a56858
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:24:51 +0000

    neat disappear delight pot scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import os
+print(os.getcwd())
