f467d5cab050c970edb7f96345d1fd7cde63db51
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:16:46 +0000

    abroad couple accidentally contact confident

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
