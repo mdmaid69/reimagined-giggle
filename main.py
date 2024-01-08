c03bbbec3b134952b38ec324e686555618010b9f
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:25:17 +0000

    understand nice even ilovedicks realize

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def set_array_item(array, i, item):
+        array[i] = item
