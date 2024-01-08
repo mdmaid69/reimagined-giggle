1e584f305473982a9b15fb65c18d3d7b9549a8aa
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:44:21 +0000

    difference platform pour community power

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
