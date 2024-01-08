d7c56c9fa9e7983d11714900b0a6dd26cfbde017
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:04:14 +0000

    thinkthrough settle if left complain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_min(lst):
+        return min(lst)
