01db5f90a55a16628e33185debe4ee4eb8d8371c
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:13:11 +0000

    may back project scream bury

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  def count_elements(lst):
-        return len(lst)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import datetime
+print(datetime.datetime.now())
