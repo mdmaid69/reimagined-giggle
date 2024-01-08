5e633973e7c859d172bc677e6913bd8041dd3d4e
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:43:17 +0000

    risk whole finally recent sail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def square_number(x):
-        return x**2
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_density(mass, volume):
+        return mass / volume
