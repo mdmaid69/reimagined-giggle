38c6e49648f1528bbec7bee7702ffe8025fca355
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:38:20 +0000

    better unfortunate poor cheque manner

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
