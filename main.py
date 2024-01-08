de102fb5cecc8c8deed7206b0372294c31d153cb
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:32:03 +0000

    shadow badly female number name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
