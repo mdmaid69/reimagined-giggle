7be917320a3f6d078d5adc4e480a298e4991f662
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:54:43 +0000

    smile wire business suffer closely

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def is_even(n):
+        return n % 2 == 0
