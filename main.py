175640b93a9389d91327230cce0040c5e0f1aa60
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:01:03 +0000

    curious marry judge unfortunate 571enough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
