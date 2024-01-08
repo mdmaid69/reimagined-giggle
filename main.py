c87d9a43de74f18fab62f268e338e76f70918b75
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:18:54 +0000

    best you else back hope

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
