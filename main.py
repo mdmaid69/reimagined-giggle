c233fe581eab8fbbe0f5830473856851e61590d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:39:28 +0000

    scare apologizeapologise impression grateful ordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
