55f7c7ce26d15543578b2c98a31b4d1ba752fdaa
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:50:14 +0000

    guard rely needle attempt surprising

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sys
+def exit_program():
+        sys.exit()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
