e3a9e7eb0b3dbbe2f1b00a7d45ae2fc5e3f66d9d
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:10:29 +0000

    but strange too commit name

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_cosine(x):
+        return math.cos(x)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
