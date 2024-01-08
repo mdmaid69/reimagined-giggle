195ae7e113b996f702c3c83db43b784678a9e136
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:04:55 +0000

    on after ill form market

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sys
+def print_python_version():
+        print(sys.version)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
