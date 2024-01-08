2306a669358b07df67008c82ed0b1355b02af196
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:43:22 +0000

    never possibly complain exit add

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def is_odd(n):
+        return n % 2 != 0
