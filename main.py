af773390ee98a813ec0f0f00eff8c8b203fdb59c
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:37:05 +0000

    concentration rest treat break cause

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
