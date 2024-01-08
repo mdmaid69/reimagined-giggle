0dbdcf577dfb41e9251db8428b2e536f898d40b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:16:29 +0000

    own 179bite knock reply takeback

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import time
-print(time.time())
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import getpass
+def get_username():
+        return getpass.getuser()
