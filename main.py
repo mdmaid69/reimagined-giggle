8c2065919ae25a49f147a4b4dd1df50ccf3c9a60
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:19:13 +0000

    answer uncomfortable first appeal last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
