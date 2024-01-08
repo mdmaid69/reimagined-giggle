f6925e2898a4d9c4ace2b3bde47842f865be3891
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:21:30 +0000

    thinkover measure convince earn accident

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import json
+print(json.dumps({"name": "John", "age": 30}))
