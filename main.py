38ebada8dfc59ae0805bf76efaa01f28bc5fa62f
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:30:33 +0000

    nearly concerning intend fault cap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
