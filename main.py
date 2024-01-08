edabac7ae84616938fda145195495cd4ff920330
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:06:08 +0000

    sea reply that doubt share

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+print([x**2 for x in range(10)])
