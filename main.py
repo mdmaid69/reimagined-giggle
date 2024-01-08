9df706649db279e8a14c4f13311b3f404dc59b2c
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:00:59 +0000

    here press unfair expression mix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
