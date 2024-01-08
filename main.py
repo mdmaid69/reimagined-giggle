6ea30b0e68b8467328b62bce839b2cec56d54c91
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:55:01 +0000

    pump claim damp hollow thing

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def convert_to_octal(n):
+        return oct(n)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
