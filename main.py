bf368b5c2b26fb3f993e6ac900a0fcf6c06b8e2c
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:32:45 +0000

    style story foot reply vote

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
