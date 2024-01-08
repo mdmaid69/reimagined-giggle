34aa9734a43983d49731227b73c66221f706d653
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:08:26 +0000

    club skill everyone Qq bang

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
