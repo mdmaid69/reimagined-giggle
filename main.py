672d8ed5f00cc27737b94b227c44a54fd00da0c3
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:15:11 +0000

    sight weigh sudden feed win

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def remove_duplicates(lst):
+        return list(set(lst))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
