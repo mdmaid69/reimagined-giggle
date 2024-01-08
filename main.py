255926bd891c7e5e6cd2cac31b94c20d9b77c7c7
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:33:57 +0000

    plan rush exploration Ll path

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+print(math.pi)
