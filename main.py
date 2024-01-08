02ff19b9b06192f6f90f155025c43364de941026
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:03:34 +0000

    discussion drink Ll battery better

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  def sort_list(lst):
+        return sorted(lst)
