57a5026602cd075cc6659257fb4e11dfde9b1277
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:39:24 +0000

    Ww temperature operate if town

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_acceleration(speed, time):
+        return speed / time
