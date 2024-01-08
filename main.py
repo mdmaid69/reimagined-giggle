231a77e2a508aa796eb4273aa497ece82a14bdd7
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:05:22 +0000

    cheek once anyone bother plate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_area_rectangle(l, w):
-        return l * w
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
