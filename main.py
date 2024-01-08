4d614418f0741101cf49a5db9f2da97f891894f7
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:30:08 +0000

    roof crowded Ff freeze home

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
