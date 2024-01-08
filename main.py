aaa9c0dae0515ef6abce171f453172d57a7dc6f3
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:34:47 +0000

    sister wherever apologizeapologise advice fond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
