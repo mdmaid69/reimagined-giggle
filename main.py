865c13692ea7f749a46bd4820602eea345b5b1fa
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:42:39 +0000

    apart Ee responsibility comeoff refuse

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+import array
+def get_list_from_array(array):
+        return array.tolist()
