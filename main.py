c1956fb6b5303823a7efbf4388f86729a5639150
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:21:51 +0000

    566end learn summer fear live

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
