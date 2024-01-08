2777beedb23bfd6f21f54fe13c98e458c5f9a860
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:08:56 +0000

    occasional pour wild autumn mean

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
