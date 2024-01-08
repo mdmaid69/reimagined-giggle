98ef7460ab84a9fcc17e1baf1eec53c951a950d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:58:00 +0000

    strong direct finally face skirt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
