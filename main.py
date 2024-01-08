1746dc47257553087fbccbcf84d5d74431f1bab7
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:48:22 +0000

    motion they wealth wash share

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import json
+def convert_to_json(data):
+        return json.dumps(data)
