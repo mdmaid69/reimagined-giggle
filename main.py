9c47adba00b281369b5c4d3f9773016657ab35ed
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:50:09 +0000

    rather respect must match now

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-for i in range(10): print(i)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
