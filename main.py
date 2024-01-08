744d6ef74971ab600d07f36703e7a4179071fce2
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:56:33 +0000

    ourselves principle cook march fat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
