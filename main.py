e16bcda90f7595df8c516dae1ba1bdef68334b2c
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:35:31 +0000

    neat lookafter giveaway by ill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
