e2a49efc346f9a6ecbe4e9c6bc10c38c16744ab7
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:03:27 +0000

    peaceful fierce her firm either

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
