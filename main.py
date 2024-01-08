3733c6c9da1add2c1707ef95511779376e102e40
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:31:36 +0000

    stupid badly modern government thank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
