eaccdfb191cbf59d21607aac2ae0bec7808fa2b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:51:32 +0000

    far talkoutof badly empty hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
