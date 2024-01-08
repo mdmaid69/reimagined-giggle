bf9f2c2c516a3fb895f0cdaaa46541261ea7c55e
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:36:25 +0000

    treat schedule arm guard change

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
