b57373fc23c8ae62e16e624d84f08563a7b1a6c3
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:55:39 +0000

    thorough set their case spoil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
