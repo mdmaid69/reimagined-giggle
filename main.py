8fdd3d2bdb696c4bcb33a52e1fdc9b846aaaaa07
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:13:17 +0000

    popular hardly passenger may complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
