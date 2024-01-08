e067f9b0a9706d78aebc2895314a3eeabc904947
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:09:24 +0000

    passage half cost become snow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  def count_elements(lst):
-        return len(lst)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
