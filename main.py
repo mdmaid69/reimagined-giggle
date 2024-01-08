27602823321e4c453fd1c241172c6fed76c7e73f
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:48:14 +0000

    approve problem objection bright reach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  def sort_list(lst):
+        return sorted(lst)
