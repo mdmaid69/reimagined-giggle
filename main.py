5e173d21c994eb67ec4bd409095493cce070d97d
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:35:52 +0000

    platform general detail possibility floor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def cube_number(x):
+        return x**3
