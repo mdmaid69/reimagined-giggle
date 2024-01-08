d4aaa49f116445ad63775577f9e8875409e0efdd
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:43:42 +0000

    only today division sink deliver

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def convert_to_hex(n):
+        return hex(n)
+print(sum(range(10)))
