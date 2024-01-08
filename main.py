e73f9e3ee9908f2bffd60637441e27af444af783
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:51:32 +0000

    agree force score meet 565end

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
