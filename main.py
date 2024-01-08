ddb27e01bfcf94d4b79c3189161ec4d561abd744
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:00:04 +0000

    another for swing emphasis break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_permutations(n, k):
+        return math.perm(n, k)
