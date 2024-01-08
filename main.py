141a4d823f9b7b7f095c42163f0d3c0a6cffbe80
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:01:11 +0000

    spend wise himself brave pure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
