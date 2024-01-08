067c5d1e8a5025b8ec17fd06fdfd86d0c036442c
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:37:00 +0000

    scale suit joke swim smile

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
