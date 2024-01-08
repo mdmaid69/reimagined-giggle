c46cae0521fa23027025218590379338becd6c86
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:14:43 +0000

    cry possibly snow moral towards

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
