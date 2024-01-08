4a6c319bea78cf636dab0f8f81d1a3a4f1083a51
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:49:56 +0000

    own necessary worse conclusion left

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def find_unique_words(sentence):
+        return set(sentence.split())
