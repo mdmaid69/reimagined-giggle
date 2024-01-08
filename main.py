9cdd16af2c4e75a9feaad54d9ce0e78da8288fa9
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:05:15 +0000

    Zz off stay politics something

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def square_number(x):
-        return x**2
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def convert_to_binary(n):
+        return bin(n)
