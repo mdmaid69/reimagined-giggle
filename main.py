cde84094099f8b31d40c12d25d378d2bd8380bfb
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:45:24 +0000

    right shoulder sad complain turnon

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
