24e672d0e83548545a3fb932324af1c3d6f8bab0
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:22:48 +0000

    takeaway refuse run keepup immediately

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_factorial(n):
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  def calculate_factorial(n):
         return 1 if n == 0 else n * calculate_factorial(n-1)
