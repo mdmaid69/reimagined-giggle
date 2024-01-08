7131aaafa9cda79c7f1cbddd68abebf124693c34
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:19:44 +0000

    unusual acceptable advice factory tense

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def add_numbers(x, y):
+        return x + y
