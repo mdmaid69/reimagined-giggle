09120f283c6f081d9dd7a0e5658596da6a28b898
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:41:57 +0000

    possibility active plate steer justice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_perpetuity(payment, rate):
+        return payment / rate
