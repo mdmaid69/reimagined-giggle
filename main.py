eefc924e2df9f7d32309d34913683d8535364aea
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:41:42 +0000

    shame able generous ashamed risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import collections
+def create_user_list():
+        return collections.UserList()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
