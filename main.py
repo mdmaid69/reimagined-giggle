920630b4cdae1106f4363ac65e1cabbb2d39e070
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:43:21 +0000

    thirsty bend professional stare needle

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  def is_even(n):
+        return n % 2 == 0
