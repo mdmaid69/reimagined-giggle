11a46907c44f179600077db4815098b96bedc1fb
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:00:19 +0000

    nigga lunch heart turnback tool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
