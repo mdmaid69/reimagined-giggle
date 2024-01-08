6b429199c7549b2f0d0e0b9a4797784f222dbd27
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:34:15 +0000

    concentrate hang air squash really

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
