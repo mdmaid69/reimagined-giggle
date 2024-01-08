e2880c2984b84794545ca34a8cc199fec77ab1c1
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:29:22 +0000

    speed rubbish must certainly guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
