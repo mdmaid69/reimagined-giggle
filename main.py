f2311bf1dcdafb90ec8b6c543983c32cde257afc
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:23:26 +0000

    party too usually declaration partner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import array
+def get_array_item(array, i):
+        return array[i]
+name = "Python"
+print("Hello,", name)
