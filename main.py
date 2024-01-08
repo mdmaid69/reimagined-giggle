b2a718f0aa2e8d57c2758a8cb6a0e3cb1d7374de
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:00:29 +0000

    bread care ago careful ago

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_acceleration(speed, time):
+        return speed / time
