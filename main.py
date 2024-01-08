6b384f99f54f4cbeb9ed29c6bf479f8e0d3ab6b6
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:33:43 +0000

    talkback pressure depend sick after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
