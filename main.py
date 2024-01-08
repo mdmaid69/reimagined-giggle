2585291646e5a14a5cd1cb3067a09bef7627a1f9
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:17:45 +0000

    exactly bite detail crowded new

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-for i in range(10): print(i)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
