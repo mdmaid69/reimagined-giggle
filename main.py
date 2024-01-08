c9ce1b42e7cb35317adaa3ab9f2c4423ee665443
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:00:05 +0000

    type keepoff spin goup heat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
