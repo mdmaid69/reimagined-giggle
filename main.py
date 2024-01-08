387176e39d8c624ba8b7150c842a56bf1c306851
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:05:00 +0000

    shelter mark soft ride interview

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
