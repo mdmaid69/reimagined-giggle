c7ea3f5d23834f986d6b75bc628d11682884067d
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:32:15 +0000

    shape share switch mail smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def square_number(x):
+        return x**2
