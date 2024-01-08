d3e9f527b94e8ca66886e81e3536cd2987b11da3
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:05:50 +0000

    manner delight afford lend progress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_max(lst):
+        return max(lst)
