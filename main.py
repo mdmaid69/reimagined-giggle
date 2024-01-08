a5d4e7365a37985bd013737a470541141b4b4e90
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:52:36 +0000

    cruelty fast healthy whichever cry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
