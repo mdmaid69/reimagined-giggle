c72545c124b0dab57327aab9b805da7b496256d5
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:21:28 +0000

    defeat produce breathe relief imagine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
