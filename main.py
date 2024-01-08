ef5b83daecfdb35cdcdd10b9fd70e1274cb29c6a
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:18:01 +0000

    mark change small hit criticism

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import numpy as np
+print(np.array([1, 2, 3]))
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def clear_array(array):
+        array *= 0
