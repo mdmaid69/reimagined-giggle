c167bd3f93207e382821acc30a305ae92d724ea9
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:08:38 +0000

    near run mean respect Ww

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+print([x**2 for x in range(10)])
