522fd2d36bba11ab66818cca62e9595dee85c42f
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:25:27 +0000

    pile should straight impressed difficult

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
