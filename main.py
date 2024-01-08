5c773068922ebaf9b1780e9080a7a2ad370a62d0
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:07:04 +0000

    used oil talk task with

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import random
+print(random.randint(0, 100))
+def convert_to_octal(n):
+        return oct(n)
