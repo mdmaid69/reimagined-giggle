adf174c575c9fb402a5798a9b4f7d827f73bbd3c
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:11:57 +0000

    beside away show either neither

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_min(numbers):
+        return min(numbers)
