86dadef5ea03ce10a1ebb90854a2da9447ffaa7d
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:49:19 +0000

    comeover land feeling past round

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
