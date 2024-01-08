b7ed2ce14faef8bfd85beaebe6d5d0ffe4b84dc3
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:56:24 +0000

    threaten mix best difference deserve

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+def print_python_version():
+        return sys.version
