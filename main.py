a586ff5c2797418c0954ae6ab46f1f348b589dc7
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:20:09 +0000

    visit press copy black straight

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
