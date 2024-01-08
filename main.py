0dec1b5bb029770e2a57b75a49bb30043161324b
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:55:37 +0000

    make wing underneath discover section

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
