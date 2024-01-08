ef9568be9096e47657fd6d7db7a4af2faba12522
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:04:44 +0000

    waste desperate loud smell evening

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def multiply_numbers(x, y):
+        return x * y
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
