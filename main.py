6cc77a1a1cead1b22680b6d833d6e125f88b3b0c
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:53:48 +0000

    performance share immediate condition book

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-name = "Python"
-print("Hello,", name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
