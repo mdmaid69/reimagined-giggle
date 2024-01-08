ecafb4b1aa2a41b94362388e7005e25dcb6c9c1e
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:41:19 +0000

    circumstance shut lay satisfactory quarter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
