473af35495524abbcfc3e5e0d3b7e6c0c4fd7b5d
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:30:27 +0000

    though equipment shrink read thinkover

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
