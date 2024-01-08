664e4d35b790886265700a53ca1af61ca198e7ca
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:27:51 +0000

    change distant from strike contact

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
