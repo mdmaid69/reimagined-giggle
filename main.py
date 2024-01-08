b070bf64ba7e78172610f82e4a75b2f435598128
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:46:52 +0000

    rough sport tragedy man lose

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
-def add_numbers(a, b):
-        return a + b
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
