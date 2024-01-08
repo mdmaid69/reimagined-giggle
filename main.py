2770d6d528618b1b53499e52b1511ed84b6748ad
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:01:53 +0000

    firm within straight involve extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
