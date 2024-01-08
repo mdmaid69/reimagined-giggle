68134afcb6a2cec5d8ec7624b4d53a23f49cd55b
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:17:39 +0000

    under raw 565end no goaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
