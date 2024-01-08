f98d080af8e02cbfc30b8ac93063230fa7da7d17
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:45:02 +0000

    makeup fold fast considering turn

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
