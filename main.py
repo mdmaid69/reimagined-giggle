314545d8f50dc9ed8ad66bfa1beea8c4ebe74426
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:42:26 +0000

    part deep I set year

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
 import collections
-def create_user_string():
-        return collections.UserString()
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import getpass
+def get_username():
+        return getpass.getuser()
