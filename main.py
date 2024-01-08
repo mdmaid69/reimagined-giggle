533cd0e4af2781467cfcf1bfdee79d6591a3c462
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:00:15 +0000

    modern scream Uu near increase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def append_to_array(array, item):
-        array.append(item)
+import os
+print(os.getcwd())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
