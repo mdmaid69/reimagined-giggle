2cc25c70a9e71b4d15356b8436f957f5256bff97
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:12:19 +0000

    introduction idea pronunciation only straight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def add_numbers(x, y):
-        return x + y
+def sort_list(lst):
+        return sorted(lst)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
