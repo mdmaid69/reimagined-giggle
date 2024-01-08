df56ce1c0a8eed0d02661e6aacbea60744a49e34
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:11:40 +0000

    question but happy into holdout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import collections
+def create_user_list():
+        return collections.UserList()
