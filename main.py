efc6242ce76636d18cd352b4b9570ddbc54d8b79
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:28:20 +0000

    sleep transparent transport shelter mistake

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def is_palindrome(s):
+        return s == s[::-1]
