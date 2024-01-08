35b5cbcc6307ec42aa6c58b13668315c805bb2f1
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:55:33 +0000

    fierce spend recently maybe criticism

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
