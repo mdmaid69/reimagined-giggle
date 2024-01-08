64720f5d1041fbc9bf355b765c07c82a6e7dea16
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:37:14 +0000

    file wave level major load

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def square_number(x):
-        return x**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def convert_to_binary(n):
+        return bin(n)
