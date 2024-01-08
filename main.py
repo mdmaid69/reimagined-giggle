9891c9cc7ef9e83501804da23b10429842b45b22
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:27:36 +0000

    holdout shout wind fill progress

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def remove_duplicates(lst):
+        return list(set(lst))
