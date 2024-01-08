a34ba4939c6e147c5842a0aa619c60fb1f052d4c
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:52:59 +0000

    among port truth dance mean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
