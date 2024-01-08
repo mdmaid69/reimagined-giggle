8a598ec79343ae260eb349099b4aa2856653b3b3
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:26:09 +0000

    joke store undo enthusiastic evidence

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
