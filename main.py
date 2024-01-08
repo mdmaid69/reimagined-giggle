6d416ef6aaeeb831d7ae8ffaf674243eba439ea5
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:42:13 +0000

    anxious hate Ll behave onto

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
