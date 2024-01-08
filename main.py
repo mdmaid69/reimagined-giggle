4ffb6b6e833094fee2c3da18a4160ea84947d5ef
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:33:17 +0000

    report journey ask all fold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
