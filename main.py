375c3cdd29c12fe34fc7936acbd32f20a4a251c7
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:22:33 +0000

    consider culture swimming pronounce band

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
