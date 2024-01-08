c673840bc8df1448bf83deadf7ecb2604e8519f3
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:13:09 +0000

    otherwise sister born too Vv

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  def count_elements(lst):
+        return len(lst)
