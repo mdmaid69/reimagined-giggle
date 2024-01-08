c8c756935e52cdb86e9153b3a338435b7a0b3673
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:35:22 +0000

    achieve left mine but speed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
