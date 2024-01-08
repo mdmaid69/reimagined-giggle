56b92bf3f6cf6f7b04b232118e30ed9311aff339
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:42:38 +0000

    fire exist people thumb mad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
