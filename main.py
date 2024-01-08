a54fc935a2e5c7c063b66d837fe364ce7cbad15b
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:12:21 +0000

    place chase child safety convenient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def split_path(path):
+        return os.path.split(path)
