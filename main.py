7cec2cb35323553346754aa340fb39b9955e0fd5
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:33:05 +0000

    decision stone into guide gentle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+for i in range(5):
+        print(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
