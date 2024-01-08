03a80d0c4867e9d32c527a3a0238fa223b5d07f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:35:30 +0000

    finish study spin damage war

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
