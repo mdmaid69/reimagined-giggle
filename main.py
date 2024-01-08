2bc76eabd191ffe79048aa6c5768c0afe9dea8d2
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:21:11 +0000

    better couple artificial transparent copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
