4cecbb768d229839aeaeb8ac47e9925c09393883
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:55:15 +0000

    ask free secret let comefrom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
