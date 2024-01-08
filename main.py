e52c983f4691d13093112cf806b86e9d29ef320d
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:47:31 +0000

    opportunity better breakinto trouble later

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
