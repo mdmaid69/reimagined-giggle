86caae0fe3b6759b80e4b963aff144975817a5b6
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:10:18 +0000

    cross argument flight exchange uncertain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import array
+def reverse_array(array):
+        array.reverse()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
