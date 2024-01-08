503fc93d21608a0bdfcc33b673c706915ce379a0
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:51:24 +0000

    pressure further behave floor sticky

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
