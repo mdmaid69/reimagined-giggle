eb278f8dc5f90afe14652a7aa9c671714dedfa60
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:22:19 +0000

    shock talkinto holdup Bb lead

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
