823f7964e99154f16aac805ce915d8fe89f9872b
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:08:14 +0000

    deliberately combine me recommend sensitive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
