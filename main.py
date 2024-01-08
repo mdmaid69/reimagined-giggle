eb74b5b0208a50faeeedda60448b989c1149210c
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:13:33 +0000

    convinced blue imagination price touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import time
-def get_current_time():
-        return time.time()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
