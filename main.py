53f9d85c8b8a0d04f1d9e6d982b723d0d0d90393
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:47:14 +0000

    switch cost born cheque slightly

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
