f0bea8e5ec6b9a72bf3f2b8e6475e58c80cad2ed
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:25:24 +0000

    nice limit pot switch cheek

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
