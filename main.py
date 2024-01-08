1404792c6977917be8204c45d44547ebaedbf38a
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:53:15 +0000

    ocean some powerful whom probably

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-print([x**2 for x in range(10)])
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
