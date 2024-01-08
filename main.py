9eb83f69c8650b85ae85547ce11e79e43cec1e36
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:03:58 +0000

    fold whisper love but account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
