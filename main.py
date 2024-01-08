2806f72f0cc307fad7ff35f8555a7aa6eecdae6d
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:41:46 +0000

    appear question workout century attach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
