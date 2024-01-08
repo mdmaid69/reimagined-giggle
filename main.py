f00ece49c7da013a8ee93c53bda4fca52bd631ea
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:33:50 +0000

    conduct hole die love compare

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  def cube_number(x):
-        return x**3
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def reverse_string(s):
+        return s[::-1]
