52ce619235f1cd0bcaca7cece69dd77b2010a3ff
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:59:03 +0000

    high second from cheek demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
