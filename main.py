dae7b196795e83bb995605f278e0dde6a83a1cbf
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:02:52 +0000

    out rain rise strike rush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def reverse_string(s):
-        return s[::-1]
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
