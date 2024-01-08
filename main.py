d2d86f4ab37199c3614c14a70cc42826125b4c97
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:45:11 +0000

    inside means cause accidental pollution

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def cube_number(x):
-        return x**3
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
