ce4886ab1ab6fb2401b4e86a427316a1dda9333b
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:28:12 +0000

    thirsty sell party otherwise dig

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import platform
+def get_python_version():
+        return platform.python_version()
