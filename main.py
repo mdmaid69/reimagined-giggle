af5fec96bd9b892557290b171e57b28b18f0b48d
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:18:50 +0000

    form less smooth believe base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
