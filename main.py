8f5af10c412941618cafb9d5dfaf59a48963f955
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:33:43 +0000

    uponon interrupt scale window influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def get_array_itemsize(array):
+        return array.itemsize
