b2ba37f693e521395f6ba4b1ea9615872b78ebb7
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:36:32 +0000

    bored within various under cruel

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  def count_elements(lst):
-        return len(lst)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
