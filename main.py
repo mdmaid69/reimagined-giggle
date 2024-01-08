3e1bc9ee4a54e3fbbf01bf921c138cd3c8c05fa2
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:22:57 +0000

    to anybody instruction convince first

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import sys
-print(sys.version)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
