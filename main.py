5ffdf374753d46b2d68ce15a700394feb7d3658d
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:16:29 +0000

    scheme evidence smile 566end finish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
