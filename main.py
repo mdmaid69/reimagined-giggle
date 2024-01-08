63114e0b32d27348f777d1ca6655281258e93530
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:16:45 +0000

    just ready character suggestion dinner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_ceiling(x):
+        return math.ceil(x)
