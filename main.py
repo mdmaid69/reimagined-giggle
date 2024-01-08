50a0a965191cf87577345fd4b128d2526ac6c625
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:59:01 +0000

    drink empty rush report people

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
