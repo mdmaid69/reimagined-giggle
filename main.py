bd5bb5d33ecc5dbdab53912e94360542aca0dfaf
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:05:00 +0000

    inform interruption meal storm meal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
