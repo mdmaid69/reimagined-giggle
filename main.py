da5a70f33dbf4599384f06ec68a24eabbb466fc2
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:35:14 +0000

    callout pipe walk touch scene

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_array_to_list(array):
+        return array.tolist()
