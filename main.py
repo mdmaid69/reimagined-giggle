a553220839bf0db40cef9ee43a4e06a7246a79fb
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:05:50 +0000

    impossible one downstairs dry pressure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
