82280c64107a3fc3e27457203a1bc640c73b51dd
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:15:40 +0000

    average little choice favourite loss

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
