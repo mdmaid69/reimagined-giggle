2f5b819c288b0d68b2d29275f4bc750e71955b3b
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:35:51 +0000

    afford tongue interruption top wet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
