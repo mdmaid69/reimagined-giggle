efb35cec31351da6e69ac3a450c7e2e09b723e62
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:18:09 +0000

    label solve attract surface information

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_power(work, time):
+        return work / time
