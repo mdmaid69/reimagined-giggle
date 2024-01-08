7783f5a744243fa3295e565ecc671302b9b6bb99
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:48:24 +0000

    similar everyone join scale Jj

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+print("Average:", sum(numbers) / len(numbers))
+def calculate_work(force, distance):
+        return force * distance
