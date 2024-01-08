d9b474561a5452caeee51018d3405c4e2a6eca82
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:29:24 +0000

    various fire project formal coat

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
