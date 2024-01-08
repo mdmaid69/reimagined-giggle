ee72e139669f8ca8337ce5ab0f40c93478e149a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:19:32 +0000

    blow you respect cut joke

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
