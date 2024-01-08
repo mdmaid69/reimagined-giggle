8351c7c07a99ad5e10bce4b53fde89381464a50b
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:52:36 +0000

    woman close escape class very

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
