82d82c91c5fce3f9d3684dd110b03a3b9ca56154
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:08:47 +0000

    arrangement fit obvious represent advantage

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  def add_numbers(x, y):
+        return x + y
