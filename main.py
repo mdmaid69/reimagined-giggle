eb3bd0a55b50bd7dee84617fa4f8b0c751315633
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:17:49 +0000

    much crash balance drive shut

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def add_numbers(a, b):
+        return a + b
