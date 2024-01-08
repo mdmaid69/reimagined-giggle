0e4a12c9956d17364147003239b700848c4f5876
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:06:35 +0000

    workupto cinema yet available ring

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def clear_array(array):
+        array *= 0
