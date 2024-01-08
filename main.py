05b07b63a9a5945bf5928ce6c7e566da8124a0fb
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:45:57 +0000

    form begin moment fasten easy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
