572e330b384dbd235ad3459183af7276ce92e062
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:45:56 +0000

    niggaacademy mark theirs port same

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import os
+print(os.getcwd())
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
