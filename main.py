7642f57a84774626d2949a1d7cd9a27935a6f1f2
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:52:38 +0000

    someone last complaint Vv keen

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-print("Hello, world!")
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
