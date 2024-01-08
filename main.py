413577890847900a9295c7855576bfaa7b49d2dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:03:54 +0000

    violent sink great invite last

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def sort_list(lst):
-        return sorted(lst)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
