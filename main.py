15b0a3af23815dfcc8aca09a40ea3a7dc9c43995
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:18:46 +0000

    move drop march hardly matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
