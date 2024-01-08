b94b61c0d7413d91c2267cd0db88b6ae49ba0b7c
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:10:44 +0000

    constantly breathe pressure shock contribute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def square_number(x):
-        return x**2
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
