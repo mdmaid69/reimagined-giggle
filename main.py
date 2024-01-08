90f0beab7c0fe488429709fc553e14eb6653343e
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:25:31 +0000

    maybe bye goback(to) pattern especially

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
