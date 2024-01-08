20cfba68c5c03704b5e1aad691986d27f329b534
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:15:47 +0000

    chance bread store crowded flow

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
