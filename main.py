ad37850200d2879b8e4ccd1ca8f30f7c0dcedf61
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:02:45 +0000

    school straight key acceptable otherwise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
