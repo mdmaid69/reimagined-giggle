d6596f9321681cb99a2c474b0cddd72caa362d89
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:17:17 +0000

    geton bridge any block hear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
+  def get_file_group(file_name):
         return os.stat(file_name).st_gid
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
