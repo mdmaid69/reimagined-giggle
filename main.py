912dbf8fcc10d52b654105ded593b7732222b37f
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:54:50 +0000

    doctor police somebody size favour

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def square_number(x):
+        return x**2
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
