f087281e85b9baa3df9f8b24ef0266e403dc5382
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:52:47 +0000

    impression entry fierce account runaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_sign(x):
+        return math.copysign(1, x)
