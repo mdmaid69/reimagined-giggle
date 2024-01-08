5f6968c775dd4cc9523729ac300def9d453d0c95
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:15:59 +0000

    now flavour cinema hold disappointed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
