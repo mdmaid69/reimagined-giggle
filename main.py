15fd31aa37acc3905eda8d14115cd931256c2469
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:59:05 +0000

    order excited fence worst walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
