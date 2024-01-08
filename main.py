165f54078a971b901facabc76ddf84ac31655fb5
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:28:20 +0000

    zero wind depend Ll majority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  def is_odd(n):
-        return n % 2 != 0
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def reverse_string(s):
+        return s[::-1]
