705790da116c1ef280e5d9a6cf6273829e2352be
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:10:11 +0000

    man welcome search complete hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def split_path(path):
+        return os.path.split(path)
