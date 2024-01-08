fbf755630cb4e3e4375fc0b871369c004b4e0f75
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:54:39 +0000

    accident rather polite learn scale

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
