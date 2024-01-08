3f0cceb8a206a3fce296ea840a5ed3bc9de7327f
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:41:42 +0000

    fork attention bored herself call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
