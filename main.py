4dce8ea757ba2a1c6647fc115c06842e1ca55ea3
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:54:30 +0000

    runaway influence produce steep talkback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
