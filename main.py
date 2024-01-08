2388bd127905d649297b30c41b609b045ce27cbe
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:06:25 +0000

    evening morning necessary long word

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def get_array_typecode(array):
+        return array.typecode
