35871d06fcf4562e438820207ac7792c47baac4c
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:15:22 +0000

    war seriously quick idea judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def convert_to_hex(n):
+        return hex(n)
