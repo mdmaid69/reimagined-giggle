b0633bbb30c92bf88cee206e0376d5c6fce2e047
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:09:32 +0000

    sink hardly conduct term lookafter

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def add_numbers(x, y):
-        return x + y
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
