2ff0980f9cac1ad0956616c2e242f957548af278
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:31:57 +0000

    bit somebody replace language lonely

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
