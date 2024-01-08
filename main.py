7deb84cea50330a9adeed409533d6d7920c72908
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:12:51 +0000

    beat record patience rush result

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_current_working_directory():
+        return os.getcwd()
+  def subtract_numbers(x, y):
+        return x - y
