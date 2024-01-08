669d15a39619171767b9eddcc3203b25ab7d8396
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:10:41 +0000

    custom famous shape gift ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
