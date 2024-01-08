b47660f41c6c849af37fcf76c217ae0c262179ba
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:38:40 +0000

    relax pollution dangerous trouble replace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
