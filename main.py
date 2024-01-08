488172d9262500da6b25ec178de7916033aeceec
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:57:44 +0000

    task busy though communicate convenient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import sys
+def print_python_version():
+        print(sys.version)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
