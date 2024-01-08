ad04d617baaf160705220b9077405ab36c063e85
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:49:54 +0000

    centre agree bye medium world

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
 import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_array_length(array):
-        return len(array)
+def get_array_as_bytes(array):
+        return bytes(array)
