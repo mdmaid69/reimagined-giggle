515eb6b96581d69a7be18ebcc6d23cac9da28fb4
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:28:09 +0000

    wherever drink fill whistle company

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
