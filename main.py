d56f2c32ab3e73a832974726bff7e2896f18aad5
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:30:19 +0000

    your giveback fire lucky conduct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
