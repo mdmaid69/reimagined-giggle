af32892c762fcef87756703a690b8625d33c7fb1
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:52:23 +0000

    tidy fear wind bye identify

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def is_odd(n):
+        return n % 2 != 0
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
