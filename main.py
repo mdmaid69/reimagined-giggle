e36fa9f65d5cd7c342abf620362176975bccda4d
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:53:50 +0000

    mail rescue total holiday save

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_list_from_array(array):
+        return array.tolist()
