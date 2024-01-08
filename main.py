2baa5d9d4e6ef320180d2a2621da2df79f4303eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:55:25 +0000

    hurry part square us dress

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import re
+print(re.match("h.*o", "hello world"))
 def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
