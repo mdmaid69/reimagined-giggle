e300253493596b27df9422a90ee93f2208f5b2bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:30:18 +0000

    leg official picture return hungry

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
