6f14bdbd39f15bef43ff5a24e7d67e4ea7fa3398
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:08:00 +0000

    similar reply sleep present account

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
