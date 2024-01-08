94841346a3c3d5837aec73aefa089f3cd6f8985e
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:25:20 +0000

    exhibition yet comeup loan satisfied

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
