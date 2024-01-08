5e47d7ef41b40ba54aada6ede765306950f23975
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:06:26 +0000

    minimum tape rough pick rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
