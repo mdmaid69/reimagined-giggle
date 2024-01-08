a8010baf521f38ce92a84c3d7fbb108952deb06a
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:44:33 +0000

    correct alive polite common unfortunate

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
