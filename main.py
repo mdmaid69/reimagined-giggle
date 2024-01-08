7e08133fd93daacd74aae067eb423b30150f68d8
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:09:57 +0000

    left character voice spin trial

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import array
+def append_to_array(array, item):
+        array.append(item)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
