1d54cb88e974231a07e7044bd5ab8ed1abbb34f8
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:57:51 +0000

    full concern side evidence trust

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
