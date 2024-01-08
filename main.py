51c6b4d321fbd3bf6058c5f4a8bdbeef2267d5f5
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:22:09 +0000

    space lead cruelty lead related

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_current_directory():
+        return os.getcwd()
