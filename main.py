0b6f7d6c97ba9e949e8b65769a83d38c6d67e51a
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:33:29 +0000

    above it compare description side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
