0a725c4f2a1d2c0c0025d9ba55c154ab17834ad0
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:29:04 +0000

    Dd centimetre future write idea

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_speed(distance, time):
+        return distance / time
