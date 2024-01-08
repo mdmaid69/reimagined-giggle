d6b99100b6514b6652ac6b97e117aa367f7a747e
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:46:06 +0000

    opportunity so hurt edge hate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def multiply_numbers(x, y):
-        return x * y
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
