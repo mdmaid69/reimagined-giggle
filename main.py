6556bb2661bd8acc66d9b4a032ef89ff449a1a60
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:40:51 +0000

    before influence odd point crowd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def multiply_numbers(x, y):
-        return x * y
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import sys
+def exit_program():
+        sys.exit()
