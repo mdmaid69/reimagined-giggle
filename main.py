3a1a38c8cb692cb4a913d8ba88e3af95407b9806
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:46:08 +0000

    gap spread powerful deliberately sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import math
+print(math.pi)
