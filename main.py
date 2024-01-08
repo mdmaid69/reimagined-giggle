daef47ffd829a973321a3eeacfcab39595793a70
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:25:23 +0000

    event direction roof popular order

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def count_elements(lst):
-        return len(lst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
