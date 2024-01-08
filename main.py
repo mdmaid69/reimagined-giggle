054990c97907e52bffb8244233f11c595ca596d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:37:37 +0000

    either protect pool beyond arm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import os
+print(os.getcwd())
