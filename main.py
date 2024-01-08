cd7f1be60b019b5aacd3ee8418bc6a418c583999
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:25:28 +0000

    say matter shock skirt top

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
