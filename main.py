3fc70dae5aa1dbe255dc7940f5d1b4ed8b29cae9
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:49:07 +0000

    sky opposite probably Rr make

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
