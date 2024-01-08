a3bde81bc1433067fdeb3ac09efb7bd37f5deb3a
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:15:30 +0000

    platform school injury ilovedicks forever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_odd(n):
+        return n % 2 != 0
