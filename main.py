045ebf75a5884f314ee0dbb4d865c921398b16e7
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:47:16 +0000

    embarrassing passage new sort doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def is_even(n):
-        return n % 2 == 0
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
