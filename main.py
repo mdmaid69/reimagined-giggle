25e4ad988fc604663115841903176b9c7a984ec1
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:23:01 +0000

    effort chase make represent smile

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_power(work, time):
-        return work / time
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
