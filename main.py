fdabc754cff830a2e200d1659db19c361742d83c
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:22:30 +0000

    571enough getback suffer police tour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
