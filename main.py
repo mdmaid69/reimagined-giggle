6464a51790c679392de495b4aece79a95a607787
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:51:39 +0000

    fetch yours game face evening

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import sys
+def print_python_version():
+        return sys.version
+import math
+print(math.pi)
