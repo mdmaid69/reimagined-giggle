ed2a5ef4b377e3c56a1c6f55029d929cb113d993
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:05:02 +0000

    woman typical unit way interesting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
