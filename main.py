4e50ae1ae85f0ca49d54a64d3c2994b41e7a988d
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:34:16 +0000

    repair remember bet goaway bother

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
