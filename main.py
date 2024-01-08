be5a3a0da25d28aedd501fae3fcd7f9fe4325b87
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:03:55 +0000

    exercise present early serious light

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
