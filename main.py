b903a60abbc97e3481fb2f13747dedb313856057
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:27:36 +0000

    smile invite glad material help

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+for i in range(10): print(i)
