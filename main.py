6c0ee5f5f3b40066e01676c3f16eae49a2aac7d4
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:36:25 +0000

    rather lookup opposite work through

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
