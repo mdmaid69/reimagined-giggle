b917022bd2d4e5f5ccae7ec6997c7d26b47e3412
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:54:12 +0000

    point stop breakup 567enemy priority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-print(os.getcwd())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
