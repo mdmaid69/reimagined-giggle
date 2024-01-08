e1e4c09d011baf5f645bdc9b782031d37bb8dbbc
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:23:51 +0000

    forget interview escape form involve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def divide_numbers(x, y):
+        return x / y
+def divide_numbers(x, y):
+        return x / y
