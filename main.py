b4ce185f2933ab7198fa22770d681dc1641fd4f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:44:58 +0000

    favourite variety organize call better

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
