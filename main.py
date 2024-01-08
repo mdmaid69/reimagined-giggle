9efa172e1a3466428a66b1c4ada0a8f02a357429
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:06:07 +0000

    calm makeup cool model emphasis

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
