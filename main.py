e13ef069b803f6d35e06a3ceb85277315b2c896b
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:57:57 +0000

    bottom start rubbish fire then

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def sort_numbers(numbers):
+        return sorted(numbers)
