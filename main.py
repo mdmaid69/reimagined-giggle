76099b4057295b86392024c218963a7d5299873f
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:46:36 +0000

    rid back spend seat sail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
