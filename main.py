4679d2e78f69e832f82014f2b0fcf9cbb3b2e1d8
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:41:41 +0000

    sky foreign poison grow maintain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
