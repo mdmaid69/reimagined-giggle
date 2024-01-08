4830263119b4575f36c1e0baa59157b289320267
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:41:13 +0000

    concerned tough obvious comeout complaint

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  def cube_number(x):
+        return x**3
