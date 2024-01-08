7a32041a10659cf62694efce70d89bf6bfd61de5
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:56:34 +0000

    deliberately thin happen plenty century

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+for i in range(10): print(i)
