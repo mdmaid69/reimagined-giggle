359889922106249843078b180b7d1cea6e3fd18c
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:12:26 +0000

    tap build excuse lean far

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def sort_list(lst):
+        return sorted(lst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
