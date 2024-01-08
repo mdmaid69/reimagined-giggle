1a25b6c7059fedf2a0531dd0eb79d6337053799f
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:40:29 +0000

    store apply know check nowhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_as_memoryview(array):
+        return memoryview(array)
