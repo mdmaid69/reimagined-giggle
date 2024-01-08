5ff876428487960999f9dd019353457faffe6cf2
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:41:00 +0000

    judge send sleep strong grand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
