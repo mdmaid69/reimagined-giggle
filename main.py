e15cd107c9416ae45bb4a91c14f59af2dfa2d700
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:32:28 +0000

    design so to exaggerate need

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
