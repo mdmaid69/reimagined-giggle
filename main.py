dda4275b97baa3aef4d42549877398375e132204
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:17:36 +0000

    present plain steal face afternoon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def find_max(lst):
+        return max(lst)
