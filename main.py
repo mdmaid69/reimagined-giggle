a78b0071e614b10e9397f99a9fa05629249474b1
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:28:25 +0000

    shock slowly question cap work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
