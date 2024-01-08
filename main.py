15868b2248df28afdd0e7c22ae02745fc81153cc
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:39:11 +0000

    geton course half improve it

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
