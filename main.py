d9207f81afad47f4717dbc7ba54d7d9489c956ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:10:41 +0000

    praise discovery discuss pick Ee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+name = "Python"
+print("Hello,", name)
