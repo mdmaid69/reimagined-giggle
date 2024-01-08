8f0df139ab15415e4dc79d8a500ff8677cd9dd7c
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:57:07 +0000

    rare week risk thick compare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
