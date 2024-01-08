e25acd765909da5d0fb5e09becca87a78fc83030
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:46:44 +0000

    Rr ever offer anything strength

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
