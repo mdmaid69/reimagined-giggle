581073910a43bfe5561046b8e3fe061674a640e6
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:32:27 +0000

    young lookaround clever computer mail

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def is_odd(n):
-        return n % 2 != 0
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
