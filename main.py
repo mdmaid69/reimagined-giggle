2ba0b6264bab53be6ed395471cf91e5bdd33cce7
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:18:04 +0000

    left callout use surprising reduction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def append_to_array(array, item):
-        array.append(item)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import os
+print(os.getcwd())
