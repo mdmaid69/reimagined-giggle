7752bbcb479847d8531f10a719d26d2740e11259
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:41:05 +0000

    exchange affect sink bear life

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
