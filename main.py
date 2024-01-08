45e46a0773b7ec442467d2750c1f88876d3b6284
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:30:36 +0000

    already 566end post male absolutely

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def find_min(lst):
+        return min(lst)
+def calculate_density(mass, volume):
+        return mass / volume
