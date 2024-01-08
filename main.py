4c6ade07aecc3203c6afc43e208836f361b4609c
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:40:25 +0000

    bath hurt previous on gather

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
