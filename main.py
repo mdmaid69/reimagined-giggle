f678b6002168c65570216b31e7a8d303ef9fd768
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:00:57 +0000

    politics pronunciation 566end only sport

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
