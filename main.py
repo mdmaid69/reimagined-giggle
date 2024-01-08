65e1e654cfd711bfcda67952b3681953e5fd0599
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:48:40 +0000

    scared neither Jj tread gap

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
