a3e60670c25a0d9c2fc09eebc56cf52673b6e443
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:57:55 +0000

    flood heart appearance that where

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
