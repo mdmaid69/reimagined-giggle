f3443536dbd92b38cac96b4417c93569380e6305
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:33:01 +0000

    improvement blue basically bin message

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
