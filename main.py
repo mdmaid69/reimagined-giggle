478277f853fbe9bb4c76cd2d511f17dc33c050a3
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:33:29 +0000

    please strange thick confuse matter

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import collections
+def create_user_list():
+        return collections.UserList()
