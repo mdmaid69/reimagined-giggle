0d87ef92652ec5f49c9d3ef4e42f041ddfb70adc
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:11:52 +0000

    tell anything takeaway life scrape

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
