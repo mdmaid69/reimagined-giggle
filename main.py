b3548f48d039d1eac59482178d55e759c6a11000
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:02:53 +0000

    close bet inform sign doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
