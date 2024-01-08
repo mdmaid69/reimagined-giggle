1d9d4533d310b30b0295161eaea6fb704b2e7ede
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:21:26 +0000

    fact entrance home in harm

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
