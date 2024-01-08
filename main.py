6afc518d3ca70fae9ea909d0e7643dd6b39ed476
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:56:33 +0000

    ride patient permission steer through

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  def cube_number(x):
+        return x**3
