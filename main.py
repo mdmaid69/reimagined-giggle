13f6355858d265cefc76f8e6054ac30d203bf9bb
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:58:33 +0000

    occasional forever pencil sting loss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
