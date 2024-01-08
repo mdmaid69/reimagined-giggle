d842dc161842af3d923bbf7ef20389fc93175652
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:38:07 +0000

    quit connected sorry breakthrough holdup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def sort_list(lst):
-        return sorted(lst)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
