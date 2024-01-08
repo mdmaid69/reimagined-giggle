92ecdbf339e7b0e893ea6856d6d61cc259977f9c
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:44:43 +0000

    basic accept future mean loss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
