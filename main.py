b23eb782b85af5494ed15ff92f05b36df05e23ac
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:50:26 +0000

    sign phone 573entertainment proper short

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import getpass
-def get_username():
-        return getpass.getuser()
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
