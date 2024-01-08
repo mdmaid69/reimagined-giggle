8c51d09612c37dd0b84c07e75a70ffa005028379
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:32:31 +0000

    develop sure programme store break

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-  def is_odd(n):
-        return n % 2 != 0
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
