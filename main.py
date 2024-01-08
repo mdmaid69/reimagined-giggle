7a3029531cce1b577edd59eb6831c01f8053aa53
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:59:16 +0000

    giveback buy seriously rate search

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
