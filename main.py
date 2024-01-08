cbd115525933fd834b7e43b2c23fef0168d09102
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:34:29 +0000

    fine trousers support none which

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_array_to_string(array):
+        return array.tostring()
