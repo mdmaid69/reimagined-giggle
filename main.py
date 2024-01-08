1dadee91808cd7c6351c997e3a71fa194f10d4dd
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:42:44 +0000

    gather begin chase instead traffic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
