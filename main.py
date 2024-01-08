90a6f76a7bfe76995dea943969082492a157aa3b
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:41:41 +0000

    birth well past factory couple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
