5d79e8086c9344be782de05cc0a06c95b0668cdc
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:26:57 +0000

    top crowd measure occasion 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
