c45cc3c522d18548993dbc450b7c431bc695656b
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:05:20 +0000

    brain closed lift typical 567enemy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
