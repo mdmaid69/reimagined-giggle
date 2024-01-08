7fddd4265ebb4e4a247c0dcd7ab6017b9b179e5e
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:50:31 +0000

    destroy lock compete guide know

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def find_min(lst):
+        return min(lst)
+import array
+def append_to_array(array, item):
+        array.append(item)
