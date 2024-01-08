5cf5f5cf1cac40d30138d46fa70e26a9611d68e8
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:57:50 +0000

    smile brother clever fall successful

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
