7fe8b3ca152ad5d85af3ca5ce3b073190601e9b9
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:20:49 +0000

    increase plenty mix crisis breath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
