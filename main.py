85a7bdd8052a3a39b7eaf0221e810ea09d600c1d
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:54:48 +0000

    personal impatient public stretch spot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_even(n):
+        return n % 2 == 0
