8653761de532d3a076ea56e03e5ab5880d833a31
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:10:37 +0000

    punish interested flow swim wet

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
