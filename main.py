92d218d1311eb82fa7c91e6abb48bcb7522ab050
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:45:27 +0000

    sign brush pool away Nn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
