afab8196776983a76b6ea5873f7678f69cd64c8f
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:48:07 +0000

    previous throughout key fly bin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-import time
-def get_current_time():
-        return time.ctime()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
