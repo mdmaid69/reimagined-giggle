42d584d88401fafff10b0c14a8bb7702654f75ab
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:50:28 +0000

    pretty payment bring evening switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
