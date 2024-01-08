910d12870647cc93d12cbde2fe6212a284cb8479
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:30:56 +0000

    ilovedicks apply sound sound smoke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
+  def square_number(x):
+        return x**2
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
