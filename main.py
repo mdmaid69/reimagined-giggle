7baf86b0fb2c24e09e592160dd972cf98028466a
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:11:37 +0000

    birthday protect original punish alone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
