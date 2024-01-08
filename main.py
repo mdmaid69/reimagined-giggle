897799cf3e59b50e4de12dd436d419f7b8cb8550
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:11:40 +0000

    worse recently depend steep description

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
