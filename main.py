246a0718e6d0e88c3ce89d1209ea1dba0daadb66
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:22:14 +0000

    usual often mistake ours could

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def count_elements(lst):
-        return len(lst)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+name = "Python"
+print("Hello,", name)
