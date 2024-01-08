8247e7f37ec145797a4551348d6fcfa8de3b4e8e
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:36:58 +0000

    class than draw part yours

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
