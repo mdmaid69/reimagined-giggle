9ab8bca92108459dd70bad7c9628c61edd701dc3
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:48:57 +0000

    oil even command affect task

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
