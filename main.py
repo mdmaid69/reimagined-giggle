031c9165fc04d092e6be06bf82e0cc7fb400cb80
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:32:37 +0000

    basic wrap attract importance fortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import os
+def change_working_directory(path):
+        os.chdir(path)
