8ee464b190c8cf15d12600842a1648c7d55afd1c
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:11:03 +0000

    soft she die direct Ff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def find_min(numbers):
-        return min(numbers)
+def calculate_ceiling(x):
+        return math.ceil(x)
