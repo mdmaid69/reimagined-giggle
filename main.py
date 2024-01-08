c5ddcbb559f485a7464b3114823995dcb358bdf5
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:12:45 +0000

    stone makeup stir would waste

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-print(sum(range(10)))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
