884133685ecc8e491eaa38a07027cd37929b76f2
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:01:10 +0000

    gradually near light I raise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
