5671404d79112974764651b435176ba065777a0c
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:05:13 +0000

    discussion claim sink bury pencil

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_speed(distance, time):
+        return distance / time
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
