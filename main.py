64e7eda4e8322bb362f55b24bef611339849e3b7
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:49:52 +0000

    tear wet service situation night

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
