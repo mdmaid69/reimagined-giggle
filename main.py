0e087840e06706ef79fdcb255aa56ca479bee9e4
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:34:27 +0000

    her contribute possible bridge information

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
