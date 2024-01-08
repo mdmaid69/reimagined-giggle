c066df586597d18ece5b9e4afa10276cedd5eab1
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:49:21 +0000

    degree several ill flavour takeout

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
