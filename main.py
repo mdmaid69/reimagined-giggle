f2e35699fb6531b0c0fd17460bc108097ceac902
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:44:25 +0000

    quantity believe emergency meal Tt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
