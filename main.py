93130d97254544b26fe1a63131117ca7de6bd906
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:01:50 +0000

    man anywhere public interruption breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def is_odd(n):
+        return n % 2 != 0
