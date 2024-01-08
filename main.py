979adeed5019e9f994e52aa35c7722a49cd5de6a
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:33:06 +0000

    price top generally contact stupid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import os
+def get_current_working_directory():
+        return os.getcwd()
