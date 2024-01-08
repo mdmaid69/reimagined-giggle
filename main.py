87127a52a25005622a84b07bfe918e51f6e6ec8d
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:40:06 +0000

    scrap smile outside cut provide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
