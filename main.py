e5551844d87b4553182469a24126cd634a87f1f4
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:35:31 +0000

    manner variety clear mouth street

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
