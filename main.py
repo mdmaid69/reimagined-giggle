a4259ba169cf812f9fcfd74ef42c9543a014511d
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:38:19 +0000

    whether cap wait pull here

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
