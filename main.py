dc44cb8dc0cd4364bf317973abec405313cf7220
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:13:16 +0000

    consider sad name travel greeting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
