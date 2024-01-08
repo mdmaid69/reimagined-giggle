a6e84123af384547bc3bbeafa237f1cf6087cfd6
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:37:29 +0000

    force stranger fall apart wind

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import math
-print(math.pi)
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
