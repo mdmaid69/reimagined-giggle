3b965f559cc75e7889cde4b56bdcf168df00de38
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:32:08 +0000

    woman teach suit could copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  def add_numbers(x, y):
+        return x + y
