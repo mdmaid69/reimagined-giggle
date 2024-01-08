d27480caa4564295f29ad6b8f8c1d4b9743402ec
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:56:12 +0000

    open start offend at probably

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_index(array, item):
+        return array.index(item)
