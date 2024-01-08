148beb2a12c8c8ae8573986a1528fd781a288a95
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:20:29 +0000

    pile pretty against uponon anywhere

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def get_array_as_int(array):
+        return int(array[0])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
