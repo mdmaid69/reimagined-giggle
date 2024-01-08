990f179656fd395529ffd5da644915f33a1bc3a2
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:42:34 +0000

    571enough over habit whether style

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
