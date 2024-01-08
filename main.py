d75329688bfc827d22ccc5c96d1acdb10a4fcec2
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:31:34 +0000

    far back present office stop

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_power(work, time):
+        return work / time
