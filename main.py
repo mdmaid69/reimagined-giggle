6606c3abc8406ab5d7223dcd7ebfbd5450a5dac1
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:24:47 +0000

    neither Hh week deal division

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
