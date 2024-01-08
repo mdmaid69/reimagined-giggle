1da449af30730d26f150125bd5bbf2ff4a90815a
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:03:06 +0000

    tense walk study unless payment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import collections
+def create_user_dict():
+        return collections.UserDict()
