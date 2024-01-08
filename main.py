edb6d0f561de5e9e7492e0d0dc41f47142c1b99e
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:33:47 +0000

    fat might getout neat switch

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Reversed:", text[::-1])
