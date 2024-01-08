2188e5e7a85d05d9184c9493b094c632f30a6a69
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:40:13 +0000

    demand generally identify holdout protect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
