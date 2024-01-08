098b729dc55c1978ef6d894b423faebb58e07f86
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:39:42 +0000

    clearly pronounce suck before its

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
