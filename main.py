8ed42e018ba931a09700df184059ea5871a86ba5
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:42:24 +0000

    dinner her runaway runover improvement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
