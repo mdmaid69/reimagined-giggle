f871e458c4c79d9dad31c77747cc7c433b958647
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:26:05 +0000

    fun confusing wealth Nn among

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-  def square_number(x):
-        return x**2
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
