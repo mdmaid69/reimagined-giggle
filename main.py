2ba71a46fc0fb2e50613c350ee748f51795a0ee8
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:19:48 +0000

    flight shrink pattern justice say

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
