ae0ea643cd19b818b93428047ca58394682fd045
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:39:32 +0000

    sink getup lunch argument safety

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
