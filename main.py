fd0921ddbb35af240732e005e803159adc919d77
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:38:50 +0000

    tongue whisper pot sister how

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import math
-print(math.pi)
+def calculate_cosine(x):
+        return math.cos(x)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
