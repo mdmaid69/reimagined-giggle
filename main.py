f564631539ad26aeee941065366d10fcd232c8c3
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:33:33 +0000

    only recommend noone lay average

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import time
-def get_current_time():
-        return time.time()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
