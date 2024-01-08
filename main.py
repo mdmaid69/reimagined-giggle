26079ec1a21c64d092ca06414a8526d1b1775bed
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:03:25 +0000

    now powerful hollow evidence tax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
