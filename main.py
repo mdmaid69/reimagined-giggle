6b55edb902e5fd4373f6d7354b150d007b82e6b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:25:35 +0000

    minimum creature spring appear education

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def add_numbers(x, y):
-        return x + y
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
