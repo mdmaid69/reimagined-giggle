cc28f1d8061593420a0427a9ccd4487658ea5e2e
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:18:03 +0000

    cheek drop thank afford passage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_min(numbers):
+        return min(numbers)
