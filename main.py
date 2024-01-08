03b039afec276e6038fc7b20debec7bdabdf3020
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:50:54 +0000

    punishment maintain cut target door

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
