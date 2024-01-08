e620d162c72dd2aeec9bedcbe78dd1fceb270ae1
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:55:57 +0000

    pride you gap people score

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
