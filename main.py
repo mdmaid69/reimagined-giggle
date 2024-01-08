7b926b079a24152108e7c3855e01ef4aaec99213
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:39:18 +0000

    scrape last along knowledge idea

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
