4f4fa5e4e7d0ca80d2d161c87127399c856714f2
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:48:03 +0000

    forever record towards sharp appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  def convert_to_binary(n):
+        return bin(n)
