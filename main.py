14822d52d020edc81b49cfd1d6dccbc8b8bf5567
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:24:00 +0000

    second but will wherever distance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
