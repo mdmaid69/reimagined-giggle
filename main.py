e1e48653bc4da2309af0acdffe1604c2892511c1
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:01:11 +0000

    thorough my estimate see rate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
