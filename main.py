a6469690807a16b6ccab64a49156c6c54cd0d0cb
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:57:13 +0000

    information keepup cost his keepout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
