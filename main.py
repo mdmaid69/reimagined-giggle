339390c3f79a47e9f905573214529b519c6e4c4a
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:21:19 +0000

    provide seat pipe risk boring

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
