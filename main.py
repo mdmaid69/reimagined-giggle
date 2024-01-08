935ff05035686c71f8e7f2239e429fa073ad50be
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:40:03 +0000

    present damage position approve classroom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
