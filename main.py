ddabca824f1af9ec20ee1ceb4c14306f04ecb37b
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:39:43 +0000

    getoff often mend bottom down

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
