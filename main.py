0a4d9769086dce772f7a285d7660e8fd39511612
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:49:47 +0000

    outside communicate among fresh Cc

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
