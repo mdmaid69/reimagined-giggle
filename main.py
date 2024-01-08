165e085dff0f5be4f2a7865dd02b3c5549f058d5
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:02:09 +0000

    seriously flow perfect die difficulty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
