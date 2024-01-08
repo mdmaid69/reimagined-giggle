e58c29adb21d4076fe904c96afec492aaab8f191
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:05:42 +0000

    rob period themselves pity dish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
