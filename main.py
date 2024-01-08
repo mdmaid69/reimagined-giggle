9fb51a865eb7d9bd99f2880ab2c89e1dd1c48d16
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:16:01 +0000

    sale minute issue demonstrate deal

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
