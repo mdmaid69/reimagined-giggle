e0bd50e6d58bf3b7bd7ba71bbf3a66524deb66f3
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:25:48 +0000

    meaning order hurt position go

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
