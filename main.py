1c2944923f88c11dd583438d615634dc8b0e70cc
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:21:13 +0000

    obvious opposite picture hurry clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
