bf6013797e369c9da2266a417b1c521a262b5675
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:14:04 +0000

    object walk part afternoon roof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
