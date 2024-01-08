c276c663958bfd074854b6c26d041c19f8910b71
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:10:22 +0000

    decision capital second nearly acceptable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
 import hashlib
 def hash_string(s):
         return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
