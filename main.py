f6ac639d736f608e05c8baaf9d8c221f36f4dd2c
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:20:05 +0000

    workon win height relation blame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def find_min(lst):
+        return min(lst)
