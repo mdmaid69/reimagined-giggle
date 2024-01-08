37afd8d690b3e040ec31e8bd4792a393f73d00e4
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:06:23 +0000

    realize coat much combine quiet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import collections
+def create_stack():
+        return collections.deque()
