7fcb6df4479e22683ce2e1e1395d2b694b11661b
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:29:41 +0000

    undo adjective rest equal thing

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import random
+def roll_die():
+        return random.randint(1, 6)
+import time
+print(time.time())
