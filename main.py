22d4ec0648eac50cf36e5fa6f0d288454e20a6b5
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:56:38 +0000

    scared whichever degree contrast bath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
