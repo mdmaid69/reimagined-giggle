5568c5a4460c485574bd173e660a657ba9201429
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:20:02 +0000

    book range obvious possibly follow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
