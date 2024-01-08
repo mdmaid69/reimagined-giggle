85a34d777445be60868fbabede1250744e96650d
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:54:08 +0000

    minimum wet might everywhere inquire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def count_words(sentence):
+        return len(sentence.split())
