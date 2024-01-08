1993eb4143b415a75f140335c3a0fd4d7351c14a
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:50:51 +0000

    replace angry in callup nigga

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
