cf8ac5c6e747f21ae2069ed773d3eafe25094b38
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:53:49 +0000

    head deal genuine whom murder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
