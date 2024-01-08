ad2dc69ca189081b5b978025be9c43fe5c4c499a
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:09:50 +0000

    sick interesting wealth free base

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
