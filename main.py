23e0b528e9f5dcfe9a869bcaeea8c260b43d092a
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:29:26 +0000

    statement butter react complain scream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def sort_list(lst):
+        return sorted(lst)
