8bc5c93b713f17c146172a7c1261993014d17e3a
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:25:42 +0000

    minute whichever impossible exciting something

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
