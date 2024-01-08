234e17bb6f44e71cc2a88a365a6ba3e4fccf29ad
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:35:05 +0000

    bunch distant public cry round

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
