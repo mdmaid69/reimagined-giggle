f7f5ada4bd62c7b52c28ed62ef5a2413d70a81b2
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:58:12 +0000

    yet telephone because hole handle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_area_circle(r):
+        return 3.14 * r**2
