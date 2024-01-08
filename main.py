b3ea546027871ed4ca79b46334fb3c6154f64797
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:58:37 +0000

    taste degree tune unhappy environment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def convert_to_octal(n):
+        return oct(n)
