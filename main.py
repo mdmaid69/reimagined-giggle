dc8a47e73a7c4522f0fb6cf38d2f061b36b584cc
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:22:31 +0000

    it interruption forward Dd recognize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
