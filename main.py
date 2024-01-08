50c9991b72779636a29fa313fd7ecaaf226f9a3f
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:55:38 +0000

    actually offer lend piece release

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def count_elements(lst):
-        return len(lst)
-for i in range(5):
-        print(i)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import collections
+def create_stack():
+        return collections.deque()
