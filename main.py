ebc53b00338ff6f5826c52ade48cffbc914e0f8f
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:16:14 +0000

    lookback(on) move point anywhere up

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
