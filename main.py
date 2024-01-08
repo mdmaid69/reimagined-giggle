e57627f83e199cc715346e6f1fff8020002fed87
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:26:56 +0000

    call punishment go later pleasant

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def reverse_list(lst):
+        return lst[::-1]
