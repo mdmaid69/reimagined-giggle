c4bbda34bc6e4322be4429c33ab8a8e68add6892
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:28:42 +0000

    star problem character book long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
