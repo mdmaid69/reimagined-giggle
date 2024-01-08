ff2c9460c175b19da03522cf4dd0e0bf79260433
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:04:19 +0000

    up almost and along deliver

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def convert_array_to_list(array):
+        return array.tolist()
+def count_elements(lst):
+        return len(lst)
