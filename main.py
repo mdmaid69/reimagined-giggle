36fb2c7d39001b823a53f8b13f08f24064e63eb4
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:02:25 +0000

    friend comedown likely promise giveup

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def reverse_string(s):
+        return s[::-1]
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
