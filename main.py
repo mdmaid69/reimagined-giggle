6cfa0aab5d5c7fe4e964fdf16da3925fd3679046
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:56:57 +0000

    building collect pretty trade slowly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def get_array_as_set(array):
+        return set(array)
