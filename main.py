b71e9d559972e0d1f0e8911eca6fdcecdea24974
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:59:25 +0000

    fortunately square sweep repair hello

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
