a25e05a05a06eff33812c4fcf2d0f3bf4b4c28ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:54:22 +0000

    look breakthrough force likely maybe

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+for i in range(5):
+        print(i)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
