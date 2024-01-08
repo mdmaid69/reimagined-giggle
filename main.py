2c6feda68f5806263aa6a0fdc965a8f9c651ec64
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:07:29 +0000

    sore call arrangement minimum either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
