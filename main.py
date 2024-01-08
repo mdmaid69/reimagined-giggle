3e31639cf20291198ffebdce5dfb067159d87429
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:48:38 +0000

    guess load stage invitation impression

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_speed(distance, time):
+        return distance / time
