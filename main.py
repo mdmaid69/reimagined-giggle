7c0dc7e55f4dafa3755a484e544db39af0d4a3de
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:16:15 +0000

    ordinary machine giveback Tt chin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
