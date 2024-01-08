3f301a79f23b1fd2e901c132a0ad4ec69e1216f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:26:08 +0000

    equal age pattern brief bus

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import re
+print(re.match("h.*o", "hello world"))
