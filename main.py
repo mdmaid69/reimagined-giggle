69b35f98e7b3d56068dd9c0ca257ed0299bdd672
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:38:30 +0000

    cry dry second offer travel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def remove_duplicates(lst):
+        return list(set(lst))
