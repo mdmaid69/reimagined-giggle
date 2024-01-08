19574be1bcd646cb03b0735a07ccddbbf878eac0
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:59:35 +0000

    guide risk vote run challenge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def convert_to_binary(n):
+        return bin(n)
