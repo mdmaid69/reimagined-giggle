ca7831cb38f8b97642fd56f64ffc2782e7b22dfc
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:44:21 +0000

    create ashamed whole beneath stop

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
