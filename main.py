a981207433e606e69ce4b641c1e59be28e0784f1
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:15:31 +0000

    any protect offend benefit present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
