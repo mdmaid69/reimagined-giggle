4e9d941369959b73a0bfa42f38809e91bcc0a017
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:42:27 +0000

    thick cheat comedown evening nor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_perpetuity(payment, rate):
+        return payment / rate
