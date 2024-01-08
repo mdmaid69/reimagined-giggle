fcbeef24aa1e9036dfd4371c75576f3cc40606de
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:57:59 +0000

    situation satisfactory must fairly afternoon

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def convert_to_binary(n):
+        return bin(n)
