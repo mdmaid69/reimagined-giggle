09b5990b586a6a5d6d5f0df81b7878d3d69588d1
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:29:57 +0000

    truth row lean tape way

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import collections
+def create_user_dict():
+        return collections.UserDict()
