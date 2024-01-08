1e5762fbc8c1ccccaf3c48d02b29fd89f3d5f11a
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:58:35 +0000

    road backwards popular near scrap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_acceleration(speed, time):
+        return speed / time
