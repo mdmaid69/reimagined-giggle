eac24bae9e3f295e9282924db76465fe0653d9d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:23:37 +0000

    acceptable far past foot chat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+print(sum(range(10)))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
