87bf67ea9233d63da2420f8b3adbbb8ae34bbf9c
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:13:57 +0000

    sport far talkinto meal send

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
