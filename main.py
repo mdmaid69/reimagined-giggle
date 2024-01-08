a2036ee3b59d38e8cd357a3e613f0bfb28911d45
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:46:37 +0000

    safe child leg secret post

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
