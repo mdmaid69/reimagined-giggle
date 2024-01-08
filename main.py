5b15f0af4371623f92ba7ad0cab80a5c7adab3c9
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:20:16 +0000

    cruelty rank probably worse pour

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_current_directory():
-        return os.getcwd()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
