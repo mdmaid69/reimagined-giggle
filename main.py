ab17cf0ba946eb53e8bafdf01596965cbbbccc09
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:03:10 +0000

    whenever exchange language delight breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import numpy as np
+print(np.array([1, 2, 3]))
