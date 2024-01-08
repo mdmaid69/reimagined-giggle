6454bc7e0e39fa94c7b85bb520ca26bd398444cf
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:42:16 +0000

    progress least mainly hurt grade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
