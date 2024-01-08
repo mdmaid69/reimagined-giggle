006f5f45e09cf9d7f89b4fc2ff711704054249c4
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:38:27 +0000

    travel admit like disgusting ball

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import collections
+def create_user_dict():
+        return collections.UserDict()
