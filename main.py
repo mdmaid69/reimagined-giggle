072ef0928b97cd1331dcdbbfc5fd82f678de005a
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:22:34 +0000

    569enjoy already watch detail underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
