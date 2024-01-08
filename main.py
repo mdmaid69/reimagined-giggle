01cd4a2479565964590d74182320a739ccd27d17
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:57:13 +0000

    slice anywayanyhow bunch each confused

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def append_to_array(array, item):
+        array.append(item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
