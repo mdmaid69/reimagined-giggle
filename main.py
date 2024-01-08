0ffa1486a428a3984879fc55ae92fb10caed3423
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:04:26 +0000

    apart talkinto success may suit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def convert_to_hex(n):
+        return hex(n)
+import os
+def remove_directory(path):
+        os.rmdir(path)
