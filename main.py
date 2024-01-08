f9a3be80915aa2825fe1a801f1dffe29caff9bee
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:35:32 +0000

    since guilty collect Yy country

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
