ab93aacc6b7cc055b4ea61e341f2fe1eb8e88171
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:33:50 +0000

    listen juice impressive study die

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
