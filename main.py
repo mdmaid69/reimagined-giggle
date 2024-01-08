c90072562a83db143bdb13f9313fde5ab91571f8
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:57:18 +0000

    thin turnoff children further only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def reverse_list(lst):
+        return lst[::-1]
+import time
+def get_current_time():
+        return time.ctime()
