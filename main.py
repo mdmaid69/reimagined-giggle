4c613bf23af08616017d77f69128b720c586082a
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:57:49 +0000

    communication year fairly tone collect

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def count_elements(lst):
-        return len(lst)
 def calculate_acceleration(speed, time):
         return speed / time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
