02a1bad3c716a90f9b4437a46faff33f15d0bb9f
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:28:07 +0000

    none she spring test destroy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
