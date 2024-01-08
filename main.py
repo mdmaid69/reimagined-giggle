a4c1818dae338e53097ee9ab269ac0ae86010979
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:12:45 +0000

    call bit turninto juice example

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import os
-print(os.getcwd())
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_list_from_array(array):
+        return array.tolist()
