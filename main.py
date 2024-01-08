dd183df45927aeae156e687c9adc6d550605d002
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:13:20 +0000

    whenever bridge weigh apologizeapologise project

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
