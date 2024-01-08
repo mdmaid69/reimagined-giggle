500e89dc7c747d37e702b401a4eec31976b9c779
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:44:30 +0000

    used worse aim accident slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-def get_current_time():
-        return time.ctime()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
