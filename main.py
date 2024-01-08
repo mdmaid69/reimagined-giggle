4ac38081263fba0a1ab5ad4c65398af341c6c434
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:49:47 +0000

    disagree small level fix edge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
