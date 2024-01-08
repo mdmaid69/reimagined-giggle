c7b98a6f949b8317a0ff7f2c5f66f567be62c536
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:41:21 +0000

    cap bridge reply blame wish

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
