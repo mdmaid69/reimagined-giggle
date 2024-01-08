fac4eabf068c8e59ac6dd684c71baabaa4d12401
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:16:15 +0000

    receive early successful honest separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
