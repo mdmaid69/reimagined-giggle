dcff2012bb4784df3b5575fb30e3b8c3081f2202
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:26:08 +0000

    answer us drink seat identify

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
