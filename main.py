a64bf9bf94b138b461da7043818a78b503e2279b
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:55:03 +0000

    off frame sleep someone help

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
