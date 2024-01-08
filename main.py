480d664f1a1b6baae51796b205825b4c683897cb
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:13:24 +0000

    long free sell up excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
