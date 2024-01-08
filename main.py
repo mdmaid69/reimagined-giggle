e598f4590bfee5b776d6a577cc2d0a68203e7f54
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:13:22 +0000

    church thing lead tonight patient

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
