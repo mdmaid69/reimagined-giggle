7df7307ac51d0f9776e53501767b348898414289
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:37:19 +0000

    crisis grow notice home history

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def count_elements(lst):
+        return len(lst)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
