e028c32e6a8ed37fab4d981ded05aeb83f3ed5dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:39:52 +0000

    offence guess raise fair pity

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def is_odd(n):
-        return n % 2 != 0
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
