d87923baf4131acdda5563183c9cb22ad36f91f9
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:47:09 +0000

    guard violent whichever mistake pity

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def multiply_numbers(x, y):
+        return x * y
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
