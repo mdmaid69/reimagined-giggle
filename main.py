fe6edbf843f30758fe6e8b20ef6c300924c21f6c
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:33:16 +0000

    cut since objection stranger big

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
