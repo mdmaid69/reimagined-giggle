fb9f5ea022f610c471a51306195709d54fc274b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:26:05 +0000

    inquire exercise account street wave

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
