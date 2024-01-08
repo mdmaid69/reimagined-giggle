5fd7ad3c363f8c3f10163a11bbc18d3578970b67
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:32:17 +0000

    no thought crash else last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import array
+def get_array_itemsize(array):
+        return array.itemsize
