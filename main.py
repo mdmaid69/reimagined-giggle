f5ee2714bdfb5c709de6ab2ba9bd6d3c2efd5209
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:38:50 +0000

    problem choose depend ill always

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import collections
-def create_priority_queue():
-        return collections.deque()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
