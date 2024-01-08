8f033263fa75c742af8402637d8ceedf2b141278
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:09:49 +0000

    either line raw spite address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import tensorflow as tf
+print(tf.__version__)
