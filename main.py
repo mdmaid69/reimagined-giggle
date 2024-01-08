ff817d35987b3abe340f82eba15bc5d695f0d87b
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:04:23 +0000

    block power getaway only pretty

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
