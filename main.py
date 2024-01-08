090b4735896c3ccd6884402a0726e9ef906ea591
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:45:52 +0000

    acceptable jump meaning waste ours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def reverse_string(s):
+        return s[::-1]
+import math
+def calculate_exponential(x):
+        return math.exp(x)
