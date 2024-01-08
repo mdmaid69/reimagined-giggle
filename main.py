25149f70c0616c82975d455516d09a3dc0fda038
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:26:57 +0000

    long this show thought confused

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
