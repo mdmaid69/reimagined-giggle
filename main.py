121eec36cd7abf8890e3b60628dfed9d3c04872b
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:39:46 +0000

    middle collection damage bang treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
