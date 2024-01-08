64514edaea3d646df243daeb8bc075ac42fc8ab9
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:10:24 +0000

    convinced sell consider bury defeat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
