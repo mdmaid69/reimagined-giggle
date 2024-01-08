7ffdaf6a6161059a803434d7d9ee01987d5438cf
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:47:37 +0000

    say fit top behind we

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def find_unique_words(sentence):
+        return set(sentence.split())
