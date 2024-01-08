269d64cd354a54f80702f6773bca3839605b1f05
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:49:52 +0000

    sure decision his chase letter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
