e80f81e8088400c989a8f27d5efdfd4163ee8c66
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:12:28 +0000

    hurry top substance brave communication

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
