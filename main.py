2a1e8d9c7b90e5830ef41d8e629bddf59fd3d61c
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:04:59 +0000

    contrast patient everyone unable future

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
