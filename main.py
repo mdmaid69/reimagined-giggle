b20abb322434d16ff6ac24a231fe1fad1f6fe028
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:41:49 +0000

    dangerous bottom right fall change

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
