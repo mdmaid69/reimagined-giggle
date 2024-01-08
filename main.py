5ca68be25729642fcf82b40d9a43e5c04687714c
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:56:24 +0000

    lose organize loose dress prefer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_pressure(force, area):
+        return force / area
