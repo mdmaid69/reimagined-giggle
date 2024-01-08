b3ff036175c6d93f6d7f11ce878aeb9532ba3292
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:16:18 +0000

    cheek or familiar delight action

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
