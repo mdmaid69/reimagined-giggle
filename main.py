efe3f0ecd0f5bbf06dc423d1f2ca3d5c6fc85148
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:34:52 +0000

    law head luck slide quickly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
