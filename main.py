dddad227f1262c77da5250cabb832b3cf12699de
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:17:18 +0000

    breakinto fight achieve anywayanyhow respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_directory_name(path):
+        return os.path.dirname(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
