5b918ab6551f90afd32b83aa3e3ba2d037608471
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:03:05 +0000

    block pen uncomfortable independent demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
