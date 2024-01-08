265f80c4b1116d92389f2f44b1b6f6de4b80586b
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:16:37 +0000

    away summer start area worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
