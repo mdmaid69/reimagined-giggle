b53a40827499a657725d32ae3a2c7a6fc6fd633a
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:37:11 +0000

    experience sign debt hurt area

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
