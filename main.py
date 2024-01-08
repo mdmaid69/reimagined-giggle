a9372b79b6e62aa923c4d4ff3d3ed673382b04c1
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:06:58 +0000

    pinch middle cheat sure range

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
