2fce5c1e54ef39520bd2aaf91ca6511577db2989
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:17:44 +0000

    cover set perform instead runover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def cube_number(x):
-        return x**3
+import datetime
+print(datetime.datetime.now())
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
