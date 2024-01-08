78733e0df575dd65c05da5908893e6157bb4e1ff
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:44:36 +0000

    lay oil broad move possible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
