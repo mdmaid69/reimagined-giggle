2bcb067669f6dcea3aa415cbf6f40ef6128018b4
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:40:41 +0000

    lesson near will passage direct

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
