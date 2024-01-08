cf5d10e3aa0a26a752373af98826705aeef604cf
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:29:07 +0000

    alone summer smell passage scared

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
