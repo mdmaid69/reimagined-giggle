07896848d5d784ad04fa489910e507c0aac7b400
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:28:35 +0000

    somewhere smile last health child

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
