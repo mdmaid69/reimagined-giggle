ffae77c4bbce15771faad2c29aa1211522180c0f
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:33:14 +0000

    concerned towards combination total ought

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_area_circle(r):
+        return 3.14 * r**2
