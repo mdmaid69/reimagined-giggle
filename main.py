c207ef3d5fa1c3979e8d6ba0c8859f14c7281e8c
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:09:33 +0000

    wander admire spot excellent prefer

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_power(work, time):
+        return work / time
