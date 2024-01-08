251b225c7337f1b86c6e70549e533e0771d3d6af
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:39:32 +0000

    interview polite sure pride turn

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import sys
+def print_python_version():
+        print(sys.version)
