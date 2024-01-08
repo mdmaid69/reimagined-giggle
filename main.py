34721380b8d56412c81f2b0fd2060e960c4e4c57
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:51:04 +0000

    cheque spray begin agreement walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
