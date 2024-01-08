0b96d8c070d961a7d4283a272fa51f7664c8872d
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:45:36 +0000

    anxious gate excuse immediately wave

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
