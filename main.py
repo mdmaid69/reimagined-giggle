450d31440d2df1d129ee7fe1447f4e4d3af198bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:42:14 +0000

    old care in time mind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
