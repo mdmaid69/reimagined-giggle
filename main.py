c9e45b3731c71b0ad5837656af0e82477de5ae69
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:30:34 +0000

    bend so relax care Ee

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
