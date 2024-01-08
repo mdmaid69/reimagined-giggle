3a7dcaeb4880524ea1113bc98801a6bc30a6e74f
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:17:19 +0000

    brush keepup murder true tie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
