8c7d904d6d69bf93776446ed9f23a99969b2dad9
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:14:26 +0000

    reduce special wait emotion tone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Characters:", len(text))
