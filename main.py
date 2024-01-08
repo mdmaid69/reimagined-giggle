8b8a8d3c1d09fb3b06b7f1dcab33f4b305299d7a
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:55:23 +0000

    convincing new declare spread surface

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
