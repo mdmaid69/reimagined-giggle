1d8e863b14dadbf8b5d9f2a661b7be2900db32de
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:52:20 +0000

    how pile before relief mean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
