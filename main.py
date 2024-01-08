92fb12f3227f3e93fcd4ff961ab6a60aa28fd89d
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:37:23 +0000

    rise advanced else every return

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
