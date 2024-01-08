a6c7437eb6c6f607ca2e8d023c43f1fbaa87c756
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:45:45 +0000

    talkback press drink go hear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
