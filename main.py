4df54e9c6eeb3d454ba51b5bcbc334410d18777d
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:19:06 +0000

    base free stare trust square

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
