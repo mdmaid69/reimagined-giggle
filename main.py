fe891aff39b2401f18d2be89f7a9c4d9f141a226
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:34:11 +0000

    extra result holdon wherever noisy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
