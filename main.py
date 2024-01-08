904ab61a4895c7002a47126f778058cb85cdd368
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:21:15 +0000

    compare laugh press duty wind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
