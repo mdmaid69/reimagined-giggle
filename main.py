b6574121c30d6bc8f4eb55c05d27096b3d42125b
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:27:19 +0000

    appear great pronounce dream behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import sys
+def exit_program():
+        sys.exit()
