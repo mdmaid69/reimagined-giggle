2284bfdc3ca08891f05211ee39fb6f3187554a61
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:57:16 +0000

    strange impression vote nervous tradition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
