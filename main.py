24297d782df77077957db161a7f960a8933dde2f
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:28:33 +0000

    our left sure attempt raw

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_pressure(force, area):
-        return force / area
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
