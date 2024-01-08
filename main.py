2e0b88d3f78fb10363ac9d232fac291a84ff5b81
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:26:18 +0000

    out below rush instead difficult

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import os
+print(os.getcwd())
+  def convert_to_binary(n):
+        return bin(n)
