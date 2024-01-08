d92a88a540dc100a39eacd412aadc0f5b7cbce3e
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:11:45 +0000

    ill square warning birth him

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import collections
+def create_user_dict():
+        return collections.UserDict()
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_itemsize(array):
+        return array.itemsize
