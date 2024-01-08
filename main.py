0c34fdedc17579e8539bd4f4492a21dd866a7e88
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:14:37 +0000

    bag meeting willing nonsense serious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
