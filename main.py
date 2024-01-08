f3d89b40988e71cdaee5a91b80d3e92a25ee8465
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:36:22 +0000

    on generally school match body

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def greet(name):
+        print(f"Hello, {name}!")
+for i in range(10): print(i)
