d4e4f7a085c4bf1eaaccfbcebabbeafca5364ecb
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:28:39 +0000

    average die game tradition manner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
