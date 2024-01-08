e60da10241e49ac09acb93de01f141db4072eefb
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:54:16 +0000

    any disturb list nurse pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
