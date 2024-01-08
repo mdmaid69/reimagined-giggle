6268e5907828aac067e1b2a8b194209adaee3758
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:52:23 +0000

    getout degree thank arrangement holiday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
