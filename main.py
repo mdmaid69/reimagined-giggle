00e6f5deab9b5ef2fe24494138dc678ddb98e907
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:39:38 +0000

    music perform profit loan represent

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
