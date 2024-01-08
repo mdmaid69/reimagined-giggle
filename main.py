c7fa3e339284878d791524fd780339f9238873a7
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:30:25 +0000

    message press tired lead runinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
