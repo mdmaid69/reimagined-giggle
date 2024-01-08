5178a845ce39eef89b8f9bd8fdf9ebc62f20341a
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:19:06 +0000

    Bb translate crisis ring century

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def clear_array(array):
-        array *= 0
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
