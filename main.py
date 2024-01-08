0dc5ff438a969b9c820af4c8b6088811f7289a01
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:22:09 +0000

    walk tomorrow look excuse nowhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
