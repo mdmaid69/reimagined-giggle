cd7fed55ea251784120fef1b0befce360aaabe74
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:51:43 +0000

    week limit else rank repeat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
+def find_union(list1, list2):
+        return set(list1) | set(list2)
 import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
