fa5e9356043bb40b4bfb10d88b9ed9e71e6aed37
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:09:00 +0000

    clothes nonsense very well build

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def reverse_string(s):
-        return s[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
