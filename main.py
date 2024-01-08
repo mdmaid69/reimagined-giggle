cf08960e55f2ad0b3bf9d8990dbbae66e6082b9e
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:19:05 +0000

    different repair show comeround discover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
