979ccc56c7053fd33d5fd90c6d607dad091fd072
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:22:00 +0000

    software knock card mass swimming

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
