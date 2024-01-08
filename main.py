ab552a80b6b8588ef9e6aa6e949ce864fef0297c
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:43:25 +0000

    quick out form paper popular

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import time
+def get_current_time():
+        return time.time()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
