01447bac07c47845113a687900a0a51e91834ec3
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:50:22 +0000

    think tight tour explain in

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
