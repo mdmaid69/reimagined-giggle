c2f65855165051cbd93e51b95c22972d6400056b
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:05:25 +0000

    excuse takeback match burst motion

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
 def get_array_from_string(string, typecode):
         a = array.array(typecode)
         a.fromstring(string)
         return a
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
