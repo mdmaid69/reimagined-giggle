5dd557a5f5563db395e41de29acb45bbf5288c50
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:00:42 +0000

    towards shine fall suit desperate

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
