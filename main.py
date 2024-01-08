0d21a0d33183028d2d3650766df25022e45cbb77
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:55:47 +0000

    will stick represent benefit different

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_min(lst):
+        return min(lst)
