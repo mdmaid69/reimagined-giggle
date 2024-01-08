fd33620eda6d1f887047ea4731722ffbbde60d43
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:51:22 +0000

    left keepup learn positive opinion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-print("Hello, world!")
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
