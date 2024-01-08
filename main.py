b54a347f7f7e46cc34a851e0ea45073e3b7fd1e9
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:58:08 +0000

    blame grateful advanced doubt strength

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
