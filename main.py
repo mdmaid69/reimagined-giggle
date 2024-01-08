6f59ae7646f75e073a0bf5cd89ec019265c7dc0c
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:33:15 +0000

    place good great lend steal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def split_path(path):
+        return os.path.split(path)
