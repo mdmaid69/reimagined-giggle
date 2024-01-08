1fe857dba9ee3221f954d75680a342049242bee6
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:17:27 +0000

    lonely left fun storm closely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def clear_array(array):
+        array *= 0
+def calculate_work(force, distance):
+        return force * distance
