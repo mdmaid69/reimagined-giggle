4b500b6f71d3a1758230424b16b4fa36c9520461
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:23:35 +0000

    gap guide position lower proof

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
