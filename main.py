2a0cecdd1b6333f8b758056ca86309246239da25
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:09:17 +0000

    steep getaway promise injury flat

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import json
+print(json.dumps({"name": "John", "age": 30}))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
