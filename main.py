f250f3144b7d1d19c43a23c90271ba049525236b
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:24:42 +0000

    injury dance pot measure shine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
