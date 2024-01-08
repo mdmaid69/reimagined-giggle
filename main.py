d7cf1330bdcd7323858e8e3d6b72b1427dcac2fa
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:38:46 +0000

    rule broad risk feeling interested

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
