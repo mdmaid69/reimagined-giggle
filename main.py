92f0888805e556b6740d2112b4432aa499edfa1e
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:59:14 +0000

    dream talk tread right different

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_speed(distance, time):
+        return distance / time
