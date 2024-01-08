4c3f95a03f9169085389d4fc824ad7289bace41f
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:18:51 +0000

    responsibility doctor switch point politics

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
