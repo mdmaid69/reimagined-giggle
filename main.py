ed0d46fa4293b149a27e8d7e16fb8ff27ceaccbf
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:08:01 +0000

    either minimum level trade pleasure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
