29b756bda2505b75a5028cb6cdbd2a5615ad624e
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:05:50 +0000

    software middle market storm plain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
