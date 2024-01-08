df91fbce4b90fba576ebe3f63791812a70f38904
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:42:10 +0000

    flavour boat bringforward might jump

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_volume(length, width, height):
+        return length * width * height
