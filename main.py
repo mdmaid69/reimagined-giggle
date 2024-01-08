7216556d1481f4c433731346d3997b715da8b9e8
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:22:57 +0000

    broken contain keepaway defend slight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
