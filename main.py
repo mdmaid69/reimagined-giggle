9a7e85a57607e6d0ac10318126b12eaecf2b35eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:42:05 +0000

    side worth bringup brother like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def clear_array(array):
-        array *= 0
+def get_array_as_bytes(array):
+        return bytes(array)
+import tensorflow as tf
+print(tf.__version__)
