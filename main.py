59c46dc1c8783816593af446a78cb6c72f3b2be9
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:10:34 +0000

    onto independent typical occur announce

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def count_elements(lst):
+        return len(lst)
