bf74e43dab0cdcbcab1f5c529b94f6e72a11b1e7
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:16:59 +0000

    catch brush fit funny behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
