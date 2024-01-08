53a7db323416bdcb002e33643fb97e22f98f9485
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:11:20 +0000

    keepoff warning school tight spoil

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
