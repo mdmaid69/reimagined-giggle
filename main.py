190dc48ac8cf542168e68bd82e84efff67ba7365
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:05:39 +0000

    police outside often circumstance over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def multiply_numbers(x, y):
+        return x * y
