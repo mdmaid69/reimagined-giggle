25736cc909a5ceec192356e9dec87b7fd616901b
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:24:57 +0000

    moral wrap reason disturbing thin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
