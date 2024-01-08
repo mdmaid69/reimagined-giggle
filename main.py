d4f28f0f0a7b2c283d353142a9bf62087f047b52
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:12:20 +0000

    appearance bet doubt front free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def reverse_string(s):
+        return s[::-1]
