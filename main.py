ef01c59cd259f3796657b8b5bec534c29dcd2162
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:40:02 +0000

    busy comeover with necessary grade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def add_numbers(x, y):
-        return x + y
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
