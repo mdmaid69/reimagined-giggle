594af09c47fcfae3492a609ac5069a10d3e712d7
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:17:05 +0000

    behind boring condition fun care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def subtract_numbers(x, y):
+        return x - y
