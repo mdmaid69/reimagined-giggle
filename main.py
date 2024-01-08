39459dbe25eb183d5199c5873fb3a6c47c384833
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:52:28 +0000

    foot gentle turnout voice absolutely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
