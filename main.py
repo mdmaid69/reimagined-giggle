1160a3586a7bb58dd2d7862926a60d355355c8e9
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:06:35 +0000

    practice lookaround generally murder arrangement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+i = 0
+while i < 5:
+        print(i)
+        i += 1
