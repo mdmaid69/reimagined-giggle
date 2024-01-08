4590d77ac8153a9e95f717efe8115ce201f42388
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:10:46 +0000

    turnoff tip listenfor explanation stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
