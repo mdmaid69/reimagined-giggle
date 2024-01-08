5667bb061835e72b53719cf37fe02a2938aa05a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:29:52 +0000

    thinkover bother beat OKorokay listenfor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_error_function(x):
+        return math.erf(x)
