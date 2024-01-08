35c3f4545b615963e7c6f74fca8a14155a519415
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:38:35 +0000

    hungry spite deliberately display radio

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
