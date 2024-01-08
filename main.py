90300a07410be0c745c4ead2413bca305408c969
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:36:52 +0000

    maybe lay coffee capital balance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+text = "Hello, world!"
+print("Words:", len(text.split()))
