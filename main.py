c945d698a3680ffa8b9bde339040c2fe25aa52d6
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:16:52 +0000

    appeal needle copy takeoff trust

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_directory_name(path):
+        return os.path.dirname(path)
