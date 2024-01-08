6e44a483282df9dba1f0681e78257b8b4b772a73
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:50:38 +0000

    smile see pause mood excuse

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
