45f3ca5de61acbd17635916016babd87d10bd911
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:07:52 +0000

    comment community effect discovery proof

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def get_current_date():
+        return datetime.date.today()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
