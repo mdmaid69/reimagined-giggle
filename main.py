0d01b1e01d22460309a57e57bc9017ee938a80ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:22:16 +0000

    spoil any stop attitude injury

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_pressure(force, area):
+        return force / area
