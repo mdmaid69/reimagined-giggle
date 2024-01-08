019361c6cb9b983658c330037286c7518803d645
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:22:28 +0000

    calmly church determined minority nerve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
