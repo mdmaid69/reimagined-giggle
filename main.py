c3cb42a4f0319d5ad6a103b6bc58d3a2f0095139
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:41:27 +0000

    hole nonsense shelter luck ignore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
