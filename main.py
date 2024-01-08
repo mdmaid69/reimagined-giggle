ec2497640c29ea644bbe1f959949053ea731b145
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:08:55 +0000

    comeover him hot soft press

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
