7d3bf6c1a6144f3a9820ad190f21743c1ae29fae
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:10:16 +0000

    memory have pot left below

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
