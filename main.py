1930a400322d9ea9c9666b74f4d818be00a0cfdb
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:03:45 +0000

    description declaration distance steal left

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
