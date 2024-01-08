78700ff6f058f57ab6ad7b9e46230b380759364d
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:51:01 +0000

    ear street reliable close average

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
