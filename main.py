2f492eda1823eba496ecff83e3eff06a0b3c882f
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:26:38 +0000

    tip safe murder within view

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
