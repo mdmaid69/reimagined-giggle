ab3425642f18d8390f0d47cecafda5e4e0d552ee
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:59:27 +0000

    fetch throw petrol obtain hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
