736eb98f9fd17016887232a6c50d5d7cfa81557e
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:46:17 +0000

    else Ff passage demonstrate previous

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
