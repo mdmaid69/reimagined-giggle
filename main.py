84a87f9129e3618309bc676273d49b47d00f8351
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:46:40 +0000

    demand recognize intelligent top 572enter

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  def reverse_list(lst):
-        return lst[::-1]
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import os
+def change_working_directory(path):
+        os.chdir(path)
