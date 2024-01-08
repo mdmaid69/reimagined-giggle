56f8cbb7b29327e38f84c84e9d8b874753575fa9
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:42:54 +0000

    raise slight tight replace last

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def reverse_list(lst):
+        return lst[::-1]
