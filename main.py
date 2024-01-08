9fe6c4fd3e234fb03da7c0e979be857a14bcb523
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:58:58 +0000

    music plain trick cruelty phone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
