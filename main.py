336d7420c5f8af72c742e59baf7a8e5052aa1209
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:31:41 +0000

    age turnover ticket against light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
