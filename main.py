a86436f4d3220d6f3fd42d61f72f52e0db43bca5
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:47:35 +0000

    runaway comeround vanish mind within

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
