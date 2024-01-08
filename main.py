a828ca614aa37665cb1642ecfe3a1d01ccbbda69
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:36:03 +0000

    repair keep personal likely compete

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
