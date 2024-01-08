28d9118af9c83a5c822b5b3ac57db5e513fd5383
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:11:17 +0000

    deaf throughout marry getup wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+print(os.getcwd())
