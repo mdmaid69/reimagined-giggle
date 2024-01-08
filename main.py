c73a64a3c2edf9450c4a2695f3f41b0bf9b379b7
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:26:45 +0000

    freeze smell weak giveup close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
