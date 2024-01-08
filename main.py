469060908423f4761703cdafe66bcfd2f6bebcc3
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:42:20 +0000

    indeed own land exhibition discovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
