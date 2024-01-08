f0729cc8ff97b5deadd0a5170638580a8178bde0
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:19:00 +0000

    fetch size police believe run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
