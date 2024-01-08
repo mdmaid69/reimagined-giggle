82f4023eeacbac28b6ba7af8c5bf418366952e3c
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:35:41 +0000

    bet spoil silent noone very

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
