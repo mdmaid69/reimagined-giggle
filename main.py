9882f063f07534c109c273a7bc2a59728f8b38ed
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:17:35 +0000

    today close spring display once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
