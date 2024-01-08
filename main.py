068b0e07d46f3b1f807ab8173e740834f4a37c38
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:03:56 +0000

    usual scrape unhappy stage home

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
