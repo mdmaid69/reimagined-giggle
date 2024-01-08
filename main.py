bbbcc986d5b4c5f523d9bf0df5e68ebe66cd87f7
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:35:18 +0000

    eager unfortunate practice acceptable stay

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
