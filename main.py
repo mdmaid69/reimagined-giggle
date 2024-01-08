fa47f8ec779bef60cccc2a3145a9694171364ba6
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:42:41 +0000

    key worth respect best earn

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
