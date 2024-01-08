809c2837298fb6d1242abdfe77330cefc0632f8c
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:03:54 +0000

    exit substance mine this vanish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
