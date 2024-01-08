c96e46ddf19e5053ebb02310b4453fad530efb8d
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:43:10 +0000

    table public hold authority decision

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
