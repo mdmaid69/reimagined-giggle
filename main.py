1d2443f4ac8d6cfc9d2128aa91180058c6de0940
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:47:08 +0000

    teach damp 566end run believe

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def append_to_array(array, item):
+        array.append(item)
