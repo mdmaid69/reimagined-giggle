7f503ca5d2902b96b15421181bc5b59255ba180d
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:17:41 +0000

    callup lost press yours fit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
