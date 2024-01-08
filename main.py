1a6f20571a1bbe50937e8eebe48a4e969d1ac12b
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:12:21 +0000

    sure repair record slightly help

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
