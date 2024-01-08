d4eac9a07c69285caaa7ed4d963ac52fbbbeacfd
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:28:38 +0000

    them breakin noone ready inform

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-for i in range(10): print(i)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
