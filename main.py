78ac95894e850252003ee106a2fe40ceffe37068
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:12:08 +0000

    definite explain fall question punish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
