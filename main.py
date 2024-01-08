61b772c4768216e7c6639fa0189bece52257ee3a
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:21:26 +0000

    care tradition experience plan judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
