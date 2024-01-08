491c711d4892edd21c1647b19d389359cb49c6ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:49:44 +0000

    empty bite bright shut instead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
