cbce87207631353bfa3ff29076b92b2aaafbbe4d
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:19:24 +0000

    understand dead whistle children call

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import sys
-  def get_python_version():
-        return sys.version
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def find_min(lst):
+        return min(lst)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
