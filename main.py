e808efdbedd4d5ddef7d71f8939b5a36ab2e75d7
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:45:32 +0000

    they politics never total offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def clear_array(array):
+        array *= 0
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
