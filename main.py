ceb2c44ddf1ce3eecea324dc432eea21c62c4421
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:50:33 +0000

    awake land scare Zz exit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
