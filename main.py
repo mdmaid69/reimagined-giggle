489bef789640141e6885f49119fdbb64410513e9
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:36:38 +0000

    Nn aim tell ahead none

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
