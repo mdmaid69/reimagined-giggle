cc1a3750aad88250885540a8e9e27d2f700359da
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:21:09 +0000

    impossible contrast fix brown past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
