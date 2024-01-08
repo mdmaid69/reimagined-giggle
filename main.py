c51880b1b5d3c4e7bb0906f089314f5c1609f53b
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:55:47 +0000

    born crime nurse sink field

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
