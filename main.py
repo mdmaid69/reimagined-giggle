59b12f4e9962f7c4bd02c3f94943a9df16059354
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:51:04 +0000

    light wherever market face terrible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
