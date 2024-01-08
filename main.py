037f18a8a8f14c95dfce48abb30c0aa2fccc438b
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:16:11 +0000

    rather decide better you drive

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def get_array_as_str(array):
+        return str(array)
+def convert_to_octal(n):
+        return oct(n)
