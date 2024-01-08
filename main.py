3604107490c8754d256b0cff6524175a7b728321
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:00:19 +0000

    constantly soon trip rush relief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
