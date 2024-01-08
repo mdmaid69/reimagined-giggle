2cc40ded3e763e4f1ca6d1a23b3d7cf49e950e1b
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:51:27 +0000

    react turnup Rr information time

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import os
+print(os.getcwd())
+import array
+def get_list_from_array(array):
+        return array.tolist()
