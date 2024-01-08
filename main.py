910e8bd6d4ed687a146a56158cada189d0624634
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:10:50 +0000

    once disturb minority spray concerned

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_hex(n):
+        return hex(n)
