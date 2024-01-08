dfea8b98c5b6f17be476adcb037a57d1d45022ad
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:55:41 +0000

    bar violence Itself about official

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_slice(array, i, j):
+        return array[i:j]
+def find_union(list1, list2):
+        return set(list1) | set(list2)
