b6fdc30de3f998c56f5068f31bedc461845a1a02
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:21:37 +0000

    race receive height shade bush

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+print([x**2 for x in range(10)])
