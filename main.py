995cc003f3372cd850a4750506ef0a99d6bb6fc3
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:33:03 +0000

    that thinkthrough take part centimetre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
