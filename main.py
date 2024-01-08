6bd4cbc298590240895b04235a005ff147dfca2e
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:57:15 +0000

    edge contrast punishment fill tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import tensorflow as tf
+print(tf.__version__)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
