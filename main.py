53a35fd4a04d4bc00f687f2e51411062a32a2776
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:24:59 +0000

    paper service fork fine people

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
