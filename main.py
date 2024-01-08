91479aa7b633028a98fb8c0349ae16320871670e
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:02:03 +0000

    rather scrape environment improvement afford

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
