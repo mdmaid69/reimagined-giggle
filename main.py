e814efa686d62e9e38a06f56822444bc59c3ccca
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:23:25 +0000

    stiff partner pause flat common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
