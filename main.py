2287ba501fb818e53ce081f81d1bc7f37c7cf982
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:20:02 +0000

    best wave rate minimum last

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+print(math.pi)
