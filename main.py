e3c820923646bf4401da6f42dfa79cfbcf7f935b
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:31:13 +0000

    moment set happen mine disturb

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  def convert_to_binary(n):
+        return bin(n)
