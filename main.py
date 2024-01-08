7244e1ab746ccff369a533a2584266ee73b9db8b
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:25:38 +0000

    practise feeling exhibition wind complicated

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
