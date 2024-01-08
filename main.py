f92377b2fae8460a8a8529a4020b59434ee81baf
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:56:37 +0000

    curious contact Dd between announce

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def remove_duplicates(lst):
+        return list(set(lst))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
