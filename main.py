5cb93aafbc537f35e2a79d888e3d94dc3fd0a791
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:29:03 +0000

    care free succeed year explain

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def find_max(lst):
-        return max(lst)
+import sys
+def exit_program():
+        sys.exit()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
