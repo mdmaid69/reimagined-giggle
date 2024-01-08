931ebd671465dedd18447a15c59b90fa3bc4c4a4
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:37:57 +0000

    sudden quiet accident freeze accidental

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_area_rectangle(l, w):
-        return l * w
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
