ea954dcea89103d3d1d65e79d2811a39d5886cd3
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:32:41 +0000

    callout battery bringdown old valuable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
