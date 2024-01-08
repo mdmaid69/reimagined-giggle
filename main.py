ad99719ec4d0d613928c1e5a50106ea5e6ccb288
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:03:48 +0000

    plan to force Zz season

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
