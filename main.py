1c8fb2a0af686e70a661563a9bb51e4205f345c3
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:50:51 +0000

    bringup rush fine limit talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
