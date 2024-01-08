6cd4167cc295d2e72657163b2fc60901d6aeae05
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:39:33 +0000

    stick handle hit stop rule

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
