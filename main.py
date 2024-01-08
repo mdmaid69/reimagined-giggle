5d57e0d530ce0087ec817986a4cd30f700699816
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:14:23 +0000

    hit afterwards afternoon model culture

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import numpy as np
+print(np.array([1, 2, 3]))
