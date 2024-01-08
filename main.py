fc752511c9c6f237796d00ea126fbdce32d4c071
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:02:35 +0000

    himself care Bb string strength

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def remove_duplicates(lst):
+        return list(set(lst))
+  def reverse_list(lst):
+        return lst[::-1]
