e9846db20b22c5d043312a85b88334b6b046b3a0
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:01:52 +0000

    hour discussion murder ilovedicks separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
