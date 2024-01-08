18aaa5913b558ed8db2f84afe71ef5de3b0321d4
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:12:48 +0000

    bang mean slowly process top

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
