a96fe64218416bbaa923c8295bde6d75f0983c9c
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:04:29 +0000

    closed gooff doctor stone extraordinary

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
