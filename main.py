8d219fa337181cb1571af0648c0f1e3e3afb6633
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:23:05 +0000

    whoever passage perform basically scared

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
