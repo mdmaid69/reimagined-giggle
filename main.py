75f8ced1406bb520b065fc3f9cc23ca48aabe621
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:52:52 +0000

    show instruction weekend offence guess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_as_set(array):
+        return set(array)
