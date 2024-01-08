c0bf3aed6666bdd618a6dc7dd295614b3a228ae4
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:53:28 +0000

    left original pain tear translate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
