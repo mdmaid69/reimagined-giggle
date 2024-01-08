071c477e16ce7e3421d01c02da2b530a61114ac7
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:38:11 +0000

    bill kind negative different Pp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
