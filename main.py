e68f9d54c9e57e49acae8933e594077e1a201d64
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:51:01 +0000

    backwards lot relax neither flat

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def reverse_list(lst):
+        return lst[::-1]
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
