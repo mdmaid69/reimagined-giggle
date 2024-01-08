3ac3ecd675c5c46a4675f679f4d3c619aa897a62
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:21:22 +0000

    store file proper character treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def add_numbers(x, y):
-        return x + y
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
