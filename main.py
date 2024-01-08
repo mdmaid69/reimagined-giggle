fe62919ba45eaa0113b3fe833f5ed999d5d59b72
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:58:05 +0000

    lot provide swim lonely colour

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-print(math.pi)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
