0d75603f884acd757068978c9436595d7ca55f4e
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:58:55 +0000

    weather range eager me relative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
