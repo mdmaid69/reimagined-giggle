9cfa6e1c5c0a7395141fbf38590a7e48413df441
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:45:48 +0000

    favourite open government remember greeting

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
