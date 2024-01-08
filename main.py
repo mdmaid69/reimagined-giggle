afe0cc22437584c108245902c32a3c7feb709ad9
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:55:23 +0000

    thin in might about respect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import random
+print(random.randint(0, 100))
