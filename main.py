630c788dd24a244ec26e5f22583c3fd7e6c70f94
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:42:51 +0000

    blue area lookaround smoke unable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
