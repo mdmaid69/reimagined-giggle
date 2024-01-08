962a1555b84dcd838bff200a4ec3e7fd753f4b1e
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:20:15 +0000

    clean cheap home divide right

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def find_min(numbers):
+        return min(numbers)
