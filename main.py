6adb6c331b6235fd61f07648900ca3992fb30fb2
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:21:37 +0000

    body expression complete head discuss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_power(work, time):
+        return work / time
