9f3549c7320d8ebd5063bbfa2dacf4bba1d5d9aa
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:35:36 +0000

    equal charge smell everybody complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
