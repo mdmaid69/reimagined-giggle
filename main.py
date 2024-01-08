fafb6b39d21717f336f68d9ab14b9290a9e9a6ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:23:33 +0000

    party even organize expect say

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
