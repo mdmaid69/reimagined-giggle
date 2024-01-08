aec95509517145767f85b0c0a8c21b5596a16a3f
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:58:35 +0000

    temper paper Jj feel push

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def square_number(x):
+        return x**2
