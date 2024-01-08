a453656eaf1894569c17f6286996633ba48d396a
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:02:28 +0000

    wheel dangerous within top getout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def reverse_array(array):
-        array.reverse()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
