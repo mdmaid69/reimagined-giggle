a6cbc7fdafb09bb05ec1832b457adf90daca634a
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:30:26 +0000

    window walk by ear stupid

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import collections
+def create_stack():
+        return collections.deque()
