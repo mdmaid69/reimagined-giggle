4c5231c567f0700532bb526f66d854d420959f24
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:56:53 +0000

    myself bridge joke calloff opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
