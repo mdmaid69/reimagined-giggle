675f73bacaff5aca2749bdb4e787529079152341
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:07:31 +0000

    question report person agreement instrument

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
