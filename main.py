73b1d36116659ad99f665853ba447006b72d0bba
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:05:16 +0000

    protection band music immediate smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
