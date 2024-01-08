8feaa3f6bed238ef2d8b1d421e52ab883101a5e6
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:23:46 +0000

    morning react match own nearly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def sort_list(lst):
+        return sorted(lst)
+  import sys
+  def get_python_version():
+        return sys.version
