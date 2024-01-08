c6193f7a8d722e154e14a8565fe8a90f00d7954b
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:33:48 +0000

    write snow keepout develop mass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
