56cdd0ef8f1f0157bbe34a07011b806b348e061f
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:05:45 +0000

    all transport tell comfortable replace

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_tangent(x):
+        return math.tan(x)
+print([x**2 for x in range(10)])
