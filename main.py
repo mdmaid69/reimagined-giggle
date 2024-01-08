a15ef486cdbec9642b6d275e0fdaf960b03e9032
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:47:39 +0000

    secret Zz call feed defeat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
