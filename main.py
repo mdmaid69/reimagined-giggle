9460a884abfbcb43b1eaf2a42bee5ad5b4440529
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:01:23 +0000

    lean contents break get disapprove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
