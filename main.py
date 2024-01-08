5f4bb7fdc15655e322f33481911a1f2de05e7802
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:26:34 +0000

    steep wonder control crowd complain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_counter():
+        return collections.Counter()
