2b3bd1d5c6958cc577b42dd52653e07363465535
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:14:41 +0000

    there later generally exchange declare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
