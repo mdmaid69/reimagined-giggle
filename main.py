242b3af0b35061f538d41225369751507d7f5bc2
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:57:44 +0000

    period sight pot crime low

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import csv
 with open("some.csv", "w", newline="") as f:
         writer = csv.writer(f)
