f54648dce094e5f1b1775de9e8dd890f4a0a281b
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:57:38 +0000

    add lay crowded busy through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
