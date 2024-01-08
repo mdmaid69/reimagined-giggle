81e106f646e2c02743b3b1a6fe9cd42c7fbe46bf
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:05:50 +0000

    sad result early city actually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
