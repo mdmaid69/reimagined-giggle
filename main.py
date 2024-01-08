b2e0fc37de6eace3db4e32948de4475943e808de
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:50:49 +0000

    oil Dd appear raise manner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
