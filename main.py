ba78971365bb24c4c95bc7330d6769ca23c87241
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:45:41 +0000

    phone each reduction buy naturally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
 import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
