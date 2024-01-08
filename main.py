2626f7018cc594f82c6346d5b425e8e42e601669
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:16:58 +0000

    report search history whatever talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
 import array
 def get_array_itemsize(array):
         return array.itemsize
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
