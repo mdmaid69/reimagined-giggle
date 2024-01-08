060f188e26b6a3638af8b3163775725df67e4f82
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:59:17 +0000

    vote list imagination closely noone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
