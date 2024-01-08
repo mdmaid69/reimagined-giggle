07bc85e2a595dc41b0a8ddbd89dc32e84f45ce29
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:00:12 +0000

    fortunately natural any regularly dinner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
