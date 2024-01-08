ee72c76db269e5c97e578a44e1aa1ff5c10f7501
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:41:11 +0000

    amount shelter deep thought pass

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
