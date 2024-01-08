062506cde591964feb1e2e81ccb1fdd399cfa14a
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:53:48 +0000

    572enter back over seriously forgive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_acceleration(speed, time):
+        return speed / time
+  def is_odd(n):
+        return n % 2 != 0
