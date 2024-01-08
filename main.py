70d80fb95f045de47a468f74d6fdcb54f27873fb
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:54:42 +0000

    platform hope congratulations opportunity arrival

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
