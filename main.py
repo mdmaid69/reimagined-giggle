b90ca93e78ee87fa6ecfd09f2122257ca0d60237
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:52:15 +0000

    pot appeal worth slight store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_tuple(array):
+        return tuple(array)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
