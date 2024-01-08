bad626f6c6b0f4fc72d676ac4cd7e7b923a40bd9
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:40:58 +0000

    empty meal correct goon goon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
