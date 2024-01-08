d50f855240d940861170302d2e3d33c6fca63b4f
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:12:15 +0000

    minimum once adjective share butter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def split_path(path):
+        return os.path.split(path)
