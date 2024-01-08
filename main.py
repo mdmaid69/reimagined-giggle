179b0123f33294f77ab70cc188dd784e4695e3ce
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:09:44 +0000

    test thanks treat more able

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
