1ff7541351e131917ab0372c2bcf03cef21e3993
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:40:17 +0000

    cost ilovedicks sensitive bad obvious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
