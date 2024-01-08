4262aa816428d69e485e7f8a123bd069f3700b54
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:06:25 +0000

    little common clean zero sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_sine(x):
+        return math.sin(x)
