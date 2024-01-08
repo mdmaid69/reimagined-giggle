230e7b9bf10cb68b8d4e46a14bb83cd4890e1623
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:30:05 +0000

    travel Nn space proud rain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-  def convert_to_binary(n):
-        return bin(n)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
