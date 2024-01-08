1c7c8d1c92e21918c4c2c912c39e61898e60bc90
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:55:51 +0000

    lay though past I music

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_acceleration(speed, time):
+        return speed / time
