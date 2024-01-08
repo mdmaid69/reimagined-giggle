aeb4f64167b66bc77047b1b5d33b8c7a6945a33e
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:58:36 +0000

    slightly left opposite evening software

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def calculate_volume(length, width, height):
+        return length * width * height
