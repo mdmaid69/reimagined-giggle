684d75422606b7669cacda5552fb502a1559edb6
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:43:42 +0000

    recently even realize frame common

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
