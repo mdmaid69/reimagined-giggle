681264eb30b752a8c3dd672b6b07a7e48abd8c1e
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:32:31 +0000

    bite calm instead search about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
