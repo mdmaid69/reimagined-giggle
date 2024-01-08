e7bfe0aaf91ebfcfa084f8bc942f243f30c85ecd
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:39:12 +0000

    of proper murder extraordinary group

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
