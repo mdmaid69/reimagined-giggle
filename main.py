6afd822677a7a52a0e0e2f3e34c23fd9c3b49ff2
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:26:18 +0000

    moment involve ring promise loan

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
