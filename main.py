8c258cc6a3863159c8cd3813c35283d7288e8c3e
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:41:31 +0000

    even watch score situation middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_sign(x):
+        return math.copysign(1, x)
