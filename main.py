3872a5d52775b5cd8b133f12b828fa68473db916
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:40:56 +0000

    tip memory exist flow ability

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def get_array_item(array, i):
+        return array[i]
+  def convert_to_binary(n):
+        return bin(n)
