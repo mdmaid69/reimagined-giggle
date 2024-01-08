8f63876149c5d316c5c14c650b4c42703d9b3d5a
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:03:37 +0000

    shop eat play sick original

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
