f021a335ee413afbbe1a19ad1bedbe30c87dadf4
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:02:34 +0000

    unpleasant point substance edge sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_file_gid(file_name):
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_group(file_name):
         return os.stat(file_name).st_gid
