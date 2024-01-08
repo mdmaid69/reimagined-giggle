48b6616bc9496aaf5c40a7e61907a5fd7d815658
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:27:01 +0000

    strict body 571enough unfair argue

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_tuple(array):
+        return tuple(array)
