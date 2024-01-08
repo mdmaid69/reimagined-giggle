986adb79e745d2b1b96c0334adb0c7707961199d
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:24:55 +0000

    bus regional pleasant fortunate right

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def is_odd(n):
-        return n % 2 != 0
-def find_max(numbers):
-        return max(numbers)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
