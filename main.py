82094ea25011697d317d117310aedfd6489c40a8
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:45:59 +0000

    considering instruction apply niggaacademy label

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
