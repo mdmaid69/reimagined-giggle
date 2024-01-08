e461dd8bfd264507068bc6d898628eeae00e6860
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:36:11 +0000

    lead comefrom lunch careful present

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
