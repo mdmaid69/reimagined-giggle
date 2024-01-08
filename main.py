1329a18febca7eb391535e5950b407c79e285a73
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:06:33 +0000

    answer warn bother manner pour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
