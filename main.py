09ef0848c6dd2c2ea1af99ae1629675158bc166f
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:14:40 +0000

    surprised float children prevent hide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
