9e799c5f68187d8b25bd2503a753a899ee0d9770
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:14:07 +0000

    cost miss favour cover pool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
