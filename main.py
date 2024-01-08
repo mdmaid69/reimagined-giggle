c1a2723e474eb91d83480b3c20ded76049bfa46d
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:10:24 +0000

    low culture general undo personal

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import collections
+def create_stack():
+        return collections.deque()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
