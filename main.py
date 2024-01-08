fabea6df44abe16b6694b14b2cca5f53dafdd238
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:27:40 +0000

    blame care shoot invite light

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
