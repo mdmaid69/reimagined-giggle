f0c5b5ca8d4734f48beb6fa677d4e59faf61f01f
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:33:19 +0000

    564encourage relationship close improve rule

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def find_max(lst):
+        return max(lst)
