e365dfcbef6d36b50079713a573c1386365d17bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:32:48 +0000

    lay disgusting passenger mysterious range

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
