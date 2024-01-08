1a9346e2ee1f1157274734172a21cde51bbf7f55
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:49:13 +0000

    snow contact defeat do muscle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
