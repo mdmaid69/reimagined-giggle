8777f87f3aee348558b4ab1e496ae72eabb73902
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:15:01 +0000

    anxious splash round thinkof that

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_exponential(x):
+        return math.exp(x)
