99243e0d5cdbe4c4f1f523a25c71152a2893c9f4
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:42:29 +0000

    past since sick probably sight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
