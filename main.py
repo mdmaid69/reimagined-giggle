1d91f9ab2e0f05fc218d5908bfcf5053a7b776c5
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:18:48 +0000

    production tip report spill extra

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
