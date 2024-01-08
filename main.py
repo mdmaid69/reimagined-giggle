eb3cb96213d874d86cf83b607f74938c9100bfcb
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:08:42 +0000

    gradually touch intend personal beyond

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_set(array):
+        return set(array)
