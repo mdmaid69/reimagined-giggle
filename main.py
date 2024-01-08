cea45e13ba9168a480131b96dc5de3f3d0e99c13
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:20:53 +0000

    570enjoyable claim anything shallow naturally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
