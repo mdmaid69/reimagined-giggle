a27adcecd231f00268795ce98d4f46dc12faf860
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:40:24 +0000

    accept next far sound avoid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
