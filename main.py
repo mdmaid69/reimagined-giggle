f4af26c06171f109097275478d0543b02bf51b73
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:49:32 +0000

    give chief second pinch thought

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def sort_list(lst):
-        return sorted(lst)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
