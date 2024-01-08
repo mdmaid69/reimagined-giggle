5db545e8c191122b7665f8bb44bce32613787d78
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:15:56 +0000

    wake each switch shoot spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import sqlite3
 conn = sqlite3.connect(":memory:")
 c = conn.cursor()
