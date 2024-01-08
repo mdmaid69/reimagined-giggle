894074b25304a039db1ff51b971c48767738c7dd
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:40:07 +0000

    matter life ashamed couple well

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def copy_file(src, dst):
+        shutil.copy(src, dst)
