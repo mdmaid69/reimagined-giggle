95047f9719b59a0b39aca50018ea3c4aa3e7ca12
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:12:12 +0000

    plan let yourself clearly against

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
