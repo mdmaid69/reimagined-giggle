7d50d160c7b23628d6dcaf44b702a53e2ddd9f9d
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:29:55 +0000

    central anyone thin temperature repeat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
