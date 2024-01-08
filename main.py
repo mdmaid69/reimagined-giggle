0c2b4fca695925f450b69d4a96535f1c9ce59d22
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:38:47 +0000

    takeaway grand combination it negative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_list_from_array(array):
+        return array.tolist()
