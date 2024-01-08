a7f07713cd46f57811524af70a0c11590865b7ab
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:04:34 +0000

    past free cheek dear now

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
