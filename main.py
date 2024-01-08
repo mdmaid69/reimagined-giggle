bb961cf348ee057a21364981b8232d9262c8647f
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:53:19 +0000

    centimetre personal spare speech along

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def square_number(x):
-        return x**2
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
