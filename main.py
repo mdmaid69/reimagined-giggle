826c87fdb728ef5938047035dc6ee0a762046517
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:07:21 +0000

    key line speed hold rain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
