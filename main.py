29e16e553842f1e94ce4db076b905489033588ec
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:25:50 +0000

    final heavy clue design list

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
