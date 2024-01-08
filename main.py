a26b8d7aafe4bf44bd3ca3e8564f172786def3fd
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:47:32 +0000

    eat no fault turndown wave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
