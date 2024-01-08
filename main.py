ac041a23b27f86092d4b80ad33eee65ee472284e
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:31:00 +0000

    rescue again land stand statement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
