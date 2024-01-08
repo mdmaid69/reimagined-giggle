596b0e59b1ff3b0c02db53c45492a76c20e44590
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:32:23 +0000

    concentration fall grow us bread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
