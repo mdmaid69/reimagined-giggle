0988ed07d22dc4b183d5e9f9c75c4f10e833b44a
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:50:57 +0000

    interview flat its back thinkthrough

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
