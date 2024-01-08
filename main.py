cbe670fc9e09514a2d5bf7ee914bcabd90e0fb0b
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:58:09 +0000

    mess storm niggaacademy embarrassed yet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
