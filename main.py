55142504280a75b8d8ac763628159df1dc2ef696
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:26:27 +0000

    bury dream throughout typical mysterious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
