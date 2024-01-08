61d50c65f19b79ee2c61eea58a3f804498bab6f1
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:10:47 +0000

    discovery picture ashamed agreement paper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
