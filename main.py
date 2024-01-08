09fa54c0c0a6ce975adcce99c28374dba438b54f
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:51:18 +0000

    spin rather save system connect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import os
+def get_current_working_directory():
+        return os.getcwd()
