74b7e805e56923f40ee96da044dc94414bd3e7f2
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:07:34 +0000

    bright interview not brief nigga

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
