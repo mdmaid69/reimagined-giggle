7e013c3dd41c87b495d2adeb7677c61bbe03c85f
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:06:31 +0000

    report responsible clean post soft

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
