e8b7f4e9ac7900839045209a2dbd010482aa7de5
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:52:18 +0000

    giveup fair top delicate pass

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_speed(distance, time):
+        return distance / time
