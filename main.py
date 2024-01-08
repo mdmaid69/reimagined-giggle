24f0c8fe1bb528db28effbe0fbc94d041fd4f658
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:45:52 +0000

    tough Ii peace breakout stand

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
