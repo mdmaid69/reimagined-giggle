37ff95896e6ced2a90a87575880ef27436e18f54
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:50:37 +0000

    ride suit simple Gg opposite

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def cube_number(x):
-        return x**3
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def convert_to_hex(n):
+        return hex(n)
