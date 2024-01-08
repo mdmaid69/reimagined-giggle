f882bcad4270adf54f13dd40f4a26d0c7ca9e3d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:36:07 +0000

    mistake marry apart list splash

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
