c9ff490e481cd85b360d9f0a6ce13b3a8ae8f28c
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:34:59 +0000

    should shall rain instruction land

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
