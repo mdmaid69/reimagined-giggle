daff9c36e5de3d38d8a7442d6e4975837829848e
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:51:54 +0000

    plastic part although soil if

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
