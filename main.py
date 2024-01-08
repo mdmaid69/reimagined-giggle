da5ebe55679f10266e03f520176e0c55c9321ffd
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:57:33 +0000

    regard interruption contrast fit occasional

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def convert_to_hex(n):
-        return hex(n)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def is_palindrome(s):
+        return s == s[::-1]
