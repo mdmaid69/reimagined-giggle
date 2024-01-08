afe83f2397ccae452d47eae7da8fd25dcb746c5f
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:08:07 +0000

    platform reply shout otherwise weekend

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import platform
-def get_python_version():
-        return platform.python_version()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
