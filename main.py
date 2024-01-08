8639c6247a27f0cac0f1364337d54d8c71f57c2d
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:56:20 +0000

    no pay show Kk real

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
