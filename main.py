3460a52a5034411f4cf4a0e73262797e236ba8da
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:06:40 +0000

    deliver development sing damp right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
