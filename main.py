b1587ef124d51935eacf89848d91b7ece9e98308
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:10:26 +0000

    adjective just regular mistake order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
