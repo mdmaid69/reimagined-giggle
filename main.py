f130867cbbdc19993a19e67b4ffcb9d30168cbd8
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:10:53 +0000

    address food beginning satisfactory single

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
