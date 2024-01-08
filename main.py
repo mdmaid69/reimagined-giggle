00d1c712fc52d454e1a3b676454557e30c7d1069
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:19:02 +0000

    thought powerful achieve roll centre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
