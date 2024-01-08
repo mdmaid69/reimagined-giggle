38c43610c66d908fb1c01dcae7342708df2012c5
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:49:01 +0000

    tune exam yourself programme through

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+def count_elements(lst):
+        return len(lst)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
