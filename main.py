acca429ad96230b0f3f041a75e742164f8da874b
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:42:03 +0000

    combination pick only sell suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
