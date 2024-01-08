d9989eda4526e8f1dfd520215c3652862ee900d4
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:19:54 +0000

    breakin keen mistake we organize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
