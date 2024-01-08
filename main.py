39b675a7db86c24a61421266821983f821fe3fe0
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:38:00 +0000

    figure record exam illness night

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def count_elements(lst):
-        return len(lst)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
