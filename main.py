f0bbbc6f0bbe2139afa3da0b5d6774e12cd07a7b
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:42:33 +0000

    number butter conduct never active

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-print(math.pi)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def convert_to_binary(n):
+        return bin(n)
