6c2afe4477a4dfc07e5157e86a8d08bb191c5304
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:35:25 +0000

    search deserve taste again thing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
