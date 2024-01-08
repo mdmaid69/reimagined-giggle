f9ba21c0c172c535de9ca627e69f80caae7b6546
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:24:40 +0000

    slice justice autumn keepoff bringforward

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
