85b939153460f31d800c2dd2651b97508200a738
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:14:12 +0000

    cruel imagine excited holdon behave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def sort_list(lst):
+        return sorted(lst)
