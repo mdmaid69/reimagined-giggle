28dd704273b3d3e60997fcafa655fe5a2041c3ef
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:40:39 +0000

    black emphasize mixture maybe long

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def square_number(x):
-        return x**2
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
