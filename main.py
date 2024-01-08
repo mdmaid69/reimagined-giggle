53b9be6b576bebf80c9a1d58cf923f8f0e07d226
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:07:28 +0000

    pencil quick motion accordingto mix

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import datetime
+print(datetime.datetime.now())
+print(sum(range(10)))
