2b24e0f51e1e767f94e8cd20dbefcff0bfd945d6
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:15:35 +0000

    block harm close prize strike

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def count_elements(lst):
+        return len(lst)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
