2802c835b478aaeaac77c7078d5957d71303e342
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:03:49 +0000

    tongue plain rank impossible perfect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
