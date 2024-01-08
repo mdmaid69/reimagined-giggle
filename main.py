3208f65a88c5e9f170b885194272f0bb4b41813d
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:14:29 +0000

    fault typical holdon on phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Characters:", len(text))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import array
+def get_array_length(array):
+        return len(array)
