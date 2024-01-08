581e5824ed9db0a042c5068d3cea997c554b6587
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:23:43 +0000

    day each be throw need

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_speed(distance, time):
+        return distance / time
