5b0da395a54de48a5c3ad3ac4107f7e611866e71
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:49:26 +0000

    crowded consider pride city tired

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_list_from_array(array):
+        return array.tolist()
