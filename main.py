b473fc9881d7843de2b8c3afd4b9cbfd2a55f114
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:38:04 +0000

    experiment act name night knowledge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import array
+def get_array_as_float(array):
+        return float(array[0])
