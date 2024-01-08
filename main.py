2d7d8b60e0d79dd512014e98050e7b8f902bf57b
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:45:47 +0000

    uniform loud amount ask matter

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
