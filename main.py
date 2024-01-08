a5fabefeaff9947a047e9e955f756c103808f087
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:28:56 +0000

    wonder fact offend disapprove action

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
