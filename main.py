e91a68daa118475e93084ff0b1f8de583c7a8064
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:25:16 +0000

    favour Cc thick callout hardly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
