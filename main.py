84901685279801c2e0ccc38150626b9c090ad794
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:22:12 +0000

    thankyou nothing distant minimum train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
