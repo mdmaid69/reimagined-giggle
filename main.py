ffb34b4922260fa240482520d597a384ebd4de05
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:45:02 +0000

    right chief wish afterwards idea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
