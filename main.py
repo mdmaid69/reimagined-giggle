400819eb6932ab045565153b62ff899bd351db24
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:32:24 +0000

    like safe complete hear year

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
 import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_as_bytearray(array):
+        return bytearray(array)
