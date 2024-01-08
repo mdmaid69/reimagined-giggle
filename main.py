d45bdf428657daecacf8edcbae8a064f72f6a083
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:37:40 +0000

    whenever command time declare previous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
