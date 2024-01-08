f0424662bb4f7cdb1055ac590dfae6b03cfe9594
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:05:12 +0000

    sky port extremely way dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
