986b901a39225bc9bb48181ae65341f500024720
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:20:29 +0000

    fashion major medium like before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def square_number(x):
+        return x**2
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
