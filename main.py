dcc31c71a5126c7508f8bc5b6bf55e938ce56d87
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:19:00 +0000

    square development evening storm drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
