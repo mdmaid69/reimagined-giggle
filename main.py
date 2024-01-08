68312343ef272c7e2f780a3365c584dab2d42304
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:34:01 +0000

    ring bet congratulations through pair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
