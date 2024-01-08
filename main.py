d8e8e3ea11cef8708fadeb0c6febfd57639f375e
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:20:53 +0000

    opposite drive angry simple journey

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
