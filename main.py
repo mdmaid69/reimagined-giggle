19232b360f4629137e89cb63fd4fd5faa691ad60
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:12:52 +0000

    month even news few type

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_tangent(x):
+        return math.tan(x)
