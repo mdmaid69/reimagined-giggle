e4c9594a698547a29e562b64aea2ddc2d1be0140
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:49:59 +0000

    book move float report sore

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_item(array, i):
+        return array[i]
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
