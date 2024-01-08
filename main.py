d0f9aa85225af6d35821aa517b6003aff928a499
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:00:11 +0000

    congratulations future out clear promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
