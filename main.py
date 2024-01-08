4865deff1035e00ee02f949f7fd997ed9f6249bc
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:53:34 +0000

    position shadow surprise car drop

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
