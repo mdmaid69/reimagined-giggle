0da6bb78e659c597b43e41319aa83c79e280cb24
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:13:21 +0000

    better rare opposite Dd downstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
