2cff99cedb4a4bff00017eb612950ba922c1e8ba
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:47:04 +0000

    feel pride conclusion callup whoever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
