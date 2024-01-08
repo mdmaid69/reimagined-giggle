5623307527c18f16413dd5ed31527eb818cc7a1d
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:58:24 +0000

    press matter ought rid thing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  def convert_to_hex(n):
+        return hex(n)
