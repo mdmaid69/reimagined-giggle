de5b59a90b5f7b8821551a0132673e3ef6f91d49
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:38:10 +0000

    increase by rather wide your

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
