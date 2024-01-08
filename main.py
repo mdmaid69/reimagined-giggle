7d29ac8a14a572ab880c697e1df8b0b727a65f54
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:45:39 +0000

    eat stare break light judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def remove_directory(path):
+        os.rmdir(path)
