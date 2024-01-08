db45076ed2fe8bd7b45223ac7fbcea56ff8bba3f
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:11:56 +0000

    occur regret substance stamp lay

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def add_numbers(a, b):
-        return a + b
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
