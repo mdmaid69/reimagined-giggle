5af403af5e831ece2abcbf5a23772ef19eace0e3
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:53:00 +0000

    tough smile attempt contain slight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def greet(name):
+        print(f"Hello, {name}!")
