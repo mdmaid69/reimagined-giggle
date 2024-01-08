1c736854f06f9115896cbf9960fdda00d82ff94a
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:46:50 +0000

    shut children far turnon save

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
