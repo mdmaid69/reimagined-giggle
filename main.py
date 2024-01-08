3422cee77db274bd8cae06bc95a334a7e28b4a25
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:52:56 +0000

    seat smoke thing before promise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def find_max(numbers):
+        return max(numbers)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
