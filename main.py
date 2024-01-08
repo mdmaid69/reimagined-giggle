14471f5a767e08c09b812713140e534e104a5cb0
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:54:29 +0000

    sea angry say later check

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def multiply_numbers(x, y):
+        return x * y
