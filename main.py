3b78ffa1705e12717f4aac7e3f4fafe211792789
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:22:27 +0000

    powerful waste morning live man

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
