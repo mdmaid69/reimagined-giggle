8c9671269b7027ae533b3fdb7528b8593f2f4d05
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:08:23 +0000

    grade turn able stranger speech

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-print("Hello, world!")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
