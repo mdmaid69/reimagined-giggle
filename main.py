dcd0ebefef58447b70297a0c72f485c46bf9d0b3
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:41:26 +0000

    narrow keepup pin search sensible

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
