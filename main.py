39673df92c408ed39032ef1f1046bff44aaa2b48
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:26:17 +0000

    war father us dance between

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
