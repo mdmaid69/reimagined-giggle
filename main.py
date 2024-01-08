f79e72d7f3f8e860d1969230d1c999eba5d0128a
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:53:52 +0000

    thirsty emergency age morning tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
