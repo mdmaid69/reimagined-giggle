d2924fb4f41f93372de81945430b45fb58483023
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:23:20 +0000

    beside month turn space on

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def convert_array_to_list(array):
+        return array.tolist()
