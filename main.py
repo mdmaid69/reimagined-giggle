6d92cf1e5fe61c8ed26715f3d341ec07af8c936b
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:46:51 +0000

    do production somewhere fit responsibility

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def sort_list(lst):
+        return sorted(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
