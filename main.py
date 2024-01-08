86d6590639d5b447270efcfb2d76508c568367b7
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:01:03 +0000

    direct splash clear experiment centre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
