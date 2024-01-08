63ba0bddcf15c531e229524d673066448f07ef79
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:53:40 +0000

    somewhere various area fit keep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
