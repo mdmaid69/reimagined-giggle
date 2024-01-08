c3b3beb1c2bf7ec1e365b607c6d8200a97998ad5
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:38:51 +0000

    various consist tap danger room

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def cube_number(x):
+        return x**3
