28b6c43eec8268d8bcaa9d094836cd86c1b005fe
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:55:45 +0000

    name debt centre consult cruel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
