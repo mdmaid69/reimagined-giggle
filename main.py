1ca682371cb80b3718244319d1fd9a5e50ee9910
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:48:40 +0000

    check runaway priority demonstrate perhaps

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
 import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
