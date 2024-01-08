8c1da6bb82204d25ed91d903a1c01e1143e75803
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:49:57 +0000

    plant fetch advise inside set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
