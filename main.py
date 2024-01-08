de46463e70e8b774ea65547fe5ebdd11e79b0536
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:23:12 +0000

    takeout idea nowhere true here

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
