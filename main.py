93246db2a790b232619a7d66a293a24441c6a984
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:03:59 +0000

    level demonstrate road include wind

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def add_numbers(x, y):
+        return x + y
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
