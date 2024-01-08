3714b439065b288af8d7b2d543cbda5330bb7a4c
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:17:42 +0000

    soak boat block wrong flow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_directory_name(path):
+        return os.path.dirname(path)
