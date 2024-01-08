a0b308fdabb297ec738298b286a1cc3385075275
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:53:36 +0000

    not must live leave open

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  def subtract_numbers(x, y):
+        return x - y
