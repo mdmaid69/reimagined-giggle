fcb34fec970969383667180e28cfd610464bbe30
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:17:59 +0000

    habit day joke mind wrong

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
