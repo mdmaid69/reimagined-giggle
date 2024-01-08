cdaca4a8a82c9a8c92b3e3684298396b260a9ccd
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:27:15 +0000

    involve trip deliberately cheap scared

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print("Hello, world!")
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
