89560a17454d58cbc4eaf1cc9857800d8692a5f9
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:37:07 +0000

    influence translate choice correct moon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
