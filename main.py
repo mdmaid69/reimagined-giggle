e9397693405aa6b96b23e0dc59090ea7e2afba21
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:27:08 +0000

    platform only kind arrange within

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
