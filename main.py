02c734e2f189d6064792d0d5f85fe3685d54d914
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:06:23 +0000

    after suddenly reach pause desperate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
