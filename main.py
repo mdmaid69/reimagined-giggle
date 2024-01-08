23c4417397976e4e82c59d27f982bd633dc9fddf
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:48:37 +0000

    add clever generally fierce Ff

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
