d8d5e0aafa1a6aecacdc6d76ca10313b4bd70c42
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:56:35 +0000

    large apart few stir lost

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
