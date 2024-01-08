cf047a1f3cf9b44fec8f5f08c93ee8f9be477a38
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:39:52 +0000

    there Ss injury remark slice

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
