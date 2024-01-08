34d8f0db1d0d019e95255b851c1212347b88b532
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:04:16 +0000

    vanish home pronounce quality mistake

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_min(lst):
-        return min(lst)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
