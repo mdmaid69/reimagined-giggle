54b94a29c47d76e77d1997c79ca1a62566d47602
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:45:30 +0000

    pen mine can fault leave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
