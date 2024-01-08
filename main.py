bbf465f2cecf879b3ee7b290312936c01603c752
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:14:08 +0000

    fine sight trade declare breakfast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
