fb027dfb35d30d548e410024760655c38fd79233
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:28:19 +0000

    war example unkind pencil quantity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
