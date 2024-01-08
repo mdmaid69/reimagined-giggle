729c19522e64a560f68f6ee0b3022b512c95f3dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:37:14 +0000

    limit foreign Cc bush land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
