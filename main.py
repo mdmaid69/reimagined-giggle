7b17c2e991bd005e36f465f3f55c652a6f73d216
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:48:02 +0000

    cool wipe truth refer interesting

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def convert_array_to_string(array):
+        return array.tostring()
