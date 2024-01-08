afce4b7bc16fff892abcc52045aaa9ce87ecb777
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:26:23 +0000

    occur across meaning cross answer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def count_elements(lst):
-        return len(lst)
+import array
+def get_array_item(array, i):
+        return array[i]
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
