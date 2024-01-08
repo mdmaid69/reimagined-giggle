dd39738a67e758a444d258273e27b10a080ec1c9
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:02:49 +0000

    firm worth pay car group

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import random
+print(random.randint(0, 100))
+def find_max(lst):
+        return max(lst)
