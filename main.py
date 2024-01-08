07c76e17fb6c6adbe796ab81ae097878282bfbf2
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:28:33 +0000

    majority catch calm divide undo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
