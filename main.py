933d0a5e3084804934ad9debde20186b11a9dbb7
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:29:29 +0000

    clear Ww man dull hurt

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-import math
-print(math.pi)
