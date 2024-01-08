4993d96ceebfdf449c1c7cf84b7242ce1855ffb0
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:37:48 +0000

    grow share sense hard live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
