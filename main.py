71f56db6382de393cf1736616194371a064bb4ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:03:34 +0000

    own persuade neck major interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import os
+print(os.getcwd())
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
