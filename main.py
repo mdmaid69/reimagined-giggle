3e9b2039fe144c0d8e980fe38349b93c3a0cbaa5
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:02:29 +0000

    agree nose past about affect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  def remove_duplicates(lst):
+        return list(set(lst))
