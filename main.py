9bcc5d256e266f712c4caacb2fb821a40dad054b
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:04:19 +0000

    hungry giveout age last smile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def reverse_string(s):
+        return s[::-1]
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_sign(x):
+        return math.copysign(1, x)
