8bb22a3ae955eba83df83631bb2f373c8707ea2e
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:53:43 +0000

    female rush produce bringdown Ww

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
