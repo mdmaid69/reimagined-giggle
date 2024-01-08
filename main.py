3cb63df7e13881701a39e63c8136268d9013a048
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:55:40 +0000

    have generally dream Ff listenfor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
