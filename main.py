d2cc0d556b1f9df7322b95a4b468445e3125b64a
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:09:43 +0000

    approval fat us phone song

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import sys
-def print_python_version():
-        return sys.version
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
