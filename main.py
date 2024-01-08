97f61a87623c767178edb6fb8f8fb5f5dc11d7fa
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:29:22 +0000

    pour usually answer late few

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
