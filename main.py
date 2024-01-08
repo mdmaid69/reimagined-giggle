be6677f840815ab07ffa3a1d28c1be041ace85bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:47:41 +0000

    frequent empty shout Kk whole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import platform
+def get_os_info():
+        return platform.uname()
