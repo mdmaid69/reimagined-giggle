155e3c29c2002fbdc946da2c9261886b01139772
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:36:31 +0000

    cold cut opponent steady alive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
