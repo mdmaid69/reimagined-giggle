a67066bc7e908a77c0895f325525862f7ce734ff
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:05:07 +0000

    better crash Tt charge past

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
