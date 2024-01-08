8e4e6e9f86a33e4fbdcd58f4b612ed38d3de5456
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:02:37 +0000

    attempt visit guess gift follow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  def reverse_list(lst):
-        return lst[::-1]
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
