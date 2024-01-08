2f7e8d095aa2dda1a58dc5a8822a3ad38b92786c
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:35:01 +0000

    live giveback light book rather

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import math
 def calculate_hyperbolic_arc_cosine(x):
         return math.acosh(x)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
