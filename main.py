540dfc1d0d5b7037fa66a2f226634fd9591614a0
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:16:48 +0000

    hand leave measure art dare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
