dd8b1f3bfb003577cc1be50ffb5d0d96b6b120c7
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:22:58 +0000

    separate professional sad software careful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
