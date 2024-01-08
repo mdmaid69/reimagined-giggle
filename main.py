55cb365e18cc91976fe732de570af94259b7e8a4
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:29:06 +0000

    turnon pen separate sore unable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-print("Hello, world!")
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
