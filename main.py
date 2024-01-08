7be2270ba0f31b4f304feef68c298f2921027e6b
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:46:17 +0000

    surprise blind nonsense crisis image

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
