79a336f4c716bb3cdc17f2fe9f3f446c9741ed16
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:17:49 +0000

    variety change terrible formal home

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
