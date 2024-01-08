11e36ffb388245ce2a02d00654b74b75f00c069d
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:02:43 +0000

    software chief roof shrink practise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
