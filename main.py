797a88161dfe757a2c13c7c5b5a1f539df9d135f
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:51:48 +0000

    sail shoot quick glad government

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
