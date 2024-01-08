4fb22071d66d5d92eae78e2e9710f9e5be292956
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:21:23 +0000

    comment wind dare refer side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def split_path(path):
-        return os.path.split(path)
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
