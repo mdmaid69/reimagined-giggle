5b22ba53ace67025f2ae8301940f5254ce901a83
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:14:34 +0000

    me agree hair tell strange

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_density(mass, volume):
+        return mass / volume
