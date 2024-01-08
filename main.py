c493bee3597c3f923ca52bb6cc91be6a5f9f439e
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:03:36 +0000

    loss sweet statement intend put

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  def sort_list(lst):
-        return sorted(lst)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
