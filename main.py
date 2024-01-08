e41e368876ba0852eabea1422f28b8476b890b48
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:17:05 +0000

    easy shock lie nature minor

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
