ae989f23191069a0cbd43e7a7becaf21f6a0ebd5
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:53:47 +0000

    until chance set defend face

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
