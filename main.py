1598ffafb29dd164388ea99b6cae221759b60527
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:55:15 +0000

    poor same tip protect taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def cube_number(x):
+        return x**3
