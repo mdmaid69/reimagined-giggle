2efdf15df675fe155793f154bb75a912257a2791
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:46:52 +0000

    wet favour conduct correct shop

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
