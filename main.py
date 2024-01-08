c0689a61c9d01415d42fbcae144afef307531cb0
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:14:39 +0000

    comeup still carry would our

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_list_from_array(array):
+        return array.tolist()
