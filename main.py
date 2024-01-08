99f5ca6016969bbd46040fb2416ccb0850434849
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:59:19 +0000

    law negative ready crash contain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
