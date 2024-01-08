2a75c2cd34424a1d62cf8c4e1e4afd5f76ae9f00
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:42:22 +0000

    shut himself offend tour neck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
