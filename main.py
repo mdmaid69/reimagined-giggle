e9a7dd67146fa48b0aacb05ee8d6ec8cfc9a7553
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:54:43 +0000

    beat town although communication equipment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
