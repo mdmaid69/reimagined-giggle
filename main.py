aae4db7492ad15a1363f3c8e23a56dea5f7c9339
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:36:00 +0000

    fork drink proud everybody him

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
