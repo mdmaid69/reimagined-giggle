240e1f8351e48caa0eeeb06ab7280c278e89c150
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:13:55 +0000

    rough quickly bury me family

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
