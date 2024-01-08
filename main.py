31bc89c9dabf484258f83b9a42d57e459bf74467
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:37:10 +0000

    guess love serious clean square

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
 import array
-def get_array_item(array, i):
-        return array[i]
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
