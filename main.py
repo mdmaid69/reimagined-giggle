19041e3e6d700c8a6a6030de5b90fd5f9c4d4235
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:52:27 +0000

    set rain complaint forgive valuable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
