47ff9e2f8123518bc5af762d6b92fd31047d48bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:09:10 +0000

    calloff hair spill comment tired

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
