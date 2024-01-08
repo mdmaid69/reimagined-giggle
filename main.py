a23cc06e635fe424b42619971b3f0ad623b167e1
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:06:37 +0000

    hope possibly wind land light

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def clear_array(array):
-        array *= 0
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
