4e394f2c12e70d309d269a455606f7993a10b684
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:49:26 +0000

    foot organize amount while unlucky

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
