76fbda527970095035caa08f0c1473ab7a0eb4a7
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:53:54 +0000

    record perfect cause refer another

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
