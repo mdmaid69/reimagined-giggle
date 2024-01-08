aaa8211caf27ea6c1d2f4d35dd29333444c710e2
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:20:13 +0000

    why can degree fashion treatment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
