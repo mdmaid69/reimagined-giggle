3eae08b3b653646ef97e192b6d8fc93edfee05c2
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:11:53 +0000

    force frighten through anxious relation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
