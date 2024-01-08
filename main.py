ee7c5feb6bfa41a9bca1434c7f31496f4657a6d8
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:12:15 +0000

    city experiment certain runinto week

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
