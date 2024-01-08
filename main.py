f923187a9899e20cad6d714bd1feb79de45279d8
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:11:57 +0000

    intend broken lookback(on) drink describe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
