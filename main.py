dfa5c134b51968dec8bb9c48780a563e84ea816b
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:21:59 +0000

    571enough decide bet snow line

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+for i in range(10): print(i)
