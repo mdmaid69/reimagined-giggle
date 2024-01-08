d1dbfd89b71b48ea30659208e8669003c61e86f7
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:17:41 +0000

    572enter moral free mistake inquire

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def reverse_list(lst):
+        return lst[::-1]
+def convert_to_hex(n):
+        return hex(n)
