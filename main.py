cb3ee8fd2d44b8c730787a9798ad6b6809372959
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:45:49 +0000

    thought who lucky cruel involve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_odd(n):
+        return n % 2 != 0
