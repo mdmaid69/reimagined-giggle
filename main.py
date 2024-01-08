500d17c8c2a99e3ea910b2aa3aa560918b5ea5ab
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:39:56 +0000

    find probably gas demonstrate bang

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
