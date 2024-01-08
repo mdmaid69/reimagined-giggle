0769988007ae805f937676a9def78e1c2f1c6d2c
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:56:45 +0000

    wander comedown loose picture fortunate

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-print(sum(range(10)))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
