c2ead79379c7028bc9caf7788f5f173b913e3a16
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:31:25 +0000

    cool boat present label burn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
