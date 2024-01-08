6ae42952db23ebda8df11b728085f78a714b9993
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:24:11 +0000

    crowd interesting important develop hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
