65099e5bc17fda8b31f90ead81b61b4fee31e7ef
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:37:45 +0000

    discovery circumstance measure beside brush

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import time
-print(time.time())
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
