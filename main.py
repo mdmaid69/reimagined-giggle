9f00e93a26a763c9f231793b2235b4434f8ee8bf
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:59:37 +0000

    more makeup ordinary wise extremely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+def calculate_work(force, distance):
+        return force * distance
