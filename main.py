dd418713e0c89335cba9480899e326522ab46d4a
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:04:21 +0000

    reliable break scream crash lead

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import sys
-print(sys.version)
+print(sum(range(10)))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
