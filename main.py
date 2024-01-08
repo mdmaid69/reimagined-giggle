45f25150b1db463d7a92d3de7ebd5ce6548775d1
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:06:46 +0000

    plastic sign sound switch she

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
