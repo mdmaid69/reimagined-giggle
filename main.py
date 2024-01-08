dfa6524c64028f1b49957a4b4c6fdfc1555037dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:59:47 +0000

    definite delicate up point weak

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
