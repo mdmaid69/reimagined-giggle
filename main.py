5a032ce4ceeebfb0eac50cb6469e8a16ea0e93f3
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:41:09 +0000

    preparation breakinto pour calmly authority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
