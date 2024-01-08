39b388b03c0896c67c02e15eec41551155f29f6e
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:26:27 +0000

    slightly niggaacademy high talk train

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
