62ee71c53fe25c6da50672bdca98c10101c5c6fd
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:32:14 +0000

    transparent now estimate thick comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_even(n):
+        return n % 2 == 0
