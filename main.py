d4e2fad71725eb2d142cbb886d729735994527d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:45:33 +0000

    guilty cold wind worried afternoon

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import sys
-def exit_program():
-        sys.exit()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
