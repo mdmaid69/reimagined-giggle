440cc2fb64685221af605be956d44da1cdb25e31
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:46:30 +0000

    similar arm every lookafter noise

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
