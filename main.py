027ad7ec9bd093b434c9bf282e86c71649d157f3
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:52:45 +0000

    similar consult exciting demand stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
