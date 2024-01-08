213954804a9a1d48932635a85839f0bb9bb55519
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:31:55 +0000

    moon cinema indeed fall ignore

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
