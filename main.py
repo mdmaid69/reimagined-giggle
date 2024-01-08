0b0fa137d324a54ee9b2b8da649400fc12ad233e
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:22:34 +0000

    deserve arm left blame tonight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import os
+def change_working_directory(path):
+        os.chdir(path)
