5555cadc10e67a390569276cf88acace7ec33cc2
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:15:18 +0000

    interruption tear set hard unlucky

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def clear_array(array):
-        array *= 0
+import tensorflow as tf
+print(tf.__version__)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
