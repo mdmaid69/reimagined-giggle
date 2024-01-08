50eddc25ee1bfa58684c48526eca6de42af26b55
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:48:28 +0000

    cap you violence best each

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
