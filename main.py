bad79fa11321cca34cb048e5898c9f3e32a24e88
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:08:01 +0000

    whatever yours offence lost suffer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
