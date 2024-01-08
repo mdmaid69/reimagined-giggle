97caa7dc95ea747714a68c1a73204e65c307589a
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:42:24 +0000

    invite begin rich always owe

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
