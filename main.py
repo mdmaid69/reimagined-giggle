4f3e5fea34befd414d3b9860414ddc6b288d6fbe
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:19:18 +0000

    safety during direct protect pride

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import csv
-with open("some.csv", "w", newline="") as f:
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
         writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+        writer.writerows(data)
