54cdd1b1acf9706360cd628ca5a0f465c9e3c3cd
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:56:27 +0000

    takeback wild discover sea grow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
