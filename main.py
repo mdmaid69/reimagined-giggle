d31655445bdd61fd1e0c79a55371a315d4e04c11
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:06:02 +0000

    manage uniform straight boat unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def get_array_as_bool(array):
+        return bool(array)
