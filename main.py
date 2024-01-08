b1d54d6681dc65fb5ed2f21847fcc8acf8e2ec7f
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:42:16 +0000

    destroy everybody situation form cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+from collections import Counter
+print(Counter("hello world"))
