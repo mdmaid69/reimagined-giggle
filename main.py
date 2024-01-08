0b1f25be5915f852721201f466a3400087c73903
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:33:39 +0000

    may treatment corner fit side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
