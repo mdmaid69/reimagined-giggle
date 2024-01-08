593ec08cf61b5d9851da979af3fc5c1806cf0f89
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:27:51 +0000

    himself wrong excellent bound side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
