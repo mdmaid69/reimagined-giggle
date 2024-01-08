8c557b2355ef7742b475d9fa92a039eca01b56c1
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:59:54 +0000

    stop improvement branch dry angry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_speed(distance, time):
+        return distance / time
