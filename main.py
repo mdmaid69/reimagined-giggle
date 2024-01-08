fc3fa85adbf3b4d8dd810276761c386bdf550660
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:08:28 +0000

    accept platform Gg front considering

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
