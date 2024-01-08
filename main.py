d692dcc4fa56a0e477e80f6c0c71c672dbcc38d2
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:37:40 +0000

    word treat holdout delight might

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
