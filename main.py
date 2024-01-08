ef7986ba9a9f1efe6ddc674d3e929b057e563a1a
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:14:45 +0000

    female giveout here balance reach

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
