883bee46351d16ce1b939cb408a5320531130c21
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:31:40 +0000

    loose confuse when at later

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+text = "Hello, world!"
+print("Reversed:", text[::-1])
