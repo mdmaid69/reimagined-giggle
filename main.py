74518a15b400a34e5df810942f8b0d8c668d0020
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:58:27 +0000

    stand transparent connection trouble if

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
-  def square_number(x):
-        return x**2
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def subtract_numbers(x, y):
+        return x - y
