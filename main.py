0f40d4bbe97d9de1e09033d53ebdf50d2f926c06
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:49:56 +0000

    knock everyone pair tend around

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_user_list():
+        return collections.UserList()
