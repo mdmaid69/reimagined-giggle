62e0c7c1a1466ae61589b2ee21289ee26938812e
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:54:23 +0000

    instrument side whistle tooth pen

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
