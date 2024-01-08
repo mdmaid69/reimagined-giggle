aa54bc74cdb1e394f2091c8eaddfdbf34117095e
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:16:03 +0000

    complete suck though mind back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
