5bc81838160ceff129cb04bad1304fc52d255e07
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:17:13 +0000

    father bringup pollution circle like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import datetime
+def get_current_date():
+        return datetime.date.today()
