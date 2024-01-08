ed5a1ddf1934d75bd9ee9071a01f93cf28e662f5
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:52:33 +0000

    big respect disagree prize embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
