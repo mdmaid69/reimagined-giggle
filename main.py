bae8d531f49e8b6fc5a4a0d0d81a4ccd4045d740
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:32:55 +0000

    exam thing failure tall strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def clear_array(array):
+        array *= 0
