f86ed0a5fbd55c027fa19f77fa0ca27f1171b6ba
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:20:35 +0000

    experiment need scared carry medium

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
