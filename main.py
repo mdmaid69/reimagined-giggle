5335b1943e364f51460f70fc5e24a3dd13733145
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:15:01 +0000

    war raw force exact fit

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def convert_to_octal(n):
+        return oct(n)
