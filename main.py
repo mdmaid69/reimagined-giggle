e5043423e2edd0e9b67fbfb113e81a6cf5e65f10
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:10:15 +0000

    in lookup meaning union relationship

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import random
-print(random.randint(0, 100))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
