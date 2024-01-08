d5608759c9d85d27619185afc03ba25f98061204
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:06:04 +0000

    interview repeat once offer future

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def count_words(sentence):
-        return len(sentence.split())
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def get_array_length(array):
+        return len(array)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
