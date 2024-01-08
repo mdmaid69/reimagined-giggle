62e535abbeb7c227b3531b02b6b675a458c5b631
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:01:28 +0000

    shall pass mysterious night declare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
