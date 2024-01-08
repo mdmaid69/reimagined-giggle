dfaacb81da3208f3c4bde7c653fa91a14c48be14
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:26:19 +0000

    judge doctor scream voice you

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
