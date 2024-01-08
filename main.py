d4d1c474ec3d67bdcaa0885b4e1416d6da3895d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:23:07 +0000

    tip pattern just organization public

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_union(list1, list2):
+        return set(list1) | set(list2)
