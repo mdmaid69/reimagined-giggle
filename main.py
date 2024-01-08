5be0f340d252805e9f04c938e010f1c0b32580e2
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:16:38 +0000

    ago would meaning lot basically

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def convert_to_binary(n):
-        return bin(n)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
