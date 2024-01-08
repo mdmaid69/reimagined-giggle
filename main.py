c57fb3bb0d9665e36103d56c69439c36c6f673f8
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:15:40 +0000

    Kk see makeinto nice team

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
