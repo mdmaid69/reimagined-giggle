7ea1989c2a378429834233d2e117325edfbe820c
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:03:36 +0000

    plan fire majority arrive brief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import array
+def reverse_array(array):
+        array.reverse()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
