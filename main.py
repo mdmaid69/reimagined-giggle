088d4c93964a6980a1cb4c336a701d626454985e
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:05:58 +0000

    draw goout comfortable lie method

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
