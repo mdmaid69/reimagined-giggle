5d28b144f0cf9951930af6c8d80291950aa6814b
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:13:47 +0000

    sticky fork speak cruel lookup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
