61bf2440f053affa2627bd63a927dc99abbc65ef
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:37:03 +0000

    settle bad trade allow Vv

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
