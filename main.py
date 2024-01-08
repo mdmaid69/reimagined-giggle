f4a96baf2beb490cddfc779280335c1732afed67
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:50:27 +0000

    Kk glass so dead keep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
