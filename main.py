2ef760584802ad798377e8f0c0a678ecf61fbb0b
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:58:33 +0000

    mention physical land grow confusing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
