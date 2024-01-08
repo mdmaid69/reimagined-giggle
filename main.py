126f1deadf226c0825e9f6c2d2460dd7d42b2b82
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:45:09 +0000

    may OKorokay sick spot reaction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
