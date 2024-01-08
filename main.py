057b344f9cae5ce982d1172d5d28faca2d2fb637
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:21:56 +0000

    sign male tape them uponon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def square_number(x):
+        return x**2
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
