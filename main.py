74dbc20a45c14a1119e9333eb187117ee842db79
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:19:46 +0000

    close corner feel before undo

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
