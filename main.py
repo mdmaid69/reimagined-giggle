758c40889d8ceca226f29dbb8186c2bf11651c5a
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:25:41 +0000

    live unite care brother mine

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
