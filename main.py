4ea99a8602f9f9fd2ec82ea4e819c3e67ea4fd86
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:30:46 +0000

    bear frighten no write scale

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def count_elements(lst):
+        return len(lst)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
