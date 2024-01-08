e64cadb6ba0ee8c2b770edd58556da1f7aba43d9
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:21:17 +0000

    mixture away bet frequent too

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def get_array_as_set(array):
+        return set(array)
