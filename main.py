a30f4f2df239631ffecaa3d97d0f9da27f1931c6
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:06:36 +0000

    drop hello that total abroad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
