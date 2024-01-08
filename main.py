3f064ff42e28806b2f3aada1e969000a0044f0d7
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:42:06 +0000

    place trip instant shrink concern

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
