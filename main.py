89d5d5f1e7e82080b1fc1908b1c8fcebc06aee68
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:50:43 +0000

    snow immediately view tomorrow raw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
