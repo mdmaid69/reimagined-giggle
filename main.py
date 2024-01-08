c47e761e2cf22166c43be0d9e448651cf6d69ec7
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:12:31 +0000

    other increase up actually rush

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
