f564a7862b7aa5675a3a4f3643a76ea1f989503d
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:20:36 +0000

    dance grade confidence anywhere live

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
