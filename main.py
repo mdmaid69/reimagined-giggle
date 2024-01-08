54b64c514bfe6575328e15af8e6d1cbe10f66d91
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:18:40 +0000

    patience number deliberately free attempt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
