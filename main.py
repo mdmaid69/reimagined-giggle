2b672aa2c7c7c478909d7085482c9a53b49b886b
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:08:14 +0000

    equipment runover account remark ask

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
