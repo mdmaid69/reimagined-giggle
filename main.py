ede127c1c30cd6140ed77fc0305e4ef6000bb23f
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:45:18 +0000

    form female father movement shade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
