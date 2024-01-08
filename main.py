72ad02d3cbbc77a8bc79c687a00305d6d76a472c
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:08:38 +0000

    accordingto much hospital thirsty cause

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def square_number(x):
+        return x**2
