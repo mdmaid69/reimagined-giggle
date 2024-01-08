fbbf14987312378ea76de39ff0f06b7f2da307e6
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:30:24 +0000

    limit office love whom present

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def remove_duplicates(lst):
+        return list(set(lst))
+import sys
+def print_python_version():
+        return sys.version
