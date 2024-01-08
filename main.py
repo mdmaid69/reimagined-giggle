54ca380e003ccd203b8fe7375c200feaa05a6c76
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:54:22 +0000

    single test apply practical rank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
