567ef6c332f2ea871509892b76ddc42a88af7e89
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:57:14 +0000

    here strange along care damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
