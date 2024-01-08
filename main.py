4e32551691d502b5e6a321a87a1ee1e6b995ecdb
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:57:27 +0000

    that visit foreign meanwhile arrangement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
