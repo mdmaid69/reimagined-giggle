cca2daa77e0c7fcba482456e2055f89fa39413df
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:15:39 +0000

    brush introduce ignore safety tape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
