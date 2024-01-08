c4178ca8e764fac3cf3f3e3440244071c89fdfd2
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:21:00 +0000

    choice consider news prize sure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_volume(length, width, height):
+        return length * width * height
+def find_min(numbers):
+        return min(numbers)
