de54de6be1cb38d0f52e0aaf39836eb430125f21
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:52:06 +0000

    realize quiet trap remove concern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  def is_odd(n):
+        return n % 2 != 0
