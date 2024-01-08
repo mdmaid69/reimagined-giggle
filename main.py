16d4afaec464e6e88166cfe258fd6c9414bf0caf
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:37:05 +0000

    sound black turnover check find

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
