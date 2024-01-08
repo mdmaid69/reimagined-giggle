9d26123453e21c91ac80adc359bcb080c4072ab2
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:25:35 +0000

    pocket turnout prepare force failure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_density(mass, volume):
+        return mass / volume
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
