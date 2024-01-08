b5d6b164146c3d24a7c199e73c3b751d521e6a3d
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:57:40 +0000

    second start appearance public promise

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import collections
-def create_user_string():
-        return collections.UserString()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
