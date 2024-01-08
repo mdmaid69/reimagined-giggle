94cf7fd6f14c31555aa9e231d5993fab8f7324fb
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:46:49 +0000

    reply deliver better godown path

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_length(array):
+        return len(array)
