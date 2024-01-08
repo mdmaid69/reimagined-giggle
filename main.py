0ca83c21febde936e1577d5061a12416564baa7b
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:15:35 +0000

    comeback base turninto healthy get

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def reverse_list(lst):
+        return lst[::-1]
