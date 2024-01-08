8a58c37c5a99b66f619d16b021da8455d4bcd448
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:48:35 +0000

    popular character respect event fast

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
