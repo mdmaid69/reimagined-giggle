3bf619f063ea9ef2a57258b4fef3bb32a89631e2
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:25:19 +0000

    square put fail practise field

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
 import array
-def get_array_as_list(array):
-        return list(array)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_length(array):
+        return len(array)
