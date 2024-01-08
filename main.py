3c5e03b85ada5b712e2978291332b6b39ec33419
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:25:22 +0000

    conduct send later platform suckme

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_area_circle(r):
+        return 3.14 * r**2
