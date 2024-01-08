822b69d295cca71e37327106824dc1f41ccaad9e
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:00:43 +0000

    getout year low giveup dark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
