f0ab6a70f21d482aeff0a478aabba946ceaa1867
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:34:52 +0000

    crowded collection appeal goback(to) meanwhile

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def convert_to_octal(n):
+        return oct(n)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
