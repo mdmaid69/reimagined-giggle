18c2bf50e099b2f053e1bfe5c086cfa67754fe60
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:05:31 +0000

    strict bound story type stir

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
