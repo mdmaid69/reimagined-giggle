5bef7abb8acc39436c3632b55466b5a8737cbcf5
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:23:44 +0000

    embarrassed better Hh post form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
