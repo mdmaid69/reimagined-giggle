f19b986f4846438e82807564986a9dc19c14d060
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:04:26 +0000

    excellent interested injury situation second

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
