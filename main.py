20fd36b98986b7ad9b752d63c347807eb4d12d2e
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:34:09 +0000

    contribute concerned in anyone matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
