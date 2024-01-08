62adb2bd08ff22bf603fd91615933d42778f6abd
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:07:51 +0000

    emphasis arrange sign exactly no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
