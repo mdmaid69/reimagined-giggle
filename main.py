8b80b5a3f78ce6366162562586c95358f6303163
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:06:45 +0000

    favourite sort admire language organization

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
