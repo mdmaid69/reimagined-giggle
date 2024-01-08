fb1d4fe19d5de02c1279fc811a391d050ff5f84e
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:32:57 +0000

    breakfast nonsense discover estimate properly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
