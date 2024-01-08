ce1bab8896b16e4fb29cf670b7f2c9102e739533
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:12:40 +0000

    last cut straight contain type

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
