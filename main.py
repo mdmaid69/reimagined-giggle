c41aa87471a8802d289716f5f7843cc8c571a8e8
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:36:36 +0000

    unusual collection business through emphasis

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
