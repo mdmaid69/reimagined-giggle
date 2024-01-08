7c00981e9a04c1fee5dc54cf9209b71e7c3f4b30
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:39:45 +0000

    pure march of pronounce usually

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
