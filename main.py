175bde80700d38b838755cc8f7696ffc2f58aac4
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:56:35 +0000

    obtain hard address failure spread

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
 import array
-def get_array_from_string(string, typecode):
+def get_array_from_file(filename, typecode):
         a = array.array(typecode)
-        a.fromstring(string)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
