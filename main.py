1039db9f946bd26e879aff7db555d53b6f0bbe24
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:51:26 +0000

    pain result necessary speech pretend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def find_max(numbers):
+        return max(numbers)
+def find_min(lst):
+        return min(lst)
