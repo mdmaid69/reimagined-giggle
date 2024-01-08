f311c010b9f18ec8fb03765a998b84499b197d51
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:26:37 +0000

    but tooth surface address activity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
