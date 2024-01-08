c1ad546d5ca4d627fb0aade9a6efb028b89e18ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:56:39 +0000

    country design pronunciation load person

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
