2a22c6ab3fa6eb876026d3db94b884c6e2ec9302
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:49:52 +0000

    official wash look doctor noise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-def get_current_time():
-        return time.time()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
