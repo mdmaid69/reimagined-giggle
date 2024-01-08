b7ee1b9423335f0d1e2c660cd891913a12787874
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:21:21 +0000

    572enter thumb dark because declaration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
