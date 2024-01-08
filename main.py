bdb53e5ffe732ea98d6955abd40e6a66d2083751
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:39:10 +0000

    talkoutof somebody so fire thumb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_speed(distance, time):
-        return distance / time
+import time
+def get_current_time():
+        return time.time()
+def find_max(lst):
+        return max(lst)
