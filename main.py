a4d6dc28d368bbd28e73e03f3246f265cdee6871
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:36:07 +0000

    nor respect part story floor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
