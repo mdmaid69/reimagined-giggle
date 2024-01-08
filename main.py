d313b302bc7e49c543e6302f1e1510a43f87a52b
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:18:50 +0000

    gate female low shop create

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
