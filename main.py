9f97cde0623cf5daa5c9250c9b74208ee8d99994
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:03:46 +0000

    backwards occasional turn arm house

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
