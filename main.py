8d4b2bc0876b4b2d8a05666e68898eb5d16ab17a
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:42:57 +0000

    recently equipment quality recommend scene

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
