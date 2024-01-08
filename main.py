219c2d6953c0b91397694d3e1e6be527df1c03b4
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:07:05 +0000

    lower deep greeting finish inside

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
