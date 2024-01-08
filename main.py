ba8ddfd5e3ccf435cc02e46e4f3af45169503980
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:19:15 +0000

    cruel rent material downstairs crime

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
