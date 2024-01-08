5f0832e69fb07ddf5d8b2e8f3f358f8907d2cd8d
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:19:42 +0000

    wake object recognize direct bush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
