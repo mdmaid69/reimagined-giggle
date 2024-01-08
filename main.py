1b51bda6f6f5aa3660d36a0e2824b43f3d7c477a
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:43:56 +0000

    variety suffer beginning takeout exploration

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
