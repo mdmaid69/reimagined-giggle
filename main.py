46195c8e5dd18f364eb8340ade383201d9f67c05
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:53:38 +0000

    against instead trip seem giveaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
