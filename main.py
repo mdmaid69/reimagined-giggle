86d7621d2619f6afa0a7e810da1f3f56921925c6
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:29:34 +0000

    actually fond supply supply bar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def find_max(numbers):
-        return max(numbers)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
