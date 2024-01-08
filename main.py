61c871468bfbe763213e40d6b4064d408183ed52
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:43:45 +0000

    smoke tonight time say introduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import collections
-def create_priority_queue():
-        return collections.deque()
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
