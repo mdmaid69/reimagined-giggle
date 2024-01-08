35198cda1b37372f7252fc8dc5a2227ab02fcbf8
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:19:19 +0000

    schedule lower street body dinner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
