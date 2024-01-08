fd9c45069fb978a75ae8792e09e4f22fe787eb7d
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:14:51 +0000

    approval fence dear frightened smoke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
