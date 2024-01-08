ff74c001667ea788243133eee2a2cd76dcb71d1c
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:00:59 +0000

    remain conversation plant man progress

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
