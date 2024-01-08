461bbb61783a92d5d2ca6ba5c97b3a3dbc08d4a6
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:20:00 +0000

    instruction move gather scared but

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-print([x**2 for x in range(10)])
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
