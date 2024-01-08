75da7c1ac45b9d61eba01d527045aea5832e9056
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:49:05 +0000

    573entertainment shock write model spot

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
