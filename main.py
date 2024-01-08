8c404edcc89a973f6f36cf3d765fa6014f910bb0
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:20:57 +0000

    break file still tell matter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+name = "Python"
+print("Hello,", name)
+import math
+def calculate_floor(x):
+        return math.floor(x)
