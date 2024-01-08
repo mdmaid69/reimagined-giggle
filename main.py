c23dfbae78c7bd6298dc6f72dd9719533d40bf4e
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:37:03 +0000

    notice mine dare prize talkback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
