f7b7b107668bc2b1966cec10082c19dfe7619e7b
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:55:33 +0000

    rob check stare pencil piece

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
