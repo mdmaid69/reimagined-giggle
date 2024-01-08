89ec5cf1aa4157ee5e4103d1480b13c23f71dabe
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:51:32 +0000

    sentence negative holdout ashamed artificial

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
