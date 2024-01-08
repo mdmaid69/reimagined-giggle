68a3ea58dcfbcbcc5468e579568a9e9977bde26d
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:12:34 +0000

    aim view settle 567enemy payment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
