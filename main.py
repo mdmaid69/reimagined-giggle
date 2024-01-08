4371418fef6521c055cb086186c7ad700489c25b
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:37:29 +0000

    influence wear reserve race cold

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
