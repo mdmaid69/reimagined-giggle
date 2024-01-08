60915e65f131b0c9aee920fc471d0f5c8a627109
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:33:14 +0000

    high sentence change short reasonable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
