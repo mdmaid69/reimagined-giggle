a53db356e697c1a9931fb39f376efbd5cc0f3af2
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:14:16 +0000

    contrast capital street congratulations quick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def add_numbers(x, y):
+        return x + y
