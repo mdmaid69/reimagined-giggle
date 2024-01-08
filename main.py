75f979f3392203e9f1c8cf904f7ab0a9c5c4e32f
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:22:21 +0000

    turn thing almost stick blame

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
