4e0ddcbdede57e7cc7b46402d048f958dbd12301
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:08:50 +0000

    point equal peace secret culture

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
