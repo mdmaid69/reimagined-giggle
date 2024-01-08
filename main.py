bc5313082a4900bc2afb201379f064c02747cda2
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:32:12 +0000

    car rank impatient likely gap

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def add_numbers(a, b):
-        return a + b
+def reverse_string(s):
+        return s[::-1]
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
