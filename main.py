8c484fa087c73839d208f6164cee9b505cee7236
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:30:50 +0000

    story future maybe many squeeze

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def calculate_acceleration(speed, time):
+        return speed / time
