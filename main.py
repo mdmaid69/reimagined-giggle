702a87b413e7e9e9e942ff5dafa2819811322a19
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:14:04 +0000

    clever hold still relax season

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
