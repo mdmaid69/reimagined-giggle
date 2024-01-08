a6f398e8a645edc2f7a1caad6001da8e5417b1b4
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:22:05 +0000

    general turnout big drink responsible

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import numpy as np
+print(np.array([1, 2, 3]))
+def count_words(sentence):
+        return len(sentence.split())
