412dbb39ffe54f021139f9192c92d5c4b7c3124d
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:13:59 +0000

    like set base recognize with

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
