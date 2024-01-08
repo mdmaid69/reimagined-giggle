cb6639e61d6c886c242573841914e40e8b52c88c
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:14:47 +0000

    Ww ahead patient plate hear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
