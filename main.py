44f6e52989d27038cf426c5afd312e398d75f78e
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:38:35 +0000

    everybody crowded sister demand foot

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
