2a8e46b7d46b4d60ea86d7c6253ffc1213b88871
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:42:41 +0000

    gather thumb bet difficulty spot

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
