b7a690494de0970daa6d6db6bea598998c8f061b
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:20:55 +0000

    clean thing memory everybody unhappy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import random
-def roll_die():
-        return random.randint(1, 6)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
