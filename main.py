9845935f45ba4fdeab1973931d4959be07691b9a
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:15:56 +0000

    fine rather game prepare previous

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-def square_number(x):
-        return x**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
