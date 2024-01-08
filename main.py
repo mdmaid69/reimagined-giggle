0776c0d022eba31f153ce45f901878cf315586c9
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:46:23 +0000

    him 179bite obtain pure throughout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
