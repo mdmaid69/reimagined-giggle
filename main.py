be72eac70b6ecf17a471f0368ef115150fec36ac
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:27:07 +0000

    here welcome direct carry noisy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
