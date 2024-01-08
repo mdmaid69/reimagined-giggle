9ce16dd8e40b66a0851bd8451bbd710935540cb7
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:34:05 +0000

    transparent exam confusing occasional street

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def add_numbers(x, y):
+        return x + y
