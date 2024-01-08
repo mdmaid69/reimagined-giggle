10c66dd2526559f1ecc41103d7668443f63d6905
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:01:29 +0000

    defend offer urgent possibility exploration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
