553141b257a3c8e85c13fbe629896eec88dfdf9c
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:06:54 +0000

    motion accordingto spring gap month

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
