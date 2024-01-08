c32e86bf942fd53b46e35a8b915b633ea23ed578
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:17:42 +0000

    react prepare post embarrassing fact

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
