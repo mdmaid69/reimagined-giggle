7c909de2462b2fc0b12ff65275ed415f05a39f98
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:11:14 +0000

    believe spot injury colour whether

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
