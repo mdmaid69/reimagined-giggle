ae8b16ce38a720c613c115fff297e37fc9a74b4e
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:34:19 +0000

    drive change crowd bad past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
