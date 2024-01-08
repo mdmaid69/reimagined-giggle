6716bacaf2ff8801eb615295a12c3e8c60bf5d4d
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:10:49 +0000

    bored search word taste level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
