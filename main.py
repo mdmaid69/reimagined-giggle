ab0305808aae6dd7fe964514c43d8fa240abf13b
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:00:33 +0000

    flight famous content colour exist

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
