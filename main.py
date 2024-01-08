53bcd6576fda3b571b642c69c1077884232d7045
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:41:07 +0000

    opportunity comeoff bus forward wet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
