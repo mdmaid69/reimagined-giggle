146b3b9f5328ba0d35bc6c63f6924a54da56f30c
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:14:34 +0000

    imagine loose hot tight direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
