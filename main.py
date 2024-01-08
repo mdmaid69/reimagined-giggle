8c7891f2220bea0816dbe546e7506e0292f1165b
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:30:24 +0000

    influence hope back test someone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
