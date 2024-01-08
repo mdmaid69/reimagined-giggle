237782a06026034b02166d1f3f57acaa27ea82a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:23:36 +0000

    teach tend finish independent settle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+from collections import Counter
+print(Counter("hello world"))
