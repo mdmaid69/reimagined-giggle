45eb50dbebcf2dc75d5fc2c5b950acb08b3cd1a2
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:42:16 +0000

    creature but rain now still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
