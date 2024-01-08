dedf4fa1d5c7544300b0592b554968bf44b01f1c
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:53:45 +0000

    afternoon community congratulate fetch one

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-for i in range(10): print(i)
+import collections
+def create_stack():
+        return collections.deque()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
