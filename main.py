ffaac476e90d08da60b1b041398d94b01c690e4d
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:17:13 +0000

    through rank since lunch answer

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
