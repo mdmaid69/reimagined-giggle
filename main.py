0fb72993fae710c868546a2a9c30eda87666b0d1
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:33:56 +0000

    variety time suckme thank trip

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
