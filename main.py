1ab4621b28b89d7529628a90c6336ddc790d4702
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:05:19 +0000

    prepare trick treatment criticize recommend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
