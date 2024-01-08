b8b5b30264ff464c68558fd423cc7f04b280619e
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:44:47 +0000

    surprise hardly hair knowledge birth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
