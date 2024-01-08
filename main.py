11bc6b565f70c0277176b8a2f6771f59e6bcff1f
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:25:36 +0000

    almost usual appeal consult loan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
