c17ea70a82fcb4f3510a5db46b9cb9f89ef682a4
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:34:18 +0000

    help congratulate fall Uu battery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  def cube_number(x):
+        return x**3
