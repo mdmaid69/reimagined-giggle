4958575541e455c866f651c8ead4d77b0f035bc5
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:43:58 +0000

    evidence up command consist behave

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import array
 def set_array_slice(array, i, j, iterable):
         array[i:j] = iterable
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
