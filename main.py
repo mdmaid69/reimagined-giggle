3340d2db4e4274e5d8950dada69c769b3891831b
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:12:21 +0000

    activity powerful land well firm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
