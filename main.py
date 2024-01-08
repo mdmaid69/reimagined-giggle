c0b9195cc6fede6c550ae7930dc6b72b4e07d744
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:21:15 +0000

    anybody miss main swim takeover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
