239853d302ac08d200155810b4d9a65d73f98aa9
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:03:23 +0000

    next attitude doubt uponon play

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
