08a4425ea8ffb8b798a2b2d5be95afede6f5177e
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:28:42 +0000

    raw excited knock eye shelter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
