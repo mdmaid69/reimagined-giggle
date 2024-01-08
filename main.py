70fa20a717afc2a0928758d5ea2559086f731754
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:52:35 +0000

    lonely process funny wonder firm

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_pressure(force, area):
+        return force / area
