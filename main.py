ac515619d3ce04c9d426fdd322c024acf28b6f2e
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:54:41 +0000

    next door degree pile maintain

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
