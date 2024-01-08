efbb4b81de55a2d41850f0f613271420844741f0
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:49:04 +0000

    anything opposite thankyou play off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import getpass
+def get_username():
+        return getpass.getuser()
