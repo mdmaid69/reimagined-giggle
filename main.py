c317549a109eb48c38d454baa6db779df8695dad
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:28:44 +0000

    stir reject smell for turn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
