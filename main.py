ba01c4fa026ec082a5f3681f5faa3b23da472624
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:02:39 +0000

    frighten mail read study mixture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
