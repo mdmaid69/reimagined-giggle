e3a6284beabbf402bafe97fdb99ef2bf8a4a40c5
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:54:27 +0000

    book brain runinto break lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_speed(distance, time):
+        return distance / time
