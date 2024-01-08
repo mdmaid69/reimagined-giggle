64f6b09bc24ffea96a95a8d2f3e6645206de89b5
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:58:43 +0000

    giveout aware better week blow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
