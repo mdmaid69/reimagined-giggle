1407b1490e9084ef0b5ec7282baf484b760d8998
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:43:32 +0000

    any last keen fly later

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-for i in range(5):
-        print(i)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
