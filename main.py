fa611a8e8631bd91892ae1357058f691377e7625
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:16:10 +0000

    chin sign pupil want image

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def convert_to_octal(n):
+        return oct(n)
