a908720d35946ce2662463a017a3f7b9739193d2
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:02:43 +0000

    awake rise ilovedicks opinion hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import random
+print(random.randint(0, 100))
