d5d3a75b4ca2a22866e7d5aca3276939466724b1
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:59:29 +0000

    matter hide release loud callat

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+for i in range(5):
+        print(i)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_tuple(array):
+        return tuple(array)
