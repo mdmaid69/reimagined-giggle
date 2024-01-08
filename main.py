b8213472b63127ce2159ca960bdbe63b1102240c
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:22:51 +0000

    practical please advice switch goup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
