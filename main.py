b18239594ade275dffbca2b301217468e376327d
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:22:44 +0000

    you power achievement succeed serious

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
