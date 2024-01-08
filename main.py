ddb3c03b9016e168d9f2720477549137412ea7d8
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:47:53 +0000

    step here offer rain mail

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def greet(name):
+        print(f"Hello, {name}!")
