d1e7c472a28b0fb88d4586e4259f884986afc35a
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:05:58 +0000

    plate problem tradition our team

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  def reverse_list(lst):
-        return lst[::-1]
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
