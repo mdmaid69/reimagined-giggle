2779f5dc93ebcf03a0ca614097b340898d398286
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:32:06 +0000

    than mother hang commit row

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import os
+print(os.getcwd())
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
