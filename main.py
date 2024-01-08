11460be0b82733f24a42f0cee42977d5e7bf5cb6
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:25:14 +0000

    need luck rough low content

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
