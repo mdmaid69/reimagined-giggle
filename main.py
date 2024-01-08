82ad6eadd4ecb929777bea68678fd7551dd081ea
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:08:22 +0000

    brain guard dry dear Zz

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
