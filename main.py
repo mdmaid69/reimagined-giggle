4a5094b65fbb616f66dae8ea83d486fa62f296fe
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:39:36 +0000

    quick tragedy declare safety spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def clear_array(array):
+        array *= 0
