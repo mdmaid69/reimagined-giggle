15784623a7aabd83b55044b09b8962d889119b78
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:16:50 +0000

    soil principle upstairs take word

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
