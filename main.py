25e8dffa37ce2c0f2c2604c6e85c84aff6953e39
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:51:58 +0000

    lucky raise shout wake inside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
