b18aa97ca1d9ed0ad14ce1536d8cffb0c07fc64e
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:39:23 +0000

    language frightened record recognize hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+print([x**2 for x in range(10)])
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
