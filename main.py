38a729f23425edbbc57ecdaddc9ceb2dcda9a237
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:35:00 +0000

    stop similar knock evidence swimming

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
