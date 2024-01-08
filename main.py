1760e6fba37691c2c11888849d0d6b142babb5c7
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:31:54 +0000

    experiment succeed war must child

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_min(numbers):
-        return min(numbers)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
