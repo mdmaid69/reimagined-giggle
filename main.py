e4719794065534f52f20d68f5bdff8db7bb9f373
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:06:34 +0000

    abroad majority problem face trip

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
