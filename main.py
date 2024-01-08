73de2da9f67e6807a1ba241beec6040966ac2145
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:32:25 +0000

    until sense deliberate advise type

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
