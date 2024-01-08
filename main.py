e4141577fce3fc9e52170d9089b4b5141c2f84ef
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:53:40 +0000

    treat get burst special fortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
