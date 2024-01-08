ff4da9b3ccea0759ac7d18193c17274f56cd3d83
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:11:18 +0000

    tell arrive smell repair chief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
