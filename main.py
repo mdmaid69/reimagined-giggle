c4d865c33cc56fd2bbfc9c110baf497537fcc6a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:15:02 +0000

    idea edge father heat variety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
