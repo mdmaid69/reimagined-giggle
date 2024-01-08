0aeeefaeb6bdbb0001c06066b9d9b3ee96b5a28b
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:35:47 +0000

    grateful round pipe switch official

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
