0965641728ca90fcc9bd1dc65e0f55c2a1137c24
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:19:50 +0000

    no duty anywayanyhow hang firm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
