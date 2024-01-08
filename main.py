2f404d19b67b2c6056ac6ec2d676a7ff442414a7
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:27:50 +0000

    long involve even through study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
