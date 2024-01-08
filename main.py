9ec35f609af9b07b8bf52156a73fe2dd8b3f9748
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:48:30 +0000

    add lend flood mistake win

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def add_numbers(x, y):
-        return x + y
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
