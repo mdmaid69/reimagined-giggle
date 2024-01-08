37fa77b338c86285e4e8a5ab3c33ffa76d3f9ea3
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:10:07 +0000

    swimming however trade bound us

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
