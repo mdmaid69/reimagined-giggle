de5022850f59447c58e0ee530977013e51963938
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:49:14 +0000

    politics operation stamp whom blame

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
