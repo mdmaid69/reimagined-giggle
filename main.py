cfe6bf2918bd1acebfea0d19b472d7231ddc71ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:54:25 +0000

    tear past rid room will

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_volume(length, width, height):
+        return length * width * height
