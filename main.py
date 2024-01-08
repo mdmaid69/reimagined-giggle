657dfebd509cacc3f40d71bd82db30001b32974a
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:40:29 +0000

    head tone over breathe bring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
