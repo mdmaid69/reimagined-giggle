448da901fb4dc60ae097c316f0330b45d2e8abe9
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:14:20 +0000

    bring geton teach thin spring

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_work(force, distance):
+        return force * distance
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
