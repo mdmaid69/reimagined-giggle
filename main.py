dc431795c702f425565c145f6185a866196d9a9d
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:27:31 +0000

    week earn pick fierce straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_cosine(x):
+        return math.cos(x)
