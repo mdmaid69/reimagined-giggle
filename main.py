2aa54b336053bfe9d778c5f364fa2a8bdcd7fbf6
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:48:44 +0000

    least taste wheel knowledge calmly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
