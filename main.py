4d8337e7f88578024cd247d303819b59c4a6e306
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:46:42 +0000

    tax blow style close bit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
