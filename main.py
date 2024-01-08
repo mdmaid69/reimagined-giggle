9f06777050b63008273e4e4c4b9a790c01653d24
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:52:08 +0000

    limit probably walk the forget

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_current_directory():
+        return os.getcwd()
