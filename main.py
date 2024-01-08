d78ab72ae1d8c024d3cc63c7d35286a47f9f0215
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:35:41 +0000

    argument classroom crowded powerful tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
