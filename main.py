1596ea437a053420f1d738fa8e37bb3f6e89052e
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:47:00 +0000

    the punishment fit their win

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
