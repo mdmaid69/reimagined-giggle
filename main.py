c547f31f81b6a08a6b01bd8f239560a3225284fc
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:58:03 +0000

    exact claim person steady squash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def square_number(x):
+        return x**2
