cbc39ec89180b41e3011e5876a92586798342fcb
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:51:06 +0000

    average large blue moon cost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def subtract_numbers(x, y):
+        return x - y
