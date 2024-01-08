81ef90a786b79e20f14d94287a3d75347353e08c
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:40:42 +0000

    past lay 569enjoy typical class

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
