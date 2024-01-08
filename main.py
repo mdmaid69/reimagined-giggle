a848c6eccdbab1c81508d8b83bce6e25f693cc2b
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:29:55 +0000

    ordinary the every attract pot

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+name = "Python"
+print("Hello,", name)
