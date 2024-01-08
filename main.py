17d82f2bc5f934d79831bc5112ae1a884aa83bae
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:48:28 +0000

    careful scream treat their combine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
