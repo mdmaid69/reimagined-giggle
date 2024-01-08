8bbf78cb7481a483271eb8567a1b9f26d04f78ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:50:35 +0000

    still move exercise increase hand

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
