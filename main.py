4a7e0211d9bd5c66af23f054d0e63642eb9d54c7
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:17:55 +0000

    translate narrow pull call fairly

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
