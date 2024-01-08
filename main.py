dbcf138c9f3d0efa90b865f03cdd5c34dbcef78a
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:24:20 +0000

    round better name suit shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import sys
+print(sys.version)
