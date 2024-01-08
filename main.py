d800c3650b38ed7751866b4fcc1bbe0ee5847d4e
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:51:08 +0000

    bury rule hope object doctor

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def subtract_numbers(x, y):
-        return x - y
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
