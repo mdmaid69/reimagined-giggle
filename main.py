8136f199b9aa4dce873907ebcb3deb1299692ad1
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:38:38 +0000

    year occur bored collection crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
