16f91ed41f7d1069679dc1900dacccfca62b6aed
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:51:32 +0000

    freedom she whenever send wing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import tensorflow as tf
+print(tf.__version__)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
