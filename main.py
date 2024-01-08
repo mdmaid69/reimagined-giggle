bf3b2d1980abeef634ccf2f9ba91af29e4e9eee3
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:19:55 +0000

    balance exercise creature object pain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def count_characters(sentence):
-        return len(sentence)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
