5e174746de5efc750e52586718a61b5d150cceb6
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:57:29 +0000

    traffic point although standard journey

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
