2cade517256e60d5ef588f4393b67a4ae5e5ea01
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:36:26 +0000

    kind genuine inside practical convince

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
