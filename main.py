92c8d8d116e8c32c2591ffcadaada4573c5a3d63
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:57:02 +0000

    adjective naturally between meaning beautiful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_tangent(x):
+        return math.tan(x)
