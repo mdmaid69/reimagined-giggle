a04f7186acc4b811a908ceef0774ba2722cdd6d9
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:27:29 +0000

    natural 567enemy heart punish cost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
