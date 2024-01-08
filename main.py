a5e05b239ffe4b501c7b1e0555d7a00992602191
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:07:08 +0000

    comeoff sign band small situation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
