62d6c3995cda40c11f4f4759792f8169f97c9062
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:40:25 +0000

    condition spill stiff state vote

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
