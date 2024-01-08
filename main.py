6b1a96d45b3e7baec756171fb27eb128712762ee
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:44:16 +0000

    obtain refuse branch order Jj

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
