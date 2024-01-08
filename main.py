2b55b99ad40875037259ed314c041dffc8cfd1ee
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:08:48 +0000

    woman unpleasant frame fear disagree

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
