34671c3bbed565681c4cd96a22d15ab41abe7b3f
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:06:59 +0000

    furniture tell Jj swallow slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
