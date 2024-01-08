fe7a9d224836198d7904fe71f79a36147dfa7b58
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:10:32 +0000

    opinion wind let crowd stage

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def calculate_pressure(force, area):
-        return force / area
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import math
+def calculate_floor(x):
+        return math.floor(x)
