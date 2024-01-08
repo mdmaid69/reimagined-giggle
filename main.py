f4e9efb59ed3874518319990a321c7ff4c06f313
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:26:03 +0000

    agreement real his active comeround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
