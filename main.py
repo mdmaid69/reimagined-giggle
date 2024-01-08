b9bd951b0316f7770f273964c63decea7c410cd4
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:29:48 +0000

    jump sale hide drink accept

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
