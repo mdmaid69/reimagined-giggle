2455070779f8b4e4267743ce713bb33a64257828
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:03:41 +0000

    satisfied result lunch Jj study

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+print([x**2 for x in range(10)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
