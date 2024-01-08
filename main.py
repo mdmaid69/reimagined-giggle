9563bb352fde81c0d8c6abf22ccc2a3b98bb7f03
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:13:54 +0000

    loud any your rain lift

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_work(force, distance):
+        return force * distance
