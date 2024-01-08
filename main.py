743ad18014d4f65b223c944e6af709642b17d5fe
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:32:11 +0000

    custom trade ocean confidence love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
