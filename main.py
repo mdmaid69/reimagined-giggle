4cd28398c2dce51a49df6fa37da8e6b275f31eda
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:14:07 +0000

    pour study teach welcome convince

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
