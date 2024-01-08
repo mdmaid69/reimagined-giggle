fe2e879e9d7c70fd907cb9d7147056d0404fd311
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:46:47 +0000

    reason stranger spring load period

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
