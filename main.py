cc77cb85ddd2664aa0882f875006f367d6ff2ba1
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:48:11 +0000

    lie recommend what urgent rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
