5aa5b1f7c3633fbb2b5987f1a52d55eedc9c1486
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:55:49 +0000

    organization know earth success he

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
