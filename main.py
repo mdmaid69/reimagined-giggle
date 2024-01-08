dc6a69b477f8d69e02a8c6702f409002a83b3e28
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:19:50 +0000

    mad trust difficult seat top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
