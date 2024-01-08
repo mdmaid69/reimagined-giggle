076b64723e8fe6aaaf4a5ff44b4cc7a335b89055
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:54:51 +0000

    sleep sign use dinner holdon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
