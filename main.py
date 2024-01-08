5aec6ba9d50a958c3cf7c04a13d1e14006a231bc
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:10:52 +0000

    smell goon rough development cross

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+print(math.pi)
