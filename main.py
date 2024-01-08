0bd1ecde82f89204305794f863ad9fa3c3575897
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:12:37 +0000

    waste blame ocean custom century

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-print(math.pi)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
