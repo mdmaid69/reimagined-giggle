28ecbb76b33dcb8fd2259c032a95398fccf0df26
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:09:58 +0000

    tomorrow loss depend emphasize address

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_odd(n):
-        return n % 2 != 0
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
