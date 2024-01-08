b1aa6c35915e97ef7bb69ef75fe61e7ff5b8a982
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:57:29 +0000

    country shop pocket sleep label

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
