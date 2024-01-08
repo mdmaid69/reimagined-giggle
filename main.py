d1ec1f91f581b25784ad94139208d09a62527fa2
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:40:07 +0000

    convenient tax breathe damp many

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+print(sum(range(10)))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
