ab254ef4eb5df04fec799939dfabec1f08ae07e5
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:09:00 +0000

    beneath plastic boat children contact

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
