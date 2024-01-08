f83c5516f9dda07469ea3155a77b841d451896f4
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:44:39 +0000

    task cause Ii bread process

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
