b68d671aa4672af402c55b9a1bdf8774b545f799
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:45:24 +0000

    only century score 572enter combine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import random
+print(random.randint(0, 100))
