f4855609a8f8f5725e477202cc2d64611d98547a
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:26:15 +0000

    comment Yy drink so repeat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
   def cube_number(x):
         return x**3
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
