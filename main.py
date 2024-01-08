9cc88fb94c06e468e28d5e9ff2155ec415a943fd
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:07:43 +0000

    about transparent practical born circumstance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_counter():
+        return collections.Counter()
