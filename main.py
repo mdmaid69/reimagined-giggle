7288f4b9c509a8b2e62f25d15601315ca1406127
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:08:15 +0000

    we book forward climb harm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
