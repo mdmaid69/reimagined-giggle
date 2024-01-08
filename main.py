b9045f55801db2245184c05bef03e5c421c85208
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:13:00 +0000

    smooth pause measure but communication

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  def reverse_list(lst):
+        return lst[::-1]
