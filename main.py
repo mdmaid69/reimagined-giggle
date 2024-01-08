3cc856d6a8efe95c72d2bc1f1e4b5a8c5fe35554
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:58:27 +0000

    final strike together true recent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_as_bytearray(array):
+        return bytearray(array)
