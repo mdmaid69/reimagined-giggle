a28b4766645c49d091c08e40b4517f8aef23c592
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:27:26 +0000

    sport satisfied agreement far contrast

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def is_even(n):
+        return n % 2 == 0
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
