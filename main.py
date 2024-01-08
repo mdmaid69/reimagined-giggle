e61d2248229cdc633b8f025f626a78e83a545711
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:34:55 +0000

    action worth aim cough children

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
