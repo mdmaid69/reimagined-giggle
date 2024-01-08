5b008fa773c3dd08d4fb9d84b389ed700cb66e86
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:28:06 +0000

    glass matter land welcome pupil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
