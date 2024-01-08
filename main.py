5d65f5960a66707a3f1e12499331f1320ee9a50e
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:54:20 +0000

    warning OKorokay favourite fortunately proud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
