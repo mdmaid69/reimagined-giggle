cec00c97800e0cb7e6058aaead8275db1f99a302
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:33:36 +0000

    only look tooth dead luck

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
