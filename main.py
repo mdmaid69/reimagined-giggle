8503c3f1e67a654995da07dec6829021faa901fb
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:49:03 +0000

    plate only whole violent announce

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
