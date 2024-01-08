d79082be900097ececb9f88b1db19ff4473357aa
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:54:51 +0000

    grade large comment shout provide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
