3aa86279fb3f0558e288d03dcb7e66521bcfee27
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:20:45 +0000

    ago hurry record drink fill

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
