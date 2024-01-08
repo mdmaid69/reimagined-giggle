b1a06446a6adfa25a877d3236ca489e040e3df15
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:39:13 +0000

    takeback scale path keep bang

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
