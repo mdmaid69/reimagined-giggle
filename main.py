02b03b63ab31cbb869a3d730424978ba06dcf60a
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:29:28 +0000

    abroad only Jj depend feel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+name = "Python"
+print("Hello,", name)
