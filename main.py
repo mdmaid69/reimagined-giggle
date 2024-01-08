6993be148782af0239f30f1ee69e60c351eaa6bb
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:10:35 +0000

    lookup official workon 571enough aim

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+print(sum(range(10)))
+def sort_list(lst):
+        return sorted(lst)
