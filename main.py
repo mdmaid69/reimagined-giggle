41a3d09c1a59018b9ddd8c4b7d28e474e5266ee5
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:59:55 +0000

    charge rub doubt produce summer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_list_from_array(array):
+        return array.tolist()
