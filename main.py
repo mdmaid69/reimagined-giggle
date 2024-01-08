8c7ba0cc50c8ae08462250edf44d051b8a00fc3a
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:38:54 +0000

    join city possibility length mean

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
