7054b20af031c154ca500ace60a089f32975556c
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:48:01 +0000

    dear forget modern ever throughout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
