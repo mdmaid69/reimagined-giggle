761176efe67c3c0cb9941570ae5a61178588ab72
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:20:33 +0000

    shine minute damp crazy Ii

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
