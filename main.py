2f9b6bc0a72aa72564feb0b4da1d671131d6bd53
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:24:22 +0000

    free backwards lesson none scene

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
