34a6efc044d69ead3140ca3f778a73997aa0274a
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:35:38 +0000

    imagine pretend sink neither bring

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
