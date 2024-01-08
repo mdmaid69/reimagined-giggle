2881c45cead37e32a47619347e6038d395afe5e6
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:20:59 +0000

    definite delight at level tone

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def remove_duplicates(lst):
+        return list(set(lst))
+import numpy as np
+print(np.array([1, 2, 3]))
