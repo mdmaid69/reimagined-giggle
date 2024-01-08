394612639fac89b093fa4d4acf39833a12229bf9
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:44:10 +0000

    please smell pocket rule trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
