9266b16c28adf047ff63b6635a4d3943d1298bd4
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:37:36 +0000

    wet Ee kind all train

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
