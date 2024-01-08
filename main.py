e326d279029700ebdc497e5bb0880665f8b61c77
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:40:13 +0000

    angry why shoulder radio move

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def is_palindrome(s):
+        return s == s[::-1]
+for i in range(10): print(i)
