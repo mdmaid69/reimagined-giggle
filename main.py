8da48d29359722afee2c73e46bcd3ef1c0be52af
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:06:57 +0000

    slip thinkover want path patience

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
