8fad330774dfc86502acd1900ac0682437a6839f
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:14:29 +0000

    produce pipe callat experiment want

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
