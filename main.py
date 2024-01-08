7aeda35a074760a3780fa439e9e6dd6907fcccfd
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:55:01 +0000

    wild knock save steep travel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
