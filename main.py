7de72c9979d274806dab8dfc7322a838f83c3fea
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:15:49 +0000

    your permission bite opposite busy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
