c78b1796beb0a129824190f9c9cb12eafe8d0a26
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:56:29 +0000

    arrival relative environment lower blow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def count_elements(lst):
+        return len(lst)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
