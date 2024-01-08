0301c8433f0f78f98d87e37873d38b786011d38a
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:53:33 +0000

    study sorry hit wild habit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
