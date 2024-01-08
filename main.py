59d4152d57af66d1fbe2ffed33eb3bf548e1127f
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:38:05 +0000

    acceptable remember drop copy home

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def sort_list(lst):
+        return sorted(lst)
