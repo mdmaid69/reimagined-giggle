d93d9e01fc77b27b158667e7ad46bff2d388bd45
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:51:36 +0000

    favour something our science makeinto

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-for i in range(10): print(i)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
