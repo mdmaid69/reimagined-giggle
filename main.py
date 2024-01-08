2874df4ba69d7a28bf027ad7cdadaf8605458e0b
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:42:33 +0000

    difficulty contact consider gooff cry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_speed(distance, time):
+        return distance / time
