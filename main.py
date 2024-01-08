46f3caa1f90703f9fc62819538e7744203ea3f3f
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:53:28 +0000

    private pretty neat Cc plate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def is_odd(n):
+        return n % 2 != 0
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
