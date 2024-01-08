ff31e5b05411510ffa01c3d6278b25ffed3d604c
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:24:41 +0000

    loan bringforward television season seat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def add_numbers(x, y):
-        return x + y
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
