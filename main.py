0ebcdc7227183247854ce0a47f9fd2ac31dbf5e3
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:01:28 +0000

    last habit shout total correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(10): print(i)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
