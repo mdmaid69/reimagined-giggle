df04a6d5a9a9a53172b3d8191d0fe184068006bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:09:13 +0000

    old which frame ride new

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
