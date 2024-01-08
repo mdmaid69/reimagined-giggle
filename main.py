a585bf352330c2f8aa61c581c550773a026ae32e
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:59:03 +0000

    report authority mean turnback excellent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
