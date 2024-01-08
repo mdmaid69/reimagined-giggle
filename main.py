f02bed1decba0af18909a43d585ccff18131bf21
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:49:37 +0000

    difference rain punish copy early

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import sys
+  def get_python_version():
+        return sys.version
