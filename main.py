6e19cfa60971a7447d0e4a1a8d08646c28435208
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:49:55 +0000

    separate clean square basic tie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
