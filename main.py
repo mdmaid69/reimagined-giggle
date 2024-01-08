e55c5d6307acf911b4f736583f0b9fd99aacda16
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:46:11 +0000

    dance brave tap switch cold

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
