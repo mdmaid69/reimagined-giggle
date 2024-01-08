37ed6d88312b433ffdcf87b2b28fcc68148ba9aa
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:10:55 +0000

    wrap vanish centimetre control exam

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
