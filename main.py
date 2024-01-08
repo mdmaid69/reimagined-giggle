1ffda830f476dd8c650a49e866aac2be355ac808
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:46:42 +0000

    represent phone react tone comedown

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_power(work, time):
+        return work / time
+import time
+def get_time_since_epoch():
+        return time.time()
