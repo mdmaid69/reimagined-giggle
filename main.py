0ca63aa713971e4ff9a981483658caa22de7db30
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:44:14 +0000

    tear calm accordingto feel order

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
