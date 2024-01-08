2a219167185512f53f98dfe9b20d408474c0ae5a
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:10:51 +0000

    pin exciting throw move seat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
