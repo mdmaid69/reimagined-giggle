e177a26613233b06af382867140343494f070e13
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:05:47 +0000

    charge send zero since politics

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import collections
+def create_priority_queue():
+        return collections.deque()
