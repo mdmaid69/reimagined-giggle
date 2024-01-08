e2e73cb113dab6198e5837452937d0ef1283b25e
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:14:56 +0000

    ignore steep pleasant recognize present

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
