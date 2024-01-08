33a3d65b2e27cc415bd13700324f50f235f9bc72
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:14:57 +0000

    disgusting flavour especially safe neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
