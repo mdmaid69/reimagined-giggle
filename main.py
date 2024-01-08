3d54e81fe3e2fbf243579419e532835cbedadaa8
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:26:29 +0000

    keepoff makeup professional still dance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def reverse_array(array):
-        array.reverse()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
