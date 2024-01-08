58a30f1cfd9fcfd59585c534103ea151b7453ac5
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:47:03 +0000

    account account sit pot stamp

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_work(force, distance):
-        return force * distance
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
