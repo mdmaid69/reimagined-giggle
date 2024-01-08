e5fe5d915c589abe6763bb9656cbaf236507d906
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:04:08 +0000

    belief hour yourself collection danger

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_work(force, distance):
+        return force * distance
+import json
+print(json.dumps({"name": "John", "age": 30}))
