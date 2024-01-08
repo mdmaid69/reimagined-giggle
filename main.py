9c34b97b47b6935a9790ae0a7fced6b42dcc700b
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:26:12 +0000

    intend emphasize Ll sea remember

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
