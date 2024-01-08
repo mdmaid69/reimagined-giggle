03c4ea8551b5552a48ef160cfeffaa13314ed1e8
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:13:08 +0000

    slightly gate key we slowly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
