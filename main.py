ed5121463eaaa60a67cfb470b97d79d27f3a2a45
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:52:45 +0000

    ground guess issue moral share

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import array
+def get_string_from_array(array):
+        return array.tobytes()
