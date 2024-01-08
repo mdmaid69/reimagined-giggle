26e540231f5902392b9ab133b7633b96ca63789b
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:21:13 +0000

    talkback bite repeat general slow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
