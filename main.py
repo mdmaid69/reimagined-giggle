367dbb43b1956d7cedb0eafcc51faddfb2fbc54c
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:17:26 +0000

    team better see left sort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def is_even(n):
-        return n % 2 == 0
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
