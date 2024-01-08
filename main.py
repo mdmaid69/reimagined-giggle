0b9fac86e3e74bc664d07e67ae5894b591d6d923
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:37:28 +0000

    model birthday ring underneath go

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
