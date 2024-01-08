9da69a9e0561d192ab0f76e0dcf9046bd250768d
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:28:24 +0000

    piece print yet flat view

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
