f8b89eb193fc141377a01541d1780034070ca987
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:37:11 +0000

    bring attach conclusion sink strange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import os
+def remove_directory(path):
+        os.rmdir(path)
