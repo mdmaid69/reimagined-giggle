9a27a2e0d7abbcf23dcd4773780cbf6436c548bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:22:30 +0000

    invite official unlikely note run

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import sys
+def exit_program():
+        sys.exit()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
