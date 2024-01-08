8784635cec3854ca28db861805021d75be007a1d
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:36:58 +0000

    kick careful limit responsibility sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
