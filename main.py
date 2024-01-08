6b4223a0dc6365cc974a3f6e1e8937c514a6e08f
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:57:01 +0000

    570enjoyable top cross sticky natural

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def get_array_as_bool(array):
+        return bool(array)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
