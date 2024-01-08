56cd916d7b57b98f736e5a795744bbb863bbade9
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:17:59 +0000

    upset book achieve record tooth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
