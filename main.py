e83b51f2c4e4f2d2870ce419b191455bd2ca9c94
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:27:00 +0000

    destroy wander design determined difficulty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
