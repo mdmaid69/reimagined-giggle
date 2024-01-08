01bb7e24e038bcfcf13d85bdeb3b404739c2c640
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:51:54 +0000

    age female important lift state

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
