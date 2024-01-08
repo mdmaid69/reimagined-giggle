4962bbb04d976e4cc5b9b11d7b5126ecd26caabe
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:55:02 +0000

    shame contrast seem voice form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import platform
+def get_os_info():
+        return platform.uname()
+def find_max(lst):
+        return max(lst)
