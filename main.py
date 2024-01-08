bd47961114a92e211647708c9562fda6c2876e4d
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:24:05 +0000

    trial blue debt must party

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import json
-print(json.dumps({"name": "John", "age": 30}))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
