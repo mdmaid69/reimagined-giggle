1f7855ad9fb586541e7afb99e580fe2a2db049e4
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:54:15 +0000

    completely stamp relief mistake keepoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
