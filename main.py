80225dcd35b6d061faae8ba1309ea35aabddcead
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:22:49 +0000

    account government shock wealth learn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print(sum(range(10)))
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
