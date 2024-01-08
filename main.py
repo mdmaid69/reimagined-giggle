1547d3adb35440f2bd94bdf1ca0a7da55ceaa963
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:36:58 +0000

    tomorrow try detail attempt flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
 import math
 def calculate_absolute_value(x):
         return math.fabs(x)
