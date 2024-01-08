82f091b8d6554c87579d4e47dc61eb5e35469440
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:49:27 +0000

    dark tooth should worth while

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import random
+def roll_die():
+        return random.randint(1, 6)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
