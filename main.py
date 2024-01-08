85a587ee4c80bc0a8eae408ec8445691f17121d8
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:12:11 +0000

    exam by watch wear onto

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
