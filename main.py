6b5121c57bc153ff02211f592ca5aac09e279660
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:30:07 +0000

    length long cover happen car

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
