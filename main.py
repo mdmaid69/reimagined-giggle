2b16b86a16468ba5c404b13e9ac4140d768193e4
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:29:11 +0000

    fear tall around fast runaway

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
