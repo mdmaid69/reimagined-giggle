7c73793708e0eba48080898dcd74868bfc2c3145
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:35:31 +0000

    shape normal wire street pity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
