96f9f380abfae93ba8a311eb48836ba53d9c675a
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:54:53 +0000

    179bite weather slight group answer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
