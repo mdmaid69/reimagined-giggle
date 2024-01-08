080a04b4ac4783b545a268d620f5d6c506da6dd7
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:53:32 +0000

    tune bread natural fit throw

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
