cd2c816b30ffecb4bc35f46c752d7aa5a146447f
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:14:37 +0000

    might yesterday water black advanced

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def remove_duplicates(lst):
+        return list(set(lst))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
