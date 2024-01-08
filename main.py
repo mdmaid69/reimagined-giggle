42582770fab191e7ff170560244ccac0058ef9bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:52:39 +0000

    Ss money dream complaint fair

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
