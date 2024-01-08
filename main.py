bd0bb8caa507b02934dfa197edf129cb0c7eb543
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:50:20 +0000

    relative than couple brain behave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import random
+print(random.randint(0, 100))
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
