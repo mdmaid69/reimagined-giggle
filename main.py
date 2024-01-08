0b181519eacce43554ad82e73cd18812042a5289
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:09:54 +0000

    typical introduction permission thought soak

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
