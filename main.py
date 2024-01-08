14eb66cff6e0738ec81362cf0aed7753447d4eea
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:08:55 +0000

    callat actual war decide light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
