2248c8ed1c2c2b7f2be43dd2c8dea8703e2ae1c7
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:24:26 +0000

    hungry difference without except settle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
