84b36aa2a1f9b48673de1660dad54bd287ac53aa
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:46:39 +0000

    ring turn tooth advanced discovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
