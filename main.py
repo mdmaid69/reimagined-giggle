e4988ce638b9e044384963348739fcf4e5f51ced
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:46:51 +0000

    Hh behave anywhere congratulate thank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
