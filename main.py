5e78111a7abfbef477d999837b0dd0056353123b
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:37:39 +0000

    pride hope fat breakthrough method

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
