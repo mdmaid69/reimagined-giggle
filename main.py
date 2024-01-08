1ec790a90cc2e0291f7e0695a8b01c80fca1bfc6
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:13:16 +0000

    lunch else holdon belong dinner

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_error_function(x):
+        return math.erf(x)
