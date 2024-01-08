243a8a2985404653633d014be0b35a0373ded460
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:53:37 +0000

    oil fierce line square unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import time
+print(time.time())
