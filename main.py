7f880eb36f8c822f72a3055f5e6b400a5b9be719
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:36:59 +0000

    feeling achieve action persuade brain

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def is_prime(n):
-        if n < 2: return False
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def is_prime(n):
         for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
+          if n % i == 0:
+        return False
         return True
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
