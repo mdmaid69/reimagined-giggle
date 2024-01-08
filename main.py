0243eb3ef9f7c79306f9840076397c84d64a0b1a
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:02:12 +0000

    goback(to) Pp breakfast manage last

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
