b1ef677e64ed2051254c7ef45feeab8b6f864819
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:32:14 +0000

    block broken during rather improvement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
