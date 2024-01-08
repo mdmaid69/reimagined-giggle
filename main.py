6a80eba95a0949d297b3c5c776255fa0284a0321
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:41:39 +0000

    makeup die mainly change pronounce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-name = "Python"
-print("Hello,", name)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
