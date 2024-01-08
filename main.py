43d1fdef4c0929ae5a11f532ea2a6c20afeade45
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:49:41 +0000

    ordinary secret how comeout risk

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_work(force, distance):
-        return force * distance
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+print([x**2 for x in range(10)])
