25b7f4851d4a1ca5f580ef79b03736f5d9783ba2
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:44:19 +0000

    appear agreement sweet aim shrink

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
