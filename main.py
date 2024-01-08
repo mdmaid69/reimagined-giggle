4bb866f90c0b1a9aa93242ca15a027ba67dc63d8
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:52:00 +0000

    card hate take him balance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
