784d6faa44d67ffbc3379ba294b0621e520ebc13
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:25:28 +0000

    down language underneath trap scare

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
