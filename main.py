9b5a8de489f019435458888057395a52f7d0864a
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:32:17 +0000

    grateful land transport police container

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
