e673eb503fa86018ac57f3a1183d853990fa77fd
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:08:45 +0000

    scream easy fast careful 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
