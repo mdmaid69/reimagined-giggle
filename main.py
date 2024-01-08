1ab973a346e46c002c3b30106ae3213729149d87
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:03:08 +0000

    since imagination search risk hour

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
