f76dd7fd8e2e7f464b0a8ddd2aa18dd63da7171b
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:50:23 +0000

    plain central sing middle talk

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import sys
-print(sys.version)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import time
+print(time.time())
