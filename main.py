5c48c04e9a40a946db4e2809e7793b0575f666e7
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:38:19 +0000

    legal threaten various office turnup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_work(force, distance):
-        return force * distance
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
