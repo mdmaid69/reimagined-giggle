e1e67cbb361acbbd033954ef7403054417a238b2
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:53:55 +0000

    sister soil blow emphasis aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def remove_directory(path):
+        os.rmdir(path)
