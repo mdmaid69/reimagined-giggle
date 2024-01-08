97fc06b723b0e1b86fd2b0655ceacab325ea9144
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:47:40 +0000

    tomorrow that separate yourself unable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
