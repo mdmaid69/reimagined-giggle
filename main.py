64861bd46cf24917ae066abdcfba68be1d9ad59f
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:16:43 +0000

    freedom smart want commit switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
