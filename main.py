f9f4c665e0ee76a11a42e2d831bb9c130954261c
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:05:13 +0000

    part swim week block wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def square_number(x):
+        return x**2
