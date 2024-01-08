015e4d52f0b33563b183427cf9a29ec45c1d2d49
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:27:25 +0000

    dry everyone along score danger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
