0b50128467fbcdc631774d6163c2dbc6471deb10
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:45:20 +0000

    ours friendly impatient move official

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_odd(n):
+        return n % 2 != 0
