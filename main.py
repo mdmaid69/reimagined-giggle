41ebb3b532201a5a44db07906fa7c693e2dacd8f
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:53:30 +0000

    detail afternoon concerning success plenty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
