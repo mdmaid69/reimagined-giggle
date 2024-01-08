187ae70c0e47be7004611a342e9e4e88c66c8bfe
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:13:20 +0000

    situation cause visit cheat excuse

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
