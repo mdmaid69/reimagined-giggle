71f9d0174d17530eca5ad0810fbfa469f4d69698
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:32:22 +0000

    feature 179bite declaration female full

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_set(array):
+        return set(array)
+  def calculate_area_rectangle(l, w):
+        return l * w
