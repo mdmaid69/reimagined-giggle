7b2eaa16feaa2e027382cc1e4262d458e1324d8f
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:38:06 +0000

    prepare transport later pleasant dull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
