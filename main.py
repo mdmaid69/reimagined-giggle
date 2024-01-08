6cc2630ac1bbdc6fd76899dc563fc5ba82d1cdac
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:27:54 +0000

    material tooth careful appearance part

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def count_elements(lst):
+        return len(lst)
+print([x**2 for x in range(10)])
