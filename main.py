f8b84bec9bfc3ff485030d064c46c18093281d7f
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:40:48 +0000

    improve respect terrible tell always

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_power(work, time):
+        return work / time
