8602090cfd02c8743b1cd24bae134f216fb03938
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:17:10 +0000

    lose flavour cold upstairs tape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+for i in range(5):
+        print(i)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
