120bbf07e2d81e930149344291bb999307612d96
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:00:46 +0000

    let breakup neat lost main

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def remove_duplicates(lst):
+        return list(set(lst))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
