1225bcbe16cba0b65c61e89e6e9c2a036b10b49a
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:23:31 +0000

    form breath whatever hold independent

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_current_directory():
+        return os.getcwd()
