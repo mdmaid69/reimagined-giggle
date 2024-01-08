5a12626b84c71a708766104dccde0e3c40de3f92
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:04:21 +0000

    means impatient table narrow closed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
