e956337545d8f7d1412c1d158365dde86ad046b2
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:01:18 +0000

    left receive corner shake perfect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
