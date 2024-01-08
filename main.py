5f264655a8b13676e24a103e037d9933d4fd55ef
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:14:46 +0000

    reject stiff step round wild

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def remove_duplicates(lst):
+        return list(set(lst))
+  def square_number(x):
+        return x**2
