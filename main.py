89b43431b0762f5bd96a3f89d81721f61beccd9f
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:45:39 +0000

    bus regularly warm judge survive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
