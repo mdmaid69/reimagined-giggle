010aeaca5901a450ff1c90ae81170c79c236ccae
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:37:44 +0000

    light spare poison weigh thinkof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
