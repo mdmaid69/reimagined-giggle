889a66a9d9fbc08dc9e3c220b0172396f152bf3f
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:31:11 +0000

    lookback(on) arrive among film aware

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
