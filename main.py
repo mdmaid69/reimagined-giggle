78316c509c02bab5885962d910376af410b49f33
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:23:40 +0000

    so cruel spring again burst

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
