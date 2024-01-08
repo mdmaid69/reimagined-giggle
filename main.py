2bf1382a07961d4969b8ae5224ebf34cba9ba5f7
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:22:31 +0000

    fast capital 566end except wheel

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
