1e952fb07216200c5024ca0da7f01733c80b828d
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:58:41 +0000

    fall charge apologizeapologise uponon name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
