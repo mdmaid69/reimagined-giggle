a5117d6eda8cc4bb67dcd0a24af955f5e6a9e136
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:22:13 +0000

    around obvious gather beautiful intend

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def cube_number(x):
-        return x**3
-def reverse_list(lst):
-        return lst[::-1]
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
