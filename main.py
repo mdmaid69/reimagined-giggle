3c19bebafc522922aa7df9f4e5da7d3eaec92308
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:31:08 +0000

    weigh punish frequent treat he

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+print(re.match("h.*o", "hello world"))
