a839900e94b7087f01d9e7ce6e4762e16b1a7628
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:34:36 +0000

    angry evening congratulations pour but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
