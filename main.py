45725aa100101276858992e16681b7ceae231e40
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:42:58 +0000

    opposite statement cold bath sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
