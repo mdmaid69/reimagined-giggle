d76df38d6f9c0fa49b79aacfbbee8aab845b96cc
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:37:21 +0000

    stare whistle swimming conduct bush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
