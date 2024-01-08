34bbefebb5d9090acd374307b628df479e9041b9
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:17:52 +0000

    action please debt before insist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import sys
-def print_python_version():
-        return sys.version
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
