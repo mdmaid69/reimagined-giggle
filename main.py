da35964d14068109205701b28c559fd46b4b0866
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:00:16 +0000

    manage pain repeat letter against

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
