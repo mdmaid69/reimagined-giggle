43f50251b6028bfccfba21358c54a5aee6323531
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:02:24 +0000

    shoulder gate slight attach wild

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
