08742978dec53303037e7733af84ab735ace7e50
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:53:55 +0000

    region shock event runover succeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
