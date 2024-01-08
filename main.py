5e713ea9b56792f65af129653793b7243f502886
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:36:56 +0000

    keepout safety noone example grand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
