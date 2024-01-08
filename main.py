d08441ca8f4ec99d0d613686bbf7cb34728aff88
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:55:29 +0000

    welcome point direct adjective early

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
