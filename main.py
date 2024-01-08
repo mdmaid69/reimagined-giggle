fe919361ba6105656be72ad3c76c5850209fb142
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:25:31 +0000

    interesting general anything calm feed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_string_from_array(array):
+        return array.tobytes()
