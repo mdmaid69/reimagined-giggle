c6adb653c1077a42c60a6d9916ed6193c06d6c4f
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:17:16 +0000

    low rather Ee laugh mess

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
