ffd5e3656f2d1544b86e2918f0feaa90455fcadb
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:17:20 +0000

    habit pattern exciting yesterday calmly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
