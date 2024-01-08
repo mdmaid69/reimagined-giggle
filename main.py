17ad0f96502119f33ded18177e93cda893a51cee
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:34:48 +0000

    foot bringforward memory peaceful along

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def get_array_as_set(array):
+        return set(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
