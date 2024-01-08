8d3823db266544d41432a7ca6f93ca33da3ae548
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:01:07 +0000

    actually below place exploration same

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
