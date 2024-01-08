8f797294d23e58a08f7c0b6693cebda5be4be988
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:07:25 +0000

    suit train turnoff mine anxious

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
