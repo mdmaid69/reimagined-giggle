03db47ab0f4a1ddbee94135f5ce086568410f6c8
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:18:27 +0000

    keepup dance sensible destroy one

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import platform
+def get_os_info():
+        return platform.uname()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
