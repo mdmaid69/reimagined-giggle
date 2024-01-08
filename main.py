4da5df6fb69befe417fe470a00f8884ad19447ba
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:40:59 +0000

    tear region awake effect spin

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
