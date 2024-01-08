e9548ac0f85a02a16f5f3e5f7c317499ba7a7fac
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:30:56 +0000

    radio early history variety chin

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
