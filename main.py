40c282998d4afd90fad5501dbf0b5f6f69f6bee0
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:49:58 +0000

    interesting better strange compare communication

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
