2b4f280f3817635aa884b467a78cc69b7a978c39
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:21:55 +0000

    pretty unit save defend black

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
