b8eeadaa600bdfacf1e00abb0bf67dac8a14af21
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:14:40 +0000

    wind now transparent make must

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def is_odd(n):
-        return n % 2 != 0
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
