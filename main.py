176142533e6036468a2c3680f02b6f9620fcf389
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:56:33 +0000

    aware breakinto with game innocent

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
