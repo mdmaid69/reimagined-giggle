2f58f545bc8ce38820b90f31525022bf041c31d1
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:21:39 +0000

    takeover mainly benefit replace expensive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import numpy as np
+print(np.array([1, 2, 3]))
