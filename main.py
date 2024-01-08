6d7dcf0c538c7c44edfe55be05a30f4c96355b6f
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:02:37 +0000

    opinion stone production thanks children

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
