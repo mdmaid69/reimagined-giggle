8be34af291da14a6e988d8d093d8958b6012c528
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:54:57 +0000

    material late new base just

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
