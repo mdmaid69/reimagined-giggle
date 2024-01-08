225ccd131c24c5b1d8b157bdf1f70a6a4e503975
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:07:06 +0000

    glass along science size wipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
