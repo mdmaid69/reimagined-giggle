2da7a7140fb4c13fbef1979b1810e3211955c6a4
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:00:21 +0000

    save comment shout able first

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_power(work, time):
+        return work / time
