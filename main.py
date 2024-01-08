c4c131d07426f6daf1a1e23da6089ba9b7fb3a2e
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:56:33 +0000

    demonstrate normal party Dd noisy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
