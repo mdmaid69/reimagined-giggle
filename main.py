91e69302a6049a1e66e1635cfc2edea7fadc84df
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:26:47 +0000

    hungry dream purpose keep sort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_acceleration(speed, time):
-        return speed / time
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
