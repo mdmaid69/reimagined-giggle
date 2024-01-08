b88b4826cd385439f366cd5621fe73c425a1d3dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:23:52 +0000

    mistake love group win artificial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
