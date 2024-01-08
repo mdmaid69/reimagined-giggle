ae117e56160d023153b7c49cc7c7c8b732d3c7e4
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:52:33 +0000

    majority lock date telephone declare

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import collections
+def create_user_dict():
+        return collections.UserDict()
