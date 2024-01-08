453c53151beea535fb84c4a217ffe8d5a2aa1d1e
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:02:55 +0000

    nowhere responsible effort remember praise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_permutations(n, k):
+        return math.perm(n, k)
