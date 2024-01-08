24b2de827fa0c507d0276bc4f69812545e9352f1
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:31:57 +0000

    improve danger dish rush support

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import math
+def calculate_cosine(x):
+        return math.cos(x)
