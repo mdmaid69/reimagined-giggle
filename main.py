baada8f0440f85ebb8299b610feb659143b9dd3b
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:02:14 +0000

    hardly takeout rather culture emphasize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
