6fabeaa68fbdc2ee1a638143155e60171eaa5d8f
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:40:00 +0000

    tax fierce battery allow material

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import sys
+def print_python_version():
+        return sys.version
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
