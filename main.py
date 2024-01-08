19ad8767acc66f0e7cb79eef8d8c1440df7e6fbd
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:57:23 +0000

    also nor speak occasion Ff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_palindrome(s):
+        return s == s[::-1]
 import array
-def get_array_as_set(array):
-        return set(array)
+def convert_array_to_string(array):
+        return array.tostring()
