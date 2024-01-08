3bf642a9fc042a7d9fd85c1e81bed4c159f5a411
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:12:23 +0000

    blind thirsty music getaway off

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
