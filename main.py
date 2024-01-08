3a11d622dd8b6bcda9a00d918a3cdffd89cc9fb1
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:46:44 +0000

    forgive joke plain answer we

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
