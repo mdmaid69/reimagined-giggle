6c124a1b8c51ce5430c4bf635365b846d30b993a
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:47:20 +0000

    mistake wipe give callat wheel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
