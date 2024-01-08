693d755079d3f28a9b11cdb2e11b97ff7b804c9d
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:01:25 +0000

    performance shall fault constantly mail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import random
+print(random.randint(0, 100))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
