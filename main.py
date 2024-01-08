20c8f955b1695e4a6ddbf87443e33dcfbaaf5f72
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:23:01 +0000

    art Yy near suggestion statement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def is_odd(n):
-        return n % 2 != 0
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
