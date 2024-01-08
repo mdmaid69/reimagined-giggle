f07beeb009e268a94d0d27c8107c14be854e14aa
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:08:37 +0000

    cross language attractive change meal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+print([x**2 for x in range(10)])
