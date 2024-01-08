d276e871220fb29d1b47196e936e1175096cd806
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:28:06 +0000

    straight row label autumn possible

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
