73579f6443c7bdcd50ab1339a939a1cd67034932
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:04:26 +0000

    damp weigh to doubt Itself

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
