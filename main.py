c15dfb1608d0e3716a429dd1a2cad31eef8bb180
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:25:25 +0000

    partner live act brave this

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import random
-print(random.randint(0, 100))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_area_circle(r):
+        return 3.14 * r**2
