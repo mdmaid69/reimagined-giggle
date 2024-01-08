a10a5009d92204a3bd8add15440f3804ea9a3d87
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:49:13 +0000

    question defend score brother lean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
