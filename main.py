c6f7ca5ebbe89b5e0aeb28ffe041cdcd00267291
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:19:32 +0000

    spend spring concerning rough congratulate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import getpass
-def get_username():
-        return getpass.getuser()
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_typecode(array):
+        return array.typecode
