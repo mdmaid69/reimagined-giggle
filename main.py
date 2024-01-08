83cf91c3a0a9f66d39f562000dce4fd058ea1ea0
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:02:13 +0000

    plan afraid tax radio nice

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
