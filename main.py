82a9899aebee5e7abe6c743db1fb6c2103d0cfc7
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:38:54 +0000

    now famous wealth whichever embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
