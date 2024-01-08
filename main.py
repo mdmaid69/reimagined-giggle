84df60cfdda5a0ae1f1f51fc5d23a8e11c9be647
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:05:35 +0000

    minute middle unite fork answer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
