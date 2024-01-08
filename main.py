fcff61bb8620cce4240fb0cf528bc1495c9494c3
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:17:15 +0000

    above view game afraid type

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
