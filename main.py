bf3307548dc4acf880e73d904d4501ec30e5227a
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:04:52 +0000

    rain pinch shame model side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
