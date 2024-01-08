d9926eec5a5166c0c80e3450b2c191aaa271476f
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:48:39 +0000

    life test part breakout work

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def add_numbers(x, y):
+        return x + y
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
