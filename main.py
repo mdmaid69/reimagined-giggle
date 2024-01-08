49c2f9b90480afd5bbadfb199c0a45e34aeec58d
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:54:12 +0000

    visit form butter turnout makeinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
