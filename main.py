f9833a07e01179d17382cd9c5011d24d56d3757a
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:36:39 +0000

    label stop sign activity quit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
