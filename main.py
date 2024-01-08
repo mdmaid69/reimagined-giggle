d2b80aa9250e0596d949f2a3fc700bf59f58f9b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:25:08 +0000

    state noone event thought sharp

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
