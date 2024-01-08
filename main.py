1e10423f2018bfce5abffadb5b3d9593852e6925
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:54:49 +0000

    mistake book mad bank expensive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  def cube_number(x):
-        return x**3
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
