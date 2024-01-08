339d63d5414a104ef767ad8b7e0f1afe7e351d53
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:14:31 +0000

    sound turn winter impatient spill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-def find_max(numbers):
-        return max(numbers)
+import array
+def get_array_as_bool(array):
+        return bool(array)
