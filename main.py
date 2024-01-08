4f579bf246ea476f9b1bc534f32fb5d4fc76ad19
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:34:21 +0000

    equipment sweep sometimes belief congratulations

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
