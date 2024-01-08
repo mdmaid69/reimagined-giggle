649fde1351b93a30bc46bc7edca833549a1873f2
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:01:40 +0000

    noisy not us getout reduction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-print(math.pi)
+import tensorflow as tf
+print(tf.__version__)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
