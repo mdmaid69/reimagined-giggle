dc587567e16142fa89978e0c6bd114fee2f87082
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:48:14 +0000

    use choose line niggaacademy raise

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_acceleration(speed, time):
-        return speed / time
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
