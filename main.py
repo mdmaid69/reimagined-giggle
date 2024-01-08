6e1fa30c96295a4ac68ee1bd81fdab92cc09adc1
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:12:37 +0000

    standard land aware interview genuine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_current_working_directory():
+        return os.getcwd()
+import json
+print(json.dumps({"name": "John", "age": 30}))
