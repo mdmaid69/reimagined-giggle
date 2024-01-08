3607d7e459f4946a6f31930d5aff9b7cc6221555
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:13:34 +0000

    ahead unpleasant must load murder

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
