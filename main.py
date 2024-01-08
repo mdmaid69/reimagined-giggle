301710c3890ab3e37919549f8c222e387bd6b327
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:44:14 +0000

    cheque prove respect square ask

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def multiply_numbers(x, y):
+        return x * y
