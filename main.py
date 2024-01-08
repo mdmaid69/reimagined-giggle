e7895efd5747a115e4c66c028ad8c9553f64bf88
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:36:06 +0000

    happy innocent remain scheme familiar

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def greet(name):
-        print(f"Hello, {name}!")
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
