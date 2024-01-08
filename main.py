7c0dec04f46443a9b83eef3b39b462b87bfb5444
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:40:23 +0000

    consider little house possession wealth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
