09e7d05cae3353394a3270a6f98d7e8b9461baf4
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:12:48 +0000

    perform anyone month manage wild

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_acceleration(speed, time):
+        return speed / time
