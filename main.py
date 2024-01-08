b52a76184443dc41db215ef6d3d6b943c3b3d18e
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:50:17 +0000

    pin ring change plan material

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def convert_to_octal(n):
+        return oct(n)
