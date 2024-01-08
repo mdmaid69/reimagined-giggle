8d8c508051ac83f8a8bcbd224aab29edcfb66d13
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:56:34 +0000

    bury know set different breathe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
