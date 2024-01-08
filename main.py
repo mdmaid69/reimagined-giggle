405a07708fe985964b6b6c6a4ffd83cbc7816f7c
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:03:08 +0000

    chat load check human give

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
