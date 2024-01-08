5d27553c65b0bf2abcf3656f1092a06b25dddf22
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:46:25 +0000

    accordingto married objection fast so

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
