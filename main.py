ffedb4eb6dee950694b0cf3ff82a19f28a44eea7
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:51:55 +0000

    alone everywhere rest pain objection

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def count_characters(sentence):
-        return len(sentence)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
