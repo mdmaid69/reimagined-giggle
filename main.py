bd45847b3550a1c4adf6603eee979d458a4b64c3
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:29:48 +0000

    earn mother taste vision sale

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_list_from_array(array):
+        return array.tolist()
