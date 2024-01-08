3f06125bd1e596cbb1cb3a8b003dccabe0ef5e89
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:13:20 +0000

    learn but seat flavour awake

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def create_priority_queue():
+        return collections.deque()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
