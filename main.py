dd0742da0f578390047a39ede588d26a560fb626
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:21:07 +0000

    division necessary trip still hers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
 import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
