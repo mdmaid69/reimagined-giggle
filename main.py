955d5f39b74ba5ed6af4193c05217464ef5c2a7d
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:56:43 +0000

    friendly absolutely need Itself central

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
