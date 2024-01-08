f86ca8c982d9abbd30494f2d3a17efe5b20affd5
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:42:11 +0000

    weak emphasize sale onto row

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
