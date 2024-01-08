34734f3c982d83189e0e7e1060b3caec31b0e18b
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:45:38 +0000

    narrow getaway fairly slowly see

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def reverse_list(lst):
-        return lst[::-1]
+for i in range(10): print(i)
+def find_max(lst):
+        return max(lst)
