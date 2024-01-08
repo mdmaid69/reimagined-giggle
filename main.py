6eac398159a3ee990cd40a6132773dc5ca12fd90
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:02:15 +0000

    fetch left post crazy both

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
