aac40aefdeb63559c01ebe1d0c87f983b89f8d22
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:37:41 +0000

    second schedule bit influence fork

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import time
-print(time.time())
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
