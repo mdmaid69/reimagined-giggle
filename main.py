2c3b475db889ddf8b00258476fbd7aa6456502e9
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:08:25 +0000

    compare birth slightly skin spare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
