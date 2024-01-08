80882ffe103cf94d9d009eeeef0d09dd10cceeee
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:38:16 +0000

    warn possibly high whatever late

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-for i in range(10): print(i)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
