e077e1b560a73fbc3e083283a37352c905ea0be8
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:39:25 +0000

    nowhere bet raw contact put

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
