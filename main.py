22b40d007b195a8aabafc0edf6b0ba6e9d44c704
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:37:39 +0000

    smile right comeout consist run

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
