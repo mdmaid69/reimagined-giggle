1440762374b142263c88a1608ab089fe73ce8c6e
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:45:18 +0000

    young inside fashion scream ticket

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
