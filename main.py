a30ef49f14dbd2b9a1030bef4df6c460599e9cab
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:06:32 +0000

    relationship excuse language straight smoke

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
