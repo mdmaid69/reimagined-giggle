96780758419c8027f7cec410577456fd18c3ed69
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:30:10 +0000

    trap confuse slight now school

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  def multiply_numbers(x, y):
-        return x * y
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
