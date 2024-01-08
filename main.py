519e768e950c9a822d975bf7945d4264e39e1440
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:20:40 +0000

    deep top information tape makeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print([x**2 for x in range(10)])
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
