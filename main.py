f216e9d5eed6844cb22a8c6126ba12e9a4d190d5
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:29:09 +0000

    great holdout bang very mainly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import numpy as np
+print(np.array([1, 2, 3]))
