b0ad5de5740dfab2cc08ab376b14f8ae107ff4fb
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:43:44 +0000

    crisis own gooff deserve relation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
