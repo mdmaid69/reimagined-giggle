3d589c6ea3fc7f85a43961263ca0b2d86137bfa5
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:47:41 +0000

    would threaten crazy nice average

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-for i in range(10): print(i)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
