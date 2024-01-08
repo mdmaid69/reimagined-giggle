e13b0f6c38232b1a367d2a01db3628868d155b69
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:46:01 +0000

    discovery suit light somewhere high

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
