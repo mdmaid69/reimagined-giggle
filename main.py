24c9df30eb699e2bb339f35b4a3aadeed6f81301
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:28:06 +0000

    car contribute process scare frequent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_interest(principal, rate, time):
+def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
+import math
+def calculate_error_function(x):
+        return math.erf(x)
