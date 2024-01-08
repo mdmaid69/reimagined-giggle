6ed2655067978f77c2aa55ae7460396c2d8c4575
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:40:19 +0000

    thinkover identify outside push name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
