a5f03d1533fc7f9a23a6ad06d74d2e6ac47e1232
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:35:40 +0000

    look reason health obtain separate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
