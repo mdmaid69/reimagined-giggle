10f05fbbff96396358aa45e9cea58bc979f33ca5
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:20:26 +0000

    fun behind quality food solve

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
