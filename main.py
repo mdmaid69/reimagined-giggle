df4e881fc094f9f64ff30bb4f9216a22e23e4252
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:58:03 +0000

    light start begin pronunciation squash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
