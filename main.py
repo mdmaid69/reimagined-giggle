d13b9f0fb871765bb3b1a93b7d6030df740b7651
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:37:31 +0000

    them shade behind rely advice

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
