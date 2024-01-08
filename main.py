d862a41fa73aa600cc8ebd27fcfcaa64f263ad76
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:59:43 +0000

    child themselves guess Tt fond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def delete_file(file_name):
+        os.remove(file_name)
+def count_words(sentence):
+        return len(sentence.split())
