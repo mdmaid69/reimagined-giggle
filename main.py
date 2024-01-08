9661c73a7a54c73c9ef9376a8f8d69e2844667a1
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:26:19 +0000

    talkback either space Oo child

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import time
+def get_current_time():
+        return time.time()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
