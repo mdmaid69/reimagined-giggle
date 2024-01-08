5f799aa8147e2e01ee09868580deda72eb9cebb5
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:31:53 +0000

    attention Hh better value bend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def calculate_area_circle(r):
+        return 3.14 * r**2
