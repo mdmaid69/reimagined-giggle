df710c75940a037e14e53008f88c1cb4957f85dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:30:50 +0000

    low backwards float team heart

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def get_array_as_set(array):
+        return set(array)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
