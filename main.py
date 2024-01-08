3162a2967bd2fb31442d3fdf45e455005baff1bf
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:03:36 +0000

    drop fit burn familiar label

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
