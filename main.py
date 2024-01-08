9ff934c00e9de906912e9e5f5226762c23726949
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:50:55 +0000

    by compare reduce main increase

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-for i in range(5):
-        print(i)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_pressure(force, area):
+        return force / area
