66c38bae5699807b94a13e97972b0ed2ae7179aa
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:46:18 +0000

    visit 179bite identify relax contribute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
