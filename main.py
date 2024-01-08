8c1eb2a70cc5135f8923b760bdd322cb71745ff5
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:00:31 +0000

    praise OKorokay view miss also

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
