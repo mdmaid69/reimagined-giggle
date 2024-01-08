8bb91c8ed5b1dc87ac98bbd85e32c72ec8713b46
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:17:42 +0000

    nowhere right confused address drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
