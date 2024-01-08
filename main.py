d68305d5e4d8a89a4b01b0b67f4869770e329343
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:00:40 +0000

    include less range Ee except

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
