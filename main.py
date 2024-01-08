f5c433ed08aeacef1d2df94478978eebd3ccb441
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:55:06 +0000

    trousers conduct shine both nigga

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def reverse_array(array):
+        array.reverse()
