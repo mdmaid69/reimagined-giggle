7a12195451a5636a767abe6b5d5ecd61c0fa8d65
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:47:54 +0000

    narrow thing copy occasion draw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
