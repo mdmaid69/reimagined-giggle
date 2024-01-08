cb37f7fcc9aa6a67a83433fe1e2415dad1b6743c
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:18:31 +0000

    impatient habit giveback adjective Gg

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
