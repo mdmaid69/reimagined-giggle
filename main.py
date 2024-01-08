b4c628e1a1cea682c68f7bad6096ce01a73e0b9f
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:08:33 +0000

    bang treatment any begin underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def check_if_array_contains_item(array, item):
+        return item in array
