494a370a1c01d56cdac7accebd2e9a76983f881a
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:19:05 +0000

    mark rain freedom injury mean

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
