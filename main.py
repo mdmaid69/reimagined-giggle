f6d838a2e429862b424817381dfab9939dca6735
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:41:34 +0000

    like concentration adjective turndown worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
