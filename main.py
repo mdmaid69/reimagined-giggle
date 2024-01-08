d2abc833967c5c3e1a666dbd5607c067199dd71a
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:48:56 +0000

    high shout thinkof direct tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
