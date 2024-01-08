72678854ece156ad6ba64dfe0971a6fcd35b077d
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:05:57 +0000

    comeback air protest cut swimming

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def find_min(numbers):
+        return min(numbers)
