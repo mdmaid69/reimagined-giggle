ff61897817f9743e3a70639c7aa06f80d79bdabd
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:29:11 +0000

    movement key Gg popular discover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
 def calculate_power(work, time):
         return work / time
+def calculate_perpetuity(payment, rate):
+        return payment / rate
