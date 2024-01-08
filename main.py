effa4caf186cc08cee7d1b407db135ad65ff4f12
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:26:00 +0000

    otherwise live present natural report

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
         plt.show()
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
