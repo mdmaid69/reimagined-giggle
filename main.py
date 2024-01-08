04931ee037a18a0ed2c92a67e4651082e8cf48d8
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:03:38 +0000

    nearly cry approve stupid waste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_base_name(path):
+        return os.path.basename(path)
