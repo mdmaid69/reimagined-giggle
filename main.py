31eff6850cdd8d95f6553cdfd9ed382033d872c6
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:23:13 +0000

    fit abroad guess recover train

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_pressure(force, area):
+        return force / area
