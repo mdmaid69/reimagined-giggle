348bef204f2af351c1b2e834f8b1c7a9e02278db
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:04:58 +0000

    thinkthrough bet below even convenient

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
