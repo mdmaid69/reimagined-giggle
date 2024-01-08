9688bc058c09c80081a8d34411cc25c37e391a0e
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:14:38 +0000

    greeting come repeat must state

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def is_odd(n):
+        return n % 2 != 0
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
