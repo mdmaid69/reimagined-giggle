3ad452340a97bc890e13e4ccd2b2edd20fce4522
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:28:23 +0000

    fix shallow hurt little kind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def remove_duplicates(lst):
+        return list(set(lst))
