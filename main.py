1f5fbfe517d440b600f20287d68b1ffc83a495a1
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:53:26 +0000

    flight from brush famous refuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def divide_numbers(x, y):
+        return x / y
