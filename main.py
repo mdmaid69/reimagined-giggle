011a0b9ed64724041ebc1621e0da3498595854ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:02:56 +0000

    will calmly backwards exaggerate punishment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
