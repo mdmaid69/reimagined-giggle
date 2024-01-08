5dc1cb13a916d30ab613eeda5d2e83fa89c76b7c
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:05:48 +0000

    people hello uncomfortable affect surprise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
