d50377ea6cfcda6b4d03b3acad536f063d553335
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:03:06 +0000

    live beyond spite object upset

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
