250c7b5d4cdfac9b6320ac86d2ce691b61f2fbae
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:54:06 +0000

    invitation Cc father previous speak

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print([x**2 for x in range(10)])
