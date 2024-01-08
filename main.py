646146f7ef08cc2d9af9567d09357c99c782b3d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:21:49 +0000

    turnup already enthusiastic teach communicate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
