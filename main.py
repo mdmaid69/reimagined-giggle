3d22694069d4193cf9fc3984b4749556eaf5b7c9
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:48:31 +0000

    behaviour break flow succeed swimming

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
