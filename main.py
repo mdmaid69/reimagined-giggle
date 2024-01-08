716fb27a862e22d1ca9d3e354a1a2a6f5358d37a
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:09:13 +0000

    hope watch defend piece blow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
