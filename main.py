d7e85cfb7438108019f2f16b45418b7fa93fce73
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:15:44 +0000

    moment lock exchange plan right

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_array_as_list(array):
+        return list(array)
