d9e5514603e94d93996631e9cac481f3283fa7b6
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:27:53 +0000

    someone band popular state me

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
