8bc5d6700ac541ab3f55ec88d6dcc48b76ab7a07
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:29:40 +0000

    hurry solve colour risk share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
