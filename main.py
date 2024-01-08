a8729ccf7992e56f68a5b6237f400c166c6ee2e5
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:53:23 +0000

    comein circle remember weak wander

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
