73a0b62e7d04587658884ae70e5ddcd898e9b2fd
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:24:59 +0000

    provide organize shade shut earth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
