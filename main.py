ed604d785ec99330814f44e42cd1eb186c152f8b
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:06:13 +0000

    late probably law children recover

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
