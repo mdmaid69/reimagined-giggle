a3deb18125e58ad5047e8fb82878f41dcf48622e
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:45:55 +0000

    occasional combination callback direct she

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import json
+def convert_to_json(data):
+        return json.dumps(data)
