601e4cccfe7d317b3010b4e4e8792bf87fc768f4
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:40:35 +0000

    strange order hospital their minimum

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
