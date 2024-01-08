d3609d7478ddc3a462390c44f75039a8b18db907
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:27:14 +0000

    under software mention meeting manage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
