22864567a748316234a490fe9ffbbd70f4bfe67c
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:43:04 +0000

    after positive state your sport

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
