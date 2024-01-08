59b6d6e79085a93ea0e9396fb53c44083772087a
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:29:59 +0000

    line mark plan different side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
