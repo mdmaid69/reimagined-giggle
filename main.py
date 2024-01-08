942c2c1fc32865475496ff3e8c06354d57e4ab9d
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:27:21 +0000

    operation able cheat rarely yours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import os
+def change_working_directory(path):
+        os.chdir(path)
