529e23ff0690bfdebf8a9960c61a3d539167310d
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:51:20 +0000

    sale distance future sky then

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_pressure(force, area):
+        return force / area
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
