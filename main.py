5e2982261dbd41aaa4910dd8fd6c870a04cabc9f
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:49:47 +0000

    tap classroom over plenty impressive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
