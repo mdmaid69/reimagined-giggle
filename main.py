fe3441ca7937679d5831420147b0007db8ba057e
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:38:32 +0000

    hurry act borrow exit type

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
