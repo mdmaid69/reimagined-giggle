8cb6e9cbc8dddba919017d0fd97a0a6f77f47e57
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:54:38 +0000

    travel risk comeround profit reaction

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_combinations(n, k):
+        return math.comb(n, k)
