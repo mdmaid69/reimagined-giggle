fcf8b5632f1491251377b4d6e82595c698726150
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:15:08 +0000

    difference church majority trust tense

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def remove_duplicates(lst):
-        return list(set(lst))
+print([x**2 for x in range(10)])
+import array
+def get_array_typecode(array):
+        return array.typecode
