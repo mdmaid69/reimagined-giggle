babd6ff4eaab429ade69a9e0d091d3c683313980
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:04:51 +0000

    account progress expect bottom basis

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def append_to_array(array, item):
+        array.append(item)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
