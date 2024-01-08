45212347c696361e74561b18507deeca5ff0f46e
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:06:09 +0000

    centimetre have warn splash exciting

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def find_max(numbers):
+        return max(numbers)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
