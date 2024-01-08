bf9f5979d1f89cb2f87010575b0038c78d9c4fb1
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:27:53 +0000

    friendly creature face press voice

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def square_number(x):
+        return x**2
