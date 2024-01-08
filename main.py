281f349c28c9d1fd53ef474699e457c32932d9d7
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:22:18 +0000

    lot back little further prize

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def sort_list(lst):
-        return sorted(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def find_min(numbers):
+        return min(numbers)
