d1c8acd0bdaed683d0b285fff2a5450fb6d884bf
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:45:34 +0000

    naturally suck opportunity 565end 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
