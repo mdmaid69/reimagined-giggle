93e4a8037eb13fe9a8073484c2754ca0405d668c
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:32:45 +0000

    total plan married plain nigga

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
