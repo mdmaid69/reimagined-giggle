a480f9a52468aab9e27eb53860a373f427a78a57
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:29:37 +0000

    tight move out free share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def square_number(x):
+        return x**2
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
