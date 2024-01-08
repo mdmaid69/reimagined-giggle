70b38f7d91e5264e0bace2d36ecd7fc1b3127d80
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:32:33 +0000

    intelligent deliberately breakfast warm shelter

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sys
+def exit_program():
+        sys.exit()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
