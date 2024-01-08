a2fcf7bd7f3ba253710830aebc6006d9e7c912ea
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:35:17 +0000

    arrange minute pleased shout gradually

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
