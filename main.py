c0493cc242b92bbfdf20d01ad2da1bd795368f89
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:24:54 +0000

    seem sit yes blow brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
