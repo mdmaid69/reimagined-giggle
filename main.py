047858dcf9a36255744ed900c2d28e39453e91ff
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:15:04 +0000

    television criticize goup extremely post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
