62dc57b9354eee43285c477b0610072eb8466d59
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:15:15 +0000

    squash sore lookafter test rich

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  def is_odd(n):
-        return n % 2 != 0
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
