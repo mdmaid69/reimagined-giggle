9361dcda7bd07887520f42cedf91824bc3cc7c2a
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:04:02 +0000

    wing fashion rain brush respect

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import getpass
+def get_username():
+        return getpass.getuser()
