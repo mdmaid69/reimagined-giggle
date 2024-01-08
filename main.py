3627908ecbd6aeb3d6d6136f1edbd1dd4844c489
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:10:55 +0000

    quarter anybody store ring black

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def get_string_from_array(array):
+        return array.tobytes()
