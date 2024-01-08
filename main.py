140a57101f37b13e176e80bdb55b3dec44c4134b
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:42:48 +0000

    comeoff why measure wonder mail

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
