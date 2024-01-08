c4a9409e6e68ccd5441bb167061b78ecd30e2fc2
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:00:14 +0000

    deal in method justice basically

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def convert_to_json(data):
+        return json.dumps(data)
