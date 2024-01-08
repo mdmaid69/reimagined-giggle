f70314a43016b60ce11e73b990f4260c3113c01e
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:09:30 +0000

    clear chief zero develop introduction

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
