c1a423596b8104f57f547b7e0828f1c08d21b7f9
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:37:23 +0000

    criticize 565end this connect flight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def convert_to_hex(n):
+        return hex(n)
