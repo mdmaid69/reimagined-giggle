be594038acfa81d7aad29662cf52d1181ac2370c
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:33:35 +0000

    split ride time fall Rr

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_pressure(force, area):
+        return force / area
