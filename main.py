2f3ca04a5ef637b601744408c5d6b558f7323302
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:06:42 +0000

    passage earth lock repair judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
