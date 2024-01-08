8eddd301027035d8bfeb31b400a9316b94228c10
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:20:20 +0000

    ear bringup maximum plant shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
