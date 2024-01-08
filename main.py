32b73acfb605abf96502fe80660536c41cb53fd2
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:37:04 +0000

    material petrol shine gift cheek

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
