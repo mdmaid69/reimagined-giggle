05e91250293c05d2726f3d8da0d9dadd48f603c8
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:17:53 +0000

    change balance must taste bury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
