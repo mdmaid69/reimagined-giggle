7a609771d33b605e47a43dbb554dde1e89acca96
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:33:48 +0000

    unlucky getaway especially forget immediate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def convert_to_octal(n):
+        return oct(n)
+  def is_odd(n):
+        return n % 2 != 0
