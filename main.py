2e2ff91a88b730b3b0976e2098b6e03e0c4dda70
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:59:45 +0000

    regard fold journey realize top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
