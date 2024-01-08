fdba6c856bea61150a27be03e93b9e9a828b28a6
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:24:04 +0000

    roof always used protest taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
