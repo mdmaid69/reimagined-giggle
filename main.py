bb6ed186fc19d4a4e91f3ce8ae652d8bb6ce66da
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:12:17 +0000

    table shrink perhaps balance plan

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
