8a11fa0dec804b283dfe6f4aba7261562dc69b27
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:05:36 +0000

    exhibition marry handle real block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
