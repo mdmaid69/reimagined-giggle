f1ccfc43bab6db79fff40d91ff400047f68a8758
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:42:50 +0000

    meeting club admire brain somehow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
