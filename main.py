11aa99b338deab40cbad8ba90dc2a15875f15d20
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:26:39 +0000

    raw push anywhere tidy less

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
