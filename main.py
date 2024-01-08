380e6a080ca5db17b8dd0d9fc4939c1364b4c663
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:31:37 +0000

    voice pen slightly praise foot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def reverse_string(s):
+        return s[::-1]
