cae33d741940bd9ea8f816e702c911f502079b12
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:36:33 +0000

    left mystery hospital willing complaint

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
