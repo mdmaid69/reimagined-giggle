e18cfba6987cc975b564a3d831a654dbd57dbd30
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:06:28 +0000

    mood comeover fear tired feel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import os
+def remove_directory(path):
+        os.rmdir(path)
