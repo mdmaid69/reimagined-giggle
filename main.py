f6ef92cdc4bd33cb063701b7688a9fc52a225a34
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:53:43 +0000

    mood flood early shake broken

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
