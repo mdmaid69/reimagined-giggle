ee5f22d6aee8cd42b31d780fedfca62e84b7a456
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:01:26 +0000

    name responsibility runout Cc hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def get_current_time():
-        return time.ctime()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
