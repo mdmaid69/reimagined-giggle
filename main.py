b9797aa11efba095aeb1ad85bcf28666cd235453
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:02:17 +0000

    container that collection obvious breath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def is_palindrome(s):
+        return s == s[::-1]
