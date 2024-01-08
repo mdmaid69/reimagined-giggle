5d4c741b0c0d6999604695fd78b0cedac7499b33
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:14:48 +0000

    birth performance contrast case drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
