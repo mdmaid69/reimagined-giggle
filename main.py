f619b2fa7dce3e9799f8b4e4ad95c528c73fc3ad
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:12:18 +0000

    wish shall car form so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
