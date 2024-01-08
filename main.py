f603a8a1dc8404c59f7ac3ccdebcd6a2cba2efa1
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:24:49 +0000

    rare his skin mass decision

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
