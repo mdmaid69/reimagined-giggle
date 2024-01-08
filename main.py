fc40f4342efa0ee67b9ee132dd01a1584aefcdcb
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:01:52 +0000

    between generous middle admire too

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import time
-print(time.time())
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
