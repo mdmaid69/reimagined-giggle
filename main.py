f64b3e79f4b845d4ab5f9c5b09b46e76eba5bad8
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:31:53 +0000

    achieve speech capital light goodbye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_user_string():
+        return collections.UserString()
