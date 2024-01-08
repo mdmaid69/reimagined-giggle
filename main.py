298a52124dbaae22402643d4aa10a1f4391a8a26
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:35:14 +0000

    common quick season spray fill

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
