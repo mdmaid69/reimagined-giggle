599c21a781c6bbab3b91d64521d04b0198e6399f
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:22:28 +0000

    trade occasion though flood familiar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
