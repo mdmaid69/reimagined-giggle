80e9f86bbe089d103ab6705c3b90af8d916b5ab2
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:10:08 +0000

    street run score heart slowly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
