a9fcf68074bc8aed645dba8808fd21325389d493
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:10:51 +0000

    pay naturally godown church gift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_work(force, distance):
+        return force * distance
