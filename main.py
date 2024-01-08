c58f8440856868ca9fb6068bc8a56cd5a85d1a9f
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:31:54 +0000

    easy opposite rain tap combination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-  def reverse_list(lst):
-        return lst[::-1]
