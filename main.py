ec302d672db264c4063ed2017aba099480a6675d
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:00:50 +0000

    deaf hot soil new young

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-for i in range(10): print(i)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_priority_queue():
+        return collections.deque()
