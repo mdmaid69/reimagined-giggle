4045137f8d6600a8e8e9b459206d2b12a9d771ff
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:19:02 +0000

    pipe ilovedicks confused believe protest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def square_number(x):
-        return x**2
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import platform
+def get_python_version():
+        return platform.python_version()
