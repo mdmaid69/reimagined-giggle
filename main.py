bc97f49bd587b5e37c6ed52b955a4d216705073f
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:49:57 +0000

    possibility hello plastic avoid awake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
