97ef9a38b2ac688c4c39c0855956133b9b67ceca
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:55:08 +0000

    organization health until pleased politics

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
