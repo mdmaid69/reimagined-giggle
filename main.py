c888d178395537731ea9e3bd1799443a78dcee16
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:43:14 +0000

    scared rubbish hit young independent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
