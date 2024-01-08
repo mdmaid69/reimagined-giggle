46b261428220f60e4f382dba111fe058f58f9146
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:41:01 +0000

    black turnup measure pen involve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import collections
+def create_user_dict():
+        return collections.UserDict()
