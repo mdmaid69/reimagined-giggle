50f3dd3d86efdf179c7bae9f62e7b0c353445a0b
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:30:39 +0000

    one accidental her reply after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def convert_to_octal(n):
+        return oct(n)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
