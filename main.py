03827781113684cc3673c40755ac956ab00ec8c0
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:48:57 +0000

    hole such family list soon

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
