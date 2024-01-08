1f1efb44c2e23d30b5ab1fea8df139427e8241e8
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:51:16 +0000

    long doubt means worth turnout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
