762c9da6aba34c417f5c54d8c388650be358f112
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:46:25 +0000

    fork determined risk precise drop

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import random
+def roll_die():
+        return random.randint(1, 6)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
