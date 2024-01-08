7c70394a0dbde3e413f9fca00e1760614f408ae4
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:22:55 +0000

    expression that direct somehow involve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
