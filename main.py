ff93d448f60c979862fccd8f4ce5964f0865cc14
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:05:05 +0000

    giveup true crowded yours spoil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
