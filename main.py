ddb401c32e71c1517b2a61dd3ce7e5bb58108ee2
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:40:27 +0000

    luck near over boil fight

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
