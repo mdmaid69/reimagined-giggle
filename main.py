2f7bad93817ff2f6850c3adf10664366416be48e
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:49:00 +0000

    instant conclusion impressive aware scare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
