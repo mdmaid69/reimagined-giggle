4c2641b684bfb1fed370ed3e4b5deadf78552c4c
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:49:58 +0000

    all spoil great space arrange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-print(sys.version)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
