4db27999af153a736c7ad80b0751e5ed2db3e6f2
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:12:24 +0000

    spin spring follow welcome medium

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import array
+def clear_array(array):
+        array *= 0
