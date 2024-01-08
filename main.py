10ab982d6a0cfa74ed12967cf85556cc733dcb01
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:49:35 +0000

    swim deep grade difference trial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
