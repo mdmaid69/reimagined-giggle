fb1eae7001b6f75f8691f40bef7a870120fbf387
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:53:50 +0000

    grateful dream pay bright the

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
