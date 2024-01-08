17cfb4056b726172410e875e8f9ec2beac61568b
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:40:27 +0000

    glass over usual equipment band

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def add_numbers(x, y):
-        return x + y
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
