1e717511adea10d531d5b9e5b52575fb664f26f5
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:26:21 +0000

    finish nervous cinema tonight rough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_number_of_links(file_name):
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-import sys
-def exit_program():
-        sys.exit()
