d07bd73afc54e1447ddd19f35abadbac264508fb
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:59:00 +0000

    up modern conclusion official official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import os
-def get_environment_variable(var):
-        return os.getenv(var)
+print(os.getcwd())
