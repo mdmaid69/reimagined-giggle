fae4aa8bce95143b0be749a672cf22db803ed101
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:09:54 +0000

    money confuse Nn exaggerate must

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import datetime
-def get_today_date():
+def get_current_date():
         return datetime.date.today()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
