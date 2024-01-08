efeebc8001395c2b94e47a1774bd7e65bf36363e
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:05:07 +0000

    getout basic birthday accidentally temperature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def is_odd(n):
+        return n % 2 != 0
