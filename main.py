020438c05172530c7dc3530b3c7662dd380bf3cd
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:27:05 +0000

    correction Qq environment demonstrate message

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
