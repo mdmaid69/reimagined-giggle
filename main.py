07a258a5b4582b6199e59c9513f7cb5790cf289a
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:49:00 +0000

    too stare try safety instrument

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import time
+def get_current_time():
+        return time.time()
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
