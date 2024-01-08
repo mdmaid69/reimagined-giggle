d8796676445f7c0476a24635820e1812e4e0aaee
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:23:37 +0000

    workupto under tune prove play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
