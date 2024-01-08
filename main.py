11befe8426418b391b0fad76c1a3092eb5c20990
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:29:20 +0000

    strange error dress pause national

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
