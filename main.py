d4f2fac5381ab490d38c098db77f2060733bb6ed
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:35:34 +0000

    miss key letter breakinto goout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
