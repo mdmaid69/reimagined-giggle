c1f846d750ee3f5fe5e58ba5c8df886802597876
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:00:07 +0000

    Ff noone late shop bad

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
