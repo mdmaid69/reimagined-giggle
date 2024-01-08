ec86a25df90aaec3081d42ff277784beaab5061e
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:37:13 +0000

    finger store forward late want

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
