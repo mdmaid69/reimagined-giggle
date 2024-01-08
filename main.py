237de1648f41a1e0867304f1e01b675f4a0f06f5
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:54:47 +0000

    disagree change course below government

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  def multiply_numbers(x, y):
-        return x * y
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
