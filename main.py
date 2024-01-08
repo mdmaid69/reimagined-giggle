7f34d0e24bbce71631221dae23fb79fa9be087d2
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:09:40 +0000

    bottom 567enemy final estimate home

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+name = "Python"
+print("Hello,", name)
