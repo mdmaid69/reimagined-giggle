5844d213c832832a184d885d06c5c726f789cd91
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:03:47 +0000

    between very they target nature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
