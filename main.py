fdbd59139c7ca048a85d5d37599a6de0fc0f3f6f
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:22:20 +0000

    container serious carry light block

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+print(sum(range(10)))
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
