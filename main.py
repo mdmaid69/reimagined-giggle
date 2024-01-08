ed8eb830c37455f89ca74e4b4f8d5373080dc5b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:51:15 +0000

    independent insist ball better expression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
