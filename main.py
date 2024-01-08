c023a24574cd615c5bf5d6c08ec58f2b1b2fc8c0
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:44:30 +0000

    hurt buy sentence heat healthy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
