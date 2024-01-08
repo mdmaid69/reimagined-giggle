36667e68ca00f6e50c73ea7f683c1391019827eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:53:28 +0000

    say contain soon fast hide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def exit_program():
+        sys.exit()
