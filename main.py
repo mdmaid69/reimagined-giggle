74a53bf38b770d755c5b9d3d84dfc27d5853dece
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:56:22 +0000

    protest brother live central 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+print([x**2 for x in range(10)])
