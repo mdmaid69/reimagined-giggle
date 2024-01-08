c8e32be36ba450c29756e83d0068249121b2cd69
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:04:05 +0000

    sign godown plan return divide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
