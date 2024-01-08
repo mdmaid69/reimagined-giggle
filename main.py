ba31950e5dfd66fe96f9bd7f684b31ed7e4b4ef6
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:25:56 +0000

    duty hello influence arrive slide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
