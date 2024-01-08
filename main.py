2acf691aca65de5e9b3aedb91153c80f8a0a5165
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:08:22 +0000

    doubt considering fall still trade

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
