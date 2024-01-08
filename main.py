751527e047f17fac3cab87c44d5f1ada4c2c3ba6
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:58:32 +0000

    close for hour duty speech

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   def convert_to_octal(n):
         return oct(n)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
