f314e99731f37c0b0c58b458f14a5cccf6f8a906
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:53:24 +0000

    near drive hit free rest

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
