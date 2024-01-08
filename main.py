bb0244df525d6253b4c0e5ab61c8cf9445beb849
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:31:08 +0000

    defeat arrive leave muscle score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-print(sum(range(10)))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
