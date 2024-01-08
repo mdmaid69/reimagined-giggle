2c336124327756255d29061026b9563bee27675a
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:18:12 +0000

    culture guard gooff keepfrom wire

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
