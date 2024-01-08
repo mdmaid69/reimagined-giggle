3d802cc776ec06cac802ff8b84913856e3688794
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:17:52 +0000

    love close cap oil waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
