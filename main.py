52001a7a6ba5bf348eb99749b9195abdafb9232d
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:24:32 +0000

    reduction software market basic which

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def remove_duplicates(lst):
+        return list(set(lst))
