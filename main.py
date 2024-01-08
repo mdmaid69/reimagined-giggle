90f3a446e4d2e77446e030339f63a60b1d96d234
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:58:42 +0000

    container home post coat any

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
