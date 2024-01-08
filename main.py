c067efb517cd10bce8db4755ab20b16ab99c9059
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:51:43 +0000

    defend best live correct comein

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
