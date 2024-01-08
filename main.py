ebe63e6d810f27366b72d0ff0857dfed0e8334b1
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:05:04 +0000

    workout his continue track order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import sys
+def exit_program():
+        sys.exit()
