20db80a3f0d54e16696a1b7df17aba5340b843ea
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:26:33 +0000

    watch loud certain thick whole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
