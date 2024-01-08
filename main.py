3d224a87ab15f24324a4dcf5599132047c615d07
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:50:15 +0000

    cold cost load respect tie

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
