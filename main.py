8c61c4b833c5134fdafe0a53e3cf45a9464f750e
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:01:32 +0000

    careless change occasion watch stare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def set_array_item(array, i, item):
+        array[i] = item
