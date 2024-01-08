39b5582d835343680a891b6ddfe8fc82f77e2f4e
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:19:05 +0000

    colour control match interview cough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_as_list(array):
+        return list(array)
