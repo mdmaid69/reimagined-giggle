3f5aa56e5e79cd6ee794dfa78c4c18629e2a452e
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:55:32 +0000

    environment rule young Vv everybody

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import random
-print(random.randint(0, 100))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
