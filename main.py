0cee97ca3d7fe5d2b42bd63a58c62d7ac62d89fb
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:32:19 +0000

    ask return big face gradually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def count_elements(lst):
-        return len(lst)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
