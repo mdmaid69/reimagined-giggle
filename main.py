e69c68fb13fd8411f61987383c4e711a76409f38
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:11:53 +0000

    pause century sound attempt tap

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
