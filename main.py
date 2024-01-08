b7883a8c33a8a6464e10fb82517d7c92a0948f66
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:16:56 +0000

    direct big aware thanks learn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_work(force, distance):
+        return force * distance
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
