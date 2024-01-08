3c0fa1c742854a230c67d66a4f6ceea553a95325
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:00:25 +0000

    allow period new which undo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
