d09736124a35cb9bc188cee2edfb9b925986977a
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:00:39 +0000

    Dd bringback though noisy face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
