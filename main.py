58ae6f951d03ca909fabce924a5e9dc0d3c99ada
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:32:03 +0000

    doubt lookaround leave cool kind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
