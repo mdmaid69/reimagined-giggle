152402eb3a8f6772ed19b78e59038b852b1f653d
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:49:29 +0000

    long question underneath holdon help

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import collections
+def create_queue():
+        return collections.deque()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
