f34d34fbd6c1456ddac09c6de68584521623fd12
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:16:30 +0000

    cool partner limit will violence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
