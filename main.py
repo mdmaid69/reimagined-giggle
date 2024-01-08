03ba22b37485d122c87376675c28f0dd3bd9899e
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:07:02 +0000

    stick plain runinto failure either

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
