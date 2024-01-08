f134663da0ae02672501deaa5d215aba05a2e84b
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:08:10 +0000

    door dry further busy regard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def convert_to_hex(n):
+        return hex(n)
