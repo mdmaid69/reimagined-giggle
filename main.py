5fbd758f41b98673372dd3f4f6b131217bee9d7e
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:49:09 +0000

    play free juice post doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
