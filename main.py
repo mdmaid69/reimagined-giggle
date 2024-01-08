a90388aad948873340d98dcfaff076ba7f05c495
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:43:29 +0000

    how birth praise probably partner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
