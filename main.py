fbdef59dbd13da5b78d481e734062b64fe72b9b9
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:06:25 +0000

    close stamp persuade injury arrive

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import sys
-  def get_python_version():
-        return sys.version
+def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
