3eb388d1cdbdc4c4f3b4b5af709c4cfd39a683af
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:57:42 +0000

    congratulate should whole hers degree

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
