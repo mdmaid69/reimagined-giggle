514b5b94b1339de907f8e1097925684877c290cb
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:49:38 +0000

    border answer thin every talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
