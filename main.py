c32b3a0c2dcf2b6979d48e564559e79147edf835
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:37:00 +0000

    work score row people bush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
