c5464bda3caad536762c48e5523746006994803c
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:39:14 +0000

    duty notice acceptable shade everywhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
