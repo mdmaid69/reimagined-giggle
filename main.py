24d64e8c91b0d14a79756015cf74cab1e1266c7b
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:56:50 +0000

    fresh side near storm sweep

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
