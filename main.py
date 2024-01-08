139431290d71aabf51733b2caa0685b0c73a009b
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:00:54 +0000

    crazy speak note cry along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
