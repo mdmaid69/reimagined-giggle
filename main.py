a851ddafe246dab8af093003d8ab3fbf013f0169
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:18:48 +0000

    help operate today turnon figure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_typecode(array):
+        return array.typecode
