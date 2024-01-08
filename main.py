b5bbc0a2778f882c8b2f29a907504ca28a750921
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:29:20 +0000

    unpleasant pity responsibility decision argue

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def is_odd(n):
-        return n % 2 != 0
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
