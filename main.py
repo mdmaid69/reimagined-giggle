d24b134dbde9695179ffe605c7778c8e318b62d7
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:10:06 +0000

    declaration giveback team protest backwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
