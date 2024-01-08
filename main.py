5ee0cf3e9d56260c1f83a8cb90e6169e230422c2
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:52:48 +0000

    thankyou Hh mark rain man

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
