045ee6741815ae2567f35ed0ab49df0365b45975
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:58:22 +0000

    clear activity oil pronunciation idea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import random
-print(random.randint(0, 100))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
