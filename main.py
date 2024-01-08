c680fc411d43457e5e41b4da70bf4fff39f3f5f4
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:21:33 +0000

    person shine skill beginning interrupt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
