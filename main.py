4f719658cc8b6518727bcf0290a12b6c121fce78
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:18:32 +0000

    formal turnaway neat give ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import collections
+def create_queue():
+        return collections.deque()
