c96ffc94e3697426028c7f503dfc3bdeb1c775e3
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:44:42 +0000

    concerned tall straight rush sing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
