b08ba5bb43426bee72c7b6a389209b46e67114e1
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:07:31 +0000

    move tough story drop wait

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
