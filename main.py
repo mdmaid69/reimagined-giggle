fa43b7e554e2c8e760a54df2d1281aee06d6c872
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:43:58 +0000

    thin future lead runout nature

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def divide_numbers(x, y):
+        return x / y
