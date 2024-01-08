7ede262b36a73fea0ff57b71c5c211fd0520b20a
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:30:18 +0000

    contrast fill even climb fold

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-print([x**2 for x in range(10)])
+import time
+print(time.time())
+def calculate_speed(distance, time):
+        return distance / time
