34e9c571cc42ff75524e4685a239f2dc902ddd52
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:21:04 +0000

    problem house matter contrast score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_as_set(array):
+        return set(array)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
