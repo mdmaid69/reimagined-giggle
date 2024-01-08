d591b842068f5ba4d3f6eda7a2c6451f366c69b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:02:31 +0000

    ear play ilovedicks care today

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_power(work, time):
+        return work / time
