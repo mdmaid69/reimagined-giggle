89c3fa83d15a5c7dbc1b9a8bf86abf8b36d4f57d
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:12:05 +0000

    holdup ready near bang ocean

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  def calculate_area_circle(r):
+        return 3.14 * r**2
