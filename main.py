eea341d35d189bbcc56b103c5f2bf9c626fa8cdf
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:35:33 +0000

    image 570enjoyable appear behind keep

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def cube_number(x):
-        return x**3
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
