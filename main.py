f6b76268c5603dd4c4ed1b4c3e5189a165c73637
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:32:35 +0000

    criticism pen list few distant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_sign(x):
+        return math.copysign(1, x)
