e8c1c414cc59957539ba3d96f8463b2eb52da2ee
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:45:36 +0000

    centre attach father plenty taste

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def reverse_list(lst):
+        return lst[::-1]
