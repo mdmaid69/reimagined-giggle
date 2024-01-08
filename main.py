dd0205afe66e6dbf156ab1b0d66fed0ea7d69fcd
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:55:18 +0000

    legal mean inquire service hungry

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
