451ec1d8297ba4b710f2bb160407888881f8ae65
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:45:38 +0000

    draw 567enemy furniture impatient chief

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
