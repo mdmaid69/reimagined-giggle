ad38a671c0eab01518563aaab236d2c3961f8a76
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:00:03 +0000

    holdon introduction length speed sail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import json
+print(json.dumps({"name": "John", "age": 30}))
