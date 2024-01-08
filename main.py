ec722a45fa5796cdbc945902a81585dcadcb4b40
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:46:21 +0000

    solve illness frightened play Jj

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_user_string():
-        return collections.UserString()
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
