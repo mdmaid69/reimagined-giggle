bca5e93154c1dc95f915f0814a98dc84735c69c1
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:24:49 +0000

    narrow brief process morning warning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import sys
+  def get_python_version():
+        return sys.version
