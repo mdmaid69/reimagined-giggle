c08ab2e2549b18ab5351a0c4a33d7889fdabe7a9
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:55:35 +0000

    happen field contain react protect

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
