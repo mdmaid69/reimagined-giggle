e66daa5a6744c16e1ee9d727d7e657515fd1d7f4
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:48:49 +0000

    572enter visit together anyone argue

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import getpass
+def get_username():
+        return getpass.getuser()
