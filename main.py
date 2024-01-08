fbc7ee991c45c84af0ccd257e107564ba8f9f8bf
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:26:19 +0000

    which chance exaggerate equal challenge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
