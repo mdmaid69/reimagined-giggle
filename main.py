23147170ad1a5eaff17a67be142b9bdca1dae542
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:15:18 +0000

    loose keepoff yes manner public

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def convert_to_hex(n):
+        return hex(n)
