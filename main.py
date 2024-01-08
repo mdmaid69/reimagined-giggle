f0f59c891cb055b08cf5217fc4b1cd433b627fa0
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:10:35 +0000

    afraid address giveup Itself little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def count_words(sentence):
-        return len(sentence.split())
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import collections
+def create_priority_queue():
+        return collections.deque()
