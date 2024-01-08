2f4c7067b660393f2cdee3d46fcb082f4f79aeb8
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:14:12 +0000

    poor turndown ours on independent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
