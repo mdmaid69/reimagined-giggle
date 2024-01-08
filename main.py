95ca4ee5ac4dc3b0645ee6ee2d297dc4ff1cd502
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:12:46 +0000

    repeat frighten environment leg motion

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
