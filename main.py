0131003005846aaa9a5508f90f60c04018c5ba03
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:25:27 +0000

    water speed square length past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
