a57971b7e1c10899da7736ebb79e5bc34adbc35b
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:36:30 +0000

    situation small related passage rough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
