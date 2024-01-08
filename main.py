17f1a20a857bb955e11437dedee9387777b1cd1d
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:36:38 +0000

    treat street upset Ss interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def find_min(lst):
+        return min(lst)
