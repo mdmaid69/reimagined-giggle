977324f535842487d21d5845724ea52fc280a67d
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:04:20 +0000

    own base uniform circle tense

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def find_min(numbers):
+        return min(numbers)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
