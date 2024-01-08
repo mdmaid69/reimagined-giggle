3cf4a50bfa2b45f96e12fd948259dbe1ca10eb9a
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:14:57 +0000

    familiar appeal fond truth giveup

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def reverse_string(s):
-        return s[::-1]
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  def reverse_list(lst):
+        return lst[::-1]
