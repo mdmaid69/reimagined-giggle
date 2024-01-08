516fb6a8f2e7d8721ce8a08b4def7fc5f1ebfaa8
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:40:10 +0000

    familiar second if strong sort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def reverse_list(lst):
+        return lst[::-1]
