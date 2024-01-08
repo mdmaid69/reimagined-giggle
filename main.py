fec7874cd5e43b3af7cef0d2856cce521861e68b
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:56:19 +0000

    home protect comefrom piece disturbing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_owner(file_name):
+  def get_file_uid(file_name):
         return os.stat(file_name).st_uid
