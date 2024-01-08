149abd4cf86bd124028bf5735adf70f0f9d79817
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:17:24 +0000

    pump fierce look safe your

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def remove_duplicates(lst):
+        return list(set(lst))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
