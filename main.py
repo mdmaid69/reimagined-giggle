25b654dc6dd149044dd8c00da708a14f02583162
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:51:17 +0000

    this ready behaviour spoil calmly

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
