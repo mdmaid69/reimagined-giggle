62050f20b2977be796e5b887ee4ee09a67cdadea
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:47:29 +0000

    welcome dry good beside continue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
