8b9985b2ede9df5391db65e01570a8a8e41dcbf1
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:35:33 +0000

    question Bb fence event guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
