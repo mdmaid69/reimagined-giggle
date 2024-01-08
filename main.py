17407821d7d2ff5929990357a44470790f86d8dd
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:02:51 +0000

    married tidy money perfect answer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
