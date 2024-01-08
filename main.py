afadc0b8d1413f3d4d070aff0cb55c90d1a2823c
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:17:41 +0000

    break war comment mouth fashion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
