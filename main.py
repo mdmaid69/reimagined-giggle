34e2979fa268ebfc66ff2e6f24df3cfd4db4a5a0
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:59:23 +0000

    hurry now war unfortunate divide

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def is_palindrome(s):
+        return s == s[::-1]
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
