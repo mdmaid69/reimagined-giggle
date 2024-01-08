c4f5168313300f0a94a2eca017ea9187b908c25e
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:16:47 +0000

    mess only too destroy spray

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import sqlite3
   def connect_to_database(db_name):
         return sqlite3.connect(db_name)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
