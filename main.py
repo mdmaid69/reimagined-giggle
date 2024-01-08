0725cd62fa2f04eba363e679e35a97444720986f
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:07:07 +0000

    escape get live certainly rush

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-print(os.getcwd())
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
