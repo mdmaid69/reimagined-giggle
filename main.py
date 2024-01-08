1b71e4c00d66c0b855ab01f46bfb8b17f5ad14c3
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:04:45 +0000

    here measure attempt table unit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
