d36f30fa3281bfb405f4641090eb820a65561813
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:35:44 +0000

    course final Yy match clue

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
