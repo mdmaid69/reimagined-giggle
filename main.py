c82180bcf4d017753e01ba9a1b460c359191a62b
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:04:34 +0000

    woman badly chin herself main

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def cube_number(x):
-        return x**3
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def count_characters(sentence):
+        return len(sentence)
