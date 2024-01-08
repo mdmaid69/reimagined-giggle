d1b63bb9e36c05eac69bf97417fd7eaf331bf3cd
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:52:25 +0000

    live wear object guess backwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
