468ad14942b13e5b9bf45fa612a4ecc298eae9ee
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:49:54 +0000

    care attempt port consult reduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_string_from_array(array):
+        return array.tobytes()
