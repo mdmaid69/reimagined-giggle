d7b08a0e3f6b489a89db56d5ebc8c755adc0333e
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:51:49 +0000

    call rescue stop fall consult

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
