6a05baf8c0e855d740e32bfa4abff9953b33fc5a
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:05:19 +0000

    material lookback(on) I ear dig

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def count_characters(sentence):
-        return len(sentence)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
