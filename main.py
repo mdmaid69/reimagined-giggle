6ad3208b6182988483d559aad60d2e9b7522d65a
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:12:27 +0000

    legal ilovedicks bet certain callat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
