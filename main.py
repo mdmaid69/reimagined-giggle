8754a5eaf101a97c4095e3c4359c818c5094c71c
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:53:12 +0000

    block usually disgusting promise force

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
