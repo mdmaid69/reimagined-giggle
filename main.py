93daab655b0348aa1706f97d6f9dc1c5ac1ee2f7
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:08:09 +0000

    lunch trade friend cause giveout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
