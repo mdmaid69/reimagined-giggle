4824026bfb05ed09cb509b6375058cbaed7151fc
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:00:42 +0000

    careful usually goaway someone objection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
