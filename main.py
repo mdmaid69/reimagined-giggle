5439482d532902a330a6fcd9110ca62e8568c180
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:12:22 +0000

    favour dream apparently mouth load

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def find_max(numbers):
-        return max(numbers)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
