398a03cb0e4b91b90ae9b8e8935d18f15f38d21e
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:54:46 +0000

    stir cope under just raw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
