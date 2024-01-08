773101bc2e81b5b9f32e81f2e40477be54786d96
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:56:17 +0000

    spring rather tall comeround consider

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import sys
+  def get_python_version():
+        return sys.version
