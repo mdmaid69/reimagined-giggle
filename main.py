2c1d146f502af58dcab98e3e5de6aee58d7782b6
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:59:50 +0000

    attractive might lift music rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def square_number(x):
-        return x**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
