07167b88d462726d8067b548a983706a2f709967
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:55:51 +0000

    well getback public lookup excited

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
