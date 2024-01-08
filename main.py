fba2ca74071f26f2c8561b3aceb0e98f8b874508
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:57:31 +0000

    pattern true relax connection black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
