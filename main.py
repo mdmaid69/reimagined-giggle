e7ff87c188bd787dd76ddcd20a60cd89afe11fda
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:58:40 +0000

    mix wet bunch copy partner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_base_name(path):
+        return os.path.basename(path)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
