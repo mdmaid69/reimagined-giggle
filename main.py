eb0b6f45f68422959a6da52efd0b7233ead81fc2
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:02:22 +0000

    far crowded wear exam refer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
