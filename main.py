eabb973aa3937014c5760cfe486b76f80cf88be6
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:34:09 +0000

    press open fond dangerous price

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+print([x**2 for x in range(10)])
