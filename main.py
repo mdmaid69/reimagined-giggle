800ae21b17af09ed1ce4025efcedb2322836bdf7
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:32:25 +0000

    either ignore theirs amount intelligent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def reverse_list(lst):
+        return lst[::-1]
