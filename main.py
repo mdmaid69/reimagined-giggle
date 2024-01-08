9c867ce31c9c21abf44fd15cec4e6672101a44d4
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:16:57 +0000

    damage seat question concentration declaration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
