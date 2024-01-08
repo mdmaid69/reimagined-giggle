4cd3cfe78c514ba033f36e4adb2121572e53cf7f
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:55:39 +0000

    wait open contact return callback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
