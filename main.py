d7d13c23dcf57b6b24a18ec6acc7ec5a55421d70
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:48:16 +0000

    tune activity when attract interfere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
