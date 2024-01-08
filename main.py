2cb1cad19012a14759eb062116d27f03792a7f40
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:31:15 +0000

    temper protest round divide concentrate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
