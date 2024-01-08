9188949dc797b42cb7b87b2bc032619211ebb02c
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:03:14 +0000

    impressed involve apart leg ever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_volume(length, width, height):
+        return length * width * height
