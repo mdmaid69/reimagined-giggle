3381b6f4106ea99549562fec847bb11815caf5d5
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:13:59 +0000

    rough disturb teach development terrible

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def find_unique_words(sentence):
-        return set(sentence.split())
+def add_numbers(x, y):
+        return x + y
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
