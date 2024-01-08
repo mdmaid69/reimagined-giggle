1d6544d1044a5bfae724912a44bae2718130bf97
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:11:55 +0000

    means new off all fine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  def convert_to_octal(n):
+        return oct(n)
