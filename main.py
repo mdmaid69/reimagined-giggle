d0e6a845b626f7b08458a298d5d1fec530f8d4f3
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:44:33 +0000

    inside during sale danger container

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_queue():
+        return collections.deque()
