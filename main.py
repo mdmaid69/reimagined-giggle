3f6c944547e4d20d118176cc68f6b5494a8f9a24
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:54:17 +0000

    benefit partner worst help reduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
