9919e2baef86b561642f2756a49723cbdf0c669f
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:35:45 +0000

    aware wave communicate event sea

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_max(lst):
+        return max(lst)
