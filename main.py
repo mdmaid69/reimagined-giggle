b838f919a3490b193961e1503594ac5a1d53f05e
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:36:04 +0000

    switch bring borrow heat keepout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
