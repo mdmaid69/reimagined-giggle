08438600e30187caae514783c9101834829306b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:59:37 +0000

    bite operation telephone scream stage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import time
+print(time.time())
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
