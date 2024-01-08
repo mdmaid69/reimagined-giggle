04d39be0b0ec322d168673725dda94fd3eb4d5e2
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:55:38 +0000

    colour appeal authority grand thing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
