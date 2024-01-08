fafe71d6c9b8f5c494117d9a2ffb7912ec8ae37d
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:32:53 +0000

    light taste closely prize polite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-def greet(name):
-        print(f"Hello, {name}!")
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
