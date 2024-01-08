dc3151204f4d76422c956e19a7acf7c390c95997
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:03:00 +0000

    alive live square door report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
