560a0650122565f8b6ca8ff989dbfb14fb774e6a
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:47:03 +0000

    soft male check air store

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import http.client
 conn = http.client.HTTPSConnection("www.python.org")
 conn.request("GET", "/")
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
+def calculate_work(force, distance):
+        return force * distance
