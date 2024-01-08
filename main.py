09b68aa399ba0b57aa5a924d343fb652da6027c1
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:33:07 +0000

    last hide further reject along

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import time
+def get_current_time():
+        return time.time()
