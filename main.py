81e8114035e4ab6df4ac9fef4e74ac32b8ffbc50
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:21:04 +0000

    innocent score better comment tragedy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def add_numbers(x, y):
-        return x + y
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
