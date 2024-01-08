11b20e51ec72d6a026dabe29b2628b1ba142224e
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:57:55 +0000

    bin Bb game change rush

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
