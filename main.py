7b3a24737981b1a03f7ed69803a676580266f733
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:02:29 +0000

    activity such key take concern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
