8e8a314377cb1bebca1589d0e224eb6ff8d1e1f1
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:42:25 +0000

    promise strike which failure high

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+print(sum(range(10)))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
