ee588b8693e9336badac3739ddbf868ef5edf4ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:38:31 +0000

    little not recovery wind arrange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-import collections
-def create_counter():
-        return collections.Counter()
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import array
+def get_array_index(array, item):
+        return array.index(item)
