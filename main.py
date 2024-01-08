cc128bbb7c5d641041a0d7178119d54d9e69bef8
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:24:25 +0000

    university Zz doctor friend smile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def calculate_power(work, time):
         return work / time
-import getpass
-def get_username():
-        return getpass.getuser()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
