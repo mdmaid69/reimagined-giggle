44c050014140a33bb8e90a029f0fbbadf4698a97
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:24:30 +0000

    disturbing begin fall creature child

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
