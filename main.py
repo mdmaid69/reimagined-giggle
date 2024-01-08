dc56a88f03801b748510e92985be8d8197770e69
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:08:21 +0000

    arrival rain to pile point

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def reverse_list(lst):
+        return lst[::-1]
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
