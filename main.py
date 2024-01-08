e5735822a45dfe7cd1c5096a8a3a2621208a08a2
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:43:10 +0000

    bear thankyou apologizeapologise dig talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
