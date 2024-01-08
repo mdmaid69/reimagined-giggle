119bd24cc61f2ede997acdc63771b2fe10819f85
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:39:57 +0000

    maximum figure frame dangerous accidental

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def convert_to_octal(n):
+        return oct(n)
+def is_palindrome(s):
+        return s == s[::-1]
