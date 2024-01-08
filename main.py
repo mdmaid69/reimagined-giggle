0dde68863e1692050256ae5ee5e5a7b908fae072
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:01:37 +0000

    under skill dig breakin factory

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
