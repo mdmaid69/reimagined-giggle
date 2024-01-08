b515a8e65d8bc257702a895ae31fe00aa685df28
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:55:33 +0000

    exactly education pocket clear crash

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import datetime
-print(datetime.datetime.now())
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
