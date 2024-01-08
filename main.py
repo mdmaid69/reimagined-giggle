09df35deb02a4f78f00bd207537d82a2e06e949d
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:23:37 +0000

    continue event still result praise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def is_even(n):
-        return n % 2 == 0
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
