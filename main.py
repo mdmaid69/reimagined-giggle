3ca564022fd7ef9960a4336d086d618e1a7d1f9e
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:16:31 +0000

    warning perform interview sticky ahead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
