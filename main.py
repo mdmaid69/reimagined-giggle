673185ba94a3008fc734029c482f81f4ac008b96
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:46:07 +0000

    simple offer threaten terrible confidence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_list(array):
+        return list(array)
