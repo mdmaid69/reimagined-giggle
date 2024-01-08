153eefaac84cefa2e41589881664f9240ed0d631
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:21:52 +0000

    thinkover popular instruction indeed shoot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def convert_to_binary(n):
+        return bin(n)
