832761470c20dab60bd2f727b093b669bc0cdc20
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:33:11 +0000

    desperate pain for sign proper

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
