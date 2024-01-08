3f1164f34526dc787be966e4087a326844238e42
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:00:21 +0000

    generous with temperature recognize single

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
