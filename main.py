91b3d25650ba026cc5cd73b8777df6191d11d1e9
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:54:38 +0000

    weigh without limit wear bus

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_density(mass, volume):
-        return mass / volume
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+print("Hello, world!")
