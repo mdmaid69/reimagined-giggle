d5c72ffff3077e3128826acaf0b507cd539ca2c6
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:45:35 +0000

    replace carry clue sight remain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  def subtract_numbers(x, y):
-        return x - y
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
