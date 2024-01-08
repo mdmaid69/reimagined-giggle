6416265d03056e1da2fdeb75f7281fa3110f2bf2
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:03:45 +0000

    available offence blue press definite

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def square_number(x):
+        return x**2
+  def add_numbers(x, y):
+        return x + y
