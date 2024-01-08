6ae348aac420fe882576db3e9ac29b63ef50efd3
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:34:35 +0000

    together own float reaction offend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
