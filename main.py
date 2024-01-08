3d8e9bd635a986a479ae04828ec86bf63ae66b88
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:37:48 +0000

    prize power big far black

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
