5ed5f2df09a556ce2e04905af4494024f257ae61
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:48:39 +0000

    become luck today public wonder

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
