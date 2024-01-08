6920ffd11e8ea2de147116ff70215a4ae8a9dfec
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:52:59 +0000

    scheme purpose fun unpleasant loose

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+print("Words:", len(text.split()))
