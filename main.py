ee5ffc582c5369323272f6c99c756663e489747b
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:59:25 +0000

    bringup rich rule recognize flight

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
