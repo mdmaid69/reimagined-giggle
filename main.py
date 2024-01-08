e77aa17c54c2bd5ca3fb664c14d1d61562a54bec
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:05:58 +0000

    side control wide wrong relative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
