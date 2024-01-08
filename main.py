40721881b4d2a2b2b2750a91d2f39a1d31922798
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:10:19 +0000

    standard come bye ignore frighten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def square_number(x):
+        return x**2
