7602ea63f2f1509908a785fbfecf446e71bfc270
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:58:57 +0000

    enthusiastic survive poor splash success

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
