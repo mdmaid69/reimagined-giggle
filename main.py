dc5a61c5480d7a42061696bdfbcb1f9932847989
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:41:33 +0000

    well cause uniform hole busy

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
