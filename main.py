28977dd2b41876c206d1825b64c28abae6bc2d3e
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:28:02 +0000

    until big contribute noise popular

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
