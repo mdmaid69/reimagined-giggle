64de7ff6ce8f09c0273dbb1356e181cfe0af62e4
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:22:34 +0000

    slowly telephone themselves past sorry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
