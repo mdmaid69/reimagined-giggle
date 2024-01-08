c4b28d61a90ffc75959608d23163671a515c6b62
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:39:34 +0000

    book plain choose situation game

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+text = "Hello, world!"
+print("Characters:", len(text))
