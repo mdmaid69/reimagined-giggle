b539799732c47fc165bb55c0904e10c73bd5564d
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:44:22 +0000

    influence concentrate complain bottom cook

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
