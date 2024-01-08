73048e0fe715c05fe6c851d888af5ffe46017f0c
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:07:51 +0000

    excited when practical entry nor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def split_path(path):
+        return os.path.split(path)
