19769e1c69c8bd899a9c8b8c3d4cecae639aa6aa
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:09:43 +0000

    hot imagine takeout thinkover flow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
