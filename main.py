3be0d429b8d8a53f8f892ba73859c85b1b8328fa
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:16:40 +0000

    excellent Yy behaviour promise chat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def reverse_list(lst):
+        return lst[::-1]
