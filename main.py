bbe75659f5bb79a85bd53fdc034a12011483e565
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:54:35 +0000

    freeze switch they responsible legal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def count_words(sentence):
+        return len(sentence.split())
