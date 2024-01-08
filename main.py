5f41dce62b3cb5b557e3d9991513b74833a1e40a
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:27:11 +0000

    pump no near keepoff slice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
