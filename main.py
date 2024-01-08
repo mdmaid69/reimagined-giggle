ccb87fba003622456cb787a5e6077f8d2572c2fa
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:19:56 +0000

    tongue raise rent fast normal

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
