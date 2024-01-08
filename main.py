7c85abc15a6990d8eb0ff119d17bcd5e1e00379c
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:16:23 +0000

    exercise thing open shock cause

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
