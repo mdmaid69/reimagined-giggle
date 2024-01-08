e661a3925dc45d92f7998db87597a83d08682520
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:46:27 +0000

    stone wave see married perform

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
 import random
-def roll_die():
-        return random.randint(1, 6)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print(random.randint(0, 100))
