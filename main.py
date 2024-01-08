dac9c1f5f12c99a83640e6afd97ca8b4806bc46b
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:45:48 +0000

    determined interfere yes throughout what

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import time
-print(time.time())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
