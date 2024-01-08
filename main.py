e335d719e178d505dd062371fc6ea1807b529886
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:12:35 +0000

    pen formal lock near match

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+print(math.pi)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
