c3a45dc9ec510b4405cd66c918888f45bd22074e
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:18:53 +0000

    shout disappear fault shop opposite

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
