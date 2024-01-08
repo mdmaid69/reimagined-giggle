ba2189cbd5fa65d5a245e317a162e66bf6c54e59
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:11:23 +0000

    than file unable ring most

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def create_user_string():
+        return collections.UserString()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
