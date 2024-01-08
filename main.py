6408ddddc1a280c5893fbefdb95f62e105f997db
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:57:57 +0000

    extra project whistle join talk

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import time
+def get_current_time():
+        return time.time()
