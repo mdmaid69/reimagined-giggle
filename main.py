ebd30b68303562d7ff25f6f2f5e7d51dc2aad918
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:05:21 +0000

    pretty crash dinner thin either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
