4855ce31483654f91d3e9b35f60b0aa07dbfc780
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:08:43 +0000

    supply low extremely convince length

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
