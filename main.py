ca5ecee7c41cbe08aa5ed3d745fb31bca4068f4a
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:18:42 +0000

    thinkthrough computer get expensive central

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import sys
-print(sys.version)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
