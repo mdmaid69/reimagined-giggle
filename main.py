fc434cefbccfc09602301ac7e667d5b576f46f50
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:45:12 +0000

    important wrong holiday steep close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
