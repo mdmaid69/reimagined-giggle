ac6de36509aca355a422d5dfc70d7233cc33d284
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:08:15 +0000

    challenge takeback comfort hope away

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def find_max(numbers):
-        return max(numbers)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
