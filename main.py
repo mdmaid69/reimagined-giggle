7d44dfbf42cbf60a9d6c317fb8474e5e6e6f59b7
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:52:44 +0000

    tonight introduction tidy basic task

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import array
+def reverse_array(array):
+        array.reverse()
