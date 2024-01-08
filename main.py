61004217dfa0f3ed0a708354b08d7bb5f2c43bd3
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:41:44 +0000

    suck present test better all

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import time
+def get_current_time():
+        return time.time()
+def find_max(numbers):
+        return max(numbers)
