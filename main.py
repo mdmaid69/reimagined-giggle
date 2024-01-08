536367985edd5d7abaaca88b22808c3d86ce42c3
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:02:33 +0000

    size takeoff intend negative write

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_int(array):
+        return int(array[0])
