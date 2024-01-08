4b55a2c80b4a42866cccb4d4dd1cdc04550e2054
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:25:14 +0000

    avoid anybody week practise if

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
