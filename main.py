fa84a4044ddf1a34071c96c59bd8339b396b9c77
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:34:20 +0000

    thick empty along unlucky name

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
