ea493098e47c9ebb8a82cdf59e81c2d3cb27592c
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:38:54 +0000

    impression makeup getoff accidentally trousers

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
