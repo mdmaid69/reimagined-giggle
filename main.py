e76c07379762d7d09560423fe527ac9c2edcb9ca
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:05:18 +0000

    travel will front fail set

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def convert_array_to_string(array):
+        return array.tostring()
+text = "Hello, world!"
+print("Reversed:", text[::-1])
