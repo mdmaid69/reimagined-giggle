8dfa7604a1f18b8e3390802cc627342dd80acd46
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:48:19 +0000

    true sport imagine foot damage

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def find_union(list1, list2):
+        return set(list1) | set(list2)
