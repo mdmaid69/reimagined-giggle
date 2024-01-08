bdc43748711d48bdfb2ccf4ad53b47a0c168d004
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:33:26 +0000

    determined bringforward suckme picture watch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  import sys
+  def get_python_version():
+        return sys.version
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
