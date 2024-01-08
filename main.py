81c4e80e37b006c28aac45e4368caedd08df989c
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:04:55 +0000

    regret offer manage lay standard

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_as_set(array):
+        return set(array)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
