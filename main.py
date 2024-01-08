bd217ad61b6ce392474dae463eba40f4a6c238d6
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:56:37 +0000

    temperature centre concern step reliable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
