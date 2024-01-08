71e6573668e63ff1c0b976a93126a12dc6619b85
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:07:38 +0000

    congratulate wild suggest lookback(on) dear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def remove_duplicates(lst):
+        return list(set(lst))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
