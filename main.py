4be79d3bbf7b1bb2f45132b8271be50176cc13e7
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:55:26 +0000

    right imagination destroy thinkthrough drawing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
