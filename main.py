d1bfaa1044c00a90cc34c18b25938745c5c6617b
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:29:43 +0000

    design quit acceptable clean hair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
