35b86658303ab9e15f7ae34c32adb666c6dc38c4
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:50:00 +0000

    used thick Ff hollow ordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
