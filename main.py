2e8ddf112f813b929c38920e282012a6d96a674f
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:06:18 +0000

    vote soil claim great art

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
