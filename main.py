ec9980ae0bbfeb79939b71074e97c942ee3458b5
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:43:58 +0000

    anybody equipment shoulder centimetre stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
