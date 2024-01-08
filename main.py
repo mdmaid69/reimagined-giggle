2ccc1c905d72ecfed7a67dcb9e891be67523e819
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:09:51 +0000

    phone frame runaway trust past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_speed(distance, time):
+        return distance / time
