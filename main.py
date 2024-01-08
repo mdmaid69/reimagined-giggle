1f86cb3c790e27409e6b87f3cfcbdffa3e3f2e00
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:43:11 +0000

    during fail advantage ready floor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_work(force, distance):
+        return force * distance
