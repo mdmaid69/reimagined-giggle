7ac7db7271fb267bd514d02ad453992885150519
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:15:48 +0000

    understand Vv cause accept modern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
