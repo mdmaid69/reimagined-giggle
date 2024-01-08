ad26d017a570f3bb6a8936d4c4de3b7e0785a203
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:52:17 +0000

    remove often from interesting choose

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
