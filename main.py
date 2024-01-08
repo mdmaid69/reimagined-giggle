5c04d2209f18f34e808fdffc8b3ec5051637deae
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:30:41 +0000

    make chase official sore interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import tensorflow as tf
+print(tf.__version__)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
