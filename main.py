d505d5806e2da4c4bfc5e9a20aa45d4960cdccb3
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:40:49 +0000

    paper fairly giveout Rr rid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+print(math.pi)
