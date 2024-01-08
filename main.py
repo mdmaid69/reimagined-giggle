13dd605aa2350c9eacb7e2fb2d17c1f0760fad5d
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:03:42 +0000

    shopping excited concern even oil

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+i = 0
+while i < 5:
+        print(i)
+        i += 1
