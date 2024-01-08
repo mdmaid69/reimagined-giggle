f768612b20d81078309b7834a69fd1b281f32276
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:41:14 +0000

    load its knock few recovery

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
