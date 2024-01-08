c1f5b72709da2a26a10afe88b187ee13ccc9d941
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:57:37 +0000

    pretty out buy he anyone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def find_max(lst):
+        return max(lst)
+  def convert_to_octal(n):
+        return oct(n)
