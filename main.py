cd603e250df591843406c2dd5e04208d79cae79a
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:00:29 +0000

    design simple succeed shadow use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import time
+def get_current_time():
+        return time.ctime()
