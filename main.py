9a5d7fd9eb916ba8162377c0c499ac0182705bdf
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:04:10 +0000

    flat young band pass past

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
