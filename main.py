a2e06cf953bc3d9822cabafac6ba21ecaea59ac8
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:00:48 +0000

    breath prefer pride arm mad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
