137a306ba8378d4029a13513feafd96a26c449b4
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:24:50 +0000

    entry Zz large shock together

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def square_number(x):
+        return x**2
