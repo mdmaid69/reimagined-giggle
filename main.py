e8bec8eca911d9d04ed5f00f3a93e08fc0d6e409
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:55:47 +0000

    wear line gentle relationship children

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
