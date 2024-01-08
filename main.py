3585b6c49afc5acf66ebd53666a8e05cfaa442ed
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:22:49 +0000

    hit show platform brush post

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import platform
+def get_os_info():
+        return platform.uname()
