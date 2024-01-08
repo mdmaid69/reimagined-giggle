79e337cacb4202f50116f0e8707cd7e66ec41724
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:33:20 +0000

    means trial after callback out

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  def subtract_numbers(x, y):
+        return x - y
