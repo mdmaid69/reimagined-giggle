eb6168bd9c72ac846a08f603e4cf2a32490b1955
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:09:53 +0000

    story oil positive organization congratulate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
