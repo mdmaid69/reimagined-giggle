c77385a38b0c6b27c1203ad187a3c20f1d2454da
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:22:23 +0000

    callout want common print complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_array_as_repr(array):
+        return repr(array)
