c7d7cd0745c9394a9ce1793409d276d33c092531
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:55:32 +0000

    fun ahead expensive question comeup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def is_odd(n):
+        return n % 2 != 0
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
