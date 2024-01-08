18badb2cfcf80e521b3f23d82f3e32758f6ac362
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:32:22 +0000

    wash sure jump brother colour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
