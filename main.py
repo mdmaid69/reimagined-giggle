2ab481fb2be05504fc3b35fd581ee9feef2e3483
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:18:28 +0000

    goaway obtain telephone case cost

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_power(work, time):
+        return work / time
