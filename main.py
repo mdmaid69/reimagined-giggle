80f3d11f69570f3ae171a0e92c24f93babf9ba91
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:09:30 +0000

    organization walk person reasonable delicate

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
