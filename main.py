03c07f595685bbcb9ebc18748352ac3eec35c325
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:49:40 +0000

    children skin aim minimum rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
