1b7bac258d8a0ec4f8daab05824c2fd550b25159
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:42:10 +0000

    declare practical admit plan matter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
