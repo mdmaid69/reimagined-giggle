cb0da347c140471727ea453d3ad400889443486c
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:47:18 +0000

    wake file provide accidentally morning

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def find_max(lst):
-        return max(lst)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
