2408a4099109a03253e6bbe80fd3a6c4aa3d3fef
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:00:49 +0000

    congratulations decide well flat sell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import time
+def get_current_time():
+        return time.time()
