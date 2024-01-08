c53f59487a650381be5eac6949f625c074c080f8
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:57:10 +0000

    excited sometimes wild thin naturally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
