b9f66b16e86027ef84755cb425eb48a29af9b01d
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:04:50 +0000

    off closed trust beyond average

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
