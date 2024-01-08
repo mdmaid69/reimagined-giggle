6582f76fc9ab8eb00d46a3c285a94d071db21700
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:27:18 +0000

    lookaround blame exploration shrink be

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def reverse_string(s):
-        return s[::-1]
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
