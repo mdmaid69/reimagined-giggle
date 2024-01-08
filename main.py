6166375583974172c62ee09236faf78cb5d21cb5
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:26:23 +0000

    reliable top night forget drive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def sort_list(lst):
+        return sorted(lst)
