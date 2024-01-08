08d36025a70b79b9e51d8dff85aa1df43c093414
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:54:45 +0000

    still calmly explain star safety

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
