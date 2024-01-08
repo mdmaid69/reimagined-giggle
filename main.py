40eedc5167eab849c228bcfc360f2b436c2ed60e
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:30:45 +0000

    shame none meeting different join

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+for i in range(10): print(i)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
