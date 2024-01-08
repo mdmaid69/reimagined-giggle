fb3f313a42f9577f84df8658944f6a7f4f902937
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:05:30 +0000

    whenever mixture whether damp fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
 import array
-def reverse_array(array):
-        array.reverse()
+def get_list_from_array(array):
+        return array.tolist()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
