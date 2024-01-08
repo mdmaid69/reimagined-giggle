f02f55f2b4f966ec187d49ca771d8904d4cf86ec
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:48:37 +0000

    floor someone where push stamp

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def cube_number(x):
+        return x**3
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
