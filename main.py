452746290724b644c2cd7cacae2baa07ab1562b4
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:57:43 +0000

    whistle way waste improvement occur

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
