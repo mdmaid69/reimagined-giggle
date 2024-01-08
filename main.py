a90fb71970f77783153946eb4ecb681d4fe82959
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:15:15 +0000

    scream transparent trade replace close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
