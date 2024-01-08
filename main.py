5bc0b3cd108237c9cc02698aa6abf9d8dd024ab7
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:29:56 +0000

    dream cold hear keepfrom pretend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def multiply_numbers(x, y):
+        return x * y
