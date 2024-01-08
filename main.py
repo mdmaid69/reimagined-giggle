1dff960d40fd6acddc45bdb97ff5e92d0025a98c
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:27:20 +0000

    perhaps stage declare hold tell

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def multiply_numbers(x, y):
+        return x * y
