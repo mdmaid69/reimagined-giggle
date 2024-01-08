87f6cf95912d37f4c27c0ab3b1fff38fd6e2b94e
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:49:49 +0000

    about people watch same pin

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
