7224c6b27fc3a7e30a0e0b3fce68f1ef1afbd38c
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:47:42 +0000

    threaten school difference motion straight

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
