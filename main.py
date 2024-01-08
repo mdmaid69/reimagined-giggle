df8c3bab576d456d9e381019109308fcdc6b4b2e
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:26:54 +0000

    poison take safe nerve thinkof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def find_max(lst):
+        return max(lst)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
