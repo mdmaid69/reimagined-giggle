194993005d9dec97fcb48bfb73616640419b12d9
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:46:13 +0000

    appear swim line part especially

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
