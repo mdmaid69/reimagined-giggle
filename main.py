b04c8d77293ffe57f8c410a9057f867b14687a73
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:08:16 +0000

    sell take tomorrow run juice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def square_number(x):
+        return x**2
