4944b74e788ea09b3d17dfe44e0184db53d64e8f
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:58:46 +0000

    slice attract welcome godown bring

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
