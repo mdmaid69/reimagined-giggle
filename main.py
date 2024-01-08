d69329b86ed49be15640fd003e688b1196ca7d16
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:48:00 +0000

    succeed related fun cheat store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
