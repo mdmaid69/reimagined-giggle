6abd5959296f714914be38abc3be9d4563fe3c3a
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:37:32 +0000

    sweet takeout obtain work invite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
