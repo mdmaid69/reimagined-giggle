36b086c23a2b3d0437d542f443cac5ff9140a89a
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:46:26 +0000

    collection introduction knowledge remove murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
