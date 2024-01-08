fe0bb73239f756e860161d4a7f4af327676be98e
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:42:42 +0000

    watch capital in tall be

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
   def check_if_directory_exists(dir_name):
         return os.path.isdir(dir_name)
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
