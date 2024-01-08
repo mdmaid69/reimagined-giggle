7990477b32c1783c8e6e5b2ee6b9a977a112b820
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:08:33 +0000

    second refer intend makeinto regional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def is_odd(n):
+        return n % 2 != 0
+import logging
+def log_message(message):
+        logging.info(message)
