dcf3d97752f1fc717fa1492c7a3ec6c870fbddff
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:51:14 +0000

    hole clean quality shut human

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  def remove_duplicates(lst):
+        return list(set(lst))
