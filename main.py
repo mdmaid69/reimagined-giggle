d193caedf2e52c1e09ec660e6cc1d6cd3de2e011
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:25:10 +0000

    wear read point various mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
