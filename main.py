f13c5e124c36d0b2f6b104e7c064fa22f1f98cca
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:52:31 +0000

    properly anxious bank difficulty back

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def divide_numbers(x, y):
+        return x / y
