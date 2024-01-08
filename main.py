11238f8c52ae6084eb93253f396a44564046d0f8
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:43:22 +0000

    freeze turnout who condition rise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
