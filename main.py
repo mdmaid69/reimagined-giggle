a6d654164b574fbcb44acfa9e98820e9e34196ab
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:15:43 +0000

    worry cheek handle complete nonsense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
