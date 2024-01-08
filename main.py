ded7962c921fd2b585ab6a9bcaad915ae2ff5b81
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:41:06 +0000

    familiar of complicated extraordinary callback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
