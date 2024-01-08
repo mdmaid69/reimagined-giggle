ac0d563805dbf4f9e7658e10c3cb271ef1b4687e
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:44:45 +0000

    test certain genuine jump myself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
