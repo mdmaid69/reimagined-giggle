08a8d4bb9a6051dacc4a90a3e39289d91703da0b
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:37:18 +0000

    beat recover steal duty fasten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+print(os.getcwd())
