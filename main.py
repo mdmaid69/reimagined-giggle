487f43936a5196f338ff3d2320dbd93cc400fb4b
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:19:45 +0000

    anywayanyhow inside exercise further best

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
