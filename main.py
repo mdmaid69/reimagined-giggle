78f43c582bdd745502122a16f083c904764a7d6d
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:12:49 +0000

    limit debt problem confident stay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
