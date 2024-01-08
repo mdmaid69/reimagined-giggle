17bc63015e278648b5ba1aa2ae1856efc902c7db
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:40:24 +0000

    suit doubt whisper announce married

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-name = "Python"
-print("Hello,", name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
