f3d63f32e2aa3b184149f0a0b1ebb1c9797d2706
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:00:48 +0000

    plastic far comment pay taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_itemsize(array):
+        return array.itemsize
