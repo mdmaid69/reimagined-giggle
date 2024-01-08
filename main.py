d5e7dbb6e720d64af6e3d20a2864665e5d25007d
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:17:40 +0000

    war calmly brush education mainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
