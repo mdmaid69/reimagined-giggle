377bda2bd61c4500a2dfbe6caf4e5628ae2dba67
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:44:07 +0000

    calm regularly save entrance explain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
