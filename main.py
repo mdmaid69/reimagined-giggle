7d4af88765c5fa83b014670b9fddd4704a191ade
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:07:09 +0000

    woman description stone star club

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_power(work, time):
+        return work / time
