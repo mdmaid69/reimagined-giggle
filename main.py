678b5d0b9d971e2f809d9225c241cabcf12898ee
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:46:52 +0000

    exchange both recognize tight control

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
