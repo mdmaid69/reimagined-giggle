0da0bbf77938c478dbaa4e37797bbe6d092c70d7
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:09:27 +0000

    night colour crowded thick round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import getpass
+def get_username():
+        return getpass.getuser()
