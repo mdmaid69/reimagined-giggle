f19696695737f0d8b04ab1df3d5828630e34c081
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:10:04 +0000

    tell pretty card home air

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
