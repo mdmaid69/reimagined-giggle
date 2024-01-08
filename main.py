5777fca2fa9aa0624ac2a5ca558b1b325e81bf61
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:44:19 +0000

    rubbish proof loss wave may

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
