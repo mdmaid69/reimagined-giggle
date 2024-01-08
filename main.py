823ad2fc897476713dd2031b2f2dfecec476ca71
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:54:34 +0000

    number angry average goback(to) find

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def calculate_ceiling(x):
+        return math.ceil(x)
