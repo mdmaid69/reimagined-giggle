f7ec8c1cd53fa43a2088a8e55ae5f0ab9984d8b7
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:06:31 +0000

    blind immediate this practice anxious

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
