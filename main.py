ff3d82631e5e24be0836ce965b2fde294c2c56a9
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:43:52 +0000

    war passenger something no gift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
