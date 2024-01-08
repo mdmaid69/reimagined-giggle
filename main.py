11d02ade0cff8217a10af3948796a9ebc4f94eb2
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:45:42 +0000

    introduction notice culture her story

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+x = 10
+y = 20
+print("Sum:", x + y)
