dfeb15a862a1de9e1535b5a1aaed7884f53ce1f9
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:44:58 +0000

    shame cheque challenge trick level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
