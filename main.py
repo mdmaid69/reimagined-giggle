5bf68fe61ef002b88a841abe725b1596e2ad2eb9
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:18:47 +0000

    introduction benefit giveaway uncertain border

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
