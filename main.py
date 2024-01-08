25a9dd21d158e3cc83d0397f40758ef58459dc10
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:18:32 +0000

    team boring explanation breakout praise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
