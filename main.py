06120a36ba80b7557ccf73623898f0b540953133
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:45:22 +0000

    mean present improve change truth

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def is_odd(n):
+        return n % 2 != 0
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
