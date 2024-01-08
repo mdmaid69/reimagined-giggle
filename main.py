003ddafdf1df4a4d5f962f28abcd4b08ea542d7d
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:56:08 +0000

    refuse tonight strange prefer Ii

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
