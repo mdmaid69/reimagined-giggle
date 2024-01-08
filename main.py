f01c82e69e7cb7ea9529c19f84ee829f4e9ecb3d
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:18:54 +0000

    beside split sense neat goout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
