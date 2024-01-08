21b44a0b2be98ee7a26fa4c6fa19366158c15ff7
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:56:58 +0000

    journey programme smile it callout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def cube_number(x):
+        return x**3
