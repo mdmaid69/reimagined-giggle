0943de7e9773ea3ae9b242ef8582691c95054a95
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:47:06 +0000

    part voice best question key

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+print(math.pi)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
