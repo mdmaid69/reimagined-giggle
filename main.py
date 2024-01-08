ec7792cabbe0e5791faeea9abc43d6e10a28d4c0
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:08:38 +0000

    emotion instruction get some smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
