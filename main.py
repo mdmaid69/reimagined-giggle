161152130d55b548ef2b96d69b73f9edf6ca1e40
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:01:41 +0000

    print square honest perhaps along

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def add_numbers(x, y):
+        return x + y
