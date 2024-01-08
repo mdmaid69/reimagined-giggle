5a9df4cde5a4b2cc8ba4c9e21176ff0fd3c7accc
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:30:22 +0000

    spread runout team check message

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
