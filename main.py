ba4ca628a9b6a0dacdb851ec6a9f0fe2082c6a97
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:22:27 +0000

    under confuse we chat stay

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print(time.time())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
