059bc00765d8160d64f14cb81d3c3b7e23c21a02
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:31:32 +0000

    reliable secret over high wire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
