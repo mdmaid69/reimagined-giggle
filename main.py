55fde3a996258a36d51806146ab069636258a8eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:50:09 +0000

    intend thin near dangerous false

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
