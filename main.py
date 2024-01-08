f1337ad32b48e9e3b5f7f672ddba5107055c5ff3
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:55:30 +0000

    century exactly rubbish communication distant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
