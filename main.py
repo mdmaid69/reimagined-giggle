b16fdc2eb2180d32b9de751ecde0cdb1bb150e6b
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:43:14 +0000

    with separate plan morning bill

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def square_number(x):
-        return x**2
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
