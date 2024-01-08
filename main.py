29c51e48fcf48148d07c37dd4831be655d024d88
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:26:38 +0000

    television excited thank increase fault

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
