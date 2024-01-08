a917570683b329fd160d86bbf64e6aa932bcdea1
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:16:57 +0000

    among lonely nothing benefit lucky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def is_odd(n):
+        return n % 2 != 0
