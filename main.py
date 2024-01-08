dcfd384704619b917d37e3f3b7866d1618f9929c
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:55:30 +0000

    bath breath class calmly calm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
