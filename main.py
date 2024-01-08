50b6d0a71adc4e562da7e175a9ac80679abf3230
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:08:55 +0000

    we tap regional there scrap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
