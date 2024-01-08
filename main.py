3de94a38310d01d0b8e230bce55e2ca537b0ebbf
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:29:07 +0000

    comeout sort charge pipe play

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
