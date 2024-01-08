b6a66641f893a03bbe2eeed31807bcbe9cd51a11
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:03:21 +0000

    Ee sell favourite from Zz

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
