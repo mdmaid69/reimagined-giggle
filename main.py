8468d0eede512a198e5839d2a8202e904462b124
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:13:08 +0000

    late admire properly mark holiday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
