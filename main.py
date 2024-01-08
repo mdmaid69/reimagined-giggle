591714efd7eb3c0f18162f9132db312b6409dbe4
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:53:30 +0000

    geton various match let direct

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
