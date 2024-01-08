15257f67842b7c2c4d97f772d3ff304dc92b799f
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:54:14 +0000

    fear left sing instrument each

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-for i in range(5):
+i = 0
+while i < 5:
         print(i)
+        i += 1
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
