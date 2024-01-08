942aee5318a9201bd58d1a21cac3dba16db2dac1
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:14:17 +0000

    set exaggerate squash divide upwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
