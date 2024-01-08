30ed3a7bc7476ae5fd68f4454154168a640de4d5
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:31:17 +0000

    special workupto recently nigga fetch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
