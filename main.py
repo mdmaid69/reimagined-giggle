0ab93201b3413330b8d07d4e0bafbe003fdd0fd8
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:06:28 +0000

    idea near lend raw comein

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
