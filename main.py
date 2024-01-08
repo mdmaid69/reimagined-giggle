1a7e32e5d2ed2d1cf3ed5e230807f4fc304e0cf4
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:11:22 +0000

    post compete clue breakinto continue

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
