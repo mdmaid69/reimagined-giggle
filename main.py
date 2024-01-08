a939551d47ab49f41c68393c3cef015df2f6ef68
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:30:31 +0000

    scared sure tired steal software

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
