74bc95ace12551d990a3b6b9b5ec7cea0fd49c1a
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:41:11 +0000

    damage mood star dinner talk

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
