945d0afaa634deea6970a853f320b8a738298c39
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:27:12 +0000

    pin within play quit call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def convert_to_octal(n):
+        return oct(n)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
