aec62b955a90d8037689419f2deaf55da63b7372
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:14:10 +0000

    runaway string fight tight summer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
