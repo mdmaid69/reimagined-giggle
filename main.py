38dace238e34080274f2f72a96154b885fab884e
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:45:30 +0000

    course bottom need group runover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import sys
+print(sys.version)
