2f3476eab8b0abc433aced104c3f9991d6b11867
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:22:37 +0000

    public cook bag wave cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_set(array):
+        return set(array)
