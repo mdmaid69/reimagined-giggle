6d6dc148dadfcbeb6304b18fb831a1248d30b8c5
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:21:39 +0000

    567enemy pleasure disgusting Yy nearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
