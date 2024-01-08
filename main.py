06034c4fc3f0f2facd7d78a9581f68e4cd476e2e
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:15:35 +0000

    popular bus generally describe frequent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
