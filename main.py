0bea8d1bc1918368f847da8a21195b3dd2a835e2
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:03:09 +0000

    bus account failure equal Oo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def find_min(lst):
-        return min(lst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
