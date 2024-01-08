b0114f53edc2bf08c453fc4b1837ed6a77c7669a
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:40:51 +0000

    equipment ashamed moment else lonely

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def get_array_as_str(array):
+        return str(array)
