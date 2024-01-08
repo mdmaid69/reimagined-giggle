bd5da454f9093fb07e082c540783ed0475152f37
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:00:41 +0000

    quick treat late friendly Vv

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def is_palindrome(s):
-        return s == s[::-1]
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  def square_number(x):
+        return x**2
