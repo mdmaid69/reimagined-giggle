8e40db8d623845c0e516ceee303b1fb8f5199008
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:13:36 +0000

    same depend life dare reduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
