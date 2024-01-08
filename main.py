c9609d2dc78c1db37f331b5cd66ba4c5cfbe70ad
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:59:54 +0000

    smoke couple contrast idea truth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
