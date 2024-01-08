3383561e14dc6f9e89030514f4b15616379c2b20
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:16:48 +0000

    brown service pity Qq brain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def is_odd(n):
-        return n % 2 != 0
+print([x**2 for x in range(10)])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
