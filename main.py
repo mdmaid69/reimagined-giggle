55f9757acd61a2a9c8e597d33739850a7dc5415b
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:01:16 +0000

    trip phone kind legal gentle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-print(re.match("h.*o", "hello world"))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
