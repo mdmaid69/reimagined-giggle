fc7a6fd560f63381f9ec35e356a3b5f48d2ca062
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:35:00 +0000

    comeback under rare accident inquire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
