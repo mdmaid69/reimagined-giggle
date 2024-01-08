7315adf8e2e32a3d90fb16cf2c3249c90cd7af5d
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:57:50 +0000

    expect sick passenger address television

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-print(os.getcwd())
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
