65e5e935c7521be7343b82e2cc6ed041147cf370
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:33:58 +0000

    building bath moment help back

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
