c2bd3408ae287356c7198230574f17c301328c03
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:18:23 +0000

    571enough no keepup respect narrow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def convert_to_hex(n):
-        return hex(n)
+import sys
+def exit_program():
+        sys.exit()
+import platform
+def get_python_version():
+        return platform.python_version()
