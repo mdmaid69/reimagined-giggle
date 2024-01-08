82e5d1876232a563c24dc7f1732b746e08ce833f
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:57:01 +0000

    close help them that to

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
