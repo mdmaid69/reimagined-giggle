7561a2a6001f8f97020ed0b997d874379dfc7df5
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:59:00 +0000

    stand major soil likely all

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
   import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def create_numpy_array(lst):
+        return np.array(lst)
