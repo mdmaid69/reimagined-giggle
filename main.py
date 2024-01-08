eb6779b36b6d32e1030a1f93a3c9a44ed51cc38b
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:30:35 +0000

    thick clothes takeoff dead hate

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
