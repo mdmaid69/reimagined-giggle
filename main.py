ae1bdb7ab4810e7788e9f6df1808048e01959323
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:44:35 +0000

    depend beat touch achieve week

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def find_max(numbers):
+        return max(numbers)
