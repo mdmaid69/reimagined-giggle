2307fb70de6650e43775e258118fdd98e58c9bbe
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:25:38 +0000

    so last anything bring off

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
