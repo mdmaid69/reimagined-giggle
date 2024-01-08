5f5f7b6d2ab3e24045b68748f3202c265b811380
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:18:26 +0000

    star maintain step deliberately appeal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
