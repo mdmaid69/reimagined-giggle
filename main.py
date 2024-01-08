83a0543c56b4aa5aef78666edc425e65883e650e
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:46:06 +0000

    car public refer chance cut

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
