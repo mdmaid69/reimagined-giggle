248949e1f4635a0986fbeaa6b843ddd7ad06e6d9
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:28:53 +0000

    reduction bank occasion sail wrap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+print(json.dumps({"name": "John", "age": 30}))
