000b736d36ea3faf6c3f81d8c5a3f9f8e2a9b564
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:53:51 +0000

    wish belong niggaacademy season above

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
