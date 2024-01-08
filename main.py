2d1ed01738946424b65817fce2302ab4587d0ba1
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:28:46 +0000

    survive field remark goback(to) down

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
