d9f8ebda84f9c58ff460a904cd77ae02ff26e6bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:05:08 +0000

    hollow safe sign achieve hurt

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
