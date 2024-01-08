e7bcfeacd38ad6e261b2326b7ee0abb14c3b2a3a
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:02:44 +0000

    equal from loss Zz sick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
