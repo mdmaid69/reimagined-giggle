c0e5cc7edd24cb908e5be73cdb668775e237fbcf
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:41:55 +0000

    taste drawing holdup no nervous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_pressure(force, area):
+        return force / area
