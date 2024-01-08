05bfae99159c63d97834e814afb5341ff0bd14e5
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:26:23 +0000

    slowly emotion gift behind actually

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_acceleration(speed, time):
+        return speed / time
