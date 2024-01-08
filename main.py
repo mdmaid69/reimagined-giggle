88b5051bb4e0ecd98dc6fd1149fff95bad27d989
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:41:40 +0000

    success today difficult before more

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import array
-def get_array_length(array):
-        return len(array)
+  def convert_to_binary(n):
+        return bin(n)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
