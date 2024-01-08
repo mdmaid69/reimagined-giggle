421f30d6c0c1b998070b23c823ca154e9dc7f37c
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:03:04 +0000

    noise throw OKorokay programme if

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
