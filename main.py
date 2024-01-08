064b857962ec8aac4afabdf65005cdba0adb2005
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:55:10 +0000

    factory total lift foot woman

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
