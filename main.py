49fd34181118ffd046c1c1cf0d49edbeac40295c
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:43:58 +0000

    home black mark wear kick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+print([x**2 for x in range(10)])
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
