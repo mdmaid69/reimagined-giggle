038beaec6346335e79442c29ebfe77fe4d14b3a0
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:31:46 +0000

    complete heavy mail unfortunate careful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def find_max(lst):
-        return max(lst)
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
