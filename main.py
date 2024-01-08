077f241d0b78778c7267889f99d9142dd961c6db
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:32:00 +0000

    bet shallow generous flat better

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
