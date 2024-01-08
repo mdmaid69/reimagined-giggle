5a285bc90327e770936bad71463c16603ac9484a
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:13:08 +0000

    different opposite round past worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
