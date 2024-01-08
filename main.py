61ff90282715664fffac4f5e04bf69837295aada
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:51:38 +0000

    process regional again platform himself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+print("Common elements:", set(list1) & set(list2))
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
