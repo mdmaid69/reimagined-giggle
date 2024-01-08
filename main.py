958ce07902ff6693c55019b8cce21227c40f6f28
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:05:24 +0000

    art soft recently speech estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
