e747a071a029d7e66e027d796b8d91466eab1afe
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:34:00 +0000

    life easy cough surprising amount

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def count_words(sentence):
+        return len(sentence.split())
