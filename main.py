3a12279e44e40f4c7e11915516dd08dd57144550
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:23:23 +0000

    feel quiet thing appearance work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import array
+def append_to_array(array, item):
+        array.append(item)
