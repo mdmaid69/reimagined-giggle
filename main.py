9f948d51ff7d57f1fe935d2c53c44924f3823638
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:21:46 +0000

    choice work art minute delight

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
