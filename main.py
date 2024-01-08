4fce0efbecbc8c1d7905ab1933bfe1fe6c06abf8
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:35:34 +0000

    belief possible pattern can comein

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
