cf0db8cd2fecc3d91a7350987bf898e848d7d5fa
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:01:44 +0000

    breath suggest belief tense boat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def square_number(x):
+        return x**2
