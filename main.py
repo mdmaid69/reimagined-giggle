78d144985ffb67b16fced3f876bd6d8a7490bc7d
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:05:07 +0000

    table benefit art other season

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
