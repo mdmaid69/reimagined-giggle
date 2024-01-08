7fc802c011d96e6c6278252fe4e4bba5f8d93c10
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:20:06 +0000

    lean hospital strength relative cheap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
