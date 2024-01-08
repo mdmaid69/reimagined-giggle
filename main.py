401e4e785143f443f2238594f08cf1e933c907b2
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:43:38 +0000

    wise takeover sleep fit speed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
