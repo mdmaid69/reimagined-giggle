3e52a5d278eaadaa3a3038eef3950d7da82d791f
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:34:53 +0000

    shrink apparently responsible nerve loan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
