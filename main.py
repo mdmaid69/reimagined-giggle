623f7c8197e26f783839259d49b01fad82c5760b
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:07:47 +0000

    another look sister promise congratulations

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import platform
+def get_os_info():
+        return platform.uname()
+import math
+def calculate_cosine(x):
+        return math.cos(x)
