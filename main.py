e8febcf549d6b39cbfc0b987c5d31b12ef97ee3d
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:46:37 +0000

    turnoff cough worth choose deal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import array
+def get_array_typecode(array):
+        return array.typecode
