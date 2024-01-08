b472983b752543ed76fc128e40a325ae44d4d1cc
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:51:22 +0000

    fair common throughout worried force

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_power(work, time):
+        return work / time
