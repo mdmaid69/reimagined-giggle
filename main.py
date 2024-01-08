27b774ce06c19150849daf37830c287ed026da4d
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:05:56 +0000

    wrap taste bye reduce pin

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  def reverse_list(lst):
+        return lst[::-1]
