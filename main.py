c8fdc3411a4a110fa1adfcc13dc020f895264b85
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:04:42 +0000

    hungry firm quiet ring snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
