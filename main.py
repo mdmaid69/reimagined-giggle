4b606922e2df8a0d4650d0dc252c7a2e02a0d3dd
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:26:21 +0000

    up common within lie increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
 import collections
-def create_stack():
-        return collections.deque()
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
