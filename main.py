4c5c5b50642ac0de25163ca45e4564526190cb98
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:42:30 +0000

    patient hot interview direct official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
