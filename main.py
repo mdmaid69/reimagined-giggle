e99f4fdad22c517ddab9092615f257cb298cacbf
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:22:53 +0000

    spread pick clean survive support

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def is_odd(n):
-        return n % 2 != 0
-import time
-def get_current_time():
-        return time.time()
+import collections
+def create_user_string():
+        return collections.UserString()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
