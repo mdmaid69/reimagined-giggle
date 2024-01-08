0963eaac49ce9a28a5c2453341bf09b4a41bddab
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:08:28 +0000

    dear appearance scale as butter

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-x = 10
-y = 20
-print("Sum:", x + y)
+def count_characters(sentence):
+        return len(sentence)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
