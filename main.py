41a1453a693d56235fef043c7e4d56a9d6b68c89
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:12:11 +0000

    something notice tooth condition tip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
