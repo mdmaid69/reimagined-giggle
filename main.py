9ffb90ad3d745021131c3bdbfa9aa8e3e99eb1b3
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:30:04 +0000

    no interesting bend reply corner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
