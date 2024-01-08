df05bfa3439d16073560d3480a29083d0809c8ac
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:16:08 +0000

    necessary pupil clean crash soak

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import time
+def get_time_since_epoch():
+        return time.time()
 import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
