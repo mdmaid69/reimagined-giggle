e0d345c050c8ba4a8d83c0e1108c42044579d585
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:31:10 +0000

    between sting himself cry usually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
