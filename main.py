bfddd975fbf08a09d78f67af447690ac1e241d36
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:22:25 +0000

    no Oo fairly by poor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
