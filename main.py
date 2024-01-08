140a521ccfd66c342f8f5caa7aeffb2dd7a533c5
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:56:48 +0000

    clever beginning her near advanced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
