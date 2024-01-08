fbcc20d1ae11215c893cd5bee79380604d51c160
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:04:48 +0000

    result new shout afternoon wander

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
