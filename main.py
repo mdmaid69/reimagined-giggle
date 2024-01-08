3d551df3fc1455ad5d77b56c64ab17ae0c25e702
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:44:00 +0000

    tear persuade tidy motion other

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
