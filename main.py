01973f0433037ad079fcf03c5102d38a251f760f
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:56:23 +0000

    reduce find music argument respect

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+print("Hello, world!")
+  def is_odd(n):
+        return n % 2 != 0
