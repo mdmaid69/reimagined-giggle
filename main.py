727c3a3cbb7015a26d5622c961c373064d2e68ba
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:17:29 +0000

    aim gather fire telephone mine

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
