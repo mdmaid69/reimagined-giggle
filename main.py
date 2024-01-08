7f2db5ff2a2082e214b176fdcf3c4857a53a1fbd
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:26:40 +0000

    main shake advise persuade central

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
