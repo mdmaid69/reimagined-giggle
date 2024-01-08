caade5da78a6c5cd7dd9c6dee5b265081885c481
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:05:41 +0000

    tap walk crowd influence this

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
