e2ef7db360c2b03166d2b42976a5862291933df5
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:07:56 +0000

    fire apparently 565end takeback neither

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
