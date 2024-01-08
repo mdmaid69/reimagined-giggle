2725134f9d56bc3653119fa5e3a76fb423e6ebde
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:55:45 +0000

    between recent front hide born

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
