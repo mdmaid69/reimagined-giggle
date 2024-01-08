81d1d35aee75af33fc747288935f26ea36beea0d
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:56:33 +0000

    pull anyone wish heat 567enemy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import re
-print(re.match("h.*o", "hello world"))
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
