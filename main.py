d78a7a1b474d2818efa047a37f065ed02a967b86
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:29:57 +0000

    claim sister guess bet rob

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
