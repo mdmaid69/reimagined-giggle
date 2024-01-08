76ed8b034be3c296ec6cef817501744963b8230e
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:40:49 +0000

    attract praise under characteristic tense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_density(mass, volume):
+        return mass / volume
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
