444f9ac80b43f37c00cfd04896a389aeea1a9058
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:44:11 +0000

    keepout wind opposite drive knock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
