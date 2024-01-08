20f627d779812eb08ade7679af74621f93617462
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:48:31 +0000

    mistake dry moral break rid

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
