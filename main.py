923d068532ce1c741149edb59ac64140e6e79f22
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:35:34 +0000

    involve bush breakout occur turnback

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import sys
-def print_python_version():
-        return sys.version
-print("Hello, world!")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
