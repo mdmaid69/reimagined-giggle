46f624618a2abf9aee0b67952ae8b870447e9b0e
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:25:17 +0000

    need congratulate uniform include turnon

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def get_array_as_list(array):
+        return list(array)
