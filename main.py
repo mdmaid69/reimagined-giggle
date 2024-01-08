5ac6220f7e726be021b54adf0f0c10ccaf116469
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:49:20 +0000

    poison motion lend any opposite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
