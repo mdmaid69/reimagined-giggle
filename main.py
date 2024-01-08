dcdca73dcd677699b8c4fca3c5037d3fd2cae2c3
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:23:49 +0000

    who keepoff breakdown yesterday open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
