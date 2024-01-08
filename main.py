51b2b8e465f94842809063afbfb77868fe162efb
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:24:49 +0000

    circle squeeze apparently wait law

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
