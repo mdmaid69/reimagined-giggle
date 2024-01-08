583b0470f16ed6dc047cd713796a81e34b83973b
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:30:25 +0000

    hello tip regret above should

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
