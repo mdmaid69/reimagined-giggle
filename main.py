c1b3dca148400217e167b69ae686436bdc7e67af
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:54:18 +0000

    dance proud politics round refer

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
