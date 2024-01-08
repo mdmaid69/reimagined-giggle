acb3f7daf6efcdac01402c9cad50aa50846166c9
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:37:22 +0000

    to sound deliver equipment lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_itemsize(array):
+        return array.itemsize
