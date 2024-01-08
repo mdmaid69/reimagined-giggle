dcf7fbf58c1174edba2e929bbac292fc5b13a72a
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:12:39 +0000

    comefrom production fall breathe issue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import array
+def get_array_as_repr(array):
+        return repr(array)
