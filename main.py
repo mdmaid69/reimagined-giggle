6fe03a60ab14ae58a32cdcc5240ec2723851d721
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:05:09 +0000

    lend rank contrast row wide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def is_odd(n):
-        return n % 2 != 0
+def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_as_int(array):
+        return int(array[0])
