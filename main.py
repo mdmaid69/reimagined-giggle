cfd4c2447c77af589f8c565db9cfea0de57ebd4e
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:59:53 +0000

    clean ought split tough ill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
