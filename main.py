6dd6632ae9827cfa158ec3a2670eeb1729f95aaf
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:19:49 +0000

    collect guess building tend black

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+for i in range(5):
+        print(i)
