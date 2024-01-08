137d49c2dce997233f5a45c775ebc4b79dc351ec
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:59:44 +0000

    average pass ignore thorough yes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
