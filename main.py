a56a6475a44e5ef2bf0dd05ab10f6def56e018ee
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:04:12 +0000

    rest mend together simple tone

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
