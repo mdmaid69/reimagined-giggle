d8e9864999943f41abb7fd727e17d8fb382e384d
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:47:01 +0000

    load equal stage search limit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_work(force, distance):
+        return force * distance
+def calculate_work(force, distance):
+        return force * distance
