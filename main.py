61f9bac129f9d97c9afb1bb07019b24b5df3b04c
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:21:29 +0000

    after cry separate even apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
