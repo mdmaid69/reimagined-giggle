ab4c4fe2d0984aa527ecc6d796d8aa720aaa4bba
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:44:24 +0000

    book Hh influence easy makeinto

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
