5f10d36347cdeea6c0ae4294ffe4c7fc1c4c8c47
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:19:34 +0000

    threaten mystery month in frame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
