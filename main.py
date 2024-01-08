5b928a15fc3fb24948c40c77c44c68aad0be4a08
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:26:12 +0000

    know back curious touch my

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_as_str(array):
+        return str(array)
+  def sort_list(lst):
+        return sorted(lst)
