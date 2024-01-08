fe0ad22c29e98467e58776ac47ba2974d241bb56
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:34:07 +0000

    hope limit impression react mine

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def cube_number(x):
+        return x**3
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
