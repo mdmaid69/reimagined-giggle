0eacce652f53d410ba9d7121bd164d8382bd0a3c
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:30:40 +0000

    fairly for close connect truth

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_list_from_array(array):
+        return array.tolist()
