062c16887b374b2e94e57173897cb4254f105ed4
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:28:00 +0000

    find public on will conversation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import collections
-def create_user_dict():
-        return collections.UserDict()
+def create_priority_queue():
+        return collections.deque()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
