74171b1781ba46fa89e9ff2538cf54b0c587332c
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:34:47 +0000

    error last key smart us

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
