d7b949992c863bea89d9c33414823070ff5daf11
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:00:41 +0000

    onto want past free thankyou

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
