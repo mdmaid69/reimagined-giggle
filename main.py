6ef5d501b07778158c32b1bb52dc8c0d990a36dd
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:32:11 +0000

    note nice accidental though dear

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import os
+def change_working_directory(path):
+        os.chdir(path)
