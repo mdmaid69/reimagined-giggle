b05989a33fc26b2a9927b9713b3f4bd534438bf1
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:00:35 +0000

    platform new mail mind to

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
