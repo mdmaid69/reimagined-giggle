5cb302ae5d74ab4fb146f93dc53828d16e8fe1dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:22:59 +0000

    mass turnover commit used pause

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
