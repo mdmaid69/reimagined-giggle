3cd8dda6365a8e0558e43b7e90eaba524dfaebba
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:56:17 +0000

    problem nice prevent talk note

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
