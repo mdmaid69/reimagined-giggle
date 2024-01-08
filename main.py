a3e5cc5547cbbf891b6495b9a4e740a8e4b761b4
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:43:28 +0000

    often occasional container wing suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-print(sys.version)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
