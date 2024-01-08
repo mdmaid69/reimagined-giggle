0c679b6e698ffb240057014e4727dbe6e837d9f4
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:56:08 +0000

    nurse damage study way interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def find_min(lst):
+        return min(lst)
