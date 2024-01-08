ea21539877f8ded354890c65eab152b9178647a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:55:04 +0000

    no sign idea previous ask

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
