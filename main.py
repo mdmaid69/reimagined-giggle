ef74a91729709847fe45d91802384ff34b96272c
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:55:33 +0000

    poor independent object smile deep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def count_elements(lst):
+        return len(lst)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
