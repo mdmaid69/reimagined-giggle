24973bcc1c03c2a2bcf802d859acdb465fc1cb72
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:46:01 +0000

    wall complete confident left authority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
