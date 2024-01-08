8598930251f4b30a5399ef315e551da1962f502d
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:17:12 +0000

    waste impressive fairly advantage rent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_union(list1, list2):
+        return set(list1) | set(list2)
