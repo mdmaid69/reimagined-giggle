6339a42fe928e504799e5f1625310074326b8c63
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:52:44 +0000

    surprised space neither create characteristic

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def is_even(n):
+        return n % 2 == 0
+def is_even(n):
+        return n % 2 == 0
