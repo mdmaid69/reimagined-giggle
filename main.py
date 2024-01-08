70768e7e2a3c7420663e7ff8942fd20e3fc81226
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:47:39 +0000

    tense vanish accordingto cruel load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
