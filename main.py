bfcb8a0df4343552efc5742ede144217059b8a96
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:36:54 +0000

    name sale extraordinary calloff treatment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
