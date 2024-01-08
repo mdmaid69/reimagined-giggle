4d016e89d6016b03e3868a532377b789b1f5c71f
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:04:05 +0000

    story move press laugh entrance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
