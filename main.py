9bb56186bad8764865da3fdbcb9e50342dc39f2b
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:56:16 +0000

    to mouth my thirsty urgent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_item(array, i):
+        return array[i]
