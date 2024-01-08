e18f817348773c89fa270d3e8774bb7297fdba06
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:53:47 +0000

    notice solve stage sentence job

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import platform
+def get_python_version():
+        return platform.python_version()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
