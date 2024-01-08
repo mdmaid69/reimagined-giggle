e9e30cad7a2b6abd4580f1cdd98b288b363764e4
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:46:57 +0000

    middle face plain pronounce pure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
