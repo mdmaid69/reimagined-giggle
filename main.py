a341d8463bef9f37eda5cb35db8419647c4996a9
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:25:42 +0000

    nervous chief strength exit crash

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
