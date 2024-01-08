dcffed8c45a8ac802c59b351fd542c550b56d302
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:34:47 +0000

    release disappointed contact attractive conversation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_float(array):
+        return float(array[0])
