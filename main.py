8194871034806d1eb1cf9a700d464d8e985586c7
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:43:22 +0000

    development return shock development into

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+for i in range(5):
+        print(i)
+  def convert_to_octal(n):
+        return oct(n)
