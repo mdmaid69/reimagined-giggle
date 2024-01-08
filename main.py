fc6eb4cac9cecc0a3a6c11783d5438bf5274b033
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:24:37 +0000

    mind whole piece whistle count

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_queue():
+        return collections.deque()
