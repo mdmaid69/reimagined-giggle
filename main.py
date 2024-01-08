2fcc7a2d715418223171e161a23db7a18226e28d
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:24:16 +0000

    partner frighten proud square finger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_area_circle(r):
+        return 3.14 * r**2
