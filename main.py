72d2f3e7c00ccc382f79fba99f251554669c6913
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:04:52 +0000

    possibility hear write way cough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
