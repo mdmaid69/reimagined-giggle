707ab3ea3ff55b89dae634c94f36acd15f82a40b
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:27:46 +0000

    wherever circle dig allow ocean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def cube_number(x):
+        return x**3
