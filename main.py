e55838ca1f57cbd1fd90c1a0fd0b2133bae7bbe9
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:48:13 +0000

    precise through chase satisfied loan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
