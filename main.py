061d7776cf7ccd1f45056af2f91400b754064817
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:32:19 +0000

    even aware close interview nonsense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def is_even(n):
+        return n % 2 == 0
