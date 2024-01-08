c6bd39ed8843555d14f56511b9df26f584f453ec
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:04:39 +0000

    similar base tonight defend ability

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_speed(distance, time):
+        return distance / time
