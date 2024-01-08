974a1856b41cbf606d05e4af01d1e5c77271ebb5
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:45:26 +0000

    deserve somewhere until interest contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import tensorflow as tf
+print(tf.__version__)
