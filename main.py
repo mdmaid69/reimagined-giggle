06650f2427c86fa646808fe8194a7aecbe94430d
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:54:24 +0000

    meeting anywhere behind practical demand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
