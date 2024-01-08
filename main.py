9382c51c1c01eb79a46c348a0e0ba5898afe1ca4
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:48:13 +0000

    make quit exactly chat rescue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
