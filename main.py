bc7b778954cfa20f7388cf231001f31081f8c515
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:38:20 +0000

    pay centre man back challenge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def find_max(numbers):
-        return max(numbers)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
