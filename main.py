49539a7c60a70ef6e6f052614062e8f6398718ee
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:43:25 +0000

    nerve spoil deaf connect history

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
