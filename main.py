3abcada72c24b9a7effea99147994e4628dae41f
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:01:22 +0000

    heat report definite identify either

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
