1daf814772d37d3dd5b09b66613d5b03b6ed3efa
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:51:34 +0000

    quiet guilty safe payment warm

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
