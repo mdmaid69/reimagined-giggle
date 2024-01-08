c7112e7315820e6ad91d3ad165cb71dcbc2de445
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:39:20 +0000

    sky watch unless keep admire

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
