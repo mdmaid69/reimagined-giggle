d9479ae50079cb92604eefade24a894a544e82db
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:35:47 +0000

    fold total confused plenty serious

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
