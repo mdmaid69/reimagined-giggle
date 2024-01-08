fc31285e7bcd8b646dc4fe01d54fb7d92f2584ab
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:57:24 +0000

    improve system refer school underneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
