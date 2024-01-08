dbc4a6a3d960e31fbd6e47a3ed3f4ad8f065ca1f
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:14:08 +0000

    force embarrassing hope their 568energy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
