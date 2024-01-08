1637bf6c22ff200160504124b538f987ab57a24d
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:12:45 +0000

    sail whom anywhere either Jj

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import sys
-  def get_python_version():
-        return sys.version
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
