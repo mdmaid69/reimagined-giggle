dbcdbbeb0144dea58b9ecad1db6b101770649932
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:43:53 +0000

    fond female hot answer shut

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
