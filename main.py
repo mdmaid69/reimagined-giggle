10dc9a1a5223ccd7a4941bde39c3e15b8c7b3b3b
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:53:52 +0000

    dish downstairs measure cheque complaint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
