6d16022455e583f9f9575f292f72ec1223646753
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:10:29 +0000

    study thanks reduction computer manner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
