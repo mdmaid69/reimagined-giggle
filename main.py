f95dbeeae09a95a9b2184c7410c333e01d5b3aeb
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:25:52 +0000

    people regular fill last breathe

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
