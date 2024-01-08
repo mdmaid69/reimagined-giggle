a948af121a8f9837df9ccb599b19e89b2ebb9855
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:56:09 +0000

    through far gift home distant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
