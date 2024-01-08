e1f5ab16a26d7fa7661e30ed84af64450ced49d7
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:43:38 +0000

    journey still sleep fact try

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+for i in range(10): print(i)
