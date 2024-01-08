449a656366a4c1e24f37b8cac52f2fa982d31657
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:48:22 +0000

    hurt relief willing squeeze name

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
