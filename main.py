bb6fd29e73c35469deafa2ac0d076df06e074e36
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:36:35 +0000

    keen aim charge climb thank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def reverse_list(lst):
+        return lst[::-1]
