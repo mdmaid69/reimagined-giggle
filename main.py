25081ecd3a5ffdee023bf76f7f57e1a4f238a602
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:30:44 +0000

    goback(to) copy ours wise during

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def square_number(x):
-        return x**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def count_elements(lst):
+        return len(lst)
