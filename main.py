25f03af54ada996dd834140db2dfd09575cec182
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:47:43 +0000

    family great twist ill spill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+def calculate_pressure(force, area):
+        return force / area
