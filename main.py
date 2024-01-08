1b5d40bbfd2b4d4518af29dedc27a2bb2380c84a
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:21:46 +0000

    condition community neck tomorrow difficult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_ino(file_name):
+  def get_file_inode(file_name):
         return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
