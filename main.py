36b06bb3b4fed97544f2919a7de0d725b3a2209d
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:53:29 +0000

    polite butter miss hang sting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def square_number(x):
+        return x**2
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
