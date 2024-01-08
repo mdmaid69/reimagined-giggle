e083e391cc5870665b3f59b6e8771637973d0c72
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:35:09 +0000

    size foreign bear opposite contents

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
