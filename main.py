1712c9d75fe188d0221e31ee8423d3c77a7c4708
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:17:07 +0000

    record independent estimate collection crowded

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_slice(array, i, j):
+        return array[i:j]
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
