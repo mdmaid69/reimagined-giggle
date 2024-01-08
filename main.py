18b63de3d7dccca187884365fa4837764b2fa799
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:15:56 +0000

    during release announce judge wall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import logging
+def log_message(message):
+        logging.info(message)
