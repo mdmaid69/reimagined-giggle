c19f375095bf2cf4359f144adf2a625a999d68c4
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:44:24 +0000

    want payment hold sweet sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_length(array):
-        return len(array)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def get_array_index(array, item):
+        return array.index(item)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
