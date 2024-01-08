6d01e68a96389d3d6e9f1010cf073c385f2ae55a
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:02:48 +0000

    train thinkover claim suffer yours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def get_current_time():
-        return time.time()
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
