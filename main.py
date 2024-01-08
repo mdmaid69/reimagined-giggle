466b6d9bf9e8b9a3da4af879ef1edb78df4ef61c
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:49:01 +0000

    storm plenty getout style dance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
