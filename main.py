6b19658d9b33f5bc038467daacff946c161fc988
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:54:46 +0000

    die heavy form rule train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
