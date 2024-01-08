73b75b71de203042f530e2e3bcf0d250ead4887e
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:42:59 +0000

    bear pour gap file worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
