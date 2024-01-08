11eb065bc8b617aa7461b4d50690f42079d3f790
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:29:22 +0000

    consist closed over belong approve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
