3d81291621e4a6bff3c8825e4a829708f9bbf0ea
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:39:13 +0000

    city thumb buy hope describe

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_as_str(array):
+        return str(array)
+import os
+def remove_directory(path):
+        os.rmdir(path)
