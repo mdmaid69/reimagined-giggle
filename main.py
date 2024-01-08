b63f0fd9140c82f33840457fa5bca4f34a03340f
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:10:16 +0000

    special score upwards comefrom tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
 import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_as_bytearray(array):
+        return bytearray(array)
