27ca226e292996a99eaa67564f8fff3990e60caf
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:34:03 +0000

    weather description pretty hard exact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
