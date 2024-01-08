ea921ab8bd44268b1439939a396108623461f127
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:43:15 +0000

    later or holdup careful could

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import math
+print(math.pi)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
