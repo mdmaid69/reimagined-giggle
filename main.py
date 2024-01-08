8fae351f097fe0b71af80017f18b18cf60bd5618
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:07:29 +0000

    before story later before balance

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
