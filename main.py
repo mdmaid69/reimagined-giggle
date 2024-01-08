ae01eca84d84f8c94825f45fba1d09accd04b88f
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:40:54 +0000

    spray guess mixture decide sea

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
