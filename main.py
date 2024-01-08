1dd37709ee0c2b11732f76ef60de244dfedb99da
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:46:06 +0000

    basically neither cinema rain mystery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
