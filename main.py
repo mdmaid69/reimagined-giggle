fb5c9954ba5ba56029202f083a927cd1bac4d022
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:00:12 +0000

    what thirsty divide practical respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_sine(x):
-        return math.sin(x)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
