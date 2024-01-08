cd7911964ad7cb9c0d372cec3451329092d8e8b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:12:49 +0000

    pity hurry suggestion coat statement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
