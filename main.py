ce04facb37eb81af58bb1f206dbfdb2ce48ac386
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:46:42 +0000

    dress by suck hang broken

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
