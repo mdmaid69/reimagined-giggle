63a2c02a35370274fe5bb45d8a140834a13d36b1
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:03:23 +0000

    interested anywayanyhow morning worry but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_force(mass, acceleration):
+        return mass * acceleration
