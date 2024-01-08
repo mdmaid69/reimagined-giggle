d07ed84d3441d725fbc420bf57b5441cda98e859
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:14:33 +0000

    like say among definite hand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
