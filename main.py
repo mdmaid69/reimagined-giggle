e931f8315041b8ce464977ebb014185e957fd8e8
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:52:57 +0000

    onto congratulate chance ring fond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
 import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def log_message(message):
+        logging.info(message)
