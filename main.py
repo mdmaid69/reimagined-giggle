00d5abfecd95e7385c3b247d0c1c8e9940d5c46f
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:56:42 +0000

    race across worry prepare squash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
