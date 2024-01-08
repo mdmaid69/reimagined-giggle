8091d0fcdcc28538fa234e4e69190931003f9044
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:49:41 +0000

    roof furniture holiday familiar skill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_perpetuity(payment, rate):
+        return payment / rate
