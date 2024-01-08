b64939ea11f407267c19db3ed8324462ad45888b
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:13:36 +0000

    rush escape flavour beginning polite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
