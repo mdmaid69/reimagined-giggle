01a2a4967c5bc115e8c185166a19f52fdfcf6470
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:46:30 +0000

    door discussion history correct repair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
 import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import collections
-def create_priority_queue():
+def create_stack():
         return collections.deque()
