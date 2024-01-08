e9e0a4cff583aa1106eccb99b08e3fb22f48153a
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:29:56 +0000

    feel letter phone calloff perfect

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
