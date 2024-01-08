3bcebf6bd9d8fdc55c6269b711de7cb6822ea12c
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:24:40 +0000

    stone instant pocket natural nose

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
