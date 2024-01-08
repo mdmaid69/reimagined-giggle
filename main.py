a61a1e83c82c83c70098e6fb5549805153cbe739
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:52:34 +0000

    shopping unpleasant giveup their cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
