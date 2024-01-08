b23da0b15d82c2b97ef33662744140511ed04223
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:22:33 +0000

    shoot phone wave coat persuade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import re
+print(re.match("h.*o", "hello world"))
