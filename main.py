29bf3ffa754fc0a31147c2b37561d2f7b2c4d890
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:18:46 +0000

    square carry final break comeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def convert_to_octal(n):
+        return oct(n)
+import os
+def change_working_directory(path):
+        os.chdir(path)
