69d1d768bb7bf7b68c1bf6a8042f4334ccd0b3bb
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:04:28 +0000

    clear style bar national climb

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
