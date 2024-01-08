45fe13024255f8aba836335b52aa48dbcdd1af4d
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:10:11 +0000

    almost hit repair loud positive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
