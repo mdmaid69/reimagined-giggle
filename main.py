94f8edbf1c54565254b939ed69ceed20d0ea4062
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:19:55 +0000

    ill apply match strict between

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def subtract_numbers(x, y):
+        return x - y
