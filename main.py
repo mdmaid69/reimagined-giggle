bee683b8b21619060264cfeb1d97094aad430709
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:03:29 +0000

    afterwards live as nearly war

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
