435b6e1ce7a1e6b5ba719320f38471f22c26354b
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:19:24 +0000

    Ss could love equipment protect

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import sys
+def print_python_version():
+        return sys.version
