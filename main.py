448ebb12a4b4b8df3c41f4184d5c391de20ef00b
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:11:29 +0000

    happen lean platform pen stamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
