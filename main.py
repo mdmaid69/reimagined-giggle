515850d261ebc758b1663f404becdb2ee3df2c1a
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:56:35 +0000

    lead whistle her limit since

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  def cube_number(x):
-        return x**3
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
