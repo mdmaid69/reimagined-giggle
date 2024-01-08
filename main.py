e2a28c593a67acd3b674d147452227f4ed28c6e1
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:59:45 +0000

    programme stretch nature give smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import array
-def get_array_as_str(array):
-        return str(array)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import os
+print(os.getcwd())
