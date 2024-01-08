35a8287c4ea41dc28e6e2f811591de5501ff527b
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:19:37 +0000

    circumstance suck hope compete tongue

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+from collections import Counter
+print(Counter("hello world"))
