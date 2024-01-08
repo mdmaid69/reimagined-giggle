5625bf309aaeef79d75d1e77903bc854203b454c
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:33:50 +0000

    suit care sing minimum performance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-print("Hello, world!")
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
