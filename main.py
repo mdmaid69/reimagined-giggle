4f12afe4ead057a48477a1c5d480a8ae4eee61d5
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:46:25 +0000

    eager steal turnup underneath something

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def get_array_as_float(array):
+        return float(array[0])
