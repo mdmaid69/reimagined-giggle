574130dc53981717f6cc63000541525fe1cc0a38
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:23:22 +0000

    chance borrow balance congratulate confident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
