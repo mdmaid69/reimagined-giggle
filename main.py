37ffef2177afe8132f53f23c88d0225faa693380
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:37:17 +0000

    young call bother season interest

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+i = 0
+while i < 5:
+        print(i)
+        i += 1
