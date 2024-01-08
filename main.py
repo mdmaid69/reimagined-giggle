9ef3d54d8ce453d69b9e62d24eef882e6b45ddf0
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:21:01 +0000

    slide school announce alone similar

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def square_number(x):
+        return x**2
