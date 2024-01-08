7415f48de2d2721b2329c8050aef97b4733c0caf
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:32:23 +0000

    fierce habit science surface discover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_current_working_directory():
+        return os.getcwd()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
