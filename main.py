2f28c308bd0541030db48fc7ecb4df31ff6bc7d0
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:51:41 +0000

    sentence sale pay practical accident

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def convert_to_binary(n):
-        return bin(n)
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_as_float(array):
+        return float(array[0])
