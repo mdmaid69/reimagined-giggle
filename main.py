b76e5b0bd11d032ddba899bddfe31e5caf1174f5
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:20:09 +0000

    strike circumstance frighten sink tool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+for i in range(5):
+        print(i)
+import array
+def append_to_array(array, item):
+        array.append(item)
