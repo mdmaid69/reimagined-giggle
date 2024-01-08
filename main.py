698d7adeb5edc06b720fdc5565fd613d8dfba735
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:10:34 +0000

    kill now hit relax cook

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def is_palindrome(s):
+        return s == s[::-1]
