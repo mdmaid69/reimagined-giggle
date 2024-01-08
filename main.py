d5b28cc5b658a93a1aaae59983a7047ec450f700
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:37:54 +0000

    bring fix press slowly feeling

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def add_numbers(a, b):
+        return a + b
