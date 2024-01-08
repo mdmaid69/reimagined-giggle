fa3019cdb5fabba5d10791f3631f1d9f99693b7b
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:13:42 +0000

    party bush would light long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
