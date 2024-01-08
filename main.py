f5dcf280f83dfc60658aae0e305f23d6148448a6
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:22:10 +0000

    float statement watch belief him

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  def is_even(n):
-        return n % 2 == 0
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
