ee28ce2c4d40bf22b1c95d6a61dbcce6a4e5d77f
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:43:30 +0000

    confusing busy provide funny breakup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
