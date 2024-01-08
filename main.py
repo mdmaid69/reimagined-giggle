9e2388ae4ae647fad9921cf8c7046d773deb07a7
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:36:56 +0000

    sweep sail square uponon indeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
