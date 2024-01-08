9f165a71e73e61f1c90e44c7c16127141504954c
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:55:53 +0000

    passenger book opponent whistle storm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
