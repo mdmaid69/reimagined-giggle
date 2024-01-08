02efab157e63ac867384c501909c8f23bb8e5706
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:31:06 +0000

    satisfied juice keepfrom past crime

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
