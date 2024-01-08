a6571f5b04d5ad91178b7582b1bf30987b175ab3
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:14:42 +0000

    spin shop rush bank always

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
