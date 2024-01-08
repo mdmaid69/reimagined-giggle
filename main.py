79c99d9369bd497c343f53ea02d4688092430177
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:17:51 +0000

    us drink rise takeout exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def is_odd(n):
+        return n % 2 != 0
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
