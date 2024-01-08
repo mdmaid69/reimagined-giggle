ad4329c30bb5d5316874a8929124f84cc8ff8f60
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:31:54 +0000

    566end insist prize spoil offer

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
