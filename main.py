1f768e58f13f39965023206a6645d7abca2d2fc6
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:57:28 +0000

    instant tone mine throw crowded

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
