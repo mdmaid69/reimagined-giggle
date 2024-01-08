1497685f4531e2c3faa4673d51c9de36bda3c3ca
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:46:33 +0000

    turnoff bend Pp pause language

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def reverse_string(s):
+        return s[::-1]
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
