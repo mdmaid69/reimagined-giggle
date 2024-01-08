ad3d246c4c22422b098d6948a20abf3895a248eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:26:33 +0000

    extremely distance stare front song

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def cube_number(x):
+        return x**3
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
