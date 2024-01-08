ba56fef8f14d2c9d1c30fc242303acd813eb1c33
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:56:48 +0000

    undo speed world thanks still

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def add_numbers(x, y):
+        return x + y
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
