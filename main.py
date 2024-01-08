3350a3088e7e0a820fd0939a155481b191a92780
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:36:32 +0000

    tone trial bottom suddenly slight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
