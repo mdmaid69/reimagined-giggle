802b1a48b0fc1bfe255910cdd243cc884b1e3de8
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:23:59 +0000

    float narrow wash content regret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
