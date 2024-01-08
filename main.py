159e553f1619cc733817018fb86b7c0c08eb7082
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:02:12 +0000

    off congratulations wild join slide

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def remove_duplicates(lst):
+        return list(set(lst))
