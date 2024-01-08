8fdce362b66b5c39ff0e7ddd01502d1060fea27e
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:30:01 +0000

    whistle sense belief within suit

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def add_numbers(x, y):
-        return x + y
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
