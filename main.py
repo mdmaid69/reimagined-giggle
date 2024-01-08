5714fbe3450c43f2c87623998f23c649593c503d
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:42:52 +0000

    price complete smell model shallow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def get_array_as_set(array):
+        return set(array)
