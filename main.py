bee821c5d26a8af1436f3655c57d27ed515f6e65
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:24:28 +0000

    concentrate Ii pain snow back

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
