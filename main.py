624979138e9ac1f2178c928a4ab683d888b1ba0a
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:06:27 +0000

    pretty trap upstairs light forget

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
