6076145d2439fa9dfdb5e1a9c789d7ebc54ee8e2
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:53:30 +0000

    hour tidy seriously what detail

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
