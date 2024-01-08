d14f26f6139eb77f5033339df72dd45acbb375ea
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:16:32 +0000

    turninto year clue physical honest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
