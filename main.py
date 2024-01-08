cfb0aaed2e78d2b0fb15f72d185f99a4f2451a33
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:14:40 +0000

    lonely emphasis consider thanks hurt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_list(array):
-        return list(array)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
