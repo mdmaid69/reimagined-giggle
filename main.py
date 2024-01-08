2213416ebcdd975a9e361057a1de1b58c1770894
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:04:38 +0000

    die sticky pain brain face

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+for i in range(5):
+        print(i)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
