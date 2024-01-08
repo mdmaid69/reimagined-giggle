2431fe834c61bc920bd6db773e2313e44a43891d
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:29:47 +0000

    less again interruption usual hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
