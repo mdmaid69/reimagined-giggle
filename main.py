d656ea6cf0b7deb0b254e1bd983d92fcad566e00
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:10:10 +0000

    comeout centimetre season question experiment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def convert_to_octal(n):
+        return oct(n)
