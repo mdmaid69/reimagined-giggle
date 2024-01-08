ae0c4de31780cbfaaa1b6516476a11373999abf6
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:30:03 +0000

    later schedule choice disapprove against

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import sys
+def exit_program():
+        sys.exit()
