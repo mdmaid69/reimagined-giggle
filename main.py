151ee8be1da48f48c278e0cb6fbc84b68bbd0747
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:42:35 +0000

    bad central sink its sport

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
