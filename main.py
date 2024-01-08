f67caba25a6fdc44a74da29414fcf52d2127dabe
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:22:49 +0000

    develop bus broken power vote

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
