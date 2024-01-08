c6db67f0890bdb6798ad7413551c80e1f8843448
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:14:07 +0000

    declare pride reliable meaning discussion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
