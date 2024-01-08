fbb3f675b24744b0c1673ba212d5840e6792100f
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:31:01 +0000

    dull relax blind tip ever

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
