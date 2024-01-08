357aa49ed60e4342b672fd434df2363311059008
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:49:30 +0000

    spill hand getup picture container

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def sort_list(lst):
+        return sorted(lst)
