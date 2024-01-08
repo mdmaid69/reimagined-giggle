1b765e56564e432175ced4ef88aa9cd9900a6444
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:18:24 +0000

    church his convince shout enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
