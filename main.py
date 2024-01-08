4a069473b78042b33e3e7953348698dea614acec
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:09:17 +0000

    invite rate present form wash

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
