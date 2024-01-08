430785112c80c34c2e16bb8fd34fd312c4ba3c13
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:37:25 +0000

    attractive tough everywhere operation pinch

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import datetime
-print(datetime.datetime.now())
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
