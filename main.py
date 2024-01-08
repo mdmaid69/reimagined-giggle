e009b4bb02341b29531e944585fd2762f6dd87e5
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:08:32 +0000

    careless 571enough content breakin read

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_current_directory():
+        return os.getcwd()
