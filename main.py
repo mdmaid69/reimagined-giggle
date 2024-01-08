18c8d51ad0522f34e4015339df5939dc13b09bf8
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:58:05 +0000

    entry lean workout wide smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
