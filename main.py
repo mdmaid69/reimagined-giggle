0d4c900f1c4c090c873bb9b1d388365a9b801a40
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:09:34 +0000

    available grade almost sensible breathe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
