ab5c9de313c6ac907827902605f2806d01bc4701
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:23:59 +0000

    bang beneath believe dance appeal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def convert_to_binary(n):
-        return bin(n)
+def get_array_as_str(array):
+        return str(array)
