9e309f81028258bf02043d47de003f964fe57e5d
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:57:17 +0000

    bread freeze advice name rob

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
