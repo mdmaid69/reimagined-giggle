18e9b84f257a201d5de847b47b4aaab5be35e55a
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:25:04 +0000

    hardly pot busy field anywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
