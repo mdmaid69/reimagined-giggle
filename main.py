52d98f8703a6ccfd599957cd9489618a5b298a25
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:04:48 +0000

    light exit eager question naturally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
