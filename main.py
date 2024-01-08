5172c5d5f8774f413a21d2ad756fdbaed1b5e84f
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:13:49 +0000

    even which temperature warm part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def convert_to_octal(n):
+        return oct(n)
