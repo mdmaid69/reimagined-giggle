b6e02d4c9d779903de85bee5063761fd17fd5c72
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:04:01 +0000

    slice show discover learn favourite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
