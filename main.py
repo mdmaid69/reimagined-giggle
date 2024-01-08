2292b9264dba0258c46f308ac804881c0df64a9c
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:03:06 +0000

    deliver embarrassed Ll authority official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
