fcdb949a59b57a9a82ab7eacc7c3c25ed905bb62
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:01:48 +0000

    debt reaction instrument owe Yy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
