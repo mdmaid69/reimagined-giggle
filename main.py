2e00e5cd4e17432fda09c31fdf96f18f296d3f38
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:17:39 +0000

    breakout feed this suffer opinion

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import time
+def get_current_time():
+        return time.time()
