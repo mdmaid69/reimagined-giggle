8c035086503b83cbb2160159a4200f6a88ecfce9
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:53:55 +0000

    shock serve front fasten interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
