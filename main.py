aaba82fc90c225444d04bb02d27658cb33a54a49
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:08:46 +0000

    relative tear demonstrate term pipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
