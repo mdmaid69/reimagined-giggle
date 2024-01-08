119c2a990373b0b7d5b0df87a3f8a3be2486f928
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:22:02 +0000

    normal impressive wish uniform well

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_max(lst):
+        return max(lst)
