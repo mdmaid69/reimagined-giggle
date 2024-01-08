8743d40840d09575665d50074475bca5f3d4b055
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:17:47 +0000

    nose improvement possibility subject wash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
