8f1d5e7841084d8ae39ac7a168f11ee2a38b14aa
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:08:35 +0000

    free both punish nigga 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
+def is_odd(n):
         return n % 2 != 0
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
