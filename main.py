7ac6a99df3b13737d598bd3af81e6f8772e3b37a
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:20:01 +0000

    search start area muscle by

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_current_directory():
+        return os.getcwd()
