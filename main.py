30f7a5b79a1d2fee2f7d2ec22ab4d7a83c703188
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:06:57 +0000

    truth smooth unfortunate familiar accept

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def count_elements(lst):
+        return len(lst)
