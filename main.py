9edd1d8b553d1788d663d39a29072fba8762fc59
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:35:35 +0000

    impossible shock national stupid over

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def append_to_array(array, item):
+        array.append(item)
