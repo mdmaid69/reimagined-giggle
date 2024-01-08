0a2db2f8a65af7b86ab3c4cb5aa69933428c40db
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:41:34 +0000

    more pen destroy goback(to) straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
