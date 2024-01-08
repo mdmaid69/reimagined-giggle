7da6f896c4872623a6e82ff8ee2d0d5520fad21d
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:24:00 +0000

    smile fair move tear wire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
