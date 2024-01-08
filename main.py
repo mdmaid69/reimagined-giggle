197ced31e557bb1eb06f634ee63e111a2a25af40
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:52:44 +0000

    breakfast passenger further tidy giveout

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
