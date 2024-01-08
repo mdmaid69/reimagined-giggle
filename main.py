d14cc17a820293c2952b51de714e498f33047135
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:23:18 +0000

    admire order school left move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
