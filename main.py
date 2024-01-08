1c057fd75d6714ea645171609e1cc1716eb129c3
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:46:25 +0000

    matter point store label Ee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
