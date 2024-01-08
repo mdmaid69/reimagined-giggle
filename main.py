f5ed8d6e345615d02839a604efa8259c533197a9
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:15:13 +0000

    keepout pollution drive judge congratulations

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-print(sum(range(10)))
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
