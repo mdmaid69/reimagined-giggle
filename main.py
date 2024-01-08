0e1d3c9c6e33799c317173f086d58ea6269027a4
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:59:33 +0000

    wild emphasize slow string escape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
