bdb79c35d8b1f49434d586cb2e2abcbcd592c6b5
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:33:02 +0000

    mass sticky friend brave grateful

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-from collections import Counter
-print(Counter("hello world"))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
