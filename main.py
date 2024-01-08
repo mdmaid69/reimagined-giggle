c6b53b192e917668c287d0056a3bca436f59332d
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:55:49 +0000

    more slip treat of height

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import time
-def get_current_time():
-        return time.ctime()
+for i in range(10): print(i)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
