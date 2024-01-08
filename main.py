a7db32b8456518925b36398a18e2dd1c6a31c174
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:04:02 +0000

    pupil flow light store prepare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
