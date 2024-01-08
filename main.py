f8736662e779992612ab0d64096c8bf3a2b7490a
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:52:44 +0000

    represent disturb government value ocean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
