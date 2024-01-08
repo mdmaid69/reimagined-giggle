0deee134e1fe383ade028f47551ca8e3a56ea948
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:34:33 +0000

    soak neck share any no

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+x = 10
+y = 20
+print("Sum:", x + y)
