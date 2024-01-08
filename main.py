882108f3e8f80089997e583257b53e7279f6efc0
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:29:05 +0000

    wealth turnout kind mystery believe

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
