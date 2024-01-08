f8ee6802122bc42c646a567221d6e2664afe6176
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:46:29 +0000

    run minimum their my ignore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
