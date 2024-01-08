f0706cc23f217b9026217072c4318ec98888d19c
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:22:31 +0000

    defend escape far warm accidental

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
