196ca7223abe21b93ac4a1026e3a8f20137b2ea9
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:07:11 +0000

    upstairs male far transport bit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_area_circle(r):
+        return 3.14 * r**2
