2b3bce043b103d71018cf591da40bfff718e693a
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:39:42 +0000

    wherever prepare match solve typical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
