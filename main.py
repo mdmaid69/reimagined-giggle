924eab536c0785bbfbc21fa01cde8542055ac87f
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:05:08 +0000

    one mark fear interest address

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
