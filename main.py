8f45e2b12ffe384da3161825dfcf66748b56fba2
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:31:41 +0000

    education stupid full uponon excellent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
