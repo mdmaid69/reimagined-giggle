b48105a883fda0224c8f585c7bce93efda83707a
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:36:57 +0000

    recover both praise past window

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
