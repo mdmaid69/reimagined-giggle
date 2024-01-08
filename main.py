f8c895fce266336deec9fd0ebf8f8571c662abc9
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:05:51 +0000

    both change limit put smell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import re
+print(re.match("h.*o", "hello world"))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
