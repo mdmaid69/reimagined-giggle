7875e88040a4efc720438179dcefca1932fdfb15
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:04:09 +0000

    answer forward everybody arrangement rather

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def convert_to_hex(n):
+        return hex(n)
