6fe27abff3de5a314c3fc9e2fa2d8e5ab0541b47
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:06:34 +0000

    contrast drink lie suit communicate

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
