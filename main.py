dd551a5e3c790ebab391f4e7fa76f1b291c29e8e
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:27:23 +0000

    finger pretty around law manner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_volume(length, width, height):
-        return length * width * height
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_work(force, distance):
+        return force * distance
