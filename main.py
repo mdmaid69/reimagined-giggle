ec6f4b19b5fe6a9837f1a56726ca81ef55f683b4
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:19:26 +0000

    present steer half hello recovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
