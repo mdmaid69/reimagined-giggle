f5aaf385bb5270985ce0cf4ae3b77f8aff7a9bff
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:25:20 +0000

    open dry sink Mm live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
