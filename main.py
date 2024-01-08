eb85cc43efa84b5ee432ecea72c271a8acfed998
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:21:18 +0000

    tip manage pleasant sit beat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
