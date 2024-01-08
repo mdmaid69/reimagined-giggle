9d2fab70b3084f12e75e8f1a5e1b23ba7ee73e09
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:25:34 +0000

    appearance stare invitation lend stage

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
