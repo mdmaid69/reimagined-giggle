ec3434fb96c0a0792004645f72eae25233c33d47
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:43:52 +0000

    now properly bunch lift run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
