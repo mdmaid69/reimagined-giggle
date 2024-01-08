8c17ab9199e7be3999a032143cfed5f3a3c49784
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:08:11 +0000

    noisy cry replace outside keepout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
