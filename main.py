102c4844de48a20c7d8169c5862f4ef6d2eea984
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:53:10 +0000

    up sort over gradually bread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
