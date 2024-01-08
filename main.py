1dd641d2b556dfa96a5709c21574a67d36830e76
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:56:06 +0000

    concentration number disapprove mention careful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
