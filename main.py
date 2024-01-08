2216f1fdf6c42933f4741adce4bb4e02dd9c7970
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:16:18 +0000

    shadow beginning pen belong accommodation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
