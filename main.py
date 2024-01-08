f5e5cafa0e53ce86fdfea3bdf4f136b0c1deaba2
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:23:32 +0000

    attractive excellent back sky market

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
