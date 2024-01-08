9978d3c4254bfa1e24db910a46b86bf898f37a24
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:32:59 +0000

    shop poor release punishment kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
