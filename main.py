2b557b7694144f2727bb61ab9465c52eccc626dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:10:38 +0000

    plenty correct arrival only while

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
