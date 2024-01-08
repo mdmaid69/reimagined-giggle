c86b7dd701fdb87a8ee7db956bab84688c722c31
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:03:06 +0000

    along star scene real understand

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
