693e953bb2177b193d92c519cf45e16c2db08f03
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:26:56 +0000

    get memory violence aim hole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def get_array_item(array, i):
-        return array[i]
+def convert_array_to_list(array):
+        return array.tolist()
