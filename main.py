3529bed01b2497e709714f9d32ff3568b1a0aea3
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:03:57 +0000

    most pool wander frighten any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def remove_duplicates(lst):
+        return list(set(lst))
