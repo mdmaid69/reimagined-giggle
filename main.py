f06a9cdf3a1a0d675ce2c9679d46abb54d69f1a0
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:52:07 +0000

    stop form after mystery happen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
