4ebdf07da345d94a806e4a61a26a69aa3307aa28
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:31:41 +0000

    classroom crazy need probably main

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def count_elements(lst):
+        return len(lst)
