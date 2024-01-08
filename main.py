c06fafbe16e7036fab49ddcda0e1a1df26e03aee
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:21:18 +0000

    spare light develop fast joke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
