a12a90be8b8adc926d3b6eec20df31554138ee58
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:42:59 +0000

    part fall treat lot kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
