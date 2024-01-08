5f6b3d62d2ba53f2e48397393e273cc36ee0f0bc
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:41:26 +0000

    keepaway invite broken reduction travel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
