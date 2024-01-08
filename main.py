e1a54c43a504e9a838d8621c5c701f87ce8c608b
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:28:08 +0000

    pinch music tense identify thirsty

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
