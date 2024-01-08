e45f210f28b3a8a8b4ef2e97a39ac76520589259
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:51:54 +0000

    final moral petrol cold total

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def get_array_as_int(array):
+        return int(array[0])
