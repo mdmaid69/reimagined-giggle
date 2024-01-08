815fd55502def731d68f408029ee030ca3ed2758
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:20:57 +0000

    any keen content Pp attention

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-print("Hello, world!")
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
