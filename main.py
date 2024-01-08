68ec5fda21b669986e2e845599ae9a0cfc6bcf14
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:30:15 +0000

    unlikely scare shrink still sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_slice(array, i, j):
+        return array[i:j]
