dc1f93954dd80d1191e83fda3f6b4db865896634
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:24:12 +0000

    wash job actually off pleasure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import getpass
+def get_username():
+        return getpass.getuser()
