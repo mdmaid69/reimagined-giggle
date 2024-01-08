ad8bfa20255548f25a4400247d6a265961535dbd
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:33:33 +0000

    guide dish radio view ours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+print(math.pi)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
