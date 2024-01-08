ae980c7b7b8df303fe2ab2a1265aefc8bab72728
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:07:58 +0000

    bored single large patient into

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def clear_array(array):
+        array *= 0
