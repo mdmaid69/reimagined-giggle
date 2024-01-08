7c2f1bc298988d41565b186ed799f7aa21dc3147
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:20:11 +0000

    fly power identify score perform

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
