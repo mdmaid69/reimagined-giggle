7699cef21453b97ed5065a06fcc6001b49d67bb1
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:20:06 +0000

    that speed left snow actual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+text = "Hello, world!"
+print("Uppercase:", text.upper())
