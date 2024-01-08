875b6c8f4274a07bce65e162375144ace1667dd4
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:32:35 +0000

    about extremely mess art maintain

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
