fd7fa1a127f79c0065163c2b47d11934ad5cd664
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:52:40 +0000

    total wipe hope as improve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import datetime
+def get_current_date():
+        return datetime.date.today()
