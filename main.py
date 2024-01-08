31f1586d8c1c82cc566376dd84cdab4b3be0dd96
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:09:25 +0000

    dare pay drive set little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
