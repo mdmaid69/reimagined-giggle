981d957ae260bec6ed9535df3c3f46f7895bb807
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:32:22 +0000

    during best consult calm people

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_user_list():
+        return collections.UserList()
