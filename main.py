0f8e90fc92c163cfcd4ce4b5600d58ddffe66861
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:24:18 +0000

    cut welcome feel famous visit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_list(array):
+        return list(array)
