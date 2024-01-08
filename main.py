06eb966c2f8d430849271533e761f7fb58942611
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:57:41 +0000

    level himself substance become off

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
