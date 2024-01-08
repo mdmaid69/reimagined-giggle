616502febce39065668474393982b460a840ccb0
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:15:44 +0000

    language separate artificial name government

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
