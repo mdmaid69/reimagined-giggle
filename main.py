35066bfb52658b32569141e39346f473ff15f461
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:28:42 +0000

    mean church back reduce brother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
