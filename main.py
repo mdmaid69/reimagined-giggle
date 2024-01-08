9237c9b5ff94f8a2f6cf86e85ed553563b500d76
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:04:23 +0000

    tall considering deep report pity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def get_array_item(array, i):
+        return array[i]
+def calculate_force(mass, acceleration):
+        return mass * acceleration
