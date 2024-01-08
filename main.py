29a115b64db194dbbf13b38947bd76a8e0d41a36
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:49:25 +0000

    affect book sense mass argument

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import numpy as np
+print(np.array([1, 2, 3]))
