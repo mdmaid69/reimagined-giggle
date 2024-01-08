f3b28e905e9672eca6845f9ac7334f62ca38528e
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:47:15 +0000

    goaway report estimate cry whom

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import numpy as np
+print(np.array([1, 2, 3]))
 import array
 def write_array_to_file(filename, array):
         with open(filename, "wb") as f:
