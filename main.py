afb2dfca5820cbc6123db8f70849d2def7055fa6
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:37:58 +0000

    official fairly charge marry deserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
