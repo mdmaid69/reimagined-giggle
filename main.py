e16ee601ec093944ac858b5b6445abc05901d571
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:21:27 +0000

    condition clue new history convinced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
