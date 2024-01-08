6ef89a19ede6c7806b980e01025aebed32107845
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:19:25 +0000

    none surface very cry ours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def subtract_numbers(x, y):
+        return x - y
