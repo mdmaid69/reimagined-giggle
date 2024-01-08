36e2dfdd0603815bd9801c35fc3fb8fb796ce4e4
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:00:48 +0000

    expression protest settle knock dangerous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
