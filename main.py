a3f3f8b7e83f640b19d3e15a6693d634c1b90f88
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:51:24 +0000

    black but bag listen intend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
