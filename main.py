e1fc24a937e312f0d39a7c4206952c63447b309a
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:50:02 +0000

    appeal advanced mistake design demand

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
