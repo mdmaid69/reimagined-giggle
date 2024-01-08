637adcf3f70fa1794fbb64ab3bce8e7cc69fe4e8
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:02:04 +0000

    sentence band and careless turnoff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
