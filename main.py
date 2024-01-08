8447778c99843092e726c7c52033c4ed373b8cfd
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:55:05 +0000

    sing here correct mind hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_area_circle(r):
+        return 3.14 * r**2
