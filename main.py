27b6bd58cec3227a0408162d6b50978f4e390085
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:09:07 +0000

    patience increase schedule substance safety

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def square_number(x):
-        return x**2
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_density(mass, volume):
+        return mass / volume
