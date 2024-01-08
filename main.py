3aebd2aeb525f39f04a38e07b2b3ddd5d117b813
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:42:00 +0000

    weekend cinema allow black say

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
