1aa91db6f8eb4aee277045e2191b87a5edff64ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:59:25 +0000

    mind pleasant rely suit person

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def clear_array(array):
-        array *= 0
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_bytearray(array):
+        return bytearray(array)
