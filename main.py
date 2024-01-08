3c0b5cbe0acd9727b60f48a417c6c914a7922333
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:04:10 +0000

    mother much result environment comeoff

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def is_palindrome(s):
+        return s == s[::-1]
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
