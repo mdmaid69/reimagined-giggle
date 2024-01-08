5b41bbb1eff8a68f3c3eca4f59c3cade9f5cb7cf
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:31:11 +0000

    least check sky agree past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
