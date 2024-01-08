6cb89946f37b2bab3b7a4143408d85f47661a4af
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:35:23 +0000

    guess they year mess happy

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_area_rectangle(l, w):
+        return l * w
