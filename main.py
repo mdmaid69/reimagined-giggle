5f6618ef66871362d23329d286f70779b0b28b26
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:09:20 +0000

    kind wander proud keepout right

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
