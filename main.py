af3a9ca1c5ff92c4cf89bbe31460b362abc789b7
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:21:44 +0000

    replace break basic arrange treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
