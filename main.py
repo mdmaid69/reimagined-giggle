bd53b514fad40b70e25eac1a197101a576ec1bf2
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:38:13 +0000

    Kk slide guide circumstance expect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
