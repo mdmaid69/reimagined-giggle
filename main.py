2daa1120e85e5a04de5733d865bed25bd725ebf0
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:30:01 +0000

    promise on once completely rise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def reverse_array(array):
-        array.reverse()
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
