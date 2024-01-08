305f6d1d61673e5904545746d6932267c03a554f
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:22:29 +0000

    escape imagination language it greeting

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
