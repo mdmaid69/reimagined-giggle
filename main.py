4701dce8e91a3aa4cc38f43b0c8821995fae1c45
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:06:22 +0000

    quickly test dream impression though

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
