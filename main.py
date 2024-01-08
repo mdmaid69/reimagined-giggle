b22a2598af1c03bc5373e7366dadb789e0bd6b9f
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:26:39 +0000

    neither profit polite involve damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_density(mass, volume):
+        return mass / volume
