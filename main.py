8dacadc8ff75b84c4558186e24029f14b5c78fd3
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:37:19 +0000

    team tool loose subject choose

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def pop_from_array(array, i=-1):
+        return array.pop(i)
