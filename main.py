7b97fd0f82a491d9638be5617f60b4fb100d0f96
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:40:39 +0000

    mark pleasure example slowly your

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
