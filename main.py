05825aa8ce10e1dd039c65210e05ba4cc35fb859
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:57:10 +0000

    hollow repair pupil no turnup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
