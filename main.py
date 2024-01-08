9409b4f72d06e9407796459a42f30dc2337ec529
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:22:55 +0000

    today during punishment collection forget

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import collections
+def create_queue():
+        return collections.deque()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
