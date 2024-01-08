40d900fbb3ab635a828399ceda160afbcd5a14db
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:26:49 +0000

    exploration suckme ready stir list

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
