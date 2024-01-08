08da6655051e82de3a95ac6864965dede7ba9d3a
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:57:59 +0000

    through steal until concentrate call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_gid(file_name):
+  def get_file_group(file_name):
         return os.stat(file_name).st_gid
