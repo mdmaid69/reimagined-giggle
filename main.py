765e6620435a1b1c67792990152bd3000d3d4389
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:22:44 +0000

    star here dance sweet offence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
