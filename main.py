fc9d59b18a15c31ade09ba909fd5cfdbef4b62bc
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:17:17 +0000

    benefit active workon likely perform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  def add_numbers(x, y):
-        return x + y
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
