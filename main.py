85747b67fe20c349da28db0abfd5de83123fcc91
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:43:50 +0000

    lonely compete month bang thankyou

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
