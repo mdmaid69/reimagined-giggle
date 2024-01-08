3f2950d9617fffd6539fcf8179aa2e5eec2a9c16
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:34:59 +0000

    score invite basic rid comfortable

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_power(work, time):
-        return work / time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
