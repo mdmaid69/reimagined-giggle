c89498f9df49a71e0fe877315043f9cf9dffed0f
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:56:57 +0000

    trap fold increase stranger later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def square_number(x):
-        return x**2
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_str(array):
+        return str(array)
