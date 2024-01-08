6592d5aabd227c79862b285ee62226e5bf058623
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:16:12 +0000

    century pronunciation pollution dangerous bath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def convert_to_octal(n):
+        return oct(n)
