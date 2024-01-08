b6235c9d62f1a0b5fe992938113dbc5e1c3043d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:53:46 +0000

    care container dig dull talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
