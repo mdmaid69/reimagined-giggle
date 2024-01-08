89cf9f99fa531b1336ec58716d4e410d19f4f417
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:55:35 +0000

    do clean outside disagree do

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
