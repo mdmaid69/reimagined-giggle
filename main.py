eb4270b0a4a26f633a5a5a6d79ba96767b4e4dd2
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:15:00 +0000

    foreign burn familiar building country

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
