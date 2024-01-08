6d9ee1b7f36793af71132d2c8b87def00dada2b3
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:49:12 +0000

    sweep top next along recovery

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
