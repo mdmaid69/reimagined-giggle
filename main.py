191fc96098ddfd6ed9644d166283ae8a0885545b
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:13:56 +0000

    trip opinion theirs pure ill

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def add_numbers(a, b):
+        return a + b
