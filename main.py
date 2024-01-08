ef1073624de81f83246ef579f6968b15f6dfd1b6
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:59:50 +0000

    many careless callback show professional

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
