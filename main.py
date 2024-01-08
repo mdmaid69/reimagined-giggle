b26fcb5658861045b4124ff36763b2a936eaa80e
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:54:40 +0000

    many independent declaration cut lift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
