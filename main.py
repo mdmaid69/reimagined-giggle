1fed3e15f9dc8a7101302c0da33097f903738afb
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:59:20 +0000

    weigh short plain clear scrap

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import collections
+def create_stack():
+        return collections.deque()
