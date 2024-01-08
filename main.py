0d779da6fc19f31884f1beab124ed4e6b95237dd
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:39:48 +0000

    subject rub smell convincing rough

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
