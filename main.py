639bf4cb8dd94481a2c6bf55a6d50c9268a80fbe
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:18:27 +0000

    us accommodation organize will hungry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
