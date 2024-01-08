86bd5fbf32df5f692b775f5750b6bee5bdb6e0e4
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:31:21 +0000

    relative sell total related do

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def count_words(sentence):
+        return len(sentence.split())
