18dec0c3b1e6d92d547e11e14f100b1696c55645
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:09:32 +0000

    consist pretty instead cum comfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def divide_numbers(x, y):
-        return x / y
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
