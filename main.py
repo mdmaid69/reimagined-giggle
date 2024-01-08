c4e9f09710e9c5804e3a7d6c2f75c5f62ea326da
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:37:21 +0000

    cost apply through clear frame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_density(mass, volume):
+        return mass / volume
