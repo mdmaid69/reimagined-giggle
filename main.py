9fd102a2305243666a29cb2e9367b242f2beb9a0
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:16:06 +0000

    talkinto something gentle sure concern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_speed(distance, time):
+        return distance / time
