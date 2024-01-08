98864cd8212754255a5b2c29fd05242698178c20
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:44:43 +0000

    good loud mainly key block

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
 import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
+with open("some.csv", "w", newline="") as f:
         writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
