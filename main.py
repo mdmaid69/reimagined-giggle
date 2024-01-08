7e19b2bb97beec46f9fc618e160986151365aa96
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:51:15 +0000

    bringup time takeout compete story

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def square_number(x):
+        return x**2
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
