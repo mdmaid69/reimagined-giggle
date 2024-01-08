5bdcd5e81df142e172f90b298d1a3a48c883a4ea
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:59:54 +0000

    moral record many fasten recommend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 def calculate_present_value(future_value, rate, time):
         return future_value / (1 + rate)**time
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
