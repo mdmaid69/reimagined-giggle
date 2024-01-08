27063895d50f33119a9977907b0ff0368411c213
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:43:57 +0000

    sure nervous reply agreement problem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
