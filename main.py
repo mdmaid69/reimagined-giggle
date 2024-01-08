d89c60b8fdb84d77fbbe0b6517b8fb53e1883213
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:48:12 +0000

    home dead shop vote consist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_current_directory():
+        return os.getcwd()
+def is_palindrome(s):
+        return s == s[::-1]
