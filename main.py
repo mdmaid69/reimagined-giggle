fd8876567c7a33415f048db2b65b885b73167890
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:51:31 +0000

    probably food level piece finish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
