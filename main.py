1081151d0e50313fd19c0f1687b75e11eeeb6f7f
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:42:03 +0000

    sure classroom sport surprise corner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
