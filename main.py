6db41eb7caf0a5bc1f30112ba9efbf07651377bb
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:57:25 +0000

    taste no snow announce plain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
