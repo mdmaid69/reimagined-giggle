60fa6928b183cb60b4ae3dc09c0f82c82009c96e
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:22:38 +0000

    emotion control satisfactory not wind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_density(mass, volume):
+        return mass / volume
