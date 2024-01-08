3297d29187ff719bd2e153f04f659ce5a9a24803
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:17:40 +0000

    stone talkinto patience start bringforward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def calculate_average(lst):
         return sum(lst) / len(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
