f132d7d075e09e55ebe310dce45347b04ed6cde6
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:28:15 +0000

    style general proud arrangement shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def remove_duplicates(lst):
+        return list(set(lst))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
