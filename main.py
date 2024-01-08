f8774b8fcee7e9c3d1736068f131316c3abe9e65
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:21:46 +0000

    pipe car sort affect below

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+x = 10
+y = 20
+print("Sum:", x + y)
