e7146ab20ff9468580a9e110cd55ef08d54b9b8e
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:26:48 +0000

    save poor before quick mass

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def convert_to_binary(n):
-        return bin(n)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import tensorflow as tf
+print(tf.__version__)
