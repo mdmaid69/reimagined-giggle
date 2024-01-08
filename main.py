77b3a7406d27672fb54f43a74a555f70bb9f0c34
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:05:25 +0000

    shock near finally turn where

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
