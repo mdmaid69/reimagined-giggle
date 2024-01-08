58d4ae3d1ff254f5dc994c916a39c6e598c51a16
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:11:48 +0000

    to narrow deal need through

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import sys
+print(sys.version)
