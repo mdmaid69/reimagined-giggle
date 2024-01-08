ec4b8b28c52aadea072e33c7965788d4fab13ecf
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:10:43 +0000

    declare punishment immediate noisy play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
