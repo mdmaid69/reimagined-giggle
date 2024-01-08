46178a7618adde8885f73ff7360cb52f8a5f40cf
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:20:46 +0000

    common either public hang black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
