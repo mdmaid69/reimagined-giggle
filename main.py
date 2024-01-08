6dfdbfed3ef3aca6b54f240e0489fff03df2ee7f
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:50:01 +0000

    careless story slight history remove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  def subtract_numbers(x, y):
+        return x - y
