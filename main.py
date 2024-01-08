4c1d71eeb300d5b24c34d2f84cdf871ea0ff360b
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:09:46 +0000

    rubbish no conversation weekend fierce

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
