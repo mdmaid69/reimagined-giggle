2d08568cb57e39cf6fd1790d0b452dbc2ea0fa69
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:03:57 +0000

    business equal affect guard cough

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def convert_to_octal(n):
+        return oct(n)
+def count_elements(lst):
+        return len(lst)
