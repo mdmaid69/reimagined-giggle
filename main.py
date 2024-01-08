2d0e95d8a09001b3ff09d0262de632b29c87c87c
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:58:12 +0000

    soon recovery convince suffer club

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
