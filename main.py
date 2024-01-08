b85adbdf24fc67bbfaef766fb01da52b30f59a12
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:30:02 +0000

    meanwhile touch size Oo appearance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import numpy as np
+print(np.array([1, 2, 3]))
