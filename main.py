4cd6cf5e2f4babb9fb34859ee63a68c7d411a5c5
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:45:13 +0000

    let mixture nowhere knock happy

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
