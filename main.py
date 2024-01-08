0eefc7905dcf98949a69354b0376ac2505891963
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:08:26 +0000

    pump impressive intelligent pure rate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
