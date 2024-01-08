435b56b38fb1da12ac23d523558955aa43e51970
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:42:32 +0000

    crisis talk reason introduce glass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_max(lst):
+        return max(lst)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
