3686cbc3273e353aa7c61c9e1218d50e115d01c4
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:27:47 +0000

    identify arrangement fit underneath heart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
