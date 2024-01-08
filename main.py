e5ec09ca141ab937cdb9f86754e0c50dcb87af1c
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:15:58 +0000

    second they of move work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
