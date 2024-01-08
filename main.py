952397e40d007f244f1924f444c5aaabf82c0262
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:41:10 +0000

    around proud protection cheque angry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def count_characters(sentence):
+        return len(sentence)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
