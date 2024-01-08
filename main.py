6725e17f11285087246c41ea7fd0cb8a43e7c897
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:09:22 +0000

    peaceful pause brother ever port

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
