9ba57ebebb5f50260f4ab790def2bd7c8bf63859
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:18:16 +0000

    this say freedom likely see

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
