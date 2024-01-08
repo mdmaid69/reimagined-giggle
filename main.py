e8a0465beae9f386f7ac5e5b7cbdb3ba98ed44a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:56:45 +0000

    whichever occasional age refuse mystery

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-print([x**2 for x in range(10)])
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
