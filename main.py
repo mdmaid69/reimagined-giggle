c050c165380831cf7920120eea60084f395aef38
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:57:38 +0000

    chin getup surface unable sink

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
