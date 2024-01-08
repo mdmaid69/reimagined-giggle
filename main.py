82ed9ad3da82c5a3abe080e4ca76eaafba5979c8
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:02:17 +0000

    around runover street everybody what

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
