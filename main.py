3ee6c1e5b2a8dcb08287d9a4835179dd5557e22f
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:25:50 +0000

    failure risk while priority bill

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
