a4a529994e34cdcb064a9b2d287e9cb3c3ece36b
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:46:08 +0000

    effort they taste shout tell

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 def calculate_amortization(principal, rate, time):
         return (principal * rate) / (1 - (1 + rate)**-time)
