8d09c9901b879600eae9f69c1a3794a45642cbaf
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:51:37 +0000

    fall importance front exercise test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
