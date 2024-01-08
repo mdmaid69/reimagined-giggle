a206fa051e3921e32e2067b3f24e0885fb2feeca
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:15:08 +0000

    cover bit much important somehow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
