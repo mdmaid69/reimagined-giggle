0cb41c73c504fadfdb702645513b30d414cb9a65
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:01:48 +0000

    without forever there interruption no

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def multiply_numbers(x, y):
-        return x * y
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  def count_elements(lst):
+        return len(lst)
