b60f144524ac3d5008dc8bbdb5beecd756fab71e
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:43:24 +0000

    post touch probably obtain female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
