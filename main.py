bc6b600f2065450bc5b8feb334c3f8f903f6af14
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:00:44 +0000

    plant bang furniture table sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
