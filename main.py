8009acaa6d05fe3aff548a4696817d74f86df431
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:56:24 +0000

    create before absolutely office flow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
