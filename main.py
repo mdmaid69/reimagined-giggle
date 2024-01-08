2f3385cfdef1105efb5ae2cb350e50bd113beff9
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:27:49 +0000

    bath 567enemy shape mood cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_tangent(x):
+        return math.tan(x)
