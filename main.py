59f2c22afc17f564e1cb16ad87b5576f2fac901b
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:33:01 +0000

    one takeoff could over battery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def square_number(x):
+        return x**2
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
