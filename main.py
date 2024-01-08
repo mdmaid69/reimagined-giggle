871ba102ba01ef255351010859322c90c944afc4
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:34:57 +0000

    when breakinto doubt depend getback

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
