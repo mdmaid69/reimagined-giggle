139b5b04d8d695bced40649aa79a7a3dc7b87ec1
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:00:00 +0000

    negative might stretch little proof

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_pressure(force, area):
+        return force / area
