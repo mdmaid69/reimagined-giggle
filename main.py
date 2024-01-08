ee8473952445d27759524242a4c6ab701f97cbc3
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:57:17 +0000

    owe half perfect contribute connect

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  def remove_duplicates(lst):
-        return list(set(lst))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
