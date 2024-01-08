9e8afbce0939e45af4ce591db6eca26def6a83bb
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:59:00 +0000

    pause circle high year post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def add_numbers(a, b):
+        return a + b
