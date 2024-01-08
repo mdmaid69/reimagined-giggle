f2a2abf716a66bbb30975f289dd749926de4998a
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:03:27 +0000

    report myself active luck programme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
