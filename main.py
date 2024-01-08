694aac65ff191e67df2183b56d7ee3ed2ebd6661
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:46:24 +0000

    substance squeeze discover strict protection

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def count_elements(lst):
+        return len(lst)
