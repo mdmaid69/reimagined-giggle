b4711bbaf9714e5045a583534915d3b802cd587c
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:33:26 +0000

    passenger properly bank rush fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
