e475f4f7596dc153f33b29432dad579de371ee79
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:20:38 +0000

    comment grand scrap straight because

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
