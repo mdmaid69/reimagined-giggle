37496910a8b49e81ff9bb40a76aa4e227a551618
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:23:31 +0000

    other callback this concerned agree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
