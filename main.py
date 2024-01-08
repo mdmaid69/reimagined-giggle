f8118d5d01903e93adf0e44eb9d65b69ab579085
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:09:05 +0000

    fun swallow concentrate public note

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def remove_duplicates(lst):
+        return list(set(lst))
