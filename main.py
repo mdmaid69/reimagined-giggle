3722a2394b668ad9a4bc89abd7ecbe447a66435b
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:52:12 +0000

    coffee rent save grow mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
