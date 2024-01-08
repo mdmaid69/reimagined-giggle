2aee4122034dff8ad0053d71fc0f9b91737c2bf3
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:15:37 +0000

    intelligent over flood friendly confident

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_work(force, distance):
+        return force * distance
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
