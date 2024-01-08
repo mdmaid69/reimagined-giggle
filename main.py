90166c07a46ed193c229623960bb792fba5026e0
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:09:55 +0000

    back whole least appearance 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import sys
+print(sys.version)
