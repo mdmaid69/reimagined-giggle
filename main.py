0ba7b519e6a3fee232c6b9e53f41300457d2ce30
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:29:02 +0000

    noise steer science relief age

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
