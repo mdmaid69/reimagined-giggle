cee364571a751f21fb8ef0ba32be41ab5a1d5b89
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:44:12 +0000

    mix imagine middle judge punish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
