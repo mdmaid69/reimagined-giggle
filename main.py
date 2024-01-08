c68164a9238cc42c9b81fcb580ff401c37d7115f
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:44:17 +0000

    getaway unusual either sense new

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def square_number(x):
+        return x**2
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
