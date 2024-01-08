7802612a410a2a3a2def55d6279f795924edccf3
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:00:09 +0000

    any mine conclusion want nurse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def get_array_as_frozenset(array):
+        return frozenset(array)
