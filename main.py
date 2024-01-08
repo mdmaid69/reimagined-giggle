44b97d06b09c6281525ff5e031c5f514f7161faa
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:47:53 +0000

    well keepoff now freeze comeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
