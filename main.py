aa7a8995a802df07664541376b1bb22a4e1c29d1
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:49:32 +0000

    hope file rush star neat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
