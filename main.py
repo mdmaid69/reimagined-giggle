cb82d84cada8d974bf928e9acd065c364bd99c99
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:20:51 +0000

    level hang print space unhappy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
