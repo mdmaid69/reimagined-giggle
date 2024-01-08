f36a1ec4a29dec7ae8e4d54da6bd9b1ae7b14bd0
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:28:52 +0000

    wash watch conversation trip bottom

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
