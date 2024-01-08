514b449071f9a6fb12f75705b83b0925c08354b7
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:24:11 +0000

    cry comeback compete favourite early

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
