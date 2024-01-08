5a80df9ae2364c2d9cc30144708cfb85f0931200
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:19:22 +0000

    interrupt empty fair choice second

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
