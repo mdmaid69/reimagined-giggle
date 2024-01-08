8c5ce3ab9d5e5673414b4efc3dfd7a3db04fda89
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:18:56 +0000

    pupil suddenly spring recent spoil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
