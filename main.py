0afcc1eb64ea43390823c320c6f7240ade27c331
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:36:41 +0000

    sit middle justice poor negative

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import platform
+def get_os_info():
+        return platform.uname()
