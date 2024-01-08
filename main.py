0115ac54ca875fdb0d098262bd24e7d983cd0e2d
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:55:04 +0000

    may so importance song rent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
