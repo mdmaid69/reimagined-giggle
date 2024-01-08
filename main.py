b07b5454041ccde0b406f8d2cafd6322b1853743
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:32:33 +0000

    relax keepup question bet aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def sort_numbers(numbers):
+        return sorted(numbers)
