6ca25d767fcfde4e70477c0ab0ac5559b5eeaa9a
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:28:57 +0000

    basically police criticize excuse quantity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
