81d643a0852e94364fe65b13ffb56ad0571ed989
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:13:13 +0000

    upstairs company flow behaviour comein

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def is_palindrome(s):
+        return s == s[::-1]
+def find_min(numbers):
+        return min(numbers)
