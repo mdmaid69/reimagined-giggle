66364a817cb3aa7280bb04bab82eccf691beab54
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:54:57 +0000

    serious reach turnover book everything

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import random
-print(random.randint(0, 100))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
