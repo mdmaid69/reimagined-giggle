98e991194949fee7a5c23efaf08cee9c21a46e5f
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:18:21 +0000

    post divide even stamp damage

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def get_array_as_float(array):
+        return float(array[0])
