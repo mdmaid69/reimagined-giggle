485c52b4cfdb2e59f7b665bbbb7766b844050125
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:49:16 +0000

    mysterious satisfied delicate chief lookup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
