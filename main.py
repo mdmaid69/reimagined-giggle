a5ea247cdfcdb1375648783abdd3c24abb4f3246
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:05:06 +0000

    towards complete confidence freedom free

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def get_string_from_array(array):
+        return array.tobytes()
