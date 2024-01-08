5f5dbe5b4f44d3b089e24d89b71ae5e37607e8a8
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:39:07 +0000

    good dance interesting furniture regret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import datetime
 def get_days_until_next_year():
         next_year = datetime.date.today().year + 1
