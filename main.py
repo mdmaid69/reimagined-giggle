2877e942ce42e4eb81176f737cfd928785852247
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:02:29 +0000

    concentration unlikely half collection slice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-for i in range(10): print(i)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
