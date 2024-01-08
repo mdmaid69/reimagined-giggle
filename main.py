340681911e12b49108e1ab89feb23964ac68133f
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:27:55 +0000

    discovery charge already either cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
