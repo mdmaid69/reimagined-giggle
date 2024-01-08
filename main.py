c7729d67173276efa1a91b002a319ab8f4b4c26f
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:07:57 +0000

    traffic Ff glad party bush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def find_unique_words(sentence):
+        return set(sentence.split())
