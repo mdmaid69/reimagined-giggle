78105085ff4566fbf343ec1329699807d86fa944
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:43:35 +0000

    cost 179bite myself previous exercise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
