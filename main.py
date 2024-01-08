c3455d9c86fb96d9d30046106888ba36bb6423af
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:07:40 +0000

    uniform tonight respect rough way

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-def find_min(lst):
-        return min(lst)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
