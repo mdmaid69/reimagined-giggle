143aebe7c4a9a3e67240b8cfa726531285678457
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:30:55 +0000

    left change bang truth well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
