73855b958f7df6726df382060b69b2a7dea5684a
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:16:04 +0000

    eye unable punishment always drive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-print(sum(range(10)))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
