c839ac9a676b784ba561fc783091de1467f21bba
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:57:56 +0000

    Ff clean where everything failure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def find_max(lst):
+        return max(lst)
