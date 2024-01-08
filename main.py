06d69c33f1ea73e7aefcf1f5827d571a75dcac4b
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:14:57 +0000

    correct so matter position neat

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_list_from_array(array):
+        return array.tolist()
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
