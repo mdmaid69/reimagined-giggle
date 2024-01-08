d305e0609643958b5e62f814dae3490f4ff1bbaa
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:30:58 +0000

    runaway see theirs near commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
