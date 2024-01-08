df033d52717d37f2ddc64a52765418278960ab60
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:29:48 +0000

    path other board future lock

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
