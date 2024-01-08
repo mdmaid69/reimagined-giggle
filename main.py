9e9978b729eecd6c9e093250a4e507792ca6af4e
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:53:21 +0000

    gather worried wave accidental union

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
