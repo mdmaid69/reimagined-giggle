ffbd9cac89030b6084fc31c3ef09a1b12afba750
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:45:47 +0000

    emergency have here please opposite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+  def cube_number(x):
+        return x**3
