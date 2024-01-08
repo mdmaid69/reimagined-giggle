83d3c14d846a7b92dc1dd13f65669fe161c08a94
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:26:39 +0000

    equal cover regard goodbye listenfor

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
