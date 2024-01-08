640ab69c6c25f0c8d2d67b417244031408f20c55
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:42:43 +0000

    mouth married table comment tie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import sys
-def print_python_version():
-        print(sys.version)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
