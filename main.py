9fce5f62af93663dbdc1ece56a15fd2d50dc62c3
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:17:04 +0000

    empty whisper emphasize waste explanation

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
