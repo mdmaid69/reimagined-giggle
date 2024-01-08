639690c0bdcc7afc4a5e7b1feb8fb7992b637f75
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:09:00 +0000

    order change previous few wide

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def cube_number(x):
-        return x**3
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
