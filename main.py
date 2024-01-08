b5a743c08a5720cbaf84e4f8e318163766a95822
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:21:21 +0000

    hit plant expression think war

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_area(radius):
+        return 3.14 * radius * radius
