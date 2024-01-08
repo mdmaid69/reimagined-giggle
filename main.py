cbff022e7075d5058a583ed3a60c807be78dac55
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:26:31 +0000

    aware lower talkback genuine move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
