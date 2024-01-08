05cac0f63d69acb2960faa82039dc67aae6ed716
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:32:12 +0000

    add chase concerned there them

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-print(math.pi)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
