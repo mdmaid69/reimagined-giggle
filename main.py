b3d7f2dfbb12be7dfd87f46a9517dea04ff47b56
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:06:26 +0000

    emergency impressed back matter before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
