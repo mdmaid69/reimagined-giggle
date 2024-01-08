ffcea2986dba223660038750c0c23814b9e500b9
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:13:46 +0000

    touch detail they behind clear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
