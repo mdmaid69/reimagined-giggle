36eda736f78bdb4b83631085bf8660182f2b9d3b
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:17:31 +0000

    authority it still poison dance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
