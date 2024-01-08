f167171677d86f45c855f22d1624601b8d950c36
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:53:23 +0000

    there tap bringup unhappy cheque

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def subtract_numbers(x, y):
-        return x - y
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
