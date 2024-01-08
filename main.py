6d562c45ca1f7446df37bfba6ea6215d5d58a56f
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:20:54 +0000

    wait instrument advanced accept appear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
