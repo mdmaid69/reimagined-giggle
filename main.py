cef56d793ec4a0e61be045539212e130336926c7
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:20:36 +0000

    simple table possibly intelligent health

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
