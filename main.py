3d0f9e022c7c6e1dded404dcf292e7fba1c8ea65
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:02:14 +0000

    join concentrate inside law size

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
