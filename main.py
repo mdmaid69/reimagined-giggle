af0bf697e18b6362e30c52c8b9fa1ba2ebe89cc2
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:31:42 +0000

    clean minor deep towards safety

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+print(os.getcwd())
