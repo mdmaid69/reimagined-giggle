e2c4dd6e7df9346ce6cfe31128031c3140dc7703
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:10:43 +0000

    brush change limit aim learn

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print("Hello, world!")
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
