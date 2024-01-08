9ed103c91af80f136f511833f1796d9772bad17c
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:16:05 +0000

    wait friendly slight 573entertainment spite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
