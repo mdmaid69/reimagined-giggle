38148b4a35af63807c42c1e2fb4cb9fbd2233e99
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:55:54 +0000

    right debt force confuse worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
