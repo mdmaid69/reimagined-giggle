a33d5381a3a8a0b0518dfcd132620df284c82acc
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:22:57 +0000

    them otherwise programme send cope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
