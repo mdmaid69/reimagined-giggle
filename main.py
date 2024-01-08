060ac93d6028320ae2db6d56291be4a72ac6560f
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:51:02 +0000

    plain cruel where later speed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
