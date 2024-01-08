3df5bea73469d9737e54973c14b4ece38fc67ee4
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:40:37 +0000

    wet error each mine influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def square_number(x):
+        return x**2
