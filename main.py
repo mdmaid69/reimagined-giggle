b6f9f27f970660563145510b696c7fbd81021cd2
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:03:31 +0000

    get comeover table team less

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
