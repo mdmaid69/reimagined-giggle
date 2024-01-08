3c9c04dd5106cca23710c9ecf5d716fd22002a37
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:20:35 +0000

    fall sure snow leg disturbing

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
