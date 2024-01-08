cb9f877be7d3980d3b7f8b8c150a108c8f5db57a
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:55:53 +0000

    now future towards knock nonsense

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def calculate_acceleration(speed, time):
-        return speed / time
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
