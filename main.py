0f21ba678451b0b72643e500b034074f6e98d4bc
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:28:11 +0000

    pay conclusion giveout avoid spring

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
