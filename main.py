99d24f601af707c16fd7b67775d251eb8d6efeb9
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:07:05 +0000

    left greeting ill Ss dangerous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import os
+print(os.getcwd())
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
