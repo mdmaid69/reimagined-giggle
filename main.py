3c277396a0ac55a4b05863e4c3424aec3d9bd38b
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:13:10 +0000

    pronunciation base runover wash get

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_area_rectangle(l, w):
+        return l * w
+  def reverse_list(lst):
+        return lst[::-1]
