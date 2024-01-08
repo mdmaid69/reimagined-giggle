fa3abd5bea4bfe6cc721ef506b8ee23b1f13be63
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:23:45 +0000

    face almost shut recover dress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
