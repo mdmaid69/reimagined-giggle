b6f0e123de4b08b3d1af4ab8a384216d94bdf091
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:43:12 +0000

    easy serious present doctor lean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
