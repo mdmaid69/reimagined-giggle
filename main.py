5e5037d8400705d554ee28a8c91cd52f86bbca7d
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:01:39 +0000

    sink branch juice rarely event

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def reverse_string(s):
+        return s[::-1]
