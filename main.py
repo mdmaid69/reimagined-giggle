481df7db56e90da4b5b1e07ef22d385e25c6362c
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:16:57 +0000

    manage better say uncertain furniture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def append_to_array(array, item):
+        array.append(item)
