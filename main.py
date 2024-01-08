b4e148344da593f30b5b3f66d9e60852522e947f
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:19:58 +0000

    recovery soil wall number yourself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def remove_duplicates(lst):
+        return list(set(lst))
