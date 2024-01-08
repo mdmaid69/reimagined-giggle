a1e5f983ec37219506a0c36dd483157facbe3acb
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:05:23 +0000

    spot none near fetch move

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
