1f2bcc3778008f38b2040ed9db938a9030641564
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:24:15 +0000

    story inquire leave black cool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
