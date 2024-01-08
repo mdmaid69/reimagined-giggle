50a19fac4c30ab72bd77bd7dd901236449c4d7f3
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:13:42 +0000

    division height top similar mystery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
