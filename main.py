ba2b8730c8740aee68c2f9981ae85c2ac7af6bae
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:39:59 +0000

    inside talkoutof straight wealth 565end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_power(work, time):
+        return work / time
