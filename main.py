ff25429b3f9f4e4e50bf3f4af8941985efb8f526
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:34:44 +0000

    secret move keen left sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
