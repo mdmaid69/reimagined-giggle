5a4fb59843d531f2d5372347e6ad45cdcb470239
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:26:25 +0000

    introduce praise question recent doctor

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_force(mass, acceleration):
+        return mass * acceleration
