2ac01d9c1e99c27562d01111be248d45b49c4c86
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:23:58 +0000

    design factory brother glass hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
