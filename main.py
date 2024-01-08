c9bd8827325e18acf3767b857b31dedb30b82281
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:15:24 +0000

    company plan cap ability smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def get_array_index(array, item):
+        return array.index(item)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
