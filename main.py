18cf25b847c0c253f8395d2735c93fc1efbeaea0
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:39:24 +0000

    live below game equipment shade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
