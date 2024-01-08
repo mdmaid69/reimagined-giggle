7cb50c27f4c6880532e9cd444bef273dc2872c01
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:52:03 +0000

    compare group onto exactly fail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
