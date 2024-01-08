fcf0078001f8c0ec1a3dedcf99b18ad31757ed34
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:54:03 +0000

    factory various hurt impressed extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
