01c70825e84144ff5013853a2e305a0d15c254af
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:13:59 +0000

    to custom pour poor nerve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
