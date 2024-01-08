2fc32c17ba95d8b7f92f4d4764c8d8aa19b0c4a3
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:19:23 +0000

    child square chief weak comment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+for i in range(10): print(i)
