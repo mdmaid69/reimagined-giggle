85fb8acfc7e2558f55ba619b844c90be2729103a
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:37:03 +0000

    cry pure centimetre bringdown weekend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
