d109bcb4fd5f92482dcd35710c5e8efeaa436ebf
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:35:56 +0000

    over turn impossible how shop

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
