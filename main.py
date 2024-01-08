d4a813dae4420a8c654613f3cbf71b9110b331bc
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:54:22 +0000

    mind goup mark shame conversation

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
