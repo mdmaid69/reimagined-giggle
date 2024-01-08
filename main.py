489ea3a7a03da7eec50f8dac3c9150b8b5b098d4
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:50:39 +0000

    mass drink vision serious rare

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_pressure(force, area):
+        return force / area
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
