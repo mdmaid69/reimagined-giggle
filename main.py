41106abdb9c471e32803d9e4f00698ebe82beb4f
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:37:17 +0000

    extremely appearance burst 567enemy reserve

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_work(force, distance):
-        return force * distance
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
