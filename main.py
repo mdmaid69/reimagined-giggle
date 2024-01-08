606c1901fbff97566b6e36f2f1d8b65cb1706b69
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:59:29 +0000

    ground bill geton trap except

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import matplotlib.pyplot as plt
 plt.plot([1, 2, 3], [4, 5, 6])
 plt.show()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
