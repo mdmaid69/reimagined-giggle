a4034e2c21e4d685bbb8eafd2b02dd32ea58d44e
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:45:19 +0000

    pipe grateful question rate too

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
