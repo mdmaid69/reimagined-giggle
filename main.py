12f2bb15acbf36ae81e69dc70ffb1c40bf88dc0c
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:27:29 +0000

    deaf pronounce vote suppose Qq

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_density(mass, volume):
+        return mass / volume
