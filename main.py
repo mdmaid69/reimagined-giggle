aea8c7451e2a4eb734ae348db48ca2397d34fd9a
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:21:52 +0000

    spill finally female spend how

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def cube_number(x):
-        return x**3
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import numpy as np
+print(np.array([1, 2, 3]))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
