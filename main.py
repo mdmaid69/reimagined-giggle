2d76d9dd6dcb508eef435aeb5e512a40ca1c1c95
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:34:13 +0000

    share solve movement exhibition Hh

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
