8180bf7bdcbe148d445cb44762e2a39fbe0c65b7
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:19:20 +0000

    Hh serious doctor say pretty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+print("Powers of 2:", [2**x for x in range(n)])
+import platform
+def get_python_version():
+        return platform.python_version()
