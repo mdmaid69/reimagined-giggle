ee1bb86d0e6318bd3518d7ef1ec4f7b2ac598298
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:02:33 +0000

    become how live eat here

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_area_rectangle(l, w):
+        return l * w
