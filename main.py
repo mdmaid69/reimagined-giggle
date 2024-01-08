4883f9f174f860d2f1c1faaf47ed231941886a15
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:55:35 +0000

    discovery temperature flavour ground comeback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
