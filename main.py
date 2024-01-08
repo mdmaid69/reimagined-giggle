a772b247e7aaa5a7fec382d22c48b110488723c7
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:43:26 +0000

    rest gap pencil degree keepout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
