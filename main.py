5d895080f92227d52152907e1fd88a5582c07e3d
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:01:37 +0000

    taste stir before provide way

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
