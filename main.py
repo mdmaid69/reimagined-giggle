68794e195bda71e50fa57343f4eb8a8cfedc69b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:00:52 +0000

    slightly character manner study afternoon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
