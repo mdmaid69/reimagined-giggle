a01d1ce9dceacaf21618908f5068e165fe08cf54
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:41:15 +0000

    bet slow naturally accident murder

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
