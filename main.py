8922608e43313d850617452913481967a6fbbdcd
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:37:10 +0000

    world bit content behind address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_work(force, distance):
+        return force * distance
