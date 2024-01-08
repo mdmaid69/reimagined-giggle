79d9f2ae56602fef3d9dfb2ab11b89864dd3d15f
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:34:55 +0000

    interview to someone minor miss

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
