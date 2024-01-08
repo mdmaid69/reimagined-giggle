e310ddb2c54f1c04a24cafb0ae166cbf10b50c47
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:23:50 +0000

    police that crowded invitation greeting

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
