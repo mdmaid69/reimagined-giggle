5e0d6773313d1e4daafacecf412450808593b30c
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:31:09 +0000

    generally bye instead keepfrom so

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+from collections import Counter
+print(Counter("hello world"))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
