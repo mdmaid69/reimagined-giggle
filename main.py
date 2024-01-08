d650dbd4e24a3ec6350229b4ce231526ba20b5b5
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:05:16 +0000

    turnaway choice schedule suggest silent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def get_array_as_bool(array):
+        return bool(array)
