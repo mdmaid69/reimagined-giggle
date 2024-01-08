8d27bdcab6c030a6855307c43bd287db1b82f9d0
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:52:52 +0000

    mark education reach however honest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
