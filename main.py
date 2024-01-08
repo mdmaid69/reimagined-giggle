bdf2a7424b10dc4f1349dcba22fd51bc1b7a359e
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:16:54 +0000

    company drive sound afraid continue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_pressure(force, area):
+        return force / area
