2534c7d268df2aee46063f3acd20d85d540e53ba
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:52:08 +0000

    the comfortable slide cope listenfor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def reverse_array(array):
-        array.reverse()
-import re
-print(re.match("h.*o", "hello world"))
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
