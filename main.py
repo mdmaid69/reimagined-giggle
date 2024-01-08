2d2c897d27c5ca4cdfeeb9e14a4deffaa298b321
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:59:23 +0000

    possibility now lookafter inform square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-print(random.randint(0, 100))
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def sort_numbers(numbers):
+        return sorted(numbers)
