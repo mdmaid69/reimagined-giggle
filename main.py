4846fda0f9383a402653f24a5ec04992981af35e
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:49:41 +0000

    deliberately spread uponon dream repair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
