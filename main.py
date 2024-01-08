5af2be9d2911c1bc2491f786ab512b42c346667f
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:57:14 +0000

    used lucky season free shelter

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
