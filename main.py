32e2bdbe3ed7ec3119cc5017f12f66b7a194aca5
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:54:29 +0000

    cost better nigga survive smoke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def convert_to_octal(n):
+        return oct(n)
