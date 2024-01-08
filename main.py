0064e4bb0527c7c4d5ca4ed4841779cee4762895
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:52:41 +0000

    result reach happy goaway release

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def get_array_as_set(array):
+        return set(array)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
