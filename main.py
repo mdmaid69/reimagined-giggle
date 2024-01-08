fc202ed6f757de8e9b548eb2fcd275199c91dccb
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:43:49 +0000

    confuse check peaceful thankyou weak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def square_number(x):
+        return x**2
