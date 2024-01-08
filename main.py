4cdf50ba7c375a89aba8cca2408b96953b53a5a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:17:37 +0000

    sink discussion somebody extra prove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def convert_to_hex(n):
+        return hex(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
