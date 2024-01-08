82f35ddfcd14d207bf180be5af1a20962b5adcb7
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:28:14 +0000

    beneath hollow tonight during mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_index(array, item):
+        return array.index(item)
+for i in range(5):
+        print(i)
