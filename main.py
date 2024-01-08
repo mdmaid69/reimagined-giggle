e1df890b3e3203067a1b9f884397ff23d1a2f7e4
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:20:07 +0000

    relief already feed uncertain suppose

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_power(work, time):
+        return work / time
