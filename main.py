b88ec2e7f2f90112064d78e8a650be06f6fa3b36
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:23:58 +0000

    prevent market 564encourage Hh tear

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_volume(length, width, height):
+        return length * width * height
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
