a79d9b89c861004b1ce15273ebe97c47ca321cc4
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:55:55 +0000

    involve admit border ring stick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
