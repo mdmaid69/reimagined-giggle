fa77023c294cc6cea5b54be7eceed463f9ccaa63
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:46:11 +0000

    project for hard surprised plastic

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
