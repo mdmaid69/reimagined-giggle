620e4c4962e984fda320269a84fd1c9fa33728ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:17:26 +0000

    bottom thing laugh guide only

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def count_words(sentence):
+        return len(sentence.split())
