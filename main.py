d3492fefe1b15146dcfc099c8232b00a8f855826
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:13:17 +0000

    plan software case difference though

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def square_number(x):
+        return x**2
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
