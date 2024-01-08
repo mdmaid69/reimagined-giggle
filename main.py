668c84174fb18d76fe78d5c191c4bf4052af80a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:11:36 +0000

    tradition flavour union wire sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
