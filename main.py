e627a1b37fea2944f9542546b3cd8a37538187e9
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:45:03 +0000

    fix pleasure visit cough organize

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
