d35d67801edba788912fe0d46247dd4333caed99
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:49:11 +0000

    exaggerate pipe admire need offend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
