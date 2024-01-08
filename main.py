eae4dcfcb87e327d50de31e8e370e51044119fc9
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:06:08 +0000

    through count maybe switch thinkof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_speed(distance, time):
+        return distance / time
