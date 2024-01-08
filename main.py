80cc22d0a3c24840ed885e8b1f047cc00ac10331
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:50:55 +0000

    interview apply begin factory matter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
