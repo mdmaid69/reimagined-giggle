1e5913b25c6c7e6a76e8c7aa5b04b5c780ec54a6
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:54:52 +0000

    old owe that sorry miss

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import os
+def remove_directory(path):
+        os.rmdir(path)
