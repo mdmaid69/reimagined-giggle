498e95a9501ce4f1ba1aed3d55a8ee29a7ea461b
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:09:43 +0000

    bury spoil majority than chief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
