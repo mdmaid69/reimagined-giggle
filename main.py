4293035200b7c1bc043927b15345e913e9486c08
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:23:08 +0000

    tidy down chat shame suggest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
