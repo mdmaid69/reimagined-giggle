7090bfaf1ff856f6e1e31aa4a07e290845a4ce48
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:44:31 +0000

    traffic complicated relative welcome old

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import tensorflow as tf
+print(tf.__version__)
