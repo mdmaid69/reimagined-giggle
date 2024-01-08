1ceb14b7964980d20e931b60347960bccbd50a8d
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:24:53 +0000

    building complete I drive height

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_max(lst):
+        return max(lst)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
