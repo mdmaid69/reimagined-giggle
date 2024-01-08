7f672cd71c8fa5eebec8da506f2c7ee8a62e8aa5
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:50:41 +0000

    long speech capital late change

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def calculate_density(mass, volume):
+        return mass / volume
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
