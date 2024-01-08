4945689c85ded17c5bb45ee50cf9412acebf36c5
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:12:47 +0000

    breakout reply generally lot tour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import collections
+def create_stack():
+        return collections.deque()
