4243efb7607686b116ed04439fa62a9efebd5d52
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:17:36 +0000

    rich interested worry promise vanish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_hex(n):
-        return hex(n)
+import time
+def get_current_time():
+        return time.time()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
