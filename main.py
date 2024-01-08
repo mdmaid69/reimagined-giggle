57de22f10f817598779d5ea52c883987c847853a
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:56:00 +0000

    intend rub change arrangement before

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+for i in range(10): print(i)
