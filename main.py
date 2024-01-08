aa77391f6541e948ab4e5e4b804461756d3dd57e
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:54:15 +0000

    solve rubbish crowded achieve excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_index(array, item):
+        return array.index(item)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
