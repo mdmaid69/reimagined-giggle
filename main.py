59808de0d0c42f007873a9795bd1c728a88af6d8
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:57:27 +0000

    design flow example cinema room

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
 def exit_program():
         sys.exit()
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
