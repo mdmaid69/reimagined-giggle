db20d0d9e998eaedb342d229c6a2036bed52b52b
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:23:27 +0000

    turn replace interrupt sister turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+text = "Hello, world!"
+print("Uppercase:", text.upper())
