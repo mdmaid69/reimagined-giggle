f80001f73c41d48fdf7145e5e99c0f6576974d9b
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:00:12 +0000

    successful during either successful base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
