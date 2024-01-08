f56b4cc1f23bf89ee0f9d725a9cc05d884433a17
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:05:52 +0000

    tour minor stir picture only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
