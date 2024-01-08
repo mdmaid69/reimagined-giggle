770a3d9ce8f10d65decaf5be470a860d3556cc1c
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:56:58 +0000

    must sense rubbish shape debt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
