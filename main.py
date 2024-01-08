39a5d77c39b865456cb7ff46d95a53af7e8c3424
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:51:33 +0000

    easy attempt insist lead thumb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import random
+print(random.randint(0, 100))
