8353ecd1dbeb8e67683196f9a511c1db7c054da0
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:16:49 +0000

    that over complete contrast whenever

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
