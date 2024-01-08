a8b08fc4d182c7384fe116bda2278d4d0f8d5c37
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:53:32 +0000

    language manner flood plate sink

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
