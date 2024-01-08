c4bc3dfc747a8fe566f89f7a47a9e4066dd78868
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:26:08 +0000

    reduce blame purpose reject city

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
