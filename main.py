54129430598b82dd82439ef07fad9ca5e6bdfd50
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:03:15 +0000

    fence plan difficult lesson damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def sort_numbers(numbers):
+        return sorted(numbers)
