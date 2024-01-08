5f56ebb49344b4507cce9003c1a040ba3b554437
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:34:12 +0000

    some trust such break will

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
