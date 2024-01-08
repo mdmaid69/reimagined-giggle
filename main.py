9607531a6436be4125a02b2edc7d6ca289289355
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:38:08 +0000

    lot history hot worse him

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
