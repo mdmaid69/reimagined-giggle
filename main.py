d82ba0feab2ad4a182e1099e29f652bb57d84687
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:50:53 +0000

    physical bunch bury share office

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
