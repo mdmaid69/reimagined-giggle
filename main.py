d854087ec454dc8bff9e501a945fbec9a245225e
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:54:05 +0000

    judge day alone shop recommend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
