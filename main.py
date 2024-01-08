64cac4a0b9c5bc6cf268d2ac7b5111d5e3eecbae
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:33:15 +0000

    way while plan base more

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_pressure(force, area):
-        return force / area
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
