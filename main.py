498d11887e89f3c61f999800b59fb6cee36f3cb7
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:09:39 +0000

    person scared guard aim police

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_ceiling(x):
+        return math.ceil(x)
