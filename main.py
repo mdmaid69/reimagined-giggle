2a920419c9bb6d1c13dcd93ab90126c81b18a61f
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:05:49 +0000

    alone it broken frightened pollution

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
