70f05ac8df1f74d7e2f5ba15ef95275bdce5e532
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:22:29 +0000

    spill loud sell glass home

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
