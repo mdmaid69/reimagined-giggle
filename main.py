30a67ea9221893ddb19b8bc034d545ba39412b21
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:14:25 +0000

    scream print history immediately ours

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
