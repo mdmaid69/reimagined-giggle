1c03d91a6d46eed3bf6430c1803b57f0c57b9f11
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:21:34 +0000

    meaning weekend completely flat mind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+print([x**2 for x in range(10)])
