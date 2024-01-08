f00ead9c66c6efdbb48ed2bdf40b469691015e9e
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:05:19 +0000

    by turnon tooth never male

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def reverse_string(s):
+        return s[::-1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
