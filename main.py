5c10eec39d29a7e1743c2fbcf45ede7b8a2aed93
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:40:37 +0000

    famous male right judge thinkof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
