e3953d853fda34c3def2f039e1f45b0bba8e8eb6
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:35:43 +0000

    nose complete Ss positive fail

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def reverse_array(array):
+        array.reverse()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
