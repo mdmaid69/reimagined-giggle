0df57511ebcdf05f789a7e46b4e536b80e246606
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:03:14 +0000

    part begin develop noise height

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
