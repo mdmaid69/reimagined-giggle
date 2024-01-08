dc84ce1c928e895d88c50866617f8288bb182b4d
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:39:04 +0000

    opponent choice skill group roof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
