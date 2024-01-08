e6c6a20cb690a365b2f300132a0b07af305e14d7
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:41:17 +0000

    spread waste any kill Kk

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import sys
+def exit_program():
+        sys.exit()
+  def reverse_list(lst):
+        return lst[::-1]
