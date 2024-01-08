0edc74215625771a1efe54edcc1bce55d888b473
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:34:14 +0000

    heat godown mention stand shine

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def convert_to_hex(n):
-        return hex(n)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+print(sys.version)
