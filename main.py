19b0aadb0ae4c2b3890384c55e64d7a199ce7b16
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:36:02 +0000

    would man distance pupil pin

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
