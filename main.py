462784dbaa10a7c083ba8c5c3558cfa7eb4993cb
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:20:51 +0000

    easy turnon follow deaf please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import collections
+def create_user_string():
+        return collections.UserString()
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
