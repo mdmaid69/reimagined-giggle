fb2b953e5c5b421d15612eb2923da38b0ba8aba8
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:44:25 +0000

    sticky open offer command rest

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import datetime
+def get_current_date():
+        return datetime.date.today()
