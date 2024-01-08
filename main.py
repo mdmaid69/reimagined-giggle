32788ab33965fe475c100a0e4aab03ed77213947
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:42:53 +0000

    loose fall wind different shelter

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
