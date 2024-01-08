642d96327fc4f8b485494d8e81c76e710dafb078
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:10:22 +0000

    before large close track male

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import time
-def get_current_time():
-        return time.time()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
