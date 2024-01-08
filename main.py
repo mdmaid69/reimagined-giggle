f19a6d18995349a33d508679e754af3b44b9d837
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:04:27 +0000

    this want petrol action figure

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def greet(name):
+        print(f"Hello, {name}!")
