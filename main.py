2f506715c985e70a2518f1cedeec58343be7b40c
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:45:15 +0000

    pause dream awake callback tie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
