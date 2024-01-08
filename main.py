ccd7777d4521a7b9fdbeb12a6925ecbd17646db8
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:22:27 +0000

    shadow divide sound workout live

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
