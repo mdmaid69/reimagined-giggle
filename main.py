a35e2b8d9a94c3ba6c667dc31e722267e8051c33
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:39:02 +0000

    important push children bottom turnup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_min(lst):
+        return min(lst)
