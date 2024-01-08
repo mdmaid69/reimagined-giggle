ded1cb06c0fdafbc0911591a766d44fee166c273
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:03:39 +0000

    sense hole free exchange whether

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def add_numbers(a, b):
-        return a + b
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
