8ac281be2847a69e28ff94dc9cc47e4722ec65e5
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:59:57 +0000

    worth thanks wall estimate scare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import datetime
+def get_current_date():
+        return datetime.date.today()
