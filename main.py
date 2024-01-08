4059ba3e0f2501d7bc8a158d816f8debd76b44a3
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:27:06 +0000

    desperate improvement worry myself always

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_acceleration(speed, time):
+        return speed / time
