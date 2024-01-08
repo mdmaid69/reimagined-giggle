525e75f44fb31e09e6f9ef2bb7ce078d441188eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:34:01 +0000

    bright themselves material keepaway round

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
