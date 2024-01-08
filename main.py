e38b10e389ba9d2f1a0a2e0baf24e7c360a94a3a
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:30:52 +0000

    stare violent and design turnover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
