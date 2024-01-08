b48249e8517bd24d33aa6e94e94342462134a405
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:06:27 +0000

    again set hard chance date

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
