4acf9c91c80f86f5e380d9637617480aeb3d3314
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:42:55 +0000

    another legal shock complete sail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import sys
+print(sys.version)
