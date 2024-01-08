4613c3e9911b2a7ab666bc77a55b284b3cbf0a4b
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:35:46 +0000

    coat exciting crazy tap alive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
