741151c51b85a2bc5cf57c18bc74cf03c6c72e81
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:49:12 +0000

    near store bound idea this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
