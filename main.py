cf482d41386aefdef79b43d7f49f80da02431afd
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:05:13 +0000

    still tie sweep flight Cc

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
