81ab5a5cd89ebb96f220e47300b8d2a67d5fa59c
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:51:19 +0000

    spring future allow mail impatient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_sine(x):
+        return math.sin(x)
