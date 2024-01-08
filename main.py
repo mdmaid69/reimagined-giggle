30b7cc59862d66796493140efab3da0acdb1d219
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:45:07 +0000

    aware cough Ss unpleasant band

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
