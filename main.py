758cfca93687c31d24dfd3731df03325f3c33935
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:56:11 +0000

    unlucky today trip doctor Gg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+print(math.pi)
