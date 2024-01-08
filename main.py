42c413c85327b8c178b2bdd5e65200d18431d499
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:11:51 +0000

    doubt field argument pool shake

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-print(sum(range(10)))
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
