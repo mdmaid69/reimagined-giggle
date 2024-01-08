f2234d90dc00daefd5e7924aa2eebaf9439d06a4
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:47:28 +0000

    feeling hard national undo matter

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
