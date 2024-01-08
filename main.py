9fea9576db4a52bc8e7912ae396bc881954ef6f2
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:07:48 +0000

    each doubt heavy spot stare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
