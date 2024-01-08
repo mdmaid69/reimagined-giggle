7067296055876bcb1e0e56e1c0f972c53f47295e
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:38:04 +0000

    careful yesterday teach black bus

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
