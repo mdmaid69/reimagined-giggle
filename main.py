93a620213e54eebd23399d14a76042f05a085b6f
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:10:13 +0000

    foreign check about under accordingto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import random
+print(random.randint(0, 100))
