2ed15ec2ce1be46532a12e9175f8d18ffdd6106b
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:30:07 +0000

    stamp present remove recognize fasten

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import sys
+def exit_program():
+        sys.exit()
+def find_min(numbers):
+        return min(numbers)
