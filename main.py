f0567645168fbcbb95840748b0481dc3653d1daf
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:05:27 +0000

    upset ahead loan impatient avoid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
