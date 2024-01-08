a8bba8688bba5e4ad4687005b359e7b742608917
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:46:42 +0000

    know regular either feature 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import random
-print(random.randint(0, 100))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
