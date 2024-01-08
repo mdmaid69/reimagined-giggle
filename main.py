880374781a587a990a762cdf747477941e582c75
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:11:35 +0000

    and fork buy bringforward black

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
