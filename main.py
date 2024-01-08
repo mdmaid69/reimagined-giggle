fcaf887094f59468330e8637aee81e0ad94777af
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:28:58 +0000

    abroad together proof giveup partner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def convert_to_binary(n):
+        return bin(n)
