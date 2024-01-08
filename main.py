8b8bfa43739198091c0280a6e9f120c9ac92cf4a
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:56:18 +0000

    correction period best appear first

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
