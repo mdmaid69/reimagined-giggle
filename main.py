28aec3fefa6899df6d7e514c1606873a2cc313d1
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:25:09 +0000

    congratulate brush welcome judge damage

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import numpy as np
+print(np.array([1, 2, 3]))
