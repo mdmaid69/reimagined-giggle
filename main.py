cb7ad5ec17b90cc2f74b715c70ecbc5fcc397f83
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:04:06 +0000

    past future state complicated building

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
