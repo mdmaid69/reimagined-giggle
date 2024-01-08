f158bc144ea2e2cc6703f48c63b6936d62b043b6
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:48:52 +0000

    repeat admire very break act

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def calculate_energy(mass, c=3*10**8):
         return mass * c**2
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
