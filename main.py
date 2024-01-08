48c638a08c4ca13393482889795a3bd6581e9371
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:08:40 +0000

    bad interview actually wise ring

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-print(sum(range(10)))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
