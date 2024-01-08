ba958da7d116ae0a74bf7d4cf5de347a861af6cf
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:08:42 +0000

    through heat quiet relax container

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def convert_to_binary(n):
+        return bin(n)
