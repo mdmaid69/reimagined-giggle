df5f7977b734b7494755e4a93aa8162b4eaeda36
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:58:16 +0000

    peace near move comeback occasion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
