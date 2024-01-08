2ae0e61fd6b628dfc7f0c31f338f5988ad6071ab
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:08:18 +0000

    read smile shock rarely free

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-for i in range(5):
-        print(i)
+def find_max(lst):
+        return max(lst)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
