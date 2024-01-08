bc7aa947c9e0aa616be0db07ac59f041a7a8a291
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:11:08 +0000

    fill interfere whenever legal conversation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import random
-print(random.randint(0, 100))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
