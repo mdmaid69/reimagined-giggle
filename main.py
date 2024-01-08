4cd736c9bab29fcacfc35fbea2fadb756f4a9f54
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:18:14 +0000

    case slightly object mood risk

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_floor(x):
+        return math.floor(x)
