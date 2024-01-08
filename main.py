e7c3bcd3b688f26db4166de8f4f64202b52b258f
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:31:29 +0000

    opponent promise expensive comeoff original

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
