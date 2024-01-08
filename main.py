e543341e11d3e426cd816082b4b2bbc5135b5ddf
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:08:44 +0000

    play how smoke both related

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import numpy as np
+print(np.array([1, 2, 3]))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
