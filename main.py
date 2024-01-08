7feb10f99a04c5e008d6f98f69be3a385ed2259c
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:09:58 +0000

    inquire gift shame union lucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_user_string():
+        return collections.UserString()
