41ca5c1dc7fd71de494d5916fc3f46d011903bb1
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:16:09 +0000

    favour glass material peace shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
