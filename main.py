2218f54b52fde9dc860261b34b85bd86faf412b1
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:06:52 +0000

    form declare public always closely

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
