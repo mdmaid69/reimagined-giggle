2bc727faf23ef07442f01455b4d999f50b76dd77
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:50:16 +0000

    embarrassing shock education disturbing crowded

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-for i in range(5):
-        print(i)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def split_path(path):
+        return os.path.split(path)
