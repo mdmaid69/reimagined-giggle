0dceceaaec03b4580debe00f6dc1d2aa37988b14
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:35:36 +0000

    bite why appear shallow permission

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
