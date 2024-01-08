4cbe16df9c212372a5b0fbc90213e45468a34724
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:04:16 +0000

    relation before call steam boring

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_pressure(force, area):
-        return force / area
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
