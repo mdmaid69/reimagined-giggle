374d982b60081aaeabf7b4310cd22ab69ff15f52
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:07:01 +0000

    573entertainment acceptable concerned visit shame

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def convert_to_octal(n):
+        return oct(n)
+def calculate_speed(distance, time):
+        return distance / time
