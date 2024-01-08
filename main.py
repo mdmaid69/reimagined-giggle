25d134fb6893bd782b4f70b9650b22894e948b75
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:06:19 +0000

    bad date yes building several

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
