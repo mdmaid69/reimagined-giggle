35f04bb4b601e4e1ebbff1415c8f9b435ce86eb8
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:44:55 +0000

    before behind dig study fond

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
