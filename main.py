5fa462555cfc3cd6e8ba4b033ed1ed18a4b7a681
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:29:18 +0000

    spite right appeal drawing sale

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+text = "Hello, world!"
+print("Characters:", len(text))
