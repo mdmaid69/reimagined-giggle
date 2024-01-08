88b6d73e592227e84c6c5d7dd04bda1f63b00b38
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:13:04 +0000

    different fly failure road mainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_as_int(array):
+        return int(array[0])
