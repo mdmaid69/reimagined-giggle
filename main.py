3e8544baeae90456b47da3b4979ecfd670cb0f38
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:06:23 +0000

    slow use recommend one lot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
