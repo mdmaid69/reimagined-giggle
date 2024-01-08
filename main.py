e7b1ac79f0170fa0bb197a448f779921edc89606
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:00:37 +0000

    geton father afford mess refer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import getpass
-def get_username():
-        return getpass.getuser()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_density(mass, volume):
+        return mass / volume
