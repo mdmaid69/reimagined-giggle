9ecf2bf0379a5fe0a60d82d36876bf7d3ce5f628
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:41:14 +0000

    film bang trick doubt name

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
