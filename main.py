f8943d347624f40e44624e5a63374f300d7a508b
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:21:52 +0000

    big interview convince set callup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
