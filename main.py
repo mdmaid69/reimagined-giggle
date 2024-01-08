572f0b52e05e241a23649c454392bd649c91a0c8
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:51:24 +0000

    stamp fear dead pleased professional

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
