83361a42d108d7e160a858d3031c30824a31f103
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:03:58 +0000

    Ee Mm free children hardly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
