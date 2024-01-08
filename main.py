a58f9d61aba6136ff17d4abcd982466350d2b3ac
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:58:45 +0000

    announce certainly question whole freedom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
