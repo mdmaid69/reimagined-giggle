3117b8244378671ab2db852a810871df616de260
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:04:51 +0000

    both artificial turnup now me

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
