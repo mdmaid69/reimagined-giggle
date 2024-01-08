8b702312c05f136d68518272ec2ed1627ffa511a
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:56:30 +0000

    shade judge convinced one flavour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def delete_file(file_name):
+        os.remove(file_name)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
