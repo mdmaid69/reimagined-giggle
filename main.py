928a1c561b6de1f81473b9a58526d0cdc16933ff
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:26:18 +0000

    tired pile silent dish unless

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
