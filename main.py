ab0f31d9a07152fb4e91653b5a9889906f49240c
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:22:11 +0000

    intend prevent tone offer goup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+  def sort_list(lst):
+        return sorted(lst)
 import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def subtract_numbers(x, y):
-        return x - y
+print(list(itertools.permutations([1, 2, 3])))
