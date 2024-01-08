2a72490e68409b71b3aab41cb2245df4192f2fa5
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:00:07 +0000

    chase affect change platform somehow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
