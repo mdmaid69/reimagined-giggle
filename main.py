30ac7ff5c9a51c50fb0462f65c135d45aa2b506c
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:05:45 +0000

    excuse blue quarter official value

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
