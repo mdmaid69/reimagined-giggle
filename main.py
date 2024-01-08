216c13a41378a5cd772cfd7704518c4c9e01001a
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:03:16 +0000

    spot face sport rush involve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
