4c0eff3b8163d9c22523f23f28f69f0934a5d3d2
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:47:57 +0000

    hello top bet phone style

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
