a6ff7d4ee87cc514c4c8a7c27570a68064d6f77c
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:23:06 +0000

    dish government apologizeapologise up consider

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
