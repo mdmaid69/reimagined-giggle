c0988bbac8765cb90bef6404f10777c58c8f18f9
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:22:51 +0000

    power suckme wonder if advise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def insert_into_array(array, i, item):
+        array.insert(i, item)
