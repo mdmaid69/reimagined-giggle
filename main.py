671be70019c6c147e12579d72cc3f6f6172e9a7b
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:21:10 +0000

    keepout think whether of male

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def convert_to_octal(n):
+        return oct(n)
