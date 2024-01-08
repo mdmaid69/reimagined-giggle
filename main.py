6f7a6d24c9f51a5a365b98f68049df636a66d3af
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:00:05 +0000

    contribute rid reasonable tense content

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_as_int(array):
+        return int(array[0])
