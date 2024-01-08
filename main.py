798bd6f817fac6e922c25441dcc2de552cc8e31b
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:12:34 +0000

    pleasant round contrast each room

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
