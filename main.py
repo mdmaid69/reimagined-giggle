3273445c4f775dbe3912b241840e2d3679c20427
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:00:56 +0000

    nerve tell collect feature respect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
