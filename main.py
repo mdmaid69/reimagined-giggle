978e43fc0c0fa6152898d2f559b691ac758959b1
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:16:36 +0000

    closely service debt appeal talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import sys
+def exit_program():
+        sys.exit()
