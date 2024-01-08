a9395c235fb970d6a38da8a3a75c09afe441fc9c
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:09:48 +0000

    move start finger compare visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import getpass
-def get_username():
-        return getpass.getuser()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
