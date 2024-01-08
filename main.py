60797ca0eb48ff7de39683b814f07e945e082214
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:12:42 +0000

    wealth swimming exhibition eat breakfast

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
