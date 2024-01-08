54d9791b6619dbb4d4482d93c2c881dd6b5f7a93
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:43:21 +0000

    upstairs thorough slip group before

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
