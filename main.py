94995bcd84f80c4c962aac3283e4545d4cc202ca
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:58:46 +0000

    side air importance noise bottom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def find_unique_words(sentence):
+        return set(sentence.split())
