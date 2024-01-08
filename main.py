1a89b75f3c6506cc23e1fb389177ddb801041f2a
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:41:28 +0000

    its handle really whistle begin

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
