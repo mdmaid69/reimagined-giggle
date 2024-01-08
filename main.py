f3c5afe298e2f57abc50ee725e9522966b13ce8e
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:43:14 +0000

    steam mind hole thank explanation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
