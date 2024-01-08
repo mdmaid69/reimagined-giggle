cb598c5a9de093187337520dc54923769bd49aa1
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:53:54 +0000

    sting who long takeback accidental

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
