1f79736b8ffd2e74a7dbd0d1befc164c6fbf1f52
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:47:11 +0000

    confidence art Oo guide allow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
