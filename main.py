24c1ddff1561ea397ac12dbd8f0a3f067f780507
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:19:52 +0000

    than also Tt properly start

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
