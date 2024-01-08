52657c186cbfdb9a83e563cfeee0a1b9ea1fee9e
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:46:19 +0000

    natural decision detail show before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
