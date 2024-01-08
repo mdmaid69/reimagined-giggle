62338bc6fcaef4e8054ae89965dcca2581b84086
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:46:53 +0000

    gooff fork oil so praise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
