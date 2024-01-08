6b3520e35a6b89de84f1aebd391c7712d7e22fac
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:22:50 +0000

    politics frame look congratulate row

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
