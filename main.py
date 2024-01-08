32420d757f731910ada13de8110d52ba5224432b
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:21:41 +0000

    cough spoil cry tax match

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_length(array):
+        return len(array)
+def remove_duplicates(lst):
+        return list(set(lst))
