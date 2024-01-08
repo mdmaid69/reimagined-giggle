1b3575d8803c766fa80c837843710d2716bfb759
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:10:10 +0000

    prefer giveout suit provide Ll

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
