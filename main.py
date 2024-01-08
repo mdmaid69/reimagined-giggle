dd5193dc0623c152c8828775a275200d92c48862
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:20:35 +0000

    worth speak promise themselves turnaway

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
