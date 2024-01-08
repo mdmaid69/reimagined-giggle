3b07fd447d3d7f4cef46710026753f5f956db4db
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:33:51 +0000

    low shame human goout do

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
