43132e37b41c75661672e0f89d7e9055aa544a3d
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:06:15 +0000

    level trousers pencil communicate lose

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def is_odd(n):
+        return n % 2 != 0
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
