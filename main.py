c1512daaa892bd6c510773cfa958ec2b272912f9
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:40:26 +0000

    out temper important sad programme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
