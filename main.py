4cee40df60e2b2e8629e6e39328fdd4c758bf135
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:53:24 +0000

    careless general push goback(to) professional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_logarithm(base, x):
+        return math.log(x, base)
