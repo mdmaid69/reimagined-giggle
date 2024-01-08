10730a1c63db64dd4741eac55022f4ec0f9aef69
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:27:44 +0000

    lesson love value sister unable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
