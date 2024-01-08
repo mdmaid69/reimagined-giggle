ea254d871dd908b6a9bb7844217652bc1a4746da
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:12:22 +0000

    exciting work thirsty share hide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_current_directory():
+        return os.getcwd()
