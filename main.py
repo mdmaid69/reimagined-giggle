f85a9f4084c0b93ee6afdf29c48133d5ec09d76c
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:45:43 +0000

    unpleasant free soak further demonstrate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
