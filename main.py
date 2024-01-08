7a0b1a38e4d373b0c17fccdb17879b88f334eb42
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:01:59 +0000

    hair place instruction help clear

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def is_even(n):
+        return n % 2 == 0
