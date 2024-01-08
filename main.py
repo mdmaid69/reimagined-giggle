376db40b304446e27c0fa57507123bc0f46a011a
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:11:46 +0000

    drink hit business complain afford

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
