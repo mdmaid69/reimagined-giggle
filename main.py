8c71165758e0f042bf2ba8f6fd7fcb427c5abc9d
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:12:45 +0000

    recently period part approval warn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def count_elements(lst):
+        return len(lst)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
