e7fc8238508c396c6a564a5c96dca17c201bae5f
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:33:00 +0000

    front lookup stamp national wave

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import os
+def remove_directory(path):
+        os.rmdir(path)
