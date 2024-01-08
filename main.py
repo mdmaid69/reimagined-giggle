9b060c0d0ae0e3f63f9b0be0d80c413cbcfb08e1
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:37:31 +0000

    strange left interview moon affect

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
