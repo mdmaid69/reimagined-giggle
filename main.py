5fee38af0fecf02a04f707dfef65f526d006672c
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:45:06 +0000

    over perfect pin except surprising

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
