a24bd24ec3394bbd29e14c270b735d13bc0d229d
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:27:32 +0000

    platform regret stretch people pause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
