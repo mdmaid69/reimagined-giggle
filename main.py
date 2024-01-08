0aa52deb8a084b52605db6ca3d60f899a4e77276
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:30:17 +0000

    knock border amount smooth final

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
