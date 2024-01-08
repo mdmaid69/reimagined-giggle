6204c0ee60066939f37bebac918ca56bca5b981e
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:07:46 +0000

    change music 573entertainment cap fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
