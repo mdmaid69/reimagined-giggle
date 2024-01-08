93fabc6224a6983bee794bf40ec35725dd55bccb
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:06:40 +0000

    to attitude well trial beyond

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def convert_to_octal(n):
+        return oct(n)
+import json
+print(json.dumps({"name": "John", "age": 30}))
