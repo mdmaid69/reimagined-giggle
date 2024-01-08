0faa260d698047f2d5a5ac1b0233774c87e6df30
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:43:46 +0000

    card thirsty spray cause education

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def multiply_numbers(x, y):
-        return x * y
+import tensorflow as tf
+print(tf.__version__)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
