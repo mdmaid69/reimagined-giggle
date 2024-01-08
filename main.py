d2462f2b7495d1028c093d80b95350221fee7e74
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:35:13 +0000

    scene realize police judge company

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
