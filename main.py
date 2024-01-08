7bd70c7b6ae731d1ca562e6e2fa3a0b87963e040
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:43:47 +0000

    dance world bus situation find

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  def convert_to_hex(n):
+        return hex(n)
