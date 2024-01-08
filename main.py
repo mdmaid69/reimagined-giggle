07315fa6f077df190fb63afdaba87d22064b969f
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:48:36 +0000

    declare sort professional fly mysterious

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+for i in range(10): print(i)
