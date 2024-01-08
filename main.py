9810301886b76b5bcf03f81abaf28d8a1791359a
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:15:39 +0000

    inform comfortable plenty alive arrive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+print(os.getcwd())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
