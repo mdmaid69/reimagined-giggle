94923cacf59b37ae27eae87bc87a51cff56aac93
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:31:36 +0000

    bringdown free space brush stranger

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+print(sys.version)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
