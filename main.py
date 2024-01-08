e34b5dd16ebf0ed2ed9ac90498325008e6b054a9
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:26:13 +0000

    provide choice value claim fortunately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
