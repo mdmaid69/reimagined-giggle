e7e4986543eaecb1cf105875622325ec1e14808c
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:54:05 +0000

    no walk contrast any write

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
