3e6dd5c5b66475d26a7edc5a4bf583827d229978
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:00:52 +0000

    bin equal live pause satisfactory

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+from collections import Counter
+print(Counter("hello world"))
+def count_elements(lst):
+        return len(lst)
