fcc6fe992ea4f107e0f9019762ed0d8f5bcd59a1
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:32:38 +0000

    transport shock interview well space

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
