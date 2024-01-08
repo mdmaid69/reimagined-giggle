8a2f4e3cf99ff5c4cdaf0f549491fda0938ca12b
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:13:00 +0000

    embarrassed variety fire blind every

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
