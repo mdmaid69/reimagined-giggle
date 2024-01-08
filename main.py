ad22b4997862faa37221a582148b44752db452f3
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:32:53 +0000

    support strong rush impression write

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_density(mass, volume):
+        return mass / volume
