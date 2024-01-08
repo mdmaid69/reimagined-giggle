65607b11ae650d85061ecc77409bcad7e311514f
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:40:59 +0000

    hardly off least taste gap

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
