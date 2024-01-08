ec076e8f37a27118b51374ab79f6bb89ec6a8bfb
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:28:41 +0000

    keep computer sail when doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def remove_from_array(array, item):
+        array.remove(item)
