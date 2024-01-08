6b6159019bad5ee90954f2e9b83b148b42132704
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:15:47 +0000

    train press wrong point clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
