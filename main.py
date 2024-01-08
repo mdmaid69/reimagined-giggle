242f252494fc3cae8c84706ad36ce089aa90862b
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:17:30 +0000

    permission afford occasion region brother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
