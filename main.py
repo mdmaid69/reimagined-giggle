bde73fd620370c3833a283c5812307442e9fbd67
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:19:53 +0000

    mean risk union typical offer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def multiply_numbers(x, y):
-        return x * y
+  def is_even(n):
+        return n % 2 == 0
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
