9e9caa95f41f85f7051ee403a83fda95bd59c6a8
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:40:32 +0000

    clean culture progress previous this

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_area_circle(r):
+        return 3.14 * r**2
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
