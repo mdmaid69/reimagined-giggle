6f7926aaabcf7072f423e344797325b2f71b7dac
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:10:10 +0000

    science Ee cause drink part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
