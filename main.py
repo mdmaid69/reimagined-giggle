02b4a6e8d86e2f2edfe82637bff94ba1f184b795
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:01:21 +0000

    everywhere cost goout professional whisper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
