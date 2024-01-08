c1bb103d264ad6edcb3bb6b22fbc35ddb89cafe0
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:03:22 +0000

    giveup muscle none several big

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
