d56949947011e0f693157432535f2e9b5adb01de
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:14:07 +0000

    hurry wherever speech state certain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
