f936c109934f683dcd64396f05fd2d164ac9f08d
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:54:53 +0000

    spring treat advice production process

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def remove_duplicates(lst):
+        return list(set(lst))
