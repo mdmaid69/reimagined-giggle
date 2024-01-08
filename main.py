79ecff0f15b60a867fd4e602f6f0baf5f3b6dcac
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:37:17 +0000

    environment club turnon takeback rest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_set(array):
+        return set(array)
+  def multiply_numbers(x, y):
+        return x * y
