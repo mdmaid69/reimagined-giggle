15420398bc4c83a1fd1f0b19e09b168582c732b8
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:55:47 +0000

    takeover word confident enthusiastic what

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
