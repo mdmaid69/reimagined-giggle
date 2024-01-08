11233162bb80b5247f7ea134c490289f6a31fa89
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:39:36 +0000

    load machine love standard breakfast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def convert_array_to_string(array):
+        return array.tostring()
