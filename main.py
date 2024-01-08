4c4d6c8f91b0373749e5aaf2b42efd075c9e0732
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:51:20 +0000

    contain climb water object room

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
