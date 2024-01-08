30c37e1ac531cf7c2962aaf001c5ba3bb6da554c
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:30:48 +0000

    there mix precise door major

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
