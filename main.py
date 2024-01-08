626801db0a1cb53c777cf74a11f1b65d5b6f2d9e
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:52:31 +0000

    wash patience finally charge government

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
