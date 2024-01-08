e6e116e4c792b97ccda958b04151e04b9016b450
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:29:43 +0000

    factory clearly stiff could drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def add_numbers(x, y):
+        return x + y
