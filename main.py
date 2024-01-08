18859d14a9862f613ba048e34b2298f57975873b
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:33:44 +0000

    Gg home centimetre rescue middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
