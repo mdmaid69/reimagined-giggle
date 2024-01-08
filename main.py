a70d2c2a4479f2622cce5da0c94bca06934a5b2d
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:57:51 +0000

    announce own content light truth

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
