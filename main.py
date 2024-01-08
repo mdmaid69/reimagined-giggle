f9ef6164f4b7814242eda3cbc114a8a370a07860
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:22:27 +0000

    measure risk deep expensive tear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
