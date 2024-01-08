e640eb6dc4ff3a1e500888f835f2861d9c918466
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:20:11 +0000

    which attempt through song major

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def add_numbers(a, b):
-        return a + b
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
