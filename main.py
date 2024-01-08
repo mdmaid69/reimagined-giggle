b1f38b88e2647e6dc8183948dc9c91261ac42fa8
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:57:23 +0000

    hurry constantly still development centimetre

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-print(sum(range(10)))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
