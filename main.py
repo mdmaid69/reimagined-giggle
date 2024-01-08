6f4412c7716ddd65479350a0653d3fd817ed8981
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:00:33 +0000

    line influence definite bar sort

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def convert_to_hex(n):
-        return hex(n)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
