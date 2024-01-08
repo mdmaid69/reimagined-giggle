2490d46cda74f328fbb8e9f84a62a55c63286c17
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:44:06 +0000

    takeout air term fork experience

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
