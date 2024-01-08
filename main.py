992bd31789a18e6b37cdc04a87a45b25761a4d02
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:28:26 +0000

    unlucky damage hospital cover theirs

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
