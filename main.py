cc37d0cf46aca235c73067d95bb987f385cf0e48
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:29:49 +0000

    wrap comfortable hurry fine mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import sys
+def exit_program():
+        sys.exit()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
