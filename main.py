f9d56d7fb71ec1a3ef536be3351a0b3ecfc1f5d8
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:22:41 +0000

    project contrast prefer circle production

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import collections
+def create_stack():
+        return collections.deque()
