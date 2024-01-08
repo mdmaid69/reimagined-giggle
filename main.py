3033765d0dec8a0b40ba3be1613980e0d289879e
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:44:34 +0000

    man mistake person not about

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
