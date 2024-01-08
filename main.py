f4e44e5bb7f124ab083d6a3080446bf38ef3c5c5
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:47:28 +0000

    takeaway never search stand brown

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import tensorflow as tf
-print(tf.__version__)
-  def square_number(x):
-        return x**2
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
