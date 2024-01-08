c39c233d70a0aee2fa6ed6326ace3e40a15a3689
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:32:57 +0000

    distant smooth radio splash true

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def clear_array(array):
-        array *= 0
+def get_array_typecode(array):
+        return array.typecode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
