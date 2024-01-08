e9a01a84b3829a6225fb6a24ed60b51266021f8a
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:33:40 +0000

    tread bend forward crash sticky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def add_numbers(x, y):
+        return x + y
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
