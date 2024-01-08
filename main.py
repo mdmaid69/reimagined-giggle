7bd47fa0b4a597a75b8e5f3ab9f1f7bbda9b2ae1
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:06:52 +0000

    undo forgive personal stupid tongue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
