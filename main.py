37b84ef561a89e8b251fd903f499ad94a2f3d70e
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:48:11 +0000

    slice tight bin listenfor anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
