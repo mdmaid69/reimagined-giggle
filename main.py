4ba12867709735b9a6e25aa8fb69d9a25bbaa0a4
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:43:05 +0000

    either challenge improvement knowledge uncertain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import logging
-def log_message(message):
-        logging.info(message)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  def convert_to_binary(n):
+        return bin(n)
