2d562fe1b0cf9aba4efe5334fdbfd60bfca7af4e
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:49:15 +0000

    test care sky concerning disappear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import platform
+def get_os_info():
+        return platform.uname()
