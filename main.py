ec5c9dd050b254880e712e691070c9ba1e7f2701
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:01:58 +0000

    prove difficulty spin happy criticism

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
