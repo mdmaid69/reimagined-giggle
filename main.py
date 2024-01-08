6dbf82f9944498d57cc031e6de546df7a7d91217
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:00:28 +0000

    appear persuade worse complete takeout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def is_palindrome(s):
+        return s == s[::-1]
