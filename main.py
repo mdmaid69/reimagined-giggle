9c0c145f04b80d1d6c14e17a364027f873cedf41
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:13:44 +0000

    whatever surprising drink cost knowledge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
