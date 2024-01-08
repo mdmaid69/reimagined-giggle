7ddca197ca268b59cd37019d56479a6bee6f7f78
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:40:26 +0000

    doubt pain mistake would scrap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def multiply_numbers(x, y):
+        return x * y
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
