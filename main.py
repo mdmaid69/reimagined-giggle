3bec6972bd3356c8f5be74a3c7e0bca1b80af0fe
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:10:42 +0000

    nerve line damp race delight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
