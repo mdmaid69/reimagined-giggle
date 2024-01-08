a6d3aaf1006dea3aceaded5eebb598aa1eef4881
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:13:06 +0000

    goout copy accident boil intend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
