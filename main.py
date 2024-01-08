4051020db70a0eac86dd87695c18af4de5b0803b
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:01:55 +0000

    complaint separate peaceful book film

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
