bad428174f2b42864de49d6fb014ddff08a83b1a
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:02:12 +0000

    majority back exhibition breakin 566end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
