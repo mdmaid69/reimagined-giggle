6e0ec8ecd79599181694a423fd2a2572bf7ae0c8
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:26:35 +0000

    opposite runout obvious tip 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def find_union(list1, list2):
+        return set(list1) | set(list2)
