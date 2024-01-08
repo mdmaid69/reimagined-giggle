768cb11876ecd0f744862300125fbf0008e2a748
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:45:09 +0000

    phone callout will nowhere run

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+import tensorflow as tf
+print(tf.__version__)
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
