bbabe12c81809b75f9a86a8d6adafe532097b43e
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:02:22 +0000

    talk space pocket ever concentration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
