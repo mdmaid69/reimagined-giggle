49eed1d9c79bb9ad0741a22eb463f3bc3625ec0a
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:42:41 +0000

    rich play hang tap steer

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
