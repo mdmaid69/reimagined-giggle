497bb1ea7b1431fa1a38868de761722558e23600
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:03:22 +0000

    table mood spoil most mind

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
