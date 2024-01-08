f9169d46a1f674c611df9942ef733017c8c11b16
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:01:55 +0000

    print central produce approval force

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
