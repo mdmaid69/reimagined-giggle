33a79e5242d15b002805cd6bffccd60744f5aa97
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:35:48 +0000

    good wake independent that its

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
