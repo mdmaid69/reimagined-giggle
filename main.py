127d0868c110c7f5fcb3281622030b4efaf9d12c
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:50:09 +0000

    below broken delicate priority wild

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_as_set(array):
+        return set(array)
