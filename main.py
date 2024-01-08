140e0b207c3dd790f49c507aeb5482c0e8ced265
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:33:07 +0000

    hit plenty watch catch attention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import numpy as np
+print(np.array([1, 2, 3]))
