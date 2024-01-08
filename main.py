862e806a486229d875a2f6959ccf37444ddd7803
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:46:26 +0000

    hollow environment runinto otherwise last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
