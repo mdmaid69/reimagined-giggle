c4dca5c00eae0114be8eae05e3957d30079967e1
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:18:57 +0000

    visit through extra tap bit

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
