a29dc76a464851dc90d630e9838d06993faa1a0d
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:03:01 +0000

    it against describe television smoke

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def find_max(numbers):
+        return max(numbers)
