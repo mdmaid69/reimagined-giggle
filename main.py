29ed70a7be388895038b714a576101218758a343
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:00:35 +0000

    somewhere famous scream 564encourage inquire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import getpass
+def get_username():
+        return getpass.getuser()
