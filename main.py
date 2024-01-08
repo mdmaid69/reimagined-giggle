00b8258e642ed97fa53745803de2ff2e3d150c57
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:29:18 +0000

    split comeround comeoff runinto blue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
