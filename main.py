8f74a9c234c546c2a87c595edec329ab65799688
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:24:17 +0000

    great meal apparently medium patient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def convert_to_hex(n):
+        return hex(n)
