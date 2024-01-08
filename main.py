efca9da3c31ffda4251257d03d858d1ef78ecbb5
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:54:37 +0000

    blind confused large ought tax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
