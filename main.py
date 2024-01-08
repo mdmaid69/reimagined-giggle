c090a164da4f52f299663557da902faf037759ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:16:47 +0000

    swimming to show door office

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
