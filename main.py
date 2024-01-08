a0897f1aa7dc34aa645bf3091e9a3c8d3e4627b1
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:51:40 +0000

    confuse bunch clothes instruction step

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+print("Hello, world!")
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
