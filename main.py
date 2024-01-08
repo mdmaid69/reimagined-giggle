f440c3183b8c001afa96804c119f2eb0f53d11a6
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:17:11 +0000

    forgive sell bye just everybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def clear_array(array):
+        array *= 0
