14ac3e0d9539671515f0a827dcff922dcbf805d1
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:48:12 +0000

    wide spend fall skin chief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_acceleration(speed, time):
+        return speed / time
