3bef8245966d74228727e79a8316bd74f5e3d402
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:13:18 +0000

    separate long inform many this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
