5f45ce796e30f9fe0c4c07cce8fecb2ddc3d0c92
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:06:42 +0000

    forget suddenly once shout fill

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
