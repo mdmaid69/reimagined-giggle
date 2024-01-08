9f0f17243adddcfdcaf75cbf7c25167127bc6d17
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:02:49 +0000

    interruption ear interview equal lift

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
