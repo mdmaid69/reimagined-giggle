c20a129046766130ebbe3b0f1d04ab5c7e21fc0a
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:29:06 +0000

    continue stand firm gas container

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
