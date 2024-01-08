6af7aec89085e9c47fbc3c495fe9f276f5ad85b3
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:28:42 +0000

    mention legal word start introduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-print(datetime.datetime.now())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
