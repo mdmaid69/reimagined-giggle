578b971a128b341da00858f7d8a4e15ffe2f4c46
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:49:56 +0000

    information undo goodbye schedule plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import collections
-def create_stack():
-        return collections.deque()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
