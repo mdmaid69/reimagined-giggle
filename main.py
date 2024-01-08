73c3442fd60c4a5bb1f03832128cdf431c1c6454
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:01:37 +0000

    come talk same pull clearly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+print(math.pi)
