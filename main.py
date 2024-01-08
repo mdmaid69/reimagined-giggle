a917eb82b82e524b3150d3d04e2aa7dbdbf2685f
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:56:28 +0000

    shock physical write future style

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_as_int(array):
+        return int(array[0])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
