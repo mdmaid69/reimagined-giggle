5137404e1783aed993c602804cd45b212785284f
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:46:44 +0000

    back regional either herself method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-print(random.randint(0, 100))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
