9a5e72ea605b3bca6b184df48ed9a80cac9f63ec
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:25:18 +0000

    notice mark capital risk commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def get_array_as_bytearray(array):
+        return bytearray(array)
