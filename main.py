1d4efc48e37cc5d69ee28077cd1e790af8a6c7a6
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:08:29 +0000

    put begin shopping across break

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
