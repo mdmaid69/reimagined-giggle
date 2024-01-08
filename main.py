8e970140be7622eebfeaedb70d50e01e4171a9d2
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:55:33 +0000

    even then guess slide search

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def append_to_array(array, item):
-        array.append(item)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def reverse_string(s):
+        return s[::-1]
