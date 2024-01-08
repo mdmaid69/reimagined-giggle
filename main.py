1d6ed45e1e6b98ef848abea24e3b189ab51c945a
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:33:55 +0000

    pronounce proof body proud track

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import sys
-def exit_program():
-        sys.exit()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
