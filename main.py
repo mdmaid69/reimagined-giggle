39974b663998e2c967b7a77a56e7fb3e8a77e3b1
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:21:09 +0000

    worst pocket beautiful impressive imagine

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
