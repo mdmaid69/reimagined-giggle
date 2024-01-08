18ccdbe67c1ba6c60de5ae6c2fd61f0182c3e133
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:46:46 +0000

    right kind care draw original

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
