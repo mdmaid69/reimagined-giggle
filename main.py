a718939bf269faf8380df1b0576d30fa872d6854
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:33:15 +0000

    stand brown office escape breakthrough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
