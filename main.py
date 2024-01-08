c7196c23fa823c13748dca05062f20c2618f1468
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:10:08 +0000

    holdon comfortable edge sound previous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
