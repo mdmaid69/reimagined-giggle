8111c2169ada80362f55de603d8fd7cd70fdeab2
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:59:38 +0000

    floor never increase continue laugh

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
