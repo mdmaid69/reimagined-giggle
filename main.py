82a94c633130ba524f3681cb1600e108b073fc95
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:39:12 +0000

    disagree bin between getup calmly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import numpy as np
+print(np.array([1, 2, 3]))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
