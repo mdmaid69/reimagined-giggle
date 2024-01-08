43ea736e527655fc896db88b2609b93fe42656ac
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:02:39 +0000

    obtain equipment active contrast store

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
