58d61ab7c608489706ac72e7ec19871cbbc2c4a6
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:11:03 +0000

    convinced appearance fasten plan well

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_average(lst):
+        return sum(lst) / len(lst)
