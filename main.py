cd93e07edfbbd52dbcc6b9f8d2301ce715214387
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:02:24 +0000

    risk noise square description organize

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
