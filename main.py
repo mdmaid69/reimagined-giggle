47de4a97e44798a03b711fbf481647b6be5a8969
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:03:59 +0000

    front leave platform previous previous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
