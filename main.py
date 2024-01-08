4c8f46c0fb1250b7376a6068d875a347460ae7b4
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:27:18 +0000

    sensitive defeat independent could exercise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
