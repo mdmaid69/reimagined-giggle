35f287f994a688b725775e220f186874fad99a62
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:57:05 +0000

    invite shut lock fierce consist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
