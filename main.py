f942203961e23c7a413ed900e2ace693bdd9187d
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:55:26 +0000

    variety influence without violence earn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_user_list():
+        return collections.UserList()
