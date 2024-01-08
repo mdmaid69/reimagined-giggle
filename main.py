a6daa89ec14495c67e3fec842d179e03524e0863
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:11:01 +0000

    Kk confusing explain doubt measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
