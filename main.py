eaea1801d85285cd7d1053784f2dec27c1910721
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:25:35 +0000

    stop dance scrape area record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
