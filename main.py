2cb32c897ac99e06ce5391bdd3e83ae2f808e5cf
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:06:04 +0000

    tired art characteristic goon interest

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def reverse_list(lst):
+        return lst[::-1]
