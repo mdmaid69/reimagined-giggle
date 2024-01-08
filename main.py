babc972133c6b13c31537673bee4318c135f95be
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:53:16 +0000

    nor copy strike basis gap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import datetime
+print(datetime.datetime.now())
