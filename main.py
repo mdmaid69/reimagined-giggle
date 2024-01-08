7c00074fbfb6f41867713baf65ef36080e360d68
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:59:36 +0000

    so able storm quality pride

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_density(mass, volume):
+        return mass / volume
