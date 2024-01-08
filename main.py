f30b67cf2da9612dd58096c43d0722afacdd8ef3
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:39:42 +0000

    undo question offence rough would

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
