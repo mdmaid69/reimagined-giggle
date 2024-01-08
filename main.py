105b1d5594bde66544f6612bbf0ab0cf11f81094
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:00:03 +0000

    excuse he instrument mistake guilty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
