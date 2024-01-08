8577f46b6965d3e52c5de94ca7b95e1422606239
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:51:11 +0000

    win walk let fine square

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import os
-print(os.getcwd())
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
