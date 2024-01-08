0f9667ef395b7d2de2cdb4224428cfd46b2827c6
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:19:56 +0000

    average mark somebody rate pour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import tensorflow as tf
+print(tf.__version__)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
