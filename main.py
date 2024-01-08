12793ed0cbc656157a77a032c9f128067d20f6c0
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:26:54 +0000

    consider sail reserve key organize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_item(array, i):
+        return array[i]
