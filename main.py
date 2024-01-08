9a7bd1dcd5bd43fdd5d4c0c7366e786c8bcb0046
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:39:08 +0000

    discover polite 570enjoyable tip turnup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_item(array, i):
+        return array[i]
