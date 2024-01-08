c9caaa9a1ba1fbd04529da6689a33790563202a1
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:55:46 +0000

    fail obtain holiday total Dd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
