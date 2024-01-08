462138253cb2c91c3cefbfcfa3dcad1ae81c99fa
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:20:41 +0000

    fork please callat object stupid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
