e375a1a1b61f3c4f37569d7bd266c9daed48ab37
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:35:36 +0000

    action provide pipe free awake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
