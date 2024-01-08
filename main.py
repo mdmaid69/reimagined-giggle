c4c26536a79907ba2e075b6ee1431bb83b1648c6
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:11:50 +0000

    problem custom tap spill instrument

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_volume(length, width, height):
+        return length * width * height
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
