2bab84e049fd5862815087b74cebf548da1c6c91
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:37:52 +0000

    warning today lunch rob couple

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
