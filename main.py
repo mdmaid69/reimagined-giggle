a2af2da9c08abc3508467c04be5a703951b8ad90
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:24:16 +0000

    sink hope his guide perfect

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-print(sum(range(10)))
+def find_unique_words(sentence):
+        return set(sentence.split())
+  def subtract_numbers(x, y):
+        return x - y
