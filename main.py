f2d929ebb5e09a16212a4ca2fff483071147e1f0
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:25:03 +0000

    pile car in 179bite steer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
