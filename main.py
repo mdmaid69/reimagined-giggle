9182dc82197269ed97d31c173c2a9883c8a4762c
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:04:27 +0000

    date stiff Uu communicate clear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
