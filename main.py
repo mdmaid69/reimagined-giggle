72ee5bb5bcc2f2145a2e6381898a0039f17fade6
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:08:33 +0000

    kind mean base communicate ground

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
