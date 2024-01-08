2b6ef429c85c51ecf8d9999eb5a98a6d8088400c
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:28:03 +0000

    decision round never rate cinema

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
