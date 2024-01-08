77ebf8e64dced88a4891045dcb52f0f622db7212
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:20:56 +0000

    much degree transport trap open

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
 import os
-def change_working_directory(path):
-        os.chdir(path)
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
