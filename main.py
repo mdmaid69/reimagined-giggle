1341413f579fb1a2aa9aa2b3471b34f6a159cb69
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:24:02 +0000

    satisfied attitude slide damp claim

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def convert_to_octal(n):
+        return oct(n)
