f8036e06b466173055f4122ac7cd665e87f362d5
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:55:03 +0000

    miss even alone wind really

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
