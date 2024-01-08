67fa6f45fc17d132d2b97e5921cb3eecd66daf34
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:06:33 +0000

    bringup rush bite one slow

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
