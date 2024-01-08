38621f79b8c1fc5201d1f4cf3933d056ee1045bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:16:28 +0000

    reject disagree responsibility bank concerned

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import numpy as np
+print(np.array([1, 2, 3]))
