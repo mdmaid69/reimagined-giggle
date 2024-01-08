2d2de9be2e492d74f2965aa5b7dc0571d50636fc
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:59:18 +0000

    bringdown sound frame petrol shoot

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import sys
+def exit_program():
+        sys.exit()
