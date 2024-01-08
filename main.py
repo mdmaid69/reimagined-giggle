075c8605a145bfbccd5077881f552b7373153285
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:46:52 +0000

    fit yesterday put university talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
