8d877c99058db9f9b3ae1a93a0af054b0853c2e9
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:46:06 +0000

    broken boil sad imagination anything

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
