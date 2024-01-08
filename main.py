b5ea91dcaaaf0a8d0debc5960fdaf433951d4226
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:58:15 +0000

    maintain contribute respect blame straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def count_elements(lst):
+        return len(lst)
