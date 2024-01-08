450e21de5c0e57af90997209b7c84782ac0bcbaf
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:13:14 +0000

    press certainly minority runout precise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def is_odd(n):
+        return n % 2 != 0
