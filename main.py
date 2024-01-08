6ea0ddf4645539beee51bdaf6446d5c3b73106b0
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:13:09 +0000

    while become furniture shopping country

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
