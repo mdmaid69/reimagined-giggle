7326f681ddc9bab377bd8cbd248934a9e08c6891
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:42:22 +0000

    unit quarter translate fly love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
