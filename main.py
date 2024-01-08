8ce215a995f63853a26dcea4067968539ee2fdd5
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:55:02 +0000

    human quality fear sentence hurt

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-print(sum(range(10)))
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
