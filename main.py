07f32ddc7d0c52d8c16b24efb25d8c43c9c434e2
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:10:39 +0000

    thirsty paper gate minute backwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
