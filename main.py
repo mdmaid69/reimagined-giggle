902e3ba4834bd38113643390b1188a9c6ec6ff56
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:33:52 +0000

    form event please freeze failure

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+text = "Hello, world!"
+print("Uppercase:", text.upper())
