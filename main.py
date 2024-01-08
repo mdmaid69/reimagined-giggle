058a1593e71553eec63270de43108e4e2294ea6b
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:50:55 +0000

    hang forward wind dull plant

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
