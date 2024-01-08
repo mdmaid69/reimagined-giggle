e0b19446bbc7568c8672b84c682826b9f81613cd
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:43:45 +0000

    born busy drop scared rid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def calculate_area_rectangle(l, w):
+        return l * w
