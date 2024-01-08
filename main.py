3bc5794148b2af122283b51e55e45e0d9c04ae51
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:08:21 +0000

    describe continue port statement offence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
