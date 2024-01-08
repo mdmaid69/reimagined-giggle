b8a4b7f289d3f134273291606d9c106e8b2c8ec4
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:26:35 +0000

    quality acceptable year emphasis pain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def multiply_numbers(x, y):
+        return x * y
