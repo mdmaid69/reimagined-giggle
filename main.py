ec8cceeda9d269da5c9be4574e45375dade1014a
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:49:18 +0000

    pair set relative invite plain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
