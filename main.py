b172a8d3b089694608d2321f28901df12aaf02d7
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:29:29 +0000

    range occasional minute cover time

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
