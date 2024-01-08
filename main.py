32d66c86113d6f80320f8ee22777d8184fcc33d3
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:27:00 +0000

    train much transparent forgive luck

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import time
+def get_time_since_epoch():
+        return time.time()
