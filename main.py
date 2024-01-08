d784dd31e2a2fa5c5f52ba4430861cf3097210ef
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:09:56 +0000

    basically suit better fit cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import random
+print(random.randint(0, 100))
+import datetime
+def get_today_date():
+        return datetime.date.today()
