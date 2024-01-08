a92deee8815d2685ef99a6ca6637847348894be1
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:51:06 +0000

    sort scheme love grand sleep

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
   def get_file_modification_time(file_name):
         return os.path.getmtime(file_name)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
