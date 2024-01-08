175417fe3edfebf77cf3863c6632e764d3be1b64
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:15:35 +0000

    team separate minor turnback fond

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
