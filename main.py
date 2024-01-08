6910186bf750c6121ac8ecea622cc055d3ca8d3b
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:56:31 +0000

    breakinto sea reasonable movement politics

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_power(work, time):
-        return work / time
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+for i in range(5):
+        print(i)
