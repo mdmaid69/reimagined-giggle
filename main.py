cf8faaae85505a93ee23cf0747b809b99f4e00af
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:11:31 +0000

    card wander feeling tap pupil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
