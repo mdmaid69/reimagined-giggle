bd5732467b43f0f5d5bb02079c261b63d118be10
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:12:33 +0000

    me round country pronounce bush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
