14fccdd3da88bf77793e8c6eb65babeed99d30c9
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:28:57 +0000

    sentence please destroy land forget

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
