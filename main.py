eb73b541d20f8ae30a954fa8882ac3e513ca07db
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:44:50 +0000

    emergency fashion honest community brush

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
