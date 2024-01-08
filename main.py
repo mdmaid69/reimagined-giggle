9af4a4fda590623fa2976395bf49c6d449e20365
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:06:49 +0000

    comment activity sister sort march

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
