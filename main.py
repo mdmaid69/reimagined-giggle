6f5ccefb17bdec53d56e67436b389dbaba168652
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:31:49 +0000

    sense ourselves kind image pocket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
