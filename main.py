576de3300adf5d196b685ee575cd4ce88a4b6486
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:07:27 +0000

    base spend bin suggest hope

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
