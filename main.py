a8e3eef69b7e81206952802fcd6894581e153bff
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:08:05 +0000

    get Qq reject service rough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def add_numbers(x, y):
+        return x + y
