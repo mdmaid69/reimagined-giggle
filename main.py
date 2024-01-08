da5d3c641f1593c86e1461f51a35e6d8a56bddae
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:14:48 +0000

    poor leg unfair think correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-print(time.time())
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
