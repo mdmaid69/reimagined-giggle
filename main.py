073dcb7c8cc4d11314434edcfdb268cca84f745c
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:46:59 +0000

    gradually effort enthusiastic world skirt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
