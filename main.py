63093779f261e5f7f2c91bf20575e934bbf73358
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:57:08 +0000

    confused twist generous experiment Kk

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
