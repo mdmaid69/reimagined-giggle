d2d5dffd77ee4087c29d08568e4a029c8d19415d
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:33:29 +0000

    active water above treatment amount

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
