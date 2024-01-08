d5e3bcae838b995468da173e78235d53b76880a2
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:42:24 +0000

    downstairs clothes shade size underneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
