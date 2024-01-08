ac61a0083fe7db44379b474ec77032c3e3fa50fd
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:25:43 +0000

    show sound refuse apart tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
