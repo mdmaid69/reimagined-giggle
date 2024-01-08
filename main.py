8738f150c4efdfc4c9ac9fcdc224d80804deb3f1
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:36:35 +0000

    crash deliberately search bin tie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
