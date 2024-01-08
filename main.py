cd4fdaaa88d2af09cf7c5b3d7b1e431fa31fa3d6
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:57:58 +0000

    practise uncertain mail cover noisy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
