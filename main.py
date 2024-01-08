d501027c591095bc43141e06637691c3253d1c81
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:56:36 +0000

    recent here even bus funny

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def count_elements(lst):
-        return len(lst)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
