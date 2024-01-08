52ac80efa099beeeb02e4f18370e17d1a05b914f
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:19:03 +0000

    block fetch recommend rush all

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def find_min(numbers):
-        return min(numbers)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
