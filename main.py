64bb78823fc44cff210c05394590cf7456b38cf5
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:22:39 +0000

    quarter awake but lower process

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def reverse_list(lst):
+        return lst[::-1]
