e1e0b104f92b9045ea0ab7134f953192fd5db34e
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:42:27 +0000

    suck pool lay border no

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-print(sum(range(10)))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
