38561b4a90406c90ff04a718824c6d66278dd660
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:07:01 +0000

    goup bread like spoil head

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_area_rectangle(l, w):
+        return l * w
