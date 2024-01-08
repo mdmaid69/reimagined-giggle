925e3289d3deaed807e0f6109eb4a0fedf7c8622
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:46:12 +0000

    famous attempt bath science rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
