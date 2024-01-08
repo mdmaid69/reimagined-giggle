09fbf228825395f2d67b1673ab647c9b3d506721
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:11:53 +0000

    year 566end runinto class dry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
