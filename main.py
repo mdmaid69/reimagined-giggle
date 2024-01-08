59717b1998224622198559f63e71f393f3352c4a
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:53:31 +0000

    much dance arrange blame unusual

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
