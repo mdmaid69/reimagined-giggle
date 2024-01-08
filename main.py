c0ecdc3cacd3ffefb180bcf6d54a7723b7d6ca51
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:20:30 +0000

    attitude face love school speech

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def print_python_version():
-        return sys.version
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
