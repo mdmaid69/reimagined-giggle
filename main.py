1339cf257d544d7e12ab4abb7df8e1c8fdbf8139
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:04:42 +0000

    near fairly rather most turnout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
