226cb5f4a169ce6de9ad0262a5f454326fd9056d
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:32:53 +0000

    he idea shop lay side

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
