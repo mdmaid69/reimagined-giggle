9ead26ccf30b73d9c7880c7809481facfb5bbbb7
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:56:42 +0000

    advice environment because muscle prevent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
