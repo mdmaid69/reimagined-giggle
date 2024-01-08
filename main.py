4730b24e8a2af59dd153b3c487275e5d094fd123
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:55:33 +0000

    form squash soft drink occur

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def is_odd(n):
+        return n % 2 != 0
