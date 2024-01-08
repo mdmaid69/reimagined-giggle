12947afdfacfe0468cd11d2e07338d552107f510
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:17:26 +0000

    slow machine cover car wish

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def get_array_as_set(array):
+        return set(array)
