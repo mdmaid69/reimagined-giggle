68f187dcb0923bd5495e70eada096cbb15159fd7
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:35:35 +0000

    cost set letter task row

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
