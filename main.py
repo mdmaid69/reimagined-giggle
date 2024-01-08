328796f7f54b5c7485b773ddb6b94d1c8026eb62
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:29:04 +0000

    control either exist holdup block

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def count_elements(lst):
+        return len(lst)
