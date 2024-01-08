f7603a5ba70cdf21bc4c616008365b3f2f2f229b
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:15:06 +0000

    connected tap goodbye failure land

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_pressure(force, area):
-        return force / area
+import random
+def roll_die():
+        return random.randint(1, 6)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
