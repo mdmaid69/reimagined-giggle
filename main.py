eae8830141e1e5c4d17b7852772786770c70e51c
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:57:55 +0000

    rate deliberately beside stone any

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_min(numbers):
-        return min(numbers)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
