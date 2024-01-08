c99aa5b78705105599171c86fc6f8048f7626b80
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:23:50 +0000

    impossible better partner light silent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
