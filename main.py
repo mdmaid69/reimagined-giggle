6d50fafe0900cdfdf6e7ff45b4d75c95bc73578a
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:31:54 +0000

    explanation region until talk tie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  def add_numbers(x, y):
+        return x + y
