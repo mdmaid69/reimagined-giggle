2a5441174e756e3631743d714e856ff5e2124145
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:53:16 +0000

    achievement steal gas more slice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+for i in range(5):
+        print(i)
