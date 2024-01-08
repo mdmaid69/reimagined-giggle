de31f2657ce4c213fbfa9e1fa3d3df6220e3dbb7
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:18:33 +0000

    approve report on wind middle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import numpy as np
+print(np.array([1, 2, 3]))
