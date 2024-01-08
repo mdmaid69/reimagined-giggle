d32e7a82a2a5caff4d92b2fa0f06ec12042f2da2
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:22:40 +0000

    thinkthrough draw minority stay practice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
