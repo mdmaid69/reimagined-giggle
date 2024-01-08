0fb6153d60c2954cba0fdab1a429f494ddc8b441
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:14:39 +0000

    square along maximum afterwards obvious

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def find_min(numbers):
+        return min(numbers)
