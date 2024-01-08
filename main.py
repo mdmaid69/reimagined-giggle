065fd7ca7f0886c5e9e1e354d404b15568468b60
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:06:14 +0000

    base open certainly effort place

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_as_int(array):
+        return int(array[0])
