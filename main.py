91e036cf2f0316fb50738a6dffcca25d64ffc66f
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:01:20 +0000

    noone surprising even rent computer

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
