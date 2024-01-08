9d50ffa0747bd398af58fdbe8a4dc9fafffeb63f
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:36:34 +0000

    congratulate new suggest very earn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
