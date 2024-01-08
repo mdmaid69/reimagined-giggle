49beb218ee9b6bab1b39af1db96f7c4e08bd5efe
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:24:14 +0000

    skill odd deaf memory any

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_user_list():
+        return collections.UserList()
