47c374fb75389b8305ba3fca042c60ba55c063f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:47:15 +0000

    scream past his rule flow

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
