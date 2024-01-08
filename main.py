0c6d4016e79621f5acc25f7845f957e1270ac417
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:35:20 +0000

    chat person 565end disgusting favour

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
