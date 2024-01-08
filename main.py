19504a1907db1749471fdc1ffe0746cd4e66270e
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:19:32 +0000

    repair so exchange authority explain

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
