ca6764443a1cd353f3565e764082977c3f2e8de3
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:30:16 +0000

    stay stop evidence balance basically

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import array
+def get_list_from_array(array):
+        return array.tolist()
