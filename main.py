48b8b757bbc9b3d9ae7c956b0a75cbfb775b237c
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:05:21 +0000

    nothing tell long main tax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import collections
+def create_queue():
+        return collections.deque()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
