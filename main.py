0d9a7e9f035937789e8513ac9735b207b24e7247
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:09:06 +0000

    such coat popular talkinto style

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_base_name(path):
+        return os.path.basename(path)
+  import sys
+  def get_python_version():
+        return sys.version
