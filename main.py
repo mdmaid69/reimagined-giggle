6a3f9bb69c3ca82fe8466ef7b430dd4804430938
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:36:02 +0000

    after deliver complicated turnaway represent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
