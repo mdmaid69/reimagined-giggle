5a185119f55c4bfc76f3703f268a4d0c19538ee9
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:08:02 +0000

    march about other church perform

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
