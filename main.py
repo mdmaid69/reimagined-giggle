599554779c7232b9d57f3a85919e46dd16424b66
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:10:04 +0000

    possession 179bite forward string cut

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import time
+def get_current_time():
+        return time.ctime()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
