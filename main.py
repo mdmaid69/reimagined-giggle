eb737cd5ff76df94cb13acb748995cb94dfa749b
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:36:34 +0000

    place far turnout holdout keen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def convert_array_to_list(array):
+        return array.tolist()
