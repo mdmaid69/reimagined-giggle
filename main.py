0491b583092c88a1739764e1b1c08a30d08b4209
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:22:09 +0000

    accidental behind hurt light lookup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def subtract_numbers(x, y):
+        return x - y
