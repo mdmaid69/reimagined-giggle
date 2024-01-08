f2b73d442e38c27a843da7d70cb2a092deaa8d46
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:08:38 +0000

    memory read scrap line breakinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
