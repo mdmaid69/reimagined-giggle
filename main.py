a0453f35834cbb41e1096f3341c5d15377a6e6e5
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:37:34 +0000

    tax mix continue fly gas

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
