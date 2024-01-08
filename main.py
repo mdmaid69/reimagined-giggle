5bb9b132ad07c412be1581556b247484a45e58fb
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:27:44 +0000

    music something question bite ago

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_area_circle(r):
+        return 3.14 * r**2
