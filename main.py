79823397bf893951ea1cf7bd6db290fed6e4438b
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:48:21 +0000

    shop able kick space enthusiastic

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
