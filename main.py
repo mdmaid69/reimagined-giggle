cf32e820038d004f6acab5c0587cccd29c0c60ec
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:08:34 +0000

    from theirs if instead pipe

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
