12a16551c8d60d6f89c0215ac8bd30df99fbf199
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:30:50 +0000

    handle content prefer support set

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
