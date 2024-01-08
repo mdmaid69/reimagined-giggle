a0c507bb104b93dc7393652865bf065f40b39316
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:57:57 +0000

    visit learn very reject goon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
