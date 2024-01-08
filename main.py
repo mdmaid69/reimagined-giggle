d0aad1323f1acb3b07a139e0a93c5b7e0dab5af2
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:04:01 +0000

    interruption argument anything please say

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+for i in range(5):
+        print(i)
