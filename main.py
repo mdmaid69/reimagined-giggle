0bb6ba421761efd82e805282d72c996d9f64e559
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:46:59 +0000

    cum central speech rely meeting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
