56bf19306a2695431fa56b29c71798629fa681f7
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:34:30 +0000

    average order attitude downstairs recover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
