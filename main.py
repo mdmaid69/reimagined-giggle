e93a7dd863fc97002ab66bb93d74a9aab944bb2d
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:23:11 +0000

    mood science walk operation time

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def find_max(numbers):
-        return max(numbers)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
