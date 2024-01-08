5080cea45a5ee88fa1b4c2d1051a7373e3566f4f
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:05:18 +0000

    throw order question tell watch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_acceleration(speed, time):
-        return speed / time
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import os
+def remove_directory(path):
+        os.rmdir(path)
