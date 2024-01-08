2dabafde4edc53a2ef4e5610d2f19417ff486ed8
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:17:34 +0000

    measure loan than yet space

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_perpetuity(payment, rate):
+        return payment / rate
