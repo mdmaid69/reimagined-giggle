4fed8da2a6210d98810f45d716baacf0d044f2d1
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:27:18 +0000

    cruelty tune correction turnon only

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
