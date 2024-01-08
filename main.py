3bc51b91fe3e931eb8837a6cb3971f6dbe268c0e
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:21:44 +0000

    fit tear commit fierce dick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
