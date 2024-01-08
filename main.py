955364177f50953b2901c9089646ee109fd007b7
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:04:48 +0000

    thinkover few rain for Oo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_speed(distance, time):
+        return distance / time
