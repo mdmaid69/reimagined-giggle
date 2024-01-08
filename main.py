5a0ebe2fbc32479647c567b7627bfc82a6231a67
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:40:52 +0000

    show react strict hard breakup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def convert_to_hex(n):
+        return hex(n)
