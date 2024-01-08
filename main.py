6287e358bd87aa72278324f4bd9f19f0fc0e427b
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:03:12 +0000

    pocket thin mind relief help

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
