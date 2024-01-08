03c694d7765f618a3b5d89848919da56278793eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:07:16 +0000

    discuss quick slice Tt cum

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
