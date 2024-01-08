429c832d126947b28287ffa2083464b2ae0a9c8f
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:41:42 +0000

    disturbing breakthrough prefer independent pleased

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
