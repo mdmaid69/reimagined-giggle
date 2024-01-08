f1d4b084ae4b079b774bfeca08023597aad7517f
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:32:58 +0000

    damp zero deliberate thanks likely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_work(force, distance):
+        return force * distance
