fbd5f4cc9200c8951e5a011bc2f4e74289347fd3
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:05:31 +0000

    letter game improvement switch welcome

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_area(radius):
+        return 3.14 * radius * radius
