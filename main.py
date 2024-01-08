a1677fea755bdce9b6b0e51fc51518c038c6f040
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:11:19 +0000

    talk Vv justice quantity turnon

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import datetime
-print(datetime.datetime.now())
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import sys
+def exit_program():
+        sys.exit()
