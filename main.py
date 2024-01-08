b30fed11b8a4d2dd817f248ed464d919863f379c
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:52:56 +0000

    contents cold correction clear refuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import re
 def split_by_pattern(pattern, string):
         return re.split(pattern, string)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def remove_from_array(array, item):
+        array.remove(item)
