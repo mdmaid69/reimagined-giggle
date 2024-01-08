c04ce04ea2d65dc14d30bd01950d5f961b739527
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:21:35 +0000

    wind interested late help may

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import collections
+def create_priority_queue():
+        return collections.deque()
