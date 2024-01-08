de01920663d0ba2233c69d081c1074d4d0402a8f
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:40:11 +0000

    beat warning likely influence comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
