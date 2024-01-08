7b9e481fda38912a07714dd7da7cf4290ae25c8b
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:53:46 +0000

    apologizeapologise permission breakinto connected stage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+text = "Hello, world!"
+print("Uppercase:", text.upper())
