3ee98196d143091f52c60d3bec8e3751c07ebaf9
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:26:14 +0000

    creature arrive regular persuade suppose

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import time
-print(time.time())
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
