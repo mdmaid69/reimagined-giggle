e3c4bebbecb6a6e8ee29dcfdde58fd90465c1dbe
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:01:45 +0000

    knowledge oil usual to accept

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
