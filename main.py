7467e5e7322548aff28bb141f23f91d9795bd757
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:50:13 +0000

    angry admit Zz turnon even

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  def multiply_numbers(x, y):
+        return x * y
