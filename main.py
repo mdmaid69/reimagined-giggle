b2d9278e138582a5880ece56bcb4846393a6d3e3
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:39:05 +0000

    law little condition tool maybe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import sys
+def print_python_version():
+        print(sys.version)
