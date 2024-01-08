1ce2bbc6c0ce554959c82fe9af94121d5c08af7a
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:26:18 +0000

    plastic increase getout deliberate ball

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
