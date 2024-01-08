8f15b6cd74b9ebcecd59ea113195b17913de6468
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:16:24 +0000

    pot embarrassed fierce cold whenever

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_power(work, time):
+        return work / time
