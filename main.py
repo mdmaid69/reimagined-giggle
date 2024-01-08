02765c2aa8f71236f36a5f2e89116d219230b3da
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:15:24 +0000

    improvement passenger will apart payment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
