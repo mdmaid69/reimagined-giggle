b47eb516be3c7b0830f3f11dcccb863d3e75e5cd
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:43:15 +0000

    prefer central murder deliberately hungry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def add_numbers(x, y):
+        return x + y
