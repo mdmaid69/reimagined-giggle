7f2f4d7f64c3d45c3d906991e0b9e66e9616f186
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:26:27 +0000

    connection talk rare free advanced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def reverse_array(array):
+        array.reverse()
