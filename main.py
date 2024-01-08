e37134f357aa9a3964b948b92c92dfec8b4dadff
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:43:59 +0000

    trouble train let any rare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
