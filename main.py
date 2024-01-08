96bdb24a18c2200f526a6c6a3e7f087035c561ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:54:21 +0000

    strict wall ground personal notice

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def find_min(lst):
+        return min(lst)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
