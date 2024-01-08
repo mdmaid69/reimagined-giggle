003ebd3f35837c773428b61e9497f175033d6dd9
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:38:49 +0000

    climb breakinto intelligent shoot trade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
