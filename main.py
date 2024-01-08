4b1227036f928b2bd1a7554361f64c39c5505e2a
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:23:14 +0000

    destroy person can read block

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
