fbc31bc3ad55f096f11b8b7b66eaabbb441ca331
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:17:25 +0000

    listen backwards walk properly best

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import re
+print(re.match("h.*o", "hello world"))
