a15a798c643ed5ce54dbfb93e13ad70750a5988d
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:27:58 +0000

    understand command when twist port

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
