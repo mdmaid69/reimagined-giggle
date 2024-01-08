9e556a605f6144f837f6f5ed67001c5c2748af53
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:12:40 +0000

    winter list family music track

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
