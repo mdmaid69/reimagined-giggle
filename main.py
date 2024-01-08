5e5449d0f013b84bf47ad24e11a65438eb8c760d
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:52:09 +0000

    weak getup side dangerous common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
