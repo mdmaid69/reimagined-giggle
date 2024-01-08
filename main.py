8ddc42effc6fb212e9712f2707e878b891233a26
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:53:18 +0000

    pollution lock exaggerate nature feeling

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
