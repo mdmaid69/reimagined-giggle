48392359b306d9117179e74ede491aae2e2e9dff
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:32:56 +0000

    benefit statement behind responsible month

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
