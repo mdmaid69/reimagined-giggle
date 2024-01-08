4ca4aa8a3b9249b5b89d8413e70b3ffa85050f03
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:58:24 +0000

    scared flood Cc takeout respect

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def clear_array(array):
-        array *= 0
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
