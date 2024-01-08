986ec80af3db50ba4cf9cfbd1f3e2a2abc4bf741
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:32:56 +0000

    trouble man holiday for receive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def calculate_npv(rate, cash_flows):
         return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
