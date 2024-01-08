74a6af8accb2d3889b2efdac31339f2bd110216d
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:52:25 +0000

    rely trial replace accident suggest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
