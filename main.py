6e81d2fb2333ecdab8b3b1ad074ba26efd58e130
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:57:56 +0000

    call study low wrap mad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
