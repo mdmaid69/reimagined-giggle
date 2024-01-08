b4339f86207b80cc15129802c93888abba063cc8
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:33:26 +0000

    interfere name mail back settle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
+for i in range(10): print(i)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
