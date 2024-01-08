1aaf9884e2de52f17fc9b8a1edbbf7fc565fa482
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:56:39 +0000

    recent traffic mix identify lock

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
