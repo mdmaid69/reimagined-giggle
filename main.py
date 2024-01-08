f7ba4c670b584b292f125ff706d0b04afb82dd3e
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:45:54 +0000

    punish in absolutely comeup train

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
