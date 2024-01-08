0fd6f27bb7394dbc5944338f03769c91c13d9bcc
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:38:59 +0000

    both term tie takeoff pull

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import time
-def get_current_time():
-        return time.ctime()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
