36b0e830c9937299ff3bdc48dd95073ed6f5868d
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:17:51 +0000

    coat forward exist brain depend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import random
+print(random.randint(0, 100))
