930379375d0adfd104d0eb964b4b856fad512d42
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:26:07 +0000

    touch to tradition trick Jj

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def convert_to_hex(n):
+        return hex(n)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
