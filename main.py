8ebe9840a40d1195dd88e73b6594f35cb0da10d4
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:30:34 +0000

    object education surprise argument spend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
