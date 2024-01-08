0b36107029f7182a3b29a297b97471cc9d9ef79e
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:12:01 +0000

    attention protection phone born back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
