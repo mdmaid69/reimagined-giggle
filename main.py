4ce83fd0a6fe2efa7797b25303cc0c8bd2b6b76d
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:11:37 +0000

    happy damage pot mad argue

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
