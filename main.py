424be3febeb14a7372a6c3a3b8dc42b429cbddaf
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:55:45 +0000

    oil part ring drop communicate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_density(mass, volume):
+        return mass / volume
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
