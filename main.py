761637aeec04d3696598845c94b6be83fcd3e486
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:25:25 +0000

    survive late Gg runout thinkover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
