8b63dfdede84c954d74594b1f86ac872f4579057
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:49:35 +0000

    music takeout congratulate close friend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_cosine(x):
+        return math.cos(x)
