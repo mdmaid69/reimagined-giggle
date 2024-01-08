b072a975cd8f274f2621d5c6ff869a19e06308c7
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:38:51 +0000

    bank search put if ticket

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  def square_number(x):
-        return x**2
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def find_max(lst):
+        return max(lst)
