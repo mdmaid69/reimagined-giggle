0c0b1c03009b67b6cfcf4de65d0a4ad7f7d5e60e
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:37:40 +0000

    equal fall takeout block complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
