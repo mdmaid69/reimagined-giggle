8c88104e85d9afe4d2928debc7a381b25fc5f859
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:01:28 +0000

    catch true punish weigh account

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
