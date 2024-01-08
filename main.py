22f239589f16dbd1f73f9f5c9eb403efe9ae8cc2
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:25:53 +0000

    side spin visit carry positive

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
