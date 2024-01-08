3768e29d5d79d117bd83b82fbebbb7b12ac2feca
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:25:40 +0000

    powerful light waste insist old

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
