91d95132d275992ddab239f7937b2c0253a4f52b
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:14:31 +0000

    more alive polite sit workon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
