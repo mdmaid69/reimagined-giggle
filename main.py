a84dbd1b2be31efb3db8ea3e74330c2e45dd1af7
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:31:07 +0000

    near compete Ww ring think

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+print([x**2 for x in range(10)])
