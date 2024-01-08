8efd1ebf3cf7cb904e2c5d5ce60136798583db8e
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:44:05 +0000

    runinto centimetre comefrom Uu simple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_pressure(force, area):
+        return force / area
