7922c07655dc30506f2232030c9be4402edfc7ef
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:33:54 +0000

    roll deaf target guess feature

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_max(lst):
+        return max(lst)
+def sort_list(lst):
+        return sorted(lst)
