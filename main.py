ace20a88abcf87b4305910dadb6799a2111e52d6
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:39:10 +0000

    interview squash laugh related cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
