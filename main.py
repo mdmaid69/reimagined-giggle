ea0654d48e18191f850fd3ddf118443490bae3b0
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:42:30 +0000

    past capital discussion new safety

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
