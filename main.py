1410aa3034d471d76a5f0cbc4b40dd95491bcc78
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:10:50 +0000

    with pay somebody future gate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-print(random.randint(0, 100))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import datetime
+def get_current_date():
+        return datetime.date.today()
