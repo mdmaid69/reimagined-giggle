fbd806a98ecd8b3baf5d3e6450e57f41c53f136c
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:26:05 +0000

    doctor demonstrate both precise theirs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+print(math.pi)
