3141a27acf21498835483b062ece0d8f52f18027
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:41:39 +0000

    letter gift would here reach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
