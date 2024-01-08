6e9ce3c4e60c73abd23a884cd52855bbcccc6751
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:46:32 +0000

    die several out last kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
