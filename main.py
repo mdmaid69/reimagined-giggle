b792e28a443ac2229137a632b9ad5d0650251c77
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:55:06 +0000

    teach shock quarter listen for

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
