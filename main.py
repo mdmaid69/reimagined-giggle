0e713eec66f770e0cc34e03553df871d60568da4
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:02:42 +0000

    Oo stranger point reduction imagination

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
