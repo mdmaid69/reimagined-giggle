db50a985aaa1c6f7f1f5debc9f2b3874d663c826
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:13:50 +0000

    distant base respect squash unable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def count_words(sentence):
-        return len(sentence.split())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
