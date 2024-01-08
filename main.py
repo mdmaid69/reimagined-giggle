babe0b164b434f7e58edc08aa8757b2ed3e339f7
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:49:33 +0000

    passage fair match yourself no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
