33ab347e150d6ad6c1b9ee87fadbb7606e229a15
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:24:11 +0000

    hardly actual indeed Gg last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
