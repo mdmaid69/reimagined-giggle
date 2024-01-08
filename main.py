4ed809a1e6eaa1da1e7599ec16bb36023a5160c0
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:15:13 +0000

    play keen you somehow will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
