463d55f009e3a0c222c7fd0037112989f75b3f8e
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:21:05 +0000

    scrape right actual everything next

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
