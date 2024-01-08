3dfe2899800aa596c2093008b963467157ca13d1
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:00:14 +0000

    564encourage point cut scrap situation

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def subtract_numbers(x, y):
+        return x - y
+import numpy as np
+print(np.array([1, 2, 3]))
