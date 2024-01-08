ae40268a296ffa2faa00520c485c72eac228f4c7
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:51:31 +0000

    blue mix slight tape firm

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
