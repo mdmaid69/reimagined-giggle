2da4fbaeeef2daea439eeb77357e88523d386dc9
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:14:15 +0000

    concerning sweet against wing rule

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+name = "Python"
+print("Hello,", name)
