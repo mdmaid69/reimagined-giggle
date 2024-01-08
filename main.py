59592b9a15be625e66878fca3dcd4710991d015a
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:06:55 +0000

    may ready choose sit tall

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import collections
-def create_counter():
-        return collections.Counter()
+text = "Hello, world!"
+print("Words:", len(text.split()))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
