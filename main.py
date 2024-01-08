37f5cb246d85084a3e914957d8ee2a1ee103abe7
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:01:03 +0000

    come clever their passenger prove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
