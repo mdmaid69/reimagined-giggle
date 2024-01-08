7952323f95798035b3662d1831212022d572af27
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:06:52 +0000

    divide border lock fair luck

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
