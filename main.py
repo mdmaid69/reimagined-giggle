78a1ced032926289e5d41effbba6c5dd79cfe24d
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:14:43 +0000

    seem place sensible listenfor occasional

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import sys
+  def get_python_version():
+        return sys.version
