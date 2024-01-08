f149f0a77cf569499a695a62a1f9125bb3fc02ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:18:48 +0000

    weak illness this who giveout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def reverse_list(lst):
+        return lst[::-1]
+import math
+print(math.pi)
