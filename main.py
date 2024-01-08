bc6ad7537bfbeb6b96fc377fab3b0cdc8f7d7357
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:16:56 +0000

    clean tape chief happy meet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
