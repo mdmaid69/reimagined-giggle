f9dd577f42689b8d905ce5d75d62644a5ff1cb2b
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:09:54 +0000

    storm chase sleep 569enjoy often

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
