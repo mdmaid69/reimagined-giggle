95a8626f05899db2da7e1feda5e9d903c63ad2ea
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:21:30 +0000

    565end store lock grow twist

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
