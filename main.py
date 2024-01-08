d4deb0b2cad700708e29737540b5c6535e34edb6
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:24:21 +0000

    story waste private badly hate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  def remove_duplicates(lst):
+        return list(set(lst))
