0a83905d156562038c7ccac43ffe80be1a680d11
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:19:12 +0000

    invitation contrast squash tidy attach

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import time
+def get_current_time():
+        return time.time()
