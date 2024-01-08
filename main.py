16dbbc19ca4263ec98b1013c16098f06cf0901eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:13:24 +0000

    please music equal pollution reasonable

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
