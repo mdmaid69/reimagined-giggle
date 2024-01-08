b1a6a95b31d65aac0d33fd3e65f3e5139441f8e4
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:15:04 +0000

    square perhaps runinto brown theirs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_density(mass, volume):
+        return mass / volume
