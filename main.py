dfac823cfc8e0b99845722715146ba9622b593f7
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:43:41 +0000

    bury drop emphasis impression disappear

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def add_numbers(x, y):
+        return x + y
