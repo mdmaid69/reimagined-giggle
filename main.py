01d96dfc767657909c6ef7404e185d51a2d82637
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:23:25 +0000

    best tape zero block but

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
