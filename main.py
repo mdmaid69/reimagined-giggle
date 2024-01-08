0adc289c15140207f81a7e29173aa1aa0c265571
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:53:10 +0000

    goup stupid quit friend face

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
