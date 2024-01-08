dc57d7aeb0ec321c1814746c55b8a0fb1baf9680
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:53:11 +0000

    divide clever total middle tread

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_speed(distance, time):
+        return distance / time
+def reverse_string(s):
+        return s[::-1]
