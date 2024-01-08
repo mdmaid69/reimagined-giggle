3e271001685a6262d70a0b863c9bbdd25b217485
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:43:20 +0000

    skin preparation back convince regular

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
