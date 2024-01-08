2a7742107ad747f8a09f96ac2a14fadfc30e4682
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:24:55 +0000

    comment thick Ll interview so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_density(mass, volume):
-        return mass / volume
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
