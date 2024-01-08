b32638686d71c5fdfee3faf0a1308c87a2856ebd
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:21:40 +0000

    artificial steer favourite bound deliberately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
