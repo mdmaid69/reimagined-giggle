5481e6591c11202833279a5088df11ffcf806de2
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:43:25 +0000

    even ability popular interruption noisy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def cube_number(x):
-        return x**3
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
