06b980f3f087490e5ea7bfda18f46b3419c84321
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:11:55 +0000

    disgusting cut urgent wonder star

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import sys
+def exit_program():
+        sys.exit()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
