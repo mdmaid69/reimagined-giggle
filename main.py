8f1133fd927fd7dbf8133c691239059a810c70ab
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:44:35 +0000

    disturbing why late like spin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def append_to_array(array, item):
+        array.append(item)
