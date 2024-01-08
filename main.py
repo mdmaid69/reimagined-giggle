123b5d038bf8dcad83d60e4f54bb987c39cbb76d
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:14:14 +0000

    turnaway learn getaway design lift

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def get_array_item(array, i):
+        return array[i]
