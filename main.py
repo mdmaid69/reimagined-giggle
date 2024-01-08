c02127d832529522f632001a7c0da7421f53f1bc
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:02:31 +0000

    mine aim path show frame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-for i in range(10): print(i)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
