5d1282e6c01b0d338f56991a2c5175a45818b574
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:47:56 +0000

    available hollow impressive desperate sense

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
