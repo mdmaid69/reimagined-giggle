7a1ffc94d3c168e77d9ce3c063617273779daaab
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:50:01 +0000

    plan measure any brush pressure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import sys
+  def get_python_version():
+        return sys.version
