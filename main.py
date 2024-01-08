35d1cd79b74c57fe4fd99a3b602492d4a7ebf188
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:50:17 +0000

    nature whole development chin passenger

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
