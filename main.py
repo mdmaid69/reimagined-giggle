55dfcbd3d439cbad528135a5371240dd7ddbc347
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:38:04 +0000

    walk town proper cope education

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_combinations(n, k):
+        return math.comb(n, k)
