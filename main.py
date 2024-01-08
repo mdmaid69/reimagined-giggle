6c4a2de80f7cf47b27ea641505add3951674e67d
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:09:13 +0000

    think used advanced give offence

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def is_odd(n):
-        return n % 2 != 0
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_work(force, distance):
+        return force * distance
