c33299050f1ccd08839d83600aa8b63096a672ed
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:51:09 +0000

    age 571enough pleasure wipe comfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import math
+print(math.pi)
