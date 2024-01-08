ff7c9ae8a84871b510ff2c055fdc6bc2aec3cb11
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:41:18 +0000

    still Ss drawing wealth present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_list(array):
+        return list(array)
