cce801721c63fea22e6ef1cb63e325c6734cba01
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:23:20 +0000

    lost shock train sink much

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def add_numbers(x, y):
+        return x + y
