631029efaa37ee2e0de9e1b398f2bf42cf0fae51
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:36:36 +0000

    large improvement fight national ill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
