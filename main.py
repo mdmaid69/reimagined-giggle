d5ce2394aa73bba973bb417cb33edb588d698161
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:32:19 +0000

    sore guilty gentle settle calmly

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def is_odd(n):
+        return n % 2 != 0
+  def remove_duplicates(lst):
+        return list(set(lst))
