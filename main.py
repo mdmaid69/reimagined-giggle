719260069538c97251f554ca3b6ce2ce55f47463
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:20:55 +0000

    sign roof lock rate crowded

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+print("Hello, world!")
