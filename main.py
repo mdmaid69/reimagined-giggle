35c385456a491de92b3615bdcf50b4ac0ddffd78
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:51:41 +0000

    fill curious when callat intend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
