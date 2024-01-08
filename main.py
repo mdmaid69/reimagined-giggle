690b354abe58f844aff40a7f589b18567ecd6f33
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:42:26 +0000

    standard attitude meal majority design

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
