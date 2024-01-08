6b37a751b26ce3a8318ab3e80fd41800ce7378a1
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:44:44 +0000

    turndown question damp agree mine

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import time
+def get_time_since_epoch():
+        return time.time()
