619e4cf03067d0f778724ea9d426414ed3993732
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:12:05 +0000

    window comefrom increase cope destroy

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
