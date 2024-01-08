39b1b7a071a7bb0315ec7c2180eda28a5b76886b
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:10:16 +0000

    change somebody plastic stamp container

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
