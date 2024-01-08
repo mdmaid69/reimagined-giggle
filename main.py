fd3fe9bb90244bfef8dd65ba165b488190e6d965
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:23:12 +0000

    finger occasional back score fact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
