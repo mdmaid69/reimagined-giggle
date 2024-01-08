dd2907757f15793444edb5f3423ffa4dbc17beed
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:32:53 +0000

    floor lean want interrupt instead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
