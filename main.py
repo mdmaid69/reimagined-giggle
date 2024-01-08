3e1fbb824d2128d556c29b730fbc1fa8bf33687f
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:16:44 +0000

    treatment alone mass poison start

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
