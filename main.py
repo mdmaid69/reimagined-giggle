a3ed760bff9b43a04503a704faa89fcc36917288
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:47:14 +0000

    cinema bad forget closed bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
