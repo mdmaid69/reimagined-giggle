7dbee5de4cf0fa722f5543bab77036cd9ad6ab88
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:13:25 +0000

    manner join unlikely similar evidence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
