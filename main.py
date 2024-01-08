cd3649a71d657dd04f3c6a21ff9a930e858d2c79
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:12:25 +0000

    machine display simple healthy like

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_area_rectangle(l, w):
+        return l * w
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
