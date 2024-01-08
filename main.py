49042d0886e0cd89ae0d0225df1a7bcbea822ea0
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:38:10 +0000

    deaf silent operation this motion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-print(math.pi)
+import time
+print(time.time())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
