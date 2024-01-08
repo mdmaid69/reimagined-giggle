47eec4592a6e1736eae7dbccd5c2bff551fbfb26
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:34:05 +0000

    like join adjective onto turninto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def is_odd(n):
+        return n % 2 != 0
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
