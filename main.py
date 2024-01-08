d8bdecf437d070546b1ad89c5272edd844a6f5e6
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:28:01 +0000

    thanks keepfrom stop sound shallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
