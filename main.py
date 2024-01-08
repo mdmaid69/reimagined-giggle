53d6f43cacd72dac1810b7949865abf0f8a96067
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:32:51 +0000

    compete sharp giveup copy respect

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  def reverse_list(lst):
+        return lst[::-1]
