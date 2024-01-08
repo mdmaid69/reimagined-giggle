e9623ebd0edd55c6d0804cf70e69631aa0fa1940
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:11:16 +0000

    trust ashamed arrival content until

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def convert_to_octal(n):
+        return oct(n)
