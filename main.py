5e4d2bac096cf0de186efd2bc801fd02df16923b
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:48:05 +0000

    win recognize criticism well acceptable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import sys
+def print_python_version():
+        return sys.version
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
