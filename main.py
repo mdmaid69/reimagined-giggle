f032c91b920d0339ff774a1d73db7949133377a2
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:39:49 +0000

    task rain accident behave mad

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
