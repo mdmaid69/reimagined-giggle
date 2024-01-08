6e3c8ed45d2daf6f08f38b5504de6c27611eb05b
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:13:38 +0000

    key than nor flow serious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
