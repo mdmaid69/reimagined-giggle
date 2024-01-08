9387fd9c6bd6ab008cc14ba6b6367fc1a4dc2f38
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:53:46 +0000

    prevent promise rate pollution world

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
