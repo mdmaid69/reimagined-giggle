d17ffddb19f3a7214591c9ed60934c68053d9adb
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:47:49 +0000

    empty conduct bringback worth runover

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
