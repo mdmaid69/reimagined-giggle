571cb739ee7de0cb0e255ec72835be3c4d2bd238
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:27:54 +0000

    chin soak bored pipe generally

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
