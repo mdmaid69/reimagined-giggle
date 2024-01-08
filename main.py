e1fec960ad5588263afeefb0de21022593367ae6
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:21:29 +0000

    news fence twist continue stiff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
