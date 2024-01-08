2d2f62b71d569184bbe21a709d1e3fe5227903c9
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:38:40 +0000

    state protest familiar inquire underneath

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import tensorflow as tf
-print(tf.__version__)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
