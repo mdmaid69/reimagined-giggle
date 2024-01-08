2fbadd0ccdad61f7e77f234ad4fe7fdb10d04903
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:04:49 +0000

    negative false favourite fashion keepoff

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+print([x**2 for x in range(10)])
