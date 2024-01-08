bb3f26f05d5dd5184c0ded98e24790cb60191be2
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:53:11 +0000

    call alone water getback free

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
