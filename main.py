96a8b25adbae54b1d4e9b250b40328b8c45d999b
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:10:51 +0000

    join thumb chin effort within

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
