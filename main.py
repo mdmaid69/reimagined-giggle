85aa0e7e7b70aa6a57f2db665eccf24e8bb8c0ff
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:41:02 +0000

    disapprove injury correction tall trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_directory_name(path):
+        return os.path.dirname(path)
