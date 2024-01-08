62ad24bf83804552a07caeab3afcfa6e39075462
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:29:13 +0000

    joke proud between declare sick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
