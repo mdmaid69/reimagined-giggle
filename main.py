6a383fea7605259515fda891f9f2856af758b39c
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:04:41 +0000

    extremely flood hour the public

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def convert_to_octal(n):
+        return oct(n)
+def sort_numbers(numbers):
+        return sorted(numbers)
