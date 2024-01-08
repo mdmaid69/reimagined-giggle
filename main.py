c6883cf8edcaa79d83baf01d8b1528c2da4eff60
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:49:13 +0000

    miss announce clue date usual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
