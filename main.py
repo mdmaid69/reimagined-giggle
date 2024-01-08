60e5bef4d5226ff9d712d4a318e823693da6358d
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:10:49 +0000

    seat rid tip corner instrument

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+name = "Python"
+print("Hello,", name)
