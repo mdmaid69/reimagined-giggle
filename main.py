d07625057ffa91fa8f04d4d1bab1b881fd493364
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:48:41 +0000

    deep spoil few consider pressure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_density(mass, volume):
+        return mass / volume
