efe86a289818c2b229f8603d91eba4fcb752006d
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:08:09 +0000

    recovery string regard finger careless

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def find_union(list1, list2):
+        return set(list1) | set(list2)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def append_to_array(array, item):
+        array.append(item)
