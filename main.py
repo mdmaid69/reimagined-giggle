dd7655d8905b12ee320010215443dfc2f322edfd
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:49:04 +0000

    excuse swim cause something hurt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import time
-print(time.time())
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
