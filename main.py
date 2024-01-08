6c8b433ae67e5527ab391b64e992cb28a308dbd4
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:00:00 +0000

    detail move nonsense sensitive very

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-print(os.getcwd())
-import collections
-def create_stack():
-        return collections.deque()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
