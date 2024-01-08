177de8a9564daba40561f9f18f395ec39f3f176d
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:29:32 +0000

    tidy holdon trap thank tonight

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
