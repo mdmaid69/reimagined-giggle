2dc8b7a0e5b6f74ce7fd0fbc840668a36c117484
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:58:31 +0000

    else impossible reject phone violent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
