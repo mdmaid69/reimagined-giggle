c446c02a9740c94da330626c24241e798e3862a3
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:28:12 +0000

    affect own approve protect hurt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
