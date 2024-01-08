9811451e9bf8cc48f6345716be2d4b621a587f4b
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:59:03 +0000

    have off typical trip sell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
