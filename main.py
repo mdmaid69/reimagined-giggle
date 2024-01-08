c3135d240c30cf6fa703198d0f46ddf8da336654
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:53:51 +0000

    far correction holdup advanced people

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def get_array_as_bytearray(array):
+        return bytearray(array)
