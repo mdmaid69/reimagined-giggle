fa984302b0ce72d49d94a1fd11fd202e368ba0eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:01:18 +0000

    smile attempt study out during

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
