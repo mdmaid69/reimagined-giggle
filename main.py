e957cf5ca3d2193ebc4fb69f9bdd16ed05ed204d
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:31:24 +0000

    become present method subject level

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
