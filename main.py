28366de0a0eb0eb33465f7425db1433a770c4ab6
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:21:48 +0000

    that race fair splash squash

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
