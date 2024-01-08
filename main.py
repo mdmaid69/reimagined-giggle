69b9150a998a988fa167641d87cd8905404debdf
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:41:45 +0000

    politics somebody sell separate limit

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
