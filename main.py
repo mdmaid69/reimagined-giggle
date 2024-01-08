550b362a01980c8c3742744b91627c93b77b6c7c
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:26:21 +0000

    hope top pretty load name

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
