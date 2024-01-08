47b9decb70ab683a4c4b905d87ceb685ab31415e
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:23:01 +0000

    gift repeat legal ring object

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
