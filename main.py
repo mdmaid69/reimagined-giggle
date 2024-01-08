6f79df1a5f65e0d14ff3605e68d9eadc236e7c2c
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:04:53 +0000

    such pool black also entrance

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
