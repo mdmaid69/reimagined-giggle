0892f607e859eb4e6852f7c7be2dc00d42c15b8b
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:09:06 +0000

    week production extra opposite throughout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def count_words(sentence):
-        return len(sentence.split())
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_speed(distance, time):
+        return distance / time
