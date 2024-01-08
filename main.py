4213c4536223321aa12734d95c19b995affd266a
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:23:10 +0000

    I match strong accidental takeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def get_array_item(array, i):
+        return array[i]
+import sys
+def print_python_version():
+        return sys.version
