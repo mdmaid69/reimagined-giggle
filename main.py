bec05d6a4eb01108bf7f11890581ce165397474f
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:59:25 +0000

    interruption collection pattern threaten hand

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def sort_numbers(numbers):
-        return sorted(numbers)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def convert_to_hex(n):
+        return hex(n)
