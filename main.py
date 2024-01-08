6df4caadd01b9d75bfffb3ed4d592c69109bcced
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:59:16 +0000

    pay hello own powerful tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
