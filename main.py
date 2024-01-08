cd9453c275c623bdd773aea21826b6c4331ab00c
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:51:56 +0000

    both report and explain raw

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
