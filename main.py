30ce7a68192ecc6e536c5338a406c84055c2cf2b
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:08:41 +0000

    giveout use wheel tall left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
