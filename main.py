aba76afa5a00d1f3e95c9a11a5059b9ddb9faceb
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:55:55 +0000

    holiday deep damp rent sometimes

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def split_path(path):
+        return os.path.split(path)
