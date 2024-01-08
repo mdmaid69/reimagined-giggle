3da1ede51122a479b54ae4a18de12e71699b2a94
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:39:48 +0000

    public tooth spread real pocket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
