c236e65e11c0f92e277e69dd333dda9bc257da61
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:34:17 +0000

    combine habit fit warn weigh

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_floor(x):
+        return math.floor(x)
