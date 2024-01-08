b8f2e2d74b38804be996d163d9f6e67f1152a0d1
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:52:05 +0000

    law party completely table mysterious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
