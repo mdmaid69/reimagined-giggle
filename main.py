a831cefa7e2f9d918c04cdc9af8945f742198a87
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:21:52 +0000

    down mood advice hurry about

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
