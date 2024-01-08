5aa7291b96b1ca05ee30922c3a4312086e2e9b57
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:45:31 +0000

    this dangerous chin objection before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import sys
+def exit_program():
+        sys.exit()
