99eb04d302e83a3d07037741d4d6bd5a12bb659b
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:35:45 +0000

    unit boil use go lunch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
