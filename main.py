c51d6da53c5588b93d4a85b3c925deb716ea5a82
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:05:15 +0000

    shallow Oo physical agree finger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
