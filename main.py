93c17653a31a7ea7ad371bf3599d2c329d5a77c2
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:28:43 +0000

    admire genuine respect pupil charge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def square_number(x):
-        return x**2
+def find_min(numbers):
+        return min(numbers)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
