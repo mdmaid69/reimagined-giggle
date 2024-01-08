7d31f15570edc552ef71f7f26f68bc53e44f693e
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:02:01 +0000

    stupid necessary dress in practical

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_area_rectangle(l, w):
+        return l * w
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
