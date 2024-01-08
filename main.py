d334e528fe5a5eb365fcbe18d0d06d911ea33faf
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:54:30 +0000

    sale correct slowly ilovedicks either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
