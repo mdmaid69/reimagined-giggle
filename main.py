e4a0baa0d4649d9e422596b7057d46c07701b416
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:15:44 +0000

    callat stupid yes pronunciation total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import sys
-  def get_python_version():
-        return sys.version
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
