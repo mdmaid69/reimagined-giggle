a990cfcc1189383f3aeddf5111f6c08c4204fc20
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:11:01 +0000

    act unite accidental strike listen

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
