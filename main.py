869c594fd1d8d41cfed06f9af312b0e1627166d9
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:10:10 +0000

    back accident own here callup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
