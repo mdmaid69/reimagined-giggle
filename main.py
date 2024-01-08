dac864f935332b8b80943e465e8e30b2d82aeabd
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:16:30 +0000

    label Pp miss dare translate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def find_max(lst):
-        return max(lst)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
