301468a7fa9876e698b16a0a38c62443fcb52bc4
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:27:03 +0000

    stand road runover press step

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import datetime
+def get_today_date():
+        return datetime.date.today()
