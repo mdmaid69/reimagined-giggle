3ceaac2cf07dff82ba3875f6cbc4aa34790f7278
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:40:56 +0000

    forever chase clever bet fortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
