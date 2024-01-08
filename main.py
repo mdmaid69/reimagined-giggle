fc94d8483f90597ad5acfca5a630291b55119966
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:21:09 +0000

    worst come persuade dark sleep

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
