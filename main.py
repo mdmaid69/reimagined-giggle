b0c25251b89ffecd3563aa2abc1bac70653c1c72
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:18:59 +0000

    remember manage keepout sharp radio

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
