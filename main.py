e0fe96a6ab0dfd31d48d0233eebae38f56bdce4b
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:39:07 +0000

    valuable protest count finally breakinto

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import sys
+def print_python_version():
+        return sys.version
