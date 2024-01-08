5f424ec6c387195a5c4deb861cb35af31730ac42
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:09:25 +0000

    sea last seat beside shoot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_pressure(force, area):
+        return force / area
