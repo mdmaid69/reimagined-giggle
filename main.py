f06c201c04c942c130239766a5f0771e5c18e220
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:19:20 +0000

    computer may already declaration sea

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
