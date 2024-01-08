26f8300940535b7827ae99f301cae1ed1897497f
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:48:52 +0000

    load sound doctor object sing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_string_from_array(array):
+        return array.tobytes()
+from collections import Counter
+print(Counter("hello world"))
