406c2116b34a4b34f97f60738fa5dc0e34b0da0b
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:59:14 +0000

    raise serve central pronunciation effect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
