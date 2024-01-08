ce4e85b57d8892f43305346b8058b7939ed4ee49
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:19:55 +0000

    sort debt lesson cause gap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
