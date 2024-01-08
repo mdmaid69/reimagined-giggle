ae82d3d391847c2a681ee079318ebc74457fd286
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:27:04 +0000

    please flat necessary wake machine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
