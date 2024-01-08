3fd75f9e55d09ee708f459c74090e2abe5554d84
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:40:00 +0000

    perfect breakthrough good fortunate admit

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_density(mass, volume):
-        return mass / volume
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
