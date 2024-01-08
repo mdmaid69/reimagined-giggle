6922c93d4956c801c930285bab263ed4649ba03f
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:25:29 +0000

    snow theirs cut stamp against

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
