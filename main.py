34a01a55b755a1077fdf640a92f1ed238bafa11c
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:51:39 +0000

    pronounce exact turnoff wish organization

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_speed(distance, time):
+        return distance / time
+import json
+def convert_to_json(data):
+        return json.dumps(data)
