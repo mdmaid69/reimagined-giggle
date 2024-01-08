3050ec57a993932e756c95573851c1266e8e8eb4
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:50:20 +0000

    fold shade promise car contents

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
