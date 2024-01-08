305ac60d0dd455cdc0642661eb09280e102c1eeb
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:51:18 +0000

    children possibility university fierce else

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-def find_max(lst):
-        return max(lst)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
