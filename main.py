90ce5e99c6ec8f15fc8ced24eb29a2b3879cd003
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:43:53 +0000

    holiday change work cheek guard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
