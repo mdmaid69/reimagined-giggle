e1fe0b5a8120c60f4926e615e844ae7046f3d167
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:02:31 +0000

    doubt dig expect Ii quantity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
