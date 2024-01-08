9d1a733e2c1f04b2abe54ae5bee400adfed89953
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:58:25 +0000

    once add next crowded complicated

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_pressure(force, area):
+        return force / area
+  def square_number(x):
+        return x**2
