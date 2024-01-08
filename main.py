f7f3b21bec6cffc3fa4588e9b256acf6130652e1
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:15:19 +0000

    ticket interview pot whatever gentle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
