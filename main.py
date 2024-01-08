b1acbcda87594698706cfd81c1d37605ab35cef5
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:07:09 +0000

    skill such false fresh relief

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
