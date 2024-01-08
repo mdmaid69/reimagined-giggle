15ac84d21fe8aa43cf4023d2aae5c3257d6ae93f
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:16:54 +0000

    exist legal watch weekend pull

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_acceleration(speed, time):
+        return speed / time
