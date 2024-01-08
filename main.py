14f50c0949a9295d5ccdbb331a8725edc6b9a61a
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:41:55 +0000

    negative and corner naturally speech

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_density(mass, volume):
+        return mass / volume
