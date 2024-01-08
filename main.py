ea3c280bb48c63a0b7da53f31756e8edb3d3e0b0
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:48:36 +0000

    pure may seat road test

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
