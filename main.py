41f09400fbe311330d8dbe8138d3eb95c7dedd49
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:02:35 +0000

    band bear turndown few announce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
