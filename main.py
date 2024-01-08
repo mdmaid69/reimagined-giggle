5134ebc11c723a75bda77551c17ce244f2e91736
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:48:04 +0000

    best excited level fresh somehow

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def square_number(x):
+        return x**2
+  def convert_to_binary(n):
+        return bin(n)
