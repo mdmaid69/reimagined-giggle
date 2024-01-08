b9f1c721adab5e94c8ba41f60b7be33fb9af9333
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:55:54 +0000

    declaration set centre previous just

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
