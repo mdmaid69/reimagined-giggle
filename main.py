8d40dc8bc06edc697267269dd3427f77d2541cce
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:27:02 +0000

    about quickly note mine emergency

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
