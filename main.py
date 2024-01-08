b4cca6403187e4e7b7e0983abccbd963ed8b7d33
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:36:10 +0000

    tired interest contact argue machine

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
