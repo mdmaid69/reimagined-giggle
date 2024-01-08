15ecdd24a216b99e28c8bc6f44fbb24a09be9e68
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:30:15 +0000

    quick constantly my notice experience

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
