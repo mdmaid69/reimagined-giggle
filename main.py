38e48b8cb4aadc8b666bc41d32c9546009f11347
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:15:06 +0000

    market treat ordinary report wander

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
