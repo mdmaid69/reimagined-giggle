f2e525390b504194740964ac231cfe9e814c4428
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:38:48 +0000

    beside interrupt which tip fierce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
