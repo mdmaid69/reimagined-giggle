f7d4b661700487d5e61d3bb6ef16c4d1741a1767
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:51:14 +0000

    grade train less they pattern

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  def multiply_numbers(x, y):
-        return x * y
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
