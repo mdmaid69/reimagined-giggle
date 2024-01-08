5f91c8b9d21597b97068754dade6c30a8f2c28d8
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:21:42 +0000

    care extra point news sit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
