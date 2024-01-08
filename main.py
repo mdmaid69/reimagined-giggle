8fbb570ce3c9b333b9a1323146e3bd65e6851cf4
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:05:04 +0000

    comein of fortunately tradition rush

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+print(sum(range(10)))
