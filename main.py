269169d2b718586ca79ae68b81642c796417d77a
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:41:24 +0000

    message tour attract question exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_int(array):
-        return int(array[0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
