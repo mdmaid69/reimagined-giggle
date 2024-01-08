bfeb41888092e3186be491de41426bcd99d4e772
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:20:07 +0000

    punishment discuss dull few mad

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
