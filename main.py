92918f1a3f5173233a5a81a7e4834ec46ae424eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:25:43 +0000

    switch respect lead peace conversation

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
