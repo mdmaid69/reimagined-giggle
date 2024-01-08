d190d34f51864ec79d8e13bca5769094b4d8f2a7
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:20:36 +0000

    breakout normal sail hurry murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
