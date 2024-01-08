2eca7c84d83584c9097503e8141d679c61c49f59
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:48:59 +0000

    bury record cry prize boring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import array
-def get_array_as_set(array):
-        return set(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
