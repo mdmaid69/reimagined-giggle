0e40acb8933df5e3bc1fcbffe8b6fe1f684b50bc
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:43:26 +0000

    usually definite software spite takeaway

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-print(os.getcwd())
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def find_unique_words(sentence):
+        return set(sentence.split())
