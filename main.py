09ef13aaafa1ebc55d33f8cec172ac43874a80c6
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:04:07 +0000

    priority lonely cover find spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
