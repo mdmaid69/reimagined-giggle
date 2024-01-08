1da462e04653353f0e3ff3f1213ccc77ea605d37
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:30:14 +0000

    accommodation calloff strength speed greeting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-def multiply_numbers(x, y):
-        return x * y
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
