05648aeb36dc918b62ddee177eba45ef5c83ccc2
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:42:23 +0000

    bang talk interested because clue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
