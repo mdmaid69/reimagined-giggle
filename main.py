2040bf65c91e3238ebb2d249efbc88c1642ff5eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:52:39 +0000

    rest search base tidy but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def remove_directory(path):
+        os.rmdir(path)
