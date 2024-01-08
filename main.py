8fba9d11e28a54a6af57d1e0101d0005a570e27f
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:37:15 +0000

    lend 570enjoyable call callout small

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import array
+def get_array_as_str(array):
+        return str(array)
