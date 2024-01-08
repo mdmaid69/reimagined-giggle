20c9517eac75ad61f09220ab140ffb1ae2551f9b
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:14:22 +0000

    means suck length immediate basically

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import collections
+def create_user_list():
+        return collections.UserList()
