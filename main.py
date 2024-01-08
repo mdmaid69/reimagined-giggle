3c971c51d1d967607e0d2beee0942634af4b7e9d
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:19:18 +0000

    whichever ill sure drawing once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
