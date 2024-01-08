433a4f005a27b87d2b4136203c8a424fb1e2f924
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:09:02 +0000

    upstairs improvement nose balance common

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
