5eb4a279261aadd1a17130b7e650507471d6bf72
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:09:41 +0000

    question programme week history car

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
