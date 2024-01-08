b3740918c60110e60d503ace26a48be860d97b3d
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:05:32 +0000

    bite her ticket shout sleep

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import sys
-  def get_python_version():
-        return sys.version
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
