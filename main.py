25f59fb581fc5f4e0e0cd7921874b1ad7fbf8e18
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:26:08 +0000

    good 570enjoyable threaten unlucky emphasis

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def split_path(path):
+        return os.path.split(path)
