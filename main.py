5ef4682323a7e4fd9a6cdf6621761b1e2694c405
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:56:04 +0000

    visit idea act note speech

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import array
-def reverse_array(array):
-        array.reverse()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
