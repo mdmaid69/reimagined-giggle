1fc6afabbd7fd6628b947a6359dca82349945d26
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:16:05 +0000

    callback Ll reaction achieve achievement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
