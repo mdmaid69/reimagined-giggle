42ec87c7e149363e96fda6db60cf41cb9208cfcd
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:08:54 +0000

    shock want praise left vanish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+for i in range(10): print(i)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
