a71c92da3fa7a26bfe221a23e9f6514fd7a1d6da
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:34:50 +0000

    year count grateful service leg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
