5807f4f0965fe87abbb27ed0b547e1029b28e09f
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:05:52 +0000

    deliberate range food burn sometimes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
