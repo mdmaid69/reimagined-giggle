8b897f8bea628bff83849537e8c528dd67e0edbc
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:48:31 +0000

    reaction attempt live tear really

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
