9f4275661a900fb48a0b043431409242fa919b1f
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:14:17 +0000

    rather crowded rise past possible

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
