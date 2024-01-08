e4faf732b6a24624f6cac6ff06f7f7ab73bd22bf
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:57:52 +0000

    makeinto characteristic game shape food

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
