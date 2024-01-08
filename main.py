c80abdc4bbf287ce92ac91fe6ea47aa8d276db84
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:01:52 +0000

    take shrink freeze opponent turnup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def is_odd(n):
-        return n % 2 != 0
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
