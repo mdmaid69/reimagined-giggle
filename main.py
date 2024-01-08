579e4c603fff5be040f0ad97c6f340c3301d987c
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:39:03 +0000

    absolutely person hang home protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
