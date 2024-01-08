35f442aa75f2d0c17ff0c8d83a32d853dfc772b5
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:59:37 +0000

    deserve change responsibility meeting whenever

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
