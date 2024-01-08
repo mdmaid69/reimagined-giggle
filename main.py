f24c2cf13b1aeb8b293c10b7b7d4058b7cf8e7b2
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:31:49 +0000

    even suck splash lookup cook

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
