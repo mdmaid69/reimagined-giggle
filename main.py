c5a0fa9fc511f988e8a1edd74d09fc151bae7b58
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:37:56 +0000

    way realize print strict store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  def reverse_list(lst):
+        return lst[::-1]
