44ea080744ad4937128118556eaa0314159bb6b4
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:09:47 +0000

    some active disagree bang forget

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import sys
-print(sys.version)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
