93a9d0fb218bbec2ba7b89f6a1f0d11d883533a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:12:57 +0000

    profit yet offer definite achieve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
