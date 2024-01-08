093af2eaf00ff3c0cc45367eabfc72969a6db5be
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:56:55 +0000

    everyone any discover delight task

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
