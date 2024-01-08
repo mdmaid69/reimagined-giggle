70fc503f869371576fc614124371ce9fc3eda62e
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:16:33 +0000

    never fresh narrow Zz sting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
