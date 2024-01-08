6b06ba436a72ae21105ecb4493525ad07001f959
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:41:59 +0000

    turn early low treat else

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import sys
-print(sys.version)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
