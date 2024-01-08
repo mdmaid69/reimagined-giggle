5e24d3ac6cdcfa7e6ad436519ffd06cf91e70dae
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:12:35 +0000

    go start speed goout term

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
