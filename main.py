bf6e399a24b559c2910cf8e0c4c9b2f1611d0bac
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:50:45 +0000

    soon distant takeout load compare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
