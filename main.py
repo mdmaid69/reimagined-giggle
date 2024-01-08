8d14563ab3d12ff13ef08c348a5a0f2794ea2463
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:52:08 +0000

    instant sort curious flavour level

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
