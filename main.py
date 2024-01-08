3ce674c0e716c2b2d1d60b3224b1b29e289f716a
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:54:56 +0000

    mixture meanwhile charge change by

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
