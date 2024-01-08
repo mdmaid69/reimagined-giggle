0c79ba994d4f73f11115381d9c88badc3f3fdc81
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:10:12 +0000

    keepout up shame fact advantage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
