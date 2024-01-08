b833f70572cd59a8b88e1da60bbf0c2708b3a3ea
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:23:03 +0000

    mark either easy whom consist

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
