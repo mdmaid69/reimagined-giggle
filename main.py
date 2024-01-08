351db2137fd499c6a7c9bde69b08388b17f5578c
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:56:26 +0000

    too workout company nerve pile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
