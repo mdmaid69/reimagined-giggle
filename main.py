717969df2a86fec78ce16e710f9e9eee18e27d45
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:28:59 +0000

    warm confident mention reliable system

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+print(time.time())
