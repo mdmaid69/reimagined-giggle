249f33306fb48e2326109dc52e76aff08ca57f3e
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:16:03 +0000

    head Ww represent increase choice

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+print(sum(range(10)))
