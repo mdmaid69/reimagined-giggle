ed93103e2e1dc08dd5701bf5a09261d99a4b5669
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:49:28 +0000

    reject wind depend impressed cheat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def greet(name):
+        print(f"Hello, {name}!")
