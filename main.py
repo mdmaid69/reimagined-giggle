05e7d10973d650c1d7903b0ee5286446ce610e81
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:27:05 +0000

    error opinion fire doubt change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
