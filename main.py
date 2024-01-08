ed5478a111854507e0338be737ba47fb91da6325
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:55:49 +0000

    confusing comfort say market best

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def reverse_array(array):
+        array.reverse()
