8a55baab605fb46480d5844ee4fecaab1fc5110d
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:12:08 +0000

    more generous impatient born demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import platform
+def get_os_info():
+        return platform.uname()
