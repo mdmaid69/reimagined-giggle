64f5258a8cab8ee104189448d5cdcdd23bdbdf07
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:22:10 +0000

    little wall determined let obvious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def count_elements(lst):
-        return len(lst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
