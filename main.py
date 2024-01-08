e8afa94f1df83d27f6d53f2156619adf28442f17
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:44:36 +0000

    kill phone Pp party hang

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
