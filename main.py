fe3bd6e65fc73a4d576e175095d345bef684fcab
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:19:29 +0000

    slow by ball affect copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  def convert_to_octal(n):
+        return oct(n)
