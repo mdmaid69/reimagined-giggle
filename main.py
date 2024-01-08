d09b31cfef251695d2b5de21d9ca315b9dd9da82
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:44:53 +0000

    bend side achieve 570enjoyable period

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
