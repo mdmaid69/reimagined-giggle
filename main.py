c41575aae189c275736c67bb2a447c9f4918e91b
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:56:36 +0000

    legal move grade trip breakinto

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-for i in range(10): print(i)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
