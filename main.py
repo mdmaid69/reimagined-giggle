97aed8fc778cf4e47ca8216812807a947f2266fb
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:15:33 +0000

    not close seem ready relation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
