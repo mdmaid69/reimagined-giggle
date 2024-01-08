0024e9fa2a509ed5f04936f8ac2897f2a6391482
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:35:03 +0000

    more bringup politics help all

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
