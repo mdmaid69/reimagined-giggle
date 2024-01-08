566939ab080721e50cbac98a6a4427d9f16c5d26
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:16:49 +0000

    one that hurry defeat anxious

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
