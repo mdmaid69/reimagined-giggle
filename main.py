30bd2c716923c8860e7e1e7563c02e6e01a87ba6
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:43:45 +0000

    first imagine message car pump

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_current_directory():
+        return os.getcwd()
