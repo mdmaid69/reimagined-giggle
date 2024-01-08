19c1f84ccac98ca5a9b99edaaf0763914cb9c09a
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:07:09 +0000

    nature high more man private

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
