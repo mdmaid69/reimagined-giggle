18ea2d63660b825975fe6fb9812982cabc650c44
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:17:25 +0000

    command warning congratulations contact snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
