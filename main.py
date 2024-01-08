da6ae0ed99b70807d65571570856b5211d4f346e
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:33:56 +0000

    depend pupil behaviour past today

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def count_elements(lst):
-        return len(lst)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
