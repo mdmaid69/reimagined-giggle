2377d2e1b3f7dde766a83ba4dc7b96efead894f4
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:31:34 +0000

    you narrow discussion accept history

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
