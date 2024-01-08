a0492345bed26c7fdf64c1f13d2f177079d6cd61
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:20:55 +0000

    neither weigh century stop sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def is_palindrome(s):
+        return s == s[::-1]
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
