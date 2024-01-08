885f391d918db144fe80c2cf114830e317936e8c
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:04:38 +0000

    field exit bag coat far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
