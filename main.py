e8f1d2fb3959d43248f586b0da1a3b7c560eaf65
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:14:45 +0000

    comment flood hope tie soft

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
