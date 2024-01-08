da7b14070bee0d1875f0991b5941c5f57f068189
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:47:02 +0000

    pure wish yet getout exploration

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import sys
+def print_python_version():
+        print(sys.version)
