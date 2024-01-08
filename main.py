9092b1c0acc52a64b7c76b07e223e656b2d2410c
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:29:35 +0000

    condition upstairs loan willing swim

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
