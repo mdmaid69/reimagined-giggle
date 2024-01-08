3abcd795c239cb7652b4b7df9deee4967a5b022c
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:04:23 +0000

    generous tip person mark decide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
