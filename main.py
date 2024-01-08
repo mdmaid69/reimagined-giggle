5be675a9c70d41f93cf1e191f4cb227364867694
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:14:32 +0000

    aware broad concerned regularly soil

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
