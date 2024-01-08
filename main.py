f0b1a142a5a8bddadfaff2b961ea12a971b94e06
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:34:11 +0000

    ought road shopping watch section

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import tensorflow as tf
+print(tf.__version__)
