90247e0f25ab8f3193c62139056cca4ef37e7a0b
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:24:04 +0000

    distance ride tidy match day

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
