c38789d5471015db0ab3086e01ed714d57475010
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:43:24 +0000

    about back image turn dull

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
