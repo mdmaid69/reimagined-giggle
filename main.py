359c9ac73d05047de1631dce339e6316e110f809
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:23:46 +0000

    cost ticket manner result better

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+print(os.getcwd())
+x = 10
+y = 20
+print("Sum:", x + y)
