d681d2fd4780d65cff30cdbe51bda780bb751cad
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:55:50 +0000

    difficulty case before wise pair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
