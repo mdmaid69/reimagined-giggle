b357a396c8cddbd31d4c84df5a741d06e22d9fd0
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:43:32 +0000

    friend cover once floor force

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
