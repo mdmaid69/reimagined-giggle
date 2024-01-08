0c80afd0471e8f1e5542e0081aa85d6ffd441dec
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:16:05 +0000

    frame risk interrupt still claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
