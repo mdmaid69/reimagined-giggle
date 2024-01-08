03d5ceb21a54d2f5567ea1ab91577c243ca5584e
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:46:13 +0000

    shrink fierce run Ww getaway

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-print(sum(range(10)))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
