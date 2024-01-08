b7627f8786613615e0a7e5b98d6aa02060e5a7e5
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:42:31 +0000

    consider view other very mainly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  def is_odd(n):
+        return n % 2 != 0
