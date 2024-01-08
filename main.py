71b8078d28d758d43619a2e39693ac36ba0b001e
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:06:52 +0000

    pinch sentence cheek base recover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
