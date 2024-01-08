c40d90b002fb61fc574d192c923cdaabb6ed60e7
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:24:39 +0000

    change watch lesson try care

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def add_numbers(x, y):
+        return x + y
