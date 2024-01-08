bef124bc86dbad22c65e219ecb9507994dd13060
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:18:05 +0000

    sure spread afraid trap bit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_min(numbers):
-        return min(numbers)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+text = "Hello, world!"
+print("Words:", len(text.split()))
