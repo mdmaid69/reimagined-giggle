3da8bd278c4704dabe306b5515eac7344c033e6e
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:54:34 +0000

    handle plant ill concerned our

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
