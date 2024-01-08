ac9a1eb8454850a262f501c69e7625ff439c1a00
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:20:24 +0000

    base spring use stamp order

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def find_max(lst):
+        return max(lst)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
