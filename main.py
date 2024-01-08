0cc727dfd411f4e9c45bd19f57b2d8aac636dc64
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:14:13 +0000

    fast part rest dry control

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
