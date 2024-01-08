e810272ce50440d0b8b63818fd8f5740c568f665
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:22:49 +0000

    estimate good out second they

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
