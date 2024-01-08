ad0b23333f75ef9ca97154e2792c27acab7dfd8f
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:27:03 +0000

    whenever answer upstairs meaning sink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def split_path(path):
+        return os.path.split(path)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
