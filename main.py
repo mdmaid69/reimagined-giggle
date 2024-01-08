84b7b5524a3bd067fe5a64754ae538a233e73d8f
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:40:10 +0000

    send suckme authority excited further

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
