07fbf874c5c189a97e8345ce99028950d75ff18c
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:53:57 +0000

    visit adjective education wipe used

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
