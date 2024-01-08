e1f96d98d45279a1e04c7f1ab577274bdf5ba74b
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:09:27 +0000

    skill wish hello 569enjoy improve

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
