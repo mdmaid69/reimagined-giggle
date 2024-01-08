1eee4e235447fff6d950abe7c887b46519eb85dd
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:11:21 +0000

    heart sing honest centimetre exact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
