305ed63ee283284de9c9c5319fa91db025cc1373
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:51:44 +0000

    immediately term whether if towards

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import os
+print(os.getcwd())
