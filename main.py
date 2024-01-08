b13a31cac30ec37ebb8cb40f3e2ca5b94706232d
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:45:23 +0000

    exactly each turnback pity violent

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 def calculate_amortization(principal, rate, time):
         return (principal * rate) / (1 - (1 + rate)**-time)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
