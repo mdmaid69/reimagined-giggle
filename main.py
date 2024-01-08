c8b18ae300dfc68b2ba512e9c07dec4ccf977bea
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:23:44 +0000

    somebody flood issue go sister

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
