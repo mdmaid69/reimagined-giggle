29c01e757613ee3d5859f151813ab80cbc697b1d
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:25:54 +0000

    health comeoff guess matter pull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import getpass
+def get_username():
+        return getpass.getuser()
