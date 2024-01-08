1f54a5767d6e04c0722bfe34ab06d2adae38111b
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:08:56 +0000

    especially piece equal glass man

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
