1f27e844f9f1f026ebc577231fcf75a5564bfd2f
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:34:32 +0000

    city there have doubt born

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
