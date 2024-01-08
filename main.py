2234e6c4e834f30302945f0e65949c341dc948a7
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:08:43 +0000

    entrance paper copy before key

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
