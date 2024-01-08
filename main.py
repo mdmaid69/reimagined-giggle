afbb23b278c728174fe90bad5df655386046296a
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:00:37 +0000

    better mass spare supply hang

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_work(force, distance):
+        return force * distance
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
