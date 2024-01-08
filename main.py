845dae3a288b9b4823602a95f667bcbdff22a35b
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:13:11 +0000

    pleasure arm seem unhappy float

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_density(mass, volume):
+        return mass / volume
