a73891a30bd75513c0a465f02c2c3e7b1338dfb4
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:50:03 +0000

    lead cough pinch deaf race

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-print(time.time())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
