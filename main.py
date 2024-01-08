34ed0195206842e565129fea35587f39d127cad2
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:11:54 +0000

    little combination crazy cinema embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
