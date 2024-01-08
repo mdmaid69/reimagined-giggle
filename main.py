71bbad2852b74f77913204a2b47f301da768a440
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:33:08 +0000

    ride hungry bread tax noisy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
