fe84c95965129b1ad955cb6e356f76e49437c901
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:45:18 +0000

    act least face mess achieve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
