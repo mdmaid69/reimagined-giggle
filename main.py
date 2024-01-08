7bd34c86fefc279c3f7d7e256891debb5b451a6b
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:13:40 +0000

    drink coffee aim regional finger

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_work(force, distance):
+        return force * distance
+  def is_odd(n):
+        return n % 2 != 0
