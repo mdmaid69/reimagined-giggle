81da786763501f26ef67ed612e0cf459f98b832e
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:52:36 +0000

    attempt order smile rush gooff

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_power(work, time):
+        return work / time
