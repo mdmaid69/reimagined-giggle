6a8139d55148d77135fd24b0d7b86d149951335b
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:33:31 +0000

    enthusiastic behaviour crowded Dd ill

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def add_numbers(a, b):
+        return a + b
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
