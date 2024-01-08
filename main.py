95747b68f44377a347709fd8e3a382d7d68ba181
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:11:37 +0000

    set soak fashion deaf complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  def convert_to_binary(n):
+        return bin(n)
