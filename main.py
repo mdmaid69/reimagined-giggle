a70f1cc180ad1b45d8728c2ffe9c8fd60d36ed49
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:57:18 +0000

    draw ago company progress space

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def split_path(path):
+        return os.path.split(path)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
