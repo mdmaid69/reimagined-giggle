ef608aebe67d7be321e1bfae1ff2bb87fb77d82d
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:09:52 +0000

    afford meet distance beneath container

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
 import math
-def calculate_pythagorean_theorem(a, b):
+def calculate_hypotenuse(a, b):
         return math.sqrt(a**2 + b**2)
-  def is_even(n):
-        return n % 2 == 0
