49f86086a6295b4464b355e292d920a80232d8f2
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:24:32 +0000

    combination shout callat practise pump

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
