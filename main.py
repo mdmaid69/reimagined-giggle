35f91e5e94b91bd28f576397598edbe6438b22f5
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:43:53 +0000

    manner wander payment experience your

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def convert_to_octal(n):
+        return oct(n)
