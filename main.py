1df2e9f1aba4fb8f6b9c0669ac9fc848c98de86a
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:50:57 +0000

    pronounce scrap description repeat unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
