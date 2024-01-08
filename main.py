da7b7ab7d33fba7185d68b91fbe61647edc7aa92
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:46:48 +0000

    sink announce unusual unable than

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
