7017efe8f75ee8c7b35611b3abf3f2560625fc1f
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:33:07 +0000

    open sense train warn picture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
