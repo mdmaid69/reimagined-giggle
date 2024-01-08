14cd0b6ea7e997efde0aaaf5e7974f2358fe8110
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:39:24 +0000

    sorry claim road something very

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
