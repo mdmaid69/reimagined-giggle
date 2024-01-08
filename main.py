8bf731ded76413575148c537bff072c56467e526
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:24:22 +0000

    display in pair declaration closely

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_acceleration(speed, time):
+        return speed / time
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
