0a1ed1f1c4be9abda0989d96801a7307184e6e70
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:08:58 +0000

    interrupt progress inform breakdown support

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
