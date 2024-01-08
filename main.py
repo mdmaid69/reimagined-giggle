0b134678b4de979fdba6d3f284dcd974da09672f
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:47:24 +0000

    dull comedown rent convenient needle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
