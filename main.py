e6099e5fe4d55928d5ec9998d44940aff51194ea
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:20:35 +0000

    hit neat firm keep watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
 import array
-def get_string_from_array(array):
+def convert_array_to_bytes(array):
         return array.tobytes()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
