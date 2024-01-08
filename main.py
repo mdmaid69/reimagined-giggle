bccbaa8debdb704420ab1db858130bca00de3770
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:27:52 +0000

    our cool sure from distant

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
