92d84a373905f3b0aa33332391c0737631691787
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:39:10 +0000

    sit pay not visit left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print("Hello, world!")
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def count_characters(sentence):
+        return len(sentence)
