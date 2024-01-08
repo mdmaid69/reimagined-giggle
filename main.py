bc1abcf9f8ad5f5f1fcac6fcc29a73e8ec308668
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:59:59 +0000

    variety another of safety house

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
