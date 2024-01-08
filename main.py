1ffc7491a0f6817cabd98988caf93edf7395cff0
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:23:57 +0000

    comment remain open tough bush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def convert_to_binary(n):
+        return bin(n)
