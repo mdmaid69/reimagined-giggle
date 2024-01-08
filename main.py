457dc137b1fe9e9386301f5dc8f6f4cfd948769f
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:15:16 +0000

    occur work imagine equal proof

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-print(sum(range(10)))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
