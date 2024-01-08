67a4453d4e4570d94cfb238bc8aae5fa968db0cc
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:48:32 +0000

    last suck feeling justice escape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
