b4a678dc2cd0e7213dd2dba3a4f928e8827128fa
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:05:27 +0000

    contact thorough unlikely strike average

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+text = "Hello, world!"
+print("Uppercase:", text.upper())
