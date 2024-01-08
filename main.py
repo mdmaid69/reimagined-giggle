6ec9caf936a504788b182129f604c0c0e1570227
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:08:03 +0000

    recently out uponon achieve unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
