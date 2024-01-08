f999e5f97a38e74bb2fc0bdbddce1b62d4ae46ca
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:09:24 +0000

    score computer loss performance lookafter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
