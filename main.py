a196e979b728ba910779a485f670c4d152ee94cf
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:12:48 +0000

    vision special need getup space

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
