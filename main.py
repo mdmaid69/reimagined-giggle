8fb1e78ee76d6f8c9abb44875fb63623348b1aba
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:03:28 +0000

    suddenly may constantly earn listen

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
