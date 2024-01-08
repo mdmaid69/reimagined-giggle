ebc61d8f1170ff2c11e0912673fa05763c2317a3
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:17:38 +0000

    view heart stick lay direction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
