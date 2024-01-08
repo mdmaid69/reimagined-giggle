6efe8bd0d3bd7ba3b1c255a6c2a265a5d2c13c2c
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:22:17 +0000

    ticket change thinkof contrast Gg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
