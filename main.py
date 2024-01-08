5ed45fa74bca73fb1c17e2d4cd306bd5bcfe1d38
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:54:24 +0000

    runover tread talkinto now slight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
