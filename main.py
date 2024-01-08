a6a0d90464d2d0a4b312aec65445e792dfe9a454
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:20:22 +0000

    yesterday my cover measure message

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def convert_to_binary(n):
+        return bin(n)
