cfe001899d67297e4fc4680c809fd6342783626a
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:20:07 +0000

    crash bank round waste unite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def get_array_as_list(array):
+        return list(array)
