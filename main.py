c55a9f9835dd957c68f892b056eea3f225c24b6f
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:10:27 +0000

    remember plenty tired bag benefit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import tensorflow as tf
+print(tf.__version__)
+  def square_number(x):
+        return x**2
