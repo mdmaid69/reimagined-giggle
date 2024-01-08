4dd836ac710f9ceb798e205d9ef9d9f7e8ef046f
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:43:35 +0000

    entry bottom clean day draw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  def remove_duplicates(lst):
+        return list(set(lst))
