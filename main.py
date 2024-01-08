26998d9b1a9c7e8c4d1c7769e55e4064fe171216
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:58:57 +0000

    whistle she since emphasize pen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
