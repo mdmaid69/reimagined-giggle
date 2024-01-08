1cec2d194751c74583de2b995b0c2ef3ae0fa26b
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:11:50 +0000

    importance disturbing meaning blame embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_current_directory():
+        return os.getcwd()
