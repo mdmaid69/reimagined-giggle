42f6ca85f8effc9a108bd891afc85ae955cd58c9
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:50:16 +0000

    feature rob office knock whether

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
