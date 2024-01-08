a9547a19f1f9f5068985a9caca500faa13273f59
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:41:11 +0000

    and estimate plan satisfactory accordingto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
