ea147aa6f61fe5dfb20560ae11f4c1621e5591b5
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:38:44 +0000

    feel variety dick succeed switch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
