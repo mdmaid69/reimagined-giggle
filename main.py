d75542e6ca6fe34b8c892b6dcf15c6879f879dfb
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:27:40 +0000

    unlucky constantly promise usual correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
