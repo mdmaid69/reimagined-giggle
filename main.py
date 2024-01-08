1f25e3baf1c85469040411eae6fb3c768f02767f
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:28:17 +0000

    over become somewhere disappointed odd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import tensorflow as tf
-print(tf.__version__)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
