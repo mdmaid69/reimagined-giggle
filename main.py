06589c54b4917770962e0bd7d0d34acc22e4ec24
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:26:05 +0000

    thinkof even afternoon fun stare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
