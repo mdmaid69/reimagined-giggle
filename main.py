809364f873cc3861e5cafc51da6608991ef42fcb
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:16:12 +0000

    offer afternoon turnup radio claim

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
