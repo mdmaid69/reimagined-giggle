9032aa59c6fb4e9f5f6aa5577a7c7ef54eb61345
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:47:10 +0000

    criticize figure tidy generally trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
