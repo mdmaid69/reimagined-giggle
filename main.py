7254e19530e644d78c678c873737da224d51b861
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:40:31 +0000

    tell programme cruel somewhere about

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
