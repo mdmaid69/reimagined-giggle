ec9e567121cf427f21108af56a7ecbfd2ebba1d1
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:37:13 +0000

    giveaway in 569enjoy result giveup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
