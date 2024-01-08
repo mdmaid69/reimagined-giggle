121b4db6f9afaf9ddf437e8b838abdeb8ae187ab
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:45:50 +0000

    this everything shoot look ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
