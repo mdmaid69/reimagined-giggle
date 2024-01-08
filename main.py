07bed68bd51a8109839481e509bcc5bf03c2f356
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:59:58 +0000

    comeback year measure already fork

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
