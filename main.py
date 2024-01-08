957501d9575c8cf28f5a7e3270ecb9c875b13f00
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:32:47 +0000

    drop drawing whether flood suck

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import sys
-def exit_program():
-        sys.exit()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
