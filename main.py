fbd1811e9b7b06c2c7bbed224fef43b92c5b09aa
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:10:58 +0000

    keepout disagree before serious immediate

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
