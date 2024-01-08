0aa8199cfacfa067cf42f340053f4e31b695102c
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:45:32 +0000

    wall sick wind insist avoid

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
