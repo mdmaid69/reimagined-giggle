9be406bf75b3308bd2d048a266f2b09f08a9456b
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:17:47 +0000

    mistake need love order about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
