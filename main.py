e62727b41f424eea8f5712bcea6feb00768cee24
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:26:55 +0000

    sweet success future connect statement

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
