1020eefe65b0b2eebbd227a8937da4ce932f53fe
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:39:46 +0000

    light discuss exercise recommend wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
