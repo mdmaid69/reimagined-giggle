19ebcee20c76d2f1dea45f83294766b05d7f8d2f
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:35:02 +0000

    press character neck can damage

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
