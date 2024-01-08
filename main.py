f56926537b4a6f057fd4b97774e813cacbfa253f
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:32:26 +0000

    edge close how wave inside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import datetime
-print(datetime.datetime.now())
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
