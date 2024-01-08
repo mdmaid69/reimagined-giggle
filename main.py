822eb5e71f4c154dd0a2944fee874cdb56abb6dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:42:50 +0000

    nothing land both power before

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+i = 0
+while i < 5:
+        print(i)
+        i += 1
