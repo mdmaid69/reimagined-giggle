d53fcf96f00c4f8cd119af73bf01dccc5eeb9416
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:00:05 +0000

    winter 564encourage use distance vision

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
