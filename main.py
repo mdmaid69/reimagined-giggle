6c62c7be41698f7d94735dd388afb4e78827dc2b
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:46:42 +0000

    laugh last copy tell tape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def find_min(lst):
+        return min(lst)
