b6ced254386441a530dc201b63c8e3bac58b88de
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:59:39 +0000

    judge score visit mixture above

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
