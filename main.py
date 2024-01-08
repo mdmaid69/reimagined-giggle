54cd4a7ceed683319aa82bbdbe29b0b7b116d7c8
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:58:09 +0000

    cinema gap wire language cap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
