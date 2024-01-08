76f4d6c5fabb1c2cb25090b4f80809ea7b91da63
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:24:05 +0000

    giveaway breakinto move trick undo

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
