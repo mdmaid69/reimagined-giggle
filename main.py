a9e4e836eb3172eecf9dc44918830c12d305ddc8
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:00:54 +0000

    worth delight cheek agree university

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def convert_to_octal(n):
+        return oct(n)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
