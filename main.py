06a6fa325132f6ae85f0998ddcbaf8e7f2df5a7b
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:28:10 +0000

    yours guilty doubt bound whatever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import platform
+def get_os_info():
+        return platform.uname()
+import getpass
+def get_username():
+        return getpass.getuser()
