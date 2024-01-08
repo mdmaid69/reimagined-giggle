842993caabdd620d4a0eecb5764bb86241b47a44
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:51:40 +0000

    line influence yours indeed sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
