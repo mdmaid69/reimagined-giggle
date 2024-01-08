093154898a8c6bd395bb42d30d9c652be39514ed
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:27:22 +0000

    land machine positive best science

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def find_max(lst):
+        return max(lst)
+def convert_to_binary(n):
+        return bin(n)
