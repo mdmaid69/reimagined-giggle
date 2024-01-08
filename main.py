b90ef95b166cb168aa83acc8135342505d59fe6e
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:06:47 +0000

    themselves slice jump trade drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
