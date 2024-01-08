49168aafa6d28110092c182fed9ef2a65bad4ad0
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:54:17 +0000

    off century succeed body contact

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+print("Hello, world!")
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
