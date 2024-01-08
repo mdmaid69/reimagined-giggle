84d49bb9b71de9966c707a19dae44756cb1f71f4
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:21:11 +0000

    morning 573entertainment polite bend except

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
