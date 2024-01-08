17cb16100cd5da71ff741fe8369c22643543b8d8
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:16:52 +0000

    string simple mistake or tall

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
