515ffadcc978153f9622ee043c071520e75351b1
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:50:36 +0000

    car holdup let getout young

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import time
-print(time.time())
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
