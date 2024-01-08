74f978095745f74dd4a9e3beec95896b01acdec0
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:58:10 +0000

    rest attention check capital spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
