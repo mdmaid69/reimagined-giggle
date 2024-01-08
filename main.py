f8822ea799b4a5cdb4bb9edfe977955c6a2ad6d2
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:04:30 +0000

    child holdup immediately again slightly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import os
+print(os.getcwd())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
