e32ff84f04c51de042137165dbad8b3b2f73c85b
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:45:24 +0000

    uponon relax base shadow record

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
