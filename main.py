cf7fc499afc1f6d14d30959db668668821227eb8
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:06:57 +0000

    now other pretend opportunity smile

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def sort_list(lst):
+        return sorted(lst)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_typecode(array):
+        return array.typecode
