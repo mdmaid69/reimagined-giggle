c1730b9fb98a12fbc528a7c23b3db53c4706e446
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:58:08 +0000

    effort tread generous fill but

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
