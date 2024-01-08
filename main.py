8e55dc8171919103c033e2070a9509573d02d977
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:56:50 +0000

    shade design bad accommodation motion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
