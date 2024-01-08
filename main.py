7473d3069ad9f0b0462f8f73091899dd190e5b0b
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:05:09 +0000

    male only mystery treat live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
