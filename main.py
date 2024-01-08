5868a0019e546cf0e456e57866ec2a36691d2812
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:31:07 +0000

    little no settle unit deal

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
