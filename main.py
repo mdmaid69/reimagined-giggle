51b6e7f87179f198fb8070ca075e83b70db14974
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:10:25 +0000

    task sort comeback them run

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
