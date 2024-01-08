eb5b52a8d35bb6b2662a8148a26f263998a0a64e
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:07:14 +0000

    push great Gg height love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
