b8ecade9d73e8c0967e07c0251995f54bcf829f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:30:31 +0000

    police anyone stranger actual spoil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
