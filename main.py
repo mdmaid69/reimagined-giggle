ba1d7359f9d49233b3dcf3c1edc2966ed7f7b6bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:10:39 +0000

    571enough eye children no tune

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
