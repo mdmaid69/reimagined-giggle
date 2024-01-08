4c7547d0fca336126a0fd11bfd6e965477c09fdb
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:57:17 +0000

    protection Zz name address maintain

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
