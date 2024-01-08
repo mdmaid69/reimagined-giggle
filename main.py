6dfba8b913cf2a8f212222b52c06d08ce88ebd03
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:46:06 +0000

    century cook shame complicated fortunate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
