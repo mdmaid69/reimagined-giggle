df204c1181fcdfac0c7edd7ab65e3e3f8867556e
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:07:36 +0000

    threaten reserve immediate create hard

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
