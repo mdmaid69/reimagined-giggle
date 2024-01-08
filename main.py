511be606f4405d7d7a35582623ea13c887e1f79a
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:58:32 +0000

    recently deal neither see trip

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
