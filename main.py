abdc46f1829dca6504ff930c2aacc752e6807048
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:52:42 +0000

    now border call getaway connect

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
