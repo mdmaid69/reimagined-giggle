076c6d8c96175607ffc166c05bd745712b0d74a6
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:25:06 +0000

    awake after shut dress break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
