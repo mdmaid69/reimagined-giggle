cbe1a0818c43b9a386eef1ac85564f9f853d6514
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:10:20 +0000

    far swing comefrom small lesson

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
