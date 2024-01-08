cab70a47d51a84e1c1ff8cf2ff7b590fe3e2ad98
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:15:02 +0000

    gap final kind train summer

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_min(numbers):
+        return min(numbers)
+print("Hello, world!")
