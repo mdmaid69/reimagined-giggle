4350cfa73c2763591ca2c093646117d4eaae8ea1
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:07:03 +0000

    hide need beat moral fierce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def exit_program():
+        sys.exit()
