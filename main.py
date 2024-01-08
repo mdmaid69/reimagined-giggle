ab5dc63b2536b9b71a569a99f3e47d173bf05ff2
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:22:57 +0000

    correct sit ocean cruelty joke

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
