bf649b0eef2f930f9a5648d2a89446c0de37e863
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:20:53 +0000

    Ss Ee value whether boil

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
