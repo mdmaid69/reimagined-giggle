a6c0b035022a1f17839cc5670e2ba31b41dfdafe
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:38:40 +0000

    hair bet please future fall

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def convert_to_octal(n):
+        return oct(n)
