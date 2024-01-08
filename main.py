3884e9305b7f80c3985aac6eeb6a8bf857bbdab5
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:16:35 +0000

    embarrassed bye settle rush as

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_power(work, time):
+        return work / time
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
