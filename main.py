57662ad1d46fab3ba0e894c905eaefd430210ba5
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:23:07 +0000

    solve 569enjoy present fork trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
