dfd44dfa0f893f40862c067b4e3d23d3b4c0da63
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:58:59 +0000

    damage every ahead ignore actually

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
