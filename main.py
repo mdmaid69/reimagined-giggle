63fd00e0b96aa867dfda4ba1a1edc5831b9dec75
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:04:22 +0000

    protect last prefer pause couple

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import os
+def get_current_working_directory():
+        return os.getcwd()
