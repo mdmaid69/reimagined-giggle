024f9b317a758f372746af2e2ce62acf23088b4c
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:17:36 +0000

    firm so thanks neat label

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
