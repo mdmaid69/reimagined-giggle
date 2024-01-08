3e9d986887a06cfdf6d92a87d8b176a87ebc39a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:18:04 +0000

    dear aim check I hold

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import time
-print(time.time())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
