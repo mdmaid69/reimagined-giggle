16b68fff0decd9b5a9b623e8d8fbbb1693ba619e
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:38:18 +0000

    tomorrow pain only ever quick

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
