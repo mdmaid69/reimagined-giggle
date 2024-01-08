9da33a9d1d0092452425c0c2e4464631df797f46
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:57:55 +0000

    whether crowd disagree cum issue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
