924592bd8ce5642d952a6b1661f775350a0959e3
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:17:40 +0000

    sell honest remove snow lonely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
