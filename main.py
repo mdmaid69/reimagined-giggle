c2d2b694f0dc5621e43bc94bf0062418ebef0576
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:19:39 +0000

    business Zz valuable even straight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
