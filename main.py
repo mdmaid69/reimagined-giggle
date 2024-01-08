0bb0635e2663be536b18d3b6c9ccf81240b2ef63
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:24:51 +0000

    long address play environment straight

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
