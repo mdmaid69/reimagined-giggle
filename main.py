1586f86db3bc6e32d5193929b67a6bee826d8e21
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:56:55 +0000

    long loss exploration post single

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
