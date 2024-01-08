be8fb1775bc0e6a62c1a724232dc3562fd73cb33
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:28:08 +0000

    thanks convenient to concern excuse

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import sys
+def exit_program():
+        sys.exit()
