e469c0c8bfd208993a50cc8561eac3ebb00518ae
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:29:36 +0000

    she pinch deliberate Hh sting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
