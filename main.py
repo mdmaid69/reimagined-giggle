8d762fd3092d1bf3137aeb3acb91a44f68a4b749
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:51:13 +0000

    soft sorry charge practise proof

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+for i in range(10): print(i)
