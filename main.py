cfb51a8778cfbe536ff8d7de7e24b44ea0039084
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:56:23 +0000

    game satisfied punish not affect

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def calculate_distance(x1, y1, x2, y2):
+  def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
