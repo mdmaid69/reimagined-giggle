3f1d6927a764605b09d12b917353cc0d8fc9d87e
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:44:52 +0000

    content perfect mistake juice turnout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def reverse_list(lst):
+        return lst[::-1]
+  def sort_list(lst):
+        return sorted(lst)
