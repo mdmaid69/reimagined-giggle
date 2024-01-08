5b1cfb4548e3fedd2f8b1fbc7e5e5caa74349ec0
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:47:25 +0000

    into completely food account hit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def convert_to_hex(n):
+        return hex(n)
+import os
+def change_working_directory(path):
+        os.chdir(path)
