e3aeb0a9fd322e0a83dfc3c5947e6d230b55dfa3
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:41:00 +0000

    disgusting splash lay regularly length

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import getpass
-def get_username():
-        return getpass.getuser()
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
