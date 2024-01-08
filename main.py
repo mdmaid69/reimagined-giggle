daf1a5c021f755dca13cb2a2d3be5018bb63fb95
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:01:21 +0000

    takeover standard steal needle science

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import datetime
+def get_current_date():
+        return datetime.date.today()
