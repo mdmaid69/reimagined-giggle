d236f8d7e5058d85d8d689ce14143910e1be9d50
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:54:51 +0000

    much breakfast quality whole union

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
