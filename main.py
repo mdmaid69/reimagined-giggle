13ea8b8376be28bf94fe45dbde19390c7bef1044
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:14:13 +0000

    splash season head considering fortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
