d5ee0793b337b26d845a0ec9dcad3138897f4705
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:52:24 +0000

    reasonable comeout attempt pleasant sense

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import sys
+def exit_program():
+        sys.exit()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
