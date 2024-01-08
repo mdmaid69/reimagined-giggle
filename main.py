c0330c05ffbe9053278ef1f5172dfc8c335b04e9
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:32:49 +0000

    crash port lonely breakthrough fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
