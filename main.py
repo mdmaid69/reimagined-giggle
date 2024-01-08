c3d99f3e8af431aceb2a95e965bd548cba9dee43
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:22:36 +0000

    bother image apply keepup progress

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_current_working_directory():
+        return os.getcwd()
