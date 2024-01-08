f023c65b3316f6b7504a01ead940b840784a8a4c
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:08:47 +0000

    direct decide hot reach thinkof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import time
+print(time.time())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
