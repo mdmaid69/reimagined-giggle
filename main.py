f3ea11bdeed60b1ee6a9d7141eef29102d9df306
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:45:28 +0000

    birth heat child join bored

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
