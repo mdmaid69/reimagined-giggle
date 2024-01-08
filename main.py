598edb705fbc3f8fb3390a82fd5b03bcb6455ba5
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:36:14 +0000

    bring name treat somehow tooth

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
