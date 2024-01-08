18893787885f2ec0ae663549add006270a7d41fa
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:41:34 +0000

    return you her bored busy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def reverse_string(s):
+        return s[::-1]
