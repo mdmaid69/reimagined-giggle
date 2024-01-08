b7ac6c4efc46868e4223febe1aa7375652bdb8a8
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:38:13 +0000

    scale where weekend combine shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import re
-def find_pattern(pattern, string):
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import re
+  def find_all_occurrences(pattern, string):
         return re.findall(pattern, string)
