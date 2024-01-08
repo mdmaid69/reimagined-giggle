963f6cabafd210ab7e075452ad3c2508c393ae81
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:01:51 +0000

    interfere talk interrupt doctor run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
