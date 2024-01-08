9bbc48405d5b3f2d78bdf91010a58ba6e9ebfd6b
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:51:00 +0000

    main upwards proper feed rarely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_as_set(array):
-        return set(array)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
