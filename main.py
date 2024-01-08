b1ecfd69e11690cccab55b3d5c764ae84e0c2fd8
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:35:43 +0000

    exact at drawing not lead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_speed(distance, time):
+        return distance / time
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_length(array):
+        return len(array)
