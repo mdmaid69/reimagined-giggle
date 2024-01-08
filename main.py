0d89f2c12c341c8f2bf7bbd4be1ddf67347cce5e
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:22:17 +0000

    quality hit takeout false before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
