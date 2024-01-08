1978fe8dc6b4018f8d1f3a55fb97d152bf4ecc24
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:02:27 +0000

    sister please key win phone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import tensorflow as tf
+print(tf.__version__)
+for i in range(5):
+        print(i)
