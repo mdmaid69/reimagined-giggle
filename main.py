ee9fc6e4894a7002bc9a64e76a5c81ad65df1317
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:21:47 +0000

    her party dull as wash

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import tensorflow as tf
+print(tf.__version__)
