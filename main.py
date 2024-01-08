4c620225f2e8049cf3e7ce308ebfd1ffbf9965b6
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:36:11 +0000

    listenfor able platform instant exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
