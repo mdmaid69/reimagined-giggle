8421f6e2009b8fa691a4a6ea213254187b590331
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:27:29 +0000

    comment since cheat target tax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_typecode(array):
+        return array.typecode
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
