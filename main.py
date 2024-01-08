72b59c0fc1089f555fad275b07c5773e646721c7
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:18:38 +0000

    fear easy generally neat instruction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
