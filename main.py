71e1a99b73d82913a99c30a5ff058ad860a9040d
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:14:38 +0000

    board thin medium call noisy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_area_rectangle(l, w):
+        return l * w
