990e5ff71216db7f37bf62614ced3aa1042aa431
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:29:21 +0000

    fire destroy talk present last

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def is_odd(n):
+        return n % 2 != 0
