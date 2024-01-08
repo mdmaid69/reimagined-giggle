994119891e1b3739a43e0669808a1b43b76e59fc
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:39:43 +0000

    tear concerned opponent custom introduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
