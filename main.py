d98dbe8245ee8d5347102731ebbdfa86903ac704
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:05:15 +0000

    temper brush maximum free here

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import getpass
+def get_username():
+        return getpass.getuser()
