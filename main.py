e7bb2c6b384f3c91edd83edf8c393468eabef0f3
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:31:22 +0000

    care custom treat suggestion luck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
