532b531702328b2cd937f76984439e6a40fa7922
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:05:52 +0000

    pupil angry rather advice whatever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_max(lst):
-        return max(lst)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import sys
+def exit_program():
+        sys.exit()
