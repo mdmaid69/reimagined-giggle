812717f838e51f94b4f88569ad8065ecf727ef71
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:49:58 +0000

    state part tune confusing close

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def convert_to_octal(n):
-        return oct(n)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
