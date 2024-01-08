27eebc86b677517f4775400f5a3c07a864277b97
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:53:48 +0000

    welcome sea politics once simple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
