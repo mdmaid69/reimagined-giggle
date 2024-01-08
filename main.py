6c962d9e868128ff503a8577147d8797e77296e1
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:01:42 +0000

    design them present chief familiar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
