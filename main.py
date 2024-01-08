798daa1cf9e034e2aa872c57ff8a487b928bd876
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:28:21 +0000

    skin remain future trust nerve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
