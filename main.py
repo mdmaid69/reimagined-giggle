8981fc9f8ca4099b7f657f7a265e32ed687a7305
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:56:02 +0000

    hide base board expression smell

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import math
-print(math.pi)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
