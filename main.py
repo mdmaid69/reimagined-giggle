cad271072a509ffa0d3885dd9229cd75576fa53b
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:39:03 +0000

    bound mix increase season sink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
