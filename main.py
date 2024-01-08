89edd0c1fdcdc3b83fb3a4199ca67a2fc259867d
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:55:30 +0000

    hope shoot beat characteristic loud

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_item(array, i):
+        return array[i]
+import array
+def get_array_index(array, item):
+        return array.index(item)
