e5abc13c4b5e939f0cf057fae3f3a5e5e03d4089
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:06:35 +0000

    team serious along good fence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def change_working_directory(path):
+        os.chdir(path)
