1cd09aab404523c8d5efa835a228eef80322ea46
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:04:58 +0000

    raw deaf until neither step

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
