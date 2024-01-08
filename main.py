3d5e3ebe5e2c5a7aa19baca1c74894bea7bc563a
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:44:47 +0000

    swim habit matter square attract

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
