d2835283248cc41928b7e24393c9aa931353973a
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:45:30 +0000

    ability hit church as thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
