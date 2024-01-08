801f70fda99f095fc0eb08f4209b7da6056df69b
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:29:41 +0000

    loss better float divide various

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import collections
+def create_queue():
+        return collections.deque()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
