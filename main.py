88b35a8b4df4000d2d9ec8ef89c2aaa16af104f9
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:26:29 +0000

    again food worth profit free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def calculate_power(work, time):
+        return work / time
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
