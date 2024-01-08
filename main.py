2282ea65200718de6492aebb23d937e138eb5441
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:56:52 +0000

    great but yet healthy do

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
