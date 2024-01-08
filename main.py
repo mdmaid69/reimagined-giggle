f3ad0d4400e247299a98fc76961f1be3991c0843
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:14:38 +0000

    as improvement glad pretty mother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_list(array):
+        return list(array)
