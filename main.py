e976f45527dfe71be05467ee0946d9079016dccb
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:43:51 +0000

    tell art blame return face

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_arc_sine(x):
+        return math.asin(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
