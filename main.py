d12102bfa34bb2a88d9fd8ce927be2a2db93a415
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:19:09 +0000

    turnup model workon short desperate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import sys
-def print_python_version():
-        print(sys.version)
+  def convert_to_octal(n):
+        return oct(n)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
