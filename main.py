733b7952c24feda7513ec5cd9bf92ff5bfab8f16
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:35:12 +0000

    meaning history unable get Kk

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-print(os.getcwd())
+def calculate_speed(distance, time):
+        return distance / time
+  def multiply_numbers(x, y):
+        return x * y
