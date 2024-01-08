4b4c9d42167801ce831ec5952f6d21b6dc0f46d7
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:22:40 +0000

    ours nor exit guard discussion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
