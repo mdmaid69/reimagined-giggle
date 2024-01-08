c89322ca18f75f501435a5f817132237bb775fdb
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:49:36 +0000

    that dig cost speak possibility

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_power(work, time):
+        return work / time
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
