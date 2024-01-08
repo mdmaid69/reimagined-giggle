e31be4a88c4f91a920de98290b79ae1330326152
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:40:06 +0000

    shape track keepout wind repeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
