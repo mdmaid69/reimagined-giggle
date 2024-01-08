ad67424a719042321a7868788017c963fe5d1d82
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:12:01 +0000

    accident foot goout concerned hole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
