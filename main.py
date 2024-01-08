3f58796451a874b5f7d85480693d369092ef4b5d
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:23:24 +0000

    nerve well excited burst grow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
