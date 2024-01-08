b4a30309affc1bb512d8f54e99eb70dbe58d83d0
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:13:45 +0000

    quit Hh share bring time

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
