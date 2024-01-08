575e515b0cbb55ba328867b59c42c7459682b182
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:34:08 +0000

    character stare wet entry same

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
