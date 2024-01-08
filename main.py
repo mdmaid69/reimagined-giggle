f84eeec2884a5c5438d2278e777f984f7af6f394
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:20:37 +0000

    apply Ss introduction onto fly

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
