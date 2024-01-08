b194f32d265a25bf2c3cc14287c7018099520878
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:43:09 +0000

    test cook basic silent transparent

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def remove_duplicates(lst):
+        return list(set(lst))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
