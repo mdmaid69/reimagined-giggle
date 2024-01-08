0b8712800a62c4d8176ea31d7aa57558aed86442
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:45:37 +0000

    content but book people blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
