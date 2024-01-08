5345ba1dcdd6c62e25eceacb01defba543d6eba4
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:43:47 +0000

    convincing suddenly worried phone read

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
