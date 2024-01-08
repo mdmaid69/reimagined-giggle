80ddddae7232b735546667493682b73de7899e37
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:28:39 +0000

    have entrance true blame play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  def sort_list(lst):
-        return sorted(lst)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
