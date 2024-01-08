73ad60b9181c08203c00297e187c521fd59f6d43
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:23:25 +0000

    than tight best kick dick

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+for i in range(5):
+        print(i)
+def calculate_acceleration(speed, time):
+        return speed / time
