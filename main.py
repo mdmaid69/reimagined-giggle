2ffdfed357451384cca37fd691f2edbca54ba7b4
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:32:11 +0000

    university used every keepout advanced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def add_numbers(a, b):
+        return a + b
