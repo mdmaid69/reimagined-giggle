1fad06adc2f597422defda3f5c5e86def623c14b
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:23:32 +0000

    sink spill weigh minor close

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def convert_to_octal(n):
+        return oct(n)
