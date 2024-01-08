5ca23d329f06300afaf40a78ebd4ac98eac28b4a
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:26:01 +0000

    bag turnover she mystery system

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
