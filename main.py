cbc25f751a0bbbe55fd97e7603f5c03d29e1aa77
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:05:25 +0000

    escape satisfactory dark especially pin

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+print(os.getcwd())
