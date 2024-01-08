b1cb91bd980a5c00d1d629c78bf767ba09bafd63
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:48:10 +0000

    equal soft come heat peace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
