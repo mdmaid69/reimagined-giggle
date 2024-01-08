d546461652e31b321d768b4f7f9f7267f82296a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:56:25 +0000

    tall error lookback(on) cost boring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def clear_array(array):
+        array *= 0
+import array
+def get_array_as_repr(array):
+        return repr(array)
