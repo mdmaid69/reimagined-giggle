eb9667fbdde03920ecbb239f33610c9750a47519
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:13:16 +0000

    false thumb his ours holdup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
