2a42c1102fb3193e6057ba15b87bf11a49bcd2f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:59:48 +0000

    guilty light century mark lift

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
