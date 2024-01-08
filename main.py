c43e31095bc087a0420c908c9f5423c7737b6709
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:01:09 +0000

    first care breakthrough accidental remark

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def convert_to_binary(n):
-        return bin(n)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
