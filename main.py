80c13969d235c87f446590ddb706288adf7f408e
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:01:53 +0000

    opinion work risk woman entrance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
