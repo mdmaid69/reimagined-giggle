189fb170bc5fd4d5eb7c864b64142d247c388905
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:00:29 +0000

    after deep make occur prevent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def add_numbers(x, y):
-        return x + y
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
