f6b0029282369db2cb2b8f668e56413b396cdb64
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:33:03 +0000

    leave people little spoil standard

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
