095196eb6afd61def6d23a4b4e562e713f794952
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:23:06 +0000

    deliberate total tip stone lose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def sort_numbers(numbers):
+        return sorted(numbers)
