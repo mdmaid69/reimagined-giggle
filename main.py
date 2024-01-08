705af81232a427d3aab3f36253889a92539c6070
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:02:53 +0000

    declaration right name discover job

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
