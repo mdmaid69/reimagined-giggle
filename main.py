07009c415e9acd8bfecff494c5d2ba18376ca886
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:02:26 +0000

    under even sort emphasize description

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
