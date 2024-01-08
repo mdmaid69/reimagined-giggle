9e653a3431ab36902cc6fdb7c8c1236432f92742
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:28:56 +0000

    laugh address idea objection switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import os
+print(os.getcwd())
