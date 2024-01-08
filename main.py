c60c793d5bec02fb13b557e2254c7b10a5e8575f
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:01:16 +0000

    best purpose better worry aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-def calculate_area_circle(r):
-        return 3.14 * r**2
