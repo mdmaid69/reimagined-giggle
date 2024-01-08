2b128487c7bf8e7a3e37f3f59fec02b310440060
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:26:10 +0000

    also smoke period she handle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_pressure(force, area):
-        return force / area
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
