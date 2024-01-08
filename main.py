f8b1302f88a964d743b157f0c260a890cc410bd6
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:16:50 +0000

    team temperature listen underneath wear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
