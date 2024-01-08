7d381995bbde6c1c230db74fb9828cb2c42519f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:47:22 +0000

    report apply appearance rain shrink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
