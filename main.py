f8be34bbdb606524601dd6dc5d4814fc792293b2
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:19:05 +0000

    fire character affect tip dry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
