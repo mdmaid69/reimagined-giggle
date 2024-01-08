c3b497a6500b0df40fb75e2f25d6ee511263d26a
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:32:14 +0000

    runover example otherwise include flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
