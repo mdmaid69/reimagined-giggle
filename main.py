e6375450eaf31bc3505d88f8e5dd8869c50653b0
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:36:40 +0000

    under plan heart problem shadow

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+text = "Hello, world!"
+print("Characters:", len(text))
+def convert_to_hex(n):
+        return hex(n)
