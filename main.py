9099dd8495f11093efea51b20a4e141a37c97825
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:33:39 +0000

    pipe everywhere stranger flight family

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_power(work, time):
-        return work / time
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
