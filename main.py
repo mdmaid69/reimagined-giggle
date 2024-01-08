1392878694432a4754affee62fb8af72f8f1d6de
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:48:41 +0000

    damp lookafter murder maximum man

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
