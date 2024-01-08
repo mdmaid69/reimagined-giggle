0ba7824907cd7895f00c3b61e8504abf904aece5
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:20:50 +0000

    cry improve charge turnover block

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_typecode(array):
+        return array.typecode
