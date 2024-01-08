39f4713510c24f692d60537b2f28cabaaa7d787c
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:29:08 +0000

    bang Ff terrible trick from

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def add_numbers(x, y):
+        return x + y
