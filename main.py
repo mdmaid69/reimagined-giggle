c180678b6acdb75860fe189770022a6734fda8d7
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:20:05 +0000

    situation touch negative lift curious

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
