3b621ee8f1b0ea70cb79c866ad193c9ab0701c40
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:35:16 +0000

    fetch radio public pay under

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  def calculate_area_circle(r):
+        return 3.14 * r**2
