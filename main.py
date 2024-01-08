15b8ee5155ddb0d7bc29dfbc261fa1546904ac91
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:50:33 +0000

    fashion contact principle shopping lost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
