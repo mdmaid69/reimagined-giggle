15e56eae9493470507d30f7182d63778e33daf29
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:51:21 +0000

    ear colour along sale cope

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_item(array, i):
-        return array[i]
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
