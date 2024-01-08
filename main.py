d16d6a5ce0afaf77d4f3b459d9106e6eba6106cd
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:28:06 +0000

    space lift themselves offer read

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
