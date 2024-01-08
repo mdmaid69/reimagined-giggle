5a12dd9a1df73836be7d2d6e226174f05bb61c8f
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:59:13 +0000

    describe sister up past combine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
