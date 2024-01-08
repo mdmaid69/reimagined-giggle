5dc3ba164faffd1703a410dd89c9ee40f90d8d64
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:23:09 +0000

    figure head attention autumn responsibility

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def sort_numbers(numbers):
-        return sorted(numbers)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def find_max(numbers):
+        return max(numbers)
