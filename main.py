91e3d6822db480fba73cf52d8aef99cd8c53cda5
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:34:28 +0000

    wave wish fat bread theirs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
