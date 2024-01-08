f414877347512f27e6f54c715086c38b38d39a3c
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:05:02 +0000

    react comeround chat drawing emphasize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
