9095230383dd0ae2ccca23831853b8736bda2734
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:27:44 +0000

    port invitation answer talk movement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import sys
-print(sys.version)
+  import sys
+  def get_python_version():
+        return sys.version
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
