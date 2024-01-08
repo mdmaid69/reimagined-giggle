796eea416bd40149bafb813d9e062e640151ac90
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:16:10 +0000

    run getaway mean television my

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import os
+print(os.getcwd())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
