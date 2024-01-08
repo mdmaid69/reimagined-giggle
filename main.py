642454a2a8e5bb57ae40e9caecf5b0552a7c3f9c
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:02:37 +0000

    neither turn return more easy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
