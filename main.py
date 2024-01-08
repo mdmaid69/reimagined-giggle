027bd30b5b3468e348f2e9e2247be28b56b0600e
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:38:46 +0000

    prefer matter likely wipe soft

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import os
+print(os.getcwd())
