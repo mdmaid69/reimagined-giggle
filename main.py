effccda7e5ee44578d06b57f79dabe9619132a19
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:26:51 +0000

    smart congratulate Ii accidentally everyone

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
