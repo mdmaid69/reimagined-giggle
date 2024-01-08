cada3e5fdf18bf78f89801af8ee6e3ee21f31040
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:10:23 +0000

    towards spin absolutely else regard

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_float(array):
+        return float(array[0])
