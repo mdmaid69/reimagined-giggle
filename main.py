5159a5ea4b8669e33f00a75c0205162a575451e8
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:05:38 +0000

    claim dry flavour protection heat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
