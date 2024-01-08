fe848aec17fc01e3387f4858c9422c3bcf0442d5
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:55:29 +0000

    confuse shallow memory away law

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
