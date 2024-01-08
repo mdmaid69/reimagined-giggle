1e7ebe3b412a7d736fc89dc5856b014289389f63
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:33:03 +0000

    up what already hold ocean

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def split_path(path):
+        return os.path.split(path)
