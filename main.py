848aa35d50f26155144e3f3edd4ce565627551bb
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:51:34 +0000

    interview total private central song

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+for i in range(10): print(i)
