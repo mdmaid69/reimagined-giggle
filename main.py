771281b63d59140e485bf07cdc675bda85686e43
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:48:26 +0000

    plate ago address talkoutof through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_item(array, i):
+        return array[i]
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
