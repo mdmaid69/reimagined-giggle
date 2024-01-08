dab9177cce371cdb95c58558d411bce97d8b755b
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:44:26 +0000

    amount it moon failure damage

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-print(sum(range(10)))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
