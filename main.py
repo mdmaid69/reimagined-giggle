8c5503f46d369dce2a13f8592ac453ddccaa1891
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:12:55 +0000

    throw complete taste winter have

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import sys
+def print_python_version():
+        print(sys.version)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_buffer_info(array):
+        return array.buffer_info()
