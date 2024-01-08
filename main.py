20ea70e3f3a9cc92a6c3c8b1d0002db7145a28d2
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:31:25 +0000

    dick guess meaning shoot movement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
