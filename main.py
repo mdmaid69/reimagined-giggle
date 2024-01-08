669ac6106b2ad80200cb98b0f270b1e7bd87393a
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:43:20 +0000

    otherwise spring course fond instrument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
