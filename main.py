b06814d22beae169e787d266450e8887fbeb6baa
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:41:26 +0000

    disturb loan off contribute even

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_user_list():
+        return collections.UserList()
