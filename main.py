fb16063de3786ee496c57e4214d9b59b7c227d79
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:42:49 +0000

    centimetre greeting division beside tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
