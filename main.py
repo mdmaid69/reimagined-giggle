887f0533bfab4f460b50cb4b165ff4b7525bf7e8
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:57:35 +0000

    against free correction criticism kind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import logging
-def log_message(message):
-        logging.info(message)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
