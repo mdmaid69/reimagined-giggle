32d38a903e2b22ff87040511246e4250d6fbf890
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:41:15 +0000

    for character undo left sore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
