99d8d533f738119fa0962bc93915c62a8731c8dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:26:59 +0000

    will oil broad rain tall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
