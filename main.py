eefb6b7bf0021f609431ce7964b252f0b4d3ccca
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:45:26 +0000

    single best position escape there

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+text = "Hello, world!"
+print("Characters:", len(text))
