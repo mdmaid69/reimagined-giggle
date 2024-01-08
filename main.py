0892f2dc342b0cd6dd781a84aaefb3b17a1aaa73
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:43:41 +0000

    heavy holiday badly frightened drawing

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
