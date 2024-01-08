368071ea00c911e074bac4aad3fce4b1ae8ababd
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:56:25 +0000

    failure fall tense concentrate pass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
