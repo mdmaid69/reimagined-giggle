e28114d36177492c267ef9339bcbd93368d46ce8
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:27:57 +0000

    own first tough beside keep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
