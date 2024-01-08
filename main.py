473134c194e8d46405cab3899d9927d94969fc04
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:50:12 +0000

    back late pretend object carry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
