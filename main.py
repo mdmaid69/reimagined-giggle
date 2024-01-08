da4a1d087fc732f02b31d602eeecb1f82ee00fa4
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:21:55 +0000

    sentence think opinion amount punish

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
