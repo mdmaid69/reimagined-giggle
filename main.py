cb2582cad3abe049f2aab09c8c2f6112896d1ee4
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:27:09 +0000

    mystery pause do light base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
