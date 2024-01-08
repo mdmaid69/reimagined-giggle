7aaa15692eb95c9561e82521d471523b4062a454
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:16:57 +0000

    hold discovery capital uponon service

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
