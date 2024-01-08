ccbaf4c512a225ec52c53ee568323101bb2349eb
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:18:52 +0000

    treat believe regular that she

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
