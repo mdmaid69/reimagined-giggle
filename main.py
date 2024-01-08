2dd2eb67b07dd2f4d564fc62ce3050af34f01fa3
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:26:44 +0000

    before somewhere until offer phone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
