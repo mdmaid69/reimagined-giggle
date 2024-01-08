49f61ec18665b48095462883c9b6486a35176871
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:24:44 +0000

    shall raw again sad fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
