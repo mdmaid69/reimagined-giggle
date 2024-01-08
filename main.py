607d1b8a0cf889bb5db890a941d1050ed7212d5c
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:46:08 +0000

    wet ready advice care ordinary

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
