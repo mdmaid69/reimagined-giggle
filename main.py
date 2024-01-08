ed2fe4e4b29096e70c1eec47d61a07d47dce49cd
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:57:19 +0000

    centimetre busy march by doubt

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
