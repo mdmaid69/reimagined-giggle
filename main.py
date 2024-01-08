99bc919f91240fe03ea5ba9e2231b3b4db610f92
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:10:05 +0000

    completely dry ill improve base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
