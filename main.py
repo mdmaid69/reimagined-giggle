1813265c6e5c777eeadb5d4519b26ef1b4327e38
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:06:55 +0000

    sit the trust mood day

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
