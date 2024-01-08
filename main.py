5031c167ee40fb610629f1de10b1334cca86809f
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:13:39 +0000

    contact lean embarrassed mad takeaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
