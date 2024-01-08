ec631ac43fbadbc9a776b19e2cef3718092aeeae
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:34:04 +0000

    effect trial century mail better

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_density(mass, volume):
+        return mass / volume
