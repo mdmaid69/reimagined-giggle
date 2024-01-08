a67cbda1b3df80f928f5fef671fedf65a9588cd1
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:07:04 +0000

    brush aware shop stupid minimum

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def close_database_connection(connection):
+        connection.close()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
