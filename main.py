3d5c3beec4d5a4163bd412d4d9d15db1d3d8d167
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:08:46 +0000

    with decide contrast anxious answer

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
