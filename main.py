43474fec50cbf8f8613cc6d92cd897d6f324e375
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:23:31 +0000

    turnup regular else sleep possibly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
