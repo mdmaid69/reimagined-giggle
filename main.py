06dab7f77e1da39573cc2875877f5b2db94fb9fe
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:41:29 +0000

    suit frightened bridge also comeout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_work(force, distance):
+        return force * distance
