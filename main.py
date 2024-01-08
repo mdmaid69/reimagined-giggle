b110dbc26f2d26ee5030ba3447d7b262ff537f44
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:27:53 +0000

    birth snow nature refuse idea

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
