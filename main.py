fa532d4849079658439d48a6286a2ea9011b0e54
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:56:27 +0000

    guilty onto weekend wrap minimum

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import array
-def clear_array(array):
-        array *= 0
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def extend_array(array, iterable):
+        array.extend(iterable)
