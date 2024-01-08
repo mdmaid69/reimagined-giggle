11ef8aac9696d7fc0e73d3dfe0524c1609ed1050
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:10:13 +0000

    heat keen usual chance pleasure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_acceleration(speed, time):
-        return speed / time
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
