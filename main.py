ead91edc250fa5118da6b44158e1c2426deb235f
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:23:03 +0000

    autumn record cut nigga trust

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-import sys
-print(sys.version)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+print("Hello, world!")
