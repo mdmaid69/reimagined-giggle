dac6c3a382333fc0fdc98d1788b8ee0687e256f8
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:56:57 +0000

    rely breakinto quality Jj Ss

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
