bddda3aebc95484fa73e8107772305eaea19d185
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:58:18 +0000

    deep speak law scene through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def split_path(path):
+        return os.path.split(path)
