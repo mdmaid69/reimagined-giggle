b7ea58c86f11ac6f4b3837e34e667bcd481462df
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:21:17 +0000

    naturally criticism set turninto invite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
