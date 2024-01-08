611c718dcc4dfb1cb5f7882adc7a9dd145109a42
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:51:19 +0000

    hot peace warm month ourselves

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_tangent(x):
+        return math.tan(x)
