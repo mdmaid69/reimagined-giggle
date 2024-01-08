5c3a34bd264139839431fdddeb2d7b5838d5f448
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:19:40 +0000

    dick slice road free division

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import random
+print(random.randint(0, 100))
