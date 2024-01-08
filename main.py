b0627a04e216a3418456441f1f89498168eaef44
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:12:42 +0000

    trousers drive dish bridge example

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def find_max(lst):
+        return max(lst)
