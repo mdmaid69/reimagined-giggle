898c507537d41cb02093d8ae13078574e39d7d1a
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:04:43 +0000

    talkinto present change busy question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
