cbf741b3694282c668ce779a641660f2df3ac691
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:05:16 +0000

    anxious rank yourself motion shade

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import collections
-def create_user_string():
-        return collections.UserString()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
