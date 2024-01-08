51c557e851a8dd7dc12efbd1dc1b004b3873c612
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:53:25 +0000

    edge Bb discovery afternoon inform

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
