9913021c294fe27a83d0df3c14d436707c990519
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:10:46 +0000

    exaggerate even blind hers acceptable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
