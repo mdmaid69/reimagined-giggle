8e6d327e1741bb37aa1d843666439c153bfc9695
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:38:33 +0000

    wire treatment doctor everywhere urgent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import platform
+def get_os_info():
+        return platform.uname()
