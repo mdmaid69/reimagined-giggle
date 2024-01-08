e2398effe87f1e1e7a4274788aa99af60d97c7c6
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:49:26 +0000

    direct know spare life before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
