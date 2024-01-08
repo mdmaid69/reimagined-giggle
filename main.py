0561f61424d76dc95e8e6f7b93f972b8213fe122
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:49:19 +0000

    content whether satisfactory count ill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
