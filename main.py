4c79610f08d8b8aa568347a4c3a7fc952b57e05c
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:39:25 +0000

    wash confusing suppose total sight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
