71cac08a041b66dfdd75e7aedea4ef55b7af801b
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:46:29 +0000

    travel plan land squeeze 572enter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_power(work, time):
-        return work / time
+  def convert_to_octal(n):
+        return oct(n)
+import time
+def get_current_time():
+        return time.time()
