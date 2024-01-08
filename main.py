188635a709db2385091adea46dde27bc26be033e
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:47:52 +0000

    possession detail search once feed

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
