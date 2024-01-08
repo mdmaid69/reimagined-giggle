9e4fc62c3fbfb358a5f106ee7616691a4ff98075
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:01:28 +0000

    sweep change fast dream rubbish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
