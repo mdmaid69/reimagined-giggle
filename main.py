e0cc3804bce67bac477a40fce47b13c03ee1ddc9
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:22:20 +0000

    quit design eye please blue

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import sys
+print(sys.version)
