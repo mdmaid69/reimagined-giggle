9d0b5bcdbd371343fe9b985391efd525c992b1b2
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:24:03 +0000

    arm ground violence figure watch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+print("Hello, world!")
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
