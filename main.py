4c7d24bd7cde77db4ca32b29ef36bca2f8904259
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:25:40 +0000

    convincing catch unit switch delight

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def square_number(x):
+        return x**2
+def calculate_perpetuity(payment, rate):
+        return payment / rate
