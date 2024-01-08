3ebd58c5ece800a2324f54f7b6a79d3f48e3d3cb
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:57:19 +0000

    each spare lean 569enjoy basis

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_typecode(array):
+        return array.typecode
+import math
+print(math.pi)
