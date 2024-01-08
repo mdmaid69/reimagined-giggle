e350ea914ca58e07717eef78a30b6523af81efab
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:08:21 +0000

    standard last survive protect neck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
