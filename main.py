35de690fe1bbc410168b8a23736b61efa8d09c51
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:45:07 +0000

    right find release strike watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
