db6274f7e47bd6817fced43a3c276183f0da7eec
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:08:15 +0000

    rather conclusion clever opposite organize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_item(array, i):
+        return array[i]
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
