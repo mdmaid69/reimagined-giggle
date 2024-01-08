23be89403a429b651cefabad059b26825c61a482
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:24:24 +0000

    rest smell favourite opinion deliberate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def multiply_numbers(x, y):
+        return x * y
