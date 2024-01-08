a3e19da8f5993b0c96b1e12fd608c74cdacd56bf
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:16:45 +0000

    appeal tour raise steep tool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  def calculate_area_rectangle(l, w):
-        return l * w
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
