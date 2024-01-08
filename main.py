4ee527224732861cf0e381011572620a0974f5b1
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:23:19 +0000

    perfect press shadow favour name

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
