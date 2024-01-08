0cea882b419b03a2ce9723e2a3c167e3e7c230cc
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:39:51 +0000

    worth company minute or remain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
