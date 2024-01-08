aaecb91ed09fbd9fb9b71084ef1ac08bb2ca23f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:21:50 +0000

    necessary adjective sad answer whoever

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def get_array_index(array, item):
+        return array.index(item)
