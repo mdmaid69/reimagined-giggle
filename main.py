a982a4e4d84682fba38f29161b7dd5c0984a68bb
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:57:05 +0000

    best discovery hello empty wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
