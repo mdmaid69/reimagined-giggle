c905733bcc82535cf4a5823e0ef998f2abfcdb89
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:35:08 +0000

    separate ground trust peace increase

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  def subtract_numbers(x, y):
-        return x - y
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
