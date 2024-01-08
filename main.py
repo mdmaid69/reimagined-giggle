6f212195301515822d734daaa206bc55f1197056
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:00:39 +0000

    store please dance crisis could

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import platform
+def get_python_version():
+        return platform.python_version()
