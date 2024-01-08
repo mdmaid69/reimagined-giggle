1d14edcbcd78685ed3efa1aa5995d6fbb300b856
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:00:07 +0000

    blind comefrom mother good risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
