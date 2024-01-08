02c768e45c63c24f46dc65072ad28bb812fc4816
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:45:55 +0000

    balance bin there load bar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def reverse_list(lst):
+        return lst[::-1]
