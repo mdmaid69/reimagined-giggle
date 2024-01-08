437f5cbe7b10673bce13ea477faee21131c2094f
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:08:03 +0000

    send thick scene official welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  def remove_duplicates(lst):
+        return list(set(lst))
