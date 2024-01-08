b6300fa73fda3d468f37408d59e24f54e1d7bc96
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:31:22 +0000

    yet attention may hear listen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
