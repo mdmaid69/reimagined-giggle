a6d6bc317473a6f3ee68f7547ffb0b03905e1a53
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:42:10 +0000

    air cinema behind mind near

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
