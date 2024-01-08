720833b03fde12ada206f8c3b3ccf68609bab42a
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:13:12 +0000

    team raise rescue stop pain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import time
+def get_current_time():
+        return time.time()
+def find_min(numbers):
+        return min(numbers)
