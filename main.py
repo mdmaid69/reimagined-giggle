87ea9b4758c637b86389402b3a35b4799fbcbff6
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:22:44 +0000

    bank join sea card until

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
