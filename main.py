90dcc8751c07ab1d4259f24194f0ed3c7634dd3a
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:10:02 +0000

    account argue talk defeat name

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
