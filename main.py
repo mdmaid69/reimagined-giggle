378772d981a0172a147975d2be1a887b23fac186
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:02:49 +0000

    godown comefrom wind indeed land

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
