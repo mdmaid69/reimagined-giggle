58845e33b4baa3d0b72bb32597c7ae999d665879
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:32:39 +0000

    trade contribute calmly damp respect

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
