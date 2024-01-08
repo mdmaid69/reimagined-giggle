1a84677d775952c8c54ec1d5effd10a949f0f50f
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:51:11 +0000

    bite equipment this shop finger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def get_bytes_from_array(array):
+        return array.tobytes()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
