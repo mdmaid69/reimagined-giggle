666660633efffe18c891c8007be5701012da5f65
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:02:26 +0000

    whenever physical would scream which

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+import array
+def get_array_index(array, item):
+        return array.index(item)
