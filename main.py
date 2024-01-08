1249406a1b32622a7c0262d788045b1cb01725d5
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:39:51 +0000

    broad union busy flat nerve

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
