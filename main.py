89b11b055eb20f9e02686f8714fb3076d90ab808
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:35:56 +0000

    street start light copy education

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
