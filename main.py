7010da73d4c41ec94ad51776f8cc32c164286c87
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:05:48 +0000

    young tear guess spoil rough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
