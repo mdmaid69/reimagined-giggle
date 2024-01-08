f3a710fa9d520ce21efc0efb934e011120cce476
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:07:55 +0000

    contribute consist evidence fear benefit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print([x**2 for x in range(10)])
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_logarithm(base, x):
+        return math.log(x, base)
