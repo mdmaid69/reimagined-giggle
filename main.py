8306e7130276062f7b1a35d8d37f396ac29147ad
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:48:00 +0000

    neck translate above draw release

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def calculate_acceleration(speed, time):
-        return speed / time
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
