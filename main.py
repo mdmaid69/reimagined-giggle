34b523092d79a088755d8c393634df2bf1f29cd3
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:27:40 +0000

    may write dear beat recent

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
