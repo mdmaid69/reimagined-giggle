848c68db84bf9c95263af42bf21cf512839cef9c
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:50:37 +0000

    criticism unit drop fortunate flat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
