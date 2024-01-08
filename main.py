84049b19432dcb8671f027e64ac5d4daaaa57df8
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:30:10 +0000

    purpose motion hour lead memory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_density(mass, volume):
+        return mass / volume
