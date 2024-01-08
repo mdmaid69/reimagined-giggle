4eee537092b65a0977d5436e28393bf5f7d0d2bb
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:44:30 +0000

    along shock keepout jump address

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def split_path(path):
+        return os.path.split(path)
