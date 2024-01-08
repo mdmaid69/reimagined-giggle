d393bb61ab4926818f4bc1397b1029e7220a07d2
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:59:57 +0000

    gooff own punish your sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
