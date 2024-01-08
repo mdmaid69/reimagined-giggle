0be590643c2db8d890b7d2324d9b99e7612f6c32
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:46:27 +0000

    course benefit way mind backwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  def calculate_area_rectangle(l, w):
+        return l * w
