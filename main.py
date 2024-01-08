2a569a0f3cf6617da5e3866107441af0b33b9e52
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:17:03 +0000

    home branch goback(to) chance positive

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  def count_elements(lst):
+        return len(lst)
