558934a52866913d72f5514f780263477dffc385
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:59:38 +0000

    float neither knock limit contribute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
