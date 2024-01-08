d7ca4fc30ed89c858aa0b11a4de82d5a2333e4ea
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:50:30 +0000

    edge cough scrap coffee completely

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  def multiply_numbers(x, y):
+        return x * y
