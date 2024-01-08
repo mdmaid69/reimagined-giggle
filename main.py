f93fdf7fc0379d39f162ef1599da59eae101aeae
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:29:30 +0000

    reach road clean subject close

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_array_index(array, item):
+        return array.index(item)
