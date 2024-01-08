bc2bb98b5b6d3c30e20a661b3b4aed2ba68be69f
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:25:07 +0000

    somebody wander complaint sight happen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
