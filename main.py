24ba640126224b64746aeeb712d65c5fa30c35af
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:15:45 +0000

    breakfast make something stamp free

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_typecode(array):
+        return array.typecode
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
