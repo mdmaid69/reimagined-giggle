88998103a80f955c0ab947e668dd08564d64a97f
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:38:11 +0000

    before set add complaint 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_work(force, distance):
-        return force * distance
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
