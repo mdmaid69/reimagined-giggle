3777ca2a22f3baa1bd9c8a6670f8baaed45f4e35
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:46:10 +0000

    easy breakout trial office spend

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+print(sum(range(10)))
