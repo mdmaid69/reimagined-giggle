bb64cd147073ee8df43f1569f71af0ec6f00c330
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:00:33 +0000

    fair cap silent congratulations stay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
