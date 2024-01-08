3391133cf0a684aab860d61b42d63851e22750ee
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:34:20 +0000

    hold over protection takeover shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-print(sum(range(10)))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area(radius):
+        return 3.14 * radius * radius
