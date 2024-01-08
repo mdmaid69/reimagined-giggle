55d78b4b72b1598571477ecd6d1fecb2b80a68b2
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:13:26 +0000

    standard plastic cost skill increase

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import os
+print(os.getcwd())
+def calculate_area(radius):
+        return 3.14 * radius * radius
