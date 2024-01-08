c7ed39d33f1c480c340308161e50a34baa91be4b
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:19:21 +0000

    earth nearly tongue bound do

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-for i in range(10): print(i)
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
