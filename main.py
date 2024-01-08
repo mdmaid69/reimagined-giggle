c2964724e7f2c4a695d6ac84174dd7e9acf678e2
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:02:43 +0000

    careless pattern upset over even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  def count_elements(lst):
+        return len(lst)
