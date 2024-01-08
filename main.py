5fb196f1f605ffb5fefc690a76510763a8c6c81a
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:35:02 +0000

    front adjective hear maintain familiar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import tensorflow as tf
+print(tf.__version__)
