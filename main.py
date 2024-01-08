6cc8ca4112ec27ba20d7d0c44778ad7438d90803
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:07:38 +0000

    will feel telephone large awake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
