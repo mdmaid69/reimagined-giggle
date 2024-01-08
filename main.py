2f8be1a255ba95fd1ba62c27744a1b4e114b7b05
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:20:10 +0000

    happen fit crash the section

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
