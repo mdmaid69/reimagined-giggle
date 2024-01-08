3a05dba0d0318cf4d929398482a66f1c809849db
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:06:44 +0000

    yourself callup petrol company turn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
