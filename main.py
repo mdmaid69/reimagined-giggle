737368ba553d1f417aeb47a38d5c4632bfe66250
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:55:41 +0000

    trade tall guide description gooff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
