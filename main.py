ad72812ebffcd0676a691b143fed0e74d2ddbe44
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:03:36 +0000

    profit out someone reliable gift

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
 import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
