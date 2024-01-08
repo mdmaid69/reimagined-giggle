262348008a971b5d8ba990a70977026095aaa6bf
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:59:26 +0000

    smile contact exaggerate brother dark

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
