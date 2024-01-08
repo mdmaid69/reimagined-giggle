e85c0d61834cda0ee4feed661b2affa9d2b45b5e
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:26:18 +0000

    except ground connected determined offend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_current_directory():
+        return os.getcwd()
