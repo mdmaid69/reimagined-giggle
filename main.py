438dbafc6ed9d816ec6fba55cbfae7cb1c4b83d5
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:24:30 +0000

    swing breakfast surprising tend block

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
