7b58ebee61d3327d91b7bbf6d01ba48cc1ffdaf2
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:18:41 +0000

    squeeze best characteristic duty steer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
 import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
