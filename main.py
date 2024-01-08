7d802b1ddf35c91e7af08f477ac1a415d1254fe5
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:00:36 +0000

    forget great environment difficulty shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
