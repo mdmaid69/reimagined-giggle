3ac8b4c238954d30ba9cbec71649e7aa35d428e3
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:08:28 +0000

    quickly smart busy peaceful poor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def count_elements(lst):
+        return len(lst)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
