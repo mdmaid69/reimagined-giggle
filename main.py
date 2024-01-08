81c452ed572a1a924f4aee576573e5eb91ba5ef7
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:13:23 +0000

    passage deaf learn spare fence

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
