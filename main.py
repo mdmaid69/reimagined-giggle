71972c6453d4e73c84a772cbc64af92a0473a549
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:22:27 +0000

    accept centimetre continue disagree lucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
