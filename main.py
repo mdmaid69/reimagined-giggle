e2e969869383232c51333f628a3b2b89a69c15ed
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:57:40 +0000

    likely tend sister thirsty world

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
