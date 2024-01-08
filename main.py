8295a9854c432cab6c10e8b0e714b63bad676f96
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:32:39 +0000

    communicate region congratulate solve through

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+print("Hello, world!")
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
