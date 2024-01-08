9f2de7f2b591971ca347658789722950257dea89
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:56:50 +0000

    fall spite may careful accident

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
