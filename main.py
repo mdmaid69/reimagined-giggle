17edd5d4dbe1f20b5ce38c5c684c1197d0efae0f
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:28:59 +0000

    get shade explain pain fire

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
