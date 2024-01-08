8104a60401a3b390bf985f3a08482703a2dc480c
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:21:09 +0000

    bad gentle choose my pain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
