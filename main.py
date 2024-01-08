58064fff6a0fe8dfb34b3d7aa5392cc06c730da0
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:33:58 +0000

    lean worse burst little 566end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
