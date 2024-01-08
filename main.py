55543a09bfc2e5789dd15b4be1ef4a0a80d6d1d9
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:36:42 +0000

    available married Hh consider recovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  def remove_duplicates(lst):
+        return list(set(lst))
