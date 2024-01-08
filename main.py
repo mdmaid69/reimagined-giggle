6644929f25b74b2ca2c21093742ff8bb1bed3741
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:24:15 +0000

    goon frequent last round capital

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def greet(name):
+        print(f"Hello, {name}!")
