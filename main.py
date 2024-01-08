dbb99d37fabf7b9ebb1960d7df58bfb6593c515e
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:37:04 +0000

    over muscle against idea truth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
