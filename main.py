e2bdbbe7bc0bbc6662eec7ede93982fcd814c07a
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:10:40 +0000

    even pipe steady pocket shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def add_numbers(a, b):
+        return a + b
