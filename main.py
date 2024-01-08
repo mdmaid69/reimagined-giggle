5a1195d199a0cd2a464a65cad5473a486921ec6d
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:34:06 +0000

    spare die compete underneath bright

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
