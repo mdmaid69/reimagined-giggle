a050ae70be150ed787a63168ed0aa856524e0192
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:38:04 +0000

    own compare beside story slice

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def convert_to_hex(n):
+        return hex(n)
+print(sum(range(10)))
