cf51e6b0e5e17127973d0496b64abd65305bfb00
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:08:52 +0000

    unusual common journey benefit ready

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
