3a67b97291d043111197aecd30367930114d2535
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:46:41 +0000

    production hard crazy hurt last

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def add_numbers(x, y):
+        return x + y
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
