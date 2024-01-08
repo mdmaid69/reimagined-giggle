dfb2c5a7234ba8249500a61d50d5610f46a1cd6c
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:29:15 +0000

    short extraordinary punishment bottom smart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
