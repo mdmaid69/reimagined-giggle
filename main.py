ba2b6d194ceb30327b6122cae48516bf483ceac6
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:59:01 +0000

    effort hungry full stay happen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
