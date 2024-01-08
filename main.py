f3f3584e7f0f9060c397cd502a3e1c11edaafe2f
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:45:08 +0000

    character if welcome their female

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
