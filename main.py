4a63707c295818e901b35546a4a83003532c83a1
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:27:37 +0000

    priority add drink ever activity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
