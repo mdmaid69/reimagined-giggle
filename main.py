eadbd5c97244ef0e7d2360e23b34f0fb762fa9ce
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:21:33 +0000

    as true motion bored dangerous

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
