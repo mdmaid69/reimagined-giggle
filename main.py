b2d6ad3bbb23a5ba0c48ec92bf906a2df2538f7b
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:24:07 +0000

    past professional close business circumstance

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_acceleration(speed, time):
+        return speed / time
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
