97dd97bacdb3b48caccba7746a55ee6248996198
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:33:28 +0000

    band report discovery bet block

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def square_number(x):
+        return x**2
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
