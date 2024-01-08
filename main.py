fb970b1e5c6ab095d80d8d64036045176682cb62
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:24:43 +0000

    live use safety evening condition

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
