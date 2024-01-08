bb573778eb56ab3b35ae3b5e00183a5322b2011a
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:36:08 +0000

    discover curious ring cruel concerning

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def cube_number(x):
-        return x**3
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def count_words(sentence):
+        return len(sentence.split())
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
