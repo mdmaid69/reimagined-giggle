6ea1df25d66b2b02a5b7dd9bf17db2a9b639f53c
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:45:27 +0000

    crazy production previous wherever land

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import sys
+def print_python_version():
+        print(sys.version)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
