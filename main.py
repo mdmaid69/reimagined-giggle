8b1621e6edfd62070964e8b36388004b4f0dab21
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:05:17 +0000

    by where healthy smoke no

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_power(work, time):
+        return work / time
