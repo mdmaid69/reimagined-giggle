8a81bcf423dc8630d9cceb7df29a960bbaa68702
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:12:13 +0000

    temperature feature fault comment mistake

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_list_from_array(array):
+        return array.tolist()
