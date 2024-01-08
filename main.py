fb910e0c7358d85f49557a6c100eab3c9abc6988
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:54:28 +0000

    damage commit ourselves risk breath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-text = "Hello, world!"
-print("Characters:", len(text))
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
