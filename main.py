9545c60e00d4daa1b05e4dcba11897da3c23928a
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:18:38 +0000

    force year might leave he

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
