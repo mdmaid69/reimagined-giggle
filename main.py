1de269da849d1eeb3d2c65b8b39059c54c4d2818
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:18:33 +0000

    near why breakthrough car poison

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def set_array_item(array, i, item):
+        array[i] = item
