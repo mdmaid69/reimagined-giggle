fc13ce62e7f032e4151032fd296659f015953b85
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:08:18 +0000

    method strike steep weather shopping

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
