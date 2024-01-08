28ca75c1006cc238ab665fc5d6e258b1f37c636d
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:39:29 +0000

    high repair quality difficult or

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  def convert_to_binary(n):
-        return bin(n)
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
