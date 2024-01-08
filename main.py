a64971c71527d7cb92c61e52e67fa9749f491882
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:02:37 +0000

    wire frame there common several

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
