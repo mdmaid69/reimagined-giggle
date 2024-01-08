dfea6cc35987db142d6a1a6fe2c3455a6e3bfe45
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:00:34 +0000

    education pour roll perform detail

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import collections
-def create_counter():
-        return collections.Counter()
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
