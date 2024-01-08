53d3109f87add9d7308fdcd79244b28536760fcf
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:26:43 +0000

    pupil left history shine affect

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def reverse_array(array):
-        array.reverse()
+import logging
+def log_message(message):
+        logging.info(message)
+import os
+print(os.getcwd())
