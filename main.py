d004fa8584fc94833e1d5fbb170433f6eed95efd
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:15:36 +0000

    before start breakout congratulate size

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
