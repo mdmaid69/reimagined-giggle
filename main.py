2dbcfb33503aa0a580d662ece46df9d73eb14761
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:28:30 +0000

    it callup tense sign forgive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
