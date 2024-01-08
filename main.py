1230616dc1467cddb51b2b5f8c6486257e018c4a
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:02:44 +0000

    hospital turn affect chance thirsty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
