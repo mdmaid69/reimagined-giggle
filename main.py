bffae167d66c1d9c23fd0226ec983f845fb214d6
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:48:05 +0000

    inquire break act speech race

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
