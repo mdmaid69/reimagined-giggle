97023e32987a2db1abf11de34f24af97f2c5e25b
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:01:20 +0000

    speak so thinkof record soft

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
