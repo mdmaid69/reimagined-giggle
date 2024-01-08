b1a1ad1e9b245636c39ba91efe84280ca6232d58
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:49:58 +0000

    block sport clearly refer breath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
