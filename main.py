13c56c4f9f8d0ad8d33d2f186a63a07404904b09
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:43:07 +0000

    emphasis think ourselves land content

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
