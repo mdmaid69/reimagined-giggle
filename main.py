3554edb33c571aab3889a839a6d3abd7403a9356
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:30:03 +0000

    earn minority full cause holiday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
