a2d2bc34747251586bcdb716bd74b7c8fdb61a98
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:06:25 +0000

    walk slice woman convincing minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
