47b9a8c5b7c63dd5c7919e3cbf96ffe04e839467
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:07:25 +0000

    special day public bite godown

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
