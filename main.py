77afa8e7df11fce1f31951fb2935cc1c077b5c9a
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:25:05 +0000

    bit create whichever suggestion juice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
