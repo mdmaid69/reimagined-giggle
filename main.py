28830eade000a35fbf340c44f538513b0dde6d93
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:49:28 +0000

    type immediate neck expensive lunch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_pressure(force, area):
+        return force / area
+def find_max(numbers):
+        return max(numbers)
