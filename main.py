075009a7c9add2db46e376fdde0a5967a47f1078
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:20:20 +0000

    design sell getoff sit sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
