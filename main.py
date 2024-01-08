0ea8089c2be33fd924ebacde62ba53a5a242c5ea
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:16:47 +0000

    pile exaggerate arrive nature winter

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
