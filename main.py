dca2d3f071c14b9272ad47b02ee56d14f3ae388b
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:41:47 +0000

    past service bin repair suppose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
