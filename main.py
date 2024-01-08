62f6f9349e70a84ca8e7957d8b332fe394bbbcd8
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:33:44 +0000

    benefit wish walk central eye

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def cube_number(x):
+        return x**3
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
