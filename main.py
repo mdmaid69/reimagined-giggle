5d7962e68b155f07858a3390bcbe98a2a5cef07b
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:56:02 +0000

    poison announce swing let learn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
