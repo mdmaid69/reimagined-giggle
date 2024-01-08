e5199eea7189bd907081ca4f5008c0d2cbdfc52e
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:39:27 +0000

    style bath market left underneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
