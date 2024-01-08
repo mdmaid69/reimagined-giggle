60dd2a84ab1503d2bc35ec877b830ab595e9331a
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:40:07 +0000

    obtain common cinema separate stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_list(array):
+        return list(array)
