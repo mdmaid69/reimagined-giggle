7054917f0ff19e6a633985e8b1edcb9fe564bd6c
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:32:36 +0000

    round thinkof uniform instrument wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def create_user_list():
+        return collections.UserList()
