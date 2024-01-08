5220e02d10e3ba9b3bc4522cd2178e450ee1f936
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:18:23 +0000

    side false value tie pain

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
