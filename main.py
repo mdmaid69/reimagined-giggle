c8912523f4283e0ff7a57bfadaa9f9058bd56685
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:32:08 +0000

    mad personal how kind slice

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def sort_numbers(numbers):
+        return sorted(numbers)
+print([x**2 for x in range(10)])
