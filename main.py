7db873fad4b3be5a5c4d65ff7cc932f2484bf9e9
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:33:37 +0000

    rule passage escape operate petrol

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def convert_to_hex(n):
-        return hex(n)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import sys
+print(sys.version)
