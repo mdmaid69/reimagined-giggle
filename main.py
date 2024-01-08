e53def53f060b383e2ae505e4757a09c71615a4e
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:11:05 +0000

    suddenly explain pause close best

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
