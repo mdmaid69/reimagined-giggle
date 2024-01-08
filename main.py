2f58dd7523efd87de39ad2324b6fc297f19eced9
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:35:14 +0000

    likely replace relationship win by

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
