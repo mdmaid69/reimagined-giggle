4e36867a64b13d1468983ed3f30254a957b7561c
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:46:59 +0000

    course burst hear comment suggest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
