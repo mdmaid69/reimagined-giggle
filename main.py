59fa85db1da49f403c9cb26660fd960ccd92a91f
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:54:16 +0000

    whistle note steal win use

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-def find_min(numbers):
-        return min(numbers)
+def is_palindrome(s):
+        return s == s[::-1]
