6c5e74e119431636f4a625a7b9315d5d7558faf1
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:24:49 +0000

    573entertainment direct present holdout life

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
