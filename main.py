50c5cf31c9751a596311b39020173c2a465f4f2e
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:31:28 +0000

    means war settle neck reasonable

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
