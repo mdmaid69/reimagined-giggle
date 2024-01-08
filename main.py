8acb41b643efc382c048b03ac3852b81ef05fb24
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:18:30 +0000

    willing wherever experiment ask suggestion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
