b77e2b0b242a091642ad01c4516d6f0e022a62aa
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:41:34 +0000

    until mistake swing hang grateful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def reverse_string(s):
+        return s[::-1]
