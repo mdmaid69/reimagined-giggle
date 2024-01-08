b19fcec9322ac351211de735e07566ea4a7b6812
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:45:48 +0000

    opposite heart certain pool Tt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-print("Hello, world!")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
