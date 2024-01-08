992e5b5c6dea0c3d422c312bb985390303b46eec
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:21:15 +0000

    conduct backwards steep goback(to) fly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
