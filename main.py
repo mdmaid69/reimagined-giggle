e46e89994022da57c38e3d811c66cb9bb588db22
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:44:08 +0000

    environment suddenly wander problem wealth

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
