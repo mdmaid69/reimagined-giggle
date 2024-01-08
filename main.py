0fbc41fb557d4995673e2908f3c44382b57ce2b4
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:10:45 +0000

    whatever mood normal pick official

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
