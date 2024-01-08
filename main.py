212dd07d3945f046a434820d80ba052edee280e8
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:09:02 +0000

    sell education direction party become

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
