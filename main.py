87d7fcdeef4d5783a7fcd2aa3e3886eaa1dbe489
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:48:43 +0000

    pinch likely right high escape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
