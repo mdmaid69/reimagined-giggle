250d0d30ee63acf898e308b47fdbd9f442c1a5ed
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:25:41 +0000

    stage subject bunch this frame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
