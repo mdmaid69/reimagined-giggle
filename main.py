2421ac1ed89edd40a5f5a0308f9db466017e4c24
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:29:32 +0000

    perfect follow people label bye

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
