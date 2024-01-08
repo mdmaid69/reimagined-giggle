e305f65070a73ca1cd1679b7f3a4a4382efab374
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:14:23 +0000

    hit through stand receive familiar

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
