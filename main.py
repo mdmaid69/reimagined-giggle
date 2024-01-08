4a98594482618d296c40ac4f8c1598d648c20c9f
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:47:18 +0000

    play introduction dangerous he admit

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def subtract_numbers(x, y):
+        return x - y
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
