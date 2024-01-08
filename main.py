0c3d17ea4217b679e611dadb36c5a33169b5aeaf
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:32:07 +0000

    draw threaten praise fork wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
