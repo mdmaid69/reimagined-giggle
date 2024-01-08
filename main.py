205a216f80c0850c3bb189483ecf06cafa9092c8
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:58:55 +0000

    broken fast bath maximum wide

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_typecode(array):
-        return array.typecode
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
