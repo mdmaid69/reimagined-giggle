ad90e08589271b214695f84fca2163dbda36fe7a
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:52:20 +0000

    bear most by talk silent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_acceleration(speed, time):
+        return speed / time
