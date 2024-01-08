7c10e40f6bd40563ae7a1565131a4808486d55cd
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:18:55 +0000

    settle welcome meanwhile die road

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import collections
-def create_user_list():
-        return collections.UserList()
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
