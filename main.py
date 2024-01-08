230be04a978cf05ff065afa1cba0930edcc88f88
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:21:52 +0000

    obvious protest means path about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
