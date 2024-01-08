6f9a9c70c42db0c825ecbdc6d998ccace77c767c
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:00:25 +0000

    trick offer rid wing ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+def print_python_version():
+        print(sys.version)
