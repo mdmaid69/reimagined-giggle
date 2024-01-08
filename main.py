c4fcaa26637c9073758520b43ef89f6e8ca62094
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:15:13 +0000

    free now which team game

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sys
+def print_python_version():
+        print(sys.version)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
