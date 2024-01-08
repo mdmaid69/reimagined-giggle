799d2ae20fd8549f6a439597d201c467f87d9a94
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:34:39 +0000

    lead though interview table arrive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
