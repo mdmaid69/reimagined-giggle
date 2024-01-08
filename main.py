d60d809ba0a3e1de332f6669086f0018bfc9a814
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:28:21 +0000

    equipment question impression opinion warning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
