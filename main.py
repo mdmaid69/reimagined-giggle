73877a4530afc313aaa1c2a89d36ecfd2ea0984e
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:41:33 +0000

    share occasional twist dream window

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
