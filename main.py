43f59b92dc35bc2e9e185aa020892e7ee62d039b
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:24:52 +0000

    want trip guess deal there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+print(sys.version)
