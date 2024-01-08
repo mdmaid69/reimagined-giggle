8346fb12638fbdc2ded0c170067cabe6ad2da303
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:48:21 +0000

    responsible separate tight tune use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
