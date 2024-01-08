a73fca978ad0176f778e2f3e1ec3bec6b7ee55ab
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:20:54 +0000

    reply relief fortunately pour listenfor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import array
+def append_to_array(array, item):
+        array.append(item)
+import os
+def change_working_directory(path):
+        os.chdir(path)
