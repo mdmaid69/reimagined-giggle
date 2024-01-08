88a124b19df1e4afc86b08940e71a42581096a55
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:54:14 +0000

    full pollution last soil fasten

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
