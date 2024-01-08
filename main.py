5f96779cd276231322551e2d7c173ec1fdb7902d
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:22:48 +0000

    remain damp target difficulty compete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  def remove_duplicates(lst):
+        return list(set(lst))
