9436cb6859893509451d1345e91f294bc7674797
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:00:00 +0000

    tense where still company spray

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def convert_to_binary(n):
+        return bin(n)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
