00e5f4cb80e2ea925f17070270c11ff333b6a3e6
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:56:05 +0000

    nature accidentally mine rush standard

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def clear_array(array):
+        array *= 0
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
