1f2d86adb17c609f6afd186ab3dc7fb03b2051f3
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:05:56 +0000

    urgent usual extremely concerned punishment

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def convert_array_to_list(array):
+        return array.tolist()
