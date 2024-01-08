7809c263b0c71b39754eb00f06541d8c6d05ba7d
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:26:57 +0000

    care round write dish cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
