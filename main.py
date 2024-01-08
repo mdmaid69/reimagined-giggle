f6b916af461735a577a6b5e53995f50dd19cdd37
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:57:22 +0000

    disturb catch this combine fire

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
