f33731c8f68fe23c6e475cbd5c9b687a075d4d49
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:45:43 +0000

    border area kind image bet

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import sys
-def print_python_version():
-        return sys.version
+def cube_number(x):
+        return x**3
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
