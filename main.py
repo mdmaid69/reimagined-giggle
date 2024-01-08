09eb97a3c04c12be55e4438c3e48b9fb1615fbdf
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:34:32 +0000

    makeinto comeback lookback(on) pattern regional

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
