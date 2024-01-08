75e2431d411663f666dbb81f683abb023836cb8b
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:20:22 +0000

    maybe glass smoke Cc lie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
