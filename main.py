29be94468d06e4bfd519a0c4bd82a58d8b70a81a
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:15:52 +0000

    will journey drop well hit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def convert_to_binary(n):
+        return bin(n)
