ee6d74a6147ea9048eefb84752d54b446131533a
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:47:26 +0000

    plenty treat watch after own

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
