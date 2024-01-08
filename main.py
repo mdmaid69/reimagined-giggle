876945c1d495fabd08489a63c6b12828ee4d4a0b
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:55:43 +0000

    accidentally memory protection professional punish

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_power(work, time):
-        return work / time
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
