9d32478bf26fd3e0c3e322c9ab2102d1f65d1094
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:29:13 +0000

    noone science will favour doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
