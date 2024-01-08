103519f7ecf613181ec2c4bcc4a3b12cc18f302a
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:20:33 +0000

    paper concentrate can contrast clever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
