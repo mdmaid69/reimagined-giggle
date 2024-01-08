5a1e0a830bc4b2ffc207caa2cba78b4e17b14a5a
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:06:21 +0000

    comeout run unusual even talk

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def convert_to_octal(n):
+        return oct(n)
