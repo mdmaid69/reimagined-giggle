0b744c499fc7d4de1f41282f7ac13e44c6b15776
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:11:03 +0000

    suddenly record fork before possibly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
