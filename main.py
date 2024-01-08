436af2762be71afe4324ceb508b740ea5dda4fc3
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:24:52 +0000

    damage scream view crazy turnaway

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def square_number(x):
-        return x**2
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
