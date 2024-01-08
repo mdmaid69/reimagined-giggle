8923d18d044a0d881287a39461ce9a75ee54eb81
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:09:26 +0000

    hit life opposite advantage around

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def reverse_array(array):
-        array.reverse()
+def sort_numbers(numbers):
+        return sorted(numbers)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
