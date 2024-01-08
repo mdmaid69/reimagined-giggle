3b680270baf63a122089a11750edc371fb404c6f
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:57:17 +0000

    spare really section bank difficulty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
