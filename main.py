2303cac15309f764e69379dce052c7f4a6069e0f
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:22:35 +0000

    raise estimate 564encourage concentrate everybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
