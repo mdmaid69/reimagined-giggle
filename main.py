a6cfd71f6e3c898c7b9eedadba2730e10322858a
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:39:21 +0000

    glass exact boring workupto blame

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import getpass
+def get_username():
+        return getpass.getuser()
