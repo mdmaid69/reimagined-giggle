59a998172fe6e62dfd31aa5dffca8bab8979a940
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:15:36 +0000

    imagine dinner attempt hit nice

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import collections
-def create_stack():
-        return collections.deque()
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
