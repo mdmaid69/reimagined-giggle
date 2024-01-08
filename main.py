59c8c12ef8706c9e7215aa54b427e49e830d944e
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:39:21 +0000

    yesterday flow emphasis quantity century

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
