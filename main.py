f1a8f9506b7518ee26758d40ea18854ccc04ac51
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:37:06 +0000

    letter flat police almost fierce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
