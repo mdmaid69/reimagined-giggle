799362688c6215b6028eda934063154f47bcfc41
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:42:56 +0000

    shut surface surprise satisfactory difficulty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 text = "Hello, world!"
-print("Uppercase:", text.upper())
+print("Reversed:", text[::-1])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
