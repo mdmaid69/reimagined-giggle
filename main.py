3a7e763d27bc71e5f53fc00b6af1d845381cc31d
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:42:18 +0000

    involve introduction acceptable male hello

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
