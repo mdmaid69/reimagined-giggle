58c8b61d3a6ea23a56bdb8851c254ec556af6a1e
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:21:56 +0000

    rarely press trick original certain

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
