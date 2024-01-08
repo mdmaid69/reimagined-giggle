54a01a149a489d0b22dffa8d70338cc1448a4750
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:08:44 +0000

    nature wide accidentally few sea

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
