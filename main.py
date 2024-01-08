00cc27d40f90f842c932a5753511a98535a483c6
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:19:04 +0000

    trade union chat scheme suppose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
