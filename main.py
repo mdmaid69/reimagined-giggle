3135f35af7753df137c33db5c50b2ad8d90851f9
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:40:00 +0000

    nerve song talk distant include

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  def calculate_factorial(n):
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_factorial(n):
         return 1 if n == 0 else n * calculate_factorial(n-1)
