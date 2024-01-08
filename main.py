5565bcbfc8b30bd501c062406dda565a4425d250
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:59:20 +0000

    slide swimming slight gift provide

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def convert_to_octal(n):
-        return oct(n)
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
