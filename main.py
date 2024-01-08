170f48c21eb1293ea1392b4772233e2405913c80
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:02:08 +0000

    follow neat underneath back alone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
