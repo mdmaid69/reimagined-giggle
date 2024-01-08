d7b5d9a1d10802ed414b41023fbeab912743185d
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:33:53 +0000

    ill sink niggaacademy polite move

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
 numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+print("Max:", max(numbers))
