3a78e8745776a29c504720ae01698440317797bf
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:52:11 +0000

    bang risk lead quit exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
