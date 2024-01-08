65066dc102094b24abc12d54b0c9165b899ab694
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:24:29 +0000

    dare stage scare him poison

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
