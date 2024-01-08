6a23781796d01d442a91e3d72d75b1548d61e7f6
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:45:14 +0000

    modern little life refer base

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import math
+def calculate_floor(x):
+        return math.floor(x)
