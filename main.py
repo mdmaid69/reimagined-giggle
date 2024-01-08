d30e6512fa3eed021919b208139a13aa45b3747b
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:12:17 +0000

    Mm especially tour need may

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def find_min(numbers):
+        return min(numbers)
+def find_max(numbers):
+        return max(numbers)
