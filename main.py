b99d3ede441eae6755924fb2840707c7f666dfe7
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:36:17 +0000

    back soft market except street

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
