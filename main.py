e02eea79e8b329ebfcd6537e98b9ccbe8042154a
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:54:34 +0000

    false tip promise section hand

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def add_numbers(x, y):
+        return x + y
