b9ebe457afad201dae855076a4e9374f06786d26
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:41:59 +0000

    doctor hurt conduct comfortable strange

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
