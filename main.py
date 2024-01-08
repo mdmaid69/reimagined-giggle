60fb6201c385891be360a99c13cd254b04ca719a
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:37:20 +0000

    dead comfortable bridge opponent thought

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_odd(n):
+        return n % 2 != 0
