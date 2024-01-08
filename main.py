3f0e33d8de4d3f5328eb094cdb79c8bea7c15898
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:20:45 +0000

    572enter birthday storm exaggerate example

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import time
+def get_current_time():
+        return time.time()
