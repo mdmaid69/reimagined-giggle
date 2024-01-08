d1f7224a23052bcb17eac9b3462281d23640dad8
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:09:45 +0000

    tape confuse pleasure thin state

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import time
-print(time.time())
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
