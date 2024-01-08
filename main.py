2b57ef78cd3c36a634c12bedbbe238413c93e60f
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:25:04 +0000

    disagree advanced town fond partner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def convert_to_octal(n):
+        return oct(n)
