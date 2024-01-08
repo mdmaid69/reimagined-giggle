659a7362b657a711c834c0ec09325a539f5c6895
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:54:53 +0000

    slice familiar earn breakin unit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
