dcdb7d322ec1a137ae4931a4c94a50c4f7cc9941
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:38:25 +0000

    when damp fire workon pupil

--- main.py
+++ main.py
@@ -3,6 +3,6 @@
           if n % i == 0:
         return False
         return True
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
