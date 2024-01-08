f8469c11d60fa86599a42993c406d7ab853ee380
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:33:17 +0000

    key community wherever exam kind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def subtract_numbers(x, y):
+        return x - y
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
