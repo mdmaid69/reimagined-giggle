31d401c478dd4e1585d9ecad534ec4d8a2c71e69
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:10:19 +0000

    kind instruction weak fire blow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
