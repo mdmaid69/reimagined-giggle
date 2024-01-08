02d3eb0b8c351a97aa9b33b714c1c244e643e335
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:33:00 +0000

    base computer pride important exist

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_work(force, distance):
-        return force * distance
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
