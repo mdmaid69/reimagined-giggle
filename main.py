8b1e53503d6ef61748aec796c8f5e2bbe6489016
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:53:10 +0000

    cover national also power silent

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
