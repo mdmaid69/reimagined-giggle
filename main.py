2a91e2a0e8149c1263d61fd9b1ee7dd3ba4a05b5
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:05:54 +0000

    sweet classroom minimum treat load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def square_number(x):
-        return x**2
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
