b930e991c8de8d12a7e92571692323412d23b4e1
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:14:04 +0000

    since disagree manner building change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
