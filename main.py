68c47b981413bfb978779798ade05d0252934d65
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:54:30 +0000

    operation bread worry deaf work

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
