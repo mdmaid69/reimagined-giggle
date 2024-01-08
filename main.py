9260671d766902697f7c639a805d5b8da19a2988
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:35:37 +0000

    practical keepaway slow lesson use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_as_tuple(array):
+        return tuple(array)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
