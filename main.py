4fb4c358fb3edb1413768d5e27d3d071a10ee188
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:40:14 +0000

    scream climb holdout next issue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import os
+def change_working_directory(path):
+        os.chdir(path)
