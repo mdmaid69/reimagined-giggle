2b4624e34bcbffaa9d56bcc7f73dfc51c0881c30
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:29:38 +0000

    talkinto vision trip send complain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def get_array_as_bool(array):
+        return bool(array)
