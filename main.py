e2a42d143c1eb7e25d128ce2af85c7d57edd67b2
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:21:00 +0000

    detail score part choice charge

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def get_current_working_directory():
+        return os.getcwd()
