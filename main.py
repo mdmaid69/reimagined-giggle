42bbd01372a2e7e0a8dfb743e9f96e33ea46171e
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:37:25 +0000

    free 564encourage satisfied shelter music

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
