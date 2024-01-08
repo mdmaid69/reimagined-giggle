bf129934f3a2d54fee9a3db3adabcb8c11c7a3f8
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:15:31 +0000

    cry put mind suddenly give

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
