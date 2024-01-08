4c2d3ac4584b05ec94620ab4dc26a33851f89ddb
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:09:22 +0000

    than interview run bag machine

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import time
+def get_current_time():
+        return time.ctime()
