675e4bee16c4d9e4d01fc3e838edfd29221a5853
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:57:23 +0000

    guilty happen clearly Hh wash

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
