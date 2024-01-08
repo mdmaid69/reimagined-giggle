84454e07ddfd29bf7469c279896236a1fc01ce14
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:51:52 +0000

    in stop one typical mother

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
