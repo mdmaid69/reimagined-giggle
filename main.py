005f4f8fc7b704f76f67cc4be654faaf178cce71
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:29:08 +0000

    dick date weekend matter search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
