bd5859dc278815f79415330ff1af23b1fb5d6c13
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:31:42 +0000

    earth swallow store belong message

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
