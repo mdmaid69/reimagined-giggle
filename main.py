4ea7bd328a8b151fddf5ce01c852264b42e9a924
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:07:42 +0000

    comeout hardly uncertain plate past

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
