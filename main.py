6b5ee146fd138f7b87a0b1fee50cd6d455914980
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:48:07 +0000

    broad ilovedicks science mixture waste

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
