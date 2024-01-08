dfe45003c5e9fd756fad9a2577809aee9a57bb9a
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:16:50 +0000

    deliberately hear ball view order

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-print([x**2 for x in range(10)])
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
