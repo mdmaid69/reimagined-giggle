ac1dcd3498168342bb96eb3a2b005a74558b93d8
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:09:07 +0000

    except learn sensitive convinced Zz

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def cube_number(x):
+        return x**3
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
