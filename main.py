f7a0aff13b9e398e1313eff3950c4cafd66088e5
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:34:02 +0000

    beyond doubt entrance extremely 566end

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+x = 10
+y = 20
+print("Sum:", x + y)
