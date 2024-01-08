7680b4019595550edf581ce2f23e3082384de311
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:03:07 +0000

    finger prevent better keep offence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def check_if_array_contains_item(array, item):
+        return item in array
