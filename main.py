9a0f38c66e804029c9cd4230af4f6ffa44033470
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:28:08 +0000

    question sticky apparently base blind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
