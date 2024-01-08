ad0af56b49003611e063c987912bcf68b5a371d6
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:31:58 +0000

    fit dance characteristic brush himself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
