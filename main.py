5be5d7538ac99aa3754d8d0dc5a930452505dd5c
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:16:49 +0000

    roll callup flood characteristic characteristic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
