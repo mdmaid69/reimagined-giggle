1ed8cb7ddabbed5cd3e4e5791d71655b0c89cad0
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:49:02 +0000

    beginning immediately getback Rr earth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
