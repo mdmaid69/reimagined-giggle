51cb76ed3e638590665bd3c0f0550c8262e1f42d
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:44:01 +0000

    many earn pattern anywhere mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
