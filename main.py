a93768da598fe53bb318e35df8c01340e7ce58fe
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:15:37 +0000

    chat soak hard concentrate my

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
