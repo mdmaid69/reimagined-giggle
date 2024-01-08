32f53a5f5bb150c35219f717f26c8549fcbff08d
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:04:56 +0000

    shop sale degree come Uu

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import datetime
-print(datetime.datetime.now())
-import collections
-def create_priority_queue():
-        return collections.deque()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
