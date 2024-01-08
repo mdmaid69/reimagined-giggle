effcf29d6d08cded542b41e0edeff808e0624437
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:10:58 +0000

    press mark fierce convenient trial

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_speed(distance, time):
+        return distance / time
