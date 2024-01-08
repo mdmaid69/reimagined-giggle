7741e806aaa314f092edbc2c30ff0031f87e1742
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:36:13 +0000

    smoke rob yours talkinto workupto

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import tensorflow as tf
+print(tf.__version__)
