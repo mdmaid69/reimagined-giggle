846bcb3cd634c121659d2ed380ff56da9107ac27
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:52:24 +0000

    almost this walk ocean nature

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
+print(sum(range(10)))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
