0ac5b35c3aa54fc551a6db2b965acecc4b6893d2
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:01:43 +0000

    new cook one quickly describe

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def find_union(list1, list2):
+        return set(list1) | set(list2)
