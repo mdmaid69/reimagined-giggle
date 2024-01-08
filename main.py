34add0ec338d3523205f8310fff20e6027a13a97
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:07:01 +0000

    knock development night sort snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
 import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
