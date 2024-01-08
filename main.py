1c16e591847d45c3acc0a02d7a776764ab3e6d4b
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:16:33 +0000

    opposite survive skill probably unlucky

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
