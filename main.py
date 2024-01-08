2022ee8a8716a87ea0780fff23ff95d92009bef7
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:11:49 +0000

    unless getback piece attractive thirsty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
