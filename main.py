1cbc1518aebefb95e69183441d0694885e7ef8f4
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:18:27 +0000

    depend rub building once thirsty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import array
+def get_array_typecode(array):
+        return array.typecode
