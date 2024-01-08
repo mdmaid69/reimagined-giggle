a30dafd602e66a33c9d64163265bc59a1783845c
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:56:55 +0000

    here talkoutof excuse ago influence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
