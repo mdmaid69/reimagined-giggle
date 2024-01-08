3786193a74b1108ab59440f0ebf5409db5cb0b03
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:37:53 +0000

    trap goout show seem bury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_base_name(path):
+        return os.path.basename(path)
