68aaf5ababb473526e01fbb1851aece84a4cf787
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:52:32 +0000

    than whom funny firm practical

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
