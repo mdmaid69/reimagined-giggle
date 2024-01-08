7e2a32d0cdee7a50215379ebf0d9aedd16914a2a
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:06:00 +0000

    wish ocean tongue sound whatever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import numpy as np
+print(np.array([1, 2, 3]))
