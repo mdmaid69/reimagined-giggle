0635268048f72ce9b63726b914f68e1e800700a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:49:03 +0000

    habit here gradually appeal side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
