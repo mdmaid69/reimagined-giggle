db9a45154cc5895e06ad53e02b60ee3817db10d0
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:18:50 +0000

    each issue effort check cheek

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_slice(array, i, j):
+        return array[i:j]
