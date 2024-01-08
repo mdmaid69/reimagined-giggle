71151a90f0093c78d5b2ae953274c8706ce1f7de
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:37:42 +0000

    debt before means confused male

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def count_elements(lst):
-        return len(lst)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
