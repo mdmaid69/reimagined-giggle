7331592cf7bfdf3f55ab0b13491517dd48b4feb4
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:15:22 +0000

    mess behave soft on university

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
