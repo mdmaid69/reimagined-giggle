f1bbf0ac4280ab216ef55afd98a634dbf34233cb
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:57:39 +0000

    television closed Ss freedom space

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_volume(length, width, height):
+        return length * width * height
