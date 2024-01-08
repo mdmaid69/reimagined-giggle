f0d28fe7cbf3564f6721c4271f5d2cac4e0134e4
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:53:07 +0000

    cover basically couple cum hide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
