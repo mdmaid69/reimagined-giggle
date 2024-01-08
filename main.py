fad89dd890147c05cd98f59d1554d4393b4c1380
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:46:51 +0000

    send mass type paper chin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def add_numbers(x, y):
+        return x + y
