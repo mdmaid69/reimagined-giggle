930280804d517b11c4b2205f1f0fbc7a0a923805
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:35:21 +0000

    urgent point left later flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
