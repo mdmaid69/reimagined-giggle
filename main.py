3fe52fe08a2397787de5224953d3b82cb75e927c
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:12:56 +0000

    gooff slowly children confuse cover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_pressure(force, area):
+        return force / area
