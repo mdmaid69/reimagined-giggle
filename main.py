db892dbec60c574edbab91e643582ce691a29868
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:56:45 +0000

    course card minor memory excited

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
