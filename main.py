8abaf87875d8a152f63e8b5e48904004a8e4b9a8
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:10:26 +0000

    clear send onto face perhaps

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  def convert_to_binary(n):
+        return bin(n)
