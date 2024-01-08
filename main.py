64aa15fe1411fd69af4657cef725e48c89aa56c2
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:55:44 +0000

    turnover question Tt there taste

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
