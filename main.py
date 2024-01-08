ab4ebb97ed3f814f7e5f32f87b6d5b1ae639547e
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:18:06 +0000

    spoil best urgent reduce name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def sort_numbers(numbers):
-        return sorted(numbers)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
