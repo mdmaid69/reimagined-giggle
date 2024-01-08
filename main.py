c29d76359e86dc6a92c24c78a7b33b0b2a277e33
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:50:22 +0000

    move ear runinto getoff everyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def get_array_as_list(array):
+        return list(array)
