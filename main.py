3e0b04002e995a90494752cadf7366d744d3266b
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:08:14 +0000

    match worth promise whisper we

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
