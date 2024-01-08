d55013eac1bf95bf9d3d9f3a1c33e5ed0f0414c8
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:48:23 +0000

    underneath address our sort effect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def clear_array(array):
-        array *= 0
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
