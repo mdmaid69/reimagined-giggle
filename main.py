4f55dd8429d106162d86edbd8a2d358561ee2ab5
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:25:37 +0000

    treatment course store worth cross

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import math
-def calculate_floor(x):
-        return math.floor(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
