fbfad7c1f01a779c6aad93f264d1d783cc5f99e5
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:49:31 +0000

    pair bridge passage offer add

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
