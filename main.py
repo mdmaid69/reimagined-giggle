8facc2dc948b7429c26fd77f15f4dfaf881c9e86
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:15:04 +0000

    shout slowly characteristic breath handle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
