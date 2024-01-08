8497986614527714347948e23146bc43dce46fa0
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:33:24 +0000

    steep separate failure pleasant regard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def delete_file(file_name):
+        os.remove(file_name)
