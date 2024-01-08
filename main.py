20e16a95bbd0b217d98b182b9efd0b851dbb66d4
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:20:17 +0000

    attractive land minimum flavour bear

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_power(work, time):
+        return work / time
