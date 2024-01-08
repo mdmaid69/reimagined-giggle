2a42c91d05236182c44987d82a7b2582930eaeec
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:15:04 +0000

    hardly kill far top matter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
