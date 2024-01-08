3c3365f48b46aa56943eb444f227b8b911b97f9b
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:35:32 +0000

    goup passenger comedown comeup blame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
