8781c59d14f7df9d85b8d51b14cbea760e4eeb8c
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:01:06 +0000

    complicated inquire grade sale guilty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_set(array):
+        return set(array)
