e70e954dda1df67dff71bd0522d463accfc67e47
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:39:09 +0000

    wash below sit below 572enter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
