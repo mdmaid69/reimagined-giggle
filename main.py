3472578e6e0f0d855ef8bf3cec593ef795f360fe
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:16:47 +0000

    scared stand shoulder offer deliberately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def cube_number(x):
+        return x**3
