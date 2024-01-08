e3180bf55b7ac34f3cf31aa265b7b51eaaac77a3
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:36:58 +0000

    knock public exaggerate anywayanyhow shape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
