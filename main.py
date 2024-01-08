7290eec3a5fef9bb23cb5a81423506a79b8f700a
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:15:29 +0000

    makeup temper job its deaf

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-name = "Python"
-print("Hello,", name)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
