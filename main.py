ee83ccf8947c7129cf2d4de4fe1ac71c5f8ad22b
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:49:45 +0000

    regional giveout should lunch date

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
   def plot_histogram(data, bins):
         plt.hist(data, bins=bins)
         plt.show()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
