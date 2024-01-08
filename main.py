b7ed379f1cbe2f5647f4fb5a825a3d6a72a2855f
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:02:35 +0000

    worth cheek tragedy shape dare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
