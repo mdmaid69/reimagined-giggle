92732911a57682350732f71738697d819a6e4f08
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:28:40 +0000

    high from concerned certain breath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_pressure(force, area):
+        return force / area
