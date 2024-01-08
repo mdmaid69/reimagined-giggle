dee70d212870428a3a44c32adec56c225c6aff19
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:17:10 +0000

    run view group object fasten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-def is_even(n):
-        return n % 2 == 0
+def get_array_as_list(array):
+        return list(array)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
