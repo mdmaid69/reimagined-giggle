33402b9e3a0eb3f57bd4da395db5371b283167ac
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:52:14 +0000

    no terrible measure work circle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
