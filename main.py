535bdae4521225c3208406fc9f227d4a4041321a
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:28:08 +0000

    far to never pocket bar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_as_repr(array):
+        return repr(array)
