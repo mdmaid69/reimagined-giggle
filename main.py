2f41f6bbc85efce6c8e3a7c4a1d0d4ffadd86f7f
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:07:23 +0000

    someone let string claim tough

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
