5803b48390d71c618b9097d27275899ad33b5407
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:11:31 +0000

    town pen passage record nurse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
