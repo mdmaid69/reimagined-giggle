0d8b70eb82218c185416b59d8a60828f9d15a775
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:42:18 +0000

    run extra Itself silent memory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_area(radius):
+        return 3.14 * radius * radius
