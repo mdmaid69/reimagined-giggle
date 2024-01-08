70eb03a0eabd7d5596e3bdfad5414338fc6118d6
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:06:03 +0000

    side soak suffer please share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
