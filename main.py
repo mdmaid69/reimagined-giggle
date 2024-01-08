f578880808e8cdf1f2eee3eae5026495e2b7ff7b
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:53:05 +0000

    somebody unite read instead tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
