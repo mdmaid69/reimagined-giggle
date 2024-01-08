372696192d1eefa4f37bec9b6afadf471a2f5c60
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:19:27 +0000

    rank distance heat goup original

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
