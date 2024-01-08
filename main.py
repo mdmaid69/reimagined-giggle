b4ee2d9a008381c65f3b33d151065c7b248489d5
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:37:59 +0000

    help tap although favour keepup

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def cube_number(x):
+        return x**3
