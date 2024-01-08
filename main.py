cab2a86bbb06a7012b98b71748b69d1920f8e2c4
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:34:11 +0000

    top minimum each holdout band

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-print([x**2 for x in range(10)])
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
