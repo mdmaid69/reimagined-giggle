a3be0cf2f4d4df3af2a9b8049e07372a55df20fc
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:59:03 +0000

    effort maintain direct scream aware

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
