9f2a2dc5b3a438b1c7a773da87de11bc25d944f1
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:21:25 +0000

    fact about swallow push shut

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
