1e68a55e7328c4fafe84962d368eb1d26647e02a
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:31:25 +0000

    sorry pot somehow judge major

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+name = "Python"
+print("Hello,", name)
