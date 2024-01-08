c0d3ef413bd4915d8f29a6e9a656cb3be70fd4c7
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:15:18 +0000

    loose touch match flood shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
