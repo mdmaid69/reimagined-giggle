ecba1bfe512ee768c96744f0c21aa58b5ce142f2
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:07:47 +0000

    band breakfast tap criticize afford

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
