721b746dd6b204af65de4985c63c52faae66a789
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:35:59 +0000

    maybe hard well drive praise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
