bc9cef7683d6f87ffcb0a3420ea8f65d0abc3664
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:26:06 +0000

    be put once neither study

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
