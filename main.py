95d3578e1b667d8ff2f56e1acc37ff492fd32f68
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:44:18 +0000

    performance badly mind spoil few

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_volume(length, width, height):
+        return length * width * height
+def calculate_density(mass, volume):
+        return mass / volume
