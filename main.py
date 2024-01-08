401a9ab635d19b62f9c7eefb4e47e62584e506d2
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:06:03 +0000

    sweep lower meeting common eye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def convert_to_octal(n):
+        return oct(n)
