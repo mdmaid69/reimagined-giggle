ffb3295d119d137429bdaa37b87f8e59cbd6a949
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:42:50 +0000

    call start progress punish impressed

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
