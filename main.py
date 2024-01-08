f52d71137083415eab2c039eb3b9fe18b47e2f87
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:38:27 +0000

    passage complete indeed however record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
