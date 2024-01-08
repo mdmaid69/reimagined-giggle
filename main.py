9ce2ee58c60f46a077b59858d1b5cef718a6f447
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:08:11 +0000

    idea immediately print strike rely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def count_words(sentence):
+        return len(sentence.split())
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
