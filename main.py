0b8af640cc820ffd01830806e30ba4ad6371869f
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:58:53 +0000

    peace hurry extraordinary experiment sit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def is_odd(n):
+        return n % 2 != 0
