fa1384efbde248e47db368da70efdbecb9d869b7
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:32:28 +0000

    punish to fix quickly Ll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
