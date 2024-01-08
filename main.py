b3d7046581b04d8465997f4cb61bf52f85aadb23
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:45:15 +0000

    date themselves afford determined burn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+print(sum(range(10)))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_base_name(path):
+        return os.path.basename(path)
