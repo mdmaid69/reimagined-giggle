36c1ae962d4f48e5e16ea2535d96413a57b09a40
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:12:53 +0000

    include fun since objection up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
