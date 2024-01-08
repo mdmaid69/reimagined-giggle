5fa97c79d788f5ea0edbe9df4828750ea93eae79
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:39:30 +0000

    since supply expect fat takeaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def get_array_as_tuple(array):
+        return tuple(array)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
