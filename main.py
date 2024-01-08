49369926bddae1a7050c77d5c2a8e7eecd22e608
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:47:40 +0000

    lead already store operation degree

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def get_array_as_float(array):
+        return float(array[0])
+print(sum(range(10)))
