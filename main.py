90a6361f8da196a6e5b5f423c9286c27143bc337
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:58:36 +0000

    shout shoot onto prefer takeaway

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_typecode(array):
+        return array.typecode
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
