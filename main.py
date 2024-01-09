c0512569c11791c502622a5de1df34bd0ddf9ac7
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:30:02 +0000

    then shrink sink fasten confidence

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
