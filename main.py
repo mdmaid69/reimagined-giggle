f712fa09a08e91d4528be3b343954b6137559327
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:51:08 +0000

    night release waste normal slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_sign(x):
+        return math.copysign(1, x)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
