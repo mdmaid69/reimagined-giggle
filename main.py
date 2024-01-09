0fb235978da76da5ff2f08901bf24aff79685e29
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:01:15 +0000

    judge view nice offend fight

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
