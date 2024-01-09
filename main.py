94556fd940f86e246216d5cc9792186f5a556527
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:58:53 +0000

    whistle except could situation convincing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
