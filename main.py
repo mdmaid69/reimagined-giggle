986fe09b4e5526ac98c8244ccc6687608fdc087a
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:46:02 +0000

    fairly single comfortable bath risk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
   import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
