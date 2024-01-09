24641122008192bc0d25c54b34d0e2d1ebeb1620
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:00:35 +0000

    contact turnup mixture force uponon

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
