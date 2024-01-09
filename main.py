6190cd829b253f5d6352d3378c9fe690c4d59d55
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:15:18 +0000

    nothing design visit suck will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_index(array, item):
+        return array.index(item)
