69a872cbcca83689b4021fae1aad49cb954f2b89
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:03:34 +0000

    guess neither country pick comeout

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
