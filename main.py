5da763d14ca6846f7590a6f12d685f03158a610b
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:26:05 +0000

    middle history memory separate doctor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
