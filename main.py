46b9f47fa9f3ae44857ab8ddcd8decf457289c0b
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:00:21 +0000

    hurry regularly vanish wait independent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+for i in range(5):
+        print(i)
