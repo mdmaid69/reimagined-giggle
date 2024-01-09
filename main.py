ae6aab0e7332be9a65fbc0bc8b1f8460b27ca34c
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:25:16 +0000

    fresh scrap lay effort field

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
