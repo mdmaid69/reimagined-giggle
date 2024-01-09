5178e1eee619aaa506fc4f1dd89e1e968238235b
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:57:13 +0000

    cap determined fit comeoff concentration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
