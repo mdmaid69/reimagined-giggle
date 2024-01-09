b2a3a3e482dbbb93c0390982598c9838e4a160c8
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:21:13 +0000

    by keepout rush share wet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
