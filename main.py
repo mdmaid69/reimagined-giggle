ca402198450597997859b48e3bcb263cf712f131
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:40:45 +0000

    herself whenever beneath unkind course

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+x = 10
+y = 20
+print("Sum:", x + y)
