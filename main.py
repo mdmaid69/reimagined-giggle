91699978e64849736a725900a8cdf9e83d035438
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:27:32 +0000

    discover comeover gradually if turnoff

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import sys
-def print_python_version():
-        print(sys.version)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
