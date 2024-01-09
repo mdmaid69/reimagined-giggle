2cd349b70b9e5cda756170e8bc40dbc0fb3c6682
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:14:36 +0000

    reasonable unlucky late runinto phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
