50bfddf2f28b262c312642967bd07e642162e49c
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:07:32 +0000

    passage plan coat announce wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
