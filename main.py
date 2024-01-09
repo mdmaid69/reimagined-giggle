e75259730715a76543fc78f52e62baa0668f4ca3
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:01:11 +0000

    taste phone clever fairly talkback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
