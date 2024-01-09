a87aac3456596f81320fdad70008ad03b21979c2
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:03:16 +0000

    exciting watch limit dream short

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def cube_number(x):
+        return x**3
