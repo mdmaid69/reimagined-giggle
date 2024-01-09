7b65a46f48778c32afb575a2a416208d8bf3e999
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:01:23 +0000

    rely polite general high behaviour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
