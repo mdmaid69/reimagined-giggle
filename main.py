a8a8a511f6a5ccd4c7e89cff82fa17d0028a6f66
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:47:42 +0000

    wrong grow stone pot gate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_string_from_array(array):
+        return array.tobytes()
