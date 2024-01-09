b8664f46a40a0d72e26982ab15f3dcfa45ba46f0
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:12:43 +0000

    song consist money to shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def find_unique_words(sentence):
+        return set(sentence.split())
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_index(array, item):
+        return array.index(item)
