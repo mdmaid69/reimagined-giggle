71ea2177d79f0686d60ed7b8d87599dd50c2d650
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:31:08 +0000

    spend book fly spread company

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_os_info():
+        return platform.uname()
