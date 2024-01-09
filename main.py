79495f6828d90c067ccf669f09db337a9bf2860d
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:59:06 +0000

    something getback OKorokay visit address

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
