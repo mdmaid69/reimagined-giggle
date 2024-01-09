6682a5cdd9339a5130813c871a079d5777cd2e84
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:57:01 +0000

    majority contrast chase one measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
