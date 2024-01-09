e080cde984be586f0e4e8a1a67f21ecd81d0e11b
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:33:31 +0000

    bar centre job Pp one

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def add_numbers(x, y):
+        return x + y
