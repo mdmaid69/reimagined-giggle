32734f2a80d589dec4e3ae90600293076deec907
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:00:19 +0000

    properly official only habit later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
