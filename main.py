52042ccd4009d6e683e3c4caad5f579bc8f7046e
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:43:14 +0000

    glad angry comfort neither flavour

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
