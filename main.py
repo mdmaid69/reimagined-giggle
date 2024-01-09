8c480987426dfe609241f916df657bcdee1f027e
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:07:17 +0000

    godown reliable professional shrink or

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_power(work, time):
+        return work / time
+print([x**2 for x in range(10)])
