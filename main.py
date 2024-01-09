08d6a568296a07825a56e5240b4329822436f1e9
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:20:44 +0000

    throughout taste call slow say

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
