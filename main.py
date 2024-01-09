b6c71c5ac26f711d7a6046fb7f6a3cf46618e238
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:01:57 +0000

    treatment love busy win instruction

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
