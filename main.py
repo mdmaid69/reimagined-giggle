71b3f1d43dfa51201963858170901e75e73bf33f
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:15:07 +0000

    pattern makeinto hang month announce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def find_max(lst):
-        return max(lst)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
