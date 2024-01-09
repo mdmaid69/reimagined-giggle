9659004dae25a0707d055c068b2237694635a170
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:04:11 +0000

    bite refuse description who hair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
 import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
