aa6271fc78e909a85ea6f2074150a8f4ecaf4a9b
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:00:42 +0000

    secret read communication unite track

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
