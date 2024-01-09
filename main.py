6576e474933900e46609a1e2883bbc2c82959f08
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:01:40 +0000

    clue workout raw eat bringup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import time
+def get_current_time():
+        return time.time()
+import array
+def get_array_as_float(array):
+        return float(array[0])
