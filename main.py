6327eecb9352596d7544078483d7da83c11c8afb
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:24:05 +0000

    test situation trouble turnup manage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import os
+def remove_directory(path):
+        os.rmdir(path)
