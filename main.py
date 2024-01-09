ff8c079b5025be1dde591bb9f5c43240d76f8d50
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:29:49 +0000

    gather rescue prize read turnup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
