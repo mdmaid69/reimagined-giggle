bf7d60d5ea9b4e056d1ddf33044cd593b7ff3b7d
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:28:53 +0000

    rely generally listenfor advise century

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
