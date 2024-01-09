8936b98927c08a4f017bf08f96cb986b388e3e07
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:31:59 +0000

    film demonstrate occasion fast proper

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
