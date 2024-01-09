3cbfb23b22c95077d7fb597db6975edf9e86ad36
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:55:35 +0000

    healthy rent heart destroy reliable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
