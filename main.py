769e39d6c33ec4e1ad681e7cec404ea840de10dc
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:07:22 +0000

    care software regard hurry independent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_power(work, time):
+        return work / time
