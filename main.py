70977962cc4ca5f59c2d01ba1d23b49b552b069c
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:33:33 +0000

    black sleep get shop value

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
