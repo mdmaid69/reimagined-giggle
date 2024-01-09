5b0b80440028a0846794ab66ddf0fd5789aa533e
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:07:05 +0000

    test close free influence father

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
