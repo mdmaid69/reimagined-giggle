90137e5c62310c8e76f2399d64220ffe8fd6c0e9
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:42:28 +0000

    work area ask increase spray

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
