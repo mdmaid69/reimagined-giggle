c8ac66592060bca1a05e818d0e9f91180f47cfc9
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:55:18 +0000

    world ill brief slice like

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
