8ed1fcf527f7b203f1b35d26852289960caf294d
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:46:15 +0000

    understand Dd fast relationship month

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
