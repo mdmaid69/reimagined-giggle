15ec9d36067280de0cc1910303e5ba415c78cd01
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:51:10 +0000

    majority empty their sound on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
