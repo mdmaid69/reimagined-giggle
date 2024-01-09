00547c6f59a7feb829434188f9741a69ef5412a3
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:52:04 +0000

    pronunciation know demonstrate talkoutof your

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
