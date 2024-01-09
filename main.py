53d0a37384b391819423ada409a140afbc6c275a
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:03:39 +0000

    weak breakthrough unlucky chase public

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
