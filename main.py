c0f6d9fba4103a2ffc7cdb3d56166757f0bb34e0
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:58:25 +0000

    block sit personal rent stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
