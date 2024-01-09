c04c656506747d8c071b8314d5a2401ac218fc28
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:16:51 +0000

    pause read strike suffer guess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
