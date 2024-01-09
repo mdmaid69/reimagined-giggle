72faec028aafc5cbef395a18bc275494f20fd9dc
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:36:23 +0000

    smile grow dry payment detail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def subtract_numbers(x, y):
+        return x - y
