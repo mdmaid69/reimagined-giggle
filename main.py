3f6b194af1316c921e7f4ffafc61c89e82b4830d
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:14:26 +0000

    worth unhappy completely false meanwhile

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_acceleration(speed, time):
+        return speed / time
