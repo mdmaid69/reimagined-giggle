8d18cb917b356430ba3a6c01e7539a31e48044b1
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:03:27 +0000

    criticize pause solve person free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
