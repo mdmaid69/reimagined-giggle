d1a84b7670944c017f36052f8a8ead6eac2c6697
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:35:23 +0000

    deserve plenty father 570enjoyable count

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
