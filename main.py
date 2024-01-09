505306f34645df360c7e5308dbd6b2e3e8c66b35
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:20:12 +0000

    size giveup among sight boat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
