41801a58ec621de072e6c2e97c1ada712c19a775
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:49:58 +0000

    talkback Jj steam clearly same

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_acceleration(speed, time):
+        return speed / time
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
