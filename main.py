bdea49a5829bde8e90ee57e8d2cac2bfab938987
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:39:48 +0000

    desperate shop telephone joke unite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
