68e2ccfaa621178ba30bb7a1b73ed83175584e6f
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:21:33 +0000

    callout seat considering wall strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
