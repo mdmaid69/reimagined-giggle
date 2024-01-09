f24bf71056013daf7d088fcc552ff9461bed34ed
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:23:46 +0000

    immediate on want indeed watch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def subtract_numbers(x, y):
+        return x - y
+def find_union(list1, list2):
+        return set(list1) | set(list2)
