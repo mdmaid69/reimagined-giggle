f38b7877990f3fb2e112ec702a0c4533efbd30d0
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:50:20 +0000

    success spare satisfied already clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_index(array, item):
+        return array.index(item)
