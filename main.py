f2423f2417522b82ace0ee51b31efb879a7753f8
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:47:48 +0000

    excuse shape share substance last

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def square_number(x):
-        return x**2
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
