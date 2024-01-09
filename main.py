ba904483fac26404eccacc02d769e513a4944c4b
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:36:15 +0000

    drop consider surprise congratulate innocent

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
