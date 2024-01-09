193a688d18b60b54b42f8d603022b87347477ce4
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:12:57 +0000

    often bringup blue straight understand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
