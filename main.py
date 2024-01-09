9a9a23fd7e4d97dc8cf4c27795ee4d9adf21f37a
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:08:21 +0000

    shadow style chase section difficulty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def count_characters(sentence):
-        return len(sentence)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
