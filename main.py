009ae0261d0ac04d33f6431d09003a437b06c26e
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:48:36 +0000

    visit Cc kind connect ever

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+print([x**2 for x in range(10)])
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
