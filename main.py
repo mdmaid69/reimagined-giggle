4f9b637bed1fda7787ede7f7dc3bac20b3044f6f
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:08:55 +0000

    declaration where quantity want intelligent

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
