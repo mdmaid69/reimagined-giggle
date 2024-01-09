39b39c8e329fc288ca645541bfb94961a5926966
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:01:21 +0000

    responsibility big careless task interrupt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
