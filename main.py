92334d2ce5d1f78f73578e10c651a07495ec059b
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:45:50 +0000

    brave traffic boat total niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
