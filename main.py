092d471016b7a4a7f8b5a6701878fc298fcc8e9e
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:45:29 +0000

    572enter everywhere late so drive

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import array
-def get_list_from_array(array):
-        return array.tolist()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
