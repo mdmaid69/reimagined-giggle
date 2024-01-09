db4bd643c95e330a97088d8b3d60c1f9966e30ac
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:20:43 +0000

    straight tongue let port ill

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_work(force, distance):
+        return force * distance
