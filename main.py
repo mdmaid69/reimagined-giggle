d67495c8cd491cedc99c78932f9f855e7e00ab87
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:07:52 +0000

    record project runinto towards left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
