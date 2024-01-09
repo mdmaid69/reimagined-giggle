474d03a4c86c38c5a082e1ea212219c3fc6590aa
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:00:22 +0000

    grow add lower job free

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_power(work, time):
+        return work / time
