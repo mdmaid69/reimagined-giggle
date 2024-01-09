0cc656079a5aef408dff893fcab1a8620598921a
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:57:06 +0000

    turnup silent ring stick Ff

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
