8b636e5da031769c07f6f857b325bdc7b12a6df0
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:46:53 +0000

    sting night none sensitive hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_pressure(force, area):
+        return force / area
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
