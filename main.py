6ba2835fa421c0f94a8f8dc6d50b4861607fbbbc
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:49:29 +0000

    influence trousers special refer steep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
