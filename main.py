6a3f78deb667373df58b5bebaa7b0b376b71a6fb
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:29:15 +0000

    big future lonely doctor forward

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import time
-print(time.time())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
