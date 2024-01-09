021394bdf9ab5eebe9e515ca160cb07bd4b8a465
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:31:01 +0000

    search complete save clothes lost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
