1f6dae7892f3b8f5f3a41f966e42702aeddceb72
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:06:13 +0000

    door section later commit tap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import random
+print(random.randint(0, 100))
