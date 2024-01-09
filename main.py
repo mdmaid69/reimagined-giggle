2cba71f6d5579aaa6ea797d869424a2915f55d66
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:45:40 +0000

    turnout area this it level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+text = "Hello, world!"
+print("Words:", len(text.split()))
