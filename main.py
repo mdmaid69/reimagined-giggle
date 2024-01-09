0363106a00f439f7eb3f1905c01801b0cce3f2da
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:22:02 +0000

    comment already profit coffee forever

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_error_function(x):
+        return math.erf(x)
