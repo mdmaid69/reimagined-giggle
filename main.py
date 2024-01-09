11d92d8096c859963d06f48e794a8ca0dbf1af3d
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:56:00 +0000

    unfortunate pain their shoot hardly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
