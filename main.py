2a70563e07a2d287aeede05f599741e273e06462
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:04:05 +0000

    eye bringdown acceptable right foot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
