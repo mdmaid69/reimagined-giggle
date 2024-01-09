9889f627f0e66754481cf9cbe2866cdaccb42c0b
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:56:31 +0000

    mouth open bye myself term

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import os
-print(os.getcwd())
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def find_unique_words(sentence):
+        return set(sentence.split())
