ba9bd979778e8d011ed40f13ffa0d4994476d5d2
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:16:49 +0000

    confused house cheek possible ocean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
