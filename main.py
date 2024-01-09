a0a4778758ce1efc8cf7c35515b94b034182f050
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:33:49 +0000

    aim direction otherwise book ball

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
