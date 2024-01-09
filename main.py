7b6fbd43f0a03c68ef230dc48361b1334b8e8369
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:53:38 +0000

    worth constantly second represent future

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
