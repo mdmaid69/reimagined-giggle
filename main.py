2d0260e4bb06a49ce9cbc03cc90bb0b8eec0cdfb
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:47:12 +0000

    wealth back crisis anxious lead

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
