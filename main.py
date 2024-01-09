3be546c2e8db7395e923e5a377fb290da55c51be
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:53:19 +0000

    Ff behave from prize crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_speed(distance, time):
+        return distance / time
+import getpass
+def get_username():
+        return getpass.getuser()
