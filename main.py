256d41716e09f2b0d1aeb68556d41c6934db0dd8
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:18:03 +0000

    design action edge us interview

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
