1cf02cfd8a665d3321aad908cad9d57a084689c2
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:20:25 +0000

    near comment edge difficult rank

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_sine(x):
+        return math.sin(x)
