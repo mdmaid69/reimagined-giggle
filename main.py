54e663fdb3ebeefedafd1482895ed3a9c79835f0
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:53:13 +0000

    prevent very look back chief

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def multiply_numbers(x, y):
+        return x * y
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
