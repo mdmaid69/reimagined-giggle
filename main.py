584f7ecc1bd8417290583bbe29c029898c5b08c3
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:07:15 +0000

    slow smoke production gooff reply

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-print([x**2 for x in range(10)])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_counter():
+        return collections.Counter()
