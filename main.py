35f3e0a4d89cbe62cfd9add06c4c343bd453a567
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:08:57 +0000

    surprised really sting satisfied sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
   def get_absolute_path(file_name):
         return os.path.abspath(file_name)
