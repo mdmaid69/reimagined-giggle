0d8f0f4b21159c4b75f3810fcc573cb3cfdd438f
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:06:42 +0000

    Itself main suck light without

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import sys
-def print_python_version():
-        print(sys.version)
+def is_odd(n):
+        return n % 2 != 0
+def find_unique_words(sentence):
+        return set(sentence.split())
