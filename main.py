def20570e1c02e76a3cd22ee05ae36a5772fc9b9
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:13:47 +0000

    child front smile trust compare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
