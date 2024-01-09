6ee717cc4391b28badad580eee3237c0a8582ea9
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:58:48 +0000

    reach difficulty bye exist nothing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+def reverse_list(lst):
+        return lst[::-1]
