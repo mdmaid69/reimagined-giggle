28aab35c0e3f74b7f9cc4fa10579b1a496236381
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:34:14 +0000

    kind slow hot ball break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def create_queue():
+        return collections.deque()
