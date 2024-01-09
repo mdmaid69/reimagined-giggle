82fc18dbf2100d008ee80a8a1b088131026c231a
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:01:11 +0000

    result sale warm worry well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import platform
-def get_os_info():
-        return platform.uname()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
