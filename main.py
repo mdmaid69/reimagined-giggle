9a393bf47d7d6b18655b2ce40f2bb724d6dbb762
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:32:16 +0000

    game track noone cry nearly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def is_palindrome(s):
+        return s == s[::-1]
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
