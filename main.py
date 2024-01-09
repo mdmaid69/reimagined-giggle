e0785e996a42f2b352a1cf586748d87a29c92d16
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:46:13 +0000

    father pause pretty otherwise cinema

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
