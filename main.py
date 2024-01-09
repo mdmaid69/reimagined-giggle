cca39bc5adc8f56e03647d42597287bfa041961e
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:54:21 +0000

    trousers arrive tax emphasis meaning

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
