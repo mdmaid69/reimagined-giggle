661645a5ba0518164a79c61237539261c7354be7
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:26:33 +0000

    build doctor afford typical emphasize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
