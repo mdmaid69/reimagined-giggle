c382f55c1575b30719d12153c9c5160b7c3b7ccb
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:34:59 +0000

    hit note job subject wet

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
