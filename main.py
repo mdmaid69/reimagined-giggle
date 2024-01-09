293a79702f4955e5fcaae6793b4e3832c7c01473
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:26:48 +0000

    various little comein dare poor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
