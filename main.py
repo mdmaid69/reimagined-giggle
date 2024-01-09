60f5d20280dfa01539232144b5279863a66c4cf4
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:43:44 +0000

    although remain respect tap sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_density(mass, volume):
-        return mass / volume
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import math
+def calculate_exponential(x):
+        return math.exp(x)
