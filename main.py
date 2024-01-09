f65abe57e2963c34c5e8cc748c076d91454e7754
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:35:09 +0000

    cross about teach workupto within

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  def calculate_area_rectangle(l, w):
+        return l * w
