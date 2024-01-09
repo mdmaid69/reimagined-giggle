d0bdef845da920df6df37de91443899807f5fdb9
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:43:44 +0000

    neither admit loud tie slight

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
