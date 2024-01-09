90de693ec5d7fe74df1bc9a1c1ee69e251ab87d4
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:27:49 +0000

    offer find takeback however abroad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
