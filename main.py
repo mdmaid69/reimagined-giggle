cd32c0734514450e06cbfd153c381350137cca47
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:57:41 +0000

    expression realize pool giveout beginning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_area_rectangle(l, w):
-        return l * w
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def convert_array_to_string(array):
+        return array.tostring()
