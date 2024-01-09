d9fa85df97e7f294b7ee0dd4878c69124d15deee
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:33:49 +0000

    home imagine unfair rubbish keepaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
