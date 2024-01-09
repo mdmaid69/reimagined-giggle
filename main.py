ad47af7cd748e0061f4ac4e85794a453bb510f4e
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:41:26 +0000

    sense spill brown present exam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import numpy as np
+print(np.array([1, 2, 3]))
