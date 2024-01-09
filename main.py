d6d59e29a7b8c746c7950a791fbef53dfc4b1271
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:55:28 +0000

    hope crime likely hit prize

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_as_repr(array):
+        return repr(array)
