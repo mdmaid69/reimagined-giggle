f67a5d37ba2711e7a9bfdbb327d44d17b574ecba
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:15:42 +0000

    bag instrument silent who here

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+for i in range(10): print(i)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
