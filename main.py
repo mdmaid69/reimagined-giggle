71fa197d52de65278e676fbb794b13708dc695ad
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:37:05 +0000

    bear story yes preparation set

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def add_numbers(x, y):
-        return x + y
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
