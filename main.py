ee247e0040217539e93e7370479bd5819c9fbe90
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:29:02 +0000

    amount mouth burst scream lonely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
