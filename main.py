03cefef5a75d35ca8995381a154f52ceb9891d10
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:00:54 +0000

    train think trade side direct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def reverse_array(array):
+        array.reverse()
+  def square_number(x):
+        return x**2
