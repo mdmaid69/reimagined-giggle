9e50b22bdb220c9e6ccd1c897649b72abf83ec84
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:38:29 +0000

    ready necessary temper turnback Vv

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
