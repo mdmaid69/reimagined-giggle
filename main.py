a36ee4b6ac5856568b8cea53f7ba3504e1ef01b6
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:50:31 +0000

    whistle state than design dress

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def subtract_numbers(x, y):
+        return x - y
