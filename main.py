f01695a20e14a9aa9c98db977dd12ae1f36b1bd1
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:16:36 +0000

    warning easy just will off

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def convert_to_hex(n):
+        return hex(n)
