b97fdbf282747257e0d676cad950b477bcec8715
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:25:39 +0000

    various between runout closely round

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import platform
+def get_os_info():
+        return platform.uname()
