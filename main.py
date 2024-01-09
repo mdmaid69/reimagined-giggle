fd8b7bf21226d1938a01ca29a8c3276409b75b3a
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:04:34 +0000

    brain nervous appeal confident insist

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
