e01ce7062b09cf13a0e869a586df916f7fb46dc4
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:21:32 +0000

    consist tip voice recent apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
