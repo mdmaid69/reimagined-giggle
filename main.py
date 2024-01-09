f0b42f2a9320177f3e6793ebfd004d5638367416
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:11:15 +0000

    cross bill everyone smell survive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
