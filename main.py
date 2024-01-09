c2cfe85de5ae6a2c5e0e5383983736d67acd6bfb
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:50:21 +0000

    famous vote detail lost interrupt

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-print(random.randint(0, 100))
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_error_function(x):
+        return math.erf(x)
