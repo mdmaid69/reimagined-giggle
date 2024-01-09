387a986ba16e3224dd260e0c8d9cdb827f62041f
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:57:10 +0000

    level satisfactory bin exciting watch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
