a6f89cbac67b1d1d1e8a053971e693725b7261d5
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:36:52 +0000

    extra suppose piece curious human

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def is_even(n):
+        return n % 2 == 0
