2e55088c3445a1cf25b20fcaa4576a2391e483a5
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:03:35 +0000

    either blame extremely close minor

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+for i in range(10): print(i)
