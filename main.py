66a84c763035702eb4cc844bf0db7fbb75cfa49f
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:42:47 +0000

    press maximum busy learn wear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_max(lst):
+        return max(lst)
+def convert_to_octal(n):
+        return oct(n)
