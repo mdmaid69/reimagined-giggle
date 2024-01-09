df65ba9f6c4182604c1d5dad55a2852e70df831b
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:11:23 +0000

    show care list remain exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
