e2cee307be5396441c634ab7cccb1294d0821b9a
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:33:57 +0000

    judge word oil cruelty honest

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
