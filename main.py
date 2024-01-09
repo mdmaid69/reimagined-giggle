1b9f8abeb97c921920a90baef96f743875dbac22
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:10:37 +0000

    scheme however failure thank scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
