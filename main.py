3ebedb6442fc359d7abc5eab79fc6a25a51b8804
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:38:54 +0000

    soon trap speech laugh healthy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import getpass
+def get_username():
+        return getpass.getuser()
