7c75ff208842eb4c6f75df11f501b5caae490666
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:36:09 +0000

    sink snow slide pick disappear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
