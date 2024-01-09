6c0002147842afe012f221cad973b2931c1fa7f7
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:04:45 +0000

    taste moment nothing criticize succeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
