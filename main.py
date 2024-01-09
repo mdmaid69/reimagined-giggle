9f1e3c588b1c8c6fd9ea191aa3cd2bc99b0bd5ad
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:55:42 +0000

    unlikely cut rule his everybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
