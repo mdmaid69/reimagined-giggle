e31d6dfa9c1e06aa59a60d414fe7a1fdf75c7865
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:51:06 +0000

    stupid ring real create exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-x = 10
-y = 20
-print("Sum:", x + y)
+def create_queue():
+        return collections.deque()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
