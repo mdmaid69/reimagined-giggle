4b30a5869cc435acb88530b5e6aa17c13868219c
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:27:27 +0000

    weigh here reduction politics offend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
