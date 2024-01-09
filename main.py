75222830a14413527bf2280ab1366369cbe8af9b
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:09:42 +0000

    unfair but tomorrow Hh too

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_set(array):
+        return set(array)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
