c217a8441dec18875c17e1fde5be05ec9eae4c6e
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:31:13 +0000

    injury meal balance amount voice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
