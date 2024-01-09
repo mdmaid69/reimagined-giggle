22c7ca53b6eb1a0c302d1f82440fb711aa988bb3
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:58:51 +0000

    wonder new pool ring air

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
