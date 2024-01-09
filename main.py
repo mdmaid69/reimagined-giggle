2da0d55f30df66cf88a8ca069903bff5b2a43340
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:44:41 +0000

    sign field tongue swing approve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def reverse_list(lst):
+        return lst[::-1]
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_int(array):
+        return int(array[0])
