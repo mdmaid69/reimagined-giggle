880144340d2d05d8ca8f20a9f907ce5a59ac0c6d
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:42:26 +0000

    start distance fork on across

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import sys
+  def get_python_version():
+        return sys.version
