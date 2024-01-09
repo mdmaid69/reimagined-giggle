b137efa86107b6033cf8dbdb0863c12085bddee0
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:10:21 +0000

    exploration occasion bread fall woman

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_volume(length, width, height):
-        return length * width * height
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
