63bab1f2f0254306ae57e2c818df84fde30cead5
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:52:43 +0000

    allow trade list watch leg

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_as_str(array):
+        return str(array)
