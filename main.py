9406676c6f7622f8786f8e3e49581a5bb2d0869c
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:40:28 +0000

    interview needle uniform sight hungry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
