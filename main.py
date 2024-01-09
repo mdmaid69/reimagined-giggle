fa81a8d3bb1281a4a621d86d1bf0e566229e4314
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:31:00 +0000

    answer forget square anxious treat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
