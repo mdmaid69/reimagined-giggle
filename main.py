e5fdd2ca5ef2c99575cdbd2da6c0cc99452dce21
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:21:13 +0000

    total objection press happen travel

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
