fa43303a6645fb41405686b8c52a49476d2d7e98
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:22:19 +0000

    scale eager change impossible heavy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
