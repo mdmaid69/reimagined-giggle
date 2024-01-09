7a77d9ba59071859730ed8335511a1bace04e66e
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:07:47 +0000

    letter acceptable war drop bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
