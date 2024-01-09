2aaf4427c234dd34499d99a59a85d2ee96f8ffd2
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:02:03 +0000

    cough concentration otherwise sail but

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
