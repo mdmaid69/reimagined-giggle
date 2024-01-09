2e0e9f6232e383153953338742c5511a1d4277fa
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:09:42 +0000

    between influence love compete what

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-def multiply_numbers(x, y):
-        return x * y
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
