609f6a0cd3ded7c72a73a89aaf51be599dafff7d
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:49:29 +0000

    name excited offer depend approval

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
