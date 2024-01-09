229249cbed86b0cb44b313bd049bc480faa67043
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:04:21 +0000

    carry closed demand arm brief

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_volume(length, width, height):
+        return length * width * height
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
