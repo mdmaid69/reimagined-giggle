0d3c5ebe65e4c4336138c748d7f13e6d70345089
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:10:36 +0000

    custom fat straight through design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
