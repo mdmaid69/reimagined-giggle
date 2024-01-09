cff70bbd2f4c525c255b8b1b2d03c91b8b1af85b
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:02:30 +0000

    start obvious discussion sing guilty

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
+def get_array_from_file(filename, typecode):
         a = array.array(typecode)
-        a.fromunicode(unicode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
