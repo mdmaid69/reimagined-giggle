e0ed2942b921201a6e8edfc90bd0bce0d09893bd
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:02:38 +0000

    book aware physical call television

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def convert_to_binary(n):
+        return bin(n)
