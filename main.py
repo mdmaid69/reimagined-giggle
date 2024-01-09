51f26798297e3b699c9a8494e2f1a92bba067a73
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:29:48 +0000

    throughout rest lie us mood

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_as_str(array):
-        return str(array)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
