0d1906e431c42ef4b63b82e9c94d142704bf83d0
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:36:28 +0000

    minimum history thirsty ill top

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def add_numbers(x, y):
+        return x + y
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
