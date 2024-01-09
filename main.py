700c069c735920d6ad9641607e4ed965c4792516
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:48:55 +0000

    scare off previous tight cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
