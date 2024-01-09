6caad4837def4761f0a6b442d2cc2a595b892618
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:14:57 +0000

    takeoff later mind report vanish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def count_characters(sentence):
+        return len(sentence)
