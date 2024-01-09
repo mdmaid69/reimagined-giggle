2aa7590cb835e69a6d709514b234acfa26e5ab74
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:43:12 +0000

    police late partner mad change

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
