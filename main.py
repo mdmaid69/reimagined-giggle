e54099d846e2e7a19242b12a60962ac7ca508970
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:15:11 +0000

    realize dig fire rare innocent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
