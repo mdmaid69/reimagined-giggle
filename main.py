2be94b2758bdef2907476dffcd3eccf8690eeea3
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:13:49 +0000

    serve position how part across

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def count_elements(lst):
+        return len(lst)
+  def convert_to_octal(n):
+        return oct(n)
