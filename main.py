c90233ba645cf42c33eb26f1e30be5d23a4f3db1
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:19:54 +0000

    capital section loss centre bill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
