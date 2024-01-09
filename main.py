9f3ac0db1d1ccf3c6d8170c5f1ba9a20643fd615
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:04:08 +0000

    cross wake thinkthrough shall spare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
