319e32dd24f049e36edd592e77162cba8a60aa08
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:56:51 +0000

    compete scrap down poor Kk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
