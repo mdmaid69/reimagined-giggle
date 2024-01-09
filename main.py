b82d427c36d1eea6c580e35bf03c8015d533a1fa
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:36:40 +0000

    especially weak short influence opportunity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_pressure(force, area):
+        return force / area
