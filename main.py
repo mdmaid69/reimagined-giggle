eafea5940b70153b1ccaa63a3102b49d1da5a443
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:00:18 +0000

    fast war however pity put

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def reverse_array(array):
+        array.reverse()
