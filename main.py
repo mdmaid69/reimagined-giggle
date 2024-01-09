eaed676634d1d1e5aec8afb6343fd930f820901c
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:16:21 +0000

    mean meet characteristic pot spray

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def is_palindrome(s):
-        return s == s[::-1]
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
