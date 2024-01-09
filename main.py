6a62390528eab297b016cc58f7ed88147a6ae346
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:37:15 +0000

    anyone importance pleasure fun bill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_counter():
+        return collections.Counter()
