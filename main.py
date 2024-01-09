da2dd022a7895576c7986919edd62ca83c740833
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:45:29 +0000

    could brush anywayanyhow lend world

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
