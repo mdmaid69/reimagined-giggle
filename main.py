d7eaa67277b9cb178bfb26119404ed1da76ab700
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:39:08 +0000

    start change shoulder suppose gift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
