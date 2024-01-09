a9ea1bb775b4331bdddb0578235ecb4883154275
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:29:56 +0000

    blame each foreign rubbish pretty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
