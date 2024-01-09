6a46e0a6459df342260d9d72a2ecc67fea5ccba1
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:47:17 +0000

    sound insist such far excuse

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def subtract_numbers(x, y):
-        return x - y
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def convert_array_to_list(array):
+        return array.tolist()
