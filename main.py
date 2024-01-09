874a5590dddf150c271fff452d138c6014d391c5
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:22:40 +0000

    train regular slow open extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def split_path(path):
+        return os.path.split(path)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
