2f2d2cdf1bca9aecba5ef60fd84f1a1f69003ede
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:44:26 +0000

    against sure accordingto variety estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_current_directory():
+        return os.getcwd()
