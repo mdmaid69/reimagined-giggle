9040e491f56b54029ac9cd8cbfd111622c835fd3
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:19:28 +0000

    talkinto will church urgent 179bite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_queue():
+        return collections.deque()
