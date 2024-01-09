89a9f43e86d600dc064b8ef0a2fa26ce95856637
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:47:35 +0000

    apart healthy delicate string godown

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
