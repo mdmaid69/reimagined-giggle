ac00cec5b975318d095d32cd17ba14d4a642297a
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:07:58 +0000

    star convenient turnoff lookaround instant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
