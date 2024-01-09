42db6272f08ce23aa3d697d0b124b3466c912770
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:38:57 +0000

    reply turn snow recent you

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
