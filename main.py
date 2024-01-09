1be65d95057f844e682d0fb15edb281cf9c5977d
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:25:09 +0000

    right several murder Gg interview

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-print(os.getcwd())
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
