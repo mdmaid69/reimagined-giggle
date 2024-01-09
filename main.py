4b93f3a68905fe376b5f4e9b6fd36d72727ba357
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:18:52 +0000

    fair furniture fair wind poison

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
