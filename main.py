b88bf6da3cd7e113048955635f98d69886dcf8ce
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:03:36 +0000

    gather do opportunity popular reply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
