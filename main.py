5018b73445d12d18fd51a1a49eca1ab7570db74e
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:13:40 +0000

    everything official shopping likely round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sys
+print(sys.version)
