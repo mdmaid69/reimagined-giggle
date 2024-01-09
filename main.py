fcfe7cc52c7039c8f0da43d4ead9edfdaef9b5cb
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:54:20 +0000

    shine course half large drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def get_array_as_str(array):
-        return str(array)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
