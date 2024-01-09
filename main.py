f74869b5bbc154f7716702feeb981ebf21fc7565
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:22:20 +0000

    short search everything advantage estimate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def sort_numbers(numbers):
-        return sorted(numbers)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
