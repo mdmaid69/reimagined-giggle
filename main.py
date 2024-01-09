b5e2e92d35f3cbb499c150aa216358e75736c5e0
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:48:10 +0000

    pretty rubbish bar already people

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def reverse_string(s):
-        return s[::-1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_list_from_array(array):
+        return array.tolist()
