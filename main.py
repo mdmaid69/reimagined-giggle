a0b1bd86943b2f3b09175dbc53f1fa305b1d272d
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:24:13 +0000

    sign injury benefit comment anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
