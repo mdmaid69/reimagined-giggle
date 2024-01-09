05bd9fa0108b8c8c8eb7910215d9aab30ced2197
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:00:48 +0000

    popular regularly immediately soon most

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import array
+def append_to_array(array, item):
+        array.append(item)
 import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
