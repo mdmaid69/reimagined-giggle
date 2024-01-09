4af7f0bb77b08cd877adaf9512f67b5c21173be2
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:39:30 +0000

    foot count wind welcome never

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
