0454c5f07a3d1ce64db7f4f602ed1cef4150a289
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:50:42 +0000

    drink someone reach could Ff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import getpass
+def get_username():
+        return getpass.getuser()
