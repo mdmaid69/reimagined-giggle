ab5c78b07e14d8e0548b815f817874dc621d5de6
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:13:46 +0000

    spoil card brief contribute Ss

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
