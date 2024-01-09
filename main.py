2204fd6f54ee73dcc89e8399f9e7041fb5c3e058
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:46:56 +0000

    side refer comeoff extraordinary good

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
