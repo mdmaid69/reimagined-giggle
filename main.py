09416641b76e01079b92d4dabcd64e82f12ce8a6
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:25:01 +0000

    but run mystery enthusiastic usually

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def is_even(n):
+        return n % 2 == 0
