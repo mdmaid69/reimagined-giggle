cfa0682f0bcd3a94bde5c8872255663b32b99664
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:08:45 +0000

    fork tour system patient tour

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
