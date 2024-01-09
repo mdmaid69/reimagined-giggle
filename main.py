b9de7a2346d43243881e2c68dca1ab6b3f0e289c
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:14:54 +0000

    ought place complete welcome field

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
