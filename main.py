e0f664f81ac60b199e136364ca33e48e25f55a1e
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:37:19 +0000

    spot turnaway up much crowded

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
