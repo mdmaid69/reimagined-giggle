21ea5ee8f8d1ede6b2ee975a0d195fbee1ba9cc2
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:03:07 +0000

    scrap even lost hurt movement

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_current_directory():
-        return os.getcwd()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
