d973b4919c32edb509a1926ecaa6617a75c12747
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:44:49 +0000

    tongue broken head up shade

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
