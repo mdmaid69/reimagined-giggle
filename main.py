9c6c9dc383e753cd324978916542847f09608726
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:22:08 +0000

    and level cost breakdown crazy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def clear_array(array):
-        array *= 0
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
