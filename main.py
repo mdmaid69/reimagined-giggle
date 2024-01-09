38f5e486b3582342d6f8d40b4e7a408a2a121ad2
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:50:14 +0000

    play separate burn practice yes

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def append_to_array(array, item):
+        array.append(item)
