ea79afec75d03dbb9d52655d82c1dafa829e3705
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:11:50 +0000

    Vv secret warm length concentrate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
 import array
-def clear_array(array):
-        array *= 0
+def get_array_typecode(array):
+        return array.typecode
