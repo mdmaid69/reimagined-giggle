86e6e061dae4a3d8953247a51d485a24bc43753e
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:54:48 +0000

    mood another whenever track wide

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
