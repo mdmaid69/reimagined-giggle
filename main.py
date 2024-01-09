d50f8e7d605cff0bc225709abac053c903c1a836
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:41:50 +0000

    trust bear many actually length

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import time
-print(time.time())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import json
+def convert_to_json(data):
+        return json.dumps(data)
