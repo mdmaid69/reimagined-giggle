9306b8a8b5aa45d7051de7e93db25378c658e89d
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:22:19 +0000

    giveback present callback formal side

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def square_number(x):
-        return x**2
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
