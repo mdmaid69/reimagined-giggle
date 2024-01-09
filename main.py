731765a6d285cbbd8e879f76513f055deb129204
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:11:07 +0000

    dance exercise trip collect reaction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
