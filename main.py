2161279540ad68b807dcd2f5a74d8c0c8ee0e47e
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:01:22 +0000

    surprise alone near underneath welcome

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
