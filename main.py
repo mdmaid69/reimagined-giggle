036979db88750a0d0e4f02920f1038b5458afd7c
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:16:57 +0000

    now pain home plate likely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
