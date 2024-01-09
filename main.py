5f3af7e558c889f2f740a8fca8f346365e8eab4c
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:29:43 +0000

    vote complicated floor kind doctor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def multiply_numbers(x, y):
+        return x * y
