9fed9079ffe2a36976cda9f0aa28279cf848d09d
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:35:26 +0000

    rain brush set really country

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
