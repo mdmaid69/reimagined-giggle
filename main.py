8b1479e8956481ad46dfc1bbc886e830f45a6e60
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:41:45 +0000

    support trust coat star complain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_power(work, time):
+        return work / time
