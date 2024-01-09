13c2179fc60064c8c3e976b2bcf383c54e250ccc
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:45:51 +0000

    attractive plan peace tense track

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
