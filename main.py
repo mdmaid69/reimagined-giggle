a5646d5185c1ed73d340fde7e53e055618bbb66f
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:17:01 +0000

    debt yes confident order part

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_pressure(force, area):
-        return force / area
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+for i in range(5):
+        print(i)
