94f46a067f262eba8bb2337d907fca356ac02397
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:50:55 +0000

    town shame dress one suck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+print(sys.version)
