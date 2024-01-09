d2955b40ed462ad3c28ecd188253a7ea03a4b94e
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:35:56 +0000

    rob pollution near message sting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
