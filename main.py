2a0cc710642220ed48253375d2bed283e219c7c5
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:54:20 +0000

    when wash tonight consist protest

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_density(mass, volume):
+        return mass / volume
+  def is_odd(n):
+        return n % 2 != 0
