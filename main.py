8937c6a870172d816b44c4bc11a782e6fe67101f
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:40:06 +0000

    definite relief sticky soon person

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def sort_list(lst):
+        return sorted(lst)
