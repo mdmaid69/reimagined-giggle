233fb9b477455301b31964b03490289cac3ec027
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:16:51 +0000

    comfortable content love oil far

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
