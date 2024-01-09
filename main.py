08b0c8e21994cf3754cea034ee8458b8bcccbb4e
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:32:33 +0000

    do deliberate getout account later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-for i in range(5):
-        print(i)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
