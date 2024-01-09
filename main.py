188d9ae493c8b8c1d896f167a438c015fd32f9cb
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:54:08 +0000

    original tread order project effort

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def is_odd(n):
-        return n % 2 != 0
-import sys
-def print_python_version():
-        print(sys.version)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
