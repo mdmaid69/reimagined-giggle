ed71b60f1ba057d56047d322b579f7e461268e4b
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:14:54 +0000

    well mother store smell broad

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
