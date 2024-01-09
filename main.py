beb2a22db9855ae489c2b63ce4fec3954b5129b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:42:14 +0000

    several basic land different fine

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_work(force, distance):
+        return force * distance
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
