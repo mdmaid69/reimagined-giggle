557bc1af58d55c2f2cc65a57713cfde4560636ad
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:18:55 +0000

    change official trust rare maybe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
