9499f7ce7063b79161767c44106a919193a11afb
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:29:01 +0000

    male uniform bye leave imagination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+        return os.path.getsize(file_name)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
