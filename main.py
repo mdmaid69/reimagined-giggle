5a196ef2929a1527bc822354bf22631b8001ebc7
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:35:15 +0000

    describe ignore demonstrate second nearly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
