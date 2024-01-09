20782ebc285f90f3242add9cb4362853e156b835
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:52:40 +0000

    realize all calmly alone runinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
