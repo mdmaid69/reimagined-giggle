a9c5420b0c7d615cf01eaa5f5f5e2ac6dc9a38ff
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:32:00 +0000

    realize pronunciation age shop window

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
