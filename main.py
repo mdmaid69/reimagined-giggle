66eaede18d0d85a25180851ef591955757e11234
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:16:37 +0000

    plan roof talk calm professional

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
