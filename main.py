1ca2cde4cd57114b200e55fabdd5770a5b30e3fa
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:12:01 +0000

    left theirs pump own hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+text = "Hello, world!"
+print("Reversed:", text[::-1])
