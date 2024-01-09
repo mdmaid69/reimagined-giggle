09df0c3f3ae5e3b4d3b7d56b65f7ed3e5485555c
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:31:28 +0000

    lean suffer Jj goback(to) reply

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_density(mass, volume):
+        return mass / volume
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
