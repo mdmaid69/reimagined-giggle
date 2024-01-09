ff53fe1ce8286070f2d10f5cad0b65ac8734410d
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:38:05 +0000

    pump godown introduction used past

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import tensorflow as tf
-print(tf.__version__)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
