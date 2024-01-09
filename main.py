e82a268b9fe041b8c7bd9ce91f27913eb2fe63fa
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:44:03 +0000

    describe direct prove water length

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
