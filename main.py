ea2f87a626a638b6a6023cfa2db00e97ef80e778
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:04:42 +0000

    greeting market crisis able introduce

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
