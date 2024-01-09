4d7bcc3f5fb02f91cfe40fb593c696c1430474b3
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:55:11 +0000

    advanced behave goup behaviour nor

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
