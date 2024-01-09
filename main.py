8c8c0b88dfc66b5bc4ec7ea73d6afe8ed23ed23c
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:42:09 +0000

    blue nothing determined purpose nurse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_working_directory():
+        return os.getcwd()
+def cube_number(x):
+        return x**3
