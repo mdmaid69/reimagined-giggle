3cd5a565deb5b4452b637e080d4d09342570d803
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:50:55 +0000

    emergency guard but before tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_min(lst):
+        return min(lst)
