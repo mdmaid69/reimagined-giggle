b8cd99c418671c30771b2da87bc0f877b8e2e3f3
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:20:13 +0000

    popular makeinto about choose taste

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def remove_duplicates(lst):
-        return list(set(lst))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
