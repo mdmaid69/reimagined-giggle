acdc23f814b1f54653f38119325e5019f4e92f6e
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:32:16 +0000

    any advanced wipe clean improve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
