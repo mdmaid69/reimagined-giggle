283fd7749bb359955ea899fc9fc503010b18cf4d
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:32:25 +0000

    Ss question getoff plan all

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def reverse_list(lst):
+        return lst[::-1]
