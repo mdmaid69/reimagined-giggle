d60bce556f8e4587ed6c0c1281305159298284b3
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:26:34 +0000

    computer possible spare more possession

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
