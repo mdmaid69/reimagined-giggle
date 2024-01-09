34be4d02b7d5dc5b91b9e6616eb636079708642e
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:02:58 +0000

    blue concerning few exchange within

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
