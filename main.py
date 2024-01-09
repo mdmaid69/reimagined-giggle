570c461840c1932d9925dc057008b9dd05e105db
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:20:59 +0000

    deserve fly square comeround properly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def sort_numbers(numbers):
+        return sorted(numbers)
+def find_min(numbers):
+        return min(numbers)
