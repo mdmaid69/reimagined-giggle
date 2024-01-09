2d9ec75246b3ea08b534476b8e5b50f402573e30
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:57:36 +0000

    suit game university head term

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def is_even(n):
+        return n % 2 == 0
