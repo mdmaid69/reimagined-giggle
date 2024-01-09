0365925d4d0cb19f3b5207e35837b9681066fa89
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:45:17 +0000

    Gg drink giveup usual contact

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
