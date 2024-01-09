52a0fb82efa2b2025fe703466560c00ee00568ba
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:54:02 +0000

    refuse defeat bus ticket force

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  def convert_to_hex(n):
+        return hex(n)
