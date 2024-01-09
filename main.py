f398eb09d80a35141aa5af39f989326facbfb5f1
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:56:47 +0000

    appeal complain sound indeed how

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
