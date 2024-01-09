5a1479defe7c79528d14e0b7add8ad4059135277
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:51:21 +0000

    film well never whichever fill

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
