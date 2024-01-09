4d917d1ee24a42bdee5a181d1c38735440e90e63
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:31:41 +0000

    so praise suckme arrive plate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def sort_numbers(numbers):
+        return sorted(numbers)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
