84f738c8324ac8e55d90fd31c87585e98a6b7e21
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:16:21 +0000

    face scrap cinema time plant

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def convert_to_hex(n):
+        return hex(n)
