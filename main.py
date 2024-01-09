c3cc00702110998ad4ee3a2f05b16404def2bf0a
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:56:29 +0000

    anyone exchange wealth basis authority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
