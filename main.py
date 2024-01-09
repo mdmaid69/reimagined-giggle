9646a57dca1b021a5109d62ccb9e119ea4f56c8b
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:54:42 +0000

    port throughout tie stop relative

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
