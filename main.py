bd4db3ee352de3d3c181647341ebb1dfbb51887a
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:24:35 +0000

    thought prepare head soft probably

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
