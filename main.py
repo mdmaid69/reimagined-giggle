b041e1ea5fbb4bf77ef3482ab646d0da7c7beacd
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:22:37 +0000

    post attention in content knock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
