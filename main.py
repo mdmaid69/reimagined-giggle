476039b90febe3e1037b838905b2b6a3ac72ba4c
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:40:47 +0000

    rubbish runover pride meeting usually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
