9c6cc8d958dab424138bd8b37f6c2dafd610f040
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:21:54 +0000

    Rr sleep party meal mention

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def add_numbers(x, y):
-        return x + y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
