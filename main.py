eb73dfaa694f7b0865ae5516e5996e6c52c71fcf
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:21:55 +0000

    press traffic write tune determined

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
