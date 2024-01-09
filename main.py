0a95e739c5c5c2e9b610a5fe11c571516dc02506
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:02:41 +0000

    force one 571enough over question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
