82e1eb424e3b7f5e27569c58393d1b5613798769
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:00:41 +0000

    trade professional window agreement regular

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
