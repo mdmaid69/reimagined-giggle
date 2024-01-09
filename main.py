afb3bc2820f3d9f4b0aff9c84c910b3f9fffedf4
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:23:47 +0000

    rate stupid stage government dinner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
