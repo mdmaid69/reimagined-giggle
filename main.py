2339995ccef44796ead02fed2b08c67386f86430
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:34:52 +0000

    Uu exhibition separate side personal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-print("Hello, world!")
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
