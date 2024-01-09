53bb7648ebc8cf788021fd46a2709eb46e515c0a
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:30:22 +0000

    period reserve will slice push

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
