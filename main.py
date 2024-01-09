3927270285337c0d42899d7d88fa318a23b09dc8
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:28:26 +0000

    communicate Ll port skill study

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_density(mass, volume):
+        return mass / volume
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
