2810831fcca5ac0ae50b651b0d9c58fe5f02d7d1
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:00:32 +0000

    pain scream dream call miss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import os
+def change_working_directory(path):
+        os.chdir(path)
