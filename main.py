74ed1fe586bc9010ccd9e403c49fd69f698a822c
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:07:32 +0000

    opposite hold me unit lend

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
