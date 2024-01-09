2392966cfdf227643e948c3021a67ff94e49243c
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:29:00 +0000

    door yourself patient connection various

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+for i in range(10): print(i)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_current_directory():
+        return os.getcwd()
