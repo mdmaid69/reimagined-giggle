8c15f3b5cad8320760fc0e30ae6ab6d648b59e08
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:14:47 +0000

    yourself study precise ordinary second

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
