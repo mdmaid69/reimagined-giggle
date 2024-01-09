2280b707bb20dee0d446a09d0c2f8cf65e21c27c
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:30:55 +0000

    but succeed peaceful cook frame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
