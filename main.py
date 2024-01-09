4a9c8da7db001d3c7bb8450bef2ee9a14c12a066
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:48:51 +0000

    cause means lucky vote ear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def subtract_numbers(x, y):
+        return x - y
