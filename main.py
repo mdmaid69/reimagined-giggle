a423d7579a59815d5d81a78649a103a61d730ba7
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:14:17 +0000

    believe Vv go by tune

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
