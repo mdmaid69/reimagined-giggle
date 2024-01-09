cb0aa48d005c906123b6134cb3110397bc68eb9f
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:20:19 +0000

    poor company tie indeed plenty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
