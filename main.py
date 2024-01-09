558649dbcec07881a86d81be51c589c0fe95e1c6
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:09:47 +0000

    572enter exhibition method happy fit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+def calculate_speed(distance, time):
+        return distance / time
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
