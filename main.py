bd6ad7833c6d0798eb1058530d73226700034392
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:48:52 +0000

    street join act trap how

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
