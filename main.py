ba2abb62b28dad709d015dfa4d88d4274d9079fd
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:54:08 +0000

    temper during sorry trousers we

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def reverse_string(s):
+        return s[::-1]
