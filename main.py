2e7304b1ca01d3736f8cfa6a20c7b5ecde8ab5b8
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:25:34 +0000

    explain float activity along workupto

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
