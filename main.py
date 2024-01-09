99171764b5e9766e06f0be02da7773c040e00500
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:10:53 +0000

    drop criticism border variety exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_working_directory():
+        return os.getcwd()
