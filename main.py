afe353ac2f16e1285440720c419951b7b5789ccc
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:54:20 +0000

    pump to influence contribute getoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_array_to_list(array):
+        return array.tolist()
