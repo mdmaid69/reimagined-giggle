fbf6923c6513eb425202fefdc775b8fdc39d2e52
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:17:45 +0000

    pupil issue confidence I throughout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def split_path(path):
+        return os.path.split(path)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
