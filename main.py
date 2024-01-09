85399df68f3ada7fb437239f6feb25112605965f
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:06:02 +0000

    warn blow unless tap estimate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
