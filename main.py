ee458a0e2f5e5d7682cb704b01b650cad7f1862b
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:47:41 +0000

    ready better instant exchange vote

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-text = "Hello, world!"
-print("Characters:", len(text))
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
