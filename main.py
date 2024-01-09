fa7d8fb8757242025610e5fa0863b91a5bec5845
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:41:08 +0000

    border deliberately attractive worst phone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
