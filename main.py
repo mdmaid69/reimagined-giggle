29ec37e0c85fdd0c473997709e2ae8337824ccb1
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:49:13 +0000

    under fair embarrassing mean how

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
