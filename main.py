83cb81c890e2d2d185ca3f8533de69deb97028ec
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:45:28 +0000

    separate fail watch commit most

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
