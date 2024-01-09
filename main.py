7c0d1122a6af6576911df738331ba8da209d6f80
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:12:51 +0000

    which software leg fork breakin

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
