96dfbe1b5b435ef8ef652dee71852cfc00d63dcb
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:56:13 +0000

    after still roll vision example

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_power(work, time):
+        return work / time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
