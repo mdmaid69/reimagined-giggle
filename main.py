1006a0541f21cfbabcdebf16ece7fce671149b03
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:25:27 +0000

    finger comment still arrival alive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
