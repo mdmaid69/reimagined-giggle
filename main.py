81331c7832feae62b7240e32aa6a9cfb4ffa5bee
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:19:47 +0000

    file arrange design fix friendly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
