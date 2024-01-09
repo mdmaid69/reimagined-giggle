548625d619d6d46cce65b44755e5201848e35623
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:50:39 +0000

    patient store male little best

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
