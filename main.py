eac928b315a90e7587b50bb77bfaab4f322d808d
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:05:30 +0000

    Yy treat read position force

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+print(math.pi)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
