bd92a062c524d317562aaaaa335ff1f03f20ad4e
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:00:41 +0000

    himself night minute previous wide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def multiply_numbers(x, y):
+        return x * y
+import os
+def remove_directory(path):
+        os.rmdir(path)
