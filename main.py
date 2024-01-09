2fd1394e16f9044ffe8a79803e183d69a20c1b5e
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:09:03 +0000

    teach colour talk care find

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  def convert_to_octal(n):
+        return oct(n)
