d2c8aa54f3ac4461c12d97478911839d1d63f789
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:07:53 +0000

    hurt Qq hungry feel except

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
