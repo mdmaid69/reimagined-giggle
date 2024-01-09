0284c5bc60ee38ce50b11acc75340afb2c42bddc
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:33:59 +0000

    juice promise kill blow hungry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  def cube_number(x):
+        return x**3
