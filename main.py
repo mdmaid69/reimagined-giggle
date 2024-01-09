098e8ec574aebefed1f1d22da988fea803596030
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:36:48 +0000

    copy project terrible popular leave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import sys
+print(sys.version)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
