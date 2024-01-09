82ce2cfc9db49dbaa2c6e663516db16ddde0b4d8
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:48:44 +0000

    please talkoutof light score left

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_area_rectangle(l, w):
+        return l * w
