8a6635fe95be97051a3cb68dd2da64a008bb2356
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:09:18 +0000

    discovery callout arrangement acceptable sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_power(work, time):
-        return work / time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
