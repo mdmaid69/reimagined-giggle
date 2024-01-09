116755e4501771e01403cd2ba45291085c3e8c10
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:45:00 +0000

    top speech spite at show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def reverse_string(s):
+        return s[::-1]
