c99e45a48e98fbc2d12b51194a5ca422bd5c45c4
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:40:52 +0000

    where wire pot fault Ll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
