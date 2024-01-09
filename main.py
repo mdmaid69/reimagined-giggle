c7e64c6298b5acd14e42d1f65912f468f32adbd6
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:30:52 +0000

    benefit workout post drawing year

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
