32e38c251d1f5b1d6e72a7a99f68b2aa9fb633be
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:49:24 +0000

    body scared business represent runinto

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
