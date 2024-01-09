f9c8abb64a919f84af49e466b776d919c44c29cb
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:11:31 +0000

    demand free interview surface why

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
