f9b1a0d9493109df7cd819ea3380cbc439f783f1
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:32:56 +0000

    raw exchange laugh free keepoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
