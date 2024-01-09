66269f1f0297342e74914772d84794118e49ad2f
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:35:38 +0000

    any recognize file clever very

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
