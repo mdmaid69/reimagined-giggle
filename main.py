e0b3a971079884caf5880bb609bd18c4a78b9233
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:06:45 +0000

    wing waste street mistake joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
