437e475b005c0feb3c13b3deea0922c68ccab515
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:01:13 +0000

    combine model sell gather embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
