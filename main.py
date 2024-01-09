e79419ed67135297e62fdff8a07dbe8b41e5d27a
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:12:48 +0000

    fast breakup odd boil police

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
