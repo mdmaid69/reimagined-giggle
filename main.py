91bb036ad26de666ebfb047e010eeaa51103ba02
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:54:55 +0000

    peace law danger off majority

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
