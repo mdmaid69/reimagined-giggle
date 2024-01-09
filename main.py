3a8970a0c1a8e7ee5d5611ead80721a59394ae84
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:23:05 +0000

    average pump cruel spring last

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
