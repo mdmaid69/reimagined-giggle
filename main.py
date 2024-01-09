a39c8b302304f128b23b2cddbb661439d7241c64
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:13:54 +0000

    further keepoff themselves sail moon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
