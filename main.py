74d7c23edb1f7f67b89ec6d307c3ec56353f23bf
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:18:47 +0000

    success payment money middle no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
