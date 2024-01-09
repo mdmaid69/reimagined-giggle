0e17afa4ebcc829339c572a4bbc452703ae4f9aa
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:10:51 +0000

    within genuine ought what couple

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import datetime
+print(datetime.datetime.now())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
