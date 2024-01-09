e71a170ed35ce3c860fde57e4ad2b167174cd435
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:27:52 +0000

    finally peace soak bringforward dance

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
