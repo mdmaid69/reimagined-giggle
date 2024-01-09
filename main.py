4def54cea61cdfd9a8cd8de93c7c219d325c19b8
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:04:09 +0000

    approval other getback warn opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
