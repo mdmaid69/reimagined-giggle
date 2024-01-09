863ff731600bc0831f784ef0d579fa1728c8cce7
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:03:37 +0000

    thank noone love begin loose

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
