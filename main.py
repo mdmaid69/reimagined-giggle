bac24e355186e326b24517601070985178f4b4dc
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:05:53 +0000

    embarrassed science instrument on depend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
