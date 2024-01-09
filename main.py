4a855de87aef042ec207f6ccfdec5635077c67fc
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:04:21 +0000

    means tip distance excellent spite

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
