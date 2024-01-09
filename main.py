2e286afd022bb2781a6431ef2d42ecc64d0c08eb
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:19:01 +0000

    reason shut sort bottom this

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def square_number(x):
+        return x**2
