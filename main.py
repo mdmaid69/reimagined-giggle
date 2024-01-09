6a6f2b226e6d0fe14661685cef41af0828ac0641
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:38:58 +0000

    street tired bottom shout write

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
