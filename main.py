1c25b6f09fb119a8a31237b40b87f332fb0bd0bd
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:57:01 +0000

    simple then favour hungry body

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
