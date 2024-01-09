984d4de36e47158fe9700289bdb199cf6adab3c8
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:23:49 +0000

    guess occasional pity market less

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
 import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def get_array_as_bool(array):
+        return bool(array)
