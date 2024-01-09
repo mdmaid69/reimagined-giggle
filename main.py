33ed0e6ba79aa92771a9a4a401770a6b2253cb51
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:44:03 +0000

    goaway fasten material object something

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+print([x**2 for x in range(10)])
