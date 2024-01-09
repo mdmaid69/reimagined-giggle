41e7ca6ff56de3b392f65b6f2530a93a192bba43
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:24:17 +0000

    needle takeout temperature turninto size

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_volume(length, width, height):
-        return length * width * height
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
