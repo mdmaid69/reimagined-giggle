ef308fcce85c1ef9f4d236cbb27beebd3e731acd
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:11:55 +0000

    phone 567enemy 569enjoy height sell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def remove_duplicates(lst):
+        return list(set(lst))
