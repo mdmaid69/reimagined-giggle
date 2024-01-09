e49016e1e7dd7f42272d19e1691ce13d960c0f2e
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:17:57 +0000

    unpleasant shallow mother frightened especially

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def greet(name):
+        print(f"Hello, {name}!")
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
