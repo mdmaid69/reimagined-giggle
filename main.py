3f683ce8e01f98254f3c4d56b9c141316c81b066
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:43:45 +0000

    promise exchange pronounce contain no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
