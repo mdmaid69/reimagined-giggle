a96c92f293936a33f54dde14e73aa23f71285fa1
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:41:45 +0000

    value foreign runinto bad level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
