599e8b2643dffb21d89470644a39402c76ec1fb2
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:54:46 +0000

    equal uponon takeout cheque hour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
