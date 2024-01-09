49b6ab98ec7d4eaa7b8229a500216fdcfc23a60d
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:25:55 +0000

    opportunity write fast transport today

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-def exit_program():
-        sys.exit()
+def is_odd(n):
+        return n % 2 != 0
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
