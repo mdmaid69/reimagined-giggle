61091d03c92467a286217fe29173fc4071ba7854
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:52:53 +0000

    tie summer memory reject set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import time
+print(time.time())
