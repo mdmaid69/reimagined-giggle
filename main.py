708f84e5003dc60576ceb66ee380a7392527515a
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:56:25 +0000

    schedule follow Zz promise wing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  def cube_number(x):
-        return x**3
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
