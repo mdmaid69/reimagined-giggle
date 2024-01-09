4b32f1e5fae369eae0e9067732ef4ef6d7164cac
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:35:52 +0000

    welcome play wall smell thinkover

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-name = "Python"
-print("Hello,", name)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
