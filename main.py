6989fa517004a1a9fcf83d6fc1f5b0c79d5c4644
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:52:25 +0000

    splash taste successful ashamed listenfor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
