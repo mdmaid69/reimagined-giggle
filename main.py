ea0a10ca6b09d17391c4e67d8685cf2216a09a8a
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:25:02 +0000

    572enter blue against watch fold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
