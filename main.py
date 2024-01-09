fce69ff692cbe8ced61156e449732a2b0ef20ac4
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:03:55 +0000

    congratulate improve us hardly soon

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
