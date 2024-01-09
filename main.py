2e87b78140d8ae5fb7a820cc3d900cda7f587954
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:16:21 +0000

    because risk fail naturally narrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
