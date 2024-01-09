59b82ca8a668bb95838a81acf38e64ab7842b0ed
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:24:00 +0000

    thankyou on normal hang children

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
