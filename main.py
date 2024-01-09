536e9ff9cc0be45dd25b68fd26497101eac6b2ae
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:43:39 +0000

    room trousers exactly total everywhere

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+name = "Python"
+print("Hello,", name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
