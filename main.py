203fd1bd5d00968b32da4c0d5a28f52592c2c6f3
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:23:06 +0000

    keepup bottom base practise favourite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
