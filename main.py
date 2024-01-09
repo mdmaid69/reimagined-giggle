dbb35b91c8a3a47167008d3053efde55569222c4
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:52:42 +0000

    statement would work place shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
