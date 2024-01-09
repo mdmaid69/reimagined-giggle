7366826aff6d37a36707126c4e6bf46ba9d7d185
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:41:25 +0000

    talk left window keep protest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
