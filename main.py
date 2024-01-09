0b3ca1fb10cf7c9c76cddf16296689f553600b45
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:31:43 +0000

    section correction how study issue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
