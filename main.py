dc38e0dbe25be8f989ba9941e7943fe6a184390e
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:57:30 +0000

    relationship load warm unhappy every

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
