3e955db11652703c33f4a995056999f01c580c48
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:24:30 +0000

    straight rain right achieve zero

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
