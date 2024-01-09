3f0733af9f4b51e73faeb8acc647286e0c8af1c8
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:00:08 +0000

    rain proof sad intend of

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
