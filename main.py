48d7494fc6fb9b39aa5b8b05d404b2af6a8518d6
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:38:07 +0000

    offence behind also connection still

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
