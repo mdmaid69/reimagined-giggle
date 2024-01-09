cfe7e31f126e294b1d3a3f34e4cf3011df8a6472
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:27:57 +0000

    government release only here cough

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_current_directory():
+        return os.getcwd()
