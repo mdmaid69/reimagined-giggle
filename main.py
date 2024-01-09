e4b93590fd4c2406c5862662a5dee3a3f02dee91
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:08:10 +0000

    wheel turn pen hello block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
