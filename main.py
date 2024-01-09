439723fab671a1e7e7251e7481a69105cccaf219
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:35:08 +0000

    mysterious stage sport operate argue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
