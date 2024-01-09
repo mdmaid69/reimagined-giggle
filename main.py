4d6ddf7feab9a22c0e3f4ec0158777bd64b945d7
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:17:00 +0000

    hit without bound water hide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def square_number(x):
+        return x**2
