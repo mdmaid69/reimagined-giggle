a9c171fbdf947b0631583f6b23080c9e2dda17db
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:51:34 +0000

    secret factory brave female move

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
