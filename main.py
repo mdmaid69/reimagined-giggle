ebc5f6ae967e9f051ca438c8974822964c0bf5ca
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:19:42 +0000

    definite congratulate lay concentrate strike

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import math
+print(math.pi)
+def subtract_numbers(x, y):
+        return x - y
