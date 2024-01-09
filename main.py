6b57525dd57a924aa48ca166f1adcadcaf5b0e78
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:20:13 +0000

    roll secret phone danger anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
