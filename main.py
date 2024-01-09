79c56b80982f606b993914b59fd5b5aa2e80d570
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:39:31 +0000

    everywhere extra pen cheat clear

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import array
+def get_array_typecode(array):
+        return array.typecode
