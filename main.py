c7d425a8b095a7bf3015f296d4ebadf7e95db1dc
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:11:42 +0000

    exhibition beneath go pretty during

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
