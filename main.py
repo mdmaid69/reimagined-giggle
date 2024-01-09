4ccb185e2724efbab275e42fa3c84518173f5824
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:34:19 +0000

    collect side sky agreement original

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
