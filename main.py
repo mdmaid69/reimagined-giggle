5f3e04fcefb5ef1e8c7afd02e0e7a7bcc286effd
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:52:53 +0000

    target obtain fast bag contrast

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+for i in range(5):
+        print(i)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
