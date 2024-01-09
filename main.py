61aada28a2ac0e64ca49ae71307c71f87be42152
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:20:02 +0000

    lesson violence eager peaceful hour

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
