b36a7487119dd260d62da20a709db9386d3d059a
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:05:28 +0000

    underneath actually about mark goout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
