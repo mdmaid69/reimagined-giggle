7b24e6fd7a70c5665772543f38946032db721449
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:31:10 +0000

    process own runover sleep guide

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
