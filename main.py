2268ab481dc2a40ba3a2e0f098f69efa425d872d
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:11:46 +0000

    trap success empty male load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def reverse_list(lst):
+        return lst[::-1]
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
