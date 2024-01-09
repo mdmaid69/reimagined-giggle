3a6437d4be7ca3b91fde29bfe275483242228518
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:18:07 +0000

    full really hand indeed hungry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import os
+def get_current_working_directory():
+        return os.getcwd()
