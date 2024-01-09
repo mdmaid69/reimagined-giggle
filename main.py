8f6771900fd45434ffe95ff147202d8e704f54a1
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:17:47 +0000

    tell true declaration between willing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
