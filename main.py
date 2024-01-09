7c28eb6b836760db0b10e95dc0d99dc21c0e32d4
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:31:09 +0000

    arrive square wash path move

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
