634c119a7bf574fc99e2fa2758eca5875ba4a542
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:52:49 +0000

    estimate punishment win taste record

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
