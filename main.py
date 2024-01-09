d1258826602903bf5025e87259ae663bab6dfeb7
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:06:10 +0000

    piece material complicated thinkof difference

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
