9b04525ba8b3cf1b793d012011a209ce7f9a5d2a
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:44:19 +0000

    straight over mainly declaration convincing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
