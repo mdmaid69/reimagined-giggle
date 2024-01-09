66d778ad33191b7a621dc30461e0fdb466615d73
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:38:27 +0000

    towards practical damage trade aware

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
