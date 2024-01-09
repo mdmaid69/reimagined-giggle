b557189ae76cf680069769c4638c5cbb6b6c15d6
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:34:23 +0000

    plain already cross risk rich

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+print([x**2 for x in range(10)])
