50cdad59400992ba1f4602c0ae6d2acf413a2e92
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:25:16 +0000

    ignore step private child walk

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import time
-print(time.time())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
