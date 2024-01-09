66fd416b0527779937e255dcf710f1d4d8b6486b
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:08:01 +0000

    someone hardly reply getback busy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
