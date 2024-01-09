e1e1f696dd153b72e813460fe0882b6f063d164a
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:11:43 +0000

    search recover dark age change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+for i in range(5):
+        print(i)
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def cube_number(x):
-        return x**3
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
