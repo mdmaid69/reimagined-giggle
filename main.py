026f4b7ecfdc58e223b279df14147a63addae17e
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:38:31 +0000

    lost future flow criticize offend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+def remove_duplicates(lst):
+        return list(set(lst))
