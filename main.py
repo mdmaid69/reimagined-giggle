30dc76d660e6822186f6a0f070d17de2ccca83b1
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:06:34 +0000

    area second wait deliver interview

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def find_min(lst):
+        return min(lst)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
