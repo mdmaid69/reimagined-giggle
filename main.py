3b1d039de55ecd4c87d4716576c2cdd24cd8ceb6
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:14:22 +0000

    human drop old practice party

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
