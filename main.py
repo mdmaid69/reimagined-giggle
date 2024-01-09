addc7b5dc49adae5c3337afbd3879b1dc5af87e6
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:21:29 +0000

    hurt ticket half foreign to

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
