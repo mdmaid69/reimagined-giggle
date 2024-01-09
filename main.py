37492e22005e75c3c48c72087383e1e60752b691
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:14:13 +0000

    honest famous principle peaceful passage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def find_min(numbers):
+        return min(numbers)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
