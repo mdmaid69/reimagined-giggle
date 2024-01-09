9e19bf0705294f8ce87187e4deceedeed3611f79
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:25:48 +0000

    thumb contain release about Yy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
