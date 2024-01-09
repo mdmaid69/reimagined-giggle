7bf28f39fbb07b303de069d2e942c42990012578
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:56:58 +0000

    charge expect scare possibly possibly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def cube_number(x):
+        return x**3
