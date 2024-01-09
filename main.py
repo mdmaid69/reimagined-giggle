5e02b658c69ecabc626b1a87983062f67aaddec1
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:13:48 +0000

    careful little similar fail yours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
