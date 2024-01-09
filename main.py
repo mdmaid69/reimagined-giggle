a6c518dae1ee9bd79d56c74cb55125086d10f3fa
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:58:59 +0000

    object whoever describe excuse beginning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+def remove_duplicates(lst):
+        return list(set(lst))
