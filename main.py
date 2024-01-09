311c29234fafcd5a5f22eb4c8a15eaf2a95d284f
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:24:31 +0000

    unit crowded check famous impossible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
