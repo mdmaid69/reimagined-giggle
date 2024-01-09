27c4bb28f4f67ec22ad4abe8bbf21d17896cc825
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:39:16 +0000

    old theirs transport experience instant

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import collections
+def create_counter():
+        return collections.Counter()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
