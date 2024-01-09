c44fcaf6b779493faeb982a8903dc721565a6ef3
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:56:53 +0000

    ball comeup ago cry wrap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_work(force, distance):
+        return force * distance
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
