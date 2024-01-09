905e418933ef4a8ae1585871333acf5a89ccd300
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:33:21 +0000

    similar disturbing suffer sail influence

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def print_python_version():
+        return sys.version
