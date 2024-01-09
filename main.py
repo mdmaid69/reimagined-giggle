9c26140b96df780221979738978ed2eaa5e8cc50
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:39:54 +0000

    speak soil copy roof perhaps

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_odd(n):
-        return n % 2 != 0
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
