2836952c1d76dcc21c6050efc8d549b9d276f1ec
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:31:42 +0000

    with avoid order back image

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
