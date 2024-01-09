a3af994f678c50a1c6a4a8b3bab16a059849b982
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:37:51 +0000

    hit command should pen explanation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_density(mass, volume):
+        return mass / volume
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
