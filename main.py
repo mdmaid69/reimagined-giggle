d33395ed1e5f6c3295d57e95091ef5cf9a996e8e
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:13:37 +0000

    wild skirt hello star bang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
