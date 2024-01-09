925f7bec1b15cdfd36da9cb65af46d81243f808b
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:37:48 +0000

    on town national direct system

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
