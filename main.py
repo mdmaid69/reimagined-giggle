308cb157206b22ff645afad28568cba6dcea4b34
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:30:48 +0000

    pleasure variety attention communication challenge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import time
-print(time.time())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_union(list1, list2):
+        return set(list1) | set(list2)
