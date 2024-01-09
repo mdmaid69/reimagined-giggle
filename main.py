a92a97af6c3f615df49431a03eb5d2fd62883cd0
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:11:22 +0000

    now cough far phone about

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
 def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
