eb8e0bfaf4087c8f3058cfaa164f97b36fb50b97
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:16:13 +0000

    answer declaration file turnover first

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_max(lst):
-        return max(lst)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print("Powers of 2:", [2**x for x in range(n)])
