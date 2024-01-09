3af8f69c130a4348f8dafd38eb92aa429d9696ee
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:18:55 +0000

    side slowly good Oo dry

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
