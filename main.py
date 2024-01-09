7c875beed6f68a126d6ae82c79f7e0e934a1dc36
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:34:26 +0000

    moral subject hit relax command

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
