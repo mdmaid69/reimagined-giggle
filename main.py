995fc814e2f1aec09ae6b42e4e705f8919a19417
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:13:35 +0000

    community suffer giveback life cross

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_speed(distance, time):
+        return distance / time
