50fbf2bdb67bf458900b724d2b2a8c0b99e5ce42
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:28:43 +0000

    onto eat spread any Ll

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
