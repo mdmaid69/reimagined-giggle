2f0ba522c149714d319e472528437b4b251fbfc6
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:30:37 +0000

    term before speech brush introduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
