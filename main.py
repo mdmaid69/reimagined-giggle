d9131592e5d9d7c66cd65a27d337e78dcfd7785c
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:33:05 +0000

    importance valuable school mind interesting

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
