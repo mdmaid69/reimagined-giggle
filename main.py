bd518141ce899337781533cdf48f193d8f165ab9
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:17:53 +0000

    slice movement easy bet bush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
