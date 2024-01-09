cdff3df21701fd1e40e13f9d15f4e466f5880df2
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:18:07 +0000

    567enemy please getaway long bang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
