08ea0c68df052547f15d85e2629dc5f4fd542dfe
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:35:23 +0000

    section unpleasant loan face understand

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import tensorflow as tf
-print(tf.__version__)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
