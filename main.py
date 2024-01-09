27b6b100ef03afe7e79dadc0ded92bd3c45be712
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:02:19 +0000

    news stranger plenty history shop

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def count_elements(lst):
+        return len(lst)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
