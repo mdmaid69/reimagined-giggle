f933d338bc524749f6c0b91a9d33bfa874e2b6ee
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:28:57 +0000

    noone close in Vv tidy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
