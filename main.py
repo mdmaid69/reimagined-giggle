a2b1de13a6e74cd185684ba923ef1ba81e0ce2ca
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:16:27 +0000

    late worse plant fine supply

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+print([x**2 for x in range(10)])
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
