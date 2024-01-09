4c345c245918ef25127bd0ba6f3abdec0bf4a35e
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:09:57 +0000

    thirsty Cc wet any base

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
