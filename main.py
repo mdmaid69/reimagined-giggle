23c320ad1f1844d549e0b6e26c802ecbce7d2575
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:49:03 +0000

    stranger appeal generous pile acceptable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
