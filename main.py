76fff0e0f7b34de843e986bab738f6cef90cb4c8
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:34:28 +0000

    trade support practice argument mark

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
