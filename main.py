60663ad6b6037a7696f70ca6a8782377f52300e7
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:52:17 +0000

    board mean pull space come

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import getpass
-def get_username():
-        return getpass.getuser()
+import math
+print(math.pi)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
