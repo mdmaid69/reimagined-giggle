b1872eb1cc85b9cfc05f6cccddcdf0c382d03792
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:39:41 +0000

    occur street pollution cover hair

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-  def count_elements(lst):
-        return len(lst)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
