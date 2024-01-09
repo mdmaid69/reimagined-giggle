8239a68dcdeec2b26a36db422775b9ad22e42a02
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:21:22 +0000

    serve give how term underneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def reverse_list(lst):
-        return lst[::-1]
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
