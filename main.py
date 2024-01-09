1fda0a89f7cc3670696195b70eb231ba1bcb211f
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:02:43 +0000

    too tooth morning punish godown

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
