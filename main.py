462e3072424e54b2b2322c5d4d7c2cb974135cf9
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:22:00 +0000

    fetch niggaacademy operate use wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
