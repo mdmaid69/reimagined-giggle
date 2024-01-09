1ee998232334fc0b8aa5c3a723e0c79e416fe996
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:00:23 +0000

    once fly sometimes cheat seat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
