048bf9d215a8876294df18c971ef215f1959e885
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:33:26 +0000

    tone earth then interview meaning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+def exit_program():
+        sys.exit()
