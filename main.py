5d50a2cc4dabe6cdfd1a28eab84b7595250f66f0
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:20:32 +0000

    scheme Jj lean injury above

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
