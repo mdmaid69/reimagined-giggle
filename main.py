34f15558b5d99b9727697689954e68cc6caf7347
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:47:49 +0000

    hang peace cap breathe tongue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
