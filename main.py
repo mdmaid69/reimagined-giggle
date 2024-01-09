12902fcbe144e8ebbfddf04b0936ab0a64dcb862
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:37:45 +0000

    situation spring attitude tight day

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def convert_to_octal(n):
+        return oct(n)
