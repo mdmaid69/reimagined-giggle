2bb1a629a11640a6e2f65601dcd3b77ff65e610b
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:19:57 +0000

    mine behind improvement smart Oo

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
