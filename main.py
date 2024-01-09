e44dabc318930dbe88f82127816166a49e1ab9ef
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:27:59 +0000

    indeed principle blame study lose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
