8049572d1681744bf6d07203a9b83b013f8e8a6d
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:34:05 +0000

    crash capital land correct experiment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import collections
+def create_queue():
+        return collections.deque()
