a6de7897d0197a9b98d1b877522d157b117bf9f1
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:07:48 +0000

    region autumn damp admire test

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+def calculate_power(work, time):
+        return work / time
