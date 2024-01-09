dfebeac6f14836ffc2b47cf79076e8242f01d898
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:50:05 +0000

    Ee rule union holdup takeaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
