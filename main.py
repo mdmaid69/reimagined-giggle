88b5892d2245ff099e5c450f4532883bd4c6a719
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:11:23 +0000

    keepaway maybe usually frame brother

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
