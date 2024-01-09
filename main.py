b263c4a84ce00a352783a976af70541be0f032ee
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:06:09 +0000

    he judge arrival about funny

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def add_numbers(x, y):
-        return x + y
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
