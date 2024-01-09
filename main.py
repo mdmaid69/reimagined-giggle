e4e00494fb347747f25aae8ebb709a6a9effced6
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:15:12 +0000

    work hurry instruction show mix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
