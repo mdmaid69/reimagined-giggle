cf736c49d4eb04bb77662d8bee4897eb18953a06
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:10:05 +0000

    cough discover string account change

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def find_max(lst):
+        return max(lst)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
