bdd55915845abe9eb2bdf3f73d628e892c441d30
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:51:32 +0000

    anybody brown reach so record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import tensorflow as tf
+print(tf.__version__)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
