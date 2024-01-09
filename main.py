c9c25684ab6e256dd89b44caa90dba914d7344ec
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:01:36 +0000

    worry precise fortunately blue tired

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
