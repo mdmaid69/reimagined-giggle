57aed687b7a6fb7f861a70521e0bc73867872555
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:55:38 +0000

    patience case so safe feel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def get_array_itemsize(array):
+        return array.itemsize
