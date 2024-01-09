dfe06ac62818bf4d7bcaec165d7fa802a302bb63
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:43:04 +0000

    reply rough finally turninto light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
