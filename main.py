016b64f27137897150444a48d23aaff41546d56b
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:02:33 +0000

    generally hot angry hand hurt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def find_max(lst):
+        return max(lst)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
