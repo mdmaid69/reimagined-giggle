08e3ea731f5c62d15587152b0231c167971716db
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:45:19 +0000

    seriously scared press bin stay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_density(mass, volume):
+        return mass / volume
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
