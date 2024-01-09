f762538d86a7891dac130eca84688c8de0b7e859
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:11:29 +0000

    attract war tax switch train

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
