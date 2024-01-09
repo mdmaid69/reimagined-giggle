69b2662f310ab7398366662d9ec1b4f62d77cea2
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:54:38 +0000

    there crisis present develop sudden

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
