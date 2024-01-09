49e646163ab1b9e71e26a4895fe60869fe3ed276
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:18:40 +0000

    joke prepare brush politics question

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_speed(distance, time):
-        return distance / time
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import collections
+def create_priority_queue():
+        return collections.deque()
