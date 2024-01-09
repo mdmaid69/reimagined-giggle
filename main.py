7038e6875cff536f1febc28cc7b299fc658d483a
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:42:08 +0000

    below popular around object mine

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+x = 10
+y = 20
+print("Sum:", x + y)
