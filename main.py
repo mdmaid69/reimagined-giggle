695d00bd134d8553c7d990901f0280599c6eaa81
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:24:16 +0000

    fill wake admit prepare correction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
