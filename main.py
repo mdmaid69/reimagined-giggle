9a536ef5198f5b236bad9c8c7ef15a085b7b1417
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:27:52 +0000

    deliver tonight exit fold declare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def multiply_numbers(x, y):
+        return x * y
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
