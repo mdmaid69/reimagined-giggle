09de9cfb1f1d62321e5c0947e72da9b737ec4d80
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:03:07 +0000

    defeat mean performance turn him

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
