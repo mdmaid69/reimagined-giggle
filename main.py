ac30c91ca7c64670db7b9ed4f2d90fcaf9318e0f
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:13:50 +0000

    attention scream chief situation story

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_current_working_directory():
+        return os.getcwd()
