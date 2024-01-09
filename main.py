2096e661bf16f279440d97685779c281cac141e3
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:21:57 +0000

    wet cause comeback change cross

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
