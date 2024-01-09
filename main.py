1082f629c713cb9e7f046ec59a05c3aa9af3de7a
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:19:40 +0000

    dry thick sign dull design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
 import math
-print(math.pi)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def find_max(lst):
+        return max(lst)
