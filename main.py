8ccc52bfdb4cb9e2e7b838564daa7625b8f18a88
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:21:05 +0000

    part doubt bus normal less

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
