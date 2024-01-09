0e6aaa20bbc30523142edb8421c5240190f8f0cf
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:59:21 +0000

    still depend warn only proper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import sys
+def print_python_version():
+        return sys.version
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
