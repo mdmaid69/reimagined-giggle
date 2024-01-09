b6dc1c21fcf31f08590af2a9c468bb197e5233c9
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:55:57 +0000

    sink film some little operation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import array
+def get_array_as_int(array):
+        return int(array[0])
