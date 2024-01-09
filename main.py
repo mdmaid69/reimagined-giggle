5a8878ffb802701766541e44412cd93aec3b8d34
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:18:33 +0000

    gooff plenty demonstrate container with

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-print(sys.version)
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
