66a029c456fdd000f6d5a155b90df0bd5369306d
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:57:37 +0000

    trip seem rent prefer suppose

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def cube_number(x):
-        return x**3
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
