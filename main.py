4fd0d65705324dee40171a1b37119a780320c413
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:54:04 +0000

    distant Oo dinner clear Yy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
