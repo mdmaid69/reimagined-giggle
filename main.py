1aec0059c269594cb6cb78a495ebee2a53df555e
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:55:02 +0000

    however scrape Gg become mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
