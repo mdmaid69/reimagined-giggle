768d1d48cd77de49307eb14cc11f35fd909f7218
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:05:23 +0000

    instead holdup noise unless introduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_user_string():
+        return collections.UserString()
