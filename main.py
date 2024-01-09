f8f88a317b4a06f2169f0f70899d90e168a4c208
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:09:16 +0000

    separate range stop win boat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_power(work, time):
+        return work / time
