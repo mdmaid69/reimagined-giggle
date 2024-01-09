bd33c7ff37f5d61b8df95d78ff30fa55e42fda9c
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:16:07 +0000

    relax still through remain consist

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  def multiply_numbers(x, y):
-        return x * y
+for i in range(10): print(i)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
