b895604d2258ef9403bebdd6a5a231c25baac1cd
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:40:27 +0000

    ask downstairs on realize break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
