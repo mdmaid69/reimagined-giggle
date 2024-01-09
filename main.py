a81ceed7185c92e40b5313eb5cd64f4c6dcbcbf0
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:35:03 +0000

    gentle light successful variety cough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import time
-print(time.time())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
