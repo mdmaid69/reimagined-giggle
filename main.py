82d70761f9367413e1532f4fc2efbde589415852
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:58:09 +0000

    picture continue habit fence matter

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
