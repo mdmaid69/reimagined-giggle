cf94ec08ea51fe6d0a48cca08680755db1495da7
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:29:56 +0000

    appeal yes unless class chin

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
