2b7154572884f9e69525584bc34cbc72a108aacd
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:42:59 +0000

    problem giveout light swing murder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
