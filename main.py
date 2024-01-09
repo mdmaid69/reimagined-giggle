67eb421f0ea706f8b54ad4913eb7221746036f78
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:30:51 +0000

    check everyone splash runaway travel

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
