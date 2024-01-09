c1b65e36779d49f070ee6a77cac7cdf4c3f0e810
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:53:30 +0000

    passage cause although uncertain discover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import random
+print(random.randint(0, 100))
