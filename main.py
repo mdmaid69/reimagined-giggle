2539cb582993bfaeb7da1c27615de50dc931fbc4
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:17:56 +0000

    float better against health usual

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import time
+def get_time_since_epoch():
+        return time.time()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
