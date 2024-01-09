2492b6b5e07deb2b86ceb74ff929dd100438fce1
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:57:28 +0000

    anywhere Ss common benefit worth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
