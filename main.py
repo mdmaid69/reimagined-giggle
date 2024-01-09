0160d24d4044b226d498bd42d0b76e5b72d93fa3
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:57:47 +0000

    soft occur depend when power

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_str(array):
+        return str(array)
