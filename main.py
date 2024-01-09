8b87a40224033b8f41d0cd81f8a1ac338c9bcd88
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:53:19 +0000

    move close law rid appearance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def reverse_string(s):
+        return s[::-1]
