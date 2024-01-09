477e67cffe1e7a1c14ebf8154ce08ff78cbf22b9
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:35:16 +0000

    practise entry about suffer her

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
