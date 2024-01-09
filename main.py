2b6a7987f907cf31fc142586448db927168d2604
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:07:57 +0000

    score together total rich sit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
