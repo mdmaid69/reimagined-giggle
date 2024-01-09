a5a5a8dffc0ee171cc0caac4274d21d725eadcf2
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:17:37 +0000

    another comein dead invite succeed

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sys
+def print_python_version():
+        return sys.version
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
