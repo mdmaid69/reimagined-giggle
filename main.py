c7fb4df4a13e6ca8778116889c90e672f58ac6f6
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:08:44 +0000

    price unlucky brother pretty negative

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_repr(array):
+        return repr(array)
