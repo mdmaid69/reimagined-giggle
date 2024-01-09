dd03ae6e698bd8270675fd7cfa550facf9b4c225
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:26:03 +0000

    total hope well ignore false

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def divide_numbers(x, y):
-        return x / y
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
