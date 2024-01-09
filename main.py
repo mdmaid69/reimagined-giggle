532bc8c57d369482f416424e8cb6a3aa6a8e82a0
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:05:19 +0000

    agree embarrassed except confusing neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
