273df3b5fa161b78ab4ad49e541711f9ba28d3b8
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:08:26 +0000

    friend wonder well report advantage

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
