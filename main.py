676fa3ae0fe1ea1a209c98489f931b814d200f95
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:54:22 +0000

    operate blame positive marry wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import sys
+def exit_program():
+        sys.exit()
