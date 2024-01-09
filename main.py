680af7a4c989ee11643a8583ba11d833c67d17f3
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:11:50 +0000

    clear receive evidence getout goaway

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
