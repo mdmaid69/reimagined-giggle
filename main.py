002846adf5ed8809dd776b1181080995bbec8020
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:59:48 +0000

    because dish she Gg respect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def count_elements(lst):
+        return len(lst)
+import sys
+print(sys.version)
