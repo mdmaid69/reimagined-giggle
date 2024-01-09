af95123a200fc859c21c457e7984b80810066aee
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:48:09 +0000

    noise dream Pp shout nature

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
