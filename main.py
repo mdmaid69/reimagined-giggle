1ba5240516fcb5e5a2bcef94818ae70c6b90b16b
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:06:22 +0000

    ready must wave chat office

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_as_bool(array):
+        return bool(array)
