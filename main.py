d1a7547c33f02f108c4de93241ae0a73e2a45f6b
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:26:20 +0000

    union shut song close wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
