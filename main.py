b19337d141732fc9c020ed6762d1b051d64580ee
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:51:27 +0000

    would software continue confusing lead

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_as_str(array):
-        return str(array)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def remove_duplicates(lst):
+        return list(set(lst))
