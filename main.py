36c0fe59e28738dc58481cf949c9fc969281440c
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:38:41 +0000

    uponon declare team drawing build

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def calculate_area_rectangle(l, w):
+        return l * w
