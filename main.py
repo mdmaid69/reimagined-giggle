7a8960da1392ae86292bc4ccfe29a4c8565d1f49
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:04:11 +0000

    turninto mistake suck impressed cheap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def is_odd(n):
-        return n % 2 != 0
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
