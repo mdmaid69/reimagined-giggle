69b4bc52343fd572d3c44090cc9495625e38134a
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:17:32 +0000

    squeeze later middle scheme region

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_area_rectangle(l, w):
-        return l * w
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
