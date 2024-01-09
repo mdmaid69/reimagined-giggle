4d6ccb8e1acf1e9cd9204a38077beab2afff714b
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:37:40 +0000

    pure fair funny welcome achievement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
