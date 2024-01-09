fea4af2d6874bcd667f6da7e6b295c191d188005
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:00:30 +0000

    pay blame hang again regret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
