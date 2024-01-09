3803fe20d2c7f55d0713fa06c9c4b650db4c0251
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:30:50 +0000

    public split Tt window modern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
