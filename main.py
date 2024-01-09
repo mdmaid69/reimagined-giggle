b08a7020e6034a84fbeb5ab0fffe6ec4fe8aefd7
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:02:54 +0000

    than term deliberate close business

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
