f40a9ac97b80184f9a8e9a6b8c32e52d66ac013f
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:02:03 +0000

    us somebody dress memory piece

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
