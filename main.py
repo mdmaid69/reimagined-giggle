8b7d05529b5a587e9ee2ced7320227fd8ac6020f
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:28:30 +0000

    pump apparently ground lot upstairs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def remove_duplicates(lst):
+        return list(set(lst))
