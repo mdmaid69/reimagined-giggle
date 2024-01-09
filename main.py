f6d8f740794070b313392772268d157e2f9cdca9
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:23:53 +0000

    minor artificial indeed under month

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
