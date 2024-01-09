7e325ce1a0d0809213e3a1fda680874780b30ebb
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:15:20 +0000

    disturbing anywhere glass truth nowhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def reverse_array(array):
+        array.reverse()
