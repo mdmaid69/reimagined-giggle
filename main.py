dc4f1d58e5b55fed5d00da0e024421cf40ba16c8
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:31:54 +0000

    side company release remark believe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
