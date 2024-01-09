8be5524585f87fa5816cc6b2cf4990739e620cd8
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:27:55 +0000

    illness tear sister yourself hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-for i in range(10): print(i)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  def reverse_list(lst):
+        return lst[::-1]
