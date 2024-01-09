40049429b33970cc120a600f50f62bf3fe7545e7
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:04:11 +0000

    university remember takeback soak which

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import platform
+def get_python_version():
+        return platform.python_version()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
