ecfeddcaeacc8056ad960f72ea2c661a9a601b15
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:01:52 +0000

    position purpose male hit emergency

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def reverse_list(lst):
+        return lst[::-1]
