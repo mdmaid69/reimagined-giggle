85eac8d8a2253fc6d1391ecfd776e234a4a97ed9
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:22:47 +0000

    display wipe health fair base

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def cube_number(x):
-        return x**3
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
