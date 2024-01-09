c91402d0d91c911cbf4a9aaa16b2d3010cb6e432
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:21:32 +0000

    basically right probably rescue survive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
