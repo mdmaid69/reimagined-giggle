d22f56869c7d62ffb74776ae9e00cf45f546027d
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:52:01 +0000

    beyond create dare spring meaning

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-import collections
-def create_queue():
-        return collections.deque()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def find_union(list1, list2):
+        return set(list1) | set(list2)
