d4c9129fc9f8668a4bd859829b5f0042cd7357fe
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:45:16 +0000

    what straight grateful anyone by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
