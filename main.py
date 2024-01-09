a927725401c3b6eceb02a66aa7748d98e31c9203
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:28:14 +0000

    brush rescue curious fall worse

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def is_odd(n):
-        return n % 2 != 0
+def remove_duplicates(lst):
+        return list(set(lst))
+  def convert_to_octal(n):
+        return oct(n)
