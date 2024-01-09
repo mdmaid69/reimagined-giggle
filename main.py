cc7f6b364f3eec1a25fbe97ce88816a53f5aaeb5
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:18:00 +0000

    air workupto comeup right absolutely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
