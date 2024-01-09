b39c74d8c344f3fd6cd57adafc769dea9a553827
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:30:26 +0000

    exaggerate 179bite choose be able

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def find_max(numbers):
-        return max(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def is_even(n):
+        return n % 2 == 0
