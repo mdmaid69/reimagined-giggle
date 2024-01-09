08d159314998b40aaa94907f9e66dfaf3646ff4c
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:24:37 +0000

    splash touch turn arrive risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
