56d670e0ee8587bdf94ed3d8dfd82c3f1608bbfa
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:43:07 +0000

    delicate instant tomorrow trial write

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
