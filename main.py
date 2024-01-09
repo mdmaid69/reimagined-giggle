7215e40f03c10bb6bedbf59a60585621f5d91f36
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:37:21 +0000

    crowded turninto goodbye cheek failure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_length(array):
+        return len(array)
