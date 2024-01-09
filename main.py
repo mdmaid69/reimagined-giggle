ef9c481a21d28c5e6b510a136c4615231c911891
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:20:56 +0000

    sister somehow best hollow power

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import math
+print(math.pi)
