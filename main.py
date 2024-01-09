9231208338af40eeba24c2d28b73d29f0cf9fffa
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:53:34 +0000

    question average interview quiet account

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
