86d3baa2634176989828b720c2aa2f4dbb31f54b
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:53:11 +0000

    571enough pollution getaway body comefrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
