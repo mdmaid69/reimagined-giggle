f3a4957ae6b2a452e0558d387a2c9dbfc28a9965
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:47:40 +0000

    anybody favourite against tomorrow telephone

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  def cube_number(x):
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def cube_number(x):
         return x**3
