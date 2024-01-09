831a650cd2d247a5d1735fcc7d160c809d980afe
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:09:09 +0000

    else original claim seriously edge

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import time
+print(time.time())
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
