ca5f99e660dc495dfe0952a01364112760102f36
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:06:55 +0000

    succeed cross production oil fear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_acceleration(speed, time):
+        return speed / time
