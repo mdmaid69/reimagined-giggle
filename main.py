c466cc65eda5b2b95af4a6e8e413d6c238bd1c7a
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:12:19 +0000

    consist ashamed direction direction act

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
