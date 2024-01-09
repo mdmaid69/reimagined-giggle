8aeb05ace34311182d7dfa221d74c04d4a11e3af
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:37:12 +0000

    565end square middle set detail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
