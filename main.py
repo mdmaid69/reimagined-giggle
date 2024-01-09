539d2b9b32bfc6cc91e9fcf2af2c62d567ab1827
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:24:47 +0000

    trade tidy try relative period

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_string_from_array(array):
+        return array.tobytes()
