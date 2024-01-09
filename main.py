1cd895e72a527dc3fb0b023f1fc3187f2ec4eba5
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:02:47 +0000

    regional well interview handle also

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_volume(length, width, height):
+        return length * width * height
