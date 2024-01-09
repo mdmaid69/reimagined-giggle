eca187dd9b0dee9392a758e6365be2261cff3526
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:15:15 +0000

    brief after after passage sweep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
