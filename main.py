fee11b0997f9f9dc97699899b2955725d04e89d5
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:18:22 +0000

    ask schedule shop contents talkoutof

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
