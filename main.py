f7b2f329180007d990e4892551909845fd668ed7
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:32:26 +0000

    rid among teach challenge hour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
