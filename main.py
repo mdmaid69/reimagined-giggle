649a2b366fbaf691ec0b314a8d80eede6d4392ad
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:16:23 +0000

    together suddenly damage factory ask

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import tensorflow as tf
+print(tf.__version__)
