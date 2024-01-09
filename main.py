8348ca7eaa2d265ef32b675fae2a0c0dc5943ede
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:13:44 +0000

    safety slip early talk to

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
