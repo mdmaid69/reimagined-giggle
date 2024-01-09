820d1b4dd92d8b9b4f84357a30265118b8ef5efb
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:58:35 +0000

    murder drink general progress worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
