0b5778eec8c49cdec10922cfa0423654ebc1b358
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:19:57 +0000

    closely pity mind factory hour

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def is_even(n):
+        return n % 2 == 0
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
