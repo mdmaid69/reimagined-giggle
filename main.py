0de8853bba3294b71ab69766d8ae9acdfb08113d
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:29:16 +0000

    slight single moral office effect

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
