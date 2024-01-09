e2cedea29c3efbab0869269f59df5378d8bb1bcf
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:26:56 +0000

    just smart journey soon good

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  def convert_to_octal(n):
+        return oct(n)
