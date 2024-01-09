fc4fa21924c47c90aec1031244af113098c2fa36
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:58:58 +0000

    profit rough warn art second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
