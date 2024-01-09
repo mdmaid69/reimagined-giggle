7531274f18f0a49d4e445f9c44f8c11fe9591683
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:41:14 +0000

    arrange occasion feature fight develop

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
