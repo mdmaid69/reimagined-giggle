90f79a90b84de2fe2b326893c864509e2b60815f
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:54:11 +0000

    news 179bite worth joke recognize

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
