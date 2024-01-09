0772c9d92511bc8944efa5e3f4aba90a564c44ad
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:35:56 +0000

    whole service loud rush man

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print(sum(range(10)))
