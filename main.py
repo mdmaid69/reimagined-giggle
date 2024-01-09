73c1458ddb72be47400edccd95b415e7f8d0d79e
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:59:12 +0000

    estimate catch extremely mistake respect

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
