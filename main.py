7fa90f5fc9dd575907a696cfc8569f6d1dc09f14
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:45:40 +0000

    present back shout church contain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_current_time():
-        return time.time()
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def sort_list(lst):
+        return sorted(lst)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
