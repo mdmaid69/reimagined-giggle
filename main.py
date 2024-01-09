79535f2816acfd02f65f29cc32b39fb7e0455e93
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:54:24 +0000

    lunch hollow male buy sea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
