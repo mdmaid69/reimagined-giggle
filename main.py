e5bf41a5892393e199578104e55d15ef4cbec56a
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:21:33 +0000

    petrol miss declaration big 568energy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import tensorflow as tf
+print(tf.__version__)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
