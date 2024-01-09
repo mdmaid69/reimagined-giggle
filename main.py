31514cd522f596d48b40037121b129e07f4fc64e
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:11:32 +0000

    instead skirt let force back

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def find_min(numbers):
-        return min(numbers)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_density(mass, volume):
+        return mass / volume
