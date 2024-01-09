e18e3faa47abd0d24e71181ad9034de8c584f49b
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:07:19 +0000

    workon mood acceptable pause secret

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import platform
-def get_os_info():
-        return platform.uname()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
