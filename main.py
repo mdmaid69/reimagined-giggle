b04a0dbf378455589429951f8de98dc975685161
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:53:20 +0000

    community difference breakdown rush black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
