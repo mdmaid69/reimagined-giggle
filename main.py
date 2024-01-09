e79047ff888f84637449f4c37d16d5c5350c1bad
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:24:29 +0000

    curious store weak here OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
