1c18c61e9ef3523be1ca6e458ffc780a5f3f2069
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:21:59 +0000

    compare side activity authority trousers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
