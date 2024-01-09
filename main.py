a7eb98775fdca48495ea08625917f18448212f17
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:50:36 +0000

    bored process movement whistle later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
