46c7ffdb87934042beba68e422fad129528d541f
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:38:54 +0000

    eager getback absolutely thumb emphasis

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-def find_min(lst):
-        return min(lst)
+def calculate_pressure(force, area):
+        return force / area
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
