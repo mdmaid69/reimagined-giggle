48e30ccd1a5a747e69207394246d19279ab43e54
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:55:04 +0000

    type refuse second century block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
