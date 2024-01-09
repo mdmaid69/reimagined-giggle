aa64a41989fdc4adb01da7a7da90e2d2fb6b8949
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:12:49 +0000

    excuse eager smell noone none

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def subtract_numbers(x, y):
-        return x - y
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
