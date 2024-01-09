8e732147ad123ac4bc34bbe6353f3b85c901682d
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:42:34 +0000

    store blue if church complain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
