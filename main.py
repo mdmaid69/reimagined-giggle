4756f33f8422ae76e21ffce34297529309822373
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:58:03 +0000

    effect smile tap music in

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
