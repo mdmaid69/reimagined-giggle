7f5184c0f976658f11fa65a4b3c5d93eeea03b1d
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:42:14 +0000

    variety inquire floor smell correction

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import datetime
+def get_today_date():
+        return datetime.date.today()
