042dc8bf667fce3b79e036ec9d550f89c0d948c1
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:53:44 +0000

    phone always autumn lock season

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
