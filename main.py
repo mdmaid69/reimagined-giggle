2140c2f744fbc8aa9967b7f73f6730b77fd56abd
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:04:07 +0000

    system desperate plenty change soil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
