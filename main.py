3e44c87870eff901a1aeb2530549660a8406c32a
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:41:48 +0000

    design cover sort test save

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def find_max(numbers):
-        return max(numbers)
+def is_odd(n):
+        return n % 2 != 0
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
