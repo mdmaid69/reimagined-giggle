08ea5d6f5abfef5a6fe3a616d8c5e3dcc6a7031a
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:01:55 +0000

    own long form power born

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def multiply_numbers(x, y):
-        return x * y
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def add_numbers(a, b):
+        return a + b
