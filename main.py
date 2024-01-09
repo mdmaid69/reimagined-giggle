755a636c6b66132e1c8b03853971b4827323972d
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:26:44 +0000

    heavy develop bag set tell

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def count_words(sentence):
+        return len(sentence.split())
+def is_palindrome(s):
+        return s == s[::-1]
