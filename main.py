c1b27b41384c68f560e6caa9acaa7ce5022d0ef5
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:02:38 +0000

    maybe firm careful store majority

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-from collections import Counter
-print(Counter("hello world"))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
