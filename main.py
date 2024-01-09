720bd57ba8fa55c4e7a2b06655e8c480bea858e7
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:07:27 +0000

    brave fill upwards bringforward follow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
