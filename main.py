717f2905c75ff49a562d594ae3b7eceaf93257cb
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:23:15 +0000

    hide object weekend 564encourage although

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_logarithm_base_e(x):
+        return math.log(x)
