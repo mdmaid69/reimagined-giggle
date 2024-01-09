17692ed17c08108dea0e7db4115c8ada46037305
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:18:48 +0000

    unpleasant shake perfect fault whisper

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def square_number(x):
+        return x**2
