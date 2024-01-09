06e040cf94b1b80f5cf76fa7d08c4eb2cb17e431
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:24:20 +0000

    heavy march benefit minute bunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
