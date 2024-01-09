a45ea0211c2feacd1aae3fb4f7aed3a62840e000
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:27:02 +0000

    freeze mysterious responsible pay realize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
