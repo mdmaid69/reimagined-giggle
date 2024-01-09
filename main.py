1ecf14e799dd5a693b7e6d38c1b37daf1d290775
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:41:30 +0000

    knock Ff maximum history snow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def convert_to_octal(n):
+        return oct(n)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
