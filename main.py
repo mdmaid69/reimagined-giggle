7de933769999bb62cb8d0126304db7b57b14423c
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:22:23 +0000

    naturally prepare off since previous

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_string_from_array(array):
+        return array.tobytes()
