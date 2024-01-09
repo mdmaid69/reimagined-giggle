29b5805b1a2428511a65a186d21ef73862f2cb73
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:19:27 +0000

    none skirt valuable meanwhile beat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
