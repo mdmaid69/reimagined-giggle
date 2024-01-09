36b43eb49315031cb2433953bb5f8e82081c50e1
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:05:37 +0000

    lead flat follow risk guard

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
