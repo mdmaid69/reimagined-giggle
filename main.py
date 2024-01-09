b9f2aa568800eff9362f55ee2e77da3efe34c5ec
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:35:11 +0000

    clue separate in operate sense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_list_from_array(array):
+        return array.tolist()
