b41e32e1dc5d941785e3caf77d4f064962aa1fda
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:29:57 +0000

    condition sound workout hardly year

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 text = "Hello, world!"
-print("Characters:", len(text))
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+print("Words:", len(text.split()))
+import tensorflow as tf
+print(tf.__version__)
