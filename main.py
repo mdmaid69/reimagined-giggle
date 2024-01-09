849fc68d485e2624333bd6f8c4d55e9ab5c1438f
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:53:41 +0000

    sore pass together hot reject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
