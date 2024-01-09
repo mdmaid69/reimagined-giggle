34696350997c3f093caf122c103e8fbc7e5a17d2
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:23:01 +0000

    dry crash scene meet few

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
