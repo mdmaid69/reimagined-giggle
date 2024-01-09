7a7875ff12616bbe74515391b58084a8c9f92c96
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:16:59 +0000

    display ring hollow uponon government

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
