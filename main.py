2a10dbf2743c6d628872185805d204097829a710
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:26:55 +0000

    takeout because tread consult notice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
