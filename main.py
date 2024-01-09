aeab945aad4e61cc46172bfc9e53a2d0fa689a23
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:46:11 +0000

    change book giveout pay giveaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
