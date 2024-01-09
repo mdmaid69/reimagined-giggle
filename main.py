f3ddb02c98fdfc0bb49377a484401b6ce9d5512c
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:11:39 +0000

    bin identify offend slight cool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
