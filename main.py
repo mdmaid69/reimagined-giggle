f808439e914a674a5ca05dbd8e592214132c49c5
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:21:19 +0000

    win telephone influence fortunate impressive

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import sqlite3
 conn = sqlite3.connect(":memory:")
 c = conn.cursor()
 c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import getpass
+def get_username():
+        return getpass.getuser()
