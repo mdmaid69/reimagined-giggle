d3138e92a8bd8fdcdf3f5ec4014b001cb438fb73
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:32:44 +0000

    level rub rob pretty shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def convert_to_binary(n):
+        return bin(n)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
