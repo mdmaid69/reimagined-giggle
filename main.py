73ca16d53595e0a89fa489e4bf840618df2d6e79
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:50:00 +0000

    niggaacademy influence usually public its

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import array
-def get_array_index(array, item):
-        return array.index(item)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
