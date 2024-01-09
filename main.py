47ae534b86678819bd6ac11b0e8e36e597b13ff5
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:40:14 +0000

    happen thing disagree sensitive meaning

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
