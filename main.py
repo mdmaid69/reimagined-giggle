c536f97a620f6d882c2a12abb601cf21745dd7cf
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:53:08 +0000

    forget drive throughout anywhere exhibition

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
