86c0a5f3a785e40f281ea0d44b506b5cd74c8295
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:12:34 +0000

    attach must ilovedicks female unhappy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def get_bytes_from_array(array):
+        return array.tobytes()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
