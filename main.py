b5fac3f176942cbfe6b6c4e63a3ef841271f8987
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:15:35 +0000

    the injury problem permission tool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
