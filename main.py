d0a96b4f0a76e42ce7c8e4ed9d4c82d5cb905c75
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:02:36 +0000

    side goodbye spare far Gg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  def multiply_numbers(x, y):
+        return x * y
