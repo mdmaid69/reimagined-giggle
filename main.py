6feb48165dc171ae82471470eecc23f228b1c306
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:54:15 +0000

    beneath pleasant state year experience

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def convert_to_octal(n):
-        return oct(n)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_power(work, time):
+        return work / time
