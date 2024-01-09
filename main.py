3c68a866b618309f306cc73728f1588624ad6963
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:06:12 +0000

    grade pollution pen and mass

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_itemsize(array):
+        return array.itemsize
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
