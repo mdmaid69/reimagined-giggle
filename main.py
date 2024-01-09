d453e9f38385a3b4190bab51ee00642d39f4d675
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:09:36 +0000

    understand equal pronounce morning 571enough

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import time
+print(time.time())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
