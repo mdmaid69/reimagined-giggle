3e68ea5b4aae40047836d83bf8c6c8066a0a3f1b
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:39:44 +0000

    money news adjective rush either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_combinations(n, k):
+        return math.comb(n, k)
