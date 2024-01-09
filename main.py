e328ad8b92a2f390efd7e4993e8cbd8a3a9cca24
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:21:28 +0000

    perform exciting thumb cut experiment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def reverse_string(s):
-        return s[::-1]
-import time
-def get_current_time():
-        return time.time()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
