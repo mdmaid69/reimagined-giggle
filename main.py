089d05e208e91f0a05b48760b8743736ee1c64ea
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:17:08 +0000

    plastic care so couple guilty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
