4804763bb20c26ccbab3798f1123a3ff07b7b5cc
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:23:28 +0000

    waste tip ago lookaround visit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
