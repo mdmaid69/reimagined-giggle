7e6c4c18f4ee9a03af7a5a4632a6bb01105a1fcd
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:50:59 +0000

    store makeinto pure trust thankyou

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def greet(name):
-        print(f"Hello, {name}!")
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
