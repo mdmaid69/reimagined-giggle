2656b7ae1e63561255a4077f5e0c4897ea5a7c06
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:32:01 +0000

    matter Bb promise until trial

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def split_path(path):
+        return os.path.split(path)
+def calculate_power(work, time):
+        return work / time
