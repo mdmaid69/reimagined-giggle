80b1dbb28a62947b3c37b84a84b855dd51494a14
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:01:12 +0000

    middle flow exhibition everywhere pleased

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
