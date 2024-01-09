23d7e4608a6f6da41fc68005678f1e2ac3f1d633
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:42:57 +0000

    pronounce compare continue ring praise

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def square_number(x):
-        return x**2
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
