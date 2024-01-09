8ce1c0260b512b496f0fbf2203d1e5bb174be4e6
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:13:17 +0000

    firm each course opposite petrol

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
