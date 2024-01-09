25289cb67425f976f72c1ed367e5645f3740ab2e
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:50:21 +0000

    sensible pencil clothes bar thumb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
