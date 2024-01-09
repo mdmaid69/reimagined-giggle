31a6ad70bb6b051199cc1bd97bbae21aa48bd880
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:48:45 +0000

    place view exit tragedy beyond

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
