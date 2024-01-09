46f30f6a1f69941b73bb961797026ef5c4ad98f3
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:46:46 +0000

    less steep increase card avoid

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-print(time.time())
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_acceleration(speed, time):
+        return speed / time
