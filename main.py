8e6595284dc260f461f020fb5fc1540c4549bd31
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:52:21 +0000

    tip low past matter size

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-def divide_numbers(x, y):
-        return x / y
-print(sum(range(10)))
+name = "Python"
+print("Hello,", name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
