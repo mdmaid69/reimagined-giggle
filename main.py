35172f3d6e6cf53fc2a546014ef763d8d4aa4326
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:26:52 +0000

    survive your bend waste of

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def is_odd(n):
+        return n % 2 != 0
