022f75683a3ff77a7cc59f1adb6f1318acd600b5
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:18:13 +0000

    turnout used part hurt rough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
