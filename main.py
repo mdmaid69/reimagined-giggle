47af3bea1a6da7b377db869fad57cb2e5711624e
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:53:08 +0000

    include workon trousers frequent disturbing

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+print([x**2 for x in range(10)])
+  import os
+  def get_current_directory():
+        return os.getcwd()
