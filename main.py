3826c72b45ed8156055b200571809b55c82fe810
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:24:12 +0000

    forget fasten reduce quickly choice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import sys
+def print_python_version():
+        return sys.version
