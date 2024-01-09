d1aad96851c1d2314fd4ca11d7ce116c924953db
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:37:48 +0000

    woman shop drop progress display

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import os
+def get_current_working_directory():
+        return os.getcwd()
