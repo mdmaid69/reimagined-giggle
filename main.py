b8dbfb2f9393fc0d0b44f9e01f8162e2e98cbc12
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:48:20 +0000

    takeback one round understand fine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
