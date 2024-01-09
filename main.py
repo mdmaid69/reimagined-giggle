ba8307491bc5206e992a6091006fe3ad723b1998
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:30:20 +0000

    check takeaway back sleep sell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
