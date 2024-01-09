f8fb470c79023ff9804993268a28bfd8bd589f15
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:24:10 +0000

    action experience conduct nurse 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
