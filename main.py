cc8f1d1f67a65757fbad343348cac2c24696f09d
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:43:16 +0000

    design transport damp shape well

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
