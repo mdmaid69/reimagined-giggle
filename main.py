a472147d28635c170776876a770627b3ab4f67d7
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:22:17 +0000

    bringup wheel even identify indeed

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
