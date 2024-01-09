89b67749ca5c3f5126663be2c0b7510ec56012a3
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:15:18 +0000

    turn programme contribute shout of

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+import datetime
+print(datetime.datetime.now())
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
