936a98a7fa16846644702a3eedfb041a05d3317f
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:29:44 +0000

    impatient closely shop attract approval

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
