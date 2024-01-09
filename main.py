be27953cd677ea97bd211b49d9f3ebb3432023ae
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:27:16 +0000

    continue comein complain nervous keepup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_power(work, time):
+        return work / time
