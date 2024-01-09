e131a09afab659942d75d963728a3e34bc4afc77
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:25:11 +0000

    degree careful popular Vv rain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
