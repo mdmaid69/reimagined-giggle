611ddc34fe49c11b12a6df3e34cde8d9abd58edd
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:38:28 +0000

    train form yet work store

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-print(math.pi)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_max(lst):
+        return max(lst)
