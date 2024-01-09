8bf3f022f57345d86a9015609ef6f8b545bfbcb3
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:29:17 +0000

    glad where notice truth post

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+print("Hello, world!")
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
