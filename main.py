b6d9a55be8686b613f2cb0e47e68fa0639f33b0f
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:01:56 +0000

    tip slight light scheme attempt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_list(array):
+        return array.tolist()
