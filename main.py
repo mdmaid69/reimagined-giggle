bb8600606149137b574e04c489057d35a3af0a6c
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:25:30 +0000

    wide store explain spring chase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
