39b8669a316697244318a0521b2d5a0c04928602
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:18:50 +0000

    why information actual remark congratulations

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_min(numbers):
+        return min(numbers)
