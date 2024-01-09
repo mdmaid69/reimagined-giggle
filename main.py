55bbeec8f4fe7dd4675dfa4b7b87e7bae5ab10a1
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:14:26 +0000

    moral comfort meet ground national

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
