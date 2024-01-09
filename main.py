4bd35c0f869ec4206ac970112abdacf1bd192b4e
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:41:53 +0000

    noisy funny so culture store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
