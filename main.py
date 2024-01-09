294fd150fb4922629261d68d606f6af0b7d39882
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:24:21 +0000

    trap example such Uu minor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import time
+print(time.time())
+import datetime
+print(datetime.datetime.now())
