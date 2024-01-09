ef9ede5ee8bf9c80b717e27ec675051b7e03e596
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:46:23 +0000

    see long usually appeal relation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_min(numbers):
-        return min(numbers)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
