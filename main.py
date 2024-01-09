c3665810d7746c0711300a34dd86cbeef549a5e2
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:33:42 +0000

    choose material godown behaviour deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
