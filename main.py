c39fb55557b796a3040b63ed504edcaf1e9e95a3
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:17:09 +0000

    join organize street giveback work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
