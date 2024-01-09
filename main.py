2ea6741c1a202f8fd04dd7446c7cdb63160a1cf1
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:22:44 +0000

    lie luck interruption voice interfere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def get_array_as_repr(array):
+        return repr(array)
