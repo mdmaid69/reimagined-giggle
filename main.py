ebde31ecf8ea247a950460f9642facf9c65ba9c9
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:40:19 +0000

    patient that spread move immediate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
