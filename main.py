dc8034a47e65a0e5d3049ada6a32b74eb2f5497d
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:41:19 +0000

    sick private invite admire since

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
