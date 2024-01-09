d994942afa1850fbd67e4f71d5ab6425959cd785
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:22:24 +0000

    force flow waste interruption excuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import sys
+print(sys.version)
