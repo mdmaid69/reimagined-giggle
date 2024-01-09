fcbcb8127ab3673db8740b01e322fd46681fc55e
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:54:17 +0000

    if petrol vote say street

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
