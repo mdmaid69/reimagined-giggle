f6a69555ec27db18904099e1bfd89c9ca0a3bf3e
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:38:36 +0000

    workon platform freeze set pollution

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def convert_to_hex(n):
+        return hex(n)
