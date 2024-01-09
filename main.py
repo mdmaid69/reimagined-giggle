ea00db5a5ec7b45250d3c63b0c38a4b50f018c49
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:45:04 +0000

    tense runinto life colour plant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
