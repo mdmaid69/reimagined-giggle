75ef9db9f3a4d35b4cd6d218ad79f50f5662301f
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:26:05 +0000

    lose machine ought whole delight

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
