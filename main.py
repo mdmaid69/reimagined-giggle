739649bcd9fc8bfd376de3f861334a57355aac2b
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:02:12 +0000

    just fierce really command accident

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
