7fd91e0c28c4a10cd05f98154d90dffe6d0d885d
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:48:36 +0000

    activity promise mistake frightened emphasis

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
