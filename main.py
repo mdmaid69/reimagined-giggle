72369a7c3e62fcbdd429f18daa9262ff0472b94a
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:49:22 +0000

    himself consider fly thinkover target

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
