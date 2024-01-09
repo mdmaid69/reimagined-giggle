8f8aace18980dd5e3b089de5972a4df1a121b7d9
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:07:07 +0000

    prize uniform flood arrangement stamp

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def find_min(lst):
+        return min(lst)
+for i in range(10): print(i)
