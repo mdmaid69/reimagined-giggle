8a2c7d356a3db4f2b302537213c8ecb5ae7994d6
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:31:06 +0000

    watch birthday your occur desperate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
