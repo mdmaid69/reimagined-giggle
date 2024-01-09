9ec08a89bbdf183582bd57404f8aceb8f7ff3df7
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:30:08 +0000

    argue towards point fast break

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_base_name(path):
+        return os.path.basename(path)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
