b1a8e321779939de2c7e1b953ecd0bf1b705dcc0
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:18:58 +0000

    development yesterday thing finally weather

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+i = 0
+while i < 5:
+        print(i)
+        i += 1
