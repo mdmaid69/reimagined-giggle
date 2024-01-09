2a10c5952d8b5197abc9c39ba4213917b8df5230
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:15:52 +0000

    party opposite breakfast space theirs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
