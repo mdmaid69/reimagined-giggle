f4f38b96cc70ce20c7a9ea17d9075451c3125319
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:12:07 +0000

    dish calm comfort sharp treatment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import math
-print(math.pi)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
