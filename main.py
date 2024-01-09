ef7cc4734683fd6bb2f2c8e806ac6517f0c4fbe4
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:08:41 +0000

    complain winter clearly let brave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
