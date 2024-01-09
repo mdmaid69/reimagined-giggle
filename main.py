2ba51e25ede3b06962387b870440c9928087a5b6
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:08:16 +0000

    organization die ocean another throw

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
