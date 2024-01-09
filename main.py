bac60645cfa2531a16cf5ead8c9811810e50a633
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:21:13 +0000

    say save supply pool sorry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
