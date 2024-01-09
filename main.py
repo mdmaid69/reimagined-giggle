94fa9a921b44debe99c34721004ec95819893d9c
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:19:11 +0000

    old wire Qq band concern

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_power(work, time):
-        return work / time
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+text = "Hello, world!"
+print("Uppercase:", text.upper())
