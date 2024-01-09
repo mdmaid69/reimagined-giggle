f04b9ab0017efd9089f4217dce4b5275d9ef629e
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:04:57 +0000

    admit praise impressive object giveout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_acceleration(speed, time):
+        return speed / time
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
