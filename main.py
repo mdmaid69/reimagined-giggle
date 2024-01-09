e40f73a11e9bf167dbec203fd85aa87220ba03a6
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:08:10 +0000

    stop mark foreign use blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def remove_duplicates(lst):
+        return list(set(lst))
