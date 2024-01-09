6b4190c4565ded0ff6fa8c4b82e6472beff0eb38
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:07:57 +0000

    shall pupil first much lift

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
