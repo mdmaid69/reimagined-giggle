c82c13a0164df125b94ff17b9f465a5eda4e1090
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:55:53 +0000

    ours colour development brain subject

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-print([x**2 for x in range(10)])
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
