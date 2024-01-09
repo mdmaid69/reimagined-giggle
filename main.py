382bea63dac46d957f0bbdbb9de6b828bb04e4f1
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:04:38 +0000

    declaration goup would its live

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def print_python_version():
-        return sys.version
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
