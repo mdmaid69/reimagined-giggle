7434a0a633fe352f0360308649d8141c4ee3796b
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:04:56 +0000

    introduction quarter trade steady plain

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def square_number(x):
+        return x**2
