bec4bb3c3a7a90d7453f237616b5690728dac8fe
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:50:19 +0000

    Uu supply limit board send

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
