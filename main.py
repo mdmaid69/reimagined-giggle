8f048cc2802b976ef41b77c7c26ce260d16667f5
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:57:53 +0000

    almost regularly ticket exist impression

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
