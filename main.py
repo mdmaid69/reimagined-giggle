d59c3c57ab90bc8f941d831b36d3fbe0913bf44a
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:20:51 +0000

    previous fall later key thinkof

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
