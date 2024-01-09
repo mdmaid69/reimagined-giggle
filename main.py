4331d423a6be0b05e0726cd7085ae4354045c6be
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:47:56 +0000

    belong sure ball tragedy last

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def square_number(x):
+        return x**2
+def calculate_speed(distance, time):
+        return distance / time
