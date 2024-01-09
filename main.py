20ad3cdc09055fb3fd086b1a00dc6d9ec6d2bc0d
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:14:48 +0000

    store proof embarrassed closed cover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+for i in range(10): print(i)
