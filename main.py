96bc617da05c86d495aff75d4730872b74e47168
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:28:03 +0000

    relax giveup pair earth polite

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def square_number(x):
+        return x**2
+def calculate_perpetuity(payment, rate):
+        return payment / rate
