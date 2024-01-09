1874fe14b1d583563479865f3a00182eef89686c
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:36:17 +0000

    much because Hh example motion

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_power(work, time):
+        return work / time
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
