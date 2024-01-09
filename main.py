55fdace8d119cbfd8b0f6b8b7240c274245d488b
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:49:00 +0000

    cut steam squash advice push

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
