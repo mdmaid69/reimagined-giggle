b773ff5be78a44cb0059e5d279f02e968176caa6
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:13:29 +0000

    Ss direct comeback address word

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
