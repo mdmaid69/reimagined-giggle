41c3de6c914a2e6b68b354ec2a967ba8adb406dc
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:28:31 +0000

    contrast mark finger Yy develop

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
