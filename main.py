f5fe7ca59a6f2e3d74c7534767bb1931cd701178
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:04:51 +0000

    damage capital on round guard

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
