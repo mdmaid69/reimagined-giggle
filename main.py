2418851e809772b10069e921f11e13756b64a5f9
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:08:55 +0000

    can straight thinkthrough too off

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
