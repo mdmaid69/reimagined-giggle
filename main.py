beab308409809c50d458b0eb3f994c2bf905746e
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:05:05 +0000

    extra trial doctor precise thorough

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_pressure(force, area):
+        return force / area
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
