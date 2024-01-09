c9dca4890d404053d004553ef789ce0129f83275
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:39:34 +0000

    actual quality difficult set view

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
