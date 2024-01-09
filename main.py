75f52786d12d2543d378b63eb9a9b078d4740d1a
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:26:07 +0000

    fear spring board dance female

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import random
-print(random.randint(0, 100))
+  def convert_to_binary(n):
+        return bin(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
