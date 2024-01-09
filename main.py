992a3658e27c80925995c4528fa908a097b4fc98
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:37:26 +0000

    be bring trip block family

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
