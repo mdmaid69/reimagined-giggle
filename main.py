d7a15e091dc201cec7fdb25f1c5f98be976d634d
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:35:27 +0000

    gap surprising probably more kind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
