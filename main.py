ced1d3a84b32da8c385fae6e6085c4a410bfe4c8
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:40:22 +0000

    shade underneath question waste whom

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
