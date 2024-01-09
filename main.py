cbe9c10868d38f53834350b4eb7a93b3beaa8a9a
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:21:01 +0000

    low dark trap until strength

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
