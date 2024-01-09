5ce5df040cc0e88faa7961282aeb275cf4a08408
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:25:00 +0000

    protest raw again though worst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
