1a50e44d7f671e7db416b8fd42c2654180e16b49
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:37:16 +0000

    everywhere connected birth satisfied corner

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
