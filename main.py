321ff2eab0d9b94c1c8a898483129e8f63cc7e1e
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:09:56 +0000

    blame spill combination treatment dull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
