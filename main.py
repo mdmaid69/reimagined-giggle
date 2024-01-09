cfca7dc192f5e39b964a87a4ceba7420d7e5a4a9
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:22:31 +0000

    profit practise sell slow earn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
