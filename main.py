f38d55e5768c66e4ea00c8780bb779dcc1a3fe85
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:49:19 +0000

    workout 573entertainment exam sport exist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
