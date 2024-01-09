373e7a24af32e964b15756e72e56c12b9f559362
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:36:26 +0000

    lookup sweep straight giveback office

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def divide_numbers(x, y):
+        return x / y
