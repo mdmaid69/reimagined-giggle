e87b1c90f41f61cfe6f02182f8b3ef4e0635871a
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:48:20 +0000

    national shadow give some means

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def square_number(x):
-        return x**2
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
