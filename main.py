5d9a23a5897f2704798db55c8f5a2394a64fd43e
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:43:03 +0000

    Bb anybody familiar react explanation

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def add_numbers(a, b):
-        return a + b
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
