6b7fd50914d23b8c04c165163c8c69dacf958563
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:06:25 +0000

    politics start journey picture persuade

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import array
-def get_array_length(array):
-        return len(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
