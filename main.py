1728b074e417353fc2eacedd9c202c193894c80b
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:07:01 +0000

    Bb climb world pressure slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
