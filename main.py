401893346438f086c1d1ad4d737e90ed07b74ef5
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:29:01 +0000

    comment passage someone ours close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import getpass
+def get_username():
+        return getpass.getuser()
