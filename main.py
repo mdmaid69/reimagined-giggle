0403c9e87f0d7d58c96c1b70f65831757c5bf087
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:49:29 +0000

    never phone rather piece embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
+  def get_file_block_size(file_name):
         return os.stat(file_name).st_blksize
-def calculate_work(force, distance):
-        return force * distance
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
