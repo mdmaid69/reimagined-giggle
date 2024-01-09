1f908f3060b4ea8d511b40e26959cad5c4ecacf3
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:14:15 +0000

    carry draw impression unlikely fear

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_bytearray(array):
+        return bytearray(array)
