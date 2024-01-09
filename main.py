b9e32c035e8b579779e9ba04b7d41f4406567f2e
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:31:00 +0000

    opinion check 179bite argument set

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def is_even(n):
+        return n % 2 == 0
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
