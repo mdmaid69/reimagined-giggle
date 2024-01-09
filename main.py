bb9dce8abb3911d20a994e2d50633128aa2824b7
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:05:37 +0000

    safe definite suppose steer add

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
