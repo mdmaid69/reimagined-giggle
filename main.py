b71636f7b437e366d025afc1a8210dafdd2f49c4
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:29:29 +0000

    suit surprise Cc hardly minority

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
