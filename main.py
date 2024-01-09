8372f1a0f124133a912b026169776d0d60cb08a7
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:11:34 +0000

    she off confident holiday else

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
