d6993d73785626af7ec0a2d90a97ce8e8e3c2a33
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:22:51 +0000

    level score custom straight government

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
