a8ddc9ebe7d1e951097300c9bce52b702b1d838a
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:16:45 +0000

    string translate which afford simple

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+print([x**2 for x in range(10)])
