f256ba84472e0f749df7d851a1c55e4b96e4b233
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:30:00 +0000

    stranger afraid urgent shape indeed

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_from_string(string, typecode):
+def convert_unicode_to_array(unicode, typecode):
         a = array.array(typecode)
-        a.fromstring(string)
+        a.fromunicode(unicode)
         return a
