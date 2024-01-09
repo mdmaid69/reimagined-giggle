4958f12fdfe71203d6b31d6f7a2ba7fa22d83fe2
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:53:34 +0000

    mother cruel convenient correct wonder

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def count_elements(lst):
-        return len(lst)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
