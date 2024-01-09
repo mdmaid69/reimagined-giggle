a51c65289c44d03ddb4bf49deecc6e484985a98b
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:25:32 +0000

    drive dead chance claim treat

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-print([x**2 for x in range(10)])
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
