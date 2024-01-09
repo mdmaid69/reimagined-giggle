f82f68183b3f100481b9d40471b9e8189da0b54e
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:01:54 +0000

    trick repeat luck fast various

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
