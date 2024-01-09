7385714605cf51ac46c4e96c4b0cb8db55faea03
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:29:58 +0000

    heavy handle lookaround play then

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_work(force, distance):
-        return force * distance
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
