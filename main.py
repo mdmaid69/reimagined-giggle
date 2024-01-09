052911debabf59cd7e4c7c07e437a66931800595
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:45:30 +0000

    trust force impression object comfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
