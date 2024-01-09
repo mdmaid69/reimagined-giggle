6e906fab86af432c6f19a020116114ce79ef6a3d
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:07:29 +0000

    body ring pay present broad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
