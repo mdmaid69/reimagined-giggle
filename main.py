357ce0aa7d8046902187c151dfad735979187131
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:05:24 +0000

    money lost us love Nn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def find_min(numbers):
-        return min(numbers)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
