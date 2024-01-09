07faee4bc5c105a860d4caa550b2bfa87c6fca2b
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:40:09 +0000

    scheme mistake shout party power

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
