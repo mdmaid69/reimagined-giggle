3e9b287c3ca0343fea7f839ae21a502e994fc1bf
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:33:49 +0000

    us information calmly period straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
