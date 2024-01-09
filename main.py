31ddf62f89e776f4422e531fad500888e546fd29
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:56:18 +0000

    slice dinner both for circle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
