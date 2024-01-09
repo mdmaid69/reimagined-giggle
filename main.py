6a04d8cb6d8b2292154efc4f0f26fe70fdce1fbe
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:19:20 +0000

    explanation minority someone first film

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
