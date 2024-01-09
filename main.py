7d6e724c4cfc153e28384982619328ac7a8ecdd8
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:30:33 +0000

    godown think rescue impressed little

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
