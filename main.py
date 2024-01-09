4a128c174bc15545bf510178210e9f070ec7bc16
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:46:21 +0000

    pin welcome form skirt back

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
