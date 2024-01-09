89d8ce804859e32246c1e59072cded3d4b288050
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:57:37 +0000

    bye play steam glass practical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import datetime
+def get_current_date():
+        return datetime.date.today()
