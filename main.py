343176d17cdc028e44399e34c437fbfff88a1cc8
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:43:10 +0000

    target turnaway spray speech Bb

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
