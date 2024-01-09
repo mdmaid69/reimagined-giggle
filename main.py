0ccc76bdbbef1fa3e80cfc1ea51c7e4475cea9e5
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:03:59 +0000

    ashamed ought minute each talk

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
