1723f06cd5ad93e9134302741224b02dcef80bc3
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:24:32 +0000

    term sentence lead where post

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
