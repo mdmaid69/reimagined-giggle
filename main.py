0b4ad5a0a81e918503cb4ab94b56db14323fcc04
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:29:25 +0000

    play surprised breakfast perform married

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
