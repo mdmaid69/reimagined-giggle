6c5a3c0600a3fdff0a24d867a8564c5b5209418b
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:51:45 +0000

    never calmly Itself along bury

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
