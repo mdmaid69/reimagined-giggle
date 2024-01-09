f3376d362d239263a72970daa8893100d2e17ced
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:10:20 +0000

    gas 570enjoyable strict terrible improvement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
