a2b2328e2daf9728479289908bd4f9d2f3eae456
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:00:29 +0000

    lookback(on) fetch interfere action drink

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
