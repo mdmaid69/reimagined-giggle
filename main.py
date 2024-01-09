5cd86afeaae828339bbe4a6f6fd6067d27bd64c1
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:55:09 +0000

    round yes disturbing search whether

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
