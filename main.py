03170e39f993f4022bceba871fa96be27639931a
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:27:58 +0000

    confused gap under himself rarely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def clear_array(array):
+        array *= 0
