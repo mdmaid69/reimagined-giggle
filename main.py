0ef7f88490b84e2c481902aaf0dd63bd4f1abf8a
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:28:28 +0000

    perfect figure child direct swim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
