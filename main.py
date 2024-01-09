3d70151f12d49728ec93335822dbb2756889c8ae
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:08:57 +0000

    recognize address treatment completely unkind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
