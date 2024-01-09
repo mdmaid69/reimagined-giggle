66e6a0a0aa616bde6c0ef1f40a627db74231c2a7
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:22:58 +0000

    dry rather conduct interested connect

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def cube_number(x):
-        return x**3
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
