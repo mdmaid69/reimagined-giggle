a870a9f103dc8b2b3186f35474298b89cf0e448e
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:30:20 +0000

    intelligent thanks long form loose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_work(force, distance):
+        return force * distance
