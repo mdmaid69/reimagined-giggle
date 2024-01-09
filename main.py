9ee35768f7c9dac4aa521485044a9e97be587939
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:56:49 +0000

    protest tap sentence needle sweet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
