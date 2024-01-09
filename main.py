8092cdce449c48ade3ceecd8840a967bb6be9a2c
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:33:52 +0000

    close expect praise vision announce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
