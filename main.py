3a6ea110ee2ce598a12c1395362ab7487f0f9866
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:27:28 +0000

    any case luck neither side

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
