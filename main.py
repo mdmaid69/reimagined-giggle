343763b75f24ac659c6e3219d6571d3f4aac9d86
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:13:52 +0000

    family mean deal yet clearly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_density(mass, volume):
+        return mass / volume
