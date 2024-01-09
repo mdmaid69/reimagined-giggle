66d5146a0d089ff6f7bb68afb68ed0567335d0ef
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:17:14 +0000

    quantity hole care preparation degree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
