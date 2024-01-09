a068e52687dab02a0eee7cc5f323720d7a112698
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:27:54 +0000

    touch exaggerate unpleasant children open

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
