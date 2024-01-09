fd559bc36eb31691c18e0de7a42d74f8fb818630
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:10:02 +0000

    kick burst mean true whatever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import sys
+def exit_program():
+        sys.exit()
