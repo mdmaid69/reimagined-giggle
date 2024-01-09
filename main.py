cb48fc0cbb208d473b3bcd851e5b3d52ccbaeaea
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:59:52 +0000

    argument grateful chief hand identify

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_acceleration(speed, time):
-        return speed / time
+import os
+print(os.getcwd())
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
