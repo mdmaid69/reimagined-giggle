8b9b4cb0146bbc0a5a270435b44417b0d4d72ec6
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:29:41 +0000

    ready correct tool characteristic will

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+print([x**2 for x in range(10)])
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
