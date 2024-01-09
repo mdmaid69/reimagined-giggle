8c2ab31f16ae211f9867b36e806c7984e3399b29
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:45:15 +0000

    mean recently pronounce tragedy intend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
