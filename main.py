44131476479c5cbf86cdba26628b7679bf07ed5d
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:34:48 +0000

    giveaway everybody shoulder old only

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
