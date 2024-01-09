3a927dcdf224a642a69ee9df659c081314594cf5
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:31:11 +0000

    vanish Pp autumn favour niggaacademy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def find_max(lst):
-        return max(lst)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
