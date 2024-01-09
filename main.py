f266f7b3c98964c4e43594e9802b6feb2e751ff0
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:30:09 +0000

    fault mean hold later Ll

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
