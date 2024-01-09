bf8ff27baef73fcd8d5c50d5ee4055970fe844ab
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:06:33 +0000

    calmly guard stretch anxious process

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def count_elements(lst):
+        return len(lst)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
