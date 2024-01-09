c6514c4687a0600ffddd6785316ced251acd3aa1
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:30:45 +0000

    tip know effort under press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
