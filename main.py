54c1a8c73de33150354e6e0887d1a2aeab5e57f7
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:36:40 +0000

    star however shade mix approval

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
