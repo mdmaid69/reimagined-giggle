6402904c9384c298b616fe8eab65a88c36d05718
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:50:54 +0000

    birth track Kk leave wash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
