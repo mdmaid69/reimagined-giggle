dbd25380ddf93ebd6c3cc027618d365ec37ee431
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:02:21 +0000

    finger press earn average sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def get_array_as_float(array):
+        return float(array[0])
