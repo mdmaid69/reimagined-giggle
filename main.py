5bc9ecfe7e098d2474e6a0376f82ed1f484fd233
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:53:05 +0000

    ever spite snow opposite callat

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-for i in range(5):
-        print(i)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import array
+def reverse_array(array):
+        array.reverse()
