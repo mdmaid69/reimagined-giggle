eb5d0b2985f66dac7ab7a89b75312bbf7b69cdcd
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:10:45 +0000

    pool mean be tax calloff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def convert_to_octal(n):
-        return oct(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
