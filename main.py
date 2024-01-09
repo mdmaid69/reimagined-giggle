bf40a231b633fdd285807b6ffcbb46262638495c
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:05:59 +0000

    shopping run important difficult business

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
