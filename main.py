07564d52fe1721791c456b7685a4a0123f8816d4
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:46:16 +0000

    promise rescue crazy point block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
