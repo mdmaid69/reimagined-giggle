d16b5adac50686cd90358cee9fbe4d56f2998898
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:39:08 +0000

    male downstairs throw beat original

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
