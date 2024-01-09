a29dda4ce381b1dc008e01ce4c0f6213c13c7fbf
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:43:34 +0000

    final delight leave contact wash

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def is_even(n):
+        return n % 2 == 0
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
