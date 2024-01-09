e39144be65585c796c89f6a222a3ea3b39d32b58
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:31:06 +0000

    thorough glad through danger occur

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def is_even(n):
+        return n % 2 == 0
