d6c9b30fc1669ed644ca00f1b3a61b01b31e5efb
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:51:59 +0000

    scream present strange quantity through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
