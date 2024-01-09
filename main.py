a3ce94c3494f93c9c20d548026a610128250fba8
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:45:19 +0000

    mention national exchange crisis both

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
