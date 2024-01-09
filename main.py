a759d54bd3bf18bdfdbf23fec95d89718bb7cf0d
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:46:22 +0000

    evening moment swimming piece stir

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
