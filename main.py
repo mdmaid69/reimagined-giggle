98149d98f7bceadc36738b492cd75ec58de996da
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:06:30 +0000

    Ww survive become contact uncomfortable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
