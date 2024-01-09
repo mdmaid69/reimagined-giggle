5e1f2034c7a915ce76f823a85366ca908be4f459
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:06:12 +0000

    battery than culture size uponon

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def cube_number(x):
-        return x**3
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
