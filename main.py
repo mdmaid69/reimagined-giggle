6415850d7597f85446ab568f14ef6173de4b10c4
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:59:49 +0000

    judge present quit board they

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
