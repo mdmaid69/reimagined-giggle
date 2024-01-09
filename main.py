685245e561fc70ed382a5a9f5a9d1603ede21286
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:59:09 +0000

    flight neither secret comment crowded

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
