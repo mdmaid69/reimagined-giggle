ca76fe914a94ec533a05b9913f35f87fbbc6c161
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:20:00 +0000

    read study intelligent deep always

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
