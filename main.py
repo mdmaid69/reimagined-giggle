3db1d26242d8af9f5f9a1a7e7f3f1a73275f3d0d
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:27:54 +0000

    moral tired history sometimes black

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import math
 def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+        return math.factorial(n)
