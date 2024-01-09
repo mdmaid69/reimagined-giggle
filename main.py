4448de1d7f16e34174d20c4f609a2f0a5afaa3fb
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:27:01 +0000

    square else uponon honest other

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import collections
+def create_priority_queue():
+        return collections.deque()
