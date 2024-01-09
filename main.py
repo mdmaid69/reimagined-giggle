d5ae9fa4a8d8d7bed4690b20db16219a84851642
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:03:22 +0000

    winter door your bottom complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+from collections import Counter
+print(Counter("hello world"))
