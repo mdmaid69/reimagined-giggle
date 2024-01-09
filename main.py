8d3a8fd359a0f7895ddb18024de97333073de55a
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:14:12 +0000

    transport provide fierce except church

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
