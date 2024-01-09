ee2ec482a15fc2ae54a6de9750656852c1befc44
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:26:52 +0000

    task number valuable high rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
