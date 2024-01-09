c5d6dc81357bcf963846635e6b3ad234a5f4ac24
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:06:15 +0000

    choose language or getoff free

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_palindrome(s):
-        return s == s[::-1]
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
