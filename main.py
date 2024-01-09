721dbaf1aea7011f5545753381ed1e05337290f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:18:56 +0000

    platform OKorokay trust tense mystery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import sys
+def exit_program():
+        sys.exit()
+name = "Python"
+print("Hello,", name)
