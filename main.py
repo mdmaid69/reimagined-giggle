aa8a75526a4b4424ea6dd632f828fca70b7202d0
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:41:18 +0000

    government opposite statement remark point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
