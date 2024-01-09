19bf0e27195c83360353a021d414959be9479434
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:44:48 +0000

    set appeal wave bus suppose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
