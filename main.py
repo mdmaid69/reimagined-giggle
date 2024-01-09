6fd54b9ac94615f3c117f9b7d24162d3b1b4459a
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:50:42 +0000

    pinch join until protest clue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
