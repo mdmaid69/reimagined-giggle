a567446b411983660896be0acf1d6ec68d1beb1f
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:31:34 +0000

    promise own human flood best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
