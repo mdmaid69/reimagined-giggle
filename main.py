4c9eb14888941e171328657d9465908a44bcf2b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:52:02 +0000

    either contents unfair finish sit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
