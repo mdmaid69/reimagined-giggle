e408ccd8fa91f20b1fa48b666bb2fb0fc6a4ebf4
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:01:14 +0000

    spite proud finger neither tape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_min(numbers):
+        return min(numbers)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
