59256d954a91b7334a809b4aa7018efad2d38784
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:41:44 +0000

    pen worse ball field excited

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-name = "Python"
-print("Hello,", name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
