037e88f854b7075b0115436c0556b4bc4fc38c90
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:14:30 +0000

    authority shop secret motion stick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
