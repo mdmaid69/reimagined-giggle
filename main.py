c4214a9110a226ef577f4acb741b0a271fd2147e
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:42:44 +0000

    aim will friend brave comeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def convert_to_octal(n):
+        return oct(n)
