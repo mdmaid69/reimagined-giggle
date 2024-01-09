234b7e898d2334a912dfb1b874b2d62bd19aa97c
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:51:56 +0000

    bury that for knock word

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_max(numbers):
+        return max(numbers)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
