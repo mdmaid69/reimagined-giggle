0bb8f09cf0cb6d23b54f2e5ba328a103f6e26258
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:40:59 +0000

    charge process present ever since

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
