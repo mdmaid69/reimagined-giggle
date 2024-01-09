b7fcf547c2832199b113be7f3fc3d2970d887981
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:24:53 +0000

    draw force squash natural careless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
