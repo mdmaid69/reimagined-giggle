11ab5ed1e64bcbb7a1cfb73ba37079e5c8bc3855
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:04:36 +0000

    challenge lay pay range variety

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
