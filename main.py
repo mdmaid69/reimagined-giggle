12f7c9af08809bb20f5b3fc517ab8ae144425e1d
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:09:13 +0000

    between herself confident watch fail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
