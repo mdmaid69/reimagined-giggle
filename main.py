cfc62d76177fce47035ef2329b737427001961d0
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:26:53 +0000

    national about reason health someone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def is_odd(n):
+        return n % 2 != 0
