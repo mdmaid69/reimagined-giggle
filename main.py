b29010f7490fd51063a5bb288dc03058f58b5459
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:03:52 +0000

    develop himself pretty good wherever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
