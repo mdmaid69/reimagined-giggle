1c2a11b909203e4b7f482f5a70c0cf2b277323f7
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:04:22 +0000

    whistle shout close once popular

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
