3a92988ebc820e6267935c5020464776f608225a
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:07:48 +0000

    translate small teach drop stranger

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def convert_to_hex(n):
+        return hex(n)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
