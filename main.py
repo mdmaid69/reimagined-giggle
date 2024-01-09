1d6b3ce6176f2fa9d12a1261ea9baa3f26a12dc9
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:14:03 +0000

    winter rise but attract tell

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
