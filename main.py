df8a413a52a5e68d28d48ed4b0b4fd7d8a5ea7d2
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:07:16 +0000

    sensitive variety smart paper support

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
