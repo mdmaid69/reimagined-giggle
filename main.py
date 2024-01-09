3d295d69eb314477ab5d1607ad2f0d427f440a26
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:58:54 +0000

    last tragedy reason tune favourite

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
