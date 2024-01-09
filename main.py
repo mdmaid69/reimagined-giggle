28b79958a9896934a4ccaa83e8040dd7bc1d7005
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:47:41 +0000

    hurt flavour entrance involve go

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
