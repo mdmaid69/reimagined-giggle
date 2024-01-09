9aa539b6bf5c3a602f25ad709f7c08d1651864a0
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:50:25 +0000

    matter official pocket used first

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
