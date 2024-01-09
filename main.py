112df3d7c53079bd4c4fb0f1817aae23939dd252
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:21:38 +0000

    rest confidence decision wear pick

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_work(force, distance):
+        return force * distance
