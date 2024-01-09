a4e045e5be83b1581ccbbd90c3eca646f47177a0
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:04:18 +0000

    record basic various needle send

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
