26bc4cdef83ef94896de5e565554d06609a26dae
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:30:58 +0000

    mention admit afford fear somebody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def split_path(path):
+        return os.path.split(path)
