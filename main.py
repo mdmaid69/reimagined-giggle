72f1bc004f4cb0bb792f7ec00783e134f6ba081a
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:00:48 +0000

    head stranger telephone grow past

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
