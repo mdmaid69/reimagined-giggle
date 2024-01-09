4e9fb27c87f1378d8e92f97580cd46ab27998e28
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:52:55 +0000

    love past succeed please maybe

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
