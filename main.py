a93bdfe43dc8f2ddfadf9b4f4d0ee081578bc5bf
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:58:07 +0000

    cut together pretty play occasional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
