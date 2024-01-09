69a110519ac03a435cd62b16f003be8d8f9ba255
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:39:32 +0000

    many tread set fact rain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def square_number(x):
+        return x**2
