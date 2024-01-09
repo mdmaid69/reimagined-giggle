7b7ca3ba7a65d30a5ad85aff55723e51774239d5
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:36:22 +0000

    average dance train yesterday juice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
