34061ef183afafb073c36dfa24e61284eeda5246
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:55:49 +0000

    turn which true stamp date

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_area_rectangle(l, w):
+        return l * w
