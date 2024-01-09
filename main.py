bbd1b04df1793351eb4b3e00200a63007a9f572f
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:09:11 +0000

    science respect naturally he heat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
