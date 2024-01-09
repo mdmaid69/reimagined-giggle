0dd6308ecc1d58b10675a9bccfb98a8e4c7c8e66
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:13:13 +0000

    floor pour naturally hear after

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
