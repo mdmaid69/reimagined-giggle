25bde1ea29cf9d80eaccdebc038569e65831f31a
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:13:29 +0000

    manner present happy same fall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def is_odd(n):
+        return n % 2 != 0
