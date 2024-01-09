3304c8be45bd6fe7d545df62d43ca81feab3b33a
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:43:43 +0000

    many type exercise guess ought

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
