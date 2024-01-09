79d6b8a1d0874a5c4af8941725dee43e2d28f228
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:37:57 +0000

    actual neither shallow speak any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
