029b796e3ef8f363a0f7207f69c3f03a3de34ec7
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:38:43 +0000

    geton among lend rubbish breath

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_area_rectangle(l, w):
-        return l * w
+def reverse_string(s):
+        return s[::-1]
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
