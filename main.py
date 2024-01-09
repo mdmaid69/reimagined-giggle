f63a3f704f31c26e096b2e43c0e77d44dc68dfb2
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:02:53 +0000

    might generally dick place admit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import json
+print(json.dumps({"name": "John", "age": 30}))
