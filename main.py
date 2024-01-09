337433ed2618940bc4c44d68fdde0a7274b47215
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:45:10 +0000

    recent care translate disagree object

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import random
+def roll_die():
+        return random.randint(1, 6)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
