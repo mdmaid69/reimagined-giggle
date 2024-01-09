3250ee13a5b7f310ade6815542edb5b176b50f2e
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:24:07 +0000

    ourselves shoot recommend breakthrough reduce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_pressure(force, area):
-        return force / area
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def remove_duplicates(lst):
+        return list(set(lst))
