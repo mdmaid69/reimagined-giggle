d16ad1c4d8fdf7bf66fffc0fdc613467dc3177c7
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:41:49 +0000

    cheek will rent peaceful doctor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_floor(x):
+        return math.floor(x)
