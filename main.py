74989e61fca20c68ab95dc6ddf21d3f7260c0ef8
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:44:16 +0000

    angry turnup bar hurry progress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
