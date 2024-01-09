a71899160164beeea81c371bf9cc2d871165182c
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:54:00 +0000

    move rid bury sort extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
