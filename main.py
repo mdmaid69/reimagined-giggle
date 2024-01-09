29ed6d60e2d7b1a6648f019241e8206e1726a2ab
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:19:41 +0000

    past direct crowded plastic rob

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
