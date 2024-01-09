aac36acc929a6b005e366a9f3c5ed66e2f5c11b8
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:00:47 +0000

    reduce together big substance 564encourage

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
