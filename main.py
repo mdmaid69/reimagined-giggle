9fa6eab0ed4e14ea5fd17c04a6acbdb02440a4af
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:35:18 +0000

    print yes listen show last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_area_rectangle(l, w):
+        return l * w
