0e1dc203d5c06b471a8f7891190db4d3ffa34d11
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:23:35 +0000

    answer reaction act term pay

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def is_odd(n):
+        return n % 2 != 0
