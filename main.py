de35b0f46ff99fc8e27ef80eef43860cd03c3249
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:14:52 +0000

    piece world clothes sink ready

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
