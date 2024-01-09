8fcfde9a8a581c5f0abc8677e955d0d824679ce9
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:24:23 +0000

    extra frighten question adjective start

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-text = "Hello, world!"
-print("Characters:", len(text))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
