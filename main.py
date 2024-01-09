3ae86f512534823a8a66d8be03d9af74bb8c495d
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:38:26 +0000

    control from stone month worried

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
