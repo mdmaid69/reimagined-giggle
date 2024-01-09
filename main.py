4dc4afda58b8b1c6a182f91967d3614bfa0eb224
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:28:49 +0000

    mention breakdown late only spread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
