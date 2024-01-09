40befc98c8cf563cfe1edb34ea35dfdb492a522c
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:23:00 +0000

    drop free mess trap work

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_power(work, time):
+        return work / time
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
