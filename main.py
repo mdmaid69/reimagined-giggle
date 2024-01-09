06912940bf42e5571399f6345e356b1b03765708
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:50:18 +0000

    snow idea back beside estimate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
