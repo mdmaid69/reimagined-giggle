f7d6df53fd82609e85ddb83171adf8b35c849dd2
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:00:01 +0000

    fit gas sale confident special

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
