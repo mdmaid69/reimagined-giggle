7b8a493f85010b8c5e9d2b3155d8a4cad852c939
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:11:30 +0000

    trousers load if conduct clue

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
