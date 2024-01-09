c9b8c1fd0767ec917ebcd53cb6a2206bdcb6ed79
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:21:10 +0000

    previous big anxious bye his

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import array
+def get_array_as_set(array):
+        return set(array)
