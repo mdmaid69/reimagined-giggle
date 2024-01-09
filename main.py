06698d9ad1c7c762576bfa96de687e15e9832a1e
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:18:16 +0000

    tour tear swallow hospital easy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_union(list1, list2):
+        return set(list1) | set(list2)
