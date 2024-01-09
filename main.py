da3c2fd4aca79fca060d7dee0895e25d48f45676
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:33:33 +0000

    snow accept various and pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def is_even(n):
+        return n % 2 == 0
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
