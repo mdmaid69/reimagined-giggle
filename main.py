275817f1eb274b35a687f74c9e9efea7a58e3af3
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:54:10 +0000

    month plan Ee completely turnout

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+from collections import Counter
+print(Counter("hello world"))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
