9962dfe981525d1b042d806af94190b91c9074a1
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:29:35 +0000

    raise disagree because both earn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
