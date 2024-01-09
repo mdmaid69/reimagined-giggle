5d8ef1318dba7199a301c5bfe370a27c8e2f78f2
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:34:27 +0000

    179bite healthy nonsense shame fairly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
