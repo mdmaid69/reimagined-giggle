4351099f17b0cc4b331328bdbada373997b070fb
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:03:48 +0000

    true giveout dare among wash

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
