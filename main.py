ef46bb6b1bee538d20d299e30de87b984702e442
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:28:47 +0000

    snow home section pour search

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import time
-print(time.time())
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
