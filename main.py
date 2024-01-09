34f742cde37df142938cd093fd5f836947e91ae0
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:29:38 +0000

    public public keepaway smart car

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
