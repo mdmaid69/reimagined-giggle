60da93cc26b3cc5efeb32b7bcf4838812e329425
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:31:13 +0000

    wire fence concerning contain computer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import sys
+def exit_program():
+        sys.exit()
+  def convert_to_binary(n):
+        return bin(n)
