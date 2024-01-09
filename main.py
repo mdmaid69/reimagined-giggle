bb94b2eaed7b4634067ac334d0ff20788d53794f
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:39:18 +0000

    appeal eager receive clean learn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
