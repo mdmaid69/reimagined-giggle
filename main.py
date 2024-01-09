d49ac7e0a3da502f157c71e1786d121b1c686604
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:07:27 +0000

    look regular warning late thumb

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
