e363b5ceae575fbb638c60f8fa7d216677b14c8a
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:09:29 +0000

    brother confuse keepup sit regional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
