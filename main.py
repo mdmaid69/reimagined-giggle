87226dd654bed30a7035c9acb83ed5d75d4c53df
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:44:29 +0000

    above direction destroy account direction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
