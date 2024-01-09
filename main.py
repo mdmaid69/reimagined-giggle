1dcdfc4d3f1dc08a4179347c6c9fc2f89273e10d
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:34:30 +0000

    modern branch convincing suppose low

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
