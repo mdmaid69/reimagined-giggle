e53d43c4d194c633ad5e71318eac13f74799e63f
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:14:14 +0000

    scale joke worth race breathe

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+x = 10
+y = 20
+print("Sum:", x + y)
