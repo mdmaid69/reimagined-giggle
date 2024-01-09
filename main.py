15031fd080d3caa978a3c9b547ed4675896210d3
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:42:04 +0000

    school shine ear break 566end

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
