44d2f3423945e07dc1a25bd932ff886c4ad11ee1
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:34:10 +0000

    receive fortunately extra sit lead

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import collections
-def create_user_string():
-        return collections.UserString()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
