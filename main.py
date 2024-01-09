6d909c44b949dd9108f6316e9af6d0ba753ce874
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:21:08 +0000

    certainly spoil dull wheel listenfor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
