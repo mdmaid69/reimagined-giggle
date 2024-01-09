db3af853dbfb40112735a95b421d5ebb2c58b4f6
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:09:19 +0000

    speak tongue transparent burst music

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
