a07b4dbd7555c2135a8ebbb148af0df33b4b9fe1
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:20:45 +0000

    talk add doubt tour content

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
