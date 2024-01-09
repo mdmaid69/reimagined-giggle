3ba1f0ec883b64fab46381d8e8e1ab04da553b90
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:39:03 +0000

    chance defeat sound everywhere improve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
