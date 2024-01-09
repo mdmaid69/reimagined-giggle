3cc3b40016178fc7a879fa5528f2c2bbe2071b5f
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:39:52 +0000

    Ww protest juice turnaway shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
