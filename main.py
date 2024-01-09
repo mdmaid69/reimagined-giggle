154b5ee874c8d1716f7200c6a37392481ba8de93
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:06:16 +0000

    why rest language feeling relation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import platform
+def get_python_version():
+        return platform.python_version()
