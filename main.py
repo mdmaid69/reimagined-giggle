9ab820c1a20336c6fa497718f54f828615571109
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:26:11 +0000

    draw proud tear record thing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def convert_to_binary(n):
+        return bin(n)
+  def remove_duplicates(lst):
+        return list(set(lst))
