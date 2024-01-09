0145118179b06b131c8bf0d8f28176b61562b4da
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:00:49 +0000

    nothing guilty earn put lead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
