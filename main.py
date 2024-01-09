37857aca6b05c07ae8e873a7cad112ff4404cdc2
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:59:05 +0000

    sound direct none ought delight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
