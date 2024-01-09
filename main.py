0f26ab47c77feefc3d88a98f492e3efd16fa915b
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:41:44 +0000

    trial tooth drive circumstance occasional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
