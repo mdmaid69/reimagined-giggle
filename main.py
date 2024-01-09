3f2431cd457fa71f290969317366d8e4df77abc3
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:34:58 +0000

    receive public connection mark wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
