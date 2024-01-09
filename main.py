4d61ddfb8923d1f5857c70a8fa0f475c340b3796
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:57:55 +0000

    send repair lookafter care receive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_speed(distance, time):
+        return distance / time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
