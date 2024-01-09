53fa128b65421c9bf1f4ccce1fd0d24e2d6b2489
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:18:04 +0000

    coat classroom greeting foot forever

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def convert_to_hex(n):
-        return hex(n)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
