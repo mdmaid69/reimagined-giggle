93407a96fa47d5ed8b4d7068bb62ee086bd9d22e
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:00:36 +0000

    out deep waste trade callat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  def add_numbers(x, y):
-        return x + y
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
