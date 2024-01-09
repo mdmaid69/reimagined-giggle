5176fd82121e07b4426e16d0c9445f4f11196e54
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:54:32 +0000

    recommend judge argue bringup learn

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
