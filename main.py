cc969767e0873402a003ef2696e38245febe30ee
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:34:55 +0000

    pocket trick in unpleasant breakinto

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def is_odd(n):
+        return n % 2 != 0
+import tensorflow as tf
+print(tf.__version__)
