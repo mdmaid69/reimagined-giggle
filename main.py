ffe31be1bf5947f7f1a769a2666bc8bf35afaf72
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:27:02 +0000

    mess fast transport party official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import numpy as np
+print(np.array([1, 2, 3]))
