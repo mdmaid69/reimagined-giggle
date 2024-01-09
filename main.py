bda8067d9a6a8859b40531200e43cd194e0e7006
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:10:25 +0000

    place sensible alone block anywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
