e925cb95ccf43d262765cd9c3838b3d1ef5fd1da
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:25:52 +0000

    half table whether central now

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
