737db35ef20249160592ebf4cd83f7c4dc7ea0d1
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:51:37 +0000

    mind claim murder again crazy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def add_numbers(a, b):
-        return a + b
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
