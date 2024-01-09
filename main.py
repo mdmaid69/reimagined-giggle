03f19121ca6bc170bf28dec0fe1ed879d2b997cb
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:50:47 +0000

    sound callat realize equal dish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
