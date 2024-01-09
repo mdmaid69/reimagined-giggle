80b8074604a0d8984bcf1985e81b50576cf89fe8
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:58:44 +0000

    unfortunate human unusual dance lock

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
