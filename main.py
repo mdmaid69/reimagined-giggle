0e64dc2037519c740e0b772baab85ec9c73b6597
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:43:41 +0000

    leave conversation set amount lot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
