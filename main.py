1e6e74fa95c58196d09684de6143a7f04d45ef2c
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:58:14 +0000

    exam tough I opposite noisy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
