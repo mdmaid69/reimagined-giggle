96dd2b5ca806938516097f97df7ee0c354f67f11
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:34:17 +0000

    blow bridge present lose height

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_work(force, distance):
-        return force * distance
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
