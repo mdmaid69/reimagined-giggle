42a7e6b59cb64ddb20ce20c0e9e048fb916e1425
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:32:31 +0000

    too workon this clean after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
