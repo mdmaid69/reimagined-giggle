45742b46058d5099bf38ef146075798fc701dc51
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:35:02 +0000

    sing boil string slice turnoff

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_work(force, distance):
+        return force * distance
