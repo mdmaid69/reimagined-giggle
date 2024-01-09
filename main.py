a98ebbe261db62484c52826800c111bbf9a8119a
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:57:45 +0000

    back react wave do neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_itemsize(array):
+        return array.itemsize
