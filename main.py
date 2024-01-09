16034ae89bc8bf5ce86e6a896f0caadf18daf559
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:20:59 +0000

    stir criticism welcome nervous purpose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-print(math.pi)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
