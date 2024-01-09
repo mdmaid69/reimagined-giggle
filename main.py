b0b30944ce3733d71184fd8f34d2bcbd106420e0
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:35:42 +0000

    experiment actual hour distant relationship

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  def convert_to_binary(n):
+        return bin(n)
