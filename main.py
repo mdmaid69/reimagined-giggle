66ee3bac94fae7090b3b9a9029261014b54944b0
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:28:37 +0000

    top view complete fence knowledge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import collections
+def create_user_list():
+        return collections.UserList()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
