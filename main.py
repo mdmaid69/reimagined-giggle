8b2087e0fb9bdef8adfcfaf41c30b6206d01a302
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:59:50 +0000

    boring really interruption modern die

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
