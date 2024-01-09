80bd13dd9accf7bf715bccc5a7f3dac5390de85b
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:27:48 +0000

    bottom nice power throw measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
