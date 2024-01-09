458a55d294f3047249db24c4afbb72806e8f9d6d
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:49:53 +0000

    soon comeback settle amount pick

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def reverse_string(s):
-        return s[::-1]
+print([x**2 for x in range(10)])
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
