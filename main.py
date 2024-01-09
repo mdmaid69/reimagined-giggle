1d472b0f726b958aeaf3b50c3808a2d370d4a78b
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:56:23 +0000

    bin confuse impatient spite brave

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  def cube_number(x):
+        return x**3
