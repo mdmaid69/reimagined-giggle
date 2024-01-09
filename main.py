aa4582af778c51fe3cd86c77335b4d153e37b6d9
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:58:20 +0000

    pupil work share wild describe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+def divide_numbers(x, y):
+        return x / y
