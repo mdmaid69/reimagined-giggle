830f7a6bf4b86d2e87867bcc1502911093713bab
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:36:24 +0000

    her spite protect cool offer

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_min(numbers):
-        return min(numbers)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
