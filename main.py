77b7a22259f0b2934b84157cad8bb8f4dea34941
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:08:38 +0000

    wall over consult Ww reserve

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def add_numbers(x, y):
+        return x + y
+import re
+print(re.match("h.*o", "hello world"))
