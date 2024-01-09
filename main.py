2377e001fca623299560d55ad7919af4f751af87
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:13:26 +0000

    rescue honest used sometimes total

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
 import datetime
-print(datetime.datetime.now())
-import math
-print(math.pi)
+def get_current_date():
+        return datetime.date.today()
