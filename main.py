54387d940e3b942b0f09f56ffb2d40f271e01f6a
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:50:13 +0000

    steal dish cry gentle smart

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_density(mass, volume):
+        return mass / volume
