6fe27fcb0e8eeb74008fa419543b3c92135fcc05
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:42:31 +0000

    excellent silent scale achievement stage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
