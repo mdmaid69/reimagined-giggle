59ba72448f1fc9f7fc8ce734aff35408bea6d789
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:51:23 +0000

    use fear hollow damage last

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import random
+print(random.randint(0, 100))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
