c2e377ac1534aa489f476bfae8462763fa10155a
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:17:05 +0000

    dream runinto Cc doubt watch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
