488ea03abad8a2cc7931fa798089980ae3d34ed2
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:39:22 +0000

    spoil carry fasten balance force

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
