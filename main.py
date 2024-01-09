ebc732324c0c6ff1a931216ae58bef47b150cc4f
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:48:45 +0000

    study mysterious innocent phone frame

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import time
-print(time.time())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import datetime
+def get_current_date():
+        return datetime.date.today()
