04ede4f0835e9bf08323fc54cb3016adab8f80bd
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:38:54 +0000

    bank completely tax soft building

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def divide_numbers(x, y):
+        return x / y
