9fe9c41b1112ee416930dd7b100396cdfc72abbc
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:38:49 +0000

    stop equal bang excited thumb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
