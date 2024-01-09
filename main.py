7924b876d1a06b4203aca4fb91383b0265c9fa14
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:42:50 +0000

    rain too criticize crisis announce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import time
+print(time.time())
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
