002fc0bfdf5b78e5162b06875a9008ca18f805cf
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:33:25 +0000

    praise sky keepfrom better run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
