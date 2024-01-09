1cc0e612f888e170e7d7bb631ceeec4dc4441576
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:30:25 +0000

    twist too try brother future

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_area(radius):
+        return 3.14 * radius * radius
