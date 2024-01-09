0f3b18d70dadfee3195175c317dd18dbeb40e340
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:47:53 +0000

    impressed heat left clue read

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-print(sum(range(10)))
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
