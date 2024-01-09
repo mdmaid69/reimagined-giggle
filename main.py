10273ed5132d68f715a07d13925e64b135dcc751
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:48:38 +0000

    innocent cruelty regular everyone without

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
