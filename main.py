3d33826a0e6f82d95c974930d52db1bb2711bc26
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:11:54 +0000

    179bite conversation list company less

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import sys
+print(sys.version)
+  import sys
+  def get_python_version():
+        return sys.version
