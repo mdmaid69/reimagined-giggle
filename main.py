683776cfec22e2a1c3851cdbf86f1fcb66a71407
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:28:11 +0000

    complain accordingto patience almost corner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
