0f132cf91f8404e73bb66cfd88769b892d372171
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:26:10 +0000

    quit stick roll write deal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
