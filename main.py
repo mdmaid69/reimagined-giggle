67157a75c821cfdf98c6835033477fe15ab956dc
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:38:09 +0000

    loud by treat figure tone

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
