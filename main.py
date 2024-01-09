eef7b9a8e40f2859680bfb38e00fef8c6be3bc60
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:50:13 +0000

    open nature big like lock

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
