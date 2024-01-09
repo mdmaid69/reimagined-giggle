e99c03e389f33f554710c6485d019ba79f047c92
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:30:00 +0000

    cold takeover rely on available

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
