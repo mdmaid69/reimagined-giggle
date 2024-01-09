9834fe1dfc6c2aaea7508917a264e82b6e714c3a
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:03:05 +0000

    visit sad juice attractive hang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
