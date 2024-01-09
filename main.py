d63092b54583cfe4e149e80f0ab9a252c03a55ff
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:48:41 +0000

    software cause Mm similar pull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
