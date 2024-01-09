5c990451780b13b02f5d658f1d4bd9bb2647c204
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:23:11 +0000

    post class pile project rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
