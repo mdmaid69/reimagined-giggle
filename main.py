01e79a69f352cd9c1d83ebcded09068be7273791
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:59:31 +0000

    different sorry contrast hold further

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
