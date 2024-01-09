b41d3816ed728fdb1c1671468b9a9361765aabfd
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:34:26 +0000

    image tax excellent hair careful

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
