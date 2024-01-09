74f371ec7dfdc3f634064d9f94bc1099900f71f5
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:32:45 +0000

    concentration fine tape anything flavour

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-def square_number(x):
-        return x**2
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
