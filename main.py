1f9b276a961c050dc60d574f5f5909d5b0ecf29f
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:20:56 +0000

    soft lesson everything debt ignore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
