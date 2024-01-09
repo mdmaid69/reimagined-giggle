c8ae6c43d1517eaf1169fbcf00f3bf0d6ff07981
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:40:49 +0000

    straight ticket answer pattern anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import collections
-def create_counter():
-        return collections.Counter()
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
