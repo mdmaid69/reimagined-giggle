f72d396c211331f99a96e5ea838826c6f32cb363
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:19:12 +0000

    side single I sort honest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
