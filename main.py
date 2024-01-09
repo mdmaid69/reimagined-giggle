671b9ba08abc4b20f6bc6802dc89c725b8d20ea5
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:45:26 +0000

    who entrance beneath warm ordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
