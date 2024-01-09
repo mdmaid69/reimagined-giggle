49877928bc4d828cc5447f87bbefdbbe5422169d
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:54:19 +0000

    reliable Nn calmly mainly raise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
