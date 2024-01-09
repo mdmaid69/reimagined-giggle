2b1fc51613060b00376e9b457d064678ef2d419f
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:18:18 +0000

    second strict measure extraordinary soil

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
