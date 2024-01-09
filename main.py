1fbaeb221f7943b30be51a63abbf9d68c201277b
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:05:37 +0000

    confusing curious put small emphasize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def get_list_from_array(array):
+        return array.tolist()
