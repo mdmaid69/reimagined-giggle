cb9f16125f532015ef7effed7514ed34ab7d1101
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:58:24 +0000

    slow treat correct mention takeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def square_number(x):
-        return x**2
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
