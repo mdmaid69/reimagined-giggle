2ddfecb704b1283a0dd329654651f7a5ece5eaa4
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:36:02 +0000

    support still keepup service well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
