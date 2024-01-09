b24923753d71d172b0a77adea1a0e386288a43a5
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:45:01 +0000

    clean shop sell previous actually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Reversed:", text[::-1])
