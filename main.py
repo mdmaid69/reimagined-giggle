e89deb83a9d47ffb179c3da8b7cdafeb56d09c5f
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:09:40 +0000

    test remark brown welcome whistle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
