b4fa195ef17409083615d563984930e1c255dc0c
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:10:47 +0000

    sometimes immediate shelter lunch basic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
