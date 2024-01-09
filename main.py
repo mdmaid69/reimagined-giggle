40ca2c8438ce2e37a30b175e7419ebdd17e97f81
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:45:29 +0000

    stiff rid one probably visit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_as_bool(array):
+        return bool(array)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
