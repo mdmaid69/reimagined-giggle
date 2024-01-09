6a185dac17012d4c14420721dac4e083e2995027
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:04:26 +0000

    find absolutely breakinto murder advantage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
