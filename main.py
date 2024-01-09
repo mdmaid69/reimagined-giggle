6177c23fef30ab729aa91845d65c36ae73fd18bf
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:42:08 +0000

    drawing scene treat getup badly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
