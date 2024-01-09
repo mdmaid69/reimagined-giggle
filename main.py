0dde5631104a9debb0f931e798e1cf20b20619d8
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:34:18 +0000

    reliable able snow identify life

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
 import math
 def calculate_sine(x):
         return math.sin(x)
