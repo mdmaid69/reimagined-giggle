5163bbef2aa83f6460d40e9296db9a72f04a0c54
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:06:55 +0000

    before under your free satisfied

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import collections
-def create_queue():
-        return collections.deque()
+import array
+def reverse_array(array):
+        array.reverse()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
