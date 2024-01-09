8838ec17decc5ea114f7cdcde6615adbf9e75328
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:58:12 +0000

    kick imagine afterwards walk business

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
