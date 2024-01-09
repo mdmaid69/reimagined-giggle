3e60fbe02fea49d7199e5018481db405a4704de5
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:06:45 +0000

    wash comedown kind telephone reply

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def find_unique_words(sentence):
-        return set(sentence.split())
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
