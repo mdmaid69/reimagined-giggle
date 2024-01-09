9a56f733268b1a6620bf301f04a224faec7e7745
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:31:46 +0000

    trade shape whisper embarrassing make

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
