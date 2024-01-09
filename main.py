0d6b029dae976060cd441bb12e27ddcdcc6aa473
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:02:10 +0000

    goout free target rush press

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def convert_to_hex(n):
-        return hex(n)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
