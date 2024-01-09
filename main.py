bd106c235f9f0a96563623fbadc88379f1bf3647
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:02:27 +0000

    trick hurt wonder brush error

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
