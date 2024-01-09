da31594a6fe72d001cc9f70b0ce40b34e124d135
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:42:06 +0000

    increase see statement would settle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import random
+def roll_die():
+        return random.randint(1, 6)
