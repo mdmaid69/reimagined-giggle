7cd23311c6c0d8f9a9e3d6fd099a60a1df091cb2
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:33:27 +0000

    worth bag medium hear factory

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_pressure(force, area):
+        return force / area
