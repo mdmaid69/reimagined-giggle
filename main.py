1cac3cff777412a137018288a2fc202fb7ee33d1
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:11:27 +0000

    because report estimate level lock

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-name = "Python"
-print("Hello,", name)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
