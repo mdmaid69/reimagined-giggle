7a5a4b42a9d76ce1e7c7106ec53edbdfab9b25e0
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:14:06 +0000

    announce clue noise increase movement

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
