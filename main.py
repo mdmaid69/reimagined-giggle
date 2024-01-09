44881d5292dd3e71608dd7db33efee9a779e38d1
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:53:06 +0000

    region delight rough steer style

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_work(force, distance):
-        return force * distance
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
