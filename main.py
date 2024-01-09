994d39b1d761ec57c43792fa8ff35f850589780e
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:03:15 +0000

    bother valuable interview danger pressure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
