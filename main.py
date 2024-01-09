f66abac52d9c7270aafd3da4145523efc67c2ae2
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:48:58 +0000

    fear question produce slice way

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
