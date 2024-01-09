a28ad49082e35768632f2bcde7342ffec8334f88
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:57:52 +0000

    prepare gas become go land

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def read_from_json(json_string):
+        return json.loads(json_string)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
