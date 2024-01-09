2da6b4f8e2c79dee6bb14898e9189ec6b15cc808
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:05:55 +0000

    set switch under short love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
