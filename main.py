26ba5b6d11796db4948db3a23e24281a747f28d6
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:02:50 +0000

    he shape only object clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
