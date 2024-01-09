87b75739173f2776415e832292ac4d1b123321fc
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:25:23 +0000

    post weather suppose characteristic accept

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
