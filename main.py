d4178613e40838b41e242f84138efa97af293ed1
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:07:42 +0000

    excuse embarrassed birthday move shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
