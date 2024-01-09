c277fccbadade26b8138c402d0149bb4f6ad5761
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:21:48 +0000

    drink spare reduce deliberate wrap

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
