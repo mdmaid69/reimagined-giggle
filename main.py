bd85c1697f9d36794317d78bf8ec8705b62ae6f0
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:42:19 +0000

    usually attention improvement damage window

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
