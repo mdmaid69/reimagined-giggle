1c1b237a648c7f9af35e16f43634ae58a052a5a7
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:02:01 +0000

    in matter join money anyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_speed(distance, time):
+        return distance / time
