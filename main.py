1afe8f0dbdfa32bfa546a40c3053f3a8750ea422
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:25:16 +0000

    science cough several long holdup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
