72d481a86a2776afe31be8ba8cc995b87435e557
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:07:22 +0000

    please mess charge appeal long

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-x = 10
-y = 20
-print("Sum:", x + y)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import getpass
+def get_username():
+        return getpass.getuser()
