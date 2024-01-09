cd5e1ddc66adb8ef4231378fb420e5f1ee8292b3
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:31:40 +0000

    fork mad his behaviour splash

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def square_number(x):
+        return x**2
