fadec25e705809c03e305381bc1bbe7bc8a81488
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:51:11 +0000

    centre appeal weigh possibility place

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
