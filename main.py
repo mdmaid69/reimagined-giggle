48541deb125fa07d2792c61e70e186e4bf9a7d80
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:18:39 +0000

    though Zz several pull same

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
