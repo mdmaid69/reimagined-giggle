920fd8e1100304b4d2547ef18cde5edf1c7aa300
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:38:24 +0000

    except right sick behaviour minimum

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
