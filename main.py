72b7ad2cf320853b78b686eb005235fbee412e80
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:16:59 +0000

    slide innocent however chief flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_bool(array):
+        return bool(array)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
