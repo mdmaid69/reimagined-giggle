23089712a0885217806067c65af2911f10763f5b
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:15:03 +0000

    mood earn apparently cheap father

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def reverse_string(s):
+        return s[::-1]
