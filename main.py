40cb0688d5bb4d20f3725edb8a8b85309f8b40f5
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:42:34 +0000

    square here actual last support

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
