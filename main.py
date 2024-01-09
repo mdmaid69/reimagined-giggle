c1abc761cf79b850d4dc8033762261c0eb11c257
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:28:08 +0000

    deal impatient stupid correct straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
