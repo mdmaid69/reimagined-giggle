7555f5bfa391a42a27b0ce7eaf047abf40d380b1
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:03:00 +0000

    race holdout present guess rub

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
