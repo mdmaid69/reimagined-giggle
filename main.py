90d6b9ae96542b39b10559710d8340989a33444b
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:52:31 +0000

    fortunate joke home twist doctor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
