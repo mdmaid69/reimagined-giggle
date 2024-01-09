06ae2ded9dffa1ee1969dde1972f55392c1bb741
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:34:16 +0000

    release really it suggest stand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
