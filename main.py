8c86490fd5bd6d7c8533578486b60afc14947bf0
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:29:10 +0000

    her geton forgive pleased unlucky

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def square_number(x):
-        return x**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
