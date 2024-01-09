9cbfa011c9b6c9a27f1530fef6dd5a8e8302a2f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:39:14 +0000

    label round guess cut judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def calculate_area_circle(r):
+def calculate_area_circle(r):
         return 3.14 * r**2
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
