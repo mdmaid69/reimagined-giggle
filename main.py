1e3d45586af1d95eaf50df514dc5d53a5d76aeb1
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:03:38 +0000

    about comefrom upwards represent prize

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
