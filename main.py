3bea253fb2e63ad084a873f380dea41e277ad451
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:19:14 +0000

    difficulty understand brush care scream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def is_even(n):
+        return n % 2 == 0
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
