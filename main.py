c96e70f9000d7220935f7a8d9598b1cca3504870
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:48:23 +0000

    Ll hurt talkinto rid abroad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
