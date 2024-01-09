98e87245a10b97f8d05050678d9b37ead716dc33
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:45:49 +0000

    indeed smell wander ought interrupt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import os
+print(os.getcwd())
