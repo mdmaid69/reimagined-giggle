e2a047835aa1b95401c9bbfb10008e5a3cd9b28b
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:44:18 +0000

    always wear discover eat company

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
