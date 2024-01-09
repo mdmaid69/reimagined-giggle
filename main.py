e3561f814126a7b666e7437ab45707b503e9b188
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:16:48 +0000

    heavy circumstance road fire sale

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import os
+def change_working_directory(path):
+        os.chdir(path)
