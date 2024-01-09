ba45fe17b054b7eb85b8f0aa4e4ea4b607889cc7
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:00:48 +0000

    reason roll slightly variety forward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
