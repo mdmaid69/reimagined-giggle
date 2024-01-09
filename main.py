c219c88d59243d531bff007e8d7f4a0f87c7e9ac
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:23:57 +0000

    completely discuss church extremely upstairs

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
