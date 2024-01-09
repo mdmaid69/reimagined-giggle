cf639fdf26da2736bcf38b4277834b367e9e97a7
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:23:41 +0000

    realize whisper really basis they

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
