8ca8d0ec136e8dad000172bbac5c038ef7530ec6
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:46:53 +0000

    trial trust mean cause good

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import sys
+def print_python_version():
+        return sys.version
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
