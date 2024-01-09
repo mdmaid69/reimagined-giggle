c867d72e4f4b6446e6d54bfc2656a205241895a1
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:26:46 +0000

    whom track failure whole sound

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_density(mass, volume):
+        return mass / volume
