307fda8fe4959267dca6b7f55165596e53ca5b65
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:14:42 +0000

    break correct pleased maintain remember

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def remove_duplicates(lst):
+        return list(set(lst))
