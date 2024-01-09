39024cb2fd906bf92bc5ac95d889d7f011afd306
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:46:10 +0000

    generally Cc confident work work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
