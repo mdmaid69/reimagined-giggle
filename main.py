e2633e6870eae3c1c7717911250c5bbe6ae31a52
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:08:01 +0000

    deep draw discuss event dry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_characters(sentence):
-        return len(sentence)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_volume(length, width, height):
+        return length * width * height
