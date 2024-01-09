187c6b05c768b0f326a083b20c4ece4aca89725b
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:33:16 +0000

    more discover shake dream by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
