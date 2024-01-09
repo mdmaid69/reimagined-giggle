853f4bca9d328a6ac9411c9a76f965d9d09b981a
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:43:30 +0000

    tend why breakin repeat right

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-print(sum(range(10)))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
