30385fd7a079d6cbb22d5a931c9bfb0726febb8e
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:41:08 +0000

    cut somehow what exhibition burn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_max(lst):
+        return max(lst)
+text = "Hello, world!"
+print("Words:", len(text.split()))
