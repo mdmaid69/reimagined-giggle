b160c60dee73a1bf1e05cbd99d37bda9b453f3a6
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:32:43 +0000

    race typical later contents holdout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def square_number(x):
+        return x**2
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
