5b51149d4e65093382b9936094c835563d96ffb9
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:22:04 +0000

    beyond him talk tonight dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
