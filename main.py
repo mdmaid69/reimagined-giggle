4d1d7f1e280ee56586bdfeedd34759f6c554b15e
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:12:20 +0000

    cook good favour first break

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
