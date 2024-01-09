315a530d42f82303b28f33bddfd7db75d465bb7b
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:53:28 +0000

    free neither indeed present dinner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
