46cb0cb08473752a56c249b46beb3b017772fc8b
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:09:42 +0000

    realize possibility size conclusion nowhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
