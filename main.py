bf4a76b4248fed84ade949ff61b99fcf832fdd3b
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:42:26 +0000

    pressure fun bang distant group

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
