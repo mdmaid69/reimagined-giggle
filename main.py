fd14eb3eb9bf95f97af833476aca6df819c663c6
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:17:13 +0000

    swallow dull mainly transport rain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_as_int(array):
+        return int(array[0])
