06fcee64daaa3e269b0321d9931e12f586bed47b
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:18:31 +0000

    major rank copy under entrance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
