33eb820387a274fbff8b11b415476a162ab30bbd
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:37:04 +0000

    war little count flat myself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import time
+def get_time_since_epoch():
+        return time.time()
+def is_palindrome(s):
+        return s == s[::-1]
