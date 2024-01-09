a04cc1465aa8f2b7aa38285b8de3e0b4e54c9e9d
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:07:19 +0000

    wrong agree birthday whatever announce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_min(lst):
+        return min(lst)
