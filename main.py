8772d5d16878cb09adb14e664b5f343dc55e329c
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:44:53 +0000

    ball part expression platform comeoff

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_force(mass, acceleration):
+        return mass * acceleration
