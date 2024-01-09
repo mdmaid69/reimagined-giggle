98b9964170d239e94f771ccedae9bda59d7801e0
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:35:16 +0000

    Cc attract emphasize again suddenly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
