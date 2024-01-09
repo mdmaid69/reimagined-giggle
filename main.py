078ae45a04577d777fdc85ed21602ff86abda656
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:01:55 +0000

    emergency upwards agree blame connected

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
