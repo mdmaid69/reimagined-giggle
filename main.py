18b4a452b1e59b7591139690a1ced1e5ae7f2e42
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:04:55 +0000

    top separate last public design

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
