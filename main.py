aea0e2d7242089a2fec32df5f3392e60555e1626
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:02:39 +0000

    flat yours calm shop school

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  def is_even(n):
+        return n % 2 == 0
