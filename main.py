7ca2b1b21e3034a2c34e11452f89ba5aef27553c
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:08:07 +0000

    yes height intend tradition else

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
