73aab8c1cd04738adfe9c17e00321f11f8a02c40
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:05:32 +0000

    pair development less tend pin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
