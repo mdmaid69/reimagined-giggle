fa5e0dd3e35d8b5508d37b25f7fcfc56415e848d
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:50:12 +0000

    reach string young defend muscle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
