020a368d34dcc14899e00945362fae88c0560111
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:36:16 +0000

    brush live purpose action effort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
