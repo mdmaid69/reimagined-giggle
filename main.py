b0251ad88ffdf4e2a6b304f76f8661f7550272d5
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:46:02 +0000

    but after rain pupil everybody

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
