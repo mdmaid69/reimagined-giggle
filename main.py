08ea77142d9127b40b437a4c65afe63aa19fe97e
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:20:24 +0000

    section downstairs band many centimetre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
 import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def move_file(src, dst):
+        shutil.move(src, dst)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
