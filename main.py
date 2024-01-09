7bec5b8cb470299356d684e83f2feffea4640c4a
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:29:16 +0000

    telephone careful image workupto skirt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
