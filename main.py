fd01bcf7306bc3bb9ad75f734ee86849f683cd3b
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:07:22 +0000

    few government mess turnout bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_speed(distance, time):
+        return distance / time
