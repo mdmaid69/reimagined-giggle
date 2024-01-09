7595f11fbf5ecf52c403975db78429ededfbe8f6
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:50:12 +0000

    else slide float crash fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
+  def get_file_inode(file_name):
         return os.stat(file_name).st_ino
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def square_number(x):
+        return x**2
