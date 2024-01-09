85cab7f5bf8718f6754ebb2b2765192b362a450c
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:03:50 +0000

    lean instead where measure basis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import collections
-def create_priority_queue():
-        return collections.deque()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
