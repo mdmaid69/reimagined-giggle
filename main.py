86eb59bdf3af9c9d163522b0b80b472f638dd2f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:41:50 +0000

    false us necessary here onto

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-def greet(name):
-        print(f"Hello, {name}!")
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
