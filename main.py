9306f6c7f2f8ff8d66b4b1d14064a8672ff552b6
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:58:10 +0000

    build offer quantity service mess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import collections
-def create_queue():
-        return collections.deque()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
