db34fb4afa2b193c8d88b330cb8e2954155169e7
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:37:34 +0000

    grade card summer task believe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+def is_even(n):
+        return n % 2 == 0
