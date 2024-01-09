9540f5fcfa8ade8e2bb2f704bc393d8224711bb7
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:38:39 +0000

    picture fetch drop remark nearly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
