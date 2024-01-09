28e4475b0c4e4dd1de42ed59aa10ef2b4f69c801
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:23:28 +0000

    damp gradually born feed pride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def convert_to_json(data):
+        return json.dumps(data)
