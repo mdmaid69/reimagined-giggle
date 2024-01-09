b0aa257dcbe59de31e69d8c88510bf0599037e19
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:21:25 +0000

    neck tread tongue empty attention

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
