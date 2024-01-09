12893ff29b40124f3f3ab53ecbd19bab553cedcb
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:28:38 +0000

    unhappy usually ready runover tense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
