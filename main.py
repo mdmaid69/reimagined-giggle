16c1d1919220c08b75acb1ce16aa59ae94112573
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:01:30 +0000

    point suggest evidence field term

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
