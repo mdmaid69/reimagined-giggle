8cc19274ed95c12477dc2597a37e480e29b3e291
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:44:28 +0000

    business dinner brave reply thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
