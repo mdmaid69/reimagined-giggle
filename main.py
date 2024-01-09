a9a1088969ccd6f79bd1aa12bd63d85492120163
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:03:31 +0000

    care dinner worried exaggerate brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+from collections import Counter
+print(Counter("hello world"))
