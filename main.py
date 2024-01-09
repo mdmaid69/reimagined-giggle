b0cc09cc2e3792f0961d113da8f49e60bca9b331
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:20:09 +0000

    little wish now round expensive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
