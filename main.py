96c85746e2a968d71db372e92539364ba5a071f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:43:15 +0000

    spray cool uncomfortable eye street

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
