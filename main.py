eff8a850a818c86c931034e01ebb493933f7d714
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:10:42 +0000

    trousers indeed eye immediately depend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
