88ee7b25a57288d570171ad6ba8746ff0bcc2b8f
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:30:42 +0000

    die turndown communication prove plan

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Words:", len(text.split()))
