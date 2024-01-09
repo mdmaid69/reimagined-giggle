eb34b81f7a20b2d1b003d94a2a3df9b900431b0a
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:35:18 +0000

    himself mine suck time brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
