1db898e303ac77f8089e89c60f57ec37e285f747
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:05:41 +0000

    mark total me exhibition invite

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
