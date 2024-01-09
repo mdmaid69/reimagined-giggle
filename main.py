16277acd4b0263863c4b1700cf007d56a6e494d1
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:43:56 +0000

    hour fault miss comein bend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
