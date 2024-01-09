81e540181f809c22f54f5257f94ee31e58ea7f63
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:13:29 +0000

    range judge low no difficult

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
