b564dd0b22f64aa81885f63086939a71a6ffd462
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:07:20 +0000

    some hard preparation operate illness

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def remove_duplicates(lst):
+        return list(set(lst))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
