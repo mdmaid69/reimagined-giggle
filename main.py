d5fdec2b1f71c869be6b253e6f049a374b988361
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:38:55 +0000

    personal honest breakdown rough however

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sys
+def exit_program():
+        sys.exit()
