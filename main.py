795b15b273a42f8a46ce9f4b3d9d2edc706b48a8
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:34:05 +0000

    rough even should shock pain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
