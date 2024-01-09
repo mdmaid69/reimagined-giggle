072368d77094699b5ebb88137d2a054e590d08eb
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:00:21 +0000

    able boring consist connect Cc

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def square_number(x):
-        return x**2
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
