4d0e459cee7c07ca862f499718b3890724fdd261
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:30:00 +0000

    bit Tt acceptable by defeat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import tensorflow as tf
-print(tf.__version__)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
