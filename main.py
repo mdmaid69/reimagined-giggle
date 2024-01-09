a7cb21338be86f1e3e7ff4c16d79b0ef0cfc4c04
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:57:05 +0000

    Qq comeoff after drop birth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
