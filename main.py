736cd4d08f5d35b0603d6b877c49b71831588890
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:19:30 +0000

    common only everybody mend confusing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
