6e612a7b51dec4763bb99b7d186fdcfd58d77796
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:15:52 +0000

    mess murder lie voice skill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
