3a1e4630a307454881081b0f57b998aed77f5b19
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:26:20 +0000

    head aware responsibility variety precise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
