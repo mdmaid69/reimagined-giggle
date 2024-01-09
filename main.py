55c94ed1fca43eb07f41aa598eca1fee0828d78a
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:32:46 +0000

    callback tear politics guide Jj

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
