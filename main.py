65244872f4897952a15539b021327937bfa4c7de
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:16:01 +0000

    support sound short look similar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import collections
+def create_stack():
+        return collections.deque()
