012dad2a6c01ccbe06400c167efbfff7b75d9f75
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:26:25 +0000

    form breathe strike schedule family

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def multiply_numbers(x, y):
-        return x * y
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def add_numbers(x, y):
+        return x + y
