c3d5f992caa1a21b30c94f537954c099d8e6b564
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:23:27 +0000

    again repeat regularly want share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_arc_tangent(x):
+        return math.atan(x)
