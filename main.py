89226f3c11ad90b664dbd13fba2644d6415df571
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:58:23 +0000

    sink with normal artificial gift

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def get_list_from_array(array):
+        return array.tolist()
