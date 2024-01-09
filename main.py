f462a8dfdc9c6065226b0b2d1d284b64dd24920f
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:02:02 +0000

    shut swim about narrow file

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
