4ba52c9716716a7ad6c1a533b419b7612f0ff691
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:00:20 +0000

    Mm afraid skirt exaggerate cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import time
+def get_time_since_epoch():
+        return time.time()
