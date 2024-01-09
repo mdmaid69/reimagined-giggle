fa2f466df4ad5d2c677ec625619eaa76db1c414b
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:12:12 +0000

    complete neck to tear emergency

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
