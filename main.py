7e3c8d0e100132ea4dcb3b1c91a5bba7de24740d
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:30:52 +0000

    action who science comment getoff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
