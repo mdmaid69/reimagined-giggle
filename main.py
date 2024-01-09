c998d5ee68ae51e8955a9c0e2791c295f0e4932d
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:59:12 +0000

    more bound fortunately back human

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
