09d6b5855c3600b2118ee0bf87f69b47c2b08ffe
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:24:45 +0000

    empty press actually impression sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def remove_directory(path):
+        os.rmdir(path)
