97b4496a01fea32cea8873d600cedfbc658aaae0
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:47:04 +0000

    tip pleasure before find combine

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
