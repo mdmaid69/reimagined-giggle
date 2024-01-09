20e0a791d0eef4e0dffb1743e98d72a71778d218
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:05:08 +0000

    kick on creature organize sound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
