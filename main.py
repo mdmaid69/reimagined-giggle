1f402047da5220c79c715a4287662e7a1cf553a3
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:35:06 +0000

    dance earn nonsense 570enjoyable avoid

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
