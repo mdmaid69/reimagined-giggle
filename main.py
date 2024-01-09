23383058e36eb22fcf2a6c76154af8dd5c8f2dd5
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:35:04 +0000

    score somewhere friendly gradually laugh

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import numpy as np
-print(np.array([1, 2, 3]))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
