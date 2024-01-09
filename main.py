27961fc590ed21c10045e0fb7b3aaea1861b7730
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:59:17 +0000

    length contribute nearly ashamed impossible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_area_rectangle(l, w):
-        return l * w
+name = "Python"
+print("Hello,", name)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
