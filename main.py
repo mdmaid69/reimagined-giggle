c1e402e0ef1ef78a4610a62fba483b4d6bce9be3
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:30:02 +0000

    pressure full complain false form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
