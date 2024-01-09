0ccd5df43c285f310b9b122d39aec600be20356b
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:41:47 +0000

    behaviour rule anything pollution trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
