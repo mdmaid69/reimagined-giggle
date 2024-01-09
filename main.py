42673ccfd56ab36d95c4d548aedf0e81028fefe8
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:56:23 +0000

    turndown close carry road confuse

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+print([x**2 for x in range(10)])
