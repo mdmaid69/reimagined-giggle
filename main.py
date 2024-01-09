511eba22c930643c3702796938d9422a551b11c2
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:16:02 +0000

    conclusion attractive aim congratulations finger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_pressure(force, area):
+        return force / area
