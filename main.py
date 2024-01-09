bb55dcfa607f1a6c34f24ad554a0ba53005b4f21
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:55:09 +0000

    arrive thick Nn lookaround about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
