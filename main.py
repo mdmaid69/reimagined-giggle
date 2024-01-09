237373d570de3fd8406544a0303f605d8a01c7df
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:50:59 +0000

    television hello necessary born recovery

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
