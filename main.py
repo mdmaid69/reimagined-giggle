75a1d41ca577f653f2520e3a3f21745779215b48
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:49:42 +0000

    family cheque regret translate always

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def is_odd(n):
-        return n % 2 != 0
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_density(mass, volume):
+        return mass / volume
