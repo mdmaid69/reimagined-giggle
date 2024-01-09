ce11f53bdb6871fb01ab988aca587b0d988ff632
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:47:52 +0000

    open time serious joke furniture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_current_working_directory():
+        return os.getcwd()
