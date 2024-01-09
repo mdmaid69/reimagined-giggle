ac317dd3bb43534c6dfd1bef096faf72129a47f7
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:10:12 +0000

    book few worried while change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
