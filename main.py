299e491c672f38e1991bedab4c1e2018a9b9518d
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:00:53 +0000

    without pollution noone disagree spill

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
