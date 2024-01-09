c0994ec2212d667a33b131d25784c7b27abb0bda
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:59:04 +0000

    increase wealth round develop far

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-for i in range(5):
-        print(i)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
