4323be83a7fcf90990bfbbe56fda3497f9604f69
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:20:13 +0000

    lean Vv wander manage sudden

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
