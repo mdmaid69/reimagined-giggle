4d90fb95aff43754a6d199c92cfa5984f426d533
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:50:09 +0000

    reply cheque left offer trick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
