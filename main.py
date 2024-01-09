d8270b2c8f8fdb0e5835d7d16e06ee127cbabdf0
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:54:44 +0000

    spill smell shelter cry advantage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
