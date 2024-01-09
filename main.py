14a67967f58c450182e133a2007e58b2ff6538ed
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:51:59 +0000

    everywhere car drop from foot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
