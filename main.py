4100d9855502bf58da77e6065cd566c10774a25e
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:28:06 +0000

    season weigh tool crowded apply

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
