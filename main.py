de2a797c0b80cf5ffdf73f5e85da57467e09f0e5
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:22:45 +0000

    worse now gas behind open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
