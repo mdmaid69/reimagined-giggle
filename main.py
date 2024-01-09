e8d03c7f617dbea53e4cd08eb0bef5cb1cf9b38f
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:18:51 +0000

    agreement gate naturally bet previous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
