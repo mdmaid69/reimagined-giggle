fd5612c4ec25aad8f155bb9d05f36d8f7185ee5f
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:37:06 +0000

    try always sweep few lonely

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def is_odd(n):
+        return n % 2 != 0
+def remove_duplicates(lst):
+        return list(set(lst))
