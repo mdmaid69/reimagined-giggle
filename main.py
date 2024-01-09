ff821b0c6b1c4a78f6c3b829ed5060a3cb047ed7
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:18:23 +0000

    discovery stamp group party really

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
