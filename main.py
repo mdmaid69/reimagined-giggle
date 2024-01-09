1ea63a5981d090d21fbe3f6fe02020368da8a67f
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:13:00 +0000

    urgent recognize practise build regard

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  def multiply_numbers(x, y):
+        return x * y
