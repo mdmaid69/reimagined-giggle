6e937e542b9d0a49fc4811f2983b3ea6b34b366e
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:33:44 +0000

    dinner juice below figure fit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import os
+def remove_directory(path):
+        os.rmdir(path)
