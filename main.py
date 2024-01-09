e89ea90c51c1aa4901f3246d4844faf312ab384b
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:42:06 +0000

    wire to seriously meeting back

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
