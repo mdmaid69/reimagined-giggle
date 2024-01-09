920ded87f5ac3a8207d950b20a7248e4466e9af0
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:56:51 +0000

    bringback alone hold fold now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def convert_to_octal(n):
+        return oct(n)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_bytes_from_array(array):
+        return array.tobytes()
