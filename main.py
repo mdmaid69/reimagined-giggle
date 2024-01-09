d97d70874bb5843e3b607d9568005862e6dfc075
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:05:24 +0000

    smell collect regret difficulty another

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
