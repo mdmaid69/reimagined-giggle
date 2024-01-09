aef2eb7deec9661669ae1717ef9c0f26c4019de2
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:57:23 +0000

    order holdup state joke fine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def sort_list(lst):
+        return sorted(lst)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
