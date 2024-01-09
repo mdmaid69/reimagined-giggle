e56b0d3c61c607ac3e741f45cb256e109362bc41
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:29:45 +0000

    mention constantly normal offer pool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
