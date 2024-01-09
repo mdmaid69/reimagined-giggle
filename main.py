7f29b245a94ca5ee819a92ff0be9e62693f8e770
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:33:01 +0000

    crime broad comeover uncertain turnout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
