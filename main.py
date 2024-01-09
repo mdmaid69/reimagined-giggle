669ae78636691c1e02b06de7f8bf893c3abf3d91
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:47:15 +0000

    stretch between period somebody generally

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
