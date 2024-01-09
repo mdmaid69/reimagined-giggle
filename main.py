9d6bf8a529c2b0600a104c40d3ba4addccc31468
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:00:17 +0000

    inform crash breakthrough force breath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
