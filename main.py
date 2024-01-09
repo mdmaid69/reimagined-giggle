bbe45da7fbbe9b6de291bb898757230bbe20955d
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:58:59 +0000

    favourite fat whistle dress schedule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
