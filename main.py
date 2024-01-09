067f219e6f4014c98e6b2d9aeec66bad5514b081
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:35:42 +0000

    or else somehow goaway correct

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_set(array):
+        return set(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
