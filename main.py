31c53edb7a51f1a400aba1b80ec3564aea094a3a
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:10:06 +0000

    want rush exchange artificial club

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
