c5dd977579385c720ca23b92f0bbe8ed2ea24b7b
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:27:43 +0000

    fly though correct strict piece

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
