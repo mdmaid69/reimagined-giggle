40d8b8c52446144c8b09038bacf2d186c427aeab
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:19:06 +0000

    insist down legal smell but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def cube_number(x):
-        return x**3
+import array
+def get_array_typecode(array):
+        return array.typecode
+import collections
+def create_user_dict():
+        return collections.UserDict()
