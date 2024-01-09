d2c05e0ff2e44d980d350bf79d956333d5cd386a
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:29:05 +0000

    man drop trip before little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def multiply_numbers(x, y):
+        return x * y
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
