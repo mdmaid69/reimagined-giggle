8e6d188178c68244f09a603853d3254b436b900e
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:47:47 +0000

    comeout mysterious desperate ourselves police

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
