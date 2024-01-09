8a65c0da925611e65738ccbb956c248195ccc11b
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:31:19 +0000

    rare I fall turninto contribute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def get_array_as_bytes(array):
+        return bytes(array)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
