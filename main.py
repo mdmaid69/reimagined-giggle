a6e613369b13cabf2524b74ac0f526fa79e79a08
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:32:16 +0000

    equal herself prize nose gas

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
