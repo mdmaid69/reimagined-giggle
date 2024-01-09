c793ce77585295fa38c84e20721ef9dc0181c235
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:32:54 +0000

    godown as plan achieve build

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def convert_to_binary(n):
+        return bin(n)
