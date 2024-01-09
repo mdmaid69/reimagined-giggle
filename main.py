6ce614192acd9d78e5d7bd11c57123bf4944ea2f
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:43:11 +0000

    mess secret please development female

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_acceleration(speed, time):
-        return speed / time
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
