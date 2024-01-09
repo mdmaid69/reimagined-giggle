287e6cc00e912fdced1ff3269fc4c82608d740eb
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:33:30 +0000

    doctor innocent just measure shadow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
