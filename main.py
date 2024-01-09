3ef837aa463ed847ffba04f3ec71d47546dc7bca
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:02:24 +0000

    weigh high urgent official raise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import time
+def get_current_time():
+        return time.time()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
