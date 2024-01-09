914a3ee1b2147ddc041c30560709ac61ed54f06e
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:48:40 +0000

    attract hide once activity nor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
