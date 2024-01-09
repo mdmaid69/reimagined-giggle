6c6739a39ef82fdb0fb1e005630e8471e8d343cf
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:59:09 +0000

    cross talk influence know development

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def calculate_area_rectangle(l, w):
+        return l * w
+i = 0
+while i < 5:
+        print(i)
+        i += 1
