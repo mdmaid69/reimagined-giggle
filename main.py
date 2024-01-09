20f566fd9736908ba5a1c51762c9681ce28d42fd
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:16:53 +0000

    appear figure glass extremely behaviour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
