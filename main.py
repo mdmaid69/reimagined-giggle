f7db27b72cd92d3de1f080e83445a3a0b3bb62b8
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:01:10 +0000

    club 569enjoy 570enjoyable apply spend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
