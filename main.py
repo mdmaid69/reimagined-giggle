34be1dbe53eccfdcba001e8fc8531b3e694e7c3b
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:22:51 +0000

    allow majority immediate while start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
