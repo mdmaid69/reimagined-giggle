d9044c61f454ab0f1a318dbb741aa120c69eda48
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:43:38 +0000

    science happen basis purpose Kk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
