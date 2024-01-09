146524b6d173d86f15f527390718dfa74ff61929
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:21:19 +0000

    sleep vanish illness alive opinion

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
