92480e13126e55569ae90a3e85441d567df3d1fc
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:46:09 +0000

    sure Ee shock develop band

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
