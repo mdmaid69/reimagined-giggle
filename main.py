fc044f9ea52c46bc41c7c622186d2c94c2d1fee0
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:01:29 +0000

    memory build entry communication them

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
