6c9fd28c973ec96debfe3f9d137b40b5fdfbdc26
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:51:48 +0000

    star train underneath interview show

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import platform
+def get_os_info():
+        return platform.uname()
