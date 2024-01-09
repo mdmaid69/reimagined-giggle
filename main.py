2b4f27f328b397788e6aaa992634b29a6b944fdc
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:54:45 +0000

    generally high on heavy invitation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import math
+def calculate_cosine(x):
+        return math.cos(x)
