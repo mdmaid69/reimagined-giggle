66f1c2c9b5915bd984e7b42344afa407c42aa1cc
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:33:17 +0000

    runaway characteristic last evening sea

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import array
 def get_array_as_memoryview(array):
         return memoryview(array)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
