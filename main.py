e462e9484891e82d68502a77dad8a7278886fcb4
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:25:03 +0000

    mend correct substance car rid

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
