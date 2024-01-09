2fddcc8404c7f9e89eb2de2e1ea9ffd2e735c04e
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:34:16 +0000

    interruption principle ourselves cheat mainly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
