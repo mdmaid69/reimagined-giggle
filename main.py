823faf4a4b84a0d57d5947bf414b84d76e897d48
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:25:21 +0000

    arm proper when whoever large

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
