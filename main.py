3f2236d89565010bb4d31be0b0884bb8d5270bba
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:36:31 +0000

    even else promise spare identify

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
