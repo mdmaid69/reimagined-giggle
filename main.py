51711edee6c767b088b611f8edcf998873f15469
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:52:27 +0000

    whenever cut Jj process character

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
