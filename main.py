85315bac1f2657b1e330338fc0b81696fef3978e
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:04:03 +0000

    rush cost connection opposite flight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  def subtract_numbers(x, y):
+        return x - y
