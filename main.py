e70aa74fec4d2f7b52740f5b589d703ea5f110f9
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:42:02 +0000

    office Dd news union whatever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def remove_from_array(array, item):
+        array.remove(item)
+  def square_number(x):
+        return x**2
