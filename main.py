3365b7e45f0af7f98fd99724d9d17c9fa564bc96
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:49:21 +0000

    their line eat more confusing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
