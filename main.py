7eb9d01b21086dabd539761a57ea7ae711b77ec6
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:18:59 +0000

    small below work tragedy picture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
