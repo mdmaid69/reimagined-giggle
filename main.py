95fb6a8114322e932df9b0fb05cf05341bdb125a
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:11:23 +0000

    watch tune fierce earth would

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
