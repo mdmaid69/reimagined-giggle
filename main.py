a36574d3b0e9544067ed5fd653a4ae6e15fd4f33
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:18:52 +0000

    left completely tear detail long

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
