d33c005066eae9c7835989c88d2901f254628e40
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:39:24 +0000

    until thinkof provide team slice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
