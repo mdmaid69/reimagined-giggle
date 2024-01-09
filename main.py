03d9bb218944d6dd5007cd1cb622be80b04dfcce
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:30:26 +0000

    concerned piece Kk computer callback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import platform
-def get_python_version():
-        return platform.python_version()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def find_max(lst):
+        return max(lst)
