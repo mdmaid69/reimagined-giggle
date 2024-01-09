52c89485a768fb7a259b78a52bb922c907ecd450
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:41:20 +0000

    near circle smile body design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  def multiply_numbers(x, y):
-        return x * y
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
