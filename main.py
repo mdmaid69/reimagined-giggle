c6463e538665f9bda51e0ba546593563abe145cf
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:16:48 +0000

    everyone confuse callat course order

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  def square_number(x):
+        return x**2
