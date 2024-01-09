365de0212a1e5f34fb7fd571fa58b6a3c5a53e03
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:46:49 +0000

    side cause country wall workon

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
