839aff817ea782d5d58966409826dc6bbb4669ea
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:10:39 +0000

    everybody deserve stamp correct take

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
