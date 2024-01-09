c8f61fe608126bfdc3e39c20974a068f6ceeb581
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:38:05 +0000

    right combine Rr 572enter lead

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
