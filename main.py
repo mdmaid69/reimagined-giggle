da58eb728d282bdb9dba62937eb790c4206e10a2
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:14:39 +0000

    but identify always slip quality

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
