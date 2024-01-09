27f029615c4c90f2fb846b175f5c0bb913eb0b34
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:07:39 +0000

    reply Ss still blow connected

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def count_characters(sentence):
+        return len(sentence)
+  def reverse_list(lst):
+        return lst[::-1]
