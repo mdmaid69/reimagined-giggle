fc098bdd04dc18416eeae362fcd8c9a70169b13a
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:39:06 +0000

    hope definite affect Ww introduce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
