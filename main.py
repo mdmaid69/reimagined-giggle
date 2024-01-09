6d7ddedba78923d26cb86164d4f0f2d804d89986
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:55:14 +0000

    attempt along Ll scare total

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
