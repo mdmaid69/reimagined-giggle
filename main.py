27c58eed464b75deae892cba38495ac8c381d032
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:32:03 +0000

    neither family care fierce correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
