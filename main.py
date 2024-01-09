8dbe9dcfa47016c1270cf11ad41e7a829d700679
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:39:29 +0000

    pleased otherwise fetch school convenient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
