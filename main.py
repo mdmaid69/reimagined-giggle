33be9cfe5a050c0f3d28bce15640aa9230853080
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:38:17 +0000

    talk themselves impressed weigh wash

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-print("Hello, world!")
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
