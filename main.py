8b0f84d1ce027179a40727c9dc6ca21fb76c2724
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:09:51 +0000

    town fall type mysterious pronounce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def add_numbers(a, b):
-        return a + b
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
