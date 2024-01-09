75c9e7c1e2f9c1e1fe7414c732070bed46615fcf
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:07:25 +0000

    involve guide always day dead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
