52abc2c845da213e5207ed9b3b62f613c478bd21
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:32:59 +0000

    later damage trick branch fairly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-print([x**2 for x in range(10)])
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
