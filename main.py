7e6a3f27fd5be08c77b876eb38869f968081f21b
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:04:18 +0000

    peace smile sentence recommend tidy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import datetime
+print(datetime.datetime.now())
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
