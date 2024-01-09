da195c62a5069d2f5cab025abf60feb133c8b2ee
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:45:04 +0000

    clean conversation crash record Bb

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-for i in range(5):
-        print(i)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
