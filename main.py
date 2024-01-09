784c053a21efb7a1b55c6ace951d74ab661dc946
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:26:29 +0000

    company suggest catch crowded peaceful

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
