73e58a80fd1d41ffbf2d64fd1ff8d9cd5a914e44
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:26:48 +0000

    emergency still alone string recover

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
