02ad8598c178cb210cc449aa76b6a76b4a7702a2
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:56:38 +0000

    from comeover hello thank wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
