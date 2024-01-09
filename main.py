958ea50993b7cf1c20205f28d8b3c5e5b86c8d38
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:18:45 +0000

    square comeoff soak class welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
