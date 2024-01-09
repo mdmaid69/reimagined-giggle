4293e1c9d9cf52cb07ab0f8b5e340b494d85e31b
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:05:21 +0000

    before vanish congratulate ordinary poor

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
