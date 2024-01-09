ab1b15555843e275445f2c64446a9ed246c4f6fc
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:45:06 +0000

    correction takeout simple takeover through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
