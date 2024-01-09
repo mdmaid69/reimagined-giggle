92fa690e8d69e29aa7899b7793ac4dcdd0478d82
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:22:45 +0000

    language bag considering convinced possible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_current_working_directory():
+        return os.getcwd()
