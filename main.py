9588a22fe5d5b340cd6172a085765c139c6c3450
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:53:40 +0000

    spread limit disgusting respect lean

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def convert_to_octal(n):
+        return oct(n)
