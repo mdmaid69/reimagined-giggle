f1d44ee2b4a5a801e142df02c43640bf242f95fc
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:54:20 +0000

    sound copy breakinto especially cough

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import math
-print(math.pi)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
