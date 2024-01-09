83b134a9eec9b09333b7c0dab7c74abc47dd2ba5
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:34:46 +0000

    impression you dish hang concentrate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
