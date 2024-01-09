2d699f7be82955b698d10edde48bb75f477eda89
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:20:05 +0000

    shoulder whichever experience release hurry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_min(numbers):
+        return min(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
