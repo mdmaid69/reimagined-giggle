210ba225d80d1f2a224f5487b85c31b65a11d913
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:36:11 +0000

    bar during sore its immediate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
