f7a3bee6f643c19fea219738b8e68edb371fb211
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:26:31 +0000

    work whole approval switch consider

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_density(mass, volume):
+        return mass / volume
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
