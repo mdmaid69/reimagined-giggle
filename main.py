c838a67489cfc338babcafdce651dc36090f80f7
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:47:10 +0000

    compare press pretty minimum treatment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def convert_to_binary(n):
+        return bin(n)
