19102e9f1e4d2af05d95d0b23f46e9ae03a1f838
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:25:06 +0000

    means breakin instruction comeout common

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
