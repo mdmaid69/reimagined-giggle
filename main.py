023e78c86656922f83ce87b832bba6963cb79de0
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:43:23 +0000

    form long exchange attention enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
