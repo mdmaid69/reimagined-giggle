c8c429c33b3925c602312b929349d0f50352af33
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:58:17 +0000

    direction keepout still any out

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
