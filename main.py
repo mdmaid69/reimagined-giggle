6521436b7ff9dc0256f5b8463d8a120eb0d88099
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:07:38 +0000

    just down issue bread spoil

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def remove_duplicates(lst):
-        return list(set(lst))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
