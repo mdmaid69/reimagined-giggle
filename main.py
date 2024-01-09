464938bf0a9c5a1535fc7299a6ec4a117de85258
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:49:58 +0000

    poor unpleasant wonder morning man

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
