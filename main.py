f95761363ed10c81c5cba4745f4cb812afa77db3
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:41:39 +0000

    so draw club supply future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
