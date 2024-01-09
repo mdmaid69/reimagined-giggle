030d82224c015584b8dae736611a14d9c92bfda2
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:29:56 +0000

    back calloff last steep trust

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def is_even(n):
-        return n % 2 == 0
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
