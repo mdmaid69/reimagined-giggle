cc2dd3a712e9b3c08d530999e2b57bcd7c6026cf
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:51:43 +0000

    cover size finally heart ashamed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
