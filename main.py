494ca2107281a8a478a6110f798e8228c47a887b
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:58:49 +0000

    could tend nervous beneath rough

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
