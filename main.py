8d8577e2bc6adc53981f41e5c236246efde02c3b
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:40:38 +0000

    straight accidentally machine nearly geton

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
