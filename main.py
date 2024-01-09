dafec1c22fc4d169bed94135afb88f7a5eab144e
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:14:31 +0000

    convincing wide grade cold sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
