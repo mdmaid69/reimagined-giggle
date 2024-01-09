d9d915cc599021e04f214e61a63bc31b96b9a9b1
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:10:58 +0000

    they square arrange yet belief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
