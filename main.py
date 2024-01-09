e27e32f8c9ee68d6081c23eba817a33e80991dea
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:58:27 +0000

    describe public indeed advice sentence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
