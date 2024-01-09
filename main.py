31abfffba776bb3d686d7d4e1b44ed9e938d5510
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:44:03 +0000

    something question colour grateful standard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
