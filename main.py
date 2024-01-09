2b0fcbedfd24ba3153d097c21d4d174c85f46ead
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:07:41 +0000

    cope copy common vote herself

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-  def reverse_list(lst):
-        return lst[::-1]
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
