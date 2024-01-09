e60d6012aef3ab543015dccb83975a1f0aa3ccc3
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:16:43 +0000

    live drawing furniture bad doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def change_working_directory(path):
+        os.chdir(path)
