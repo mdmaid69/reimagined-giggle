dad64bf7f4aaa1e93f5c1131aea78cd818eb919d
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:08:59 +0000

    various dull common demand afraid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def is_odd(n):
+        return n % 2 != 0
+import array
+def set_array_item(array, i, item):
+        array[i] = item
