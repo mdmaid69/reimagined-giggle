4e68fab9ea359867ceb458d1f3da0ef8c006562e
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:10:52 +0000

    fond increase threaten right let

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
