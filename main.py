4145eb9516fc393f8a23e786fadafb5e79af4f43
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:11:02 +0000

    exhibition awake comeup value tone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-print(sum(range(10)))
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
