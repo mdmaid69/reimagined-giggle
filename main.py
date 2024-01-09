8fdc2f29bdc67595ba06ed0152fa766760c4971a
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:08:58 +0000

    see insist really closed niggaacademy

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
