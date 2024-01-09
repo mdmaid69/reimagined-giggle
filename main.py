a94fc5ea62507f56d74f128f2491788dcb427a9f
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:33:48 +0000

    dull I sort team well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
