3d78fc1087ea2cc31e7cfb095a1352947724da27
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:23:03 +0000

    several born soft along conversation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def convert_to_octal(n):
+        return oct(n)
