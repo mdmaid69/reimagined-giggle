7cfb2ba2b9183206eade27bc97c2fa7e6aa0f2f9
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:35:44 +0000

    cause anxious child allow injury

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
