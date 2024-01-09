72ba31843785ad6ec4296d00d8ec6a0de2c07b34
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:26:57 +0000

    earth set food hour complicated

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-print(math.pi)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
