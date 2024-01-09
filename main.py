4c2051d3262d7f3da875fc8ca16ab020520051bb
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:41:57 +0000

    punishment load write notice train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_item(array, i):
+        return array[i]
