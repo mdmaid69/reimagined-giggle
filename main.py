213feb18504b3d70af019ba97f921fa62ab79495
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:31:39 +0000

    rarely excuse evening combination rather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def calculate_mean(arr):
+        return np.mean(arr)
