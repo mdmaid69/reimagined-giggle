2eb33c215b2d23dca62fbedf026659b586a1cfa9
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:16:11 +0000

    tap sometimes this apart giveout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def find_max(lst):
+        return max(lst)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
