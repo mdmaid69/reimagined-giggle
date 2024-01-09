cfe16f940a7fd5c701af53ed1234d74fc5cbd3b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:02:04 +0000

    mark dull owe there lesson

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def remove_duplicates(lst):
-        return list(set(lst))
+import datetime
+print(datetime.datetime.now())
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
