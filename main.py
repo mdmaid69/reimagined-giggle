63e8ea312f2c07f6460771c66e5dfeb4a641a830
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:15:48 +0000

    wealth pot beneath profit contain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def square_number(x):
+        return x**2
