f63b5c3fbd70faea616ef9a5a7a2a5f0311dda4f
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:03:17 +0000

    target stop beyond callback post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
