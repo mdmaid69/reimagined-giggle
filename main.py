1b02682bc42258500b3cb6401bda828a99b77b10
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:20:15 +0000

    sensible new scared Dd own

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
