df890ac14458a700e7003cb73a327dc6df8b2901
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:07:01 +0000

    false equipment immediately after run

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import collections
+def create_user_list():
+        return collections.UserList()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
