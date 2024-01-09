53d96a68500d5920b836fef53e4cb2f42bd9c556
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:05:40 +0000

    process television light suit translate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
