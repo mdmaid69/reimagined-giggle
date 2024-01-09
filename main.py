7fd7c378225613cb46cf02c588350274167f0e00
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:20:51 +0000

    compare 573entertainment unhappy confuse its

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
