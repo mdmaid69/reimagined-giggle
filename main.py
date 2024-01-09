6fb4f3015165add92c5f0752b9177892580dfb81
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:51:55 +0000

    we goodbye secret escape mail

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_area_circle(r):
+        return 3.14 * r**2
