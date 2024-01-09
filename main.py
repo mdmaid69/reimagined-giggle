1849b029bd1bc3140652f28d5600c891de457440
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:11:41 +0000

    favourite cruelty join complaint division

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
