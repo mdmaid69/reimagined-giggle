cf26c908172fc43981023cf0123559ad71232686
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:54:28 +0000

    human Jj serious vanish cap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
