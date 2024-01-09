369b4acd579ba5863b7b6494b92b152300317317
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:27:24 +0000

    little total respect closed practise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
