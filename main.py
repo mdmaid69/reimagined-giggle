5f0ad49a48efb93ca1002bda8146a8141f140a1e
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:30:58 +0000

    list sharp need basis visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_bool(array):
+        return bool(array)
