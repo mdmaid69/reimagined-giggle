bc4f723ce1cc8c8df7fb4d512b4b10e28ba29dd5
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:09:57 +0000

    interested single declaration persuade home

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
