d14d4c2a9a466d7c0b2aece1916190ea89c270e3
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:52:07 +0000

    dark dead amount level flavour

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_area_circle(r):
+        return 3.14 * r**2
