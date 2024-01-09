374b52aa00e345c673626f778091cb2e02fa373d
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:59:23 +0000

    though figure left believe steam

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-print(sum(range(10)))
+def find_min(lst):
+        return min(lst)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
