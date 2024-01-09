2e2f074db579f17f8fcb50b676a6c3270d365373
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:22:59 +0000

    across carry shut taste ago

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
