e529bc819f92c11a0d6ff865f9b288507eabf72d
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:29:45 +0000

    public brave last plenty jump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
