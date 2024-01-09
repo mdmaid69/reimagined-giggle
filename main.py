717b61ab3634a2909cdb75195365fa204e7bcc70
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:05:30 +0000

    tooth takeoff tidy mark share

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
