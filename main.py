1074162f76b03084ae594c6b5d64c7c78121dc74
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:14:41 +0000

    black downstairs upwards still reasonable

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import array
+def get_list_from_array(array):
+        return array.tolist()
