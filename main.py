8682c5993bc84f95d2c3998e223fd3be9b6c5883
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:26:40 +0000

    congratulate later land condition face

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import numpy as np
+print(np.array([1, 2, 3]))
