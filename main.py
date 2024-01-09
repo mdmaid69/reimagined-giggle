e2a8d59a4e14b6dc2dbf06fe0ba2e56b97d82bec
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:43:58 +0000

    born their cool wind greeting

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sys
-print(sys.version)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_area_circle(r):
+        return 3.14 * r**2
