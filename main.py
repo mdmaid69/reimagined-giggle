0a6646c41081a470dd8b82acacac1308b08c0276
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:27:35 +0000

    dry sure store nerve anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
