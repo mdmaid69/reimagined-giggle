692727216d21280a25ef7cc3bb26e636be5b5f01
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:12:16 +0000

    moral unkind ball natural patience

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
