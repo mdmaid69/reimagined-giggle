3743ff0daa1e656ceffab99de07c16a259863633
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:35:59 +0000

    many thankyou everybody other either

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
