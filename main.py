57ae0837561ee057bebca521c2839ba492ae229f
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:55:51 +0000

    waste television artificial direction tip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 def multiply_numbers(x, y):
         return x * y
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
