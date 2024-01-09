aefa53b80fcefd9e53a2e72948753300ee4d9ca7
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:28:13 +0000

    free duty track familiar stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
 import sys
 def add_to_python_path(path):
         sys.path.append(path)
+import json
+print(json.dumps({"name": "John", "age": 30}))
