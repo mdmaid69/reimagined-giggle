c1010a9c850fabdc9a8d00c065ec7748b7ed93b8
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:12:34 +0000

    kind practical high correct depend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
