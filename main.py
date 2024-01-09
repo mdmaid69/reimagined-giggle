aa79fbd8417668e6b1c317a079d22316cb212d3a
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:27:08 +0000

    slight office light spring mistake

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
 import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def get_time_since_epoch():
+        return time.time()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
