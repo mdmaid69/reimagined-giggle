5a7f8f5bd045011b3fc469f0ff608eb115c92f10
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:54:23 +0000

    disapprove bush mention sticky slow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
