e6f9032563aafee9bbdb8a04fc11a18c525cf840
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:17:28 +0000

    pupil journey identify Yy negative

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
