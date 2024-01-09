a133be669ba6ba76e45e1f4bb07488d51df195a0
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:28:03 +0000

    meet uncertain maybe amount tradition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
