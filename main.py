fad38b2c97c2dc4fbe397380867742f2249f9fdf
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:16:55 +0000

    keepup insist square stage national

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_work(force, distance):
+        return force * distance
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
