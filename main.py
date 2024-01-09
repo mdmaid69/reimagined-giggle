8163aca3426d1fcbf010327bd7353212ed502cb7
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:06:42 +0000

    brother special concentration loose load

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
