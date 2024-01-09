5e43dec015def560d89bf334b78d5c263ac84c5f
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:07:01 +0000

    meet line raise arm street

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
