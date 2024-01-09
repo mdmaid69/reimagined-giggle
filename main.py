fb91853bf031a56206a5c4b33ba4f0e2aef0291c
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:41:54 +0000

    somebody high effort smoke apply

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import collections
-def create_stack():
-        return collections.deque()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
