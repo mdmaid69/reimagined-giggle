d9df0c3c4658b4eb3fd9e82fc37d12b373b83610
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:25:50 +0000

    face attitude contrast boat visit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
