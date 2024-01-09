7d58b16500e6bbe3864877d1481c5b3406986936
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:54:00 +0000

    sting history contain use refuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
