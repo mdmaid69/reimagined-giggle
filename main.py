16fb21b04761f4fcf25e7b4566995d6eff4e9f90
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:20:11 +0000

    favourite stamp practise listenfor private

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import getpass
+def get_username():
+        return getpass.getuser()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
