d4b3dfe528569ca9add5bc804107566bc097bb4e
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:28:36 +0000

    responsible turnoff border whatever light

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_work(force, distance):
+        return force * distance
