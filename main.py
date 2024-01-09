ec6dd984cfd8274300966c12d50466d0628b794b
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:57:35 +0000

    rather violence avoid fast thorough

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
