fee5203532f7bced56f7f18911a0a09044f7720c
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:22:17 +0000

    under bringup bite mother spite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def square_number(x):
+        return x**2
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
