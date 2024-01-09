db29be3b5ea30a5c217a63cea586d7209b189afa
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:18:08 +0000

    combine convenient find punishment mainly

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import time
-print(time.time())
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+print([x**2 for x in range(10)])
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
