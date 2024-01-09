d8bd56962158dd4f9fd1a72b2a7cdaedb5f98550
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:12:24 +0000

    depend stick temperature unable 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
