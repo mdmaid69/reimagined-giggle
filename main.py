e725eea2eafc8dda9eed9b480045ac7c369b0cae
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:29:31 +0000

    speech question creature keep tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
