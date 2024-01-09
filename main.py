87bb91534c01eff0eff64477b8eb08d662577982
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:11:29 +0000

    turnup bite closely proud zero

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_work(force, distance):
+        return force * distance
