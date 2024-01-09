39bb2f5a7e2c37e59fac83c03aee02e75b7dea8f
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:01:18 +0000

    no within formal makeinto present

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
