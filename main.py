592445fb0b09c3a1b4ed39335d7a4ea5de91d458
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:44:10 +0000

    declaration shadow stir brush trousers

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-for i in range(5):
-        print(i)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
