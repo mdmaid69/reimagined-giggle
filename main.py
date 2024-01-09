a734f6a13b154915bdf2c338211bf153543ec19f
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:28:21 +0000

    polite keepup after more admit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+from collections import Counter
+print(Counter("hello world"))
