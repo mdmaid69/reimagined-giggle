59aa61c224ebb5b4e0ae326565d850b769b12abd
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:01:15 +0000

    talk example behind limit Kk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
