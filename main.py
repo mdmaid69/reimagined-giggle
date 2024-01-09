1bd4d349a2610d44119dbce0fcafdd6c33203da0
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:37:08 +0000

    come giveup opinion interview press

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import tensorflow as tf
+print(tf.__version__)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
