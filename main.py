af54ffbedbdb8c3079277332ec9056d60ce3af05
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:11:39 +0000

    dress healthy cry beyond woman

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+from collections import Counter
+print(Counter("hello world"))
