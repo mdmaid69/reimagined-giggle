f94a1a7ab0d1624a8b58877907c19a350cda259e
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:32:46 +0000

    decision peaceful score describe bottom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def reverse_array(array):
-        array.reverse()
-  def count_elements(lst):
-        return len(lst)
+def get_array_length(array):
+        return len(array)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
