8343a37ff6f40b76c6b7ea4cd65e94737f0bae26
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:06:19 +0000

    entrance operate needle water muscle

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  def reverse_list(lst):
-        return lst[::-1]
+import getpass
+def get_username():
+        return getpass.getuser()
+  def sort_list(lst):
+        return sorted(lst)
