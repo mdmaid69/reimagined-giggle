54e32769680984af47a9604aa3e5e4d7ebef1084
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:05:14 +0000

    pressure tired stir easy tool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
