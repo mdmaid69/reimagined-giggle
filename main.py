829e5382ae3ade860b6f2d9693e347b3a8b4ce7a
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:58:22 +0000

    such undo who pleasant fire

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def square_number(x):
+        return x**2
