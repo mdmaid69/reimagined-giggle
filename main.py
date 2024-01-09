05c46e9159cf0e4354f60dae63e34fa69edbb153
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:15:36 +0000

    sense regret please smell hair

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
