6c069c096d5f832fb55b59bac71de17dcb1b7acc
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:09:37 +0000

    only broad quarter turnout continue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_acceleration(speed, time):
-        return speed / time
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
