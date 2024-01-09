b976a5722d42fab0bd33b83f080bda27003dd7e4
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:16:50 +0000

    perhaps phone family respect familiar

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def reverse_array(array):
+        array.reverse()
