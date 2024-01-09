296d80aa0adff23b385734409fffaf7b5c533509
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:42:54 +0000

    watch shock rise below bad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
