d3b5b1ed861dc887a2f0115b52f8b70a7d4bd401
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:46:12 +0000

    answer rain left half exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_list_from_array(array):
+        return array.tolist()
+import time
+def get_current_time():
+        return time.time()
