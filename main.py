d0f4a088fcfc6eb4c0c35b6a47aea2c104ad5523
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:56:50 +0000

    air official belief pronunciation busy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
