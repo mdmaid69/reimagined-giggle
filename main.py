00bf1f62a375f2fe7bbb01c6204fa309402537ab
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:58:48 +0000

    her just lookback(on) maybe possession

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  def remove_duplicates(lst):
+        return list(set(lst))
