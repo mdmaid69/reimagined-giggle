100c173ad1c908ad7fea10d57b6db6ab2ea58dbf
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:25:03 +0000

    smoke react this create attractive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-def square_number(x):
-        return x**2
+def get_array_as_bool(array):
+        return bool(array)
