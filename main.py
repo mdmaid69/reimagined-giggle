d407cfabefd85268d17e97c2467db849b2bf27ed
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:45:13 +0000

    healthy soak noise forgive plain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
