5e195f92954ac794088ca3e7436b187daace4adb
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:46:37 +0000

    pile doubt finally apart straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
