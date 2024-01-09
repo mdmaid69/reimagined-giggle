50715d97a5476ed2dca4134437ccd5e35acfa58d
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:49:01 +0000

    public whenever dry already strange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def count_elements(lst):
+        return len(lst)
+import time
+def get_time_since_epoch():
+        return time.time()
