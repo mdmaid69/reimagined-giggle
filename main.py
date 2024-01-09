61466ea5deb40489726ff01bf34c219eaade41a7
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:44:34 +0000

    offer hang 568energy common happy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def is_even(n):
+        return n % 2 == 0
