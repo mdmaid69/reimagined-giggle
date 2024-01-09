b6b32eb50348262a533f8750eec3e9efd198e8f0
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:34:03 +0000

    cap exchange bring 566end free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
