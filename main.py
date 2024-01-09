bf44c0947c57add1e4cbb7c0a437676ce4319961
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:47:36 +0000

    base birthday cinema skirt trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
