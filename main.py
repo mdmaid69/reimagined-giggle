28a92e97b3d2ca6858afd30c019db915191ff766
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:47:41 +0000

    calloff out especially operate hear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-def reverse_list(lst):
-        return lst[::-1]
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
