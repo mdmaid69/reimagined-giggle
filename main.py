ac8dbf73bccc2e70cbb4d050970ae378f2786006
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:56:42 +0000

    forward family to noise tool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
