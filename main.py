ee248af1817c9cc8d61d5b44d3ef72f0dfd6ecf5
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:21:10 +0000

    relative touch block circumstance clean

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+print(sum(range(10)))
