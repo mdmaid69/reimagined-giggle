078f0e2e3a8b467e2bebae0f6506b1c4345716b6
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:33:43 +0000

    book cruel professional welcome cross

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_permutations(n, k):
+        return math.perm(n, k)
