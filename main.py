d6910dc96762354b68e055e661e86881d35be37d
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:09:14 +0000

    turnup flow scream explain mood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
