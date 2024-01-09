fa6b1e473a5ca89398ba14f53c98d03a6dd81195
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:40:32 +0000

    tool turn word brief pleasant

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
