7ebbbb18a03bda2a0234ecc2c3c9f4a6e5f44947
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:58:37 +0000

    excuse recently attention handle keepout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def convert_to_binary(n):
+        return bin(n)
