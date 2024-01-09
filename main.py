7ed1568fc288af9a68b0723a140e045780e251ad
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:57:38 +0000

    OKorokay hear dark seriously mouth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
