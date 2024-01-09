ad7b55492b7a505e0f3fa29be5f6a07c32fdec12
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:52:56 +0000

    giveback professional attach march sign

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
