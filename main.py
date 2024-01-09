a8ab0e03462ba822ab527139310289d800b5a48e
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:02:07 +0000

    read speed combine less touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
