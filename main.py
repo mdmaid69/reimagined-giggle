9cb618314b1589f13660983e1397c6b8cd1c7132
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:00:33 +0000

    long crisis tidy getoff statement

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
