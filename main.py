5c5129e3dcc5e8088b04b985a4fc27a7ea630379
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:11:47 +0000

    ring time square theirs equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
