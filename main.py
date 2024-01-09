d2cd93d5bcb02115b2ab967862f6ebe29b8b53fb
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:07:51 +0000

    yet excuse everything course stir

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  def square_number(x):
-        return x**2
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
