5397ec8c59818eff19b08cd688c859ff6608fbac
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:28:16 +0000

    bridge afford long please settle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import collections
-def create_counter():
-        return collections.Counter()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def calculate_speed(distance, time):
+        return distance / time
