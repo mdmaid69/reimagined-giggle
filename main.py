ffd0db006e853b6d81209cf71a3353c8b03c3815
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:31:48 +0000

    motion ever raw Dd rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sys
+def print_python_version():
+        print(sys.version)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
