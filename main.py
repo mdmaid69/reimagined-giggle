4411c42da752549d12bc1a6aac17549320b440f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:51:17 +0000

    flow successful door steam minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
