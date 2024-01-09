11b17aad85218bbb2154db900edd8f945645c535
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:32:18 +0000

    charge actually goup course breathe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_density(mass, volume):
+        return mass / volume
