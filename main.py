91abb82f4f3c7374bd28df5e03883ed82a4cc430
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:36:20 +0000

    turnoff reaction quiet below very

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def get_array_length(array):
-        return len(array)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
