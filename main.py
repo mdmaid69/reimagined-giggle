5b61fa4c86d79fd1fea34871cfbad8f3df44a697
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:39:34 +0000

    dress demand care confused excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
