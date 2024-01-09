b762ad4314a25166f899ff586cc19a7326738bf3
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:47:14 +0000

    way pure low clean question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_pressure(force, area):
+        return force / area
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
