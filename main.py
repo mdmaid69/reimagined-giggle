e40694b56c7d38bac0d86a36280307b677214e55
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:45:31 +0000

    back neither break language regional

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
