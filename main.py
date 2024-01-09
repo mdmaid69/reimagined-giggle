a7caea63f3710f94d319660163bfc8802580da25
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:43:40 +0000

    approval close sentence arrange catch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def remove_from_array(array, item):
+        array.remove(item)
