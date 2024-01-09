caa8afe5c3217d6fecbae2df202b00c95b4e244d
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:00:19 +0000

    century strict back continue wear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def reverse_list(lst):
+        return lst[::-1]
+import sys
+def print_python_version():
+        return sys.version
