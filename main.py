c0d8c5491c53ab111c03fadb113a7edd149ca5af
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:24:16 +0000

    present cross another high roof

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-print(sum(range(10)))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
