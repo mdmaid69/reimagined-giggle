199455161c3ae82d2800d299e7f284c129b88f61
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:58:40 +0000

    group giveout minute object back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sys
+print(sys.version)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
