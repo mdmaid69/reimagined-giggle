718fcf94de06d430758185cec8c2f1b9b5a71829
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:24:22 +0000

    tie line reason food sense

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def find_unique_words(sentence):
+        return set(sentence.split())
