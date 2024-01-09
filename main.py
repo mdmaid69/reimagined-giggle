9f7d59ffe1d14c1391e43bdb503c5b228809817d
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:29:02 +0000

    dick importance why above give

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
