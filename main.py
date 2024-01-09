49c287fbe04fed20be689c2ee2c7aef6633fa678
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:25:35 +0000

    round back smile limit seriously

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
