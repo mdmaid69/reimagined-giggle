f4c4b5ef1181816566761d0eb542ded9be984e4f
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:24:01 +0000

    plain goout carry this realize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def calculate_acceleration(speed, time):
+        return speed / time
