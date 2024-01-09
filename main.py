62cb3763a410004e49a5c041f00037a6347e6710
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:08:21 +0000

    hurry worth cool popular complaint

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_speed(distance, time):
+        return distance / time
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
