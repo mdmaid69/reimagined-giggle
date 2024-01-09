cadebc993b93f40a39ca6812a983364f1c38680c
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:07:49 +0000

    thing refuse ear Dd straight

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import collections
-def create_queue():
-        return collections.deque()
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
