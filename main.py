34641c7fd85f58bbeb4be603ede761cb1bdc0faa
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:09:01 +0000

    comfort confused arrange hard board

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
