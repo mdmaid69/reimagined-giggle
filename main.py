14d9d567486cca3474d34c8a8da3cac04cffed2f
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:28:52 +0000

    clever education several character Ss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
