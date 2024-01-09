93c7d59dd60bac330aae3f98ea751e76c7a76a7a
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:05:10 +0000

    calloff thorough water charge age

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-name = "Python"
-print("Hello,", name)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
