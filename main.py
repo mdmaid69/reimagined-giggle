66a25bd2ef29385f1fcd191e00ba3485d0b55468
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:42:04 +0000

    fight record between middle doubt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_density(mass, volume):
+        return mass / volume
