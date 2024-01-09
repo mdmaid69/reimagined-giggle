23026434fc9927ebd5fede13058d460352708cab
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:33:04 +0000

    mainly taste dick anyone basically

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def convert_to_octal(n):
+        return oct(n)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
