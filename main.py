e948ed89a7e619e842e372009abd69a6d7201625
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:15:44 +0000

    calmly deaf wear classroom Rr

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def convert_to_hex(n):
-        return hex(n)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
