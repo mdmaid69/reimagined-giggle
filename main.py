d70ca3f588fe60978dcb8c3386c52bb5f0fcb6df
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:45:14 +0000

    wander urgent valuable ocean that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
