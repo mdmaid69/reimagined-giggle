8ec86e0e769d4474bf5d85cb476980db59227f38
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:32:20 +0000

    touch experience bit environment pause

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_base_name(path):
+        return os.path.basename(path)
