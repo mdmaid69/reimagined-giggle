ec3c9c469bf5e955c014a87e4d011218d7d4f61f
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:07:40 +0000

    brown lend makeup reduction cinema

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
