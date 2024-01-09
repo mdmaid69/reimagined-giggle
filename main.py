afa2d4aae35262e8a0626a091ec332abdb2e9178
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:36:32 +0000

    nowhere strike contrast fork culture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
