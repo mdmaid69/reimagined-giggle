9dc50204aab2b25daa8df620f76cb7f50f490099
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:22:40 +0000

    perfect female ahead then valuable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
