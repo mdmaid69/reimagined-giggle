d4bf6b300af34ec03fb5a60da146a84f6e6841c1
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:40:45 +0000

    across onto up hurt breathe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
