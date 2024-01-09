36c764b9a745f00babfafa52b9a422e4f4f00ce9
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:16:53 +0000

    fault though throw afternoon interesting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def create_numpy_array(lst):
+        return np.array(lst)
