63da828c56fa3db96121a8f815cdbf67b8edce2c
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:12:18 +0000

    illness dull experiment Ll closely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
