dd4a4e87d10f1522e7fb929621ca60d0df4fd017
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:46:32 +0000

    comfort 570enjoyable admit certainly copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_base_name(path):
+        return os.path.basename(path)
