e860ad936d0ceb25c32d71187b6f32b58871f723
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:27:37 +0000

    speed will so trust term

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
