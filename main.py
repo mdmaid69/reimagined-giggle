9753e585d0538d7c708229fe0de1f1e7f56a67c4
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:43:26 +0000

    wealth interested test Bb region

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
