9bd3f5f74a989fdb5f299379833809467d519fd4
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:37:59 +0000

    hurt callout arrival name difference

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import sys
+def exit_program():
+        sys.exit()
