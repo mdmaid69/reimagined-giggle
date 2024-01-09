1daba5b882dbc96eff0f22df3c222f4b72b29922
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:29:41 +0000

    petrol fight late absolutely characteristic

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
