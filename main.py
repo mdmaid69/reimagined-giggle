b60b10966140c5e50435bd59bba2b6a983ab55e4
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:13:58 +0000

    attitude blame practical confidence keen

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def split_path(path):
+        return os.path.split(path)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
