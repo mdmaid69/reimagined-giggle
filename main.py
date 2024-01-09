e87ded1c395abc877c3882ef7aacd129cbf33fb2
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:07:42 +0000

    truth behind mend lookback(on) cut

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
