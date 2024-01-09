8c3c22ea5a7693d96503ff5d3c28b8d30f071b85
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:43:11 +0000

    cause fast kill pattern once

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def divide_numbers(x, y):
+        return x / y
