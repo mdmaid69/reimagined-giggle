fdec5fad27fcfa65325d5b03735365d01eb16e53
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:48:09 +0000

    conclusion free centre model whom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
