22e53f4599dfb17d95a4de420909f2bceb84614c
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:44:36 +0000

    power usually brush satisfied clothes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
