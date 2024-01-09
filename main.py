887edb5860eed3a19f72f725d62a165a458e3e70
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:12:56 +0000

    most world perfect crash soft

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
