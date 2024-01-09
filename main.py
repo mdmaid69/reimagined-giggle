117e9dc5c6e0bababcd34b6ec7e37d0ed782fcfe
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:50:08 +0000

    point mind urgent attitude stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
