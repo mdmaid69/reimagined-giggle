a308380c7fc5ab8702c1ef32be14ff17aed02ce8
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:24:07 +0000

    body false cum seem amount

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import platform
+def get_python_version():
+        return platform.python_version()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
