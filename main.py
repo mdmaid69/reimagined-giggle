4687fedbcfa8dab7773261e75d774dfb15741278
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:17:21 +0000

    strict progress body write study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def reverse_array(array):
+        array.reverse()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
