bc236b438b844afeeb7dde182de8ce1a2a036098
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:38:07 +0000

    cross everyone suggestion 570enjoyable scared

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
