954cab33a439727f57b3c97e74af729f36e52598
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:12:05 +0000

    bin Nn transport juice together

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
