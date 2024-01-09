a06feb7d26a9616af308fcd6f0edaf8a1fcd7cdb
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:06:02 +0000

    bottom obtain clear opposite simple

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def sort_list(lst):
+        return sorted(lst)
