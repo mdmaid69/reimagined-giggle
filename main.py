e45c251a99e65a16f89889c81cf5cc2dadffcdc0
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:17:38 +0000

    accident excuse exhibition nervous guide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
