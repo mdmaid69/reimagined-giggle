65ecebe4e8997e35cd6222a488d536ff1a49401c
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:14:20 +0000

    tread not doubt bush price

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
