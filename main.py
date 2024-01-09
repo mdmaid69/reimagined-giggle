e5256874e0f812826d9c5910069fefcffc98fd98
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:32:40 +0000

    conclusion whistle union shock design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
