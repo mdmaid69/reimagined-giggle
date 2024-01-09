43bdd9c24dcadbda7ef2eab389f5b46a81671b59
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:36:13 +0000

    cum best turn surprising obvious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
