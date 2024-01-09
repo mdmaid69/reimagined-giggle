a8f76ea6342527ab153ee848ecef0cfcf13d2309
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:12:03 +0000

    person statement century explanation go

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
