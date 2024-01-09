e4987e5fe88aaf1378b370552ed4b7b35848c177
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:38:47 +0000

    Ss vision voice method accidentally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import getpass
+def get_username():
+        return getpass.getuser()
