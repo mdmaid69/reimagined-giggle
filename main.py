0a570de195057f344eac6cb05e022f9d2d369a72
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:24:01 +0000

    second copy mistake breakin stupid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
