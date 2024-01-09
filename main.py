ffc365e7a9b336407e276c07f8d0eda51999909d
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:26:45 +0000

    favour permission lunch fit announce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
