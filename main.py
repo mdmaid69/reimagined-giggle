a69802cf1134b213e1117b3955508517778cd274
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:17:49 +0000

    from around grow instead record

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
