21f10982f86b3495483cfb404f135c50ac8ef2e3
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:10:19 +0000

    confidence rush hurry original forward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import tensorflow as tf
+print(tf.__version__)
