9a50e4c378bdf79d3899a14df53415ba0d7024f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:58:54 +0000

    amount clever rent clever emergency

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_stack():
+        return collections.deque()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
