b9307c1ed8ca1bf2655c6fe5e2ad9cd3b462fbb2
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:38:17 +0000

    nose dress similar closely top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
