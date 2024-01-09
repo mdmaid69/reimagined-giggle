30aefa0e091107b6ab76a92b81ff04736995c122
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:59:34 +0000

    getout left offer show centre

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def count_words(sentence):
+        return len(sentence.split())
