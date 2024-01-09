141934d28b38c068d05c7da6a6e5fe1fd5ae4b2a
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:18:27 +0000

    maintain so news give makeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_current_working_directory():
+        return os.getcwd()
