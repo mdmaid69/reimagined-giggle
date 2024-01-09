7b5843c430fd8c96d980d664e35fec5586defc9b
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:12:11 +0000

    some unusual plenty ourselves break

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
   def get_file_size(file_name):
-        return os.path.getsize(file_name)
+        return os.stat(file_name).st_size
