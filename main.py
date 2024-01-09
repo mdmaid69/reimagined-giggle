f4b2ca1520e2f9a14753cc97b7d9db16800e7e42
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:32:59 +0000

    goaway plain song fire still

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_power(work, time):
+        return work / time
