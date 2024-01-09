25f1f4a3aeaf9d3c44721bb34793bc85d15fd411
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:11:55 +0000

    active callup visit tell high

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
