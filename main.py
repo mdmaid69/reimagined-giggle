92600dc373515efe2739710d93182fd722ea4fd5
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:46:52 +0000

    Uu fear make nice pleased

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
