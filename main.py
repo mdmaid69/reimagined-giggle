9e30d485819c51ebf0eb9c5ae4251f0ecb992f77
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:11:18 +0000

    design pain stare often account

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
