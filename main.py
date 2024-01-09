c951d1e64a019bcc915a3b9077f90908adce2a7b
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:19:39 +0000

    switch advanced live ignore hold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import os
-print(os.getcwd())
+print("Union:", set(list1) | set(list2))
