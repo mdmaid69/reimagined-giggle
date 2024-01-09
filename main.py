73542fdea9cb01fda614af916ac66ee0c2678b68
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:11:51 +0000

    eye beyond doubt male why

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
