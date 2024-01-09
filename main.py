9fd95ac5b5ff99561b2d3adbd07af5435385159d
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:02:14 +0000

    anyone breath suggestion although swim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
