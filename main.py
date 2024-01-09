c55eccf9c1aa02b373bb588b475453f4a869b227
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:27:50 +0000

    comein safe future mind regional

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_pressure(force, area):
-        return force / area
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
