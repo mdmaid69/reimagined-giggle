571bf06ad611648dfaadab1085b24f9569419164
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:49:21 +0000

    properly measure accommodation design order

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
