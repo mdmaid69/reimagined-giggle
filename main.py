bed7785792697588911d07e87fe777ffd25551db
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:33:19 +0000

    lost dress clue wash suffer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
