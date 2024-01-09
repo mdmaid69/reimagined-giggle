e6dbb658b556d3e48915226b028d875c6dc8f6cf
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:25:17 +0000

    search which pretty excuse getout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import sys
+def print_python_version():
+        return sys.version
