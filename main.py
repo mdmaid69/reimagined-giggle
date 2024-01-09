81997ed2dbcab88b6a8f91bb56c0b3ffa86e1af5
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:21:14 +0000

    takeback related speak callback much

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
