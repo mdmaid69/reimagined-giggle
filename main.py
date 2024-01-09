d2c95d0737074474884427bac069bbc900a249a2
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:20:37 +0000

    convincing mind mother cheque better

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
