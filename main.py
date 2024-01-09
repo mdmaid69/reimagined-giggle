a2377957949ed35376b19bbda22dcc7a2d5fbb2b
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:09:04 +0000

    mad body breakout unite answer

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def add_numbers(a, b):
+        return a + b
+import array
+def get_array_as_str(array):
+        return str(array)
