f2eabff389889d0c0031a3ea9a4757540a20c6b8
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:01:33 +0000

    backwards properly pass feed sight

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_speed(distance, time):
+        return distance / time
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
