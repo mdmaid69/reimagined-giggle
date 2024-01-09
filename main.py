abd5c680aa92f005396e92452408d464bed1f558
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:37:54 +0000

    drink getout bit suddenly cap

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_speed(distance, time):
+        return distance / time
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
