e15e9d6ae7324445f97d4fb070bd97de9b19f0dc
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:35:56 +0000

    score why pick risk smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def get_list_from_array(array):
+        return array.tolist()
