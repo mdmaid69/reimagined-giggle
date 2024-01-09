2e588182497c477452c9e7b732ae3aa9c6c32b21
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:45:41 +0000

    group beginning myself proof slip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_speed(distance, time):
+        return distance / time
