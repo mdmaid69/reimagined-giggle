399931bcb286dd922f4dc9130ce62335ea9a5d1a
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:34:36 +0000

    smart finger support stand sore

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
