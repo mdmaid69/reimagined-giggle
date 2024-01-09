720b434e274efc1cc3cfb3034ff0499728b19a99
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:02:13 +0000

    adjective mail separate reason lean

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
