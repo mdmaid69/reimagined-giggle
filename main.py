642fdf86ba31bdb9c86efd974913e41203e8261a
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:52:50 +0000

    note small line convenient drawing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_pressure(force, area):
+        return force / area
+def reverse_string(s):
+        return s[::-1]
