5217f2aaf989310fb3c8f43bbb9836a947544ab9
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:05:29 +0000

    more hate suckme healthy clear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-def sort_list(lst):
-        return sorted(lst)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_float(array):
+        return float(array[0])
