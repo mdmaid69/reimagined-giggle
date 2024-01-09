75a382668b775bdd4ff10d2008754b7d880dbf04
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:08:05 +0000

    listen else clue safe determined

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_pressure(force, area):
+        return force / area
