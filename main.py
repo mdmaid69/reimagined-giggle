2304b526e50fba28accf699ed53f711a8ad932c7
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:22:58 +0000

    react complete Ee off only

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def count_elements(lst):
+        return len(lst)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
