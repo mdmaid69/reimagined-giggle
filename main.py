90fb223138b54b752983dd58c856dd8ac85fa44b
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:55:33 +0000

    house adjective at neither prevent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
