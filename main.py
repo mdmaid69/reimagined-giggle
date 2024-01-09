c20d838005932b79ede08e3047a1645a55f83b17
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:43:52 +0000

    indeed dry work pattern both

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+for i in range(5):
+        print(i)
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
