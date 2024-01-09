1d9afdf1c6c2c8f99b2ca06ac82a169ec438efa3
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:24:45 +0000

    tie funny food burst intelligent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
