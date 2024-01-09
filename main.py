3d4fd4c5b01059cb5346e55fbef8d06482048655
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:06:05 +0000

    perform print actually temper rich

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-name = "Python"
-print("Hello,", name)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
