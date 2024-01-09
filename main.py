22e99c134db0b903d9c98de5f2d64a6468196629
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:07:09 +0000

    under sink glad furniture excuse

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def greet(name):
+        print(f"Hello, {name}!")
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
