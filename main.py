a3bb24ea81b44d09cc8c29a393dbd0fdf7724db1
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:55:55 +0000

    on temper correct themselves 572enter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
