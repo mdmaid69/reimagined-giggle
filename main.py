7cbb0eb8fd8b49a54155a5d2c3e4cd8c9f5c0717
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:06:38 +0000

    left change temper depend snow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
