91d8400f706433995fd87029f61d6fc443b617eb
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:09:20 +0000

    every agreement imagination mix quickly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_density(mass, volume):
+        return mass / volume
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
