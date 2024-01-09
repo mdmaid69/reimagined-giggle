0fec720b36b69b0b1bc30bbaadf42a9e2c590715
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:11:48 +0000

    line sudden greeting smooth ask

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
