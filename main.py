48577dc563a9c4c342c258593bec88a3cd14296a
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:52:03 +0000

    grand us OKorokay beautiful dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_speed(distance, time):
+        return distance / time
