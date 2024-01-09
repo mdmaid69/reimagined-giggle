5e7e9430ee4670acdd327e8687077b41beaca722
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:21:34 +0000

    roof promise difficult love even

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
