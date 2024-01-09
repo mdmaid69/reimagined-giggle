cf5575b524d1033ae768161d8f52b8ae44f3012c
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:45:54 +0000

    replace period shock computer food

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
