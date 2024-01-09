75477c6efce73bd08d1f879ba1801391061765a6
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:47:42 +0000

    copy so steady control weak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_acceleration(speed, time):
-        return speed / time
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
