8484a6939312c9e5b3b4c2f1eecd99cfd7d13bd4
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:39:19 +0000

    giveup battery increase wish so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_array_as_list(array):
+        return list(array)
