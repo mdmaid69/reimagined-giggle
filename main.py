908eb11e08b907a1f308c10af74eac308e741f79
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:03:52 +0000

    date turn callout tight spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import sys
+def exit_program():
+        sys.exit()
