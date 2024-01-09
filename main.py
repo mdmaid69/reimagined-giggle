e675b631e46557f7aeff134d519d6a8e6cfa2796
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:52:19 +0000

    impression blue against coffee big

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import sys
-print(sys.version)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
