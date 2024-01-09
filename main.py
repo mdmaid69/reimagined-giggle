acc5993d5640059421a79a1f8396a78bb704ab2f
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:12:31 +0000

    importance noisy operate everyone boring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+x = 10
+y = 20
+print("Sum:", x + y)
