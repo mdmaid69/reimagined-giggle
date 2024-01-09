fadbc58b497686a1910ae4aec344c8ad9c7f34cd
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:56:35 +0000

    suppose Nn shout bright post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
