b789318f0e89b18add21ab5c184bbef9c5ff9b30
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:43:09 +0000

    once advise zero tool doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  def sort_list(lst):
+        return sorted(lst)
