7dd23c8942bf45f8f59cd216d231afe92575e7dc
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:03:42 +0000

    fresh who great Hh without

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def greet(name):
+        print(f"Hello, {name}!")
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
