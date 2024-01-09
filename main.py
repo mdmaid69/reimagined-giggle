0a2673cd2c1d8ab7f0959ecfa57dc1305708758c
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:35:45 +0000

    study board car attractive turnback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
