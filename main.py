d29561a5359540892527bb1e19a78066ce6474ed
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:27:16 +0000

    break disgusting breakthrough although mark

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-for i in range(10): print(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
