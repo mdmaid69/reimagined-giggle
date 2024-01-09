a2c1dcfb84758ba6c340b8e594ef024287bce2d2
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:09:29 +0000

    idea speed race kick practice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
