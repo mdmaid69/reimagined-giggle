3a48e8a1fc2f47acea1b8c1d8a5b0450fb8e0ceb
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:35:10 +0000

    education appearance mind damage regret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
