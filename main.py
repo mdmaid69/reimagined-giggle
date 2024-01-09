3a86bc342863ba44946cd73ac5241bf7b14c4d96
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:35:44 +0000

    goodbye paper type conversation real

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
