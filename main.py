454fb7bc1b5348d8c6771c6ad19184a37bf580b0
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:53:49 +0000

    blame feature voice lie like

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
