b1e41106a737e8e3534c9fd5f1a5e0fdd9631fe0
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:57:07 +0000

    wide threaten circumstance towards crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
