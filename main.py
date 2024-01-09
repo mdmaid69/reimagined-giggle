f602fc0f2700541355832b981307d670ed5dfe00
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:43:23 +0000

    contents post declaration scale finish

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
