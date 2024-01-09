75a1f72cb6b0e679a03c991b7921ab7b36b45dd4
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:38:58 +0000

    conduct lot nature join unable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import numpy as np
+print(np.array([1, 2, 3]))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
