c35c333f609262dfe1c45e9420fc86956bb4e715
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:26:45 +0000

    language payment able middle very

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
