30a61225aa35a08978ff760179650532e69839fe
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:09:55 +0000

    lookafter Itself brown scrape underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
