9dd966a0aa91cc0160283cc0aaedafd177805465
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:19:42 +0000

    mine discuss famous light land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
