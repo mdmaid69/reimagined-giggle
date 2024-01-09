a761dd8075e122841398d14b61b8f14ee64347c1
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:40:30 +0000

    track record pipe such own

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
