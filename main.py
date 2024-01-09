fe2ae7a48fdf36265a8aa6b974e8740f8bc75751
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:58:32 +0000

    true payment watch custom wheel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_unique_words(sentence):
+        return set(sentence.split())
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_as_frozenset(array):
+        return frozenset(array)
