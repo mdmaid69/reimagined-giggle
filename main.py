5a3fa6f7fc431bc1c3f8b6aa57d093d70ad6eb6c
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:55:36 +0000

    lunch learn exam region claim

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
