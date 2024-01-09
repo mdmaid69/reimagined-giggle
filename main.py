e36824cf65aae4814fd92d32b363975b09d6b779
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:58:52 +0000

    without minimum passage suckme workupto

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
