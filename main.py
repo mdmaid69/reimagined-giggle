4ea2475d24224f0b8b3abf7d8aa381ce29b46251
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:31:51 +0000

    act right impressed truth major

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def convert_to_binary(n):
+        return bin(n)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
