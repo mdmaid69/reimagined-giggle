49f00d1b1fd55603e38428536d7dbc8b43cba706
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:28:04 +0000

    extra miss anxious offer injury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
