76c0fec12b0586a2e75972be1ecc02c5933ef9fa
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:25:23 +0000

    sail acceptable morning book share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def get_list_from_array(array):
+        return array.tolist()
