9a7cb3d0377e18e5436d98909d105874c3c64e63
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:18:01 +0000

    cover under face sad light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
