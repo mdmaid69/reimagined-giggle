ef7416a3ecdacb23aaaad57f4ecd88cf39a15262
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:49:55 +0000

    strict post hang whichever land

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def sort_list(lst):
+        return sorted(lst)
