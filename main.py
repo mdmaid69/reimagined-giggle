715dff3f69b4c32925febe4664424c8237a005d3
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:06:49 +0000

    minimum however debt bound uniform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+def get_time_since_epoch():
+        return time.time()
