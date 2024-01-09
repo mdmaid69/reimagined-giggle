c676deb78f4d6ae4837525eff0b3e290f25e29f0
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:37:00 +0000

    actually splash easy recognize win

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
