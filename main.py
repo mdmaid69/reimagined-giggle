a8f55ba26ddcde0912a8a453c207b6d762c82e50
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:09:52 +0000

    deep centimetre throughout meanwhile ill

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
