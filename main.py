94e44709503bd6abb1b458cec198916762b5e1c2
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:07:48 +0000

    want pleased brief club cover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  def square_number(x):
+        return x**2
