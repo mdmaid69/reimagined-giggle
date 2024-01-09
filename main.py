e43269b87399a940f324fe001772c394cad16f23
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:39:20 +0000

    turnon violence trial lower polite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_perpetuity(payment, rate):
+        return payment / rate
