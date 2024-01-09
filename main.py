6dccc741157a78c2c0178113d250447d6bfa5ad2
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:11:37 +0000

    wish colour judge house shopping

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
