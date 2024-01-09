bfaf417cc8e87f76cc3d1c6e7303f95e09ee0192
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:03:15 +0000

    mixture calm squeeze hair fast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def convert_array_to_list(array):
+        return array.tolist()
