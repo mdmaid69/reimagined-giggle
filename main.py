b8dc17cfd628426a86889a69fb87a767fb22afc1
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:25:03 +0000

    personal phone Bb reason back

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
 import array
 def get_array_from_string(string, typecode):
         a = array.array(typecode)
         a.fromstring(string)
         return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
