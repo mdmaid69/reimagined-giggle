590d8d3449aa16aebaf57a454928292f40592bd2
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:04:46 +0000

    exhibition address slightly fact move

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def is_even(n):
-        return n % 2 == 0
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
