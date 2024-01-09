c1fd57a369df0490758bd42c0822d48ee18de00d
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:15:23 +0000

    hold temperature thorough period agree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def square_number(x):
+        return x**2
