d97157e1991f4f114f28a011350c912d70c3950f
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:59:16 +0000

    hollow cut wrong identify centre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import array
+def get_array_typecode(array):
+        return array.typecode
