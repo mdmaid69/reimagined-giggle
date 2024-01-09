b84fd45da7d3270b77d4ae99f8feb014ead0c26b
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:16:32 +0000

    regular later damage patience confusing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_work(force, distance):
+        return force * distance
