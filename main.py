075b960ff873932b7159e17530fea4c5dec22a73
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:57:23 +0000

    goaway expression sentence door main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_cosine(x):
+        return math.cos(x)
+import os
+print(os.getcwd())
