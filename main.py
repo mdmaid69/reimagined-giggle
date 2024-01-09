92ca4ff9a2f50144e27194fccfb005b1e5e70c3e
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:46:09 +0000

    disturbing middle just control various

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
