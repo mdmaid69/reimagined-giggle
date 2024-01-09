ac88a0463e5184d6072ab188e04eb7431668cac8
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:50:01 +0000

    contain uponon comefrom wise last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
