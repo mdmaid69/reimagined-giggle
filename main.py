72fd9dc4651da5fd83fc05bfda6bcb52f55d470a
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:38:45 +0000

    strike cause breakfast invite race

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_list(array):
+        return array.tolist()
