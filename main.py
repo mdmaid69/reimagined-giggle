34e54bed45ede56b7bc0f9266e4a366745ce9988
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:20:25 +0000

    step remain shout school compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_str(array):
+        return str(array)
