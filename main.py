502fa87a97ee6d737e2a7beef57d66fd99bae676
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:50:20 +0000

    failure wait but church church

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_combinations(n, k):
+        return math.comb(n, k)
