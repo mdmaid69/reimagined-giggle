4f1fc76ed8cbd56a72d91c4d03d7d2f17ea06a3d
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:57:53 +0000

    pretend fashion means record hot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
