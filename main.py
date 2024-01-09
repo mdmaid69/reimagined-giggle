ed274d84f6da18210628a155acd1a5775e894f5a
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:46:46 +0000

    need sleep drawing giveout point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import collections
+def create_queue():
+        return collections.deque()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
