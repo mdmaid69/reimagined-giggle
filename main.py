40b895eb5f5e7737479be0d34c5639a2d2fbf80d
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:29:48 +0000

    shine guard interview snow impossible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_slice(array, i, j):
+        return array[i:j]
