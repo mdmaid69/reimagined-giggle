641f77e9fdc56e2a23a143d278387a483552e749
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:48:26 +0000

    whether any already bin turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
