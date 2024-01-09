af480274fcc7e2222df378e3872ae30a2c12f8ae
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:11:17 +0000

    plan tidy let beyond impression

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
-def clear_array(array):
-        array *= 0
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
