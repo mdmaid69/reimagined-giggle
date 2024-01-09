6f92c3b3d1fe96f69652b1b830d5670673f29b6b
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:59:14 +0000

    height especially prepare bottom Jj

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def print_python_version():
+        print(sys.version)
