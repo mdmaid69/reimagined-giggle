2b73ddc6269e816a1806d058686a928fec4eed09
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:38:41 +0000

    Ee dream thing return congratulations

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
