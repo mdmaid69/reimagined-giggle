fb0dbf7eccefa9cffa6f4d92567af8e2b89007cd
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:46:47 +0000

    strike absolutely continue search undo

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import array
-def get_array_as_set(array):
-        return set(array)
+  def square_number(x):
+        return x**2
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
