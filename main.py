e47e03244019fabf20efd71fbe9e5c36030720b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:05:53 +0000

    chin wheel judge shop opposite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def sort_list(lst):
+        return sorted(lst)
