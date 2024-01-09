65b1b6913875740e79cb2c93bb2f71c0af55f14a
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:09:26 +0000

    just law rush cruel state

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
