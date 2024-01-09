0756bb861c7f28da051ac904577d6c70d03f884a
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:58:50 +0000

    shout birthday nose turninto colour

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
