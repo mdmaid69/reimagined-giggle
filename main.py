4aab4d8cbedcf6bc66a09efcdccbbfc21543f62a
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:55:03 +0000

    uniform public account important later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import numpy as np
+print(np.array([1, 2, 3]))
