ce78dc91037a380162d0ca9daf275fc17e1953af
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:41:30 +0000

    contribute famous enthusiastic low touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def get_array_length(array):
-        return len(array)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
