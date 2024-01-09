dfe3821dd744bb9a300739efb3c8bd1981cd5a2e
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:14:31 +0000

    lead fail pay snow learn

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
