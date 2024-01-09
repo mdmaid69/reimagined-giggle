2c38710415b4861206cbe2c4d7d8c0ed2371729f
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:45:00 +0000

    busy look immediate duty Rr

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import math
+print(math.pi)
