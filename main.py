bdf7704b074afb1b3d34ccda713f7897542009ab
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:56:12 +0000

    afraid suddenly alone floor smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
