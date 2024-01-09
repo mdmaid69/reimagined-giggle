83a81685244bdd1734946c5b5bbe2587f561d058
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:50:29 +0000

    question improve long complaint risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
