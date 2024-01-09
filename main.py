ce9f1de595adf7fc6c1b2ec689f3ed284de1db11
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:58:04 +0000

    convincing harm classroom Mm block

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import platform
+def get_os_info():
+        return platform.uname()
