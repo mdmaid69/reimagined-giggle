222cb5d29145968f4ca10fdd5c954596513878cc
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:19:42 +0000

    black conversation system open card

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def find_max(lst):
-        return max(lst)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
