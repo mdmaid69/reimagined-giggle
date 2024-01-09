5440ffd9c82c8d7ee9a64c1e7bf410ff3dfa547b
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:20:58 +0000

    goodbye trust afternoon personal journey

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
