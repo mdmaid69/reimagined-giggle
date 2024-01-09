a97bfd9c4224560cf079d46dc9b45030ebb1b003
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:47:27 +0000

    ill please call project number

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
