c621ade9eae5a29d2505eb945cc2e2b6133cc635
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:36:04 +0000

    powerful disappear it burst result

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
