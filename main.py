668d7dfb742fdc5eec053f88e71e76562b1b2673
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:18:11 +0000

    woman see worse roof recent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
