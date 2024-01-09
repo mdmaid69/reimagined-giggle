30e01fd2b13fb7ee652a0520061dd8f3264dbd41
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:21:25 +0000

    straight common exit pretend ticket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  def count_elements(lst):
-        return len(lst)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
