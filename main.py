5748f519badad8fb2b95cef9bb8d449839832751
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:43:23 +0000

    squeeze address handle female nigga

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
