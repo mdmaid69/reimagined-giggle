b7d337c0bffca8bd508f06fe9f375cc105f74c9b
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:40:01 +0000

    act ours who brain vote

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
