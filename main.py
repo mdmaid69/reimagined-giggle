8733087d1443059a541e1dcd5058c5ce1b43cce3
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:02:14 +0000

    disagree comeover advanced available table

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def find_min(numbers):
-        return min(numbers)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
