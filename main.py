e41ab004db796d148b0ec003f1e60c3eef70416b
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:51:06 +0000

    female slightly money nurse deliver

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_array_as_float(array):
-        return float(array[0])
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_current_directory():
+        return os.getcwd()
