c58addca566278f1843e73a28bd8de0c85466f3f
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:48:04 +0000

    attention autumn body count care

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import platform
-def get_python_version():
-        return platform.python_version()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
