d11714b86f3a1196d5a0ea5e3f362b531e7d4519
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:15:43 +0000

    few much during dead little

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
