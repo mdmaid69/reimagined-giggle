4e3f70bc8e355f3e6a5267cb8ecc7bcf923a5e43
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:34:28 +0000

    Pp serious firm sore by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_itemsize(array):
+        return array.itemsize
+import math
+def calculate_floor(x):
+        return math.floor(x)
