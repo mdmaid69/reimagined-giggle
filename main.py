d241919ce05d1ee94ab79143b7b42743425cad4e
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:53:20 +0000

    doctor upwards snow maybe claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
