bccff63e17a725ec73da1f69436fb192ea09b506
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:27:03 +0000

    down blue nervous benefit dare

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
