7019b27dfc0b6ad23574ded08cceb14b841a1375
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:17:36 +0000

    fortunately mood absolutely downstairs song

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
