f2cd9685c5094b771c8c4f24d011fd6afd07b113
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:13:52 +0000

    legal mess peace sting high

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
