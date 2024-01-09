e42ca2cb577af6f6baec1cc61063693d50b76d46
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:30:54 +0000

    raise holdon open enthusiastic left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
