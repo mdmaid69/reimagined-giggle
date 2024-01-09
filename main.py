d49861ac073f68caf2284f298364856285ea3e55
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:12:15 +0000

    battery absolutely advice noone dinner

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-def calculate_power(work, time):
-        return work / time
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+print(sum(range(10)))
+def calculate_work(force, distance):
+        return force * distance
