96176e66cd4aa14eae1fe034bdf7fe2c6ca9077d
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:57:30 +0000

    used mystery best complaint pump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
