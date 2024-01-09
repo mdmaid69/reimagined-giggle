d215e83752ff3bd1381ba8cec82558efb388ee83
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:27:15 +0000

    one mystery ready handle permission

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
