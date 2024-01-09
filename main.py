db9a5323dcba367ef2a12483a94a95449ee6e166
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:34:46 +0000

    tap share crash genuine grateful

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_pressure(force, area):
-        return force / area
-import numpy as np
-print(np.array([1, 2, 3]))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
