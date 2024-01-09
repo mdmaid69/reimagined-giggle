fc1a7227338e41147c7dd797033d008f9d67f4b7
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:45:56 +0000

    exist scene relative schedule however

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
