0e4f6a1a3817fe2da3c402ee67244a66350cd7f6
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:08:04 +0000

    see used sorry pride lean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_work(force, distance):
+        return force * distance
