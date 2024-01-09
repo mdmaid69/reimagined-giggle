5ced4c038108d581a97f549f1abc22b3171da291
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:40:34 +0000

    case target bright situation wear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
