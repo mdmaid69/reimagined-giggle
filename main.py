0b586d7897aafb6207e89e8d2f5edac5944aed7b
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:32:31 +0000

    brush nervous beginning afraid report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
