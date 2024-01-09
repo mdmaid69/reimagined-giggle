d2bcedceb265641116dd92790d8ce5077bc5d861
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:31:57 +0000

    question doubt tip giveback quickly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
