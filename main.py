919d821087890c5849b2524390616db8bd83ff20
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:48:02 +0000

    getup interfere broken travel easy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def subtract_numbers(x, y):
+        return x - y
