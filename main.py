b7bfd0eab7bf523d290085a6fd06e3e441b0d46c
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:03:25 +0000

    powerful after any means tired

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_string_from_array(array):
+        return array.tobytes()
