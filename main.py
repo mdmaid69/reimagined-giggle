79be7f89b1ba3508996189220af28c7a0663d8aa
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:40:02 +0000

    shake noone bury obtain getout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def divide_numbers(x, y):
-        return x / y
+  def delete_file(file_name):
+        os.remove(file_name)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
