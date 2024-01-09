c7bdd3fc65632d48e95a79a2fbb526cc4a7df45d
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:18:13 +0000

    attempt pleased street callup rich

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
