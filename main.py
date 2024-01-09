f79fd03e8ad3370dab4f14fccb03edec19370963
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:38:32 +0000

    thankyou issue arrival fill wide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
