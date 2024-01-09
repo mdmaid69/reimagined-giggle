a1f515676e08d5fdb591546fbd7383f369e74515
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:25:04 +0000

    this nervous perform limit story

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def is_odd(n):
-        return n % 2 != 0
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
