6910f03dfee7f650fc36cb43b6c073d45b32f3ff
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:18:41 +0000

    on tour uponon win someone

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
-def subtract_numbers(x, y):
-        return x - y
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
