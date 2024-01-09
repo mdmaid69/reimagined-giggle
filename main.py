18b55260ebe2ab2b77c5454ce5ba3a3ecb7f6bb1
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:49:29 +0000

    notice section activity rank sound

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
