50eaad6dc30fa1ebae8a8b7b42bb253d249f2e9f
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:14:22 +0000

    exaggerate plenty safety behind light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
