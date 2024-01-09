4de09ef7e3d63ad6b0a9b71be79d604145411025
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:34:59 +0000

    situation more concentrate mail apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def convert_array_to_string(array):
+        return array.tostring()
+  def cube_number(x):
+        return x**3
