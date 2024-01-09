f9c6c348ad75a2a1aa5e28245294fae7934de8b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:17:16 +0000

    cheap bright rain circle ill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_item(array, i):
+        return array[i]
