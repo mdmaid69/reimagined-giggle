8fed227ba75e6544f884f7a346d9495d4ed7be6b
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:36:01 +0000

    hurry over beginning tip century

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
