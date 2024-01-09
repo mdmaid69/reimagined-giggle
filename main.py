35d4ec95c8379d42e046ab1377fc6daefad28315
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:48:25 +0000

    meal smell hurry balance band

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
