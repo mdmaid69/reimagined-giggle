b9cf568ee2d449c9dfcda12821c91dfae226a010
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:31:09 +0000

    meeting tour regularly ability hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def convert_to_hex(n):
+        return hex(n)
