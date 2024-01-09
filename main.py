1e9da5f2f350e8fc729230fbbc8d6c76d56c583c
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:29:04 +0000

    yes instant watch clear right

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_min(numbers):
-        return min(numbers)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
