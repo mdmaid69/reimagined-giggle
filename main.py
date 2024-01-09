192128ee39be46841e1fa994a4bbab8289b6fef2
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:11:41 +0000

    book regional soil grand commit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def is_palindrome(s):
-        return s == s[::-1]
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
