4c98e75467f746dbdaa7e9ab64a8904322aabd4e
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:42:15 +0000

    you actually welcome properly equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
