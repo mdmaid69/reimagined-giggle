811bae8c7b27e97e406d4ed119e2b46b36800194
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:06:28 +0000

    trick rely fill tour task

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
