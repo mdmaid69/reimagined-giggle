4f9004fd427b8f8de1933da4d7749bd54a2915b1
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:16:16 +0000

    test section then far gas

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
