7c6629b5f86ab8ae5f5b1b86b4e4ade25b8923ca
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:28:37 +0000

    club worse discovery welcome talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-print(math.pi)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
