0c79f3d1fbab5db1682e3f796a12ddf1761e3cf4
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:38:42 +0000

    mind clothes and small basic

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def find_max(lst):
+        return max(lst)
