6c0e914e27eba0e858ddb4aaa75f118d2a4d75a5
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:41:05 +0000

    word stage temper holdon pause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def convert_to_octal(n):
+        return oct(n)
