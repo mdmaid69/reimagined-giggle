fa46ff0cf75d454bbb4a04d073e2e972fc3fd36b
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:58:27 +0000

    opponent drop pretty right strength

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+for i in range(10): print(i)
+  import sys
+  def get_python_version():
+        return sys.version
