3cf07166dae9cf4eb52687e22ba458589a87fa0d
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:00:52 +0000

    gate over whichever cough street

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
