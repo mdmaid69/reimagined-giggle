4db41efe05f69408afc91c0c5632f364f886a69b
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:12:32 +0000

    protect tape sharp lost description

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
