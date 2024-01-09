32935406f01d861042c55ae871fb3fe87ef2c675
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:21:54 +0000

    cook wonder closed simple judge

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
