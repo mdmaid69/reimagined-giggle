feb40fa8b9cc27e22782f5efcb53e8b7f63e04b7
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:47:19 +0000

    turn broad thick soak language

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import random
+def roll_die():
+        return random.randint(1, 6)
+import sys
+def exit_program():
+        sys.exit()
