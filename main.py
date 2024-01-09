feea19cb7f698f300c419c78511772bc0f2a4984
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:41:00 +0000

    total past office value glass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  def convert_to_octal(n):
+        return oct(n)
