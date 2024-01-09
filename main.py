1579abe3bf27666693ea71ef02dd109ccc239f4d
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:51:59 +0000

    clever case pronounce manage freedom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  def cube_number(x):
-        return x**3
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
