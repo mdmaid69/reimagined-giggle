9c1f76978397e5dca954d0c4bebbaf9c684d7e42
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:19:17 +0000

    violence destroy radio fold fasten

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-name = "Python"
-print("Hello,", name)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import random
+print(random.randint(0, 100))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
