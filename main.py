251fcd9d9484f2140a0bb86bd84ea7cf1cb14a29
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:27:33 +0000

    imagine Pp close cry trousers

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-for i in range(5):
-        print(i)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
