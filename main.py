9291a7dff23dba7e876cf526e9bf0931c69f4109
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:15:04 +0000

    ticket float keepup Ff blue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  def remove_duplicates(lst):
+        return list(set(lst))
