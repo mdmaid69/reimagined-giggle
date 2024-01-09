5492c71601207adc54ab9a6eb2fe545e346b1723
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:31:43 +0000

    account noise shout benefit rest

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def multiply_numbers(x, y):
+        return x * y
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
