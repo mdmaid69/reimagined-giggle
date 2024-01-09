e621fcc65efd3563d6950f23be84825faa3e328c
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:09:58 +0000

    ours moral successful nonsense keepaway

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_work(force, distance):
-        return force * distance
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
