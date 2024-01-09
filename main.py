bbeb5eacff980db402c31ba5553103158bec42f8
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:20:42 +0000

    natural loud wall responsibility success

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
