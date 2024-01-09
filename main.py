09dfa39b76ff0552c8f271c76de357fff3168e52
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:13:42 +0000

    neck pass transparent surface break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def convert_array_to_list(array):
+        return array.tolist()
