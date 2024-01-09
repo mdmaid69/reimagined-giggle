5d4f5b5e1a1eef18aa6a597fc80826bbdf092407
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:21:45 +0000

    although tell become basic down

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
