49a89353c4d8c0dd443e85e83fee7cb543735747
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:10:59 +0000

    tear the short matter year

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def divide_numbers(x, y):
+        return x / y
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
