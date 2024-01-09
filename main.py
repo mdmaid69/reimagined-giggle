2b21908a82f1c720afe689a420e1be4fbae9ffa5
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:17:06 +0000

    translate correct level correct occur

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def find_min(numbers):
+        return min(numbers)
