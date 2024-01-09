c9c25a6dde0f0d6ba5bc08486599cc06a31dbae6
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:44:13 +0000

    thumb meeting out shall central

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
