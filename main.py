959404bf87ed5d5b664a3f381fccf37e2bc4ece8
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:12:23 +0000

    simple mass work he tool

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
