289e89dc89d7b3d294afd67acc0f0d140422267c
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:13:41 +0000

    return regularly minority ever mistake

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def calculate_perpetuity(payment, rate):
         return payment / rate
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
