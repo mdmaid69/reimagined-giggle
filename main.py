7ad02ca7a23d144b725ab24fbc2373fd8d57449b
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:37:37 +0000

    up broad ride please hold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
