c16e932d6efb4fc10b64da49e325bdbe23ef4035
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:15:49 +0000

    bill because far emergency round

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
