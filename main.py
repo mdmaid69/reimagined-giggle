f8506332f80fcbea7e83578f39bc97c4c75042cc
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:09:17 +0000

    drink acceptable year workout smile

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+i = 0
+while i < 5:
+        print(i)
+        i += 1
