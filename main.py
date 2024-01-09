a9f7da627f96ba354327af8e93c811b71989e451
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:26:34 +0000

    crash often cheque order deliberately

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
