372e54aacfc200d3d560b3e07300595c348653a1
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:00:51 +0000

    let prize black meal software

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_gamma_function(x):
+        return math.gamma(x)
