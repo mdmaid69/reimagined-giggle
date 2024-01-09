ce2d19a428798bfc3d226f9eeb35c9e25f63c1ce
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:05:24 +0000

    conduct second refer shock whole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import sys
-def exit_program():
-        sys.exit()
+def get_array_as_frozenset(array):
+        return frozenset(array)
