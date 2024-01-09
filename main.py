8485272f0198722660cddbc5d3c2e9589c3b36fe
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:14:05 +0000

    level wheel raise voice otherwise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
