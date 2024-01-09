35124b9c9dd9dd27f0120a36a94e94f40f6e67d9
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:27:08 +0000

    drop lower knowledge 566end rain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_odd(n):
+        return n % 2 != 0
