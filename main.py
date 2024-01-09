a97e599817df4fe278482c8a60d638a5390df2ce
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:01:39 +0000

    apologizeapologise few underneath myself invitation

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def remove_duplicates(lst):
+        return list(set(lst))
+for i in range(10): print(i)
