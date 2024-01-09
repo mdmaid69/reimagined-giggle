e3ebba40a5275a105ee75d2da36561c94e0573b8
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:41:39 +0000

    soft turnoff squash guide show

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
