89e8141a8c9b2379b89d503cd458c5f850a49d5d
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:10:34 +0000

    greeting crash paper workupto inside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+print(math.pi)
