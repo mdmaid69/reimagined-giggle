c855876096bc4673a95e6ce3a1c6e12a59de7e39
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:03:05 +0000

    range gift check my decision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def multiply_numbers(x, y):
+        return x * y
