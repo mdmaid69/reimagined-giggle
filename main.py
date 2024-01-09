0491d34e62b016afe8c7ce3cf84a3c6826722ec4
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:54:31 +0000

    mail shallow pass pipe upstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
