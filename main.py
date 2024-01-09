d2c03edfa88be203ef4fd466961406eb98bc9f75
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:21:29 +0000

    juice scheme moon permission post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_as_repr(array):
+        return repr(array)
