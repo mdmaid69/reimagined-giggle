cf2b9e620e0791799ce9f250088205c0be1ceb4e
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:07:23 +0000

    wide without front climb stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
