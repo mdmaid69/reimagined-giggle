5005803f4da9b7db691f1a149a3dacea4787f15b
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:42:07 +0000

    down Gg person scheme cough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def exit_program():
+        sys.exit()
