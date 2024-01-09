96464abd8b87396666aea9d0434804b39c1fd067
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:33:28 +0000

    fast search arm 571enough like

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
