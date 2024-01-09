b9d4e5cb82b64ad4ddc9980d49278be5844fd723
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:30:17 +0000

    true of spring important unable

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
