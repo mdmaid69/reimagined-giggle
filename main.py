54fe0a25c701a1d9c18e0e851aa78159ffcf04b6
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:48:29 +0000

    contents sudden show choose fill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_floor(x):
+        return math.floor(x)
