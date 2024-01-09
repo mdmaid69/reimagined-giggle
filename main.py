70b09ef79d3550a3a0a5b9142a2d89f170f229ed
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:01:13 +0000

    factory imagine least prepare rank

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
