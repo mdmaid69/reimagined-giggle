5574e8b251d0351d86e46203efc2501369abcf0f
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:10:57 +0000

    measure group become since expect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-print(os.getcwd())
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
