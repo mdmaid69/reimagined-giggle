757c2b7af2fba4b589de3867f67c4a9bdab46ff4
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:34:30 +0000

    any cut to bin serious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def count_elements(lst):
+        return len(lst)
