3f9d6df30b21ece7bc156c6b0166b04cddb7613c
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:02:32 +0000

    degree takeover regional land live

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import getpass
+def get_username():
+        return getpass.getuser()
