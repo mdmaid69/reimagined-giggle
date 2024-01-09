d730fb3d7325245686eea776fd984b50d013b7f2
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:17:32 +0000

    name onto accidental air reject

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def split_path(path):
+        return os.path.split(path)
