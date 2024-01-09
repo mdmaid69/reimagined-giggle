2d753b15d41c31b83ee207e3c7043eca93f443f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:43:26 +0000

    transparent clear concentration comfort past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-print(math.pi)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
