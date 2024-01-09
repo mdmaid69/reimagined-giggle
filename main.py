e6b1a680abe7a3f0dd7f51957bf9b33c179618c1
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:41:29 +0000

    unlikely look simple start joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
