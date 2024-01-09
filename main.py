891091dd75f2259e3b434316740c82cb5b09690e
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:32:56 +0000

    centimetre brief sight life break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
