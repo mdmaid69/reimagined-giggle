884b95006d3cf98d83198588fff9617154b8d05c
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:39:38 +0000

    human paper mouth possibility book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def count_elements(lst):
+        return len(lst)
