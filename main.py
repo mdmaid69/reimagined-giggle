20a00fd315a30b9efc08ec38b5701780671c5285
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:23:39 +0000

    separate stare picture mistake I

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
