e197ab092ad75db03e46f9ca06b6e1c4e251d1ed
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:26:17 +0000

    wake go start notice remove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def cube_number(x):
-        return x**3
+import array
+def get_array_as_float(array):
+        return float(array[0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
