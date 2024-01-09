a564cb81b0aa92f4fb3967d004404ae08bcd4358
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:54:11 +0000

    impossible exchange Kk aim clothes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_power(work, time):
-        return work / time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
