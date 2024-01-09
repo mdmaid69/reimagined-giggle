dadc5f785cbdcc47605aae84c4ac9bf2a1928b45
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:31:33 +0000

    contact head influence mother sting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
