1c3843f72e0921fa2ac45e5eba3717bcc9de92ee
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:12:47 +0000

    serve lose correct notice top

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
