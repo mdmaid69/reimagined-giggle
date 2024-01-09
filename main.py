cc01ac02669f07053a26935765736968a0cec03d
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:40:59 +0000

    female Qq gooff bury which

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
