ced549431ca17ab9ca7f41bf9e30e1b3a5b8e682
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:48:43 +0000

    straight recently cross rob needle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
