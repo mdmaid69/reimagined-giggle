731af014a543a3e6b2bc1b03ff8b55cad8c772d8
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:17:25 +0000

    tired check family charge thumb

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def is_even(n):
+        return n % 2 == 0
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
