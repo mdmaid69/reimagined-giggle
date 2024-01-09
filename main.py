8bcbb97c00806f85e0207c2acc78014e4b889bf9
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:15:32 +0000

    rare this display related walk

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def is_odd(n):
+        return n % 2 != 0
+  def count_elements(lst):
+        return len(lst)
