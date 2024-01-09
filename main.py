a4f872457813d453b329b22fe316a7916fde50b7
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:23:11 +0000

    dress say turnback unit theirs

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
