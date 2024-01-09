2a4fc43687da3415e21d5c37d8afab1b456f076e
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:15:04 +0000

    also term goon culture loud

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def find_union(list1, list2):
+        return set(list1) | set(list2)
