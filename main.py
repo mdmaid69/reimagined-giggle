ada7348aa9038eceda62e4d1238d8ca2b10a7103
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:33:43 +0000

    prize insist bottom straight lower

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
