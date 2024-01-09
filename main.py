1ed5713ef7564c99b8db500ad251efc4aa0e17ce
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:41:07 +0000

    keepoff fix cause perfect copy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_cosine(x):
+        return math.cos(x)
