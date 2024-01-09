a5b25f434743e7f6fd5f5d570ea6e814cdf6a059
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:45:43 +0000

    lookup bunch case apart factory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def set_array_item(array, i, item):
+        array[i] = item
