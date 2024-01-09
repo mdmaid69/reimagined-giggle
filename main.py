bf0302a92e97c5dad44d5ec7fa841325f7482666
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:42:15 +0000

    each nothing earn quit division

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def is_odd(n):
-        return n % 2 != 0
+def calculate_pressure(force, area):
+        return force / area
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
