8bac5495520e05f53c3e539bf6b1fdbfec7e3d9e
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:22:05 +0000

    interview talkinto maximum sign theirs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
