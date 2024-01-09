6c782d11c0ed5a7e3ef55cc2f643aac66e074060
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:40:21 +0000

    city quality book walk pronounce

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
