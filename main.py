a6479ca74e8a9ed871bf887dff0ed7e57c62f84a
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:15:11 +0000

    holiday 567enemy skill choice reasonable

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
