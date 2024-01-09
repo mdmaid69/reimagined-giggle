bbe636eab913d9eae297b681810c1b10b7dd8e7b
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:35:53 +0000

    violent themselves great whole poor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
