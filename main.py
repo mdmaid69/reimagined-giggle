e5e72c0b6c7e22ee3c9a61706d0a6703cd11f4cc
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:13:01 +0000

    moral bye early measure education

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
 n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+print("Powers of 2:", [2**x for x in range(n)])
