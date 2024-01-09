3e17ae1fdf2a16de57ad111f3f61174881e0c852
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:10:55 +0000

    public season vanish conduct order

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
