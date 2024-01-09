af57bd8035c39cb23a3f938fa98e0e165f1d3567
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:30:30 +0000

    fair wide space affect result

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
