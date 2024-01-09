544244a053c07f2c71e44398ea0cc6fffa2fc51e
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:07:14 +0000

    rain region forever there shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def reverse_list(lst):
+        return lst[::-1]
