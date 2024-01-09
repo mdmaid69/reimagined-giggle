e701d350ddd40d7c52d492a934586f1a4fa6e7a2
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:42:20 +0000

    when press legal underneath bill

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
