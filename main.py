e320b18d8d15a5dab9edc91deec646a40396aa39
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:19:19 +0000

    trust experience Ii public limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
