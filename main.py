db31bcdf1664384ae6d2938795b92b35807f08d3
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:41:07 +0000

    sing spill system sink most

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
