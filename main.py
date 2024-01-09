7ebfd786f0c8d64bf8e210550f858dc8c7a01a20
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:35:57 +0000

    will bringup right remove 572enter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_force(mass, acceleration):
+        return mass * acceleration
