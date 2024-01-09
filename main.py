0295580beed145cbeb2d3e5c4cfa7523775068d1
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:32:58 +0000

    yet decision unfortunate callat study

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
