0375765f439ea1d8d099b10e5ae358a0f6ac3af4
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:22:50 +0000

    level back straight organization patience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
