4dbcb398a67ed37218189cfc77fea78d5f836d91
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:29:23 +0000

    flood accidental mind contrast below

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
