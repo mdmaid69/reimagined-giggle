17f52a5ec46649413ccb7de95255f3ec0a067af5
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:06:29 +0000

    mean see house tidy collection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
