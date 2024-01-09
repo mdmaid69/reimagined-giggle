beb2271d78646220233bac9d2e436672c9a006e8
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:39:50 +0000

    rather stiff comeup criticize closely

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+print("Hello, world!")
