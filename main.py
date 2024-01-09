75324a6d3cfda3e5b0bb49c4c1fd7b7afb5b47ea
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:23:44 +0000

    join play turnup prevent schedule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import sys
+print(sys.version)
+import datetime
+def get_today_date():
+        return datetime.date.today()
