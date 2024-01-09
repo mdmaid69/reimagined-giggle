bf57c59122c368ab0e6e16e86360296b81d0c400
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:02:25 +0000

    makeinto object second book blow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_stack():
+        return collections.deque()
