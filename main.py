f603becc474bc873df874d6effc963345fe992f7
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:20:57 +0000

    more switch objection when like

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
