32e6c5058aee9e8f3df942871afdc8c69c802323
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:04:08 +0000

    very protection error herself trap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_max(lst):
-        return max(lst)
+  def delete_file(file_name):
+        os.remove(file_name)
+import collections
+def create_priority_queue():
+        return collections.deque()
