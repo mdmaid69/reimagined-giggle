984ebd3201714b6bc1c898e576133f9db2c45d81
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:55:36 +0000

    improve money wash hide Mm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def get_array_length(array):
+        return len(array)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
