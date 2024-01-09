e1184d334887ea523f32a8902c8c96c2aa0c47cf
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:44:30 +0000

    clearly voice skill fire steep

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def is_palindrome(s):
+        return s == s[::-1]
