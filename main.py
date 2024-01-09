62bd853a9e40d0584090e0a3ff7c829781d3a313
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:46:44 +0000

    important range like someone challenge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
