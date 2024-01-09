b7b27ecab8a2257759b698009eb75df62aba9647
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:39:19 +0000

    everything noone fail invite bye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
