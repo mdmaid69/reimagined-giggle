ce4bdfd86e036359aa7f0abb38888538f928c028
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:50:17 +0000

    little minute dance indeed national

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_file(file_name):
+        os.remove(file_name)
