99bf56b1ac26dbc4a63c568a56a4b0a6d02a26b1
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:46:12 +0000

    acceptable under fire follow realize

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+for i in range(10): print(i)
