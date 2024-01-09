891ad045f7ba805b726b937e2b35b5a6df4f424f
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:18:25 +0000

    unfortunate matter squash him kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
