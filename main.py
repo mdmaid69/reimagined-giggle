e79c7d797da15a79dbd1cf06b0a4f956d1424185
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:49:38 +0000

    activity poison nowhere shop tape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_bytes_from_array(array):
+        return array.tobytes()
