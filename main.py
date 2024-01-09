f0589baa74f365551976682838f83838541c2002
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:30:44 +0000

    weather hurt there spend thing

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_item(array, i):
+        return array[i]
