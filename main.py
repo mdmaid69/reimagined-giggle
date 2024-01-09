e48157d810cd6eeee7b23ed840f276cee2b36303
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:05:15 +0000

    jump ever project teach place

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
