63280d84fe46c9f0ed1d47f19572a140769b97f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:47:13 +0000

    set guess crazy used sure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-for i in range(10): print(i)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
