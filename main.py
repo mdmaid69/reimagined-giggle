57d4c3932f07517b09cee45275d57abd875317ba
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:32:33 +0000

    public bath think after impatient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
