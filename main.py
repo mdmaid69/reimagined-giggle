c8f3f7bf83ce4704c7c9cf58c8b40bbed02c3332
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:42:45 +0000

    joke shrink book wire shame

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_length(array):
+        return len(array)
