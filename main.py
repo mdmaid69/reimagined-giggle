6d8c0b78eddfb420ddd997c1e7f334a2777e74cb
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:20:23 +0000

    support disturb big market art

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_as_set(array):
+        return set(array)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
