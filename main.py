5b33595357b5271b96fffb222ef31fe6efa7c996
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:06:44 +0000

    improve still welcome blue material

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import math
+print(math.pi)
