1b3936e161cc9cce66a3bc75117be00b914294f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:29:07 +0000

    pity once no behave refuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
