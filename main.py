a5d37b4a94f98f064209ab1e5e347a77e5a3677c
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:07:11 +0000

    tear trousers worried last ability

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
