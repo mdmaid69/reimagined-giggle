118177e707ef2586964fe19c1c12cb333dc26ce4
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:59:48 +0000

    hollow open vote major connect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
