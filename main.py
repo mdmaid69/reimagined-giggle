d2526180ceae58258ce3cc46f8b3e58bb8bd7ec8
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:15:31 +0000

    project Ee activity fast supply

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
