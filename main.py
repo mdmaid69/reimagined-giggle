e39d8f82f7e5402ddb7ae720dcc9cc0f68d477ec
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:31:53 +0000

    statement custom culture practice show

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
