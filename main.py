f20525b1e400bb5454bf543d0fbe7cbb9907ad45
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:48:56 +0000

    undo once tread finger fresh

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def convert_to_hex(n):
+        return hex(n)
+def add_numbers(x, y):
+        return x + y
