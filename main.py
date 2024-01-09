742f4aef487171cb26d074cef6b44a3e9f1dafe9
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:20:09 +0000

    takeoff clever thought word callat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
