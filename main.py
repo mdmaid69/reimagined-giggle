8653c555a3cd6efbb161c8bfb4308d0ef9d71005
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:45:37 +0000

    list welcome promise path community

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_user_string():
-        return collections.UserString()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_area_rectangle(l, w):
+        return l * w
