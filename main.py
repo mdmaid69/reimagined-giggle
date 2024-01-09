78f179e58f0cd80787733fbc49d6e63072b07dc2
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:29:03 +0000

    reject full slide nigga big

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
