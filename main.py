a8ad7b03f0742ba532f22249694c92fd0e4bff00
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:06:19 +0000

    steady smart tap smart suck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
