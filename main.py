0da63d1ce82b847286ce4a11025956fa13543a7e
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:56:16 +0000

    swim scared because tax file

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
