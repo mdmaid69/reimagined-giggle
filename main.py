6796035aac9129b6902e3b2cdf9324782cc3fbc6
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:48:21 +0000

    direction hope rain excited fetch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
