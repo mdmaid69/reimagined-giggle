01a9e8632a5b32c5cfe71bf1705132f13169c249
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:33:02 +0000

    float since bend acceptable contain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import platform
+def get_os_info():
+        return platform.uname()
