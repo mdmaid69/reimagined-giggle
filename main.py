e73f9fd20a7bdcef8bfeca9b9703dbd2668b6c3c
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:39:56 +0000

    clean progress heart accept accommodation

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def split_path(path):
+        return os.path.split(path)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
