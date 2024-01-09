9574fb1261d8fe9cc77b403b0c502f521d8f6b46
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:45:25 +0000

    when workon stone godown in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
