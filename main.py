f5b4681a24677e0f02d0d3c2fdccfe3683894ebb
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:20:31 +0000

    furniture ought reason plastic well

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
