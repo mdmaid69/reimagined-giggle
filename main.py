6d1146be764e45241c030989c66bc7df82065086
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:11:04 +0000

    violent suppose jump embarrassed gap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
