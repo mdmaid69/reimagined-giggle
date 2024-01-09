d0ed14ca0d2e7db6fa09b64d26a87fee16b009ec
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:19:10 +0000

    relative distance branch anyone plenty

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
