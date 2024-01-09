0ea8f2d70d09bcba0e508f1f267638c6b868ec44
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:42:18 +0000

    claim direction think service Itself

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
