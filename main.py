9ca399971babb30b339434e6cfe30652a7d198c2
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:53:45 +0000

    dull education and can minor

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
