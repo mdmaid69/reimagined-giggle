7db2625a0348938f6d3231d9d4d8fa3b72c60212
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:39:56 +0000

    organization runinto throughout cope last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
