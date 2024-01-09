144dac797677f10b028b3825035b443d6fbdd2d6
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:31:27 +0000

    little fat past sink emergency

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
