cda7540c48c14257655069192056a307ec61c33b
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:02:57 +0000

    slip weak obvious actual exploration

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
