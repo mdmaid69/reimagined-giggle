0549cbc327058dca2dc7d46dd1ef78a74107ab0b
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:56:07 +0000

    judge invitation burst but start

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def is_even(n):
+        return n % 2 == 0
