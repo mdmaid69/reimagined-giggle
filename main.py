85e97e197070d9662f1c1a06786fd83749697e52
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:33:04 +0000

    fashion OKorokay unlucky fact trade

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-n = 10
-a, b = 0, 1
-while a < n:
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
         print(a, end=" ")
         a, b = b, a+b
-def find_min(numbers):
-        return min(numbers)
