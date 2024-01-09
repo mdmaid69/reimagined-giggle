91dc748dd0bb651dd7a4ad7325267f6040b9c51a
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:14:15 +0000

    please safety frighten steady edge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
