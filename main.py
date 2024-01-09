574d69b08af2c30f15afadac34b351783f78bdc8
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:44:50 +0000

    her stage makeinto pay proof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def cube_number(x):
+        return x**3
