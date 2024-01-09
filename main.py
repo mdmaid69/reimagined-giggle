0a669ff34735a720cd9e28bd1a9a5457004e5447
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:35:07 +0000

    drive start turnaway rent die

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
