3af95e6be72ed3f20f945965ba0847a04c4e3b05
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:56:05 +0000

    normal drive hear with order

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def convert_array_to_list(array):
+        return array.tolist()
