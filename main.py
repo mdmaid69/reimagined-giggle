a219b2a730007e4368795eb7831d543b6db2c5ab
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:41:14 +0000

    into rough either disagree night

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
