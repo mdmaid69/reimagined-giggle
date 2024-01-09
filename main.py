435a7c942060315e13803f3880d13cc2ae9bfd29
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:57:14 +0000

    defeat valuable cross brush callat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
 import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def exit_program():
+        sys.exit()
