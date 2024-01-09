4db704242da02292c7f8a2c72c419160093c0424
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:14:49 +0000

    nonsense get dish production Tt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
