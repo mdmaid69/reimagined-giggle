f6ad2abf13f34dcd242848b2f54be61a057d3835
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:01:58 +0000

    ring stop band street arrangement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_current_directory():
+        return os.getcwd()
