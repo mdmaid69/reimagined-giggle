54b64fdb873db4afbb4c8d01f730fd90b3fa2750
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:50:38 +0000

    smile right deep proper land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import sys
+  def get_python_version():
+        return sys.version
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_as_str(array):
+        return str(array)
