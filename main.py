b64f4ce5a9807668c16896cad3b0f42530f51034
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:21:40 +0000

    guide pupil telephone wander interest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
