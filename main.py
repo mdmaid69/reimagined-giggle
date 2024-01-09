ac86da2dd189c59f7d396ea5ee31944aeea8184a
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:44:08 +0000

    always complain through should clever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def count_characters(sentence):
+        return len(sentence)
