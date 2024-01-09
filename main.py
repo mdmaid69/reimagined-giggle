a27e5b22c2412e73019eae7571f8043efd502b18
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:56:12 +0000

    error Nn polite ago drawing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
