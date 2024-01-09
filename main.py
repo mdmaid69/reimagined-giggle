b48ed32fdf4aa285f741071ed8e65a51dd003d3d
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:56:32 +0000

    previous spin flow arrange right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_list_from_array(array):
+        return array.tolist()
