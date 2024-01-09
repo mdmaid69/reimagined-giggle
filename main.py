ffb4f405c4b3e2bc3332487cc925214138098fe2
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:47:27 +0000

    often to trap middle interruption

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def add_numbers(x, y):
+        return x + y
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
