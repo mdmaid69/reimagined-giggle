32e1d385512157d6a58f8aa64a1cfb0eca793cff
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:23:56 +0000

    light open impossible Dd plan

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
