f906157789f34de0454effc35a1ae631b786be02
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:55:53 +0000

    intelligent foreign swimming solve compete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
