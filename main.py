cf065528049aa530c52b69470425fa6fb93d36b0
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:53:51 +0000

    reserve fortunate gentle bad perform

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+def calculate_floor(x):
+        return math.floor(x)
