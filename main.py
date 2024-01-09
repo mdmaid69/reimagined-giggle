e1e5aa87e80dd2ab0296ededf8d59330282d75c8
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:20:13 +0000

    used repair picture my able

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import time
-def get_current_time():
-        return time.time()
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
