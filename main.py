ca444ea5ebc74c62ec4791337023cae205315fd7
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:22:43 +0000

    so fierce stage plan release

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
