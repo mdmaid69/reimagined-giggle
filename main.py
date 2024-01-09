d5ca1d6b4de1bf752eb8b18b19cd22cd1121a53e
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:16:00 +0000

    occasional wet attempt impossible thumb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
