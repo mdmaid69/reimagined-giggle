9cbee0cfb14e63cd9bc10bd965dedeb49a071d78
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:26:20 +0000

    night roof share sorry juice

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
