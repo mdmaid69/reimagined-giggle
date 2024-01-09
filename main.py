63e72bfc7b23636ad7fa5ff38c781dc7546ddc0d
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:16:22 +0000

    talk degree plain needle quickly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
