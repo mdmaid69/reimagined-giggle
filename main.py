b56def4c5fb5b31a93da364190ac339f14ab47be
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:51:41 +0000

    hole rule runinto cross Ss

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
