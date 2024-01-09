cc63f926c069e0188c49e2de3accd8befa2e7a2c
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:40:44 +0000

    then work hurry spare breakup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import tensorflow as tf
+print(tf.__version__)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
