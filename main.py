e760caba32f96220425828c2c2007767e48f28b2
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:31:31 +0000

    happen deaf note force real

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
