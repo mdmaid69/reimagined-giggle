804e954d5a6bacc3ceaf89d5d132b7e238b7ca6f
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:21:46 +0000

    anybody leg might murder choose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_pressure(force, area):
-        return force / area
+def find_min(lst):
+        return min(lst)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
