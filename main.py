52c89f71d414971753792a0eef14615ccdf94665
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:02:19 +0000

    claim neither loan load opposite

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
