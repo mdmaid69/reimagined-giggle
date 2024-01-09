473c0a4fe6f2499a008da7bad242e9c923d64f2c
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:42:59 +0000

    damp calloff gather reason mistake

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
