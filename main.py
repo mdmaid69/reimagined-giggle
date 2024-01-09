6c40a68a341b9a471921759150731a5c6583f4dc
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:49:19 +0000

    uncomfortable correct climb broad force

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-print("Hello, world!")
+def get_array_typecode(array):
+        return array.typecode
+def count_words(sentence):
+        return len(sentence.split())
