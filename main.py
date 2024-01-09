71a2260b68befd43d7ddd10511fd616d9cc41d96
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:26:42 +0000

    history plan succeed comedown giveaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
