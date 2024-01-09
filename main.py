6dc97b8c281ffef6678e689edd820765c534ccdf
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:22:49 +0000

    creature deliver the age hers

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import sys
-print(sys.version)
+def get_array_as_bool(array):
+        return bool(array)
