90e19ca508debc555a34c46147b7048b85443b5e
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:22:22 +0000

    sentence concentration even round rely

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
-print(os.getcwd())
-  def is_even(n):
-        return n % 2 == 0
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
