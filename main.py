2d3d2626ffca41fa80271b03ebea81a4e3b4839f
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:44:11 +0000

    combine unlucky plate relationship very

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-print(sum(range(10)))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
