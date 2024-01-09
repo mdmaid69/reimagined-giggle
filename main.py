5e9617ebdd399613b50526611af23ee365340db2
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:56:49 +0000

    post still call callup similar

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_acceleration(speed, time):
+        return speed / time
