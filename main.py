ca5eab8e960c8685f56234525ed79bff1e61804c
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:22:34 +0000

    safe they comeover programme morning

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
