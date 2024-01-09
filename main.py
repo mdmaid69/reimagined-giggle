680a740043e1907f9b94684308ac88b00aed48a8
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:28:15 +0000

    point operate better comfortable spill

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import collections
+def create_priority_queue():
+        return collections.deque()
