97d6c05cd99201190e82b385a7cf05a186c8eceb
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:29:56 +0000

    Bb achievement exaggerate concentration ready

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def convert_to_binary(n):
+        return bin(n)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
