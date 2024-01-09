529d7c609e619d9e334ce0127c0c65c75e226efc
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:43:10 +0000

    string dear first stage arrange

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+print(math.pi)
