4fe8d69f02c880027a05c6baa5e7e407c284d494
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:31:28 +0000

    school poor habit say grow

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from collections import Counter
-print(Counter("hello world"))
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
