8fde82fc23bdbdb38a3fe02d03535a917a2a4e62
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:15:03 +0000

    study pupil mark scrap theirs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
