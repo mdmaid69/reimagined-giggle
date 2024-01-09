9ee882069d2520e94d0ff7f0c0bba853f2eafbf5
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:07:59 +0000

    charge work smile clue spare

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import time
+print(time.time())
