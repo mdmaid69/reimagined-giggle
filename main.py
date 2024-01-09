b7d2bf6e2a5be86d68b5d98d59897458d7b3b406
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:50:45 +0000

    ear less lookafter lucky ask

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
