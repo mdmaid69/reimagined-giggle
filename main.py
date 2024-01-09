b5a59ccdcc6b86a6a44efd6e6bdb6f3cb324f9b0
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:19:48 +0000

    well market occasional indeed pick

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def convert_to_binary(n):
-        return bin(n)
+def calculate_area_rectangle(l, w):
+        return l * w
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
