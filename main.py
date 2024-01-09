932c266b38d7c5b7c522c18af691d065c3042e64
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:14:33 +0000

    example bored lesson create arrangement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
