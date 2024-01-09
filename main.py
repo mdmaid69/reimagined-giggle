4fb981f908b977680b706343a566b36c38f911df
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:13:22 +0000

    zero she vanish comefrom petrol

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
