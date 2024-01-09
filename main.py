82de957c8626d5c633c979a56e55e714a746a70f
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:44:09 +0000

    embarrassed turnon backwards comeout keepoff

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def reverse_list(lst):
+        return lst[::-1]
