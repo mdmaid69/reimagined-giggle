e335794b593d313a8a6b3fbad3955a6c5d5062df
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:16:51 +0000

    week flight disappear find push

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
