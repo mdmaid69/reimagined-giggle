073a8faa195393cd12966aa05aafd946a9b68f11
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:05:32 +0000

    talkback depend tear Uu affect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
