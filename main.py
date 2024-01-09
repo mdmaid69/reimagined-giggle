32ef97346e6795fa372f36b703a51ec6d33286c6
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:51:21 +0000

    will Kk one another duty

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-name = "Python"
-print("Hello,", name)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_work(force, distance):
+        return force * distance
+import array
+def clear_array(array):
+        array *= 0
