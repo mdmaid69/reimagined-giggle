619e2b8279eff1655e0b6e73bca7c2adc4d55481
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:43:46 +0000

    consist object prize week rubbish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import re
+print(re.match("h.*o", "hello world"))
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
