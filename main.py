eaed9f6c2b228098e2447536c25da8de0b4186b9
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:28:45 +0000

    today mad direction sure rob

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
