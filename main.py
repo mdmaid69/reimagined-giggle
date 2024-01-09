d15946998b5850e8bd963dfa3673c9e31aaeacb2
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:19:35 +0000

    protect town since moment sport

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
