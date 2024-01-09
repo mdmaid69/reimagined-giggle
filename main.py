2a2655e51876f70dac9b0f6ee5d026475f70c482
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:01:26 +0000

    tip very progress wind improvement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
