b47dde3123973820e78361e8a0c4519919a3c97d
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:36:14 +0000

    satisfactory wall buy satisfactory as

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def get_array_as_frozenset(array):
+        return frozenset(array)
