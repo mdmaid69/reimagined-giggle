733eb1dcb12ad8c835904400a851aa1cfa483ef0
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:10:50 +0000

    nice feed yourself modern feature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
