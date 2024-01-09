c91acf0c14b460cbe050d579b99c7a52698deb75
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:18:21 +0000

    arrangement obvious address rest accidental

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_itemsize(array):
+        return array.itemsize
