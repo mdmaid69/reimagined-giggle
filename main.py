290fb76027d3beefd3719f602ca0373519ee311b
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:22:38 +0000

    shop appeal ago block combination

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+for i in range(5):
+        print(i)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
