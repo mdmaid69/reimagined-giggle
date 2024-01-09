1a7f171de625c3d4610c224ce9ca1da0bc439b23
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:01:15 +0000

    ever exactly measure bottom recover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
