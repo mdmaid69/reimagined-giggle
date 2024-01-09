eb74b3bc38a45c54bbf7b24303cb3324295069f1
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:59:25 +0000

    flavour warm workon satisfied leave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import platform
+def get_os_info():
+        return platform.uname()
