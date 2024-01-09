6c9b7431ba3b11734d7fdc48c31b501d82aa0e65
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:11:37 +0000

    crowded sport noisy meet here

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_as_tuple(array):
+        return tuple(array)
