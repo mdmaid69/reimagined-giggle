25b377689af34894c821d1ebd8129e1b019e316d
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:21:27 +0000

    attitude material hate battery comeout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def find_max(numbers):
+        return max(numbers)
