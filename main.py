bcbc9c25513f79669cf14cbd09d781fe5bb93333
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:04:52 +0000

    avoid fine storm increase hurt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def find_min(numbers):
+        return min(numbers)
