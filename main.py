a865ed8194746ffe74867a5b37362805decb2ae4
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:50:00 +0000

    bill cover future bag my

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
