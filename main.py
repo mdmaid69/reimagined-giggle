acc41959fad3bf1e8980cd1182e66bdf77a3c1a7
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:13:09 +0000

    boat rain demonstrate condition wait

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
