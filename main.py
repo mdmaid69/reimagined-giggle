295649577c8768feff868a263a11609d18dce488
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:32:43 +0000

    relationship against also fresh section

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
