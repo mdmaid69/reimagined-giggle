4df196928ddc7d42fa127e6f97f9563715de0c6f
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:52:47 +0000

    basis official dream supply address

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
