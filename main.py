e00ecb1cbd46a1a198ebd5a091266559bbdaf05b
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:13:08 +0000

    sign connect style disagree year

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
