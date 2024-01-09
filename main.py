ce863e1fba31e3c1861b2d2b11eb1f02b5022962
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:05:36 +0000

    rescue confident interview entry no

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
