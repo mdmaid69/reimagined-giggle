6bc3b127acc276e2596a38b4dd415232b9950d13
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:47:17 +0000

    bite poison meaning accidental shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import re
+print(re.match("h.*o", "hello world"))
