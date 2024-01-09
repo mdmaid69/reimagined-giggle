42a99dd09c4ed5e08ca6ea05e1b49b66d8071dbb
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:27:04 +0000

    opposite turnout progress material that

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_speed(distance, time):
-        return distance / time
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
