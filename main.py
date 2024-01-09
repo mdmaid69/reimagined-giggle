b91eecc50e51a26cbc6ec9096ea5dd11681ba6f8
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:56:29 +0000

    run way guide comment rely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
