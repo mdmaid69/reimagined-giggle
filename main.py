24404d6b89d95b220c1ad3cd48aa5766a542e337
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:36:29 +0000

    friendly part exact fortunate divide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import numpy as np
-print(np.array([1, 2, 3]))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
