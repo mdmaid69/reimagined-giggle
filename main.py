887eca67a677db1f369ede7cf5767435ea48ba59
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:46:21 +0000

    tap high mind wide hurry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-print("Hello, world!")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
