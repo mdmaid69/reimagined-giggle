595898179dedf123ae9b51c1c611901403e22244
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:19:47 +0000

    machine rest once brief emphasize

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-name = "Python"
-print("Hello,", name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def greet(name):
+        print(f"Hello, {name}!")
