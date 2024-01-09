f94e38b8609593d91aaf43438a472e0c0fbec6ab
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:43:08 +0000

    use cheap join ever smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
