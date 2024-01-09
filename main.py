341bbe8aa0513f4b1a9947bef0bbfec4f2e54526
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:38:33 +0000

    unpleasant impossible past then block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
