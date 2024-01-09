4e35527fca8153e5bcf5f58357a295ab89a9b82f
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:28:18 +0000

    shame shout freeze heart shelter

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
