4860e7954f6e7ca8464d9a481deda1f40784cbb5
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:29:14 +0000

    tradition turninto meaning giveback already

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_power(work, time):
-        return work / time
+  def count_elements(lst):
+        return len(lst)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
