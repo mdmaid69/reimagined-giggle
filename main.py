0b179e5aa9c8adeaf06c52cc2e2ed18a25a8a333
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:52:21 +0000

    circumstance suckme copy book battery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
