c8cf73ced36b3ecde15edf9fda2c5e219322c664
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:12:04 +0000

    professional fat miss aware sight

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def cube_number(x):
+        return x**3
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
