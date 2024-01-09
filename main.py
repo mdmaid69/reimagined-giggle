430e410476b11a8105ff033823fc2a9cfdbb8a9f
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:02:52 +0000

    window towards television war sweep

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def square_number(x):
+        return x**2
+def find_min(lst):
+        return min(lst)
