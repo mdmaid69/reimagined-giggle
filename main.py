f93213ff0e12d29bab49703db383e5636736c581
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:41:57 +0000

    considering practice scale cheap Dd

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
