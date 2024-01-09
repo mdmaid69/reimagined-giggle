1e2334e0793bba4fd3f052614737726c1e4a35ed
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:23:01 +0000

    above by tough even blind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
