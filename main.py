21cf9f6c3da5ee58560f00eb047511d15b79b2e5
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:33:25 +0000

    damp everything separate serve frame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
