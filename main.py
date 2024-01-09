7414c021f01e04d546acee9e65c6003af8030c70
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:57:36 +0000

    prize some ever wild declaration

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+print(sum(range(10)))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
