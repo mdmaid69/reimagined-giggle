21363f61665f10ffbde7163608ba170fd17ea637
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:42:08 +0000

    pump problem past be lonely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
