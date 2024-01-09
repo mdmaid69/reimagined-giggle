4910ae3fa35f769cd8706af8c15328c17e2d86b7
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:41:58 +0000

    quick criticize 564encourage accidentally nothing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def create_user_string():
+        return collections.UserString()
+import re
+print(re.match("h.*o", "hello world"))
