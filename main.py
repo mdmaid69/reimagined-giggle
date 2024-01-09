7fea05617fc29d46928d58fa0ab342e2039233e1
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:39:37 +0000

    568energy town control nothing you

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def convert_array_to_bytes(array):
+        return array.tobytes()
