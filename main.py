4634e51c8791feb297b901ba2eb482463bbb90f3
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:41:54 +0000

    over excellent instrument as contribute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
