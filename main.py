5c778c0949fb32a930584e932948227a2e2da593
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:19:17 +0000

    physical board already nonsense morning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_min(numbers):
+        return min(numbers)
