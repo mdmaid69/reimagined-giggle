aa4ba34c20558a549d35e9d31665790efab24db8
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:12:41 +0000

    fasten fond account accidentally appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
