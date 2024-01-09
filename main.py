3aa8c5c36551781e08503d43b4062bb1a6b1394c
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:02:43 +0000

    total careless several improve talkinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
