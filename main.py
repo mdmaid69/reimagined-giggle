516781a8af8ead71fedbebf879e12ecbf3036c35
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:34:39 +0000

    breakfast protection face instead remark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
