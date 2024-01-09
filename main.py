a989de28fcbf5c50a1fee487c7c1a040f062c1a6
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:02:28 +0000

    properly among hurry invitation write

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-for i in range(10): print(i)
 import array
-def get_array_as_set(array):
-        return set(array)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
