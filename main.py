166270b2a77dff56267e468d27b9717c9223b61d
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:55:17 +0000

    error tense about blind value

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
