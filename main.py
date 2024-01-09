62cd3e422708f45fcd66cff0538d0d80bd05f319
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:38:09 +0000

    film wherever mainly keepup smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  def cube_number(x):
-        return x**3
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
