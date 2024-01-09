f5afb4e5959774292a2d028bc963defe95d8503e
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:23:19 +0000

    wheel family wish quarter sell

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
