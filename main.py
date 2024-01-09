5ab013a2c26a496dccdd93bee4e0defc53728146
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:48:18 +0000

    pin space plenty goodbye drink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
