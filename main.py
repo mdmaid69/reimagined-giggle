8266270190f3bc4fff279829003b7bffddf72b83
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:23:18 +0000

    judge temperature calmly contrast criticism

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_typecode(array):
+        return array.typecode
