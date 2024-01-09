91863c81c6d9948ebd7d1e0d9da2534c2075705a
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:13:16 +0000

    grade recovery finger quit floor

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
