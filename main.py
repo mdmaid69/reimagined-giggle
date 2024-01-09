d7824c30e719fe139ef867c1d67c65943980d8d6
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:28:51 +0000

    hold poison accidental secret near

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def reverse_string(s):
-        return s[::-1]
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
