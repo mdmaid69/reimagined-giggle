c78fb20459bddda7188e939e3d47b33d02b024ac
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:25:42 +0000

    cheat pay unpleasant imagine agreement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_string_from_array(array):
+        return array.tobytes()
