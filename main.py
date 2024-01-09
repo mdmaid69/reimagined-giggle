33702332d4f62a82d3e73e55267aa005e441cec1
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:35:48 +0000

    wave some bringback cross 566end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
