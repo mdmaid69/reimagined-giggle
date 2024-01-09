d9ac2980d50f54f5b0d550eb3842fe1860caa5c3
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:06:07 +0000

    sensitive butter goon cool anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+def calculate_power(work, time):
+        return work / time
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
