bd44c51ebeef2454c6950b29f0d509ab3e75cf34
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:24:46 +0000

    jump tear study under contrast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
