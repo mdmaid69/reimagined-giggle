b343474e19d4eb3c8376975dfe6ca41fd9ee252c
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:34:32 +0000

    so live say life film

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def convert_to_octal(n):
+        return oct(n)
