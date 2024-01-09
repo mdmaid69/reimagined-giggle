3c1a9c1644d8fadd21055c9e86ba0753b0f01249
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:53:53 +0000

    tight pipe platform comein Gg

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
