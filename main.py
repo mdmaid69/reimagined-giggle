8eb592b4fe59e012958aec12242ecc1623d36c23
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:30:32 +0000

    guess catch speak beyond workout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def clear_array(array):
+        array *= 0
+from collections import Counter
+print(Counter("hello world"))
