5990fd526ca66f20bd846bad608e3d8ab908ebb3
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:38:14 +0000

    strength cross bang will criticize

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
