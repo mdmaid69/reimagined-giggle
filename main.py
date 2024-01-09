e0fc0597b2991d7bd91fbf4d43e10b88eb727ddf
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:00:55 +0000

    last start news ought moon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-print(re.match("h.*o", "hello world"))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
