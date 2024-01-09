deaedd30b46ecb8c359aafda1cc5b1fa91eae215
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:44:45 +0000

    afternoon film unit well lucky

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-for i in range(10): print(i)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
