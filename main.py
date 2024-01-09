392ac1d360d64d90da592664d2dc173f1d384f21
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:59:27 +0000

    wide only stay warn translate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
