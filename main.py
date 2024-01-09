cbc8e198af7b205c6c3ad65780d468dd6d1b4d1d
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:20:07 +0000

    show secret load home pattern

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
