df93bdf492e5684eea05579096d0af63c9e05d3a
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:53:44 +0000

    fix branch exciting deliberate whole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def reverse_list(lst):
+        return lst[::-1]
