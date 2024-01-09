a3a327a9eec5e3a01de09207b180321907970a42
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:38:20 +0000

    high majority necessary law nearly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import random
-print(random.randint(0, 100))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
