94435aa763544a5bceb923c4d922da1f595f6ab7
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:05:41 +0000

    touch smoke vanish wash must

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import platform
+def get_python_version():
+        return platform.python_version()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
