0fc8115983537e39b13ddc73d7aeec1eff113e8a
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:29:33 +0000

    short pinch no performance fashion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
