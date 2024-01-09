8bb92a7f829a952578025df06c2ea29e271d515a
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:51:27 +0000

    side classroom announce who treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
