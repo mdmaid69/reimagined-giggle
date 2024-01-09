7b62588a880283c70db24e8302e815247ddcc129
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:05:47 +0000

    attempt born few sea circumstance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
