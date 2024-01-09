fdf1070d30856e1e8c1b729148e941da4d3559ee
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:20:24 +0000

    past match choose either fire

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import array
+def get_array_typecode(array):
+        return array.typecode
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
