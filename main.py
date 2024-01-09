328e4a5254167821908f67245bf57a65b0557642
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:54:50 +0000

    work cough frame generous piece

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
