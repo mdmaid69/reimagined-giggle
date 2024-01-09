13af3edb4340d591731575c648551f7233079b22
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:04:23 +0000

    reasonable crisis sweep float rub

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def get_array_as_set(array):
-        return set(array)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
