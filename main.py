c162016cbdb4c942d20f34c224bb01d7c9a4e8e9
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:41:27 +0000

    swing ready other original slight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
