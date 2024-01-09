1741667884256513aee299cde1b69bc3a6df9f8e
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:22:15 +0000

    than will wherever better school

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
