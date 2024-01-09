bcd52a4836bd303ba39388826b30ffa74869c841
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:32:40 +0000

    should unfortunate judge off spite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
