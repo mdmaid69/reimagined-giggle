ca2313b2e13d2172b6467ba615e5ab289023136f
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:57:00 +0000

    wash couple it Zz pleased

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def calculate_pressure(force, area):
-        return force / area
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import os
+def get_current_working_directory():
+        return os.getcwd()
