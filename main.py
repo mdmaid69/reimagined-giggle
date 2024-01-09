4de2fb50b810543659ac8422298800d9d4a0b455
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:34:57 +0000

    fashion future depend ear cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
