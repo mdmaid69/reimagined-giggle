747f6e60996ecca682b2ad739ad1a380fdaf1ab1
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:59:58 +0000

    famous guess blame active advanced

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import datetime
-print(datetime.datetime.now())
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
