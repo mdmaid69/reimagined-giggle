6ba7646e6a2fe2f53ea3b883ab94928104b9a542
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:30:15 +0000

    kind this responsible conduct advise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
