b732224cc9901af65845f8e547918292d2b3f5ca
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:07:56 +0000

    Nn hold burn quantity original

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
