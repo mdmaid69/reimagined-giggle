f3efea27d6f5085285b4622254dc7939161d6d7a
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:25:58 +0000

    rain brown brave emphasize come

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def is_odd(n):
-        return n % 2 != 0
-  def convert_to_octal(n):
-        return oct(n)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
