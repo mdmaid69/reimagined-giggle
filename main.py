e64336482f604f2b3dd4f3fc6d716444a4586664
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:03:45 +0000

    though month bright join makeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
