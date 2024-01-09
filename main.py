8a369567be95b879036866123027c7de4d8a8b41
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:20:23 +0000

    warning familiar detail defend wonder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def count_elements(lst):
-        return len(lst)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
