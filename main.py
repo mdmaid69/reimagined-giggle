3478478706bf1627f96e9340c8e74d6216f9fc43
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:42:41 +0000

    through reaction lay degree support

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def is_odd(n):
+        return n % 2 != 0
