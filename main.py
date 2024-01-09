56d526b28ea657645280afdf105aea2ab371e3e2
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:40:58 +0000

    appear improve react weekend only

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
