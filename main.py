9014146a1ff06c7320b23d80f9b4dd27c40b4a41
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:32:08 +0000

    send closed will wash comein

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
