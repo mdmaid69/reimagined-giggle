e8ec250c5d8e667855bccf22ea947969ee0cf503
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:49:51 +0000

    ourselves address afterwards afraid suffer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
