475d14156d743dd1884964abfd951478c26daccf
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:21:18 +0000

    cruel soil turnaway offer thin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import re
-print(re.match("h.*o", "hello world"))
+import os
+print(os.getcwd())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
