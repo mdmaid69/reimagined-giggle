4f4d89c6566e7b15ce6f19ace1faf17fa66ce14b
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:49:39 +0000

    sensible sign search tidy once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
