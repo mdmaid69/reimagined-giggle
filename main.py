0fe385ab476a4f40b882012b0a149b715b59c1d4
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:44:19 +0000

    worth period case middle practical

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
