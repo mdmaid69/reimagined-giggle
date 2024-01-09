6daaf0ace786c3eaa850770502bca6a272a5dc24
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:25:58 +0000

    loan stop sentence hair front

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def find_max(lst):
-        return max(lst)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
