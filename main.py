87b2f42f3edf8b23adc096605735928fbd3192b3
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:07:19 +0000

    treatment work replace post love

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def get_array_as_float(array):
+        return float(array[0])
