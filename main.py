c5fd087230edfee3d3ead889d95f047e799a93b0
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:29:19 +0000

    anything poor book fashion responsible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
