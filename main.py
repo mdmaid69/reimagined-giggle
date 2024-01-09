79382a15676362dfc094b7b633a8e08a68df3d29
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:48:16 +0000

    discover love better safe estimate

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
