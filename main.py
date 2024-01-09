27db1d851a0bf123dd7d8d4efb6777a5b5a2f9d6
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:04:45 +0000

    block already tone will pleased

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
