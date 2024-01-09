92f59a70c07c994fd0184795edc2b1706a124df1
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:15:27 +0000

    hope pile seat getup bright

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
