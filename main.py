b79d4a64b6d78c76047bd9efb9568945a99dd71b
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:14:11 +0000

    action front future recognize difficult

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
