abd5f4cb41334e96abd1b4f6518612b9ab964a69
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:17:34 +0000

    maximum free muscle quarter ourselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
