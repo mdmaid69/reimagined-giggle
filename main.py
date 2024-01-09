17d8c25cc41e41605eab9f5292a4aac64bda7171
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:39:47 +0000

    man disappear front physical goon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
