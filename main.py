dc25ca272d407933ad42fd678d8b4f183ac5dadb
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:07:03 +0000

    somebody punishment join detail spend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
