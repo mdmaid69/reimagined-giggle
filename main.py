5ce92cdfc24d4444546e57a3221f75515504d524
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:35:19 +0000

    through marry test actually task

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_error_function(x):
+        return math.erf(x)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
