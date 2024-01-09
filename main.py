0380f8d83119613b6025bc19b1a334342bbab206
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:23:55 +0000

    advanced punish official price thinkof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-name = "Python"
-print("Hello,", name)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
