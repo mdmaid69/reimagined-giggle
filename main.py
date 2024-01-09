fe975be4936a3465b5c4e7aa3bd9f588684d928e
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:09:18 +0000

    up means her explanation arm

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def find_max(lst):
+        return max(lst)
+def calculate_pressure(force, area):
+        return force / area
