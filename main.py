2a5258720ed30e426cbea65ee210cc41b7a18fad
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:35:15 +0000

    steep giveup thinkof body completely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_set(array):
+        return set(array)
