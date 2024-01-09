cd45c18c02c15e29c351086ccd15f265170aa047
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:50:39 +0000

    but bridge buy trip cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
