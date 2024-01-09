c3085b48c66683581b2d0d29abf4c14141c22ffc
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:58:12 +0000

    scrap upwards sticky lock cost

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def sort_numbers(numbers):
+        return sorted(numbers)
