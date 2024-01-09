0def260d40968861d25df03265128d89fc1e9b93
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:21:14 +0000

    buy breakout vote accommodation confuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
