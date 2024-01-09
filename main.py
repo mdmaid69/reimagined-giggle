20dec21717ce551f503bd8ae7b41b783423e784c
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:44:17 +0000

    568energy business come shake except

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_acceleration(speed, time):
+        return speed / time
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
