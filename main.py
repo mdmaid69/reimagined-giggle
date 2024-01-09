288d59a423f1d315646be6aa54ccfdd3761442ff
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:52:29 +0000

    peace reason finger few runinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def subtract_numbers(x, y):
-        return x - y
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
