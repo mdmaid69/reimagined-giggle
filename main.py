5052b1ba93bff44994913ed7333593ceb5fbb320
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:30:34 +0000

    tap professional basically inquire secret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
