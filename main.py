0cd4fa58235fde3f03ce581a83ae43fa1f25fd25
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:33:18 +0000

    problem stamp you minimum whole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import tensorflow as tf
+print(tf.__version__)
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
