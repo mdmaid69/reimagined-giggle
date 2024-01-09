9294b1c7257539f39b64359124ce5372e00049d1
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:48:26 +0000

    reliable plant key small little

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
