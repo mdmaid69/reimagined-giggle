ac8136c38a94cf666381eacd0ac4ec472aad13b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:06:07 +0000

    health appeal limit level service

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
