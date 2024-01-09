d45ff6490b6812cbf5fc2ddd26899d56c446dd65
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:41:27 +0000

    refuse breakfast communication mass only

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
