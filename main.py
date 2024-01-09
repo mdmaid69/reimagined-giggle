4217a1f4394658a85cb862b033b98d324b974d5b
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:21:00 +0000

    chin stay read odd earn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
