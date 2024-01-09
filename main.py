b099b2e0dc0744c613f4c72018867bd29c906f20
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:53:11 +0000

    here steam nurse choose scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
