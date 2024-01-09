0df465e696ca4629f06b390ac54a85d488c0b889
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:02:54 +0000

    even cry satisfactory food respect

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def add_numbers(x, y):
-        return x + y
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
