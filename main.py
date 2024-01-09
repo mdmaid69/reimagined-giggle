03722d65753561ea61de32391b4f405b8be2e018
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:41:19 +0000

    fresh course trust terrible innocent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
