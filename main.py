a28457ca4a4112d9038e844ee7ea3df825491f8a
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:17:52 +0000

    even although thinkthrough degree sad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
