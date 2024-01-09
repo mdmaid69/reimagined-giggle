a031def95d757d4d66a25f564e372125e9ebd3f8
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:55:16 +0000

    wipe matter mouth after surprised

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
