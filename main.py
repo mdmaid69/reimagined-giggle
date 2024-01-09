05d2837914be52ff52b9b31e32d3842b9a4c5daa
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:37:48 +0000

    report worse positive dream far

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
