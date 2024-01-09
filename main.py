02e35e6345bba6c05eefd77c209c6536305405d6
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:38:46 +0000

    interruption so stop rule talkinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_speed(distance, time):
-        return distance / time
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_item(array, i):
+        return array[i]
