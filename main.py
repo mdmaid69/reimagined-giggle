ffc58206b308ef43a6b90259e2e939476d09827f
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:26:34 +0000

    muscle determined until manner politics

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def join_paths(path1, path2):
         return os.path.join(path1, path2)
-def find_max(numbers):
-        return max(numbers)
+import json
+print(json.dumps({"name": "John", "age": 30}))
