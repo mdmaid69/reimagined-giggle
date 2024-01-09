9b2ca1d01533cd31a31ff7891bd642dba8fff0d5
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:17:21 +0000

    national comedown pay personal free

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
