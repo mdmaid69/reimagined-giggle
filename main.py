abf8a5769f41c15ae221f3719a72b3dcdfd4c008
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:05:45 +0000

    love communicate section they scene

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
