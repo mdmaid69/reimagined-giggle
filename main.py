2ce29b842f5f3fe25580fdeea3fbd3c5c3bb5e86
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:25:41 +0000

    Kk autumn note sale influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def subtract_numbers(x, y):
+        return x - y
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
