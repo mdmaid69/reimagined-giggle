6c628451cd14520ee461f36093d270ff9137bfc0
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:25:31 +0000

    grow clear deep escape reply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
