3fa9d87822487f9b270a253e2f77363061e2584b
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:14:53 +0000

    exercise surface more damage some

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
