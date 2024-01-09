615affa3f89d597b8c45724a1f2770a867cbc8f3
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:02:01 +0000

    quantity suppose calmly traffic sorry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
