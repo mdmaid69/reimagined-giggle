e059d523185ee1a24bf95902072f34706ceda377
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:47:43 +0000

    hello tonight name succeed interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
