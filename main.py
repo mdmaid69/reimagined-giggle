919a6e73ddaaceb8ac2806adacff32eaaa9cb779
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:33:17 +0000

    machine form swing arrange love

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
