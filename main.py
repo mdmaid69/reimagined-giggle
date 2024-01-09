a0f1e9f642b92a44689385570c6c1bf856c2878f
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:34:18 +0000

    calm influence challenge promise pain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
