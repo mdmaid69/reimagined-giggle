04948b6a2f335e0cd7daec1103148a2b4e5f05ba
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:57:04 +0000

    spread trap slow course favourite

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
