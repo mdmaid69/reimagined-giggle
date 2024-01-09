d90468acf1565d0470d619daab8656edde2fd13b
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:46:04 +0000

    kill stay 570enjoyable hide flow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
