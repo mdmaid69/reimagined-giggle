ec5f65ebf69fcdec4fa91466deeb9ab793491c1f
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:54:49 +0000

    mean hers day minute environment

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def convert_to_binary(n):
+        return bin(n)
