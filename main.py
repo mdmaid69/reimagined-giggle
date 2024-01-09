4e94a57018e49b49981a648a8269321259db541d
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:53:18 +0000

    might breakthrough important circumstance model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
