17343661f2b07cb3279719166fd4d81028ebffac
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:35:06 +0000

    damage tooth settle journey Zz

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
