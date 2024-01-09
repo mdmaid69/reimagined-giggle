9316bf6489e876fb7718820ae67db63f2611eb47
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:54:23 +0000

    strong free pride platform goout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
