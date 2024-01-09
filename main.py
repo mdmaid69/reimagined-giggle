bc62d057360d62674245c03f5c7b2f7a55d79c7a
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:22:11 +0000

    rarely improve know contents determined

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
