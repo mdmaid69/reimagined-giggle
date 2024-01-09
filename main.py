e4f2b0d3b876517e80166a75ade49cadd07b4328
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:52:04 +0000

    stone occur Mm club memory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def convert_to_hex(n):
+        return hex(n)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
