0be88065e9c6b2981e0ba0d9620bab7d7612d380
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:00:50 +0000

    destroy might holdon exhibition moral

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
