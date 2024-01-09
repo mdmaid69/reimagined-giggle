fc596b682f560176ba8564ad42e3dd522da218fa
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:37:33 +0000

    base total activity cheap advanced

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
