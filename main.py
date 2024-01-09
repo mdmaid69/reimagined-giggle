5122107bd16913df8c1491503664051d9eff0117
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:25:11 +0000

    completely left once succeed Vv

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def find_min(lst):
+        return min(lst)
