bdab337f4a838007159d92081c3685daf8d9931b
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:41:18 +0000

    achieve sleep walk last physical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import sys
-def print_python_version():
-        print(sys.version)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
