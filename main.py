3a023da14dfc0bbeb5bad7a5cab638d1ecebd431
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:14:55 +0000

    keep identify business movement impression

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
