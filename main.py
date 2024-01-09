6d1db1bc41bebff08331b38b6ada34101f86d9b1
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:54:14 +0000

    computer drive make risk learn

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def count_characters(sentence):
+        return len(sentence)
