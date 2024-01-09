f62cca6c17c57d0d19446a8b6705152a094ae43e
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:05:02 +0000

    any return impressive comfort story

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
