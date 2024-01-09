2b68687baa46044dec8a907e69327b8314408d5f
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:54:36 +0000

    down draw fault song slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 text = "Hello, world!"
-print("Reversed:", text[::-1])
+print("Is palindrome:", text == text[::-1])
