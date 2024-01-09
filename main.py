688655177270495d763794dd756ef76071948b7a
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:31:22 +0000

    rate suggestion proud little allow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_octal(n):
+        return oct(n)
