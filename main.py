9c9a4ec2dd4d61138ed8e24ffaeaf0a27a475c1a
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:49:42 +0000

    shade oil shock terrible achieve

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
