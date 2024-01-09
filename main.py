1ff19b124b24e40e39695802f3486522f346654e
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:58:13 +0000

    until anxious extraordinary fasten country

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
