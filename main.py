fcfb64b35f0278a7d35a7435b3f34e075d259694
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:04:33 +0000

    final nearly band shelter press

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_item(array, i):
+        return array[i]
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
