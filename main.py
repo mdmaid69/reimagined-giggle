19b8beb30d5f40d8e780f3c746a65028a0c0b610
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:49:30 +0000

    pump unlikely money telephone fit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
