2cd9f0b4856c9ecb7b7a9236e9ef32f819e20b8e
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:26:09 +0000

    right little centre much room

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_min(lst):
+        return min(lst)
