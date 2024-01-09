593eee1218ec6b5a16aca375ad55f20ef19f23cd
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:23:59 +0000

    Jj tooth yet underneath between

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
