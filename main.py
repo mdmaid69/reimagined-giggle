46a4a16b76baa8778827c6b546a3ea94f2d77661
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:26:17 +0000

    related murder order might rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_max(lst):
-        return max(lst)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
