740823c386488d5cbc85bdcd24ba872a5f229e37
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:22:33 +0000

    criticism computer join underneath date

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def convert_to_octal(n):
-        return oct(n)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
