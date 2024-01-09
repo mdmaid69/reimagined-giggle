72a9571b0256a5f44dc39c66b9a6cf04e8952160
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:21:24 +0000

    boat duty country drink direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def find_max(numbers):
+        return max(numbers)
