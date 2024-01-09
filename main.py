d27d672fddc1dc676e63a0717e91ba7b9125d003
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:14:52 +0000

    science somewhere lock ashamed once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
