d7bd6d0fd43abd1d762b109fe70044e20f68f07a
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:06:58 +0000

    attractive short fresh score anywayanyhow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def remove_from_array(array, item):
+        array.remove(item)
