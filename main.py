f070f8ede7315423bbbfefe751cdaa7897cf5437
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:29:49 +0000

    suit news familiar risk can

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
