a4336a0e0965a829285c9db1904a04819998ddec
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:15:09 +0000

    573entertainment profit offer uncomfortable while

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
