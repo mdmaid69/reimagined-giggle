c12076a72965191ac3f2eb9fbb705e83ceec6ceb
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:57:22 +0000

    total last continue branch damp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_min(numbers):
+        return min(numbers)
