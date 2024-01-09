22d374a6956074b704fcab3e633d51931bda9ef1
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:25:16 +0000

    turn dance study realize price

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_density(mass, volume):
+        return mass / volume
