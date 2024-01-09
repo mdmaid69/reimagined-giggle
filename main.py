bcda1b399d36d7cc3060d3ed45c454c62a84794b
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:02:10 +0000

    law settle collection poor far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def create_user_dict():
+        return collections.UserDict()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
