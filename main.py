549474c17672154f018e55513a666fac0541d47b
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:48:18 +0000

    mess excited coffee tall give

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def sort_numbers(numbers):
+        return sorted(numbers)
