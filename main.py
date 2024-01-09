6bf32c96c59fdda9a18076879d0528ce6e6c1626
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:08:15 +0000

    straight visit convincing fall dig

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
