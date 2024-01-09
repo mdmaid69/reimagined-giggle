887ae0a40eb4b769285179fc47a50834f6f38842
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:40:52 +0000

    consider send free admit I

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
