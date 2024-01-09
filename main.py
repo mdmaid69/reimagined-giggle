fe87de9d9e5362cdf4a0a375e8043c4a61814d9f
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:33:08 +0000

    keep uncomfortable giveback lay though

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-for i in range(5):
-        print(i)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
