472005afc3a3350557f71dcb36d4a0c3b2011dba
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:25:54 +0000

    dig connection Ll science judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
