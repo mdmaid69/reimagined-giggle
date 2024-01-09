cc195dc85ca6d2c26e8d0dbaebcc3c857da9d996
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:30:58 +0000

    anything various deaf should skirt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
