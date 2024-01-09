92cb0e4e7ee0a911d9cafe1d295ed3230c5e30a3
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:16:47 +0000

    run wheel payment side easy

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
         plt.show()
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
