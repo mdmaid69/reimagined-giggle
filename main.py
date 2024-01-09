fa61ce7e4972db057c4d43828c306e45d3136de5
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:09:02 +0000

    564encourage season sort likely emotion

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import numpy as np
-print(np.array([1, 2, 3]))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
