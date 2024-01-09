d2dd1f32fe35893e6fad2fc8604860fbfcd7ea79
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:04:38 +0000

    broad embarrassing bottom prove breakout

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def convert_to_octal(n):
-        return oct(n)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
