19f80ad25a6aba387fc58998a79ddfd1366dcb8a
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:53:56 +0000

    mind protest someone connect they

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_power(work, time):
+        return work / time
