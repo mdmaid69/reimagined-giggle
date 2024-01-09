77e4065696846ab42678589be8f8dfd3ee90b448
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:38:33 +0000

    tape common threaten goup share

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
