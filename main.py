405220d0b1fd1c5699fab8c580b50d29115a21a4
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:00:31 +0000

    heavy pencil giveout children front

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
