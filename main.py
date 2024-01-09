a0240fc11f0f1fd27a290f228618c9351c24db8a
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:45:27 +0000

    goodbye kick score majority pass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
