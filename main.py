6cfe12d0651fdc0915f77c77828756b8bd832dd5
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:20:56 +0000

    we correction 566end speed clue

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
