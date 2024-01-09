efc612e05142e680c0d97c4ea894202467d66d4d
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:53:28 +0000

    emphasis project sign hospital congratulations

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  def cube_number(x):
+        return x**3
