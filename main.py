30db26dc6e49159c9e20d7927128e06fca8165ec
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:29:14 +0000

    quit second blame getup thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
