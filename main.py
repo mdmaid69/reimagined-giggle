1a69f3447bed8463fe8d8d2bc688a32dacdba585
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:32:53 +0000

    miss mess soil peaceful only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
