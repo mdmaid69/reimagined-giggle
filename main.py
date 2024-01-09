5b4ce42c66138bf2d8e9bf449fb781327153f484
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:13:39 +0000

    physical group reaction modern exit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_current_time():
-        return time.ctime()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+print([x**2 for x in range(10)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
