36492e3c6069c23226d626a4c05549d313d055eb
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:32:30 +0000

    secret schedule union afraid Oo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-print("Hello, world!")
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
