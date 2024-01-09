3fd1b24428af6d81ea58f3b8a0f5e21d02e8be0f
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:33:35 +0000

    worry Qq share unless cold

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import sys
+def print_python_version():
+        print(sys.version)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
