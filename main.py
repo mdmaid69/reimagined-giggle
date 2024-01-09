16c521d5450b49389b7a8010ee20f3013acfadcd
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:31:50 +0000

    stop half tired contain hurry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import re
+print(re.match("h.*o", "hello world"))
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
