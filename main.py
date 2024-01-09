f2b7dd78c71b83925d46d2bfa51b494165fe4db4
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:39:21 +0000

    machine business Zz common past

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import sys
+  def get_python_version():
+        return sys.version
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
