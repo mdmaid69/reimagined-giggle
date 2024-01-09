8f76edcc3401f4cef9b8c8a0dcb9a314912f5221
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:31:09 +0000

    get related mention your address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
