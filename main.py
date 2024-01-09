6684d4188d70ac1b05af4808abaf2e6d1781270b
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:26:25 +0000

    open show find shine moon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
