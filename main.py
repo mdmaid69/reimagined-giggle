0c1fc06e64c9fff9dde9e87d4697fb7b8947e90f
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:09:42 +0000

    cheque smoke boring thick pleasure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(5):
-        print(i)
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
