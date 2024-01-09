21aa7720790922c36f76350132941532de45b65b
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:32:01 +0000

    description roof connect keen wise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
