8f28183e7a316bf5271cecd6ddf9df8c122eca85
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:24:39 +0000

    neither sad door goback(to) touch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import collections
+def create_queue():
+        return collections.deque()
+print([x**2 for x in range(10)])
