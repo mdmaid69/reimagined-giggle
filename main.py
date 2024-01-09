539d41bbf89b07d8d33b62f89dbf5d31d68dd798
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:06:47 +0000

    embarrassed write long national almost

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
