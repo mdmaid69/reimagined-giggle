f9d0fd0e62bc053ad47281827d1f58f9d9557744
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:11:41 +0000

    class married street blue table

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
