f0507c09920684a47d49caa5dfe21e040a06208d
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:16:15 +0000

    drink hurt concerned fork part

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
