2c8dcee4bf10e3240b2022f21b29992fb2863cf2
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:22:54 +0000

    swimming subject sting opportunity half

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
