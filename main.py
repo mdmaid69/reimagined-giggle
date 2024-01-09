e7798cde0541c7a0ad805f3f92532d87f0f2463d
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:09:02 +0000

    chin appear we mistake project

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
