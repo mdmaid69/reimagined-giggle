2d7a8b2641623858d9fd86cf40949d9d04efe222
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:12:12 +0000

    manage related damage pride relative

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
