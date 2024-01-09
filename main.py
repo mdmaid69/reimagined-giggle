5d3c30d0bc484ec9ccea0861ab374aa437009af9
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:18:39 +0000

    break spill drive secret shock

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_pressure(force, area):
+        return force / area
+def subtract_numbers(x, y):
+        return x - y
