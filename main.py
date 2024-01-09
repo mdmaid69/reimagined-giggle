93c2fc1780a6151479ce682e608433e2ca45f991
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:19:25 +0000

    provide realize apologizeapologise apart letter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
