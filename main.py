588329c404f63bd6e91bf10ec758d6ebb682c208
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:32:33 +0000

    image address black responsible 179bite

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-for i in range(5):
-        print(i)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import sys
+print(sys.version)
