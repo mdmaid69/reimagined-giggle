da2701792ceea01dd40dc9cd1d216aba440c995e
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:58:12 +0000

    trouble children tip wait body

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import collections
-def create_queue():
-        return collections.deque()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
