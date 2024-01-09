ba59f3c7ed832c06cb1ca84a1da4bba31e2b20ba
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:07:09 +0000

    before maintain lead respect except

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
