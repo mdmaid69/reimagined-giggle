df41cdcfb2fcce54421ffe17b4e62068fd215753
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:24:47 +0000

    negative share warn cheat Rr

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
