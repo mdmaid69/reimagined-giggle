ebee6e4a675222a4613edf44af1a4a8ff2af021f
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:57:10 +0000

    positive cook boat custom along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
