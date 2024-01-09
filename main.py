d0c52ee818491b3a3b78514d80d6414ae03d64b0
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:50:35 +0000

    perfect bringback across argument sweet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def create_counter():
+        return collections.Counter()
