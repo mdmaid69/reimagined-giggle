ce1bb65107cd6d689ba67549ea4f47dbd1611eaa
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:57:08 +0000

    pick Pp secret news sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
