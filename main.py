91f3d79c28c988ae34d91b46333edce97b36e73b
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:25:24 +0000

    shelter disagree run relationship scared

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import sys
-print(sys.version)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
