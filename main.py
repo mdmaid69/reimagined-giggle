fcbe15eceb9a6e532af30251bcf266354fdb42dd
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:23:48 +0000

    giveout defeat woman next complete

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import os
+def get_current_working_directory():
+        return os.getcwd()
