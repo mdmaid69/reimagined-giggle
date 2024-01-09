7851deff3d5a3b668b8a852ac91726f93e66f27f
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:12:50 +0000

    narrow runaway fall really benefit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
