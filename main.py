da978d0d553ee1eda2e62b7f8af42e0ad2c26ff1
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:24:18 +0000

    other on way regional tell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+for i in range(10): print(i)
