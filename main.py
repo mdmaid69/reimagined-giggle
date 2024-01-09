05d2b73ecee6f9bc36f1ce93dbe76e83974e25cb
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:06:20 +0000

    fly public board priority plastic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
