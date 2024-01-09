5626c298a8a728b2350df7f145d3ebab70133af5
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:57:08 +0000

    example organization its warm rate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
