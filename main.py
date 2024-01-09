3de4464885af593cf9439b8c1d3de59a7b071453
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:47:38 +0000

    workon home contribute buy dress

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
