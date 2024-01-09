59490cd0a6392704fb6377413c2485a71d60f6b7
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:04:21 +0000

    young gas their callup pupil

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
