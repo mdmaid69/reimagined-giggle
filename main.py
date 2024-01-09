40cd52f8a64648aea4f1c7ec22ba009614a99107
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:35:58 +0000

    money chin period still pay

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
