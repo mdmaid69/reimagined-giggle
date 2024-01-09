d1421ab9f82ad998ab2bbd28776bd975fd8eebe1
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:13:22 +0000

    description admit scared equipment this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  def count_elements(lst):
+        return len(lst)
