5c1d08b1553b55df350a01316c05dbdb1542f8c1
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:15:06 +0000

    that common hope between exactly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
