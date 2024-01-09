942a61994904d5fa57225cb1d04ffd182225f18b
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:46:49 +0000

    swing storm tap innocent mean

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_queue():
-        return collections.deque()
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_length(array):
+        return len(array)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
