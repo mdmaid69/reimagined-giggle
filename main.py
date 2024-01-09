e666381533b13fc7f26a21b5d87e95e41657ab74
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:27:29 +0000

    switch male keepaway recover easy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-for i in range(5):
-        print(i)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
