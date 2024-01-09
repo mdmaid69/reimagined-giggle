03c4843ea9b0400ec5f88e214abff24a6d6e4a2a
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:21:25 +0000

    afterwards woman arrival row light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
