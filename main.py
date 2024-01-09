76208a08001a9bd0254f306cb8a8af83cc8840dd
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:16:36 +0000

    actually pick calloff recover scene

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import numpy as np
+print(np.array([1, 2, 3]))
