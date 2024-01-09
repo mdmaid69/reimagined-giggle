dd7a4ae6ec6b4f75a41ab417b5c78724be0bd5c4
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:20:06 +0000

    even impossible sort care there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
