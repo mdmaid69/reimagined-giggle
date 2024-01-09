22b9402209463c94eff6c5220f4092ffa2e7673f
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:39:31 +0000

    wherever fat throughout mysterious open

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
