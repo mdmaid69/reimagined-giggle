eb33ae8c2e77838acc67b64fdde33fdf72590ca7
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:33:52 +0000

    trap comeoff guess same unpleasant

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def convert_to_octal(n):
+        return oct(n)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
