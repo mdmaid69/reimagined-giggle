c66ae99f2841959f245c275dc72072f40e4b6f4f
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:39:39 +0000

    nurse game terrible this attitude

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def divide_numbers(x, y):
+        return x / y
