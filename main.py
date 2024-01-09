57f083bf01c2d9b44d30e0c6afaf69a23f74e6c2
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:16:03 +0000

    move apart like rank warning

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
