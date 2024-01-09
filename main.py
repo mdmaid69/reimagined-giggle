a2a3688725acb7f021c544fe3798600830834a7d
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:36:53 +0000

    ignore comment build considering goon

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
