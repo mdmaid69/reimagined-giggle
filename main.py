0adac85b5eff1c60e7f1ff260ce051b8306d8188
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:28:23 +0000

    fast reserve object gap story

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-from collections import Counter
-print(Counter("hello world"))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
