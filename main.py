bb383c754ad923a01effcb7fe5e770dfd8546191
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:19:51 +0000

    smoke air healthy decision principle

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
