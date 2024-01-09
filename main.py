4df743bcd22c03dca67e539fbe7669e797c614a0
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:17:53 +0000

    copy callup happy half she

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
