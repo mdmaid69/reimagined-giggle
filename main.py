a5a5ccffe871cfeefd2c0d3848e3819e62f71cf5
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:25:20 +0000

    die attempt man condition busy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
