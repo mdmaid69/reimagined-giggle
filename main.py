62be9afba1a55cd642e79ea559f4669c463d9c1d
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:20:31 +0000

    bad party practice block place

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
