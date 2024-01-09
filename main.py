490b310b83e348c0b913835a1db752cf40346231
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:36:50 +0000

    hurt determined giveout field arm

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(a, b):
-        return a + b
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
