3fd29ed4e6a76eaafabc2379db3fa6c28dfec1f8
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:35:15 +0000

    party tip window goout later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
