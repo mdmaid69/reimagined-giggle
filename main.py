635395b0c63f957611854b1fab51c9ffe017ff01
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:01:59 +0000

    suffer common perform message method

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
