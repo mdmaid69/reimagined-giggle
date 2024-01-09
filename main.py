121e21768876386c6357d956658222dc6fb2ab72
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:20:16 +0000

    right snow light holdup central

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
