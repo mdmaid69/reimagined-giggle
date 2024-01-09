95d40626f50da0c34b6ba11d4b290bb85d7b3a38
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:58:19 +0000

    large train strange benefit Zz

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def read_from_json(json_string):
+        return json.loads(json_string)
