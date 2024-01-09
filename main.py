4d098598ab17085c3466a6a605e35972f667e895
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:58:46 +0000

    act attention smell alive fear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-print([x**2 for x in range(10)])
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
