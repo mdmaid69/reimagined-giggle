f9f64d09c14b639025847cd119e7a8d4b6af82fb
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:49:50 +0000

    shrink lie among suggestion weather

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_complex(array):
+        return complex(array[0])
