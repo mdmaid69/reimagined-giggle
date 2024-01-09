28b1f1ae10b9b2097ae0edd4575572237f03505a
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:08:22 +0000

    dull direct march who war

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
