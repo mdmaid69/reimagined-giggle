57ade9ca80dea6f0af6c55eb4f9125ee29ece6e5
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:16:57 +0000

    breakup group reach turnup hair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
