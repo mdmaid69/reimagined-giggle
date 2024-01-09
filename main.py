4b11cfeab6b3b7e3ee38be97649be0135ab7917c
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:39:23 +0000

    deaf fire big press guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
