2181f7cf45f386b4cceb4551524b3f2516322f47
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:48:19 +0000

    excuse tired die promise correction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
