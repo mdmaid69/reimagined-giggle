2d61123268b3878ed1e652dbdc014f344c25ec18
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:17:05 +0000

    there cheque destroy break aim

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
