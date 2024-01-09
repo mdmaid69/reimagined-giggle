3e26b492f0baa4311bca78170755c916729af64c
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:06:32 +0000

    develop front up grow dick

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
