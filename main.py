c1aa29dfd69d4541484053d51ac8a239a81c6124
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:47:24 +0000

    outside that talkinto able sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
+  def multiply_numbers(x, y):
+        return x * y
