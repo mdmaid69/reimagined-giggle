acbf4dfb1e528f72ae653dc7b5574c65689ec658
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:39:14 +0000

    comment likely interview constantly probably

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def add_numbers(x, y):
+        return x + y
