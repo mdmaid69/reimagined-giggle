1a25d5f96474ddc52b366eddb3699811166d1e2a
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:59:31 +0000

    same Gg father prevent which

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
