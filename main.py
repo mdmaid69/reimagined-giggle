67c79cdff37626f6d27b4c4ed7d180b904f1ef2a
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:25:10 +0000

    himself and want add increase

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import sys
+def print_python_version():
+        print(sys.version)
