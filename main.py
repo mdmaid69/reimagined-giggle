b37f13aa6e73c5cae0ce82e999b08978d8d5cbfb
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:53:20 +0000

    underneath offend its stop cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import tensorflow as tf
-print(tf.__version__)
+  def split_path(path):
+        return os.path.split(path)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
