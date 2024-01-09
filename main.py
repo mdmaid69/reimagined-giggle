34f9ce7875476f12a0c27aa7bf79ccaa710767c5
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:55:25 +0000

    knock available react appeal offence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def remove_duplicates(lst):
+        return list(set(lst))
