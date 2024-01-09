643033ce81449d9fe4016b157874c208f0d55697
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:15:38 +0000

    block such plan holdup up

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
