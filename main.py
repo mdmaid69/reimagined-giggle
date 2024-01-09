0c2ee7e517a4700f7b380f3a4ce37053a2a351ba
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:10:36 +0000

    sleep morning fresh edge disturbing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
