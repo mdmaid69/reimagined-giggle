35650e739a1e9ce8bde1d104603db53201be8c0f
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:15:58 +0000

    either control deserve stage hospital

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_pressure(force, area):
+        return force / area
+import array
+def get_array_typecode(array):
+        return array.typecode
