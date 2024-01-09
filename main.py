ee66cdd119d1e78c91f758ef4df5705c7387fa2a
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:25:48 +0000

    dull name tool fill maybe

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
