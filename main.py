689bae459c38ac286dd7bdfc181abf3a01837767
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:03:44 +0000

    male plan matter account release

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
