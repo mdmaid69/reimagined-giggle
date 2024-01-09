849431c1cd9468430e6f5ac44b2edbaa26371ecf
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:16:06 +0000

    contrast shallow authority very demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_acceleration(speed, time):
+        return speed / time
