af51200dbb254fef7c3c8cd6a9345ea61983815e
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:38:59 +0000

    pleased leg important guess keen

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-import re
-print(re.match("h.*o", "hello world"))
-for i in range(10): print(i)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
