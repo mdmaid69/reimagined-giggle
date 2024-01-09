d6b366f605cd5d69d714b5d5001111412d7a477b
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:10:21 +0000

    fight bit stare single do

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def subtract_numbers(x, y):
-        return x - y
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_current_directory():
+        return os.getcwd()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
