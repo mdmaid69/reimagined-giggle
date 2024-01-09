8a8d1e50949e2783ad8bccdd56043d34724bf79c
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:21:36 +0000

    poison wind action plant talkinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
