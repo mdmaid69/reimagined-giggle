6fc9f65d1b1fb16e67c8e2f376667e920cca37de
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:21:56 +0000

    record holdout like lock holdout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
