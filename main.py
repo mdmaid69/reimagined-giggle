132440819e58865948e8d1d2d5d25a4e1fb46fbf
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:10:26 +0000

    century pay prize scare suckme

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def load_csv(filename):
+import json
+def load_json(filename):
         with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+        return json.load(f)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
