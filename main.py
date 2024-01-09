83000d1f30d581652342291c8ca3b77cee6b1c36
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:34:58 +0000

    over with comeoff custom dress

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_max(lst):
-        return max(lst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
