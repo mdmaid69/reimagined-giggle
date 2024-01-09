b56ecc19e41984a3d783edf6e226d5d2f1b86603
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:29:50 +0000

    its shallow pair slice steady

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
