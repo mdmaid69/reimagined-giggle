c8a44928c11241852e62860363a3a7d7af35d2d1
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:10:32 +0000

    little unable word this belong

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+print(math.pi)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
