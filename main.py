cfdf566b81457692bb67f8d5398441473a00d89f
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:28:12 +0000

    line food pipe point guide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def split_path(path):
+        return os.path.split(path)
