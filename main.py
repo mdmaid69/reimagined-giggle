717246f1bbc12aec7fa92c56bddabd5eefbd1f9e
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:54:54 +0000

    refuse better nervous clothes arrange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
