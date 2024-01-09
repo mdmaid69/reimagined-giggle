32e82ca15f39f7eef03ceb8ac7fb692ea2371ea0
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:42:08 +0000

    include experiment grade avoid eager

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
+  def get_file_owner(file_name):
         return os.stat(file_name).st_uid
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
