b05027634875050ba23549e085cb017fd1a38429
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:06:45 +0000

    ill through office heat by

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
