0d3e1cdce5e26c05c099e963a89a1c0fd382ce0c
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:04:29 +0000

    ours edge crash heavy warning

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
