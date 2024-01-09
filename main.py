02d8a925109a1a328bcf3d334fd68ef34bfc1dbd
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:06:40 +0000

    few wait willing tie company

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
