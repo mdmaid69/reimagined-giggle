e486edba4070a052d7d1d387c698b001b723fc4c
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:09:27 +0000

    display strict chase culture serve

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
