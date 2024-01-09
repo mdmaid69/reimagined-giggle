14a3bb5715423fae9ba8a831e17d63e4c0c8c131
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:42:54 +0000

    bother your dry pollution someone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
