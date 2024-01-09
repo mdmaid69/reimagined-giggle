40c4b397e832fa459f309bb241c786fb76f7f506
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:35:17 +0000

    new platform chief before issue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
