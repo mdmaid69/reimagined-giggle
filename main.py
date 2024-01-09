fa7d9b8a322480ae3a0bfc6a42473e1bf2226285
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:16:26 +0000

    outside so brush memory level

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
