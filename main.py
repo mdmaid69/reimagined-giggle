95eb0965ca455a6a5cadd32b1a4c8db4e0191f42
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:45:53 +0000

    night breakthrough dead card yet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import array
+def get_array_as_list(array):
+        return list(array)
