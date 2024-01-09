3311526bb240705f15500981f8d126205477eab9
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:57:10 +0000

    that occur everything partner keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import datetime
+print(datetime.datetime.now())
