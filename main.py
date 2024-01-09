cbb619043ada278c8dc0545abcdcf5c63865a4e8
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:16:15 +0000

    whistle answer deep giveaway comefrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
