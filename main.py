d7b278b4a76c8ec3f75100bfef78a383c1c221be
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:16:59 +0000

    shame most split clear effect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
