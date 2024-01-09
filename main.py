c747205c0fcb3c77e5cf81f0e6a49a2f14e6ca3a
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:51:00 +0000

    boring cross furniture taste ring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
