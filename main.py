508e28cbc286ab4dce5ffa633e043eb67318495f
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:23:53 +0000

    walk pity one unite recent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
