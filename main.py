4cf2ac38b4556dc38fc69b4dc022f4a8c5aeb212
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:00:26 +0000

    often colour more sleep connection

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
