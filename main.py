9a4dd6fa0fe2bafa6bc6b145cb95b38ebb379238
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:31:22 +0000

    doubt genuine runinto shock disturbing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
