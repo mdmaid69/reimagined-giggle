373fa1c37e256da75e830b216d56c1baf2437c30
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:00:37 +0000

    want event perfect pipe confusing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
