76b97ad0a412fc688e2d84b01f5f95dc079d880c
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:41:23 +0000

    cry either pretend stop television

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
