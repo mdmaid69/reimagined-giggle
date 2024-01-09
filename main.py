05abb96cdc2b54d375f7345271bae685fa944411
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:44:29 +0000

    spill child represent clean left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
