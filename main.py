49389a9b400582c8abcd915c4444d17a809e3f0e
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:09:58 +0000

    clue ours exam hour board

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+text = "Hello, world!"
+print("Uppercase:", text.upper())
