6d7b6d50721f6c1f216dee57d973046dbafc47c7
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:21:01 +0000

    whistle join funny something sharp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
