6c0feaeeaa3d705e3685347fc95870f5d9ef3834
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:56:22 +0000

    section cry politics play miss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def reverse_list(lst):
+        return lst[::-1]
+import sys
+def exit_program():
+        sys.exit()
