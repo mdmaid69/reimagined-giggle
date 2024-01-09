ea278fcc4bb2d269f12abb1539a8b70059bbc1bc
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:58:35 +0000

    cry pen react change fat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
