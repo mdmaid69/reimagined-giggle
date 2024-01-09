65bc591c42fa024d3a152b07f79db5e4e7e17322
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:27:14 +0000

    shout record serious cry Ll

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_acceleration(speed, time):
-        return speed / time
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import random
+def roll_die():
+        return random.randint(1, 6)
