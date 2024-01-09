25c5eaaf2bbcdcf3ead50abf4d59a30b825f4c6f
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:21:59 +0000

    top region police quarter put

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def square_number(x):
-        return x**2
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
