8342a58b4dceded8180b4cc7caf93aaf849ba010
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:21:16 +0000

    turnon load else take discuss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import tensorflow as tf
+print(tf.__version__)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
