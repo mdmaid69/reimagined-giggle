2f06e3ecc5f6249f1e8916ed62effd74d433f344
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:09:35 +0000

    matter trial stupid dead frame

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
