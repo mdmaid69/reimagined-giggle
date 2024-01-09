69e4970091051796add51fbb697e017f90a919ac
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:05:09 +0000

    adjective safe adjective little cool

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def set_array_item(array, i, item):
+        array[i] = item
