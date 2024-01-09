d4c68d24d2cbcc0137c1f2dbe144e9c83444d9fb
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:23:04 +0000

    win agree any damage tough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
