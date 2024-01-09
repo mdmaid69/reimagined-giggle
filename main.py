04a5bd7a42684c5256aed5503b83a4392958475a
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:15:40 +0000

    succeed definite include doubt string

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+print(sum(range(10)))
+import datetime
+def get_current_date():
+        return datetime.date.today()
