8a8d8e9ae4c2312cfb1fdce62929f994563bf52f
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:12:25 +0000

    already wish put extremely that

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import platform
-def get_python_version():
-        return platform.python_version()
+import sys
+print(sys.version)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
