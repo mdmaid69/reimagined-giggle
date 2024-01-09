b5c4e541ccc0fee773fea01dcee384d9e0b9945d
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:55:19 +0000

    behind circle scrap along regret

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
