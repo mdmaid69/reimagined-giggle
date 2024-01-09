4318085d24c14dd73b622c2d1ac0458a63ba66e1
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:14:31 +0000

    know traffic wire light drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
