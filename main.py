956e65fe6b33df08248ca46f71ea06108efb8b87
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:24:45 +0000

    quality dress burn successful at

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def get_array_as_bytes(array):
+        return bytes(array)
