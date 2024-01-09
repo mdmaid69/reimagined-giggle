5c7b2fadc73604571072fda212abcc84eb2eb560
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:19:20 +0000

    Nn although case admit side

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_work(force, distance):
-        return force * distance
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_error_function(x):
+        return math.erf(x)
