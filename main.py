352593362b40ddf8ab826269a2d2d009dc799343
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:53:33 +0000

    wind duty invite great wherever

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
