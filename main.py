11ccb653aa2b20719bb9a3d212c58e756b7fd493
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:35:44 +0000

    pride university clothes port they

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_error_function(x):
+        return math.erf(x)
