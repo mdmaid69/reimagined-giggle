a7403112b82817911adb19bb39b43a4f79c09b24
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:39:01 +0000

    speed he boring turnoff guess

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
