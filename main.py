381f41de127f8aea20d2ed44251963828cc241ff
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:47:30 +0000

    572enter talkoutof goon bang heart

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def greet(name):
-        print(f"Hello, {name}!")
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
