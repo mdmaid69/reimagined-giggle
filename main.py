108ce5994852c705c02f325b54af35b60f34d5b9
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:01:20 +0000

    into my poor sign job

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+print("Common elements:", set(list1) & set(list2))
