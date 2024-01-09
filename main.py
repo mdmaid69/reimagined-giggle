773969b5563661c0b92fafa722679fb72161d7cc
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:17:34 +0000

    pause centre town Ll runinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
