add1d5be421a53bdabac2cb19fd325f49832b8ca
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:33:41 +0000

    dangerous work journey his luck

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
