7a1547f3162edb52ae7e4d20574e32d327d385f5
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:51:04 +0000

    accidentally behaviour model draw later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
