f99c34056c27efc08f36f99194d2feb39e6266d7
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:36:43 +0000

    thick behind me relative fierce

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
