323d28621ce73465ec8d84cfda2f258bf49789a2
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:47:41 +0000

    Dd month office dress couple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_current_working_directory():
+        return os.getcwd()
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
