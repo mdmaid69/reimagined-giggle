rich sign can else mistake

main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def divide_numbers(x, y):
+        return x / y