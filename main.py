4b535fb1e269087e8df0c22a0eb08e2326aba22b
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:45:14 +0000

    arrival reliable until weak combination

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import os
-print(os.getcwd())
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
