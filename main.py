33a90ab4edeba22ecb0dc9a2db6330d89ec9ddc3
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:34:47 +0000

    gas bear late neither thanks

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_density(mass, volume):
+        return mass / volume
