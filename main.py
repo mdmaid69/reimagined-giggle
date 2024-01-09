fb91d13bf6af6a1126aa069d76177230d3d85e6e
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:04:08 +0000

    exploration dress unlikely female computer

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 print(sum(range(10)))
