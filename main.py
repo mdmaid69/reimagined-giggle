98cdfb0e51bb70e48134a01d2d047b15e73103e9
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:14:51 +0000

    onto Bb always anyone Qq

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
