604a1bbaa5822b75f40da16e991e2fa8141f4111
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:48:35 +0000

    uncomfortable after lot tie hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_elements(lst):
+        return len(lst)
