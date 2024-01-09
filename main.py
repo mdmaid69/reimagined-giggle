4813db75105f0dd604ffcabf812f25aac2587130
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:56:01 +0000

    attention we fence supply calloff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def divide_numbers(x, y):
-        return x / y
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
