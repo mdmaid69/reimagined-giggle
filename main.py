ae513bc7a6b231eb819a131469c9d6b86d713513
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:59:38 +0000

    comment pour activity agree progress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_sine(x):
+        return math.sin(x)
