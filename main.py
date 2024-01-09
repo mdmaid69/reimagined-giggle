a9a07c406b6fd931774f0aede4348791e2bab0c5
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:40:24 +0000

    powerful weekend square truth cruel

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
