a44a4d92db36d30083a00aff592220ae29ba31ab
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:49:21 +0000

    read coat goback(to) memory side

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-print(sum(range(10)))
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
