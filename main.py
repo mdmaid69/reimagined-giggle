ce99b5380518480c4660c570019f428c45c8356a
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:35:21 +0000

    when cry getout satisfactory clothes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
