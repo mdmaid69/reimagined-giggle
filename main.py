de659417fa8868509535d83f302fa65a5e0423df
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:13:41 +0000

    term second speech number total

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
