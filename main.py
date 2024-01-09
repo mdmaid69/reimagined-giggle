5d1206109846425a7614c562fd794d79ffdb4cc5
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:02:09 +0000

    accommodation order computer thick brief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_power(work, time):
-        return work / time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
