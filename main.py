48b204b3985833c0aaea23c155aae43d5c211d41
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:57:20 +0000

    low push getoff calloff interest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_pressure(force, area):
+        return force / area
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
