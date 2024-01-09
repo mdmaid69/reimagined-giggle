e325a3b031014f5e9026c542f1955e0dc60c1d1d
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:08:18 +0000

    choose note tragedy computer autumn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_hex(n):
-        return hex(n)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
