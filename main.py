5c0441979321c95009ad0540a85b1fe97276824f
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:22:40 +0000

    fold operation issue say shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
