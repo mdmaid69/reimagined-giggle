8d700af7c937672869122c313d70a17f20755f60
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:49:57 +0000

    we callback watch drop attract

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_min(lst):
+        return min(lst)
