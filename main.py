e5b56974f026bae03f77feb608341f1a8c93a5c6
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:03:03 +0000

    tap hide round warm warning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
