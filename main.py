0f271b1709707e5760f060f65e2bc9f75a56cc97
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:19:04 +0000

    either whatever material breakup nerve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
