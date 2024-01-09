8384df05ebe2755fb49c102f84b4ada88e5b846d
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:26:33 +0000

    scrap far murder pocket seriously

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def get_array_length(array):
+        return len(array)
+def count_characters(sentence):
+        return len(sentence)
