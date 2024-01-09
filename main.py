e9f993863acde0416b5c5aeaf06ce190179bb423
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:49:11 +0000

    above OKorokay interruption steep lock

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def add_numbers(a, b):
+        return a + b
