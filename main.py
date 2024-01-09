fd197c476ca7c95589ff9938b448ba957d5ebbfc
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:01:26 +0000

    holdon black relation great home

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def print_python_version():
+        return sys.version
