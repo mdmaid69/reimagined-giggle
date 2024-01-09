2de1ccc9551705ebcde3e5c3708056d8689757a5
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:06:31 +0000

    interview unlikely safety pretty hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
