e1e611cb47082e57130d86f705cf68bcc99ce721
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:40:08 +0000

    cum door summer pure hospital

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def convert_to_hex(n):
+        return hex(n)
