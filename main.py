af108e8cc5c2ea2ccb47776a11354913065a74ca
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:28:54 +0000

    even pin art apparently Ee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
