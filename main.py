a22184c1e56cf61df17d2ad6b3bbdd73a549e168
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:56:37 +0000

    involve makeup convinced note ready

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  def convert_to_octal(n):
+        return oct(n)
