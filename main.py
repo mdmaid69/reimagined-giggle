d88628eabd544a940389eecee4e963a155511bee
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:57:32 +0000

    exact nurse home something worst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import time
+def get_current_time():
+        return time.time()
