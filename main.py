181837a2013f9e07c4f6fe684ac94f2b9864855b
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:02:50 +0000

    yours generally communication change plenty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
