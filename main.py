ffadc922c8d1314acb518e7cca80776dab0ed99e
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:07:04 +0000

    spring protect ashamed manner cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
