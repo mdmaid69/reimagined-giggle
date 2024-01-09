2a4bd464b3f526ea8a8aac9849fff6ef06c2b8c5
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:30:37 +0000

    might round coffee legal present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
