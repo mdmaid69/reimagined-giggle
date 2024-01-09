f240f447ed02d92a25d4a9bfd2e3c8b83dff2155
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:59:59 +0000

    announce Hh tear aware other

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
