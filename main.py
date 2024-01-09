2863998508e38b3cd488b8f185c19f6c7cbd838a
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:18:58 +0000

    control show back callout roof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def remove_duplicates(lst):
+        return list(set(lst))
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_as_float(array):
+        return float(array[0])
