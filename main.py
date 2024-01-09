cd0e17660c716bae34c01274fb3b6b04fe3ca4dd
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:30:24 +0000

    danger fun find unit affect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
