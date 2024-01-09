6e788468739e112af0efbcac4bfd182cf5ac7f60
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:27:11 +0000

    land raise such thin harm

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_acceleration(speed, time):
+        return speed / time
