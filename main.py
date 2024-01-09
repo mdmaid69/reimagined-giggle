8087205195683a1b49b286004af569c3aa35e1ae
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:52:13 +0000

    politics urgent embarrassing skill capital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import array
-def get_string_from_array(array):
+def get_bytes_from_array(array):
         return array.tobytes()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
