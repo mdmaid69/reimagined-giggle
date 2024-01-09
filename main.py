2346497f444b2529bead59c6fdba5465ba0c1d00
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:03:31 +0000

    once holdon live indeed risk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
