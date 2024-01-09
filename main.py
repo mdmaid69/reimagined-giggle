9cf592f62bed418c248c30f67da94d84c9eea779
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:36:08 +0000

    further below attract comeup nice

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
