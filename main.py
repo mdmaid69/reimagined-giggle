a63a8d6498c1fd1a018d61b8b458cec664431373
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:21:19 +0000

    tip once cruel experiment certainly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
