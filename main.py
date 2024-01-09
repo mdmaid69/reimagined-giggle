6fb879db4b1119254f5b7eeda6dce5faca204e22
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:26:35 +0000

    frame ashamed how commit live

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def count_elements(lst):
+        return len(lst)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
