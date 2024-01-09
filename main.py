cb49592e1ac90cf07a256df8a80cc9cec5c3cfa1
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:17:50 +0000

    big earth wall confused telephone

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
