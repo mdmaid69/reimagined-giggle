a0a4ba361bc634819f4dbfc5e2ce4017eab603c2
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:27:26 +0000

    guess Nn pretty community answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
