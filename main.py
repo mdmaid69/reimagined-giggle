1aa47775fbc8d8d499eae674bc6c0c627f8e903e
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:58:40 +0000

    terrible transparent art case bye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
