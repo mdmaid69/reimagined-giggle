23d791cadc86dd9a64af34a5dd5e8739d6426769
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:56:45 +0000

    forever interesting cap national date

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
