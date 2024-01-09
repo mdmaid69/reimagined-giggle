23a8817b3219fa04c6fc6bbe7339fbfd8cbe59e9
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:27:02 +0000

    thorough for curious appearance image

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def square_number(x):
+        return x**2
