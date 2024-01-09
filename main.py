e9c42bc73bc970ed4487dd301e8ad86d313558cb
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:29:58 +0000

    sign zero night prefer behind

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import os
+print(os.getcwd())
