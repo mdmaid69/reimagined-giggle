2bf0cbd4979e5767bb9b6c7c24b4651b63e5d07e
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:19:39 +0000

    maintain payment take shopping add

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
