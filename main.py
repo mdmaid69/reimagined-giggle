00c185d99007a793720c8201f3d5ff303b584461
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:52:42 +0000

    will still disgusting corner badly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
