87bbab4e98a0648d2f1f199c6781effc8184c2b5
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:20:30 +0000

    tough confident wipe check straight

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
