bc3f867db78da381da167aee98a2ef9c2a379467
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:41:12 +0000

    authority anxious knowledge train progress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_base_name(path):
+        return os.path.basename(path)
