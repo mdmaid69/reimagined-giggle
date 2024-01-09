15dbe9418605344ca21ba930a1131d88039b2920
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:27:18 +0000

    question attach object base crash

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+print([x**2 for x in range(10)])
