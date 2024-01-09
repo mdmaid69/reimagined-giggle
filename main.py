e615a4f5e1970f84edc2ddb2907f41ed98e3c788
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:33:10 +0000

    importance mail room aim lesson

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-name = "Python"
-print("Hello,", name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
