1119b5a8dd299cffe2ba1225137103ff81301ab6
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:02:05 +0000

    normal before noisy obtain no

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_acceleration(speed, time):
-        return speed / time
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+text = "Hello, world!"
+print("Words:", len(text.split()))
