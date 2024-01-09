dd8c2ea568c9ee0bcc977b8dc32dcdc5de39c77c
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:22:27 +0000

    take win swing adjective pick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
