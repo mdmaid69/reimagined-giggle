04282c5ab25184c5d729e61c40abd9140e2a74da
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:59:37 +0000

    so off punishment public lookafter

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
