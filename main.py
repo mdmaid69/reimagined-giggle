b4b1af9f033a02ddb0b61ac84cdf83dd0d5433d1
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:30:18 +0000

    error admit gentle past emergency

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
