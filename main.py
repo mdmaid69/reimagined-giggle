6b702650b7c00ba0ce0111bdbecfc81cb01453e2
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:46:16 +0000

    arm home list birthday smell

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
