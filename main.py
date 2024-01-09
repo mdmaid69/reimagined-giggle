c0a29dd0d270286a8e239e113627afcb03e3161f
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:47:51 +0000

    mine condition geton negative priority

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+print(math.pi)
