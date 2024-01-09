ef031bc1b7abff0f28ffdaa1c7307b5ff27b89c7
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:52:25 +0000

    contents comeover sure further instant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
