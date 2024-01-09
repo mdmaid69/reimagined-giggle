f24621432ab665c4fe5916741976fbc1c0603887
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:26:34 +0000

    mine father familiar centimetre unable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
