aafcd53cfc052e460598d9531cc44c2960cbfd73
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:34:44 +0000

    exercise Bb catch about present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
