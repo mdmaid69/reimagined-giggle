c0b8aa7d9c2fb0ad5b4c0e48fe2672c4b4c7100b
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:59:02 +0000

    characteristic shout maintain lay push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_arc_sine(x):
+        return math.asin(x)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
