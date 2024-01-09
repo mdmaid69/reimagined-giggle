76b5b275168c7a0df29589bb71b56070d51b64e2
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:49:39 +0000

    bright light sound challenge tie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import time
+def get_current_time():
+        return time.time()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
