0b4ae10f75c65516fc353499074c07938646ca67
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:13:31 +0000

    question tooth lookaround lock boil

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
