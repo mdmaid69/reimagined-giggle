61f95a739de170d49aa49332783ee1f20503deff
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:51:58 +0000

    star entry callat change danger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
