d958405eb72753fbd6273830e406bf64a653d542
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:05:19 +0000

    runover identify congratulate reject practice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
