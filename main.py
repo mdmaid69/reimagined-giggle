0777f18b7977f5ce164478742d6ad659b40d48a5
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:44:23 +0000

    intend getout several hate dress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
