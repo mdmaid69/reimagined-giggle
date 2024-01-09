f86a6654b3800f7f5a9ea34da9b149f61eaa3200
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:29:04 +0000

    achievement roll but pride temper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
