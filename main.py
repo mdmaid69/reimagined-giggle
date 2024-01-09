ca62f79ea8b772f0de8160c4c88e66686909e859
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:34:21 +0000

    creature slow band empty much

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
