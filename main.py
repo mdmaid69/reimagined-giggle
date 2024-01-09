3e669de78965f65efe9c95ed1b1b0fd46d5a9422
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:52:12 +0000

    material follow upstairs crowded wander

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
