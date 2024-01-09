df725217eeb5d933315c59e3dcee660b2d1f159f
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:13:04 +0000

    distance sweep surprise fault contain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def calculate_work(force, distance):
+        return force * distance
