6ae31b7baa5415684eb79e4a15b47004269ad693
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:09:50 +0000

    pencil getup on trip scrap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def split_path(path):
+        return os.path.split(path)
