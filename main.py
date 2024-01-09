d459ce3218032b5ba1dc536994830baa4f7634d4
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:58:40 +0000

    grand matter thinkover touch thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
