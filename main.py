646ee3d7cf48750b74b4ea32435969ba7e0f3601
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:11:57 +0000

    goback(to) contain smile meet correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
