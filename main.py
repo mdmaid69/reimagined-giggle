48e60f977102aaf8b209b2a5251e49c9d7e90e50
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:50:02 +0000

    dear interesting side swing hurt

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  def remove_duplicates(lst):
+        return list(set(lst))
