7662c621a6f88bf8573ed1a79eb4c84f18f7b35f
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:23:58 +0000

    be spring strong door behave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
