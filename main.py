11013c9712eb9d5245bb7bdc9d7b997e144567a1
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:22:16 +0000

    sweet concerned mood dead sweet

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_area_rectangle(l, w):
+        return l * w
