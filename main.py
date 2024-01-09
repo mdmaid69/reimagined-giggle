15455e8b670bd2a0a93e20eedd8700c90a5da7fe
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:08:53 +0000

    remark load wire second welcome

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import array
+def get_array_as_list(array):
+        return list(array)
