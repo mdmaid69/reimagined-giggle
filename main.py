c42af8fcd80199afe9df4294d782903e9d5a4f32
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:40:14 +0000

    indeed hurt guilty Yy destroy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
