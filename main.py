123c4e166972d88096f97ce9f9702bef1e03c8df
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:38:34 +0000

    morning high Oo behaviour quarter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_as_bool(array):
+        return bool(array)
