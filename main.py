bbae8855e763ad8b33b16c736578bc2f8c7f5aee
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:35:39 +0000

    actual whistle appear mass purpose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
