6ad073abebb541b42cea5520cd77aa5eba5a5730
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:13:40 +0000

    previous detail report mystery discovery

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import platform
+def get_python_version():
+        return platform.python_version()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
