4a559191eabb9fc446aa5f715808cee4625bf1ee
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:56:40 +0000

    interview thirsty coat about say

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_work(force, distance):
+        return force * distance
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
