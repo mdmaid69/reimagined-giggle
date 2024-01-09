9a7025f2c92ae09e0da6680946f730af304bc8f2
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:45:42 +0000

    separate change alive eye fair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
