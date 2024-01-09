06e6ff90b696a0bf2214aef53ca9d8aea057132f
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:31:35 +0000

    keepup error use practice road

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def count_words(sentence):
+        return len(sentence.split())
+import time
+def get_time_since_epoch():
+        return time.time()
