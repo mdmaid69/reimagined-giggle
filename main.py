e3df61a0104764731bed955ad32792eea05aa269
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:24:20 +0000

    sometimes band fold switch goback(to)

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_force(mass, acceleration):
+        return mass * acceleration
