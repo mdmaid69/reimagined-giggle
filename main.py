8c1c5b4a3cda7f864021c23c2f00b661d1fe67fb
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:22:55 +0000

    loss sudden minute book unable

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
