176f6cadb85d6c2729befcd4d062acac42504e35
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:23:44 +0000

    may naturally improve personal confidence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def sort_list(lst):
+        return sorted(lst)
