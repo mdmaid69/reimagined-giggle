18b693185b7f325524e9c263e654cae19766abb0
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:55:54 +0000

    announce service confused sometimes edge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
