d66d3452c6e8d56360a41857d108f96d3b935d5b
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:42:25 +0000

    572enter mend around lookback(on) usually

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def clear_array(array):
-        array *= 0
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
