908ceb0c2a182c20116b7e6dc18e2e06d8b03afe
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:23:48 +0000

    cut grand butter back raw

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def subtract_numbers(x, y):
+        return x - y
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
