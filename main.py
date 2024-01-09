d19192ea45be73f6e487c4a0eb9a90d5106de2b8
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:24:23 +0000

    day another shoulder abroad between

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def multiply_numbers(x, y):
-        return x * y
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
