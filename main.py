495c247ec44d83ef7fb95d58d7968ee835f45741
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:38:02 +0000

    into condition close but train

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+text = "Hello, world!"
+print("Uppercase:", text.upper())
