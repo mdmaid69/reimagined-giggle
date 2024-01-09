17e1f09389b82941202e7c1a417a7160a11297ed
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:47:27 +0000

    disapprove trust turnoff warn spin

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
