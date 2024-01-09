0099485d30832d5ea68575bb748206dbbfb59d69
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:40:57 +0000

    relief argument still peaceful breakin

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
