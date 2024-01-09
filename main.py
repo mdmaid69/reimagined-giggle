0f63e9992826f603bf40a442d77cde1cc8dde950
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:29:56 +0000

    light bend pen ear label

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_volume(length, width, height):
+        return length * width * height
+def calculate_pressure(force, area):
+        return force / area
