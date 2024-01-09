4c1a2f6efe3170d47ab66863ff88fa256a3a7212
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:26:44 +0000

    very opportunity cheque care sticky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
