eb4cca4465edad17e1549ee910893eb6bba6a0a3
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:59:14 +0000

    cap slight spread wind unit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_octal(n):
+        return oct(n)
