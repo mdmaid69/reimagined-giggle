d4dbb3744563ba7411d2007a2ab670256a505a37
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:59:59 +0000

    ashamed space present makeup part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
