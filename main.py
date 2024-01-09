3070e6e2e47ee1a4196b775751289220dc954377
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:52:47 +0000

    scrap century comment can intend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
