16106ff0ec5521cbdf0fa8e541261b6e485e6d17
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:49:00 +0000

    bush between turn suck travel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def is_odd(n):
+        return n % 2 != 0
