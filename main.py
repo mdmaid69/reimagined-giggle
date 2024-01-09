9fda3557c09f33b0ebe56f9748c336be83a40635
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:14:09 +0000

    possibly next lean post recently

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
