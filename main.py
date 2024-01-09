aa93703355e92e4aff3a7936e02ffd2c4c5a737c
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:40:57 +0000

    snow order loan pretty bus

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def reverse_list(lst):
+        return lst[::-1]
+import sys
+def exit_program():
+        sys.exit()
