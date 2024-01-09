e896033326ba2adbdebc8b57ae2e80d80b77dfe9
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:43:28 +0000

    ring firm hit none rid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
