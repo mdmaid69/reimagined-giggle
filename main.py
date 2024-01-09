b808ab8f524534b8e8bf0a462918fc15b7a4605c
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:30:02 +0000

    quantity with earth centre sink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import re
+print(re.match("h.*o", "hello world"))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
