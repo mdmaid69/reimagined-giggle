3db6ced6fc436c1a5c98e3f8b9f0ad912b0c6aa0
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:46:31 +0000

    react people ought lose 572enter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
