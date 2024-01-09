b008181042a5d7d2bc0e2045f75ebbc2e052ee0f
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:32:48 +0000

    exchange near turnover protect clever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import numpy as np
+print(np.array([1, 2, 3]))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
