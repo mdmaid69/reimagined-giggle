2f3f1dea1c34f980cff8014d3b14475f4a97934d
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:11:25 +0000

    discovery claim typical everybody herself

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import sys
-print(sys.version)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
