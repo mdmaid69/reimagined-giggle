3c784effb194599c924513a8f55e62f1d4a9ac0f
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:26:02 +0000

    hungry protection cover charge lesson

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
