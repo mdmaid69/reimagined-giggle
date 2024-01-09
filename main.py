6c0396f101c0b804119ef5642a7df585f5c35359
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:24:40 +0000

    call face call open border

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
