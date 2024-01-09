08fcdcfb034a798b8c3817eeb847f3f2d1f0886f
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:05:47 +0000

    drawing spread store pick run

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
