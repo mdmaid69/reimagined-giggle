514ae96da72a94848797446ec0719506e2b2d589
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:41:13 +0000

    run grateful stop ask front

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
