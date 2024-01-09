fd6dd2d26be65dab1f709d8ac525d48a5f3c9dcd
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:46:01 +0000

    cut everyone content forward later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
