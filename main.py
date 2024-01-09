e72c13d67fcf84778b64f96ed43496535223b0ae
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:05:26 +0000

    increase food expensive taste limit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
