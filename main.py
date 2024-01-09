0f4b42360661bcc3708937211747c2b40431ec76
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:12:06 +0000

    big fond meanwhile pause couple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
