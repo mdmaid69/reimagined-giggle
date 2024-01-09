16060fb33ff9c5214b2de30f47bc45b55eff674d
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:49:11 +0000

    reduce heat team emergency face

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
