48ffae7a1674487a0f5a3aa63bd9d21a68d0590c
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:02:51 +0000

    can spring touch cry neither

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-for i in range(5):
-        print(i)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
