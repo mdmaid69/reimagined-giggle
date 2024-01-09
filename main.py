9e2c5e4769231001e390870bc3855f293d7472e7
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:17:41 +0000

    company old persuade supply tough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
