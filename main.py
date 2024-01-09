1ca3f7b1d53432234d9cf103fc245b4ee50b3704
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:03:29 +0000

    birthday balance exact house basically

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
