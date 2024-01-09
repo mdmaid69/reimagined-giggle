b60bbf8539399dde5f9d6195c1d6f01a4df78ffe
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:40:59 +0000

    sport traffic performance mind hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
