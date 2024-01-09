7f12456a515da0efdc14c523883b7c18af3d2af2
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:12:06 +0000

    natural fit home free him

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def convert_to_octal(n):
+        return oct(n)
