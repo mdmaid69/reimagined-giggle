d700a228e2853ecac84321755a47e62b0338ab24
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:39:03 +0000

    cheat door find nervous discover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import array
+def get_array_typecode(array):
+        return array.typecode
