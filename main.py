3c8d9e830eff98127f9014ed89c25024db331c5d
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:47:04 +0000

    rain in brush cost whistle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  def cube_number(x):
-        return x**3
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
