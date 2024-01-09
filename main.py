4e83bb5ff61086d8997c0ec13bb6b94f24a3822b
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:44:35 +0000

    suddenly exercise sudden shadow everything

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_max(numbers):
+        return max(numbers)
