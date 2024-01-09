bacc2569684c19807bd24f61916ecf485e1742c1
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:32:53 +0000

    scheme prepare cough crime formal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
