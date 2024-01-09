14db88732aa529fd9c9aa4d756ceec8bb5d743a0
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:37:56 +0000

    exchange obvious stop alive container

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
