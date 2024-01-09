54b9f7e78117a32ab3c05e8142f982ca7d215403
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:51:54 +0000

    hide emotion lead pride have

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
