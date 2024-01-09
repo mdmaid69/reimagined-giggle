aa8526f2b65c305f6015ab5eed33ba00bb600fb8
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:52:51 +0000

    compare trousers male mention scene

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
