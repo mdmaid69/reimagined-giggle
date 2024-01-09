fd23b0a3aae6250d1242d42638ebcdd734660c83
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:06:50 +0000

    real would comeback report fortunate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def multiply_numbers(x, y):
+        return x * y
