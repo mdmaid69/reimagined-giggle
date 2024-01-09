ec24cbf56e859360d9a4d3b3349bbdc1e92bd933
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:11:42 +0000

    shadow only Hh well broad

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_list(array):
+        return list(array)
