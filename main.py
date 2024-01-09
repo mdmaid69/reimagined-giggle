a0edea8e3d121f4dc073080933a8cbc4f38ab7dc
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:18:59 +0000

    raise determined risk cap upstairs

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
