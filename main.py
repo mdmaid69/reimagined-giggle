995e78f9a1382ef6d0d1f1edf74d9823e1076381
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:45:38 +0000

    window admit accept please spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
