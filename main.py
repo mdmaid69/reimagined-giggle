b7b45f9b9f4924c80174b88c7f9f1368ee5d4170
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:58:05 +0000

    cinema wild dare acceptable letter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def delete_file(file_name):
+        os.remove(file_name)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
