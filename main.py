70441a3021cd3d5c1ab321a3f5d147a3be4f7649
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:28:30 +0000

    undo last hole giveaway question

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def multiply_numbers(x, y):
-        return x * y
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def sort_list(lst):
+        return sorted(lst)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
