da799227b738fd131a3fc0bead902e49bca97be2
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:21:07 +0000

    criticism then approval smile low

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def split_path(path):
+        return os.path.split(path)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
