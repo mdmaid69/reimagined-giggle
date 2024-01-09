ac3f4288475ddb2bbe3ffa06963b19db64e912c1
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:10:07 +0000

    smell recognize jump giveback warning

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def count_characters(sentence):
+        return len(sentence)
