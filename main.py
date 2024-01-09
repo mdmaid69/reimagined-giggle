d1f65d599d43dab8a4b300384468373e28a30611
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:59:29 +0000

    free team level certainly ourselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
