59ee625a11444d9608288e64abf9843566dff909
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:41:44 +0000

    task container drink much serious

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def is_odd(n):
+        return n % 2 != 0
