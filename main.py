09683a0292469e893eb8c112665e186dbd04dbc6
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:01:49 +0000

    fact circle storm chat wing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
