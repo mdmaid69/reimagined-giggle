5aad9ac33ffc7593d0aa996bdfd28ce684f2bb06
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:37:00 +0000

    breathe only difficult measure off

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
