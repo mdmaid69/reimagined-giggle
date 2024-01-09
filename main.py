5a45aeaf146c7af422514b2be9c1d56f13103640
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:26:59 +0000

    danger admire card idea note

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
