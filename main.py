5ebc6eba6530ed89617a0496fafc3ed6de8922f2
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:29:32 +0000

    warm challenge surprise scrap this

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def add_numbers(x, y):
-        return x + y
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
