37b4cc6990db511a49eb4541ebe8437d2dbcd440
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:46:16 +0000

    hide practise hospital tear theirs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def count_elements(lst):
+        return len(lst)
+import getpass
+def get_username():
+        return getpass.getuser()
