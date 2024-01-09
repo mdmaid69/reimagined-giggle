701bb20d43acdca53322030282514d264edd1550
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:17:42 +0000

    custom warm trade that tie

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+for i in range(10): print(i)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
