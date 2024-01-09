0d4dcf75723b081f6e7ce69c871810a4c6a9ee1d
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:55:02 +0000

    rise takeoff invitation adjective runinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
