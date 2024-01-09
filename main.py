ff5dd88838a31862646fb73fc7208d624f342b20
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:42:42 +0000

    level tear now law spoil

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import random
+def roll_die():
+        return random.randint(1, 6)
