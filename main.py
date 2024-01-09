6d55aa7487df5a2615e3e5e481fe624d2b28eb48
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:53:56 +0000

    strange preparation high nature patience

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def count_words(sentence):
-        return len(sentence.split())
+def get_array_as_set(array):
+        return set(array)
