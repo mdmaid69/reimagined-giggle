4bd51c56900d7b2811615d113f32bc1542962205
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:09:45 +0000

    ignore lean wrong after greeting

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 def calculate_pressure(force, area):
         return force / area
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
