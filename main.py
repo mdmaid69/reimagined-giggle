5381ae376c1f26c802347a6c7969cf01cceb1091
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:44:36 +0000

    dry start not mother contribute

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def remove_from_array(array, item):
+        array.remove(item)
