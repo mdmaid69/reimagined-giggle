d6ac81a92d579865aa504d46eb6bed32f336c2a9
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:52:12 +0000

    obvious aim relative handle lay

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-def subtract_numbers(x, y):
-        return x - y
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
