3a8f6c7109aa3f0d219eec2a6db9366faba24f2d
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:05:01 +0000

    personal score print she prepare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
