d0e64776533befb5036a45408a28a08f85195108
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:33:18 +0000

    square amount empty sign shame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def remove_duplicates(lst):
+        return list(set(lst))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
