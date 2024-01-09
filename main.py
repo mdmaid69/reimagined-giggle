95eb5ac5caf037140586c50dae8f0d267eef37ae
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:02:17 +0000

    crime pot natural watch declaration

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def sort_list(lst):
+        return sorted(lst)
