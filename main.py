c8f944d941dfba0e55aeea1083625faac7e67d87
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:14:17 +0000

    workon value climb expensive would

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
