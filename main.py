182d2b8b26aef1daccaa79817a97f3f797c1a426
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:42:51 +0000

    ask sharp relation busy responsible

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
