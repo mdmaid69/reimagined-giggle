d198442487ed76a380fd2fd7a60f1e5f9c1f18e5
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:39:31 +0000

    direct refer act drop last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def cube_number(x):
+        return x**3
