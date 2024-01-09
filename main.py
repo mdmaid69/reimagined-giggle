bdd85fe35f2ece7e115af59fbc5549f10bb61376
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:10:11 +0000

    sting pressure actually through law

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import array
+def append_to_array(array, item):
+        array.append(item)
