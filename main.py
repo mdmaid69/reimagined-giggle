f8d528efeaba8f95e70defbdf51a6c31565773b7
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:34:16 +0000

    further able hour accept before

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def sort_numbers(numbers):
+        return sorted(numbers)
