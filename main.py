39dd2173f1d00c9515e3933fe2b037192af2a244
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:28:15 +0000

    impression manage mystery pattern block

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_odd(n):
+        return n % 2 != 0
