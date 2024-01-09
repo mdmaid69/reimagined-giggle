ce77c1744d37b991a6c2d4576630ee177ef787b5
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:42:00 +0000

    fault bother club age back

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
