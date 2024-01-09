73fff004eaf29c90d34a5d9af897919d9383627f
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:06:16 +0000

    correct thought test near pleasure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
