5defd19fa1c9297d8dd6e82cfa0c667a52d608be
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:55:58 +0000

    warning key dig goon hour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
