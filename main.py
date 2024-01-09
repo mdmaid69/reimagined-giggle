705312c4a3db62c94246d8a34fc7bef483e6b0ff
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:46:06 +0000

    warn bound sting beat extra

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_directory_name(path):
+        return os.path.dirname(path)
