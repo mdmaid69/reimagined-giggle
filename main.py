938821b2aafc7d59fca325ee97b687361bd0b997
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:05:58 +0000

    you punish relation health accidental

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
