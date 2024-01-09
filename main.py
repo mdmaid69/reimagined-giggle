123ebe8106dcac30bb78c4bf5eaafddde3452829
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:16:09 +0000

    behind heart fortunate container maybe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def find_union(list1, list2):
         return set(list1) | set(list2)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
