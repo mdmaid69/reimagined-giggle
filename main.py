be5532fdfb28cee3a975bf9822252eac1afbdfa8
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:20:47 +0000

    murder runinto doctor breath one

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-text = "Hello, world!"
-print("Characters:", len(text))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
