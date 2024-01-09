3cde90be1266b5d492aae5c2911e5d59d5c863bc
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:08:48 +0000

    watch phone everyone any all

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
