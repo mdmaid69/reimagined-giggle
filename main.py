22eb06863e829c2cbed1ade08c146b8b2a8498ab
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:30:36 +0000

    win culture freedom accidentally shake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
