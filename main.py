c59450bc20be091314b11aaddebb70371a1660ea
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:08:32 +0000

    month when square they protect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
