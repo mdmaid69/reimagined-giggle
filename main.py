3e3921d233bb7653045eb0e00013d8c8038e438a
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:11:35 +0000

    temper separate address total goodbye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_pressure(force, area):
+        return force / area
