a5a5f9ea4eee462f5a376eed7d69fa1494ce96cc
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:18:21 +0000

    drive rank treatment belong emotion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
