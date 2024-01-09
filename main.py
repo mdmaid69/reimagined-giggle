a5a9efe4fe2da9f685d94b900b5dbee91b600bf4
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:41:15 +0000

    frightened explanation try unable weigh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
