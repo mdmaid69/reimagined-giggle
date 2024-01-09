d79c537284500eb35cc8a1f7beab0b9609a6303b
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:19:53 +0000

    upwards needle act ear progress

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import sys
+  def get_python_version():
+        return sys.version
