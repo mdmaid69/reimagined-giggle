ad454ef143c1bc77ad86bd00db6296d36d245359
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:40:01 +0000

    takeaway popular swing good ours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
