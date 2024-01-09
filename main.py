a2b64c0b54295500257fbafc5699ec8d314de9c8
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:11:30 +0000

    reserve dare tour frequent modern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
