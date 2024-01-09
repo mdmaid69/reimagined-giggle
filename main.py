fedc2d885279a7a77669a8ba35b58be44b6ca770
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:59:22 +0000

    further downstairs sweep noone keep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def is_even(n):
+        return n % 2 == 0
 import json
-print(json.dumps({"name": "John", "age": 30}))
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
