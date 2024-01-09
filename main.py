331e1fa1a06034e8a82410c12a9093cd3c7553a8
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:13:16 +0000

    uncertain settle urgent rush impressed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def find_min(lst):
-        return min(lst)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
