a4b5f83cd7fdc24ca24d23ae4fe8b692942f6760
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:28:47 +0000

    improvement threaten reason care rub

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
