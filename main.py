f65818c606cc674e5d9341f7e34174e677d8b368
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:11:07 +0000

    call reject race company female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
