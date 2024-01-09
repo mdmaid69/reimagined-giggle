08b3357524e418e0fc01d8fdcb73708561085e46
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:11:50 +0000

    science thorough scare watch period

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-def add_numbers(a, b):
-        return a + b
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
