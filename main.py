c71d761b5b305522fe9d64e8c5f2fdfceeba387d
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:43:01 +0000

    visit takeoff measure match tap

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
