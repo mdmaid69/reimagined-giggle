c32c3afa1ab8534009ed72f5728bc894dedb55c3
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:48:51 +0000

    smile news division tight shop

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
