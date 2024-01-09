34df29c665f38353c70aa168c68414020c2611a1
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:43:48 +0000

    condition bear excited display stage

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
