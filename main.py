e6ac89cddfdc0cff5c7329baa63ffe0341f60ebd
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:59:41 +0000

    disgusting right complain persuade land

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_pressure(force, area):
+        return force / area
