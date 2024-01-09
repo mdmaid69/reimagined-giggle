e309fa4ec574d86d6ceb8b0fd2300acb24a55807
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:08:39 +0000

    class brush actual phone lay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
