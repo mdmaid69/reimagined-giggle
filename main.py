f1d894cd7d59447df2971c237a824f28b4323ff7
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:20:52 +0000

    open for pick minimum table

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import platform
-def get_python_version():
-        return platform.python_version()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
