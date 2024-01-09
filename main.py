24c3841ad8068babaa6d0a69cff6cffd5a5433fd
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:16:19 +0000

    mad idea various scare below

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
