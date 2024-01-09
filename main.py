c13ce587f95938001edca0793f4bc77f77937226
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:39:36 +0000

    style Ff sound port university

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import time
+def get_current_time():
+        return time.ctime()
