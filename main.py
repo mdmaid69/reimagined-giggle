1dc2fcd6605b5ab20e458bdea69ccc5b3b9ddcec
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:52:37 +0000

    spread you mistake family choose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def is_palindrome(s):
-        return s == s[::-1]
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
