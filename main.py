9743113efe70392aa2ba46c7190d13a3a2d9babd
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:01:41 +0000

    breakout agreement talkoutof completely invitation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
