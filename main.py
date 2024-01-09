cabfc1a58921d76945f951740ce510c9532772b3
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:49:06 +0000

    sticky meal yet used downstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
