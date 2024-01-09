c880e0e6d83c190acce8c475462902ec2620bed5
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:43:22 +0000

    exhibition secret list ability rich

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
