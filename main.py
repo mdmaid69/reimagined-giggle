27a27c8f51a446194bd855b85a97e4d00d2e3a66
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:27:36 +0000

    music independent compete find and

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def append_to_array(array, item):
+        array.append(item)
+import platform
+def get_python_version():
+        return platform.python_version()
