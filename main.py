a3175d6cdbb975034e534094df816caa29bf14ac
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:24:33 +0000

    cost save touch water note

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
