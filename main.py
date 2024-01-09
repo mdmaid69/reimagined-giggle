87685bc0fad87fa1b64596f03e7f69f7039ad326
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:08:38 +0000

    pool their brush knock forgive

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def convert_to_binary(n):
+        return bin(n)
