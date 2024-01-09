a4eb834f91aaee3de58c0b393d3c44f3cdf78b70
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:46:58 +0000

    active knowledge bet tragedy rest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import collections
+def create_user_string():
+        return collections.UserString()
