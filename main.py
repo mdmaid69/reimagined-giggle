706de659f5c031429970cf11521a0ae1587896b1
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:41:52 +0000

    forget effort importance print lookup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import tensorflow as tf
+print(tf.__version__)
