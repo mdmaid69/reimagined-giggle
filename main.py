9bac299c64548f02d6b98f8531e7b150d740a183
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:24:03 +0000

    drawing generous sail goaway wait

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def convert_to_hex(n):
+        return hex(n)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
