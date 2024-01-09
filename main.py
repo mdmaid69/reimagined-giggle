c0f9c8a7b565ab6d420ebc4e4fdea79f039e3149
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:58:40 +0000

    picture pass slightly its interrupt

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
