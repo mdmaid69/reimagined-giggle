28243dbf7cad604d57a337b080afc6f9cd732ff2
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:21:17 +0000

    charge partner business turndown mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
