c784a77271829677821b52798f8a45e857f65f10
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:46:04 +0000

    debt anywhere mass normal dangerous

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
