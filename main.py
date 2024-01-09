e86150ecb8badcaed1a625c562f647892417f749
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:53:11 +0000

    blow float direct connect furniture

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
