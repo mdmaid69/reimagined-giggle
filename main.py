f61c2ecf3aac4371a179f2f9d089289f68d4fc64
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:21:40 +0000

    centre getaway Uu breath great

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def get_array_as_set(array):
+        return set(array)
