f0ef2d897a34a207b874024e1907277d41fc9d14
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:56:07 +0000

    tap strong plenty cruelty inform

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
