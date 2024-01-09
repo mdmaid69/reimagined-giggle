ad437da885f9a53bde48f490c0e0c7e6779f6c46
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:23:27 +0000

    estimate tap black bank breakin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import random
-print(random.randint(0, 100))
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
