08a38e6bc9236797c50393fc9e57e5e3409d5431
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:36:18 +0000

    act dare careless proof embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def get_array_length(array):
+        return len(array)
