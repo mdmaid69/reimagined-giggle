0ad7660f0b292da67851378917d865653be51d4f
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:22:12 +0000

    connection character say miss loose

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
