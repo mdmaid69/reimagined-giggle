3bcfc73eef7e26b9f85ef766bf059897bff62c2c
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:45:33 +0000

    trap likely worst present to

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
