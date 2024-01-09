5bf5c87490b31233046ffb1c1169663b77abe3a3
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:33:54 +0000

    real boil uniform recent everywhere

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
