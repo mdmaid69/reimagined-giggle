814fb1b5cf156c3724b85c38916bcdf68dff32c8
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:31:47 +0000

    pleased through regularly guess promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
