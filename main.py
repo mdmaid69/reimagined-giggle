496a97975f51efa7ab12e20bd6f5409b2061baf3
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:34:24 +0000

    worried wave lay thanks funny

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
