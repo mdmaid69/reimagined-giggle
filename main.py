9f53088481ff9a42c5c85f53ab1d48af6b88b25c
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:00:37 +0000

    demand hard turn shopping impressive

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+print(sum(range(10)))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
