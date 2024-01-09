e3b0a00f5e974235a2cd62e3539e7b78ce3f7921
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:40:57 +0000

    whistle difference warn not tape

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-x = 10
-y = 20
-print("Sum:", x + y)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
