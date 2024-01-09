e1b79867add26e07e5740226dff5c1eafe85413d
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:56:13 +0000

    fun let rare suggest and

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_density(mass, volume):
+        return mass / volume
