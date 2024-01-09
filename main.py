b13d8c102e76ee8c09e8f8a5ec40834bb81bfaba
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:26:49 +0000

    really keepup usual must good

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import datetime
+print(datetime.datetime.now())
