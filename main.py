96479b159b2cec4ad8a09adce7b7ce485bbfae5d
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:47:33 +0000

    anybody coat record simple education

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
