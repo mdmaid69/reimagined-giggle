79233eb573f70db5885eb85af4070e887788f190
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:00:38 +0000

    myself tune sister summer acceptable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
