2005e5d04b4878dda79a6ccccc0aaed3aef2d074
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:13:31 +0000

    lead any contact gooff but

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_square_root(x):
+        return math.sqrt(x)
