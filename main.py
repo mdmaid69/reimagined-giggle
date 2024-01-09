079b72bc84e1d28a143a9cbbc598db45797c8c08
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:51:22 +0000

    nice practice tonight importance previous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
