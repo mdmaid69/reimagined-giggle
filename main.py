54a27c5b5f2aca479b3afb4ebb023443b14c6256
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:24:58 +0000

    one pay little right some

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
