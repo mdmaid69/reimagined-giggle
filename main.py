3c6e8b8bc7c407fc4bf56e6f3fb41242dbd53e96
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:00:12 +0000

    still regional abroad history 571enough

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
