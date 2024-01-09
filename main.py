55ef032be47ad2cc341eae844810d17fdae8a708
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:17:09 +0000

    male capital forgive Yy maybe

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
