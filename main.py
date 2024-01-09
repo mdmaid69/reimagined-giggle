76b5def8f09e75d7972db06ce2f1489500b65826
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:22:37 +0000

    out opposite science cross doubt

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
