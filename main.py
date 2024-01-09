33643b706843ded63ddd94195554bbd1b8ae993f
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:45:08 +0000

    upstairs watch genuine strength usual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
