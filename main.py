39d1fa2f60f1b2ab68883deea1970f4581c345cc
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:33:06 +0000

    unfortunate disappointed lower fairly earn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
