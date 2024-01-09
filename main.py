de2b910a0a09f2db8b3c3b1725611fca53770121
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:59:17 +0000

    giveaway see poison comment hair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
