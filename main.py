cc070823019b2760a947a44a4c5c4f3f77f8bba1
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:58:47 +0000

    exam together vanish full delicate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-text = "Hello, world!"
-print("Characters:", len(text))
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
