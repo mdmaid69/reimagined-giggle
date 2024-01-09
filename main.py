80eef743cf470c3f853a0db49b0dd659a5d436b5
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:00:45 +0000

    forward near interested average guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
