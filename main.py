6976137e57da30bb58fc01172539b7b7b5246a3d
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:09:54 +0000

    bet concentration only where jump

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_power(work, time):
+        return work / time
+for i in range(5):
+        print(i)
