375b886aa18340964e8e8aaeffc3aa88162d0ecb
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:51:03 +0000

    contrast public prize slice downstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
