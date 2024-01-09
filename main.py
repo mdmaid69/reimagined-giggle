416bf0313c61c83ebb3bb62059cd8eaa57080107
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:23:56 +0000

    yours certainly interruption tap thankyou

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
