fd57aaad230706ce7730df6385982c17804ecfb8
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:03:16 +0000

    will somewhere throw manner autumn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
