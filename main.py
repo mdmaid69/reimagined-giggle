4e9e68e589a0c84296b0ab915e0ce2ccdfc10164
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:33:28 +0000

    everything willing top take theirs

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import random
-print(random.randint(0, 100))
+import math
+print(math.pi)
+  def subtract_numbers(x, y):
+        return x - y
