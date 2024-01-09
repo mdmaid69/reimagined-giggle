ab6b87e3fce8203c26df732ed994b7f6b7528be0
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:21:35 +0000

    loss beneath carry whistle door

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+print([x**2 for x in range(10)])
