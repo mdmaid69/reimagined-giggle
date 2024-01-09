ed6cf0e7c3ad8ea33661a258c6e8d1b8ebbd8895
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:45:15 +0000

    clue belong gap front introduction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
