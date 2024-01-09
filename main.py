7a2f1666cb51ced7e1dde7ca95768707a40fc44f
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:54:22 +0000

    crowd free eager steer rest

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def square_number(x):
+        return x**2
+  def sort_list(lst):
+        return sorted(lst)
