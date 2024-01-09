0a62b41b184125a47fcf5d5f7c827ecbaef0c7e5
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:32:30 +0000

    high generous name appeal edge

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
