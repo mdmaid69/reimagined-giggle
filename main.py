071939277ae7325a1b4ec83be066b29da263b874
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:01:12 +0000

    under advantage break refuse at

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def reverse_string(s):
-        return s[::-1]
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+print([x**2 for x in range(10)])
