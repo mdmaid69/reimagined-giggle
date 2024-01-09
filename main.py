0a2fd15687b703f082f1e9ac6b87da336eff14bc
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:44:03 +0000

    rush represent when rich lay

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
