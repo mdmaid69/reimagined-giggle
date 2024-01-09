ba724b5edc87a30bb74965733ff372320816d196
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:56:53 +0000

    manage needle convenient trust match

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
