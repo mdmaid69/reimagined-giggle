7a56d8b289fa4f0df00b0afb415f9951e8c094f8
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:04:36 +0000

    here behaviour sure strange fall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def greet(name):
+        print(f"Hello, {name}!")
