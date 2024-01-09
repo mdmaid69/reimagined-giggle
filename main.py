bcef6de3499b9d9dc25cdca96e921ec5aef90c03
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:52:11 +0000

    tragedy return course free fold

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def split_path(path):
+        return os.path.split(path)
