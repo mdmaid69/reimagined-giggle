79f6e4fd5714374baa7b4d87b4944c8ed366b5ce
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:13:20 +0000

    organization disagree central pinch accordingto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
