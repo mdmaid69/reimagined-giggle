9c4022f5355fb85eb70423c3eacc0add4be0aaf7
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:07:57 +0000

    spin share opposite Bb abroad

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
