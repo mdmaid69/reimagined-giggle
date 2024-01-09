dbe06e6912fb405518c9c3c3d2685a5a789b33ae
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:04:31 +0000

    accommodation some set content tap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
