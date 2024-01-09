3d6243f2dc910dc2d2cc07e4ab64560d2008094f
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:27:08 +0000

    cough medium precise street top

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
