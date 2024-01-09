9df982ddab64c04d3d3637a5d580823e0f677e61
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:33:35 +0000

    570enjoyable table spread as later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import re
-print(re.match("h.*o", "hello world"))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
