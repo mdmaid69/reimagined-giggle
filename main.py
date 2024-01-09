85784a03e214edaa8bdcfa5bf3b3f7bdec6eb525
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:51:33 +0000

    interrupt realize into anybody contain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_index(array, item):
+        return array.index(item)
