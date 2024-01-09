d7f37226b275752f3b35fb3f218b745d1e67e24a
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:13:37 +0000

    perhaps accordingto contact there hole

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
