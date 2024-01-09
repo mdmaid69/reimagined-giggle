8245e24b0ce3ee3ef280a884a53b95d9bffcf889
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:36:52 +0000

    complaint load hit rise decide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+text = "Hello, world!"
+print("Words:", len(text.split()))
