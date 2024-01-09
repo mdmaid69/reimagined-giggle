63ee355ab3b4017e7b9f849b9a5283af48c2eb55
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:45:22 +0000

    medium quantity guide bottom loan

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def find_min(lst):
+        return min(lst)
