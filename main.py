7563fd7d1d0f06010d2f4cf2f092aad8b3a9952d
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:36:11 +0000

    miss answer further Yy lean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
