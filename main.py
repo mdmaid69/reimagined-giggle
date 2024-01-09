97b1145f556d67456d05b93121cbd19aed7c487f
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:28:56 +0000

    cap mine nor chance range

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
