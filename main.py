6b70e2d506306bac8250c299b89cc4e7d4995f02
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:06:35 +0000

    mother reaction remain freedom vote

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
