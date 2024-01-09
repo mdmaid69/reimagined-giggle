d31bd1ab8d626b13611a3eac4fafb4c30f686622
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:31:49 +0000

    I brush anxious flood dangerous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
