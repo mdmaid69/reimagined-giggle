8bbbefe9ffc4d9d213e17f5356bb03f6f23d20f3
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:03:34 +0000

    room stage approve coffee rather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
