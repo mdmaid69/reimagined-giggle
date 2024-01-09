b3595e2e0548a1a162e2aa2d4925a02063cf3d40
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:59:05 +0000

    point journey flow put search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
