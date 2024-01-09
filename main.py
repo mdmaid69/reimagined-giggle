898974deebf5a0ef8f3fc29ac1df1d2a6af7cd63
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:50:43 +0000

    afford section traffic brush calm

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import tensorflow as tf
+print(tf.__version__)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
