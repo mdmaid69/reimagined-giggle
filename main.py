05ee5dca4ebd89a1ae320e630fbffccc1db4ac99
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:41:25 +0000

    convinced stop regard new tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def convert_to_binary(n):
+        return bin(n)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
