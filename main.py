e718e5d3426c8ce43eee41e355570df3cdd2ffae
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:47:43 +0000

    reduce pull juice consult store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
