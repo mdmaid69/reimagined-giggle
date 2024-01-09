ff1ad0c9be6d7813dbc3ced93af9dbe39e25b71f
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:38:59 +0000

    issue healthy stop late difficult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
