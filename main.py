a392b9073ecfec60aec98c2033dba73202bb27fb
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:01:26 +0000

    middle only noise Cc direct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
