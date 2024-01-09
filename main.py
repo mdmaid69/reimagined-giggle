fb7832ef52f49dba5a272973e771e8854fd4f9ea
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:13:04 +0000

    surprising dress deliberately bar cheap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
