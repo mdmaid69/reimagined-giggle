86bf8e9f07f43c1730e34ea8ad6ec3704a226ee2
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:48:31 +0000

    bringback wear book matter copy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import datetime
+def get_current_date():
+        return datetime.date.today()
