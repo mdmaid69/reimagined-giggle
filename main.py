e8368359b7b7d10eaf973a21e50d4844dcca1d0a
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:06:28 +0000

    mine damage confuse bus part

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
