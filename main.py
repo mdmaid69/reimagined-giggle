92ebed86a222f11958f5107e738882921b2d8755
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:01:13 +0000

    the pronounce drawing takeover medium

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import random
+def roll_die():
+        return random.randint(1, 6)
