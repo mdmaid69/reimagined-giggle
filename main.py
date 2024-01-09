9f5ab6ae538fbeca79750bcaa6baf854696211d6
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:36:43 +0000

    really tell Yy pot doctor

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+print(sum(range(10)))
