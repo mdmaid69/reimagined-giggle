3170ab4336273fe132d03b63e9b946f495e2cf74
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:31:47 +0000

    Ll store hurt print injury

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_min(lst):
+        return min(lst)
