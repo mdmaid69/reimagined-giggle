51159ba017e8b989adbe169b777538b0828541b9
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:37:36 +0000

    today limit weak flavour honest

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_user_string():
+        return collections.UserString()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
