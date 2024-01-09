a172d04d6714903a6e89ec4fd419f7b2c548d103
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:12:18 +0000

    loss freedom recent sentence harm

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import numpy as np
+print(np.array([1, 2, 3]))
+print("Hello, world!")
