e5cba964de849c66ed9f0b1b4d6c54d9f62f4945
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:15:09 +0000

    pool ago grateful grateful direct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def find_unique_words(sentence):
+        return set(sentence.split())
