b132d14f96b93c4f2b64f0119abae7e3e39c0c28
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:29:53 +0000

    tax respect family school brush

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
