0a5ddd9f0cc5af240662b22764137cc78e4e594b
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:49:37 +0000

    touch imagination rush cum medium

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
