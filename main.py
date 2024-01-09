5bec4d966dbc9c9a4c4e669ce85c7af7150e5d27
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:41:03 +0000

    until hear admit avoid light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
