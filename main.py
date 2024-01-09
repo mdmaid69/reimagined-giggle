74daf429c2509fe38994ad14c85f92d8f38cb641
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:01:48 +0000

    home wrong round face cruelty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+for i in range(10): print(i)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
