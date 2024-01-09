11d4dc8488b40b9dac4b1d0fef8096dec4f9b907
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:57:47 +0000

    cost surprised mistake medium present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_string_from_array(array):
+        return array.tobytes()
