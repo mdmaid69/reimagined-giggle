6f69f38bfa32c065c2171365bb719ccb303b8098
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:33:15 +0000

    increase level wheel workupto basically

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
