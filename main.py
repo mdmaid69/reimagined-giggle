e2c2585b56a21004aba41da92946c5aaab4ed203
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:04:35 +0000

    again even pupil perhaps exhibition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Characters:", len(text))
