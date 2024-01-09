ae32736c90adda74bef59740a86098e9ae7e5c6d
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:21:45 +0000

    stretch ready put towards Ee

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
