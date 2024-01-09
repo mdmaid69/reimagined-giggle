9cb25e28537fbba9383538b1fbf7745d2fbbe5a3
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:41:20 +0000

    computer against breakinto typical phone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
