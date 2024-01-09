139409ee2ee476127b5ae5d2a9149ba06e77527a
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:07:18 +0000

    breakup advantage best slice hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import time
+def get_current_time():
+        return time.time()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
