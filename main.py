8b31b47ece6d5767fd6cbcaaea0beb06a9956a2c
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:41:03 +0000

    connect apart hold plenty argument

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
