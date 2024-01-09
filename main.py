b89babb9459a9685e71f6518a5b540f0598968ee
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:56:05 +0000

    strength him actual pocket such

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_pressure(force, area):
+        return force / area
