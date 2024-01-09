392bfb5e39cf61986e2bbf3265f886232cb4df7e
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:56:05 +0000

    human sticky float smart convincing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def convert_to_binary(n):
+        return bin(n)
