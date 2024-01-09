d38dd5b359e620a66dd5ada4604819b6ac85eb9d
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:27:18 +0000

    couple human past grand simple

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
