6b550d184e8ff60fedf1f29e457edc4048d9f66d
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:56:25 +0000

    failure hate shadow high admire

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import platform
+def get_python_version():
+        return platform.python_version()
