54309666f6a0b145029cbd2372537d92fabe2d5e
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:00:08 +0000

    left really actual disappear sweep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
