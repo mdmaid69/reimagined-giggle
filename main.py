e6818674e44ef242d7cf436f94184fa6ccb30756
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:10:22 +0000

    subject live experiment reduce blue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
