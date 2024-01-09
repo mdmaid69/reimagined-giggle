a7160ef8e7b951d9a86800f58e42f1f88376e58c
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:16:47 +0000

    straight film anxious serious constantly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-print([x**2 for x in range(10)])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
