695c431310b13c5455e7099746490617b1dd5d73
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:02:45 +0000

    goodbye sense comeoff its shelter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
