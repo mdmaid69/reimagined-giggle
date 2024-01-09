9790b0e35ae2b742237b9dd533a126592ebc4508
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:50:31 +0000

    anywayanyhow half thumb truth old

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
