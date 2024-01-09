ac9f8b0f697b335c555c521a2363981fdb84acc1
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:34:19 +0000

    computer turnback beside once arrive

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def count_characters(sentence):
+        return len(sentence)
