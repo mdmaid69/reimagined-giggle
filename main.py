6522ad56f712862d3a7ec1d6ec27e31f3a30ed23
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:48:41 +0000

    any true surprising print quickly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
