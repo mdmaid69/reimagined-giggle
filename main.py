4443d989b59247cdbc103d5392d6a59e628d2ac2
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:49:53 +0000

    down scare earth strike attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
