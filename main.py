7d840ee7a77cf74611f15513f4864e7aff1511b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:08:18 +0000

    meeting hole however us door

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
