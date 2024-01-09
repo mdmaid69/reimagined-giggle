d019548be1a52855e953c03f15d6b28d70dcf5c0
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:26:22 +0000

    warning strict pretty fashion sensible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
