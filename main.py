14402dea6b03623347829362f3fbfc2bea69504c
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:35:40 +0000

    of praise reach absolutely interruption

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
