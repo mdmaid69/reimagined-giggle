df1b6259d4f12140d91e486ec9feaf94848aad38
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:03:18 +0000

    gap plan afraid special shallow

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def cube_number(x):
-        return x**3
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import re
+print(re.match("h.*o", "hello world"))
