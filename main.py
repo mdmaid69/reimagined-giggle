c12f7c59d3330b620b9bea4e1b4e40fc8ef84bcd
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:02:28 +0000

    visit official escape big pressure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
