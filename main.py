922b71a40dd687b997bd16fbe2e72dca94afdd44
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:19:40 +0000

    offer dry buy lucky should

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
