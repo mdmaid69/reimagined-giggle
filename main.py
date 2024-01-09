96c503e35c036387418488394b5dfaff392101a1
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:40:52 +0000

    exam brush popular already health

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_length(array):
+        return len(array)
