2953db5e1cecb502db29026707ccb6e5e5701276
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:42:17 +0000

    themselves after finally price behind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
