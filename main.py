175b090ee337601d2f37f1d69d2818053de5b317
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:48:20 +0000

    meanwhile final hold personal into

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
