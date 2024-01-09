0c8e257933be037446d76768534df2261025dd10
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:18:59 +0000

    thinkthrough be talkoutof wheel smile

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def subtract_numbers(x, y):
+        return x - y
