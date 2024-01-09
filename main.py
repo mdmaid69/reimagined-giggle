92be9434d6684e2743da1eb18ab5ac0c3a55c7aa
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:09:11 +0000

    comeout reply coat shock furniture

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_density(mass, volume):
+        return mass / volume
