5556286813ac414cfd4dc4e7add69d1223135da0
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:51:30 +0000

    admire collect hard post behind

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
