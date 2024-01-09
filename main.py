c8e2c4ef8feefac96739b7d2e72dc7f76ac83bf8
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:50:49 +0000

    space identify welcome guard condition

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
