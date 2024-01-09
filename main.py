c4fefe565ed07adf3167d17bbeaf56f984c81be2
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:37:26 +0000

    inquire sense close turnaway violent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def convert_to_json(data):
+        return json.dumps(data)
