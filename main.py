5a3676baa42484187511fea914b9a9fc204c6ca4
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:44:31 +0000

    squeeze rent nor soon breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
