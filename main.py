86fde0814a214332c98fce4470aa4816876da67a
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:06:50 +0000

    week block old trap spend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
