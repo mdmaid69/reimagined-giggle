ab807499f5a866e9dd1288988bcf3e6cf505ed4a
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:46:14 +0000

    yours conversation divide tear custom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
