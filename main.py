8e9a6620c3b956d8d0ec8efaa2444abed87882ee
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:21:08 +0000

    shock history talkback much careful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
