6713f0bf10bcec017b30db21fc303cfab5493225
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:52:23 +0000

    attempt belong complete print wave

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
