1959a76fd68c444c9ebe749060506c70d826e31d
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:04:57 +0000

    board tone pin cheat tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
