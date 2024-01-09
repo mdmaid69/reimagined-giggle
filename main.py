21706e2990ae3f95a7d115cc2237d994743ce32f
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:10:57 +0000

    learn dress crime Ff provide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
