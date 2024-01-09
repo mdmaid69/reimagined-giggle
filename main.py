0abc5662c0d384414ec3196497c2582ac2c1e29a
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:30:23 +0000

    quarter about operate perhaps journey

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def find_max(numbers):
+        return max(numbers)
