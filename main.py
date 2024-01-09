f02b67a863231c7cf853ec588024b058d20a8936
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:50:38 +0000

    divide until size demonstrate fasten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
