84982fadd5e77bfa08a3c048169809625508e3e3
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:05:01 +0000

    practical think sort issue deliberate

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
