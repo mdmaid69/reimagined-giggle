4af170bceb084bc11378bf6d72d31e6e29cc996f
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:34:03 +0000

    rush damage turnaway fun although

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+  def add_numbers(x, y):
+        return x + y
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
