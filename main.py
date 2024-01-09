1004587a6a25b5e54d718660435909f1195c914d
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:29:28 +0000

    hit underneath very scheme carry

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
