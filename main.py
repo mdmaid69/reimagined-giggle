12c697607c04527653fcbcebbf4c465b6215b088
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:39:57 +0000

    respect mark let plan reduction

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
