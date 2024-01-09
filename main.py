a6b8da763a8a23bcd8872c72965238f99054eed8
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:11:57 +0000

    conduct crowd plate attempt square

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
