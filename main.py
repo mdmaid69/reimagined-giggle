3726e54acd54b89bf6fb3547e017561ba951aebe
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:40:44 +0000

    path wave guilty surface account

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import collections
+def create_queue():
+        return collections.deque()
