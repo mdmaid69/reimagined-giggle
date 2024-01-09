610594de6bb04b839800c69503694b0665127c86
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:45:58 +0000

    turnon command politics bend long

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
