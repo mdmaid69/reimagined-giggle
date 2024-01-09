a2536ff45dea9e0e14bf1660480c8ad057194cd5
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:21:17 +0000

    Jj age exercise all feel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import datetime
+def get_today_date():
+        return datetime.date.today()
