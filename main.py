368eec763d4c6f9eddae8489f6fa6564a3b9245f
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:23:09 +0000

    fill fun search border crazy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
