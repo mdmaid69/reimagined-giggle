4c5ea6e70aa82831956a6b6537adc9deb7c6f52a
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:37:01 +0000

    tradition on once turninto sting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
