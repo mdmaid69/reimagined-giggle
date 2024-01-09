57ab3243497f7d8f904bf24a56dcb810664be57d
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:07:39 +0000

    full wherever obtain pretty snow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
