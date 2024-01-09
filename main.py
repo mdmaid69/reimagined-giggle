e20530b21fed64711a9fd778289a32ec2cc00542
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:28:08 +0000

    pressure behind appeal tomorrow bye

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
