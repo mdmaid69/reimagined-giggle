b2a7a270fcf8e85585b75164aad43323db013a07
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:08:04 +0000

    dangerous exaggerate depend similar rain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
