4b0ab3ee80aec7de765bfdd772a08118fb4a21a6
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:08:44 +0000

    past later fear take estimate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
