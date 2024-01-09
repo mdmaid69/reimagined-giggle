fd65f25f540ce4320923e7e2721716db7c427d84
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:07:54 +0000

    learn study guess treat thick

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
