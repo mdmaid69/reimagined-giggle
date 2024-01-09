bbefeffe75c4390c6f4f17d90322b81d858fb6cf
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:59:34 +0000

    warm carry represent contain chance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
