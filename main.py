590a5ecbb830be9316df44c76f281e0ad0c2a53a
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:58:46 +0000

    furniture trap child style arrival

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
