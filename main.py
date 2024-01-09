1c2baa01b64b82b377da6f4756f545a1d9f35162
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:24:09 +0000

    fortunate fetch cry learn public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
