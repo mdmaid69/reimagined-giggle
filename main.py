927a307fe9fa0197e6feb7253f02d605bd0a5076
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:14:01 +0000

    cough far obtain call speed

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
