d9c0774c1f48339b1fbd280a85e45185b78939f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:04:28 +0000

    trouble ordinary 569enjoy unfortunate themselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_floor(x):
+        return math.floor(x)
