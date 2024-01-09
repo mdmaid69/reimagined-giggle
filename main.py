f220bc75a7458b9e515279dbb73b38ea56a6caf7
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:09:01 +0000

    platform exciting bad on success

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
