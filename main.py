2911fbb74e0899a673774967ca376d7c7ba216fe
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:05:43 +0000

    area support relative importance yes

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
