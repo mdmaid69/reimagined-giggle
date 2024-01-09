f2d780741c2e51b87ba08e9571fe23f1941000b7
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:54:56 +0000

    twist suit perhaps cruelty plenty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
