8e69dd096a439a2539296abba39b38dc2025f135
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:01:07 +0000

    cross takeout pin ever tidy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  def is_odd(n):
-        return n % 2 != 0
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
