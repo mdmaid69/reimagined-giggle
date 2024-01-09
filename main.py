fc7babcd7a7ce8bc07273f7b2c7f674bfa554de6
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:38:39 +0000

    form even fence doctor last

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import array
-def clear_array(array):
-        array *= 0
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
