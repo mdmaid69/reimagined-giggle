77f8281cb0874fc1f2302ffe1d4b0ad30b8875a2
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:25:33 +0000

    fit speak sweet ticket hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_tangent(x):
+        return math.tan(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
