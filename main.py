9b3fa3d36aecd226f7cc9f62fc994b64c5313956
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:22:44 +0000

    solve call so future store

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
