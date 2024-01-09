0b0262ebd3296186a1326cd457e545418219e7bb
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:02:46 +0000

    thorough fasten peaceful regular government

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import platform
+def get_python_version():
+        return platform.python_version()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
