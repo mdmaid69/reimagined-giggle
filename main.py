b9008423b0484cf9d7be4a973f904ea108857b5f
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:06:15 +0000

    embarrassing afford listen odd Qq

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import os
+def remove_directory(path):
+        os.rmdir(path)
