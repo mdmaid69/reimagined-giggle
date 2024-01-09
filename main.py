087e3551aec46004d7415ae7a0201453de7806b5
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:09:27 +0000

    clear desperate appearance radio giveaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_sign(x):
+        return math.copysign(1, x)
