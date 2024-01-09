805bfe51fa13dc12d5d5cdd6cab9b863a2c7dc7e
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:09:32 +0000

    whether valuable suit occur have

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
