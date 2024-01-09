34c203377f3fca977140eae3695e00a54018b8da
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:08:11 +0000

    public keepout warning peace steal

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
