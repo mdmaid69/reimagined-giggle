605adad328075aca9818ac2800329f35eb841c75
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:04:25 +0000

    quickly shoot near most future

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def add_numbers(x, y):
+        return x + y
