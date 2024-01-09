773ed316a030b2150e783d323d85b09587dca40d
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:33:54 +0000

    although sure below drink hit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
