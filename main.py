a8847af977ea799f10d247d157dde860325b366e
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:38:39 +0000

    cruelty excuse moment keepoff main

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
