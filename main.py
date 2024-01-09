3a4dbfecc99b93d148dd6774af8fabffa3e420d2
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:16:14 +0000

    involve nose watch interrupt cough

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
