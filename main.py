affb9dea17a4c812456f4aefa69151b062164fe1
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:32:30 +0000

    question temper school hot Pp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
