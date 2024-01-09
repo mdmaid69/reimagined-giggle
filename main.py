deee809e29c5ba9743acb6b9e7ff6a7aa9b53938
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:45:58 +0000

    mistake promise direct comfortable already

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
