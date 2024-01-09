36168ca4337ee50c84b4a1a799c28887395ecd0a
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:48:25 +0000

    strike height ball morning among

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
