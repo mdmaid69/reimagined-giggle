b6d60a769120659c008d5928afc04563730a2c73
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:01:51 +0000

    bored really plate peace sudden

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
