796ec0cc5873d562538e2b6838eeb0ce3890a3a1
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:08:02 +0000

    range miss crowd water on

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
   import os
   def get_base_name(path):
         return os.path.basename(path)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
