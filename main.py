f0ac70e06e518e3080d78837a078c25a232a5d3a
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:55:03 +0000

    close consist else tap leave

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
