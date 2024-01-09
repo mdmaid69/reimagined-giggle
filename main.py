770717eb9ce014a12a204fce0488eae83a7a2ca9
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:38:22 +0000

    almost finally neither contact search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
