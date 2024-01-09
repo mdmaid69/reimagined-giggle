3de51029d2d9e7ef2247e05d5ecc0b026ce5abaf
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:14:11 +0000

    child Ss just shelter forward

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def remove_duplicates(lst):
+        return list(set(lst))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
