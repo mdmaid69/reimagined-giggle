f3ed8db084fcb1c2f711e5650cce5ae176b73968
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:29:26 +0000

    squeeze Zz ever whole regular

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import getpass
+def get_username():
+        return getpass.getuser()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
