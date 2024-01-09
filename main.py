e7eecc724df6aedc11a98d3a04dd7cd7e7903fad
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:09:01 +0000

    kill bill worth Vv yours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def reverse_list(lst):
+        return lst[::-1]
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
