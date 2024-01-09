13891681f7538540f07f3ef8cce21d5809a7215c
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:57:34 +0000

    unable offer dick turnout disappointed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
 import array
 def append_to_array(array, item):
         array.append(item)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
