fbcfcc62768970f4a95701d44b5f4775eeef71a9
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:20:54 +0000

    mix him afterwards show date

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
