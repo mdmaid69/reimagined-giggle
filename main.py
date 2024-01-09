3f2191229e37320c9e929059a606014a2ba39b49
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:36:06 +0000

    minority your them creature practice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import sys
+def exit_program():
+        sys.exit()
+def convert_to_octal(n):
+        return oct(n)
