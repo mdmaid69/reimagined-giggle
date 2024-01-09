c28b0999a6c88bb974f736e50bbf602e5e8a6aab
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:11:06 +0000

    choose ocean general near fail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
