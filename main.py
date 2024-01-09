c0a582748bc8d116ee06d3cb771fa4ab8e3f2bd8
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:59:07 +0000

    strange winter lend suddenly lucky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def convert_to_hex(n):
+        return hex(n)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
