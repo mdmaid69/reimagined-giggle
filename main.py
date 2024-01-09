99129ef87f30fa9fc723f21eb2503b749e1477dd
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:31:09 +0000

    ordinary goup respect market OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def is_even(n):
-        return n % 2 == 0
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
