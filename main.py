47d37c36a8c507c11f0f2d2c1202744d927905aa
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:32:12 +0000

    any natural soft neck moon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+text = "Hello, world!"
+print("Uppercase:", text.upper())
