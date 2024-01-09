755fcc0c79d549923cf89544f4c7e247e4b22070
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:31:14 +0000

    bin Vv would excellent plenty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def multiply_numbers(x, y):
+        return x * y
