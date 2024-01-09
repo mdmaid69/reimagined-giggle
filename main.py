48a9056130012e48c12a900a0da9db26fcb9f228
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:33:02 +0000

    emphasis keepaway dream young poison

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_work(force, distance):
-        return force * distance
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def find_unique_words(sentence):
+        return set(sentence.split())
