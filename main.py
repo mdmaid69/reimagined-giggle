04b455c457f4387fdd2775c16ef65c5aa6be93ad
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:52:03 +0000

    of too else invitation lead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
