e008093887c34ce66e1fb54db9097ec3b0fb43db
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:24:41 +0000

    find moral Mm famous in

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def multiply_numbers(x, y):
+        return x * y
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
