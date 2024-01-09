aecc01804988a6d031822614ad94898397f6457e
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:45:24 +0000

    shake how do Rr year

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
