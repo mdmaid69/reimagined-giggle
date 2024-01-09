5902ffee46b2f6d9dfbbb96ddd0746b5f15aa450
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:08:19 +0000

    emphasize fork complain clear mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
