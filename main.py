54446da79342ad80302cab135fbb4e71aa18dcc0
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:15:40 +0000

    board play before experience wet

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def count_characters(sentence):
+        return len(sentence)
+import tensorflow as tf
+print(tf.__version__)
