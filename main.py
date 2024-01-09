4f965b9bb16dbb43fdf4b34d0cf69eb219dc7c52
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:31:34 +0000

    pleasant ride sky guard emphasize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def cube_number(x):
+        return x**3
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
