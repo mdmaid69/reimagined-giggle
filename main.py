e21fe3b4d7f6edad6ed77c2ed83136657f96269e
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:09:51 +0000

    regional comment admire dance clean

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
