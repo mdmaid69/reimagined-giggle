f377382899d64745f0e6f619e277b5615148444e
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:27:46 +0000

    probably calm its whisper storm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def extend_array(array, iterable):
+        array.extend(iterable)
