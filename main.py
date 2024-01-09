01d2d0907f1b503c10f4b924b2d54f8d929532b8
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:18:46 +0000

    upset goon keepfrom sense breakin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_power(work, time):
+        return work / time
