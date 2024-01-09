a0b5d5be0880cd73e09cf263c23f43b5610fe570
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:11:48 +0000

    accidentally pool adjective repair cheek

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def sort_list(lst):
-        return sorted(lst)
+def get_array_as_int(array):
+        return int(array[0])
+def add_numbers(a, b):
+        return a + b
