9d08f1ec6d7f3aed1c93cb7e0b0aedc9bbe2de8a
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:24:34 +0000

    principle bringback dance single side

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-print(math.pi)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
