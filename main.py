6d3b31caa0f5d37091a40a9e78f7976f92ff9b17
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:27:44 +0000

    repeat fall feeling progress spot

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-name = "Python"
-print("Hello,", name)
-import numpy as np
-print(np.array([1, 2, 3]))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
