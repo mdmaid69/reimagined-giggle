5710c827f7690cd72500bbbd98d44ba54741f59b
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:44:20 +0000

    seriously card glass week smoke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
