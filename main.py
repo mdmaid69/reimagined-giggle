691244c127e9033925d8897249adc05efbf5fc49
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:11:27 +0000

    next as exact maybe morning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
