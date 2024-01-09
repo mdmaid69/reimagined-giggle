6af2ddeec355601f023631c7fbfcd55894077b91
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:33:21 +0000

    cough thirsty dangerous uponon him

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
