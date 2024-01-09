c78c96eeb552e896142431fda82a14c34c1ad303
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:28:47 +0000

    make deep benefit impression brother

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
