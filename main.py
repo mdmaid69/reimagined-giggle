bda88ab6871b5fa652d2b9a296a7f234857bdf3c
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:23:42 +0000

    climb story pay sea jump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import os
+def remove_directory(path):
+        os.rmdir(path)
