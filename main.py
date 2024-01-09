463229359c41eb213d82c26f791e7678b8f33a6f
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:43:56 +0000

    offer scream worth detail left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+print(sys.version)
