180aa3f5e449e08b07f2fb95644a8527235d751d
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:34:37 +0000

    in dark race grand left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_perpetuity(payment, rate):
+        return payment / rate
