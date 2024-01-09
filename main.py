b0c4917af3054d9a0cfe4642c8174b6d6977fa8f
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:28:35 +0000

    payment prefer Pp community back

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import sys
-  def get_python_version():
-        return sys.version
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_list_from_array(array):
+        return array.tolist()
