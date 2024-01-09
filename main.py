42d6c65d23aff51591ba3deff6731efd1728f410
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:30:20 +0000

    instrument turnover she rate black

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def get_array_as_list(array):
+        return list(array)
