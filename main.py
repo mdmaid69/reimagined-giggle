29de2d20f594dd2451e590661f5c143086da9fe4
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:24:09 +0000

    mess disappointed whichever national nature

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
