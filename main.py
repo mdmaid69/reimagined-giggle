d59be5ce909c1233466f92f0036f12234ed94814
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:55:49 +0000

    wise interview book visit shoulder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import time
+print(time.time())
