f89e9dce292043fa0a08a2aeab5ab5ceac285236
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:00:31 +0000

    colour interest immediate learn Vv

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import datetime
+def get_today_date():
+        return datetime.date.today()
