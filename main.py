eb41115b7f27ce33f9ef06865ba89413916cef70
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:02:39 +0000

    distant thick rescue skill sight

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
