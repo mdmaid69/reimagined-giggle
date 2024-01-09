7fa8a5f360ec0f367b89f534b3ee8b6e303fc09e
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:30:31 +0000

    Itself soon spread breathe agree

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def square_number(x):
+        return x**2
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
