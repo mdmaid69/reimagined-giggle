18b40fff037dee7493fb6bc70e339ff18f5ce241
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:52:08 +0000

    science comeup sound quiet thin

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def is_odd(n):
+        return n % 2 != 0
