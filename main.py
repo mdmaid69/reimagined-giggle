9ab20c260c6bd390e0db5b7546579df441ddccb4
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:13:35 +0000

    thinkof emergency even trial home

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
