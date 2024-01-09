67dc8f48fa43ec9c2dde3c7ca822a6dafbeb4028
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:00:45 +0000

    by connect force muscle or

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
