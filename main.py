c3d365e12bced76946df3ca865a01ec82a698f06
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:02:16 +0000

    average swallow line mark nothing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
