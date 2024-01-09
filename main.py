4f9f267af6f8f22aa95292849941e21938459d75
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:59:47 +0000

    extraordinary hide organize complaint left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
