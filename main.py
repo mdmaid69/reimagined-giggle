f9ac7629c9746d48c6c61ae7ba25e4d8e5634f7a
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:42:11 +0000

    question godown operation majority march

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
