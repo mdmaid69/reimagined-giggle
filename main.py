4ccff6f88803ad566d51dfc8c80cd49cbd5c956b
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:08:22 +0000

    ring anything view prevent hold

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
