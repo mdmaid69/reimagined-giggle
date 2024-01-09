dc3bae99faccacd49f55f9602bc445c8c5546b68
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:03:16 +0000

    funny fit group bet usual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  def convert_to_octal(n):
+        return oct(n)
