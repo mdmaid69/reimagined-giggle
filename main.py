546fab1646aceb9798fa04fd87aabb290fdd365a
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:36:31 +0000

    kind invite school appeal handle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
