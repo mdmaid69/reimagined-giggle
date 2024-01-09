644f01a55cbd289fd79b64b1f524c4da1844b48e
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:52:32 +0000

    demand tape moment mistake Kk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
