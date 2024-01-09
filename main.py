495d71becad32752a003cb6f66013b51bcb0b037
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:46:42 +0000

    shake careless result exchange fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
