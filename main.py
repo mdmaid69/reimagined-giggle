165b0b5f92495141e7dfba17b2cff862bc7cbd84
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:41:38 +0000

    nowhere stone result giveout key

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
