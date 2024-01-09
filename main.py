2a7165f161f4a8fecadc7dc7e483bfb27625f80b
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:58:40 +0000

    heat birthday breakfast aware pot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_item(array, i):
-        return array[i]
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
