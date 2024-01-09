df19f5ce09c6ce94d14531f47240261d92f4d8e5
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:15:22 +0000

    bright sell play language ourselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
