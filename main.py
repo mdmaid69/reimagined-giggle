895e05e6e85a1ec14498252556abac4ec1fb33b7
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:35:32 +0000

    purpose pool anyone solve look

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-print("Hello, world!")
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_work(force, distance):
+        return force * distance
