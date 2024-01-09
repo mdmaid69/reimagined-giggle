58d2e36d2119c2beb7a09fada13b9167a9f92ec0
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:55:04 +0000

    pay whenever attach fold wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
