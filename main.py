63c03c101a382a0ac7a43a09f8b1fd929d14ed00
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:48:34 +0000

    handle game raise whole charge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
 import math
 print(math.pi)
