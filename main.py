81ada0544842a13ee73cdf9a69afc6934a82ed36
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:58:38 +0000

    this rain murder block cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
