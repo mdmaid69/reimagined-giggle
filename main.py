5612377a88549740f16b3332ccb01d5a33bdc972
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:48:53 +0000

    ball prepare too accidental talkinto

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
