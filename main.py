09683fb8f0bd92b5bb19b640c47431b95412f386
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:45:42 +0000

    sale exchange worst clear rest

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_density(mass, volume):
+        return mass / volume
+def count_characters(sentence):
+        return len(sentence)
