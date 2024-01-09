ba2c532828edff915561812493c2349b3b9e8b45
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:21:15 +0000

    promise please apologizeapologise patience imagine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
