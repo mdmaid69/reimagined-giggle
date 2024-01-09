0483ebbea0d9db3dc045670b1fcc6d14642d73f8
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:11:48 +0000

    remark shoulder question recently convince

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def clear_array(array):
+        array *= 0
