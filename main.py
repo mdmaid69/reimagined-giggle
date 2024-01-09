cef13b3821418d5da78118c45023e23f13ddbe54
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:20:29 +0000

    off design danger far road

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def find_max(numbers):
+        return max(numbers)
