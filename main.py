53cb8cfbd46691c41e3341876defb8e94076a73f
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:23:21 +0000

    situation drink OKorokay off region

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-name = "Python"
-print("Hello,", name)
