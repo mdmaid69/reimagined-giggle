86e4baae10bd7b837f16bf22113f65743adf69d3
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:18:11 +0000

    vision live pressure rather punish

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
