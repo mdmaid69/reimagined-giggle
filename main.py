3e1ddd836831ac33dd14268ca9ce3aaecffbc955
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:58:19 +0000

    path prove science autumn soft

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
