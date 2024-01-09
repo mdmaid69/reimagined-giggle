64ad680614b4a86f97adb9113f437ba0870dd04e
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:02:23 +0000

    feel performance open emotion Itself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
