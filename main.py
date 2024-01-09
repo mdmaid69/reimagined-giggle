31dce382fed152792f0602d5965e4e0ab4eb4203
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:02:40 +0000

    dark roof train yet thorough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
