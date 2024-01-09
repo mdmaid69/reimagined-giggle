d7ff90158cdffbc38cab5edca4426a26d707faba
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:32:23 +0000

    to wear door borrow disturbing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import time
+print(time.time())
