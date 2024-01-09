c1ab1d2a335585391884c4cf2d30796f75a0f9a4
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:30:11 +0000

    573entertainment drop clothes treat community

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
