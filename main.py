de49a85b55c37ce99b282de04f9d5e409cd0b4c8
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:58:37 +0000

    past minority formal ask high

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
