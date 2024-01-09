895b78a308fd53dbadb93aa3b3e802a8c55083d0
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:50:37 +0000

    turnout might form goon broken

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_acceleration(speed, time):
-        return speed / time
+import os
+def get_current_working_directory():
+        return os.getcwd()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
