baaaece395e9d49b78c78dd21351e28fc990ef2d
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:19:08 +0000

    mixture argue sweet keepup nearly

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
