c96fc7cd16002f4f2766cf8541d74e3568f900e9
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:34:45 +0000

    reply squeeze mysterious cool square

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
