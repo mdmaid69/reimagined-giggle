00040caecd5438841a58408399e5b7859047203f
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:46:55 +0000

    black speed no form good

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_density(mass, volume):
-        return mass / volume
+def append_to_array(array, item):
+        array.append(item)
