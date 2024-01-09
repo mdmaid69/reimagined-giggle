94a14c55716cc7fcdbdab4d7526e2f641bb17c97
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:57:25 +0000

    activity Ee unusual embarrassed emotion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import platform
-def get_python_version():
-        return platform.python_version()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
