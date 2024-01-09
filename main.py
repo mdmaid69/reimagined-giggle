432015a80f41fd78f2e721d4e7b230e27e949241
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:28:44 +0000

    proud slight age law dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
