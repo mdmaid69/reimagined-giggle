581e50376ccf8fd4f792d1ef93124d35bde0b127
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:24:43 +0000

    stamp help material off makeinto

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import random
+print(random.randint(0, 100))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
