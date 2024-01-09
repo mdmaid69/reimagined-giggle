0ee3e56492c001338ab4cd238d8115cb341ab1e6
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:43:28 +0000

    contact test confident care miss

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
