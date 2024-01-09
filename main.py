312c1ed23f092bf6d125d2cb9c3c61f182ef491b
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:01:33 +0000

    event slight bear hurry proud

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
