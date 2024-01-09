3ac1ec9f02b00e66a1ec01970922c9473e166622
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:03:59 +0000

    last unable exact deliberately reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
