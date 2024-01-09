afc27cf1d88d338baaee7432d395683051a1c3fb
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:15:14 +0000

    Uu action matter sticky public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
