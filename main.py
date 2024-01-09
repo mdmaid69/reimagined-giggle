770b0a707cfaf32eb8c4f853fffc78b39087bdc7
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:00:26 +0000

    getoff open angry less object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
