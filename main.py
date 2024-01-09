96eccef79a342b98e44f08cdec3f7ff81c6fdc23
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:12:09 +0000

    successful dark petrol another shame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
