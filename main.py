ecd0c90333cf06c894cd363afd337963a9c25f3c
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:57:13 +0000

    how dream crazy sign violence

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
