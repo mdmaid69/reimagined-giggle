009440e7eb1d6e8343c44535ab4be4e48dc5673c
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:30:03 +0000

    person promise imagination number immediate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
