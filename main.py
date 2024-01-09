23907ac55b103967026ea9c358585e968d1df6db
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:30:57 +0000

    anywayanyhow workon yesterday teach getup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
