7d9339070a9894273d41f82413a60c28f1e0247a
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:49:08 +0000

    turn swallow quiet fault advantage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def change_working_directory(path):
+        os.chdir(path)
+print(sum(range(10)))
