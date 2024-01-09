b84ff0681340f10010efef7b71b4fdc6e5465bf7
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:53:18 +0000

    move walk thorough brave abroad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
