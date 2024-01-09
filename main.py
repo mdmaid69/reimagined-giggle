35c070623f01430b8d7fcb1453a62ede01b298c4
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:39:42 +0000

    Yy getoff hand careful sweet

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import os
-print(os.getcwd())
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
