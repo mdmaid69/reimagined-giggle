f13b0dbaca1d1ee13297d6a9c0f3cfaba7feeea9
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:47:02 +0000

    instant comment without slice borrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
