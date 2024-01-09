c2c378ecbdadeb6a652308a9f68a9f407fdf8e8b
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:31:16 +0000

    concerned allow suddenly tongue season

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
