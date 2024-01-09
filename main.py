a77af2c3876a624a8654ad6d86c4a6957638c1c9
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:33:19 +0000

    influence thanks choice deep drink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
