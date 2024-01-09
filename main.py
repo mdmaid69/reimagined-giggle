bc060d4301e3eda6bc09d227797e201f80d02f67
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:31:23 +0000

    hold honest probably protest high

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def find_unique_words(sentence):
+        return set(sentence.split())
