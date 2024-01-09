1c37facd38a88dc3973a1d8975d077cea1bff64f
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:14:14 +0000

    be Pp radio separate rare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def reverse_list(lst):
-        return lst[::-1]
+  def delete_file(file_name):
+        os.remove(file_name)
