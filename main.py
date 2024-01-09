7bc9a6603289896888eef88c3d4a44b6bec932ed
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:43:49 +0000

    confused act benefit attach simple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def get_array_as_str(array):
+        return str(array)
+for i in range(5):
+        print(i)
