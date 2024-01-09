ef1f813434feeddfd2fb406b2ed207835ec54fc8
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:29:34 +0000

    hole belief life cruel slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
