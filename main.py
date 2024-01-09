054af8c4e96c8620fb962c73342f8cde3102d240
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:50:44 +0000

    have switch bear replace still

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_acceleration(speed, time):
+        return speed / time
