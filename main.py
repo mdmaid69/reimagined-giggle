adf8a983efc226489d74475b6212b8a3df5fe41e
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:42:54 +0000

    interview total rob this clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
