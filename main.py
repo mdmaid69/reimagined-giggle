0e42559b7516791736fb716c300a1f566e182349
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:01:18 +0000

    strange loud slowly guide though

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
