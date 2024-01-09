ef6ab40bd5daa3ac78a1c77970d024c57e4f556f
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:26:54 +0000

    get object surface mine pause

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
