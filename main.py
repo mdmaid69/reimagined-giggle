d9a62cb00ff325de7b427d3b8d6e427644d6e6e6
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:02:40 +0000

    shelter brush sudden body tooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
