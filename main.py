78778c5006c26ae8bf25cd79c903491077561403
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:03:04 +0000

    test usually impressed standard she

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import numpy as np
-print(np.array([1, 2, 3]))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import tensorflow as tf
+print(tf.__version__)
