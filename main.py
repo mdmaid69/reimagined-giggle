1877712a18f9088535941677a0cdb4e3afc1b517
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:01:09 +0000

    stop cause attempt cheat common

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
