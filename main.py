892cd6d18882252e3e8fbac354a931a0cd6707e2
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:44:47 +0000

    breakin pause another worse keen

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-print(sum(range(10)))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_pressure(force, area):
+        return force / area
