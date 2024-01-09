82c13df7dfe45fd868c9e71c1d332f59639484b2
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:38:19 +0000

    hole proof professional available hit

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import random
+def roll_die():
+        return random.randint(1, 6)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
