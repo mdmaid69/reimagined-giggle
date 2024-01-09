301a65dcd70dbe5fcc0b475daa6d29f6d13980c1
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:28:33 +0000

    wet embarrassed path turn pass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
