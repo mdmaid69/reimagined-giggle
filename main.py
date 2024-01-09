9ac1ed1e3961d98671936c5d40c3b65cf317ab69
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:19:23 +0000

    frighten getup suffer ability twist

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def reverse_string(s):
-        return s[::-1]
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
