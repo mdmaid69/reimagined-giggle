6b2bbcc6f30714b5b11623886b58d3d8a1de5e70
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:25:50 +0000

    thing control centre pass beyond

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
