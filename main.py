a72249727fcc291557a79dcd87c354a424613698
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:27:10 +0000

    order concentrate swallow production row

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def convert_to_hex(n):
+        return hex(n)
