522b9e74dd3dc05d375415e7dad279e23512afcc
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:51:46 +0000

    drop calmly deliver or feel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
