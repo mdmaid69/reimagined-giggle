5fa08234b0a2e78493003334913a497a91faabf3
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:13:26 +0000

    fat male takeaway pay fence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_ceiling(x):
+        return math.ceil(x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
