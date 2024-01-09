fd2da67f77d8b52a3d232c801390645b7cc5337c
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:49:36 +0000

    bringdown dig appear space skill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
