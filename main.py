48db648081ebbe9abce311cd6664a658ba3f34a9
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:20:40 +0000

    noone strike trip upstairs less

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
