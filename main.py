82913405ce55a93c2a785554e2670ebd870d48d4
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:36:17 +0000

    twist sign art beat cross

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
