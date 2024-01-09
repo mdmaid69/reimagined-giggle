94b9febf393786ab39fd3b1063ce8a33e0d1b495
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:02:30 +0000

    Yy wave exaggerate fit narrow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
