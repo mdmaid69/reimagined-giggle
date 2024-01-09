5e858fcbcae5b32ed4ca120fe57610e1a0e4ab48
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:02:15 +0000

    level usually mouth bag who

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
