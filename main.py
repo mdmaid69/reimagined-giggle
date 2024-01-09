56935fe8c4f0016f6bfa5e7c535e0cc6ef4b1f31
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:16:51 +0000

    legal yours hole really away

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
