fd44d5ada1345bc6fb21daddec3c5ad513f5f373
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:16:26 +0000

    foreign callup own ignore marry

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
