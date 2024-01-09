d04a3124cce51d165bcf52b0be27d00a040ce553
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:34:27 +0000

    reply strong genuine comment wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
