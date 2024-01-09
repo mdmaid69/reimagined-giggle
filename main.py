c2ddf373731a7a5ade9450cec8ac1b607c89d0fa
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:45:25 +0000

    spin lie try tool because

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
