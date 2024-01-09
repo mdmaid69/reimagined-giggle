92cffad918f9dcb4e94a92e7cb78f7b85ca7b451
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:01:20 +0000

    towards appeal finally attract piece

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
