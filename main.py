1c93c7ff6551d61eb7776ef5737b4f204ae6d6e0
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:08:43 +0000

    shut prize weather still time

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def is_even(n):
+        return n % 2 == 0
+def find_union(list1, list2):
+        return set(list1) | set(list2)
