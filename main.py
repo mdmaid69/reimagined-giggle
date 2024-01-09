7150edaa09e914a16c7c3906dd5b2de2169c5bbd
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:42:24 +0000

    scared frightened fetch myself hello

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
