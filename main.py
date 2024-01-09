2287ad2262b0d76f740caac1b4161da4536fc5f6
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:40:49 +0000

    equal tonight far order only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_exponential(x):
+        return math.exp(x)
 import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def change_working_directory(path):
+        os.chdir(path)
