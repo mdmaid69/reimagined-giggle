f582c2ea088b8ed1fa550e4e64a71e36caa51c6d
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:52:43 +0000

    holdon pick load office present

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
