0132e1cfe5253e19fa5684d766058038f531173e
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:25:39 +0000

    far let point will tired

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
