6ff8225a8b0fce33cf8e6e932dcb4a6818d57717
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:43:03 +0000

    clean insist legal to lock

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
