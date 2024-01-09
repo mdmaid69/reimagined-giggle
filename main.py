25f1ebda9e4d7e01b98ff2b4467f646bde095939
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:05:30 +0000

    turninto lean confusing must free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
   def get_file_size(file_name):
-        return os.path.getsize(file_name)
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
