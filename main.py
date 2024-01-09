17454851c006806f70563debbed48d5fbf1f1cb9
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:16:50 +0000

    guard project hello minimum us

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+text = "Hello, world!"
+print("Reversed:", text[::-1])
