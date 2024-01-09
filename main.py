8b6568ed4b1c3e5fe09bc0e8cda6f10b3e6ccaff
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:48:41 +0000

    afternoon Rr something win embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
