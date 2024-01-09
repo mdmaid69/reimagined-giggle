a20239fb52bbe173a596bc70c0f3db8b62ae70c4
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:02:07 +0000

    able to thank excuse job

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def square_number(x):
+        return x**2
+import sys
+def exit_program():
+        sys.exit()
