4deb39b426926a42f2d71ba6e85bd2dfe69f00ae
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:14:16 +0000

    previous cold whistle comedown born

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
