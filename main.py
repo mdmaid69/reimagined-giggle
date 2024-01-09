d5d7ecc4a9635e427630f24400309359ae25bdf1
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:38:32 +0000

    failure underneath world border climb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def count_elements(lst):
-        return len(lst)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def add_numbers(x, y):
+        return x + y
