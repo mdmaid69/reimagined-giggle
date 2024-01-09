6952389664ad6c4e882c41af656296a927d84239
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:26:25 +0000

    scheme beyond sharp charge thirsty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_list_from_array(array):
+        return array.tolist()
