3d0975fd8a0633dae56ecc6ca09336eeda1c2ef5
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:02:11 +0000

    birthday roll dinner bunch present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def sort_numbers(numbers):
+        return sorted(numbers)
