527f000664ccf8118b0a2fe7a56a847c8d259e6e
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:42:45 +0000

    burn slow hope transparent print

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+for i in range(5):
+        print(i)
