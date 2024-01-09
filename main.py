1fd027eaf88268be8753b30891751644cb53058e
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:51:00 +0000

    bottom gentle worth frequent complicated

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
