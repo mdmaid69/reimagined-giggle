e27733f391245a1d2d04888b594ba3108a473f65
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:03:37 +0000

    achieve notice unite bad might

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
