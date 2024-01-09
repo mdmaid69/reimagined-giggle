481d0deaf2203ee9b957d6f51b3d3955aae6ea65
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:56:05 +0000

    float just must not recognize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
