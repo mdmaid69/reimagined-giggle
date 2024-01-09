63dd423b78bd4945fa0f83f70c1fec634f766498
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:23:30 +0000

    unfortunate us forget fairly challenge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import time
+def get_current_time():
+        return time.ctime()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
