8692b8b6baf1ddb33b5f767a128f0bd63524568c
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:36:30 +0000

    until doubt will zero undo

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import os
+print(os.getcwd())
