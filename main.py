6014e104bb69db680985ac4ea656a4bb71e81995
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:53:20 +0000

    national cough opinion building charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
