63ee58346568cd19b4df130dcad8acd3430165b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:13:46 +0000

    familiar appearance refuse runout though

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
