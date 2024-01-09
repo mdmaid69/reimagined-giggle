6fc425bcef8dd26e805369019f3dcf467827e74b
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:47:22 +0000

    image welcome care before leave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import sys
+def exit_program():
+        sys.exit()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
