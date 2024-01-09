cbfee23bea248e329403b1a09e465e2e95f99af5
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:31:41 +0000

    gooff fill grow quality force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
