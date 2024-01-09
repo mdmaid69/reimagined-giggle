c1d1f9cfc94daa22df188ade916d75c720caab0d
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:53:25 +0000

    keepup trust continue slow especially

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
