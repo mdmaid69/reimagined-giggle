382f932b4a44e44a9cc2cf9c299854de5f0e1c80
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:31:27 +0000

    happy second float exactly about

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def reverse_list(lst):
+        return lst[::-1]
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_index(array, item):
+        return array.index(item)
