5f206707a238b0b438844cf1025fbd897891946d
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:04:33 +0000

    band cruelty famous present bite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def convert_to_binary(n):
+        return bin(n)
