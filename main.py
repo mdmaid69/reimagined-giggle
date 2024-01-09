c54d31324e15e1cf95440d75e55158011116ef8d
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:51:15 +0000

    fond strength bringup expect mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
