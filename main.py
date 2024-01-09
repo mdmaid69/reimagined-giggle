a62f5b1a116a7a7a4f28c6a0daeb398573a004a0
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:54:22 +0000

    pocket to date physical final

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
