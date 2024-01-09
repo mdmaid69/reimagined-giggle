21257c49732fbef755612a37d8cc102c23531ecb
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:35:45 +0000

    collect there smell friendly mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
