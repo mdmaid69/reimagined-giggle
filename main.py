9123c00eb4e1a3661c3d2a6eb5c91037e90063f3
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:35:50 +0000

    share well respect turnaway point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
