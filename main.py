33f2af6c851bb9a08ec71b060b04648429812207
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:51:17 +0000

    appearance ability minimum undo satisfied

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  def sort_list(lst):
+        return sorted(lst)
