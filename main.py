a8479aa4a167cc3eddca65e075a2a47aa4f93c3c
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:23:58 +0000

    interrupt walk amount underneath tend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
