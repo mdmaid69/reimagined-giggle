0e892f503acef9abac3e112463a9a2a3639e96c3
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:55:17 +0000

    dress practise something them cut

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_acceleration(speed, time):
-        return speed / time
+def convert_to_binary(n):
+        return bin(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
