89f900b7055fcf3d936ee24da901aa8747878c06
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:11:01 +0000

    rubbish practical manner sure traffic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
