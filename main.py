745f4e076f5f185e16dd8d3e7fd9862760356fed
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:02:21 +0000

    service point cap behind issue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_typecode(array):
+        return array.typecode
