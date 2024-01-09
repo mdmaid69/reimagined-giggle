a32d0144cbcc790481227efc412fecf9c8c25138
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:08:36 +0000

    high tonight bringup wish own

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def find_unique_words(sentence):
+        return set(sentence.split())
