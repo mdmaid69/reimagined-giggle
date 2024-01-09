6c71030f4c1e03cac5a39b5037e75504a0248844
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:27:17 +0000

    understand opinion side scare distant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
