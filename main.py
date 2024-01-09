654555855ee8a9a18f82a7fbc04a1a213cdd9e35
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:45:48 +0000

    raw until claim colour very

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
