fc4909df8ef490d8d3e36870a0d87c56624ac207
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:52:40 +0000

    failure soon pure nice but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
