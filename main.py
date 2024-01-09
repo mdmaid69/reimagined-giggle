e5fe47ef0210057460e1171efbe048139c7b38b8
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:49:36 +0000

    566end below bottom justice main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_int(array):
+        return int(array[0])
