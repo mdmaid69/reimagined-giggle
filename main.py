ce132c3dfc4bf3dcc135c88ff9e71ab777759d24
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:56:47 +0000

    spring consider with yours front

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
