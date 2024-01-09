0b16f9649976d971245113c59de79faf4f3be323
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:17:12 +0000

    concerning bye piece emphasize should

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
