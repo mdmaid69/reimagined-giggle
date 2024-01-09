fb38c08dc32f1d0248b82df69ce5056560eddba7
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:11:04 +0000

    nervous sale plastic bet switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def multiply_numbers(x, y):
+        return x * y
