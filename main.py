241f6837f4688ab019dc5ea15c63fb60a56d5ddc
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:34:11 +0000

    appeal interesting business might well

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-def cube_number(x):
-        return x**3
+import platform
+def get_os_info():
+        return platform.uname()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
