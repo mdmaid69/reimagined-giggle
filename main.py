7826b5ba014d099a1cfa29fe120b9db09ddfa037
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:18:03 +0000

    violence record wake notice cool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
