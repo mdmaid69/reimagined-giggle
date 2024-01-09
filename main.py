a47dfd6d5d5c35c4c5d83cb508e9d8392d852e28
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:36:09 +0000

    workupto date letter most quality

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
 import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def get_array_as_memoryview(array):
+        return memoryview(array)
