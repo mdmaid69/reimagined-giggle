8f719bfffab2ee8051605de57c90b4ab1dbcc6ad
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:22:03 +0000

    soak effect course fortunate turnback

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def divide_numbers(x, y):
+        return x / y
