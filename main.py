a678f8d5fd30894012efb526ea761b81deb0ed57
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:26:15 +0000

    long thinkover man fill hospital

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def cube_number(x):
+        return x**3
