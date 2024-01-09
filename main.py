55655130a8e5ac9cc913cb76d050895d768f37fd
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:59:37 +0000

    dance identify Ww capital dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def remove_from_array(array, item):
+        array.remove(item)
