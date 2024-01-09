5da5c4cbe49bfb991c9f57ed0f548fa37e4aafae
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:49:37 +0000

    race Kk wait damp kill

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
