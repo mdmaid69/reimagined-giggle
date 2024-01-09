6f7cf0ffdfabf7928f52a6887c973e86b62645ec
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:40:41 +0000

    such often turninto trap breakup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
