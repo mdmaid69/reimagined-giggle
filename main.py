2c9abe7b0783b071558f8b10d6ba0d028d72e89d
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:21:58 +0000

    praise takeback Gg search circumstance

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
