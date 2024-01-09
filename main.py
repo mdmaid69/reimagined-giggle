84ec77a82b6ed08ceed4ac472c6a6bc265ed3770
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:45:16 +0000

    hold steady describe wild agree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def convert_array_to_bytes(array):
+        return array.tobytes()
