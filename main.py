686f1878484f8c132a21c96c24379908a292b235
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:40:56 +0000

    mean score inform entry opposite

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
