ee70b5b07c522e9bf36c0d1753dc2e48bbd7789f
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:01:14 +0000

    permission mistake usually naturally combine

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
