8ba2ea01f430e138b109b80508c6b1c02641cd3e
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:38:28 +0000

    mark injury minority new situation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
