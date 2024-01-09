0ef186371cdd76e8367a2362f8a77f4b13650c0e
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:18:34 +0000

    worth hardly spray care goup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_work(force, distance):
+        return force * distance
