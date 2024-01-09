be552af82c7b69bb10b2b049078fb90f869859a6
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:02:16 +0000

    godown perform address dry miss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
