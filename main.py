b49d7ff5eda8f15a3109470c10becac70706edd5
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:05:00 +0000

    correction welcome black sort empty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
