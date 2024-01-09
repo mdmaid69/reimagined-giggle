7c8bf914d190bcb7e905d068c752507755cd28fc
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:13:22 +0000

    constantly aim sister calloff clue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
