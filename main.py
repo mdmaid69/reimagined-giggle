0e80df1734efc3b9f50fecdaa31ae54c134b9ce1
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:14:39 +0000

    owe hope crowd speed science

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
