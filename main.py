21bb12580369e16d0179426b034313bb3c6573f9
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:37:14 +0000

    full figure introduction record show

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def cube_number(x):
+        return x**3
