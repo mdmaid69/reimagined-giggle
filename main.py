62da6649c5f143af4bee4ab724cca2c5ff79ee00
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:48:18 +0000

    Cc blame size low gooff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
