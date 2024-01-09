f6c7b06f0fe9ea3222db47605fd7cd7022285225
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:41:51 +0000

    Ll steer date under it

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import collections
-def create_queue():
-        return collections.deque()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
