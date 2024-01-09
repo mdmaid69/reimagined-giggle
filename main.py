82cbbbe1b0c9a14e2993524b7b89ce4113a5fee3
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:25:46 +0000

    down little fortunately best bend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
