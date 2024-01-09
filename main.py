2b196a530ebb2040c77d39afa604fe91a5df4d64
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:06:40 +0000

    top equal thankyou protection roll

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
