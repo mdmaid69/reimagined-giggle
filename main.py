026c60023aca6ae69fef1e75aba09c58b3dc4455
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:14:56 +0000

    add weather underneath mend tough

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_power(work, time):
+        return work / time
