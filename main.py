751827f1279cedae60a75a09b6bfe22c0d5eae12
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:05:23 +0000

    partner uniform middle fast bringdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
