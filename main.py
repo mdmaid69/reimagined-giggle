ef6885243d45d1f94deddab1a9ad5d37b88ab9a5
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:25:17 +0000

    ask corner scream chat transparent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
