3006ecaa11e81e41dc0a7c6edb4e0d5e69e2f7a8
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:30:41 +0000

    height thought trick wonder maintain

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-print([x**2 for x in range(10)])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
