3105d7f3ae09d09c271aaaa7ed33ce2c592b2926
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:38:19 +0000

    activity morning consist raise close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
