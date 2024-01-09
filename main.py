6dddf44cb092ec5181472c98b2650e2b1bc73b05
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:12:06 +0000

    depend squash cheap ill steep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def reverse_string(s):
+        return s[::-1]
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
