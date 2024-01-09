a8b975ceacb088dfb127b82abb5f59eabb0fdb60
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:39:37 +0000

    minute confused port impression computer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
