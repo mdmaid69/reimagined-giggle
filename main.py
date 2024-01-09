7193b340b5ba84f79ffe4332e4e33b8db5153be1
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:47:15 +0000

    stamp makeinto attempt disapprove valuable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
