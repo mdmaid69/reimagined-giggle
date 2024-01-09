f3edc6dac61497aa9e6a0fe672f6d1e253efce1e
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:13:58 +0000

    achieve bit in past comeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def remove_duplicates(lst):
+        return list(set(lst))
