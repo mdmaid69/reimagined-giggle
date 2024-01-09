2cac28beea2690f29c7f682a2787b4ebc94732f7
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:01:43 +0000

    naturally separate stop border mass

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
