54312ec137c15b6bee421f89b98d5ab43f3cc24f
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:06:31 +0000

    involve sell gentle throw test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
