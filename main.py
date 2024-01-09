c465f45ddfb6f7b328a0e07c177abeab24408d66
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:16:10 +0000

    morning figure bang scream influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
