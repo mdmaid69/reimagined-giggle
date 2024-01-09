cdc408d457542b46026e174272f090bb26f7a12a
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:48:00 +0000

    its full powerful reply bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
