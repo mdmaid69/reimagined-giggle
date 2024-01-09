172251e2c6e77548e8e3365e7991fd8d0334a3a7
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:27:47 +0000

    use throw refer listen add

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
