24875cea6324db7c536fa6a16e177d5fa55d5b87
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:42:18 +0000

    money form rich account replace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def cube_number(x):
+        return x**3
