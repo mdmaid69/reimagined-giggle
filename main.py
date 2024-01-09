77645ac8e697f4a3edef12bce90257e78524b1f5
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:13:58 +0000

    exact suck crash rent gooff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
