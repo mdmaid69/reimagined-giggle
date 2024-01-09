9077f2431997e1297ab9b498368d0de472b36d51
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:03:12 +0000

    place place close rescue calmly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
