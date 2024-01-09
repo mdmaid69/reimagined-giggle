98511f94d8b2f7dc5aed68a121209295405cc040
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:59:21 +0000

    before level unhappy free comment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import datetime
-print(datetime.datetime.now())
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
