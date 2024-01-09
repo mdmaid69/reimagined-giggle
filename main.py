a55730dd7032b57ae89cb29bd659f75e178bac1c
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:42:39 +0000

    month responsibility computer journey ever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
