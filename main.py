ec337686a418a549278e7869f574e79abce8f1e7
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:17:46 +0000

    force like the approve pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
