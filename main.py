f0cb89bd523b9205a87a696d78d89bd2c81a094b
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:37:11 +0000

    because beginning frightened only sight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def find_min(lst):
+        return min(lst)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
