7ec52cc63b3fcaef78319aa4765bef0e8988ce26
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:08:57 +0000

    bus jump might importance expensive

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def is_odd(n):
+        return n % 2 != 0
+def greet(name):
+        print(f"Hello, {name}!")
