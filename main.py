b99cc1e33c13b37288076d431ae1c0bd764f8b30
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:56:11 +0000

    OKorokay Ee search bag pass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
