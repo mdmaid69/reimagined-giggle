b5b6a82e3383d3b9faa9e281136390deea17f3e5
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:06:39 +0000

    large me wall callout clean

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
