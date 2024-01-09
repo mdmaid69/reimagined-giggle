c55689899718b9d2044bb78ae03a00aa96512473
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:49:26 +0000

    disappointed deliver opinion start be

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_power(work, time):
+        return work / time
