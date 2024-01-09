c54ec5bb563ada860d1cea81c7ab3b4c6b6283f9
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:43:22 +0000

    fast Hh perfect rather feeling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
