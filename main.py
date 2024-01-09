2286f42ea771f502dbe290b75d716ac62189c0bd
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:48:39 +0000

    turnout time responsible mother question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
