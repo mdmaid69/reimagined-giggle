0d135f626e1e047ae0e6ac06ec78ac037adabad6
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:24:17 +0000

    solve sorry mark active calm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_bool(array):
+        return bool(array)
