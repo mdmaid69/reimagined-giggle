ec977b4c29dddcb8689c287447013fe6b33b5315
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:59:41 +0000

    deep tone truth walk wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def get_array_as_str(array):
+        return str(array)
