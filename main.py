c87455ab5993c6aa5a301ecfa741084a05fb9d43
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:32:15 +0000

    black thick up less agreement

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
