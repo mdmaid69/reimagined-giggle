eaab94e00f0f894a34b57e007ac5e4d5e0505963
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:03:47 +0000

    negative gather chase run unless

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def is_even(n):
+        return n % 2 == 0
+import collections
+def create_priority_queue():
+        return collections.deque()
