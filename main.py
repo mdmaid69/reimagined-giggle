f796dbb13c62ae783558132c1074cb58144bfcc7
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:44:46 +0000

    recently cross report rush order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def sort_list(lst):
+        return sorted(lst)
