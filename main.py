6c75d3d09b72c8932fdc25b0c0a50a873808173b
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:20:43 +0000

    profit several operate start lower

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
