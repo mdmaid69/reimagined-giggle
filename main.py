85a34a7a029b88722bdfeb3bcb072573bd6b9cee
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:10:43 +0000

    Ss suckme interesting impossible keen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def convert_to_octal(n):
+        return oct(n)
