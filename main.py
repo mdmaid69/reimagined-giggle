fc02d6121230b5d00b5cd0664c3252758bc642c1
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:32:53 +0000

    during circle belief wake laugh

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
