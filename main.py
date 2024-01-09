77b87bc8ae219f9a1da38f887ab6c0b981f532fb
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:59:43 +0000

    teach somebody fortunate us comfort

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def find_unique_words(sentence):
-        return set(sentence.split())
+import math
+print(math.pi)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
