80514713b45554cf29144bcd4237315acdff744d
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:04:47 +0000

    own wrong gather smile future

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+x = 10
+y = 20
+print("Sum:", x + y)
