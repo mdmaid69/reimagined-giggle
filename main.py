0ec0c17d87e9c293bd11b6cd5d4356562dde6377
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:50:05 +0000

    badly lookup juice steal interrupt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
