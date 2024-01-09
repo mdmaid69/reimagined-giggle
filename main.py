d779ad63e7c3b5766201b9544faf9adfb5a7f972
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:22:06 +0000

    grade card autumn increase respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import array
+def clear_array(array):
+        array *= 0
