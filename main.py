df57e17497ce6d216da6a3ac950528fc21531e3b
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:08:50 +0000

    somebody disturb getoff fail emphasis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-for i in range(5):
-        print(i)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
