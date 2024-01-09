5438af032eaa91a6c12aa137362463185c0135dc
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:10:32 +0000

    little advice keepoff cry improvement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
