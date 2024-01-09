43c68259bf89fdfda4238b9eb734ed405c9b5a3e
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:40:04 +0000

    damp cruel position worried cross

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
