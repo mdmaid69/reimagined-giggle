e598de5afe772590c2477bdd1c32efe77f2b2f07
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:16:26 +0000

    turnup happen woman congratulate turnup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import tensorflow as tf
-print(tf.__version__)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
