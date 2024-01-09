4a7be5ec89bd76e658bf0ad29750c6add15153d3
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:56:01 +0000

    dangerous thin deal couple obtain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import datetime
+def get_current_date():
+        return datetime.date.today()
