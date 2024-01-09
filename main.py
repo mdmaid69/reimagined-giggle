572d4f17454a798aa1d2ee9841077db6b9ec43d2
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:32:53 +0000

    sharp choice interested seriously bye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_max(numbers):
+        return max(numbers)
