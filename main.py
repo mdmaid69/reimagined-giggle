14082d01e92012410c92789d558a1cf5fed159be
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:03:48 +0000

    bus care beside steal shoot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
