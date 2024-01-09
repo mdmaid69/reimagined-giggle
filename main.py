a4fe8f93de0c84239e9fcaf5604630175c23edc8
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:56:51 +0000

    increase because mix bottom emphasis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def add_numbers(a, b):
-        return a + b
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
