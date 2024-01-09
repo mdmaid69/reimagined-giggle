4f48b16176e437a6b0451b4e412c26a0397a5170
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:42:33 +0000

    shop class there bringup Zz

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
