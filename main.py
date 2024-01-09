0c589df40e85166fb26d7dec57c88abd0eb70d94
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:54:13 +0000

    Pp appeal present friendly pair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
