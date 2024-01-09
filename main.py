39ded6c5a863bc258122bd5e8a389feccc6dd12d
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:55:55 +0000

    variety which definite wide Pp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_as_set(array):
+        return set(array)
