119345c2d18f706155610da71b58ad38a6a59880
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:09:25 +0000

    control even kind fetch who

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def multiply_numbers(x, y):
-        return x * y
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
