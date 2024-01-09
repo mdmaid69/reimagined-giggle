fd3f45f519de560f60d9a0d406f693ec025f68f2
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:45:56 +0000

    table learn they there make

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+print([x**2 for x in range(10)])
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
