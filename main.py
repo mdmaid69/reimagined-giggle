c1c0241a18976cd4a8b30f4541cbd4373aa7fec9
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:16:15 +0000

    perhaps slight stop flat more

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
