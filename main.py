e923d7c982edb6c4c279f927dc11fc8b2e604f68
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:02:09 +0000

    complain we kind joke better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import sys
+def exit_program():
+        sys.exit()
