b7eeed05db262ebb41235944326c212d38a1d9bb
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:40:56 +0000

    beside better mistake refuse scheme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
