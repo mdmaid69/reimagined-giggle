27d2cd57be95838d1b9ad79d2e334fdbc2eda69f
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:48:22 +0000

    club comeback rank Tt offer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def get_array_index(array, item):
+        return array.index(item)
