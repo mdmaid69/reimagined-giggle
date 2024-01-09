95a2d3e7f32f3d628a3cac45d1a3ac2faef13058
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:49:34 +0000

    wet pay though themselves row

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
