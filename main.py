0c54efb9400fe611dc09e4baa880956f90cce26b
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:42:18 +0000

    getoff talkinto lock imagination value

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def sort_list(lst):
-        return sorted(lst)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import platform
+def get_os_info():
+        return platform.uname()
