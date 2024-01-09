ddf9cd5e4f9f13f5a792c544b967c5fc8eef6d04
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:42:11 +0000

    build convinced church rain almost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
