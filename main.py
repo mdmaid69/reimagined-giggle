a3e79532ceb35f5fb42d04ddb3cd288a420c4fb4
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:31:16 +0000

    disturb effect grade unless tune

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_density(mass, volume):
-        return mass / volume
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
