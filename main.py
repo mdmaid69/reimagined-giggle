4dd47ee6f982ec1fcbb802aa82143037fb556b1a
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:53:34 +0000

    demonstrate to believe bringforward squeeze

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
