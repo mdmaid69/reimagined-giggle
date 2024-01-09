242a5eaf0d8b370ede740367702f7f126f4e1d38
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:05:24 +0000

    lift reserve male reduce material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
