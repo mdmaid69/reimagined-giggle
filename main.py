3ca77e745263d80eb876e42d0b59e02728e19b84
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:18:45 +0000

    loud aim basic work rough

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
