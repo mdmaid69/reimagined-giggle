870eaec80e3d1e8120a442d2f7fcb539b1c485c7
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:14:12 +0000

    lose important uniform decide interrupt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
