9b89e494da0cc4c19ff220d05c2662d5b97d0c88
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:04:23 +0000

    opinion strict information corner practice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
