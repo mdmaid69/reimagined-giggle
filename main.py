eade7199aac4ca3ef0fb124944e59c2687d1b4a2
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:47:03 +0000

    workupto cover disagree church much

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
