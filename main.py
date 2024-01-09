eb8b66bf60f631cf45d9ffef0bb9097075c2b3bf
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:10:59 +0000

    relief translate silent slow sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
