d30069e78e7abfb95d699e2bcef3750403fae35b
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:02:17 +0000

    second home Oo mysterious door

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
