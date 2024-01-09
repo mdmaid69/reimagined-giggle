4690176045486aaaa809c42f1ce5f711519f42dd
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:22:14 +0000

    beautiful brain miss fear myself

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
