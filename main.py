39b8cdd636aa41ee6322fa3ca43df3c7ea4bfea7
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:24:36 +0000

    benefit so yesterday length callat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Reversed:", text[::-1])
