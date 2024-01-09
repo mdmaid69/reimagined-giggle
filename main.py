750255a91969a4965649f6d865da292d7c6d310f
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:18:43 +0000

    breakfast brief dress add switch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_density(mass, volume):
+        return mass / volume
