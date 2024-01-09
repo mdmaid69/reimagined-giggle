3bdf174f251e6bb2ffe99a0aff66a75adbd08012
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:53:28 +0000

    past estimate quick press squeeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def find_min(numbers):
-        return min(numbers)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
