caafcff81d457d8438e44015539b902cfab37c86
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:27:22 +0000

    right hang keepaway cope flood

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def clear_array(array):
-        array *= 0
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
