80a5d1c77c5d233de3e37b0a7ceb38eacde386a8
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:02:47 +0000

    tap peace threaten sentence unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_work(force, distance):
-        return force * distance
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
