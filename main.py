e37ff44571d77ba11db38db91a922d215ba13239
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:41:14 +0000

    treat false suit fill people

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
