848323eac675d79dee0673338ef6f52b32e2d804
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:22:25 +0000

    wish Ee ignore intelligent platform

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import sys
+print(sys.version)
