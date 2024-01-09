5f06433fdf6aaa04306d6fb4b3276910f0bf56e6
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:17:54 +0000

    contents game backwards so remove

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def subtract_numbers(x, y):
+        return x - y
