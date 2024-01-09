b9066f5b56143eb338ef636cfd32502689295f4b
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:02:47 +0000

    short pile file train belong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
