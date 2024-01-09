7a45055062f8d45d1ad38a2e21db8bdc9f369436
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:26:47 +0000

    forward shape interruption if slide

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  def calculate_area_rectangle(l, w):
-        return l * w
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
