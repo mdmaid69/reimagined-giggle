15834eb32aacd9fd896ed34825aac270c64c4c27
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:01:47 +0000

    weigh scene tax print door

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import tensorflow as tf
+print(tf.__version__)
