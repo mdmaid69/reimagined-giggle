1dcecc29113e8adba0d32e35381363d944e13f5b
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:55:31 +0000

    door straight turn slight comefrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
