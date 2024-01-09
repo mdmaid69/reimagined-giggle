8937038861a28dfbdecf195e8894200835086ae9
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:23:28 +0000

    thick character interview score touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import matplotlib.pyplot as plt
   def plot_bar_graph(x, y):
         plt.bar(x, y)
         plt.show()
-text = "Hello, world!"
-print("Words:", len(text.split()))
