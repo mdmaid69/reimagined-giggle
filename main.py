2fa67b9f92cd4d661ca7473f9d65837ae98eaee4
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:55:15 +0000

    people sore shame private snow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  def remove_duplicates(lst):
-        return list(set(lst))
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
