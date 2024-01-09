810ce9632c01ecbc26a98c06fcceeae46f2a7120
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:19:58 +0000

    grade group ball breakinto previous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
