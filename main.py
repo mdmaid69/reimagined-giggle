b9196a1d745fc23187ff2dddf998e7358b562014
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:10:41 +0000

    boat benefit hard remember wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def square_number(x):
+        return x**2
