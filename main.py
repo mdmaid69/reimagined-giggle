f51526b5d1a1d8b5b9ab3a1e91747004426b4337
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:58:20 +0000

    will basis tooth admire drink

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
