0684aa4bb656036a2c97bc57d0a062bc89cdb109
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:16:06 +0000

    path ourselves quickly progress book

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_speed(distance, time):
+        return distance / time
+def find_min(numbers):
+        return min(numbers)
