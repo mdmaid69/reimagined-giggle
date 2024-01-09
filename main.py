7ef27248437d60e46e3a2187566f6ddc11c8c70c
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:53:15 +0000

    breath holiday pleased software note

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
