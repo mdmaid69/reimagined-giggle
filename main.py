fc56613afd38951c62b9c19604cd6228fd9bdacc
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:45:45 +0000

    attract embarrassed rate event fly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def convert_to_binary(n):
+        return bin(n)
