9cdb1db6585dd0c8691a3dccfa88e51663e8cccd
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:35:07 +0000

    exhibition equipment national remember continue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def sort_list(lst):
+        return sorted(lst)
