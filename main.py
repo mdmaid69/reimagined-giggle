116ae2b78b5bef3717acd48d82b79cf109fcefe3
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:23:44 +0000

    winter cruelty vision service manner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
