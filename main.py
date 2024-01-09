fdfcc25712fb3770d80e79e773f31f59c50baf8e
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:19:12 +0000

    above part event write finger

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(lst):
-        return min(lst)
-import array
-def get_array_as_set(array):
-        return set(array)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import sys
+print(sys.version)
