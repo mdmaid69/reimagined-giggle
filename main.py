7cf9d3c9976852677c61a5eef17d325748bec049
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:34:14 +0000

    lunch production me pattern bringforward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
