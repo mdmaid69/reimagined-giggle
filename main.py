c560c0a9bb80532f830028c6c2d201d36fb3faed
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:07:47 +0000

    share another wear crisis keepout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
