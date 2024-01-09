aff61c8031bbd9333cadb776ce3c32ed0ad129df
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:36:14 +0000

    such regularly sink introduce ahead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
