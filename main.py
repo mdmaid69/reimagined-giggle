f8b3e98deee5d50212ad91dd3975015c0e24020f
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:44:06 +0000

    final exercise wild bring steam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
