41562614a3ea95ebc5393bfdd1177ad79d298db0
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:50:57 +0000

    limit do scene unpleasant anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def is_palindrome(s):
+        return s == s[::-1]
