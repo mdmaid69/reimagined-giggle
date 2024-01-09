dd6f89755acff34d02e6c07b6ed16b05624bc555
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:12:43 +0000

    ordinary cum speak wave party

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import time
+def get_current_time():
+        return time.time()
