2c948a1ee25a0003c156963fc23b3a69775be5ba
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:27:23 +0000

    look fasten use forward trip

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def reverse_list(lst):
-        return lst[::-1]
-import time
-print(time.time())
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
