9d28ed4c6cb17c42a11f8e2cc98399e6abf8ecc3
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:25:56 +0000

    coat pleasure luck breakout female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
