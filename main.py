e12be05749a9c46ed46aa9d5f491786473a394b6
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:32:21 +0000

    building leave cum most name

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
+import math
+print(math.pi)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
