89d6f5f7d74cdb61fcc7a585907bdb8da4425ede
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:09:14 +0000

    somewhere recover whole city hers

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def count_elements(lst):
+        return len(lst)
