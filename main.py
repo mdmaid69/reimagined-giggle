e29f4a08c069b61ef6f316854a5fd28ebfa377d1
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:55:27 +0000

    secret mind board speech winter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+print(os.getcwd())
