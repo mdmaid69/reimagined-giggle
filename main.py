ad0bc993fabf3107691c33254d7e281261aa6b0b
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:59:03 +0000

    loose tongue tense prove produce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
