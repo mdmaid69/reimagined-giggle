163bb433aa4eeb69f7a6d60c1fdc9af81ee67639
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:40:26 +0000

    conclusion muscle involve angry we

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
