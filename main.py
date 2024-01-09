f0f08f68055434f1b3d4052d3edb461fd83f6f6e
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:27:38 +0000

    bread waste comein backwards involve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def greet(name):
+        print(f"Hello, {name}!")
