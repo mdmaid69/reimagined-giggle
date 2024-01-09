3748a3370ebcbf040572a5aa1200cc1013258df1
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:25:41 +0000

    sharp emotion equal feel few

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
