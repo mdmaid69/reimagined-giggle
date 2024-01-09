f2959ba47b083d295edbe65eac662b3fc15f8385
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:10:33 +0000

    happen culture if direct news

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_string_from_array(array):
+        return array.tobytes()
