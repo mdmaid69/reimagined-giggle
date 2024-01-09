a9a1b49fa24dd5a981bc3a0142a60f18ea3f9f0b
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:51:47 +0000

    getout personal set calm wall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
