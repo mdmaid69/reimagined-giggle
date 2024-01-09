6a9a29866ef3b552d21cc00aa09de1567947abb2
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:32:55 +0000

    money collect recently slight catch

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_acceleration(speed, time):
+        return speed / time
