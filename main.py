153fe77ed7c9a3e04b27a5d96e88208368416031
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:16:51 +0000

    far guess impressed regard between

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
