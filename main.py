9c249a3a12297bb093ea1995f21c2f109d11f18b
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:52:14 +0000

    add spot visit drop comein

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
