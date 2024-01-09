7a249f8b7612ff72a07ce0bf3d86a9ed9f5fa4ac
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:37:23 +0000

    guard earth yet nowhere breakin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+for i in range(5):
+        print(i)
