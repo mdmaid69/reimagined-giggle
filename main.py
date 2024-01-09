f858ff1cc4a5309343f455ec332937f9365b74e6
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:47:24 +0000

    answer roof dream well language

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
