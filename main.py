d5d4b611f493e8e21b815cedd892cbd7d919ff22
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:39:23 +0000

    centimetre boil minute anybody suffer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
