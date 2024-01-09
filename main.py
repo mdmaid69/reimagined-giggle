cfc8f8b275d82aed8f022bc415823f7ed27f483e
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:41:11 +0000

    threaten upstairs false factory meal

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
