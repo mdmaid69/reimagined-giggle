8e4b0c8daaf52e476f4b1d3dcf79e970461e2395
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:48:49 +0000

    talkoutof through welcome so pot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
