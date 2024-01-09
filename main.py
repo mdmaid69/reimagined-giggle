b890757292abb20f709b1c93af1c98f923aec9a6
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:07:55 +0000

    world shut apparently region bar

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+for i in range(10): print(i)
