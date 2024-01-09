1004fe856fd4828d92aead8650318dd7d4f2085f
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:21:14 +0000

    goout inquire learn pleased recognize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
