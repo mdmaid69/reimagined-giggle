2273f3ed3b0e650d0a2cabf5ccba3194c1790191
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:10:24 +0000

    Gg war mean along stamp

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
