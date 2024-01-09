3fbdb012fc10457421f4094e7ef52a41f5f99cb5
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:52:32 +0000

    community light character lend another

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
