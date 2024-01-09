d07882c7faa68b24ae9b2c086c6e030fa57f2c58
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:42:03 +0000

    declaration picture total lose total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
