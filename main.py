c1f5a2724a0e8e990233c0d715a4da415acaec3e
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:02:26 +0000

    miss jump basic goout will

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+for i in range(10): print(i)
+import array
+def get_array_as_set(array):
+        return set(array)
