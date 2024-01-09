2b0ecd76223fc02d40ff3d62ec4ea6a1e26da6b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:04:50 +0000

    Mm absolutely himself nature scared

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+for i in range(5):
+        print(i)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
