e38a6448dffdb95c5b99d05962399549e8e08fa7
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:41:36 +0000

    keepaway continue rush up woman

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
