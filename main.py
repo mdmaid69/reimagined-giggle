1302e41fc7c319cf208a00239b917c1a78f51de5
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:22:04 +0000

    exercise system goout ticket secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
