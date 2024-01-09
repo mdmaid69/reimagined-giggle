1bdc2ccd4463d20e2a1714d28836c8bbeafb2224
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:15:42 +0000

    foot wear pure deliver never

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
