9884429f8981e3af9a71bd78f3c77a3fe30135bd
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:09:12 +0000

    control turnback paper upset flavour

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def multiply_numbers(x, y):
+        return x * y
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
