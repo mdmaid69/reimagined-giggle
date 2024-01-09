dce4db2f10a067c6c57c0029f5f5680e94722e6c
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:29:09 +0000

    gas slice reaction rob getoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
