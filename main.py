4204c22ad64e947a345de26d637ebce16cd7e1d0
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:49:28 +0000

    waste they after platform ordinary

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
