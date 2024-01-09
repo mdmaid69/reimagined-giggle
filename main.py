c7da6216afd97f7f74080fe17d10f4f2d093f005
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:23:41 +0000

    rank taste regularly bringforward excuse

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
