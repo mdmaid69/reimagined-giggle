c965f72108e09df0cdf29838624fbc51abb87093
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:08:20 +0000

    language protest Ii sit usual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_list(array):
+        return list(array)
