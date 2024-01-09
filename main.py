df861ac55c7ca1f0ee373fde13ce5925f0c57d31
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:06:53 +0000

    before proud satisfactory near door

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
