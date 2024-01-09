f8fdebd7c63d890420a459c286cb9d86b99e6343
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:50:19 +0000

    whistle strike thin what adjective

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import random
+print(random.randint(0, 100))
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
