823f5610a0bd43deea86528fd0bec5926d34adeb
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:43:48 +0000

    decision blind street half smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_area_rectangle(l, w):
+        return l * w
