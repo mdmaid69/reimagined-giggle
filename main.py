347aee79329855c3709ac09483e5ff738624dcaa
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:38:48 +0000

    permission demand really wall offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
