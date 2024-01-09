baa9a91659e812dee5939bd685edb688dbbd3582
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:39:27 +0000

    report trouble style public noisy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
