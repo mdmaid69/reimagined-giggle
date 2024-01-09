4a43663ec5f52790ec8295dd02650955c1d5575b
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:54:48 +0000

    floor shoulder answer difficulty question

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
