8254864f3bdc2f614687b0a7a919190be0a3bd2f
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:46:26 +0000

    fence right rule container Tt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
