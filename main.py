c26911ec7da585c5a7b74fb7d888d47d372d4f62
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:40:05 +0000

    afraid plenty exaggerate near separate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import sys
+def exit_program():
+        sys.exit()
