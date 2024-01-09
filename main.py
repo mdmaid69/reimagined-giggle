f8ba0bf06811a498e944d18911355c50c1baaa89
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:20:14 +0000

    bottom big patient possession exhibition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def convert_array_to_list(array):
+        return array.tolist()
