6306b340459123d710528cd7f6475cb898583e97
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:30:39 +0000

    situation sport stay politics arrive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+print(math.pi)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
