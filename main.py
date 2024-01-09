b155b2bbc160cc7ceeaaa83d3e212410eea63c9b
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:25:21 +0000

    shop period fight in service

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import sys
-def exit_program():
-        sys.exit()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
