b9ebd50438b540b463f3d2ad2b6a33cac3b35fec
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:39:07 +0000

    spite borrow busy makeinto comefrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
