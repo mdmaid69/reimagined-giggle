138f5fc288fc8cd9c8f6b89847539af1d7ebe470
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:43:09 +0000

    branch quantity violence blind low

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
