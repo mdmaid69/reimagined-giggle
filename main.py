a5680f38b41e96121cbc5486f48ae6ebb4e63c19
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:57:02 +0000

    rate visit knowledge area during

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
