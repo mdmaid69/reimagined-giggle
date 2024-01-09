c6ba1dd2e9bfcdee15721b5c1f1ca98ef44755fc
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:23:33 +0000

    certainly nature push takeoff forever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import os
+print(os.getcwd())
+  import sys
+  def get_python_version():
+        return sys.version
