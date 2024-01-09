be2b5cadab09eee7ef32e4fdc1b5ce526ed44ccf
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:15:50 +0000

    later pen foot fill independent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
