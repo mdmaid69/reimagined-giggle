e0e4b9edf5a27d9a37b0ea48afdf29ba01cec159
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:26:12 +0000

    set again basis pleased consider

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
