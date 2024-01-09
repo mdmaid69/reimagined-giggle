076b41e4b59b2ec864d4db705e8381a903e0f9c7
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:52:49 +0000

    nurse shop occur question convenient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_tuple(array):
+        return tuple(array)
