60ec5aba75d4d2849b69a6c280fdaebdd58c9444
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:47:25 +0000

    importance round clear 569enjoy calmly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
