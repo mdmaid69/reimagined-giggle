5881e21a41ee4795d434985092634baf7be1bffd
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:35:13 +0000

    remain come cut sensible mind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
