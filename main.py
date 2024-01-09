d9cb599cc29176bf509f8230fd6ff60185ed6f67
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:44:38 +0000

    dear suck coat sensible capital

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
