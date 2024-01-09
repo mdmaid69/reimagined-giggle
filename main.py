ead9c18c64557857f2cbb794cd1caa214ee1aee8
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:13:17 +0000

    situation period such object fairly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
