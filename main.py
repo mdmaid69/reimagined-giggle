f90bba57e2ad52a16435083df2c70dce1ff3cd4d
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:59:44 +0000

    dick once makeup character please

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_density(mass, volume):
-        return mass / volume
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
