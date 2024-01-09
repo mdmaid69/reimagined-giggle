ca163e3f3fdc3506d2db72bef4f5e1e2a0f5b1c5
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:03:02 +0000

    right fetch nonsense formal bread

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import sys
+print(sys.version)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
