6c283dda7fc72169c567423f1beb1754f9a5171f
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:41:38 +0000

    stay answer operation delight lift

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
