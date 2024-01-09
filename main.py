a2c569cc4b11df30619d6e62ffa9fcb3e522af61
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:23:55 +0000

    treatment quality come instant apply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
