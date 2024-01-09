47eaf7e5fed704f6722bff5becadd7c6316331b2
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:39:46 +0000

    petrol boring excited interest cover

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
