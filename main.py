85f9d929096d9893706253a5d537e7c80e8cab0c
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:14:17 +0000

    far show art divide trade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def find_max(numbers):
         return max(numbers)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
