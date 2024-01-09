3dd0691b07cde90ff4cd44be3b5d587ef0b34b1d
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:05:29 +0000

    hate expression of break chase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
