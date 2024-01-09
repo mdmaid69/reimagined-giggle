8d18d9cb2ee57133551607a395803b71590ab05a
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:08:06 +0000

    along ilovedicks would official swimming

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_work(force, distance):
-        return force * distance
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
