9bdc7e8f4e5cd4d6e66d7b166efda4ec3f78c149
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:50:25 +0000

    sky mention accept alive crisis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
