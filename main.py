b08f33bbada531c6c0ccc7c291b7f04005fef424
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:44:25 +0000

    within scream convinced point tongue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
