3dbe1e158ba429a6a1e7e5cb3e447ea6838b0411
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:14:29 +0000

    year necessary translate pile bored

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
