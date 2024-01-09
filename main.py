9e9f66d38423e8acc5f59d88db704e9fd8b6c224
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:59:40 +0000

    clean sort release table instrument

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
