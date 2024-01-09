880ead3cf5b43b248945a729396a3af351b29da6
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:18:29 +0000

    giveback lesson argument spill accidentally

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
