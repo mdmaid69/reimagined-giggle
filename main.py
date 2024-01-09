d87b9d0c52d40b9c8b2b63a20215acf9f36f794e
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:53:04 +0000

    situation heat old well shoulder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
