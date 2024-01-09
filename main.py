ce488cde5a55ae2cae1786160d1974c3ec252cec
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:39:05 +0000

    pin history move either mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
