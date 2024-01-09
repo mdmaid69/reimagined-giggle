a36edc40aec5e9c1c210ee5501cbb87d0e5fe734
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:29:28 +0000

    under under nature snow view

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
