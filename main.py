a80c13bf49fadb48fec18b18f54f07460c7b53f7
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:23:12 +0000

    constantly thankyou tread weak confusing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def multiply_numbers(x, y):
+        return x * y
