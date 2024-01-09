ad7e9c568c677c81dd97db731e52d158ab6a1f68
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:14:47 +0000

    disturb meeting mail admire whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import array
+def get_array_index(array, item):
+        return array.index(item)
