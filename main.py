f389c2595780354cfadd1dab6c49382087e2c8b5
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:39:05 +0000

    holdout defend lucky to write

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+print("Hello, world!")
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
