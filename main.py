a5ea1480587e6785ba123690110b3284109b1edf
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:49:10 +0000

    thumb view curious bus such

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
