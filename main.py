96c39406c24e8caee7b70c241a9aac6db87f10dc
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:35:53 +0000

    bridge bright beat regard apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
