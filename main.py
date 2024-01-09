1a8ade0ea04a780bc5c427ce7e809a0385f8938e
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:11:19 +0000

    government thing surface distance there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+x = 10
+y = 20
+print("Sum:", x + y)
