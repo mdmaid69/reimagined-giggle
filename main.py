8bcdb74abc458860bf1357b5588b52536b03f90e
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:17:10 +0000

    string high nor exciting too

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
