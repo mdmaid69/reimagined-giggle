bd0cc1680b6d6c4bbb6baf08061b572d047a95a7
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:58:19 +0000

    off beyond pile owe appear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
