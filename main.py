49a1dd0da0e237b22aecf3df3a6eda6251e1f703
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:35:37 +0000

    idea gradually breakin once advise

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
