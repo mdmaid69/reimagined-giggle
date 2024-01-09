b746ef0b0b6e3e6f37fa5bd90ee8a7a013088823
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:16:45 +0000

    be emphasis set control do

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
