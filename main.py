0b1d1103546c7209494e99e01c6e989313d6d171
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:09:34 +0000

    church important suckme whistle communicate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
