ec4c2cb3c5f2a85eafc76fe8849b602e66000e2d
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:47:38 +0000

    emphasis march allow carry justice

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
