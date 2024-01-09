5d69447f63b2017c74251393554ed61e046ec244
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:08:33 +0000

    turndown attention you available noisy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def add_numbers(x, y):
-        return x + y
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
