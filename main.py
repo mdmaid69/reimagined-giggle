22370c91197720f5ba50d45db6a5228772d2dfcd
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:07:42 +0000

    separate legal tape figure dead

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
