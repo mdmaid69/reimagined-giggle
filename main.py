f86b7f4fd50a4e0d548c9dca4c137ec74173e5d4
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:00:18 +0000

    report ticket cause poison squeeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
