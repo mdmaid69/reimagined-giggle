28306c3c922b62961db9f84f4d406eeda50f23d1
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:09:59 +0000

    mention capital capital radio play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_array_to_list(array):
+        return array.tolist()
