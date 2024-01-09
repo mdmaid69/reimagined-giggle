0003477bc1a25ca18500949c2437c0debf3df1f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:42:00 +0000

    fond minority science above spare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
