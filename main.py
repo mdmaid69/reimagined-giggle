2c350aeebc112c4b97627cf4d7967536cfa2686c
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:43:15 +0000

    door equipment suddenly undo of

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+print("Hello, world!")
