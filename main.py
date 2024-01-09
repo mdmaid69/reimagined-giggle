4b121112991191cd194ff3b22437c104af47e9b7
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:15:14 +0000

    inside chat rescue test treat

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def convert_to_octal(n):
-        return oct(n)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import math
+print(math.pi)
