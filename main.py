c8a96e93bf65d9c4e015d31d4bab3645661a72ae
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:48:05 +0000

    careless lonely calm giveup hope

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
