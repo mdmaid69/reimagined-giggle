0c7a41459e7fe4f684167d3b4ae07bae08519d40
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:33:10 +0000

    pen tread wonder reasonable eager

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
