c2f97ad1db1ec8d286d0f8d17107843c2f6cabb4
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:48:33 +0000

    present rank slice kick government

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-  def square_number(x):
-        return x**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+for i in range(10): print(i)
