eb8c63b6c35cabe1facffe75ed9b1f9719eca8ba
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:04:10 +0000

    cause label job community broken

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
