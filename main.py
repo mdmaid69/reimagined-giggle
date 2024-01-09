1ab6d9ea7b227573c507276576c8ebdc6e571189
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:13:26 +0000

    common myself afterwards out shoot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
