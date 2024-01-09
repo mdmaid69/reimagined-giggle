e289e37df9997d834aed6e900c866af19db2aa96
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:50:03 +0000

    pupil switch entry occasional speak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
