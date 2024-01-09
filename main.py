2c76fe30966e5061b0a1ea59e69a1f156bea0500
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:36:41 +0000

    future sticky love friend store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
