3b97de135ee14f2e37d34ea84a9b4c88f74b6e96
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:50:28 +0000

    home rise difficulty moon instead

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
