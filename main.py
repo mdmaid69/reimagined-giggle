f37272d32393041245b1e5d6f2240e348e49bfc1
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:52:02 +0000

    minority journey play easy pronounce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
