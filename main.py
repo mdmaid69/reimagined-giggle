c2012bb84e648ff78100b1c8be9df718f76d7ca7
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:30:20 +0000

    perform tough preparation earth aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
