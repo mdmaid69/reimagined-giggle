e7011967b9ace7e9b26e199085eb2cd608bc7e2a
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:22:57 +0000

    delicate Yy organize wrong wrap

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
