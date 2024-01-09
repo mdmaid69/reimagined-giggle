e67f50918010a88f1e2a18945ed493cd87339bb4
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:44:12 +0000

    later modern reliable lot pay

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
