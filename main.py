62d54279b326f6e048a2cb7ae7ee11d364ecefc6
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:35:24 +0000

    find bunch unless sentence seat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def convert_to_binary(n):
+        return bin(n)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
