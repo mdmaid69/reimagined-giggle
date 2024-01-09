6e0b748450cb70e72f7a8a5542a1e3ae3af0752d
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:51:36 +0000

    post giveup disappointed welcome voice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def get_array_index(array, item):
-        return array.index(item)
+print("Hello, world!")
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
