ba4151e3d4ab669810585c800ca114a9644d6e67
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:55:16 +0000

    outside probably score underneath wealth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_length(array):
-        return len(array)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
