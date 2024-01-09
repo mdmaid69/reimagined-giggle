5ffa970d98f9593d8837c9e9408e5b404dbad056
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:32:55 +0000

    satisfactory issue unless direct rid

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-x = 10
-y = 20
-print("Sum:", x + y)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
