376c54c9c4214f2c7006a288c1fbe20e1eb4c5fd
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:47:31 +0000

    goaway suppose neither turninto prove

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
