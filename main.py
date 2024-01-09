995945ab2ac5b13fef36f31f57f82a08b0d101cc
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:33:41 +0000

    convince roll oil kick base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
