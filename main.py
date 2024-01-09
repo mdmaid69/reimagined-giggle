cd82f267399659f433d103009981deb75c3e1b93
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:39:41 +0000

    basis previous possibly boring swimming

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
