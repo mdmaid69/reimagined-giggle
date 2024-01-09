b1553023507c5ff61f75a6cc550ab0b80d2b9eb1
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:24:22 +0000

    dress well plain notice pile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
