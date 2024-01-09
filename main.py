c9661c0fa1050ea143128c09bf5f4a043b85e4de
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:31:03 +0000

    wherever message frightened promise little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
