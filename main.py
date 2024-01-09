1ca330b115941eb37822bd295071e8544e9f0a3b
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:03:59 +0000

    avoid hear unkind bright getoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
