7bc44f7b48447f083dde482a137db75b26a39109
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:03:35 +0000

    knowledge post turndown turnoff above

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
