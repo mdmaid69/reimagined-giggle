7997bafeb57d4585f5ebbd6df67e07bf5b99d4f0
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:36:54 +0000

    hair actual force peaceful later

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
