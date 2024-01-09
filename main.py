e3620b955038cebf23e6ee00e46a6ab172878d32
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:08:59 +0000

    to worst difficult remark group

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-name = "Python"
-print("Hello,", name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def get_array_as_bool(array):
+        return bool(array)
