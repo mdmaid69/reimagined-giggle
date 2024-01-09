6f4bcdc806711325c1f7aca881847d3fd729623f
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:40:35 +0000

    will trip threaten trick reserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
