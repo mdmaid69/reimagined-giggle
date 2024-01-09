5a22d4d50913cf22c033ae35620281cbc0156b7b
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:45:49 +0000

    freedom sale lot remove will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
