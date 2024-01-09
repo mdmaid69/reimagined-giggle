2bc9e1df6f801048d2f76c683f7b277a9162cc8b
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:04:42 +0000

    emotion usually production power whether

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
