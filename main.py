f9b6de29d8dc517fc1ef7395c64ccbec491e4a4b
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:14:45 +0000

    public yesterday mine unit down

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
