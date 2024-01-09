5f9d7d26749d698df29785445b373fc842636e0c
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:56:29 +0000

    telephone month thumb memory rain

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
