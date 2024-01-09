859363d6b966c51f4beca458f1b2e6cf7915c00f
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:03:09 +0000

    calmly plan equal pinch treat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
