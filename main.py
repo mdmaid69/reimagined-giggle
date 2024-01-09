e23fb2959eaa5519c6e05c39080c1ddf65bd1678
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:01:55 +0000

    main process holdup whole crowded

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def calculate_pressure(force, area):
-        return force / area
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
