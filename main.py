595352bbbe674051a7f2608ba54ad93e90424142
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:56:33 +0000

    often arrangement lunch anywhere past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
