72c98268da9a9973929ce806d4d108ac78a3eec6
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:12:05 +0000

    speak keepfrom pleasure rescue float

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
