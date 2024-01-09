91e8784c4d0487c21ab35aa426114f3bab9ee3ea
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:02:17 +0000

    arrive land nothing normal unit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
