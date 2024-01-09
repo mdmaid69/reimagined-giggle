3d3d5fea55866ca85b2ccd8cb93610ba41648627
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:38:29 +0000

    interfere unfortunate opposite whisper face

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
