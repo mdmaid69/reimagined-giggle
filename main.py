b9cbaf534af579d7a9a32a9971c10cead1b3d441
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:30:15 +0000

    walk trap make comedown help

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
