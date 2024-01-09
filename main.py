be97f1928c3260a5eb93b8c495ba9e4c6f93b6a0
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:49:21 +0000

    treat rush later film board

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def get_array_typecode(array):
-        return array.typecode
-def count_words(sentence):
-        return len(sentence.split())
+def get_array_item(array, i):
+        return array[i]
