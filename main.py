d90d220d324e80b50e39dc28c9661a48ec39ac0d
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:36:02 +0000

    regard fairly Nn lock wash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
