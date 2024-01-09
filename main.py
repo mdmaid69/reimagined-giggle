52a00c34d60755901124f65b1eb7aa5bf4468a17
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:41:04 +0000

    left check actually pleased on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_user_list():
+        return collections.UserList()
