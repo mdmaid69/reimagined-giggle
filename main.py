2ca2ffd60c813d5658ba15eaf6e826b735bda1ab
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:01:45 +0000

    label workupto accept true short

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
