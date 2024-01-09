815daddf52f6df9091c1b731beee1e2a9f9eaf8b
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:42:27 +0000

    meet unfair argue comein OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import sys
+print(sys.version)
