ab3d2768590e16d3e94b3fabdddda15b5b4e1dbf
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:18:11 +0000

    try trade thinkof me put

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_work(force, distance):
+        return force * distance
+for i in range(5):
+        print(i)
