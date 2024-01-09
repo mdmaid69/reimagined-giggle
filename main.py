d17522df1d40df98da776208ffbc88d977f01ed6
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:20:22 +0000

    education impressive alone under food

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import re
+print(re.match("h.*o", "hello world"))
