efd745aec80a18d23293965b022a45fd753893b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:08:49 +0000

    shelter stand strike call comfortable

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
