455d545d0ef720eafe70c7f72a46bb94fccaab8e
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:49:08 +0000

    plenty me powerful star load

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
