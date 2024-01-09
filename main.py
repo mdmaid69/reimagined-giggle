f5564966010bbbb0651cd39f8411d76f1f7ac4fb
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:55:05 +0000

    copy everyone violent laugh join

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
