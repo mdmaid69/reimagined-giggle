4ce4fc37fe146264b12b283d850ddcd431e427cf
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:41:54 +0000

    group main check deep free

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
