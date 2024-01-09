bbfa90069b686e8d9e74b728073d712cae6fbb38
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:41:59 +0000

    goaway introduction cry language Jj

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def append_to_array(array, item):
-        array.append(item)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
