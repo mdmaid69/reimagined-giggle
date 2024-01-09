0ab9c3dd3e5c6c0279cd19785ad83a8ef792af47
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:10:52 +0000

    and classroom ticket limit natural

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
