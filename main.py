cfbe225b7eea973e9d50dd5df90cc7741bf50115
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:12:39 +0000

    top mine recovery cover raw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
