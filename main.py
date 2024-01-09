11d3b89d207c6e62b30313c1f83b23e54469b996
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:36:06 +0000

    flavour very deliver recover inquire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
