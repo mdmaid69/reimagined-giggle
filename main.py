fcbf6f9c87338fa975e0b90645899796b54a834a
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:00:09 +0000

    example takeout prevent introduce importance

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_list_from_array(array):
+        return array.tolist()
