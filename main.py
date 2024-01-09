6380bf29b120f2d5c436b5a2f4502ed4b9977705
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:25:11 +0000

    find absolutely hour sister software

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
