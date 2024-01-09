7c305cde931c2cf0774000f900776ecc5ce7bdef
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:29:32 +0000

    567enemy wonder risk rain sing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_max(numbers):
-        return max(numbers)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
