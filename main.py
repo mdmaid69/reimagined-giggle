a7e27f34ca6e443a9130d87d818f0d7523144a48
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:22:08 +0000

    number petrol trust object thick

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def multiply_numbers(x, y):
+        return x * y
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
