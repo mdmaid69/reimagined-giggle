f3754a3ddee7a62dd2efc6f3dd4559230a85649c
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:53:13 +0000

    fire suddenly inform getup area

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
