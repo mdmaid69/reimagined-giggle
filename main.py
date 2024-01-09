a35d0f23c32879af3fa45cc20de9c6dbf8976fd0
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:47:00 +0000

    truth write formal comment exam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
