caebe2350e1dc5485cc92ebcfff1e28261499e8f
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:42:37 +0000

    spread steady reply convince better

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_unique_words(sentence):
-        return set(sentence.split())
+import array
+def get_array_as_set(array):
+        return set(array)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
