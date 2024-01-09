20e17c9c4bad95a35de42faf2082d4c87818a406
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:24:52 +0000

    failure onto hit raw feed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_itemsize(array):
+        return array.itemsize
