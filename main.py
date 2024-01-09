b0f7e438ba97910ccdc2cef4816268e72ea4a468
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:30:04 +0000

    recover phone dish break connected

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
