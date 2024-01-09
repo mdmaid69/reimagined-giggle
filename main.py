a35b5942742bf60cd50b292b0f0f79e625689b4a
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:31:35 +0000

    embarrassing introduction his peace gooff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_exponential(x):
+        return math.exp(x)
