3b5373ab273dc16cb2e68dfb996c8d59dd803aa2
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:39:37 +0000

    just law look successful straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def convert_array_to_list(array):
+        return array.tolist()
