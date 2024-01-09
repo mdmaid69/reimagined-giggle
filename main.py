75a84d7758520df87ac4a940c3cee5174f7fb3cb
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:42:54 +0000

    machine live roof tell disagree

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+print(sum(range(10)))
