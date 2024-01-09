0bec9f855f6f43aaab5971f4594c061f1c10ddde
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:34:06 +0000

    error stay bringforward godown scheme

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def remove_from_array(array, item):
+        array.remove(item)
+  def convert_to_octal(n):
+        return oct(n)
