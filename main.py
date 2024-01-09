15aecdc4a4182882cc632242a92dedd814e463be
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:38:02 +0000

    dig material crazy advantage piece

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
