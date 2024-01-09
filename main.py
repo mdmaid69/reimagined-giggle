b753c5a496bc34855c15d73208bef4092a315335
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:12:59 +0000

    authority twist congratulations lead imagine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_density(mass, volume):
+        return mass / volume
