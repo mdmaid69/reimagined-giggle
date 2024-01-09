324288f4ea677b96c81b57d0ca9bfe653e2d1b64
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:56:57 +0000

    force flood mystery tall unless

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
