8971917013539683a528c9342619691147f348f3
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:04:04 +0000

    peaceful separate getout stop Pp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
