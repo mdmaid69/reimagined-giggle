c667f0af47e8a93bf0552dd8061d5f9f4cec8f2d
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:32:55 +0000

    sport perform event make stiff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
