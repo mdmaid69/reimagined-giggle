5c46f0efd8117d9d84c33062ee338779287f56d5
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:02:42 +0000

    please talk nowhere vote rank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import os
+print(os.getcwd())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
