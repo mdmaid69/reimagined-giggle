ebd16fff4047639fe55999eb2df3ecbd41ab04be
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:58:08 +0000

    draw know term when perform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
