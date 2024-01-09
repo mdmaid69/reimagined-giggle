55e8cd5e9881571d30341b9dffcb734221f7c5d8
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:55:43 +0000

    shake tomorrow exit genuine goup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
