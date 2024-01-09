ef9bbd75fd142c42f4ca7a401237f36c2b3efa03
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:38:27 +0000

    goout not persuade failure joke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
