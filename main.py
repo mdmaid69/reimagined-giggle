9a7288a47f82ea34cea000772a5618a5ae1d50d3
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:27:21 +0000

    Mm though date whether little

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_work(force, distance):
+        return force * distance
