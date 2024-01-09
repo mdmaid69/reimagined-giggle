a020f95172ebc4ca620e7fc603a767fa897c5b31
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:40:36 +0000

    through message mark point our

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
