c12c0d811cd4a582f7d128da771faea2b2d4c6b6
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:31:41 +0000

    sky chase separate meanwhile silent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
