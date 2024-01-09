89878af3bf5bb9857f01a2c805b6f3dce09195ad
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:00:49 +0000

    holdup practical evening colour choice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def convert_to_octal(n):
+        return oct(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
