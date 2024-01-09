e3fb980aed5008762d292dc2f7b4d06b9ff7fc76
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:20:04 +0000

    scream kind question ear official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def set_array_item(array, i, item):
+        array[i] = item
