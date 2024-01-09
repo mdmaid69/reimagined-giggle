d382bfc3d81f6c163c293b36aa393f58aa909ed1
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:21:05 +0000

    information at smell trial scare

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
