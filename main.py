a3a910ae175752986e312ed1f4359f2072716c30
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:41:43 +0000

    normal to address ride deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def convert_to_hex(n):
+        return hex(n)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
