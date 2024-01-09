0dd8e7c1c441b6326fbd53a726f1064e62abf23c
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:28:08 +0000

    fall game match good field

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
