2638829a547880a85f40bf73baa1ca2c185fb0e7
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:48:35 +0000

    feeling short bridge congratulations pride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
