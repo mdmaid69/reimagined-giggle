ae6af5f8383536ce19fff0a5270ef57556b778ea
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:16:19 +0000

    scheme not regret bother wait

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
