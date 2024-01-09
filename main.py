9d42df107482d6dfc5069bd18d70ec48bb40a01b
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:37:20 +0000

    quarter everyone apologizeapologise surface around

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
