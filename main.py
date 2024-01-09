e3e777aab1be3c2a2e6119fc0cbbf8a89b57058f
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:06:56 +0000

    fashion usually hand question up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def cube_number(x):
+        return x**3
