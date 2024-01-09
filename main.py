23369675c5c82c4a576e9bd9dafdcc3ff86390bf
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:27:38 +0000

    arrangement price survive tend building

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
