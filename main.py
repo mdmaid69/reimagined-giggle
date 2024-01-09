1ef5fc1dad10ae8dd256f48e3138db1741902350
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:41:25 +0000

    promise steady poor bridge advantage

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
