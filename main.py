0af033d01b378cc7cf45d4560192b62a1ae120cd
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:39:10 +0000

    polite card wake reduction dark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sys
+def print_python_version():
+        print(sys.version)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
