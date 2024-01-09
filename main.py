8d0be811bea4628f21529aa16aa70f38aa3384d6
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:49:45 +0000

    fresh price related classroom disgusting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import getpass
+def get_username():
+        return getpass.getuser()
