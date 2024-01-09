5f7e3a44243d4aa514ad7cdffef3416f751ece6f
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:56:57 +0000

    prepare now interest glad switch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import numpy as np
-print(np.array([1, 2, 3]))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
