2b31ef6b8a6b9847c173ea929f750c55209d95fe
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:18:31 +0000

    fast trick fashion ride port

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
