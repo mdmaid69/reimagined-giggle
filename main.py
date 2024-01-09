9b0eddb0db65eafa7fa2b652a6f973e3b9a84bb3
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:21:09 +0000

    shock swallow us friend quick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
