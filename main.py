8ad9fd59427a500fcab3549adc2e89878ed03de3
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:46:36 +0000

    persuade crash amount laugh period

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
