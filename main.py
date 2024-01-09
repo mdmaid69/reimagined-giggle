6397b377b5402f5717e2aee0a95f60ddbe88b197
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:15:03 +0000

    unfortunate rub mind road cut

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def count_elements(lst):
+        return len(lst)
