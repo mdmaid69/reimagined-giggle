4b60389be1269cec65e136cf199af4e5789d4605
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:20:15 +0000

    welcome happen join food point

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
