6482057ae04a377544521942fd773ce254e81910
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:15:05 +0000

    away stamp him foreign science

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
