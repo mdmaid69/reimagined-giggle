92cf181ae07e2c9b1e4a842942f5198a7727c8b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:08:56 +0000

    carry runover beat danger worst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
