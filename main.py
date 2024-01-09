7405291b44a77e0fd78e3e0ee0618185db0003a1
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:07:11 +0000

    sail harm stop Dd computer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
