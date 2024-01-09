cc0765ed7fd38d3ef9c4ac196e1f63fb256d161c
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:51:19 +0000

    plastic remove cause quiet hurt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
