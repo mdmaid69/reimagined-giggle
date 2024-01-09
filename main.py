d96f707d2d39c4a475230c61f3d138cb2d78607a
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:10:02 +0000

    inform by rent chin cruelty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
