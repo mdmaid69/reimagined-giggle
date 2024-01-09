64fd006ac8aa1d530aa76e3f4a2e68e03f0845c1
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:49:12 +0000

    programme lie risk everybody service

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
 import array
-def get_array_typecode(array):
-        return array.typecode
-import collections
-def create_user_string():
-        return collections.UserString()
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
