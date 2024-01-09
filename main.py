db88ad9b2b2860af4d7530180c26e0037fca64c5
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:43:53 +0000

    breakout part concerned female attempt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
