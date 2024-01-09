72536a78d343312c1c860e47af67421d16d4b3cd
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:06:02 +0000

    rather be succeed downstairs damp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_power(work, time):
+        return work / time
