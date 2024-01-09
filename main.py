f86f26533b60f0b2ff7124c2495d17295d53c011
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:38:15 +0000

    translate relief start care proof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
