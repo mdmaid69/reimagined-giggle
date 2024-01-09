69f822098c3807d048bbb56539dffaf979dc9768
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:10:27 +0000

    my drawing treat close slowly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
