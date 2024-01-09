96ad589a8030bca6efe60ff558a92242b26c8475
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:50:47 +0000

    getup win first surprise safe

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
