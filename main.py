0e6e6ef0e037b3c4ec187fbee4a519c0ec610107
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:57:16 +0000

    always stage round birth any

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_work(force, distance):
+        return force * distance
