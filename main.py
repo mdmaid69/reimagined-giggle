f5e7c5405bde0d73e1c1789c70dad580cefb5953
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:59:44 +0000

    bet handle rank cause cope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
