0933ced5e9a72a6f794fe6ffe91249b0676c8b63
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:15:26 +0000

    payment peace rent factory fail

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
