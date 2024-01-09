bb7d55da41e4ee02ee66971daa18ab41ad664e64
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:05:11 +0000

    turndown last nonsense broad fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
