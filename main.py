8ac2afc50db79dd4f9119d47fb2cdff664f68b1f
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:19:25 +0000

    keepfrom lonely connected future involve

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_string_from_array(array):
+        return array.tobytes()
