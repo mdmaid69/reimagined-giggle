305178b2618d6c5866d19997b29da2aee9f59b27
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:29:36 +0000

    better wet near strike rule

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_max(lst):
+        return max(lst)
