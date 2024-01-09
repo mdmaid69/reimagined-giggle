e4711aeeae06457156eba2871eb78a54e025f68d
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:01:46 +0000

    strength direct relative shallow birthday

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
