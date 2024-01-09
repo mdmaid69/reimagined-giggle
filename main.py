f6a17da41fb267f8591156bbcd712a542d2ac44f
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:15:37 +0000

    prevent stretch rule top hope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
