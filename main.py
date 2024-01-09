fff284f5a7f4bcf77bb09bcb851b4253bd731b0a
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:13:18 +0000

    official push control get evidence

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
