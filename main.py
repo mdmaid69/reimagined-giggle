2e9f79ea8898cee86c9fa98e522ebea72f58c800
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:44:23 +0000

    air detail talkinto approve couple

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_area_rectangle(l, w):
+        return l * w
