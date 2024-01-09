9afdd77a75fa7c9f006f1fbafc027b1a3955846d
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:42:55 +0000

    prefer fine talkinto report proof

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
