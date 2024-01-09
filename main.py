dbb8e3faff5570ae9a541e4f632e622103a1208f
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:27:14 +0000

    frequent turninto action trousers tread

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
