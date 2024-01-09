f84de366f39f8ea43f40fc61da9c1667348dc892
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:02:25 +0000

    once length weekend relationship takeaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
