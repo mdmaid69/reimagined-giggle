dfeac238f6304eb862c0875a470d11cc56a4948a
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:50:12 +0000

    practical obvious early rise pure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
