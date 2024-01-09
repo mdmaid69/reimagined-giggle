d917861693d5cd4a60ea94769280029fcc2f1335
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:08:57 +0000

    advantage bringdown contrast meanwhile flow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import json
+print(json.dumps({"name": "John", "age": 30}))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
