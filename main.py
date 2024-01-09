bef9b4b7d2cf437c525e7f4bfa227a45c12f514b
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:32:00 +0000

    deliberate listen mix yesterday butter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
