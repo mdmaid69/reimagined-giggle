06b6beeba845da8091864050f0403b47170839bf
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:21:36 +0000

    let culture no hospital action

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
