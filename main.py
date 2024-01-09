3e3008a267bc9331ca529b7943148e41d854ad28
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:10:29 +0000

    century behind bang wake advice

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import array
+def get_array_item(array, i):
+        return array[i]
