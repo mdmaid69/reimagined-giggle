352dd630317c56ab1e4139aa3fcbaf3f3fe7edb2
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:15:44 +0000

    float back scared off straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_work(force, distance):
+        return force * distance
