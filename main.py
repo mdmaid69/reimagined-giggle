c47caa05a79cce734584e9fb8b7513228a3f50df
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:29:40 +0000

    convince thinkthrough present bring skirt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
