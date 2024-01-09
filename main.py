2c75d077bcd5e43778abd7d559520766ab654588
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:02:43 +0000

    large unfortunate Zz pollution sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
   def get_parent_directory(dir_name):
         return os.path.dirname(dir_name)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
