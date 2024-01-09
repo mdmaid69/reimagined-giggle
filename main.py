8cc6c28b3ede2760639f0e4f0c1e86d0360df0c0
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:54:54 +0000

    skirt move check rarely unhappy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
