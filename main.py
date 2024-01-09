c4419beac02746383233c44ac0c30fffeafd80a8
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:03:42 +0000

    listen wave approval paper shake

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
