280ae55de30a5a74cdc2369ec61edd1b52feb40f
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:44:24 +0000

    human loss while ignore float

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
