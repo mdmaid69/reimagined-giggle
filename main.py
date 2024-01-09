4cbd855e325f308cf05bb6411806f091fdcbc5ea
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:47:30 +0000

    satisfactory declaration claim watch path

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
