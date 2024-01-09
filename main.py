62f314a83415407c87ad1fac966c8e7f01d8c1ba
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:13:28 +0000

    loud save consist once stir

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
