a1681b262cb85777c88c5880181ebf8f037eb12e
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:46:09 +0000

    stupid shallow related move unhappy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
