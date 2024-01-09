fd8042587b8a02aa335ff665696958cab64bbacf
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:20:02 +0000

    realize goup action also aim

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import tensorflow as tf
-print(tf.__version__)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def divide_numbers(x, y):
+        return x / y
