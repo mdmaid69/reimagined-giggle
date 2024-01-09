68991a81f62bc1573b41a71d6f7aac0b8d456950
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:19:12 +0000

    backwards present uncertain while difficult

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
