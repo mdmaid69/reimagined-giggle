e60d818332a542470dadfbba08d1c11c132e8465
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:01:35 +0000

    lead feed interested flat frightened

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_power(work, time):
-        return work / time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
