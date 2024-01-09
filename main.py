17e318d1c9f12aa3b09c9ebe4e14cc87349a0e55
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:10:56 +0000

    thirsty entry because discuss expect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
