20a81c878ffc8419463b1e2a2186fb7c80761a97
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:34:55 +0000

    steady keepoff street speech warning

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def add_numbers(a, b):
+        return a + b
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
