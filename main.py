930a8c4d4f80668aaefc19ccdedd1ef0ff15a7bc
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:53:16 +0000

    evidence reliable trip glass often

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-for i in range(10): print(i)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
