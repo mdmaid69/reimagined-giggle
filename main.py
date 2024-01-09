f35d5fafaed1fbb16e168ff2591ea16d4ede5dd8
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:03:31 +0000

    extraordinary scared though clean his

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
