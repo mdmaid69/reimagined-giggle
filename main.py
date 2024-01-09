ae9ce3da3f2479cefb189f8077bd1ccd7890599a
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:17:29 +0000

    successful trick but while themselves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def get_array_index(array, item):
+        return array.index(item)
