aacc96c5fb0a436322108bfae2e7691ff6643dfa
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:15:34 +0000

    your exhibition transparent affect describe

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+print(math.pi)
