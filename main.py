b97af2860b6e97d81d9ac87b99848294f7d3a1bd
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:59:05 +0000

    hair lean whisper bus in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
