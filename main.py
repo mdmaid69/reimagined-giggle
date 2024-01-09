ff91549cefdb1b1800d13f48f6a19a5028c08077
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:34:16 +0000

    wild dear release whoever giveout

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
