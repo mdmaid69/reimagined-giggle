d5d43361833e26d1e74c2d764c795a90c2c3e174
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:31:52 +0000

    practise positive poor paper satisfied

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
