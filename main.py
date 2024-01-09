e652ad9346fcfbf8f84b1b090ad1cd00c99b2a13
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:07:14 +0000

    introduce block this test insist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
