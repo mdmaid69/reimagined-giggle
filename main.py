91decad0dc09f65ca46ee6961c0aa864752c22b5
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:18:58 +0000

    stage personal mine strength nothing

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
