add9da27256366159e89e72737bb5f77330bb194
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:35:44 +0000

    equal uncomfortable demonstrate temperature thanks

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import getpass
+def get_username():
+        return getpass.getuser()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
