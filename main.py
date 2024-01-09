98e6d1f58753e173d11435b1e4acf9f85ba280b0
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:23:16 +0000

    surprise empty pocket pleased sensitive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
