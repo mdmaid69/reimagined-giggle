4ecd5ad893a72cf45a277d7020badd0d77da5fa6
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:03:53 +0000

    seem reduction this most boil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_force(mass, acceleration):
+        return mass * acceleration
