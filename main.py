5381c5579333e56d6015bdd2e40e90feec0c597a
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:49:04 +0000

    how design stand entrance pour

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
