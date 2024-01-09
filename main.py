7d518af70eab09763d0f5cd856ab3f2d9c46eb79
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:24:07 +0000

    post sleep charge that lock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
