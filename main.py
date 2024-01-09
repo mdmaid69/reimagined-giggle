93a075aefab86da5199d670e681e89420250b9a9
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:51:52 +0000

    interfere limit cry talk offence

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def remove_duplicates(lst):
+        return list(set(lst))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
