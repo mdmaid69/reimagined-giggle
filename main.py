a0479d6eff85b946c3273e94e02fdfd960dda625
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:27:14 +0000

    number arrive calm rid number

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  def subtract_numbers(x, y):
+        return x - y
