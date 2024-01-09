48e61e3665486f2972a58b059cfbcfe6217d477e
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:33:12 +0000

    unkind wish nice brother excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def multiply_numbers(x, y):
-        return x * y
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
