59222da5bacedf84b2d49604c96d479cf9a1bce3
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:43:05 +0000

    treat surprising talk birthday scare

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
