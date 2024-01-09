52e001d950cdec5438bf3f97861bef1571f60f62
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:29:17 +0000

    stop no necessary gather certainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Uppercase:", text.upper())
