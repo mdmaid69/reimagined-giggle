fbff604d4557a4734f412aefc0a77802969276cc
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:54:22 +0000

    cover fond reasonable top deep

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def subtract_numbers(x, y):
+        return x - y
