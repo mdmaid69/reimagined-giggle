f95b3728982dd7200ba24fd74e0a09aaaa69c188
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:18:59 +0000

    protection back whichever impatient long

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
