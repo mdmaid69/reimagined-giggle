45cfbb61fff632201f71e34e2c83438beae55cee
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:44:51 +0000

    future too odd average breakdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def is_even(n):
-        return n % 2 == 0
+import tensorflow as tf
+print(tf.__version__)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
