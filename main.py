69ff7faf5670060d3f775b5520035db39a911a79
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:07:04 +0000

    regard play meaning earn step

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def count_words(sentence):
+        return len(sentence.split())
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
