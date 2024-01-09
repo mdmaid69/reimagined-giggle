02fb03e1ed2ec9062d39e3667fa2c5595a37522e
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:20:33 +0000

    trap public shadow regular sort

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
