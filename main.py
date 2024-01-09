e1ca9cd00bce7ccaa4f4630b61adcb008c5eed68
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:57:51 +0000

    fortunately expression invitation convinced fire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
