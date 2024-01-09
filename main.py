5854088a26185838c5d5f5fdf231a754a5ce9efd
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:20:41 +0000

    square need comeback wash within

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
