26b496119eaabe749ae5b4cceab1246eea81d8af
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:26:40 +0000

    force interest mean wash picture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_area_circle(r):
+        return 3.14 * r**2
