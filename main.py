6d1ada1653250fbd18e71e6cd86f8c4bd29c974a
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:14:31 +0000

    obvious duty Gg dig anyone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
