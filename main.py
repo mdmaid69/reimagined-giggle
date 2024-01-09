55e20b96d45f8cc3a3d18dfe00fe2998a051615a
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:16:47 +0000

    word quarter excuse heat aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
