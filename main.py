2652defd10cac6c25344746b32c8fa064ef8b8b1
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:26:00 +0000

    immediate where large head challenge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
