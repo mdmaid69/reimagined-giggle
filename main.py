7f338ae672c19f2a2c5e68a78a50716b9451d333
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:37:54 +0000

    welcome welcome very tip pollution

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
