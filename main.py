29686f124496a43933e983215198708c95e4719b
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:06:58 +0000

    importance exit office to tradition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import getpass
+def get_username():
+        return getpass.getuser()
