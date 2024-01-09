cb4cf14b52523bc56ce7ed7a49eac7ec9e239715
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:04:29 +0000

    either reply 572enter try classroom

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-print("Hello, world!")
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_floor(x):
+        return math.floor(x)
