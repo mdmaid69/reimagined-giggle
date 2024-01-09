2c5803241e8e15e35dd0e3abdc0c38fa9e2785bd
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:14:30 +0000

    project chat breathe treat love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_repr(array):
+        return repr(array)
