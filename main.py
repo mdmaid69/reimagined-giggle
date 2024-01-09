8b4c823f5130077623493a704bfd4c50955ebfbb
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:10:27 +0000

    charge winter flavour accidentally praise

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
