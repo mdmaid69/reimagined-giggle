1a2942ebf3f1790bbb46e2835e76357216b70ea3
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:18:06 +0000

    wet national across gather rely

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
