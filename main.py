e36714de44c64a185f24441de16dcfe7bfaf8e26
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:28:21 +0000

    copy finally cut when object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
