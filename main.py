100f92913744df3ce32753afd7724f13e7e830f6
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:26:38 +0000

    interview plan otherwise miss funny

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Uppercase:", text.upper())
