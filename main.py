41f280c2a74d3586b39caa7ed0a928f0531467bc
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:33:15 +0000

    surprised breakthrough scared flow makeinto

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
