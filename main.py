10025cb96f1b865f92d4f11e8780624d79cb80fc
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:38:29 +0000

    such learn whisper create comeback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
