fdc896030d2472427d6d73d58967feb858c8c9dc
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:31:44 +0000

    children cover trust takeoff centre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import sys
-  def get_python_version():
-        return sys.version
+  def delete_file(file_name):
+        os.remove(file_name)
