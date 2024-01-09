3a2d59670cdb3c788ff63fb00423607318c16c68
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:19:34 +0000

    injury scrap stupid impossible much

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
