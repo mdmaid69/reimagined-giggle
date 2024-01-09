a65316407fbcb7e84f3c25b8138dc08b03bd449f
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:11:52 +0000

    money use desperate century then

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
