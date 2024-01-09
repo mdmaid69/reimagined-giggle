6d7bbbe273063db0c8d2dc25dddcb6e5ef00a383
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:15:27 +0000

    treat 565end right shape worst

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
