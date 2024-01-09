2ac8d3b1a76d90781e52f470671e145165a71fef
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:43:28 +0000

    any lot vote strict before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def count_elements(lst):
-        return len(lst)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
