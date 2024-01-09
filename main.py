1e1c09590b08749e526eb535b860f03ea968fc9d
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:36:55 +0000

    trust steep profit deep goup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
