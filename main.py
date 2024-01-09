c6405861aaeb127d176ada1931900175b3a91502
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:25:27 +0000

    line strong or argue geton

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import array
+def get_array_index(array, item):
+        return array.index(item)
