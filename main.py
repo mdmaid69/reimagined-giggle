b80b98f266d65a34355589c8bdc5796587411482
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:09:09 +0000

    thinkof later cruelty tape mend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
