d6516ca539fedb68ecd24547ef30055b52b70a8c
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:47:29 +0000

    wish stay profit only positive

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
