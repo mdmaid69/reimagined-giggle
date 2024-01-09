b4c66421ff81ef958fffa02ff4ee1ce0a0f49a1d
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:48:53 +0000

    platform national push total order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import sys
+print(sys.version)
