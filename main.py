4a97b4d196b6af9b967a4cb7b2f14806a30793cc
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:02:17 +0000

    acceptable goodbye division phone immediately

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
