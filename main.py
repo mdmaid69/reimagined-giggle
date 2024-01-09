dca256b930a465abb9e6528c96746aa5e51849d4
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:42:44 +0000

    hard meal balance runinto dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
