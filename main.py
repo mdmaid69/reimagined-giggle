1f552ff2e9f56e0be19733aa56bd31aac9cf2c45
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:59:41 +0000

    everywhere until mine always replace

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def is_odd(n):
+        return n % 2 != 0
+def count_characters(sentence):
+        return len(sentence)
