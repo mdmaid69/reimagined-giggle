5cb9d44a45f190ceceb2517dc0c65e5aed1e6aae
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:08:01 +0000

    turnover regional base noone fault

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
