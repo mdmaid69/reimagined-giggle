b8a73eb31f68f1e2fcea97ac0a80bda278c83c0a
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:13:10 +0000

    activity damp talk aim base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def clear_array(array):
-        array *= 0
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
