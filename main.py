d6c2a61fd6376b03126b4a2618c40f68a532797c
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:04:31 +0000

    fetch figure persuade start opinion

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
