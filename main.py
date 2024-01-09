8eb2c974b0badb9853620ac9e3c369bd914844bb
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:22:16 +0000

    centre however report wait dinner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_area_rectangle(l, w):
+        return l * w
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
