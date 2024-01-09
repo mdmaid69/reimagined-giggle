e8816b24d515a102ea30dc2e34d4db00b946d9a2
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:59:22 +0000

    inform middle phone become responsible

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
