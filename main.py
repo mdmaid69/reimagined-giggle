d6ad37bcafb459dedfc7f818162e603427e93a5e
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:39:29 +0000

    hardly well Vv giveout involve

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import collections
+def create_queue():
+        return collections.deque()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
