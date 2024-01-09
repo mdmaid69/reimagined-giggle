5f5bcc6545e7df93a3d2673291c942c4f5f94ab2
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:10:03 +0000

    learn comeoff life separate foot

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+text = "Hello, world!"
+print("Uppercase:", text.upper())
