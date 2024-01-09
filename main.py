9180352ba7e310de5f80cc6267c8f9121aa4e4c2
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:52:54 +0000

    bother holdon Qq love move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
