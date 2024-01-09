36c3b8359aa38aaa00774a529a495bcaa896fcaa
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:02:14 +0000

    slice young capital piece exist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
