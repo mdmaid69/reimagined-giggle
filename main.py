70be8f764052a257337ebabf249161c1fbc11682
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:16:40 +0000

    herself quality mother roof grateful

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-print(sum(range(10)))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
