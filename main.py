fe0fb88954a5c2bbbd443fe2f4f870f18ea03049
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:26:20 +0000

    front than prize should custom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-for i in range(5):
-        print(i)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
