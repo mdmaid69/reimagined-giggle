47bdbdc6ab262fdaa9e25c43fc0f00ba7931df5e
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:06:19 +0000

    top remove completely receive change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
