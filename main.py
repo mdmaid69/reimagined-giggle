23467e3c0e4c445b9a4b81ddc0895efe62bc3301
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:49:00 +0000

    sting more shine wild flight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
