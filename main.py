4914cb91f41ccf93b30033ac0ab8b71f21770ebc
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:59:45 +0000

    opposite branch cheat pump slice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import random
+print(random.randint(0, 100))
+def calculate_density(mass, volume):
+        return mass / volume
