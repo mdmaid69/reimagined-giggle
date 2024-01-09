0c7c3db1f3b354a7c1a3ae83f11e7ae7e30a4260
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:47:35 +0000

    guide there call simple thin

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+print("Hello, world!")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
