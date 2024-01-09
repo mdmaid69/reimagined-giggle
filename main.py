4ae708b77b987c40d8a3b9bd0f6d0fc51e4f3957
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:23:04 +0000

    exhibition somebody OKorokay underneath taste

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
