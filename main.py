de2f1fd922454495b99b119c8614197eca1e88de
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:31:50 +0000

    head approval sleep brush firm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
