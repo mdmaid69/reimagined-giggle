84c85bb1d56f6e10786f1a41b346183fa74045c0
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:11:22 +0000

    frequent post centre bringup fierce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
