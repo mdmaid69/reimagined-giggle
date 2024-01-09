63207beec85b0335dd528b6a9e8a4ee23747999d
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:38:25 +0000

    breakfast hospital common agree argument

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def set_array_item(array, i, item):
+        array[i] = item
