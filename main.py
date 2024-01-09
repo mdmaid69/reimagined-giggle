ce600383e85593b8246740ed46fd06c93dab2f4a
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:27:51 +0000

    attempt wait accidentally fork gooff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
