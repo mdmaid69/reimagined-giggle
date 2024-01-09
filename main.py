61f28a16c2b2ddf0a5433bbfb42284746050befc
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:16:36 +0000

    pay breakin increase pretend trap

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_work(force, distance):
+        return force * distance
