26dd6c01bab7a99884ba0d16308bf6cb93074384
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:13:54 +0000

    stamp weather choice work brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
