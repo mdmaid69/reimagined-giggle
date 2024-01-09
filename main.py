2df01f1a9ef2601e676ecafa5abbea689e42ef44
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:09:22 +0000

    fashion dream advanced bad wave

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
