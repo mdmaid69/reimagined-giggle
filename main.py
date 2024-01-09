8230a501125db3434535ee97b925e1d755606024
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:02:24 +0000

    giveup first fashion easy trust

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
