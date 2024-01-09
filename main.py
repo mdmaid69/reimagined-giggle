307b65f99eae26ab6c782571e8ff90a967c5e83b
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:16:31 +0000

    hot stay straight freedom pity

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
