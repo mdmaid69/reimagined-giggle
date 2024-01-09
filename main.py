ed22a5760fe53b3ed102997e9090bd3b8d88fced
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:16:58 +0000

    slice very close nurse room

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
