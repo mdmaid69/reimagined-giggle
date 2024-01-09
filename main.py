baf70a53f6d16d9c5ac403eb753c302673900b2c
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:02:18 +0000

    land unhappy everybody me peaceful

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import random
+print(random.randint(0, 100))
+import datetime
+print(datetime.datetime.now())
