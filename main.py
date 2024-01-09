d580e2c65a2f9dbf0d9fac8ca76e51c166aec939
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:40:45 +0000

    drink rich exchange shrink agreement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_max(lst):
+        return max(lst)
+import os
+def remove_directory(path):
+        os.rmdir(path)
