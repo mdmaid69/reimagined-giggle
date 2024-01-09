5c053af7a2515b1a75013142496c1c3b1c0fbda2
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:04:54 +0000

    soon estimate hang production sharp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def convert_to_binary(n):
+        return bin(n)
