1aab70b9bb1be13a09a1f5f1783289797efdb091
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:56:37 +0000

    generally purpose him makeinto everything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
