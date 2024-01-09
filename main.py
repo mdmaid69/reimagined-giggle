c07060a895dcb04cdb8c16357ea7ab52777735b3
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:39:27 +0000

    probably impressed tongue trust break

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
