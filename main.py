ba8e6e7345a35a5e1c8a9998aa5d9b7d202c7177
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:56:27 +0000

    middle trick holdup tend thirsty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
