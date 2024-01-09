14de9c7f2ce21bf7330d4ab1f5bff2992b3c985a
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:13:46 +0000

    still carry probably word food

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def count_characters(sentence):
+        return len(sentence)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
