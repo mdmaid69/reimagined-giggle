63ca3399d397a2db2329f8bd7d25b1a4a2d6db4a
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:01:17 +0000

    violence spare flat actual lose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
