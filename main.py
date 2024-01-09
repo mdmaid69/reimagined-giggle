75a42815e96b9daa846b29e0758552fbf6ba74fb
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:05:22 +0000

    we seat eat there sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def count_characters(sentence):
+        return len(sentence)
