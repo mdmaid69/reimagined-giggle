0eb2b743ea228f6d1bb4d64d80ca43f58465f584
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:21:25 +0000

    tough rush sign consult hang

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
