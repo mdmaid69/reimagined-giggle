e6e5548752654b912b858d80a5d1451414061f76
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:32:49 +0000

    progress before mine wherever mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
