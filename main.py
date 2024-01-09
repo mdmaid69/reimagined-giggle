eb6bf9fa386b6ce9f4c1a697a6f3b098cce61948
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:58:42 +0000

    range passage forget pay breakinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  def convert_to_octal(n):
+        return oct(n)
