9779cd86202abb6575f939ac39e08e2bbf92d916
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:13:58 +0000

    splash passage goodbye relative speech

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def find_max(lst):
-        return max(lst)
+import array
+def get_array_as_str(array):
+        return str(array)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
