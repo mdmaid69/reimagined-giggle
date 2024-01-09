aabe66cd361bae9f0c110587b73ae5c993384e56
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:55:47 +0000

    price duty before waste scheme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_list(array):
+        return list(array)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
