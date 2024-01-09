78224dbc00c7766f92e5a756c25a0a9470a30996
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:47:01 +0000

    pass terrible squash really borrow

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+print(sum(range(10)))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
