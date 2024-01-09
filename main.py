8b566cea45bf690db591569995a7bbe23a9fb6a2
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:05:43 +0000

    government relation friendly cap go

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
