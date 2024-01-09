0755524576307a6ad9d92956320e09982ff31fed
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:53:19 +0000

    listen obvious opposite professional leg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
