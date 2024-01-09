c7eb0128fb7648bf698d8c4ec480fd46ed5f2416
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:10:57 +0000

    scare guess lookafter nervous instead

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+print([x**2 for x in range(10)])
