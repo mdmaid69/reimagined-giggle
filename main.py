b99ab5febb110419e817d649c0b56f3113ebc36c
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:33:47 +0000

    party brother so important catch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
