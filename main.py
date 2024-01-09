26ffb761c79994e86d9de7f79175f28fba8ea4ec
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:15:58 +0000

    switch settle deliberate wide confused

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
