6ac06e81f703cfe55e9f197b8e961ab791ee811a
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:51:35 +0000

    dream late to dangerous evening

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
