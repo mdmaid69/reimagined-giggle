717f39729d2b00adb34896800c0de968350d53a3
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:15:46 +0000

    bet blame minute pressure action

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_acceleration(speed, time):
-        return speed / time
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
