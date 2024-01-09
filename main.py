a2e6e9360fb7f31fb134cb6afe6aeaf19931ae85
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:19:42 +0000

    herself punish nonsense well spill

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
