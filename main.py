a1ed5f863aa7aa46ff65860e7d1eec91cbb8ac5c
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:53:37 +0000

    crime properly name next arrange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_list_from_array(array):
+        return array.tolist()
