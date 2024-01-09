6a84c935fcbf9fe660814df6aa862227a5b60ab0
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:09:14 +0000

    until wise comfortable excuse bright

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
+def cube_number(x):
+        return x**3
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
