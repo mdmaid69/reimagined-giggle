d748b1443369e24954bfa053a399b09d2a60cb9e
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:34:43 +0000

    difficulty fashion behaviour crash example

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
