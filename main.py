9187dc4957fce4fafa4dc17b7fa8fa16ef86c68b
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:34:34 +0000

    go familiar deep feature thorough

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
