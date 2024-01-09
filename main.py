e00a40adf046b4a3829bc6e4d5f605e58980121b
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:42:45 +0000

    history month contact project normal

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-for i in range(5):
-        print(i)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
