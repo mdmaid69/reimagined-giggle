8eebe1e00df4113c1ff2e3fdcc86a642399230aa
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:47:50 +0000

    ocean odd string last fetch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_float(array):
+        return float(array[0])
