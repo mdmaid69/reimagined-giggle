08aa9ac22c0308c6ba0eab0cfa1d68505ac024c6
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:20:15 +0000

    survive moral keepoff each quarter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+print("Square numbers:", [x**2 for x in range(n)])
