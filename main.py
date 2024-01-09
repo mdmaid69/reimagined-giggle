33451c132fdeeea2c212d1122446f04a347dee14
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:23:25 +0000

    wipe goup getoff spot strike

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
