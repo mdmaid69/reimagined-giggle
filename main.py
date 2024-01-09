3f24e97b774675917af2c275ad572d8deea1769a
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:00:59 +0000

    unhappy either hurt trust dinner

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
