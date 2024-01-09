785c845d1a0f33fdd5dbcc7baa55812e39ede4ce
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:09:59 +0000

    bring message successful hers class

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
