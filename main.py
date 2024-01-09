82cd702bf3969978862b823274245af2e246eda7
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:35:28 +0000

    bend like square car turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_acceleration(speed, time):
+        return speed / time
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_complex(array):
+        return complex(array[0])
