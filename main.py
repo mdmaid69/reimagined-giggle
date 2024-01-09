9cd8717efd927ee0f8b1b2a2a7c31223f2df2ae0
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:21:21 +0000

    church heat then ring any

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
