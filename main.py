11aed95c7c28bbc1b21fc67b6c0186e5850d4387
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:02:46 +0000

    exactly hard lie ago rank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
