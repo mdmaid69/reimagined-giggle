20dae6ff74ba4943da265652089835af869838b0
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:59:14 +0000

    before rare plant sister together

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def get_array_as_bytes(array):
+        return bytes(array)
