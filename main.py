24be74b0103320528d60bb7532091125b660d48f
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:31:50 +0000

    alive smell roof disturb sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
