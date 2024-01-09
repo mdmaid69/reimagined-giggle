e4ba35a290ac443c858fc0f0c59b49752aa17c78
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:40:54 +0000

    skin image play term hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
