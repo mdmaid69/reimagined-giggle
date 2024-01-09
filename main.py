2938bf3a84573cd64e0b9d9f8e53ea62ae1802f2
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:58:01 +0000

    face rank direct considering profit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
