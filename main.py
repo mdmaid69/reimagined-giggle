fb4620141a5068ac0db815f896353a2d6685ebb0
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:39:35 +0000

    floor crazy criticism lift mood

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
