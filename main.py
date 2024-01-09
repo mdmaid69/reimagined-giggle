b76eb2e7b832549bf2a3b489a334edef680b67c2
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:59:49 +0000

    branch near know open keep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def reverse_string(s):
+        return s[::-1]
