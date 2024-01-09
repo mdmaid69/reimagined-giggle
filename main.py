8d869c46736561a204e0ba10707137b3af751766
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:37:43 +0000

    breakfast sleep defend it forward

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import tensorflow as tf
+print(tf.__version__)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
