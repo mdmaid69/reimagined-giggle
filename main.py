2028c661721b690957b89c9c1ffcb14403562e7b
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:12:19 +0000

    pause search autumn why able

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
