c5d8beca1ee49dbf8fa6d5687203b335fc7f6c0c
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:38:14 +0000

    while name future getaway use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
