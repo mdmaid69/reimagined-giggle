7b5b7e53858beba321208f90d805ec9ff265aa9a
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:15:20 +0000

    scream Ff along trap except

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_density(mass, volume):
+        return mass / volume
