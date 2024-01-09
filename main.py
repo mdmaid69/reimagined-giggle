22fde0175c3fc344aee8c7fac6a1c8641d32712e
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:06:05 +0000

    second strict sticky run skirt

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
