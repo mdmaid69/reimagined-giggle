159fe0821c30ba901775c563bebd696e13271550
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:00:07 +0000

    favourite trouble advantage contribute accidental

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
