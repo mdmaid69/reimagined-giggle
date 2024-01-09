0ee69ebd121b4ab6e37a6d79bdc45ef88c12925b
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:52:45 +0000

    furniture complain book lot fight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
