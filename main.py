f593f2591be0f6f34e4b6e9c992850b140f5f378
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:46:51 +0000

    shock land city refuse disapprove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
