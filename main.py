bf83c4091ff73f01e972e3503ef2ace956364a6f
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:16:49 +0000

    desperate backwards comeover load possibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
