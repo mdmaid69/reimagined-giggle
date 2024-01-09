4d40310eebefdb246acc3cc8b32ebbf9e7f9443f
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:21:11 +0000

    tie the police popular meaning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def sort_list(lst):
-        return sorted(lst)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
