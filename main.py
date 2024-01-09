3677be6a361c0b9cdffd16cf263e165cec42dff4
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:12:52 +0000

    information recognize certain company no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
