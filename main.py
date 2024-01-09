1be2f9f1c44926ab46070cac60144a14db2bdf90
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:44:50 +0000

    crash damage false weigh scream

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def convert_to_hex(n):
+        return hex(n)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
