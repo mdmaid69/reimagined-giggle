748f0292d5e889dd938250c0d299bfd68a6f8454
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:23:42 +0000

    occur maintain block pupil sell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def is_odd(n):
+        return n % 2 != 0
