239d1ab50d719bea62d791266a8dae34addca83d
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:59:09 +0000

    turninto petrol accidentally hurry high

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
