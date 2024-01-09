80a99727dde00c8dd7fb1955a30c1fe231343214
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:11:15 +0000

    Hh activity dry thanks clever

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def is_even(n):
+        return n % 2 == 0
