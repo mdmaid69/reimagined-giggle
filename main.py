24b6b52e172a28f9b140c1651589e20108aeffa9
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:50:28 +0000

    cheap duty violence tool excuse

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-for i in range(5):
-        print(i)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
