a66da4215b78668d0bf07a078075cd4d822a9d4d
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:20:49 +0000

    closed achievement worth ordinary no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def find_unique_words(sentence):
+        return set(sentence.split())
