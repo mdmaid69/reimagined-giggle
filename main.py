dfcff8b47d6ca22282c6398508b12b737b1b872e
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:04:59 +0000

    towards judge close early respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
