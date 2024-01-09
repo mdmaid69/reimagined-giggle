734f88234eb3d8ff38cc83c4134ddac99b7a8d91
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:00:45 +0000

    first music crowd swimming scare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_str(array):
+        return str(array)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
