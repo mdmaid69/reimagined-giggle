c40b10df4afd5ff0f7fc4ba1903e09d5a2bfaf2b
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:51:26 +0000

    jump improvement comeback choice pollution

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
