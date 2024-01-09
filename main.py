dcb65a1a01fee4348b8b78a1638f6246669b9456
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:00:13 +0000

    look lookback(on) determined surface imagine

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def reverse_list(lst):
-        return lst[::-1]
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
