32dc77cef0789b7b91118b8f3580ff75da98fa11
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:54:40 +0000

    general book clean meeting relief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
