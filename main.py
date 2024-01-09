94d1a16abbf08bd6a287d1649f195581c003801e
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:10:18 +0000

    towards sensible complain consist shape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
