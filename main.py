87f41d1d95249c145ef0641d14c572c3afb281c9
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:42:59 +0000

    eye curious unkind his guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
