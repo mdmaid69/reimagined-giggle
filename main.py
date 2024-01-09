76108ccfdb414ffdf92cdb35d6ed3dcab1d8f2f5
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:45:08 +0000

    because burn blind suffer pinch

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+for i in range(5):
+        print(i)
