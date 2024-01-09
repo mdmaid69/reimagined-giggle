8ca128b0dee3ecd29bec99ad89068f6fa71a22b0
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:27:16 +0000

    evening left fill language popular

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
 def calculate_area_rectangle(l, w):
         return l * w
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
