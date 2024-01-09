781867cc2a1ac87b35a6340fa64aec100f10f0bd
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:45:50 +0000

    pass stone waste hurt young

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
