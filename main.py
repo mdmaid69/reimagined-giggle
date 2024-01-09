6f489ba96faa3390b8ba02cdb65980ae7e35bf28
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:50:19 +0000

    Ww react successful clear pocket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-def calculate_pressure(force, area):
-        return force / area
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
