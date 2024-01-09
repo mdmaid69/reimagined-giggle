cab0d278a7d2609dfea67e60c0a8370d82e19b89
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:40:11 +0000

    turnup fear undo break lookaround

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def reverse_list(lst):
+        return lst[::-1]
