64679455444a3df45c921381edac01cc01f49e9c
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:12:01 +0000

    extra freedom will fact spend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import time
+print(time.time())
+import array
+def set_array_item(array, i, item):
+        array[i] = item
