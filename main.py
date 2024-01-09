3be2885169fa455da1e34597756a85a68984d25b
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:06:22 +0000

    close immediately ordinary wild fact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
