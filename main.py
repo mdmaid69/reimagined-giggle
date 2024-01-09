35c01eba445a73ec712c4cbf9da4ddbfd101d7bd
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:16:08 +0000

    used kind expensive course push

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def add_numbers(x, y):
+        return x + y
