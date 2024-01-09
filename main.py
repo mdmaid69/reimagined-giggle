d3d567fcc49b66134acd76df0052e16f4a907a05
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:47:22 +0000

    arrangement love no tidy official

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+print("Hello, world!")
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
