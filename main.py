5da07477a56b67aa252e6a94c1b996817b44c6eb
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:11:35 +0000

    lookaround stupid petrol Yy programme

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
