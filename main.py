c12441dc2a075e84d978094769aa2f65ba6f44cf
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:20:01 +0000

    bye television trade season pressure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
