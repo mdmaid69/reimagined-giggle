98581ac42eb773e391f307f3dee25668ded573c7
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:49:14 +0000

    foot necessary herself between peaceful

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
