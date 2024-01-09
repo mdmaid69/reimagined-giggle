90b5589bdb898ec5e27aee3038dd5c75234c8016
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:08:59 +0000

    interview date search escape good

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
