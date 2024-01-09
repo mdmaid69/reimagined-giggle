57732690d20aa4db72cbb100313b76cf5fe74bb4
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:42:23 +0000

    interview wide hurry calloff slowly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import os
+def get_current_working_directory():
+        return os.getcwd()
