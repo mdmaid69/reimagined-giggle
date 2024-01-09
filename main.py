981aed00f8149b5c1b137fdf02dd703a5dd40c4c
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:59:45 +0000

    thorough home track apologizeapologise frame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_index(array, item):
+        return array.index(item)
