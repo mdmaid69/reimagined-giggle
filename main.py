73caf8fd8b71315e930af8ffa49c39f77a705c62
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:25:52 +0000

    tape he naturally damage artificial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
