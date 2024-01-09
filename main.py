f5f5d50da90d12eb99fcd4cdb8f7c60939fad850
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:33:12 +0000

    preparation fill church design address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import datetime
+def get_today_date():
+        return datetime.date.today()
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_itemsize(array):
+        return array.itemsize
