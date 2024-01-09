c8622756da6efa265455c81f4848a7f39c2d1fd4
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:28:13 +0000

    fortunate rate lookup report instead

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_item(array, i):
+        return array[i]
