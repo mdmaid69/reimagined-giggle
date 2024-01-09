12cc62fa42f7509a1c5e0e59a87ef9a8dac1f33f
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:39:06 +0000

    slow giveout speed try machine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
