36b52a3cfb9838f87169fe358ee3a6ec4ba9b3fd
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:16:53 +0000

    exhibition war waste achieve deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import os
+def change_working_directory(path):
+        os.chdir(path)
