23071c3831ef4238e076e20a6f3dabf45984cd47
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:03:11 +0000

    minority patient message exercise more

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
