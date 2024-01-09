3d5cc80c628f6d2915144203b77801b98c161f40
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:38:49 +0000

    estimate unkind large land expensive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
