142347527e0fb3058c3dd4d105158e01a2115cba
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:04:19 +0000

    confuse quiet hurt ocean habit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
