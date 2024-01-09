6db02e0ce66a178f34784956cfee8952e51166f0
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:47:15 +0000

    matter tip sink criticize breakin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
