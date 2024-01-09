6875c2c3b3ac5233f390b4b8366ab40cafd37116
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:29:14 +0000

    everywhere offend nerve obtain describe

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
