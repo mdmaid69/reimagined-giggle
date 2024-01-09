46e8728649213dde70860778ed98c437fad26bd3
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:11:45 +0000

    bread once just push stone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
