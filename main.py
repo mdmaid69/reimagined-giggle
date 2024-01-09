77a1e9ef983fe6f5b40b4d594ae9f63a4900c923
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:36:21 +0000

    rely correct catch comefrom expect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
