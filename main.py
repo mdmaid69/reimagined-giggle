e11216413a0f108a3627305c8d278f29f537ab20
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:05:36 +0000

    invite score pipe frighten good

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
