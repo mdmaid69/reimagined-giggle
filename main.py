bbc78b347455c6e8daa23150d69b4ef9a35ce059
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:18:34 +0000

    even hello talkinto why keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def get_array_as_int(array):
+        return int(array[0])
