c569f79c9ff504c3bc2a921d26570fbc7c072f98
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:12:36 +0000

    search unlucky set practical match

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_acceleration(speed, time):
+        return speed / time
