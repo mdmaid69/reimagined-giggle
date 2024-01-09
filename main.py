187b98da13640ab7c76f2a4832e4eaa7cc863311
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:39:52 +0000

    possibility return wear which break

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
