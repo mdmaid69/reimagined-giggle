07fd0d57027e66589d7af299f88ac68af670ce2b
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:28:36 +0000

    warn careless move own kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
