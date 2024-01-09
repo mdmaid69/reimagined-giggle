33ff231e3f29cfba32c94d598037e28ec6a59ff1
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:21:41 +0000

    rich sit deliberate careless over

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-name = "Python"
-print("Hello,", name)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import numpy as np
+print(np.array([1, 2, 3]))
+def square_number(x):
+        return x**2
