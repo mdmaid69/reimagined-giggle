983d76d23b6ba6bad1f0674165a5dda0102905ef
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:24:36 +0000

    lonely tradition trip estimate slight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
