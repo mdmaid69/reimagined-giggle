2ae2f14d7df347d2e473516685516b9b11f5b5ed
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:29:32 +0000

    conclusion flat Gg little nigga

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
