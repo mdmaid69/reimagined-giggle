fa7967f4fc7f372223d6b01fc1c656d3b926dadc
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:40:20 +0000

    frame wing attach firm degree

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def find_min(numbers):
+        return min(numbers)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
