152bb38bb8eb4e027b3cc3e60d9f1177cc57658d
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:22:43 +0000

    fight claim small handle more

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_error_function(x):
+        return math.erf(x)
