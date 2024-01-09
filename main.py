d39b24e72803b8bd5e978a3e58bd821c80bed829
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:46:08 +0000

    job interested firm innocent before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def subtract_numbers(x, y):
+        return x - y
