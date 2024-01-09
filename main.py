3bed0354241f7a49c18572506924bcd7cbd26dd1
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:54:14 +0000

    as tragedy rate breakdown eye

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
