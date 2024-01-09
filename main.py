3373cb6b5931e11319cbcee1241e53845a663fd5
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:33:46 +0000

    tone keepfrom day effort match

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
