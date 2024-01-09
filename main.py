aeb82fe351e7726860fc704e8e10f38b0dc1f12a
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:58:44 +0000

    admit brain appear catch steam

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
