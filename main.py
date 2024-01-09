058e53df37c2be9455cf8e26c413c6d048751b2a
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:22:33 +0000

    side takeout reject would standard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
