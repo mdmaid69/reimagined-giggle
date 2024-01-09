0bb567c02ae91c36b3aba1a002525b0b46b33f68
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:01:06 +0000

    dish Tt turnoff except argue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_speed(distance, time):
+        return distance / time
