4361e9b253ac1f8de88e3623a5d67ec96ad749c6
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:54:46 +0000

    shock use steal activity market

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_exponential(x):
+        return math.exp(x)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
