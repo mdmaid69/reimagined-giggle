13d2762d0cc0f9da827899f32f8d3570c1c03687
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:56:22 +0000

    turnup stare above list stamp

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def clear_array(array):
-        array *= 0
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def append_to_array(array, item):
+        array.append(item)
