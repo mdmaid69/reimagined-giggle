c6231d943d1ac5c9382b8762d350754955479a7b
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:03:05 +0000

    admit region opposite foreign quit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
