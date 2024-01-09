613029da30c801dcddaead59dd23cb7410d421d8
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:34:38 +0000

    loud keen office wish along

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  def convert_to_binary(n):
-        return bin(n)
+def find_max(numbers):
+        return max(numbers)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
