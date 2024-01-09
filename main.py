7e86f342cb87ed7c22d39fc29ee1597316ee283a
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:24:48 +0000

    room talk occasional insist represent

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
