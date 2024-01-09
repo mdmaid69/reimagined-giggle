fd3ac244a5b57985e948b1cd1449f51782116826
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:50:06 +0000

    pick along grand schedule impressive

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
