ef5f6737e4fb093cc01fa72ccf27db07b81a3b35
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:59:57 +0000

    wait manage principle catch position

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
