4a9f56a8c951580f1161c9f3de9f31037bfba3fc
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:21:38 +0000

    workon print form complicated us

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  def remove_duplicates(lst):
+        return list(set(lst))
