c3785fd265a268c1c6c2778ca301d23bee97f9f3
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:32:19 +0000

    arrange serve top quiet influence

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
