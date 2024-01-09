24beaf99e21020234b12b811f4fad6676c2ad235
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:31:24 +0000

    must suit front possibly somewhere

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
