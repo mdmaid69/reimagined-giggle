196651cf4b2be8410368fd4cbf34f7f2b18abefc
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:16:23 +0000

    sure recognize strike the wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
