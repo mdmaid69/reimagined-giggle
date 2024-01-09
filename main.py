7b7f89325936daeb1059f205abfe2b8643fa6e08
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:54:50 +0000

    friend possible goaway adjective minute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
