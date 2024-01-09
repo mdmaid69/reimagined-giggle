c99fb60af1aab8fa8d5fc702072ae9e1ff407171
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:20:00 +0000

    demand university board temper kind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import tensorflow as tf
+print(tf.__version__)
+def calculate_pressure(force, area):
+        return force / area
