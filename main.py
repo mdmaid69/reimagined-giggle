96ada9863bf8d858a894f742675eb628b7921a3e
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:07:19 +0000

    detail tight weigh offer ever

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_pressure(force, area):
-        return force / area
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
