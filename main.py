b747dc5f7b962e8012e2a4b5e6699958f4da23d3
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:51:07 +0000

    relax vanish forgive away convenient

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
