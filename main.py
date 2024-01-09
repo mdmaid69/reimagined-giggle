b99b0635ca511f316519147c575943deec42bcf3
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:37:40 +0000

    party personal hit creature morning

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
