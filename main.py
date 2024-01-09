d904602132735b0fe7ab56a98e0cdb339b83711e
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:56:09 +0000

    once yet should challenge difficult

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+name = "Python"
+print("Hello,", name)
