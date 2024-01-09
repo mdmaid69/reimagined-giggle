667f7c86c19ace3785213462c881ba12f33a7416
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:32:47 +0000

    giveaway taste difficult persuade love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import getpass
+def get_username():
+        return getpass.getuser()
