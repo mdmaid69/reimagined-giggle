f519f873b4c1edbbad0da0936ed5c36f5f36234d
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:46:17 +0000

    fight flat total sink repeat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def calculate_speed(distance, time):
+        return distance / time
