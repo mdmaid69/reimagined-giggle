f3ca8e098af37fa3b78b47b1a9569378c845b9b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:34:00 +0000

    rush mixture aim clear tax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
