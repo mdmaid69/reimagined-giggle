6f5b4c0dcf1a87622fc5ce59bb0c377846880799
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:16:49 +0000

    down possibly emphasis apparently dangerous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def delete_file(file_name):
+        os.remove(file_name)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
