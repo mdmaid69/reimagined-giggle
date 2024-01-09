b008cad0f5cb3262c0febe37bc6902157dc9114d
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:14:20 +0000

    below range hurt team muscle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import collections
+def create_priority_queue():
+        return collections.deque()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
