bb36533ecf33cf1e98ef60a9a37e00c066008437
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:33:04 +0000

    matter imagine birth beneath doctor

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def find_unique_words(sentence):
-        return set(sentence.split())
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
