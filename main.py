46dd5dc8c4dd7581356224a1c8a95e8832c38605
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:07:59 +0000

    object it able introduce near

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def cube_number(x):
+        return x**3
+import array
+def get_array_typecode(array):
+        return array.typecode
