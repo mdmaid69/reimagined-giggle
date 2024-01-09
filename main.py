d709daef0645204e3c1bf491610a1fd8db031a72
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:15:31 +0000

    step whether score note all

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
