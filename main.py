067e8c6296fd225b11d68a1f590534206203f7b2
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:21:25 +0000

    favourite getback protect move debt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
