54b22b8bac838a56367883c107585d41dbf619e5
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:22:55 +0000

    absolutely or congratulations rub child

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def sort_list(lst):
+        return sorted(lst)
