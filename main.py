2ac840d8c163bda720645423966dd2d9de290cfd
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:16:12 +0000

    connected allow square period education

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  def convert_to_hex(n):
+        return hex(n)
