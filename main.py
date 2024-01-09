b9950cb60afea1e13b316d89e8a10950758fb885
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:56:55 +0000

    sound blind natural season take

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
