bbd57f3eed95cfdaef9df7a0887c688b702b778e
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:44:49 +0000

    doubt pile rate frequent date

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
