aa513306c85364d62668ede4ec6d1011a181b2cb
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:28:04 +0000

    arm stranger clear fond possibly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
