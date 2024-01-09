4d950a4ee3c321a8b447ad4ff479d4a9790aa949
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:47:54 +0000

    violent noone hers beautiful dinner

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def multiply_numbers(x, y):
-        return x * y
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_power(work, time):
+        return work / time
