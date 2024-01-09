019a359d1e78fa46abebfcfe98c117a1c1b24cc9
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:57:56 +0000

    better tense large heat callback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_item(array, i):
+        return array[i]
