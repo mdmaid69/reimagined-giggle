d3f28a14bed4fee7d4b5f2ac228957e989b181d3
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:21:43 +0000

    church capital practise load middle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def convert_to_binary(n):
+        return bin(n)
