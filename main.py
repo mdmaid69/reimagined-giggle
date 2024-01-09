3ee4316aa030a241c4704f37e58ffda34bc7eb3b
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:31:59 +0000

    obvious reach weigh further young

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
