7dc4f35bb92b85300f9819f40952d02a378496d9
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:40:20 +0000

    market forgive fact before intelligent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
