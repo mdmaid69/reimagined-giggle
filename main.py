4eead4549ac4cc9bd90db4a4b1016725ed0173d9
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:30:41 +0000

    I Nn share use getoff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
