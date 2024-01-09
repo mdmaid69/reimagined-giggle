47a739faf21bc05de8926baee5fcc141ec7a5788
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:50:30 +0000

    shoot movement watch float believe

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
