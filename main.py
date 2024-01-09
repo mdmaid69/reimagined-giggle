23f4f4a571e3437dc875ccbd96daa2997b65d84a
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:31:52 +0000

    back drink contain nature smile

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
