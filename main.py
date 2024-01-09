4113cbec4ffc84e19ee55e12a300f9890db6f0df
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:07:08 +0000

    anxious ahead another most once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
