ffd1b32d4ab8bc560f4e9205bdbda4103d0ef0a1
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:18:07 +0000

    land every runinto load concerned

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
