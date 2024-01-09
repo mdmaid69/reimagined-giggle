6ff645002926f44f74b6787d5ed68633c8ab4cc9
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:15:22 +0000

    holdon attach convenient suggest once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
