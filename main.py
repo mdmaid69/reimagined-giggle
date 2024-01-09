f2ec484e4b7375a9a2e8c8dd42094b605f557425
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:37:52 +0000

    solve polite up see control

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import datetime
+print(datetime.datetime.now())
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
