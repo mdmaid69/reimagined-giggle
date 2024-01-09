c8b6eaac2d09df7434952e879cc167e94b5a2b49
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:31:16 +0000

    reduce all spring popular connect

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def count_words(sentence):
+        return len(sentence.split())
