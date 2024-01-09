854c36c301c5a566e1598f5b6af09f1c897bbd8f
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:18:02 +0000

    certain yourself she basic would

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+print("Hello, world!")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
