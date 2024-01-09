72efa5304a605d1edac6a6670eff147b198c076d
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:31:25 +0000

    turnoff period empty wing admire

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
