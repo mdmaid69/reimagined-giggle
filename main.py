d68a2822cdc518370d1e915f195be204431857ea
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:46:03 +0000

    pay wake someone drink official

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
