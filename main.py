418441a0ec71694f15205adca2e9aed322942888
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:55:27 +0000

    lie false alone congratulate truth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import collections
-def create_queue():
-        return collections.deque()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  def convert_to_octal(n):
+        return oct(n)
