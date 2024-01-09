8d7f8e719b8fa1c8861c50f26359c702940f6d1d
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:19:51 +0000

    silent odd deaf slice plenty

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
