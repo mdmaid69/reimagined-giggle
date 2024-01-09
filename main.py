e4910e9188a8842186f262738f7388249d23c0cd
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:03:23 +0000

    apologizeapologise make whether switch meeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_perpetuity(payment, rate):
+        return payment / rate
