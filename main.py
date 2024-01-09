6f28277af8a9ba8cc7b15fb666e9c05c29d12a02
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:59:24 +0000

    clearly upwards influence broad although

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
