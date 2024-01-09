c28ed3ef9c691ccf04dead6b91d2bbdb1f42155a
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:40:21 +0000

    human price land house influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
