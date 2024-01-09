c1df6082e2749b0583b3cad1508a7f62df308081
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:08:39 +0000

    case business whatever swing effect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
