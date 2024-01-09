e7f09a8b76e0d192301d196ccf884fc978aeb757
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:32:22 +0000

    rough record increase waste pipe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def is_palindrome(s):
+        return s == s[::-1]
