b87f3e76aa4bb677c6fef355072fb1d224a80dce
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:47:50 +0000

    even ordinary can famous themselves

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import datetime
+print(datetime.datetime.now())
