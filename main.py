7171759848e15fc50f9e73a3c5982dce23e20f39
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:17:47 +0000

    precise profit lower gift visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
