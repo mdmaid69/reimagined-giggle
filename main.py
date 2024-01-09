c296fa9036820e7ab80cbab9e4652bf696a2a9ab
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:46:08 +0000

    never heavy payment debt female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_list(array):
+        return array.tolist()
