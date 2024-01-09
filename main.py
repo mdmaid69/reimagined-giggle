3ed9fb67c92e3d714f6e64704504a76d4831a8cc
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:34:42 +0000

    repeat there walk gooff convinced

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
