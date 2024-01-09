440ca34420a47c913cd6276a2373634e4b9d21c8
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:21:57 +0000

    talkinto guilty measure usually calloff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import collections
-def create_user_string():
-        return collections.UserString()
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def find_min(lst):
+        return min(lst)
