7e0d9f8fae4c77bb2565c726d706afebdc2b9c88
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:30:25 +0000

    family somehow track obvious brush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
