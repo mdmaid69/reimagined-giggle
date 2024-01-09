0252a61e7d9036aaa063231c81c849cd5ea102d3
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:28:01 +0000

    neither plan solve run describe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
