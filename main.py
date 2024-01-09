176b14e3af3f117fb80db114a20eda0a73a04309
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:35:21 +0000

    consider instrument nor trust strict

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
