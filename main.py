f9dbe14b81bef4b0d9cfba6a1a1b3f03dc92de4a
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:23:10 +0000

    while should bath ride turnon

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
