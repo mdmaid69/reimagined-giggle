ec98a5513d74388323bccba9411d5048e0821e77
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:29:03 +0000

    offer ability save persuade trousers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import random
-def roll_die():
-        return random.randint(1, 6)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
