799b25aa7f4656b9dab0095de45653c394a451d3
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:18:35 +0000

    discover season since thinkthrough anything

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
