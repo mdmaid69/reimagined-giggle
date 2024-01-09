5556910db9611ac36516d5d04cd5f68d39599192
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:56:19 +0000

    our goout prepare radio shut

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
