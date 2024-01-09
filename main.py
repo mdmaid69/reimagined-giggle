20954c76735e9e9b1533c8d30cf2614a17575b7a
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:43:44 +0000

    nigga shine understand swim building

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
