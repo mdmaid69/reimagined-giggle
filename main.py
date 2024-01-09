2fdcf0b60eaf4816680cafbffb03cc5b84e3a125
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:29:04 +0000

    exchange range apologizeapologise fire proof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
