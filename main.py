071273219d9981d57c8dd9d2d97b233e649a77af
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:51:58 +0000

    black row appeal book responsible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def get_array_item(array, i):
-        return array[i]
+import time
+def get_current_time():
+        return time.ctime()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
