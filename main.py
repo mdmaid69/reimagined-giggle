a4fd31b72300280ddb1528e4b475c859bb6c4622
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:10:01 +0000

    high wide Ww better many

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
