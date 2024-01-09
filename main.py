aa13510c3e88f6fa6edef1df0f552eacc04021f7
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:41:37 +0000

    lead partner visit loss often

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
