42686bac4cfb0e76fe13805be252a8b15653bc10
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:18:47 +0000

    fun inform return experiment treat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_power(work, time):
+        return work / time
+import array
+def set_array_item(array, i, item):
+        array[i] = item
