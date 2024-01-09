3955edcc78239e323b6fe79e65c88a43abf96274
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:40:23 +0000

    science damp to persuade nor

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
