c6061c41bf72691abfe6e80c8d3d083e8732e19f
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:39:47 +0000

    other inform spring comeoff different

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
