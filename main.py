0a1c5919f34e03757e666a796d71db941c0e902b
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:19:20 +0000

    only whom comeback agree stay

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def calculate_speed(distance, time):
-        return distance / time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
