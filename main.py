ea215b151abcacfeca9a1326eeb4e8aea16880cb
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:32:12 +0000

    at bag we want spray

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def convert_to_octal(n):
-        return oct(n)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
