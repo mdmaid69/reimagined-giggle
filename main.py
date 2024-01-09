eef8a0644b13e4e4224239e04944a401b3124477
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:18:43 +0000

    silent pocket breakdown plate near

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def sort_numbers(numbers):
+        return sorted(numbers)
