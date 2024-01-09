e2ac892741720c61d7df682dc4b416c93c937075
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:00:20 +0000

    goodbye cross length agree surprise

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_length(array):
-        return len(array)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
