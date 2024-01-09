65e24168e0aef4ff865c7eeae2295e9059dbfbab
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:08:52 +0000

    black ticket permission traffic label

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
