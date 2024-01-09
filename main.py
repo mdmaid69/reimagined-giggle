271586c1cf9c3abf57575baee9b9af464a079117
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:58:23 +0000

    creature head please whoever fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
