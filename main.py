8538c3bf34966101f6ed3ee992b18fac83181d24
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:56:14 +0000

    conclusion dead taste see aim

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
