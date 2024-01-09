efc0664a562f28952b11645b684408178f4cf9eb
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:59:24 +0000

    children then table everywhere rubbish

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import math
+print(math.pi)
