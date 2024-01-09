f8e0aa4bbcdacc7ff9d57ae43a6945e83ae094ed
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:13:16 +0000

    pin operate operation bite past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
