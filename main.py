7e525cd1a9845bc0d0d55a3b996ed0eb6c1c5e8a
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:29:20 +0000

    though touch border situation support

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
