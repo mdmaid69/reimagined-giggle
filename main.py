4ca575fa44f8858582770d2a1110d9451ef85708
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:51:28 +0000

    order curious tomorrow shallow drawing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import sys
+def exit_program():
+        sys.exit()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
