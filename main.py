8444bc90c024322507e647689a27cd7fde6786ec
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:15:26 +0000

    hurt holdup idea match reply

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def multiply_numbers(x, y):
-        return x * y
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
