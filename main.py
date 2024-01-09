36b4547e06bd9feb8c6adb0f1b6ffdca993a5f83
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:11:47 +0000

    block noone performance congratulations give

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
