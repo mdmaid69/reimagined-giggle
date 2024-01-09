4d18fe89d289cc7c47a6ac88c8862e1c31b8d165
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:14:40 +0000

    ordinary during themselves represent admire

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def square_number(x):
+        return x**2
+  def add_numbers(x, y):
+        return x + y
