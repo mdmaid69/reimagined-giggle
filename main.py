548620999384f4ddb740dcc705a446431be0716c
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:18:55 +0000

    responsibility force busy bin pleased

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
