b81ebc1102569bce3181ce0f3393d61fc22ce8ef
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:04:24 +0000

    time habit extremely report brain

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  def cube_number(x):
-        return x**3
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
