ccb59510212921c019c1faaa79f5b4cc615dcb57
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:02:27 +0000

    forever notice soon complain need

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
