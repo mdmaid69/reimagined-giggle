36bd26c957f0aaaa0643bed1f5dd45a7d8c230ed
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:48:34 +0000

    healthy owe indeed trick emphasize

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
