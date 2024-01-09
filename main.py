24292f5d55fb9872b011d05c8f32eeb0fb3cb806
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:35:56 +0000

    comeoff strange during centimetre young

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
