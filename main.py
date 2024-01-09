7dc1c1d285a1974d1a039cab7791f4789ed306e4
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:47:56 +0000

    foreign whether nor nose shallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
