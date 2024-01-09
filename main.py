65d3faec5822aabc2fd975c0b18050f3c6a0ba8b
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:30:04 +0000

    shall touch tie majority earn

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
