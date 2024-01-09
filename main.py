05e610167ceaf9306d008b3b098e794e29bf031f
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:28:10 +0000

    suffer square ball issue operate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+import array
+def get_array_typecode(array):
+        return array.typecode
