0ff1a96cc8316bb226442312bf3fccc5ba280514
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:04:21 +0000

    damage Tt warn basic hear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import os
+print(os.getcwd())
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
