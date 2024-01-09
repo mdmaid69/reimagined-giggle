e399bfb53ffe060750095822ce3619edae6fe3d7
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:09:04 +0000

    agree organize busy tip separate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
