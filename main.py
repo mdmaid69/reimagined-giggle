527277cf16674fe5922b34dab5804cefb9bd45e2
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:26:25 +0000

    tall uniform receive sometimes narrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_area(radius):
+        return 3.14 * radius * radius
