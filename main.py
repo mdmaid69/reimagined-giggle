7ece4783a3797608d9a93b7a70cacedcdd766eb1
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:39:52 +0000

    tie priority unkind about Dd

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-def find_min(numbers):
-        return min(numbers)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
