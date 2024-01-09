fff93e077279cc48e3b56b07da49086a6a6fc430
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:06:20 +0000

    your clear far active punish

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
