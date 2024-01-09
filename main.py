d7122da407f16e23b4c5833e6004925e9dbe71f3
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:34:27 +0000

    effort go Itself which behind

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
