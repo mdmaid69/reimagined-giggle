0cb83dbe4bfe212b0abc14ec618e27dd35385b3f
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:10:18 +0000

    attractive error before ocean hang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def is_odd(n):
+        return n % 2 != 0
