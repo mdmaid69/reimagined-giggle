12921cf9f092184ac7ae600c007a66304c8e4158
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:32:03 +0000

    comeoff priority still boring public

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_min(numbers):
+        return min(numbers)
