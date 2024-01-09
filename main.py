345e56337cbabd42fabc34f5b7e4b144133cdffb
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:14:34 +0000

    know pot usual protest belief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
