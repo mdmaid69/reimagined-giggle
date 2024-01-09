a426ecfa7750d9c460905122da8a2ee4b339d7da
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:32:42 +0000

    no subject used back affect

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def reverse_list(lst):
+        return lst[::-1]
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
