19929e908aa19431453160fd63b01447dd59599b
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:43:03 +0000

    564encourage improve right motion worse

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
