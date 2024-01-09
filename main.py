c468e402ea7e8e7cd2be33246b744786a7c888b0
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:12:38 +0000

    possibly guess dark cold hospital

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
