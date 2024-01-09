16bf7de2265dad9f8ae00fc12c915e7fe0113ced
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:29:04 +0000

    flight mark everybody forward team

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
