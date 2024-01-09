06be72508fca3d803971e567914f4f2f9a4c1ef2
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:18:50 +0000

    comein common habit term around

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import os
+print(os.getcwd())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
