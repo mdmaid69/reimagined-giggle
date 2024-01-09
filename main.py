b1713740d35773573265ed4c4095bd413843d1cd
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:00:11 +0000

    bury buy taste constantly sick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
