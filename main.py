b0f75cc6b5f885826837ceda959466e1439cf55b
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:36:46 +0000

    damp introduce side story react

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def split_path(path):
+        return os.path.split(path)
