830c21fff43da4fe7b1d80acc93883853abce6ad
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:14:07 +0000

    equipment shallow cry question kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
