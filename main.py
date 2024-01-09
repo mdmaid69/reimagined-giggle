c6a6d49b33311be02fc9f57acbbcfdc638eee2bc
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:45:12 +0000

    form thankyou hurry scrap world

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
