2becfa8e2889d43915c5afd644c184f29c5823c5
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:40:30 +0000

    clue become believe summer work

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
