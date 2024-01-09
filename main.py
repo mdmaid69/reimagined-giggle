fcf48ea7ef6a24cbf86a6708d95f39dcc3e485c4
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:04:31 +0000

    last either traffic face payment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
