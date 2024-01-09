5b3f24c6576f2483983c4aa52f98d7dba82d7d24
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:00:09 +0000

    tradition fond sensitive string mix

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
