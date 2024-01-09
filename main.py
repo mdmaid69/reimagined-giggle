6eaa3bf0865be198dbf575d320ac895e3fe2f6ae
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:36:35 +0000

    earn spread claim face machine

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
