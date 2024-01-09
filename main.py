1e568ce25ce16e8a8a6c94e2c6f1035de9653213
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:16:19 +0000

    provide offence welcome Ee blow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import random
-def roll_die():
-        return random.randint(1, 6)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
