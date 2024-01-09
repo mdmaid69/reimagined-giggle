2953a36a9e57e22d7ff134d1ab04abd4db7c40ba
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:41:49 +0000

    right harm she discuss goback(to)

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
