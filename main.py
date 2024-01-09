44e584ce27ee0aa48cc7a6f5ef577f989ded17aa
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:05:52 +0000

    anywhere to age damage worse

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
