fb3ffc7f28633788f4727070835b1b3410ae2cfd
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:22:47 +0000

    worth recover tool believe nearly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
