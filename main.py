6b05dd951df19f947226b567e15753c7a3779269
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:53:34 +0000

    whom comein language follow change

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
