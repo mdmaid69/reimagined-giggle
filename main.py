ed484e371ae07fcc16c5b84cf6e127b38681e689
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:09:39 +0000

    traffic disagree keepup figure declaration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import collections
-def create_stack():
-        return collections.deque()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  def square_number(x):
+        return x**2
