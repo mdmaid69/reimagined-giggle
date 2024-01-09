58987f62fb04bfa70d87a9668e02a9322af92857
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:47:08 +0000

    congratulate excited just fair godown

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
