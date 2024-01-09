4a961269623ec1d23bb15aece5cbe41080d295b0
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:37:31 +0000

    fasten thinkthrough and beat loose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import collections
+def create_priority_queue():
+        return collections.deque()
+import array
+def get_array_as_str(array):
+        return str(array)
