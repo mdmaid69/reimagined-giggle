f46c39c369f4e245b511ff8020bef9aed2f4a1d6
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:22:23 +0000

    no in steer organize train

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+for i in range(10): print(i)
