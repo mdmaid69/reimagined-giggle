2652388134dc9aa9fe1dc9f4511c06782aae8f2a
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:03:27 +0000

    appear preparation save kick bridge

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
