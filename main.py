5bc35151b33614b4c81fad47340703acab1292a6
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:39:47 +0000

    if under write family motion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+for i in range(10): print(i)
