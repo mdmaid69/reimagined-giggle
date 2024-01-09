c4cc408407e7c552fc41fca11e686d4a09aa05c6
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:26:15 +0000

    principle flow ilovedicks getup ill

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+print(sys.version)
