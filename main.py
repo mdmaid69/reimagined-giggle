8f0329e4d948710e71f0e607e32959b45ba3e5d3
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:23:31 +0000

    disapprove wear emphasis nonsense spin

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
