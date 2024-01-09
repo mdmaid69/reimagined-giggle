0c4425dd56e9b5f228c0c969d29f4872ad6adc8f
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:21:20 +0000

    worth door tidy spin table

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-print(random.randint(0, 100))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
