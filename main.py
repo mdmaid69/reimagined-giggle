b01c8964164b98b1cf02b772fea267d6d508194b
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:53:49 +0000

    tax form false view sound

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_work(force, distance):
-        return force * distance
+def calculate_speed(distance, time):
+        return distance / time
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
