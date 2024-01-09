8e59ec8f26a79d9908ed40a2bd682d460a7ba78e
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:00:14 +0000

    easy table turnup know create

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
