9fefd25aa4203bf2bb028e7c8911f70b1a0c1c1a
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:57:50 +0000

    last read turnaway butter contrast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def square_number(x):
-        return x**2
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
