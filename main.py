4cd2a7e528bafb5ddbe6c2eb82fc0e5d13cad445
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:28:27 +0000

    dull well wash yours tour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def find_max(numbers):
+        return max(numbers)
