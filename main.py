27cc0a5374969107abe3a2fd373aa8050357ec89
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:47:18 +0000

    keepaway image turn concentration point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
