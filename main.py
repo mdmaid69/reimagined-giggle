12a5b8c772327a8903aea67fb3e23d912608ad8a
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:57:30 +0000

    shop die contact comment town

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(5):
-        print(i)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def reverse_list(lst):
+        return lst[::-1]
