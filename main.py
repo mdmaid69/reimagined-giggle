d3194280dd68b949eeb9d774e6bd416232fbfe1e
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:01:36 +0000

    you follow cook aim treatment

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+for i in range(10): print(i)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
