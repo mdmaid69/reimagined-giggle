031cc8e23dcb54de9b690e51dc6372674767e0b9
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:28:58 +0000

    false seriously than past only

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def change_working_directory(path):
+        os.chdir(path)
+print([x**2 for x in range(10)])
