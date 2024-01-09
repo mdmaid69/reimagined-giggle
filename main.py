13dbc565ef2952c9f885d15a955821f178978ad4
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:32:52 +0000

    concentrate tradition treat drop pattern

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
