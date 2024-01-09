53469f677876793b20e50716cbfe593df1256354
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:14:49 +0000

    yes doubt schedule small glass

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def clear_array(array):
+        array *= 0
