8737bead438d9d54e55de6802f46d595d380fdb8
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:36:24 +0000

    stretch extraordinary about office complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
