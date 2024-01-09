162ecdcfb0085c0e4d67f212605b457bf39b1414
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:41:06 +0000

    top mail extra acceptable yet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_user_list():
-        return collections.UserList()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
