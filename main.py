2c79d8db838ed80e840d0a724992b33797ca4d72
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:00:21 +0000

    just punishment characteristic tense company

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
