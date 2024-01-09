245843f6106b89541f8595483fd9344c2ffff836
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:37:42 +0000

    embarrassed hope train careful opinion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
