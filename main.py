f52ded007cd8f14b5bcaa3ae362cb7f5dc49f132
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:17:16 +0000

    defend bet thin pupil proper

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
