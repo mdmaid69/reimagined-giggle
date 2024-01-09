cb4018ee6eb2b4b664a5d836e06fcc997445f1ed
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:15:40 +0000

    much contact not represent without

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import os
+def remove_directory(path):
+        os.rmdir(path)
