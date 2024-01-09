d4a0d552d65be9745def521be614c54690096e6b
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:44:28 +0000

    high limit experience either notice

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
