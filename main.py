ca1621ec67fad8aba09cd1b835392c604aa70741
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:56:20 +0000

    among getup rush turnout bury

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
