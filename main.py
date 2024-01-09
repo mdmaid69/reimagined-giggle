d0e8332a2362b9939aa3b02e195dfbeb3449dff4
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:09:06 +0000

    no terrible unfair at trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
