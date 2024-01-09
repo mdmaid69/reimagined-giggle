e331faaaad30f4f4ff5abc209d73aab320c6c8d9
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:06:30 +0000

    rather borrow cinema hard past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  def multiply_numbers(x, y):
+        return x * y
