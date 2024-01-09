087e72e26f0e70c745f345dfc77f0527d8314993
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:26:36 +0000

    hardly heat cap leg admit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
