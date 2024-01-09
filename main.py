13296b66f5aafba0462e1132a04a514689f7ebcc
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:40:01 +0000

    rather concentrate nowhere keepfrom necessary

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import time
-def get_current_time():
-        return time.ctime()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
