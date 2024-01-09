c658aeb0cb9cae05d3059d24ea7c1bacc12bfd96
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:55:55 +0000

    conduct measure justice memory example

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
