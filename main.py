b2ec5313dd78957b901d58b89acfe194f2fa74b8
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:11:24 +0000

    watch away realize basic journey

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
