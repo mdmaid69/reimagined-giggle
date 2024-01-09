6f91254740bccf8186cd66ff8491c67d73db62de
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:04:17 +0000

    advise dress mystery centre load

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+print([x**2 for x in range(10)])
