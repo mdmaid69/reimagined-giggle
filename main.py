1214c5d70d1143adfd2bf0ccb6dc7015b2214178
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:32:32 +0000

    any 572enter trick trust shine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import time
+print(time.time())
+import array
+def get_list_from_array(array):
+        return array.tolist()
