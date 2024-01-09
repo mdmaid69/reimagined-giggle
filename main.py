ce64e807cfcf4799a6cae1c51a34be012846c488
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:57:06 +0000

    hope control fly suckme split

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def is_even(n):
+        return n % 2 == 0
+def calculate_average(lst):
+        return sum(lst) / len(lst)
