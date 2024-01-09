ece62c0c685ca73f89e6f8ba1cbec4c7c05e6dd4
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:47:13 +0000

    after kind level instead anywayanyhow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import os
+def remove_directory(path):
+        os.rmdir(path)
