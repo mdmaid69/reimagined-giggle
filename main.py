2d96ec971513a43ed6bf7d80f51f3bcca62df8e2
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:18:24 +0000

    soft list bit hear process

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def sort_numbers(numbers):
+        return sorted(numbers)
