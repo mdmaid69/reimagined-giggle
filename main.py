054d59118973800149462c04eaa896cbec0851c9
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:51:11 +0000

    talk question occasional my thanks

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_power(work, time):
+        return work / time
+import os
+def change_working_directory(path):
+        os.chdir(path)
