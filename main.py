cac8733f53cb1985c1635f3643d6dffe2da41578
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:53:26 +0000

    continue shopping prepare getup height

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
