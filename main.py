b6afb71efe50f4f5ceda5fc14b664ff9435a8bc7
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:49:08 +0000

    genuine patient leave any rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
