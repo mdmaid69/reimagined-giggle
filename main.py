c73303d0286b8d7199491adf47abe3638f8ceb85
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:42:19 +0000

    complicated leave mysterious praise busy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import datetime
+print(datetime.datetime.now())
