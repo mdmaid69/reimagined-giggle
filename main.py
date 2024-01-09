046134314ee2b3317d55685fd299da3e4433b827
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:14:18 +0000

    dream pleased about remain discover

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def insert_into_array(array, i, item):
+        array.insert(i, item)
