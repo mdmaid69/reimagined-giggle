4db9240228111b7c7af702d8eb1fd4997ec21fce
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:34:49 +0000

    talkback condition fall position herself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
