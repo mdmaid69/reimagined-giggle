b479a364c1057d32f46bcdcdf2fb7f1ae5ecbfef
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:30:50 +0000

    565end thought nigga quick strong

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  def subtract_numbers(x, y):
-        return x - y
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
