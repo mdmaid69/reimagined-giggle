b5ba02b43b280c53d3e6deb5234888847f6451d2
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:54:59 +0000

    centimetre and slice partner improvement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
