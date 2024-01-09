5893b4a0a64e75eba3d3facefbd17f8b37f717a7
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:21:38 +0000

    effect surprising somebody disappointed listenfor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
