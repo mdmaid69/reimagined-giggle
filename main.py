213144ee3263655bfe931d169d8d3eb29453959e
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:24:31 +0000

    generally public stick appeal direct

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
