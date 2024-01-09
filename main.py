4b9edead8b255320ba4f5f39d3a70bd368d820f0
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:42:26 +0000

    opponent actual children sometimes door

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
