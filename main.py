298b078a26c0533ed3559d52ea0de96f842a3e74
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:40:51 +0000

    brief 564encourage tear so pleasure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
