0de5a79a6c20c70b316c39c3eb1151216cd35ffd
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:29:21 +0000

    stupid instruction conduct stay secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
