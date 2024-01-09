c3dd3091f195986046aa5674ba4445bd840eb169
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:06:36 +0000

    load character passenger before mysterious

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_area_rectangle(l, w):
+        return l * w
+  def convert_to_octal(n):
+        return oct(n)
