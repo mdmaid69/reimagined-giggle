cde69ec021070ea46e624cba84884c49faadc420
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:37:55 +0000

    breakinto direction reasonable smile rid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_force(mass, acceleration):
+        return mass * acceleration
