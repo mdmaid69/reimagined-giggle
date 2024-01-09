a0f09f8682be567cdb8e07b40896d41ea4ed83b9
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:22:55 +0000

    spread active blame send avoid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
