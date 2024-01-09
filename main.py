27b076d3636f49a90beef6398b962523048aca74
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:39:24 +0000

    everything dry child mind lift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
