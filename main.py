e9a49803814db7a68652eb3d0a0379050a07b71a
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:59:51 +0000

    properly question connected unable nigga

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
