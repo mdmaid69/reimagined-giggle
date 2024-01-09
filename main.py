b517c8d606b1546bd34524bb5900938c308a72f6
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:00:38 +0000

    recovery pollution meet think advise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
