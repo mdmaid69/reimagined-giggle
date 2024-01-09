e79038b53a4e543ed797d6ecddc01dc4e93185b6
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:31:44 +0000

    extraordinary division search sometimes treat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def count_words(sentence):
+        return len(sentence.split())
