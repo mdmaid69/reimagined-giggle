764d1717040c9934188490da76b57d0dc36ebd23
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:25:02 +0000

    way workon clothes today judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
