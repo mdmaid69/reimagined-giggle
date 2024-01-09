95b4fa7c799eba256cdce13ef322d43ce7df91cd
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:56:46 +0000

    without scheme almost actually laugh

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
