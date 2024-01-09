3c5502ea8a1f1c57d20e0ed2372a6361576c7933
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:43:32 +0000

    enthusiastic seriously fortunate matter remember

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
