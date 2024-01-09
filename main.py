fb1bb67687d987be954deac1cad0be7e6a7e72e9
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:54:53 +0000

    wind wise ours thin present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-name = "Python"
-print("Hello,", name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import platform
+def get_python_version():
+        return platform.python_version()
