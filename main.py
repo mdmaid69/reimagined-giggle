b6fae29e9e0ae74f201aa0e3aaf8893449666696
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:06:03 +0000

    skill tense mix walk appearance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
