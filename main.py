e886fe0168255903160582d5bf4fc5af152500de
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:43:39 +0000

    some recent lock ticket runover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import sys
+def exit_program():
+        sys.exit()
