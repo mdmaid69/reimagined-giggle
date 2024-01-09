94f636b9a5957eed97318afe28862c77092a931d
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:49:46 +0000

    hour measure available shoulder unable

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def get_array_as_bool(array):
+        return bool(array)
