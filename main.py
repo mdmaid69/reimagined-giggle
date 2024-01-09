3ffe8f3ae0099f9f0b25933c5dffb58c7b82eb27
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:47:17 +0000

    while quiet pair them lift

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-import sys
-def print_python_version():
-        return sys.version
+def convert_array_to_string(array):
+        return array.tostring()
