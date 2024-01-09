e1e001d6833fc533c1b169486bd99a4f37bf7fe0
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:52:32 +0000

    Ff breakin soon swallow top

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
