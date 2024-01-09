3bfe48d728565635ea3e80a2606d65b0d7b346e1
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:14:54 +0000

    man leave command mess short

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
