7f041bec3267b9bb9ad288b3addbd2df5355c230
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:37:21 +0000

    slice combine male lonely basis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def remove_duplicates(lst):
+        return list(set(lst))
