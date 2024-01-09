81bb8e7afe3c34f1b9bc297103ad63d68445e1a6
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:54:13 +0000

    handle take part this law

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import re
-print(re.match("h.*o", "hello world"))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import collections
+def create_user_dict():
+        return collections.UserDict()
