84c2edaa6bfeeebe47e9c35b47417a3958a9b987
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:10:53 +0000

    left home spend hurt breakfast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
