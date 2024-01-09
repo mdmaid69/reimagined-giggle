2478d7a5f9af0277ba291128200163c43c0b67bd
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:48:42 +0000

    make callback Gg pay clothes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
