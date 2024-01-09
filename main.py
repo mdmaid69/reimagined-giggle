601049f877f7bdc2bc33da70927cd410bbf49909
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:00:43 +0000

    scene steer course 572enter course

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def convert_to_hex(n):
+        return hex(n)
