b05ca8c1fd3d8ec53ad9615021281d3b8432f134
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:28:23 +0000

    speed rather foreign create myself

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_speed(distance, time):
+        return distance / time
+import time
+def get_current_time():
+        return time.ctime()
