f29d0bb45f0a24a2ba29c88e6eeea0abd4820f57
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:06:38 +0000

    during live authority contrast somewhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def add_numbers(x, y):
+        return x + y
