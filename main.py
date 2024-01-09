ce6d1e4d005db97a711c7aa439177af27fbd5f6e
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:41:50 +0000

    slight previous succeed temper bottom

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def is_odd(n):
-        return n % 2 != 0
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
