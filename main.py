715dd4dfbc347bcfa3896bc87972bea5769596a8
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:32:57 +0000

    pencil basically better kind ought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_area_rectangle(l, w):
+        return l * w
