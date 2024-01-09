e6071dc37b0e22ecfc5aad763a294589ed0a124c
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:09:37 +0000

    public period definite top recommend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import collections
+def create_stack():
+        return collections.deque()
