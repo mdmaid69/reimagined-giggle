936f67b93039787c782db501f09bd18bc60ede39
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:25:03 +0000

    beautiful stay offence danger store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
