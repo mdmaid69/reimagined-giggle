cc586901eabfe8fd8afbde77ec48aba16ed11938
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:48:18 +0000

    demand afterwards giveback temperature side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
