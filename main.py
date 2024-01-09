05ef266e86296c9858ea2d62b4f866d0cbeef3ae
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:51:42 +0000

    keen decide shut doctor fasten

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
