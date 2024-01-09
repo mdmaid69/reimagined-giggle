4a6a6fc8b2ca6704a5a716897ae55280933bde48
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:49:09 +0000

    row at glad tear apparently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
