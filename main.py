035b98270b9168e43d43e667f0b2c75fc938a611
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:08:28 +0000

    comeout level please force statement

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
