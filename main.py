c02947f2ee7f35fd3b8a87c6bf6683668477727c
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:19:36 +0000

    square unite rough language involve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def reverse_list(lst):
+        return lst[::-1]
