1992a36799c7e1dc9494dfb6408828bfa4cdad82
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:32:08 +0000

    exchange journey old confusing quit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def is_odd(n):
+        return n % 2 != 0
