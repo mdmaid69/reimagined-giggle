e048cd72b374c6e2d74434a50dfc235e9a1fc1b5
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:18:17 +0000

    receive much Qq Itself preparation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-for i in range(10): print(i)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
