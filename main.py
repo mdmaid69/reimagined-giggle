ef281cf0cdfe545dfeb3d56da7398c4b691942cc
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:13:27 +0000

    game great snow ahead record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_current_directory():
+        return os.getcwd()
