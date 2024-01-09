c9bb4cec525b8aa5db0ba6abe0c509a61ccc16c4
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:17:24 +0000

    through wing creature people gradually

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def greet(name):
-        print(f"Hello, {name}!")
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def square_number(x):
+        return x**2
