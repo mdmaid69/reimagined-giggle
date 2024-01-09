c9174789afe4ee14fed45bb033df31ed4e33b960
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:53:19 +0000

    wake breakdown everywhere birth region

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
