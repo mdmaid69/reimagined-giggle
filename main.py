64efa94c37b753934275a6c4fdfbe69bf6cf53f0
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:12:17 +0000

    opponent not dress it certain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
