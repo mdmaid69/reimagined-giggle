4b9463a950b798bf5ff285e33d067d6b5be180e5
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:07:11 +0000

    debt complete idea muscle tape

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
