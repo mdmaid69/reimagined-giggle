210135b26ab2facd48cb846d888eb08978930506
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:10:01 +0000

    crowd deal mean much surprise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
