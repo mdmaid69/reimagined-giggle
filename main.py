b357d06d083e7a081af507f385f1a0cd339f892a
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:01:13 +0000

    suck judge fast minimum little

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def reverse_array(array):
+        array.reverse()
