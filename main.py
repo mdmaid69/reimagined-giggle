0a7c72028bd1d53086020f5417b9c2e21969046c
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:12:41 +0000

    tradition remain trap truth almost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
