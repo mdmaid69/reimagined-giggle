0d57a1bae4d7184de4162e6c4c49bd99da9c4648
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:06:00 +0000

    child phone generous high rule

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
