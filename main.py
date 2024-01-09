9870cc0d3e95507b7cff254107d8134ec2581cdc
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:52:24 +0000

    to trust accommodation comment steer

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
