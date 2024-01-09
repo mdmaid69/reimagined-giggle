d4ee4d3b9f4b57e88599d73446f8b3dfe1ecacff
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:31:28 +0000

    570enjoyable shape fall dangerous search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_as_repr(array):
+        return repr(array)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
