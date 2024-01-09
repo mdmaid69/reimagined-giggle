bc70de66ba4a9d0fe061f1965c87d77b02b5203f
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:40:56 +0000

    hand emergency loss enthusiastic notice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
