a9ab326219808a8d722efc3dd55e499bb0195cc0
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:25:14 +0000

    everywhere fairly still sink old

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
