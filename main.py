3b85f7155f65d9fb6e96da8b4d35a0a1992f3f24
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:59:07 +0000

    think upstairs best 179bite lookaround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
