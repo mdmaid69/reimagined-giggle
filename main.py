08ba252367744a1f6e92917e564d575d29141cbf
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:40:32 +0000

    command opinion middle tough scream

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
