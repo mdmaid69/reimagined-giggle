3d52c451ef87c7e2d3bfef85ed08124007c84928
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:22:18 +0000

    less market back test preparation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def create_priority_queue():
+        return collections.deque()
+import array
+def get_array_as_int(array):
+        return int(array[0])
