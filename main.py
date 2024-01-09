e6c0afc247f5866a7aa7ca10dae470db2ea391a7
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:31:14 +0000

    takeback buy feel reject whisper

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
   def get_file_name_without_extension(file_name):
         return os.path.splitext(file_name)[0]
