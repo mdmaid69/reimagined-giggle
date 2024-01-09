7451f9b2ca9de10fab6180e1915469f49467a433
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:09:34 +0000

    doubt exercise something because minority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_pressure(force, area):
+        return force / area
