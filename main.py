e72738ee8ac60426f04f0f4b550dcb0510639a17
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:10:22 +0000

    drawing whatever quickly star pump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def delete_file(file_name):
+        os.remove(file_name)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
