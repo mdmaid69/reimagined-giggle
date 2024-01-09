efec05e0099a0bac0bb0ee987e25b197dfb48989
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:11:42 +0000

    regret nonsense result turnaway crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_typecode(array):
+        return array.typecode
