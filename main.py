727937a5e735633bfe1f76e3c5ced4376282fec6
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:21:47 +0000

    breakinto still solve splash beautiful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
