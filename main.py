d04f7e58a05b2c32a741849233dda1726c239732
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:47:46 +0000

    lift beautiful zero vision forever

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
