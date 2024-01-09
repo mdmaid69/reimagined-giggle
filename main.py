2f6aeb3c33caf293acdb7f9b3744d59e72d2f3f5
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:20:03 +0000

    broad correction powerful action workout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
