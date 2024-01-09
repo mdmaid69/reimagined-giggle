cc2298a2873c333017ba74e691305ae971da823d
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:01:16 +0000

    comeround cough part anxious arrangement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
