c594eed85900bbefaf823af67c0e55fe1888b5b3
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:29:41 +0000

    production move taste lunch clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
