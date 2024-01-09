7aa093b3227cce2e2679bf36bb33a3269b41ec25
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:16:17 +0000

    towards design change stick bread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def reverse_list(lst):
+        return lst[::-1]
