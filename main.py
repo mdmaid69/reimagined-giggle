1984a5ea86d5cb7a587d86475483f8899f96740b
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:13:39 +0000

    little bet who artificial interfere

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
 import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
