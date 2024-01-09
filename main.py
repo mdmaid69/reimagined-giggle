1fe5af6afa9634549ce0b7cfd1c215c57b1ca094
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:52:10 +0000

    bottom nowhere platform whistle cross

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
