417e8955857dac531c61e83eed2a02515691908c
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:04:50 +0000

    just passage fence occur union

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
 import os
-def change_working_directory(path):
-        os.chdir(path)
-import logging
-def log_message(message):
-        logging.info(message)
+def remove_directory(path):
+        os.rmdir(path)
