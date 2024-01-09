f2463f6c30e7cf590c6218b6f708d456b7ad0660
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:06:03 +0000

    walk keepfrom Uu comeround help

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def count_elements(lst):
+        return len(lst)
