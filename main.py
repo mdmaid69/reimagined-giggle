8850294f3c0e023047459564f5dba4c1076e0820
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:47:29 +0000

    bank distant later sound talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
