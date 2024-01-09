2ce45c0dc8e8e3cd0ff28cddaae84bc80172b278
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:36:10 +0000

    town however holdup buy disappointed

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
