922df2ae473a30487acfce08623bc98defac81ef
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:28:33 +0000

    prefer exploration shout harm contents

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
