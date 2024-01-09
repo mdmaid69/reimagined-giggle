ccec11803952c3ac7a068bbe558cae88e49806c4
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:28:00 +0000

    affect impression sail to quiet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_even(n):
+        return n % 2 == 0
