00545ad1187d6e6b2911219212d3ca3c2b9e6150
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:28:47 +0000

    they always face friendly murder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+for i in range(5):
+        print(i)
