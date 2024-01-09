533e1fa69683dcb19c68bfb1cc27d3799585c698
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:15:01 +0000

    style getout hard not course

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
