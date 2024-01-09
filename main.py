5331b53838971da2a3494b8f5d398835111ee40f
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:52:41 +0000

    wake no cost design possible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
