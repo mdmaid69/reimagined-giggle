0f9e088f5d0ddc46b84983bcf2d174330b0385e9
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:48:29 +0000

    bend habit interest floor sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def count_elements(lst):
-        return len(lst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
