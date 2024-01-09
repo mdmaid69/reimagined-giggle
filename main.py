3e67a1fde898edb76fbabb31688935918d124416
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:12:27 +0000

    authority getaway valuable break live

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-print(sys.version)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
