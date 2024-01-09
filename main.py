28c78cb8f86b234dff71fedad37a5481e3b79823
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:32:33 +0000

    dance no mark lay narrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def delete_file(file_name):
+        os.remove(file_name)
+import time
+def get_current_time():
+        return time.time()
