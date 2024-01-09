21da3cf7357233b7bee1170bb036bfafe191d8d4
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:05:16 +0000

    zero honest funny passenger mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
