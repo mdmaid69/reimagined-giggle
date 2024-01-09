8ebc12d3bdee692b9b57158801f796cabddc9747
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:34:51 +0000

    reduce nowhere sting seriously twist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_density(mass, volume):
+        return mass / volume
