a7cd1776b52550fc7cef2124a4eeba588901d2a6
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:08:40 +0000

    sign opposite noone pile boil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def convert_to_hex(n):
+        return hex(n)
