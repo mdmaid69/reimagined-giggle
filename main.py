79cf27f13509e73c0817ac57575234b75af00b9d
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:58:31 +0000

    while improve Vv add belief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
