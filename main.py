0396cd72dac11752370519021b3d9ef34293742d
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:06:31 +0000

    precise develop bring equipment independent

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
