2c4b1af8b54d9d5c7fb9d2457987f992495d36ea
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:54:52 +0000

    only clear situation regard fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
