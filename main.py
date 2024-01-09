ed02fdb4f71592826e7deb0424eb5901410e8130
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:08:48 +0000

    eat travel recognize responsibility content

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def remove_duplicates(lst):
-        return list(set(lst))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
