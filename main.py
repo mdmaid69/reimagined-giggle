e1d2fe2fb2733dc18fce5dfd9657db28dbe2b706
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:44:41 +0000

    scene talkback day pleasant heavy

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
