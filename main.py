b29ea41db5ee93a549d2e95c4e677aa6a3e450d7
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:13:29 +0000

    might colour wrap name face

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
