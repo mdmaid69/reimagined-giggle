66eef6a138f54a64e6563428bf8f23d8cce58606
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:45:47 +0000

    belong accept innocent needle comeup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
