7d5337f30867b4d16cdaeac655bd16cfedb12514
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:06:14 +0000

    hello off lesson finish woman

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
