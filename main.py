aa833ea69a893b032d501644d54f7f3982e41d24
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:48:19 +0000

    steep listenfor hurry model but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import sys
+def exit_program():
+        sys.exit()
