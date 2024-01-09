5ba1d12545512fef9b31bcd8b39197599cd06bff
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:20:22 +0000

    estimate positive warning after brown

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def add_numbers(x, y):
-        return x + y
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
