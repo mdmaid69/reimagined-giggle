0f58b780bd8dde8e263e4eb8a132310d9271d9a4
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:43:55 +0000

    funny herself rank 569enjoy loan

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_min(lst):
+        return min(lst)
