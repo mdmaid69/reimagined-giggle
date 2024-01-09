6862a3099cb2368fe268000d24a0bd30e5099c7c
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:45:55 +0000

    acceptable along us operation reduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
