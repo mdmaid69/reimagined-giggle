4ea89ceaa5c7e6a5ffad8679d7657ec1abe629d7
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:14:42 +0000

    walk everybody runover harm Ff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
