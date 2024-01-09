c49aa4ecb2dd3c5760d7b11c8ab593e97cc65ca8
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:06:00 +0000

    borrow stiff talkinto lose Nn

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
