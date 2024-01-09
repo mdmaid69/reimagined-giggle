7dfcb237eee08c53fcc56a3b0a3b5710c410a621
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:27:42 +0000

    platform approval whole contact trip

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
