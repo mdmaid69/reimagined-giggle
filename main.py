df5adebe1a59efd346b80204083aa3059717605e
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:06:44 +0000

    empty provide along stop proof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
