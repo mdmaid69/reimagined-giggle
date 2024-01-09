10daa7b14e4991661321fad3098d20f6e850c3a7
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:24:26 +0000

    walk ahead kind failure else

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import collections
+def create_user_string():
+        return collections.UserString()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
