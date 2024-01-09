0c8ac5532458a60150985a19883d0d5615486a82
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:06:07 +0000

    clean contribute how storm complain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
