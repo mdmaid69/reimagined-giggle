dd3d8228f6519eae565c2e28edbb0d84f55557b9
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:47:59 +0000

    have learn better sharp doubt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
