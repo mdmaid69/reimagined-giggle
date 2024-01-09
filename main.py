6ffcfa96e72c2f7ab2f2f16cd6039d9d68773c39
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:14:05 +0000

    we connected press border arrangement

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
