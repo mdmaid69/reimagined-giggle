d3c3bb098b34795d6820774edebc51319e16fa54
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:44:21 +0000

    score unfair spring kind normal

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def greet(name):
-        print(f"Hello, {name}!")
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
