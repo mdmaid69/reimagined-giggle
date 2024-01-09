80f5973ff447eea5b06642826f7a6e477afb5ec8
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:29:04 +0000

    no excuse definite catch arrangement

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
