8d995ba096c3a27ef5b0ac372085424d847bae61
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:57:59 +0000

    professional shop opposite oil lock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
