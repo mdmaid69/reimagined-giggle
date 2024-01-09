8401015f73c5f22fb0f8f260ae6d9f4b8cf85821
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:28:40 +0000

    interview standard drop culture new

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import numpy as np
+print(np.array([1, 2, 3]))
