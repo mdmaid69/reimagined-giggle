832f4119a650b99cb8284ea3bae68c4b2f39f182
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:31:05 +0000

    divide agree house accidentally maintain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
