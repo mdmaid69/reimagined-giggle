26b12cc7198f41739a48cd781896bd9afbb046cd
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:04:58 +0000

    address child his keepaway mention

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
