1256f90dfb9e9df621e77cc185d248800bb9a4a5
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:04:47 +0000

    fortunately force boring obtain happen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
