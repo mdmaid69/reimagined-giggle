09b04369ded547d00585504edaf2410e6530a5da
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:15:05 +0000

    habit wide centimetre Zz stage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
