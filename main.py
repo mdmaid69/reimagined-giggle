1b3a17592bcda2b011efa9cdbea461b5bba602b7
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:12:35 +0000

    road trust rely show welcome

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
