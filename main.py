6b1e8331b4799be49caba67dbce05945f38880d8
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:20:48 +0000

    right border forget sweep fence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
