0a2b6ddb0b2f5b21f905cb1fb36d9fb09c482535
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:25:46 +0000

    table buy dress fit teach

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
