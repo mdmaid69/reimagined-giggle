961df68beab65c8be9c01a013882c779613694de
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:56:49 +0000

    base motion unit closed another

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def add_numbers(a, b):
+        return a + b
