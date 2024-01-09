01fd808f4e6a6a4439aadc2e3206c401e8ce70bc
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:38:25 +0000

    prove upstairs identify violent failure

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_length(array):
+        return len(array)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
