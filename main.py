7ed67279c8199da3d81e87484c749eaf3d35c993
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:10:10 +0000

    operate inform few gas breakdown

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
