2cf64f47aabb5890bae4876a18beac9231402ffb
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:01:37 +0000

    price talk blue visit 572enter

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  def convert_to_hex(n):
-        return hex(n)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def get_array_as_str(array):
+        return str(array)
