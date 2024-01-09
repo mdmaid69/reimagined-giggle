5d96745f4a55abddbd7e32623caee8cae9ebdfca
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:03:44 +0000

    noise poison frightened right back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
