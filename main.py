3f8a6c1127854fd739b8110f66e7a1ef1134fc6b
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:50:33 +0000

    heart branch total empty wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
