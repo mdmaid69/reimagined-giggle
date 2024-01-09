8ae93d6ad9336bca35384fd11dd3c11f2aa3a7d8
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:38:49 +0000

    strong quality promise lesson attitude

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
