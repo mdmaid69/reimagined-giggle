87e6f1db95bbbb659c03e6edc664c2eddca58a28
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:52:15 +0000

    welcome wind period unlikely left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
