d9bcb2abb8df46498622c863377f3b17c22952b8
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:43:39 +0000

    path accommodation brain character idea

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  def remove_duplicates(lst):
+        return list(set(lst))
