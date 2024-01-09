33ed18902bb07e802072141ba0de716cae14fa9b
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:55:33 +0000

    we agreement problem tell so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
