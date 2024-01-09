6061733acff1458e39d51dbb7c4318d567c58fce
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:12:48 +0000

    petrol before bar smart unfortunate

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
