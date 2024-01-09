7438829cc8ee3c621fa43df3b7883305b1ba7714
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:28:27 +0000

    natural 565end should mail communicate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
