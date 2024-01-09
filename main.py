7cd0362faffd3325cab7163011778d9b5c3d7e29
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:14:39 +0000

    stay rise hear but future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
