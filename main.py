8a7f365030ca85e6eb8357a8dcf88f3772bab9fd
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:26:48 +0000

    morning slowly wide disagree bag

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def square_number(x):
-        return x**2
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
