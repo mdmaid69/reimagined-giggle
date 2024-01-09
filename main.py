0f4fc0af99b28b5a0d8d1b29df3a9bac8b01d519
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:22:54 +0000

    spot stick used mistake Ss

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
