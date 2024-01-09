21d64513851cbff3dc7206561da3877df18ea64f
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:59:45 +0000

    little gradually ticket stay another

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
 import hashlib
 def hash_string(s):
         return hashlib.sha256(s.encode()).hexdigest()
+print([x**2 for x in range(10)])
