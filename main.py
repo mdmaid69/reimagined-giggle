ace14e340e92b838ec758109f4cde586c975a35f
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:44:14 +0000

    nose information best hurry left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
+  def get_file_block_size(file_name):
         return os.stat(file_name).st_blksize
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import datetime
+def get_current_date():
+        return datetime.date.today()
