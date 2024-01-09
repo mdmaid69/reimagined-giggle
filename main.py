723a46034e0ed59d495ed7318d31bdab9cc4a516
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:40:03 +0000

    rush patience tone agreement stamp

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def count_elements(lst):
-        return len(lst)
+  def reverse_list(lst):
+        return lst[::-1]
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
