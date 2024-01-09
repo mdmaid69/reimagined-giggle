25c5d6d9c5c11d617bedbd20ee6d187607fa0c53
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:38:36 +0000

    dead burst live sure indeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
