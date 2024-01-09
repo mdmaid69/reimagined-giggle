6130b5283cf649120492d84ae7ab8d51d2ef22c9
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:46:22 +0000

    slide butter far argument bad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
