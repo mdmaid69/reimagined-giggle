8899d62c1747776bae3a56f99c7076cda1e39680
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:43:37 +0000

    behind transparent objection sound ought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
