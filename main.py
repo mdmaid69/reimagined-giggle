1e50a72528ae66b176f3992787b06bee7bff92f9
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:59:10 +0000

    slide suffer ignore lead even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
 import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def list_files_in_directory(path):
+        return os.listdir(path)
