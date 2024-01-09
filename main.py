2f566a37333599b3d1ae934912ef5a1728dae442
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:26:08 +0000

    summer period not replace fear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_acceleration(speed, time):
-        return speed / time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+text = "Hello, world!"
+print("Characters:", len(text))
