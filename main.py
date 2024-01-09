208f16a7519b6855a6ff2fa8e31e726ded7486f6
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:14:33 +0000

    568energy collect start eye mass

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def delete_file(file_name):
+        os.remove(file_name)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
