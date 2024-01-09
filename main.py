52f1123fba1e28ab28413b219790d60abac2ca44
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:13:04 +0000

    press trousers base recommend rescue

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
