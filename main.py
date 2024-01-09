9fac730636571cda71fd4d7a8400331b8b34d563
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:05:06 +0000

    whether themselves connected except suck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
