c17ecc8aab40c4bbd2bdeaa03b67528444940414
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:52:28 +0000

    congratulate store whatever terrible shade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
