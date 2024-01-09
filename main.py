749ca44afe94eedd28a2473791612abddd18f983
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:33:36 +0000

    force feel distant remain attitude

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
