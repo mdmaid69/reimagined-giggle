8e8e5772565203c37044c2e7c61b81a66946dd31
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:37:18 +0000

    production search callat bet appear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
