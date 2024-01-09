3408f9c9557e4f2d5ec4ee7de8932f8fe5523537
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:45:42 +0000

    because project shape holdout everything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
