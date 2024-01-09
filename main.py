21c6f04bfbdf79465200d2888c4f9ea2940f1251
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:30:02 +0000

    test news famous its exchange

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def is_odd(n):
-        return n % 2 != 0
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
