2bff732837edd606fc0e73d008e7eb02d4daf675
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:46:43 +0000

    runaway learn air substance attach

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
