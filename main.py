0eb70b7da0e1f51da6332f368d530914b0cbf0fc
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:44:20 +0000

    scene swing pin protest deep

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
