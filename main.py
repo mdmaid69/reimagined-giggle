dc5cbbba157b1e7f5497bb69ab0835858706d67e
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:30:47 +0000

    minute lock happy Jj stop

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
