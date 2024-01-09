d897d5225bc4974e7c8efedd160c753c27277d04
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:33:18 +0000

    wrong comment by last intend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
