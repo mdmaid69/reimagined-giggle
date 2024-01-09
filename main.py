c7b880fc4071d57c7cb614925d3903b386cd55e6
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:46:21 +0000

    thanks matter question so drawing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+from collections import Counter
+print(Counter("hello world"))
+def calculate_pressure(force, area):
+        return force / area
