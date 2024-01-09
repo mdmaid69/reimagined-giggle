c0ec6c30b110e36e7aeedb21021d65d87dc8039b
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:33:20 +0000

    complaint help shout advice demand

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def add_numbers(x, y):
-        return x + y
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
