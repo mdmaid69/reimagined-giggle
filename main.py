49237779adffe244374996b60b892595b7243e53
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:22:25 +0000

    left length shadow excited object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
