572c8ef23cfbc1a14f64dda64f897c7322cb4083
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:08:12 +0000

    zero still runout thumb attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
