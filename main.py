026250990091f48e71a483471327d09e6f0cc983
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:12:40 +0000

    store funny polite fear contact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+print(sys.version)
