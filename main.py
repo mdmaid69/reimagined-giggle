4b3ec9f3be8d7182e75e4311d581faa627ab4d4f
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:15:39 +0000

    laugh shout nonsense probably Cc

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
