31d7f7c8490c91243958d64a1c4b4ee7fba3df3b
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:45:51 +0000

    hungry distant guess draw exit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def find_max(lst):
+        return max(lst)
