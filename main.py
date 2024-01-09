84dab3af8860d88e98d13cd7f2e0e9caf1d8dd91
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:25:00 +0000

    pain spill war few swimming

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def count_elements(lst):
+        return len(lst)
