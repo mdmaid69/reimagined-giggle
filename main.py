ed0968c3f9b49865d903feb1fa93360137be7524
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:49:24 +0000

    complain clear pride uncertain home

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
