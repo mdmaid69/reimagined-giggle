ff5c56335b13fbc2d2e65fa879188d462b4aa4e9
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:21:21 +0000

    solve centimetre of influence too

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
