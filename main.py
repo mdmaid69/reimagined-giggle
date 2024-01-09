65aa0a9ba8ef8e56f14d42e53cef22718598a432
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:27:23 +0000

    pour downstairs ticket favourite late

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
