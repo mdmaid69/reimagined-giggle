c14b251150361bc5b50add0aa0a46ab70fd48869
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:10:32 +0000

    supply stamp noisy dear twist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_speed(distance, time):
+        return distance / time
