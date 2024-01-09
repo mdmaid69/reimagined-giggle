448256df9c3a483ab33a83dcf2169b8b6379deba
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:15:44 +0000

    about watch afternoon trouble getup

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
