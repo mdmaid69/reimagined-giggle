e297e6259e3639b19aab9bdba634d178d29ba87e
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:19:47 +0000

    ourselves shout born emotion boring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
