a0653bff12faa29e719a7bc828f78428ae97cf78
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:13:18 +0000

    unit various pencil test between

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
