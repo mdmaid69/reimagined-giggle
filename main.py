69aebb4648fd35f4599ce25306f182c3abf9cfb9
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:44:59 +0000

    private since term apparently before

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
