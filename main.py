c0cc0d39b4c180bffe96a09c45fb89e0fefffb60
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:44:03 +0000

    discuss problem tell attach underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
