f10a92bee2626cf020cd953df1cf49eeb9097c2c
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:06:51 +0000

    regular collect his offend pour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
