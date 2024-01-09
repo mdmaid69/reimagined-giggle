2425b2e9f7ff88ef72e6bb0715b986616dcdb96f
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:08:09 +0000

    blame hurt discuss lonely mistake

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
