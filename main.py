edd2f20647922a48e61a9952af2c4843e06b179f
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:35:40 +0000

    sink in fast care measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
