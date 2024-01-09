69d97e24ba276c2161b8d09dcd5af1fee3bee662
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:03:09 +0000

    similar valuable alive stone personal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
