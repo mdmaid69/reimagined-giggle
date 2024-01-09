a48a10f6223c527956952ee8b5c72f8b16c8c88f
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:50:07 +0000

    belief possibility blame tough noone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def count_characters(sentence):
+        return len(sentence)
