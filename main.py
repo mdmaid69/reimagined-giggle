c318b50d39dee2c4f9f758ace2b1b8e567b9be0d
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:28:58 +0000

    long unfortunate few past hour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
