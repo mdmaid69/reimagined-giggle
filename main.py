532d9bbc6e7f6a3fe1eac4687a3de905bda5db69
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:24:25 +0000

    possible storm succeed theirs broad

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
