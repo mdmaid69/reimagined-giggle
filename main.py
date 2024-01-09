2f0aa07c65c03837dc7c463334eb2d654458ae1d
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:49:33 +0000

    play suckme tradition swing band

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_odd(n):
+        return n % 2 != 0
