c73031379a564b3f88ce76f62280f6ac2c2433e6
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:17:14 +0000

    answer flow chief lay easy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
