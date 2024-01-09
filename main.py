442dcaa66438f6f13ac28a0e80c987a157222a9f
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:06:12 +0000

    computer callup ignore sleep container

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
