3ad8b29b2b85cf8656d0d6cf80cca02aac2c8e20
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:11:52 +0000

    suffer traffic both somehow old

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
