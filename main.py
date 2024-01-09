7793ac52c40b1b8f9f6e1bb97e843a06ef39f504
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:52:08 +0000

    tradition takeoff figure cut careful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def is_odd(n):
+        return n % 2 != 0
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
