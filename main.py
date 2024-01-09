36d399745e66d3a8bad795f333cb58b687190f32
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:38:01 +0000

    bringdown top holdon pretend bringup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
