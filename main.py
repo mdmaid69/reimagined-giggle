3521bf944ba2e524703a79789c9e4ede5053dacc
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:18:58 +0000

    sell waste run cry branch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import sys
+def print_python_version():
+        return sys.version
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
