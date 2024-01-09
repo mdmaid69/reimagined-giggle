ca9271cc657a766d5ba549a31d37ef141f987ffd
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:27:11 +0000

    organize vote reasonable Ff operation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
