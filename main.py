9945044546432333207edbf11929cc5143c8394a
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:54:10 +0000

    fast oil suppose ours tour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
