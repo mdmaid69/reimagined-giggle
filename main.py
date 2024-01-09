1f89bfe20db1e2b9272ec650d80e3c8942bad412
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:39:00 +0000

    squeeze cost you gradually opportunity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
