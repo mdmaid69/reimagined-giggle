b08f3479313fb7117c6219a0909607607ca4c5cd
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:01:23 +0000

    year charge politics basic very

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
