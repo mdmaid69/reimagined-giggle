a5ff6dd6352babb37a22cc20723033aea2ed78b0
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:51:14 +0000

    deliberately argue store that crisis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
