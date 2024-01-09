efbb6e40e031e11479b3a73579a6d351df5eadc3
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:28:24 +0000

    top thanks tongue pipe any

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
