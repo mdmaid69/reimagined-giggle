ab3d6954c02bb9f09d661ba33cbc4b37d464b72d
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:03:41 +0000

    build tune telephone exercise juice

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
