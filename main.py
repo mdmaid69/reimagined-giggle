3944b3364035869cbdcbaff4487e9a6c6b74ffcb
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:42:41 +0000

    piece die smoke fit now

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
