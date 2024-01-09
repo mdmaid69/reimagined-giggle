432911b2b0b1bf42b42ab5b14220fc5c564cc0fd
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:57:57 +0000

    week enthusiastic show admit regional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
