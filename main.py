286d8e2af9244788865293dbd8b09fd59dbada3c
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:19:02 +0000

    steady board belong somehow manner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
