6967b989c8f6b6c6f9b1f7179a6723ba3a19e37e
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:39:17 +0000

    either fair opportunity fasten 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
