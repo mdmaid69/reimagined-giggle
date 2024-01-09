a42cf4dcbfcca57d76a8a597867c9bcbe69e250d
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:28:57 +0000

    use steer discovery nowhere 565end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
