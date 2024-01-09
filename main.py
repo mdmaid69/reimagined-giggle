c4b3b1538da8bd079f78798ba1c194e8e158a4aa
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:25:13 +0000

    damp human forever able advise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+x = 10
+y = 20
+print("Sum:", x + y)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
