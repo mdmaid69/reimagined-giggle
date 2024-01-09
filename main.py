4e2e55606cc36f204279e8bd99f5250e0ec0d9f6
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:50:44 +0000

    exercise active tap should heart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def square_number(x):
-        return x**2
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_pressure(force, area):
+        return force / area
