5ea616459a49a98d2b1ee56f934ca12fc98b3cd4
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:01:30 +0000

    knowledge indeed entry care slice

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+import socket
 
-{body}")
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
