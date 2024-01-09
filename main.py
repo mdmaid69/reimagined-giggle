86ded26c60d0fbc36683ae6fb40a412c4509b2f1
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:52:00 +0000

    favourite man onto just typical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
