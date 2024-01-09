1b441188625861610f5cedd7fb7ef2303ee6e3bc
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:21:50 +0000

    invite match turnout achieve nose

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
