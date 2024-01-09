2d37a022d4d4d5decd3d7cb91e266ab86539bff5
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:07:21 +0000

    little field deliberately die 571enough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import sys
+def exit_program():
+        sys.exit()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
