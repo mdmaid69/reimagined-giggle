54cb18ee65a8031a10a487c395ee84f96d4995ca
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:11:06 +0000

    hold authority also small stir

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
