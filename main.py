011ba01c2e4dcccd275fd718e96073f1dade2499
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:28:59 +0000

    spring method will will sell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
