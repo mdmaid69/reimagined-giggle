b5e018c25c6bcc213477fd9f936b54ca29c9afdd
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:25:30 +0000

    message suit 572enter catch wire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
