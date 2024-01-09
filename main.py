ebc3c8a05eafa1e1418e964ee2d307297605b425
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:26:28 +0000

    will square father forward reason

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import collections
+def create_counter():
+        return collections.Counter()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
