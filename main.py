ee37affcc07ce5de47bf96a3ebfe6b1a026700d4
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:20:09 +0000

    design young breakinto mine smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import collections
+def create_queue():
+        return collections.deque()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
