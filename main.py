6f416991363690f3a426c830fea8a67baac1ae32
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:23:41 +0000

    increase no ocean care mind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
