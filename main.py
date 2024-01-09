6324255286153b4a626f4b5de89c5ebf22ab4225
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:53:13 +0000

    nature ring low lock stand

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
