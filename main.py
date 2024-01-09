5022f97b5608b52805b86fd8afb30b480616b736
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:22:04 +0000

    trade fork neat slice gentle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
