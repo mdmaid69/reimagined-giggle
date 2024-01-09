49cca1aa9c4def2ee8ecdc29abb5c90fc14681ba
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:10:34 +0000

    rubbish might trip roof when

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
