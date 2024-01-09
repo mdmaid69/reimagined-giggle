398a0a088cfa70f531d2dd34cbd98595b754110a
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:17:53 +0000

    flavour Vv better so again

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def delete_file(file_name):
+        os.remove(file_name)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
