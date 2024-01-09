8c6eceeb0a9f3ea8aa3e88abb7f06da618123b88
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:14:18 +0000

    previous acceptable autumn breakinto transparent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
