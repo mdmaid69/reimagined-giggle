d6038e451ee6765a87b618ffbe34b8979a554a0c
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:13:05 +0000

    consider excuse pen storm clue

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import array
+def get_list_from_array(array):
+        return array.tolist()
