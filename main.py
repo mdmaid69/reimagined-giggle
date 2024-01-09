0956116b26a9598a0e3bee57cb089a6145370987
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:42:29 +0000

    constantly free light ready load

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
