d2240004589982fb336b39c8c8db45e2b3e1bdc6
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:08:39 +0000

    add sink neck clear tell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
