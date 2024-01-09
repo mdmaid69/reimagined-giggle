31c4a55e31d655fbda27730e8f6ec25819749fd3
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:33:59 +0000

    godown exam crazy blow cold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def append_to_array(array, item):
-        array.append(item)
+import math
+print(math.pi)
+import math
+def calculate_floor(x):
+        return math.floor(x)
