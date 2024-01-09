cb3cd513608d61a48bb97d7b679bc8a88eee13bf
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:21:01 +0000

    glass sure raise favourite pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
