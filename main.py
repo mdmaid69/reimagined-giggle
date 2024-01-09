4af835ae2b9b205691d38e4beb59509b583b0b5e
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:13:57 +0000

    loud Hh event petrol lesson

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
