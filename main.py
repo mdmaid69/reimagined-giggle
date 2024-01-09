5c45f3c6f53dafe008900d3e3f519c10a23ef046
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:26:43 +0000

    influence depend union simple instruction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
