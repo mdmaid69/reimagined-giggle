8a806ac06b218fe5911f41a5c9c6b76bd09850a6
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:27:06 +0000

    small beside height burst plenty

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def append_to_array(array, item):
+        array.append(item)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
