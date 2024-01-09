92f2ea83f6d1bc1b559147f243f6f174ac529147
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:07:07 +0000

    band call conversation history confuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
