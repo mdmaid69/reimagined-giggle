d365f04621dc37d0106744c9e574445e155a09a8
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:26:36 +0000

    pride expression computer scream lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_work(force, distance):
+        return force * distance
