8a558665aced041980fb91423d66c9b16020475b
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:39:16 +0000

    hope interest know manage thinkthrough

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
