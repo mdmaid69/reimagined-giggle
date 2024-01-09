2facea116df32e0415896f7edb59e1c7e1f3432d
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:34:42 +0000

    there he fault guess corner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
