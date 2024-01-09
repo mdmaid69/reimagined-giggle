27b342df0cbd555427ee0007d24fabf0bfa9e50d
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:11:12 +0000

    wing as release last possession

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def is_odd(n):
+        return n % 2 != 0
