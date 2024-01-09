ce0c976332c1a7953af29db5350685141782d686
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:03:04 +0000

    bother 573entertainment different patience delight

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-print(sys.version)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def find_min(numbers):
+        return min(numbers)
