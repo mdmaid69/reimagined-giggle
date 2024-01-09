28c23a1c19f2d4e9c07738c53657f8566bc99f32
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:54:59 +0000

    rare direct gate illness wash

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def square_number(x):
-        return x**2
-import sys
-def print_python_version():
-        return sys.version
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def add_numbers(a, b):
+        return a + b
