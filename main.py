0bd668059907733877e996e9578cafa354266afa
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:54:41 +0000

    powerful wet weekend ball adjective

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 def calculate_force(mass, acceleration):
         return mass * acceleration
