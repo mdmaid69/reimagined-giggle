8705fc7a4571d8b74626e1ca1bb1c58dfbeb04f2
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:08:48 +0000

    science name married fortunately sharp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
