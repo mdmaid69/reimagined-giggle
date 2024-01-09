301a1b316745aafd03dbfa277845409ae82e7ac8
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:50:08 +0000

    read never realize believe plenty

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+from collections import Counter
+print(Counter("hello world"))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
