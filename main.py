ad5256ace8b1b39e1c3c2318e7a1b3fe5a4f6f46
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:03:11 +0000

    estimate bringforward breakup OKorokay straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
