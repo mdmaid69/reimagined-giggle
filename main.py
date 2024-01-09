0ab0105bef3d16ab3da640530e5eeb3df3d7745f
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:37:48 +0000

    belief trade thing crazy satisfied

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
