e411ba703192bc46ddf630fbff8fbd04c0689ae8
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:11:57 +0000

    doctor refer really passenger admire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
