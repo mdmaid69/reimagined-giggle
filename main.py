f5dc0e940501ee10f6b9684c0acdf15df0073fbe
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:29:11 +0000

    already steam paper deaf present

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
