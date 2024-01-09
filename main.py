a6d4debcef0e131df7893f20e03b298afe03ba2c
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:58:03 +0000

    high moon consult paper difference

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
