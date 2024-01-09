386be14bf70efd5b8b5754d6001932d3c6b58425
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:41:31 +0000

    and sweet young no always

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def convert_to_octal(n):
-        return oct(n)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
