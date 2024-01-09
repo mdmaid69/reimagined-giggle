358be0fc7fa78ff5d8a311f43bc9e926d5c2a29f
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:20:38 +0000

    exaggerate convinced estimate turnaway pretty

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
