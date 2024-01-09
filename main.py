77cbaa2a5b02a7f81d39ede491fb91883eed5cc5
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:40:40 +0000

    there bringback stay ought pretty

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import sys
+print(sys.version)
