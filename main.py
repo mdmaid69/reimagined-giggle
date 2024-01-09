baa6e03c556a100fe38db33b38f9993841e9976f
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:57:10 +0000

    slide deliberately ashamed oil mistake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
