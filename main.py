1c8017a60d906472ffd466cf20c36c5f00ab7066
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:08:46 +0000

    sign law scale claim height

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-print(sum(range(10)))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
