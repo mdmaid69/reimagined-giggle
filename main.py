4e33f557f7acc859a48d757bd9a364d16fa6b64a
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:38:01 +0000

    content block debt rain record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import tensorflow as tf
+print(tf.__version__)
