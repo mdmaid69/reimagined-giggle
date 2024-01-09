dcc6e3578d1eacf38f140962c375d02aabf14d37
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:02:07 +0000

    accordingto means tall remember round

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
