363a2789274f93e02c3c3e53a3728b04d1daaec0
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:06:02 +0000

    turnon satisfactory early unlikely emergency

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+print(math.pi)
