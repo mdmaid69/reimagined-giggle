bd5bcb40f7a15716d6be80dc2d79f6192dc4dfed
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:12:14 +0000

    rain under completely interview nervous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def convert_to_binary(n):
+        return bin(n)
