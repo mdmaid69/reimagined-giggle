b17533f86076e9a4be7ba1cd2bd6af0ec24a6e91
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:48:01 +0000

    note boat perhaps brown pleased

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
