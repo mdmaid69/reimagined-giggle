39e01fa0f4b5cc33f2e395501467a5a5b5d284a2
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:22:01 +0000

    deliberate until flight wait excited

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_ceiling(x):
+        return math.ceil(x)
