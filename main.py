90af8824319eaaad70a59b7935d7c17281bdbd80
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:39:45 +0000

    private visit crowd opinion good

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
