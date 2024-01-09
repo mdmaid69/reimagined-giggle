5cce9d61d4e9d3dd2c82cc7c76163bf57e1f9145
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:33:01 +0000

    any confusing phone juice smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
