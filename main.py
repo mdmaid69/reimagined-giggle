e537e6fd4fbdc303ad2a58383e7410cb1fea6eec
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:25:30 +0000

    gentle lock keepout comeround once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
