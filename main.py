83b68ae328f68940752dff9041b45b023e400d32
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:38:46 +0000

    creature speech Ll price runout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
