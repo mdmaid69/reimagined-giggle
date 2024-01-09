4e21f95e94044f99156e42b325193c0c4f7a24ba
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:52:53 +0000

    up stupid question intend exam

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
