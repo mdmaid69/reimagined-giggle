cef420bad070c7440bad4b221dca7cbf9dfd8d0f
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:14:44 +0000

    practical unusual trousers burst sister

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def multiply_numbers(x, y):
-        return x * y
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import getpass
+def get_username():
+        return getpass.getuser()
