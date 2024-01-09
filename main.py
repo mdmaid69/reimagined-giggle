45779bdb1a7dcb7587ab536fc8bb1537bbcf4225
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:34:03 +0000

    sister mention press through score

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def multiply_numbers(x, y):
+        return x * y
