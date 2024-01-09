fe36be31d122dfabfeb1f6a01f39411f6bdc5b2d
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:03:29 +0000

    platform repair delicate burn manner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def split_path(path):
+        return os.path.split(path)
