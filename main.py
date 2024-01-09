6fb02cb49758ca7d83d5cd7cfa3ff3a99912dc8d
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:49:11 +0000

    upwards demonstrate poison university special

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
