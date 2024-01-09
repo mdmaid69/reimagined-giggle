b8e61d5a53b7486fd37bb764b61d04f3fdb2bc7f
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:24:33 +0000

    like fond basis sign exchange

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
