9698e60db6ad14d0fd624d7fb303fec940077113
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:15:12 +0000

    who scare frame sharp remember

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
