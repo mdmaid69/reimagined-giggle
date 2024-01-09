92c02a9ee9b3c4ca84613f86ee4eea914e10a852
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:54:28 +0000

    may open storm breakinto standard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
