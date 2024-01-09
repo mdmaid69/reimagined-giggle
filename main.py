49c0cc5de8164d6736f5669e0a6b16c3830b550a
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:42:22 +0000

    objection legal rush nice everybody

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
