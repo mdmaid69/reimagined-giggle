cddd98ac8ceaa33260db68f981c335c42238580b
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:19:03 +0000

    favour walk open conversation difficult

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
