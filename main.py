b791db5b8bf120722572fb7e1b47a722ea8254e7
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:34:57 +0000

    shadow picture live pot scrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
