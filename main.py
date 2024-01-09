67d8e84cbda87879308ee688d38382a67cef4f91
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:20:27 +0000

    shock idea neat result say

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
