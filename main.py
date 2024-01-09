73ca164492e2f727b73cf1d15b8f6175323aafd7
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:44:30 +0000

    many strike across few cover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def find_min(numbers):
+        return min(numbers)
