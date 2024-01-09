21cc9f7bc32300b1e4823943fb02590fce6a2daf
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:08:50 +0000

    only uncomfortable almost increase performance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import math
+def calculate_floor(x):
+        return math.floor(x)
