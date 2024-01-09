9dca45d2f3b2136863fd68d94ebc5721a2cc2bd7
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:46:55 +0000

    cut tax chat patient either

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def get_array_as_bool(array):
+        return bool(array)
