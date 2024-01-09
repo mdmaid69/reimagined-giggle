debe74fe14a6215c973d3126f447a196d5f0306a
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:25:32 +0000

    that standard remember its undo

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
