90d5e1236cc3e3c64643d745b8ab42ae116014b8
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:32:42 +0000

    table later needle normal press

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
