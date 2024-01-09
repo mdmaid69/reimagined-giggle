64c53a3c8bf59007937ccc672134b37305ec6d33
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:33:10 +0000

    stir own discovery flow equal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def multiply_numbers(x, y):
+        return x * y
