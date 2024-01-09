32af6b752530561d6fc2332bd3aef39ee157f4aa
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:27:44 +0000

    rain comeup table conversation yes

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
