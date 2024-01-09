65c7d3e6064a1cc9ad7d4343f029619fcecf1710
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:53:02 +0000

    house attach sister face suffer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def cube_number(x):
-        return x**3
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
