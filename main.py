cf33e7ea09b33c7fbacf12551f7b274616b97c65
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:53:21 +0000

    oil Ww age shade of

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def reverse_string(s):
-        return s[::-1]
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
