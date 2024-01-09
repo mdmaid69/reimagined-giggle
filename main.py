e85fa0e2d5c838b4b0e1880767696c78edd4f246
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:28:45 +0000

    fit fold exaggerate close genuine

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+x = 10
+y = 20
+print("Sum:", x + y)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
