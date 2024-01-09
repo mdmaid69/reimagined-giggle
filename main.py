d7c7948a5bd14806d4155832d0a5d472907b87ff
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:48:29 +0000

    level doctor drop phone letter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
