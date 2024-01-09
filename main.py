97bc07d2e4f6a6700360fc1c9eca7aca4f156cfc
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:28:52 +0000

    would fence goback(to) next reserve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import os
-print(os.getcwd())
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
