b7b4b8d4f0c8b95576c7d6e51b547522254ef4f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:49:17 +0000

    reserve runover even split form

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
