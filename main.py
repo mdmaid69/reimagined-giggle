5f77b6392b22de48cfb67765822c9fe12641f707
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:06:53 +0000

    close tooth point partner calmly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_str(array):
-        return str(array)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import collections
+def create_queue():
+        return collections.deque()
