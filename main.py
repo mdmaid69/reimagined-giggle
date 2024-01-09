80db7a76d0b7f541f5f2edcecaa64535daa51abc
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:35:03 +0000

    beside discuss pain ask shock

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def convert_to_binary(n):
+        return bin(n)
