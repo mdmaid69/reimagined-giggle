c6ee013e984e185b2f8fc08249adcb96f552c9bd
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:47:36 +0000

    sight authority beautiful along ring

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
