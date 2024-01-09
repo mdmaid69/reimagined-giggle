d8233828c5c1e6ee2fdab9d0376306f803078361
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:26:01 +0000

    ear black large operate teach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def set_array_item(array, i, item):
+        array[i] = item
