2dab63a06eca8b364f4fada54f379762cad21ca1
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:42:01 +0000

    previous song this react favourite

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
