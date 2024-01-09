2a5f47abf00c7f2c876fb0f687351f466c53bf81
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:37:12 +0000

    one character condition account doctor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+print(os.getcwd())
