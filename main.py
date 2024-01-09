f2f708c07b351a42407bddba1f01c77e50f1ecc9
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:18:59 +0000

    habit entry club relief surprised

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
