2673d0707b9b92db998fb8d6227181f5b5fd16a5
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:42:11 +0000

    repair include world laugh patience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
