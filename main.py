305e4a17e3c7b3f1bf330320cd0ee2f276cab70e
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:31:40 +0000

    block Dd corner against but

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-print(time.time())
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
