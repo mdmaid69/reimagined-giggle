872b12dfae9ea0dc0ec1d74a6570b21f285b0097
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:27:53 +0000

    home family say well machine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+for i in range(10): print(i)
