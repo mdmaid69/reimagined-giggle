1e8979b04b8a7171510a0cd53af3cd37bfc7ceaf
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:37:47 +0000

    offence discover Yy hold emphasize

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
