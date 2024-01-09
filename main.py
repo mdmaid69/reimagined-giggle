1c6f76f7b0c6c05fb987b03a198fe31dd773ce3c
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:20:26 +0000

    view comeover level smoke hungry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
