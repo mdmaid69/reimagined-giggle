deed6587962aaa5cad585b341718a15129f99ec5
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:53:47 +0000

    ourselves standard too affect house

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
