b8af0e850c9d82fbd4d25268c6c5a6ec3e02c1f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:54:54 +0000

    warn performance fight bath goup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import time
+print(time.time())
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
