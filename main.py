40b6df0f8d17de0961d1837fb979f1c067be5731
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:35:18 +0000

    radio about turndown matter feel

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import getpass
-def get_username():
-        return getpass.getuser()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
