8a0ce2aef291e92a2fd5be26b300305a13625182
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:38:48 +0000

    disturb error story artificial certainly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
