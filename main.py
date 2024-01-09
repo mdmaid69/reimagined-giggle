7361eb5988639ec6503a02c2cfbe190444187740
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:37:14 +0000

    crowded plan call neither pass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
