dfcf2932ba55b4c55025ac2b73cc066fb53fdbd3
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:23:59 +0000

    embarrassed young talk way breakup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
