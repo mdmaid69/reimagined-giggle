3fe6ad3092b6df217e36de1207e5d56c5d2f8d55
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:43:54 +0000

    left tap appeal although sister

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
