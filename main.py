0c04de34d8970f97627f6a8999bb4b24a66daef6
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:17:48 +0000

    suckme search relax hang lunch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
