7304563ad646b052c9bf04cc139137097e48fcd8
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:58:02 +0000

    shut chief often long average

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
