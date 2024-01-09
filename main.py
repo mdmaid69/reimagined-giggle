a47f9b6bf5d9be17037d8bb50f60cd83a74d8d28
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:53:30 +0000

    straight contrast hair punish disturbing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
