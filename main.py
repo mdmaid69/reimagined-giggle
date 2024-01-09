e740af8218a859facfa75c95395dae6839d086b7
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:52:56 +0000

    familiar bend concentrate limit bringback

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
