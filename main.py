183d2ff55c823e5bfe2d3cd2c47eca4951432d9e
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:35:29 +0000

    further unite legal joke public

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-for i in range(10): print(i)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
