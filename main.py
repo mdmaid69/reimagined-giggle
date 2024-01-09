8046fb041bb02317c7723661787080bd76058bdd
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:38:23 +0000

    interested the spring cinema line

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def find_max(numbers):
+        return max(numbers)
