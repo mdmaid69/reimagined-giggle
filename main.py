5e3a2699d56f68728776a13184d5b647bd641b2b
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:37:59 +0000

    trust worth hear sting seriously

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def convert_array_to_unicode(array):
+        return array.tounicode()
