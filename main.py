d14eebf974cde65a74cc81c8654d95139ca7810f
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:43:07 +0000

    react operation child design group

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_array_to_string(array):
+        return array.tostring()
