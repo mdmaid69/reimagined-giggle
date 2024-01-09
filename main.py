2c43399c07a537de90bbd1027cf373cd203757c7
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:20:58 +0000

    influence related over factory price

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
