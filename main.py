2d1a008834adec4fc5a048ec261d696e4796ea6f
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:32:39 +0000

    beside pair matter world dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  def convert_to_octal(n):
-        return oct(n)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
