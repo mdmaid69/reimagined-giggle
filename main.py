c8fef53b6623bae503fc7000677513f9c579c5b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:29:25 +0000

    soft meet difficulty model black

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
