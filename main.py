e4caa9f8acf00f434774c401e4c0866a70ce4e63
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:04:29 +0000

    upset sensitive will mood gentle

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-for i in range(5):
-        print(i)
-def is_odd(n):
-        return n % 2 != 0
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
