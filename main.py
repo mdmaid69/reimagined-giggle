2727e394f8bc839f13fce7f7f2c22dbf630ca423
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:28:33 +0000

    tour sink repair relation bye

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
