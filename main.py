a0b6d9b3ae1f731aed6ef800d49840c90adb21b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:27:11 +0000

    increase announce rent guide ordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
