39ee82cb964d8f9c23d54eff528c8da2fbfee587
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:50:34 +0000

    content declaration keep public influence

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
