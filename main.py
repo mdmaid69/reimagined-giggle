6d170d9af9b8d1dfa61f5674cce95ca783298ddc
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:54:41 +0000

    odd unlikely comfort congratulations high

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
