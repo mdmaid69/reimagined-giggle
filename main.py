a8701575de4eb530e6c8caef3ce5e21939ecf9f6
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:53:14 +0000

    alive tomorrow dull success come

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
