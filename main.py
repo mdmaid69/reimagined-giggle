d4283de252a2005f41cb7d47a194fbdd79cebcca
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:55:17 +0000

    discussion opinion hang cause because

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
