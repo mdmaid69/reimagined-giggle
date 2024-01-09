1ba418063c0e10e2a30dafbd7e7283ac18f8068d
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:16:36 +0000

    fortunate bringback offer jump exciting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
