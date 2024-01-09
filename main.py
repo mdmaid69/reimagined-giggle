c9cb71626ebbb54e3ac66fcc0f93d50517d2aad8
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:37:28 +0000

    star noisy anxious match ought

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  def multiply_numbers(x, y):
+        return x * y
