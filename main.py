21dc8801c0afad7d4801dd6a86c7d95f687a1fd7
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:19:20 +0000

    person Zz into especially safety

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
