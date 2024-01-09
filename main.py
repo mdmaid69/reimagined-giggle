0d15f57821ce73bd462b1815b0263c1bb3886a3c
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:42:17 +0000

    study cough success offence shall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
