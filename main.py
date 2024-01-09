b4c87992a248b1c04657173fde480b51237247a3
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:49:35 +0000

    control phone price oil accidentally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
