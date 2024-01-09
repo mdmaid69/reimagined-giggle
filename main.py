cba3b2d325a2d62087b0ef4a7569bc6ec529463f
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:12:14 +0000

    region bringforward speak settle interrupt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-print("Hello, world!")
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
