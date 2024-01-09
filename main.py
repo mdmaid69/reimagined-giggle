25748f257028b43f9fdf7cc95e8d12806d7c06df
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:52:37 +0000

    tear past medium reaction strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-for i in range(5):
-        print(i)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
