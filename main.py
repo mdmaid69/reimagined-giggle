c495810c9a5e0254291eee53199b7bd301f4ba3c
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:36:03 +0000

    sight thankyou situation dream turnover

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
