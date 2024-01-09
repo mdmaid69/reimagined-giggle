0e455bf5625e39a08bd33116bd83f3ade8d10daf
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:47:35 +0000

    across narrow press suffer receive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
