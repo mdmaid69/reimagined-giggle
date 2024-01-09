272923f566f20de1b55e3b78c509483fff6f5356
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:08:16 +0000

    unable you Vv through mystery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def remove_from_array(array, item):
+        array.remove(item)
