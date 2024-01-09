c36ca0d00b8c66158c9bb65f7c196db2d5b3d215
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:23:02 +0000

    goodbye possible perfect capital talkback

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
