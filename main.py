bb4a00475568ef2ecbbfd7626e607bc6dca8b981
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:29:23 +0000

    heavy further best report while

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
