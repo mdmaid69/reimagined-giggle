352e378f7978e3b009a32a8b1754ae0cd47abf04
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:33:14 +0000

    town live unfortunate only ignore

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
