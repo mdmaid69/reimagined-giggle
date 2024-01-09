06da17303f61a6aac543471a21cd9513360a8b0a
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:38:50 +0000

    regular nothing sometimes drink any

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import sys
   def get_python_version():
         return sys.version
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
