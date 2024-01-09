549ee4b3d2b933e6be8ad24d024510701545689d
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:04:46 +0000

    memory disturbing contain pleased service

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
