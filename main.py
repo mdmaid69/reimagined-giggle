86166799508e201eb2233188d90ca4d96f458a7d
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:00:54 +0000

    shout interview average voice although

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
