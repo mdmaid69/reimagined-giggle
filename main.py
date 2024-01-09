b5d5077cc220d99a9d5470dafdd238230e83b4c8
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:01:44 +0000

    ashamed dangerous plenty claim correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
