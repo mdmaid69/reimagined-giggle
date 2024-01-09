a43899ef6927eb87203727d0eac82cf843b8de74
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:25:55 +0000

    length division find less recovery

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
