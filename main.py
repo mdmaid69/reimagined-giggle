2baecdc1d6869e75007f5f3017ab1dbce54dddaa
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:07:31 +0000

    tonight combination port herself content

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import sys
-print(sys.version)
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_work(force, distance):
+        return force * distance
