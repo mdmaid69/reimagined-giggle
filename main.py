36bb10d6a25e769f305562b1445d459662edac20
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:54:38 +0000

    means chief unit opponent story

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
