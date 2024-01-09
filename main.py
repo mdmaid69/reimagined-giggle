a53219eb36ab8a1f13728b229cd555abb9d660ca
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:40:29 +0000

    turn taste education comment independent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(numbers):
-        return max(numbers)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
