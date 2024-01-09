a5ea2608552e98b0f3d2f08fe20aa48381f107f2
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:15:41 +0000

    fast fit pity time put

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
