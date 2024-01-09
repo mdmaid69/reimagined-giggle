09723ef11eb7af31094ec40c7207cc4d78221604
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:32:07 +0000

    description runover wherever pride tomorrow

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
