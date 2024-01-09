7380dddd1c0a9c4c2420f8f7c7868326c568d4d7
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:22:07 +0000

    ourselves tall exactly object ground

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
