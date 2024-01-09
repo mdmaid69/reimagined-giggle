864f7a4b7b2344967ed554b5b3a32270aede5774
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:45:34 +0000

    danger whistle Hh admit sweet

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-for i in range(10): print(i)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
