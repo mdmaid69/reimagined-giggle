16ce1a49fd349298de4f69069b354946901b0bb9
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:33:18 +0000

    find performance takeaway legal connect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def find_min(lst):
+        return min(lst)
