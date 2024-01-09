8bfed45711dea2fd7f5a39a051487c714fcff44c
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:25:38 +0000

    opposite switch combine both reliable

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  def convert_to_binary(n):
+        return bin(n)
