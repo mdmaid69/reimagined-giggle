96355151cc7e8d4196e7d9fb561093320e1c88f3
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:21:58 +0000

    expression poison dream 566end neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
