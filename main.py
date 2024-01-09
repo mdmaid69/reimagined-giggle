cf3c6bec4da0eab993967c3822494301b86e74ed
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:04:13 +0000

    company next money public home

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def is_even(n):
+        return n % 2 == 0
