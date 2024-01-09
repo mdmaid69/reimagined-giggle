01d2dbec675811704f62b4bcc460746d476bf3de
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:24:47 +0000

    promise left lock impressive pain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
