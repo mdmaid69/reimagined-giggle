b7d0d761e220c220022cc4829ff338295bb41814
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:31:42 +0000

    hang situation drawing impossible measure

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def cube_number(x):
+        return x**3
