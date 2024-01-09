3932c46cee1298985a168b320b34c786e4ad5bcb
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:17:22 +0000

    between nor intelligent wire test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import os
+print(os.getcwd())
