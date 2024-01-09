387f19a1bf4d164c5d09045162a8b51c5cc26329
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:46:21 +0000

    condition rely try loose off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_min(numbers):
+        return min(numbers)
