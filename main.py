a4d9a506b28a9e320c320bceafb9b32ebfae6266
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:57:23 +0000

    benefit advanced giveback project home

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import platform
+def get_python_version():
+        return platform.python_version()
