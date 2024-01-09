0007e73c158b603b70d4577eaa40ac1ddca6093f
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:23:54 +0000

    but secret tense course ahead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
