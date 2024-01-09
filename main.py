f751b40fa645a996a210291944f0256291143e41
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:45:13 +0000

    rain birthday snow afraid promise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def square_number(x):
+        return x**2
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
