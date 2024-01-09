d58cc85deacf811ea2625d2e1dad74e628f229b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:26:26 +0000

    attractive along snow unable close

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
