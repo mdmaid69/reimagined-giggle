3775942ec19c8c11d70592d114fec1e18c9b95ed
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:22:00 +0000

    disappear tidy great door share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
