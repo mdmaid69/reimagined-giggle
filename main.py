33c141c47c48bfc2e99d75f1cc107bbe2383640c
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:59:33 +0000

    check arrive meaning ashamed scream

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-def convert_to_hex(n):
-        return hex(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
