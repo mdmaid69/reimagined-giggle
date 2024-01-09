5353797b0aedab2f51f7498def26ffb97fe397e4
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:33:13 +0000

    charge outside near exam convince

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
