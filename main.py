1710e686b1fe973eedfead94adb0d6236d91e0fd
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:56:08 +0000

    track why Oo teach somebody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def convert_to_binary(n):
-        return bin(n)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
