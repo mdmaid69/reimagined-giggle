2361f89357043e245423b79cdf3ce973165a0621
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:05:22 +0000

    left grow thank field smell

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def convert_to_octal(n):
+        return oct(n)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
