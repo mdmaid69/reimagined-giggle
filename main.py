9475752ccd844acbb6a625f751454e65bd587e04
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:16:26 +0000

    giveaway autumn Ee definite very

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def reverse_list(lst):
+        return lst[::-1]
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
