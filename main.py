fe1d31d19e31220a6b7a0467b62bba31d2b82822
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:32:49 +0000

    test scream notice film wipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
