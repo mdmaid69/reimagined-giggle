139f67e5bb1ad19cba175e25c2660bfa67b668dc
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:39:12 +0000

    telephone star balance commit window

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
 import sys
-print(sys.version)
+def exit_program():
+        sys.exit()
