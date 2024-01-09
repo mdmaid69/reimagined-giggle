cca0a4ef204a70d84187b6df58242d02b5dc8e43
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:48:29 +0000

    issue yours argument plan makeinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_floor(x):
+        return math.floor(x)
