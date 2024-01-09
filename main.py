7f9e873849c8fa28b8e764958f865ea4e9763f5c
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:54:02 +0000

    turnout greeting town influence treat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_max(numbers):
-        return max(numbers)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_area_circle(r):
+        return 3.14 * r**2
