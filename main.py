a1d8f82ece0792763a5b8c843f806159bbfa8237
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:08:48 +0000

    smell across club mad if

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_arc_sine(x):
+        return math.asin(x)
