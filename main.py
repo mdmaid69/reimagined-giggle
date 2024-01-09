1f453e41969dfa5dda6c7ee34add8ce75980d458
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:17:55 +0000

    crime sink let left different

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
