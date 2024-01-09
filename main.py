858be4cde42de6c4f8282dbc9316e2da0c0b4445
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:35:11 +0000

    spring force fasten test matter

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-print(sum(range(10)))
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
