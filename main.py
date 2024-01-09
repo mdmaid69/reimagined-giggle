c34dac82180da06c8d5fefca27027922d3a5d65e
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:33:37 +0000

    service run high sing eager

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
