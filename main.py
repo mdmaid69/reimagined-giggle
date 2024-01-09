927d0acf90efd79e36801d00016ff9467451ffc9
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:07:22 +0000

    recover much idea trick cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
