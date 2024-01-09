40af3cfc7352254356930adb47e292bcb56a2089
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:44:37 +0000

    beginning join smooth negative pinch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def is_even(n):
-        return n % 2 == 0
+print([x**2 for x in range(10)])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
