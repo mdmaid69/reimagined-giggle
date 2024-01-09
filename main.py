43df02cbded3e9090c129df63a82a32829eaa7fd
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:17:35 +0000

    sale deal strict artificial cruelty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
