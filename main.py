99d8f02316282b4623d8e533865cd9b913d1b867
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:33:26 +0000

    easy confuse interrupt unpleasant disturb

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
