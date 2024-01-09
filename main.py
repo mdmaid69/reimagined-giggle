32a380460a223f8f0d01e13840ee5965c2ab1706
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:23:13 +0000

    neck friendly bringforward fine appeal

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
