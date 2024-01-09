0a0ff8ba0844da8e47e8c6ff89725d13b540bce4
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:52:13 +0000

    goon much dance matter trip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
