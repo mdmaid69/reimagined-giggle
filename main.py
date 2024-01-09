3afa03356998c4a506f8c025d1eb831fd83249db
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:43:20 +0000

    constantly serious more left address

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import sys
+def print_python_version():
+        print(sys.version)
