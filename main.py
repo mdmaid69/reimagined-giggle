7c9644b10321e03e7bf32226052e362d2dc9265c
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:34:20 +0000

    bit female yes experiment callat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import datetime
+def get_today_date():
+        return datetime.date.today()
