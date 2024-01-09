0b8b4752cce9b72cb601dbd7a42d90107288b256
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:45:37 +0000

    pity brother tear attract level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
