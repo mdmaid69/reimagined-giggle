558d2608e68b3871f75f3d4d9252da3456a06552
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:18:04 +0000

    friendly fine run miss drink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def get_array_as_str(array):
+        return str(array)
+import math
+print(math.pi)
