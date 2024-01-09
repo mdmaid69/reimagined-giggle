d2e5817fbf61f8663c3cc744d3977cba316b3392
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:16:41 +0000

    confidence confidence prize onto this

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import os
+def change_working_directory(path):
+        os.chdir(path)
