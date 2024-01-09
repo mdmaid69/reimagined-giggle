ef74cee5117fbb5139c6b321c0d0331b47d3d4a7
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:23:40 +0000

    swing partner rarely port major

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_palindrome(s):
+        return s == s[::-1]
