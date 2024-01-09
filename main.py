ea61744cec31f5ef4f479d7ae96ff1928d137ad7
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:01:40 +0000

    pattern convince dance satisfactory average

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
