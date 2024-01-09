d25cd3d932de3f9389dfab9d06e63963d8bd432c
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:05:50 +0000

    border dead young brush mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def reverse_array(array):
+        array.reverse()
