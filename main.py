d5712a4eead1e0fc04ec8ea4f47c8edf36307e2a
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:51:02 +0000

    more sense step process goout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
