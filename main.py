7fdd2518a0ffeedfe1bcac5699f3221508b2c2a1
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:13:27 +0000

    supply lift course work authority

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-print(sum(range(10)))
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  def convert_to_binary(n):
+        return bin(n)
