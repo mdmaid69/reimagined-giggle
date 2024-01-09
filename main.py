b26b0ad80314604cb3e5a60e7b00bf9f53384f59
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:23:39 +0000

    separate minimum letter hurry remark

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
