86d3848fd29a8f37bb5111e85cca6de995cec991
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:23:12 +0000

    study cost certain step over

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
