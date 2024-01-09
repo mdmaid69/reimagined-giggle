24609274381e427fc1603880957fb0a9ba4f7f7b
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:46:36 +0000

    carry put Ww education live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
