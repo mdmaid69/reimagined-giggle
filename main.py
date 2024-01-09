7c7f3d4543e6b0e4f8c9bb8d594dc40af313117f
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:26:53 +0000

    secret geton group several company

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
