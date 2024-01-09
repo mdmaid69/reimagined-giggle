07b4d0b52527702e3142afff168bbade25f6fae0
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:35:05 +0000

    hardly rely taste stay notice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
