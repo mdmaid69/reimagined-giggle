560c6cd96749297bb00d5336f5a5e248ec251435
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:58:06 +0000

    pressure rent error yet book

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_power(work, time):
-        return work / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
