c611cdd7c5430ed8f45dc0233f6545ec73707744
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:27:09 +0000

    fond powerful angry contribute pass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def remove_from_array(array, item):
+        array.remove(item)
