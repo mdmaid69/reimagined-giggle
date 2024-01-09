d8558a0b1fca2ed1865af0c904d901e265d8f552
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:25:40 +0000

    trade formal bin quiet summer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
