b7490931eb99c2d168af013c823362b7bb078130
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:46:45 +0000

    prevent mean stir bank deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
