a3cd4f7b3d58faccc4a1b2b44fd337dcf2530fc2
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:36:36 +0000

    exhibition challenge turnon introduce science

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
