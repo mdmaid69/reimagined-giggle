1b107e9d4d4ff4e17f530416bd7260ab18f08be2
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:36:53 +0000

    embarrassing sink dick Uu genuine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
