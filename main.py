b9c5c0ba9ddebea836bfeb451cb2b6b4fa29defb
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:17:34 +0000

    comedown by behaviour form connect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_max(numbers):
+        return max(numbers)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
