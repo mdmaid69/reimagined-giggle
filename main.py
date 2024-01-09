8e860751f7f4b654b027da2cf88ef27d8c2e99b2
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:01:21 +0000

    address software judge secret prove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def cube_number(x):
+        return x**3
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
