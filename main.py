60bd08906e26858c5524a5c801b4bcc9912b702c
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:50:16 +0000

    present long class no tooth

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
