35f288f5fc3f614005419bb19231453828d45125
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:28:38 +0000

    Ss record judge fit left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
