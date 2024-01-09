f87a4fcd49179c34d3eb560c6cd42bc3f4175661
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:44:57 +0000

    fasten upwards score warn announce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
