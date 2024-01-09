b54801a4900affc9bc27a2557635dcd61551e56b
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:03:36 +0000

    pleasure direct lot temperature tired

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def remove_duplicates(lst):
+        return list(set(lst))
