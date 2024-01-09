d92e70f020b2420b41b6dffb164031dcc092dc29
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:45:14 +0000

    represent take practice face now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
