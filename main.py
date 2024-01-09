6f903cfc387ee798fa9e010c74e4e602294e684e
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:16:11 +0000

    survive ask shame window interesting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_min(lst):
+        return min(lst)
