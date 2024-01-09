ee57d95a98277a2cee404c01c21daf2ba489c01d
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:05:13 +0000

    one reliable thankyou ring concentration

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def add_numbers(a, b):
+        return a + b
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
