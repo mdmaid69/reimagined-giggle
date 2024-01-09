1d843c16c56e36826361ced0e732f24faf2a2846
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:52:11 +0000

    matter bringdown letter less word

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
