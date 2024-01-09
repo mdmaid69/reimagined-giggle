81303e8870655b1d79f64f701b03423ad41ce99d
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:50:40 +0000

    attention by responsible stamp squeeze

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
