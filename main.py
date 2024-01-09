5828d043bc2da0291bb39246941d9cae0015720b
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:28:49 +0000

    say pretend worried 565end easy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_permutations(n, k):
+        return math.perm(n, k)
