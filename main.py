72a68de492aebb5b3c27de7f8c794e65a0c647d8
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:39:07 +0000

    convince 565end kick arm scrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
