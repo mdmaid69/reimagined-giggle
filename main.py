887087355ec6351a0405bf70ff91927f582e7b1a
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:40:18 +0000

    display keepup pay debt total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
