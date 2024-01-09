a21363a7cc8f8470bb74d119aa986a4c03ed251d
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:28:31 +0000

    flavour thinkthrough love give undo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
