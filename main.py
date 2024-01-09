9ce6f96da553c10788094646b93940129f4a770b
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:05:48 +0000

    related pencil attempt breakfast part

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_power(work, time):
-        return work / time
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
