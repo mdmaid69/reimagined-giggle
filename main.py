8f4540d034bea32bd1445606f53c720dc6071aa0
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:28:05 +0000

    switch down grateful exactly true

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_area_rectangle(l, w):
-        return l * w
+  def reverse_list(lst):
+        return lst[::-1]
+import time
+def get_current_time():
+        return time.time()
