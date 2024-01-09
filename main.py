745a507234b6edfd0fe41570de313927af707e53
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:14:46 +0000

    record street 179bite question trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
