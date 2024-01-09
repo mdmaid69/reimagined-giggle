36a53af453d6ea13a6b70ed6ffdf4c84e7d4d4c8
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:29:01 +0000

    should plate storm crisis force

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+for i in range(10): print(i)
