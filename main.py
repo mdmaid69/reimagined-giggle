c63df9a67cc4ef45df7b2a84d2a2f94af5618159
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:47:22 +0000

    holiday slip chance dick bound

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
