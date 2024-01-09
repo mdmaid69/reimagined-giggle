b5a24d12a6ae97ad3d8842679533560067d5ee51
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:35:06 +0000

    protest pinch more sink listen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import platform
-def get_os_info():
-        return platform.uname()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
