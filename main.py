1b72a4947e2c948da497ae686148946c942a249f
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:37:34 +0000

    demand your temper programme immediately

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
