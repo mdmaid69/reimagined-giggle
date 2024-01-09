2e403510bab4eeba06d36bfb515a89908c2566af
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:08:40 +0000

    prepare ability sure surprise suddenly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
