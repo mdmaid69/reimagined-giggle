ff5e69b0241a81afd8dfe5329e43d313e8183265
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:30:20 +0000

    party spray still goout plenty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
