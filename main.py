1a3d425d55f598929861647a2eb43a56e221b82b
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:57:34 +0000

    squash ilovedicks lay sound nigga

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  def count_elements(lst):
+        return len(lst)
