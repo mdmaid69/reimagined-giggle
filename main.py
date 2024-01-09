e598e229ba57d32dcc79e8c55e2707d3ab85bdb8
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:04:37 +0000

    provide path pleasure goup want

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def get_array_as_repr(array):
+        return repr(array)
+  def convert_to_binary(n):
+        return bin(n)
