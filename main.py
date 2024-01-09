314a1b5d38aacb4b5b481274a7283bd56313f6fc
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:04:38 +0000

    over beyond Qq whole worse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
