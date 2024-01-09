fdf653cda61cb30885b88297e92d768db020816c
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:47:35 +0000

    lookaround complete similar reaction dull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def convert_to_binary(n):
+        return bin(n)
