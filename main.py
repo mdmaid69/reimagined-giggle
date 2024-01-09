ccff0472b53d24e775dc2eaa5bf19080fcce5bed
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:10:21 +0000

    substance try spoil blow chance

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
