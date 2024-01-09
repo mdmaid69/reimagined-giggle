aba9763d2412c7205a1c84c5204318d467c34ef5
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:02:42 +0000

    live its check slide breakout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
