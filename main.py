be33b740f94bd8b40c68f7597136da8dcd90a431
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:42:46 +0000

    refer luck ticket violence ahead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def convert_to_octal(n):
-        return oct(n)
+import array
+def reverse_array(array):
+        array.reverse()
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
