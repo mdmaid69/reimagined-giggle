abcc5f86e3c1862cbd645bf3e8ab54dd5a748e51
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:07:48 +0000

    ilovedicks impossible big date breakin

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sys
+def print_python_version():
+        print(sys.version)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
