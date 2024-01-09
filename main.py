2ceb6db505555ca7510a117fd45ee78863bff14f
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:24:59 +0000

    holdout peaceful pause wheel hard

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
