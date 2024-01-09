da7fa4ff1434a141b629537f06b101e08d5cd9fe
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:53:42 +0000

    really view although meeting through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
