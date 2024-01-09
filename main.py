2c46cd52317b1030a197c81043cc24ab11233a60
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:47:54 +0000

    fly drive immediate moment small

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
