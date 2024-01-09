eeff9c88f13a8e68cdf7e3e19a09f28854b344e8
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:09:17 +0000

    refuse get level unkind protest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
