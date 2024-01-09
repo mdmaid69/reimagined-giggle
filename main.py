5a32a7277afaec765c3e5e85fed64d7c36587a58
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:55:05 +0000

    there change state goout prepare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_as_repr(array):
+        return repr(array)
