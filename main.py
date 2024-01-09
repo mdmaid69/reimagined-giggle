1aacba97db969c24ce77df8c79af01cae7a860e4
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:33:23 +0000

    confused explain rush escape visit

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
