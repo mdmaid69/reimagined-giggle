36451e9c947964a2c974a53740df9bb3c03e70b6
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:23:07 +0000

    characteristic stop soak clean chin

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import tensorflow as tf
+print(tf.__version__)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
