0911e46c40f500c6ddd29ca15046d52487dba149
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:17:56 +0000

    Oo scale holdout cry notice

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def is_even(n):
+        return n % 2 == 0
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
