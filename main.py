d6307023995828c0ff970ad3f2f986688f0da89d
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:04:21 +0000

    sing murder measure boat awake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
