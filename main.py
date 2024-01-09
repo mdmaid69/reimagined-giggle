29c830f17deeadc97ce49b78ec0872f9cdab00ff
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:02:54 +0000

    Qq choice work split whom

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
