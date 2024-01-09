58009aad5ae7b70574b2caff901cc171780a88b8
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:55:43 +0000

    inform pity certainly spring major

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import json
+print(json.dumps({"name": "John", "age": 30}))
