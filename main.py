f0cf8eaa1e4f07b924ef32d7a2ced27b1c9a496f
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:39:13 +0000

    first doubt treatment contribute round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+print(re.match("h.*o", "hello world"))
