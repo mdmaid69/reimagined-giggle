f6c1df92c0117d8e299b8a68f9ba638356cfac78
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:00:11 +0000

    term happen consider reject suit

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_speed(distance, time):
+        return distance / time
+def sort_list(lst):
+        return sorted(lst)
