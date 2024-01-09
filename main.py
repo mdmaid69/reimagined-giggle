a21f7746d0059b75a5e539b19c4ffcc675a5e3b3
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:49:21 +0000

    neck to satisfied right feel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
