90171e9661543e6561b52d6719b1a5015539d906
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:56:04 +0000

    pause break treat your climb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
