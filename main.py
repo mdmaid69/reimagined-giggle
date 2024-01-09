7c5e726049eda2829ea47af5c0739d5f06573739
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:53:50 +0000

    relax for ball performance what

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def square_number(x):
-        return x**2
+def get_array_as_frozenset(array):
+        return frozenset(array)
