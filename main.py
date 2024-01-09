a89127671e741301e8e986e5683699fb7fcb5a4a
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:09:02 +0000

    guess block whole mother score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
