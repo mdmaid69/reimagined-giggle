af681832aed97991b4de5b7c48a70602286e25f9
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:14:18 +0000

    disagree dinner stamp clever disturb

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+for i in range(5):
+        print(i)
