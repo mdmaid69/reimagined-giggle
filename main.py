8e45767dd9a94793fd36b192c31a67ca44c5e4c1
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:43:07 +0000

    loss bringdown attach pretty whether

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
