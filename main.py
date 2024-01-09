a37796df9d46de88471863e67829c6e2c7e3603a
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:51:54 +0000

    responsible note calmly miss pot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
