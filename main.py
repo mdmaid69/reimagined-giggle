0a77a58d8649c8b7af08d0be598eb3f1621b6abe
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:42:52 +0000

    only burn public unable 565end

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import time
-print(time.time())
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
