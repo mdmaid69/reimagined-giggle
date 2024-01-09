a8876b9b72988befecbe2d81ef85a1846e2c9afd
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:02:22 +0000

    clever inform weak short thank

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
