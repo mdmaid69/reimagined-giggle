494e1746fedd8b225203d68ad00e76a4b0bbfdaa
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:24:27 +0000

    emphasize comfort without pot programme

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import os
-print(os.getcwd())
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
