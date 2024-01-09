f40f99eb4a843416af2bbe934d95ee8a528245e4
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:02:19 +0000

    ring die anything dull vision

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
