ab744c2e2475912cd447d0c380942a661a0a80b1
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:55:35 +0000

    giveout cheque lock general chin

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def divide_numbers(x, y):
-        return x / y
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
