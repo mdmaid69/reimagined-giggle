6cd20bba053e99984b31cb6073511385e1f3f0b7
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:35:38 +0000

    mainly strike punishment too half

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def clear_array(array):
+        array *= 0
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
