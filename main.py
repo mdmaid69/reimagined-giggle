4dcd13200c6bbc8666ed00cff1938bcfab1b5f1e
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:24:26 +0000

    change better claim confused regional

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
