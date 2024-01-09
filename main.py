cc5708dcc6a376527c591392029d1b67c8a8ed80
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:12:26 +0000

    confused development each people stone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+for i in range(10): print(i)
