00f3dd99c2a9a3f1efdc3f694a29b72859463bd8
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:29:03 +0000

    service language regular whatever find

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
