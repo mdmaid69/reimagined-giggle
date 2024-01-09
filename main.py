964ba709ea694c1c210d0fd0bce1b04079a1540d
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:21:35 +0000

    design have spray lookup niggaacademy

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import re
-print(re.match("h.*o", "hello world"))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
