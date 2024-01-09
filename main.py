4a9caac290f4ecd523cb60a4a2f65249506decad
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:14:07 +0000

    keepout plate tour section trousers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
