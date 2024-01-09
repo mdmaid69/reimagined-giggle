1e1228ead6cd1a5bbb6cabebb0f5ff7cfdc8ba06
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:17:30 +0000

    cool wind flood drive development

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+text = "Hello, world!"
+print("Words:", len(text.split()))
