f12c6b68ecc891c8786b2662ff74a9266341e73a
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:24:28 +0000

    regular excellent creature properly recent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_work(force, distance):
+        return force * distance
