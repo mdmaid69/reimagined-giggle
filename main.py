a7b4c7da660f0517278af42b8349adf7e2735b94
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:53:37 +0000

    block guide properly autumn shut

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def divide_numbers(x, y):
-        return x / y
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def find_max(lst):
+        return max(lst)
