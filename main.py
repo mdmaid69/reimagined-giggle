7fdf8b123ff1918d88bcd71fd4e1067fed639ce2
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:29:37 +0000

    used satisfactory right tap shallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def get_file_size(filename):
+        return os.path.getsize(filename)
