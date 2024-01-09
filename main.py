b812c250406cac5de43ecc6246cc7dc78b1997e2
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:10:32 +0000

    part justice club fault finally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-name = "Python"
-print("Hello,", name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import array
+def get_array_as_repr(array):
+        return repr(array)
