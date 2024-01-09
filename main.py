181d3c8efd600aea59455736ccebbba75ab7ff04
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:48:49 +0000

    punishment post soft direct shade

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
   def get_file_mtime_ns(file_name):
         return os.stat(file_name).st_mtime_ns
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
