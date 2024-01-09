1171dee2ad9b51112782d376c2e5f8a501709147
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:36:40 +0000

    muscle neither flat sleep operation

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import os
+print(os.getcwd())
