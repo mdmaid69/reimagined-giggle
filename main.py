93f6078b39e6d6720ed580bd67cea5514c9f46de
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:11:37 +0000

    freedom pronounce sky beat straight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_str(array):
+        return str(array)
+  def square_number(x):
+        return x**2
