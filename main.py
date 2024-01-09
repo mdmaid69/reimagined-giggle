f91e5acaf3d929c79366275ffbae38bdff2e45fe
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:56:55 +0000

    ring effect lock nothing indeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
