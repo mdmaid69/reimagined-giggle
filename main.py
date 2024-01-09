d0eede1cace32313121dfada82165d1932424ca8
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:55:21 +0000

    adjective recommend inside maximum active

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
