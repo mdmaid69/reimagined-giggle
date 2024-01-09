3e7678527e869861bbf7be5d73f2a6a43fd3c8f5
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:23:24 +0000

    mail convince spare shoulder callup

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-text = "Hello, world!"
-print("Characters:", len(text))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import sys
+def exit_program():
+        sys.exit()
