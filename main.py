8f613e1000bd4fe4f14c213f3029cc59563645ab
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:37:55 +0000

    rely one help war relief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
