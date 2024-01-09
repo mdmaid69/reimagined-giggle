74b7a2975c4b1472a5b542dba1334604f9eb5621
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:06:48 +0000

    supply apparently public stretch imagine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def split_path(path):
+        return os.path.split(path)
+def calculate_acceleration(speed, time):
+        return speed / time
