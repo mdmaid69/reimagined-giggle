90f91b5a39e632e1729e6abcc2e17df26656a8cf
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:24:08 +0000

    length equipment draw answer group

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
