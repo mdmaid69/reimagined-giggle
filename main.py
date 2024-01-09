e6b5ecde5c138a6065086e5ee1bb32f088d3b94f
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:21:40 +0000

    weather lookup gather bunch pressure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
