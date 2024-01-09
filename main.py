7e976be8adccd3546e79422dd7e6309d334fb5f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:20:47 +0000

    sort sad bang kill gather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
