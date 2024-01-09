a386df54612934e04a0c3632d438c53d17812cb3
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:32:57 +0000

    high order present correct nowhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
