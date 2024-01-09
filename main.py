5a74b83690e87b61d89bde844ac9860ea21ccf55
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:32:16 +0000

    word measure watch especially such

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def get_array_typecode(array):
+        return array.typecode
