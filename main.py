50e590d06664ab947ae2892ea9242dc9d9863bd1
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:48:50 +0000

    only where purpose desperate kill

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+print("Hello, world!")
