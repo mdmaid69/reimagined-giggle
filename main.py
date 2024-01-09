ba07e5c707fc7bb0aab116ce3e0ab7f13419eef4
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:08:36 +0000

    bite street part offer everyone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
