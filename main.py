5a20c8b12d5fdda0c3b47cea3e886692b3543b2a
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:14:39 +0000

    recent stay copy become brush

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
