0bdef55f0b0fb31f6504ffb568b0474ee0dcc95f
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:38:34 +0000

    fly curious know stretch record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
