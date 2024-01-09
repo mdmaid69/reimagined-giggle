c08a93ab6a942aebe4c67e7baa2a1ffde493b556
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:47:33 +0000

    action bank rest duty grow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
