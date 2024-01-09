e0591975d1184bf2fefba28b6f8d69b0247f9cbc
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:40:38 +0000

    loose explain build minute earth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  def convert_to_octal(n):
+        return oct(n)
