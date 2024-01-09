144e24a02e774aee832c60f32cda49c6816ab4b8
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:23:20 +0000

    rub pump increase sing fasten

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
