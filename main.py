703fa30d6aaaf57fa6578e0a91d9ca0ecf748df6
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:02:01 +0000

    disturb risk holdup floor that

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def remove_duplicates(lst):
+        return list(set(lst))
