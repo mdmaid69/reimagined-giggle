1fdf500e6ef977db1bf8280857b080b01e1ec2ea
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:44:36 +0000

    by soon everywhere behave ourselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def subtract_numbers(x, y):
+        return x - y
