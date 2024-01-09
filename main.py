2e6e6c2aba80d26d6af4f61ed33eb7c2091f2a3f
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:51:21 +0000

    drive favour somebody contrast pile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import json
+def convert_to_json(data):
+        return json.dumps(data)
