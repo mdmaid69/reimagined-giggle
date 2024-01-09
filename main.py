58b20bb91b7dac57c6fd4e29c40a69bcd4d56564
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:41:56 +0000

    necessary control yours reserve mind

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
