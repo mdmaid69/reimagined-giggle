cdf03805f5714b0c327e516d99e77c95b6288863
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:33:14 +0000

    tomorrow smooth journey talkback throw

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
-import platform
-def get_python_version():
-        return platform.python_version()
+import re
+print(re.match("h.*o", "hello world"))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
