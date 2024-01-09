eff5caa1fe047e1525a923365bf92802f05ce7ea
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:14:18 +0000

    nor either exist anywayanyhow head

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  def convert_to_binary(n):
+        return bin(n)
