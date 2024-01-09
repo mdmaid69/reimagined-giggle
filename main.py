8fe307b5a614cb3ad33aa572801501ef20218257
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:37:44 +0000

    over search post loan permission

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
