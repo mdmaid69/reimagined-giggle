e42c068c0d4ed35a347518d2dbd80dc82122cff3
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:06:17 +0000

    sink law in conversation everybody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
