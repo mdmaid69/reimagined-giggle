b63bae9fc1ba08d1b3e5cc52cde7d60576be04ed
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:15:56 +0000

    speech practical bridge wherever proper

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
