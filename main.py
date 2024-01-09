c440ffd3bf003b071b5751f590bfc3aaccd7d8ca
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:47:12 +0000

    apologizeapologise traffic cope important cheat

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
