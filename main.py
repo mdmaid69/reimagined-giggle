89b7dde385b8374b7d42273e8c4efc0882306185
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:57:00 +0000

    nearly reduction deaf healthy wheel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
