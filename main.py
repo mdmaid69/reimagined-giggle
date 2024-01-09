ddd39cbbae646c0083f7760942a0c66953e8ae68
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:25:25 +0000

    sleep fact worth bite curious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
