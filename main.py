419255d5c253d736a54483ed29ecd83a3de316c8
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:23:31 +0000

    programme careful beat colour subject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
