b591917fe19d8c6f0a466c5482fc5249c9452089
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:10:56 +0000

    near press make beautiful rest

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
