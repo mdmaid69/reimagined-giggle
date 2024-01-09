2ea8bf0e430cbb7b98c1794f2afc7d7ccdd9e3d2
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:24:05 +0000

    permission branch basis turnover generous

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
