5665a58f1af1a7543a242c18b99ffe4e8c091a9f
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:02:19 +0000

    matter involve reduce basically use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
