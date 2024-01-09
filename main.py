30e9658d049829c1946ce582f3f8a229762593ca
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:46:14 +0000

    supply zero buy quiet region

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
