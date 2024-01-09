2f0c12481e9414f289a33a109e5dbd4683d7ba5a
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:07:29 +0000

    cover tomorrow under impossible middle

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
