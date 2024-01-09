ad73d92bd9523c771aed040990d04be5aaf91a2b
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:42:19 +0000

    grand belong the long direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
