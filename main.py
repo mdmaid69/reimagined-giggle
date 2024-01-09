884f214fec8f530b8bed68fd24ea995eeea60a98
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:23:07 +0000

    frame serious arrive ball note

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def append_to_array(array, item):
+        array.append(item)
