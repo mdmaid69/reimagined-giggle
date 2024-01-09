2440f0eaafbb0f619bbcc01e0133c4ad95cb207e
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:22:40 +0000

    alive whenever size march wrong

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print([x**2 for x in range(10)])
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
