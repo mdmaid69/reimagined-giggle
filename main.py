e1d61eccb1e60c8f416be4064abe23a1c7265234
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:19:32 +0000

    whisper fence cry confused draw

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def count_characters(sentence):
-        return len(sentence)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
