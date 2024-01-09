df6c0a7636acf4ec84df55960e97bca42b07848d
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:19:13 +0000

    road statement cost sit polite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
