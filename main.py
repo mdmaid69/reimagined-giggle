79f14329981878a8427777aa7ee5be6188758d7c
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:14:08 +0000

    system habit payment firm same

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_set(array):
+        return set(array)
