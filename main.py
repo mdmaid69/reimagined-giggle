03a07d6fd7a90704dd96baa74566b80506a83b5d
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:08:21 +0000

    though completely film better eat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import sys
+def exit_program():
+        sys.exit()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
