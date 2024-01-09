b2ca3cedf5e19625aecdb1f707b6a7dab529c5a5
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:19:53 +0000

    stop bag stir ordinary inform

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
