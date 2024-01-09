a08557fc3faee399651d9ca8e1d07dc06f661e83
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:38:22 +0000

    deserve blame apparently comfort print

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
