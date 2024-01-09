cdc9909b3957af365a19ad56ae3eb62095efd5f2
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:55:32 +0000

    deaf afternoon Vv tradition stranger

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_perimeter_triangle(a, b, c):
+print(sum(range(10)))
+  def calculate_perimeter_triangle(a, b, c):
         return a + b + c
