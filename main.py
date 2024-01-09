5f22cf987599a627a641a31a063f32f52ccb7d23
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:09:08 +0000

    I run most doubt common

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-def convert_to_hex(n):
-        return hex(n)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
