303bef332f7884bc5c132cdd2457a4e01738d9e8
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:34:18 +0000

    convince create below fly convenient

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def sort_numbers(numbers):
-        return sorted(numbers)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
