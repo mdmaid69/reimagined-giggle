c21b3341c7e64dd7875498f149bebebd2fba0658
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:32:18 +0000

    practice passenger pronounce embarrassed cheque

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
