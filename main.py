24c06159c419356ac51c641c403a507c6196b7e4
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:36:47 +0000

    think common class peaceful professional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
