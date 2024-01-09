d8ead7607ad743bd4d597272dba655d0f65ff5bc
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:39:05 +0000

    want strike side prize bush

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import sys
+def exit_program():
+        sys.exit()
