edc0c9216f704ddf4987c8940c7c103042ff2997
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:13:21 +0000

    sort alone stick comfortable attitude

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import time
-print(time.time())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_area_rectangle(l, w):
+        return l * w
