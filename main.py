682fce08bc7cfab4b1168282926a14635de814ac
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:52:49 +0000

    comeout tradition instruction pay crowd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
