fac9f4d2a6f091d1ff442a07cd8e180d8a441f1b
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:54:46 +0000

    fix guilty performance whenever comedown

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
