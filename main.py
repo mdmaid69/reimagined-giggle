ee5370af39c497c108ed8f0750b307f4d93995af
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:40:49 +0000

    future word brown violent free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
