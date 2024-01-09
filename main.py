37c4de5b294defc1c8bfc07413e5dedaf6caf71d
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:59:23 +0000

    agreement broad more custom embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
