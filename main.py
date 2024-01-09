c16b155b024e20888c0582f306549334719a11d5
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:32:42 +0000

    visit figure somebody wake point

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
