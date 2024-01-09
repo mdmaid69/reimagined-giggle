09c7a226ed602e0c4bb52e482dbafa4bf229ebc3
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:58:06 +0000

    programme load goout exchange getoff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import sys
+print(sys.version)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
