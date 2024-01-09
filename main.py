637762336dcdc1383a1005364bb6233996f0d34f
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:26:50 +0000

    sky beyond lucky get deliberate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
