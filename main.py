f2471e0a6bcc61146ca70a74431789c18087726c
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:34:09 +0000

    neat teach awake bear call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
