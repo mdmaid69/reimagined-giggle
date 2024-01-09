cb8d54a0eb3b8324c9daabe64be5c98e6793e6b3
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:35:41 +0000

    look exaggerate progress substance yourself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_force(mass, acceleration):
+        return mass * acceleration
