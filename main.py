ba5d56a74d3f7e8b74495816310f2d9e707ca570
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:07:11 +0000

    sell gradually unite none way

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import logging
-def log_message(message):
-        logging.info(message)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
