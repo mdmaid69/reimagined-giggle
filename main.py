8103ac5491ae3230e8fd35a5ed660b07bfddab27
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:31:24 +0000

    respect comeround usual recent reliable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_sine(x):
+        return math.sin(x)
