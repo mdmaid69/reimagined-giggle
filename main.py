e0fc6df0e7def76f96eec9ce1a4be85b7dd8a4de
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:42:59 +0000

    declaration rescue count by own

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
