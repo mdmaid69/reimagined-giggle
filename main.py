71d8a99c4b23be57fadabcdd38576119df0ed6f0
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:29:09 +0000

    method stupid acceptable weak dark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
