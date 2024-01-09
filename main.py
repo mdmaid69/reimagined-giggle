b72f264bd39edce845b568b2b4eaf651beb10438
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:57:08 +0000

    peace him choose along joke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
