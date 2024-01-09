2c6c8ba22a07a972ccf672ca073c5c7e73e14bf0
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:34:25 +0000

    machine top without turndown my

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_repr(array):
+        return repr(array)
