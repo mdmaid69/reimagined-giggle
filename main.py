7783a6c5d54e8fb8165a593e4ac92c99822635aa
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:10:18 +0000

    age announce brush opposite otherwise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
