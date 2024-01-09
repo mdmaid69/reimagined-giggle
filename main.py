fa7d91dfb74bb03b9d40b32c57f66dd192d5c959
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:43:24 +0000

    future opposite cry only skill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
