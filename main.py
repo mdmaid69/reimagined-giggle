79de71544adc0f1800cf417c8649b2e66c8025b0
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:06:38 +0000

    wish now backwards least once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
