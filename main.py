15ec79c5ae8b87016cb8f55904ad112a41827852
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:55:46 +0000

    now smooth protect prefer none

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
