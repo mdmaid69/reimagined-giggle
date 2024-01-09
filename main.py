6a56588e1bff1c38ce4e1dedd602d64b539fc295
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:11:15 +0000

    laugh deliberately weak plenty ashamed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
