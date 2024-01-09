7648c15141567f9388c2f3169989772d691a0659
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:39:52 +0000

    remain today other instead correction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def convert_to_octal(n):
-        return oct(n)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
