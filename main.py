67dcbaf526701b26d55857b0ce1fe8c7667d91c3
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:41:39 +0000

    lay right proud failure dark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
