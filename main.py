69d7b543d4e28ae475597627a85268bfb1d3603e
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:55:40 +0000

    Pp mail air beside once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import datetime
+print(datetime.datetime.now())
