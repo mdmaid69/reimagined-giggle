d99c95c04e6094b86bb39a9a1c7648c9ea1a0541
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:03:44 +0000

    grade sweep ticket Tt reply

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_power(work, time):
+        return work / time
