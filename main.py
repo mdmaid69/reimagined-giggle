cd6eb32a4a1b01095342f3bf4bee397c1c8854cd
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:40:40 +0000

    ignore trouble equipment respect share

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
