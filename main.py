30671e0e10633718a753fa789a1eb3fc96415770
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:28:32 +0000

    range male soak common right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_typecode(array):
+        return array.typecode
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
