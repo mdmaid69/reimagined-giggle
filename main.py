1917d8f0eab84c1678f9cc599098d45c1ff5e3e2
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:01:59 +0000

    wide popular next afford calmly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def subtract_numbers(x, y):
+        return x - y
