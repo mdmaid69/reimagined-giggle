2b6a6e04f0da2a54079ca564b504252a4aecff0c
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:25:13 +0000

    among bye but possibly roof

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
