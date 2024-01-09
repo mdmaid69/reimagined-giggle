983c5c5f7a8f4a0242b2b601da456bc777cc3ed1
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:43:51 +0000

    water old just turn would

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-for i in range(5):
-        print(i)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def convert_to_binary(n):
+        return bin(n)
