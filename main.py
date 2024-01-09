b3f64c95478bb99f93ea8b67ee7f8408b9433814
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:38:42 +0000

    then interruption over hope to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
