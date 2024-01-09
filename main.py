2832130cdc29c8e0d70cb097cb0d2c7f5dac7036
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:30:55 +0000

    false creature now underneath argument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def remove_directory(path):
+        os.rmdir(path)
