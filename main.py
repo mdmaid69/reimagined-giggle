759b51d73108c8424e8d25ed5c3fed9b9e7c2130
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:52:32 +0000

    breakinto name minority drawing vanish

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def count_words(sentence):
+        return len(sentence.split())
