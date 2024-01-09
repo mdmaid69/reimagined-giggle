10304bed5eaca3c6f81b3f22e7809890d08d9faa
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:55:11 +0000

    object worry unite lookup study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
