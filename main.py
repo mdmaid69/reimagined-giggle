5514976ea4d0100c9138f59d638ffb5fb23221e4
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:03:40 +0000

    put frighten although trade thankyou

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
