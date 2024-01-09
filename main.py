bb0c878ed1e6251d678b38cc06ead4a0715cc4e5
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:45:34 +0000

    stop debt back tear direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_base_name(path):
+        return os.path.basename(path)
