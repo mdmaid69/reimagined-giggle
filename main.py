2200a2f9b4b71f40930fc2ce63ef3ac0c8adf990
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:54:01 +0000

    computer play nonsense fat branch

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import datetime
+def get_today_date():
+        return datetime.date.today()
