25753dadf3e57e2e58517a804b56b5dbc048bed8
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:39:37 +0000

    flat sell motion evening behave

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import sys
+def exit_program():
+        sys.exit()
+def calculate_power(work, time):
+        return work / time
