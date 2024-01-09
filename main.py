67ff0a26e75175aaeda93d28375f8c6be5cf67a7
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:29:40 +0000

    gentle wrong price coffee suggestion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  def convert_to_binary(n):
+        return bin(n)
