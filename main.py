4d8c9a8fe44737489ee264c82f19ae41d496006e
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:20:20 +0000

    mind previous explanation dish measure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def reverse_string(s):
+        return s[::-1]
