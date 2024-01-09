be3fe7f5f44fa287c34093a643a4c84e9c95e692
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:56:41 +0000

    punishment recovery unable male pronunciation

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def convert_to_octal(n):
+        return oct(n)
