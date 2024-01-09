000e27f39cc81be5eeca60efa9524acec5c837eb
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:08:02 +0000

    Zz type store react where

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+for i in range(5):
+        print(i)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
