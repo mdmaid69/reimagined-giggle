de2359110652c1aa9903cc05c97fcb985e622101
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:45:25 +0000

    mouth disappointed science comeback special

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
