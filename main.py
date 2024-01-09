c0f58c0c5a25bdaf67d177df7ec6941a3ada3851
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:15:42 +0000

    argument exit value party shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def convert_to_hex(n):
+        return hex(n)
