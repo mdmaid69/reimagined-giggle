0b2e294ded77c018851f0dc76955b33e2531d46e
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:05:11 +0000

    treatment use room port feel

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import time
+def get_time_since_epoch():
+        return time.time()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
