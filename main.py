75c2851a30c508c714773bf1f307d5ffa2a66847
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:50:26 +0000

    away tense cover age ordinary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import datetime
-def get_today_date():
-        return datetime.date.today()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
