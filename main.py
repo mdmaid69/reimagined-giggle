a9b59d5d8030952c2a52183b8da9c3ce8aeacf66
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:06:53 +0000

    soil protest rush maximum judge

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
