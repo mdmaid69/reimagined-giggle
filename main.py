0fdb2bfe15178ae900bd01d798304b72a9df8683
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:12:39 +0000

    unless lose basically unfortunate stretch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
 import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
