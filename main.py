ec11c5fb6b9f6f3d639e0b535630cad83fcb0737
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:14:56 +0000

    standard their measure cheat smile

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-print("Hello, world!")
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
