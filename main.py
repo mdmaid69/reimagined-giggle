4368046f0f60f0bdacaa12e2482430ed2d971080
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:08:19 +0000

    goback(to) another test lend rate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
