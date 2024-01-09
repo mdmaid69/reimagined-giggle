1548bd9fca581aa667b5ae92238dd1ee2a72fbcd
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:24:50 +0000

    shame callout night steam keep

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
