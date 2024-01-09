881ea39431d343a5503ffe17e0fc83dc9fb01673
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:33:57 +0000

    star return noisy step busy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
