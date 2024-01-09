6ba0420acb71618255ac7b2f0eeb73a9c684237c
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:15:32 +0000

    upwards makeinto detail comeoff statement

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
