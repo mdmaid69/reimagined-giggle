eb70d305b1f06534d0bbc107ad60f35210fa2208
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:15:19 +0000

    opposite instruction warn loose challenge

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
