f39d842589f1a92ffdbb28c73bea1a124bf63c8e
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:20:27 +0000

    full right anywhere command use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
