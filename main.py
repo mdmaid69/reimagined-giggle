fa9b5e87c8d47df8a1d32f6598503ad162b4978a
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:24:29 +0000

    water attempt within birth stick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import getpass
+def get_username():
+        return getpass.getuser()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
