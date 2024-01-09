8bbcbb4d60eb7df0b41a5711d5ad463db1953b96
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:16:40 +0000

    start middle contact related show

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import getpass
+def get_username():
+        return getpass.getuser()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
