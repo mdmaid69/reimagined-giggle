e583acf203065a65584688a9d7bcf78f84dae0aa
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:06:38 +0000

    strange door invitation no speed

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
