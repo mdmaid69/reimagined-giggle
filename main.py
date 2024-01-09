35492565a114104a35c933864036eac9798fcdf0
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:50:00 +0000

    friend contrast awake except beside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
