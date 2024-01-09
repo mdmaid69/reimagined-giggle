06e51b3f4628054d8b2eb0bed5279ceb540f623f
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:23:07 +0000

    though mood takeback tough change

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def calculate_area_rectangle(l, w):
+        return l * w
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
