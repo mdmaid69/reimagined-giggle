739ca77e5cb22a680c6a1bd0bfe124a8e3134bc4
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:03:52 +0000

    intend tip again who guess

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-for i in range(10): print(i)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
