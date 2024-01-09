bb9d0c666d5009a213c1ee2a0efa7c35b03383a4
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:18:52 +0000

    keepup disgusting confuse wish lie

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
