aa8c7f060966fe11bf2385d5eb20ff7426167797
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:16:37 +0000

    region lookaround aim future child

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_max(lst):
+        return max(lst)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
