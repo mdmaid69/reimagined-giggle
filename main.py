cf75767addb8a7024cd4f6fc6e6ee647db479591
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:29:02 +0000

    friend service star respect schedule

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_min(numbers):
+        return min(numbers)
