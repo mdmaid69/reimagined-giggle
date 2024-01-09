efe5e5277d74b02463bc4d043eb9e546250b8522
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:53:28 +0000

    cough sleep train grateful appeal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_array_typecode(array):
-        return array.typecode
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
