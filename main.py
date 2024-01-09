011f73d5e4735caf29a042c8b253fc915dbe7994
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:34:53 +0000

    welcome shoot emergency suit hurt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
