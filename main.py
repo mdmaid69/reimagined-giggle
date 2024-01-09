6cddabe5366bffc79993e0db2d21085d1dd35e0c
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:15:16 +0000

    callback accident offer down mass

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
