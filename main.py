c58824c15d5550dec7cc59f1ca04d153fb8bf672
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:16:32 +0000

    away juice wonder find light

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
