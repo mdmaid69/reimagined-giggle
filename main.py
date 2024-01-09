6ee0a4d8b22a185760ab6975cdefa35c47bea141
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:21:38 +0000

    foreign sky wish child patience

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
