254b4b591bfd62e1c0cc48366f1bfcd1e1376d7e
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:51:21 +0000

    whistle last order just unusual

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
