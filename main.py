4a5ac14dfdc94b2d672469e73e27504e606b9e98
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:36:53 +0000

    temperature react responsible use tone

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
