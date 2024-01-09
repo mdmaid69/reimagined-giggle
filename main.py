3ae51a3b4f36540a0886c800a8cb15d07a9ce26c
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:50:37 +0000

    eye wide land stamp so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
