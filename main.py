f52b40ab605403eb69dc4cf29a5f941f39610239
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:07:25 +0000

    course partner invite show cut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_speed(distance, time):
-        return distance / time
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
