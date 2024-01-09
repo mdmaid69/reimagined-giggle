0ad77fe17eb72ea5a73fcc6c8ebaa46cef3d5094
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:37:23 +0000

    turnover confuse round yourself address

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import math
-print(math.pi)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_speed(distance, time):
+        return distance / time
