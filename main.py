3c40c203abdde3df90c029bc96ee2101725fb814
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:59:28 +0000

    work file neither apologizeapologise sure

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
