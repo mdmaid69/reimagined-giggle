ac1dc20e385f7a838fca28a7917bdb9f0223992b
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:13:58 +0000

    pipe bank set stay turnback

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def find_max(numbers):
+        return max(numbers)
+def square_number(x):
+        return x**2
