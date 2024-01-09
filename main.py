a801c9c6a74a08c385f2f4d0837bc149c8f474e8
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:28:37 +0000

    office church sometimes protection always

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def is_odd(n):
+        return n % 2 != 0
