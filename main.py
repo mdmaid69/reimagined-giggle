03fe75a6cdf4aac10d2bf5c77c3f1dace3c338c7
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:55:48 +0000

    storm sister store drop century

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
