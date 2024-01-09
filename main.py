4f4a649a571c3dd8cfa3f1a41f78a3a041b3471b
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:34:45 +0000

    address independent event thing home

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
