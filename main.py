62ea4ee4c2113e7eb18f70b3b4929cc9ea54cb5a
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:52:28 +0000

    help suggest pause forgive stupid

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import collections
-def create_user_list():
-        return collections.UserList()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
