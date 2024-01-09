b9fe18fb645489809f41a7381494b882b3d34f94
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:12:08 +0000

    foot limit fit possible add

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
