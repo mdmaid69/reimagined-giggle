e064d781da44bcc8aba323181a02ffffb20c5ec2
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:53:16 +0000

    cross wear beautiful shop fairly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def clear_array(array):
+        array *= 0
+import collections
+def create_queue():
+        return collections.deque()
