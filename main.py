8bba312407fb3c89cb57186fe150e31d94e9da07
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:28:34 +0000

    block herself vote wander quit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import platform
+def get_os_info():
+        return platform.uname()
