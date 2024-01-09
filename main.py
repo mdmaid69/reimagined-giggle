810886c0444ed6cfcdc67ac9f24b63bbb6ef10b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:13:03 +0000

    care action heart about borrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
