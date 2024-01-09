b50f4110ac7468b198b0ed367559366411781210
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:34:28 +0000

    our by thankyou ago life

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import os
+def change_working_directory(path):
+        os.chdir(path)
