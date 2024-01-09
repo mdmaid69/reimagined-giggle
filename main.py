645b561f222fd14b24431cf209335dbdf37c994e
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:28:24 +0000

    still beautiful class up with

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def convert_to_hex(n):
-        return hex(n)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
