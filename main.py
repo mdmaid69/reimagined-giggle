2b7361dea66254a3510880a8ca839bdd8b4a2446
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:33:50 +0000

    journey stand pipe provide stare

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import re
+print(re.match("h.*o", "hello world"))
