d5d9ff37c9e3a7e3250271e9885f5cd6b7abea98
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:36:25 +0000

    normal force dry protect fresh

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def is_palindrome(s):
-        return s == s[::-1]
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
