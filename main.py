19fb9483b028d0899f2b967e2270afe9330e8813
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:14:19 +0000

    stage offence pool survive comeround

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
