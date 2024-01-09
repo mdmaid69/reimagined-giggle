f7bcb329f661cad2eca2a4cde1a3c11f9a90b58d
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:26:06 +0000

    surprising awake low soil dear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
