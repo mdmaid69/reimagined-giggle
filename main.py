5da33774b321af4cb74d3509d4a158b7f16f9017
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:14:47 +0000

    thank trust callout sport pick

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
