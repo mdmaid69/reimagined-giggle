e5393906640726628ebf785fd80c70c9c5b9248a
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:21:57 +0000

    since habit risk fashion workout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
