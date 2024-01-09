dfec343a7ece162b2064ce2e32bcfe1d02f2abb8
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:23:08 +0000

    breakin embarrassing true plain brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def find_unique_words(sentence):
+        return set(sentence.split())
