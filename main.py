7730da0fd4d47416177d722fe96f9ee6778d2262
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:05:33 +0000

    press sharp for collection switch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def convert_to_binary(n):
+        return bin(n)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
