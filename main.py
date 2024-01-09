05a81abce2f32f3dfddccf9fbab7d4db835e1e8b
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:09:49 +0000

    adjective left finish city weigh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
