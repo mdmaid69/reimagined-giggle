46b43a324d2166cb6d49ddb9ddaf31e9fba3ca0b
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:53:27 +0000

    later just whistle several much

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+for i in range(5):
+        print(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
