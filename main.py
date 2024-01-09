cebacffe582072d40f78de5159cbc537c2efc610
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:22:18 +0000

    delight dance anywhere short measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def sort_numbers(numbers):
+        return sorted(numbers)
