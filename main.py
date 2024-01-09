7795d54ec92975c37279eba55606de3095d7718b
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:11:50 +0000

    add load all city comfort

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
