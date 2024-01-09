d79acf847005a79499bd202efaf7d3545acc1347
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:58:36 +0000

    damp aware deserve honest charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
