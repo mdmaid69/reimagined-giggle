85cc2b734e3af0c9ee06647e6f954ad5a6d9b66c
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:18:18 +0000

    style level breath fashion odd

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_current_directory():
+        return os.getcwd()
