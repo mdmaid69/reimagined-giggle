58c4c8f29679b79a4e080eb4084757abed4d1d28
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:26:11 +0000

    paper lend example advanced instant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
