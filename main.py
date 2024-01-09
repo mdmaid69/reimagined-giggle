2b9aa372852a84b93a74bafdc7ba06b592b9b433
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:46:41 +0000

    condition any contrast prove manner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
