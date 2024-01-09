6b01647950314aa60d158db346e49effb19f4a15
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:59:38 +0000

    ahead tidy lucky use wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+print(time.time())
