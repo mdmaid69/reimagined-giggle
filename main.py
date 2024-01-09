3a3fc4862893c5c833daf535351da67f99e3e766
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:47:52 +0000

    murder unite shape regional sore

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+text = "Hello, world!"
+print("Words:", len(text.split()))
