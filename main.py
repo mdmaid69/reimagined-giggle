7d5711978ab64cc5e7b16a1133a532a226d57ad9
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:40:13 +0000

    commit happen phone swing hungry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
