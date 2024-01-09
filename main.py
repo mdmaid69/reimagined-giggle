ff5e9222a451f183d5c2fd7adda0146f2d326d00
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:39:39 +0000

    completely state land medium chief

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
