25fc9d24fc9f0870812b5bca8a6865fa801d1abe
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:13:14 +0000

    switch sink getback pleased responsible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import tensorflow as tf
+print(tf.__version__)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
