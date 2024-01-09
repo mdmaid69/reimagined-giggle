3089e5db4c8c18f9200232416b9c85d55de9bea1
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:51:09 +0000

    new flood Rr wash cruel

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import platform
-def get_os_info():
-        return platform.uname()
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
