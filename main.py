bfcf9bcd4f200a039c81de3945192229d8ecb503
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:04:14 +0000

    used freedom pride frame strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
