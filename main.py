4333244cc14938efb6dcc31f0ced585e88edf8f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:31:05 +0000

    insist little responsibility conduct especially

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_power(work, time):
+        return work / time
