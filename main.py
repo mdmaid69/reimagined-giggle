d892c2231ae72975cf01eb321070a0515f32f270
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:35:31 +0000

    the cook obvious gentle behaviour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
