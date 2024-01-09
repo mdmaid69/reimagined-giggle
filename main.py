c24de00fe7716025c97efb711ba6bf557324c378
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:56:51 +0000

    turnout punishment today hungry factory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
