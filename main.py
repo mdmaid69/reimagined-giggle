fe04070e24e6cd97d22dc9486d8b2b62b6fe4ff5
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:25:47 +0000

    fortunately central connect them practise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
