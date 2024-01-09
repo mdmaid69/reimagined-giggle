25d44335af452d8f813487954777933ce5c48051
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:25:10 +0000

    into plate regular somehow welcome

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
