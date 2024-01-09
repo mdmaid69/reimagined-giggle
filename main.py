5030b658098bb9abeede6023008e4632157f6210
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:11:19 +0000

    rain Jj concentrate trick lift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
