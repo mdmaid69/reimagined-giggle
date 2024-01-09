85195bc0f300dc7423e159b2514a2f0ca179c0fc
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:30:43 +0000

    into curious project quick gradually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
