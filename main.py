88c474fc864ae40658870ec64089816346e56398
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:43:34 +0000

    separate save impatient getback holiday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
