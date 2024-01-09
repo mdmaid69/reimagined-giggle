dfd7f7b489a0cabf3567a5427827d89a680e3907
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:10:40 +0000

    accident circle as everything actually

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def divide_numbers(x, y):
-        return x / y
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def convert_to_hex(n):
+        return hex(n)
+def calculate_area(radius):
+        return 3.14 * radius * radius
