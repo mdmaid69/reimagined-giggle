fbb5436371d04a6ebded7793e092f7ddf5f6803c
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:00:41 +0000

    pleasant port hurt pleasant road

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
