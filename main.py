f638b335983b54d91124ebc1b7213216e4159e68
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:15:28 +0000

    fire giveout concerning fasten fairly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
