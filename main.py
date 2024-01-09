a2698ed6c8de591dfd95872736b505bf26424aa7
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:53:54 +0000

    alive hospital get strike admit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-print(os.getcwd())
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
