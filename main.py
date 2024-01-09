eef6edd472d0a6515d3a42dd69ab2edfe85511cc
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:39:03 +0000

    wheel cough funny confused great

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
