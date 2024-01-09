79ac23ac39ff561b77bef99641a4f8db2982bf5f
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:14:40 +0000

    his noisy completely city whole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
