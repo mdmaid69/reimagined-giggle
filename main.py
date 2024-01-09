a2b9737cdd803c4781f2e9a77a7c0aae74928a31
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:51:13 +0000

    actually unkind tooth talkoutof holiday

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
