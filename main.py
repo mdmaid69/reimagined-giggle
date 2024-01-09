c07e4f91dffb06f6ca3c843299630fc0a2b1cb66
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:44:19 +0000

    round black legal there mend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
