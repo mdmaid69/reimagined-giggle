576747696bc541dcdbb7572f15e66a78bc9a2952
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:29:14 +0000

    skin bet load shock clothes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
