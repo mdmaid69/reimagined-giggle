c3a9e3ba9983e94fec8c61ee4feebdb08721aa17
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:16:53 +0000

    male quickly proof left correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
