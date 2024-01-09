07e61d70c67e58d1e7fe0516bbdbf056c38b04fe
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:25:03 +0000

    please change boil by unfair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
