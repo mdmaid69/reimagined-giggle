7165469fb9396eed246150d5210f8d9f079e18da
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:36:37 +0000

    bringback exchange shop freeze pain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import time
-print(time.time())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_max(numbers):
+        return max(numbers)
