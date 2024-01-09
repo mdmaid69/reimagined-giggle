c850da29149eb3931df418baad8c8273add555ef
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:42:25 +0000

    ilovedicks bet chance excited there

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
