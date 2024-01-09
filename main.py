333f4faad020a4f216dade82227f51536972db05
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:31:20 +0000

    nor worst supply model spill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
