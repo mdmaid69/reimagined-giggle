727e6189139a88c053e6ad55fbb2c943934b9778
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:10:03 +0000

    noisy pen switch below shout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+for i in range(10): print(i)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
