c15b8a10c9d603b9066228e84bbe240a797c44a3
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:12:50 +0000

    sit truth breath warn pick

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
