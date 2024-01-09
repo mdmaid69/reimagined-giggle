dc155adcc807d69e32cd49e3ddc1767e66d48609
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:58:00 +0000

    year glass declaration hospital or

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  def is_prime(n):
+def is_prime(n):
         for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
+                if n % i == 0:
+                return False
         return True
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def convert_to_binary(n):
+        return bin(n)
