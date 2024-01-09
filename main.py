ed4765cc9374ee44b3794387832c8a9ca0e8945d
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:24:42 +0000

    see point before wrap little

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
