6e7aa232579d56480e3c7fe1d418c3a24c5db708
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:48:56 +0000

    trousers success prize sport play

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def reverse_list(lst):
-        return lst[::-1]
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def remove_duplicates(lst):
+        return list(set(lst))
