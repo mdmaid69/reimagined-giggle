b01d849a7b2c04bf344bbf40deb260c254b2c690
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:31:07 +0000

    refer surprise operation ready noisy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
