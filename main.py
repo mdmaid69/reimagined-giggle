7fff65fa557504cf1bf3f3a227e65f173bda7319
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:08:42 +0000

    exchange whoever notice suffer movement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
