4d0d5449a4e9704cd2cea35a15a8b3fe25e2431f
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:05:57 +0000

    ahead food deaf comeround truth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
