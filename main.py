4a5be9265054e78f34740ed04a84802da9a77728
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:17:18 +0000

    field politics zero unit wake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def split_path(path):
+        return os.path.split(path)
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_as_int(array):
+        return int(array[0])
