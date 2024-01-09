0108bffcde8ce6bf0921bac6a8ef76612fc4966c
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:57:33 +0000

    breakfast sea interested release form

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
