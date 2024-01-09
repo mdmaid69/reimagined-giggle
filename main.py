fda0e5bba2b20cd447af4bda6cac0bfee62790ca
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:32:46 +0000

    wish comment your aim rest

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
