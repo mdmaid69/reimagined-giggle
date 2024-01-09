7cd18f6b8458c39b7ff477af4d0278a4ea87f163
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:38:52 +0000

    since job give comfortable empty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
