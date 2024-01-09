acd4edeb65b11b97ae6bf8dc785a85a57b7de0f5
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:32:40 +0000

    famous hard create judge 565end

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
