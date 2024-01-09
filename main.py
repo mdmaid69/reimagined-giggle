b9b10783bc4d4f75bc7dd498926c052aa22237e4
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:00:07 +0000

    of children quiet heavy fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
