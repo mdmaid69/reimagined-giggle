7f36f69746695834df58460afe6feaadce979e33
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:07:39 +0000

    spoil service often acceptable add

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def is_odd(n):
+        return n % 2 != 0
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
