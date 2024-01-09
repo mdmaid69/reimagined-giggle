83c241a39a34c713e79e255d962f05d43b849c79
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:09:30 +0000

    treat spoil once suggestion when

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
