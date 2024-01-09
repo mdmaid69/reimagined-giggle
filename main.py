8e46e530ee06aa8a12b49fb5d9495bcf516b29a7
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:28:10 +0000

    form fashion rescue set anywhere

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
