6d9a9651675d80f3c4273562563b0d236c849c68
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:19:26 +0000

    pretty division rough strength goout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  def cube_number(x):
-        return x**3
+def find_max(lst):
+        return max(lst)
+  def reverse_list(lst):
+        return lst[::-1]
