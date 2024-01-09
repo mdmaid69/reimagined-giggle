a03a82a8f6ef13c216f68df7edd292ba55cb9282
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:37:52 +0000

    pupil sound argue patient first

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
