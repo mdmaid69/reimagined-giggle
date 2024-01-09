2c19f9eabaf882ca0820441bb5ab6d0d44f7481b
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:53:19 +0000

    visit number radio neither rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
