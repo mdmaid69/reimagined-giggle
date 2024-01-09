af3c4ff773b5c103f138665fe5b317052b450b66
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:07:39 +0000

    concentration print takeback temperature long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
