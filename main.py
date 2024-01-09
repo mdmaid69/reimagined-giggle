028a2dae08520bc94b773f92324275d205515f03
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:38:05 +0000

    press song boil afraid pain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def reverse_list(lst):
+        return lst[::-1]
