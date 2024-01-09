7caf7be2d7b87f50ea45b24c6a0930499109694f
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:47:56 +0000

    still challenge stand vision unhappy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
