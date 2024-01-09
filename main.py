5c8adf1ec90807aef29691622f1aaae757b511c3
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:31:15 +0000

    gentle spare moral walk will

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import sys
-def exit_program():
-        sys.exit()
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_as_list(array):
+        return list(array)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
