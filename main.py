c322a1d1269c355d29cff1472d16f36cb8d724d0
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:07:38 +0000

    freedom delicate pin because familiar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+for i in range(5):
+        print(i)
