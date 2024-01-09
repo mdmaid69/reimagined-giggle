3eb6cb676ce274b4f2a6610712824e4cb6c82d2d
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:06:39 +0000

    save runover quit patience circumstance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def square_number(x):
+        return x**2
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
