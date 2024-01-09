5db9e3295aea5341e095cd2a784a99d449673295
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:24:48 +0000

    pencil workon attempt good 568energy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_as_int(array):
+        return int(array[0])
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
