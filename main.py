4e3ff54589c2133e00aa762b37b5d87b1dc8bb38
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:58:41 +0000

    secret damage responsible such believe

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
