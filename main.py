a44c0892d009cc06a9c26ac810426d87e64b1d01
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:16:35 +0000

    here remove under section lonely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import time
+def get_current_time():
+        return time.ctime()
