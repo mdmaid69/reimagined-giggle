f677292d8657c59304c83bd7a8a2ffe30fe3961a
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:49:36 +0000

    accordingto anywayanyhow deaf behave belief

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def add_numbers(x, y):
+        return x + y
