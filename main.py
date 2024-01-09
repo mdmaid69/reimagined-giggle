17e5eed2a98bec526f895edc78c3852e76021111
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:36:12 +0000

    pleasant where claim hello impressed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_array_as_int(array):
+        return int(array[0])
+import os
+def change_working_directory(path):
+        os.chdir(path)
