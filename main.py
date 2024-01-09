a40c2af1aaab6de8a375b82e9dc0ba2d57c0fb21
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:02:12 +0000

    conduct emphasize unless effort part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
