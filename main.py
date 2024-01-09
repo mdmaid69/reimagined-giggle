edf0b85135f0e95228e4724ff45511f48e183e40
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:48:56 +0000

    trip join paper skill deep

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-print(time.time())
-text = "Hello, world!"
-print("Words:", len(text.split()))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_as_repr(array):
+        return repr(array)
