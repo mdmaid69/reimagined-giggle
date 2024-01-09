2b717880637be2da6a791552a7ec26375692b854
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:55:33 +0000

    gather rely measure once programme

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
