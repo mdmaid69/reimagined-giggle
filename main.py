76a5e62b8ef5ccf1f7a513dfdad8056625b986a6
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:02:41 +0000

    challenge worry deep bit maybe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import time
+def get_current_time():
+        return time.time()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
