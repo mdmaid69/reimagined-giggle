1e4351846dfc680911d3c2af4e8a0bc44278e961
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:05:39 +0000

    sure relative pinch music dinner

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
