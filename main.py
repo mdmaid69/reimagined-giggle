6a8cd2adab3e4fa192acf40a50a03556aafe2f9e
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:11:33 +0000

    favourite guess pronounce breakin cheek

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
