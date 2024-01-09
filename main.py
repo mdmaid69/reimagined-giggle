1ca627f34b2e7b66671026191e6427117ffc5e1e
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:20:17 +0000

    pride me contact minute accidental

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
