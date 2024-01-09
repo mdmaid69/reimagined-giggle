e52074253d7525bd10ef3174ee77d380aeea3283
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:09:30 +0000

    account precise refuse fall shopping

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
