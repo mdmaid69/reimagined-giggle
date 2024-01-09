9f1314d7f094135fb1200b79a15e0bb5dbc70b0f
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:53:52 +0000

    free wire increase appear comment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def find_min(numbers):
+        return min(numbers)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
