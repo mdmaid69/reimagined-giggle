acee04c7161d3d39451a5cb56186ea258fd73918
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:19:26 +0000

    deaf minority well trap fortunately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
