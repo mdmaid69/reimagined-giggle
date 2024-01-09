a04ac05c83b16ba59ceb0b7123cc0f719e56d073
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:23:42 +0000

    sad do drink skill confuse

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
