5bc04a0b857952c69745951a8be57affd57123a0
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:15:15 +0000

    attention arrive shine that bringforward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
