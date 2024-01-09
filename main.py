632bb3ddcf8b279bfc5006bda7e175ba9e930037
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:35:26 +0000

    tonight possibility ought refer strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
