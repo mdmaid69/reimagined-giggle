1152c3d799ab88bdfb123284dce6760efcf5256a
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:11:18 +0000

    remark woman set another amount

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import time
-print(time.time())
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
