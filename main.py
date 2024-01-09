5935593d1de698c96dae8ce0134e89e2d583539f
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:02:35 +0000

    comedown continue admire expensive substance

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
