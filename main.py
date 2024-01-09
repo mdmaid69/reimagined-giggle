8486d29be0c7dd216c6b742dbd1ea36e5af43a4a
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:27:20 +0000

    style common calmly talkinto separate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Characters:", len(text))
