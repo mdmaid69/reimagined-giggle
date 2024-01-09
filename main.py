0273ab64caa7292b422a34961feae5ea9c0ec10b
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:58:29 +0000

    lie warning through firm battery

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
+def convert_bytes_to_array(bytes, typecode):
         a = array.array(typecode)
-        a.fromunicode(unicode)
+        a.frombytes(bytes)
         return a
-def sort_list(lst):
-        return sorted(lst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
