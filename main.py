b260ffe2a4d4b672eff37bb7c4a5440e884d346b
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:35:11 +0000

    regret smell harm close pinch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_power(work, time):
+        return work / time
