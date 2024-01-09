20e958c6ef3326bd7bfc5d8f6d8eaf67a80c03a0
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:22:59 +0000

    shadow sort wild soon let

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
 import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def pop_from_array(array, i=-1):
+        return array.pop(i)
