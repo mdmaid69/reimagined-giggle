328a44d6557f1623657c42c1d26dca0b758aa5d9
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:25:33 +0000

    sight pleasant flow dress forget

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def greet(name):
+        print(f"Hello, {name}!")
