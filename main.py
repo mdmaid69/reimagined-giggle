32424424f89de4fd3cc25d90fc7337aa6c5f9661
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:04:54 +0000

    disturb criticism fact breakdown equal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
