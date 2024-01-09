5fc14ae0ee5190cad10fdf5bbf0dcbba2b70ec8e
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:10:02 +0000

    young stick wide cruelty noone

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
