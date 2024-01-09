42de36a468cf9bddaa30073a680e268a3f27339d
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:02:49 +0000

    middle length claim deliberate stir

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
