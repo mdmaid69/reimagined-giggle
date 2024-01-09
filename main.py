578d6b9dcd944ec9c66da8f8e9b0c3a1e66fbcfa
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:33:03 +0000

    commit study callback decision wherever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
