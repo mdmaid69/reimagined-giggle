014b10538bd4bad00bce59dad4809c69a5d52634
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:32:13 +0000

    so 567enemy display provide present

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
+def convert_string_to_array(string, typecode):
         a = array.array(typecode)
-        a.fromunicode(unicode)
+        a.fromstring(string)
         return a
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
