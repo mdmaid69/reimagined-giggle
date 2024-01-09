8e2d47a5422ae9fcc4b13cd4c0f872e5395da37e
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:14:05 +0000

    bank persuade see able term

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
