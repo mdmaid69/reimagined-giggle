e3cef67c0cdeb7b519b94433fa94f5fe604407d0
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:53:52 +0000

    suggestion slowly crime criticism still

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
