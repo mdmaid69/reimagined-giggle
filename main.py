8328a32a240e6686f0fe7b6978acb77ba79bba8b
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:11:08 +0000

    instruction tap warning effect operation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
