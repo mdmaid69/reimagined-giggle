4a57de4b176a3556393b6276bc22710134ed325e
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:38:13 +0000

    quickly search below door keepaway

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  def remove_duplicates(lst):
+        return list(set(lst))
