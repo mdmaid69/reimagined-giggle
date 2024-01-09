129006822cab3d0955acd9b629e501c637d33bf2
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:41:37 +0000

    argue actual confident track kind

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def clear_array(array):
-        array *= 0
+def calculate_speed(distance, time):
+        return distance / time
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
