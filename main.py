5a6c534a12b10db0180800feac4bca5005603026
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:21:07 +0000

    pin search majority catch stretch

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-print(math.pi)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
