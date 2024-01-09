c8a8e5f0d821aa5919a90b915fc1ed84146112b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:07:43 +0000

    exciting shock stiff phone battery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def square_number(x):
+        return x**2
