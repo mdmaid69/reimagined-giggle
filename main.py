777b3893c245118531f47192ae2c49331775b4e1
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:20:04 +0000

    cry condition birthday rent get

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
