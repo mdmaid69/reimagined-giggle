d3d52e2e89d10bf1fd82cdb174989df9dde2a296
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:45:49 +0000

    about happen receive invitation under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
