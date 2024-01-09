256eb222dab32fb6fee9ed741717a9720fa6dfc0
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:23:53 +0000

    stamp house load pity contribute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-print("Hello, world!")
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
