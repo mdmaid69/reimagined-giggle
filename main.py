b400b89d3294beae98a3071033ef46d31ff6c5c9
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:56:11 +0000

    thing lose shock 573entertainment crisis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
