516d8792ac1f7e578b93409d40221ab5035a33f1
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:31:12 +0000

    influence pot accidental open custom

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
