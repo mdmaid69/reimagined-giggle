f92b68ea127c6cd633c9f2e730bacf165981f66e
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:47:11 +0000

    him earth sell interested else

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
