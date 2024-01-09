6398b033fde3cfdc947a2fc1eda1f9479310f863
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:34:35 +0000

    catch waste pleased her clearly

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import logging
+def log_message(message):
+        logging.info(message)
