53822262542d1cba17f19f3310e475a38669ea35
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:57:07 +0000

    trust negative moon little couple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
