1110497297e570e30ad040b3fbff0d0af248d57c
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:33:26 +0000

    line rid among murder argument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
