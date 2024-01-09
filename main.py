5b7d5eef8a15163dd32a0c76f56dc634ec152538
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:45:27 +0000

    name attempt instrument sure cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
