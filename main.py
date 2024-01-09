40d7d10fdf6cb244adc0f4fc582a4b568ad2ac2c
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:39:36 +0000

    can lookup relative face act

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
