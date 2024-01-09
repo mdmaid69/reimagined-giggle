4ebbb02d053c0bc47cd9bed598234ef53d04e2c4
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:57:10 +0000

    impressive quickly tend along reply

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
