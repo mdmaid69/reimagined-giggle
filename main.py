a7d4228e37189ac99f61821ac11ed98a3be11c23
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:30:43 +0000

    too man bag speak constantly

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
