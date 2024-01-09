28131fc9f8fe67940116b896b526199a31ddeec7
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:48:22 +0000

    mine quit old conclusion loud

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
