57de63284b5528096b189c6f15975541743aa36d
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:29:21 +0000

    store approve us almost often

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_area_circle(r):
+        return 3.14 * r**2
