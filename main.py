4bbeb787ef5bc14fbe50df09ede2f886625e7ce4
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:06:26 +0000

    communicate tap copy spread music

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def convert_to_octal(n):
+        return oct(n)
