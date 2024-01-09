dd26e551af82ca8180e3de942c831828dcb8dba2
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:10:48 +0000

    argument course neither stone order

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_acceleration(speed, time):
+        return speed / time
