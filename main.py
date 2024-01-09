b58b8d80c11fb98b314e129b4b280102dfb4dc9b
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:40:45 +0000

    tip government discuss although getoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  def is_odd(n):
+        return n % 2 != 0
