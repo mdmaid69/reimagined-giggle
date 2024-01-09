c5bf55f799d7020e812aa8e95f658845a21f10da
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:58:15 +0000

    prepare process skill object bet

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import time
+def get_current_time():
+        return time.time()
