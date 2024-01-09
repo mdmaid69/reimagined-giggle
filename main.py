01568d33f843d7279515ef467bbf1d386cb6a891
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:34:40 +0000

    hang usually hole too beat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
