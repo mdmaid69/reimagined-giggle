40329595257552af3606a4f4c552181f014d4617
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:37:13 +0000

    do close angry build ground

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_acceleration(speed, time):
+        return speed / time
+  def calculate_area_rectangle(l, w):
+        return l * w
