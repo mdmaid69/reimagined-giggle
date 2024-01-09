2538a7062174f5410e02b1d8fe35631aeaa533f9
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:04:19 +0000

    twist bridge theirs bridge easy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
