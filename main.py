b7c2b708d7447c5bebf9cf70921f91b6eb927fc2
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:03:58 +0000

    throw home closely nice quantity

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import os
+print(os.getcwd())
