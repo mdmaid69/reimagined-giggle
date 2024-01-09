933f07cd10871df56dd9ec6793b76f36b32dc0db
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:48:58 +0000

    silent somebody improve authority take

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def sort_list(lst):
+        return sorted(lst)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
