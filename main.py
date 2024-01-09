95e243cc5a52221aafd29e9b5024f493496b5e71
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:32:59 +0000

    join dress hurry choice lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_work(force, distance):
+        return force * distance
