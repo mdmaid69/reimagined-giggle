f686c8ad2f6396e81f57b1ddc056cab16bd68969
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:28:03 +0000

    little little plate bet upwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
