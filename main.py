3f65b79c27ca2dadd0f317b5559b5f292f8804ea
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:48:12 +0000

    peaceful whichever gap ear ashamed

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
