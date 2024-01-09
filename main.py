d2a58deb268b0fa99c59a52d62e345330e7901f0
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:19:31 +0000

    high of apart comment room

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def convert_to_octal(n):
+        return oct(n)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
