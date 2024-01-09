a8bec15a85c3344421ae43926fa2e9b4a7f31214
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:52:34 +0000

    method interview pipe first soft

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def multiply_numbers(x, y):
+        return x * y
