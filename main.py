83ca5ab48453bcf1d743ca86e638e10275a2ecd4
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:08:57 +0000

    short first forget crime friend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def convert_to_octal(n):
+        return oct(n)
