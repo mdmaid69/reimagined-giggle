11943328ef585ab9d6e3e333a27911c775d5ce48
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:32:00 +0000

    many accidental considering lookup upwards

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
