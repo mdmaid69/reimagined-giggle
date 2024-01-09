4bfcb39428e2c9790ea5f395b6b1944ddc4fc48e
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:25:59 +0000

    Ee Mm Pp own before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
