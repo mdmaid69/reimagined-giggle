2c51b293560e9c5090b3edaec3b15067f38e6dbb
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:37:59 +0000

    moment provide man guess weather

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_min(numbers):
+        return min(numbers)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
