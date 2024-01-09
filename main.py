c08ef2245863e9dd3b6b7fb23eda65b877b56934
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:55:13 +0000

    search division turnout past music

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
