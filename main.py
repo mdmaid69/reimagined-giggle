687a96cc5620c998b1f511d19ebd9d12f62b81c0
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:24:48 +0000

    film Dd file slide dance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
