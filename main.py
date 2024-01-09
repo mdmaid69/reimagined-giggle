eb8378310759efe49f36485989bd3d9177a66ec9
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:47:10 +0000

    office new against full could

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def cube_number(x):
+        return x**3
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
