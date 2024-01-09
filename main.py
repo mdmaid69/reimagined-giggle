4390baf3a63fe103a5a99d42fad6a2fd21630b3a
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:27:12 +0000

    speak skin lock chin joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
