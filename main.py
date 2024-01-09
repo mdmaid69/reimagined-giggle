91dea779b1148635acbe74e5dbd28744fff8e236
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:03:38 +0000

    reasonable law importance calloff clear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def remove_duplicates(lst):
-        return list(set(lst))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def calculate_area_circle(r):
+        return 3.14 * r**2
