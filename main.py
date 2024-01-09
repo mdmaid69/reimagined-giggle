785509120563d4d2506e3d105e61369ee3cd229e
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:40:19 +0000

    female breakdown feature decide creature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
