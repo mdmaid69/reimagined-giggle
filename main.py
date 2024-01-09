1d8e1e9b1f5796495bc6b063140026eeab18e105
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:51:25 +0000

    point cinema turnaway lucky deep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
