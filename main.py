6a425e51e537f482d0af7712c36b9147f9605ea6
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:15:30 +0000

    crisis ourselves patient attractive represent

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
