1abb0067f5905d54d96dd22917b01fb02bc29aa2
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:19:53 +0000

    life still question friendly between

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_float(array):
+        return float(array[0])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
