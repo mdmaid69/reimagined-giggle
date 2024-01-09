cd92e539998ffd3899cdaf9ac8c3c66a6f7b59a2
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:36:11 +0000

    study cover ring getaway weekend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import os
+print(os.getcwd())
