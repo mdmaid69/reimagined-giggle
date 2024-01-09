bc20fa3881d2d8fcbaa7661fb0914d198ddf3942
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:14:03 +0000

    science moment equal vision glad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_length(array):
+        return len(array)
