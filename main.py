7ac58a6768aa3e84363ab4f9ee1f31ca2cba256c
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:27:56 +0000

    practise detail most public brain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import collections
-def create_user_string():
-        return collections.UserString()
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
