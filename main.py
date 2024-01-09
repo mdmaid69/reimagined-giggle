25bac44909b5037c8795930f1561743fc382caa7
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:48:46 +0000

    ago lend allow snow pin

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def add_numbers(x, y):
+        return x + y
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
