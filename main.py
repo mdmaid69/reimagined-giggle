565f1866250c5b021e439ec9c523fdf5d9573931
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:31:57 +0000

    search demonstrate ear rate fly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import tensorflow as tf
+print(tf.__version__)
