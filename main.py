b825a0ad347fa06a6fdb15f3a2748cf2d2e6165a
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:34:03 +0000

    sale sale earn impressive head

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def convert_to_hex(n):
+        return hex(n)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
