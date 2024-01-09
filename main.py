a6a3ec52cfbf512b0fef892e60668f37674ba1ff
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:26:39 +0000

    congratulate natural main slowly convince

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def reverse_list(lst):
+        return lst[::-1]
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
