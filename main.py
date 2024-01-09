7efc52942cde481caf63fb7cf05c16ea2986aa21
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:23:59 +0000

    attention steal prize likely hot

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def sort_list(lst):
+        return sorted(lst)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
