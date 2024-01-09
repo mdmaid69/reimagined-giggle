0cb03ee660b7b65e9035269ba2f99f0aff26084c
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:52:51 +0000

    seem modern example surprised smile

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
