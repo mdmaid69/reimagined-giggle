f9475335b107c7d24d5535cc06f08a030e9db76a
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:50:31 +0000

    twist bit copy content fat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
