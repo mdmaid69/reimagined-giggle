3d784fb35fb132a2735be62e7755fccf637e8534
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:20:29 +0000

    available voice long either beautiful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
