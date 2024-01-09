ce718434bbddb03af83697e012a9ff43dbd44f07
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:10:49 +0000

    blue loose long either callat

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
