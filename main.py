d0b1866b522418adcef82781133619442d01a19d
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:22:17 +0000

    dress fair used hate Qq

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def sort_list(lst):
+        return sorted(lst)
+def calculate_density(mass, volume):
+        return mass / volume
