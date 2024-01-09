0472abdc0e5ab6052552d26a38fb38f746c6d36e
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:42:11 +0000

    believe afterwards strange traffic passenger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
