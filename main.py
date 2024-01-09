d98d0414ecb269aaf17397df163f28ef6b10061f
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:09:01 +0000

    workon change tell repair breakin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  def square_number(x):
-        return x**2
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
