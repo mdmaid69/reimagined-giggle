504af5fdda902188f501f102d1e1e7aa65efa4d6
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:04:40 +0000

    steal part praise recognize word

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
