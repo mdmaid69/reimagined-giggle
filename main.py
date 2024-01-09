2bedacdce1171ad2f5d78e7b9189385b3092ada0
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:01:58 +0000

    she favour generally mess welcome

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_factorial(n):
+        return math.factorial(n)
