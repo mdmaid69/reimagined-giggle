a88d6cbf5dcb666d3432ecc9deb553551ea151ea
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:18:32 +0000

    education ask quit insist block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def count_elements(lst):
-        return len(lst)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
