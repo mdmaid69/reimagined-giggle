a897e67486f0ffa95f5df83276e0ec869a4fdd8b
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:46:07 +0000

    excuse different might lookback(on) put

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import math
+print(math.pi)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
