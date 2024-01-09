baf6287af948cf521b25cc09c93d303a81a8cb2a
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:53:49 +0000

    ignore answer tonight used how

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
