ec91bb44b11ed640c6350d915dc92a875cefcebd
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:50:29 +0000

    choice emphasis straight contrast recently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import platform
+def get_os_info():
+        return platform.uname()
