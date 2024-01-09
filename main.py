e1274a4f9d3be809aa5dd957d9555b1ecf12727a
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:01:39 +0000

    public split develop burst put

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
