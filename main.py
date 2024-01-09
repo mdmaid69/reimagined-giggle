403c3c7cd4a8acc5a06420c1ceb6cb88822761e3
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:21:41 +0000

    part port doubt keen crime

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def count_elements(lst):
+        return len(lst)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
