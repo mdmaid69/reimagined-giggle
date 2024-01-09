bacab95cdff99d64f60a78a7dfedd253614f4a4e
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:22:01 +0000

    live conduct black phone pretend

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
