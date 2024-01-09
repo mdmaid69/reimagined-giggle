10df360df996832da61e9f1cb1b5a796f22658eb
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:38:22 +0000

    carry tell immediate mistake identify

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  def count_elements(lst):
+        return len(lst)
