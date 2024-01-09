1cfaf71dff991e473796a98accf0e3fbd635be5d
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:55:18 +0000

    beautiful cut basic large material

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
