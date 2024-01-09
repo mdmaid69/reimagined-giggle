cdbaf1186ac4420604ad8a1bdf477a57bb9647f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:41:22 +0000

    Uu way way effort degree

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
