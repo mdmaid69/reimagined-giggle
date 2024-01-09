175f4664ca2fc7a26425adf97db6cb0a7322a776
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:48:25 +0000

    temper butter unlikely pour improve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
 import array
 def get_array_as_complex(array):
         return complex(array[0])
+import array
+def convert_array_to_list(array):
+        return array.tolist()
