ca6c3f86caba5bb51ce3ddaf1afb8988ae27b5e5
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:17:01 +0000

    Gg exhibition until prove both

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
