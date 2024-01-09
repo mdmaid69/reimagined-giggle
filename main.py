24a21d8eef0bb93443645e966e76b75547138bfc
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:45:55 +0000

    solve really same empty bringup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
