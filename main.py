196d686f9ed2e69f83ae4272c0340409873ddffb
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:06:26 +0000

    glad lookaround act nigga me

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def find_min(numbers):
-        return min(numbers)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
