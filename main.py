d64c4561e530d57177399a5252c9db0d4e1c62f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:10:50 +0000

    ear compete popular mouth fall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
