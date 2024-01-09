8dd5c4a5d076dabaf3a337f56f8711e6804799a5
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:17:02 +0000

    popular around feeling plenty comein

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import time
+def get_current_time():
+        return time.time()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
