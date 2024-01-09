946741a765992155c8b19c102f5f6c71eaab7e16
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:09:16 +0000

    nowhere sad drive country ask

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
