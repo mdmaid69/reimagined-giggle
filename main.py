8db5fbd714272d24fbd9b665690fa7a408cc7a9b
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:31:35 +0000

    disapprove face underneath charge pump

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
