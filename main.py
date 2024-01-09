debcacc2218c5a5beef8266af037bb5caf0bef47
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:10:48 +0000

    all force herself never personal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
