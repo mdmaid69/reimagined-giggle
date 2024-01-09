d3710121c321b480ac619e675e1ee112ce8022e6
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:57:35 +0000

    beyond club expect journey guess

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def delete_file(file_name):
+        os.remove(file_name)
