0a951bb308b622961cd65613a1dc16927373d5dd
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:09:00 +0000

    accommodation entry defend process waste

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import numpy as np
+print(np.array([1, 2, 3]))
