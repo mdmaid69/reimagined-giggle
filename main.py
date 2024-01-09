eb46a93ee000116af77ae26a4db876fe6eec48d1
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:50:13 +0000

    fun cough warn but absolutely

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
