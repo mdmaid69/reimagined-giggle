018da427ecfd49a7173313d3e55698cef7036950
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:25:00 +0000

    recovery Hh represent wave today

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def calculate_work(force, distance):
+        return force * distance
