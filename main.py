ab80e630b50d82189d889c517aaf706007e155f9
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:46:13 +0000

    shrink nature pronounce judge wheel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
