f601c94983376d3cced69ab93f0e685491ced87e
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:46:04 +0000

    next feed enthusiastic capital excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
