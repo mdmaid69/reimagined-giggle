09c52ec58df78c584adb93cfc689d7136f3b15b6
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:57:56 +0000

    takeback complicated what comeoff pull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
