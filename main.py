40dc1ca083e9f349b1482f5d2cc31495ca41c4a4
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:53:00 +0000

    outside squeeze key tip door

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def find_max(lst):
+        return max(lst)
