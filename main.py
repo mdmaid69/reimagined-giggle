9cd3f03c59ca92a064ea7ae23ea09a3e0fc1d89c
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:16:10 +0000

    gooff health offer major Pp

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_density(mass, volume):
-        return mass / volume
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
