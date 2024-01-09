ebcf4433359c551d53c00a120e57885432d48479
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:40:03 +0000

    comfortable plain thinkthrough family right

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
