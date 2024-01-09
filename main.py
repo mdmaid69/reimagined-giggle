14e62825302e098be10f8d2efff703aacd9c3862
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:37:46 +0000

    foreign calmly error objection consider

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def exit_program():
+        sys.exit()
