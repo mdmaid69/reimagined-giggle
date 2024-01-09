5d9c7b262eb511b95a68f18784cc55ce7d6a315d
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:35:52 +0000

    quick solve show must result

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
