c5837abbdda262359b31bc8c4c3f7d5e4ed2db5b
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:43:33 +0000

    trade dance pinch likely patience

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
