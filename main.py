e5268a3336e9427a186039466f7848b455e7ed9a
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:39:58 +0000

    dry disapprove beginning already keen

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_unique_words(sentence):
+        return set(sentence.split())
