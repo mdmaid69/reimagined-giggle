617d30f043da43d4f22865b3eee4d330733b3d52
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:09:21 +0000

    long fear inquire fortunate live

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import tensorflow as tf
+print(tf.__version__)
+def calculate_area_circle(r):
+        return 3.14 * r**2
