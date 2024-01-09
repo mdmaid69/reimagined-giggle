572b2665a4b7ef90a8b2cdef0b09129f5a5de065
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:38:29 +0000

    rough share tragedy possibility connect

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-for i in range(5):
-        print(i)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def count_elements(lst):
+        return len(lst)
