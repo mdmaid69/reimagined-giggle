17f2641bca62fbc1506b94a241b5ff83b4ae67b2
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:49:38 +0000

    sharp could people woman fear

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def subtract_numbers(x, y):
-        return x - y
+def is_palindrome(s):
+        return s == s[::-1]
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
