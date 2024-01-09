a7090f71d9efa388116129ad7975fa9afb09a3e7
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:07:15 +0000

    couple second before press ticket

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
