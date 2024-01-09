394c46a8e96fee77a5c843821b6f4ad008be32c8
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:34:33 +0000

    sound dear brush giveout carry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
