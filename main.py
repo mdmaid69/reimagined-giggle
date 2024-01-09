ba295902b17a2e798fe0accb521affedc231de22
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:31:43 +0000

    muscle describe ticket add worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
