3cb7561635a07cb8c5157351c42f399c3b7d6618
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:29:53 +0000

    block flood worry load platform

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
