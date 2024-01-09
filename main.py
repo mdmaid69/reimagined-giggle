fa848be99becf58fce6868902c6daa55d6362385
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:49:36 +0000

    see praise include actually round

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
