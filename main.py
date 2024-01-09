4d526bbde4cae7f8941f11e24f3e5742cd299bfd
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:48:00 +0000

    trouble Cc community water commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+import array
+def get_array_as_set(array):
+        return set(array)
