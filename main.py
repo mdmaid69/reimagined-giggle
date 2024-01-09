3fe7d8ac81da141d9cf7e1432765647b8e7a0217
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:13:54 +0000

    waste butter taste pride treatment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def convert_to_hex(n):
+        return hex(n)
