55dfd15c1b163aedf9182cac979f190f13cae158
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:14:43 +0000

    age heat trick majority empty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def convert_to_octal(n):
+        return oct(n)
