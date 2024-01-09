204eb593dae4a2c9f376b8b8d2a0b9a1bb46adf8
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:27:54 +0000

    discussion mystery rarely shape angry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_base_name(path):
+        return os.path.basename(path)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
