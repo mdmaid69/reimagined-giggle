bbdea6606aa32adaee640a62a3abcc415bb6c7f4
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:38:53 +0000

    theirs run advise power fat

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  def sort_list(lst):
+        return sorted(lst)
