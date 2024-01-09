bcb2ddac08e9205e3d964d577476634f204eb51a
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:31:58 +0000

    length scare far so listenfor

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
