68c8f0c9772953949526a4c0a143db46e0d1c49c
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:22:20 +0000

    grand through juice relationship comfort

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import os
+print(os.getcwd())
+  def is_even(n):
+        return n % 2 == 0
