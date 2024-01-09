2ee020ad349b2a3cbc675901eb67baba72c3f282
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:56:48 +0000

    along long shame aware apart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
