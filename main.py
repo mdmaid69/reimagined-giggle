68a90f09d6ea35e232d075d423c6614f3bf76e6e
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:35:43 +0000

    fire deliberately hardly better sky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
