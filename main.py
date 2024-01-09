168ed616efcb0be70f67dd9254893d46e92269ea
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:41:46 +0000

    health cap begin storm back

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def find_max(numbers):
+        return max(numbers)
