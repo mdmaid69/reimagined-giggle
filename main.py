c30f547ef7745ef02f5bf19b5313128a8891f71b
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:05:07 +0000

    ride single press improve interest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
