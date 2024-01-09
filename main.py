17cbfac991211f3988e894d28e4d447421c3d4d9
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:52:54 +0000

    accordingto complete class right patience

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
