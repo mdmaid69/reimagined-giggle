d8937a8acf32ae3a654c5f077a06f499b17d89f7
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:49:47 +0000

    with boat base preparation while

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_work(force, distance):
+        return force * distance
