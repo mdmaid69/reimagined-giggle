b2964af1bbe925a2b6a8cfda6a13304078bec36e
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:31:12 +0000

    post touch name influence extraordinary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import collections
+def create_user_string():
+        return collections.UserString()
