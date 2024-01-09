48c6a55d880ebfc3eef1ae98c04bc1713e2f3077
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:13:31 +0000

    order ability case injury relief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   def count_elements(lst):
         return len(lst)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
