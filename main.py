2daf0f5e5f06ad975c1b533beb81847b1054e121
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:30:19 +0000

    makeinto lost brother tip take

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
