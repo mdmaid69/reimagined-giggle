84d870aac16e1e468884b4a4a0f0d813fbe5754b
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:54:03 +0000

    consider pronunciation kind contents force

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
