3331eec5e96bc8741799bf71f164b15463866ded
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:51:04 +0000

    your cover bag mine hair

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
