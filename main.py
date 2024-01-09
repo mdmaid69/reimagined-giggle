02cd3c7224502038ce515494458284cf007017e0
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:15:02 +0000

    no emphasize kind store term

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def wait_for_seconds(seconds):
+  import time
+  def wait_for_seconds(seconds):
         time.sleep(seconds)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
