b0317f1e3ba0d45a94c373770d8a60a627153e7e
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:54:01 +0000

    equal doubt obvious lay suggestion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import datetime
+def get_today_date():
+        return datetime.date.today()
