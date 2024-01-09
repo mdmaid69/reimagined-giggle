4c879849c0d03523c55078f0a7714dbb58be3d5b
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:46:38 +0000

    money splash godown breakthrough petrol

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def square_number(x):
+        return x**2
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
