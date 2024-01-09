4d98b2f98f93a79e13543a0f452fabae4963b0af
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:55:34 +0000

    often lower discuss television violence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_density(mass, volume):
-        return mass / volume
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
