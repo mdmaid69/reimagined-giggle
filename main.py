dd8316cba85efef5526186f4cf849204074316fd
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:04:21 +0000

    before 179bite offence compare burn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
