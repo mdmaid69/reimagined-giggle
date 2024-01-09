700700b0dd4ee2c3dd9cfa851688d1d63b16550f
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:56:42 +0000

    safe order the bread from

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import platform
-def get_os_info():
-        return platform.uname()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
