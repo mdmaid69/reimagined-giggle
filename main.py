1ec70d09d9be2bc82222e21c507c61286638ce37
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:35:16 +0000

    trial bite range any Nn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def cube_number(x):
-        return x**3
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
