bb34a702522e551fd1ec684cbce47ca3c5cd5bfd
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:33:19 +0000

    embarrassed back getout minority shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def calculate_density(mass, volume):
-        return mass / volume
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
