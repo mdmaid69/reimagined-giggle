603e472c05df1b456b5a76d4e4afbcb3a861735f
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:58:35 +0000

    drive community face lonely upset

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
