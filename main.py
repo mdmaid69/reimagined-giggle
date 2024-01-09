6bc0480cf244236cf03260f5983b4c44435b99cf
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:14:51 +0000

    opinion crime mess argue false

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import sys
+print(sys.version)
