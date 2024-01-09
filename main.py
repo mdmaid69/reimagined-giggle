c7fb400cda84ed286a2e40ba0eb10ddfd9633e8f
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:35:34 +0000

    who shallow beginning down they

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
