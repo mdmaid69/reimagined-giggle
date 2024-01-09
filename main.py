5b70962e3669fc5691605f3716b21ef89c8dd232
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:46:10 +0000

    town hurry want half smell

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
