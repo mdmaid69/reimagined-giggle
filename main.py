3d66047721bb96f8cda528e2a97d2e35b1d53b34
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:26:21 +0000

    transport stop cum warm 566end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
