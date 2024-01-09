d24e1d0253a88b71aefae502913ca2a98f7c405f
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:38:28 +0000

    this money perhaps square destroy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import getpass
+def get_username():
+        return getpass.getuser()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
