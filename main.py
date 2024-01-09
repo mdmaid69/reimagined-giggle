ccfa048c4788cf2ef15086bcbbc61eccc640d627
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:31:38 +0000

    lead confident fight drive cross

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
