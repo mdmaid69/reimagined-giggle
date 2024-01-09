203dd62cdfd8bb4cd34991fed6f6ddc1a7491758
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:33:17 +0000

    politics Hh drink emotion middle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  def sort_list(lst):
+        return sorted(lst)
