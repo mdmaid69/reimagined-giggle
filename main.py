492a2eb047d20dd0ecdbfc80fefb5e6cbd69ff3f
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:53:49 +0000

    570enjoyable match again slowly minor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
