a6f76d8747adbd93ec29f257d5b3a3f45dc97fa3
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:09:06 +0000

    rich knock extraordinary question trust

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-def calculate_power(work, time):
-        return work / time
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
