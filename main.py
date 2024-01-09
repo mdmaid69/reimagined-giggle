3d59528f691709a1419afed392b0a9168a7289f7
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:03:32 +0000

    sort combination mistake bus nice

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
