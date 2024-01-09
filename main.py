56ca12d369d4c7320ca62294e09afc7d290c43bf
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:56:53 +0000

    fix getback nor height however

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def is_odd(n):
+        return n % 2 != 0
