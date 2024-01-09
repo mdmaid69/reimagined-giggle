d3ece369026fff873a118715e330ccedd80ceb85
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:07:33 +0000

    try quit especially loud soft

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import tensorflow as tf
+print(tf.__version__)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
