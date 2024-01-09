8a3b3f7f9bf80c0c01ed3e471b52aa0865b9d420
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:21:27 +0000

    chin roof taste ashamed blue

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
