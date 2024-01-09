6b7c85c73b743429419490763821bd831b0f2147
mdmaid69 <robot@alexsyw.me>
2024-01-09 10:33:08 +0000

    tie send getout file bringdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
