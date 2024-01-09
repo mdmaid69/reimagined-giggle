747caed12a20ff8a29287d14cfd2305a157fb2b6
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:31:17 +0000

    favourite minimum fortunately after boil

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
-  def reverse_list(lst):
-        return lst[::-1]
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
