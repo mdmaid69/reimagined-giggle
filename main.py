41cb5cf1acff591ce1b64edc916b47c4088d8c5b
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:21:49 +0000

    address correction embarrassed history post

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def divide_numbers(x, y):
-        return x / y
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
