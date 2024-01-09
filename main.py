b51323c28586fe731dfe80a05c423c063d11f609
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:58:44 +0000

    style partner anywayanyhow beneath shut

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
