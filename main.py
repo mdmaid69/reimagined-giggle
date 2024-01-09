eff4f9e41a2ef03e8e9a705ac7ab14381f5e8a89
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:29:28 +0000

    cook deliver no worth party

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def multiply_numbers(x, y):
+        return x * y
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def clear_array(array):
+        array *= 0
