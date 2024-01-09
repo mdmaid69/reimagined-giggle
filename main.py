afec5e26d38876f22043bde54a6faa688ba1f85a
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:12:53 +0000

    direction storm thumb evidence load

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
