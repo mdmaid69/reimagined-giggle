05ca05a1bdab03471bcdf2cf42149648ea68b3d4
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:35:55 +0000

    remember earn communication goodbye murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def clear_array(array):
+        array *= 0
