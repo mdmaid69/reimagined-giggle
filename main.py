66138d811ea23bae00a0e6548b355cb893d64c60
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:56:36 +0000

    bush region knock call instant

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_area_circle(r):
+        return 3.14 * r**2
