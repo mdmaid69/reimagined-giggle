af29c75c23dfd5b795e3e53ce98216e09b7dc036
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:43:46 +0000

    content report influence worse that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
