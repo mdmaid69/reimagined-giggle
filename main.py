152821cfff4ac989ad03ffe2a92ffcb5b7d8a93e
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:53:34 +0000

    dare first plenty burst satisfied

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
