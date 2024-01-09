8152234b387e4a61c466d2045b17452461bc618e
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:16:08 +0000

    join suckme succeed include ear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
