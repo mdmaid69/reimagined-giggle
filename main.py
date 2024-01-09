8953ce94b503a979ebe7944367ff4ed60cdadd52
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:19:43 +0000

    satisfied chief eager release lost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import sys
+def print_python_version():
+        print(sys.version)
+import sys
+def exit_program():
+        sys.exit()
