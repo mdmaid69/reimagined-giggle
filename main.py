b7c721c873111fe16d139aee4dc9b41140811451
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:17:51 +0000

    fault able mean holdout community

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def get_array_buffer_info(array):
+        return array.buffer_info()
