2dbac217b35e74d068dfdc43495ab917f7874f2f
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:26:02 +0000

    similar adjective skill usual degree

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def cube_number(x):
+        return x**3
