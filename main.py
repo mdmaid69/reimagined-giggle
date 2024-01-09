5c51d2b7e7e632d00311f0fbd6a29bf348146b92
mdmaid69 <robot@alexsyw.me>
2024-01-09 12:39:23 +0000

    obtain runaway comment alive bet

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
