52915d99599b4dcd5d92a7de437a138221a4581c
mdmaid69 <robot@alexsyw.me>
2024-01-09 15:53:38 +0000

    afternoon 179bite knowledge wide only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import array
+def get_array_index(array, item):
+        return array.index(item)
