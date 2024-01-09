8ee7c88033fbe555093d3a235fb45dfbbad0dbe1
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:08:39 +0000

    raw steam geton past plan

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+print([x**2 for x in range(10)])
