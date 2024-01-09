58f3efa62df8df1cad81357a6c45f07f716188a0
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:48:54 +0000

    clothes face dish shopping team

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import os
+print(os.getcwd())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
