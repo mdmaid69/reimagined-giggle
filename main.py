75bbffdb8fd45c1530de10110764a2f0e8ef4f21
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:46:41 +0000

    wear rush interview influence drink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import sys
+  def get_python_version():
+        return sys.version
