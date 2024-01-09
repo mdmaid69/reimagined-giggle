44945737938926c2a21dbeddf2877caa2990ef76
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:10:07 +0000

    step drive bet club serious

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
