92c654789ec67f7b5d39203ad20efc3160826cea
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:22:19 +0000

    scream sink takeover pick only

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import sys
-def exit_program():
-        sys.exit()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
