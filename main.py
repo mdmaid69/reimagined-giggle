d2eda95d316cfa57ee4435978d5bd3c26dbc2adf
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:11:08 +0000

    turninto our invite quantity hit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def convert_array_to_unicode(array):
+        return array.tounicode()
