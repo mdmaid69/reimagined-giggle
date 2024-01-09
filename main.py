f566babf2ee20678a3fdcfd0caa80629c45662ff
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:22:55 +0000

    before region breakfast estimate activity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
