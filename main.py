cd063f216d47a7e610afa12741de94475937fe03
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:20:58 +0000

    nature Oo watch full confident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
