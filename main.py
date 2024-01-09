17b0920c1867ba9bcaa99dc10c6a1d911be5fe1e
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:01:17 +0000

    post cruel Yy body circumstance

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
