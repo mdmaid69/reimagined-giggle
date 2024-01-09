225cea893d40c663f4ce516fc820905a513f8eb3
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:41:59 +0000

    trust bang my stare build

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import random
+print(random.randint(0, 100))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
