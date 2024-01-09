34c10b38b26d8266c793acc5fe8d0d126c7ea417
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:41:32 +0000

    mess comeback advise personal advantage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
