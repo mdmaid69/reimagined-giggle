2229b429d0d762db6982b52c190f3b9c59a3b6b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:26:02 +0000

    matter goout anyone degree justice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
