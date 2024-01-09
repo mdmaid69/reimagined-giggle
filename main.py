cba57fe3ac1b142396ce3901c7e8112e9ff2c182
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:53:39 +0000

    comfortable over final sticky any

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def find_max(lst):
-        return max(lst)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import sys
+print(sys.version)
