1c56d185d77c3fe57412dfe5bc89bdfe43bcf9da
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:21:58 +0000

    better male climb service front

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
