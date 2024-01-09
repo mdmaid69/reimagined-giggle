35180077f04c83785cf57e31a533c126cd07b069
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:13:41 +0000

    damp excuse report thinkthrough school

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
