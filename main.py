988dd3e58f28f72014a044dc106890091628e79b
mdmaid69 <robot@alexsyw.me>
2024-01-09 17:08:40 +0000

    weather write criticism alone mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
