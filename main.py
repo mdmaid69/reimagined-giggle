80bd8262c0f7686d7d0e00be0646b8e6af2f9f74
mdmaid69 <robot@alexsyw.me>
2024-01-09 06:35:58 +0000

    slow order continue mean blow

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
