ba459a768c7155844807d08535ab9b7ac8ed5546
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:32:54 +0000

    reduce colour tidy might fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
