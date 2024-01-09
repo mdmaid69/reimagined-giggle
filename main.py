84602021e6813118a08390e59c016bd0a8b26408
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:16:37 +0000

    regularly born pain real tall

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import time
-def get_current_time():
-        return time.ctime()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
