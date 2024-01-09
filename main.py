1e9c3ba0e10862e6f5a209a378887a840e4aa5a2
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:04:08 +0000

    book party destroy ashamed case

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
