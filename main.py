a4a0c947989b2aee21cb63539103d9857817ce0a
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:26:15 +0000

    group world Dd improvement fork

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
