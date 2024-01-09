7d81558cf43c4afc66e55dba6d99c01b4cc1f394
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:07:24 +0000

    sometimes practical eat relationship programme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def is_odd(n):
+        return n % 2 != 0
 import array
 def get_array_itemsize(array):
         return array.itemsize
