29d1b1af47461bb0433998b793392a883507e1a1
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:46:20 +0000

    reserve not boil 570enjoyable difficult

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
