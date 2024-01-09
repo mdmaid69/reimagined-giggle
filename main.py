9ae723b356c77f4232051c7892fb75ddfe199506
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:21:02 +0000

    remove pipe hot community many

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
