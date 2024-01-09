0b363777556be77cb7ed85e1ee95fd65ae870ee4
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:33:38 +0000

    course confidence hole central needle

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
