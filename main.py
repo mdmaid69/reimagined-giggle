2460d8f47b0383ba64e4780c7a1e1beaa280a28c
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:12:52 +0000

    exist project compete noone Ii

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
