038ff6a992c64bd0fa3b36edfed2e458e92a0cd4
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:16:45 +0000

    square us brown into excuse

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sys
+def exit_program():
+        sys.exit()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
