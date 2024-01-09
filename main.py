e765b242c00329b28531aeef0ed081028d2a9cdf
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:22:12 +0000

    evening share considering runover fun

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
