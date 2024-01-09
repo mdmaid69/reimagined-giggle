4c86500fbe2f23ba7551464f74de18045ffead6d
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:46:13 +0000

    neither last lose cover flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
