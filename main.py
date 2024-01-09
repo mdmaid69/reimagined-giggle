d53a46c1635c153c968082bfc35853b9bc7d946a
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:58:38 +0000

    muscle move worth worth wing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
