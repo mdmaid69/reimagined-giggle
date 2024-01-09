6cc2013b078bf90a4ca1ae1a119624df903cf1b3
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:45:59 +0000

    smell common earth loose sorry

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-print([x**2 for x in range(10)])
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
