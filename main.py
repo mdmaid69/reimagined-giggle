41bd88dd1dbcb11bf66520212587403036a06dfd
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:14:07 +0000

    lesson survive eye approval cause

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
