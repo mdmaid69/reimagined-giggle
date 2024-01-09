5c81c117e40a447603c3dcf715308d0d63d670c0
mdmaid69 <robot@alexsyw.me>
2024-01-09 05:20:11 +0000

    system appearance high warning beat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
