3d81c2eb153167ba6b634b71e79d59db5eed20e1
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:21:25 +0000

    scare turninto measure raise task

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
