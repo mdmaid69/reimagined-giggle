d5f1db67993fa916dd9c7ccca7f9e3fb94f91dc3
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:53:27 +0000

    snow touch wind uncertain factory

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
