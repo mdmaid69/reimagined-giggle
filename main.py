1f17572983a8cd1e89a09e0d21d8ebc4c62ce8b4
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:27:38 +0000

    explanation body loud stand estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
