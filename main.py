975cd1071bca1bc348b1698c902c63c5d4219742
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:16:20 +0000

    promise deliberately holdup choose means

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
