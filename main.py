b92c5cc175cb97ca6a7e349c02a8b7dd7317182e
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:29:12 +0000

    please onto whom star suckme

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def divide_numbers(x, y):
+        return x / y
+import array
+def get_array_itemsize(array):
+        return array.itemsize
