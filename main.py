1fe7e2ecfafe16ffc324f462b1bb050e7ab2d8d1
mdmaid69 <robot@alexsyw.me>
2024-01-09 07:27:41 +0000

    concerned information healthy snow sweet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
