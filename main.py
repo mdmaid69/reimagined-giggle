283cc2bb50eb35e4a3828b678e618d737aca3445
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:28:50 +0000

    base lookup big serious mend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
