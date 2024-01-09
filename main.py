5d43a0ff1adf875a623fbc9289709376a7066378
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:18:57 +0000

    rate themselves fairly invitation sound

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
