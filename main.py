c0ab54bf6e30d18ad471b5aa92430efb1dba96c1
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:07:44 +0000

    hard create lift boil now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
