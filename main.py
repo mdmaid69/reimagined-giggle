93f6b57aec5461029452e45aa33cbdc94eb1fb31
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:13:27 +0000

    thumb push entrance bad legal

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
