a383ee59ddcb6b0e6b1efc7270e2f413d0b967b9
mdmaid69 <robot@alexsyw.me>
2024-01-09 01:38:38 +0000

    some not peace apart challenge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
