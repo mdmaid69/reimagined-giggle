f4a49db72098042f064b795e59ee816d47024b8a
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:27:14 +0000

    funny real maximum mood shadow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
