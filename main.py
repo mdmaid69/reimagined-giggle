957c13bcdce33d2d89f1fd3c1274a7a2b99eb656
mdmaid69 <robot@alexsyw.me>
2024-01-09 04:22:25 +0000

    period row bear afraid splash

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
