03f2615e38d46a7ea9312337fd7d99cb415f7c44
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:28:35 +0000

    first worth explanation ahead empty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
