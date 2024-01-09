b143b91e7ca08cfd9f44c351bdfb0b4ab6a81b4e
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:12:41 +0000

    peaceful fork least from back

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
