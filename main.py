d620870665a8fcc4a3b4fa4708eb2eb7b4a7e210
mdmaid69 <robot@alexsyw.me>
2024-01-09 14:44:29 +0000

    age swimming urgent combination takeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
