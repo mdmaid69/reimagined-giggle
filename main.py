71ac2f4460d1e800ec2dd484ac9a3f8f711e11ee
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:51:30 +0000

    describe ocean turnout all difficulty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
