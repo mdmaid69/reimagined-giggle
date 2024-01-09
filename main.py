9428d5dd0efdd83640b0da7cad31b9d24f0d0564
mdmaid69 <robot@alexsyw.me>
2024-01-09 13:54:03 +0000

    name wait accordingto so last

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  def convert_to_hex(n):
+        return hex(n)
