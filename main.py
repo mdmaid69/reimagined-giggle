6f9169c43fac4f5cf2b0a930ebdeba4ba42a59c1
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:12:49 +0000

    call past favour achieve only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_as_repr(array):
+        return repr(array)
+def remove_duplicates(lst):
+        return list(set(lst))
