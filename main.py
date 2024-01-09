902264b6113f76e3913191261150705d489d3e6b
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:52:45 +0000

    determined twist contribute grade object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def find_max(numbers):
+        return max(numbers)
