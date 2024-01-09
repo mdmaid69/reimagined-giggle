ed978a7b775662cb576f1ef28838e04f2cd2a2ff
mdmaid69 <robot@alexsyw.me>
2024-01-09 11:51:58 +0000

    opposite great start cold waste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
