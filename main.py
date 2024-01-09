c33929ebf68097f5f2c45fb480d02fade8861570
mdmaid69 <robot@alexsyw.me>
2024-01-09 19:55:22 +0000

    runover protect straight coat injury

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def reverse_string(s):
-        return s[::-1]
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
