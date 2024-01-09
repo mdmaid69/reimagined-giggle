cf34f1f3954d3c71edfe4e729b25a6961ae69ac5
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:39:21 +0000

    Itself manage carry wise business

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def reverse_string(s):
-        return s[::-1]
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
