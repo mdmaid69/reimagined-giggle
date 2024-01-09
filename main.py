318d533a95bcc9a4aafb1417fc35405a35736fb4
mdmaid69 <robot@alexsyw.me>
2024-01-09 02:02:05 +0000

    bury either sore nerve wealth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
