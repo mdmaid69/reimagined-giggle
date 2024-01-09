151f178f340ec14471c08274d0ed5647905bc666
mdmaid69 <robot@alexsyw.me>
2024-01-09 21:31:39 +0000

    learn its concerned influence except

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
 def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
