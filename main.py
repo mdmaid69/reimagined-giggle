cd580522b9ef3fb2b5d3fe45bdf06580a0207460
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:22:48 +0000

    tongue excellent public broken concentrate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
