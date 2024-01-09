06b6fef1da12dacef63848e5ef5b2bc2fb119c92
mdmaid69 <robot@alexsyw.me>
2024-01-09 20:55:00 +0000

    port about team ilovedicks off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
