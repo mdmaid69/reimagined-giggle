4f54039c0d1c25c666bb85a0939760cfcd0bcc89
mdmaid69 <robot@alexsyw.me>
2024-01-09 16:51:26 +0000

    turnup complain risk tip battery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
