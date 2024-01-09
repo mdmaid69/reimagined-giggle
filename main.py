049c1e003a3f4bdd765f5e81ebfd6bf746eebfb8
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:53:06 +0000

    university bar square underneath crisis

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
