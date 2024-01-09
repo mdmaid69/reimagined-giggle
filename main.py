af99fef82cc70c4c86bc11f44057dd5bc319dcd8
mdmaid69 <robot@alexsyw.me>
2024-01-09 03:48:21 +0000

    patience soft Qq breakinto guard

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
