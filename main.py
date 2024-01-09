9c9070fc61521584a1b5a8930f726f2aaed2a769
mdmaid69 <robot@alexsyw.me>
2024-01-09 09:39:06 +0000

    history nigga sort secret lift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
