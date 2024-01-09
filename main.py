f42c3ed158696e9ace7ec606090aeddcbbc93f34
mdmaid69 <robot@alexsyw.me>
2024-01-09 08:23:55 +0000

    Ww exist positive next comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def convert_to_binary(n):
+        return bin(n)
