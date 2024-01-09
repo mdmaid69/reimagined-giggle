8ccb3ebb5a785f4dccaab5aa5bc87f4524287162
mdmaid69 <robot@alexsyw.me>
2024-01-09 00:26:01 +0000

    no rarely will ever plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
