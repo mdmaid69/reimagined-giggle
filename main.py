a01fd7ce975ce4d0a424b80dc9b4b20ea75dafd7
mdmaid69 <robot@alexsyw.me>
2024-01-09 18:25:15 +0000

    ride block light eager position

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
